2818dc6523e21447839e0af5eee3dde81c8d0c24
Robert Strandh <strandh@Gracle.(none)>
2010-04-24 18:47:17 +0200

    Added comments to explain the use of reader macros in order to avoid repeating the list of shadowed/exported symbols.

--- Code/Cons-high/packages.lisp
+++ Code/Cons-high/packages.lisp
@@ -1,3 +1,10 @@
+;;; We want to shadow the exact same symbols from the COMMON-LISP
+;;; package as the ones we want to export.  To avoid repeating 
+;;; that list of symbols, we use the reader macros #= and ##. 
+;;; It is interesting to note that we would normally write 
+;;; (:shadow <string1> <string2> ...), but in order to put a 
+;;; reader label on the list (<string1> <string2> ...) we need to
+;;; express that as (:shadow . (<string1> <string2> ...)) instead. 
 (defpackage #:sicl-cons-high
     (:use #:cl)
   (:shadow . #1=(#:caar #:cadr #:cdar #:cddr
@@ -14,6 +21,14 @@
                  #:nth #:nthcdr))
   (:export . #1#))
 
+;;; In the test package, we want to import all symbols of the
+;;; COMMON-LISP package, except the ones that were shadowed in
+;;; the package that we are testing.  We do that by programatically
+;;; getting the list of those symbols using the function 
+;;; package-shadowing symbols.  But since defpackage doesn't evaluate
+;;; the symbols in the :shadowing-import-from list, we need to 
+;;; have the reader produce that list, hence the use of the #. reader
+;;; macro and again of the consing dot followed by a list. 
 (defpackage #:sicl-cons-high-test
     (:shadowing-import-from #:sicl-cons-high .
                             #.(package-shadowing-symbols '#:sicl-cons-high))
