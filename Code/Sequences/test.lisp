5ec973374b0b5e105d1e89ffb8f2b8dee8e695d7
Robert Strandh <strandh@Gracle.(none)>
2010-04-30 07:18:56 +0200

    Included tests for find-if-not from Paul Dietz' test suite, adapted for use with lisp-unit.

--- Code/Sequences/test.lisp
+++ Code/Sequences/test.lisp
@@ -613,4 +613,660 @@
                )
       i a b c d e f))))
 
+(define-test find-if-not-list.1
+  (assert-equal
+   nil
+   (find-if-not #'identity ())))
+
+(define-test find-if-not-list.2
+  (assert-equal
+   'a
+   (find-if-not #'null '(a))))
+
+(define-test find-if-not-list.2a
+  (assert-equal
+   'a
+   (find-if-not 'null '(a))))
+
+(define-test find-if-not-list.3
+  (assert-equal
+   2
+   (find-if-not #'oddp '(1 2 4 8 3 1 6 7))))
+
+(define-test find-if-not-list.4
+  (assert-equal
+   6
+   (find-if-not #'oddp '(1 2 4 8 3 1 6 7) :from-end t)))
+
+(define-test find-if-not-list.5
+  (assert-equal
+   '(2 2 4 8 6 6 6 nil)
+   (loop for i from 0 to 7 collect
+     (find-if-not #'oddp '(1 2 4 8 3 1 6 7) :start i))))
+
+(define-test find-if-not-list.6
+  (assert-equal
+   '(2 2 4 8 6 6 6 nil)
+   (loop for i from 0 to 7 collect
+     (find-if-not #'oddp '(1 2 4 8 3 1 6 7) :start i :end nil))))
+
+(define-test find-if-not-list.7
+  (assert-equal
+   '(6 6 6 6 6 6 6 nil)
+   (loop for i from 0 to 7 collect
+     (find-if-not #'oddp '(1 2 4 8 3 1 6 7) :start i :from-end t))))
+
+(define-test find-if-not-list.8
+  (assert-equal
+   '(6 6 6 6 6 6 6 nil)
+   (loop for i from 0 to 7 collect
+     (find-if-not #'oddp '(1 2 4 8 3 1 6 7) :start i :end nil :from-end t))))
+
+(define-test find-if-not-list.9
+  (assert-equal
+   '(nil nil 2 2 2 2 2 2 2)
+   (loop for i from 0 to 8 collect
+     (find-if-not #'oddp '(1 2 4 8 3 1 6 7) :end i))))
+
+(define-test find-if-not-list.10
+  (assert-equal
+   '(nil nil 2 4 8 8 8 6 6)
+   (loop for i from 0 to 8 collect
+     (find-if-not #'oddp '(1 2 4 8 3 1 6 7) :end i :from-end t))))
+
+(define-test find-if-not-list.11
+  (assert-equal
+   '((nil 2 2 2 2 2 2 2)
+     (2 2 2 2 2 2 2)
+     (4 4 4 4 4 4)
+     (8 8 8 8 8)
+     (nil nil 6 6)
+     (nil 6 6)
+     (6 6)
+     (nil))
+   (loop for j from 0 to 7
+         collect
+      (loop for i from (1+ j) to 8 collect
+        (find-if-not #'oddp '(1 2 4 8 3 1 6 7) :start j :end i)))))
+
+(define-test find-if-not-list.12
+  (assert-equal
+   '((nil 2 4 8 8 8 6 6)
+     (2 4 8 8 8 6 6)
+     (4 8 8 8 6 6)
+     (8 8 8 6 6)
+     (nil nil 6 6)
+     (nil 6 6)
+     (6 6)
+     (nil))
+   (loop for j from 0 to 7
+         collect
+      (loop for i from (1+ j) to 8 collect
+        (find-if-not #'oddp '(1 2 4 8 3 1 6 7) :start j :end i
+             :from-end t)))))
+
+(define-test find-if-not-list.13
+  (assert-equal
+   '(1 11 11 45 45 71 nil)
+   (loop for i from 0 to 6
+         collect
+      (find-if-not #'oddp '(1 6 11 32 45 71 100) :key #'1+ :start i))))
+
+(define-test find-if-not-list.14
+  (assert-equal
+   '(71 71 71 71 71 71 nil)
+   (loop for i from 0 to 6
+         collect
+      (find-if-not #'oddp '(1 6 11 32 45 71 100) :key '1+ :start i :from-end t))))
+
+(define-test find-if-not-list.15
+  (assert-equal
+   '(nil 1 1 1 1 1 1 1)
+   (loop for i from 0 to 7
+         collect
+      (find-if-not #'oddp '(1 6 11 32 45 71 100) :key #'1+ :end i))))
+
+(define-test find-if-not-list.16
+  (assert-equal
+   '(nil 1 1 11 11 45 71 71)
+   (loop for i from 0 to 7
+         collect
+      (find-if-not #'oddp '(1 6 11 32 45 71 100) :key '1+ :end i :from-end t))))
+
+(define-test find-if-not-list.17
+  (assert-equal
+   '((nil 2 2 2 2 2 2 2)
+     (2 2 2 2 2 2 2)
+     (4 4 4 4 4 4)
+     (8 8 8 8 8)
+     (nil nil 6 6)
+     (nil 6 6)
+     (6 6)
+     (nil))
+   (loop for j from 0 to 7
+         collect
+      (loop for i from (1+ j) to 8 collect
+        (find-if-not #'evenp '(1 2 4 8 3 1 6 7) :start j :end i :key #'1-)))))
+
+(define-test find-if-not-list.18
+  (assert-equal
+   '((nil 2 4 8 8 8 6 6)
+     (2 4 8 8 8 6 6)
+     (4 8 8 8 6 6)
+     (8 8 8 6 6)
+     (nil nil 6 6)
+     (nil 6 6)
+     (6 6)
+     (nil))
+   (loop for j from 0 to 7
+         collect
+      (loop for i from (1+ j) to 8 collect
+        (find-if-not #'evenp '(1 2 4 8 3 1 6 7) :start j :end i
+             :from-end t :key #'1+)))))
+
+;;; tests for vectors
+
+(define-test find-if-not-vector.1
+  (assert-equal
+   nil
+   (find-if-not #'identity #())))
+
+(define-test find-if-not-vector.2
+  (assert-equal
+   'a
+   (find-if-not #'not #(a))))
+
+(define-test find-if-not-vector.2a
+  (assert-equal
+   'a
+   (find-if-not 'null #(a))))
+
+(define-test find-if-not-vector.3
+  (assert-equal
+   2
+   (find-if-not #'oddp #(1 2 4 8 3 1 6 7))))
+
+(define-test find-if-not-vector.4
+  (assert-equal
+   6
+   (find-if-not #'oddp #(1 2 4 8 3 1 6 7) :from-end t)))
+
+(define-test find-if-not-vector.5
+  (assert-equal
+   '(2 2 4 8 6 6 6 nil)
+   (loop for i from 0 to 7 collect
+     (find-if-not #'oddp #(1 2 4 8 3 1 6 7) :start i))))
+
+(define-test find-if-not-vector.6
+  (assert-equal
+   '(2 2 4 8 6 6 6 nil)
+   (loop for i from 0 to 7 collect
+     (find-if-not #'oddp #(1 2 4 8 3 1 6 7) :start i :end nil))))
+
+(define-test find-if-not-vector.7
+  (assert-equal
+   '(6 6 6 6 6 6 6 nil)
+   (loop for i from 0 to 7 collect
+     (find-if-not #'oddp #(1 2 4 8 3 1 6 7) :start i :from-end t))))
+
+(define-test find-if-not-vector.8
+  (assert-equal
+   '(6 6 6 6 6 6 6 nil)
+   (loop for i from 0 to 7 collect
+     (find-if-not #'oddp #(1 2 4 8 3 1 6 7) :start i :end nil :from-end t))))
+
+(define-test find-if-not-vector.9
+  (assert-equal
+   '(nil nil 2 2 2 2 2 2 2)
+   (loop for i from 0 to 8 collect
+     (find-if-not #'oddp #(1 2 4 8 3 1 6 7) :end i))))
+
+(define-test find-if-not-vector.10
+  (assert-equal
+   '(nil nil 2 4 8 8 8 6 6)
+   (loop for i from 0 to 8 collect
+     (find-if-not #'oddp #(1 2 4 8 3 1 6 7) :end i :from-end t))))
+
+(define-test find-if-not-vector.11
+  (assert-equal
+   '((nil 2 2 2 2 2 2 2)
+     (2 2 2 2 2 2 2)
+     (4 4 4 4 4 4)
+     (8 8 8 8 8)
+     (nil nil 6 6)
+     (nil 6 6)
+     (6 6)
+     (nil))
+   (loop for j from 0 to 7
+         collect
+      (loop for i from (1+ j) to 8 collect
+        (find-if-not #'oddp #(1 2 4 8 3 1 6 7) :start j :end i)))))
+
+(define-test find-if-not-vector.12
+  (assert-equal
+   '((nil 2 4 8 8 8 6 6)
+     (2 4 8 8 8 6 6)
+     (4 8 8 8 6 6)
+     (8 8 8 6 6)
+     (nil nil 6 6)
+     (nil 6 6)
+     (6 6)
+     (nil))
+   (loop for j from 0 to 7
+         collect
+      (loop for i from (1+ j) to 8 collect
+        (find-if-not #'oddp #(1 2 4 8 3 1 6 7) :start j :end i
+             :from-end t)))))
+
+(define-test find-if-not-vector.13
+  (assert-equal
+   '(1 11 11 45 45 71 nil)
+   (loop for i from 0 to 6
+         collect
+      (find-if-not #'oddp #(1 6 11 32 45 71 100) :key #'1+ :start i))))
+
+(define-test find-if-not-vector.14
+  (assert-equal
+   '(71 71 71 71 71 71 nil)
+   (loop for i from 0 to 6
+         collect
+      (find-if-not #'oddp #(1 6 11 32 45 71 100) :key '1+ :start i :from-end t))))
+
+(define-test find-if-not-vector.15
+  (assert-equal
+   '(nil 1 1 1 1 1 1 1)
+   (loop for i from 0 to 7
+         collect
+      (find-if-not #'oddp #(1 6 11 32 45 71 100) :key #'1+ :end i))))
+
+(define-test find-if-not-vector.16
+  (assert-equal
+   '(nil 1 1 11 11 45 71 71)
+   (loop for i from 0 to 7
+         collect
+      (find-if-not #'oddp #(1 6 11 32 45 71 100) :key '1+ :end i :from-end t))))
+
+(define-test find-if-not-vector.17
+  (assert-equal
+   '((nil 2 2 2 2 2 2 2)
+     (2 2 2 2 2 2 2)
+     (4 4 4 4 4 4)
+     (8 8 8 8 8)
+     (nil nil 6 6)
+     (nil 6 6)
+     (6 6)
+     (nil))
+   (loop for j from 0 to 7
+         collect
+      (loop for i from (1+ j) to 8 collect
+        (find-if-not #'evenp #(1 2 4 8 3 1 6 7) :start j :end i :key #'1-)))))
+
+(define-test find-if-not-vector.18
+  (assert-equal
+   '((nil 2 4 8 8 8 6 6)
+     (2 4 8 8 8 6 6)
+     (4 8 8 8 6 6)
+     (8 8 8 6 6)
+     (nil nil 6 6)
+     (nil 6 6)
+     (6 6)
+     (nil))
+   (loop for j from 0 to 7
+         collect
+      (loop for i from (1+ j) to 8 collect
+        (find-if-not #'evenp #(1 2 4 8 3 1 6 7) :start j :end i
+             :from-end t :key #'1+)))))
+
+;;; Tests for bit vectors
+
+(define-test find-if-not-bit-vector.1
+  (assert-equal
+   nil
+   (find-if-not #'identity #*)))
+
+(define-test find-if-not-bit-vector.2
+  (assert-equal
+   1
+   (find-if-not #'null #*1)))
+
+(define-test find-if-not-bit-vector.3
+  (assert-equal
+   0
+   (find-if-not #'not #*0)))
+
+(define-test find-if-not-bit-vector.4
+  (assert-equal
+   '((nil 0 0 0 0 0 0 0)
+     (nil nil nil 0 0 0 0)
+     (nil nil 0 0 0 0)
+     (nil 0 0 0 0)
+     (nil nil nil 0)
+     (nil nil 0)
+     (nil 0))
+   (loop for i from 0 to 6
+         collect (loop for j from i to 7
+                       collect (find-if-not #'oddp #*0110110 :start i :end j)))))
+
+(define-test find-if-not-bit-vector.5
+  (assert-equal
+   '((nil 0 0 0 0 0 0 0)
+     (nil nil nil 0 0 0 0)
+     (nil nil 0 0 0 0)
+     (nil 0 0 0 0)
+     (nil nil nil 0)
+     (nil nil 0)
+     (nil 0))
+   (loop for i from 0 to 6
+         collect (loop for j from i to 7
+                       collect (find-if-not #'oddp #*0110110 :start i :end j
+                                    :from-end t)))))
+
+(define-test find-if-not-bit-vector.6
+  (assert-equal
+   '((nil 0 0 0 0 0 0 0)
+     (nil nil nil 0 0 0 0)
+     (nil nil 0 0 0 0)
+     (nil 0 0 0 0)
+     (nil nil nil 0)
+     (nil nil 0)
+     (nil 0))
+   (loop for i from 0 to 6
+         collect (loop for j from i to 7
+                       collect (find-if-not #'evenp #*0110110 :start i :end j
+                                    :from-end t :key #'1+)))))
+
+(define-test find-if-not-bit-vector.7
+  (assert-equal
+   '((nil 0 0 0 0 0 0 0)
+     (nil nil nil 0 0 0 0)
+     (nil nil 0 0 0 0)
+     (nil 0 0 0 0)
+     (nil nil nil 0)
+     (nil nil 0)
+     (nil 0))
+   (loop for i from 0 to 6
+         collect (loop for j from i to 7
+                       collect (find-if-not #'evenp #*0110110 :start i :end j
+                                    :key '1-)))))
+
+;;; Tests for strings
+
+(define-test find-if-not-string.1
+  (assert-equal
+   nil
+   (find-if-not #'identity "")))
+
+(define-test find-if-not-string.2
+  (assert-equal
+   #\a
+   (find-if-not #'null "a")))
+
+(define-test find-if-not-string.2a
+  (assert-equal
+   #\a
+   (find-if-not 'null "a")))
+
+(define-test find-if-not-string.3
+  (assert-equal
+   #\2
+   (find-if-not #'odddigitp "12483167")))
+  
+(define-test find-if-not-string.3a
+  (assert-equal
+   #\2
+   (find-if-not #'oddp "12483167" :key #'(lambda (c) (read-from-string (string c))))))
+
+(define-test find-if-not-string.4
+  (assert-equal
+   #\6
+   (find-if-not #'odddigitp "12483167" :from-end t)))
+
+(define-test find-if-not-string.5
+  (assert-equal
+   '(#\2 #\2 #\4 #\8 #\6 #\6 #\6 nil)
+   (loop for i from 0 to 7 collect
+     (find-if-not #'odddigitp "12483167" :start i))))
+
+(define-test find-if-not-string.6
+  (assert-equal
+   '(#\2 #\2 #\4 #\8 #\6 #\6 #\6 nil)
+   (loop for i from 0 to 7 collect
+     (find-if-not #'odddigitp "12483167" :start i :end nil))))
+
+(define-test find-if-not-string.7
+  (assert-equal
+   '(#\6 #\6 #\6 #\6 #\6 #\6 #\6 nil)
+   (loop for i from 0 to 7 collect
+     (find-if-not #'odddigitp "12483167" :start i :from-end t))))
+
+(define-test find-if-not-string.8
+  (assert-equal
+   '(#\6 #\6 #\6 #\6 #\6 #\6 #\6 nil)
+   (loop for i from 0 to 7 collect
+     (find-if-not #'odddigitp "12483167" :start i :end nil :from-end t))))
+
+(define-test find-if-not-string.9
+  (assert-equal
+   '(nil nil #\2 #\2 #\2 #\2 #\2 #\2 #\2)
+   (loop for i from 0 to 8 collect
+     (find-if-not #'odddigitp "12483167" :end i))))
+
+(define-test find-if-not-string.10
+  (assert-equal
+   '(nil nil #\2 #\4 #\8 #\8 #\8 #\6 #\6)
+   (loop for i from 0 to 8 collect
+     (find-if-not #'odddigitp "12483167" :end i :from-end t))))
+
+(define-test find-if-not-string.11
+  (assert-equal
+   '((nil #\2 #\2 #\2 #\2 #\2 #\2 #\2)
+     (#\2 #\2 #\2 #\2 #\2 #\2 #\2)
+     (#\4 #\4 #\4 #\4 #\4 #\4)
+     (#\8 #\8 #\8 #\8 #\8)
+     (nil nil #\6 #\6)
+     (nil #\6 #\6)
+     (#\6 #\6)
+     (nil))
+   (loop for j from 0 to 7
+         collect
+      (loop for i from (1+ j) to 8 collect
+        (find-if-not #'odddigitp "12483167" :start j :end i)))))
+
+(define-test find-if-not-string.12
+  (assert-equal
+  '((nil #\2 #\4 #\8 #\8 #\8 #\6 #\6)
+    (#\2 #\4 #\8 #\8 #\8 #\6 #\6)
+    (#\4 #\8 #\8 #\8 #\6 #\6)
+    (#\8 #\8 #\8 #\6 #\6)
+    (nil nil #\6 #\6)
+    (nil #\6 #\6)
+    (#\6 #\6)
+    (nil))
+  (loop for j from 0 to 7
+        collect
+     (loop for i from (1+ j) to 8 collect
+       (find-if-not #'odddigitp "12483167" :start j :end i
+            :from-end t)))))
+
+(define-test find-if-not-string.13
+  (assert-equal
+  '(#\4 #\4 #\8 #\8 #\8 #\6 #\6)
+  (loop for i from 0 to 6
+        collect
+     (find-if-not #'oddp "1473816"
+          :key (compose #'read-from-string #'string)
+          :start i))))
+
+(define-test find-if-not-string.14
+  (assert-equal
+  '(#\6 #\6 #\6 #\6 #\6 #\6 #\6)
+  (loop for i from 0 to 6
+        collect
+     (find-if-not #'oddp "1473816"
+          :key (compose #'read-from-string #'string)
+          :start i :from-end t))))
+
+(define-test find-if-not-string.15
+  (assert-equal
+  '(nil nil #\4 #\4 #\4 #\4 #\4 #\4)
+  (loop for i from 0 to 7
+        collect
+     (find-if-not #'oddp "1473816"
+          :key (compose #'read-from-string #'string)
+          :end i))))
+
+(define-test find-if-not-string.16
+  (assert-equal
+  '(nil nil #\4 #\4 #\4 #\8 #\8 #\6)
+  (loop for i from 0 to 7
+        collect
+     (find-if-not #'oddp "1473816"
+          :key (compose #'read-from-string #'string)
+          :end i :from-end t))))
+
+(define-test find-if-not-string.17
+  (assert-equal
+  '((nil #\4 #\4 #\4 #\4 #\4 #\4)
+    (#\4 #\4 #\4 #\4 #\4 #\4)
+    (nil nil #\8 #\8 #\8)
+    (nil #\8 #\8 #\8)
+    (#\8 #\8 #\8)
+    (nil #\6)
+    (#\6))
+  (loop for j from 0 to 6
+        collect
+     (loop for i from (1+ j) to 7 collect
+       (find-if-not #'oddp "1473816"
+            :key (compose #'read-from-string #'string)
+            :start j :end i))))  )
+
+(define-test find-if-not-string.18
+  (assert-equal
+  '((nil #\4 #\4 #\4 #\8 #\8 #\6)
+    (#\4 #\4 #\4 #\8 #\8 #\6)
+    (nil nil #\8 #\8 #\6)
+    (nil #\8 #\8 #\6)
+    (#\8 #\8 #\6)
+    (nil #\6)
+    (#\6))
+   (loop for j from 0 to 6
+         collect
+      (loop for i from (1+ j) to 7 collect
+        (find-if-not #'oddp "1473816"
+             :key (compose #'read-from-string #'string)
+             :start j :end i
+             :from-end t)))))
+
+;;; Keyword tests
+
+(define-test find-if-not.allow-other-keys.1
+  (assert-equal
+   2
+   (find-if-not #'oddp '(1 2 3 4 5) :bad t :allow-other-keys t)))
+
+(define-test find-if-not.allow-other-keys.2
+  (assert-equal
+   2
+   (find-if-not #'oddp '(1 2 3 4 5) :allow-other-keys t :also-bad t)))
+
+;;; The leftmost of two :allow-other-keys arguments is the one that  matters.
+(define-test find-if-not.allow-other-keys.3
+  (assert-equal
+   2
+   (find-if-not #'oddp '(1 2 3 4 5)
+                :allow-other-keys t
+                :allow-other-keys nil
+                :bad t)))
+
+(define-test find-if-not.keywords.4
+  (assert-equal
+   2
+   (find-if-not #'oddp '(1 2 3 4 5) :key #'identity :key #'1+)))
+
+(define-test find-if-not.allow-other-keys.5
+  (assert-equal
+   'a
+   (find-if-not #'null '(nil a b c nil) :allow-other-keys nil)))
+
+;;; Error tests
+
+(define-test find-if-not.error.4
+  (assert-error 'type-error
+                (find-if-not 'identity '(a b c . d))))
+
+(define-test find-if-not.error.5
+  (assert-error 'program-error (find-if-not)))
+
+(define-test find-if-not.error.6
+  (assert-error 'program-error (find-if-not #'null)))
+
+(define-test find-if-not.error.7
+  (assert-error 'program-error (find-if-not #'null nil :bad t)))
+
+(define-test find-if-not.error.8
+  (assert-error 'program-error
+                (find-if-not #'null nil :bad t :allow-other-keys nil)))
+
+(define-test find-if-not.error.9
+  (assert-error 'program-error (find-if-not #'null nil 1 1)))
+
+(define-test find-if-not.error.10
+  (assert-error 'program-error (find-if-not #'null nil :key)))
+
+(define-test find-if-not.error.11
+  (assert-error 'type-error (locally (find-if-not #'null 'b) t)))
+
+(define-test find-if-not.error.12
+  (assert-error 'program-error (find-if-not #'cons '(a b c))))
+
+(define-test find-if-not.error.13
+  (assert-error 'type-error (find-if-not #'car '(a b c))))
+
+(define-test find-if-not.error.14
+  (assert-error 'program-error
+                (find-if-not #'identity '(a b c) :key #'cons)))
+
+(define-test find-if-not.error.15
+  (assert-error 'type-error
+                (find-if-not #'identity '(a b c) :key #'car)))
+
+;;; Order of evaluation tests
+
+(define-test find-if-not.order.1
+  (let ((i 0) x y)
+    (assert-equal
+     '(a 2 1 2)
+     (list
+      (find-if-not (progn (setf x (incf i)) #'null)
+                   (progn (setf y (incf i)) '(nil nil nil a nil nil)))
+      i x y))))
+  
+(define-test find-if-not.order.2
+  (let ((i 0) a b c d e f)
+    (assert-equal
+     '(a 6 1 2 3 4 5 6)
+     (list
+      (find-if-not (progn (setf a (incf i)) #'identity)
+                   (progn (setf b (incf i)) '(nil nil nil a nil nil))
+                   :start (progn (setf c (incf i)) 1)
+                   :end   (progn (setf d (incf i)) 4)
+                   :from-end (setf e (incf i))
+                   :key   (progn (setf f (incf i)) #'null)
+                   )
+      i a b c d e f))))
+
+(define-test find-if-not.order.3
+  (let ((i 0) a b c d e f)
+    (assert-equal
+     '(a 6 1 2 3 4 5 6)
+     (list
+      (find-if-not (progn (setf a (incf i)) #'identity)
+                   (progn (setf b (incf i)) '(nil nil nil a nil nil))
+                   :key   (progn (setf c (incf i)) #'null)
+                   :from-end (setf d (incf i))
+                   :end   (progn (setf e (incf i)) 4)
+                   :start (progn (setf f (incf i)) 1)
+                   )
+      i a b c d e f))))
+
   
