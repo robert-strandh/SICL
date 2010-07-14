28191c95f976c51bbe9fdbd8874de3abde5adc19
Robert Strandh <strandh@Sicl.(none)>
2010-07-14 17:11:24 +0200

    Implemented macro pushnew.  No test yet.
    Updated status.text to reflect the changes.

--- Code/Cons-high/cons-high.lisp
+++ Code/Cons-high/cons-high.lisp
@@ -3690,5 +3690,77 @@
 			 do (setf (cdr rest) (cdddr rest))
 			    (return t))))))))
 
-			  
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;; Macro pushnew
+
+;;; The function make-bindings is duplicated from the sequences module. 
+;;; It would probably be better to avoid such duplication and to factor 
+;;; it out, but then I would introduce some dependencies, so keep it like
+;;; this for now.  Also see the sequences module for an explanation on
+;;; how it works. 
+
+;;; Translate from a keyword to a variable name
+(defparameter *vars* '((:start . start)
+                       (:end . end)
+                       (:from-end . from-end)
+                       (:key . key)
+                       (:test . test)
+                       (:test-not . test-not)
+                       (:count . count)))
+
+;;; For a list with alternating keywords, and expressions, 
+;;; generate a list of binding for let.  For instance,
+;;; if we have (:key <x> :end <y> :start <z>), we generate
+;;; ((key <x>) (end <y>) (start <z>)).  If a keyword occurs 
+;;; more than once in the list, generate a binding with a 
+;;; generated symbol instead. 
+(defun make-bindings (plist)
+  (loop with keywords = '()
+        for (key value) on plist by #'cddr
+        collect (list (if (member key keywords)
+                          (gensym)
+                          (or (cdr (assoc key *vars*)) (gensym)))
+                      value)
+        do (push key keywords)))
+
+(defmacro pushnew (item place
+		   &environment env
+		   &rest args
+		   &key
+		   (key nil key-p)
+		   (test nil test-p)
+		   (test-not nil test-not-p))
+  (declare (ignore key))
+  (assert (or (null test) (null test-not)))
+  (let ((item-var (gensym)))
+    (multiple-value-bind (vars vals store-vars writer-form reader-form)
+	(get-setf-expansion place env)
+      `(let ((,item-var ,item)
+	     ,@(mapcar #'list vars vals)
+	     ,@(make-bindings args))
+	 (let ((,(car store-vars) ,reader-form))
+	   ,(if key-p
+		(if test-p
+		    `(unless (member (funcall key ,item-var) ,(car store-vars)
+				     :test test :key key)
+		       (push ,item-var ,(car store-vars)))
+		    (if test-not-p
+			`(unless (member (funcall key ,item-var) ,(car store-vars)
+					 :test-not test-not :key key)
+			   (push ,item-var ,(car store-vars)))
+			`(unless (member (funcall key ,item-var) ,(car store-vars)
+					 :key key)
+			   (push ,item-var ,(car store-vars)))))
+		(if test-p
+		    `(unless (member (funcall key ,item-var) ,(car store-vars)
+				     :test test)
+		       (push ,item-var ,(car store-vars)))
+		    (if test-not-p
+			`(unless (member (funcall key ,item-var) ,(car store-vars)
+					 :test-not test-not)
+			   (push ,item-var ,(car store-vars)))
+			`(unless (member (funcall key ,item-var) ,(car store-vars))
+			   (push ,item-var ,(car store-vars))))))
+	   ,writer-form)))))
 
