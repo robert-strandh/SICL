f6a5fa83146acf13ee9302f80437fceac461f89f
Robert Strandh <strandh@Sicl.(none)>
2010-08-28 11:14:31 +0200

    Started a test suite for the reader.  Currently, the reader cannot handle a single escape at the beginning of a symbol.

--- Code/Reader/test.lisp
+++ Code/Reader/test.lisp
@@ -1,1 +1,38 @@
 (in-package #:sicl-read-test)
+
+(define-test read.symbol.followed.by.end.of.file
+  (assert-equal (symbol-name (with-input-from-string (stream "abc")
+			       (read stream)))
+		"ABC"))
+
+(define-test read.symbol.followed.by.whitespace
+  (assert-equal (symbol-name (with-input-from-string (stream "abc ")
+			       (read stream)))
+		"ABC"))
+
+(define-test read.symbol.followed.by.macro-character
+  (assert-equal (symbol-name (with-input-from-string (stream "abc(")
+			       (read stream)))
+		"ABC"))
+
+(define-test read.symbol.with.single.escape.at.the.beginning
+  (assert-equal (symbol-name (with-input-from-string (stream "\\abc(")
+			       (read stream)))
+		"aBC"))
+
+
+(define-test read.symbol.with.single.escape.in.the.middle
+  (assert-equal (symbol-name (with-input-from-string (stream "a\\bc(")
+			       (read stream)))
+		"AbC"))
+
+(define-test read.symbol.with.single.escape.at.the.end
+  (assert-equal (symbol-name (with-input-from-string (stream "ab\\c(")
+			       (read stream)))
+		"ABc"))
+
+(define-test read.symbol.with.single.escape.middle.macro.char
+  (assert-equal (symbol-name (with-input-from-string (stream "a\\(c(")
+			       (read stream)))
+		"A(C"))
+
