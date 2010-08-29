c49e31862a9010dbf9d2740d93e5141278f71edf
Robert Strandh <strandh@Sicl.(none)>
2010-08-29 05:29:34 +0200

    Added explicit initializations of reader special variables, so as to be sure to have the right read-base and current readtable.
    Added more tests.

--- Code/Reader/test.lisp
+++ Code/Reader/test.lisp
@@ -1,38 +1,96 @@
 (in-package #:sicl-read-test)
 
 (define-test read.symbol.followed.by.end.of.file
-  (assert-equal (symbol-name (with-input-from-string (stream "abc")
-			       (read stream)))
-		"ABC"))
+  (let ((*read-base* 10)
+	(*read-table* (copy-readtable nil)))
+    (assert-equal (symbol-name (with-input-from-string (stream "abc")
+				 (read stream)))
+		  "ABC")))
 
 (define-test read.symbol.followed.by.whitespace
-  (assert-equal (symbol-name (with-input-from-string (stream "abc ")
-			       (read stream)))
-		"ABC"))
+  (let ((*read-base* 10)
+	(*read-table* (copy-readtable nil)))
+    (assert-equal (symbol-name (with-input-from-string (stream "abc ")
+				 (read stream)))
+		  "ABC")))
 
 (define-test read.symbol.followed.by.macro-character
-  (assert-equal (symbol-name (with-input-from-string (stream "abc(")
-			       (read stream)))
-		"ABC"))
+  (let ((*read-base* 10)
+	(*read-table* (copy-readtable nil)))
+    (assert-equal (symbol-name (with-input-from-string (stream "abc(")
+				 (read stream)))
+		  "ABC")))
 
 (define-test read.symbol.with.single.escape.at.the.beginning
-  (assert-equal (symbol-name (with-input-from-string (stream "\\abc(")
-			       (read stream)))
-		"aBC"))
-
+  (let ((*read-base* 10)
+	(*read-table* (copy-readtable nil)))
+    (assert-equal (symbol-name (with-input-from-string (stream "\\abc(")
+				 (read stream)))
+		  "aBC")))
 
 (define-test read.symbol.with.single.escape.in.the.middle
-  (assert-equal (symbol-name (with-input-from-string (stream "a\\bc(")
-			       (read stream)))
-		"AbC"))
+  (let ((*read-base* 10)
+	(*read-table* (copy-readtable nil)))
+    (assert-equal (symbol-name (with-input-from-string (stream "a\\bc(")
+				 (read stream)))
+		  "AbC")))
 
 (define-test read.symbol.with.single.escape.at.the.end
-  (assert-equal (symbol-name (with-input-from-string (stream "ab\\c(")
-			       (read stream)))
-		"ABc"))
+  (let ((*read-base* 10)
+	(*read-table* (copy-readtable nil)))
+    (assert-equal (symbol-name (with-input-from-string (stream "ab\\c(")
+				 (read stream)))
+		  "ABc")))
 
 (define-test read.symbol.with.single.escape.middle.macro.char
-  (assert-equal (symbol-name (with-input-from-string (stream "a\\(c(")
-			       (read stream)))
-		"A(C"))
+  (let ((*read-base* 10)
+	(*read-table* (copy-readtable nil)))
+    (assert-equal (symbol-name (with-input-from-string (stream "a\\(c(")
+				 (read stream)))
+		  "A(C")))
+
+(define-test read.symbol.with.single.escape.end.of.file
+  (let ((*read-base* 10)
+	(*read-table* (copy-readtable nil)))
+    (assert-error
+     'reader-error
+     (symbol-name (with-input-from-string (stream "abc\\")
+				 (read stream))))))
+
+(define-test read.symbol.with.double-escape.at.the.beginning
+  (let ((*read-base* 10)
+	(*read-table* (copy-readtable nil)))
+    (assert-equal (symbol-name (with-input-from-string (stream "|a|bc")
+				 (read stream)))
+		  "aBC")))
+
+(define-test read.symbol.with.double-escape.in.the.middle
+  (let ((*read-base* 10)
+	(*read-table* (copy-readtable nil)))
+    (assert-equal (symbol-name (with-input-from-string (stream "a|b|c")
+				 (read stream)))
+		  "AbC")))
+
+(define-test read.symbol.with.double-escape.at.the.end
+  (let ((*read-base* 10)
+	(*read-table* (copy-readtable nil)))
+    (assert-equal (symbol-name (with-input-from-string (stream "ab|c|")
+				 (read stream)))
+		  "ABc")))
+
+(define-test read.symbol.with.double.escape.end.of.file.1
+  (let ((*read-base* 10)
+	(*read-table* (copy-readtable nil)))
+    (assert-error
+     'reader-error
+     (symbol-name (with-input-from-string (stream "ab|c")
+				 (read stream))))))
+
+(define-test read.symbol.with.double.escape.end.of.file.2
+  (let ((*read-base* 10)
+	(*read-table* (copy-readtable nil)))
+    (assert-error
+     'reader-error
+     (symbol-name (with-input-from-string (stream "abc|")
+				 (read stream))))))
 
