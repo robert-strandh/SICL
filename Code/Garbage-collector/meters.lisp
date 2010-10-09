13a92623d0e11f85fe9926c98bde168e89f85057
Robert Strandh <strandh@Sicl.(none)>
2010-10-09 10:33:16 +0200

    New file containing meters for the gc.
    Added a packages.lisp file.

---
+++ Code/Garbage-collector/meters.lisp
@@ -0,0 +1,25 @@
+(in-package #:sicl-gc)
+
+;;; The total number of garbage collections of the nursery.
+(defparameter *number-of-nursery-gcs* 0)
+
+;;; The total internal execution time spent marking objects
+;;; in the nursery.
+(defparameter *nursery-mark-time* 0)
+
+;;; The total internal execution time spent building the cache
+;;; for the nuresry liveness information.
+(defparameter *nursery-cache-build-time* 0)
+
+;;; The total internal execution time spent fixing up addresses
+;;; of nursery objects prior to moving them. 
+(defparameter *nursery-fixup-time* 0)
+
+;;; The total internnal execution time spent sliding nursery objects.
+(defparameter *nursery-slide-time* 0)
+
+(defun reset-gc-meters ()
+  (setf *number-of-nursery-gcs* 0)
+  (setf *nursery-cache-build-time* 0)
+  (setf *nursery-fixup-time* 0)
+  (setf *nursery-slide-time* 0))
---
+++ Code/Garbage-collector/packages.lisp
@@ -0,0 +1,7 @@
+(defpackage #:sicl-gc
+    (:use #:common-lisp)
+  (:export))
+
+(defpackage #:sicl-gc-test
+    (:use #:common-lisp #:sicl-gc)
+  (:export))
