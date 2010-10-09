cf8d6f5d3806c797c4e53bc8bca27351c89d64de
Robert Strandh <strandh@Sicl.(none)>
2010-10-09 12:55:24 +0200

    Added a meter for the total amount of space recovered during nursery GCs.

--- Code/Garbage-collector/meters.lisp
+++ Code/Garbage-collector/meters.lisp
@@ -15,11 +15,16 @@
 ;;; of nursery objects prior to moving them. 
 (defparameter *nursery-fixup-time* 0)
 
-;;; The total internnal execution time spent sliding nursery objects.
+;;; The total internal execution time spent sliding nursery objects.
 (defparameter *nursery-slide-time* 0)
+
+;;; The total number of words recovered during all the nursery GCs
+;;; done since the beginning of metering.
+(defparameter *nursery-recovered-space* 0)
 
 (defun reset-gc-meters ()
   (setf *number-of-nursery-gcs* 0)
   (setf *nursery-cache-build-time* 0)
   (setf *nursery-fixup-time* 0)
-  (setf *nursery-slide-time* 0))
+  (setf *nursery-slide-time* 0)
+  (setf *nursery-recovered-space* 0))
