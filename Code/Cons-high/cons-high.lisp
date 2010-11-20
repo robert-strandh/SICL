187465626a0a6d6491982e94d1775e7591ab934c
Robert Strandh <strandh@Sicl.(none)>
2010-11-20 08:07:20 +0100

    Rewrote the copy-alist function so that it doesn't signal and error when the argument contains non-cons elements, and so that it signals a more specific condition when a dotted list is given.

--- Code/Cons-high/cons-high.lisp
+++ Code/Cons-high/cons-high.lisp
@@ -2583,6 +2583,10 @@
 ;;;
 ;;; Function pairlis
 
+;;; The hyperspec says the consequences are undefined if the two
+;;; lists do not have the same length.  We check for this situation
+;;; and signal an error in that case. 
+
 (defun pairlis (keys data &optional alist)
   (loop with result = alist
 	with remaining-keys = keys
@@ -2609,9 +2613,28 @@
 ;;;
 ;;; Function copy-alist
 
+;;; The HyperSpec says that the argument is an alist, and 
+;;; in those cases, whenever the type doesn't correspond, it
+;;; is legitimate to signal an error.  However, for copy-alist,
+;;; the HyperSpec also says that any object that is referred to
+;;; directly or indirectly is still shared betwee the argument
+;;; and the resulting list, which suggests that any element that
+;;; is not a cons should just be included in the resulting
+;;; list as well.  And that is what we are doing. 
+
+;;; We use (loop for ... on ...) because it uses ATOM to test for
+;;; the end of the list.  Then we check that the atom is really 
+;;; null, and if not, signal a type error.
+
 (defun copy-alist (alist)
-  (loop for element in alist
-	collect (cons (car element) (cdr element))))
+  (loop for remaining on alist
+	collect (if (consp (car remaining))
+		    (cons (caar remaining) (cdar remaining))
+		    (car remaining))
+	finally (unless (null remaining)
+		  (error 'must-be-proper-list
+			 :datum alist
+			 :name 'copy-alist))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;
