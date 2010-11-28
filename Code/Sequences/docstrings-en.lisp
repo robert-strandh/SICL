50acc46d8a070cbb330c28bee468c3aa4cf77c63
Robert Strandh <strandh@Sicl.(none)>
2010-11-28 15:56:48 +0100

    Included files for docstrings and condition reporters, and updated sequences.asd to take them into account.

---
+++ Code/Sequences/docstrings-en.lisp
@@ -0,0 +1,28 @@
+(in-package #:sicl-sequences)
+
+;;;; Copyright (c) 2010
+;;;;
+;;;;     Robert Strandh (strandh@labri.fr)
+;;;;
+;;;; all rights reserved. 
+;;;;
+;;;; Permission is hereby granted to use this software for any 
+;;;; purpose, including using, modifying, and redistributing it.
+;;;;
+;;;; The software is provided "as-is" with no warranty.  The user of
+;;;; this software assumes any responsibility of the consequences. 
+
+;;;; This file is part of the sequences module of the SICL project.
+;;;; See the file SICL.text for a description of the project. 
+;;;; See the file sequences.text for a description of the module.
+
+(eval-when (:compile-toplevel :load-toplevel :execute)
+  (defun fmt (&rest args)
+    (apply #'format nil args)))
+
+;;; Create documentation for a function.
+(defun fundoc (name string)
+  (setf (documentation name 'function) string)
+  (setf (documentation (fdefinition name) 'function)
+        (documentation name 'function)))
+
--- Code/Sequences/sequences.asd
+++ Code/Sequences/sequences.asd
@@ -3,7 +3,9 @@
 (asdf:defsystem :sicl-sequences
     :components
   ((:file "packages" :depends-on ())
-   (:file "sequences" :depends-on ("packages"))))
+   (:file "sequences" :depends-on ("packages"))
+   (:file "condition-reporters-en" :depends-on ("packages" "sequences"))
+   (:file "docstrings-en" :depends-on ("packages"))))
 
 (asdf:defsystem :sicl-sequences-test
   :depends-on (:lisp-unit :sicl-sequences)
