d33082677b49e4ef29988a39abf4fd6be1474a14
Robert Strandh <strandh@hecl.(none)>
2011-02-13 10:24:43 +0700

    More tests for the cond macro.
    Tests for the or macro.
    
    Tests for the and macro.

--- Code/Conditionals/test.lisp
+++ Code/Conditionals/test.lisp
@@ -1,5 +1,54 @@
 (in-package #:sicl-conditionals-test)
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; Tests for the or macro
+
+(define-test or-0
+  (assert-equal 'nil
+		(or)))
+
+(define-test or-1
+  (assert-equal 'nil
+		(or nil)))
+
+(define-test or-2
+  (assert-equal '0
+		(or 0)))
+
+(define-test or-3
+  (assert-equal '0
+		(or 0 nil)))
+
+(define-test or-4
+  (let ((x 0))
+    (assert-equal '0
+		  (or 0 (/ x)))))
+
+(define-test or-5
+  (let ((x 0))
+    (assert-equal '0
+		  (or nil 0 (/ x)))))
+
+(define-test or-error-0
+  (assert-error 'program-error
+		(macroexpand-1 '(or . 1))))
+
+(define-test or-error-1
+  (assert-error 'program-error
+		(macroexpand-1 '(or t . 1))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; Tests for the and macro
+
+(define-test and-error-0
+  (assert-error 'program-error
+		(macroexpand-1 '(and . 1))))
+
+(define-test and-error-1
+  (assert-error 'program-error
+		(macroexpand-1 '(and nil . 1))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; Tests for the cond macro
 
 (define-test cond-1
@@ -26,6 +75,16 @@
   (assert-equal '2
 		(cond (0 1 2))))
 
+(define-test cond-7
+  (assert-equal '2
+		(cond (t 2)
+		      (t 1))))
+
+(define-test cond-8
+  (assert-equal '1
+		(cond (nil 2)
+		      (t 1))))
+
 (define-test cond-error-1
   (assert-error 'program-error
 		(macroexpand-1 '(cond x))))
@@ -38,7 +97,11 @@
   (assert-error 'program-error
 		(macroexpand-1 '(cond ()))))
 
-(define-test cond-error-3
+(define-test cond-error-4
   (assert-error 'program-error
-		(macroexpand-1 '(cond ()))))
+		(macroexpand-1 '(cond . 1))))
+
+(define-test cond-error-5
+  (assert-error 'program-error
+		(macroexpand-1 '(cond (t 2) . 1))))
 
