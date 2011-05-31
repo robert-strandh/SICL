072afe314935347c08871fb971fcfb4f460a06f1
Robert Strandh <strandh@hecl.(none)>
2011-05-31 08:42:00 +0700

    Factored out description of FIND, FIND-IF, and FIND-IF-NOT.
    Factored out description of COUNT, COUNT-IF, and COUNT-IF-NOT.
    
    Added a description of the PREDICATE SEQUENCE pair of arguments.
    
    Renamed the description of satisfying the test to reflect that
    it is a two-argument test.
    
    Added descriptions of satisfying one argument tests, a positive test
    and a negative test.
    
    Added docstrings for FIND-IF, FIND-IF-NOT.
    
    Added docstrings for COUNT-IF, COUNT-IF-NOT.

--- Code/Sequences/docstrings-en.lisp
+++ Code/Sequences/docstrings-en.lisp
@@ -23,6 +23,11 @@
 (defparameter *item-sequence*
   (fmt "ITEM is any object, and SEQUENCE is a proper sequence."))
 
+(defparameter *predicate-sequence*
+  (fmt "PREDICATE is a designator for a function of one argument~@
+        that returns a generalized boolean.~@
+        SEQUENCE is a proper sequence."))
+
 (defparameter *key*
   (fmt "KEY is a designator for a function of one argument which is~@
         applied the elements of SEQUENCE before the test is applied,~@
@@ -39,7 +44,7 @@
         the indexes i such that START <= i < END.  The default for START is 0,~@
         and the default for END is NIL, which means the end of the sequence."))
 
-(defparameter *satisfy-the-test*
+(defparameter *satisfy-a-two-argument-test*
   (fmt "To determine whether an element satisfies the test,  The KEY function~@
         is first applied to the element.  The result is then used in the test.~@
 	Then, if TEST is given, TEST is applied to ITEM and the result of~@
@@ -49,6 +54,18 @@
 	the result of applying the KEY function. If TEST-NOT returns false, then~@
 	the element satisfies the test.  Otherwise the element does not~@
 	satisfy the test."))
+
+(defparameter *satisfy-a-one-argument-positive-test*
+  (fmt "To determine whether an element satisfies the test, the KEY function is~@
+        first applied to the element.  Then PREDICATE is applied to the result.~@
+        If PREDICATE returns true, then the element satisfies the test.~@
+        Otherwise, the element does not satisfy the test."))
+
+(defparameter *satisfy-a-one-argument-negative-test*
+  (fmt "To determine whether an element satisfies the test, the KEY function is~@
+        first applied to the element.  Then PREDICATE is applied to the result.~@
+        If PREDICATE returns false, then the element satisfies the test.~@
+        Otherwise, the element does not satisfy the test."))
 
 (defparameter *from-end*
   (fmt "FROM-END is a generalized boolean.  The default-value for FROM-END is false."))
@@ -62,6 +79,19 @@
 	index designators for SEQUENCE, which means that 0 <= START <= L, and~@
 	either END is NIL or 0 <= END <= L where L is the length of SEQUENCE."))
 
+(defparameter *find-description*
+  (fmt "Searches the interval of SEQUENCE designated by START and END~@
+        for an element of the sequence that satisfies the test.~@
+	If such an element is found, then it is returned.~@
+	Otherwise NIL is returned.~@
+	~@
+	If FROM-END is false, then the first element that satisfies the test is~@
+	returned.  Otherwise the last element that satisfies the test is returned.~@
+	There is no requirement, however that the KEY and the TEST or TEST-NOT~@
+	functions will be applied in any particular order.  In particular, even if~@
+	FROM-END is true, SEQUENCE may be search from the beginning to the end~@
+	and the last element that satisfies the test returned."))
+
 ;;; Create documentation for a function.
 (defun fundoc (name string)
   (setf (documentation name 'function) string)
@@ -72,17 +102,7 @@
 	(fmt "Lambda list: (ITEM SEQUENCE &key KEY TEST TEST-NOT START END FROM-END)~@
               ~@
               Description:~@
-              FIND searches the interval of SEQUENCE designated by START and END~@
-              for an element of the sequence that satisfies the test.~@
-              If such an element is found, then it is returned.~@
-              Otherwise NIL is returned.~@
-              ~@
-              If FROM-END is false, then the first element that satisfies the test is~@
-              returned.  Otherwise the last element that satisfies the test is returned.~@
-              There is no requirement, however that the KEY and the TEST or TEST-NOT~@
-              functions will be applied in any particular order.  In particular, even if~@
-              FROM-END is true, SEQUENCE may be search from the beginning to the end~@
-              and the last element that satisfies the test returned.~@
+              ~a~@
               ~@
               Arguments:~@
               ~a~%~a~%~a~%~a~%~a~@
@@ -93,23 +113,69 @@
               Exceptional situations:~@
               ~a~@
               ~a"
-              *item-sequence* *key* *test-test-not* *bounding-indexes* *from-end*
-	      *satisfy-the-test*
-	      *error-not-proper-sequence*
-	      *error-bounding-indexes*))
+	     *find-description*
+	     *item-sequence* *key* *test-test-not* *bounding-indexes* *from-end*
+	     *satisfy-a-two-argument-test*
+	     *error-not-proper-sequence*
+	     *error-bounding-indexes*))
+
+(fundoc 'find-if
+	(fmt "Lambda list: (PREDICATE SEQUENCE &key KEY START END FROM-END)~@
+              ~@
+              Description:~@
+              ~a~@
+              ~@
+              Arguments:~@
+              ~a~%~a~%~a~%~a~@
+              ~@
+              Satisfying the test:~@
+              ~a~@
+              ~@
+              Exceptional situations:~@
+              ~a~@
+              ~a"
+	     *find-description*
+	     *predicate-sequence* *key* *bounding-indexes* *from-end*
+	     *satisfy-a-one-argument-positive-test*
+	     *error-not-proper-sequence*
+	     *error-bounding-indexes*))
+
+(fundoc 'find-if-not
+	(fmt "Lambda list: (PREDICATE SEQUENCE &key KEY START END FROM-END)~@
+              ~@
+              Description:~@
+              ~a~@
+              ~@
+              Arguments:~@
+              ~a~%~a~%~a~%~a~@
+              ~@
+              Satisfying the test:~@
+              ~a~@
+              ~@
+              Exceptional situations:~@
+              ~a~@
+              ~a"
+	     *find-description*
+	     *predicate-sequence* *key* *bounding-indexes* *from-end*
+	     *satisfy-a-one-argument-negative-test*
+	     *error-not-proper-sequence*
+	     *error-bounding-indexes*))
+
+(defparameter *count-description*
+  (fmt "Counts the number of elements in the interval of SEQUENCE designated by~@
+	START and END that satisfy the test, and returns the number of such elements.~@
+	~@
+	If FROM-END is false, then the elements of the designated interval will be~@
+	tested from the beginning to the end of the sequence.  If FROM-END is true,~@
+	then elements of the designated interval will be tested from the end to the~@
+	beginning of the sequence.  This difference is important only when~@
+	KEY, TEST, or TEST-NOT has side effects."))
 
 (fundoc 'count
 	(fmt "Lambda list: (ITEM SEQUENCE &key KEY TEST TEST-NOT START END FROM-END)~@
               ~@
               Description:~@
-              COUNT counts the number of elements in the interval of SEQUENCE designated by~@
-	      START and END that satisfy the test, and returns the number of such elements.~@
-	      ~@
-	      IF FROM-END is false, then the elements of the designated interval will be~@
-	      tested from the beginning to the end of the sequence.  If FROM-END is true,~@
-	      then elements of the designated interval will be tested from the end to the~@
-	      beginning of the sequence.  This difference is important only when~@
-	      KEY, TEST, or TEST-NOT have side effects.~@
+              ~a~@
               ~@
 	      Arguments:~@
 	      ~a~%~a~%~a~%~a~%~a~@
@@ -120,8 +186,50 @@
               Exceptional situations:~@
               ~a~@
               ~a"
-              *item-sequence* *key* *test-test-not* *bounding-indexes* *from-end*
-	      *satisfy-the-test*
-	      *error-not-proper-sequence*
-	      *error-bounding-indexes*))
+	     *count-description*
+	     *item-sequence* *key* *test-test-not* *bounding-indexes* *from-end*
+	     *satisfy-a-two-argument-test*
+	     *error-not-proper-sequence*
+	     *error-bounding-indexes*))
             
+(fundoc 'count-if
+	(fmt "Lambda list: (PREDICATE SEQUENCE &key KEY START END FROM-END)~@
+              ~@
+              Description:~@
+              ~a~@
+              ~@
+              Arguments:~@
+              ~a~%~a~%~a~%~a~@
+              ~@
+              Satisfying the test:~@
+              ~a~@
+              ~@
+              Exceptional situations:~@
+              ~a~@
+              ~a"
+	     *count-description*
+	     *predicate-sequence* *key* *bounding-indexes* *from-end*
+	     *satisfy-a-one-argument-positive-test*
+	     *error-not-proper-sequence*
+	     *error-bounding-indexes*))
+
+(fundoc 'count-if-not
+	(fmt "Lambda list: (PREDICATE SEQUENCE &key KEY START END FROM-END)~@
+              ~@
+              Description:~@
+              ~a~@
+              ~@
+              Arguments:~@
+              ~a~%~a~%~a~%~a~@
+              ~@
+              Satisfying the test:~@
+              ~a~@
+              ~@
+              Exceptional situations:~@
+              ~a~@
+              ~a"
+	     *count-description*
+	     *predicate-sequence* *key* *bounding-indexes* *from-end*
+	     *satisfy-a-one-argument-negative-test*
+	     *error-not-proper-sequence*
+	     *error-bounding-indexes*))
