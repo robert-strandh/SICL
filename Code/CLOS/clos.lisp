043338a1b7e9dd87b0b28d01f6b2e8ebe7dd5cf6
Robert Strandh <strandh@nocl.(none)>
2012-02-06 16:40:24 +0100

    Define copute-class-precedence-list.

--- Code/CLOS/clos.lisp
+++ Code/CLOS/clos.lisp
@@ -1205,56 +1205,60 @@
 ;;; the class in the CDR of the cell. For a class C with a list of 
 ;;; direct superclasses (S1 S2 ... Sn), the relation contains 
 ;;; the elements (C . S1) (S1 . S2) ... (Sn-1 . Sn).
-;; (defun compute-relation (class)
-;;   (loop for prev = class then super
-;;         for super in (class-direct-superclasses class)
-;;         collect (cons prev super)))
+(defun compute-relation (class)
+  (loop for prev = class then super
+        for super in (class-direct-superclasses class)
+        collect (cons prev super)))
 
 ;; (defgeneric compute-class-prececence-list (class))
 
+(defun compute-class-precedence-list-class (class)
+  (let* ((all-supers (find-all-superclasses class))
+	 (relation (loop for class in all-supers
+		         append (compute-relation class)))
+	 (reverse-result '()))
+    (flet ((find-candidate ()
+	     (let ((candidates
+		     ;; Start by looking for all remaining classes that
+		     ;; depend on no other class according to the relation.
+		     (loop for super in all-supers
+			   unless (find super relation :key #'cdr)
+			     collect super)))
+	       ;; If no such class exists, we have a circular dependence,
+	       ;; and so we can't compute the class precedence list.
+	       (when (null candidates)
+		 ;; FIXME: do this better
+		 (error "can't compute class precedence list"))
+	       (if (null (cdr candidates))
+		   ;; A unique candiate, return it.
+		   (car candidates)
+		   ;; Several candidates.  Look for the last class in the
+		   ;; result computed so far (first in the reversed result)
+		   ;; that has one of the candidates as a direct superclass.
+		   ;; Return that candidate.  
+		   ;; There can be at most one, because within a list of
+		   ;; superclasses, there is already a dependency, so that
+		   ;; two different classes in a single list of superclasses
+		   ;; can not both be candidates.
+		   ;; There must be at least one, because ... 
+		   ;; (FIXME: prove it!)
+		   (loop for element in reverse-result
+		         do (loop for candidate in candidates
+			          do (when (member candidate
+						   (class-direct-superclasses
+						    element))
+				       (return-from find-candidate
+					 candidate))))))))
+      (loop until (null all-supers)
+	    do (let ((candidate (find-candidate)))
+		 (push candidate reverse-result)
+		 (setf all-supers (remove candidate all-supers))
+		 (setf relation (remove candidate relation :key #'car)))))
+    (reverse reverse-result)))
+  
+
 ;; (defmethod compute-class-prececence-list ((class class))
-;;   (let* ((all-supers (find-all-superclasses class))
-;; 	 (relation (loop for class in all-supers
-;; 		         append (compute-relation class)))
-;; 	 (reverse-result '()))
-;;     (flet ((find-candidate ()
-;; 	     (let ((candidates
-;; 		    ;; Start by looking for all remaining classes that
-;; 		    ;; depend on no other clas according to the relation.
-;; 		    (loop for super in all-supers
-;; 			      unless (find super relation :key #'cdr)
-;; 			      collect element)))
-;; 	       ;; If no such class exists, we have a circular dependence,
-;; 	       ;; and so we can't compute the class precedence list.
-;; 	       (when (null candidates)
-;; 		 ;; FIXME: do this better
-;; 		 (error "can't compute class precedence list"))
-;; 	       (if (null (cdr candidates))
-;; 		   ;; A unique candiate, return it.
-;; 		   (car candidates)
-;; 		   ;; Several candidates.  Look for the last class in the
-;; 		   ;; result computed so far (first in the reversed result)
-;; 		   ;; that has one of the candidates as a direct superclass.
-;; 		   ;; Return that candidate.  
-;; 		   ;; There can be at most one, because within a list of
-;; 		   ;; superclasses, there is already a dependency, so that
-;; 		   ;; two different classes in a single list of superclasses
-;; 		   ;; can not both be candidates.
-;; 		   ;; There must be at least one, because ... 
-;; 		   ;; (FIXME: prove it!)
-;; 		   (loop for element in reverse-result
-;; 		         do (loop for candidate in candidates
-;; 			          do (when (member candidate
-;; 						   (class-direct-superclasses
-;; 						    element))
-;; 				       (return-from find-candidate
-;; 					 candidate))))))))
-;;       (loop until (null all-supers)
-;; 	    do (let ((candidate (find-candidate)))
-;; 		 (push candidate reverse-result)
-;; 		 (setf all-supers (remove candidate all-supers))
-;; 		 (setf relation (remove candidate relation :key #'car)))))
-;;     reverse-result))
+;;   (compute-class-precedence-list-class class))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;
