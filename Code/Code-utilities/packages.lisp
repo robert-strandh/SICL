99e74b780e0d7ec5ecafe75855b8f2532ebd32fc
Robert Strandh <strandh@nocl.(none)>
2012-02-22 16:14:55 +0100

    Added sicl-additional-types and sicl-additional-conditions packages.

--- Code/Code-utilities/packages.lisp
+++ Code/Code-utilities/packages.lisp
@@ -1,5 +1,7 @@
 (in-package #:common-lisp-user)
 
 (defpackage #:sicl-code-utilities
-  (:use #:common-lisp)
+  (:use #:common-lisp
+	#:sicl-additional-types
+	#:sicl-additional-conditions)
   (:export #:list-structure))
