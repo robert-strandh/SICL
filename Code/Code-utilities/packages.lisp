2ec0538990553d1d88a084a1e5c548b8174fc42c
Robert Strandh <strandh@nocl.(none)>
2012-02-29 06:46:51 +0100

    Exported symbols related to lambda-list parsing.

--- Code/Code-utilities/packages.lisp
+++ Code/Code-utilities/packages.lisp
@@ -4,4 +4,23 @@
   (:use #:common-lisp
 	#:sicl-additional-types
 	#:sicl-additional-conditions)
-  (:export #:list-structure))
+  (:export #:list-structure
+	   #:lambda-list
+	   #:required
+	   #:environment
+	   #:whole
+	   #:optionals
+	   #:rest-body
+	   #:keys
+	   #:allow-other-keys
+	   #:aux
+	   #:parse-ordinary-lambda-list
+	   #:parse-generic-function-lambda-list
+	   #:parse-specialized-lambda-list
+	   #:parse-macro-lambda-list
+	   #:parse-destructuring-lambda-list
+	   #:parse-deftype-lambda-list
+	   #:parse-defsetf-lambda-list
+	   #:parse-define-modify-macro-lambda-list
+	   #:parse-define-method-combination-arguments-lambda-list))
+
