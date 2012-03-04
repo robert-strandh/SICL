c29e9746432208463263992dc4a5fdaccdf32759
Robert Strandh <strandh@nocl.(none)>
2012-03-04 07:34:34 +0100

    Implemented method dispatch closer to what the spec says.

--- Code/CLOS/generic-functions.lisp
+++ Code/CLOS/generic-functions.lisp
@@ -67,7 +67,86 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;
-;;; ENSURE-METHOD and DEFMETHOD
+;;; A few definitions:
+;;;
+;;; A METHOD FUNCTION is a function that represents a single method
+;;; metaobject of a generic function.  By default, when the generic
+;;; function is an instance of STANDARD-GENERIC-FUNCTION and the
+;;; method is an instance of STANDARD-METHOD, then the method function
+;;; takes two arguments: a list of all the arguments to the generic
+;;; function and a list of NEXT METHODS that the method may call using
+;;; CALL-NEXT-METHOD.  These next methods are METHOD METAOBJECTS, so
+;;; that calling then involves using the generic function
+;;; METHOD-FUNCTION to get the method function of the method
+;;; metaobject and use FUNCALL or APPLY to call it.  But for now we
+;;; don't do it that way, and the don't do it that way in the book
+;;; either.  Instead the next methods are just functions to call. 
+;;;
+;;; A METHOD LAMBDA is a lambda expression that must be converted into
+;;; a METHOD FUNCTION.  
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;; MAKE-METHOD-LAMBDA.
+
+;;; This generic function takes four arguments: a generic function, a
+;;; method, a lambda expression, and an environment.  We are told from
+;;; the specification that "The generic function and method the method
+;;; function will be used with are not required to be the given
+;;; ones. Moreover, the method metaobject may be uninitialized."  This
+;;; all means that MAKE-METHOD-LAMBDA must do its job without
+;;; inspecting those arguments, and using only their classes to
+;;; determine what to do.  Presumably, the method argument will just
+;;; be a prototype instance of some class.
+;;;
+;;; The third argument, the lambda expression, is the lambda
+;;; expression resulting from some minor transformations of the
+;;; DEFMETHOD form.  In other words, MAKE-METHOD-LAMBDA must transform
+;;; the unspecialized lambda list and body of an invocation of
+;;; DEFMETHOD to a lambda expression.
+;;;
+;;; We are also told that the result of a call to MAKE-METHOD-LAMBDA
+;;; must be converted to a function an passed as the :function
+;;; initialization argument to MAKE-INSTANCE when an instance of the
+;;; method metaobject is created.  This means that we can not use any
+;;; information about the generic function (other than its class) to
+;;; which the method will eventually belong.  
+;;;
+;;; This function returns two values, the first is a lambda expression
+;;; and the second a list of initialization arguments and values.  As
+;;; indicated above, the lambda expression must be converted to a
+;;; function.  The initialization arguments and values are also passed
+;;; to MAKE-INSTANCE when the method metaobject is created. 
+
+(defun make-method-lambda-standard
+    (generic-function method lambda-expression environment)
+  (declare (ignore generic-function method environment))
+  (let ((args (gensym))
+	(next-methods (gensym)))
+    (values `(lambda (,args ,next-methods)
+	       (flet ((next-method-p ()
+			(not (null ,next-methods)))
+		      (call-next-method (&rest args)
+			(when (null ,next-methods)
+			  ;; FIXME: do this better.
+			  (error "no next method"))
+			(funcall (car ,next-methods)
+				 (or args ,args)
+				 (cdr ,next-methods))))
+		 (declare (ignorable (function next-method-p)
+				     (function call-next-method)))
+		 (apply ,lambda-expression
+			,args)))
+	    '())))
+
+(defun make-method-lambda
+    (generic-function method lambda-expression environment)
+  (make-method-lambda-standard
+   generic-function method lambda-expression environment))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;; ENSURE-METHOD and DEFMETHOD.
 
 (defun parse-defmethod (args)
   (let ((name (pop args))
@@ -100,6 +179,10 @@
 (defun add-method (generic-function method)
   (setf (method-generic-function method) generic-function)
   (push method (generic-function-methods generic-function))
+  ;; Trash the cache. 
+  (let ((fun (compute-discriminating-function generic-function)))
+    (setf (generic-function-discriminating-function generic-function) fun)
+    (setf (fdefinition (generic-function-name generic-function)) fun))
   ;; FIXME: add method to specializer classes
   method)
 
@@ -117,18 +200,20 @@
 (defmacro defmethod (&rest arguments)
   (multiple-value-bind (name qualifiers lambda-list specializers body)
       (parse-defmethod arguments)
-    `(ensure-method
-      (ensure-generic-function ',name)
-      :lambda-list ',lambda-list
-      :qualifiers ',qualifiers
-      :specializers ',(canonicalize-specializers specializers)
-      :body ',body
-      :function (lambda ,lambda-list
-		  (flet ((call-next-method (&rest args)
-			   (apply *call-next-method* args))
-			 (next-method-p ()
-			   (funcall *next-method-p*)))
-		    ,@body)))))
+    (let ((generic-function-var (gensym)))
+    ;; FIXME: do the lambda list
+      `(let ((,generic-function-var (ensure-generic-function ',name)))
+	 (ensure-method
+	  ,generic-function-var
+	  :lambda-list ',lambda-list
+	  :qualifiers ',qualifiers
+	  :specializers ',(canonicalize-specializers specializers)
+	  :body ',body
+	  :function (make-method-lambda
+		     ,generic-function-var
+		     nil ; FIXME: pass a prototype method
+		     '(lambda ,lambda-list ,@body)
+		     nil))))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;
@@ -216,39 +301,48 @@
 (defun before-method-p (method)
   (equal (method-qualifiers method) '(:before)))
 
-(defun wrap-methods (methods)
-  (if (null (cdr methods))
-      `(let ((*call-next-method*
-	       (lambda (&rest new-args)
-		 (declare (ignore new-args))
-		 (error "no next method")))
-	     (*next-method-p* (lambda () nil))
-	     (args (or new-args args)))
-	 (apply ,(method-function (car methods)) args))
-      `(let ((*call-next-method*
-	       (lambda (&rest new-args)
-		 ,(wrap-methods (cdr methods))))
-	     (*next-method-p* (lambda () t))
-	     (args (or new-args args)))
-	 (apply ,(method-function (car methods)) args))))
+(defun around-method-p (method)
+  (equal (method-qualifiers method) '(:around)))
 
 (defun compute-effective-method-function (methods)
   (let ((primary-methods (remove-if-not #'primary-method-p methods))
 	(before-methods (remove-if-not #'before-method-p methods))
-	(after-methods (remove-if-not  #'after-method-p methods)))
-    (if (and (null before-methods)
-	     (null after-methods)
-	     (= (length primary-methods) 1))
-	(car primary-methods)
-	(compile nil
-		 `(lambda (&rest args)
-		    ,@(loop for method in before-methods
-			    collect `(apply ,(method-function method) args))
-		    (multiple-value-prog1
-			(let ((new-args '()))
-			  ,(wrap-methods primary-methods))
-		      ,@(loop for method in (reverse after-methods)
-			      collect `(apply ,(method-function method) args))))))))
+	(after-methods (remove-if-not  #'after-method-p methods))
+	(around-methods (remove-if-not  #'around-method-p methods)))
+    (when (null primary-methods)
+      (error "no primary method"))
+    (let ((primary-chain
+	    `(funcall ,(method-function (car primary-methods))
+		      args
+		      '(,@(loop for method in (cdr primary-methods)
+				collect (method-function method)))))
+	  (before-chain
+	    (loop for method in before-methods
+		  collect `(funcall ,(method-function method)
+				    args
+				    '())))
+	  (after-chain
+	    (loop for method in (reverse after-methods)
+		  collect `(funcall ,(method-function method)
+				    args
+				    '()))))
+      (compile
+       nil
+       (if (null around-methods)
+	   `(lambda (&rest args)
+	      ,@before-chain
+	      ,primary-chain
+	      ,@after-chain)
+	   `(lambda (&rest args)
+	      (funcall ,(method-function (car around-methods))
+		       args
+		       (list ,@(loop for method in (cdr around-methods)
+				     collect (method-function method))
+			     (lambda (args next-methods)
+			       (declare (ignore next-methods))
+			       ,@before-chain
+			       ,primary-chain
+			       ,@after-chain)))))))))
 	     
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;
