47883ae2195b8f12a1ce9e47a261a40f199968f4
Robert Strandh <strandh@nocl.(none)>
2012-04-14 10:05:18 +0200

    :USE the code-utilities package.  Fixed indentation.

--- Code/Conditionals/packages.lisp
+++ Code/Conditionals/packages.lisp
@@ -1,6 +1,6 @@
-;;;; Copyright (c) 2008, 2009, 2010, 2011
+;;;; Copyright (c) 2008, 2009, 2010, 2011, 2012
 ;;;;
-;;;;     Robert Strandh (strandh@labri.fr)
+;;;;     Robert Strandh (strandh@gmail.com)
 ;;;;
 ;;;; all rights reserved. 
 ;;;;
@@ -23,11 +23,11 @@
 ;;; express that as (:shadow . (<string1> <string2> ...)) instead. 
 
 (defpackage #:sicl-conditionals
-    (:use #:cl)
+  (:use #:common-lisp #:sicl-code-utilities)
   (:shadow . #1=(#:or #:and #:when #:unless #:cond
-                 #:case #:ccase #:ecase
-                 #:typecase #:ctypecase #:etypecase))
-		   (:export . #1#))	
+		      #:case #:ccase #:ecase
+		      #:typecase #:ctypecase #:etypecase))
+  (:export . #1#))	
 
 ;;; In the test package, we want to import all symbols of the
 ;;; COMMON-LISP package, except the ones that were shadowed in
@@ -38,7 +38,7 @@
 ;;; have the reader produce that list, hence the use of the #. reader
 ;;; macro and again of the consing dot followed by a list. 
 (defpackage #:sicl-conditionals-test
-    (:shadowing-import-from #:sicl-conditionals .
-                            #.(package-shadowing-symbols '#:sicl-conditionals))
-    (:use #:sicl-conditionals #:cl #:lisp-unit))
+  (:shadowing-import-from #:sicl-conditionals .
+			  #.(package-shadowing-symbols '#:sicl-conditionals))
+  (:use #:sicl-conditionals #:cl #:lisp-unit))
 
