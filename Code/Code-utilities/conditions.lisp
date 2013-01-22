53ac28f7e236e35794fc566c4c637a4aa3cfed1f
Robert Strandh <strandh@nocl.(none)>
2013-01-22 16:15:31 +0100

    remove condition form-must-be-proper-list defined elsewhere

--- Code/Code-utilities/conditions.lisp
+++ Code/Code-utilities/conditions.lisp
@@ -1,8 +1,5 @@
 (in-package #:sicl-code-utilities)
 
-(define-condition form-must-be-proper-list (program-error)
-  ((%form :initarg :form :reader form)))
-
 (defun check-form-proper-list (form)
   (unless (proper-list-p form)
     (error 'form-must-be-proper-list :form form)))
