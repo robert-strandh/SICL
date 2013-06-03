55149b4c0aa2fa884b0d005ee4e86377cf407603
Robert Strandh <strandh@nocl.(none)>
2013-06-03 11:18:19 +0200

    Added SYMBOL-FUNCTION and (SETF SYMBOL-FUNCTION).

--- Code/Compiler/environment.lisp
+++ Code/Compiler/environment.lisp
@@ -1291,6 +1291,38 @@
   ;; value that was assigned.
   new-definition)
   
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;; Function SYMBOL-FUNCTION.
+;;;
+;;; According to the HyperSpec, SYMBOL-FUNCTION is just like
+;;; FDEFINITION, except that it only accepts a symbol as its argument.
+;;; I am guessing that SYMBOL-FUNCTION existed before (SETF <mumble>)
+;;; were legal function names, and that FDEFINITION was introduced to
+;;; make such names possible.  In fact, on the SYMBOL-FUNCTION page,
+;;; the HyperSpec says: (symbol-function symbol) == (fdefinition symbol)
+;;; It suffices thus to check that the argument is a symbol, and then 
+;;; to call FDEFINITION to do the work. 
+
+(defun symbol-function (symbol)
+  (unless (symbolp symbol)
+    (error 'type-error :expected-type 'symbol :datum symbol))
+  (fdefinition symbol))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;; Function (SETF SYMBOL-FUNCTION).
+;;;
+;;; According to the HyperSpec, (SETF SYMBOL-FUNCTION) is just like
+;;; (SETF FDEFINITION), except that it only accepts a symbol as its
+;;; argument.  It suffices thus to check that the argument is a
+;;; symbol, and then to call (SETF FDEFINITION) to do the work.
+
+(defun (setf symbol-function) (new-definition symbol)
+  (unless (symbolp symbol)
+    (error 'type-error :expected-type 'symbol :datum symbol))
+  (setf (fdefinition symbol) new-definition))
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;
 ;;; Function SPECIAL-OPERATOR-P.
