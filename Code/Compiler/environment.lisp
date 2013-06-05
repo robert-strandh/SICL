cd0a437c5e334279eed42ff6825268b7b208e1ae
Robert Strandh <strandh@nocl.(none)>
2013-06-05 16:20:57 +0200

    Implemented CONSTANTP

--- Code/Compiler/environment.lisp
+++ Code/Compiler/environment.lisp
@@ -1987,6 +1987,20 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;
+;;; Function CONSTANTP.
+
+(defun constantp (form &optional environment)
+  (declare (ignore environment))
+  (or (and (not (symbolp form))
+	   (not (consp form)))
+      (keywordp form)
+      (not (null (find form (constant-variables *global-environment*)
+		       :key #'name :test #'eq)))
+      (and (consp form)
+	   (eq (car form) 'quote))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
 ;;; Function PROCLAIM.
 
 (defun proclaim-declaration (name)
