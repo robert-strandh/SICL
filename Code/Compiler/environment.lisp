3828543bf041cf6c516669849f851fe94aaea137
Robert Strandh <strandh@nocl.(none)>
2013-06-05 08:16:10 +0200

    More comments.

--- Code/Compiler/environment.lisp
+++ Code/Compiler/environment.lisp
@@ -852,6 +852,22 @@
   (lambda (macro-function macro-form environment)
     (funcall macro-function macro-form environment)))
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;; Function MACROEXPAND-1.
+;;;
+;;; This function returns two values, the expanded form and a boolean
+;;; indicating whether the form was expanded or not.  I can't think of
+;;; a reason for the existence of the second return value, because if
+;;; the form is a macro form, then the expander is called and the
+;;; second value is true.  If the form is not a macro form, then the
+;;; initial form is return and the second value is false.  If the
+;;; expander should ever return the same form, then MACROEXPAND-1 will
+;;; be called again (at least if it was called by MACROEXPAND) and we
+;;; would have an infinite computation.  Though I suppose that if the
+;;; expander did some non-functional stuff like consulting global
+;;; variables, then, it might be possible. 
+
 (defun macroexpand-1 (form &optional env)
   (let ((expander nil))
     (cond
@@ -874,6 +890,30 @@
 			 form
 			 env)
 		t))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;; Function MACROEXPAND.
+;;;
+;;; The HyperSpec says that this function repeatedly calls
+;;; MACROEXPAND-1 until the second return value is false, so that is
+;;; what we do too.
+;;;
+;;; However, that is not what the compiler should do.  The reason is
+;;; that MACROEXPAND-1 might very well return another macro form that
+;;; in addition has a compiler macro associated with it.  In that
+;;; case, it would be inappropriate (though technically not "wrong")
+;;; for the compiler to call MACROEXPAND-1 again.  Instead, it should
+;;; apply the compiler macro first and then expand again.  In fact,
+;;; the application of the compiler macro function might return
+;;; something other than a macro form. It could also be the case that
+;;; what MACROEXPAND-1 returns is NOT a macro form, but it is a
+;;; function-call form with a compiler macro associated with it, and
+;;; the compiler macro might return a macro form.
+;;;
+;;; For reasons mentioned in the previous paragraph, the compiler does
+;;; not call macroexpand at all, but instead a function called
+;;; FULLY-EXPAND-FORM defined below.
 
 (defun macroexpand (form &optional environment)
   (multiple-value-bind (expansion expanded-p)
@@ -961,6 +1001,12 @@
 	until (eq expanded-form form)
 	do (setf form expanded-form))
   form)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;; Function FULLY-EXPAND-FORM.
+;;;
+;;; 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;
