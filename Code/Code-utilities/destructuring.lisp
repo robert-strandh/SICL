03dde4c9eba16cab85aeaafdc8733d4a24f225c9
Robert Strandh <robert.strandh@gmail.com>
2013-06-11 12:01:37 +0200

    Improved and commented destructuring.

--- Code/Code-utilities/destructuring.lisp
+++ Code/Code-utilities/destructuring.lisp
@@ -5,7 +5,16 @@
 ;;; Destructuring a tree according to a lambda list.
 ;;;
 ;;; We assume that the lambda-list/pattern are syntactically correct.
+;;;
+;;; The function DESTRUCTURE-REQUIRED and DESTRUCTURE-OPTIONALS return
+;;; two values:
+;;;
+;;;  * A list of binding forms to be used in a LET*.
+;;;
+;;;  * A variable to be used to destructure the remaining pattern.
 
+;;; Recall that the required parameters of a parsed lambda list is
+;;; either the keyword :NONE, or a list of patterns.  
 (defun destructure-required (required var)
   (if (or (eq required :none) (null required))
       (values '() var)
@@ -21,6 +30,33 @@
 			  bindings)
 		  rest-var)))))
 
+;;; Recall that the optional parameters of a parsed lambda list is
+;;; either the keyword :none, or it is a list of &optional entries.
+;;; Each optional entry has one of the two forms:
+;;;
+;;;  * (pattern init-arg)
+;;;
+;;;  * (pattern init-arg supplied-p-parameter)
+;;;
+;;; If the original lambda-list did not have an init-arg, the parsing
+;;; process supplied NIL so that the parsed lambda list always has an
+;;; init-arg.
+;;;
+;;; The HyperSpec says that if there is an &optional parameter and the
+;;; object to be destructured "ends early", then the init-arg is
+;;; evaluated and destructured instead.  We interpret this phrase to
+;;; mean that either:
+;;;
+;;;   * the object to be destructured is NIL, in which case it "ends
+;;;     early", and we destructure the init-arg instead.
+;;;
+;;;   * the object is a CONS, in which case the CAR of the object is
+;;;     matched against the pattern.  If it doesn't match, then an
+;;;     error is signaled and no attemp is made to match agains the
+;;;     init-arg.
+;;;
+;;;   * the object is an atom other than NIL.  Then an error is
+;;;     signaled.
 (defun destructure-optionals (optionals var)
   (if (or (eq optionals :none) (null optionals))
       (values '() var)
@@ -31,21 +67,68 @@
 	    (destructure-optionals (cdr optionals) temp1)
 	  (values (append `((,temp1 (if (consp ,var)
 					(cdr ,var)
-					'()))
+					(if (null ,var)
+					    '()
+					    (error "optional expected"))))
+			    ;; If the object is not a CONS, then it is
+			    ;; either NIL in which case we destructure
+			    ;; the init-arg instead, or else it is an
+			    ;; atom other than NIL and we have already
+			    ;; signaled an error before, so we don't
+			    ;; need to handle that case again.
 			    (,temp2 (if (consp ,var)
 					(car ,var)
 					,(cadr optional)))
+			    ;; If a supplied-p-parameter exists, then
+			    ;; we give it the value TRUE whenever the
+			    ;; object is a CONS, even though later
+			    ;; an error might be signaled because there
+			    ;; is no match. 
 			    ,@(if (consp (cddr optional))
 				  `((,(caddr optional) (consp ,var)))))
 			  (destructure-pattern (car optional) temp2)
 			  bindings)
 		  rest-var)))))
-		
+
+;;; Recall that the keys part of a compiled lambda list is either
+;;; :none, which means that no &key was given at all, or a list if
+;;; &key entries.  If the list is empty, it means that &key was given,
+;;; but no &key parameters followed.
+;;;
+;;; A &key entry is either:
+;;;
+;;;  * ((keyword pattern) init-form)
+;;; 
+;;;  * ((keyword pattern) init-form supplied-p-parameter)
+;;;
+;;; The HyperSpec is pretty skimpy about what happens with keyword
+;;; arguments ("the rest of the list ... is taken apart
+;;; appropriately").  What we do is the following:
+;;;
+;;;  * If there is a keyword argument with the right keyword, then
+;;;    its value is matched against the pattern.
+;;;
+;;;  * Otherwise, the value of the init-form is matched agains the
+;;;    pattern.
 (defun destructure-keys (keys var)
   (if (or (eq keys :none) (null keys))
       '()
-      (let ((key (car keys)))
-	(append `((,(cadar key)
+      (let ((key (car keys))
+	    (temp (gensym)))
+	(append `(;; What we do in step 1 depends on whether there is
+		  ;; a supplied-p-parameter or not.  If there is, then
+		  ;; in step 1, we return a list of two things:
+		  ;; 
+		  ;;  * a boolean indicating whether we found the
+		  ;;    keyword.
+		  ;;
+		  ;;  * either the argument found, or the value of the
+		  ;;    init-form if no argument was found.
+		  ;;
+		  ;; If there is no supplied-p-parameter, then we just
+		  ;; return the argument found or the value of the
+		  ;; init-form if no argument was found.
+		  (,temp
 		   ,(if (consp (cddr key))
 			`(loop for rem = ,var then (cddr rem)
 			       while (consp rem)
@@ -56,31 +139,113 @@
 			       while (consp rem)
 			       when (eq (car rem) ',(caar key))
 				 return (cadr rem)
-			       finally (return ,(cadr key))))))
-		(if (consp (cddr key))
-		    `((,(caddr key)
-		       (prog1 (car ,(cadar key))
-			 (setf ,(cadar key) (cadr ,(cadar key))))))
-		    '())
+			       finally (return ,(cadr key)))))
+		  ;; If there is no supplied-p-parameter, then we are
+		  ;; done.  If there is, we must get it from the first
+		  ;; element of the list computed in step 1, and we must
+		  ;; replace that list with its second element.
+		  ,@(if (consp (cddr key))
+			`((,(caddr key)
+			   (prog1 (car ,temp)
+			     (setf ,temp (cadr ,temp)))))
+			'()))
+		(destructure-pattern (cadar key) temp)
 		(destructure-keys (cdr keys) var)))))
 
-;;; FIXME: check for even keyword/argument pairs.
-;;; FIXME: check for &allow-other-keys and :allow-other-keys.
+;; We return two values.  The first value is a list of bindings to be
+;; used with a LET* and the purpose of which is to destructure the
+;; arguments in VAR according to the LAMBDA-LIST.  The second value is
+;; a list of variables that ar bound in the bindings, but that should
+;; be declared IGNORE because they are introduced for technical
+;; reasons and not used anywhere.
 (defun destructure-lambda-list (lambda-list var)
   (multiple-value-bind (required-bindings var1)
       (destructure-required (required lambda-list) var)
     (multiple-value-bind (optional-bindings var2)
 	(destructure-optionals (optionals lambda-list) var1)
-      (let ((rest-bindings
-	      (if (eq (rest-body lambda-list) :none)
-		  '()
-		  (destructure-pattern (rest-body lambda-list) var2)))
-	    (key-bindings
-	      (destructure-keys (keys lambda-list) var2)))
-	(append required-bindings
-		optional-bindings
-		rest-bindings
-		key-bindings)))))
+      (let ((error-check-bindings '())
+	    (variables-to-ignore '()))
+	;; Generate bindings that check some conditions. 
+	(cond ((and (eq (rest-body lambda-list) :none)
+		    (eq (keys lambda-list) :none))
+	       ;; If there is neither a &rest/&body nor any keyword
+	       ;; parameters, then the remaining list must be NIL, or
+	       ;; else we signal an error.
+	       (let ((temp (gensym)))
+		 (push temp variables-to-ignore)
+		 (push `(,temp (when (not (null ,var2))
+				 (error "too many arguments supplied")))
+		       error-check-bindings)))
+	      ((not (eq (keys lambda-list) :none))
+	       ;; If there are keyword parameters, then we must check
+	       ;; several things.  First, we must check that the
+	       ;; remaining list is a proper list and that it has an
+	       ;; even number of elements.
+	       (let ((temp (gensym)))
+		 (push temp variables-to-ignore)
+		 (push `(,temp (multiple-value-bind (length structure)
+				   (list-structure ,var2)
+				 ;; First, the remaining list must be
+				 ;; a proper list.
+				 (unless (eq structure :proper)
+				   (error "with keyword parameters, ~
+                                           the arguments must be a ~
+                                           proper list."))
+				 ;; Second, it must have an even
+				 ;; number of elements.
+				 (unless (evenp length)
+				   (error "with keyword parameters, ~
+                                           the keyword part of the ~
+                                            arguments must have an ~
+                                            even number of elements."))))
+		       error-check-bindings))
+	       ;; If &allow-other keys was not given, more checks have
+	       ;; to be made.
+	       (unless (allow-other-keys lambda-list)
+		 (let ((temp (gensym))
+		       (allowed-keywords (mapcar #'caar (keys lambda-list))))
+		   (push temp variables-to-ignore)
+		   ;; Perhaps there was a :allow-other-keys <true> in
+		   ;; the argument list.  As usual, if there are
+		   ;; several pairs :allow-other-keys <mumble> then it
+		   ;; is the first one that counts.  This happens to
+		   ;; be exactly what GETF checks for so use it.
+		   (push `(,temp (unless (getf var2 :allow-other-keys)
+				   ;; Either no :allow-other-keys was
+				   ;; found, or the first one found
+				   ;; had a value of NIL.  Then every
+				   ;; keyword in the argument list
+				   ;; must be one of the ones supplied
+				   ;; in the parameters.
+				   (let ()
+				     (loop for keyword in var2 by #'cddr
+					   unless (member keyword
+							  ,allowed-keywords)
+					     do (error "unknown keyword ~s"
+						       keyword)))))
+			 error-check-bindings))))
+	      (t
+	       ;; If there are no keyword parameters, but there is a
+	       ;; &rest/&body, then we do no checks, which means that
+	       ;; the argument list can have any structure, including
+	       ;; circular.  the remaining list is simply matched with
+	       ;; the &rest/&body pattern.
+	       nil))
+	(let ((rest-bindings
+		(if (eq (rest-body lambda-list) :none)
+		    '()
+		    (destructure-pattern (rest-body lambda-list) var2)))
+	      (key-bindings
+		(destructure-keys (keys lambda-list) var2)))
+	  (values (append required-bindings
+			  optional-bindings
+			  rest-bindings
+			  (reverse error-check-bindings)
+			  key-bindings
+			  (if (eq (aux lambda-list) :none)
+			      '()
+			      (aux lambda-list)))
+		  variables-to-ignore))))))
 
 (defun destructure-pattern (pattern var)
   (cond ((null pattern)
@@ -230,19 +395,20 @@
 	 (final-env-var (if (eq env-var :none) (gensym) env-var))
 	 (form-var (gensym))
 	 (args-var (gensym)))
-    `(lambda (,form-var ,final-env-var)
-       ;; If the lambda list does not contain &environment, then
-       ;; we IGNORE the GENSYMed parameter to avoid warnings.
-       ;; If the lambda list does contain &envionrment, we do
-       ;; not want to make it IGNORABLE because we would want a
-       ;; warning if it is not used then. 
-       ,@(if (eq env-var :none)
-	     `((declare (ignore ,final-env-var)))
-	     `())
-       (let ((,args-var (cdr ,form-var)))
-	 (let* ,(destructure-lambda-list parsed-lambda-list args-var)
-	   ;; FIXME: insert ignorable variables introduced by
-	   ;; destrucuring here.
-	   ,@body)))))
+    (multiple-value-bind (bindings ignored-variables)
+	(destructure-lambda-list parsed-lambda-list args-var)
+      `(lambda (,form-var ,final-env-var)
+	 ;; If the lambda list does not contain &environment, then
+	 ;; we IGNORE the GENSYMed parameter to avoid warnings.
+	 ;; If the lambda list does contain &envionrment, we do
+	 ;; not want to make it IGNORABLE because we would want a
+	 ;; warning if it is not used then. 
+	 ,@(if (eq env-var :none)
+	       `((declare (ignore ,final-env-var)))
+	       `())
+	 (let ((,args-var (cdr ,form-var)))
+	   (let* ,bindings
+	     (declare (ignore ,@ignored-variables))
+	     ,@body))))))
 
 	 
