b782f4eb5ffaf16ef486310d9c9e5bdf4cf3b927
Robert Strandh <robert.strandh@gmail.com>
2013-06-12 07:09:45 +0200

    Defined macro DEFMACRO.

--- Code/Compiler/environment.lisp
+++ Code/Compiler/environment.lisp
@@ -1750,51 +1750,51 @@
 ;;; want to ignore the possibility of there being a special operator
 ;;; entry.
 
-(defun (setf macro-function) (new-function symbol &optional environment)
-  (unless (null environment)
-    (error 'type-error :datum environment :expected-type 'null))
-  (unless (symbolp symbol)
-    (error 'type-error :datum symbol :expected-type 'symbol))
-  (unless (functionp new-function)
-    (error 'type-error :datum new-function 'function))
-  ;; First check whether there is a global function entry with
-  ;; this name.
-  (let ((function-entry
-	  (find symbol (functions *global-environment*)
-		:key #'name :test #'eq)))
-    (if (not (null function-entry))
-	(progn
-	  ;; Make it unbound.
-	  (setf (car (storage (location function-entry))) +unbound+)
-	  ;; Remove any compiler macro that might be present referring
-	  ;; to this entry.
-	  (setf (compiler-macros *global-environment*)
-		(remove function-entry (compiler-macros *global-environment*)
-			:key #'base-entry :test #'eq))
-	  ;; Remove any proclamations referring to this entry.
-	  (setf (proclamations *global-environment*)
-		(remove-if (lambda (entry)
-			     (and (typep entry 'auxiliary-entry)
-				  (eq (base-entry entry) function-entry)))
-			   (proclamations *global-environment*))))
-	;; No function entry exists.  check whether there is a
-	;; global macro entry with this name.
-	(let ((macro-entry
-		(find symbol (macros *global-environment*)
-		      :key #'name :test #'eq)))
-	  (if (not (null macro-entry))
-	      ;; Then just replace the old definition.  This
-	      ;; way, we preserve any compiler macro that refers
-	      ;; to this entry.
-	      (setf (definition macro-entry) new-function)
-	      ;; Otherwise, we must create a new entry
-	      (push (make-instance 'global-macro-entry
-		      :name symbol
-		      :definition new-function
-		      :bound t)
-		    (macros *global-environment*))))))
-  ;; Return the new value as required by the HyperSpec.
-  new-function)
+(eval-when (:compile-toplevel :load-toplevel :execute)
+  (defun (setf macro-function) (new-function symbol &optional environment)
+    (unless (null environment)
+      (error 'type-error :datum environment :expected-type 'null))
+    (unless (symbolp symbol)
+      (error 'type-error :datum symbol :expected-type 'symbol))
+    (unless (functionp new-function)
+      (error 'type-error :datum new-function 'function))
+    ;; First check whether there is a global function entry with
+    ;; this name.
+    (let ((function-entry
+	    (find symbol (functions *global-environment*)
+		  :key #'name :test #'eq)))
+      (if (not (null function-entry))
+	  (progn
+	    ;; Make it unbound.
+	    (setf (car (storage (location function-entry))) +unbound+)
+	    ;; Remove any compiler macro that might be present referring
+	    ;; to this entry.
+	    (setf (compiler-macros *global-environment*)
+		  (remove function-entry (compiler-macros *global-environment*)
+			  :key #'base-entry :test #'eq))
+	    ;; Remove any proclamations referring to this entry.
+	    (setf (proclamations *global-environment*)
+		  (remove-if (lambda (entry)
+			       (and (typep entry 'auxiliary-entry)
+				    (eq (base-entry entry) function-entry)))
+			     (proclamations *global-environment*))))
+	  ;; No function entry exists.  check whether there is a
+	  ;; global macro entry with this name.
+	  (let ((macro-entry
+		  (find symbol (macros *global-environment*)
+			:key #'name :test #'eq)))
+	    (if (not (null macro-entry))
+		;; Then just replace the old definition.  This
+		;; way, we preserve any compiler macro that refers
+		;; to this entry.
+		(setf (definition macro-entry) new-function)
+		;; Otherwise, we must create a new entry
+		(push (make-instance 'global-macro-entry
+			:name symbol
+			:definition new-function)
+		      (macros *global-environment*))))))
+    ;; Return the new value as required by the HyperSpec.
+    new-function))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;
@@ -2089,6 +2089,26 @@
 	   (cdr declaration-specifier)))
     ;; FIXME: handle more proclamations
     ))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;; Macro DEFMACRO.
+;;;
+;;; We can obviously not define DEFMACRO using DEFMACRO.  But we have
+;;; all the ingredients to define DEFMACRO "manually".
+
+(eval-when (:compile-toplevel :load-toplevel :execute)
+  (setf (macro-function 'defmacro)
+	(compile nil
+		 (sicl-code-utilities:parse-macro
+		  'defmacro
+		  '(name lambda-list &body body)
+		  '(`(eval-when (:compile-toplevel :load-toplevel :execute)
+		       (setf (macro-function ',name)
+			     ,(sicl-code-utilities:parse-macro
+			       name
+			       lambda-list
+			       body))))))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;
