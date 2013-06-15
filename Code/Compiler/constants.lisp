67c9e63c2e42d6924b8ace6ce05a38977d5ea1cd
Robert Strandh <robert.strandh@gmail.com>
2013-06-15 11:24:44 +0200

    New file with global constants.

---
+++ Code/Compiler/constants.lisp
@@ -0,0 +1,17 @@
+(in-package #:sicl-compiler-environment)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;; Constant NIL.
+
+(eval-when (:compile-toplevel :load-toplevel :execute)
+  (push (make-constant-variable-entry nil nil)
+	(constant-variables *global-environment*)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;; Constant T.
+
+(eval-when (:compile-toplevel :load-toplevel :execute)
+  (push (make-constant-variable-entry t t)
+	(constant-variables *global-environment*)))
