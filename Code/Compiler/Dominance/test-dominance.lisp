062dc7d76a8e6ac08063ac23c5deda7cfc6fd2d4
Robert Strandh <robert.strandh@gmail.com>
2013-07-13 11:55:52 +0200

    More test functions.

--- Code/Compiler/Dominance/test-dominance.lisp
+++ Code/Compiler/Dominance/test-dominance.lisp
@@ -78,33 +78,83 @@
 			 (name node) (name idom)))))
     (format stream "}~%")))
 
-(defun draw-dominance-frontiers (start-node successor-fun filename)
-  (let ((dominance-frontiers (sicl-compiler-dominance:dominance-frontiers
-			      start-node successor-fun))
-	(table (make-hash-table :test #'eq)))
-    (with-open-file (stream filename
-			    :direction :output
-			    :if-exists :supersede)
-      (format stream "digraph G {~% ordering = out;~%")
-      (labels ((draw-node (node)
-		 (unless (gethash node table)
-		   (setf (gethash node table) t)
-		   (loop for succ in (successors node)
-			 do (draw-node succ))
-		   (format stream
-			   "   ~a [label = \"~a\"];~%"
-			   (name node) (name node))
-		   (loop for succ in (successors node)
+(defun draw-dominance-frontiers
+    (start-node successor-fun dominance-frontiers filename)
+  (flet ((successors (node)
+	   (funcall successor-fun node)))
+    (let ((table (make-hash-table :test #'eq)))
+      (with-open-file (stream filename
+			      :direction :output
+			      :if-exists :supersede)
+	(format stream "digraph G {~% ordering = out;~%")
+	(labels ((draw-node (node)
+		   (unless (gethash node table)
+		     (setf (gethash node table) t)
+		     (loop for succ in (successors node)
+			   do (draw-node succ))
+		     (format stream
+			     "   ~a [label = \"~a\"];~%"
+			     (name node) (name node))
+		     (loop for succ in (successors node)
+			   do (format stream
+				      "   ~a -> ~a;~%"
+				      (name node)
+				      (name succ))))))
+	  (draw-node start-node))
+	(loop for node being each hash-key of dominance-frontiers
+	      do (let ((f (gethash node dominance-frontiers)))
+		   (loop for ff in f
 			 do (format stream
-				    "   ~a -> ~a;~%"
-				    (name node)
-				    (name succ))))))
-	(draw-node start-node))
-      (loop for node being each hash-key of dominance-frontiers
-	    do (let ((f (gethash node dominance-frontiers)))
-		 (loop for ff in f
-		       do (format stream
-				  "   ~a -> ~a [style = bold, color = red];"
-				  (name node) (name ff)))))
-      (format stream "}~%"))))
+				    "   ~a -> ~a [style = bold, color = red];"
+				    (name node) (name ff)))))
+	(format stream "}~%")))))
     
+;;; The dominance frontier of a node X is defined to be the set of all
+;;; nodes Y such that 1: there exists a predecessor Z of Y such that X
+;;; dominates Z, and 2: X does not strictly dominate Y.
+(defun slow-dominance-frontiers (start-node successor-fun)
+  (let ((predecessor-fun (sicl-compiler-utilities:predecessor-function
+			  start-node successor-fun))
+	(dominance-tree (sicl-compiler-dominance:dominance-tree
+			 start-node successor-fun))
+	(preorder (sicl-compiler-utilities:depth-first-search-preorder
+		   start-node successor-fun))
+	(result (make-hash-table :test #'eq)))
+    (flet ((predecessors (node)
+	     (funcall predecessor-fun node))
+	   (dominates (n1 n2)
+	     (member n1 (sicl-compiler-dominance:dominators
+			 dominance-tree n2)))
+	   (strictly-dominates (n1 n2)
+	     (member n1 (sicl-compiler-dominance:strict-dominators
+			 dominance-tree n2))))	     
+      (loop for x in preorder
+	    do (loop for y in preorder
+		     do (when (and (some (lambda (z)
+					   (dominates x z))
+					 (predecessors y))
+				   (not (strictly-dominates x y)))
+			  (push y (gethash x result))))))
+    result))
+    
+(defun test-dominance-frontiers-one-flow-chart (start-node successor-fun)
+  (flet ((same-set-p (s1 s2)
+	   (and (null (set-difference s1 s2 :test #'eq))
+		(null (set-difference s2 s1 :test #'eq)))))
+    (let ((f1 (sicl-compiler-dominance:dominance-frontiers
+	       start-node successor-fun))
+	  (f2 (slow-dominance-frontiers
+	       start-node successor-fun)))
+      (loop for node being each hash-key of f1
+	    do (unless (same-set-p (gethash node f1) (gethash node f2))
+		 (throw 'discrepancy (values start-node f1 f2))))
+      (loop for node being each hash-key of f2
+	    do (unless (same-set-p (gethash node f1) (gethash node f2))
+		 (throw 'discrepancy (values start-node f1 f2)))))))
+
+(defun test-dominance-frontiers (&optional (n 10000))
+  (catch 'discrepancy
+    (loop repeat n
+	  do (test-dominance-frontiers-one-flow-chart
+	      (sicl-compiler-test-utilities:random-flow-chart)
+	      #'sicl-compiler-test-utilities:successors))))
