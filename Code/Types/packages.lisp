f849f621ec45f26cfa8fdb8434e37df044c708da
Robert Strandh <robert.strandh@gmail.com>
2013-08-09 09:41:55 +0200

    packages for type proclamations.

---
+++ Code/Types/packages.lisp
@@ -0,0 +1,20 @@
+(defpackage #:sicl-type-proclamations
+  (:import-from #:common-lisp
+		#:in-package
+		#:deftype
+		#:proclaim
+		#:and #:or #:satisfies
+		#:ftype
+		#:function
+		#:character
+		#:fixnum
+		#:bignum
+		#:number
+		#:cons
+		#:atom
+		#:list
+		#:null
+		#:eql
+		#:t))
+		
+		
