5d64d23ca16a221461d5c344a1a2a74abe807f58
Robert Strandh <robert.strandh@gmail.com>
2013-08-09 08:32:07 +0200

    Temporary AND macro.

--- Code/Compiler/macros.lisp
+++ Code/Compiler/macros.lisp
@@ -228,3 +228,14 @@
 
 (defmacro return (x)
   `(return-from nil ,x))
+
+(defmacro and (&rest forms)
+  (labels ((aux (forms)
+	     (if (null (cdr forms))
+		 (car forms)
+		 `(if ,(car forms)
+		      ,(aux (cdr forms))
+		      nil))))
+    (if (null forms)
+	t
+	(aux forms))))
