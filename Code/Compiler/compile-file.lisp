262d28c0e3e55c73e4d7f03b501bab1c76dfd919
Robert Strandh <robert.strandh@gmail.com>
2013-08-23 09:42:32 +0200

    Embryonic COMPILE-FILE.  Only collect ASTs and do top-level form processing at the moment.

---
+++ Code/Compiler/compile-file.lisp
@@ -0,0 +1,23 @@
+(in-package #:sicl-compiler)
+
+(defun compile-file (input-file &key
+				(output-file nil output-file-p)
+				(verbose *compile-verbose*)
+				(print *compile-print*)
+				(external-format :default))
+  
+  (with-open-file (stream input-file
+			  :direction :input
+			  :external-format external-format)
+    (let* ((*compile-file-pathname* (merge-pathnames input-file))
+	   (*compile-file-truename* (truename *compile-file-pathname*))
+	   (*package* *package*)
+	   (*readtable* *readtable*)
+	   (sicl-compiler-phase-1:*compile-file* t))
+      (loop with eof-value = (list nil)
+	    for form = (read stream nil eof-value)
+	    until (eq form eof-value)
+	    for result = (sicl-compiler-phase-1:convert form nil)
+	    unless (null result)
+	      collect result))))
+
