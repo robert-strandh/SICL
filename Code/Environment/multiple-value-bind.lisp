27f547bcfcdddf86b89705306788bc1d5d8f405d
Robert Strandh <robert.strandh@gmail.com>
2013-08-27 09:58:26 +0200

    New macro MULTIPLE-VALUE-BIND.

---
+++ Code/Environment/multiple-value-bind.lisp
@@ -0,0 +1,23 @@
+(cl:in-package #:sicl-compiler-environment)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;; Macro MULTIPLE-VALUE-BIND.
+;;;
+;;; We define this macro pretty much exactly as the HyperSpec says in
+;;; the "Notes:" section on the page describing MULTIPLE-VALUE-BIND.  
+;;;
+;;; Later, we will optimize a bit, either by recognizing this
+;;; particular expansion when we compile MULIPLE-VALUE-CALL, or by
+;;; handling MULTIPLE-VALUE-BIND as a special operator. 
+
+(defmacro multiple-value-bind (variables values-form &body body)
+  ;; FIXME: do these checks better.
+  (assert (sicl-code-utilities:proper-list-p variables))
+  (assert (every #'symbolp variables))
+  (let ((rest-variable (gensym)))
+    `(multiple-value-call
+	 (lambda (&optional ,@variables &rest ,rest-variable)
+	   (declare (ignore ,rest-variable))
+	   ,@body)
+       ,values-form)))
