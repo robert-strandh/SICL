2797a91b962f04c140263d4e31813e71a89e153b
Robert Strandh <robert.strandh@gmail.com>
2013-08-27 07:50:28 +0200

    Macro SETF.

---
+++ Code/Environment/setf.lisp
@@ -0,0 +1,23 @@
+(in-package #:sicl-compiler-environment)
+
+(defmacro setf (place new-value-form &rest more-pairs)
+  (cond ((null more-pairs)
+	 (multiple-value-bind (variables
+			       values
+			       store-variables
+			       writer-form
+			       reader-form)
+	     (get-setf-expansion place)
+	   (declare (ignore reader-form))
+	   `(let* ,(mapcar #'list variables values)
+	      (multiple-value-bind ,store-variables
+		  ,new-value-form
+		,writer-form))))
+	((not (null (cdr more-pairs)))
+	 `(progn (setf ,place ,new-value-form)
+		 (setf ,@more-pairs)))
+	(t
+	 (error "Odd number of arguments to SETF."))))
+
+		
+		
