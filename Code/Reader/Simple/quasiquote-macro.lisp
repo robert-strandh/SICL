46dfe38c339d5af1a8d4d3abc738398fb21eee37
Robert Strandh <robert.strandh@gmail.com>
2013-09-09 08:24:28 +0200

    Figured out a way to use the 'natural' definition of QUASIQUOTE.

--- Code/Reader/Simple/quasiquote-macro.lisp
+++ Code/Reader/Simple/quasiquote-macro.lisp
@@ -25,76 +25,35 @@
 ;;; such as (QUASIQUOTE (QUASIQUOTE ...) ...), or an expression such
 ;;; as (TRANSFORM (QUASIQUOTE ...) ...), before we decide on the
 ;;; action to take for the outermost occurrence.
-;;;
-;;; When this file is compiled, the QUASIQUOTE macro (obviously) does
-;;; not yet exist, and that macro is required when the BACKQUOTE
-;;; reader macro is used.  For that reason, we can't use BACKQUOTE, so
-;;; we have to do it manually.
-;;;
-;;; Furthermore, we haven't compiled the conditional macros yet, so we
-;;; can not use COND or CASE.
-;;;
-;;; What we really want is:
-;;;
-;;; (defmacro transform (form)
-;;;   (if (consp form)
-;;;       (case (car form)
-;;;         (quasiquote
-;;;          `(transform ,(macroexpand-1 form)))
-;;;         (unquote
-;;;          `(list ,(cadr form)))
-;;;         (unquote-splicing
-;;;          (cadr form))
-;;;         (t
-;;;          `(list (quasiquote ,form))))
-;;;       `(list (quasiquote ,form))))
-;;;
-;;; (defmacro quasiquote (form)
-;;;   (cond ((consp form)
-;;;          (case (car form)
-;;;            (unquote
-;;;             (cadr form))
-;;;            (unquote-splicing
-;;;             ;; Hmm.  This condition type is a subclass of
-;;;             ;; reader-error, which should be given a stream, but at
-;;;             ;; this point we no longer have the stream available.  
-;;;             (error 'undefined-use-of-backquote))
-;;;            (quasiquote
-;;;             `(quasiquote ,(macroexpand-1 form)))
-;;;            (t
-;;;             `(append (transform ,(car form)) (quasiquote ,(cdr form))))))
-;;;         ((vectorp form)
-;;;          `(apply #'vector (quasiquote ,(coerce form 'list))))
-;;;         (t
-;;;          `(quote ,form))))
 
 (defmacro transform (form)
   (if (consp form)
-      (if (eq (car form) 'quasiquote)
-	  (list 'transform (macroexpand-1 form))
-	  (if (eq (car form) 'unquote)
-	      (list 'list (cadr form))
-	      (if (eq (car form) 'unquote-splicing)
-		  (cadr form)
-		  (list 'list (list 'quasiquote form)))))
-      (list 'list (list 'quasiquote form))))
+      (case (car form)
+        (quasiquote
+         `(transform ,(macroexpand-1 form)))
+        (unquote
+         `(list ,(cadr form)))
+        (unquote-splicing
+         (cadr form))
+        (t
+         `(list (quasiquote ,form))))
+      `(list (quasiquote ,form))))
 
 (defmacro quasiquote (form)
-  (if (consp form)
-      (if (eq (car form) 'unquote)
-	  (cadr form)
-	  (if (eq (car form) 'unquote-splicing)
-	      ;; Hmm.  This condition type is a subclass of
-	      ;; reader-error, which should be given a stream, but at
-	      ;; this point we no longer have the stream available.  
-	      (error 'undefined-use-of-backquote)
-	      (if (eq (car form) 'quasiquote)
-		  (list 'quasiquote (macroexpand-1 form))
-		  (list 'append
-			(list 'transform (car form))
-			(list 'quasiquote (cdr form))))))
-      (if (vectorp form)
-	  (list 'apply
-		'#'vector
-		(list 'quasiquote (coerce form 'list)))
-	  (list 'quote form))))
+  (cond ((consp form)
+         (case (car form)
+           (unquote
+            (cadr form))
+           (unquote-splicing
+            ;; Hmm.  This condition type is a subclass of
+            ;; reader-error, which should be given a stream, but at
+            ;; this point we no longer have the stream available.  
+            (error 'undefined-use-of-backquote))
+           (quasiquote
+            `(quasiquote ,(macroexpand-1 form)))
+           (t
+            `(append (transform ,(car form)) (quasiquote ,(cdr form))))))
+        ((vectorp form)
+         `(apply #'vector (quasiquote ,(coerce form 'list))))
+        (t
+         `(quote ,form))))
