83db94696a457c48832a681a6854d5b70c87aeac
Robert Strandh <robert.strandh@gmail.com>
2013-09-09 06:22:14 +0200

    Don't use BACKQUOTE in the IN-PACKAGE macro.

--- Code/Environment/in-package.lisp
+++ Code/Environment/in-package.lisp
@@ -6,6 +6,18 @@
 (eval-when (:compile-toplevel)
   (setq *package* (find-package '#:sicl-env)))
 
+;;; The other thing that does not yet exist when this file is compiled
+;;; is QUASIQUOTE macro that is required when the BACKQUOTE reader
+;;; macro is used.  For that reason, we can't use BACKQUOTE either, so
+;;; we have to do it manually. 
+;;;
+;;; What we really want is:
+;;; (defmacro in-package (string-designator)
+;;;  `(eval-when (:compile-toplevel :load-toplevel :execute)
+;;;     (in-package-function ',string-designator)))
+
 (defmacro in-package (string-designator)
-  `(eval-when (:compile-toplevel :load-toplevel :execute)
-     (in-package-function ',string-designator)))
+  (list 'eval-when '(:compile-toplevel :load-toplevel :execute)
+	(list 'in-package-function
+	      (list 'quote string-designator))))
+
