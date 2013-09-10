7870de6ad01ceff2f44e3f2938bf4ac496cbc17a
Robert Strandh <robert.strandh@gmail.com>
2013-09-10 07:18:31 +0200

    Bug fix.

--- Code/Environment/declaim.lisp
+++ Code/Environment/declaim.lisp
@@ -1,7 +1,7 @@
 (cl:in-package #:sicl-compiler-environment)
 
 (defmacro declaim (&rest declaration-specifiers)
-  (eval-when (:compile-toplevel :load-toplevel :execute)
-    `(progn ,@(mapcar #'(lambda (declaration-specifier)
+  `(eval-when (:compile-toplevel :load-toplevel :execute)
+     (progn ,@(mapcar #'(lambda (declaration-specifier)
 			  `(proclaim ',declaration-specifier))
 		      declaration-specifiers))))
