90557214121399eb5dd45caf7808691a2b36dbfa
Robert Strandh <robert.strandh@gmail.com>
2013-09-10 07:21:09 +0200

    Two versions of the DEFTYPE macro.

---
+++ Code/Environment/deftype-1.lisp
@@ -0,0 +1,16 @@
+(cl:in-package #:sicl-compiler-environment)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;; Macro DEFTYPE.
+
+(defmacro deftype (name lambda-list &body body)
+  `(eval-when (:compile-toplevel)
+     (funcall #'(setf type-function)
+	      (function ,(sicl-code-utilities:parse-deftype 
+			  name
+			  lambda-list
+			  body))
+	      ',name)))
+
+		     
---
+++ Code/Environment/deftype.lisp
@@ -0,0 +1,16 @@
+(cl:in-package #:sicl-compiler-environment)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;; Macro DEFTYPE.
+
+(defmacro deftype (name lambda-list &body body)
+  `(eval-when (:compile-toplevel :load-toplevel :execute)
+     (funcall #'(setf type-function)
+	      (function ,(sicl-code-utilities:parse-deftype 
+			  name
+			  lambda-list
+			  body))
+	      ',name)))
+
+		     
