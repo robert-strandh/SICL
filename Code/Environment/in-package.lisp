5e870b1bc3237d0752b93f6f0d8e521e19811ba3
Robert Strandh <robert.strandh@gmail.com>
2013-09-10 11:07:40 +0200

    Remove unnecessary restrictions.

--- Code/Environment/in-package.lisp
+++ Code/Environment/in-package.lisp
@@ -1,23 +1,9 @@
 ;;;; The purpose of this file is to create the macro IN-PACKAGE.
 
-;;; Since IN-PACKAGE does not exist when this file is compiled, we
-;;; must do what IN-PACKAGE does manually.  In this case, we set the
-;;; *PACKAGE* variable to the package SICL-ENV.
-(eval-when (:compile-toplevel)
-  (setq *package* (find-package '#:sicl-env)))
-
-;;; The other thing that does not yet exist when this file is compiled
-;;; is QUASIQUOTE macro that is required when the BACKQUOTE reader
-;;; macro is used.  For that reason, we can't use BACKQUOTE either, so
-;;; we have to do it manually. 
-;;;
-;;; What we really want is:
-;;; (defmacro in-package (string-designator)
-;;;  `(eval-when (:compile-toplevel :load-toplevel :execute)
-;;;     (in-package-function ',string-designator)))
+;;; When this file is compiled by the cross compiler, we already have
+;;; a temporary definition of IN-PACKAGE in place, so we can use it.
+(in-package #:sicl-compiler-environment)
 
 (defmacro in-package (string-designator)
-  (list 'eval-when '(:compile-toplevel :load-toplevel :execute)
-	(list 'in-package-function
-	      (list 'quote string-designator))))
-
+  `(eval-when (:compile-toplevel :load-toplevel :execute)
+     (in-package-function ',string-designator)))
