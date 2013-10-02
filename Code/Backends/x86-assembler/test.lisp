81f0d6d999a4cc6be1fd1af07ad780defdb172c6
Robert Strandh <robert.strandh@gmail.com>
2013-10-02 17:13:30 +0200

    More tests.

--- Code/Backends/x86-assembler/test.lisp
+++ Code/Backends/x86-assembler/test.lisp
@@ -33,15 +33,52 @@
 	     ,(if (minusp imm) (+ imm 256) imm))))
     (assert (equal (list y) (assemble (list x))))))
 
+;;; Test instructions with an opcode of #x83 and where the
+;;; first operand is a 16-bit GPR between 1 and 7.
+(defun run-test-83-reg-3 (mnemonic extension reg imm)
+  (let ((x (make-instance 'code-command
+	     :mnemonic mnemonic
+	     :operands (list
+			(make-instance 'gpr-operand
+			  :code-number reg
+			  :size 16)
+			(make-instance 'immediate-operand
+			  :value imm))))
+	(y `(#x66 ; Operand-size override prefix.
+	     #x83
+	     ,(+ #xC0 (ash extension 3) reg)
+	     ,(if (minusp imm) (+ imm 256) imm))))
+    (assert (equal (list y) (assemble (list x))))))
+
+;;; Test instructions with an opcode of #x83 and where the
+;;; first operand is a 16-bit GPR between 8 and 15.
+(defun run-test-83-reg-4 (mnemonic extension reg imm)
+  (let ((x (make-instance 'code-command
+	     :mnemonic mnemonic
+	     :operands (list
+			(make-instance 'gpr-operand
+			  :code-number reg
+			  :size 16)
+			(make-instance 'immediate-operand
+			  :value imm))))
+	(y `(#x66 ; Operand-size override prefix.
+	     #x41 ; Rex prefix
+	     #x83
+	     ,(+ #xC0 (ash extension 3) (- reg 8))
+	     ,(if (minusp imm) (+ imm 256) imm))))
+    (assert (equal (list y) (assemble (list x))))))
+
 (defun run-test-1 ()
   (loop for mnemonic in '("ADD" "OR" "AND" "XOR")
 	for extension in '(0 1 4 6)
 	do (loop for reg from 1 to 7
 		 do (loop for imm from -128 to 127
-			  do (run-test-83-reg-1 mnemonic extension reg imm)))
+			  do (run-test-83-reg-1 mnemonic extension reg imm)
+			     (run-test-83-reg-3 mnemonic extension reg imm)))
 	   (loop for reg from 8 to 15
 		 do (loop for imm from -128 to 127
-			  do (run-test-83-reg-2 mnemonic extension reg imm)))))
+			  do (run-test-83-reg-2 mnemonic extension reg imm)
+			     (run-test-83-reg-4 mnemonic extension reg imm)))))
 
 (defun run-tests ()
   (run-test-1))
