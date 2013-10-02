26a586974a06e96f05e495d16b0db473bf7998f6
Robert Strandh <robert.strandh@gmail.com>
2013-10-02 16:22:05 +0200

    More tests.

--- Code/Backends/x86-assembler/test.lisp
+++ Code/Backends/x86-assembler/test.lisp
@@ -1,6 +1,6 @@
 (in-package :x86-assembler)
 
-(defun run-test-83 (mnemonic extension reg)
+(defun run-test-83 (mnemonic extension reg imm)
   (let ((x (make-instance 'code-command
 	     :mnemonic mnemonic
 	     :operands (list
@@ -8,15 +8,18 @@
 			  :code-number reg
 			  :size 32)
 			(make-instance 'immediate-operand
-			  :value 33))))
-	(y `(#x83 ,(+ #xC0 (ash extension 3) reg) 33)))
+			  :value imm))))
+	(y `(#x83
+	     ,(+ #xC0 (ash extension 3) reg)
+	     ,(if (minusp imm) (+ imm 256) imm))))
     (assert (equal (list y) (assemble (list x))))))
 
 (defun run-test-1 ()
   (loop for mnemonic in '("ADD" "OR" "AND" "XOR")
 	for extension in '(0 1 4 6)
 	do (loop for reg from 1 to 7
-		 do (run-test-83 mnemonic extension reg))))
+		 do (loop for imm from -128 to 127
+			  do (run-test-83 mnemonic extension reg imm)))))
 
 (defun run-tests ()
   (run-test-1))
