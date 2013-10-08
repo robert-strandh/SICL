29c0357d6d55cd0974fa88e0310067a3eb2c4995
Robert Strandh <robert.strandh@gmail.com>
2013-10-08 07:24:33 +0200

    Added type proclamation for ABORT.

--- Code/Conditions/type-proclamations.lisp
+++ Code/Conditions/type-proclamations.lisp
@@ -3,3 +3,7 @@
 ;;; FIXME: we need some more specific types here.
 (declaim (ftype (function (t &rest list) nil)
 		error))
+
+(declaim (ftype (function (&optional (or null condition)) nil)
+		abort))
+
