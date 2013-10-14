2d20839e9b4a38722eaff12bd562d33be5739b4b
Robert Strandh <robert.strandh@gmail.com>
2013-10-14 18:16:55 +0200

    More methods.

--- Code/Compiler/type-inference.lisp
+++ Code/Compiler/type-inference.lisp
@@ -28,6 +28,16 @@
 
 (defgeneric process-instruction (instruction))
 
+(defmethod process-instruction :around (instruction)
+  (let ((old-maps (loop for succ in (sicl-mir:successors instruction)
+			collect (gethash (list instruction succ) *type-info*))))
+    (call-next-method)
+    (loop for succ in (sicl-mir:successors instruction)
+	  for old-map in old-maps
+	  do (unless (sicl-compiler-types:type-map-equal
+		      old-map (gethash (list instruction succ) *type-info*))
+	       (push succ *worklist*)))))
+
 (defun type-inference (initial-instruction)
   (let ((*type-info* (make-hash-table :test #'equal))
 	(*worklist* '()))
@@ -45,5 +55,15 @@
     (loop for succ in (sicl-mir:successors instruction)
 	  do (setf (gethash (list instruction succ) *type-info*) result))))
 
+(defmethod process-instruction ((instruction sicl-mir:get-argcount-instruction))
+  (setf (gethash (list instruction (car (sicl-mir:successors instruction)))
+		 *type-info*)
+	(sicl-compiler-types:type-map-and
+	 (reduce #'sicl-compiler-types:type-map-or
+		 (loop for pred in (sicl-mir:predecessors instruction)
+		       collect (gethash (list pred instruction)
+					*type-info*)))
+	 (sicl-compiler-types:make-single-variable-type-map
+	  (car (sicl-mir:outputs instruction))
+	  '(integer 0 #.(expt 2 30))))))
 
-		   
