85ad9b9bf59356ecc8219398b5312ca2942b7c8e
Robert Strandh <robert.strandh@gmail.com>
2013-10-29 10:08:38 +0100

    Added mnemonic POP.

--- Code/Backends/x86-assembler/instruction-database.lisp
+++ Code/Backends/x86-assembler/instruction-database.lisp
@@ -3517,6 +3517,114 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;
+;;; Mnemonic POP
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;; Into a GPR or memory location (16/32/64) (destination), pop
+;;; the top of the stack.
+;;;
+;;; Opcodes: 8F
+;;; Opcode extension: 0
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;; Into a 16-bit GPR or memory location (destination), pop the top of
+;;; the stack.
+
+;;; Into a 16-bit GPR (destination), pop the top of the stack.
+(define-instruction "POP"
+  :modes (32 64)
+  :operands ((gpr 16))
+  :opcodes (#x8F)
+  :opcode-extension 0
+  :encoding (modrm)
+  :operand-size-override t)
+
+;;; Into a 16-bit memory location (destination), pop the top of the
+;;; stack.
+(define-instruction "POP"
+  :modes (32 64)
+  :operands ((mem 16))
+  :opcodes (#x8F)
+  :opcode-extension 0
+  :encoding (modrm)
+  :operand-size-override t)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;; Into a 32-bit GPR or memory location (destination), pop the top of
+;;; the stack.
+
+;;; Into a 32-bit GPR (destination), pop the top of the stack.
+(define-instruction "POP"
+  :modes (32)
+  :operands ((gpr 32))
+  :opcodes (#x8F)
+  :opcode-extension 0
+  :encoding (modrm))
+
+;;; Into a 32-bit memory location (destination), pop the top of the
+;;; stack.
+(define-instruction "POP"
+  :modes (32)
+  :operands ((mem 32))
+  :opcodes (#x8F)
+  :opcode-extension 0
+  :encoding (modrm))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;; Into a 64-bit GPR or memory location (destination), pop the top of
+;;; the stack.
+
+;;; Into a 64-bit GPR (destination), pop the top of the stack.
+(define-instruction "POP"
+  :modes (64)
+  :operands ((gpr 64))
+  :opcodes (#x8F)
+  :opcode-extension 0
+  :encoding (modrm))
+
+;;; Into a 64-bit memory location (destination), pop the top of the
+;;; stack.
+(define-instruction "POP"
+  :modes (64)
+  :operands ((mem 64))
+  :opcodes (#x8F)
+  :opcode-extension 0
+  :encoding (modrm))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;; Into a GPR (16/32/64) (destination), pop the top of the stack.
+;;;
+;;; Opcodes: 58
+
+;;; Into a 16-bit GPR (destination), pop the top of the stack.
+(define-instruction "POP"
+  :modes (32 64)
+  :operands ((gpr 16))
+  :opcodes (#x58)
+  :encoding (+r)
+  :operand-size-override t)
+
+;;; Into a 32-bit GPR (destination), pop the top of the stack.
+(define-instruction "POP"
+  :modes (32)
+  :operands ((gpr 32))
+  :opcodes (#x58)
+  :encoding (+r))
+
+;;; Into a 64-bit GPR (destination), pop the top of the stack.
+(define-instruction "POP"
+  :modes (64)
+  :operands ((gpr 64))
+  :opcodes (#x58)
+  :encoding (+r))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
 ;;; Mnemonic RET
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
