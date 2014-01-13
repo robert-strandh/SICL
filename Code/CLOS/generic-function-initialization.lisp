37dbe115cfe3518a2cb1eb9c60633152020e9fc9
Robert Strandh <robert.strandh@gmail.com>
2014-01-13 17:16:13 +0100

    Split generic-function-initialization.lisp into smaller files.

--- Code/CLOS/generic-function-initialization.lisp
+++ Code/CLOS/generic-function-initialization.lisp
@@ -13,10 +13,13 @@
     (error "argument-precedence-order must be a proper list"))
   (when lambda-list-p
     (let ((parsed-lambda-list
-	    (parse-generic-function-lambda-list lambda-list nil nil)))
+	    (parse-generic-function-lambda-list lambda-list)))
       (unless argument-precedence-order-p
 	(setf argument-precedence-order
 	      (required parsed-lambda-list)))
+      (setf (specializer-profile generic-function)
+	    (make-list (length (required parsed-lambda-list))
+		       :initial-element nil))
       (setf (gf-lambda-list generic-function)
 	    lambda-list)
       (setf (gf-argument-precedence-order generic-function)
@@ -46,56 +49,3 @@
   (setf (gf-method-class generic-function)
 	method-class))
 
-(defmethod initialize-instance :after
-    ((generic-function standard-generic-function)
-     &key
-       (lambda-list nil lambda-list-p)
-       (argument-precedence-order nil argument-precedence-order-p)
-       documentation
-       declarations
-       method-combination
-       (method-class (find-class 'standard-method))
-       name
-     &allow-other-keys)
-  (set-documentation generic-function documentation)
-  (set-declarations generic-function declarations)
-  (set-method-class generic-function method-class)
-  (setf (gf-name generic-function) name)
-  (set-lambda-list-and-argument-precedence-order
-   generic-function
-   lambda-list
-   lambda-list-p
-   argument-precedence-order
-   argument-precedence-order-p)
-
-(defmethod reinitialize-instance :after
-    ((generic-function standard-generic-function)
-     &key
-       (lambda-list nil lambda-list-p)
-       (argument-precedence-order nil argument-precedence-order-p)
-       (documentation nil documentation-p)
-       (declarations nil declarations-p)
-       method-combination
-       (method-class nil method-class-p)
-       (name nil name-p)
-     &allow-other-keys)
-  (when documentation-p
-    (set-documentation generic-function documentation))
-  (when declarations-p
-    (set-declarations generic-function declarations))
-  (when method-class-p
-    (set-method-class generic-function method-class))
-  (when name-p
-    (setf (gf-name generic-function) name))
-  (set-lambda-list-and-argument-precedence-order
-   generic-function
-   lambda-list
-   lambda-list-p
-   argument-precedence-order
-   argument-precedence-order-p)
-  (map-dependents class
-		  (lambda (dependent)
-		    (apply #'update-dependent class dependent args))))
-
-
-  
