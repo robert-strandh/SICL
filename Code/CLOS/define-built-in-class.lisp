489304bd1de09ed6a8766558870e260f2d27e77e
Robert Strandh <robert.strandh@gmail.com>
2014-01-14 06:42:19 +0100

    Used new function ENSURE-BUILT-IN-CLASS rather than ENSURE-CLASS.

--- Code/CLOS/define-built-in-class.lisp
+++ Code/CLOS/define-built-in-class.lisp
@@ -1,10 +1,9 @@
 (cl:in-package #:sicl-clos)
 
 (defmacro define-built-in-class (name superclass-names slot-specifiers)
-  `(ensure-class ',name
-		 :direct-superclasses 
-		 ,(canonicalize-direct-superclass-names superclass-names)
-		 :direct-slots
-		 ,(canonicalize-direct-slot-specs slot-specifiers)
-		 :metaclass
-		 'built-in-class))
+  `(ensure-built-in-class
+    ',name
+    :direct-superclasses 
+    ,(canonicalize-direct-superclass-names superclass-names)
+    :direct-slots
+    ,(canonicalize-direct-slot-specs slot-specifiers)))
