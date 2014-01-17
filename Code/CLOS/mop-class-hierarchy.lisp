3c720ca6060344baca63b0d5825c1e21c7ac6b35
Robert Strandh <robert.strandh@gmail.com>
2014-01-17 07:34:31 +0100

    Merged slot definitions into mop hierarchy file.

--- Code/CLOS/mop-class-hierarchy.lisp
+++ Code/CLOS/mop-class-hierarchy.lisp
@@ -402,9 +402,9 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;
-;;; FORWARD-REFERENCE-CLASS.
+;;; FORWARD-REFERENCED-CLASS.
 
-(defclass forward-reference-class (class)
+(defclass forward-referenced-class (class)
   ((%direct-default-initargs
     ;; The AMOP says that CLASS-DIRECT-DEFAULT-INITARGS should
     ;; return the empty list for a forward reference class. 
@@ -430,13 +430,13 @@
     :initform nil
     :reader class-finalized-p)))
 
-(defmethod class-default-initargs ((class forward-reference-class))
+(defmethod class-default-initargs ((class forward-referenced-class))
   (error "A forward reference class does not have any default initargs"))
 
-(defmethod class-precedence-list ((class forward-reference-class))
+(defmethod class-precedence-list ((class forward-referenced-class))
   (error "A forward reference class does not have a precedence list"))
 
-(defmethod class-slots ((class forward-reference-class))
+(defmethod class-slots ((class forward-referenced-class))
   (error "A forward reference class does not have any slots"))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -547,3 +547,67 @@
    (%dependents
     :initform '()
     :accessor dependents)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;; Slot definition classes.
+
+(defclass slot-definition (metaobject)
+  ((%name 
+    :initarg :name 
+    :reader slot-definition-name)
+   (%allocation 
+    :initarg :allocation
+    :initform :instance
+    :reader slot-definition-allocation)
+   (%type 
+    :initarg :type
+    :initform t
+    :reader slot-definition-type)
+   (%initargs 
+    :initform '()
+    :initarg :initargs 
+    :reader slot-definition-initargs)
+   (%initform 
+    :initarg :initform 
+    :reader slot-definition-initform)
+   (%initfunction 
+    :initform nil
+    :initarg :initfunction
+    :accessor slot-definition-initfunction)))
+
+;;; The READERS and WRITERS slots only exist in direct slot
+;;; definitions, because they are not combined the way other slot
+;;; properties are when an effective slot definition is computer.
+(defclass direct-slot-definition (slot-definition)
+  ((%readers 
+    :initform '()
+    :initarg :readers 
+    :reader slot-definition-readers)
+   (%writers 
+    :initform '()
+    :initarg :writers 
+    :reader slot-definition-writers)
+   ;; The CAR of the CONS cell stored in this slot is used to hold
+   ;; values when :ALLOCATION is :CLASS.  In this case, the CONS cell
+   ;; becomes the location of the effective slot definition.
+   (%storage
+    :initform (list nil)
+    :reader slot-definition-storage)))
+
+(defclass effective-slot-definition (slot-definition)
+  ((%location 
+    :initform nil
+    :initarg :location 
+    :accessor slot-definition-location)))
+
+(defclass standard-slot-definition (slot-definition)
+  ())
+
+(defclass standard-direct-slot-definition
+    (standard-slot-definition direct-slot-definition)
+  ())
+
+(defclass standard-effective-slot-definition
+    (standard-slot-definition effective-slot-definition)
+  ())
