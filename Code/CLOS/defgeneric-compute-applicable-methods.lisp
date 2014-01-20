e5c0a206135e275043e7b96cb0a2777fc4dae83e
Robert Strandh <robert.strandh@gmail.com>
2014-01-20 12:06:39 +0100

    Generic function and specified method for COMPUTE-APPLICABLE-METHODS.

---
+++ Code/CLOS/defgeneric-compute-applicable-methods.lisp
@@ -0,0 +1,15 @@
+(cl:in-package #:sicl-clos)
+
+;;; Given a generic function and a list of arguments to that generic
+;;; function, return a list of the methods that are applicable to
+;;; those arguments.  The methods in the list are ordered from most
+;;; specific to least specific.  If there are no applicable methods,
+;;; then the empty list is returned.
+;;;
+;;; The list of arguments may contain more arguments than there are
+;;; required parameters of the generic functions, so that the caller
+;;; can simply pass the entire list of arguments without truncating it
+;;; to the number of required arguments.  This function simply ignores
+;;; arguments beyond the last required arguments.
+
+(defgeneric compute-applicable-methods (generic-function arguments))
---
+++ Code/CLOS/defmethod-compute-applicable-methods.lisp
@@ -0,0 +1,38 @@
+(cl:in-package #:sicl-clos)
+
+;;; Given a list of arguments of a standard generic function, compute
+;;; the applicable methods, independently of their qualifiers, but
+;;; sorted in order from most to least specific.
+;;;
+;;; We extract the body of this method into a separate function,
+;;; because there is a potential metastability problem here, and that
+;;; is when the function COMPUTE-APPLICABLE-METHODS is called with the
+;;; function COMPUTE-APPLICABLE-METHODS itself as an argument.  We
+;;; avoid the metastability problem by recognizing that
+;;; COMPUTE-APPLICABLE-METHODS is a standard generic function and
+;;; having the discriminating function of COMPUTE-APPLICABLE-METHODS
+;;; test for the class of its argument being the class named
+;;; STANDARD-GENERIC-FUNCTION.  Since portable programs are not
+;;; allowed to add a method to the function COMPUTE-APPLICABLE-METHODS
+;;; unless that method has a specializer other than
+;;; STANDARD-GENERIC-FUNCTION or any of its superclasses, we know that
+;;; in this case, only the method below is applicable, so the
+;;; discriminating function will call the extracted method body
+;;; directly.
+;;;
+;;; The special case for the discriminating function is introduced by
+;;; COMPUTE-DISCRIMINATING-FUNCTION, so there is no trace of it here. 
+
+(defun compute-applicable-methods-standard-generic-function
+    (standard-generic-function classes-of-arguments)
+  (sort (loop for method in (generic-function-methods generic-function)
+	      when (definitely-applicable-p method classes-of-arguments)
+		collect method)
+	(lambda (method1 method2)
+	  (method-more-specific-p method1 method2 classes-of-arguments))))
+
+(defmethod compute-applicable-methods
+    ((generic-function standard-generic-function) classes-of-arguments)
+  (compute-applicable-methods-standard-generic-function
+   generic-function
+   classes-of-arguments))
