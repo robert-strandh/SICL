b4d0ca3200f8ebfc2a7474e117deba1b855d619e
Robert Strandh <robert.strandh@gmail.com>
2014-01-27 17:13:08 +0100

    Several additional DEFGENERIC forms.

--- Code/CLOS/accessor-defgenerics.lisp
+++ Code/CLOS/accessor-defgenerics.lisp
@@ -6,6 +6,16 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;
+;;; Readers for specializer metaobjects other than classes.
+
+(defgeneric specializer-direct-generic-functions (specializer))
+
+(defgeneric specializer-direct-methods (specializer))
+
+(defgeneric eql-specializer-object (eql-specializer))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
 ;;; Readers for class metaobjects.
 ;;;
 ;;; For a list of specified readers of these metaobjects, see
@@ -171,26 +181,196 @@
 ;;; function.
 (defgeneric (setf c-direct-subclasses) (new-value class))
 
-(defgeneric standard-instance-slots (standard-instance))
-(defgeneric standard-instance-timestamp (standard-instance))
+;;; Given a standard object, return a list of the effective slots that
+;;; the class of the object had when the object was initialized or
+;;; reinitialized.
+(defgeneric standard-instance-slots (standard-object))
+
+;;; Update the effective slots of a standard object.  This function is
+;;; called by UPDATE-INSTANCE-FOR-REDEFINED-CLASS, and it sets the
+;;; slots of the object to the slots of the class of the object.
+(defgeneric (setf standard-instance-slots) (new-slots standard-instance))
+
+;;; Return the time stamp of a standard object.  The time stamp
+;;; reflects the time when the object was initialized or
+;;; reinitialized.
+(defgeneric standard-instance-timestamp (standard-object))
+
+;;; This function assigns a new timestamp to a standard object when
+;;; that object is initialized or reinitialized.
+(defgeneric (setf standard-instance-timestamp) (new-timestamp standard-object))
+
+;;; This function sets the name of the generic function.
+;;;
+;;; FIXME: this function is probably unnecessary because the way the
+;;; name of a class is modified is by (SETF GENERIC-FUNCTION-NAME)
+;;; calling REINITIALIZE-INSTANCE.
+(defgeneric (setf gf-name) (new-name generic-function))
+
+;;; This function sets the lambda list of the generic function.
 (defgeneric (setf gf-lambda-list) (new-lambda-list generic-function))
+
+;;; This function will be removed later.  For now it plays the role of
+;;; the function CL:DOCUMENTATION. 
 (defgeneric gf-documentation (generic-function))
-(defgeneric (setf gf-documentation) (generic-function))
+
+;;; This function will be removed later.  For now it plays the role of
+;;; the function (SETF CL:DOCUMENTATION). 
+(defgeneric (setf gf-documentation) (documentation generic-function))
+
+;;; This function returns the current discriminating function of the
+;;; generic function.
 (defgeneric discriminating-function (generic-function))
+
+;;; This functions sets the discriminating function of the generic
+;;; function.
 (defgeneric (setf discriminating-function)
     (new-discriminating-function generic-function))
-(defgeneric dependents (generic-function))
-(defgeneric (setf dependents) (new-dependents generic-function))
+
+;;; This function returns a list of the dependents of the metaobject.
+(defgeneric dependents (metaobject))
+
+;;; This function sets the list dependents of the metaobject.
+(defgeneric (setf dependents) (new-dependents metaobject))
+
+;;; This function sets the argument precedence order of the generic
+;;; function.
 (defgeneric (setf gf-argument-precedence-order)
     (new-argument-precedence-order generic-function))
+
+;;; This function sets the declarations of the generic function.
 (defgeneric (setf gf-declarations) (new-declarations generic-function))
+
+;;; This function sets the method class of the generic function.
 (defgeneric (setf gf-method-class) (new-method-class generic-function))
+
+;;; This function sets the method combination of the generic function.
 (defgeneric (setf gf-method-combination) (new-method-combination generic-function))
+
+;;; This function sets the methods of the generic function.
 (defgeneric (setf gf-methods) (new-methods generic-function))
+
+;;; This function returns the call history of the generic function.
+;;; FIXME: say more.
 (defgeneric call-history (generic-function))
+
+;;; This function sets the call history of the generic function.
+;;; FIXME: say more.
 (defgeneric (setf call-history) (new-call-history generic-function))
+
+;;; This function returns the specializer profile of the generic function.
+;;; FIXME: say more.
 (defgeneric specializer-profile (generic-function))
+
+;;; This function sets the specializer profile of the generic function.
+;;; FIXME: say more.
 (defgeneric (setf specializer-profile) (new-specializer-profile generic-function))
+
+;;; This function returns the cache of the generic function.
+;;; FIXME: say more.
 (defgeneric set-cache (generic-function))
+
+;;; This function sets the cache of the generic function.
+;;; FIXME: say more.
 (defgeneric (setf set-cache) (new-set-cache generic-function))
 
+;;; This function is called by ADD-METHOD and REMOVE-METHOD to assign
+;;; the generic function to which the method is associated.
+(defgeneric (setf m-generic-function) (generic-function method))
+
+;;; This function will be removed later.  For now it plays the role of
+;;; the function CL:DOCUMENTATION. 
+(defgeneric method-documentation (generic-function))
+
+;;; This function will be removed later.  For now it plays the role of
+;;; the function (SETF CL:DOCUMENTATION). 
+(defgeneric (setf method-documentation) (documentation generic-function))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;; Accessors for specializer metaobjects other than classes.
+
+;;; This function is called by ADD-DIRECT-METHOD and
+;;; REMOVE-DIRECT-METHOD so change the list of generic functions
+;;; having SPECIALIZER as a specializer.
+(defgeneric s-direct-generic-functions (new-generic-functions specializer))
+
+;;; This function is called by ADD-DIRECT-METHOD and
+;;; REMOVE-DIRECT-METHOD so change the list of methods having
+;;; SPECIALIZER as a specializer.
+(defgeneric s-direct-methods (new-methods specializer))
+
+;;; This function returns the unique number of the class, assigned
+;;; when the class was first created.
+(defgeneric unique-number (class))
+
+;;; This function sets the name of the class. 
+;;;
+;;; FIXME: this function is probably unnecessary because the way the
+;;; name of a class is modified is by having (SETF CLASS-NAME) call
+;;; REINITIALIZE-INSTANCE.
+(defgeneric (setf c-name) (new-name class))
+
+;;; This function sets the direct subclasses of the class.
+(defgeneric (setf c-direct-subclasses) (direct-subclasses class))
+
+;;; This function sets the direct methods of the class.
+(defgeneric (setf c-direct-methods) (direct-methods class))
+
+;;; For STANDARD-CLASS and FUNCALLABLE-STANDARD-CLASS, this function
+;;; returns the same value as CLASS-DIRECT-DEFAULT-INITARGS.  However,
+;;; whereas for BUILT-IN-CLASS CLASS-DIRECT-DEFAULT-INITARGS must
+;;; return the empty list according to the MOP, this function returns
+;;; the direct default initargs of the built-in class.
+(defgeneric direct-default-initargs (class))
+
+;;; For STANDARD-CLASS and FUNCALLABLE-STANDARD-CLASS, this function
+;;; returns the same value as CLASS-DIRECT-SLOTS.  However, whereas
+;;; for BUILT-IN-CLASS CLASS-DIRECT-SLOTS must return the empty list
+;;; according to the MOP, this function returns the direct default
+;;; slots of the built-in class.
+(defgeneric direct-slots (class))
+
+;;; This function will be removed later.  For now it plays the role of
+;;; the function CL:DOCUMENTATION. 
+(defgeneric c-documentation (generic-function))
+
+;;; This function will be removed later.  For now it plays the role of
+;;; the function (SETF CL:DOCUMENTATION). 
+(defgeneric (setf c-documentation) (documentation generic-function))
+
+;;; This function is used by the class finalization protocol to set
+;;; the flag in the class that indicates that it is finalized.
+(defgeneric (setf c-finalized-p) (new-value class))
+
+;;; This function is used by the class finalization protocol to set
+;;; the precedence list of the class.
+(defgeneric (setf c-precedence-list) (precedence-list class))
+
+;;; This function is used by the class finalization protocol to set
+;;; the default initargs of the class. 
+(defgeneric (setf c-default-initargs) (default-initargs class))
+
+;;; For STANDARD-CLASS and FUNCALLABLE-STANDARD-CLASS, this function
+;;; returns the same value as CLASS-SLOTS.  However, whereas for
+;;; BUILT-IN-CLASS CLASS-SLOTS must return the empty list according to
+;;; the MOP, this function returns the effective default slots of the
+;;; built-in class.
+(defgeneric effective-slots (class))
+
+;;; This function is used by the class finalization protocol to set
+;;; the effective slots of the class. 
+(defgeneric (setf c-slots) (effective-slots class))
+
+;;; This function sets the class prototype of the class.
+(defgeneric (setf c-prototype) (prototype class))
+
+;;; This function returns the dependents of the class.
+(defgeneric dependents (class))
+
+;;; This function sets the dependents of the class.
+(defgeneric (setf dependents) (dependents class))
+
+;;; For a slot with :ALLOCATION :CLASS, this function returns a CONS
+;;; cell where the CAR is used to store the value of the slot.
+(defgeneric slot-definition-storage (direct-slot-definition))
