63f9d5717751232273e1aa008d84fa4dc8ebf107
Robert Strandh <robert.strandh@gmail.com>
2014-01-30 15:39:32 +0100

    Exclude DEFGENERIC and have methods call default functions.

--- Code/CLOS/add-remove-direct-method.lisp
+++ Code/CLOS/add-remove-direct-method.lisp
@@ -1,20 +1,33 @@
 (cl:in-package #:sicl-clos)
 
-;;; REMOVE-DIRECT-METHOD
-(defgeneric remove-direct-method (specializer method))
+;;;; This file contains the definition of the specified methods on the
+;;;; generic functions ADD-DIRECT-METHOD and REMOVE-DIRECT-METHOD.
 
-(defmethod remove-direct-method ((specializer class) (method method))
-  (setf (specializer-direct-methods specializer)
-	(remove method (specializer-direct-methods specializer))))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;; Methods on generic function ADD-METHOD.
 
-(defmethod remove-direct-method ((specializer eql-specializer) (method method))
-  (setf (specializer-direct-methods specializer)
-	(remove method (specializer-direct-methods specializer))))
-
-;;; ADD-DIRECT-METHOD
+;;; For the specification of this method, see
+;;; http://metamodular.com/CLOS-MOP/add-direct-method-class.html
 (defmethod add-direct-method ((specializer class) (method method))
-  (pushnew method (specializer-direct-methods specializer)))
+  (add-direct-method-default specializer method))
 
+;;; For the specification of this method, see
+;;; http://metamodular.com/CLOS-MOP/add-direct-method-eql-specializer.html
 (defmethod add-direct-method ((specializer eql-specializer) (method method))
-  (pushnew method (specializer-direct-methods specializer)))
+  (add-direct-method-default specializer method))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;; Methods on generic function REMOVE-METHOD.
+
+;;; For the specification of this method, see
+;;; http://metamodular.com/CLOS-MOP/add-direct-method-eql-specializer.html
+(defmethod remove-direct-method ((specializer class) (method method))
+  (remove-direct-method-default specializer method))
+
+;;; For the specification of this method, see
+;;; http://metamodular.com/CLOS-MOP/add-direct-method-eql-specializer.html
+(defmethod remove-direct-method ((specializer eql-specializer) (method method))
+  (remove-direct-method-default specializer method))
 
