62dc68ed6b46761ddca3afea578e1bd341a31451
Robert Strandh <robert.strandh@gmail.com>
2014-01-30 16:18:04 +0100

    File with DEFUNS for COMPUTE-APPLICABLE-METHODS*.

---
+++ Code/CLOS/compute-applicable-methods-defuns.lisp
@@ -0,0 +1,13 @@
+(cl:in-package #:sicl-clos)
+
+;;;; This file contains defintions of COMPUTE-APPLICABLE-METHODS and
+;;;; COMPUTE-APPLICABLE-METHODS-USING-CLASSES as ordinary functions as
+;;;; opposed to generic functions.
+
+(defun compute-applicable-methods (generic-function arguments)
+  (compute-applicable-methods-default generic-function arguments))
+
+(defun compute-applicable-methods-using-classes
+    (generic-function classes-of-arguments)
+  (compute-applicable-methods-using-classes-default generic-function
+						    classes-of-arguments))
