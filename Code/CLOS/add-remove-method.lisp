28685148e3ec7c62094df5def1361988aa3f0585
Robert Strandh <robert.strandh@gmail.com>
2014-01-30 10:59:30 +0100

    Include only support code in the form of ordinary functions.

--- Code/CLOS/add-remove-method.lisp
+++ Code/CLOS/add-remove-method.lisp
@@ -1,45 +1,32 @@
 (cl:in-package #:sicl-clos)
 
-;;; REMOVE-METHOD
-(defgeneric remove-method (generic-function method))
+;;;; This file contains the support code for the generic functions
+;;;; ADD-METHOD and REMOVE-METHOD.
+;;;;
+;;;; In this file, there are no definitions of generic functions, nor
+;;;; of any methods.  
 
-(defmethod remove-method ((generic-function standard-generic-function)
-			  (method standard-method))
-  (setf (gf-methods generic-function)
-	(remove method (generic-function-methods generic-function)))
-  ;; The method is no longer associated with this generic funtion.
-  (setf (method-generic-function method) nil)
-  ;; Remove this method from the set of direct methods of its
-  ;; specializers.
-  (loop for specializer in (method-specializers method)
-	do (remove-direct-method specializer method))
-  ;; The specializer profile of the generic function must be updated.
-  (setf (specializer-profile generic-function)
-	(compute-specializer-profile
-	 (generic-function-methods generic-function)))
-  ;; Trash the call history, because it could be all wrong now. 
-  (setf (call-history generic-function) '())
-  (set-funcallable-instance-function
-   (compute-discriminating-function generic-function)
-   generic-function)
-  (map-dependents generic-function
-		  (lambda (dependent)
-		    (update-dependent generic-function
-				      dependent
-				      'remove-method
-				      method))))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;; Generic function ADD-METHOD.
+;;;
+;;; For the specification of this generic function, see
+;;; http://metamodular.com/CLOS-MOP/add-direct-subclass.html
+;;;
+;;; The specification includes a single method on this generic
+;;; function, specialized for STANDARD-GENERIC-FUNCTION and
+;;; STANDARD-METHOD.  The default action below is valid for that
+;;; method.
 
-;;; ADD-METHOD
-(defgeneric add-method (generic-function method))
-
-(defmethod add-method ((generic-function standard-generic-function)
-		       (method standard-method))
+(defun add-method-default (generic-function method)
   (unless (null (method-generic-function method))
     (error "method is already associated with a generic-function ~s"
 	   method))
   ;; FIXME: check that the lambda list are congruent
-  ;; See if there is a method with the same specializers and the
-  ;; same qualifiers.
+  ;;
+  ;; See if GENERIC-FUNCTION has a method with the same specializers
+  ;; and the same qualifiers as METHOD.  If such a method exists, it
+  ;; must be removed before this method is added.
   (let ((method-to-remove
 	  (find-if (lambda (existing-method)
 		     ;; They must have the same qualifiers
@@ -52,27 +39,55 @@
 		   (generic-function-methods generic-function))))
     (unless (null method-to-remove)
       (remove-method generic-function method-to-remove)))
-  ;; Associate the method with this generic function.
-  (setf (method-generic-function method) generic-function)
-  ;; Add this method to the set of direct methods of its
-  ;; specializers.
-  (loop for specializer in (method-specializers method)
-	do (add-direct-method specializer method))
   ;; Add this method to the set of methods of this generic function.
   (setf (gf-methods generic-function)
 	(cons method (generic-function-methods generic-function)))
-  ;; The specializer profile of the generic function must be updated.
-  (setf (specializer-profile generic-function)
-	(compute-specializer-profile
-	 (generic-function-methods generic-function)))
-  ;; Trash the call history, because it could be all wrong now. 
-  (setf (call-history generic-function) '())
-  (set-funcallable-instance-function
-   (compute-discriminating-function generic-function)
-   generic-function)
+  ;; Associate GENERIC-FUNCTION with METHOD.
+  (setf (m-generic-function method) generic-function)
+  ;; Call ADD-DIRECT-METHOD for each of the specializers of METHOD.
+  (loop for specializer in (method-specializers method)
+	do (add-direct-method specializer method))
+  ;; Call COMPUTE-DISCRIMINATING-FUNCTION and install its result
+  ;; with SET-FUNCALLABLE-INSTANCE-FUNCTION. 
+  (let ((df (compute-discriminating-function generic-function)))
+    (set-funcallable-instance-function generic-function df))
+  ;; Update the dependents of GENERIC-FUNCTION.
   (map-dependents generic-function
 		  (lambda (dependent)
 		    (update-dependent generic-function
 				      dependent
 				      'add-method
+				      method))))
+  
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;; Generic function REMOVE-METHOD.
+;;;
+;;; For the specification of this generic function, see
+;;; http://metamodular.com/CLOS-MOP/remove-direct-subclass.html
+;;;
+;;; The specification includes a single method on this generic
+;;; function, specialized for STANDARD-GENERIC-FUNCTION and
+;;; STANDARD-METHOD.  The default action below is valid for that
+;;; method.
+
+(defun remove-method-default (generic-function method)
+  ;; Remove METHOD from the methods of GENERIC-FUNCTION.
+  (setf (gf-methods generic-function)
+	(remove method (generic-function-methods generic-function)))
+  ;; Call REMOVE-DIRECT-METHOD for each of the specializers of METHOD.
+  (loop for specializer in (method-specializers method)
+	do (remove-direct-method specializer method))
+  ;; Disassociate GENERIC-FUNCTION from METHOD.
+  (setf (method-generic-function method) nil)
+  ;; Call COMPUTE-DISCRIMINATING-FUNCTION and install its result
+  ;; with SET-FUNCALLABLE-INSTANCE-FUNCTION. 
+  (let ((df (compute-discriminating-function generic-function)))
+    (set-funcallable-instance-function generic-function df))
+  ;; Update the dependents of GENERIC-FUNCTION.
+  (map-dependents generic-function
+		  (lambda (dependent)
+		    (update-dependent generic-function
+				      dependent
+				      'remove-method
 				      method))))
