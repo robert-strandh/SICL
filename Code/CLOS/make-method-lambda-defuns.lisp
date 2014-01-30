7181cfe13caeb94dc92314d36f2e281c79636489
Robert Strandh <robert.strandh@gmail.com>
2014-01-30 16:23:50 +0100

    File with DEFUN for MAKE-METHOD-LAMBDA.

---
+++ Code/CLOS/make-method-lambda-defuns.lisp
@@ -0,0 +1,8 @@
+(cl:in-package #:sicl-clos)
+
+(defun make-method-lambda
+    (generic-function method lambda-expression environment)
+  (make-method-lambda-default generic-function
+			      method
+			      lambda-expression
+			      environment))
