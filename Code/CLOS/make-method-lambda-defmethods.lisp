8de5a046d100d100690393996a408392455c7189
Robert Strandh <robert.strandh@gmail.com>
2014-01-30 15:52:09 +0100

    New files for MAKE-METHOD-LAMBDA.

---
+++ Code/CLOS/make-method-lambda-defmethods.lisp
@@ -0,0 +1,11 @@
+(cl:in-package #:sicl-clos)
+
+(defmethod make-method-lambda
+    ((generic-function standard-generic-function)
+     (method standard-method)
+     lambda-expression
+     environment)
+  (make-method-lambda-default generic-function
+			      method
+			      lambda-expression
+			      environment))
