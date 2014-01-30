cb1c54cd7e2f2ecb5976391d24327e9b952a4b20
Robert Strandh <robert.strandh@gmail.com>
2014-01-30 11:13:21 +0100

    New files for definitions of ADD-METHOD and REMOVE-METHOD.

---
+++ Code/CLOS/add-remove-method-defgenerics.lisp
@@ -0,0 +1,19 @@
+(cl:in-package #:sicl-clos)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;; Generic function ADD-METHOD.
+;;;
+;;; For the specification of this generic function, see
+;;; http://metamodular.com/CLOS-MOP/add-method.html
+
+(defgeneric add-method (generic-function method))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;; Generic function REMOVE-METHOD.
+;;;
+;;; For the specification of this generic function, see
+;;; http://metamodular.com/CLOS-MOP/remove-method.html
+
+(defgeneric remove-method (generic-function method))
