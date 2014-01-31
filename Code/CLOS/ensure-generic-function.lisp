490a40aaab75dac69f7b811960816b142dbc14f0
Robert Strandh <robert.strandh@gmail.com>
2014-01-31 15:17:18 +0100

    Added comment with URL to specification.

--- Code/CLOS/ensure-generic-function.lisp
+++ Code/CLOS/ensure-generic-function.lisp
@@ -1,5 +1,11 @@
 (cl:in-package #:sicl-clos)
 
+;;; For the specification of this function, see
+;;; http://metamodular.com/CLOS-MOP/ensure-generic-function.html
+;;;
+;;; This function is also specified in the CLHS.
+;;; http://www.lispworks.com/documentation/HyperSpec/Body/f_ensure.htm#ensure-generic-function
+
 (defun ensure-generic-function (name &rest keys)
   ;; FIXME: check if it names a non-generic function or macro already. 
   (let ((generic-function (find-generic-function name)))
