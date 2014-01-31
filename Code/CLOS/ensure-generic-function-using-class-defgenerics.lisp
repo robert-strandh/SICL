cb26b8458930b16dd3dfd0c64ac243741574898a
Robert Strandh <robert.strandh@gmail.com>
2014-01-31 15:16:40 +0100

    New files for ENSURE-GENERIC-FUNCTION-USING-CLASS.

---
+++ Code/CLOS/ensure-generic-function-using-class-defgenerics.lisp
@@ -0,0 +1,17 @@
+(cl:in-package #:sicl-clos)
+
+;;; For the specification of this generic function, see
+;;; http://metamodular.com/CLOS-MOP/ensure-generic-function-using-class.html
+(defgeneric ensure-generic-function-using-class
+    (generic-function
+     function-name
+     &key
+       argument-precedence-order
+       declarations
+       documentation
+       generic-function-class
+       lambda-list
+       method-class
+       method-combination
+       name
+     &allow-other-keys))
---
+++ Code/CLOS/ensure-generic-function-using-class-defmethods.lisp
@@ -0,0 +1,31 @@
+(cl:in-package #:sicl-clos)
+
+(defmethod ensure-generic-function-using-class
+    ((generic-function null)
+     function-name
+     &rest
+       all-keyword-arguments
+     &key
+       generic-function-class
+       method-class
+     &allow-other-keys)
+  (declare (ignore generic-function-class method-class))
+  (apply #'ensure-generic-function-using-class-null
+	 generic-function
+	 function-name
+	 all-keyword-arguments))
+
+(defmethod ensure-generic-function-using-class
+    ((generic-function generic-function)
+     function-name
+     &rest
+       all-keyword-arguments
+     &key
+       generic-function-class
+       method-class
+     &allow-other-keys)
+  (declare (ignore generic-function-class method-class))
+  (apply #'ensure-generic-function-using-class-generic-function
+	 generic-function
+	 function-name
+	 all-keyword-arguments))
