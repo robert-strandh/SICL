d7f1644165beca5631d9dd720d4296fdf115602f
Robert Strandh <robert.strandh@gmail.com>
2014-01-31 11:18:13 +0100

    Moved body of methods to default orginary functions.

--- Code/CLOS/defmethod-initialize-instance-after-standard-generic-function.lisp
+++ Code/CLOS/defmethod-initialize-instance-after-standard-generic-function.lisp
@@ -5,64 +5,44 @@
 
 (defmethod initialize-instance :after
     ((generic-function standard-generic-function)
+     &rest initargs
      &key
-       (lambda-list nil lambda-list-p)
-       (argument-precedence-order nil argument-precedence-order-p)
+       lambda-list
+       argument-precedence-order
        documentation
        declarations
        method-combination
-       (method-class (find-method-class 'standard-method))
-       (name nil)
+       method-class
+       name nil
      &allow-other-keys)
-  ;; FIXME: handle different method combinations.
-  (declare (ignore method-combination))
-  (set-documentation generic-function documentation)
-  (set-declarations generic-function declarations)
-  (set-method-class generic-function method-class)
-  (set-lambda-list-and-argument-precedence-order
-   generic-function
-   lambda-list
-   lambda-list-p
-   argument-precedence-order
-   argument-precedence-order-p)
-  (let ((fun (lambda (&rest args)
-	       (declare (ignore args))
-	       (error "no applicable methods"))))
-    (setf (discriminating-function generic-function) fun)
-    (setf (gf-name generic-function) name)
-    (unless (null name)
-      (set-funcallable-instance-function fun generic-function))))
+  (declare (ignore lambda-list
+		   argument-precedence-order
+		   documentation
+		   declarations
+		   method-combination
+		   method-class
+		   name))
+  (apply #'initialize-instance-after-standard-generic-function-default
+	 generic-function initargs))
 
 (defmethod reinitialize-instance :after
     ((generic-function standard-generic-function)
+     &rest initargs
      &key
-       (lambda-list nil lambda-list-p)
-       (argument-precedence-order nil argument-precedence-order-p)
-       (documentation nil documentation-p)
-       (declarations nil declarations-p)
+       lambda-list
+       argument-precedence-order
+       documentation
+       declarations
        method-combination
-       (method-class nil method-class-p)
-       (name nil name-p)  ; FIXME: check if this belongs here.
+       method-class
+       name ; FIXME: check if this belongs here.
      &allow-other-keys)
-  ;; FIXME: handle different method combinations.
-  (declare (ignore method-combination))
-  (when documentation-p
-    (set-documentation generic-function documentation))
-  (when declarations-p
-    (set-declarations generic-function declarations))
-  (when method-class-p
-    (set-method-class generic-function method-class))
-  (when name-p
-    (setf (gf-name generic-function) name))
-  (set-lambda-list-and-argument-precedence-order
-   generic-function
-   lambda-list
-   lambda-list-p
-   argument-precedence-order
-   argument-precedence-order-p)
-  (map-dependents class
-		  (lambda (dependent)
-		    (apply #'update-dependent class dependent args))))
-
-
-  
+  (declare (ignore lambda-list
+		   argument-precedence-order
+		   documentation
+		   declarations
+		   method-combination
+		   method-class
+		   name))
+  (apply #'reinitialize-instance-after-standard-generic-function-default
+	 generic-function initargs))
--- Code/CLOS/generic-function-initialization.lisp
+++ Code/CLOS/generic-function-initialization.lisp
@@ -49,3 +49,63 @@
   (setf (gf-method-class generic-function)
 	method-class))
 
+(defun initialize-instance-after-standard-generic-function-default
+    (generic-function
+     &key
+       (lambda-list nil lambda-list-p)
+       (argument-precedence-order nil argument-precedence-order-p)
+       documentation
+       declarations
+       method-combination
+       (method-class (find-method-class 'standard-method))
+       (name nil)
+     &allow-other-keys)
+  ;; FIXME: handle different method combinations.
+  (declare (ignore method-combination))
+  (set-documentation generic-function documentation)
+  (set-declarations generic-function declarations)
+  (set-method-class generic-function method-class)
+  (set-lambda-list-and-argument-precedence-order
+   generic-function
+   lambda-list
+   lambda-list-p
+   argument-precedence-order
+   argument-precedence-order-p)
+  (let ((fun (lambda (&rest args)
+	       (declare (ignore args))
+	       (error "no applicable methods"))))
+    (setf (discriminating-function generic-function) fun)
+    (setf (gf-name generic-function) name)
+    (unless (null name)
+      (set-funcallable-instance-function fun generic-function))))
+
+(defun reinitialize-instance-after-standard-generic-function-default
+    (generic-function
+     &key
+       (lambda-list nil lambda-list-p)
+       (argument-precedence-order nil argument-precedence-order-p)
+       (documentation nil documentation-p)
+       (declarations nil declarations-p)
+       method-combination
+       (method-class nil method-class-p)
+       (name nil name-p)  ; FIXME: check if this belongs here.
+     &allow-other-keys)
+  ;; FIXME: handle different method combinations.
+  (declare (ignore method-combination))
+  (when documentation-p
+    (set-documentation generic-function documentation))
+  (when declarations-p
+    (set-declarations generic-function declarations))
+  (when method-class-p
+    (set-method-class generic-function method-class))
+  (when name-p
+    (setf (gf-name generic-function) name))
+  (set-lambda-list-and-argument-precedence-order
+   generic-function
+   lambda-list
+   lambda-list-p
+   argument-precedence-order
+   argument-precedence-order-p)
+  (map-dependents class
+		  (lambda (dependent)
+		    (apply #'update-dependent class dependent args))))
