a63bc8680a1d109bdec0f43a10ccacfb31e09d15
Robert Strandh <robert.strandh@gmail.com>
2014-02-01 10:24:16 +0100

    Replaced incorrect variable by correct one.

--- Code/CLOS/dependent-maintenance-support.lisp
+++ Code/CLOS/dependent-maintenance-support.lisp
@@ -4,8 +4,8 @@
   (pushnew dependent (dependents metaobject) :test #'eq))
 
 (defun remove-dependent-default (metaobject dependent)
-  (setf (dependents obj)
-	(remove dependent (dependents obj) :test #'eq)))
+  (setf (dependents metaobject)
+	(remove dependent (dependents metaobject) :test #'eq)))
 
 (defun map-dependents-default (metaobject function)
   (mapc function (dependents metaobject)))
