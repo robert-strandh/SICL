477a5d3bee1e2c8fdd1da314347ac08a106a1eb7
Robert Strandh <robert.strandh@gmail.com>
2014-02-01 12:59:13 +0100

    New writer (SETF SLOT-DEFINITION).

--- Code/CLOS/accessor-defgenerics.lisp
+++ Code/CLOS/accessor-defgenerics.lisp
@@ -374,3 +374,5 @@
 ;;; For a slot with :ALLOCATION :CLASS, this function returns a CONS
 ;;; cell where the CAR is used to store the value of the slot.
 (defgeneric slot-definition-storage (direct-slot-definition))
+
+(defgeneric (setf slot-definition) (slot-definition accessor-method))
