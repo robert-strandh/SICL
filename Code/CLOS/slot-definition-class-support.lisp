746cbbcac24a8b270b9f517608b127b7af1dbd01
Robert Strandh <robert.strandh@gmail.com>
2014-02-02 11:43:46 +0100

    Declare unused variable IGNORE.

--- Code/CLOS/slot-definition-class-support.lisp
+++ Code/CLOS/slot-definition-class-support.lisp
@@ -1,9 +1,9 @@
 (cl:in-package #:sicl-clos)
 
 (defun direct-slot-definition-class-default (class &rest initargs)
-  (declare (ignore initargs))
+  (declare (ignore class initargs))
   *standard-direct-slot-definition*)
 
 (defun effective-slot-definition-class-default (class &rest initargs)
-  (declare (ignore initargs))
+  (declare (ignore class initargs))
   *standard-effective-slot-definition*)
