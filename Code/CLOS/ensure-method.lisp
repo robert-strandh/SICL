b26444b8148cb3e6a4a80f8d526161b8d5895256
Robert Strandh <robert.strandh@gmail.com>
2014-02-03 18:48:36 +0100

    Handle the specializers a bit better.

--- Code/CLOS/ensure-method.lisp
+++ Code/CLOS/ensure-method.lisp
@@ -6,9 +6,19 @@
 ;;;
 ;;; This function is not required by the AMOP to exist, 
 
-(defun ensure-method (generic-function &rest keys)
-  (let ((method (apply #'make-instance
-		       'standard-method
-		       keys)))
-    (add-method generic-function method)
-    method))
+(defun ensure-method (generic-function
+		      &rest keys
+		      &key specializers
+		      &allow-other-keys)
+  ;; FIXME: Check that SPECIALIZERS is a proper list, and that
+  ;; it contains only symbols and specializer metaobjects. 
+  (let ((specs (loop for s in specializer
+		     collect (if (symbolp s) (find-class s) s)))
+	(remaining-keys (copy-list keys)))
+    (loop while (remf remaining-keys :specializers))
+    (let ((method (apply #'make-instance
+			 'standard-method
+			 :specializers specs
+			 remaining-keys)))
+      (add-method generic-function method)
+      method)))
