398aa220f7bb70194ca272a9031f289342fb6cd3
Robert Strandh <robert.strandh@gmail.com>
2014-02-03 12:16:52 +0100

    Declare unused parameters IGNORE.

--- Code/CLOS/reader-writer-method-class-support.lisp
+++ Code/CLOS/reader-writer-method-class-support.lisp
@@ -1,10 +1,10 @@
 (cl:in-package #:sicl-clos)
 
 (defun reader-method-class-default (class direct-slot &rest initargs)
-  (declare (ignore class initargs))
+  (declare (ignore class direct-slot initargs))
   *standard-reader-method*)
 
 (defun writer-method-class-default (class direct-slot &rest initargs)
-  (declare (ignore class initargs))
+  (declare (ignore class direct-slot initargs))
   *standard-writer-method*)
 
