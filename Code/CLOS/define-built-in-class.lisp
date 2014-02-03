034b3afc4f83a0bb8aab9943caacfa8b026ef426
Robert Strandh <robert.strandh@gmail.com>
2014-02-03 15:30:32 +0100

    Allow for class options, just as for DEFCLASS.

--- Code/CLOS/define-built-in-class.lisp
+++ Code/CLOS/define-built-in-class.lisp
@@ -1,9 +1,13 @@
 (cl:in-package #:sicl-clos)
 
-(defmacro define-built-in-class (name superclass-names slot-specifiers)
+(defmacro define-built-in-class (name
+				 superclass-names
+				 slot-specifiers
+				 &rest options)
   `(ensure-built-in-class
     ',name
     :direct-superclasses 
     ,(canonicalize-direct-superclass-names superclass-names)
     :direct-slots
-    ,(canonicalize-direct-slot-specs slot-specifiers)))
+    ,(canonicalize-direct-slot-specs slot-specifiers)
+    ,@(canonicalize-defclass-options options)))))
