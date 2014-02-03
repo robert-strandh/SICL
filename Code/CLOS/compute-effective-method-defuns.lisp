994176ff42fecff0de56611d531a81af29d5e2f8
Robert Strandh <robert.strandh@gmail.com>
2014-02-03 12:16:16 +0100

    Fixed call to default action.

--- Code/CLOS/compute-effective-method-defuns.lisp
+++ Code/CLOS/compute-effective-method-defuns.lisp
@@ -1,6 +1,5 @@
 (cl:in-package #:sicl-clos)
 
 (defun compute-effective-method (generic-function method-combination methods)
-  (compute-effective-method-default generic-function
-				    method-combination
-				    methods))
+  (declare (ignore generic-function method-combination))
+  (compute-effective-method-default-2 methods))
