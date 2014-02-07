2fd9b36e14780801703602a2850ffd3377ccc777
Robert Strandh <robert.strandh@gmail.com>
2014-02-07 09:09:02 +0100

    Use ...-USING-CLASS versions of slot functions.

--- Code/CLOS/initialize-support.lisp
+++ Code/CLOS/initialize-support.lisp
@@ -25,9 +25,18 @@
 ;;; does not already have a value.  In that case, if no initform
 ;;; exists, the slot is left unbound.
 
+;;; We do not want to use SLOT-BOUNDP (SETF SLOT-VALUE) here.  Given
+;;; the name of a slot, those functions find a slot metaobject with
+;;; that name, and then call the ...-USING-CLASS version of the
+;;; function.  The main difficulty for those functions is to find the
+;;; slot metaobject.  In SHARED-INITIALIZE, we already have the slot
+;;; metaobject available, so we use the ...-USING-CLASS version
+;;; directly.
+
 (defun shared-initialize-default
     (instance slot-names &rest initargs)
-  (let ((class-slots (class-slots (class-of instance))))
+  (let* ((class (class-of instance))
+	 (class-slots (class-slots class)))
     (loop for slot in class-slots
 	  for slot-name = (slot-definition-name slot)
 	  do (multiple-value-bind (key value foundp)
@@ -36,16 +45,17 @@
 		 (get-properties initargs (slot-definition-initargs slot))
 	       (declare (ignore key))
 	       (if foundp
-		   ;; Found an explicit initarg in initargs.  Initialize
-		   ;; the slot from its value
-		   (setf (slot-value instance slot-name) value)
+		   ;; Found an explicit initarg in initargs.
+		   ;; Initialize the slot from its value.
+		   (setf (slot-value-using-class class instance slot)
+			 value)
 		   ;; No explicit initarg found.  
-		   (when (and (not (slot-boundp instance slot-name))
+		   (when (and (not (slot-boundp-using-class class instance slot))
 			      (not (null (slot-definition-initfunction slot)))
 			      (or (eq slot-names t)
 				  (member slot-name slot-names)))
 		     ;; Evaluate the initform by executing the initfunction. 
-		     (setf (slot-value instance slot-name)
+		     (setf (slot-value-using-class class instance slot)
 			   (funcall (slot-definition-initfunction slot)))))))
     ;; Store the class slots in the instance so that we can update the instance
     ;; when the class changes.
