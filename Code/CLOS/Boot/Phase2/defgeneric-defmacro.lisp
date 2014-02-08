7f238f989d346958816c1a64dbf02b87feb85c2e
Robert Strandh <robert.strandh@gmail.com>
2014-02-08 06:13:08 +0100

    Count on CL:NO-APPLICABLE-METHOD to call our discriminating function.

--- Code/CLOS/Boot/Phase2/defgeneric-defmacro.lisp
+++ Code/CLOS/Boot/Phase2/defgeneric-defmacro.lisp
@@ -4,7 +4,4 @@
   `(progn
      (let ((fun (cl:make-instance 'bridge-generic-function
 		  :name ',name :lambda-list ',parameters)))
-       (setf (fdefinition 'temporary-name) fun)
-       (cl:defmethod temporary-name ,parameters
-	 (funcall (discriminating-function fun) ,@parameters))
        (push (cons ',name fun) *bridge-generic-functions*))))
