4e745777a7eb25ac2c2eb6c71b5a63d0d2958dc0
Robert Strandh <robert.strandh@gmail.com>
2014-02-09 07:02:26 +0100

    Allocate additional space for class unique number and class slots.
    Since the unique number of the class and the class slots of the class
    are no longer treated as slots, we need to handle them specially by
    allocating additional space for them in the contents vector.

--- Code/CLOS/allocate-instance-support.lisp
+++ Code/CLOS/allocate-instance-support.lisp
@@ -1,11 +1,21 @@
 (cl:in-package #:sicl-clos)
 
+;;;; This function implements the action of the default methods on
+;;;; ALLOCATE-INSTANCE, i.e., the methods specialized for
+;;;; STANDARD-CLASS and FUNCALLABLE-STANDARD-CLASS.  Every instance of
+;;;; these classes has two initial cells (the unique number of the
+;;;; class, and the list of effective slots of the class) in the
+;;;; contents vector.  These cells are not counted among the slots,
+;;;; because they are accessed directly, using offsets.  For that
+;;;; reason, we must allocate more slot storage than there are slots
+;;;; with :INSTANCE allocation.
+
 (defun allocate-instance-default (class &rest initargs)
   (declare (ignore initargs))
   (unless (class-finalized-p class)
     (finalize-inheritance class))
   (let* ((slots (class-slots class))
-	 (instance-size (count :instance slots
+	 (slot-count (count :instance slots
 			       :key #'slot-definition-allocation))
-	 (slots (allocate-slot-storage instance-size)))
+	 (slots (allocate-slot-storage (+ slot-count 2))))
     (allocate-heap-instance class slots)))
