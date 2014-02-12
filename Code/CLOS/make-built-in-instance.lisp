90affbff6855b4ab30b10184dbe47e1227ec0979
Robert Strandh <robert.strandh@gmail.com>
2014-02-12 11:05:57 +0100

    Use reader that works on built-in classes.  Added comment to explain.

--- Code/CLOS/make-built-in-instance.lisp
+++ Code/CLOS/make-built-in-instance.lisp
@@ -1,8 +1,16 @@
 (cl:in-package #:sicl-clos)
 
+;;; We use the reader DEFAULT-INITARGS rather than
+;;; CLASS-DEFAULT-INITARGS, because the AMOP stipulates that
+;;; CLASS-DEFAULT-INITARGS must return the empty list for built-in
+;;; classes.  However, our built-in classes do have default initargs.
+;;; The solution to this problem is to use a different reader named
+;;; DEFAULT-INITARGS.  This reader works for standard classes and
+;;; funcallable standard classes as well.
+
 (defun make-built-in-instance (class &rest initargs)
   (let ((defaulted-initargs initargs))
-    (loop for default-initarg in (class-default-initargs class)
+    (loop for default-initarg in (default-initargs class)
 	  do (unless (initarg-in-list-p (car default-initarg) initargs)
 	       (setf defaulted-initargs
 		     (append defaulted-initargs
@@ -11,4 +19,3 @@
     (let ((instance (apply #'allocate-built-in-instance class defaulted-initargs)))
       (apply #'initialize-built-in-instance instance defaulted-initargs)
       instance)))
-
