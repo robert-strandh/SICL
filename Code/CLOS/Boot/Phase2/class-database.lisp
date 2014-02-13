b3c7231dd1bba39982665a9aec704e662d3a4dfd
Robert Strandh <robert.strandh@gmail.com>
2014-02-13 06:17:38 +0100

    Introduced abstractions for manipulating the database of bridge classes.
    These abstractions are mainly useful for debugging purposes, so that
    we can get proper error messages when there is an attempt to find a
    class that does not exist, rather than propagating NIL until something
    fails further down the road.

--- Code/CLOS/Boot/Phase2/class-database.lisp
+++ Code/CLOS/Boot/Phase2/class-database.lisp
@@ -1,3 +1,18 @@
 (cl:in-package #:sicl-clos)
 
 (defparameter *bridge-classes* '())
+
+(defun add-bridge-class (name function)
+  (when (member name *bridge-classes* :key #'car :test #'equal)
+    (error "Attempt to define bridge generic function ~s twice." name))
+  (push (cons name function) *bridge-classes*))
+
+(defun find-bridge-class (name)
+  (let ((entry (assoc name *bridge-classes* :test #'equal)))
+    (when (null entry)
+      (error "There is no bridge generic function named ~s." name))
+    (cdr entry)))
+
+(defun delete-bridge-class (name)
+  (setf *bridge-classes*
+	(remove name *bridge-classes* :key #'car :test #'equal)))
