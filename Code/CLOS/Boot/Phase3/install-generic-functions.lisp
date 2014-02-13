f6a34630f947d478f29531471a406684bd19de60
Robert Strandh <robert.strandh@gmail.com>
2014-02-13 16:17:38 +0100

    Added to the list of generic functions that must be installed in phase 3.

--- Code/CLOS/Boot/Phase3/install-generic-functions.lisp
+++ Code/CLOS/Boot/Phase3/install-generic-functions.lisp
@@ -1,7 +1,7 @@
 (cl:in-package #:sicl-clos)
 
 (defparameter *installable-generic-functions*
-  '(;; These are used by the :AFTER method on initialize-instance
+  '(;; These are used by the :AFTER method on INITIALIZE-INSTANCE
     ;; specialized for STANDARD-CLASS.
     class-direct-subclasses
     (setf c-direct-subclasses)
@@ -11,7 +11,7 @@
     (setf c-direct-slots)
     class-direct-default-initargs
     (setf c-direct-default-initargs)
-    ;; These are used by the :AFTER method on initialize-instance,
+    ;; These are used by the :AFTER method on INITIALIZE-INSTANCE,
     ;; specialized for STANDARD-GENERIC-FUNCTION.
     (setf gf-documentation)
     (setf gf-declarations)
@@ -21,7 +21,20 @@
     (setf gf-argument-precedence-order)
     (setf discriminating-function)
     (setf gf-name)
-    ;; These are used by the :AFTER method on initialize-instance,
+    ;; These are used by ADD-DIRECT-METHOD which is called by
+    ;; ADD-METHOD, which is called indirectly by the :AFTER method on
+    ;; INITIALIZE-INSTANCE, specialized to STANDARD-CLASS in order to
+    ;; install readers and writers for slots. 
+    specializer-direct-methods
+    (setf s-direct-methods)
+    ;; These are used directly by ADD-METHOD in order to add a method
+    ;; to the methods of a generic function.
+    ;; generic-function-methods
+    (setf gf-methods)
+    ;; This one is used directly by ADD-METHOD in order to store a
+    ;; back pointer from the method to its generic function.
+    (setf m-generic-function)
+    ;; These are used by the :AFTER method on INITIALIZE-INSTANCE,
     ;; specialized for STANDARD-METHOD
     (setf method-documentation)
     ;; Generic functions for initialization
