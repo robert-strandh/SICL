1773e5e9aa9df6738017e0133791c0141e7fa3b7
Robert Strandh <robert.strandh@gmail.com>
2014-02-13 06:19:50 +0100

    Introduced abstractions for manipulating database of bridge generic functions.
    For the same reason as for bridge classes, in order to simplify
    debugging, we need proper error messages when there is an attempt to
    find a bridge generic function that does not exist.

--- Code/CLOS/Boot/Phase2/generic-function-database.lisp
+++ Code/CLOS/Boot/Phase2/generic-function-database.lisp
@@ -1,3 +1,17 @@
 (cl:in-package #:sicl-clos)
 
 (defparameter *bridge-generic-functions* '())
+
+(defun add-bridge-generic-function (name function)
+  (pushnew (cons name function) *bridge-generic-functions*
+	   :key #'car :test #'equal))
+
+(defun find-bridge-generic-function (name)
+  (let ((entry (assoc name *bridge-generic-functions* :test #'equal)))
+    (when (null entry)
+      (error "There is no bridge generic function named ~s." name))
+    (cdr entry)))
+
+(defun delete-bridge-generic-function (name)
+  (setf *bridge-generic-functions*
+	(remove name *bridge-generic-functions* :key #'car :test #'equal)))
