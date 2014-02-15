6eb193f95ff9118b97b56f353fd450c0143e19d3
Robert Strandh <robert.strandh@gmail.com>
2014-02-15 11:26:34 +0100

    Abstract the manipulation of the database of target classes.

--- Code/CLOS/Boot/Phase3/class-database.lisp
+++ Code/CLOS/Boot/Phase3/class-database.lisp
@@ -1,3 +1,18 @@
 (cl:in-package #:sicl-clos)
 
 (defparameter *target-classes* '())
+
+(defun add-target-class (name function)
+  (when (member name *target-classes* :key #'car :test #'equal)
+    (error "Attempt to define target generic function ~s twice." name))
+  (push (cons name function) *target-classes*))
+
+(defun find-target-class (name)
+  (let ((entry (assoc name *target-classes* :test #'equal)))
+    (when (null entry)
+      (error "There is no target generic function named ~s." name))
+    (cdr entry)))
+
+(defun delete-target-class (name)
+  (setf *target-classes*
+	(remove name *target-classes* :key #'car :test #'equal)))
