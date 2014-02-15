d738fbffb0e1a5267d257b4b2c3184466310b774
Robert Strandh <robert.strandh@gmail.com>
2014-02-15 20:20:26 +0100

    Define abstractions for access to target GF database.

--- Code/CLOS/Boot/Phase3/generic-function-database.lisp
+++ Code/CLOS/Boot/Phase3/generic-function-database.lisp
@@ -1,3 +1,17 @@
 (cl:in-package #:sicl-clos)
 
 (defparameter *target-generic-functions* '())
+
+(defun add-target-generic-function (name function)
+  (pushnew (cons name function) *target-generic-functions*
+	   :key #'car :test #'equal))
+
+(defun find-target-generic-function (name)
+  (let ((entry (assoc name *target-generic-functions* :test #'equal)))
+    (when (null entry)
+      (error "There is no target generic function named ~s." name))
+    (cdr entry)))
+
+(defun delete-target-generic-function (name)
+  (setf *target-generic-functions*
+	(remove name *target-generic-functions* :key #'car :test #'equal)))
