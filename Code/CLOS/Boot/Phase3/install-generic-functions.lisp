151387da957d8f03f616a22fdf94c823bd06482f
Robert Strandh <robert.strandh@gmail.com>
2014-02-16 09:09:28 +0100

    Removed recently added reader M-SPECIALIZERS.  Looking for different solution.

--- Code/CLOS/Boot/Phase3/install-generic-functions.lisp
+++ Code/CLOS/Boot/Phase3/install-generic-functions.lisp
@@ -37,9 +37,6 @@
     ;; to the methods of a generic function.
     gf-methods
     (setf gf-methods)
-    ;; This one is used by ADD-METHOD to call ADD-DIRECT-METHOD passing
-    ;; it the specializer.
-    m-specializers
     ;; These are used directly by ADD-METHOD in order to store a back
     ;; pointer from the method to its generic function and to check
     ;; that the method is not currently on a generic function.
