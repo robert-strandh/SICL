8c654024117f50cabcb9fd7d79ebe5d7fd609b73
Robert Strandh <robert.strandh@gmail.com>
2014-02-17 16:03:44 +0100

    Code factoring using new class REGULAR-CLASS.

--- Code/CLOS/class-finalization-defmethods.lisp
+++ Code/CLOS/class-finalization-defmethods.lisp
@@ -1,32 +1,19 @@
 (cl:in-package #:sicl-clos)
 
-(defmethod finalize-inheritance ((class standard-class))
+(defmethod finalize-inheritance ((class regular-class))
   (finalize-inheritance-default class))
 
-(defmethod finalize-inheritance ((class funcallable-standard-class))
-  (finalize-inheritance-default class))
-
-(defmethod compute-default-initargs ((class standard-class))
+(defmethod compute-default-initargs ((class regular-class))
   (compute-default-initargs-default class))
 
-(defmethod compute-default-initargs ((class funcallable-standard-class))
-  (compute-default-initargs-default class))
-
-(defmethod compute-slots :around ((class standard-class))
+(defmethod compute-slots :around ((class regular-class))
   (let ((slots (call-next-method)))
     (compute-slots-around-default slots)))
 
-(defmethod compute-slots :around ((class funcallable-standard-class))
-  (let ((slots (call-next-method)))
-    (compute-slots-around-default slots)))
-
-(defmethod compute-slots ((class standard-class))
+(defmethod compute-slots ((class regular-class))
   (compute-slots-default class))
 
-(defmethod compute-slots ((class funcallable-standard-class))
-  (compute-slots-default class))
-
-(defmethod compute-effective-slot-definition ((class standard-class)
+(defmethod compute-effective-slot-definition ((class regular-class)
 					      name
 					      direct-slot-definitions)
   (compute-effective-slot-definition-default
@@ -34,13 +21,5 @@
    direct-slot-definitions
    (effective-slot-definition-class class)))
 
-(defmethod compute-effective-slot-definition ((class funcallable-standard-class)
-					      name
-					      direct-slot-definitions)
-  (compute-effective-slot-definition-default
-   name
-   direct-slot-definitions
-   (effective-slot-definition-class class)))
-  
 (defmethod compute-class-precedence-list ((class class))
   (compute-class-precedence-list-default class))
