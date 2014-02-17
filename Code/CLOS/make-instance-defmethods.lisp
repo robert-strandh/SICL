6142645a86fae4eaa596f9cc8d6fc323541e7ddd
Robert Strandh <robert.strandh@gmail.com>
2014-02-17 16:16:57 +0100

    Specialize the method on MAKE-INSTANCE to REGULAR-CLASS.

--- Code/CLOS/make-instance-defmethods.lisp
+++ Code/CLOS/make-instance-defmethods.lisp
@@ -3,27 +3,25 @@
 (defmethod make-instance ((class symbol) &rest initargs)
   (apply #'make-instance (find-class class) initargs))
 
-
-(defmethod make-instance ((class standard-class) &rest initargs)
-  (apply #'make-instance-default class initargs))
-
 ;;; The HyperSpec only recognizes methods on MAKE-INSTANCE specialized
-;;; for SYMBOL and STANDARD-CLASS.  However, the AMOP clearly says
+;;; to SYMBOL and STANDARD-CLASS.  However, the AMOP clearly says
 ;;; that ENSURE-GENERIC-FUNCTION calls MAKE-INSTANCE, and the default
 ;;; class that is instantiated is STANDARD-GENERIC-FUNCTION.  But
 ;;; STANDARD-GENERIC-FUNCTION is not and instance of STANDARD-CLASS,
 ;;; and instead of FUNCALLABLE-STANDARD-CLASS.
 ;;;
 ;;; We have two possible ways of solving this conflict.  Way number 1
-;;; (which is what we do here) is to define another method on
-;;; MAKE-INSTANCE, in violation of the HyperSpec.  Way number 2 would
-;;; be for ENSURE-GENERIC-FUNCTION to call some other function to
-;;; instantiate the generic function class, in violation of the AMOP.
+;;; is to specialize the method on MAKE-INSTANCE to some superclass
+;;; common to STANDARD-CLASS and FUNCALLABLE-STANDARD-CLASS.  Way
+;;; number 2 would be for ENSURE-GENERIC-FUNCTION to call some other
+;;; function to instantiate the generic function class, in violation
+;;; of the AMOP.
 ;;;
-;;; SBCL "solves" this conflict by defining two methods on
-;;; make-instance, specialized for SYMBOL and CLASS, again in
-;;; violation of the HyperSpec.  So for instance, it is possible to
-;;; call (MAKE-INSTANCE 'SYMBOL) in SBCL.  It does not
+;;; We choose way number 1 and specialize the method to REGULAR-CLASS,
+;;; which is the most special superclass of STANDARD-CLASS and
+;;; FUNCALLABLE-STANDARD-CLASS.  SBCL uses the same solution, except
+;;; that the method is specialized to CLASS.  So for instance, it is
+;;; possible to call (MAKE-INSTANCE 'SYMBOL) in SBCL.  It does not
 ;;; complain in MAKE-INSTANCE, but in ALLOCATE-INSTANCE.
-(defmethod make-instance ((class funcallable-standard-class) &rest initargs)
+(defmethod make-instance ((class regular-class) &rest initargs)
   (apply #'make-instance-default class initargs))
