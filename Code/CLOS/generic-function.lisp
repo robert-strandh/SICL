3ef0d22476fa945e8f8683f3bbb377a74028408a
Robert Strandh <robert.strandh@gmail.com>
2014-02-22 06:03:49 +0100

    Moved lambda list extraction functions to a separate file.

--- Code/CLOS/generic-function.lisp
+++ Code/CLOS/generic-function.lisp
@@ -63,31 +63,6 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;
-;;; Lambda list functions.
-;;;
-;;; FIXME: check syntax of lambda list (but not of specializers) in
-;;; both these functions according to the MOP.
-
-(defun extract-lambda-list (specialized-lambda-list)
-  (loop for rest = specialized-lambda-list then (cdr rest)
-	until (or (atom rest)
-		  (member (car rest) lambda-list-keywords))
-	collect (if (consp (car rest))
-		    (caar rest)
-		    (car rest))
-	  into required
-	finally (return (append required rest))))
-
-(defun extract-specializer-names (specialized-lambda-list)
-  (loop for rest = specialized-lambda-list then (cdr rest)
-	until (or (atom rest)
-		  (member (car rest) lambda-list-keywords))
-	collect (if (consp (car rest))
-		    (cadar rest)
-		    t)))
-  
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;;
 ;;; Effective method automaton.
 
 ;;; To determine the effective method to run from the classes of the
---
+++ Code/CLOS/lambda-list-functions.lisp
@@ -0,0 +1,26 @@
+(cl:in-package #:sicl-clos)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;; Lambda list functions.
+;;;
+;;; FIXME: check syntax of lambda list (but not of specializers) in
+;;; both these functions according to the MOP.
+
+(defun extract-lambda-list (specialized-lambda-list)
+  (loop for rest = specialized-lambda-list then (cdr rest)
+	until (or (atom rest)
+		  (member (car rest) lambda-list-keywords))
+	collect (if (consp (car rest))
+		    (caar rest)
+		    (car rest))
+	  into required
+	finally (return (append required rest))))
+
+(defun extract-specializer-names (specialized-lambda-list)
+  (loop for rest = specialized-lambda-list then (cdr rest)
+	until (or (atom rest)
+		  (member (car rest) lambda-list-keywords))
+	collect (if (consp (car rest))
+		    (cadar rest)
+		    t)))
