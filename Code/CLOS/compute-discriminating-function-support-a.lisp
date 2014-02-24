dedadc0085ea9731e01e2ca2315d9edc559596b4
Robert Strandh <robert.strandh@gmail.com>
2014-02-24 12:03:26 +0100

    Check for and handle special cases separately.

--- Code/CLOS/compute-discriminating-function-support-a.lisp
+++ Code/CLOS/compute-discriminating-function-support-a.lisp
@@ -11,38 +11,60 @@
 	 (active-arg-count (loop for x in specializer-profile
 				 count x))
 	 (class-number-vars (loop for x in specializer-profile
-				  when x collect (gensym))))
-  ;; Create a dictionary, mapping effective methods to
-  ;; forms containing APPLY that call those methods.
-  (let ((dico '()))
-    (loop for call-cache in (call-history generic-function)
-	  for class-number-cache = (class-number-cache call-cache)
-	  for effective-method = (effective-method-cache call-cache)
-	  do (when (null (assoc effective-method dico :test #'eq :key #'car))
-	       (push (cons effective-method
-			   `(return-from b
-			      (apply ,effective-method arguments)))
-		     dico)))
-    ;; Create a discriminating automaton with the entries in the call
-    ;; history.
-    (let ((automaton (make-automaton (1+ active-arg-count))))
-      (loop for call-cache in (call-history generic-function)
+				  when x collect (gensym)))
+	 (call-history (call-history generic-function)))
+    ;; Check for the special case when the call history is empty.  In
+    ;; that case, we just generate a call to the default
+    ;; discriminating function.
+    (when (null call-history)
+      (return-from make-discriminating-function-lambda
+	`(lambda (&rest arguments)
+	   (default-discriminating-function ,generic-function
+					    arguments
+					    ',specializer-profile))))
+    ;; Check for the special case when the active-arg-count is 0,
+    ;; meaning that no method specialized on anything other than T,
+    ;; but there is still some stuff in the call history, which is the
+    ;; only possible effective method.  So we generate a call to that
+    ;; effective method.
+    (when (zerop active-arg-count)
+      (return-from make-discriminating-function-lambda
+	`(lambda (&rest arguments)
+	   (apply ,(effective-method-cache (car call-history))
+		  arguments))))
+    ;; Create a dictionary, mapping effective methods to
+    ;; forms containing APPLY that call those methods.
+    (let ((dico '()))
+      (loop for call-cache in call-history
 	    for class-number-cache = (class-number-cache call-cache)
 	    for effective-method = (effective-method-cache call-cache)
-	    for action = (cdr (assoc effective-method dico
-				     :test #'eq :key #'car))
-	    do (add-path automaton class-number-cache action))
-      (let* ((info (extract-transition-information automaton))
-	     (tagbody (compute-discriminating-tagbody info class-number-vars)))
-	`(lambda (&rest arguments)
-	   (let ,(loop for x in specializer-profile
-		       for i from 0
-		       when x collect `(,(nth i class-number-vars)
-					(nth ,i arguments)))
-	     ,tagbody
-	     (default-discriminating-function ,generic-function
-					      arguments
-					      ,specializer-profile))))))))
+	    do (when (null (assoc effective-method dico :test #'eq))
+		 (push (cons effective-method
+			     `(return-from b
+				(apply ,effective-method arguments)))
+		       dico)))
+      ;; Create a discriminating automaton with the entries in the call
+      ;; history.
+      (let ((automaton (make-automaton (1+ active-arg-count))))
+	(loop for call-cache in call-history
+	      for class-number-cache = (class-number-cache call-cache)
+	      for effective-method = (effective-method-cache call-cache)
+	      for action = (cdr (assoc effective-method dico :test #'eq))
+	      do (add-path automaton class-number-cache action))
+	(let* ((info (extract-transition-information automaton))
+	       (tagbody (compute-discriminating-tagbody info class-number-vars)))
+	  `(lambda (&rest arguments)
+	     (format t "~%Arguments: ~s~%" arguments)
+	     (block b
+	       (let ,(loop for x in specializer-profile
+			   for i from 0
+			   when x collect `(,(nth i class-number-vars)
+					    (instance-class-number
+					     (nth ,i arguments))))
+		 ,tagbody
+		 (default-discriminating-function ,generic-function
+						  arguments
+						  ',specializer-profile)))))))))
 
 ;;; This function takes a generic function an returns a discriminating
 ;;; function for it that has the GENERIC-FUNCTION argument compiled in
@@ -51,10 +73,7 @@
 (defun make-default-discriminating-function (generic-function)
   (compile
    nil
-   `(lambda (&rest arguments)
-      (default-discriminating-function ,generic-function
-				       arguments
-				       ',(specializer-profile generic-function)))))
+   (make-discriminating-function-lambda generic-function)))
 
 (defun compute-discriminating-function-default (generic-function)
   (make-default-discriminating-function generic-function))
