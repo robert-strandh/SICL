076466915da6ebb432c9978709e7b969df7c7098
Robert Strandh <robert.strandh@gmail.com>
2014-02-27 09:10:10 +0100

    Rearranged class definitions.
    It is important to order the class definitions in this file
    depth-first pre-order, because that is how the classes will eventually
    be finalized.  Since finalization is where unique class numbers are
    assigned, this order ensures that a subtree of classes have
    consecutive class numbers, thus making dispatch faster.
    
    The result of this rearrangement is a depth-first pre-order as before,
    except that now, the definition of BUILT-IN-CLASS comes AFTER the
    definition of the subtree with REGULAR-CLASS as its root.  The reason
    for this rearrangement is that the subtree with REGULAR-CLASS as its
    root has some accessors for which we have DEFMETHOD forms spacialized
    to BUILT-IN-CLASS, and this rearrangement ensures that the
    corresponding generic functions are created when the DEFMETHOD forms
    are seen, thus avoiding warnings from the compiler.

--- Code/CLOS/mop-class-hierarchy.lisp
+++ Code/CLOS/mop-class-hierarchy.lisp
@@ -252,55 +252,6 @@
    
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;
-;;; BUILT-IN-CLASS.
-;;;
-;;; The AMOP says that the readers CLASS-DIRECT-DEFAULT-INITARGS,
-;;; CLASS-DIRECT-SLOTS, CLASS-DEFAULT-INITARGS, and CLASS-SLOTS should
-;;; return the empty list for a built-in class.  However, our built-in
-;;; classes have direct default initargs, direct slots, default
-;;; initargs, and effective slots.  So we keep the slots but we use
-;;; different readers: DIRECT-DEFAULT-INITARGS, DIRECT-SLOTS,
-;;; DEFAULT-INITARGS, and EFFECTIVE-SLOTS. 
-
-(defclass built-in-class (real-class)
-  ((%direct-default-initargs
-    :initarg :direct-default-initargs
-    :reader direct-default-initargs)
-   (%direct-superclasses 
-    :initarg :direct-superclasses
-    :reader class-direct-superclasses)
-   (%direct-slots
-    :initarg :direct-slots
-    :reader direct-slots)
-   (%default-initargs 
-    :initarg :default-initargs
-    :reader default-initargs
-    :writer (setf c-default-initargs))
-   (%effective-slots 
-    :initform '() 
-    :reader effective-slots
-    :writer (setf c-slots))))
-
-;;; The AMOP says that CLASS-DIRECT-SLOTS should return the empty list
-;;; for a built-in class.
-(defmethod class-direct-slots ((class built-in-class))
-  (declare (ignore class))
-  '())
-
-;;; The AMOP says that CLASS-SLOTS should return the empty list for a
-;;; built-in class.
-(defmethod class-slots ((class built-in-class))
-  (declare (ignore class))
-  '())
-
-;;; The AMOP says that CLASS-DEFAULT-INITARGS should return the empty
-;;; list for a built-in class.
-(defmethod class-default-initargs ((class built-in-class))
-  (declare (ignore class))
-  '())
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;;
 ;;; Class REGULAR-CLASS.
 ;;;
 ;;; A common superclass of STANDARD-CLASS and
@@ -355,6 +306,55 @@
 
 (defclass funcallable-standard-class (regular-class)
   ())
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;; BUILT-IN-CLASS.
+;;;
+;;; The AMOP says that the readers CLASS-DIRECT-DEFAULT-INITARGS,
+;;; CLASS-DIRECT-SLOTS, CLASS-DEFAULT-INITARGS, and CLASS-SLOTS should
+;;; return the empty list for a built-in class.  However, our built-in
+;;; classes have direct default initargs, direct slots, default
+;;; initargs, and effective slots.  So we keep the slots but we use
+;;; different readers: DIRECT-DEFAULT-INITARGS, DIRECT-SLOTS,
+;;; DEFAULT-INITARGS, and EFFECTIVE-SLOTS. 
+
+(defclass built-in-class (real-class)
+  ((%direct-default-initargs
+    :initarg :direct-default-initargs
+    :reader direct-default-initargs)
+   (%direct-superclasses 
+    :initarg :direct-superclasses
+    :reader class-direct-superclasses)
+   (%direct-slots
+    :initarg :direct-slots
+    :reader direct-slots)
+   (%default-initargs 
+    :initarg :default-initargs
+    :reader default-initargs
+    :writer (setf c-default-initargs))
+   (%effective-slots 
+    :initform '() 
+    :reader effective-slots
+    :writer (setf c-slots))))
+
+;;; The AMOP says that CLASS-DIRECT-SLOTS should return the empty list
+;;; for a built-in class.
+(defmethod class-direct-slots ((class built-in-class))
+  (declare (ignore class))
+  '())
+
+;;; The AMOP says that CLASS-SLOTS should return the empty list for a
+;;; built-in class.
+(defmethod class-slots ((class built-in-class))
+  (declare (ignore class))
+  '())
+
+;;; The AMOP says that CLASS-DEFAULT-INITARGS should return the empty
+;;; list for a built-in class.
+(defmethod class-default-initargs ((class built-in-class))
+  (declare (ignore class))
+  '())
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;
