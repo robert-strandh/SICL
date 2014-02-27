76fe7a3b4a2c5c42b9f383960b52e13b966712ff
Robert Strandh <robert.strandh@gmail.com>
2014-02-27 15:45:01 +0100

    Added allocation of new GF to ENSURE-GENERIC-FUNCTION in phase 3.

--- Code/CLOS/Boot/Phase3/ensure-generic-function.lisp
+++ Code/CLOS/Boot/Phase3/ensure-generic-function.lisp
@@ -4,5 +4,13 @@
   (fmakunbound 'ensure-generic-function))
 
 (defun ensure-generic-function (function-name &rest args)
-  (declare (ignore args))
-  (find-target-generic-function function-name))
+  (let ((fun (find-target-generic-function function-name nil)))
+    (if (null fun)
+	(let* ((class (find-bridge-class 'standard-generic-function))
+	       (method-class (find-bridge-class 'standard-method))
+	       (new-fun (apply #'make-instance class
+			       :name function-name
+			       :method-class method-class
+			       args)))
+	  (add-target-generic-function function-name new-fun))
+	fun)))
