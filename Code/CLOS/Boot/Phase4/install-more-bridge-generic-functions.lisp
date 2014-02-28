b6259596525b7637a7eb001e5bad9171750f43f5
Robert Strandh <robert.strandh@gmail.com>
2014-02-28 14:52:01 +0100

    Added more bridge generic functions to install.

--- Code/CLOS/Boot/Phase4/install-more-bridge-generic-functions.lisp
+++ Code/CLOS/Boot/Phase4/install-more-bridge-generic-functions.lisp
@@ -1,7 +1,15 @@
 (cl:in-package #:sicl-clos)
 
 (defparameter *more-bridge-generic-functions*
-  '())
+  '(symbol-name
+    symbol-package
+    array-dimensions
+    fill-pointer
+    (setf fill-pointer)
+    functions
+    (setf functions)
+    classes
+    (setf classes)))
 
 (loop for name in *more-bridge-generic-functions*
       do (setf (fdefinition name)
