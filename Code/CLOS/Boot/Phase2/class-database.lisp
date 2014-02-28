ad267834681a1e867e9dae2b7ced33ce31f068ab
Robert Strandh <robert.strandh@gmail.com>
2014-02-28 10:09:07 +0100

    Fix incorrect error messages.

--- Code/CLOS/Boot/Phase2/class-database.lisp
+++ Code/CLOS/Boot/Phase2/class-database.lisp
@@ -4,13 +4,13 @@
 
 (defun add-bridge-class (name function)
   (when (member name *bridge-classes* :key #'car :test #'equal)
-    (error "Attempt to define bridge generic function ~s twice." name))
+    (error "Attempt to define bridge class ~s twice." name))
   (push (cons name function) *bridge-classes*))
 
 (defun find-bridge-class (name)
   (let ((entry (assoc name *bridge-classes* :test #'equal)))
     (when (null entry)
-      (error "There is no bridge generic function named ~s." name))
+      (error "There is no bridge class named ~s." name))
     (cdr entry)))
 
 (defun delete-bridge-class (name)
