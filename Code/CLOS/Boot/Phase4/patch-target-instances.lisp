114e1bd6fc8fd01bb3132109a1b3dbc003d523f2
Robert Strandh <robert.strandh@gmail.com>
2014-02-28 10:10:29 +0100

    Added function for patching built-in instances.

--- Code/CLOS/Boot/Phase4/patch-target-instances.lisp
+++ Code/CLOS/Boot/Phase4/patch-target-instances.lisp
@@ -12,6 +12,15 @@
 	  (setf (standard-instance-access object 1)
 		(funcall gf target-class))))))
 
+(defun patch-built-in-instance (object)
+  (let ((bridge-class (heap-instance-class object)))
+    (if (heap-instance-p bridge-class)
+	(error "this should not happen")
+	(let* ((name (class-name bridge-class))
+	       (target-class (find-target-class name)))
+	  (setf (heap-instance-class object)
+		target-class)))))
+
 (let ((seen '()))
   (labels ((traverse (object)
 	     (unless (member object seen)
