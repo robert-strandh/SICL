bfb4da904de76a05c870d4ceb25191ab492b16e2
Robert Strandh <robert.strandh@gmail.com>
2014-02-28 09:30:02 +0100

    Fix incorrect error messages.

--- Code/CLOS/Boot/Phase3/class-database.lisp
+++ Code/CLOS/Boot/Phase3/class-database.lisp
@@ -4,13 +4,13 @@
 
 (defun add-target-class (name function)
   (when (member name *target-classes* :key #'car :test #'equal)
-    (error "Attempt to define target generic function ~s twice." name))
+    (error "Attempt to define target class ~s twice." name))
   (push (cons name function) *target-classes*))
 
 (defun find-target-class (name)
   (let ((entry (assoc name *target-classes* :test #'equal)))
     (when (null entry)
-      (error "There is no target generic function named ~s." name))
+      (error "There is no target class named ~s." name))
     (cdr entry)))
 
 (defun delete-target-class (name)
