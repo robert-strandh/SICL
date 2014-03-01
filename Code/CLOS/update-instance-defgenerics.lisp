90ccd907b9ced26ba0357653be9cbf01b4206e36
Robert Strandh <robert.strandh@gmail.com>
2014-03-01 11:39:47 +0100

    DEFGENERIC forms for UPDATE-INSTANCE-FOR-DIFFERENT/REDEFINED-CLASS.

---
+++ Code/CLOS/update-instance-defgenerics.lisp
@@ -0,0 +1,17 @@
+(cl:in-package #:sicl-clos)
+
+;;; For the specification of this generic function, see
+;;; http://www.lispworks.com/documentation/HyperSpec/Body/f_update.htm#update-instance-for-different-class
+(defgeneric update-instance-for-different-class (previous
+						 current
+						 &rest initargs
+						 &key &allow-other-keys))
+
+;;; For the specification of this generic function, see
+;;; http://www.lispworks.com/documentation/HyperSpec/Body/f_upda_1.htm#update-instance-for-redefined-class
+(defgeneric update-instance-for-redefined-class (instance
+						 added-slots
+						 discarded-slots
+						 property-list
+						 &rest initargs
+						 &key &allow-other-keys))
