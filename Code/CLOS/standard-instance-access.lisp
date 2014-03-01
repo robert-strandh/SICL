56d8ea5db9004511f41d51e8ee399983f05b076a
Robert Strandh <robert.strandh@gmail.com>
2014-03-01 11:38:54 +0100

    Added reference to the AMOP specification.

--- Code/CLOS/standard-instance-access.lisp
+++ Code/CLOS/standard-instance-access.lisp
@@ -1,5 +1,7 @@
 (cl:in-package #:sicl-clos)
 
+;;; For the specification of this function, see
+;;; http://metamodular.com/CLOS-MOP/standard-instance-access.html
 (defun standard-instance-access (instance location)
   (slot-contents (heap-instance-slots instance) location))
 
