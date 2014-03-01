fefb8cb4e08bdd62ab4892d3daf12cd201c8b079
Robert Strandh <robert.strandh@gmail.com>
2014-03-01 11:38:25 +0100

    Added references to the CLHS.

--- Code/CLOS/add-remove-method-defgenerics.lisp
+++ Code/CLOS/add-remove-method-defgenerics.lisp
@@ -6,6 +6,9 @@
 ;;;
 ;;; For the specification of this generic function, see
 ;;; http://metamodular.com/CLOS-MOP/add-method.html
+;;;
+;;; This generic function is also specified in the CLHS.
+;;; http://www.lispworks.com/documentation/HyperSpec/Body/f_add_me.htm#add-method
 
 (defgeneric add-method (generic-function method))
 
@@ -15,5 +18,8 @@
 ;;;
 ;;; For the specification of this generic function, see
 ;;; http://metamodular.com/CLOS-MOP/remove-method.html
+;;;
+;;; This generic function is also specified in the CLHS.
+;;; http://www.lispworks.com/documentation/HyperSpec/Body/f_rm_met.htm#remove-method
 
 (defgeneric remove-method (generic-function method))
