222bef818396727a359af6b5215208ad539ee3f0
Robert Strandh <robert.strandh@gmail.com>
2014-03-02 04:01:15 +0100

    New file containing a DEFGENERC form for FIND-METHOD-COMBINATION.

---
+++ Code/CLOS/find-method-combination-defgenerics.lisp
@@ -0,0 +1,7 @@
+(cl:in-package #:sicl-clos)
+
+;;; For the specification of this generic function, see
+;;; http://metamodular.com/CLOS-MOP/find-method-combination.html
+(defgeneric find-method-combination (generic-function
+				     method-combination-type-name
+				     method-combination-options))
