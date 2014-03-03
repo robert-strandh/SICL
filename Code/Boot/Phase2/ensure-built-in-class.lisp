213e34ec13576c4a721aca2186bb00c571e6544b
Robert Strandh <robert.strandh@gmail.com>
2014-03-03 12:21:25 +0100

    Added special version of ENSURE-BUILT-IN-CLASS as component in phase 2.

---
+++ Code/Boot/Phase2/ensure-built-in-class.lisp
@@ -0,0 +1,32 @@
+(cl:in-package #:sicl-clos)
+
+;;; The macro DEFINE-BUILT-IN-CLASS expands to a call to this
+;;; function, and this function is used only in that situation.  For
+;;; that reason, we do not have to be particularly thorough about
+;;; checking the validity of arguments.
+;;;
+;;; The argument DIRECT-SUPERCLASS-NAMES is a list of SYMBOLS.  Unlike
+;;; the function ENSURE-CLASS we do not allow for class metaobject
+;;; superclasses.
+(defun ensure-built-in-class (name
+			      &rest arguments
+			      &key
+				direct-default-initargs
+				direct-superclass-names
+			      &allow-other-keys)
+  ;; If the class already exists, then do nothing.
+  (when (null (find-class name nil))
+    (let ((superclasses (loop for name in direct-superclass-names
+			      for class = (find-bridge-class name)
+			      collect class))
+	  (remaining-keys (copy-list arguments)))
+      (loop while (remf remaining-keys :direct-superclasses))
+      (let ((result (apply #'cl:make-instance
+			   'sicl-boot-phase1:built-in-class
+			   :direct-default-initargs direct-default-initargs
+			   :name name
+			   :direct-superclasses superclasses
+			   remaining-keys)))
+	(add-bridge-class name result)
+	;; FIXME: this is where we add create the accessors.
+	result))))
