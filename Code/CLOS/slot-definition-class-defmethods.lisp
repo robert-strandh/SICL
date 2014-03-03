d827b96447207269e57120edc8796e42e14f1b3a
Robert Strandh <robert.strandh@gmail.com>
2014-03-03 14:12:56 +0100

    Code factoring using class REGULAR-CLASS.

--- Code/CLOS/slot-definition-class-defmethods.lisp
+++ Code/CLOS/slot-definition-class-defmethods.lisp
@@ -5,17 +5,9 @@
 ;;;  DIRECT-SLOT-DEFINITION-CLASS and EFFECTIVE-SLOT-DEFINITION-CLASS.
 
 (defmethod direct-slot-definition-class
-    ((class standard-class) &rest initargs)
-  (apply #'direct-slot-definition-class-default class initargs))
-
-(defmethod direct-slot-definition-class
-    ((class funcallable-standard-class) &rest initargs)
+    ((class regular-class) &rest initargs)
   (apply #'direct-slot-definition-class-default class initargs))
 
 (defmethod effective-slot-definition-class
-    ((class standard-class) &rest initargs)
-  (apply #'effective-slot-definition-class-default class initargs))
-
-(defmethod effective-slot-definition-class
-    ((class funcallable-standard-class) &rest initargs)
+    ((class regular-class) &rest initargs)
   (apply #'effective-slot-definition-class-default class initargs))
