114d721d5561106e067ec53c663705740954beab
Robert Strandh <robert.strandh@gmail.com>
2014-03-03 11:21:12 +0100

    Variables for STANDARD-READER/WRITER-METHOD classes.

--- Code/Boot/Phase2/define-variables.lisp
+++ Code/Boot/Phase2/define-variables.lisp
@@ -2,3 +2,9 @@
 
 (defvar *t*
   (find-class 't))
+
+(defvar *standard-reader-method*
+  (find-class 'sicl-boot-phase1:standard-reader-method))
+
+(defvar *standard-writer-method*
+  (find-class 'sicl-boot-phase1:standard-writer-method))
