977d7658793341262861eb8eddade6ae49b4def6
Robert Strandh <robert.strandh@gmail.com>
2014-03-05 13:54:31 +0100

    Make CLASS-OF handle both target instances and bridge instances.

--- Code/Boot/Phase1/class-of.lisp
+++ Code/Boot/Phase1/class-of.lisp
@@ -1,6 +1,10 @@
 (cl:in-package #:sicl-boot-phase1)
 
 (defun class-of (object)
-  (if (heap-instance-p object)
-      (heap-instance-class object)
-      *t*))
+  (cond ((heap-instance-p object)
+	 (heap-instance-class object))
+	((typep object 'cl:standard-object)
+	 (cl:class-of object))
+	(t
+	 *t*)))
+
