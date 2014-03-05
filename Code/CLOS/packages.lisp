c3e9c0edf636207ef3bde30cdf95b48461f2f982
Robert Strandh <robert.strandh@gmail.com>
2014-03-05 18:35:42 +0100

    Begin work to make the SICL-CLOS package look more like the final version.

--- Code/CLOS/packages.lisp
+++ Code/CLOS/packages.lisp
@@ -1,27 +1,9 @@
 (cl:in-package #:common-lisp-user)
 
-(defpackage #:sicl-common-lisp
-  (:nicknames #:scl)
-  (:export . #.(let ((symbols '()))
-		 (do-external-symbols (symbol '#:common-lisp)
-		   (push (symbol-name symbol) symbols))
-		 symbols))
-  (:shadowing-import-from
-   #:common-lisp
-   ;; Lambda list keywords.  The have no definitions associated with
-   ;; them, so it doesn't matter what home package they have.
-   #:&optional #:&rest #:&body #:&key #:&allow-other-keys
-   #:&aux #:&whole #:&environment
-   ;; Symbols that have to do with declarations and that have
-   ;; no other meaning, or at least no meaning that we are currently
-   ;; interested in.
-   #:declare #:inline #:notinline #:ignore #:ignorable #:type #:ftype
-   #:optimize #:debug #:speed #:compilation-speed #:space #:safety))
+;;; FIXME: The list of exported symbols is not complete.
 
 (defpackage #:sicl-clos
-  (:use #:sicl-additional-conditions
-	#:sicl-code-utilities
-	#:sicl-common-lisp)
+  (:use #:common-lisp)
   (:export
    ;; MOP classes
    #:class #:standard-class #:built-in-class #:structure-class
@@ -35,7 +17,25 @@
    #:standard--slot-definition
    #:standard-direct-slot-definition
    #:standard-effective-slot-definition
-   ;; Functions related to slot definitions.
+   ;; Accessors for class metaobjects. 
+   #:class-name
+   #:class-direct-superclasses
+   #:class-direct-slots
+   #:class-direct-default-initargs
+   #:class-precedence-list
+   #:class-slots
+   #:class-default-initargs
+   #:class-finalized-p
+   #:class-prototype
+   ;; Accessors for generic function metaobjects.
+   #:generic-function-name
+   #:generic-function-argument-precedence-order
+   #:generic-function-lambda-list
+   #:generic-function-declarations
+   #:generic-function-method-class
+   #:generic-function-method-combination
+   #:generic-function-methods
+   ;; Accessors for slot definition metaobjects.
    #:slot-definition-name
    #:slot-definition-allocation
    #:slot-definition-type
@@ -45,34 +45,32 @@
    #:slot-definition-readers
    #:slot-definition-writers
    #:slot-definition-location
-   #:make-direct-slot-definition
-   #:make-effective-slot-definition
+   ;; Macros
+   #:defclass #:defgeneric #:defmethod
+   ;; Generic functions.
    #:compute-effective-slot-definition
-   ;;
-   #:defclass #:find-class
-   #:class-name #:class-of
-   #:allocate-instance #:make-instance
-   #:initialize-instance #:shared-initialize #:reinitialize-instance
+   #:find-class
+   #:class-of
+   #:allocate-instance
+   #:make-instance
+   #:initialize-instance
+   #:reinitialize-instance
+   #:shared-initialize
    #:make-instances-obsolete
    #:update-instance-for-different-class
    #:update-instance-for-redefined-class
-   #:method #:standard-method
    #:make-method #:add-method #:call-method #:find-method
    #:method-qualifiers #:compute-applicable-methods #:next-method-p
    #:invalid-method-error
    #:no-applicable-method #:no-next-method #:remove-method #:defmethod
    #:method-combination #:define-method-combination #:method-combination-error
    #:make-instance
-   ;; Generic functions.
-   #:generic-function #:standard-generic-function
-   #:defgeneric #:ensure-generic-function
+   #:ensure-generic-function
    #:slot-value #:slot-missing #:slot-boundp #:slot-makunbound
    #:print-object #:describe-object #:documentation
+   ;; SICL-specific classes
+   #:real-class #:regular-class
+   ;; SICL-specific functions
+   #:ensure-method
    ;; SICL-specific macro
    #:define-built-in-class))
-
-(defpackage #:sicl-conditionals
-  (:use #:sicl-common-lisp))
-
-(defpackage #:sicl-cons-high
-  (:use #:sicl-common-lisp))
