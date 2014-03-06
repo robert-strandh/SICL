2fc8a4c978713033dc5af787552f75f13ae670f6
Robert Strandh <robert.strandh@gmail.com>
2014-03-06 09:34:56 +0100

    Beginning of phase 3.

---
+++ Code/Boot/Phase3/packages.lisp
@@ -0,0 +1,9 @@
+(cl:in-package #:common-lisp-user)
+
+(defpackage #:sicl-boot-phase3
+  (:use #:common-lisp)
+  (:shadow
+   #:ensure-generic-function)
+  (:export
+   ))
+
---
+++ Code/Boot/Phase3/sicl-boot-phase3.asd
@@ -0,0 +1,6 @@
+(cl:in-package #:common-lisp-user)
+
+(asdf:defsystem :sicl-boot-phase3
+  :depends-on (:sicl-boot-phase2)
+  :components
+  ((:file "packages")))
