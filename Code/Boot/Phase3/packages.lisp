9a31bb897b62c73ab6cdcedeabf33690df832038
Robert Strandh <robert.strandh@gmail.com>
2014-03-06 13:08:32 +0100

    Avoid name conflict by using names of macros from the CLOS package.

--- Code/Boot/Phase3/packages.lisp
+++ Code/Boot/Phase3/packages.lisp
@@ -1,7 +1,12 @@
 (cl:in-package #:common-lisp-user)
 
 (defpackage #:sicl-boot-phase3
-  (:use #:common-lisp)
+  (:use #:common-lisp #:sicl-clos)
+  (:shadowing-import-from
+   #:sicl-clos
+   #:defclass
+   #:defgeneric
+   #:defmethod)
   (:shadow
    #:ensure-generic-function)
   (:export
