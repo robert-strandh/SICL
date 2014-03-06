216db2c85d54fd3b8094a6233dd647d7c93dade1
Robert Strandh <robert.strandh@gmail.com>
2014-03-06 15:23:15 +0100

    Added temporary methods on BUILT-IN-CLASS.

--- Code/CLOS/slot-definition-class-defmethods.lisp
+++ Code/CLOS/slot-definition-class-defmethods.lisp
@@ -11,3 +11,15 @@
 (defmethod effective-slot-definition-class
     ((class regular-class) &rest initargs)
   (apply #'effective-slot-definition-class-default class initargs))
+
+;;; The following methods should be removed once the system is
+;;; up and running.
+
+(defmethod direct-slot-definition-class
+    ((class built-in-class) &rest initargs)
+  (apply #'direct-slot-definition-class-default class initargs))
+
+(defmethod effective-slot-definition-class
+    ((class built-in-class) &rest initargs)
+  (apply #'effective-slot-definition-class-default class initargs))
+
