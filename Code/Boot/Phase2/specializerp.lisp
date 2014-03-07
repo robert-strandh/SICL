681775afc4a5397b46a9fb08caedbfe849248931
Robert Strandh <robert.strandh@gmail.com>
2014-03-07 08:28:40 +0100

    For now, define SPECIALIZERP to always return true.

--- Code/Boot/Phase2/specializerp.lisp
+++ Code/Boot/Phase2/specializerp.lisp
@@ -1,11 +1,5 @@
 (cl:in-package #:sicl-boot-phase2)
 
-(defgeneric specializerp (object))
-
-(defmethod specializerp (object)
+(defun specializerp (object)
   (declare (ignore object))
-  nil)
-
-(defmethod specializerp ((object specializer))
-  (declare (ignorable object))
   t)
