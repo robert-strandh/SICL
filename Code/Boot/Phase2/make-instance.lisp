af45f69dc99da83e66af0b1b32251c1dae06a2db
Robert Strandh <robert.strandh@gmail.com>
2014-03-08 12:18:37 +0100

    Provide an indirection to MAKE-INSTANCE-DEFAULT.

--- Code/Boot/Phase2/make-instance.lisp
+++ Code/Boot/Phase2/make-instance.lisp
@@ -1,9 +1,12 @@
 (cl:in-package #:sicl-boot-phase2)
 
+(defparameter *make-instance-default*
+  #'sicl-boot-phase1:make-instance-default)
+
 (defun make-instance (class &rest initargs)
   (when (symbolp class)
     (setf class (find-bridge-class class)))
-  (apply #'sicl-boot-phase1:make-instance-default
+  (apply *make-instance-default*
 	 class
 	 #'initialize-instance
 	 initargs))
