a4d6f3093361c84b028f2b5f2365a4402acdf9fd
Robert Strandh <robert.strandh@gmail.com>
2014-03-12 16:19:25 +0100

    Added comment about the use of ENSURE-ACCESSOR-FUNCTION.

--- Code/CLOS/add-accessor-method.lisp
+++ Code/CLOS/add-accessor-method.lisp
@@ -2,12 +2,20 @@
 
 ;;;; This file contains very basic versions of the functions
 ;;;; ADD-READER-METHOD and ADD-WRITER-METHOD.  These functions are not
-;;;; part of the spcification, but they conventient abstractions that
+;;;; part of the specification, but they convenient abstractions that
 ;;;; are called when a class metaobject is created that has one or
 ;;;; more slots with a :READER, :WRITER, or :ACCESSOR slot option.
 ;;;; Specifically, they are called from the :AFTER methods on
 ;;;; INITIALIZE-INSTANCE specialized for STANDARD-CLASS and
 ;;;; FUNCALLABLE-STANDARD-CLASS.
+
+;;; The functions defined here need to call ENSURE-GENERIC-FUNCTION to
+;;; make sure the generic function on which a method is going to be
+;;; added exists.  However, we do not call ENSURE-GENERIC-FUNCTION
+;;; directly, because during bootstrapping, we need to do different
+;;; things in phase 1 and phase 2.  For that reason, we call an
+;;; intermediate function named ENSURE-ACCESSOR-FUNCTION, and make
+;;; that function mean different things in phase 1 and phase 2.
 
 ;;; Add a reader method to a generic function.  CLASS is a class
 ;;; metaobject that plays the role of specialize for the argument of
@@ -20,6 +28,8 @@
 ;;; method for optimization purposes. 
 (defun add-reader-method (class function-name slot-definition)
   (let* ((lambda-list '(object))
+	 ;; See comment above as to why we do not call
+	 ;; ENSURE-GENERIC-FUNCTION directly here.
 	 (generic-function (ensure-accessor-function function-name lambda-list))
 	 (specializers (list class))
 	 (method-function
@@ -52,6 +62,8 @@
 ;;; method for optimization purposes.
 (defun add-writer-method (class function-name slot-definition)
   (let* ((lambda-list '(new-value object))
+	 ;; See comment above as to why we do not call
+	 ;; ENSURE-GENERIC-FUNCTION directly here.
 	 (generic-function (ensure-accessor-function function-name lambda-list))
 	 (specializers (list *t* class))
 	 (method-function
