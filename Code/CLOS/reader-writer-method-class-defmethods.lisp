ffaaec65db85ee328b2154a69fd19b1465a19f21
Robert Strandh <robert.strandh@gmail.com>
2014-03-12 15:10:48 +0100

    Simplify by factoring methods to specilize on REGULAR-CLASS.

--- Code/CLOS/reader-writer-method-class-defmethods.lisp
+++ Code/CLOS/reader-writer-method-class-defmethods.lisp
@@ -1,25 +1,13 @@
 (cl:in-package #:sicl-clos)
 
 (defmethod reader-method-class
-    ((class standard-class)
-     (direct-slot standard-direct-slot-definition)
-     &rest initargs)
-  (apply #'reader-method-class-default class direct-slot initargs))
-
-(defmethod reader-method-class
-    ((class funcallable-standard-class)
+    ((class regular-class)
      (direct-slot standard-direct-slot-definition)
      &rest initargs)
   (apply #'reader-method-class-default class direct-slot initargs))
 
 (defmethod writer-method-class
-    ((class standard-class)
-     (direct-slot standard-direct-slot-definition)
-     &rest initargs)
-  (apply #'writer-method-class-default class direct-slot initargs))
-
-(defmethod writer-method-class
-    ((class funcallable-standard-class)
+    ((class regular-class)
      (direct-slot standard-direct-slot-definition)
      &rest initargs)
   (apply #'writer-method-class-default class direct-slot initargs))
