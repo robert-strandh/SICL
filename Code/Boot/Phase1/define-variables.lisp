b4c8947ad95594ec90893f8406bd0df182489c30
Robert Strandh <robert.strandh@gmail.com>
2014-03-12 15:29:24 +0100

    Added comment for variables *STANDARD-READER-METHOD* and *STANDARD-WRITER-METHOD.*

--- Code/Boot/Phase1/define-variables.lisp
+++ Code/Boot/Phase1/define-variables.lisp
@@ -14,6 +14,20 @@
 (defvar *funcallable-standard-class*
   (find-class 'funcallable-standard-class))
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;; Variables *STANDARD-READER-METHOD* and *STANDARD-WRITER-METHOD* 
+;;;
+;;; These variables are referred to by the default methods on the
+;;; generic functions READER-METHOD-CLASS and WRITER-METHOD-CLASS that
+;;; are defined later in phase 1.  These functions are called when the
+;;; MOP hierarchy is loaded in phase 2 in order to create bridge
+;;; classes.  Creating a bridge class (which is a host instance)
+;;; implicitly creates bridge generic functions (which are also host
+;;; instances) for the accessors, and bridge methods on those bridge
+;;; generic functions.  Since bridge methods are also host instances,
+;;; the values of the variables must be host classes. 
+
 (defvar *standard-reader-method*
   (find-class 'standard-reader-method))
 
