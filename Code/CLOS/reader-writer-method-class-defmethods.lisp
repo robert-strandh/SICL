bd3e51fba319d0692ab43a8b37458e47d313bc2c
Robert Strandh <robert.strandh@gmail.com>
2014-03-17 17:52:33 +0100

    Add temporary methods for BUILT-IN-CLASS.

--- Code/CLOS/reader-writer-method-class-defmethods.lisp
+++ Code/CLOS/reader-writer-method-class-defmethods.lisp
@@ -11,3 +11,17 @@
      (direct-slot standard-direct-slot-definition)
      &rest initargs)
   (apply #'writer-method-class-default class direct-slot initargs))
+
+;;; These methods should be removed when the system is in production.
+
+(defmethod reader-method-class
+    ((class built-in-class)
+     (direct-slot standard-direct-slot-definition)
+     &rest initargs)
+  (apply #'reader-method-class-default class direct-slot initargs))
+
+(defmethod writer-method-class
+    ((class built-in-class)
+     (direct-slot standard-direct-slot-definition)
+     &rest initargs)
+  (apply #'writer-method-class-default class direct-slot initargs))
