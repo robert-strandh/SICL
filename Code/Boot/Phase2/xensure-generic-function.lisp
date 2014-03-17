e4c9caf1f9cced0c1b46a54c0df94fa7425b0f81
Robert Strandh <robert.strandh@gmail.com>
2014-03-17 09:37:02 +0100

    Use indirection through variables for FIND/ADD-ERSATZ-GENERIC-FUNCTION.

--- Code/Boot/Phase2/xensure-generic-function.lisp
+++ Code/Boot/Phase2/xensure-generic-function.lisp
@@ -1,12 +1,18 @@
 (cl:in-package #:sicl-boot-phase2)
 
+(defparameter *find-ersatz-generic-function*
+  #'find-ersatz-generic-function)
+
+(defparameter *add-ersatz-generic-function*
+  #'add-ersatz-generic-function)
+
 (defun *ensure-generic-function (function-name &rest args)
-  (let ((fun (find-ersatz-generic-function function-name nil)))
+  (let ((fun (funcall *find-ersatz-generic-function* function-name nil)))
     (if (null fun)
 	(let ((new-fun (apply #'make-instance
 			      'standard-generic-function
 			      :name function-name
 			      args)))
-	  (add-ersatz-generic-function function-name new-fun)
+	  (funcall *add-ersatz-generic-function* function-name new-fun)
 	  new-fun)
 	fun)))
