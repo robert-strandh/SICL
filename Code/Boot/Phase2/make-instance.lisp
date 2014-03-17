435ce87b1d852da4924c485e9939a02566635c55
Robert Strandh <robert.strandh@gmail.com>
2014-03-17 07:35:47 +0100

    Create an indirection to FIND-CLASS.

--- Code/Boot/Phase2/make-instance.lisp
+++ Code/Boot/Phase2/make-instance.lisp
@@ -3,9 +3,12 @@
 (defparameter *make-instance-default*
   #'sicl-boot-phase1:make-instance-default)
 
+(defparameter *find-class*
+  #'find-bridge-class)
+
 (defun make-instance (class &rest initargs)
   (when (symbolp class)
-    (setf class (find-bridge-class class)))
+    (setf class (funcall *find-class* class)))
   (apply *make-instance-default*
 	 class
 	 #'initialize-instance
