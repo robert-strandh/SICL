e856d6a162bfd70f4f4f6532968ed57c9cf57109
Robert Strandh <robert.strandh@gmail.com>
2014-03-19 05:30:33 +0100

    Also update variable *MAKE-BUILT-IN-INSTANCE-DEFAULT*.

--- Code/Boot/Phase3/update-functions.lisp
+++ Code/Boot/Phase3/update-functions.lisp
@@ -3,6 +3,9 @@
 (setf sicl-boot-phase2:*make-instance-default*
       #'sicl-boot-phase2:make-instance-default)
 
+(setf sicl-boot-phase2:*make-built-in-instance-default*
+      #'sicl-boot-phase2:make-built-in-instance-default)
+
 (setf sicl-boot-phase2:*shared-initialize-default*
       #'shared-initialize-default)
 
