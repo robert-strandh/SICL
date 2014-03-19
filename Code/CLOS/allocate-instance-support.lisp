864fbb8698e67136cfb0f80e70a4ac3705730ef5
Robert Strandh <robert.strandh@gmail.com>
2014-03-19 17:25:15 +0100

    Improved comment.

--- Code/CLOS/allocate-instance-support.lisp
+++ Code/CLOS/allocate-instance-support.lisp
@@ -1,14 +1,27 @@
 (cl:in-package #:sicl-clos)
 
-;;;; This function implements the action of the default methods on
-;;;; ALLOCATE-INSTANCE, i.e., the methods specialized for
-;;;; STANDARD-CLASS and FUNCALLABLE-STANDARD-CLASS.  Every instance of
-;;;; these classes has two initial cells (the unique number of the
-;;;; class, and the list of effective slots of the class) in the
-;;;; rack.  These cells are not counted among the slots,
-;;;; because they are accessed directly, using offsets.  For that
-;;;; reason, we must allocate more slot storage than there are slots
-;;;; with :INSTANCE allocation.
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;; Generic function ALLOCATE-INSTANCE.
+;;;
+;;; For the specification of this generic function, see
+;;; http://metamodular.com/CLOS-MOP/allocate-instance.html
+;;;
+;;; The specification includes three methods for this generic
+;;; function.  The first one is specialized for STANDARD-CLASS.  The
+;;; second one is specialized for FUNCALLABLE-STANDARD-CLASS.  The
+;;; third one is specialized for BUILT-IN-CLASS.  The last one signals
+;;; an error.  The default action below is valid for the first two
+;;; methods.
+
+;;; This function implements the action of the default methods on
+;;; ALLOCATE-INSTANCE, i.e., the methods specialized for
+;;; STANDARD-CLASS and FUNCALLABLE-STANDARD-CLASS.  Every instance of
+;;; these classes has two initial cells (the STAMP, and the list of
+;;; effective slots of the class) in the rack.  These cells are not
+;;; counted among the slots, because they are accessed directly, using
+;;; offsets.  For that reason, we must allocate more slot storage than
+;;; there are slots with :INSTANCE allocation.
 
 ;;; The AMOP says that ALLOCATE-INSTANCE checks whether the class is
 ;;; finalized, and if not, calls FINALIZE-INHERITANCE.  However, the
