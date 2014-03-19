3cd5b6df003ba9f910683608395f0d9d4b551f5e
Robert Strandh <robert.strandh@gmail.com>
2014-03-19 18:02:54 +0100

    New file containing DEFGENERIC form for FUNCALLABLE-STANDARD-INSTANCE-ACCESS.

---
+++ Code/CLOS/funcallable-standard-instance-access-defgenerics.lisp
@@ -0,0 +1,5 @@
+(cl:in-package #:sicl-clos)
+
+;;; For the specification of this generic function, see
+;;; http://metamodular.com/CLOS-MOP/funcallable-standard-instance-access.html
+(defgeneric funcallable-standard-instance-access (instance location))
