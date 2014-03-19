25d7aa16e96098a29b57a00ec7d58aa934b195d6
Robert Strandh <robert.strandh@gmail.com>
2014-03-19 17:24:46 +0100

    Added comments.

--- Code/CLOS/add-remove-direct-subclass-support.lisp
+++ Code/CLOS/add-remove-direct-subclass-support.lisp
@@ -1,11 +1,33 @@
 (cl:in-package #:sicl-clos)
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;; Generic function ADD-DIRECT-SUBCLASS.
+;;;
+;;; For the specification of this generic function, see
+;;; http://metamodular.com/CLOS-MOP/add-direct-subclass.html
+;;;
+;;; The specification includes a single method on this generic
+;;; function, specialized for CLASS and CLASS.  The default action
+;;; below is valid for that method.
+
 (defun add-direct-subclass-default (superclass subclass)
   (when (member subclass (class-direct-subclasses superclass))
     (error "Attempt to add existing subclass ~s as a subclass of ~s."
 	   subclass superclass))
   (setf (c-direct-subclasses superclass)
 	(cons subclass (class-direct-subclasses superclass))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;; Generic function REMOVE-DIRECT-SUBCLASS.
+;;;
+;;; For the specification of this generic function, see
+;;; http://metamodular.com/CLOS-MOP/remove-direct-subclass.html
+;;;
+;;; The specification includes a single method on this generic
+;;; function, specialized for CLASS and CLASS.  The default action
+;;; below is valid for that method.
 
 (defun remove-direct-subclass-default (superclass subclass)
   (setf (c-direct-subclasses superclass)
