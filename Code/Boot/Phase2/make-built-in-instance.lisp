72675722de7e5ebda0e01c5f5e8184457f5b9310
Robert Strandh <robert.strandh@gmail.com>
2014-03-19 05:25:48 +0100

    Use indirection for MAKE-BUILT-IN-INSTANCE-DEFAULT.

--- Code/Boot/Phase2/make-built-in-instance.lisp
+++ Code/Boot/Phase2/make-built-in-instance.lisp
@@ -1,9 +1,12 @@
 (cl:in-package #:sicl-boot-phase2)
 
+(defparameter *make-built-in-instance-default*
+  #'sicl-boot-phase1:make-built-in-instance-default)
+
 (defun make-built-in-instance (class &rest initargs)
   (when (symbolp class)
-    (setf class (find-bridge-class class)))
-  (apply #'sicl-boot-phase1:make-built-in-instance-default
+    (setf class (funcall *find-class* class)))
+  (apply *make-built-in-instance-default*
 	 class
 	 #'initialize-built-in-instance
 	 initargs))
