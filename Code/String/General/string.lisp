c31bfc70e16f2bf748f45bd8709be9bc37325ccb
Robert Strandh <robert.strandh@gmail.com>
2014-03-20 16:00:24 +0100

    Added STRING-RIGHT-TRIM.

--- Code/String/General/string.lisp
+++ Code/String/General/string.lisp
@@ -96,3 +96,11 @@
       (if (null first)
 	  string
 	  (subseq string first)))))
+
+(defun string-right-trim (character-bag string-designator)
+  (flet ((in-bag-p (char) (find char character-bag)))
+    (let* ((string (string string-designator))
+	   (last (position-if-not #'in-bag-p string :from-end t)))
+      (if (null last)
+	  string
+	  (subseq string 0 (1+ last))))))
