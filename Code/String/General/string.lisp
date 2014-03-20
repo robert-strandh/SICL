1179adb00858d189cc1a194b7e92d0d1becfe156
Robert Strandh <robert.strandh@gmail.com>
2014-03-20 15:59:27 +0100

    Added STRING-LEFT-TRIM.

--- Code/String/General/string.lisp
+++ Code/String/General/string.lisp
@@ -89,5 +89,10 @@
 	  (let ((last (position-if-not #'in-bag-p string :from-end t)))
 	    (subseq string first (1+ last)))))))
 
-      
-    
+(defun string-left-trim (character-bag string-designator)
+  (flet ((in-bag-p (char) (find char character-bag)))
+    (let* ((string (string string-designator))
+	   (first (position-if-not #'in-bag-p string)))
+      (if (null first)
+	  string
+	  (subseq string first)))))
