7bab9626e5190306c99bc111f8ac4c49f6316742
Robert Strandh <robert.strandh@gmail.com>
2014-03-20 15:58:03 +0100

    Added STRING-TRIM.

--- Code/String/General/string.lisp
+++ Code/String/General/string.lisp
@@ -80,3 +80,14 @@
 	 (copy (copy-string string)))
     (nstring-capitalize copy start end)))
 
+(defun string-trim (character-bag string-designator)
+  (flet ((in-bag-p (char) (find char character-bag)))
+    (let* ((string (string string-designator))
+	   (first (position-if-not #'in-bag-p string)))
+      (if (null first)
+	  string
+	  (let ((last (position-if-not #'in-bag-p string :from-end t)))
+	    (subseq string first (1+ last)))))))
+
+      
+    
