52fbc025653975772b8550f26b723b23f0b96775
Robert Strandh <robert.strandh@gmail.com>
2014-03-20 15:48:09 +0100

    Added STRING-CAPITALIZE and NSTRING-CAPITALIZE.

--- Code/String/General/string.lisp
+++ Code/String/General/string.lisp
@@ -57,3 +57,26 @@
   (let* ((string (string string-designator))
 	 (copy (copy-string string)))
     (nstring-downcase copy start end)))
+
+(defun nstring-capitalize (string &key (start 0) end)
+  (declare (type string string))
+  (let ((length (length string)))
+    (when (null end) (setf end length))
+    (check-bounding-indices string start end)
+    (loop with state = nil
+	  for i from start below end
+	  for char = (char string i)
+	  do (if state
+		 (if (alphanumericp char)
+		     (setf (char string i) (char-downcase char))
+		     (setf state nil))
+		 (when (alphanumericp char)
+		   (setf (char string i) (char-upcase char))
+		   (setf state t)))))
+  string)
+
+(defun string-capitalize (string-designator &key (start 0) end)
+  (let* ((string (string string-designator))
+	 (copy (copy-string string)))
+    (nstring-capitalize copy start end)))
+
