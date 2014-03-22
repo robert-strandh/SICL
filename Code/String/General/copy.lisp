48621d92b7ddca1f5426274423e949929f9dc881
Robert Strandh <robert.strandh@gmail.com>
2014-03-22 06:18:31 +0100

    Added new function for extracting an interval of a general string.

--- Code/String/General/copy.lisp
+++ Code/String/General/copy.lisp
@@ -16,6 +16,22 @@
 		   (schar string source-index)))
     result))
 
+;;; Extract an interval from a general (i.e., not necessarily simple)
+;;; string to a fresh copy.  We assume that the caller has checked
+;;; that START and END are valid bounding indices for the string.
+(defun extract-interval-general (string start end)
+  (declare (type string string)
+	   (type fixnum start end)
+	   (optimize (speed 3)))
+  (let ((result (make-string (- end start))))
+    ;; MAKE-STRING always returns a simple string.
+    (declare (type simple-string result))
+    (loop for source-index from start below end
+	  for destination-index from 0
+	  do (setf (schar result destination-index)
+		   (char string source-index)))
+    result))
+
 (defun copy-string (string)
   (let ((result (make-string (length string))))
     (loop for i from 0 below (length string)
