83f45462940978361d4662394d685116f908f77e
Robert Strandh <robert.strandh@gmail.com>
2014-03-22 08:00:43 +0100

    Added special version of NSTRING-DOWNCASE valid for simple base strings.

--- Code/String/General/case-conversion.lisp
+++ Code/String/General/case-conversion.lisp
@@ -62,6 +62,18 @@
 ;;;
 ;;; Functions NSTRING-DOWNCASE and STRING-DOWNCASE.
 
+(defun nstring-downcase-simple-base (string start end)
+  (assert (typep string 'simple-base-string))
+  (assert (>= start 0))
+  (assert (<= end (length string)))
+  (assert (<= start end))
+  (locally (declare (type simple-base-string string)
+		    (type fixnum start end)
+		    (optimize (speed 3) (safety 0) (debug 0)))
+    (loop for i of-type fixnum from start below end
+	  do (setf (schar string i) (char-downcase (schar string i)))))
+  string)
+
 (defun nstring-downcase (string &key (start 0) end)
   (declare (type string string))
   (let ((length (length string)))
