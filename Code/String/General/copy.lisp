c1c87197b962085526e83eb272d3a7757c1c808f
Robert Strandh <robert.strandh@gmail.com>
2014-03-22 06:51:41 +0100

    Optimize string extraction functions.

--- Code/String/General/copy.lisp
+++ Code/String/General/copy.lisp
@@ -1,33 +1,55 @@
 (cl:in-package #:sicl-string)
 
 ;;; Extract an interval from a simple string to a fresh copy.  We
-;;; assume that the caller has checked that START and END are valid
-;;; bounding indices for the string.
+;;; assume that the caller has checked that the STRING is a simple
+;;; string, and that START and END are valid bounding indices for
+;;; STRING.
 (defun extract-interval-simple (string start end)
-  (declare (type simple-string string)
-	   (type fixnum start end)
-	   (optimize (speed 3)))
+  (assert (simple-string-p string))
+  (assert (>= start 0))
+  (assert (<= end (length string)))
+  (assert (<= start end))
   (let ((result (make-string (- end start))))
+    (declare (type simple-string string)
+	     ;; The CLHS says that the length of an array is a fixnum,
+	     ;; so we know that START and END are both fixnums.
+	     (type fixnum start end)
+	     ;; Since we have checked that START and END are valid
+	     ;; bounding indices for STRING, we can set SAFETY to 0,
+	     ;; hoping that the compiler will not verify the indices
+	     ;; of the accesses to the strings.
+	     (optimize (speed 3) (safety 0) (debug 0)))
     ;; MAKE-STRING always returns a simple string.
     (declare (type simple-string result))
-    (loop for source-index from start below end
-	  for destination-index from 0
+    (loop for source-index of-type fixnum from start below end
+	  for destination-index of-type fixnum from 0
 	  do (setf (schar result destination-index)
 		   (schar string source-index)))
     result))
 
 ;;; Extract an interval from a general (i.e., not necessarily simple)
 ;;; string to a fresh copy.  We assume that the caller has checked
-;;; that START and END are valid bounding indices for the string.
+;;; that STRING is a string, and that START and END are valid bounding
+;;; indices for STRING.
 (defun extract-interval-general (string start end)
-  (declare (type string string)
-	   (type fixnum start end)
-	   (optimize (speed 3)))
+  (assert (stringp string))
+  (assert (>= start 0))
+  (assert (<= end (length string)))
+  (assert (<= start end))
   (let ((result (make-string (- end start))))
+    (declare (type string string)
+	     ;; The CLHS says that the length of an array is a fixnum,
+	     ;; so we know that START and END are both fixnums.
+	     (type fixnum start end)
+	     ;; Since we have checked that START and END are valid
+	     ;; bounding indices for STRING, we can set SAFETY to 0,
+	     ;; hoping that the compiler will not verify the indices
+	     ;; of the accesses to the strings.
+	     (optimize (speed 3) (safety 0) (debug 0)))
     ;; MAKE-STRING always returns a simple string.
     (declare (type simple-string result))
-    (loop for source-index from start below end
-	  for destination-index from 0
+    (loop for source-index of-type fixnum from start below end
+	  for destination-index of-type fixnum from 0
 	  do (setf (schar result destination-index)
 		   (char string source-index)))
     result))
