63a3fb76d0d4d1a52d9350c1b023577aa7b434d6
Robert Strandh <robert.strandh@gmail.com>
2014-03-22 07:45:55 +0100

    Added special version of NSTRING-UPCASE valid for simple base strings.

--- Code/String/General/case-conversion.lisp
+++ Code/String/General/case-conversion.lisp
@@ -11,6 +11,17 @@
     (loop for i of-type fixnum from start below end
 	  do (setf (schar string i) (char-upcase (schar string i))))))
 
+(defun nstring-upcase-simple-base (string start end)
+  (assert (typep string 'simple-base-string))
+  (assert (>= start 0))
+  (assert (<= end (length string)))
+  (assert (<= start end))
+  (locally (declare (type simple-base-string string)
+		    (type fixnum start end)
+		    (optimize (speed 3) (safety 0) (debug 0)))
+    (loop for i of-type fixnum from start below end
+	  do (setf (schar string i) (char-upcase (schar string i))))))
+
 (defun nstring-upcase (string &key (start 0) end)
   (declare (type string string))
   (let ((length (length string)))
