d5b2789c617bf6c33ec8324f38d3583df847b388
Robert Strandh <robert.strandh@gmail.com>
2014-03-22 08:01:11 +0100

    Added special version of NSTRING-DOWNCASE valid for simple strings.

--- Code/String/General/case-conversion.lisp
+++ Code/String/General/case-conversion.lisp
@@ -74,6 +74,18 @@
 	  do (setf (schar string i) (char-downcase (schar string i)))))
   string)
 
+(defun nstring-downcase-simple (string start end)
+  (assert (simple-string-p string))
+  (assert (>= start 0))
+  (assert (<= end (length string)))
+  (assert (<= start end))
+  (locally (declare (type simple-string string)
+		    (type fixnum start end)
+		    (optimize (speed 3) (safety 0) (debug 0)))
+    (loop for i of-type fixnum from start below end
+	  do (setf (schar string i) (char-downcase (schar string i)))))
+  string)
+
 (defun nstring-downcase (string &key (start 0) end)
   (declare (type string string))
   (let ((length (length string)))
