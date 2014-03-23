5e96455ae42960f538b0d82f713b5670a44adbe6
Robert Strandh <robert.strandh@gmail.com>
2014-03-23 07:14:55 +0100

    Added utilities for trim functions.

--- Code/String/General/trim.lisp
+++ Code/String/General/trim.lisp
@@ -1,5 +1,35 @@
 (cl:in-package #:sicl-string)
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;; Utilities.
+
+(defun character-in-list-bag-p (character bag)
+  (loop for c in bag
+	when (eql character c)
+	  return t))
+
+(declaim (inline character-in-list-bag-p))
+
+(defun character-in-simple-string-bag-p (character bag)
+  (loop for i from 0 below (length bag)
+	when (eql character (schar bag i))
+	  return t))
+
+(declaim (inline character-in-simple-string-bag-p))
+
+(defun character-in-general-string-bag-p (character bag)
+  (loop for i from 0 below (length bag)
+	when (eql character (char bag i))
+	  return t))
+
+(declaim (inline character-in-general-string-bag-p))
+
+
+(defun string-left-trim-list-simple-string
+    (character-bag string-designator)
+  
+
 (defun string-trim (character-bag string-designator)
   (flet ((in-bag-p (char) (find char character-bag)))
     (let* ((string (string string-designator))
