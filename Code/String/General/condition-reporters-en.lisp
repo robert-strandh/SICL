e7d9d89e90289b591ad6fd56e5ab408d5513dd3f
Robert Strandh <robert.strandh@gmail.com>
2014-03-24 10:57:27 +0100

    Improved English langauge report messages.

--- Code/String/General/condition-reporters-en.lisp
+++ Code/String/General/condition-reporters-en.lisp
@@ -2,14 +2,14 @@
 
 (defmethod report-string-condition ((c bag-is-dotted-list) stream)
   (format stream
-	  "A character bag must be a proper list of characters.~@
+	  "If a character bag is a list, it must be a proper list.~@
            But the following dotted list was found instead:~@
            ~s."
 	  (type-error-datum c)))
 
 (defmethod report-string-condition ((c bag-is-circular-list) stream)
   (format stream
-	  "A character bag must be a proper list of characters.~@
+	  "If a character bag is a list, it must be a proper list.~@
            But the following circular list was found instead:~@
            ~s."
 	  (type-error-datum c)))
