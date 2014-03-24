b1f45e2ba925f52ea88574f74b3b0db38d812a63
Robert Strandh <robert.strandh@gmail.com>
2014-03-24 10:56:51 +0100

    Added common named condition reporter for all conditions.

--- Code/String/General/conditions.lisp
+++ Code/String/General/conditions.lisp
@@ -1,10 +1,15 @@
 (cl:in-package #:sicl-string)
 
+(defgeneric report-string-condition (condition stream))
+
 (define-condition bag-is-dotted-list (type-error)
-  ())
+  ()
+  (:report report-string-condition))
 
 (define-condition bag-is-circular-list (type-error)
-  ())
+  ()
+  (:report report-string-condition))
 
 (define-condition bag-contains-non-character (type-error)
-  ())
+  ()
+  (:report report-string-condition))
