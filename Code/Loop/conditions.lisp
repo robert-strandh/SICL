529642439bd30e9faf92f36b11be5428c0aed64e
Robert Strandh <robert.strandh@gmail.com>
2014-03-28 07:20:13 +0100

    Replaced :report lambdas by name of report function.

--- Code/Loop/conditions.lisp
+++ Code/Loop/conditions.lisp
@@ -12,6 +12,8 @@
 
 (cl:in-package #:sicl-loop)
 
+(defgeneric report-loop-condition (condition stream))
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;
 ;;; Conditions for parsing
@@ -28,231 +30,108 @@
 
 (define-condition expected-var-spec-but-end (loop-parse-error)
   ()
-  (:report
-   (lambda (condition stream)
-     (declare (ignore condition))
-     (format stream
-	     "Expected a variable specification but reached ~
-              the end of the loop body"))))
+  (:report report-loop-condition))
 
 (define-condition expected-var-spec-but-found (loop-parse-error-found)
   ()
-  (:report
-   (lambda (condition stream)
-     (format stream
-	     "Expected a variable specification but found: ~s"
-	     (found condition)))))
+  (:report report-loop-condition))
 
 (define-condition expected-simple-var-but-end (loop-parse-error)
   ()
-  (:report
-   (lambda (condition stream)
-     (declare (ignore condition))
-     (format stream
-	     "Expected a simple variable but reached ~
-              the end of the loop body"))))
+  (:report report-loop-condition))
 
 (define-condition expected-simple-var-but-found (loop-parse-error-found)
   ()
-  (:report
-   (lambda (condition stream)
-     (format stream
-	     "Expected a simple variable but found: ~s"
-	     (found condition)))))
+  (:report report-loop-condition))
 
 (define-condition expected-type-spec-but-end (loop-parse-error)
   ()
-  (:report
-   (lambda (condition stream)
-     (declare (ignore condition))
-     (format stream
-	     "Expected a variable specification but reached ~
-              the end of the loop body"))))
+  (:report report-loop-condition))
 
 (define-condition expected-type-spec-but-found (loop-parse-error-found)
   ()
-  (:report
-   (lambda (condition stream)
-     (format stream
-	     "Expected a type specification but found: ~s"
-	     (found condition)))))
+  (:report report-loop-condition))
 
 (define-condition expected-compound-form-but-end (loop-parse-error)
   ()
-  (:report
-   (lambda (condition stream)
-     (declare (ignore condition))
-     (format stream
-	     "Expected a compound form but reached ~
-              the end of the loop body"))))
+  (:report report-loop-condition))
 
 (define-condition expected-compound-form-but-found (loop-parse-error-found)
   ()
-  (:report
-   (lambda (condition stream)
-     (format stream
-	     "Expected a compound form but found: ~s"
-	     (found condition)))))
+  (:report report-loop-condition))
 
 (define-condition expected-form-but-end (loop-parse-error)
   ()
-  (:report
-   (lambda (condition stream)
-     (declare (ignore condition))
-     (format stream
-	     "Expected a form but reached ~
-              the end of the loop body"))))
+  (:report report-loop-condition))
 
 (define-condition expected-symbol-but-end (loop-parse-error)
   ()
-  (:report
-   (lambda (condition stream)
-     (declare (ignore condition))
-     (format stream
-	     "Expected a symbol but reached ~
-              the end of the loop body"))))
+  (:report report-loop-condition))
 
 (define-condition expected-symbol-but-found (loop-parse-error-found)
   ()
-  (:report
-   (lambda (condition stream)
-     (format stream
-	     "Expected a symbol but found: ~s"
-	     (found condition)))))
+  (:report report-loop-condition))
 
 (define-condition expected-keyword-but-found (loop-parse-error-found)
   ()
-  (:report
-   (lambda (condition stream)
-     (format stream
-	     "Expected a loop keyword, but found: ~s"
-	     (found condition)))))
+  (:report report-loop-condition))
 
 (define-condition expected-for/as-subclause-but-end (loop-parse-error)
   ()
-  (:report
-   (lambda (condition stream)
-     (declare (ignore condition))
-     (format stream
-	     "Expected a loop keyword indicating a for/as ~
-              subclause, but reached the end of the loop body"))))
+  (:report report-loop-condition))
 
 (define-condition expected-symbol-but-found (loop-parse-error-found)
   ()
-  (:report
-   (lambda (condition stream)
-     (format stream
-	     "Expected a loop keyword indicating a for/as ~
-              subclause, but found: ~s"
-	     (found condition)))))
+  (:report report-loop-condition))
 
 (define-condition expected-each/the-but-end (loop-parse-error)
   ()
-  (:report
-   (lambda (condition stream)
-     (declare (ignore condition))
-     (format stream
-	     "Expected the loop keyword each/the, ~
-              but reached the end of the loop body"))))
+  (:report report-loop-condition))
 
 (define-condition expected-each/the-but-found (loop-parse-error-found)
   ()
-  (:report
-   (lambda (condition stream)
-     (format stream
-	     "Expected the loop keyword each/the, but found: ~s"
-	     (found condition)))))
+  (:report report-loop-condition))
 
 (define-condition expected-hash-or-package-but-end (loop-parse-error)
   ()
-  (:report
-   (lambda (condition stream)
-     (declare (ignore condition))
-     (format stream
-	     "Expected a loop keyword indicating a for/as-hash, ~
-              but reached the end of the loop body"))))
+  (:report report-loop-condition))
 
 (define-condition expected-hash-or-package-but-found (loop-parse-error-found)
   ()
-  (:report
-   (lambda (condition stream)
-     (format stream
-	     "Expected a loop keyword indicating a for/as-hash ~
-              or a for/as-package subclause, but found: ~s"
-	     (found condition)))))
+  (:report report-loop-condition))
 
 (define-condition expected-in/of-but-end (loop-parse-error)
   ()
-  (:report
-   (lambda (condition stream)
-     (declare (ignore condition))
-     (format stream
-	     "Expected the loop keyword in/or, ~
-              but reached the end of the loop body"))))
+  (:report report-loop-condition))
 
 (define-condition expected-in/of-but-found (loop-parse-error-found)
   ()
-  (:report
-   (lambda (condition stream)
-     (format stream
-	     "Expected the loop keyword in/or, but found: ~s"
-	     (found condition)))))
+  (:report report-loop-condition))
 
 (define-condition expected-hash-key-but-end (loop-parse-error)
   ()
-  (:report
-   (lambda (condition stream)
-     (declare (ignore condition))
-     (format stream
-	     "Expected (hash-key other-var), ~
-              but reached the end of the loop body"))))
+  (:report report-loop-condition))
 
 (define-condition expected-hash-value-but-end (loop-parse-error)
   ()
-  (:report
-   (lambda (condition stream)
-     (declare (ignore condition))
-     (format stream
-	     "Expected (hash-value other-var), ~
-              but reached the end of the loop body"))))
+  (:report report-loop-condition))
 
 (define-condition expected-hash-key-but-found (loop-parse-error-found)
   ()
-  (:report
-   (lambda (condition stream)
-     (format stream
-	     "Expected (hash-key other-var), but found: ~s"
-	     (found condition)))))
+  (:report report-loop-condition))
 
 (define-condition expected-hash-value-but-found (loop-parse-error-found)
   ()
-  (:report
-   (lambda (condition stream)
-     (format stream
-	     "Expected (hash-value other-var), but found: ~s"
-	     (found condition)))))
+  (:report report-loop-condition))
 
 (define-condition expected-preposition-but-end (loop-parse-error)
   ()
-  (:report
-   (lambda (condition stream)
-     (declare (ignore condition))
-     (format stream
-	     "Expected a for/as preposition, ~
-              but reached the end of the loop body"))))
+  (:report report-loop-condition))
 
 (define-condition too-many-prepositions-from-one-group (loop-parse-error)
   ()
-  (:report
-   (lambda (condition stream)
-     (format stream
-	     "Expected (hash-value other-var), but found: ~s"
-	     (found condition)))))
+  (:report report-loop-condition))
 
 (define-condition conflicting-stepping-directions (loop-parse-error)
   ()
-  (:report
-   (lambda (condition stream)
-     (format stream
-	     "Conflicting stepping directions."))))
-
+  (:report report-loop-condition))
