cc0f9e2bc959c23d320a20db9866c6430738155b
Robert Strandh <robert.strandh@gmail.com>
2014-03-31 10:07:59 +0200

    Added parser for VAR TYPE-SPEC FROM/UPFROM FORM TO/UPTO FORM BY FORM.

--- Code/Loop/for-as-arithmetic-clause.lisp
+++ Code/Loop/for-as-arithmetic-clause.lisp
@@ -20,3 +20,29 @@
 
 (defclass for-as-arithmetic (for-as-subclause) ())
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;; Parsers.
+
+(define-parser arithmetic-up-1-parser
+  (consecutive (lambda (var type-spec from form1 to form2 by form3)
+		 (declare (ignore from to by))
+		 (let ((to-var (gensym))
+		       (by-var (gensym)))
+		   (make-instance 'for-as-arithmetic
+		     :bindings `((,var ,form1)
+				 (,to-var ,form2)
+				 (,by-var ,form3))
+		     :termination `(when (>= ,var ,to-var) (go end))
+		     :step `(incf ,var ,by-var))))
+	       (singleton #'identity
+			  (lambda (x) (and (symbolp x) (not (constantp x)))))
+	       'type-spec-parser
+	       (alternative (keyword-parser 'from)
+			    (keyword-parser 'upfrom))
+	       (singleton #'identity (constantly t))
+	       (alternative (keyword-parser 'to)
+			    (keyword-parser 'upto))
+	       (singleton #'identity (constantly t))
+	       (keyword-parser 'by)
+	       (singleton #'identity (constantly t))))
