01a6574827406750dbfef28e39d69ae0c391c4fe
Robert Strandh <robert.strandh@gmail.com>
2014-04-02 07:15:00 +0200

    Added component containing parsers for VAR-SPEC.

--- Code/Loop/sicl-loop.asd
+++ Code/Loop/sicl-loop.asd
@@ -23,6 +23,7 @@
    (:file "clause")
    (:file "combinatory-parsing")
    (:file "parse-common")
+   (:file "var-spec")
    (:file "type-spec")
    (:file "initial-clause")
    (:file "final-clause")
---
+++ Code/Loop/var-spec.lisp
@@ -0,0 +1,37 @@
+;;;; Copyright (c) 2014
+;;;;
+;;;;     Robert Strandh (robert.strandh@gmail.com)
+;;;;
+;;;; all rights reserved. 
+;;;;
+;;;; Permission is hereby granted to use this software for any 
+;;;; purpose, including using, modifying, and redistributing it.
+;;;;
+;;;; The software is provided "as-is" with no warranty.  The user of
+;;;; this software assumes any responsibility of the consequences. 
+
+(cl:in-package #:sicl-loop)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;; Parser for d-var-spec.
+
+;;; A d-var-spec is a is a destructuring variable specifier: 
+;;; 
+;;;    d-var-spec ::= simple-var | nil | (d-var-spec . d-var-spec)
+;;;
+;;; where simple-var is a symbol (a name of a variable). 
+;;;
+
+;;; Return true if and only if the argument is a valid d-var-spec, in
+;;; other words if it is a tree of CONS cells where the leaves are
+;;; symbols.
+(defun d-var-spec-p (object)
+  (or (and (symbolp object)
+	   (not (constantp object)))
+      (and (consp object)
+	   (d-var-spec-p (car object))
+	   (d-var-spec-p (cdr object)))))
+
+(define-parser d-var-spec-parser
+  (singleton #'identity 'd-var-spec-p))
