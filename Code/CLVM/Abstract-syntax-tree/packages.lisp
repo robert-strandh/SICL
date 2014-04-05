8a756f1c6c5ffce285f79695af88d01d22409667
Robert Strandh <robert.strandh@gmail.com>
2014-04-05 12:09:20 +0200

    Definition of main package and package for Graphviz drawing.

---
+++ Code/CLVM/Abstract-syntax-tree/packages.lisp
@@ -0,0 +1,32 @@
+(cl:in-package #:common-lisp-user)
+
+(defpackage #:clvm-ast
+  (:use #:common-lisp)
+  (:export
+   #:ast #:children
+   #:immediate-ast #:make-immediate-ast
+   #:constant-ast #:make-constant-ast #:value
+   #:global-ast #:make-global-ast #:storage #:function-type
+   #:special-ast #:make-special-ast
+   #:lexical-ast #:make-lexical-ast
+   #:call-ast #:make-call-ast #:callee-ast #:argument-asts
+   #:block-ast #:make-block-ast #:body
+   #:function-ast #:make-function-ast
+   #:required-only-p #:required #:argparse-ast #:body-ast
+   #:go-ast #:make-go-ast #:tag-ast
+   #:if-ast #:make-if-ast #:test-ast #:then-ast #:else-ast
+   #:load-time-value-ast #:make-load-time-value-ast #:read-only-p
+   #:body-asts
+   #:progn-ast #:make-progn-ast #:form-asts
+   #:return-from-ast #:make-return-from-ast #:form-ast
+   #:setq-ast #:make-setq-ast #:lhs-ast #:value-ast
+   #:tagbody-ast #:make-tagbody-ast #:items
+   #:tag-ast #:make-tag-ast #:name
+   #:the-ast #:make-the-ast #:type-specifiers))
+
+(defpackage #:clvm-ast-graphviz
+  (:use #:common-lisp #:clvm-ast)
+  (:export
+   #:draw-ast))
+
+  
