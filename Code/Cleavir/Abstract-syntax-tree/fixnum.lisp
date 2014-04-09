f709453740560ddce5908a5a950d06cd7710cc24
Robert Strandh <robert.strandh@gmail.com>
2014-04-09 09:45:26 +0200

    Modified the way the FIXNUM-+-AST and fixnum--AST work.

--- Code/Cleavir/Abstract-syntax-tree/fixnum.lisp
+++ Code/Cleavir/Abstract-syntax-tree/fixnum.lisp
@@ -39,44 +39,41 @@
 ;;; Class FIXNUM-+-AST.
 ;;;
 ;;; This AST can be used to implement a binary addition function.  It
-;;; requires both its arguments to be of type FIXNUM.  It generates
-;;; two values that we may call RESULT (a FIXNUM) and OVERFLOWP (a
-;;; Boolean).  OVERFLOWP indicates whether the operation resulted in
-;;; an overflow.  When it is false, then there was no overflow, and
-;;; RESULT is the sum of the two arguments.  When OVERFLOWP is true,
-;;; the operation resulted in an overflow.  Then, if RESULT is
-;;; negative, then a BIGNUM with the value 2^n + RESULT should be
-;;; created, where n is the number of bits in a word.  If RESULT is
-;;; non-negative, then a BIGNUM with the value RESULT - 2^n should be
-;;; created.
+;;; requires both its arguments to be of type FIXNUM.  It can only
+;;; occur as the OPERATION-AST of a FIXNUM-ARITHMETIC-AST.  As a
+;;; result of the operation, this AST generates a FIXNUM that it
+;;; writes to VARIABLE.  When there is no overflow, VARIABLE is the
+;;; sum of the two FIXNUM arguments.  When there is an overflow, if
+;;; the result is negative, then a BIGNUM with the value 2^n +
+;;; VARIABLE should be created, where n is the number of bits in a
+;;; word.  If VARIABLE is non-negative, then a BIGNUM with the value
+;;; VARIABLE - 2^n should be created.
 
 (defclass fixnum-+-ast (ast)
   ())
 
-(defun make-fixnum-+-ast (arg1-ast arg2-ast)
+(defun make-fixnum-+-ast (arg1-ast arg2-ast variable-ast)
   (make-instance 'fixnum-+-ast
-    :children (list arg1-ast arg2-ast)))
+    :children (list arg1-ast arg2-ast variable-ast)))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;
 ;;; Class FIXNUM---AST.
 ;;;
 ;;; This AST can be used to implement a binary subtraction function.
-;;; It requires both its arguments to be of type FIXNUM.  It generates
-;;; two values that we may call RESULT (a FIXNUM) and OVERFLOWP (a
-;;; Boolean).  OVERFLOWP indicates whether the operation resulted in
-;;; an overflow.  When it is false, then there was no overflow, and
-;;; RESULT is the sum of the two arguments.  When OVERFLOWP is true,
-;;; the operation resulted in an overflow.  Then, if RESULT is
-;;; negative, then a BIGNUM with the value 2^n + RESULT should be
-;;; created, where n is the number of bits in a word.  If RESULT is
-;;; non-negative, then a BIGNUM with the value RESULT - 2^n should be
-;;; created.
+;;; It requires both its arguments to be of type FIXNUM.  It can only
+;;; occur as the OPERATION-AST of a FIXNUM-ARITHMETIC-AST.  As a
+;;; result of the operation, this AST generates a FIXNUM that it
+;;; writes to VARIABLE.  When there is no overflow, VARIABLE is the
+;;; sum of the two FIXNUM arguments.  When there is an overflow, if
+;;; the result is negative, then a BIGNUM with the value 2^n +
+;;; VARIABLE should be created, where n is the number of bits in a
+;;; word.  If VARIABLE is non-negative, then a BIGNUM with the value
+;;; VARIABLE - 2^n should be created.
 
 (defclass fixnum---ast (ast)
   ())
 
-(defun make-fixnum---ast (arg1-ast arg2-ast)
+(defun make-fixnum---ast (arg1-ast arg2-ast variable-ast)
   (make-instance 'fixnum---ast
-    :children (list arg1-ast arg2-ast)))
-
+    :children (list arg1-ast arg2-ast variable-ast)))
