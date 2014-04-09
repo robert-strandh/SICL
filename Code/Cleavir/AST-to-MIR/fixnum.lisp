030af59974df69c11abe3a20ed9e672e2374cb07
Robert Strandh <robert.strandh@gmail.com>
2014-04-09 11:44:10 +0200

    Added translation method for FIXNUM---AST.

--- Code/Cleavir/AST-to-MIR/fixnum.lisp
+++ Code/Cleavir/AST-to-MIR/fixnum.lisp
@@ -42,3 +42,28 @@
 				 (list result)
 				 successors)))))))))
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;; Compile a FIXNUM---AST.
+
+(defmethod compile-ast ((ast cleavir-ast:fixnum---ast) context)
+  (with-accessors ((results results)
+		   (successors successors))
+      context
+    (unless (and (= (length results) 1)
+		 (= (length successors) 2))
+      (error "Invalid context for FIXNUM---AST."))
+    (let ((temp1 (make-temp nil))
+	  (temp2 (make-temp nil))
+	  (result (find-or-create-location (cleavir-ast:variable-ast ast))))
+      (compile-ast
+       (cleavir-ast:arg1-ast ast)
+       (context (list temp1)
+		(list (compile-ast
+		       (cleavir-ast:arg2-ast ast)
+		       (context (list temp2)
+				(cleavir-mir:make-fixnum---instruction
+				 (list temp1 temp2)
+				 (list result)
+				 successors)))))))))
+
