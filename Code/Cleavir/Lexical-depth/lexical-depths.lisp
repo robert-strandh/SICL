94f85a17081ab10b1ab257853877b39089d4ef1b
Robert Strandh <robert.strandh@gmail.com>
2014-04-11 08:41:35 +0200

    Added function for computing instruction ownerships.

--- Code/Cleavir/Lexical-depth/lexical-depths.lisp
+++ Code/Cleavir/Lexical-depth/lexical-depths.lisp
@@ -1,5 +1,10 @@
 (cl:in-package #:cleavir-lexical-depth)
 
+;;;; As defined here, the set of DESCENDANTS of an instruction
+;;;; includes the instruction itself.
+
+;;; Given an ENTER-INSTRUCTION, call FUNCTION once for each descendant
+;;; of ENTER-INSTRUCTION.
 (defun map-instructions (function enter-instruction)
   (let ((table (make-hash-table :test #'eq)))
     (labels ((traverse (instruction)
@@ -9,3 +14,22 @@
 		 (mapc #'traverse (cleavir-mir:successors instruction)))))
       (traverse enter-instruction))))
 
+;;; Compute the owner of each instruction.  The owner of an
+;;; instruction I is the outermost ENTER-INSTRUCTION of all the
+;;; ENTER-INSTRUCTIONs which I is a descendant.  The return value is
+;;; an EQ hash table mapping instructions to their owners.
+(defun compute-instruction-ownerships (enter-instruction)
+  (let ((worklist (list enter-instruction))
+	(result (make-hash-table :test #'eq)))
+    (flet ((process-function (enter-instruction)
+	     (map-instructions
+	      (lambda (instruction)
+		(unless (gethash instruction result)
+		  (setf (gethash instruction result) enter-instruction))
+		(when (and (not (eq instruction enter-instruction))
+			   (typep instruction 'cleavir-mir:enter-instruction))
+		  (setf worklist (append worklist (list instruction)))))
+	      enter-instruction)))
+      (loop until (null worklist)
+	    do (process-function (pop worklist))))
+    result))
