a6348c7f41e0566b776751e45ca7ef4e1241bdf1
Robert Strandh <robert.strandh@gmail.com>
2014-04-12 08:11:02 +0200

    Added translation of RPLACA-AST and RPLACD-AST.

--- Code/Cleavir/AST-to-source/accessors.lisp
+++ Code/Cleavir/AST-to-source/accessors.lisp
@@ -5,3 +5,11 @@
 
 (defmethod to-source ((ast cleavir-ast:cdr-ast) dictionary)
   `(cleavir-low:cdr ,(to-source (cleavir-ast:cons-ast ast) dictionary)))
+
+(defmethod to-source ((ast cleavir-ast:rplaca-ast) dictionary)
+  `(cleavir-low:rplaca ,(to-source (cleavir-ast:cons-ast ast) dictionary)
+		       ,(to-source (cleavir-ast:object-ast ast) dictionary)))
+
+(defmethod to-source ((ast cleavir-ast:rplacd-ast) dictionary)
+  `(cleavir-low:rplacd ,(to-source (cleavir-ast:cons-ast ast) dictionary)
+		       ,(to-source (cleavir-ast:object-ast ast) dictionary)))
