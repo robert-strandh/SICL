2544931c408237f5eb6bf53980554dff7a3c2551
Robert Strandh <robert.strandh@gmail.com>
2014-04-12 08:51:28 +0200

    Added component for cloning an AST.

---
+++ Code/Cleavir/AST-transformations/clone.lisp
@@ -0,0 +1,22 @@
+(cl:in-package #:cleavir-ast-transformations)
+
+(defgeneric clone-node (ast))
+
+(defmethod clone-node (ast)
+  (make-instance (class-of ast)))
+
+(defmethod clone-node :around ((ast cleavir-ast:immediate-ast))
+  (reinitialize-instance
+   (call-next-method)
+   :value (cleavir-ast:value ast)))
+
+(defmethod clone-node :around ((ast cleavir-ast:constant-ast))
+  (reinitialize-instance
+   (call-next-method)
+   :value (cleavir-ast:value ast)))
+
+(defmethod clone-node :around ((ast cleavir-ast:global-ast))
+  (reinitialize-instance
+   (call-next-method)
+   :name (cleavir-ast:name ast)
+   :function-type (cleavir-ast:function-type ast)))
