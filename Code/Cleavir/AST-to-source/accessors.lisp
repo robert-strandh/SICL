c1adee300838d6e2fd406568146ebe6903a98810
Robert Strandh <robert.strandh@gmail.com>
2014-04-12 08:07:59 +0200

    Added translation of CDR-AST.

--- Code/Cleavir/AST-to-source/accessors.lisp
+++ Code/Cleavir/AST-to-source/accessors.lisp
@@ -3,3 +3,5 @@
 (defmethod to-source ((ast cleavir-ast:car-ast) dictionary)
   `(cleavir-low:car ,(to-source (cleavir-ast:cons-ast ast) dictionary)))
 
+(defmethod to-source ((ast cleavir-ast:cdr-ast) dictionary)
+  `(cleavir-low:cdr ,(to-source (cleavir-ast:cons-ast ast) dictionary)))
