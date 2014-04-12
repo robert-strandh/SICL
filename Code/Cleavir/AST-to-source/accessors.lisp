7a7ee0cfafd131d16427279957fd0de040ca86de
Robert Strandh <robert.strandh@gmail.com>
2014-04-12 08:07:01 +0200

    Use new low-level package for names of accessors.

--- Code/Cleavir/AST-to-source/accessors.lisp
+++ Code/Cleavir/AST-to-source/accessors.lisp
@@ -1,5 +1,5 @@
 (cl:in-package #:cleavir-ast-to-source)
 
 (defmethod to-source ((ast cleavir-ast:car-ast) dictionary)
-  `(%car ,(to-source (cleavir-ast:cons-ast ast) dictionary)))
+  `(cleavir-low:car ,(to-source (cleavir-ast:cons-ast ast) dictionary)))
 
