bf00f8dace727256624d05d148c462481292af91
Robert Strandh <robert.strandh@gmail.com>
2014-04-13 09:10:41 +0200

    Added translation of FIXNUM-+-AST.

--- Code/Cleavir/AST-to-source/fixnum.lisp
+++ Code/Cleavir/AST-to-source/fixnum.lisp
@@ -1,2 +1,5 @@
 (cl:in-package #:cleavir-ast-to-source)
 
+(defmethod to-source ((ast cleavir-ast:fixnum-+-ast) dictionary)
+  `(cleavir-low:fixnum-+ ,(to-source (cleavir-ast:arg1-ast ast))
+			 ,(to-source (cleavir-ast:arg2-ast ast))
