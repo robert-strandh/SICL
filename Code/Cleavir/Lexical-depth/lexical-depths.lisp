760c4f1e80fa616e709e14f2cadc7cbc875cd2b0
Robert Strandh <robert.strandh@gmail.com>
2014-04-13 06:09:42 +0200

    Added definitions of concepts to comments.

--- Code/Cleavir/Lexical-depth/lexical-depths.lisp
+++ Code/Cleavir/Lexical-depth/lexical-depths.lisp
@@ -1,7 +1,36 @@
 (cl:in-package #:cleavir-lexical-depth)
 
-;;;; As defined here, the set of DESCENDANTS of an instruction
-;;;; includes the instruction itself.
+;;;; An instruction A is a DESCENDANT of an instruction B if and only
+;;;; if it is possible to reach A by starting at B and following
+;;;; SUCCESSOR references.  As defined here, the set of DESCENDANTS of
+;;;; an instruction includes the instruction itself.
+;;;;
+;;;; Recall that an ENTER-INSTRUCTION is the successor or no other
+;;;; instruction.
+;;;;
+;;;; The OWNER of an instruction A is the outermost ENTER-INSTRUCTION
+;;;; of all the ENTER-INSTRUCTIONs of which A is a descendant.  The
+;;;; owner of a datum D is the outermost ENTER-INSTRUCTION of all the
+;;;; owners of all the instructions using D.
+;;;;
+;;;; Each ENTER-INSTRUCTION A defines a PROCEDURE which is a the set
+;;;; of all the instructions owned by A.  We extend the definition of
+;;;; ownership so that a procedure P is the owner of some instruction
+;;;; or datum X if an only if the unique ENTER-INSTRUCTION of P is the
+;;;; owner of X. 
+;;;;
+;;;; A procedure P is a LEXICAL PARENT of a procedure Q if and only if
+;;;; either some instruction in P is the direct successor of some
+;;;; instruction in Q, or some instruction A in Q refers to a datum
+;;;; owned by P. 
+;;;;
+;;;; The LEXICAL DEPTH of a procedure P is defined recursively as
+;;;; follows:
+;;;; 
+;;;;   * If P has no lexical parent then its lexical depth is 0.
+;;;;
+;;;;   * Otherwise, the lexical depth of P is d+1, where d is the
+;;;      maximum depth of any of its lexical parents. 
 
 ;;; Given an ENTER-INSTRUCTION, call FUNCTION once for each descendant
 ;;; of ENTER-INSTRUCTION.
@@ -14,12 +43,9 @@
 		 (mapc #'traverse (cleavir-mir:successors instruction)))))
       (traverse enter-instruction))))
 
-;;; Compute the owner of each instruction and each datum.  The owner
-;;; of an instruction I is the outermost ENTER-INSTRUCTION of all the
-;;; ENTER-INSTRUCTIONs which I is a descendant.  The owner of a datum
-;;; D is the outermost ENTER-INSTRUCTION of all the owners of all the
-;;; instructions using D.  The return value is an EQ hash table
-;;; mapping an instruction or a datum to its owner.
+;;; Compute the owner of each instruction and each datum.  The return
+;;; value is an EQ hash table mapping an instruction or a datum to its
+;;; owner.
 (defun compute-ownerships (enter-instruction)
   (let ((worklist (list enter-instruction))
 	(result (make-hash-table :test #'eq)))
