ea52aa4ac4e66434dc174795cb197d7829d7edce
Robert Strandh <robert.strandh@gmail.com>
2014-04-13 09:12:20 +0200

    Added translation of FIXNUM-<=-AST.

--- Code/Cleavir/AST-to-source/fixnum.lisp
+++ Code/Cleavir/AST-to-source/fixnum.lisp
@@ -2,13 +2,17 @@
 
 (defmethod to-source ((ast cleavir-ast:fixnum-+-ast) dictionary)
   `(cleavir-low:fixnum-+ ,(to-source (cleavir-ast:arg1-ast ast))
-			 ,(to-source (cleavir-ast:arg2-ast ast))
+			 ,(to-source (cleavir-ast:arg2-ast ast))))
 
 (defmethod to-source ((ast cleavir-ast:fixnum---ast) dictionary)
   `(cleavir-low:fixnum-- ,(to-source (cleavir-ast:arg1-ast ast))
-			 ,(to-source (cleavir-ast:arg2-ast ast))
+			 ,(to-source (cleavir-ast:arg2-ast ast))))
 
 (defmethod to-source ((ast cleavir-ast:fixnum-<-ast) dictionary)
   `(cleavir-low:fixnum-< ,(to-source (cleavir-ast:arg1-ast ast))
-			 ,(to-source (cleavir-ast:arg2-ast ast))
+			 ,(to-source (cleavir-ast:arg2-ast ast))))
+
+(defmethod to-source ((ast cleavir-ast:fixnum-<=-ast) dictionary)
+  `(cleavir-low:fixnum-<= ,(to-source (cleavir-ast:arg1-ast ast))
+			  ,(to-source (cleavir-ast:arg2-ast ast))))
 
