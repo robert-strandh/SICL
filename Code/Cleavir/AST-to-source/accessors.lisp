991802c001531d94b24b5445763ddcc77e500902
Robert Strandh <robert.strandh@gmail.com>
2014-04-14 11:15:09 +0200

    Reformated to make it more uniform.

--- Code/Cleavir/AST-to-source/accessors.lisp
+++ Code/Cleavir/AST-to-source/accessors.lisp
@@ -1,15 +1,19 @@
 (cl:in-package #:cleavir-ast-to-source)
 
 (defmethod to-source ((ast cleavir-ast:car-ast) dictionary)
-  `(cleavir-low:car ,(to-source (cleavir-ast:cons-ast ast) dictionary)))
+  `(cleavir-low:car
+    ,(to-source (cleavir-ast:cons-ast ast) dictionary)))
 
 (defmethod to-source ((ast cleavir-ast:cdr-ast) dictionary)
-  `(cleavir-low:cdr ,(to-source (cleavir-ast:cons-ast ast) dictionary)))
+  `(cleavir-low:cdr
+    ,(to-source (cleavir-ast:cons-ast ast) dictionary)))
 
 (defmethod to-source ((ast cleavir-ast:rplaca-ast) dictionary)
-  `(cleavir-low:rplaca ,(to-source (cleavir-ast:cons-ast ast) dictionary)
-		       ,(to-source (cleavir-ast:object-ast ast) dictionary)))
+  `(cleavir-low:rplaca
+    ,(to-source (cleavir-ast:cons-ast ast) dictionary)
+    ,(to-source (cleavir-ast:object-ast ast) dictionary)))
 
 (defmethod to-source ((ast cleavir-ast:rplacd-ast) dictionary)
-  `(cleavir-low:rplacd ,(to-source (cleavir-ast:cons-ast ast) dictionary)
-		       ,(to-source (cleavir-ast:object-ast ast) dictionary)))
+  `(cleavir-low:rplacd
+    ,(to-source (cleavir-ast:cons-ast ast) dictionary)
+    ,(to-source (cleavir-ast:object-ast ast) dictionary)))
