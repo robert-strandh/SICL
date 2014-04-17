94c20086c69d02c5b1ff5292a6ef618a742e0ede
Robert Strandh <robert.strandh@gmail.com>
2014-04-17 07:04:19 +0200

    Main entry point.

--- Code/Cleavir/Backends/MIR-interpreter/general.lisp
+++ Code/Cleavir/Backends/MIR-interpreter/general.lisp
@@ -34,6 +34,30 @@
 
 (defgeneric execute-instruction (instruction environment))
 
+(defun interpret-mir (enter-instruction environment arguments)
+  (push (make-hash-table :test #'eq) environment)
+  (let* ((lambda-list (cleavir-mir:lambda-list enter-instruction)) 
+	 (pos (position-if (lambda (x) (member x '(&optional &key)))
+			   lambda-list)))
+    (cond ((null pos)
+	   ;; Only required arguments.
+	   (when (< (length arguments) (length lambda-list))
+	     (error "too few arguments"))
+	   (when (> (length arguments) (length lambda-list))
+	     (error "too many arguments"))
+	   (loop for value in arguments
+		 for variable in lambda-list
+		 do (store-lexical variable environment value)))
+	  (t
+	   (error "can't handle &optional or &key yet")))
+    (let ((next (first (cleavir-mir:successors enter-instruction))))
+      (catch 'return
+	(loop do (execute-instruction next environment))))))
+
+(defun enclose (enter-instruction environment)
+  (lambda (&rest arguments)
+    (interpret-mir enter-instruction environment arguments)))
+
 (defmethod execute-instruction
     ((instruction cleavir-mir:assignment-instruction) environment)
   (let* ((input (car (cleavir-mir:inputs instruction)))
