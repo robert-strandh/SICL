71d1ea316892257c7c02fd7c954d273be91c580f
Robert Strandh <robert.strandh@gmail.com>
2014-04-22 10:11:47 +0200

    Remove package CLEAVIR-LOW.

--- Code/Cleavir/AST-to-source/packages.lisp
+++ Code/Cleavir/AST-to-source/packages.lisp
@@ -4,11 +4,3 @@
   (:use #:common-lisp)
   (:export
    #:ast-to-source))
-
-(defpackage #:cleavir-low
-  (:export
-   #:car #:cdr #:rplaca #:rplacd
-   #:fixnum-+ #:fixnum--
-   #:fixnum-< #:fixnum-<= #:fixnum-> #:fixnum->= #:fixnum-=
-   #:slot-read #:slot-write
-   #:aref #:aset))
