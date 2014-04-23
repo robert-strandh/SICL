c86d6e3e3f4b65bb8242d5efd888a663a636fd35
Robert Strandh <robert.strandh@gmail.com>
2014-04-23 09:49:49 +0200

    Moved fixnum converters to appropriate file.

--- Code/Cleavir/AST-to-source/ast-to-source.lisp
+++ Code/Cleavir/AST-to-source/ast-to-source.lisp
@@ -71,22 +71,3 @@
 (defmethod to-source ((ast cleavir-ast:progn-ast) dictionary)
   `(progn ,@(loop for child in (cleavir-ast:children ast)
 		  collect (to-source child dictionary))))
-
-(defmethod to-source ((ast cleavir-ast:fixnum-arithmetic-ast) dictionary)
-  `(cleavir-primop:fixnum-arithmetic 
-    ,(to-source (cleavir-ast:variable-ast ast) dictionary)
-    ,(to-source (cleavir-ast:operation-ast ast) dictionary)
-    ,(to-source (cleavir-ast:normal-ast ast) dictionary)
-    ,(to-source (cleavir-ast:overflow-ast ast) dictionary)))
-
-(defmethod to-source ((ast cleavir-ast:fixnum-+-ast) dictionary)
-  `(cleavir-primop:fixnum-+
-    ,(to-source (cleavir-ast:arg1-ast ast) dictionary)
-    ,(to-source (cleavir-ast:arg2-ast ast) dictionary)
-    ,(to-source (cleavir-ast:variable-ast ast) dictionary)))
-
-(defmethod to-source ((ast cleavir-ast:fixnum---ast) dictionary)
-  `(cleavir-primop:fixnum--
-    ,(to-source (cleavir-ast:arg1-ast ast) dictionary)
-    ,(to-source (cleavir-ast:arg2-ast ast) dictionary)
-    ,(to-source (cleavir-ast:variable-ast ast) dictionary)))
