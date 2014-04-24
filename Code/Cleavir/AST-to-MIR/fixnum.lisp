e73c0cfc314d534dc03a712ce6e16bc88c5409ed
Robert Strandh <robert.strandh@gmail.com>
2014-04-24 06:23:02 +0200

    Removed method for compiling FIXNUM-ARITHMETIC-AST.

--- Code/Cleavir/AST-to-MIR/fixnum.lisp
+++ Code/Cleavir/AST-to-MIR/fixnum.lisp
@@ -2,23 +2,6 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;
-;;; Compile a FIXNUM-ARITHMETIC-AST.
-
-(defmethod compile-ast ((ast cleavir-ast:fixnum-arithmetic-ast) context)
-  (with-accessors ((results results)
-		   (successors successors))
-      context
-    (unless (and (= (length results) 1)
-		 (= (length successors) 1))
-      (error "Invalid context for FIXNUM-ARITHMETIC-AST."))
-    (let ((temp (find-or-create-location (cleavir-ast:variable-ast ast)))
-	  (normal (compile-ast (cleavir-ast:normal-ast ast) context))
-	  (overflow (compile-ast (cleavir-ast:overflow-ast ast) context)))
-      (compile-ast (cleavir-ast:operation-ast ast)
-		   (context (list temp) (list normal overflow))))))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;;
 ;;; Compile a FIXNUM-+-AST.
 
 (defmethod compile-ast ((ast cleavir-ast:fixnum-+-ast) context)
