c9204bca68c50ce7adcdf4ced22acd3a7b4c105d
Robert Strandh <robert.strandh@gmail.com>
2014-04-24 06:18:18 +0200

    Removed translator of FIXNUM-ARITHMETIC-AST to source.

--- Code/Cleavir/AST-to-source/fixnum.lisp
+++ Code/Cleavir/AST-to-source/fixnum.lisp
@@ -1,12 +1,5 @@
 (cl:in-package #:cleavir-ast-to-source)
 
-(defmethod to-source ((ast cleavir-ast:fixnum-arithmetic-ast) dictionary)
-  `(cleavir-primop:fixnum-arithmetic 
-    ,(to-source (cleavir-ast:variable-ast ast) dictionary)
-    ,(to-source (cleavir-ast:operation-ast ast) dictionary)
-    ,(to-source (cleavir-ast:normal-ast ast) dictionary)
-    ,(to-source (cleavir-ast:overflow-ast ast) dictionary)))
-
 (defmethod to-source ((ast cleavir-ast:fixnum-+-ast) dictionary)
   `(cleavir-primop:fixnum-+
     ,(to-source (cleavir-ast:arg1-ast ast) dictionary)
