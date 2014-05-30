65c26dcd29aed5fd60e35c32da03e7d69fe86d73
Robert Strandh <robert.strandh@gmail.com>
2014-05-30 07:25:10 +0200

    Added LONG-FLOAT-LESS-INSTRUCTION.

--- Code/Cleavir/Medium-level-intermediate-representation/float.lisp
+++ Code/Cleavir/Medium-level-intermediate-representation/float.lisp
@@ -707,6 +707,27 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;
+;;; INSTRUCTION LONG-FLOAT-LESS-INSTRUCTION.
+;;;
+;;; This instruction takes two inputs which must be values of type
+;;; unboxed LONG-FLOAT.  It has no outputs.  It has two successors;
+;;; the first one is chosen when the first input is strictly less than
+;;; the second one, otherwise the second successor is chosen.
+;;;
+;;; This instruction can be used by implementations that support the
+;;; LONG-FLOAT data type.
+
+(defclass long-float-less-instruction (instruction two-successors-mixin)
+  ())
+
+(defun make-long-float-less-instruction (input1 input2 successor1 successor2)
+  (make-instance 'long-float-less-instruction
+    :inputs (list input1 input2)
+    :outputs '()
+    :successors (list successor1 successor2)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
 ;;; INSTRUCTION LONG-FLOAT-SIN-INSTRUCTION.
 ;;;
 ;;; This instruction takes a single inputs which must be a value of
