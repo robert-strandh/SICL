5cdc608f6cc3565a132f139ac8f4ade21dbfe02f
Robert Strandh <robert.strandh@gmail.com>
2014-05-30 07:26:40 +0200

    Added SHORT-FLOAT-NOT-GREATER-INSTRUCTION.

--- Code/Cleavir/Medium-level-intermediate-representation/float.lisp
+++ Code/Cleavir/Medium-level-intermediate-representation/float.lisp
@@ -143,6 +143,28 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;
+;;; INSTRUCTION SHORT-FLOAT-NOT-GREATER-INSTRUCTION.
+;;;
+;;; This instruction takes two inputs which must be values of type
+;;; unboxed SHORT-FLOAT.  It has no outputs.  It has two successors;
+;;; the first one is chosen when the first input is less than or equal
+;;; to the second one, otherwise the second successor is chosen.
+;;;
+;;; This instruction can be used by implementations that support the
+;;; SHORT-FLOAT data type.
+
+(defclass short-float-not-greater-instruction (instruction two-successors-mixin)
+  ())
+
+(defun make-short-float-not-greater-instruction
+    (input1 input2 successor1 successor2)
+  (make-instance 'short-float-not-greater-instruction
+    :inputs (list input1 input2)
+    :outputs '()
+    :successors (list successor1 successor2)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
 ;;; INSTRUCTION SHORT-FLOAT-SIN-INSTRUCTION.
 ;;;
 ;;; This instruction takes a single inputs which must be a value of
