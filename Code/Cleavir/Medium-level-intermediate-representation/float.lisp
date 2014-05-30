4d7ae296431f8e15f354c88bed4b5b49f62a2af6
Robert Strandh <robert.strandh@gmail.com>
2014-05-30 07:29:55 +0200

    Added SINGLE-FLOAT-EQUAL-INSTRUCTION.

--- Code/Cleavir/Medium-level-intermediate-representation/float.lisp
+++ Code/Cleavir/Medium-level-intermediate-representation/float.lisp
@@ -391,6 +391,27 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;
+;;; INSTRUCTION SINGLE-FLOAT-EQUAL-INSTRUCTION.
+;;;
+;;; This instruction takes two inputs which must be values of type
+;;; unboxed SINGLE-FLOAT.  It has no outputs.  It has two successors;
+;;; the first one is chosen when the first input is equal to the
+;;; second one, otherwise the second successor is chosen.
+;;;
+;;; This instruction can be used by implementations that support the
+;;; SINGLE-FLOAT data type.
+
+(defclass single-float-equal-instruction (instruction two-successors-mixin)
+  ())
+
+(defun make-single-float-equal-instruction (input1 input2 successor1 successor2)
+  (make-instance 'single-float-equal-instruction
+    :inputs (list input1 input2)
+    :outputs '()
+    :successors (list successor1 successor2)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
 ;;; INSTRUCTION SINGLE-FLOAT-SIN-INSTRUCTION.
 ;;;
 ;;; This instruction takes a single inputs which must be a value of
