3313214a94e13227e4f908266e4a41fbb651116d
Robert Strandh <robert.strandh@gmail.com>
2014-06-01 06:28:14 +0200

    Created macro for code factoring.

--- Code/Cleavir/Abstract-syntax-tree/float.lisp
+++ Code/Cleavir/Abstract-syntax-tree/float.lisp
@@ -1,5 +1,18 @@
 (in-package #:cleavir-ast)
 
+(defmacro make-float-ast (name)
+  `(progn 
+     (defclass ,name (ast)
+       ((%arg1-ast :initarg :arg1-ast :reader arg1-ast)
+	(%arg2-ast :initarg :arg2-ast :reader arg2-ast)))
+
+     (cleavir-io:define-save-info ,name
+       (:arg1-ast arg1-ast)
+       (:arg2-ast arg2-ast))
+
+     (defmethod children ((ast ,name))
+       (list (arg1-ast ast) (arg2-ast ast)))))
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;
 ;;; Class SHORT-FLOAT-ADD-AST.
@@ -12,21 +25,7 @@
 ;;; Both inputs must be of type SHORT-FLOAT, so in safe code this
 ;;; restriction has to be checked before this AST is evaluated. 
 
-(defclass short-float-add-ast (ast)
-  ((%arg1-ast :initarg :arg1-ast :reader arg1-ast)
-   (%arg2-ast :initarg :arg2-ast :reader arg2-ast)))
-
-(defun make-short-float-add-ast (arg1-ast arg2-ast)
-  (make-instance 'short-float-add-ast
-    :arg1-ast arg1-ast
-    :arg2-ast arg2-ast))
-
-(cleavir-io:define-save-info short-float-add-ast
-  (:arg1-ast arg1-ast)
-  (:arg2-ast arg2-ast))
-
-(defmethod children ((ast short-float-add-ast))
-  (list (arg1-ast ast) (arg2-ast ast)))
+(make-float-ast short-float-add-ast)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;
