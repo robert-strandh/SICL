fde587f0d5f54b3511f698f2cd18eab4da5e3687
Robert Strandh <robert.strandh@gmail.com>
2014-06-03 07:03:32 +0200

    Method for compiling SHORT-FLOAT-LESS-AST.

--- Code/Cleavir/AST-to-MIR/float.lisp
+++ Code/Cleavir/AST-to-MIR/float.lisp
@@ -17,3 +17,17 @@
 (defun check-context-for-boolean-ast (context)
   (assert (and (zerop (length (results context)))
 	       (= (length (successors context)) 2))))
+
+(defmethod compile-ast ((ast cleavir-ast:short-float-less-ast) context)
+  (check-context-for-boolean-ast context)
+  (let* ((arguments (cleavir-ast:children ast))
+	 (temps (make-temps arguments)))
+    (compile-and-unbox-arguments
+     arguments
+     temps
+     'cleavir-mir:short-float-unbox-instruction
+     (make-instance 'cleavir-mir:short-float-less-instruction
+       :inputs temps
+       :outputs '()
+       :successors (successors context)))))
+     
