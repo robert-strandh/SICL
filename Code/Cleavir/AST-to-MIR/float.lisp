237f17e0b188ae8077ae2ea50d661c3a6637758e
Robert Strandh <robert.strandh@gmail.com>
2014-06-03 07:13:45 +0200

    Method for compiling DOUBLE-FLOAT-GREATER-AST.

--- Code/Cleavir/AST-to-MIR/float.lisp
+++ Code/Cleavir/AST-to-MIR/float.lisp
@@ -173,3 +173,16 @@
        :inputs temps
        :outputs '()
        :successors (successors context)))))
+
+(defmethod compile-ast ((ast cleavir-ast:double-float-greater-ast) context)
+  (check-context-for-boolean-ast context)
+  (let* ((arguments (cleavir-ast:children ast))
+	 (temps (make-temps arguments)))
+    (compile-and-unbox-arguments
+     arguments
+     temps
+     'cleavir-mir:double-float-unbox-instruction
+     (make-instance 'cleavir-mir:double-float-less-instruction
+       :inputs (reverse temps)
+       :outputs '()
+       :successors (successors context)))))
