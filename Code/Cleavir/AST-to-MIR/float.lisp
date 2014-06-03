6a61607e6ad988f8c3c4ac3316e17eccea5a5144
Robert Strandh <robert.strandh@gmail.com>
2014-06-03 06:52:13 +0200

    Function for compiling and unboxing arguments.

--- Code/Cleavir/AST-to-MIR/float.lisp
+++ Code/Cleavir/AST-to-MIR/float.lisp
@@ -1,1 +1,15 @@
 (cl:in-package #:cleavir-ast-to-mir)
+
+(defun compile-and-unbox-arguments
+    (arguments temps unbox-instruction-class successor)
+  (loop with succ = successor
+	for arg in (reverse arguments)
+	for temp in (reverse temps)
+	for inter = (make-temp nil)
+	do (setf succ
+		 (make-instance unbox-instruction-class
+		   :inputs (list inter)
+		   :outputs (list temp)
+		   :successors (list succ)))
+	   (setf succ (compile-ast arg (context `(,inter) `(,succ))))
+	finally (return succ)))
