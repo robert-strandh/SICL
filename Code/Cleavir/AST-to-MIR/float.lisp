95498263d0393ab59f34049db06a64aa20b74e21
Robert Strandh <robert.strandh@gmail.com>
2014-06-03 07:12:22 +0200

    Method for compiling SINGLE-FLOAT-NOT-LESS-AST.

--- Code/Cleavir/AST-to-MIR/float.lisp
+++ Code/Cleavir/AST-to-MIR/float.lisp
@@ -121,3 +121,16 @@
        :inputs (reverse temps)
        :outputs '()
        :successors (successors context)))))
+     
+(defmethod compile-ast ((ast cleavir-ast:single-float-not-less-ast) context)
+  (check-context-for-boolean-ast context)
+  (let* ((arguments (cleavir-ast:children ast))
+	 (temps (make-temps arguments)))
+    (compile-and-unbox-arguments
+     arguments
+     temps
+     'cleavir-mir:single-float-unbox-instruction
+     (make-instance 'cleavir-mir:single-float-not-greater-instruction
+       :inputs (reverse temps)
+       :outputs '()
+       :successors (successors context)))))
