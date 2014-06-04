721e550b3ed0b97465ff9f6e01faf3c851ce6fe3
Robert Strandh <robert.strandh@gmail.com>
2014-06-04 09:26:43 +0200

    Method on COMPILE-AST for SLOT-WRITE-AST.

--- Code/Cleavir/AST-to-MIR/accessors.lisp
+++ Code/Cleavir/AST-to-MIR/accessors.lisp
@@ -90,4 +90,28 @@
 			      :outputs (results context)
 			      :successors (successors context))))))))))
   
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;; Compile a SLOT-WRITE-AST
 
+(defmethod compile-ast ((ast cleavir-ast:slot-write-ast) context)
+  (check-context-for-no-value-ast context)
+  (let ((temp1 (make-temp nil))
+	(temp2 (make-temp nil))
+	(temp3 (make-temp nil)))
+    (compile-ast
+     (cleavir-ast:object-ast ast)
+     (context
+      (list temp1)
+      (list (compile-ast
+	     (cleavir-ast:slot-number-ast ast)
+	     (context
+	      (list temp2)
+	      (compile-ast
+	       (cleavir-ast:value-ast ast)
+	       (context
+		(list temp3)
+		(list (make-instance 'cleavir-mir:slot-write-instruction
+			:inputs (list temp1 temp2 temp3)
+			:outputs '()
+			:successors (successors context))))))))))))
