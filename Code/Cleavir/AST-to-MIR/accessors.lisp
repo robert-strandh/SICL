8df4b7aa302fad47a4219c21ec7ca17a1def69d8
Robert Strandh <robert.strandh@gmail.com>
2014-06-04 09:19:04 +0200

    Take advantage of :AROUND method for NO-VALUE-AST-MIXIN.

--- Code/Cleavir/AST-to-MIR/accessors.lisp
+++ Code/Cleavir/AST-to-MIR/accessors.lisp
@@ -35,41 +35,37 @@
 ;;; Compile a RPLACA-AST
 
 (defmethod compile-ast ((ast cleavir-ast:rplaca-ast) context)
-  (with-accessors ((results results)
-		   (successors successors))
-      context
-    (unless (and (null results) (= (length successors) 1))
-      (error "Attempt to compile a RPLACA-AST in a context requiring a value."))
-    (let* ((succ (car successors))
-	   (temp1 (make-temp nil))
-	   (temp2 (make-temp nil))
-	   (next (cleavir-mir:make-rplaca-instruction temp1 temp2 succ)))
-      (compile-ast
-       (cleavir-ast:cons-ast ast)
-       (context
-	(list temp1)
-	(list (compile-ast
-	       (cleavir-ast:object-ast ast)
-	       (context (list temp2) (list next)))))))))
+  (check-context-for-no-value-ast context)
+  (let ((temp1 (make-temp nil))
+	(temp2 (make-temp nil)))
+    (compile-ast
+     (cleavir-ast:cons-ast ast)
+     (context
+      (list temp1)
+      (list (compile-ast
+	     (cleavir-ast:object-ast ast)
+	     (context (list temp2)
+		      (list (make-instance 'cleavir-mir:rplaca-instruction
+			      :inputs (list temp1 temp2)
+			      :outputs '()
+			      :successors (successors context))))))))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;
 ;;; Compile a RPLACD-AST
 
 (defmethod compile-ast ((ast cleavir-ast:rplacd-ast) context)
-  (with-accessors ((results results)
-		   (successors successors))
-      context
-    (unless (and (null results) (= (length successors) 1))
-      (error "Attempt to compile a RPLACD-AST in a context requiring a value."))
-    (let* ((succ (car successors))
-	   (temp1 (make-temp nil))
-	   (temp2 (make-temp nil))
-	   (next (cleavir-mir:make-rplacd-instruction temp1 temp2 succ)))
-      (compile-ast
-       (cleavir-ast:cons-ast ast)
-       (context
-	(list temp1)
-	(list (compile-ast
-	       (cleavir-ast:object-ast ast)
-	       (context (list temp2) (list next)))))))))
+  (check-context-for-no-value-ast context)
+  (let ((temp1 (make-temp nil))
+	(temp2 (make-temp nil)))
+    (compile-ast
+     (cleavir-ast:cons-ast ast)
+     (context
+      (list temp1)
+      (list (compile-ast
+	     (cleavir-ast:object-ast ast)
+	     (context (list temp2)
+		      (list (make-instance 'cleavir-mir:rplacd-instruction
+			      :inputs (list temp1 temp2)
+			      :outputs '()
+			      :successors (successors context))))))))))
