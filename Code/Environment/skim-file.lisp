6775d596c712fa33f42660344b8d479ebc487a90
Robert Strandh <robert.strandh@gmail.com>
2014-06-11 07:29:29 +0200

    New generic function SKIM-COMPOUND-FORM.

--- Code/Environment/skim-file.lisp
+++ Code/Environment/skim-file.lisp
@@ -1,2 +1,9 @@
 (cl:in-package #:sicl-global-environment)
 
+;;; Skim a compound form with respect to ENVIRONMENT, where HEAD is
+;;; the CAR of the form, FORM is the entire form. 
+(defgeneric skim-compound-form (head form environment))
+
+(defmethod skim-compound-form (head form environment)
+  (declare (ignore head form environment))
+  nil)
