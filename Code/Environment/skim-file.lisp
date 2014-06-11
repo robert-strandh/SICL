bb19e8f236ad3d602706fb1179dfd357b6600baa
Robert Strandh <robert.strandh@gmail.com>
2014-06-11 07:34:08 +0200

    New function SKIM-FORM.

--- Code/Environment/skim-file.lisp
+++ Code/Environment/skim-file.lisp
@@ -4,6 +4,12 @@
 ;;; the CAR of the form, FORM is the entire form. 
 (defgeneric skim-compound-form (head form environment))
 
+;;; Skim FORM with respect to ENVIRONMENT.
+(defun skim-form (form environment)
+  (let ((expanded-form (fully-expand-form form environment)))
+    (when (consp expanded-form)
+      (skim-compound-form (car expanded-form) expanded-form environment))))
+
 (defmethod skim-compound-form (head form environment)
   (declare (ignore head form environment))
   nil)
