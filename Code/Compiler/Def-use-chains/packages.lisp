c6a971141a79619abf471c7c0199427d20e50028
Robert Strandh <robert.strandh@gmail.com>
2014-06-12 08:57:10 +0200

    Removed Def-use-chains system that has moved to Cleavir.

--- Code/Compiler/Def-use-chains/packages.lisp
+++
@@ -1,7 +0,0 @@
-(cl:in-package #:common-lisp-user)
-
-(defpackage #:sicl-compiler-def-use-chains
-  (:use #:common-lisp)
-  (:export
-   #:def-use-chains
-   ))
--- Code/Compiler/Def-use-chains/test-packages.lisp
+++
@@ -1,6 +0,0 @@
-(cl:in-package #:common-lisp-user)
-
-(defpackage #:sicl-compiler-def-use-chains-test
-  (:use #:common-lisp)
-  (:export
-   #:test-def-use-chains))
