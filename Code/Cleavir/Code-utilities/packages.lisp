fc066855d575ba8e71dd12edd0ad328055a376c8
Robert Strandh <robert.strandh@gmail.com>
2014-09-09 07:39:33 +0200

    New component: packages

---
+++ Code/Cleavir/Code-utilities/packages.lisp
@@ -0,0 +1,44 @@
+(in-package #:common-lisp-user)
+
+(defpackage #:cleavir-code-utilities
+  (:export #:list-structure
+	   #:proper-list-p
+	   #:proper-list-length
+	   #:dotted-list-p
+	   #:dotted-list-length
+	   #:proper-or-dotted-list-length
+	   #:form-must-be-proper-list
+	   #:check-form-proper-list
+	   #:invalid-number-of-arguments
+	   #:check-argcount
+	   #:circular-list-p
+	   #:lambda-list
+	   #:required
+	   #:environment
+	   #:whole
+	   #:optionals
+	   #:rest-body
+	   #:keys
+	   #:allow-other-keys
+	   #:aux
+	   #:parse-ordinary-lambda-list
+	   #:parse-generic-function-lambda-list
+	   #:parse-specialized-lambda-list
+	   #:parse-macro-lambda-list
+	   #:parse-destructuring-lambda-list
+	   #:preprocess-lambda-list
+	   #:parse-deftype-lambda-list
+	   #:parse-defsetf-lambda-list
+	   #:parse-define-modify-macro-lambda-list
+	   #:parse-define-method-combination-arguments-lambda-list
+	   #:lambda-list-variables
+	   #:destruture-lambda-list
+	   #:match-lambda-list
+	   #:parse-macro
+	   #:parse-deftype
+	   #:lambda-lists-congruent-p
+	   #:generate-congruent-lambda-list
+	   #:canonicalize-declaration-specifiers
+	   #:separate-ordinary-body
+	   #:separate-function-body))
+
