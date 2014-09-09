e2603e0f09443a0d67bee58578bbcc88b5862dc3
Robert Strandh <robert.strandh@gmail.com>
2014-09-09 16:38:51 +0200

    Comments and method for local functions.

--- Code/Cleavir/Generate-AST/convert-form.lisp
+++ Code/Cleavir/Generate-AST/convert-form.lisp
@@ -1,5 +1,9 @@
 (cl:in-package #:cleavir-generate-ast)
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;; Converting a symbol that has a definition as a symbol macro.
+
 (defmethod convert-form (form (info cleavir-env:symbol-macro-info) env)
   (let ((expansion (funcall (coerce *macroexpand-hook* 'function)
 			    (lambda (form env)
@@ -9,16 +13,34 @@
 			    env)))
     (convert expansion env)))
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;; Converting a symbol that has a definition as a constant variable.
+
 (defmethod convert-form (form (info cleavir-env:constant-variable-info) env)
   (cleavir-ast:make-constant-ast (cleavir-env:value info)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;; Converting a symbol that has a definition as a lexical variable.
 
 (defmethod convert-form (form (info cleavir-env:lexical-variable-info) env)
   (when (eq (cleavir-env:ignore info) 'ignore)
     (warn "Reference to a variable declared IGNORE"))
   (find-or-create-ast (cleavir-env:identity info)))
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;; Converting a symbol that has a definition as a special variable.
+;;; We do this by generating a call to SYMBOL-VALUE.
+
 (defmethod convert-form (form (info cleavir-env:special-variable-info) env)
   (convert `(symbol-value ',(cleavir-env:name info)) env))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;; Converting a compound form that calls a local macro.
+;;; A local macro can not have a compiler macro associated with it.
 
 (defmethod convert-form (form (info cleavir-env:local-macro-info) env)
   (let ((expansion (funcall (coerce *macroexpand-hook* 'function)
@@ -26,6 +48,11 @@
 			    form
 			    env)))
     (convert expansion env)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;; Converting a compound form that calls a global macro.
+;;; A global macro can have a compiler macro associated with it.
 
 (defmethod convert-form (form (info cleavir-env:global-macro-info) env)
   (let ((compiler-macro (cleavir-env:compiler-macro info)))
@@ -59,6 +86,21 @@
 	      ;; taking into account the real macro expander.
 	      (convert expanded-form env))))))
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;; Converting a compound form that calls a local function.
+;;; A local function can not have a compiler macro associated with it.
+
+(defmethod convert-form (form (info cleavir-env:local-function-info) env)
+  (let ((function-ast (find-or-create-ast (cleavir-env:name info)))
+	(argument-asts (convert-sequence (cdr form) env)))
+    (cleavir-ast:make-call-ast function-ast argument-asts)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;; Converting a compound form that calls a global function.
+;;; A global function can have  compiler macro associated with it.
+
 (defmethod convert-form (form (info cleavir-env:global-function-info) env)
   (let ((compiler-macro (cleavir-env:compiler-macro info)))
     (if (null compiler-macro)
@@ -84,3 +126,6 @@
 	      ;; macro replaced the original form with a new form.
 	      ;; This new form must then be converted.
 	      (convert expanded-form env))))))
+
+(defgeneric convert-special (head form environment))
+
