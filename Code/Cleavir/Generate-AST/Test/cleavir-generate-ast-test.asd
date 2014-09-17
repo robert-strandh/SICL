ce029121d14a71bc76547977d5e0de70d8b8ed51
Robert Strandh <robert.strandh@gmail.com>
2014-09-17 14:31:19 +0200

    Beginning of unit tests for minimal compilation.

---
+++ Code/Cleavir/Generate-AST/Test/cleavir-generate-ast-test.asd
@@ -0,0 +1,7 @@
+(cl:in-package #:common-lisp-user)
+
+(asdf:defsystem :cleavir-generate-ast-test
+  :depends-on (:cleavir-generate-ast)
+  :serial t
+  :components
+  ((:file "minimal-compilation")))
---
+++ Code/Cleavir/Generate-AST/Test/minimal-compilation.lisp
@@ -0,0 +1,50 @@
+(cl:in-package #:common-lisp-user)
+
+(defpackage #:cleavir-test-minimal-compilation
+  (:use #:common-lisp))
+
+(in-package #:cleavir-test-minimal-compilation)
+
+(eval-when (:compile-toplevel :load-toplevel :execute)
+  (defclass bogus-environment () ()))
+
+(defparameter *e* (make-instance 'bogus-environment))
+
+;;; Any variable not otherwise mentioned explicitly is considered to
+;;; be a special variable.
+(defmethod cleavir-env:variable-info
+    ((environment bogus-environment) symbol)
+  (make-instance 'cleavir-env:special-variable-info
+    :name symbol))
+
+;;; Any function not otherwise mentioned explicitly is considered to
+;;; be a global function.
+(defmethod cleavir-env:function-info
+    ((environment bogus-environment) name)
+  (make-instance 'cleavir-env:global-function-info
+    :name name))
+
+;;; When the name UNDEFINED-VARIABLE is used as a global variable,
+;;; then return NIL to indicate that there is no such variable.
+(defmethod cleavir-env:variable-info
+    ((environment bogus-environment) (name (eql 'undefined-variable)))
+  nil)
+
+;;; When the name UNDEFINED-FUNCTION is used as a global function,
+;;; then return NIL to indicate that there is no such function.
+(defmethod cleavir-env:function-info
+    ((environment bogus-environment) (name (eql 'undefined-function)))
+  nil)
+
+(defun run-tests ()
+  (assert (equal (cleavir-generate-ast:minimally-compile
+		  'hello
+		  *e*)
+		 'hello))
+  (assert (equal (cleavir-generate-ast:minimally-compile
+		  '(hello)
+		  *e*)
+		 '(hello)))
+  (format t "Tests passed~%"))
+
+(run-tests)
