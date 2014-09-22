401c1181e92a2d2bfdbf1f80b372d3803e8730e5
Robert Strandh <robert.strandh@gmail.com>
2014-09-22 16:52:45 +0200

    Test that a &AUX parameter shadows other &AUX initform.

--- Code/Cleavir/Generate-AST/Test/minimal-compilation.lisp
+++ Code/Cleavir/Generate-AST/Test/minimal-compilation.lisp
@@ -215,6 +215,16 @@
 		  *e*)
 		 `(flet ((fun (&key ((:gsm1 gsm1) 12)) gsm1))
 		    (fun (hello1 hello2)))))
+  ;; Test that an &AUX parameter of the local function shadows
+  ;; the global symbol macro in the remaining &AUX part of the
+  ;; lambda list of the local function, but not in the body of the
+  ;; FLET.
+  (assert (equal (cleavir-generate-ast:minimally-compile
+		  `(flet ((fun (&aux (gsm1 12) (x gsm1)) gsm1))
+		     (fun gsm1))
+		  *e*)
+		 `(flet ((fun (&aux (gsm1 12) (x gsm1)) gsm1))
+		    (fun (hello1 hello2)))))
   (assert (equal (cleavir-generate-ast:minimally-compile
 		  '(flet ((fun1 (x &optional (y gsm1))
 			   (f x y gsm1))
