48f90d8fdc7130a26f52163da887b069965baf65
Robert Strandh <robert.strandh@gmail.com>
2014-09-23 15:50:50 +0200

    Definition of generic functio WALK-SEQUENCE.

--- Code/Cleavir/Code-walker/walk-form.lisp
+++ Code/Cleavir/Code-walker/walk-form.lisp
@@ -7,3 +7,13 @@
 ;;; This generic function is the main entry point for the code walker.
 
 (defgeneric walk-form (form walker environment))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;; Generic function WALK-SEQUENCE.  
+;;;
+;;; This generic function is called by the code walker in order to
+;;; process a sequences of forms such as the forms of a body that is
+;;; treated as a PROGN.
+
+(defgeneric walk-sequence (sequence walker environment))
