d4ac7c665582fe620fa8d1e68d446c4f9ea20e71
Robert Strandh <robert.strandh@gmail.com>
2014-09-23 15:50:13 +0200

    Comment main entry point.

--- Code/Cleavir/Code-walker/walk-form.lisp
+++ Code/Cleavir/Code-walker/walk-form.lisp
@@ -1,3 +1,9 @@
 (cl:in-package #:cleavir-walker)
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;; Generic function WALK-FORM.  
+;;;
+;;; This generic function is the main entry point for the code walker.
+
 (defgeneric walk-form (form walker environment))
