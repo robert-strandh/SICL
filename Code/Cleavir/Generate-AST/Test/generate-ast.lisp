b4f1fbb1a598152ba9f2fbb556b5077169babb78
Robert Strandh <robert.strandh@gmail.com>
2014-09-25 16:41:58 +0200

    Method on SAME-P specialized to PROGN-AST.

--- Code/Cleavir/Generate-AST/Test/generate-ast.lisp
+++ Code/Cleavir/Generate-AST/Test/generate-ast.lisp
@@ -63,6 +63,9 @@
 		(cleavir-ast:lambda-list ast2))
 	 (same-p (cleavir-ast:body-ast ast1)
 		 (cleavir-ast:body-ast ast2)))))
-	   
-			   
+
+(defmethod same-p ((ast1 cleavir-ast:progn-ast) ast2 table)
+  (every (lambda (a1 a2) (same-p a1 a2 table))
+	 (cleavir-ast:form-asts ast1)
+	 (cleavir-ast:form-asts ast2)))
 		    
