ce870193346eed5cb1fd67b08154b752e2969a20
Robert Strandh <robert.strandh@gmail.com>
2014-09-26 08:56:14 +0200

    Extract package definitions to a separate component.

---
+++ Code/Cleavir/Generate-AST/Test/packages.lisp
@@ -0,0 +1,15 @@
+(cl:in-package #:common-lisp-user)
+
+(defpackage #:cleavir-bogus-test-environment
+  (:use #:common-lisp)
+  (:export #:bogus-environment
+	   #:gsm1 #:gsm2
+	   #:gm1 #:gm2
+	   #:hello1 #:hello2
+	   #:hello))
+
+(defpackage #:cleavir-test-minimal-compilation
+  (:use #:common-lisp #:cleavir-bogus-test-environment))
+
+(defpackage #:cleavir-test-generate-ast
+  (:use #:common-lisp))
