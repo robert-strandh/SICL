28ffcf75c70d7427398a0be8b39a16a0d51d07be
Robert Strandh <robert.strandh@gmail.com>
2014-09-29 16:51:58 +0200

    Function for setting a lexical variable.

--- Code/Cleavir/AST-interpreter/environment.lisp
+++ Code/Cleavir/AST-interpreter/environment.lisp
@@ -6,3 +6,12 @@
 	       (gethash lexical level)
 	     (when present-p (return value)))
 	finally (error "Unknown lexical variable ~s" lexical)))
+
+(defun set-lexical (lexical value environment)
+  (loop for level in environment
+	do (multiple-value-bind (value present-p)
+	       (gethash lexical level)
+	     (when present-p
+	       (setf (gethash lexical level) value)
+	       (return)))
+	finally (setf (gethash lexical (first environment) ) value)))
