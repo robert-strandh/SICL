31dec190fb02504e58fa4aa6ee7732f04f64e91e
Robert Strandh <robert.strandh@gmail.com>
2014-09-29 15:57:03 +0200

    Define main entry point function.

--- Code/Cleavir/AST-interpreter/interpreter.lisp
+++ Code/Cleavir/AST-interpreter/interpreter.lisp
@@ -1,3 +1,8 @@
 (cl:in-package #:cleavir-ast-interpreter)
 
 (defgeneric interpret-ast (ast static-env dynamic-env))
+
+(defun interpret (ast)
+  (let ((static-env (list (make-hash-table :test #'eq)))
+	(dynamic-env '()))
+    (interpret-ast ast static-env dynamic-env)))
