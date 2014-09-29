fcc76b823137ef9462045acf6a045cfddf1ec6d5
Robert Strandh <robert.strandh@gmail.com>
2014-09-29 16:45:49 +0200

    Utility function for interpeting a sequence of ASTs.

--- Code/Cleavir/AST-interpreter/interpreter.lisp
+++ Code/Cleavir/AST-interpreter/interpreter.lisp
@@ -9,6 +9,15 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;
+;;; Utility function.
+
+(defun interpret-sequence (sequence static-env dynamic-env)
+  (loop for ast in sequence
+	for value = (interpret-ast ast static-env dynamic-env)
+	finally (return value)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
 ;;; Methods on INTERPRET-AST.
 
 (defmethod interpret-ast ((ast cleavir-ast:constant-ast)
