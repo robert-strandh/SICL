ae0e3c940f80d59ff359721dda37c79efa0533c4
Robert Strandh <robert.strandh@gmail.com>
2014-09-29 06:47:55 +0200

    Replaced test for two &KEYs in FLET by new test strategy.

--- Code/Cleavir/Generate-AST/Test/minimal-compilation.lisp
+++ Code/Cleavir/Generate-AST/Test/minimal-compilation.lisp
@@ -104,13 +104,10 @@
 	12)
   ;; Test that an &KEY parameter of the local function shadows the
   ;; global symbol macro in the remaining &KEY part of the lambda list
-  ;; of the local function, but not in the body of the FLET.
-  (assert (equal (cleavir-generate-ast:minimally-compile
-		  `(flet ((fun (&key (gsm1 12) (x gsm1)) gsm1))
-		     (fun gsm1))
-		  *e*)
-		 `(flet ((fun (&key ((:gsm1 gsm1) 12) ((:x x) gsm1)) gsm1))
-		    (fun (hello1 hello2)))))
+  ;; of the local function.
+  (test '(flet ((fun (&key (gsm3 12) (x (1+ gsm3))) x))
+	  (fun))
+	13)
   ;; Test that an &KEY parameter of the local function shadows the
   ;; global symbol macro in the &AUX part of the lambda list of the
   ;; local function, but not in the body of the FLET.
