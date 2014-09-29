c55a90b6a2fe053f60ae59018a8395ddbf01254f
Robert Strandh <robert.strandh@gmail.com>
2014-09-29 05:37:47 +0200

    Replaced test for &OPTIONAL and &AUX in FLET by new test strategy.

--- Code/Cleavir/Generate-AST/Test/minimal-compilation.lisp
+++ Code/Cleavir/Generate-AST/Test/minimal-compilation.lisp
@@ -93,13 +93,10 @@
 	13)
   ;; Test that an &OPTIONAL parameter of the local function shadows
   ;; the global symbol macro in the &AUX part of the lambda list of
-  ;; the local function, but not in the body of the FLET.
-  (assert (equal (cleavir-generate-ast:minimally-compile
-		  `(flet ((fun (&optional (gsm1 12) &aux (x gsm1)) gsm1))
-		     (fun gsm1))
-		  *e*)
-		 `(flet ((fun (&optional (gsm1 12) &aux (x gsm1)) gsm1))
-		    (fun (hello1 hello2)))))
+  ;; the local function.
+  (test '(flet ((fun (&optional (gsm3 12) &aux (x (1+ gsm3))) x))
+	  (fun))
+	13)
   ;; Test that an &OPTIONAL parameter of the local function shadows
   ;; the global symbol macro in body of the local function, but not in
   ;; the body of the FLET.
