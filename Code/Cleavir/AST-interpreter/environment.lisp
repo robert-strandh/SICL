faacec81a97e64b52db3c5387168d057c0abc517
Robert Strandh <robert.strandh@gmail.com>
2014-09-30 09:15:05 +0200

    Implement the dynamic runtime environment.

--- Code/Cleavir/AST-interpreter/environment.lisp
+++ Code/Cleavir/AST-interpreter/environment.lisp
@@ -1,5 +1,9 @@
 (cl:in-package #:cleavir-ast-interpreter)
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;; The static runtime environment.
+
 (defun lookup-lexical (lexical environment)
   (loop for level in environment
 	do (multiple-value-bind (value present-p)
@@ -15,3 +19,20 @@
 	       (setf (gethash lexical level) value)
 	       (return)))
 	finally (setf (gethash lexical (first environment) ) value)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;; The dynamic runtime environment.
+
+(defclass special-binding ()
+  ((%name :initarg :name :reader name)
+   (%value :initarg :value :accessor value)))
+
+(defun lookup-special (name environment)
+  (let ((entry (find-if (lambda (entry) (typep entry 'special-binding))
+			environment)))
+    (if (null entry)
+	(symbol-value name)
+	(value entry))))
+
+
