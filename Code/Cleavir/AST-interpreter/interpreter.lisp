edc8b3d012655a9df628bdfdab1e3dd2460ea172
Robert Strandh <robert.strandh@gmail.com>
2014-10-01 08:00:44 +0200

    Define method on INTERPRET-AST specialized to TAGBODY-AST.

--- Code/Cleavir/AST-interpreter/interpreter.lisp
+++ Code/Cleavir/AST-interpreter/interpreter.lisp
@@ -78,3 +78,43 @@
   (let* ((go-tag-ast (cleavir-ast:tag-ast ast))
 	 (catch-tag (lookup-lexical go-tag static-env)))
     (throw catch-tag go-tag-ast)))
+
+(defmethod interpret-ast ((ast cleavir-ast:tagbody-ast)
+			  static-env dynamic-env)
+  (let ((catch-tag (list nil))
+	(item-asts (cleavir-ast:item-asts ast)))
+    ;; Start by entering the catch tag as the value of each go tags in
+    ;; the tagbody in the lexical environment.
+    (loop for item-ast in item-asts
+	  when (typep item-ast 'cleavir-ast:tag-ast)
+	    do (set-lexical item-ast catch-tag static-env))
+    ;; The variable REMAINING-ITEM-ASTS holds a list of the items that
+    ;; should be interpreted before the job is done.  Initially, it is
+    ;; the list of all the items in the TAGBODY, but a GO will alter
+    ;; that.
+    (let ((remaining-item-asts item-asts))
+      (flet ((do-item ()
+	       ;; We know there is at least one remaining item-ast to
+	       ;; interpret.
+	       (interpret-ast (pop remaining-item-asts)
+			      static-env dynamic-env)))
+	(flet ((do-items ()
+		 ;; Interpret all remaining items.
+		 (loop when (null remaining-item-asts)
+			 ;; If there are no more items to interpret,
+			 ;; this means whe have reached the end of the
+			 ;; TAGBODY, so we are done interpreting it.
+			 do (return-from interpret-ast nil)
+		       do (do-item))))
+	  ;; Each iteration of this loop corresponds to a GO.  If
+	  ;; there is no GO in a particular iteration of this loop,
+	  ;; then the local function DO-ITEMS will interpret all the
+	  ;; remaining items and return from this method.  If there is
+	  ;; a GO during the interpretation of one of the items, then
+	  ;; the execution of DO-ITEMS will be interrupted by a THROW
+	  ;; with the GO-TAG-AST as its value.  We CATCH that event
+	  ;; and set the remaining items to the list of items starting
+	  ;; with that GO-TAG-AST.
+	  (loop do (let ((go-tag-ast (catch catch-tag (do-items))))
+		     (setf remaining-item-asts
+			   (member go-tag-ast item-asts)))))))))
