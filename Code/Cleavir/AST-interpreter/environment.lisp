4c90709d9a57e85d11baeb15aedbbe7920050188
Robert Strandh <robert.strandh@gmail.com>
2014-10-03 14:15:43 +0200

    Removed dynamic environment.  It has to be that of the host.

--- Code/Cleavir/AST-interpreter/environment.lisp
+++ Code/Cleavir/AST-interpreter/environment.lisp
@@ -20,20 +20,3 @@
 	       (setf (gethash lexical level) value)
 	       (return)))
 	finally (setf (gethash lexical (first environment) ) value)))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;;
-;;; The dynamic runtime environment.
-
-(defclass special-binding ()
-  ((%name :initarg :name :reader name)
-   (%value :initarg :value :accessor value)))
-
-(defun lookup-special (name environment)
-  (let ((entry (find-if (lambda (entry) (typep entry 'special-binding))
-			environment)))
-    (if (null entry)
-	(symbol-value name)
-	(value entry))))
-
-
