b999d7e2bd4267ded4a652d8a6994ebb6b277b40
Robert Strandh <robert.strandh@gmail.com>
2014-10-16 08:57:21 +0200

    Generic function for translating simple instructions.

--- Code/Cleavir/Backends/MIR-interpreter/translate.lisp
+++ Code/Cleavir/Backends/MIR-interpreter/translate.lisp
@@ -1,2 +1,9 @@
 (cl:in-package #:cleavir-mir-interpreter)
 
+;;; The first argument to this function is an instruction that has a
+;;; single successor.  Whether a GO is required at the end of this
+;;; function is determined by the code layout algorithm.  
+;;; 
+;;; The inputs are forms to be evaluated.  The outputs are symbols
+;;; that are names of variables.
+(defgeneric translate-simple-instruction (instruction inputs outputs))
