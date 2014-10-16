c3f87e9cc6c7dd3f639bed8231585ca6816f317c
Robert Strandh <robert.strandh@gmail.com>
2014-10-16 09:47:18 +0200

    Translate LONG-FLOAT-ASET-INSTRUCTION.

--- Code/Cleavir/Backends/MIR-interpreter/translate.lisp
+++ Code/Cleavir/Backends/MIR-interpreter/translate.lisp
@@ -137,6 +137,12 @@
   `(setf (row-major-aref ,(first inputs) ,(second inputs))
 	 ,(third inputs)))
 
+(defmethod translate-simple-instruction
+    ((instruction cleavir-mir:long-float-aset-instruction) inputs outputs)
+  (declare (ignore outputs))
+  `(setf (row-major-aref ,(first inputs) ,(second inputs))
+	 ,(third inputs)))
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;
 ;;; Methods on TRANSLATE-BRANCH-INSTRUCTION.
