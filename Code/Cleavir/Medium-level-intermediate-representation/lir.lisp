b730ac68cd69d2b21c38d87d58f6ded0c0159eb0
Robert Strandh <robert.strandh@gmail.com>
2014-10-18 09:12:35 +0200

    Added definition of MEMREF2-INSTRUCTION.

--- Code/Cleavir/Medium-level-intermediate-representation/lir.lisp
+++ Code/Cleavir/Medium-level-intermediate-representation/lir.lisp
@@ -17,3 +17,23 @@
     :inputs (list input)
     :outputs (list output)
     :successors (if (null successor) '() (list successor))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;; MEMREF1-INSTRUCTION
+;;;
+;;; This instruction loads a memory location.  It takes a two inputs.
+;;; The first input contains the base address of the datum to load.
+;;; The second input contains constant offset.  This instruction has a
+;;; single output which is set to the contents of the memory location
+;;; at the address computed as the sum of the base address and the
+;;; offset.
+
+(defclass memref2-instruction (instruction one-successor-mixin)
+  ())
+
+(defun make-memref2-instruction (input1 input2 output &optional successor)
+  (make-instance 'memref2-instruction
+    :inputs (list input1 input2)
+    :outputs (list output)
+    :successors (if (null successor) '() (list successor))))
