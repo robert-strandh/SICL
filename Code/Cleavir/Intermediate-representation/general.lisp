412fdd04b9b06e9e5d58bf1af6feca43afd597b4
Robert Strandh <robert.strandh@gmail.com>
2014-10-20 04:57:45 +0200

    Moved general comments.

--- Code/Cleavir/Intermediate-representation/general.lisp
+++ Code/Cleavir/Intermediate-representation/general.lisp
@@ -35,6 +35,49 @@
 ;;;; An instruction J is said to be REACHABLE from some instruction I
 ;;;; if and only if there is a (possibly empty) sequence of control
 ;;;; arcs that corresponds to a path from I to J.
+;;;;
+;;;; The initial instructions of a flowchart form a TREE, called the
+;;;; NESTING TREE.  The root of the tree is the initial instruction of
+;;;; the flowchart, and the parent in the tree of some initial
+;;;; instruction I other than the initial instruction of the flowchart
+;;;; is the unique initial instruction from which I can be reached.
+;;;; This tree is a mirror of the tree of functions in the source
+;;;; code; functions defined by LAMBDA, FLET, or LABELS.
+;;;;
+;;;; The NESTING depth of initial instruction I is the the depth of I
+;;;; in the nesting tree.
+;;;;
+;;;; A PROCEDURE P corresponding to some initial instruction I is a
+;;;; set of instructions that are reachable from I, but that are not
+;;;; reachable from any parent of I in the nesting tree.  The
+;;;; instruction I is called the INITIAL INSTRUCTION OF P.
+;;;;
+;;;; By extension, the nesting depth of some procedure P is the
+;;;; nesting depth of the initial instruction of P, and the nesting
+;;;; depth of an INSTRUCTION I is the nesting depth of the procedure
+;;;; of which I is a member.  Furthermore, by extension we use the
+;;;; terms PARENT and ANCESTOR about procedures as well as initial
+;;;; instructions with the obvious meaning.
+;;;;
+;;;; Clearly, given the definitions above, for every control arc A,
+;;;; the HEAD of A can not have a greater nesting depth than the TAIL
+;;;; of A.  However, there are control arcs for which the nesting
+;;;; depth of the head is smaller than the nesting dept of the tail.
+;;;; Such control arcs are the result of RETURN-FROM and GO special
+;;;; operators.
+;;;;
+;;;; A procedure P is said to be CONTROL DEPENDENT on some procedure Q
+;;;; if Q is an ancestor of P in the nesting tree and there is a
+;;;; control arc from some instruction belonging to P to some
+;;;; instruction belonging to Q.  A procedure P is said to be CONTROL
+;;;; INDEPENDENT if and only if it is control dependent on no other
+;;;; procedure.  Each control independent procedures will be turned
+;;;; into a code object in itself, whereas other procedures will share
+;;;; code with the procedures they depend on.  Notice that it is
+;;;; possible for a procedure P to be control dependent, but not on
+;;;; its direct parent.  The parent of P can thus be control
+;;;; independent, whereas P will share code with some ancestor of its
+;;;; parent.
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;
