5d65137651e7ae54076d48dbb77704502db958d6
Robert Strandh <robert.strandh@gmail.com>
2014-10-20 11:52:35 +0200

    Introduced variable *COMPILE-TIME-TOO* as in the HyperSpec.

--- Code/Cleavir/Skim-file/skim-file.lisp
+++ Code/Cleavir/Skim-file/skim-file.lisp
@@ -1,12 +1,16 @@
 (cl:in-package #:cleavir-skim-file)
 
+(defparameter *compile-time-too* nil)
+
 (defgeneric skim-special (symbol form environment))
 
 (defun skim-form (form environment)
-  (when (and (consp form) (symbolp (first form)))
-    (let ((info (cleavir-env:function-info environment (first form))))
-      (when (typep info 'cleavir-env:special-operator-info)
-	(skim-special (first form) form environment)))))
+  (if *compile-time-too*
+      (cleavir-env:eval form environment environment)      
+      (when (and (consp form) (symbolp (first form)))
+	(let ((info (cleavir-env:function-info environment (first form))))
+	  (when (typep info 'cleavir-env:special-operator-info)
+	    (skim-special (first form) form environment))))))
 
 (defun skim-file (filename environment)
   (with-open-file (stream filename :direction :input)
