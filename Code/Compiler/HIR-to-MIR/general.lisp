b6810c215ca700fe19a182f356f28719effdb75b
Robert Strandh <robert.strandh@gmail.com>
2014-10-20 10:00:57 +0200

    Method on SPECIALIZE for CAR-INSTRUCTION.

--- Code/Compiler/HIR-to-MIR/general.lisp
+++ Code/Compiler/HIR-to-MIR/general.lisp
@@ -4,3 +4,13 @@
 
 (defclass x86-64 () ())
 
+(defmethod cleavir-ir:specialize
+    ((instruction cleavir-ir:car-instruction)
+     (implementation sicl)
+     (processor x86-64)
+     os)
+  (declare (ignore os))
+  (let* ((successor (first (cleavir-ir:successors instruction)))
+	 (cons (first (cleavir-ir:inputs instruction)))
+	 (immediate (make-instance 'cleavir-ir:immediate-input :value -1)))
+    (cleavir-ir:make-memref2-instruction cons immediate successor)))
