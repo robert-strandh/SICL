5d48d33e34302ea61c32decf81314867df5a9b5c
Robert Strandh <robert.strandh@gmail.com>
2014-10-20 07:17:34 +0200

    method on ELIMINATE specialized to TYPEQ-INSTRUCTION.

--- Code/Cleavir/Eliminate-TYPEQ/eliminate-typeq.lisp
+++ Code/Cleavir/Eliminate-TYPEQ/eliminate-typeq.lisp
@@ -20,3 +20,22 @@
 
 (defmethod eliminate :after ((instruction cleavir-ir:enclose-instruction))
   (eliminate (cleavir-ir:code instruction)))
+
+(defmethod eliminate ((instruction cleavir-ir:typeq-instruction))
+  (let* ((object (first (cleavir-ir:inputs instruction)))
+	 (typep-constant (cleavir-ir:make-constant-input 'typep))
+	 (typep-function (cleavir-ir:new-temporary))
+	 (fdefinition (cleavir-ir:make-fdefinition-instruction
+		       typep-constant typep-function))
+	 (type (cleavir-ir:value-type instruction))
+	 (type-descriptor-constant (cleavir-ir:make-constant-input type))
+	 (boolean-value (cleavir-ir:new-temporary))
+	 (call (cleavir-ir:make-funcall-instruction
+		(list fdefinition object type-descriptor-constant)
+		(list boolean-value)))
+	 (nil-constant (cleavir-ir:make-constant-input 'nil)))
+    (cleavir-ir:insert-instruction-before fdefinition instruction)
+    (cleavir-ir:insert-instruction-before call instruction)
+    (change-class instruction 'cleavir-ir:eq-instruction
+		  :inputs (list boolean-value nil-constant)
+		  :successors (reverse (cleavir-ir:successors instruction)))))
