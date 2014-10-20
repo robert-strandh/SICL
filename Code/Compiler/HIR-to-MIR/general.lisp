734fd0c82be9a9d5b014f31e2a2dd9f686b3ed97
Robert Strandh <robert.strandh@gmail.com>
2014-10-20 10:01:46 +0200

    Method on SPECIALIZE for CDR-INSTRUCTION.

--- Code/Compiler/HIR-to-MIR/general.lisp
+++ Code/Compiler/HIR-to-MIR/general.lisp
@@ -14,3 +14,14 @@
 	 (cons (first (cleavir-ir:inputs instruction)))
 	 (immediate (make-instance 'cleavir-ir:immediate-input :value -1)))
     (cleavir-ir:make-memref2-instruction cons immediate successor)))
+
+(defmethod cleavir-ir:specialize
+    ((instruction cleavir-ir:cdr-instruction)
+     (implementation sicl)
+     (processor x86-64)
+     os)
+  (declare (ignore os))
+  (let* ((successor (first (cleavir-ir:successors instruction)))
+	 (cons (first (cleavir-ir:inputs instruction)))
+	 (immediate (make-instance 'cleavir-ir:immediate-input :value 7)))
+    (cleavir-ir:make-memref2-instruction cons immediate successor)))
