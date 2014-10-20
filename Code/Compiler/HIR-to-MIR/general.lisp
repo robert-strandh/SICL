9d3d3558af575a53f83df472582758933295f61f
Robert Strandh <robert.strandh@gmail.com>
2014-10-20 10:14:22 +0200

    Method on SPECIALIZE for RPLACA-INSTRUCTION.

--- Code/Compiler/HIR-to-MIR/general.lisp
+++ Code/Compiler/HIR-to-MIR/general.lisp
@@ -25,3 +25,15 @@
 	 (cons (first (cleavir-ir:inputs instruction)))
 	 (immediate (make-instance 'cleavir-ir:immediate-input :value 7)))
     (cleavir-ir:make-memref2-instruction cons immediate successor)))
+
+(defmethod cleavir-ir:specialize
+    ((instruction cleavir-ir:rplaca-instruction)
+     (implementation sicl)
+     (processor x86-64)
+     os)
+  (declare (ignore os))
+  (let* ((successor (first (cleavir-ir:successors instruction)))
+	 (cons (first (cleavir-ir:inputs instruction)))
+	 (object (second (cleavir-ir:inputs instruction)))
+	 (immediate (make-instance 'cleavir-ir:immediate-input :value -1)))
+    (cleavir-ir:make-memset2-instruction cons immediate successor)))
