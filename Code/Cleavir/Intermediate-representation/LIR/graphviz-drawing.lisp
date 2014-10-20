1a58f0dead70b0486043ace2b1a328d05dd29303
Robert Strandh <robert.strandh@gmail.com>
2014-10-20 10:09:30 +0200

    Remove drawing method that exists elsewhere.

--- Code/Cleavir/Intermediate-representation/LIR/graphviz-drawing.lisp
+++ Code/Cleavir/Intermediate-representation/LIR/graphviz-drawing.lisp
@@ -2,16 +2,6 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;
-;;; Drawing datum IMMEDIATE-INPUT.
-
-(defmethod draw-datum ((datum immediate-input) stream)
-  (format stream "  ~a [shape = ellipse, style = filled];~%"
-	  (datum-id datum))
-  (format stream "   ~a [fillcolor = aquamarine, label = \"~a\"]~%"
-	  (datum-id datum) (value datum)))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;;
 ;;; Drawing datum WORD-INPUT.
 
 (defmethod draw-datum ((datum word-input) stream)
