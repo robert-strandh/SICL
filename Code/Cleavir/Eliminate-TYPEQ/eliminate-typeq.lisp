aeccaef99364e938c5873d766124aeb22d016696
Robert Strandh <robert.strandh@gmail.com>
2014-10-20 06:32:19 +0200

    Main entry point and auxiliary methods on ELIMINATE.

--- Code/Cleavir/Eliminate-TYPEQ/eliminate-typeq.lisp
+++ Code/Cleavir/Eliminate-TYPEQ/eliminate-typeq.lisp
@@ -1,1 +1,22 @@
-(cl:in-package #:cleavir-eleminate-typeq)
+(cl:in-package #:cleavir-eliminate-typeq)
+
+(defvar *table*)
+
+(defgeneric eliminate (instruction))
+
+(defun eliminate-typeq (initial-instruction)
+  (let ((*table* (make-hash-table :test #'eq)))
+    (eliminate initial-instruction)))
+
+(defmethod eliminate (instruction)
+  (declare (ignore instruction))
+  nil)
+
+(defmethod eliminate :around (instruction)
+  (unless (gethash instruction *table*)
+    (setf (gethash instruction *table*) t)
+    (call-next-method)
+    (mapc #'eliminate (cleavir-ir:successors instruction))))
+
+(defmethod eliminate :after ((instruction cleavir-ir:enclose-instruction))
+  (eliminate (cleavir-ir:code instruction)))
