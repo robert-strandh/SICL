f465b026447dfb1f9e391ca3a9141e90a8030751
Robert Strandh <robert.strandh@gmail.com>
2014-10-21 09:05:47 +0200

    Definition of generic function FDEFINITION.

--- Code/Environment/generic-functions.lisp
+++ Code/Environment/generic-functions.lisp
@@ -1,1 +1,27 @@
 (cl:in-package #:sicl-global-environment)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;; Generic function FDEFINITION.
+;;;
+;;; This generic function is a generic version of the Common Lisp
+;;; function CL:FDEFINITION. 
+;;;
+;;; If FUNCTION-NAME has a definition in the function namespece of
+;;; ENVIRONMENT (i.e., if FBOUNDP returns true), then a call to this
+;;; function succeeds.  Otherwise an error of type UNDEFINED-FUNCTION
+;;; is signaled.
+;;;
+;;; If FUNCTION-NAME is defined as an ordinary function or a generic
+;;; function, then a call to this function returns the associated
+;;; function object.
+;;;
+;;; If FUNCTION-NAME is defined as a macro, then a list of the form
+;;; (CL:MACRO-FUNCTION <function>) is returned, where <function> is
+;;; the macro expansion function associated with the macro.
+;;;
+;;; If FUNCTION-NAME is defined as a special operator, then a list of
+;;; the form (CL:SPECIAL <object>) is returned, where the nature of
+;;; <object> is currently not specified. 
+
+(defgeneric fdefinition (function-name environment))
