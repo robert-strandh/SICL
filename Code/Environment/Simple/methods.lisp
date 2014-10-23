06e640b8f60e731f26cdf33f8ea85f331ea45d16
Robert Strandh <robert.strandh@gmail.com>
2014-10-23 05:35:07 +0200

    Method on SICL-ENV:FBOUNDP.

--- Code/Environment/Simple/methods.lisp
+++ Code/Environment/Simple/methods.lisp
@@ -1,1 +1,8 @@
 (cl:in-package #:sicl-simple-environment)
+
+(defmethod sicl-env:fboundp (function-name (env simple-environment))
+  (let ((entry (find function-name (function-entries env)
+		     :key #'name :test #'equal)))
+    (and (not (null entry))
+	 (not (eq (car (function-cell entry))
+		  (unbound entry))))))
