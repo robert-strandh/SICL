daa9dfff6e29401c3d8bb40bf08414452acbf7f3
Robert Strandh <robert.strandh@gmail.com>
2014-10-23 05:21:05 +0200

    Adapted contents to new purpose of system.

--- Code/Environment/packages.lisp
+++ Code/Environment/packages.lisp
@@ -7,60 +7,31 @@
   ;; purpose of cross compilation, we shadow the symbols of the
   ;; COMMON-LISP package that name functions and variables that have
   ;; to do with manipulating the environment.
-  (:shadow #:type
-	   #:proclaim
-	   #:macroexpand-1
-	   #:macroexpand
-	   #:macro-function
-	   #:*macroexpand-hook*
+  (:shadow #:fboundp
 	   #:fdefinition
-	   #:symbol-function
-	   #:fboundp
-	   #:fmakunbound
-	   #:special-operator-p
+	   #:macro-function
 	   #:compiler-macro-function
-	   #:symbol-value
 	   #:boundp
 	   #:makunbound
-	   #:constantp
+	   #:fmakunbound
+	   #:find-class
 	   )
   (:export
-   #:global-environment
-   #:proclaim
-   #:definition
-   #:location
-   #:global-location #:make-global-location
-   #:special-location #:make-special-location
-   #:storage
-   #:name
-   #:type
-   #:*global-environment*
-   #:add-to-global-environment
-   #:make-entry-from-declaration
-   #:find-variable
-   #:find-function
-   #:macroexpand-1
-   #:macroexpand
-   #:find-type
-   #:find-ftype
-   #:augment-environment
-   #:augment-environment-with-declarations
-   #:add-lexical-variable-entry
-   #:add-symbol-macro-entry
-   #:fdefinition
    #:fboundp
-   #:fmakunbound
-   #:find-function-cell
-   #:special-operator-p
-   #:symbol-macro-function
-   #:symbol-value
+   #:fdefinition
+   #:macro-function
+   #:compiler-macro-function
+   #:function-type
+   #:function-inline
+   #:constant-variable
+   #:special-variable
+   #:symbol-macro
+   #:variable-type
+   #:setf-expander
    #:boundp
    #:makunbound
-   #:find-value-cell
-   #:constantp
-   #:defmacro
-   #:deftype
-   #:define-compiler-macro
-   #:ensure-function-entry
+   #:fmakunbound
+   #:find-class
+   #:special-operator
    ))
 
