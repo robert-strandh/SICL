ab74aa8d3ec54832ae45670a48babb87225aa858
Robert Strandh <robert.strandh@gmail.com>
2014-10-23 06:38:16 +0200

    Method on SICL-ENV:FDEFINITION.

--- Code/Environment/Simple/methods.lisp
+++ Code/Environment/Simple/methods.lisp
@@ -10,3 +10,17 @@
 		      (unbound entry)))
 	     (not (null (macro-function entry)))
 	     (not (null (special-operator entry)))))))
+
+(defmethod sicl-env:fdefinition (function-name (env simple-environment))
+  (let ((entry (find-function-entry env function-name)))
+    (cond ((null entry)
+	   (error 'undefined-function :name function-name))
+	  ((not (eq (car (function-cell entry))
+		    (unbound entry)))
+	   (car (function-cell entry)))
+	  ((not (null (macro-function entry)))
+	   `(cl:macro-function ,(macro-function entry)))
+	  ((not (null (special-operator entry)))
+	   `(cl:special (special-operator entry)))
+	  (t
+	   (error 'undefined-function :name function-name)))))
