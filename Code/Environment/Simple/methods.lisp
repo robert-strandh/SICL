54a290a7d35c6bb0bc3ca27c2753ffae2ff987f1
Robert Strandh <robert.strandh@gmail.com>
2014-10-23 06:34:54 +0200

    FBOUNDP should return true for macros and special operators.

--- Code/Environment/Simple/methods.lisp
+++ Code/Environment/Simple/methods.lisp
@@ -1,7 +1,12 @@
 (cl:in-package #:sicl-simple-environment)
 
+;;; Recall that this function should return true if FUNCTION-NAME has
+;;; a definition in ENVIRONMENT as an ordinary function, a generic
+;;; function, a macro, or a special operator.
 (defmethod sicl-env:fboundp (function-name (env simple-environment))
   (let ((entry (find-function-entry env function-name)))
     (and (not (null entry))
-	 (not (eq (car (function-cell entry))
-		  (unbound entry))))))
+	 (or (not (eq (car (function-cell entry))
+		      (unbound entry)))
+	     (not (null (macro-function entry)))
+	     (not (null (special-operator entry)))))))
