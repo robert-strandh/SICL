a9527ee160d3ad0d94f82296399addeeffac530d
Robert Strandh <robert.strandh@gmail.com>
2014-10-23 06:26:13 +0200

    Use existing function FIND-FUNCTION-ENTRY.

--- Code/Environment/Simple/methods.lisp
+++ Code/Environment/Simple/methods.lisp
@@ -1,8 +1,7 @@
 (cl:in-package #:sicl-simple-environment)
 
 (defmethod sicl-env:fboundp (function-name (env simple-environment))
-  (let ((entry (find function-name (function-entries env)
-		     :key #'name :test #'equal)))
+  (let ((entry (find-function-entry env function-name)))
     (and (not (null entry))
 	 (not (eq (car (function-cell entry))
 		  (unbound entry))))))
