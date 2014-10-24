1327f1a8ce05b0f624e7bbb9b9bdbd071a3033b9
Robert Strandh <robert.strandh@gmail.com>
2014-10-24 06:46:19 +0200

    Renamed packages and variables.ast-to-hir.lisp

--- Code/Cleavir/AST-to-MIR/Test/packages.lisp
+++ Code/Cleavir/AST-to-MIR/Test/packages.lisp
@@ -1,6 +1,6 @@
 (cl:in-package #:common-lisp-user)
 
-(defpackage #:cleavir-ast-to-mir-test
+(defpackage #:cleavir-ast-to-hir-test
   (:use #:common-lisp #:cleavir-bogus-test-environment)
   (:export
    ))
