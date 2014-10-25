c20f4de169a2ae5c72a1b2e27ee5d7c6cb3abab3
Robert Strandh <robert.strandh@gmail.com>
2014-10-25 15:45:54 +0200

    Define classe for runtime stack frame.

--- Code/Backends/Extrinsic-HIR-interpreter/interpreter.lisp
+++ Code/Backends/Extrinsic-HIR-interpreter/interpreter.lisp
@@ -14,3 +14,18 @@
    ;; A list of lexical variables that should be used to augment the
    ;; captured environment when the function is called.
    (%entry-lexicals :initarg :entry-lexicals :reader entry-lexicals)))
+
+(defclass stack-frame ()
+  (;; The next instruction to be executed.
+   (%next-instruction :initarg next-instruction :accessor next-instruction)
+   ;; The static environment is a list of hash tables. 
+   (%static-env :initarg :static-env :reader static-env)
+   ;; The dynamic environment is a list of instances of some subclass
+   ;; of the class dynamic-environment-entry.
+   (%dynamic-env :initarg :dynamic-env :accessor dynamic-env)
+   ;; A list of lexical variables that should be set when this
+   ;; frame is returned to from a deeper nested frame.
+   (%return-variables :initform '() :accessor return-variables)
+   ;; A list of Lisp objects.  The arguments that were passed to the
+   ;; function call that resulted in the creation of this frame.
+   (%arguments :initarg :arguments :reader arguments)))
