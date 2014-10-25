1ac11d316e8cb324991a664fbb85f5a9d26221ce
Robert Strandh <robert.strandh@gmail.com>
2014-10-25 15:45:18 +0200

    Define classes for built-in and interpreted functions.

--- Code/Backends/Extrinsic-HIR-interpreter/interpreter.lisp
+++ Code/Backends/Extrinsic-HIR-interpreter/interpreter.lisp
@@ -1,1 +1,16 @@
 (cl:in-package #:sicl-extrinsic-hir-interpreter)
+
+(defclass function () ())
+
+(defclass built-in-function (function)
+  ((%host-function :initarg :host-function :reader host-function)))
+
+(defclass interpreted-function (function)
+  (;; The initial instruction of the function.  It is always an
+   ;; ENTER-INSTRUCTION.
+   (%entry-point :initarg :entry-point :reader entry-point)
+   ;; The captured static environment.
+   (%environment :initarg :environment :reader environment)
+   ;; A list of lexical variables that should be used to augment the
+   ;; captured environment when the function is called.
+   (%entry-lexicals :initarg :entry-lexicals :reader entry-lexicals)))
