d3a563c62a004a5b5e5e85a2b96e4dc7dcb06eae
Robert Strandh <robert.strandh@gmail.com>
2014-10-26 09:55:53 +0100

    Removed incorrect function definitions.  They do not take into account non-global bindings.

--- Code/Environment/standard-environment-functions.lisp
+++ Code/Environment/standard-environment-functions.lisp
@@ -19,18 +19,6 @@
 ;;; that a special variable entry exists for it AND the storage cell
 ;;; of that entry does not contain +unbound+.
 
-(defun boundp (symbol)
-  (declare (cl:type symbol symbol))
-  (not (null (or (find symbol (constant-variables *global-environment*)
-		       :key #'name :test #'eq)
-		 (find symbol (symbol-macros *global-environment*)
-		       :key #'name :test #'eq)
-		 (find-if (lambda (entry)
-			    (and (eq (name entry) symbol)
-				 (not (eq (car (storage (location entry)))
-					  +unbound+))))
-			  (special-variables *global-environment*))))))
-
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;
 ;;; Function MAKUNBOUND.
@@ -41,50 +29,6 @@
 ;;; MAKUNBOUND successfully and then have BOUNDP return true.  What we
 ;;; do is to remove the symbol macro if any, and signal an error if an
 ;;; attempt is made to make a constant variable unbound.
-
-(defun makunbound (symbol)
-  (declare (cl:type symbol symbol))
-  ;; Check whether the symbol has a definition as a constant variable.
-  (let ((constant-variable-entry
-	  (find symbol (constant-variables *global-environment*)
-		:key #'name :test #'eq)))
-    (if (not (null constant-variable-entry))
-	(error "Attemp to make a constant variable unbound")
-	;; Check whether the symbol has a definition as a global
-	;; symbol macro.
-	(let ((macro-entry
-		(find symbol (symbol-macros *global-environment*)
-		      :key #'name :test #'eq)))
-	  (if (not (null macro-entry))
-	      (progn
-		;; Remove the symbol macro entry.
-		(setf (symbol-macros *global-environment*)
-		      (delete macro-entry (symbol-macros *global-environment*)
-			      :test #'eq))
-		;; The symbol macro might have a type proclamation
-		;; associated with it.  Remove that too.
-		(setf (proclamations *global-environment*)
-		      (delete-if (lambda (entry)
-				   (and (typep entry 'auxiliary-entry)
-					(eq (base-entry entry) macro-entry)))
-				 (proclamations *global-environment*))))
-	      ;; Check whether the symbol has a definition as a
-	      ;; special variable.
-	      (let ((variable-entry
-		      (find symbol (special-variables *global-environment*)
-			    :key #'name :test #'eq)))
-		(unless (null variable-entry)
-		  ;; Set the storage cell to +unbound+
-		  (setf (car (storage (location variable-entry))) +unbound+)
-		  ;; The variable might have a various proclamation
-		  ;; associated with it.  Remove those too.
-		  (setf (proclamations *global-environment*)
-			(delete-if (lambda (entry)
-				     (and (typep entry 'auxiliary-entry)
-					  (eq (base-entry entry) macro-entry)))
-				   (proclamations *global-environment*)))))))))
-  ;; Return the symbol as required by the HyperSpec
-  symbol)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;
@@ -130,26 +74,3 @@
 ;;; define a symbol macro with a name of an existing special variable.
 ;;; For that reason, we think it is reasonable to signal an error in
 ;;; this case too.
-
-(defun (setf symbol-value) (new-value symbol)
-  (declare (symbol symbol))
-  ;; Handle keyword symbols specially here.
-  (when (keywordp symbol)
-    (error "attempt to change the value of a keyword."))
-  ;; Next check whether the symbol has a defintion as a constant
-  ;; variable. 
-  (when (constantp symbol)
-    (error "attempt to change the value of a constant variable"))
-  ;; Calling this function implicitly DEFINES the variable.
-  (ensure-defined-variable symbol)
-  ;; Find out everything about the variable. 
-  (let ((info (variable-info symbol nil)))
-    (unless (typep new-value (type info))
-      (error 'type-error
-	     :datum new-value
-	     :expected-type (type info)))
-    (setf (car (storage (location info)))
-	  new-value))
-  ;; Return the new value as the HyperSpec requires.
-  new-value)
-
