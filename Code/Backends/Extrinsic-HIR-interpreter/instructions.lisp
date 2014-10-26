34c637c82cdb4cfec1a402286a7c7a5adc8909ca
Robert Strandh <robert.strandh@gmail.com>
2014-10-26 07:16:30 +0100

    Access a lexical variable in the current process.

--- Code/Backends/Extrinsic-HIR-interpreter/instructions.lisp
+++ Code/Backends/Extrinsic-HIR-interpreter/instructions.lisp
@@ -2,3 +2,11 @@
 
 (defclass enter-instruction (cleavir-ir:enter-instruction)
   ((%owned-variables :initarg :owned-variables :reader owned-variables)))
+
+(defun lexical-value (lexical-variable process)
+  (loop for table in (static-env (stack process))
+	do (multiple-value-bind (value present-p)
+	       (gethash lexical-variable table)
+	     (when present-p
+	       (return-from lexical-value value))))
+  (error "unknown variable ~s" lexical-variable))
