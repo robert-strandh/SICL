f6f7861ec1c82288c2ef08fe11d9945f92934229
Robert Strandh <robert.strandh@gmail.com>
2014-10-26 07:15:55 +0100

    Define a special version of the ENTER instruction.

--- Code/Backends/Extrinsic-HIR-interpreter/instructions.lisp
+++ Code/Backends/Extrinsic-HIR-interpreter/instructions.lisp
@@ -1,1 +1,4 @@
 (cl:in-package #:sicl-extrinsic-hir-interpreter)
+
+(defclass enter-instruction (cleavir-ir:enter-instruction)
+  ((%owned-variables :initarg :owned-variables :reader owned-variables)))
