9b7c2992d851ee42f9a1cfbba0501f2bf4732fa6
Robert Strandh <robert.strandh@gmail.com>
2014-10-27 17:21:15 +0100

    Method on CLEAVIER-ENV:MACRO-FUNCTION.

--- Code/Environment/other-methods.lisp
+++ Code/Environment/other-methods.lisp
@@ -1,4 +1,7 @@
 (cl:in-package #:sicl-global-environment)
 
+(defmethod cleavier-env:macro-function (name (env environment))
+  (macro-function name env))
+
 (defmethod cleavier-env:compiler-macro-function (name (env environment))
   (compiler-macro-function name env))
