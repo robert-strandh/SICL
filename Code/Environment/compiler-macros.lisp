7f51e311a3b475dbcfb11db57a8045541571ce8d
Robert Strandh <robert.strandh@gmail.com>
2014-10-27 17:25:17 +0100

    Removed previous (and incorrect) definition of CL:COMPILER-MACRO-FUNCTION.

--- Code/Environment/compiler-macros.lisp
+++ Code/Environment/compiler-macros.lisp
@@ -4,41 +4,6 @@
 ;;;
 ;;; Compiler macros.
 
-;;; Since a compiler macro entry exists only if the name is fbound,
-;;; this means that if the compiler macro entry exists, it refers
-;;; either to a global macro entry, or to a global function entry
-;;; which does not have +funbound+ in its storage cell. 
-;;;
-;;; I am not sure what the optional environment argument could be.  It
-;;; seems to me that it must either be a global environment, i.e. an
-;;; instance of GLOBAL-ENVIRONMENT, a local environment, or NIL.  In
-;;; the last two cases, unless I am wrong, the value of
-;;; *global-envrionment* must be used. 
-(defun compiler-macro-function (name &optional environment)
-  (unless (typep environment 'global-environment)
-    (setf environment *global-environment*))
-  (let ((entry (find-if (lambda (entry)
-			  (eq (name (base-entry entry)) name))
-			(compiler-macros environment))))
-    (if (null entry)
-	nil
-	(definition entry))))
-
-;;; If there is a compiler macro entry, it must refer to a base entry
-;;; which is either a global macro entry or to a global function entry
-;;; that is bound.  This function searches for such a base entry.
-;;;
-;;; FIXME: for now, don't require the function entry to be bound.
-(defun find-base-entry (name environment)
-  (or (find-if (lambda (entry)
-		 (eq (name entry) name))
-	       (macros environment))
-      (find-if (lambda (entry)
-		 (eq (name entry) name))
-;;		 (and (eq (name entry) name)
-;;		      (not (eq (car (storage (location entry))) +funbound+))))
-	       (functions environment))))
-
 (defun (setf compiler-macro-function) (new-function name &optional environment)
   (unless (null environment)
     (error "Environment object must be nil."))
