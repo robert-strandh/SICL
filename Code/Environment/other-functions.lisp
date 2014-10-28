280663a93d702a7fcb0dd36f461397f6b50ba01e
Robert Strandh <robert.strandh@gmail.com>
2014-10-28 09:10:57 +0100

    Added definition of SICL-ENV:GET-SETF-EXPANSION.

--- Code/Environment/other-functions.lisp
+++ Code/Environment/other-functions.lisp
@@ -1,1 +1,29 @@
 (cl:in-package #:sicl-global-environment)
+
+(defun expand-place (place environment)
+  (when (null environment) 
+    (setf environment *global-environment*))
+  (cond ((symbolp place)
+	 (let ((expansion (cleavir-env:symbol-macro-expansion place environment)))
+	   (if (eq place expansion)
+	       place
+	       (expand-place expansion environment))))
+	((and (consp place) (symbolp (first place)))
+	 (let ((expander (cleavir-env:macro-function (first place)
+						     environment)))
+	   (if (null expander)
+	       place
+	       (expand-place (funcall expander place environment)))))
+	(t
+	 (error "Invalid place ~s" place))))
+
+(defun get-setf-expansion (place &optional environment)
+  (when (null environment) 
+    (setf environment *global-environment*))
+  (let* ((expanded-place (expand-place place environment))
+	 (expander (if (symbolp expanded-place)
+		       (default-setf-expander *global-environment*)
+		       (or (setf-expander (first expanded-place)
+					  *global-environment*)
+			   (default-setf-expander *global-environment*)))))
+    (funcall expander expanded-place)))
