f358e01e0a2e65a91b9448d2a24a65daf6345513
Robert Strandh <robert.strandh@gmail.com>
2014-10-28 12:37:58 +0100

    Fill environment with special operators.

--- Code/Compiler/Extrinsic-environment/fill.lisp
+++ Code/Compiler/Extrinsic-environment/fill.lisp
@@ -1,1 +1,5 @@
 (cl:in-package #:sicl-extrinsic-environment)
+
+(loop for symbol being each external-symbol in '#:common-lisp
+      when (special-operator-p symbol)
+	do (setf (sicl-env:special-operator symbol *environment*) t))
