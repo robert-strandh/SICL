80629cf8d049dc57f549194409fae293b5697be9
Robert Strandh <robert.strandh@gmail.com>
2014-10-28 12:56:28 +0100

    Added definition of DEFMACRO to environment.

--- Code/Compiler/Extrinsic-environment/fill.lisp
+++ Code/Compiler/Extrinsic-environment/fill.lisp
@@ -6,3 +6,16 @@
 
 (setf (sicl-env:constant-variable t *environment*) t)
 (setf (sicl-env:constant-variable nil *environment*) nil)
+
+(setf (sicl-env:macro-function 'defmacro *environment*)
+      (compile nil
+	       (sicl-code-utilities:parse-macro
+		'defmacro
+		'(name lambda-list &body body)
+		'(`(eval-when (:compile-toplevel :load-toplevel :execute)
+		     (funcall #'(setf macro-function)
+			      (function ,(sicl-code-utilities:parse-macro
+					  name
+					  lambda-list
+					  body))
+			      ',name))))))
