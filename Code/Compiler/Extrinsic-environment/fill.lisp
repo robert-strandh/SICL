975ba41b670a0dd3d120c67cba142c48c2efaf99
Robert Strandh <robert.strandh@gmail.com>
2014-10-28 12:49:57 +0100

    Added constant varibles T and NIL.

--- Code/Compiler/Extrinsic-environment/fill.lisp
+++ Code/Compiler/Extrinsic-environment/fill.lisp
@@ -3,3 +3,6 @@
 (loop for symbol being each external-symbol in '#:common-lisp
       when (special-operator-p symbol)
 	do (setf (sicl-env:special-operator symbol *environment*) t))
+
+(setf (sicl-env:constant-variable t *environment*) t)
+(setf (sicl-env:constant-variable nil *environment*) nil)
