0eb73c87cc707818d4a6dfc9e01522f7c7f61e37
Robert Strandh <robert.strandh@gmail.com>
2014-10-28 11:18:21 +0100

    Fixed typo.

--- Code/Environment/other-methods.lisp
+++ Code/Environment/other-methods.lisp
@@ -1,9 +1,9 @@
 (cl:in-package #:sicl-global-environment)
 
-(defmethod cleavier-env:macro-function (name (env environment))
+(defmethod cleavir-env:macro-function (name (env environment))
   (macro-function name env))
 
-(defmethod cleavier-env:compiler-macro-function (name (env environment))
+(defmethod cleavir-env:compiler-macro-function (name (env environment))
   (compiler-macro-function name env))
 
 (defmethod cleavir-env:symbol-macro-expansion (name (env environment))
