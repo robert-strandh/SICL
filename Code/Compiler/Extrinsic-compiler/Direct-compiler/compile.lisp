841a668de6c2784a37d0592110a1081dc11b15ac
Robert Strandh <robert.strandh@gmail.com>
2014-10-29 10:38:46 +0100

    Beginning of top-level function COMPILE-FUNCTION-FORM.

--- Code/Compiler/Extrinsic-compiler/Direct-compiler/compile.lisp
+++ Code/Compiler/Extrinsic-compiler/Direct-compiler/compile.lisp
@@ -1,1 +1,7 @@
 (cl:in-package #:sicl-direct-extrinsic-compiler)
+
+(defun compile-function-form (form)
+  (let* ((environment sicl-extrinsic-environment:*environment*)
+	 (sicl-env:*global-environment* environment)
+	 (ast (cleavir-generate-ast:generate-ast form environment)))
+    ast))
