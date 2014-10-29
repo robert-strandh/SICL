e57ed9a7af071d8e7540e22f295c3feb46378ffb
Robert Strandh <robert.strandh@gmail.com>
2014-10-29 09:15:38 +0100

    Improved slot comment.

--- Code/Compiler/Extrinsic-compiler/Direct-compiler/classes.lisp
+++ Code/Compiler/Extrinsic-compiler/Direct-compiler/classes.lisp
@@ -14,11 +14,16 @@
    (%debug-info :initarg :debug-info :reader debug-info)))
 
 (defclass function ()
-  (;; In the direct extrinsic compiler, this slot always contains 0.
-   ;; In the native system, it holds a fixnum, but it really means the
-   ;; address of the first instruction of the function.  When the
-   ;; garbage collector moves the instruction vector of the code, it
-   ;; also updates this slot.
+  (;; This slot contains an offset into the instructions vector where
+   ;; the entry point of this function is located.  In the direct
+   ;; extrinsic compiler, this slot currently always contains 0, but
+   ;; we might add the possibility for the direct extrinsic compiler
+   ;; to compile several functions simultaneously that share the same
+   ;; code.  In that case, different such functions wold contain
+   ;; different offsets.  In the native system, the corresponding slot
+   ;; holds a fixnum, but it really means the address of the first
+   ;; instruction of the function.  When the garbage collector moves
+   ;; the instruction vector of the code, it also updates this slot.
    (%entry-point :initform 0 :initarg :entry-point :accessor entry-point)
    ;; The environment of a function is a list of simple vectors.  When
    ;; the direct extrinsic compiler produces a function, the list
