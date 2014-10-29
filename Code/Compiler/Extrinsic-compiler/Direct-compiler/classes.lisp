c9d033abbadf2d8ac1c12779efb545b86be9de6a
Robert Strandh <robert.strandh@gmail.com>
2014-10-29 08:39:47 +0100

    Added class FUNCTION.

--- Code/Compiler/Extrinsic-compiler/Direct-compiler/classes.lisp
+++ Code/Compiler/Extrinsic-compiler/Direct-compiler/classes.lisp
@@ -12,3 +12,18 @@
    ;; types they have.  We have yet to determine the exact format of
    ;; this information.
    (%debug-info :initarg :debug-info :reader debug-info)))
+
+(defclass function ()
+  (;; In the direct extrinsic compiler, this slot always contains 0.
+   ;; In the native system, it holds a fixnum, but it really means the
+   ;; address of the first instruction of the function.  When the
+   ;; garbage collector moves the instruction vector of the code, it
+   ;; also updates this slot.
+   (%entry-point :initform 0 :initarg :entry-point :accessor entry-point)
+   ;; The environment of a function is a list of simple vectors.  When
+   ;; the direct extrinsic compiler produces a function, the list
+   ;; contains a single simple vector.  That vector holds the code as
+   ;; its first element.  Remaining elements hold constants, function
+   ;; cells, and value cells that are needed for the execution of the
+   ;; function.
+   (%environment :initarg :environment :reader environment)))
