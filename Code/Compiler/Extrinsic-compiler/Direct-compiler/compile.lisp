3a549080a46674316d907148ebb8e1314d9e1a99
Robert Strandh <robert.strandh@gmail.com>
2014-10-30 12:50:21 +0100

    Added translation to MIR and proc/os parameters.

--- Code/Compiler/Extrinsic-compiler/Direct-compiler/compile.lisp
+++ Code/Compiler/Extrinsic-compiler/Direct-compiler/compile.lisp
@@ -1,8 +1,9 @@
 (cl:in-package #:sicl-direct-extrinsic-compiler)
 
-(defun compile-function-form (form)
+(defun compile-function-form (form processor os)
   (let* ((environment sicl-extrinsic-environment:*environment*)
 	 (sicl-env:*global-environment* environment)
 	 (ast (cleavir-generate-ast:generate-ast form environment))
-	 (hir (cleavir-ast-to-hir:compile-toplevel ast)))
-    hir))
+	 (hir (cleavir-ast-to-hir:compile-toplevel ast))
+	 (mir (cleavir-ir:hir-to-mir sicl processor os)))
+    mir))
