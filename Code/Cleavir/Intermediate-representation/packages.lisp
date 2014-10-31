8877cf0e56225a5173077e3575573f2e03f4388b
Robert Strandh <robert.strandh@gmail.com>
2014-10-31 16:11:25 +0100

    Export names of arithmetic instructions.

--- Code/Cleavir/Intermediate-representation/packages.lisp
+++ Code/Cleavir/Intermediate-representation/packages.lisp
@@ -129,6 +129,15 @@
    #:memref2-instruction #:make-memref2-instruction
    #:memset1-instruction #:make-memset1-instruction
    #:memset2-instruction #:make-memset2-instruction
+   #:signed-add-instruction #:make-signed-add-instruction
+   #:signed-sub-instruction #:make-signed-sub-instruction
+   #:signed-less-instruction #:make-signed-less-instruction
+   #:signed-not-greater-instruction :#make-signed-not-greater-instruction
+   #:unsigned-add-instruction #:make-unsigned-add-instruction
+   #:unsigned-sub-instruction #:make-unsigned-sub-instruction
+   #:unsigned-less-instruction #:make-unsigned-less-instruction 
+   #:unsigned-not-greater-instruction #:make-unsigned-not-greater-instruction
+   #:equal-instruction #:make-equal-instruction
    #:hir-to-mir #:specialize))
 
 (defpackage #:cleavir-ir-graphviz
