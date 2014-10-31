48fe3df22859f935866c6c701147fd5c2a09e4ca
Robert Strandh <robert.strandh@gmail.com>
2014-10-31 16:05:12 +0100

    Specialize FIXNUM-LESS-INSTRUCTION.

--- Code/Backends/Processors/x86/64/HIR-to-MIR/integer.lisp
+++ Code/Backends/Processors/x86/64/HIR-to-MIR/integer.lisp
@@ -19,3 +19,13 @@
   (cleavir-ir:make-signed-sub-instruction
    :inputs inputs (cleavir-ir:inputs instruction)
    :successors (cleavir-ir:successors instruction)))
+
+(defmethod cleavir-ir:specialize
+    ((instruction cleavir-ir:fixnum-less-instruction)
+     (implementation sicl-target-sicl:sicl)
+     (processor cleavir-processor-x86-64:x86-64)
+     os)
+  (declare (ignore os))
+  (cleavir-ir:make-signed-less-instruction
+   :inputs inputs (cleavir-ir:inputs instruction)
+   :successors (cleavir-ir:successors instruction)))
