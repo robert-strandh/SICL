5f74f57d74859865ebdf7c0732e4185f277034bd
Robert Strandh <robert.strandh@gmail.com>
2014-10-31 16:03:27 +0100

    Specialize FIXNUM-ADD-INSTRUCTION.

--- Code/Backends/Processors/x86/64/HIR-to-MIR/integer.lisp
+++ Code/Backends/Processors/x86/64/HIR-to-MIR/integer.lisp
@@ -1,1 +1,11 @@
 (cl:in-package #:sicl-hir-to-mir)
+
+(defmethod cleavir-ir:specialize
+    ((instruction cleavir-ir:fixnum-add-instruction)
+     (implementation sicl-target-sicl:sicl)
+     (processor cleavir-processor-x86-64:x86-64)
+     os)
+  (declare (ignore os))
+  (cleavir-ir:make-signed-add-instruction
+   :inputs inputs (cleavir-ir:inputs instruction)
+   :successors (cleavir-ir:successors instruction)))
