18906ced6545b0647680e3546b949bd2bd13e568
Robert Strandh <robert.strandh@gmail.com>
2014-11-03 08:47:20 +0100

    Removed yet another component that is no longer used.

--- Code/Environment/declaim-1.lisp
+++
@@ -1,7 +0,0 @@
-(cl:in-package #:sicl-global-environment)
-
-(defmacro declaim (&rest declaration-specifiers)
-  `(eval-when (:compile-toplevel)
-     (progn ,@(mapcar #'(lambda (declaration-specifier)
-			  `(proclaim ',declaration-specifier))
-		      declaration-specifiers))))
