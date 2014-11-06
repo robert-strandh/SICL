5557fac9ae213e1d6784350c1e92d5d50f240324
Robert Strandh <robert.strandh@gmail.com>
2014-11-06 11:00:53 +0100

    Function for finding live lexicals after function calls.

--- Code/Compiler/Extrinsic-compiler/Direct-compiler/compile.lisp
+++ Code/Compiler/Extrinsic-compiler/Direct-compiler/compile.lisp
@@ -1,5 +1,24 @@
 (cl:in-package #:sicl-direct-extrinsic-compiler)
 
+;;; Find variables that are live immediately after a call instruction.
+(defun find-live-after-call (initial-instruction)
+  (let ((liveness (cleavir-liveness:liveness
+		   initial-instruction
+		   #'cleavir-ir:successors
+		   #'cleavir-ir:inputs 
+		   #'cleavir-ir:outputs))
+	(table (make-hash-table :test #'eq))
+	(result '()))
+    (labels ((traverse (insn)
+	       (unless (gethash insn table)
+		 (setf (gethash insn table) t)
+		 (when (typep insn 'cleavir-ir:funcall-instruction)
+		   (setf result
+			 (append (cleavir-liveness:live-after liveness insn)
+				 result)))
+		 (traverse (cleavir-ir:successors insn)))))
+      (traverse initial-instruction))))
+
 (defun compile-function-form (form processor os)
   (let* ((environment sicl-extrinsic-environment:*environment*)
 	 (sicl-env:*global-environment* environment)
