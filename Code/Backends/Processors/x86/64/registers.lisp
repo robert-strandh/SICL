285864ac5c8aabea0b036294ab2d128c6e3f931f
Robert Strandh <robert.strandh@gmail.com>
2014-11-06 09:43:37 +0100

    Method on ARGUMENT-REGISTERS.

--- Code/Backends/Processors/x86/64/registers.lisp
+++ Code/Backends/Processors/x86/64/registers.lisp
@@ -4,3 +4,6 @@
   (loop for i in '(0 2 3 6 7 8 9 11 12 13 14 15)
 	collect (cleavir-processor-x86-64:gpr processor i)))
 
+(defmethod argument-registers ((processor x86-64))
+  (loop for i in '(7 6 3 2 8)
+	collect (cleavir-processor-x86-64:gpr processor i)))
