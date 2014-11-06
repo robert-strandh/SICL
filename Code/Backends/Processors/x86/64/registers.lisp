567065adfd7f058a3466060af52e5115f9660ae1
Robert Strandh <robert.strandh@gmail.com>
2014-11-06 09:41:38 +0100

    Method on AVAILABLE-REGISTERS.

--- Code/Backends/Processors/x86/64/registers.lisp
+++ Code/Backends/Processors/x86/64/registers.lisp
@@ -1,3 +1,6 @@
 (cl:in-package #:sicl-x86-64)
 
+(defmethod cleavir-processor-x86-64:available-registers ((processor x86-64))
+  (loop for i in '(0 2 3 6 7 8 9 11 12 13 14 15)
+	collect (cleavir-processor-x86-64:gpr processor i)))
 
