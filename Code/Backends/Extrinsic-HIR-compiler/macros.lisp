9ca0ae893a038f882279988d2c353a3cf3d90bc8
Robert Strandh <robert.strandh@gmail.com>
2014-11-07 09:52:05 +0100

    Defined target macro CL:CATCH.

--- Code/Backends/Extrinsic-HIR-compiler/macros.lisp
+++ Code/Backends/Extrinsic-HIR-compiler/macros.lisp
@@ -1,1 +1,11 @@
 (cl:in-package #:sicl-extrinsic-hir-compiler)
+
+(defmacro cl:catch (tag &body forms)
+  (let ((block-name (gensym)))
+    `(block ,block-name
+       (catch tag
+	 (lambda (values)
+	   (return-from ,block-name
+	     (apply #'values values)))
+	 (lambda ()
+	   ,@forms)))))
