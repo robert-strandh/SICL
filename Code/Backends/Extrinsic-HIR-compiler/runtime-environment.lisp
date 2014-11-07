2b1d0060ecd677ab7c4fb0380d74407c85395df4
Robert Strandh <robert.strandh@gmail.com>
2014-11-07 08:54:18 +0100

    Define function for unwinding the runtime environment.

--- Code/Backends/Extrinsic-HIR-compiler/runtime-environment.lisp
+++ Code/Backends/Extrinsic-HIR-compiler/runtime-environment.lisp
@@ -20,3 +20,14 @@
 ;;; The dynamic environment is a list of instances of the class
 ;;; DYNAMIC-ENVIRONMENT-ENTRY.
 (defparameter *dynamic-environment* '())
+
+;;; SUFFIX is a suffix of the current dynamic environment.  This
+;;; function removes entries until from the runtime environment until
+;;; the suffix is reached, executing any UNWIND-PROTECT entries it
+;;; finds.
+(defun unwind (suffix)
+  (loop until (eq suffix *dynamic-environment*)
+	for entry = (car *dynamic-environment*)
+	do (pop *dynamic-environment*)
+	   (when (typep entry 'unwind-protect)
+	     (funcall (thunk entry)))))
