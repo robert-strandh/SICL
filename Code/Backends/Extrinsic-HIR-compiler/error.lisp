6cbee22f4e71b68358c3fcd20d6d4521164884d8
Robert Strandh <robert.strandh@gmail.com>
2014-11-09 14:42:15 +0100

    Explain the reason for the definition of ERROR.

--- Code/Backends/Extrinsic-HIR-compiler/error.lisp
+++ Code/Backends/Extrinsic-HIR-compiler/error.lisp
@@ -1,5 +1,10 @@
 (cl:in-package #:sicl-extrinsic-hir-compiler)
 
+;;; This function will be entered under the name CL:ERROR in the
+;;; global environment.  It will be entered early on, because many
+;;; functions that will subsequently be entered into the environment
+;;; will refer to it.  Later, when the environment is more complete,
+;;; we replace this function with one that invokes the condition
+;;; system, calls MAKE-INSTANCE, FORMAT and many other things.
 (defun error (&rest arguments)
   (cl:error "ERROR was called with the following arguments: ~s" arguments))
- 
