cafac315592af148a07e8459c27cb19d69ab6e49
Robert Strandh <robert.strandh@gmail.com>
2014-11-11 15:19:39 +0100

    Added comments explaining the purpose of the code.

--- Code/Backends/Extrinsic-HIR-compiler/parse-arguments.lisp
+++ Code/Backends/Extrinsic-HIR-compiler/parse-arguments.lisp
@@ -1,5 +1,40 @@
 (cl:in-package #:sicl-extrinsic-hir-compiler)
 
+;;;; Generate code for parsing a list of arguments according to some
+;;;; lambda list.
+;;;;
+;;;; The lambda list used here has the same lambda-list keywords as an
+;;;; ordinary lambda list and in the same order.  Parameters have the
+;;;; following form:
+;;;;
+;;;;   * Each required argument is just a symbol.
+;;;;
+;;;;   * Each &OPTIONAL argument is a list of two symbols.  The first
+;;;;     element of the list is the name of the variable to hold the
+;;;;     supplied, argument or NIL if no argument was supplied.  The
+;;;;     second element of the list is the name of the associated
+;;;;     SUPPLIED-P parameter.
+;;;;
+;;;;  * Each &KEY argument is a list of three symbols.  The first
+;;;;    element of the list is the keyword to be used to pass the
+;;;;    argument.  The second element of the list is the name of the
+;;;;    variable to hold the supplied argument, or NIL if no argument
+;;;;    was supplied.  The third element of the list is the name of the
+;;;;    associated SUPPLIED-P parameter.
+
+;;; Generate a form that parses the arguments to a function.
+;;;
+;;; The parameter VAR contains the name a variable (a symbol) that at
+;;; runtime will hold the remaining arguments to be parsed.  The value
+;;; of that variable is changed at runtime as more arguments are
+;;; parsed.
+;;;
+;;; ERROR is the name of a variable (a symbol) that holds the function
+;;; ERROR to be called in case an error is detected.
+;;;
+;;; DEFAULT is the name of a variable (a symbol) that holds a unique
+;;; value that should be used to pass to GETF, and that GETF will
+;;; return in case the corresponding key was not found.
 (defun parse (lambda-list var error default)
   (let ((result '())
 	(rest lambda-list))
@@ -78,6 +113,22 @@
      out)
     (reverse result)))
 
+;;; Generate a form that parses the arguments to a function.
+;;;
+;;; The function for which argument parsing code is to be generated
+;;; takes a single &REST argument.  The name of that argument (a
+;;; symbol) is the value of the parameter named ARGUMENT-VARIABLE.
+;;;
+;;; ERROR is the name of a variable (a symbol) that holds the function
+;;; ERROR to be called in case an error is detected.
+;;;
+;;; DEFAULT is the name of a variable (a symbol) that holds a unique
+;;; value that should be used to pass to GETF, and that GETF will
+;;; return in case the corresponding key was not found.
+;;;
+;;; In order to avoid modifying the value of the &REST parameter of
+;;; the resulting code, we generate a temporary variable that will be
+;;; modified as argument parsing progresses.
 (defun build-argument-parsing-code (lambda-list argument-variable error default)
   (let ((remaining-argument-variable (gensym)))
     `(let ((,remaining-argument-variable ,argument-variable))
