9048101b37aab29fe3be43caf7169edbe168dd99
Robert Strandh <robert.strandh@gmail.com>
2014-11-18 07:49:22 +0100

    Improved code for augmenting an environment with declartions.

--- Code/Cleavir/Generate-AST/generate-ast.lisp
+++ Code/Cleavir/Generate-AST/generate-ast.lisp
@@ -2,49 +2,82 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;
-;;; Convenience functions for augmenting the environment with a bunch
-;;; of declarations.
+;;; Convenience functions for augmenting the environment with a set of
+;;; canonicalized declaration specifiers.
+;;;
+;;; Recall that a canonicalized declaration specifier is one of
+;;; following:
+;;;
+;;;   * (declaration name)
+;;;   * (dynamic-extent var)
+;;;   * (dynamic-extent (function fn))
+;;;   * (ftype type function-name)
+;;;   * (ignore var)
+;;;   * (ignore (function fn))
+;;;   * (ignorable var)
+;;;   * (ignorable (function fn))
+;;;   * (inline function-name)
+;;;   * (notinline function-name)
+;;;   * (optimize (quality value))
+;;;   * (special var)
+;;;   * (type typespec var)
 
-(defun augment-environment-with-declaration (declaration environment)
-  (destructuring-bind (head . rest) declaration
-    (let ((new-env (case head
-		     ;; (declaration
-		     ;; (make-declaration-declaration-entry (car rest)))
-		     (dynamic-extent
-		      (if (consp (car rest))
-			  (cleavir-env:add-function-dynamic-extent
-			   environment (cadr (car rest)))
-			  (cleavir-env:add-variable-dynamic-extent
-			   environment (car rest))))
-		     (ftype
-		      (cleavir-env:add-function-type
-		       environment (cadr rest) (car rest)))
-		     ((ignore ignorable)
-		      (if (consp (car rest))
-			  (cleavir-env:add-function-ignore
-			   environment (cadr (car rest)) head)
-			  (cleavir-env:add-variable-ignore
-			   environment (car rest) head)))
-		     ((inline notinline)
-		      (cleavir-env:add-inline
-		       environment (car rest) head))
-		     ;; (optimize
-		     ;; (make-optimize-declaration-entry
-		     ;; (car (car rest)) (cadr (car rest))))
-		     ;; (special
-		     ;; FIXME: is this right?
-		     ;; (make-special-variable-entry (car rest)))
-		     (type
-		      (cleavir-env:add-function-type
-		       environment (cadr rest) (car rest))))))
-      new-env)))
+;;; Augment the environment with a single canonicalized declartion
+;;; specifier.
+(defun augment-environment-with-declaration
+    (canonicalized-declaration-specifier environment)
+  (destructuring-bind (head . rest) canonicalized-declaration-specifier
+    (case head
+      ;; (declaration
+      ;; (make-declaration-declaration-entry (car rest)))
+      (dynamic-extent
+       (let ((var-or-function (car rest)))
+	 (if (consp var-or-function)
+	     (cleavir-env:add-function-dynamic-extent
+	      environment (cadr var-or-function))
+	     (cleavir-env:add-variable-dynamic-extent
+	      environment (car var-or-function)))))
+      (ftype
+       (cleavir-env:add-function-type
+	environment (cadr rest) (car rest)))
+      ((ignore ignorable)
+       (if (consp (car rest))
+	   (cleavir-env:add-function-ignore
+	    environment (cadr (car rest)) head)
+	   (cleavir-env:add-variable-ignore
+	    environment (car rest) head)))
+      ((inline notinline)
+       (cleavir-env:add-inline
+	environment (car rest) head))
+      (optimize
+       (destructuring-bind (quality value) (car rest)
+	 (cleavir-env:add-optimize environment quality value)))
+      (special
+       ;; This case is a bit tricky, because if the
+       ;; variable is globally special, nothing should
+       ;; be added to the environment.
+       (let ((info (cleavir-env:variable-info environment (car rest))))
+	 (if (and (typep info 'cleavir-env:special-variable-info)
+		  (cleavir-env:global-p info))
+	     environment
+	     (cleavir-env:add-special-variable environment (car rest)))))
+      (type
+       (cleavir-env:add-function-type
+	environment (cadr rest) (car rest)))
+      (t
+       (warn "Unable to handle declarations specifier: ~s"
+	     canonicalized-declaration-specifier)
+       environment))))
 
-(defun augment-environment-with-declarations (environment declarations)
-  (let ((declaration-specifiers
+;;; Augment the environment with a list of canonicalized declartion
+;;; specifiers.
+(defun augment-environment-with-declarations
+    (environment declaration-specifiers)
+  (let ((canonicalized-declaration-specifiers
 	  (cleavir-code-utilities:canonicalize-declaration-specifiers
-	   (reduce #'append (mapcar #'cdr declarations))))
+	   (reduce #'append (mapcar #'cdr declaration-specifiers))))
 	(new-env environment))
-    (loop for spec in declaration-specifiers
+    (loop for spec in canonicalized-declaration-specifiers
 	  do (setf new-env (augment-environment-with-declaration spec new-env)))
     new-env))
 
