c4e177771916f84df207c60aa72de1877013479e
Robert Strandh <robert.strandh@gmail.com>
2014-11-19 08:00:37 +0100

    New system for internationalization.

---
+++ Code/Cleavir/Internationalization/cleavir-internationalization.asd
@@ -0,0 +1,6 @@
+(cl:in-package #:common-lisp-user)
+
+(asdf:defsystem :cleavir-internationalization
+  :serial t
+  :components
+  ((:file "packages")))
---
+++ Code/Cleavir/Internationalization/packages.lisp
@@ -0,0 +1,7 @@
+(cl:in-package #:common-lisp-user)
+
+(defpackage #:cleavir-internationalization
+  (:nicknames #:cleavir-i18n)
+  (:use #:common-lisp)
+  (:export
+   ))
