5e59feb62e39e47c7536e2e78d2d37be44a7d6f6
Robert Strandh <robert.strandh@gmail.com>
2014-11-19 09:14:57 +0100

    New generic function SHORT-DAY-NAME.

--- Code/Cleavir/Internationalization/date.lisp
+++ Code/Cleavir/Internationalization/date.lisp
@@ -3,3 +3,5 @@
 ;;; DAY is the ISO8601 day number where the days are numbered from 1
 ;;; starting on Monday. 
 (defgeneric long-day-name (day language))
+
+(defgeneric short-day-name (day language))
