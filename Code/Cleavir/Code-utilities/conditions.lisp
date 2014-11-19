814ce386fb8dd6cb8fb60274307347cb9b7a6685
Robert Strandh <robert.strandh@gmail.com>
2014-11-19 09:51:14 +0100

    Added CLEAVIR-I18N:CONTION as superclass of condition types.

--- Code/Cleavir/Code-utilities/conditions.lisp
+++ Code/Cleavir/Code-utilities/conditions.lisp
@@ -1,20 +1,25 @@
 (in-package #:cleavir-code-utilities)
 
-(define-condition form-must-be-proper-list (program-error)
-  ())
+(define-condition form-must-be-proper-list
+    (program-error cleavir-i18n:condition)
+  ((%form :initarg :form :reader form)))
 
 ;;; A max-argcount of NIL means no upper bound.
-(define-condition invalid-number-of-arguments (program-error)
+(define-condition invalid-number-of-arguments 
+    (program-error cleavir-i18n:condition)
   ((%form :initarg :form :reader form)
    (%min-argcount :initarg :min-argcount :reader min-argcount)
    (%max-argcount :initarg :max-argcount :reader max-argcount)))
 
-(define-condition too-few-arguments (program-error)
+(define-condition too-few-arguments
+    (program-error cleavir-i18n:condition)
   ())
 
-(define-condition too-many-arguments (program-error)
+(define-condition too-many-arguments
+    (program-error cleavir-i18n:condition)
   ())
 
-(define-condition odd-number-of-keyword-arguments (program-error)
+(define-condition odd-number-of-keyword-arguments
+    (program-error cleavir-i18n:condition)
   ())
 
