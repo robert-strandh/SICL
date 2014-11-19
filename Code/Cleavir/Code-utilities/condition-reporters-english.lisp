6746595f78a74580cf975224b535c1654cf1a8da
Robert Strandh <robert.strandh@gmail.com>
2014-11-19 09:52:58 +0100

    Added condition reporter for INVALID-NUMBER-OF-ARGUMENTS.

--- Code/Cleavir/Code-utilities/condition-reporters-english.lisp
+++ Code/Cleavir/Code-utilities/condition-reporters-english.lisp
@@ -5,3 +5,19 @@
   (format stream "A form must be a proper list,~@
                   but the following was found instead:~@
                   ~s" (form condition)))
+
+(defmethod cleavir-i18n:report-condition
+    ((condition invalid-number-of-arguments)
+     stream
+     (language cleavir-i18n:english))
+  (format stream
+	  "Invalid number of arguments.~@
+           There must be at least ~d arguments.~%"
+	  (min-argcount condition))
+  (unless (null (max-argcount condition))
+    (format stream
+	    "And there must be at most ~d arguments.~%"
+	    (max-argcount condition)))
+  (format stream
+	  "The form that causes the problem was:~%~s"
+	  (form condition)))
