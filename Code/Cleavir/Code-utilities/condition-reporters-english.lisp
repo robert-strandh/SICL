2d8bf33f9afa1613c32d5bbc3382070afdf0a63a
Robert Strandh <robert.strandh@gmail.com>
2014-11-19 09:52:34 +0100

    Added condition reporter for FORM-MUST-BE-PROPER-LIST.

--- Code/Cleavir/Code-utilities/condition-reporters-english.lisp
+++ Code/Cleavir/Code-utilities/condition-reporters-english.lisp
@@ -1,1 +1,7 @@
 (in-package #:cleavir-code-utilities)
+
+(defmethod cleavir-i18n:report-condition
+    ((condition form-must-be-proper-list) stream (language cleavir-i18n:english))
+  (format stream "A form must be a proper list,~@
+                  but the following was found instead:~@
+                  ~s" (form condition)))
