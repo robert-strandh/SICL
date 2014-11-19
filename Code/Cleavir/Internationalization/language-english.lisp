8e2239542f21dc1c187bc64765cc4fbc7d532f9e
Robert Strandh <robert.strandh@gmail.com>
2014-11-19 09:23:58 +0100

    Added methods on SHORT-DAY-NAME specialized to ENGLISH.

--- Code/Cleavir/Internationalization/language-english.lisp
+++ Code/Cleavir/Internationalization/language-english.lisp
@@ -3,6 +3,10 @@
 (defclass english (language)
   ())
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;; Methods on LONG-DAY-NAME.
+
 (defmethod long-day-name ((day (eql 1)) (language english))
   "Monday")
 
@@ -23,3 +27,28 @@
 
 (defmethod long-day-name ((day (eql 7)) (language english))
   "Sunday")
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;; Methods on SHORT-DAY-NAME.
+
+(defmethod short-day-name ((day (eql 1)) (language english))
+  "Mon")
+
+(defmethod short-day-name ((day (eql 2)) (language english))
+  "Tue")
+
+(defmethod short-day-name ((day (eql 3)) (language english))
+  "Wed")
+
+(defmethod short-day-name ((day (eql 4)) (language english))
+  "Thu")
+
+(defmethod short-day-name ((day (eql 5)) (language english))
+  "Fri")
+
+(defmethod short-day-name ((day (eql 6)) (language english))
+  "Sat")
+
+(defmethod short-day-name ((day (eql 7)) (language english))
+  "Sun")
