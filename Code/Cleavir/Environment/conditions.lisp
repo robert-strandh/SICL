a26e5d5caccf54617dcabdbfce0730804c366f43
Robert Strandh <robert.strandh@gmail.com>
2014-11-19 08:56:54 +0100

    Added CLEAVIR-I18N:CONDITION as superclass to conditions.

--- Code/Cleavir/Environment/conditions.lisp
+++ Code/Cleavir/Environment/conditions.lisp
@@ -1,13 +1,17 @@
 (cl:in-package #:cleavir-environment)
 
-(define-condition no-variable-info (program-error)
+(define-condition no-variable-info
+    (program-error cleavir-i18n:condition)
   ((%name :initarg :name :reader name)))
 
-(define-condition no-function-info (program-error)
+(define-condition no-function-info
+    (program-error cleavir-i18n:condition)
   ((%name :initarg :name :reader name)))
 
-(define-condition no-block-info (program-error)
+(define-condition no-block-info
+    (program-error cleavir-i18n:condition)
   ((%name :initarg :name :reader name)))
 
-(define-condition no-tag-info (program-error)
+(define-condition no-tag-info
+    (program-error cleavir-i18n:condition)
   ((%name :initarg :name :reader name)))
