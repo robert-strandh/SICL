85fe5ce2fab2b9b0beff0e384c9f5a4d98501acb
Robert Strandh <robert.strandh@gmail.com>
2014-11-23 08:05:38 +0100

    Added comment describing the general method for processing the lambda list.

--- Code/Cleavir/Generate-AST/generate-ast.lisp
+++ Code/Cleavir/Generate-AST/generate-ast.lisp
@@ -49,6 +49,23 @@
 		value-ast)
 	       next-ast)))))
 
+;;; The general method for processing the lambda list is as follows:
+;;; We use recursion to process the remaining lambda list.  Before the
+;;; recursive call, we add the current parameters to the environment
+;;; that we pass to the recursive call.  The call returns two values:
+;;; the AST that was built and modified lambda list, containing the
+;;; lambda list keywords, and the lexical variables that were
+;;; introduced.
+;;;
+;;; The reason we do it this way is that, if a parameter turns out to
+;;; be a special variable, the entire rest of the lambda list and
+;;; function body must be executed with this variable bound.  The AST
+;;; configuration for expressing that situation is that the AST for
+;;; computing the rest of the lambda list and the body must be a child
+;;; of a BIND-AST that indicates that the special variable should be
+;;; bound.  This recursive method makes sure that the child exists
+;;; before the BIND-AST is created.
+
 (defun process-remaining-aux (aux dspecs forms env)
   (if (null aux)
       (cleavir-ast:make-progn-ast (convert-sequence forms env))
