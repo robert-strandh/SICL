e14d49ccf81abec6fc1ac61a10ffe832ce089b99
Robert Strandh <robert.strandh@gmail.com>
2014-11-24 05:28:46 +0100

    Added method for translating MULTIPLE-TO-FIXED-INSTRUCTION.

--- Code/Cleavir/Backends/HIR-interpreter/translate.lisp
+++ Code/Cleavir/Backends/HIR-interpreter/translate.lisp
@@ -255,6 +255,13 @@
   `(setq ,(first outputs)
 	 (list ,@inputs)))
 
+(defmethod translate-simple-instruction
+    ((instruction cleavir-ir:multiple-to-fixed-instruction) inputs outputs)
+  (let ((temp (gensym)))
+    `(let ((,temp ,(first inputs)))
+       ,@(loop for output in outputs
+	       collect `(setf ,output (pop ,temp))))))
+  
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;
 ;;; Methods on TRANSLATE-BRANCH-INSTRUCTION.
