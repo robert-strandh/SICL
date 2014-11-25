792f5e3f20f1a7fc0719e7bd43ec4a443a3523f5
Robert Strandh <robert.strandh@gmail.com>
2014-11-25 07:47:53 +0100

    Bind special variable to indicate that this is the file compiler.

--- Code/Cleavir/Generate-AST/ast-from-file.lisp
+++ Code/Cleavir/Generate-AST/ast-from-file.lisp
@@ -1,12 +1,12 @@
 (cl:in-package #:cleavir-generate-ast)
 
 (defun ast-from-stream (stream environment)
-  (cleavir-ast:make-progn-ast
-   (let ((eof (list nil)))
-     (loop for form = (read stream nil eof)
-	   until (eq form eof)
-	   collect (generate-ast form environment)))))
-
+  (let ((*compiler* 'compile-file))
+    (cleavir-ast:make-progn-ast
+     (let ((eof (list nil)))
+       (loop for form = (read stream nil eof)
+	     until (eq form eof)
+	     collect (generate-ast form environment))))))
 
 (defun ast-from-file (filename environment)
   (with-open-file (stream filename :direction :input)
