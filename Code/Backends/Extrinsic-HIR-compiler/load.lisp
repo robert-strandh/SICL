dedd0e5f7fd854aec755544f96bfd16ddefd7499
Robert Strandh <robert.strandh@gmail.com>
2014-11-28 12:57:16 +0100

    Defined function LOAD for loading source code.

--- Code/Backends/Extrinsic-HIR-compiler/load.lisp
+++ Code/Backends/Extrinsic-HIR-compiler/load.lisp
@@ -1,1 +1,8 @@
 (cl:in-package #:sicl-extrinsic-hir-compiler)
+
+(defun load (file)
+  (with-open-file (stream file :direction :input)
+    (loop with eof = (list nil)
+	  for form = (sicl-reader:read stream nil eof)
+	  until (eq form eof)
+	  do (eval form))))
