cd746aa2e87e948638408948e07df6f6ad51e3de
Robert Strandh <robert.strandh@gmail.com>
2014-11-29 05:40:48 +0100

    Defined condition reporter in English for newly-added condition.

--- Code/Environment/condition-reporters-english.lisp
+++ Code/Environment/condition-reporters-english.lisp
@@ -23,3 +23,13 @@
            be a proper list, but the following was found instead:~@
            ~s"
          (variables condition)))
+
+(defmethod cleavir-i18n:report-condition
+    ((condition variable-must-be-symbol)
+     stream
+     (langauge cleavir-i18n:english))
+  (format stream
+	  "A variable to be bound by MULTIPLE-VALUE-BIND must~@
+           be a symbol, but the following was found instead:~@
+           ~s"
+         (variable condition)))
