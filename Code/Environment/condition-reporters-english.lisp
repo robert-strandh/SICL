62a194769325d29a65582a7ee51f37d8499d19f4
Robert Strandh <robert.strandh@gmail.com>
2014-11-29 03:50:37 +0100

    Defined condition reporter in English for newly-added condition.

--- Code/Environment/condition-reporters-english.lisp
+++ Code/Environment/condition-reporters-english.lisp
@@ -3,3 +3,13 @@
 (defmethod cleavir-i18n:report-condition
     ((condition no-such-class) stream (langauge cleavir-i18n:english))
   (format stream "There is no class named ~s" (name condition)))
+
+(defmethod cleavir-i18n:report-condition
+    ((condition odd-number-of-arguments-to-setf)
+     stream
+     (langauge cleavir-i18n:english))
+  (format stream 
+	  "An odd number of arguments was given to SETF~@
+           in the following form:~@
+           ~s"
+          (form condition)))
