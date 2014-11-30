5fb5bf4ad1bda172edde551e18725534baa6c795
Robert Strandh <robert.strandh@gmail.com>
2014-11-30 10:29:11 +0100

    Take into account new location of file containing the LAMBDA macro.

--- Code/Backends/Extrinsic-HIR-compiler/load-files.lisp
+++ Code/Backends/Extrinsic-HIR-compiler/load-files.lisp
@@ -3,6 +3,6 @@
 (defun rp (filename)
   (asdf:system-relative-pathname :sicl-extrinsic-hir-compiler filename))
 
-(load (rp "../../Environment/lambda.lisp"))
+(load (rp "../../Evaluation-and-compilation/lambda.lisp"))
 (load (rp "../../Environment/multiple-value-bind.lisp"))
 (load (rp "../../Environment/setf.lisp"))
