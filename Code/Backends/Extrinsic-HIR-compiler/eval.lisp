908ab357664ecc5c38c49c69cd77723372e9fbb7
Robert Strandh <robert.strandh@gmail.com>
2014-11-30 12:59:07 +0100

    Added method on CLEAVIR-ENV:EVAL specialized to ENVIRONMENT.

--- Code/Backends/Extrinsic-HIR-compiler/eval.lisp
+++ Code/Backends/Extrinsic-HIR-compiler/eval.lisp
@@ -18,3 +18,15 @@
 	  (let ((fdefinition *fdefinition*))
 	    (declare (ignorable fdefinition))
 	    (lambda () ,(translate hir)))))))))
+
+(defmethod cleavir-env:eval (form environment1 (environment2 environment))
+  (let* ((ast (cleavir-generate-ast:generate-ast form environment1))
+	 (hir (cleavir-ast-to-hir:compile-toplevel ast)))
+    (funcall
+     (funcall
+      (compile
+       nil
+       `(lambda ()
+	  (let ((fdefinition *fdefinition*))
+	    (declare (ignorable fdefinition))
+	    (lambda () ,(translate hir)))))))))
