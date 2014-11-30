659e20b34029254b03d1564b858bdec26b886acb
Robert Strandh <robert.strandh@gmail.com>
2014-11-30 12:28:00 +0100

    Set function type at compile time.

--- Code/Environment/defun.lisp
+++ Code/Environment/defun.lisp
@@ -1,16 +1,21 @@
 (cl:in-package #:sicl-global-environment)
 
-(defmacro defun (name lambda-list &body body)
+(defmacro defun (&environment env name lambda-list &body body)
   (multiple-value-bind (declarations documentation forms)
       (cleavir-code-utilities:separate-function-body body)
-    ;; FIXME: handle documentation
-    (declare (ignore documentation))
-    (let ((definition `(lambda ,lambda-list
-			 ,declarations
-			 (block ,name
-			   ,@forms))))
+    (let ((global-env (cleavir-env:global-environment env))
+	  (arg-type
+	    (cleavir-code-utilities:lambda-list-type-specifier lambda-list)))
       `(progn
 	 (eval-when (:compile-toplevel)
-	   (sicl-compiler:compile-time-compile ',name ',definition))
+	   (setf (sicl-env:function-type ',name ,global-env)
+		 `(function ,arg-type t)))
 	 (eval-when (:load-toplevel :execute)
-	   (funcall #'(setf fdefinition) ,definition ',name))))))
+	   (setf (sicl-env:fdefinition ',name ,global-env)
+		 (lambda ,lambda-list
+		   ,declarations
+		   ,@(if (null documentation)
+			 '()
+			 (list documentation))
+		   (block ,name
+		     ,@forms))))))))
