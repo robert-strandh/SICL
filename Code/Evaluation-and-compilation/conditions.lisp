ce2ed0e7f0813679c11036bdac1ed584cd85659f
Robert Strandh <robert.strandh@gmail.com>
2014-12-01 12:42:49 +0100

    New condition signaled by (SETF MACRO-FUNCTION).

--- Code/Evaluation-and-compilation/conditions.lisp
+++ Code/Evaluation-and-compilation/conditions.lisp
@@ -1,1 +1,5 @@
 (cl:in-package #:sicl-evaluation-and-compilation)
+
+(define-condition environment-must-be-omitted-or-nil
+    (error cleavir-i18n:condition)
+  ((%environment :initarg :environment :reader environment)))
