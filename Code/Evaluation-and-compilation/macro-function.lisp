d5edecc22f1ceabbdc3358f10f200f5a3e32a3e0
Robert Strandh <robert.strandh@gmail.com>
2014-12-01 11:56:57 +0100

    Added definition of (SETF CL:MACRO-FUNCTION).

--- Code/Evaluation-and-compilation/macro-function.lisp
+++ Code/Evaluation-and-compilation/macro-function.lisp
@@ -2,3 +2,9 @@
 
 (defun macro-function (symbol &optional environment)
   (cleavir-env:macro-function symbol (or environment *global-environment*)))
+
+(defun (setf macro-function) (new-function symbol &optional environment)
+  (unless (null environment)
+    (error 'environment-must-be-omitted-or-nil))
+  (setf (sicl-env:macro-function symbol *global-environment*)
+	new-function))
