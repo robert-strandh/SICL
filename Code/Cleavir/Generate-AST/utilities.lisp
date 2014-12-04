ccdcaa2bf102932f72b2a8ca371684fb8ddc8964
Robert Strandh <robert.strandh@gmail.com>
2014-12-04 07:42:55 +0100

    Added utility function for itemizing declaration specifiers.

--- Code/Cleavir/Generate-AST/utilities.lisp
+++ Code/Cleavir/Generate-AST/utilities.lisp
@@ -160,3 +160,32 @@
 	else
 	  collect spec into second
 	finally (return (values first second))))
+
+;;; This function takes two arguments.  The first argument, VARIABLES,
+;;; is a list of items, where each item is a non-empty list of symbols
+;;; that are bound in a single binding.  The second items
+;;; DECLARATION-SPECIFIERS, is a list of canonicalized declaration
+;;; specifiers.  This function returns a two values.  The first return
+;;; value is a list with the same length as VARIABLES.  Each element
+;;; in that list contains the elements in DECLARATION-SPECIFIERS that
+;;; apply to the corresponding element in VARIABLES.  The second
+;;; return value is a list of the remaining declaration specifiers in
+;;; DECLARATION-SPECIFIERS, i.e. the ones that do not apply to any
+;;; element in VARIABLES.  A particular symbol S can not appear twice
+;;; in an item of VARIABLES, but it can appear in different items.  In
+;;; that case, the declaration specifiers that apply to that symbol
+;;; will be associated with the last item in the list of VARIABLES.
+(defun itemize-declaration-specifiers (variables canonical-dspecs)
+  (if (null variables)
+      (values '() canonical-dspecs)
+      (multiple-value-bind (itemized-dspecs remaining-dspecs)
+	  (itemize-declaration-specifiers (cdr variables) canonical-dspecs)
+	(let ((item-specific-dspecs '()))
+	  (loop for var in (first variables)
+		do (multiple-value-bind (is-dspecs r-dspecs)
+		       (separate-declarations remaining-dspecs var)
+		     (setf item-specific-dspecs
+			   (append is-dspecs item-specific-dspecs))
+		     (setf remaining-dspecs r-dspecs)))
+	  (values (cons item-specific-dspecs itemized-dspecs)
+		  remaining-dspecs)))))
