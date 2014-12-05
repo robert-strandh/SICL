23572685d844226b0217eeadef880f2c6f9c91d4
Robert Strandh <robert.strandh@gmail.com>
2014-12-05 10:58:58 +0100

    Handle declarations and documentation.

--- Code/CLOS/defmethod-support.lisp
+++ Code/CLOS/defmethod-support.lisp
@@ -1,15 +1,15 @@
 (cl:in-package #:sicl-clos)
 
-(defun parse-defmethod (args)
-  (let ((name (pop args))
-	(qualifiers (loop while (and (consp args) (not (listp (car args))))
-			  collect (pop args)))
-	(lambda-list (pop args)))
-    ;; FIXME: handle declarations and documentation
-    (let* ((parsed-lambda-list (parse-specialized-lambda-list lambda-list))
-	   (required (required parsed-lambda-list)))
-      (values name
-	      qualifiers
+(defun parse-defmethod (all-but-name)
+  (let* ((lambda-list-position (position-if #'listp rest))
+	 (qualifiers (subseq rest 0 lambda-list-position))
+	 (lambda-list (elt rest lambda-list-position))
+	 (body (subseq rest (1+ lambda-list-position)))
+	 (parsed-lambda-list (parse-specialized-lambda-list lambda-list))
+	 (required (required parsed-lambda-list)))
+    (multiple-value-bind (declarations documentation forms)
+	(cleavir-code-utilities:separate-function-body body)
+      (values qualifiers
 	      (append (mapcar #'car required)
 		      (subseq lambda-list (length required)))
 	      (mapcar #'cadr required)
