189dd6145509758d29e93e07f049664665055f5f
Robert Strandh <robert.strandh@gmail.com>
2014-12-08 09:35:05 +0100

    Added definition of ADD-ACTIVATION-RECORD-INSTRUCTION.

--- Code/Cleavir/Intermediate-representation/HIR/environment-related-instructions.lisp
+++ Code/Cleavir/Intermediate-representation/HIR/environment-related-instructions.lisp
@@ -1,1 +1,25 @@
 (in-package #:cleavir-ir)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;; Instruction ADD-ACTIVATION-RECORD-INSTRUCTION.
+;;;
+;;; The purpose of this instruction is to add an activation record on
+;;; top of an existing static runtime environment.
+;;;
+;;; This instruction takes two inputs.  The first input is a dynamic
+;;; lexical location that holds a static runtime environment.  The
+;;; second input is the size of the activation record to add.  This
+;;; instruction has a single output, which is a dynamic lexical
+;;; location that holds the new static runtime environment that has
+;;; the additional activation record on it.  Notice that the input
+;;; dynamic lexical location remains valid and is subject to register
+;;; allocation.
+
+(defclass add-activation-record-instruction (instruction one-successor-mixin)
+  ())
+
+(defun make-add-activation-record-instruction (env-input size-input env-output)
+  (make-instance 'add-activation-record-instruction
+    :inputs (list env-input size-input)
+    :outputs (list env-output)))
