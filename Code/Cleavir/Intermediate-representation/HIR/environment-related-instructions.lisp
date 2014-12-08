0f4c782088b5d88e3db145a02b9a048e03078753
Robert Strandh <robert.strandh@gmail.com>
2014-12-08 09:38:31 +0100

    Added definition of REMOVE-ACTIVATION-RECORD-INSTRUCTION.

--- Code/Cleavir/Intermediate-representation/HIR/environment-related-instructions.lisp
+++ Code/Cleavir/Intermediate-representation/HIR/environment-related-instructions.lisp
@@ -23,3 +23,26 @@
   (make-instance 'add-activation-record-instruction
     :inputs (list env-input size-input)
     :outputs (list env-output)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;; Instruction REMOVE-ACTIVATION-RECORD-INSTRUCTION.
+;;;
+;;; The purpose of this instruction is to remove an activation record
+;;; from the top of an existing static runtime environment.
+;;;
+;;; This instruction takes a single input, a dynamic lexical location
+;;; that holds a static runtime environment.  This instruction has a
+;;; single output, which is a dynamic lexical location that holds the
+;;; new static runtime environment that is like the original one,
+;;; except that the top activation record has been removed.  Notice
+;;; that the input dynamic lexical location remains valid and is
+;;; subject to register allocation.
+
+(defclass remove-activation-record-instruction (instruction one-successor-mixin)
+  ())
+
+(defun make-remove-activation-record-instruction (env-input env-output)
+  (make-instance 'remove-activation-record-instruction
+    :inputs (list env-input)
+    :outputs (list env-output)))
