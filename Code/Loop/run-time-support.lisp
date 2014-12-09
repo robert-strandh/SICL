1596447e4c266a82a3e7c94e2279246518842791
Robert Strandh <robert.strandh@gmail.com>
2014-12-09 09:42:09 +0100

    Defined function to call in a SUM clause.

--- Code/Loop/run-time-support.lisp
+++ Code/Loop/run-time-support.lisp
@@ -1,1 +1,10 @@
 (cl:in-package #:sicl-loop)
+
+;;; This function is called in a SUM clause in order to sum the
+;;; accumulated value with the new one.
+(defun sum (x y)
+  (unless (numberp y)
+    (error 'sum-argument-must-be-number
+	   :datum y))
+	   :expected-type 'number
+  (+ x y))
