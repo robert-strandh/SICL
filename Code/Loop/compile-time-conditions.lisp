6950d55056bff34f8bc4c1e64c04e614a5855ff4
Robert Strandh <robert.strandh@gmail.com>
2014-12-09 08:34:36 +0100

    Removed specific condition reporter in favor of the one provided by Cleavir.

--- Code/Loop/compile-time-conditions.lisp
+++ Code/Loop/compile-time-conditions.lisp
@@ -12,8 +12,6 @@
 
 (cl:in-package #:sicl-loop)
 
-(defgeneric report-loop-condition (condition stream))
-
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;
 ;;; Conditions for parsing
@@ -21,120 +19,94 @@
 ;;; FIXME: Remove condition reporters from the DEFINE-CONDITION forms
 ;;; and put them in a separate (language-specific) file.  
 
-(define-condition loop-parse-error (parse-error) ())
+(define-condition loop-parse-error (parse-error cleavir-i18n:condition)
+  ())
 
 ;;; Root class for loop parse errors that report something that was
 ;;; found, but should not be there.
-(define-condition loop-parse-error-found (parse-error)
+(define-condition loop-parse-error-found (parse-error cleavir-i18n:condition)
   ((%found :initarg :found :reader found)))
 
 (define-condition expected-var-spec-but-end (loop-parse-error)
-  ()
-  (:report report-loop-condition))
+  ())
 
 (define-condition expected-var-spec-but-found (loop-parse-error-found)
-  ()
-  (:report report-loop-condition))
+  ())
 
 (define-condition expected-simple-var-but-end (loop-parse-error)
-  ()
-  (:report report-loop-condition))
+  ())
 
 (define-condition expected-simple-var-but-found (loop-parse-error-found)
-  ()
-  (:report report-loop-condition))
+  ())
 
 (define-condition expected-type-spec-but-end (loop-parse-error)
-  ()
-  (:report report-loop-condition))
+  ())
 
 (define-condition expected-type-spec-but-found (loop-parse-error-found)
-  ()
-  (:report report-loop-condition))
+  ())
 
 (define-condition expected-compound-form-but-end (loop-parse-error)
-  ()
-  (:report report-loop-condition))
+  ())
 
 (define-condition expected-compound-form-but-found (loop-parse-error-found)
-  ()
-  (:report report-loop-condition))
+  ())
 
 (define-condition expected-form-but-end (loop-parse-error)
-  ()
-  (:report report-loop-condition))
+  ())
 
 (define-condition expected-symbol-but-end (loop-parse-error)
-  ()
-  (:report report-loop-condition))
+  ())
 
 (define-condition expected-symbol-but-found (loop-parse-error-found)
-  ()
-  (:report report-loop-condition))
+  ())
 
 (define-condition expected-keyword-but-found (loop-parse-error-found)
-  ()
-  (:report report-loop-condition))
+  ())
 
 (define-condition expected-for/as-subclause-but-end (loop-parse-error)
-  ()
-  (:report report-loop-condition))
+  ())
 
 (define-condition expected-symbol-but-found (loop-parse-error-found)
-  ()
-  (:report report-loop-condition))
+  ())
 
 (define-condition expected-each/the-but-end (loop-parse-error)
-  ()
-  (:report report-loop-condition))
+  ())
 
 (define-condition expected-each/the-but-found (loop-parse-error-found)
-  ()
-  (:report report-loop-condition))
+  ())
 
 (define-condition expected-hash-or-package-but-end (loop-parse-error)
-  ()
-  (:report report-loop-condition))
+  ())
 
 (define-condition expected-hash-or-package-but-found (loop-parse-error-found)
-  ()
-  (:report report-loop-condition))
+  ())
 
 (define-condition expected-in/of-but-end (loop-parse-error)
-  ()
-  (:report report-loop-condition))
+  ())
 
 (define-condition expected-in/of-but-found (loop-parse-error-found)
-  ()
-  (:report report-loop-condition))
+  ())
 
 (define-condition expected-hash-key-but-end (loop-parse-error)
-  ()
-  (:report report-loop-condition))
+  ())
 
 (define-condition expected-hash-value-but-end (loop-parse-error)
-  ()
-  (:report report-loop-condition))
+  ())
 
 (define-condition expected-hash-key-but-found (loop-parse-error-found)
-  ()
-  (:report report-loop-condition))
+  ())
 
 (define-condition expected-hash-value-but-found (loop-parse-error-found)
-  ()
-  (:report report-loop-condition))
+  ())
 
 (define-condition expected-preposition-but-end (loop-parse-error)
-  ()
-  (:report report-loop-condition))
+  ())
 
 (define-condition too-many-prepositions-from-one-group (loop-parse-error)
-  ()
-  (:report report-loop-condition))
+  ())
 
 (define-condition conflicting-stepping-directions (loop-parse-error)
-  ()
-  (:report report-loop-condition))
+  ())
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;
