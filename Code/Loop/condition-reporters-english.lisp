db123cc0a6fe2a27607645cc30a8a8bd1b0f35dd
Robert Strandh <robert.strandh@gmail.com>
2014-12-09 08:39:36 +0100

    Use condition-reporter proviced by Cleavir for messages in English.

--- Code/Loop/condition-reporters-english.lisp
+++ Code/Loop/condition-reporters-english.lisp
@@ -12,135 +12,171 @@
 
 (cl:in-package #:sicl-loop)
 
-(defmethod report-loop-condition
-    ((condition expected-var-spec-but-end) stream)
+(defmethod cleavir-i18n:report-condition
+    ((condition expected-var-spec-but-end)
+     stream
+     (language cleavir-i18n:english))
   (declare (ignorable condition))
   (format stream
 	  "Expected a variable specification, but reached~@
            the end of the loop body."))
 
-(defmethod report-loop-condition
-    ((condition expected-var-spec-but-found) stream)
+(defmethod cleavir-i18n:report-condition
+    ((condition expected-var-spec-but-found)
+     stream
+     (language cleavir-i18n:english))
   (format stream
 	  "Expected a variable specification but found~@
            the following instead:~@
            ~s"
 	  (found condition)))
 
-(defmethod report-loop-condition
-    ((condition expected-simple-var-but-end) stream)
+(defmethod cleavir-i18n:report-condition
+    ((condition expected-simple-var-but-end)
+     stream
+     (language cleavir-i18n:english))
   (declare (ignorable condition))
   (format stream
 	  "Expected a simple variable but reached~@
            the end of the loop body."))
 
-(defmethod report-loop-condition
-    ((condition expected-simple-var-but-found) stream)
+(defmethod cleavir-i18n:report-condition
+    ((condition expected-simple-var-but-found)
+     stream
+     (language cleavir-i18n:english))
   (format stream
 	  "Expected a simple variable but found~@
            the following instead:~@
            ~s"
 	  (found condition)))
 
-(defmethod report-loop-condition
-    ((condition expected-type-spec-but-end) stream)
+(defmethod cleavir-i18n:report-condition
+    ((condition expected-type-spec-but-end)
+     stream
+     (language cleavir-i18n:english))
   (declare (ignorable condition))
   (format stream
 	  "Expected a variable specification but reached~@
            the end of the loop body."))
 
-(defmethod report-loop-condition
-    ((condition expected-type-spec-but-found) stream)
+(defmethod cleavir-i18n:report-condition
+    ((condition expected-type-spec-but-found)
+     stream
+     (language cleavir-i18n:english))
   (format stream
 	  "Expected a type specification but found~@
            the following instead:~@
            ~s"
 	  (found condition)))
 
-(defmethod report-loop-condition
-    ((condition expected-compound-form-but-end) stream)
+(defmethod cleavir-i18n:report-condition
+    ((condition expected-compound-form-but-end)
+     stream
+     (language cleavir-i18n:english))
   (declare (ignorable condition))
      (format stream
 	     "Expected a compound form but reached ~
               the end of the loop body."))
 
-(defmethod report-loop-condition
-    ((condition expected-compound-form-but-found) stream)
+(defmethod cleavir-i18n:report-condition
+    ((condition expected-compound-form-but-found)
+     stream
+     (language cleavir-i18n:english))
   (format stream
 	  "Expected a compound form but found~@
            the following instead:~@
            ~s"
 	  (found condition)))
 
-(defmethod report-loop-condition
-    ((condition expected-form-but-end) stream)
+(defmethod cleavir-i18n:report-condition
+    ((condition expected-form-but-end)
+     stream
+     (language cleavir-i18n:english))
   (declare (ignorable condition))
   (format stream
 	  "Expected a form but reached~@
            the end of the loop body."))
 
-(defmethod report-loop-condition
-    ((condition expected-symbol-but-end) stream)
+(defmethod cleavir-i18n:report-condition
+    ((condition expected-symbol-but-end)
+     stream
+     (language cleavir-i18n:english))
   (declare (ignorable condition))
   (format stream
 	  "Expected a symbol but reached~@
            the end of the loop body."))
 
-(defmethod report-loop-condition
-    ((condition expected-symbol-but-found) stream)
+(defmethod cleavir-i18n:report-condition
+    ((condition expected-symbol-but-found)
+     stream
+     (language cleavir-i18n:english))
   (format stream
 	  "Expected a symbol but found~@
            the following instead:~@
            ~s"
 	  (found condition)))
 
-(defmethod report-loop-condition
-    ((condition expected-keyword-but-found) stream)
+(defmethod cleavir-i18n:report-condition
+    ((condition expected-keyword-but-found)
+     stream
+     (language cleavir-i18n:english))
   (format stream
 	  "Expected a loop keyword, but found~@
            the following instead:~@
            ~s"
 	  (found condition)))
 
-(defmethod report-loop-condition
-    ((condition expected-for/as-subclause-but-end) stream)
+(defmethod cleavir-i18n:report-condition
+    ((condition expected-for/as-subclause-but-end)
+     stream
+     (language cleavir-i18n:english))
   (declare (ignorable condition))
   (format stream
 	  "Expected a loop keyword indicating a for/as~@
            subclause, but reached the end of the loop body."))
 
-(defmethod report-loop-condition
-    ((condition expected-symbol-but-found) stream)
+(defmethod cleavir-i18n:report-condition
+    ((condition expected-symbol-but-found)
+     stream
+     (language cleavir-i18n:english))
   (format stream
 	  "Expected a loop keyword indicating a for/as~@
            subclause, but found the following instead:~@
            ~s"
 	  (found condition)))
 
-(defmethod report-loop-condition
-    ((condition expected-each/the-but-end) stream)
+(defmethod cleavir-i18n:report-condition
+    ((condition expected-each/the-but-end)
+     stream
+     (language cleavir-i18n:english))
   (declare (ignorable condition))
   (format stream
 	  "Expected the loop keyword each/the,~@
            but reached the end of the loop body."))
 
-(defmethod report-loop-condition
-    ((condition expected-each/the-but-found) stream)
+(defmethod cleavir-i18n:report-condition
+    ((condition expected-each/the-but-found)
+     stream
+     (language cleavir-i18n:english))
   (format stream
 	  "Expected the loop keyword each/the, but found~@
            the following instead:~@
            ~s"
 	  (found condition)))
 
-(defmethod report-loop-condition
-    ((condition expected-hash-or-package-but-end) stream)
+(defmethod cleavir-i18n:report-condition
+    ((condition expected-hash-or-package-but-end)
+     stream
+     (language cleavir-i18n:english))
   (declare (ignorable condition))
   (format stream
 	  "Expected a loop keyword indicating a for/as-hash,~@
            but reached the end of the loop body."))
 
-(defmethod report-loop-condition
-    ((condition expected-hash-or-package-but-found) stream)
+(defmethod cleavir-i18n:report-condition
+    ((condition expected-hash-or-package-but-found)
+     stream
+     (language cleavir-i18n:english))
   (format stream
 	  "Expected a loop keyword indicating a for/as-hash~@
            or a for/as-package subclause, but found~@
@@ -148,68 +184,86 @@
            ~s"
 	  (found condition)))
 
-(defmethod report-loop-condition
-    ((condition expected-in/of-but-end) stream)
+(defmethod cleavir-i18n:report-condition
+    ((condition expected-in/of-but-end)
+     stream
+     (language cleavir-i18n:english))
   (declare (ignorable condition))
   (format stream
 	  "Expected the loop keyword in/or,~@
            but reached the end of the loop body."))
 
-(defmethod report-loop-condition
-    ((condition expected-in/of-but-found) stream)
+(defmethod cleavir-i18n:report-condition
+    ((condition expected-in/of-but-found)
+     stream
+     (language cleavir-i18n:english))
   (format stream
 	  "Expected the loop keyword in/or, but found~@
            the following instead:~@
            ~s"
 	  (found condition)))
 
-(defmethod report-loop-condition
-    ((condition expected-hash-key-but-end) stream)
+(defmethod cleavir-i18n:report-condition
+    ((condition expected-hash-key-but-end)
+     stream
+     (language cleavir-i18n:english))
   (declare (ignorable condition))
   (format stream
 	  "Expected (hash-key other-var),~@
            but reached the end of the loop body."))
 
-(defmethod report-loop-condition
-    ((condition expected-hash-value-but-end) stream)
+(defmethod cleavir-i18n:report-condition
+    ((condition expected-hash-value-but-end)
+     stream
+     (language cleavir-i18n:english))
   (declare (ignorable condition))
   (format stream
 	  "Expected (hash-value other-var),~@
            but reached the end of the loop body."))
 
-(defmethod report-loop-condition
-    ((condition expected-hash-key-but-found) stream)
+(defmethod cleavir-i18n:report-condition
+    ((condition expected-hash-key-but-found)
+     stream
+     (language cleavir-i18n:english))
   (format stream
 	  "Expected (hash-key other-var), but found~@
            the following instead:~@
            ~s"
 	  (found condition)))
 
-(defmethod report-loop-condition
-    ((condition expected-hash-value-but-found) stream)
+(defmethod cleavir-i18n:report-condition
+    ((condition expected-hash-value-but-found)
+     stream
+     (language cleavir-i18n:english))
   (format stream
 	  "Expected (hash-value other-var), but found~@
            the following instead:~@
            ~s"
 	  (found condition)))
 
-(defmethod report-loop-condition
-    ((condition expected-preposition-but-end) stream)
+(defmethod cleavir-i18n:report-condition
+    ((condition expected-preposition-but-end)
+     stream
+     (language cleavir-i18n:english))
   (declare (ignorable condition))
   (format stream
 	  "Expected a for/as preposition,~@
            but reached the end of the loop body."))
 
-(defmethod report-loop-condition
-    ((condition too-many-prepositions-from-one-group) stream)
+(defmethod cleavir-i18n:report-condition
+    ((condition too-many-prepositions-from-one-group)
+     stream
+     (language cleavir-i18n:english))
   (format stream
 	  "Expected (hash-value other-var), but found~@
            the following instead:~@
            ~s"
 	  (found condition)))
 
-(defmethod report-loop-condition
-    ((condition conflicting-stepping-directions) stream)
+(defmethod cleavir-i18n:report-condition
+    ((condition conflicting-stepping-directions)
+     stream
+     (language cleavir-i18n:english))
   (declare (ignorable condition))
   (format stream
 	  "Conflicting stepping directions."))
