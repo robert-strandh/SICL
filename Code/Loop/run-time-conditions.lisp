8d3f2599a6c8eaa9a61b979cf6f1c84c012baf40
Robert Strandh <robert.strandh@gmail.com>
2014-12-09 08:56:10 +0100

    Include CL:TYPE-ERROR as a superclass.

--- Code/Loop/run-time-conditions.lisp
+++ Code/Loop/run-time-conditions.lisp
@@ -3,5 +3,5 @@
 (define-condition loop-runtime-error (error cleavir-i18n:condition)
   ())
 
-(define-condition sum-argument-must-be-number (loop-runtime-error)
+(define-condition sum-argument-must-be-number (type-error loop-runtime-error)
   ())
