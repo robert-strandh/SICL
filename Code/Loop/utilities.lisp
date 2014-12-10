543ad769f45f660266c302aec5b9eaf96fd6dce6
Robert Strandh <robert.strandh@gmail.com>
2014-12-10 14:15:41 +0100

    Rewrote and simplified the destrucuring function.

--- Code/Loop/utilities.lisp
+++ Code/Loop/utilities.lisp
@@ -22,36 +22,30 @@
   (and (symbolp symbol1)
        (string= symbol1 symbol2)))
 
-;;; D-VAR-SPEC is a tree of variable names (symbols).  ROOT is the
-;;; name (symbol) of a variable that will some value at runtime.  This
-;;; function generates code for destructuring the value according to
-;;; the tree.  This function returns two values.  The first value is a
-;;; list of bindings to be used with LET*, the result of which is to
-;;; give the correct values to the variables in the tree according to
-;;; the shape of the value at runtime.  Code is also generated to make
-;;; sure that if the value at runtime must be a CONS cell then this is
-;;; checked and an error is signaled otherwise.  The second return
-;;; value is a list of variable names (symbols) that should be
-;;; declared IGNORABLE in the LET* in which these bindings are used.
-(defun destructure-variables (d-var-spec root)
-  (let ((bindings '())
-	(ignorables '()))
-    (labels ((destructure-aux (d-var-spec root)
+;;; This function generates code for destructuring a value according
+;;; to a tree of variables.  D-VAR-SPEC is a tree of variable names
+;;; (symbols).  FORM is a form that, at runtime, computes the value to
+;;; be assigned to the root of D-VAR-SPEC.  This function returns a
+;;; list of bindings to be used in a LET* form.  These bindings
+;;; destructure the root value until the leaves of the tree are
+;;; reached, generating intermediate temporary variables as necessary.
+;;; The destructuring code calls the function LIST-CAR and LIST-CDR so
+;;; that an error is signaled whenever the corresponding place in the
+;;; value tree is not a CONS cell.
+(defun destructure-variables (d-var-spec form)
+  (let ((bindings '()))
+    (labels ((traverse (d-var-spec form)
 	       (cond ((null d-var-spec)
 		      nil)
 		     ((symbolp d-var-spec)
-		      (push `(,d-var-spec ,root) bindings))
+		      (push `(,d-var-spec ,form) bindings))
 		     ((not (consp d-var-spec))
 		      (error 'expected-var-spec-but-found
 			     :found d-var-spec))
 		     (t
-		      (let ((head (gensym))
-			    (tail (gensym)))
-			(push head ignorables)
-			(push tail ignorables)
-			(push `(,head (list-car ,root)) bindings)
-			(push `(,tail (list-cdr ,root)) bindings)
-			(destructure-aux (car d-var-spec) head)
-			(destructure-aux (cdr d-var-spec) tail))))))
-      (destructure-aux d-var-spec root)
-      (values (nreverse bindings) (nreverse ignorables)))))
+		      (let ((temp (gensym)))
+			(push `(,temp ,form) bindings)
+			(traverse (car d-var-spec) `(list-car ,temp))
+			(traverse (cdr d-var-spec) `(list-cdr ,temp)))))))
+      (traverse d-var-spec form)
+      (reverse bindings))))
