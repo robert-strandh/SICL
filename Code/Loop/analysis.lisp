481a6fd67838d3dbf89320e002b957d3664a21c0
Robert Strandh <robert.strandh@gmail.com>
2014-12-11 15:32:09 +0100

    Assume clauses have been parsed before order is verified.

--- Code/Loop/analysis.lisp
+++ Code/Loop/analysis.lisp
@@ -16,42 +16,41 @@
 ;;;
 ;;; Syntactic and semantic analysis
 
-(defun verify-clause-order (loop-body)
-  (let ((clauses (clauses loop-body)))
-    ;; Check that if there is a name-clause, the first one is in
-    ;; position 0.  For now, we do not care if there is more than one
-    ;; name-clause.
-    (let ((name-clause-position
-	   (position-if (lambda (clause) (typep clause 'name-clause))
-			clauses)))
-      (when (and (not (null name-clause-position)) (plusp name-clause-position))
-	(error 'name-clause-not-first)))
-    ;; Check that, if the first clause is a name-clause, there is not
-    ;; a second name-clause.  We have already tested the case when
-    ;; there is a name-clause other than in the first position.
-    (when (typep (car clauses) 'name-clause)
-      (let ((second-name-clause
-	     (find-if (lambda (clause) (typep clause 'name-clause))
-		      (cdr clauses))))
-	(when (not (null second-name-clause))
-	  (error 'multiple-name-clauses))))
-    ;; Check that there is not a variable-clause (other than an
-    ;; initial-clause or a final-clause) following a name clause
-    ;; (other than an initial-clause or a final-clause).
-    (let ((last-variable-clause-position
-	   (position-if (lambda (clause)
-			  (and (typep clause 'variable-clause-mixin)
-			       (not (typep clause 'initial-clause))
-			       (not (typep clause 'final-clause))))
-			clauses
-			:from-end t))
-	  (first-main-clause-position
-	   (position-if (lambda (clause)
-			  (and (typep clause 'main-clause-mixin)
-			       (not (typep clause 'initial-clause))
-			       (not (typep clause 'final-clause))))
-			clauses)))
-      (when (and (not (null last-variable-clause-position))
-		 (not (null first-main-clause-position))
-		 (> last-variable-clause-position first-main-clause-position))
-	(error 'invalid-clause-order)))))
+(defun verify-clause-order (clauses)
+  ;; Check that if there is a name-clause, the first one is in
+  ;; position 0.  For now, we do not care if there is more than one
+  ;; name-clause.
+  (let ((name-clause-position
+	  (position-if (lambda (clause) (typep clause 'name-clause))
+		       clauses)))
+    (when (and (not (null name-clause-position)) (plusp name-clause-position))
+      (error 'name-clause-not-first)))
+  ;; Check that, if the first clause is a name-clause, there is not
+  ;; a second name-clause.  We have already tested the case when
+  ;; there is a name-clause other than in the first position.
+  (when (typep (car clauses) 'name-clause)
+    (let ((second-name-clause
+	    (find-if (lambda (clause) (typep clause 'name-clause))
+		     (cdr clauses))))
+      (when (not (null second-name-clause))
+	(error 'multiple-name-clauses))))
+  ;; Check that there is not a variable-clause (other than an
+  ;; initial-clause or a final-clause) following a name clause
+  ;; (other than an initial-clause or a final-clause).
+  (let ((last-variable-clause-position
+	  (position-if (lambda (clause)
+			 (and (typep clause 'variable-clause-mixin)
+			      (not (typep clause 'initial-clause))
+			      (not (typep clause 'final-clause))))
+		       clauses
+		       :from-end t))
+	(first-main-clause-position
+	  (position-if (lambda (clause)
+			 (and (typep clause 'main-clause-mixin)
+			      (not (typep clause 'initial-clause))
+			      (not (typep clause 'final-clause))))
+		       clauses)))
+    (when (and (not (null last-variable-clause-position))
+	       (not (null first-main-clause-position))
+	       (> last-variable-clause-position first-main-clause-position))
+      (error 'invalid-clause-order))))
