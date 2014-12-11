f9728405ca86d2acfa5f69a174efd812be6d46fe
Robert Strandh <robert.strandh@gmail.com>
2014-12-11 16:08:49 +0100

    Simplify definition of test package.

--- Code/Loop/Test/test-packages.lisp
+++ Code/Loop/Test/test-packages.lisp
@@ -1,15 +1,5 @@
 (cl:in-package #:common-lisp-user)
 
-;;; In the test package, we want to import all symbols of the
-;;; COMMON-LISP package, except the ones that were shadowed in
-;;; the package that we are testing.  We do that by programatically
-;;; getting the list of those symbols using the function 
-;;; package-shadowing symbols.  But since defpackage doesn't evaluate
-;;; the symbols in the :shadowing-import-from list, we need to 
-;;; have the reader produce that list, hence the use of the #. reader
-;;; macro and again of the consing dot followed by a list. 
 (defpackage #:sicl-loop-test
-  (:shadowing-import-from #:sicl-loop .
-			  #.(package-shadowing-symbols '#:sicl-loop))
-  (:use #:sicl-loop #:cl #:lisp-unit))
-
+  (:use #:common-lisp)
+  (:shadow #:loop))
