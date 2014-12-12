87b20c32abc54103b1fd191412dbd3d3b4dbdcff
Robert Strandh <robert.strandh@gmail.com>
2014-12-12 08:02:06 +0100

    Simplify algorithm since now the initial instruction is always an ENTER-INSTRUCTION.

--- Code/Cleavir/HIR-transformations/compute-nesting-depth.lisp
+++ Code/Cleavir/HIR-transformations/compute-nesting-depth.lisp
@@ -6,13 +6,12 @@
 ;;;
 ;;; The nesting depth is defined recursively as follows:
 ;;;
-;;;  * The nesting depth of the initial instruction is 0.
+;;;  * The nesting depth of the initial instruction (which is always
+;;;    an ENTER-INSTRUCTION) is 0.
 ;;;
 ;;;  * The nesting depth of the unique successor of an UNWIND
-;;;    instruction is the nesting depth of the INVOCATION of the
-;;;    UNWIND instruction.  If the invocation of the UNWIND
-;;;    instruction is NIL, then the nesting depth of its successor is
-;;;    0.
+;;;    instruction is the nesting depth of the INVOCATION (which is
+;;;    always an ENTER-INSTRUCTION) of the UNWIND instruction.
 ;;;
 ;;;  * The nesting depth of a successor of an instruction I other than
 ;;;    an UNWIND instruction is the same as the nesting depth of I.
@@ -28,6 +27,9 @@
 
 (defun compute-nesting-depth (initial-instruction)
   (let ((result (make-hash-table :test #'eq)))
+    ;; As required by the definition of nesting depth, we set the
+    ;; nesting depth of the initial instruction to 0.
+    (setf (gethash initial-instruction result) 0)
     (labels ((traverse (instruction depth)
 	       (when (null (gethash instruction result))
 		 (setf (gethash instruction result) depth)
@@ -36,9 +38,7 @@
 		     (cleavir-ir:unwind-instruction
 		      (let ((invocation (cleavir-ir:invocation instruction)))
 			(traverse (first successors)
-				  (if (null invocation)
-				      0
-				      (gethash invocation result)))))
+				  (gethash invocation result))))
 		     (cleavir-ir:enclose-instruction
 		      (loop for successor in successors
 			    do (traverse successor depth))
