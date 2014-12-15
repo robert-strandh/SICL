6c4fa2a8bb175a934f7d6aabc76ef830f3268b8f
Robert Strandh <robert.strandh@gmail.com>
2014-12-15 12:05:01 +0100

    Added call to ELIMINATE-TYPEQ to sequence of standard HIR transformations.

--- Code/Cleavir/HIR-transformations/hir-transformations.lisp
+++ Code/Cleavir/HIR-transformations/hir-transformations.lisp
@@ -1,4 +1,5 @@
 (cl:in-package #:cleavir-hir-transformations)
 
 (defun hir-transformations (initial-instruction)
-  (type-inference initial-instruction))
+  (type-inference initial-instruction)
+  (eliminate-typeq initial-instruction))
