8a4a616af3d22ce248d920b2a37aafd030f45647
Robert Strandh <robert.strandh@gmail.com>
2014-12-16 08:32:57 +0100

    Moved data type IMMEDIATE-INPUT to HIR because we need it early.

--- Code/Cleavir/Intermediate-representation/LIR/general.lisp
+++ Code/Cleavir/Intermediate-representation/LIR/general.lisp
@@ -2,28 +2,6 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;
-;;; Datum class IMMEDIATE-INPUT.
-;;;
-;;; The IMMEDIATE-INPUT datum corresponds to a raw machine interger
-;;; that is considered sufficiently small that it can occur directly
-;;; in the instruction stream.  The machine integer is represented in
-;;; the instance as a Lisp integer.  The machine integer can represent
-;;; some raw numeric information, or it can represent a tagged
-;;; immediate Lisp datum such as a fixnum or a character. 
-;;;
-;;; Data of this type are introduced by backend-specific code,
-;;; because whether or not some datum can be represented as immediate
-;;; input depends on the backend. 
-
-(defclass immediate-input (datum)
-  ((%value :initarg :value :reader value)))
-
-(defun make-immediate-input (value)
-  (make-instance 'immediate-input
-    :value value))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;;
 ;;; Datum class WORD-INPUT.
 ;;;
 ;;; The WORD-INPUT datum corresponds to a raw machine interger that
