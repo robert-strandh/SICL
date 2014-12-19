0af5222bd296336e3923184a4e45e075e35fb3c3
Robert Strandh <robert.strandh@gmail.com>
2014-12-19 14:36:00 +0100

    Extracted definition of class DIRECT-SLOT-DEFINITION to a separate component.

---
+++ Code/CLOS/direct-slot-definition-defclass.lisp
@@ -0,0 +1,20 @@
+(cl:in-package #:sicl-clos)
+
+;;; The READERS and WRITERS slots only exist in direct slot
+;;; definitions, because they are not combined the way other slot
+;;; properties are when an effective slot definition is computer.
+(defclass direct-slot-definition (slot-definition)
+  ((%readers 
+    :initform '()
+    :initarg :readers 
+    :reader slot-definition-readers)
+   (%writers 
+    :initform '()
+    :initarg :writers 
+    :reader slot-definition-writers)
+   ;; The CAR of the CONS cell stored in this slot is used to hold
+   ;; values when :ALLOCATION is :CLASS.  In this case, the CONS cell
+   ;; becomes the location of the effective slot definition.
+   (%storage
+    :initform (list nil)
+    :reader slot-definition-storage)))
