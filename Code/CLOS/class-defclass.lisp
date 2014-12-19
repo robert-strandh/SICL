667729979f8a35f8820fc8bc96e00274d529f247
Robert Strandh <robert.strandh@gmail.com>
2014-12-19 15:10:29 +0100

    Extracted definition of class CLASS to a separate component.

---
+++ Code/CLOS/class-defclass.lisp
@@ -0,0 +1,19 @@
+(cl:in-package #:sicl-clos)
+
+(defclass class (specializer)
+  ((%unique-number 
+    ;; FIXME: the unique numbers should be assigned during class
+    ;; finalization, and not here.
+    :initform (prog1 *class-unique-number* (incf *class-unique-number*))
+    :reader unique-number)
+   (%name 
+    :initform nil
+    :initarg :name 
+    ;; There is a specified function named (SETF CLASS-NAME), but it
+    ;; is not an accessor.  Instead it works by calling
+    ;; REINITIALIZE-INSTANCE with the new name.
+    :reader class-name)
+   (%direct-subclasses 
+    :initform '() 
+    :reader class-direct-subclasses
+    :writer (setf c-direct-subclasses))))
--- Code/CLOS/mop-class-hierarchy.lisp
+++ Code/CLOS/mop-class-hierarchy.lisp
@@ -374,21 +374,3 @@
 ;;; For the specification of this generic function, see
 ;;; http://metamodular.com/CLOS-MOP/class-prototype.html
 (defgeneric class-prototype (class))
-
-(defclass class (specializer)
-  ((%unique-number 
-    ;; FIXME: the unique numbers should be assigned during class
-    ;; finalization, and not here.
-    :initform (prog1 *class-unique-number* (incf *class-unique-number*))
-    :reader unique-number)
-   (%name 
-    :initform nil
-    :initarg :name 
-    ;; There is a specified function named (SETF CLASS-NAME), but it
-    ;; is not an accessor.  Instead it works by calling
-    ;; REINITIALIZE-INSTANCE with the new name.
-    :reader class-name)
-   (%direct-subclasses 
-    :initform '() 
-    :reader class-direct-subclasses
-    :writer (setf c-direct-subclasses))))
