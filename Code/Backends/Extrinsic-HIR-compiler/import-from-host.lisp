88011cd8f7f37c4f6d969ea0350d30f069ce0205
Robert Strandh <robert.strandh@gmail.com>
2014-12-21 08:04:34 +0100

    Import all CL functions.  Replace later.

--- Code/Backends/Extrinsic-HIR-compiler/import-from-host.lisp
+++ Code/Backends/Extrinsic-HIR-compiler/import-from-host.lisp
@@ -8,12 +8,20 @@
 		(not (special-operator-p symbol))
 		(null (macro-function symbol)))
 	do (setf (sicl-env:fdefinition
+		  symbol
+		  *environment*)
+		 (fdefinition symbol))
+	   (setf (sicl-env:fdefinition
 		  (find-symbol (symbol-name symbol)
 			       '#:host-common-lisp)
 		  *environment*)
 		 (fdefinition symbol))
       when (fboundp (list 'setf symbol))
 	do (setf (sicl-env:fdefinition
+		  (list 'setf symbol)
+		  *environment*)
+		 (fdefinition (list 'setf symbol)))
+	   (setf (sicl-env:fdefinition
 		  (list 'setf
 			(find-symbol (symbol-name symbol)
 				     '#:host-common-lisp))
@@ -29,72 +37,6 @@
 ;;; Define NIL and T as constant variables.
 (setf (sicl-env:constant-variable t *environment*) t)
 (setf (sicl-env:constant-variable nil *environment*) nil)
-
-;;; Initially, we enter lots of functions from the host environment
-;;; into the target environment.  The reason for that is so that we
-;;; can run the macroexpanders of the target macros that we will
-;;; define.  Later, we gradually replace these functions with target
-;;; versions.
-(eval-when (:compile-toplevel :load-toplevel :execute)
-  (defparameter *imported-functions*
-    '(;; From the Conses dictionary
-      cons consp atom
-      rplaca rplacd
-      car cdr rest
-      caar cadr cdar cddr
-      caaar caadr cadar caddr
-      cdaar cdadr cddar cddr
-      caaaar caaadr caadar caaddr cadaar cadadr caddar caddr
-      cdaaar cdaadr cdadar cdaddr cddaar cddadr cdddar cdddr
-      first second third
-      fourth fifth sixth
-      seventh eighth ninth
-      tenth
-      copy-tree sublis nsublis
-      subst subst-if subst-if-not nsubst nsubst-if nsubst-if-not
-      tree-equal copy-list list list* list-length listp
-      make-list nth endp null nconc
-      append revappend nreconc butlast nbutlast last
-      ldiff tailp nthcdr
-      member member-if member-if-not
-      mapc mapcar mapcan mapl maplist mapcon
-      acons assoc assoc-if assoc-if-not copy-alist
-      pairlis rassoc rassoc-if rassoc-if-not
-      get-properties getf
-      intersection nintersection adjoin
-      set-difference nset-difference
-      set-exclusive-or nset-exclusive-or
-      subsetp union nunion
-      ;; From the Sequences dictionary
-      copy-seq elt fill make-sequence subseq map map-into reduce
-      count count-if count-if-not
-      length reverse nreverse sort stable-sort
-      find find-if find-if-not
-      position position-if position-if-not
-      search mismatch replace
-      substitute substitute-if substitute-if-not
-      nsubstitute nsubstitute-if nsubstitute-if-not
-      concatenate merge
-      remove remove-if remove-if-not
-      delete delete-if delete-if-not
-      remove-duplicates delete-duplicates
-      ;; From the Conditions dictionary
-      error warn
-      ;; From the Numbers dictionary
-      = /= < > <= >= max min minusp plusp zerop
-      floor ceiling truncate round
-      * + - / 1+ 1- abs evenp oddp exp expt
-      mod rem numberp integerp rationalp
-      ;; From the Packages dictionary
-      find-symbol find-package find-all-symbols
-      import export intern
-      packagep
-      ;; From the Objects dictionary
-      make-instance)))
-
-(loop for symbol in *imported-functions*
-      do (setf (sicl-env:fdefinition symbol *environment*)
-	       (fdefinition symbol)))
 
 (eval-when (:compile-toplevel :load-toplevel :execute)
   (defparameter *imported-variables*
