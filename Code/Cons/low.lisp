46b8ea0c33c57b50885f471420152375e63293ea
Robert Strandh <robert.strandh@gmail.com>
2014-12-22 16:35:03 +0100

    Simplify and use recently introduced Cleavir PRIMOPs.

--- Code/Cons/low.lisp
+++ Code/Cons/low.lisp
@@ -1,38 +1,23 @@
 (cl:in-package #:sicl-cons)
 
-(declaim (inline consp car cdr rplaca rplacd))
-
-;;; It might seem backward to define CONSP in terms of TYPEP, but
-;;; TYPEP with a quoted arguments is transformed by a compiler macro
-;;; into (SICL-TYPE:TYPEQ OBJECT CONS), which is processed by the type
-;;; inference mechanism.  Should there be any remaining instances of
-;;; (SICL-TYPE:TYPEQ OBJECT CONS) when the type inference is finished,
-;;; it is transformed into a low-level operation that tests the tag
-;;; bits.  In the implementation of TYPEP, when it turns out that the
-;;; type being tested for is CONS, there might very well be a call to
-;;; CONSP, which might then look circular, but it really is not
-;;; because of what was said above.
-(defun consp (object)
-  (typep object 'cons))
-
 (defun car (list)
   (declare (type list list))
   (if (consp list)
-      (load-car list)
+      (cleavir-primop:car list)
       nil))
 
 (defun cdr (list)
   (declare (type list list))
   (if (consp list)
-      (load-cdr list)
+      (cleavir-primop:cdr list)
       nil))
   
 (defun rplaca (cons object)
   (declare (type cons cons))
-  (store-car cons object)
+  (cleavir-primop:rplaca cons object)
   cons)
 
 (defun rplacd (cons object)
   (declare (type cons cons))
-  (store-cdr cons object)
+  (cleavir-primop:rplacd cons object)
   cons)
