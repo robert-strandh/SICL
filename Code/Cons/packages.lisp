893142dab6b96efc60a92737f84128162c7de0df
Robert Strandh <robert.strandh@gmail.com>
2014-12-22 16:36:51 +0100

    Remove initial low-level operations in favor of Cleavir PRIMOPs.

--- Code/Cons/packages.lisp
+++ Code/Cons/packages.lisp
@@ -3,13 +3,5 @@
 (defpackage #:sicl-cons
   (:use #:common-lisp)
   (:export
-   ;; The symbols LOAD-CAR, LOAD-CDR, STORE-CAR, and STORE-CDR name
-   ;; special operators.  The operators LOAD-CAR and LOAD-CDR are
-   ;; similar to the functions CAR and CDR, except that they require
-   ;; that the argument be a CONS, or else an error is signaled.  The
-   ;; operators STORE-CAR and STORE-CDR are like the functions RPLACA
-   ;; and RPLACD, except that they must be evaluated in a context
-   ;; where no value is required.  
-   #:load-car #:load-cdr #:store-car #:store-cdr
    ))
 
