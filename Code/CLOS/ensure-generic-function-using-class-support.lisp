d00d0d6f95e0cdc1fe046fcb8b1fb7b30432be60
Robert Strandh <robert.strandh@gmail.com>
2014-12-23 14:36:49 +0100

    Use TYPEP with 'CLASS rather than the non-standard CLASSP.

--- Code/CLOS/ensure-generic-function-using-class-support.lisp
+++ Code/CLOS/ensure-generic-function-using-class-support.lisp
@@ -15,7 +15,7 @@
 	     (error "no such generic-function-class ~s"
 		    generic-function-class))
 	   (setf generic-function-class class)))
-	((classp generic-function-class)
+	((typep generic-function-class 'class)
 	 nil)
 	(t
 	 (error "generic function class must be a class or a name")))
@@ -25,7 +25,7 @@
 	     (when (null class)
 	       (error "no such method class ~s" method-class))
 	     (setf method-class class)))
-	  ((classp method-class)
+	  ((typep method-class 'class)
 	   nil)
 	  (t
 	   (error "method class must be a class or a name"))))
@@ -57,7 +57,7 @@
 	     (error "no such generic-function-class ~s"
 		    generic-function-class))
 	   (setf generic-function-class class)))
-	((classp generic-function-class)
+	((typep generic-function-class 'class)
 	 nil)
 	(t
 	 (error "generic function class must be a class or a name")))
@@ -69,7 +69,7 @@
 	     (when (null class)
 	       (error "no such method class ~s" method-class))
 	     (setf method-class class)))
-	  ((classp method-class)
+	  ((typep method-class 'class)
 	   nil)
 	  (t
 	   (error "method class must be a class or a name"))))
