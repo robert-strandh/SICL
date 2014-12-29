29c628586016e012a68699b75b8a306595937290
Robert Strandh <robert.strandh@gmail.com>
2014-12-29 08:42:18 +0100

    Added test for UPFROM with complex arithmetic.

--- Code/Loop/Test/loop1.lisp
+++ Code/Loop/Test/loop1.lisp
@@ -247,9 +247,9 @@
   (loop for i from 1 to 5 for c downfrom #c(10 1) by 2 collect c)
   (#c(10 1) #c(8 1) #c(6 1) #c(4 1) #c(2 1)))
 
-;; (deftest loop.1.48
-;;   (loop for i from 1 to 5 for c upfrom #c(0 1) collect c)
-;;   (#c(0 1) #c(1 1) #c(2 1) #c(3 1) #c(4 1)))
+(deftest loop.1.48
+  (loop for i from 1 to 5 for c upfrom #c(0 1) collect c)
+  (#c(0 1) #c(1 1) #c(2 1) #c(3 1) #c(4 1)))
 
 ;; (deftest loop.1.49
 ;;   (loop for i from 1 to 5 for c upfrom #c(0 1) by 2 collect c)
