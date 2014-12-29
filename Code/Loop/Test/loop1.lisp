90edcf9b4c411b4b704e64df614d779a7fa7f894
Robert Strandh <robert.strandh@gmail.com>
2014-12-29 08:34:25 +0100

    Added test for FROM - BY with complex arithmetic.

--- Code/Loop/Test/loop1.lisp
+++ Code/Loop/Test/loop1.lisp
@@ -235,9 +235,9 @@
   (loop for i from 1 to 5 for c from #c(0 1) collect c)
   (#c(0 1) #c(1 1) #c(2 1) #c(3 1) #c(4 1)))
 
-;; (deftest loop.1.45
-;;   (loop for i from 1 to 5 for c from #c(0 1) by 2 collect c)
-;;   (#c(0 1) #c(2 1) #c(4 1) #c(6 1) #c(8 1)))
+(deftest loop.1.45
+  (loop for i from 1 to 5 for c from #c(0 1) by 2 collect c)
+  (#c(0 1) #c(2 1) #c(4 1) #c(6 1) #c(8 1)))
 
 ;; (deftest loop.1.46
 ;;   (loop for i from 1 to 5 for c downfrom #c(5 1) collect c)
