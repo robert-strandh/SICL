9316fc6fdb69c6345c2caaa718b90c4ae79a2fb6
Robert Strandh <robert.strandh@gmail.com>
2014-12-31 08:44:26 +0100

    Added test for FOR-EQUALS and COLLECT.

--- Code/Loop/Test/loop4.lisp
+++ Code/Loop/Test/loop4.lisp
@@ -12,11 +12,11 @@
    collect x)
   (1 2 3 4 5))
 
-;; (deftest loop.4.2
-;;   (loop
-;;    for i from 1 to 10
-;;    for j = (1+ i) collect j)
-;;   (2 3 4 5 6 7 8 9 10 11))
+(deftest loop.4.2
+  (loop
+   for i from 1 to 10
+   for j = (1+ i) collect j)
+  (2 3 4 5 6 7 8 9 10 11))
 
 ;; (deftest loop.4.3
 ;;   (loop
