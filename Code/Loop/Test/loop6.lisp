e55ade8a6d8b4869e3024fcdd7967d9c365dbfc9
Robert Strandh <robert.strandh@gmail.com>
2015-01-02 11:31:32 +0100

    Added test for FOR - EACH - HASH-KEY - OF.

--- Code/Loop/Test/loop6.lisp
+++ Code/Loop/Test/loop6.lisp
@@ -106,10 +106,10 @@
         #'symbol<)
   (a b c))
 
-;; (deftest loop.6.8
-;;   (sort (loop for x being each hash-key of *loop.6.hash.1* collect x)
-;;         #'symbol<)
-;;   (a b c))
+(deftest loop.6.8
+  (sort (loop for x being each hash-key of *loop.6.hash.1* collect x)
+        #'symbol<)
+  (a b c))
 
 ;; (deftest loop.6.9
 ;;   (sort (loop for x being each hash-keys of *loop.6.hash.1* collect x)
