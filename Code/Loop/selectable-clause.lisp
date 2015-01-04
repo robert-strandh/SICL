2ea37c02aa9dc9ab58e74ead85e3f18a5ef73783
Robert Strandh <robert.strandh@gmail.com>
2015-01-04 09:05:40 +0100

    New component for defining the class SELECTABLE-CLAUSE.

---
+++ Code/Loop/selectable-clause.lisp
@@ -0,0 +1,42 @@
+;;;; Copyright (c) 2015
+;;;;
+;;;;     Robert Strandh (robert.strandh@gmail.com)
+;;;;
+;;;; all rights reserved. 
+;;;;
+;;;; Permission is hereby granted to use this software for any 
+;;;; purpose, including using, modifying, and redistributing it.
+;;;;
+;;;; The software is provided "as-is" with no warranty.  The user of
+;;;; this software assumes any responsibility of the consequences. 
+
+(cl:in-package #:sicl-loop)
+
+;;; Recall that in the dictionary entry for LOOP, the HyperSpec says:
+;;;
+;;;   main-clause ::= unconditional | 
+;;;                   accumulation |
+;;;                   conditional |
+;;;                   termination-test |
+;;;                   initial-final
+;;;
+;;; Though here, we exclude initial-final so that we have:
+;;;
+;;;   main-clause ::= unconditional | 
+;;;                   accumulation |
+;;;                   conditional |
+;;;                   termination-test
+;;;
+;;; Furthermore, the HyperSpec defines selectable-clause like this:
+;;;
+;;;   selectable-clause ::= unconditional | accumulation | conditional 
+;;;
+;;; so we can say:
+;;;
+;;;    main-clause ::= selectable-clause | termination-test
+
+(defclass selectable-clause (main-clause) ())
+
+(defmethod bound-variables ((clause selectable-clause))
+  '())
+
