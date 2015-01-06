eeb5afab3195adf9e215c2d56646cc56394edfcb
Robert Strandh <robert.strandh@gmail.com>
2015-01-06 10:32:49 +0100

    Added test for FOR - EACH - SYMBOL.

--- Code/Loop/Test/loop7.lisp
+++ Code/Loop/Test/loop7.lisp
@@ -20,11 +20,11 @@
         #'string<)
   ("A" "B" "BAR" "BAZ" "C" "FOO"))
 
-;; (deftest loop.7.2
-;;   (sort (mapcar #'symbol-name
-;;                 (loop for x being each symbol of "LOOP.CL-TEST.1" collect x))
-;;         #'string<)
-;;   ("A" "B" "BAR" "BAZ" "C" "FOO"))
+(deftest loop.7.2
+  (sort (mapcar #'symbol-name
+                (loop for x being each symbol of "LOOP.CL-TEST.1" collect x))
+        #'string<)
+  ("A" "B" "BAR" "BAZ" "C" "FOO"))
 
 ;; (deftest loop.7.3
 ;;   (sort (mapcar #'symbol-name
