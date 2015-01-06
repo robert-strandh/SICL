3657fbbb806ed59851f918903fc569568ecc76ad
Robert Strandh <robert.strandh@gmail.com>
2015-01-06 10:36:34 +0100

    Added test for FOR - EACH - EXTERNAL-SYMBOL - IN.

--- Code/Loop/Test/loop7.lisp
+++ Code/Loop/Test/loop7.lisp
@@ -68,11 +68,11 @@
         #'string<)
   ("A" "B" "C"))
 
-;; (deftest loop.7.10
-;;   (sort (mapcar #'symbol-name
-;;                 (loop for x being each external-symbol in "LOOP.CL-TEST.1" collect x))
-;;         #'string<)
-;;   ("A" "B" "C"))
+(deftest loop.7.10
+  (sort (mapcar #'symbol-name
+                (loop for x being each external-symbol in "LOOP.CL-TEST.1" collect x))
+        #'string<)
+  ("A" "B" "C"))
 
 ;; (deftest loop.7.11
 ;;   (sort (mapcar #'symbol-name
