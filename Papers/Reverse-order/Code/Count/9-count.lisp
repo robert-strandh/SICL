23041bf96623877295c17347a270d3e0aa691812
Irène Durand <idurand@labri.fr>
2015-01-09 14:43:40 +0100

    improvement to version 9

--- Papers/Reverse-order/Code/Count/9-count.lisp
+++ Papers/Reverse-order/Code/Count/9-count.lisp
@@ -15,14 +15,30 @@
 	       (loop for e across v
 		    when (eql x e)
 		    do (incf count)))))
-      (labels ((traverse (rest length)
-		 (declare (type fixnum length))
-		 (if (<= length 8192)
-		     (count-from-array rest length)
-		     (let* ((f (ash length -1)))
-		       (traverse (nthcdr f rest) (- length f))
-		       (traverse rest f)))))
-	(traverse list length)))
+      (macrolet ((divide (rest length k)
+		   (let* ((n (ash 1 k))
+			  (gensyms (loop repeat n collect (gensym)))
+			  (f (gensym)))
+		     `(let ((,f (ash length (- ,k)))
+			    (,(car gensyms) ,rest))
+			(let* ,(loop
+				 for gensym1 in gensyms
+				 for gensym2 in (cdr gensyms)
+				 collect `(,gensym2 (nthcdr ,f ,gensym1)))
+			  (traverse
+			   (nthcdr ,f ,(car (last gensyms)))
+			   (- ,length (ash ,f ,k)))
+			  ,@(loop
+			      for gensym in (reverse gensyms)
+			       collect `(traverse ,gensym ,f)))))))
+	(labels ((traverse (rest length)
+		   (declare (type fixnum length))
+		   (cond ((<= length 8192) (count-from-array rest length))
+			 ((<= length 16384) (divide rest length 1)) ; 2^14 divide by 2
+			 ((<= length 32768) (divide rest length 2)) ; 2^15 divide by 4
+			 ((<= length 65536) (divide rest length 3)) ; 2^16 divide by 8
+			 (t (divide rest length 4))))) ; divide by 16
+	  (traverse list length))))
     count))
 
 (defun reverse-count-9 (x list)
