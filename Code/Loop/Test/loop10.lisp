5cc81260ebbc944095f7618d23ae49e0aaa130e1
Robert Strandh <robert.strandh@gmail.com>
2015-01-12 07:09:14 +0100

    Added test for MINIMIZE over list of ratios and OF-TYPE RATIONAL type declaration.

--- Code/Loop/Test/loop10.lisp
+++ Code/Loop/Test/loop10.lisp
@@ -260,9 +260,9 @@
   (loop for x in '(0.08 0.40 0.05 0.03 0.44 0.01 0.19 0.04 0.40 0.03) minimize x of-type float)
   0.01)
 
-;; (deftest loop.10.52
-;;   (loop for x in '(-1/8 -1/20 -1/5 -1/3 -1/24 -1/1 -1/19 -1/4 -1/20 -1/3) minimize x of-type rational)
-;;   -1/1)
+(deftest loop.10.52
+  (loop for x in '(-1/8 -1/20 -1/5 -1/3 -1/24 -1/1 -1/19 -1/4 -1/20 -1/3) minimize x of-type rational)
+  -1/1)
 
 ;; (deftest loop.10.53
 ;;   (loop for x in '(4 10 5 1 7 9) minimize x into foo fixnum finally (return foo))
