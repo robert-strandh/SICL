922335d4688a10c5b49852c44ae9114f991166c6
Robert Strandh <robert.strandh@gmail.com>
2015-01-12 06:52:21 +0100

    Added test for MAXIMIZE over list of ratios with OF-TYPE RATIONAL type declaration.

--- Code/Loop/Test/loop10.lisp
+++ Code/Loop/Test/loop10.lisp
@@ -156,9 +156,9 @@
   (loop for x in '(0.08 0.20 0.05 0.03 0.24 0.01 0.19 0.04 0.20 0.03) maximize x of-type float)
   0.24)
 
-;; (deftest loop.10.32
-;;   (loop for x in '(-1/8 -1/20 -1/5 -1/3 -1/24 -1/1 -1/19 -1/4 -1/20 -1/3) maximize x of-type rational)
-;;   -1/24)
+(deftest loop.10.32
+  (loop for x in '(-1/8 -1/20 -1/5 -1/3 -1/24 -1/1 -1/19 -1/4 -1/20 -1/3) maximize x of-type rational)
+  -1/24)
 
 ;; (deftest loop.10.33
 ;;   (loop for x in '(1 4 10 5 7 9) maximize x into foo fixnum finally (return foo))
