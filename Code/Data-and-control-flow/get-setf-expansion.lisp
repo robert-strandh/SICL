6bf6f500e781d9a71d4d43f300733e295ab57a79
Robert Strandh <robert.strandh@gmail.com>
2015-01-14 17:31:47 +0100

    Let Cleavir find the global environment.

--- Code/Data-and-control-flow/get-setf-expansion.lisp
+++ Code/Data-and-control-flow/get-setf-expansion.lisp
@@ -1,5 +1,5 @@
 (in-package #:sicl-data-and-control-flow)
 
 (defun get-setf-expansion (place &optional environment)
-  (let ((global-env (or environment *global-environment*)))
+  (let ((global-env (cleavir-env:global-environment environment)))
     (sicl-env:get-setf-expansion place global-env)))
