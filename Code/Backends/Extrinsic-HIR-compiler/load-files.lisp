90484a373812c3a427a6943c23b85e38f3812b24
Robert Strandh <robert.strandh@gmail.com>
2015-01-15 10:29:53 +0100

    Reload the definition of the macro LAMBDA and explain why.

--- Code/Backends/Extrinsic-HIR-compiler/load-files.lisp
+++ Code/Backends/Extrinsic-HIR-compiler/load-files.lisp
@@ -51,6 +51,13 @@
 ;;; the target version of the macro IN-PACKAGE.
 (load (rp "../../Environment/in-package.lisp"))
 
+;;; Up to this point, the macro function of the macro LAMBDA was
+;;; compiled using the host compiler.  Now that we have the final
+;;; version of the macro DEFMACRO, we can reload the file containing
+;;; the definition of the macro LAMBDA, which will cause the macro
+;;; function to be compiled with the target compiler.
+(load (rp "../../Evaluation-and-compilation/lambda.lisp"))
+
 ;;; Load a file containing the definition of the macro
 ;;; MULTIPLE-VALUE-LIST.  This definition is needed, because it is
 ;;; used in the expansion of the macro NTH-VALUE loaded below.
