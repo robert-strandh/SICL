1b771968a41991ab42c869e687bc3324485248bf
Robert Strandh <robert.strandh@gmail.com>
2015-01-15 07:15:45 +0100

    Added comment to manual creation of DEFMACRO.

--- Code/Backends/Extrinsic-HIR-compiler/fill-global-environment.lisp
+++ Code/Backends/Extrinsic-HIR-compiler/fill-global-environment.lisp
@@ -1,5 +1,15 @@
 (cl:in-package #:sicl-extrinsic-hir-compiler)
 
+;;; We need to start using DEFMACRO early on to define macros, and
+;;; since we don't already have it, we must create it "manually".
+;;; This version is incorrect, though, because it uses the host
+;;; compiler both to create the macro function for DEFMACRO (which is
+;;; fine) and for creating the macro function for the macros defined
+;;; by DEFMACRO (which is not fine).  As a result, the macros defined
+;;; by this version of DEFMACRO must be defined in the NULL lexical
+;;; environment.  Luckily, most macros are, and certainly the ones we
+;;; need to define with this version of DEFMACRO until we can replace
+;;; it with a native version.
 (setf (sicl-env:macro-function 'defmacro *environment*)
       (compile nil
 	       (cleavir-code-utilities:parse-macro
