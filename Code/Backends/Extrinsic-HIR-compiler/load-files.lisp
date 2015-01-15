75616876fb30296f1f2677ca1c30477b2e9b4afd
Robert Strandh <robert.strandh@gmail.com>
2015-01-15 07:25:32 +0100

    Added comment to loading the LAMBDA macro.

--- Code/Backends/Extrinsic-HIR-compiler/load-files.lisp
+++ Code/Backends/Extrinsic-HIR-compiler/load-files.lisp
@@ -3,7 +3,17 @@
 (defun rp (filename)
   (asdf:system-relative-pathname :sicl-extrinsic-hir-compiler filename))
 
+;;; Load a file containing a definition of the macro LAMBDA.  This
+;;; macro is particularly simple, so it doesn't really matter how it
+;;; is expanded.  This is fortunate, because that the time this file
+;;; is loaded, the definition of DEFMACRO is still one we created
+;;; "manually" and which uses the host compiler to compile the macro
+;;; function in the null lexical environment.  We define the macro
+;;; LAMBDA before we redefine DEFMACRO as a target macro because
+;;; PARSE-MACRO returns a LAMBDA form, so we need this macro in order
+;;; to redefine DEFMACRO.
 (load (rp "../../Evaluation-and-compilation/lambda.lisp"))
+
 (load (rp "../../Environment/multiple-value-bind.lisp"))
 (load (rp "../../Data-and-control-flow/setf.lisp"))
 (load (rp "defmacro-defmacro.lisp"))
