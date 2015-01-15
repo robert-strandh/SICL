da1a8fa68cbbac0a5764d475409f29708de4d62f
Robert Strandh <robert.strandh@gmail.com>
2015-01-15 08:54:34 +0100

    Rewrote macro IN-PACKAGE to call SICL-ENV:FIND-PACKAGE.

--- Code/Environment/in-package.lisp
+++ Code/Environment/in-package.lisp
@@ -2,8 +2,10 @@
 
 ;;; When this file is compiled by the cross compiler, we already have
 ;;; a temporary definition of IN-PACKAGE in place, so we can use it.
-(in-package #:sicl-global-environment)
+(cl:in-package #:sicl-global-environment)
 
 (defmacro in-package (string-designator)
   `(eval-when (:compile-toplevel :load-toplevel :execute)
-     (in-package-function ',string-designator)))
+     (setq *package*
+	   (find-package ',string-designator
+			 *global-environment*))))
