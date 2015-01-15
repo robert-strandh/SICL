b346b9de667b8b274d438015bd7603839bbc7e1e
Robert Strandh <robert.strandh@gmail.com>
2015-01-15 08:29:07 +0100

    Make sure the macro is defined in the compilation environment too.

--- Code/Backends/Extrinsic-HIR-compiler/defmacro-defmacro.lisp
+++ Code/Backends/Extrinsic-HIR-compiler/defmacro-defmacro.lisp
@@ -1,5 +1,6 @@
 (cl:in-package #:sicl-env)
 
 (defmacro defmacro (name lambda-list &body body)
-  `(setf (sicl-env:macro-function ',name sicl-env:*global-environment*)
-	 ,(cleavir-code-utilities:parse-macro name lambda-list body)))
+  `(eval-when (:compile-toplevel :load-toplevel :execute)
+     (setf (sicl-env:macro-function ',name sicl-env:*global-environment*)
+	   ,(cleavir-code-utilities:parse-macro name lambda-list body))))
