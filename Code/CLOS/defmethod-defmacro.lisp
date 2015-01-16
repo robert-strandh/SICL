5bd6bfc19218bc24664ddea7ee8c4ec1f47e2c36
Robert Strandh <robert.strandh@gmail.com>
2015-01-16 15:44:50 +0100

    Make it work, but must be improved later.

--- Code/CLOS/defmethod-defmacro.lisp
+++ Code/CLOS/defmethod-defmacro.lisp
@@ -1,24 +1,23 @@
 (cl:in-package #:sicl-clos)
 
-(defmacro defmethod (function-name &rest rest)
+(defmacro defmethod (&environment env function-name &rest rest)
   (multiple-value-bind
 	(qualifiers lambda-list specializers declarations documentation forms)
       (parse-defmethod rest)
     (let ((generic-function-var (gensym)))
-      `(let ((,generic-function-var (ensure-generic-function ',function-name)))
+      `(let ((,generic-function-var
+	       (ensure-generic-function ',function-name :environment ,env)))
 	 (ensure-method
 	  ,generic-function-var
 	  :lambda-list ',lambda-list
 	  :qualifiers ',qualifiers
 	  :specializers ,(canonicalize-specializers specializers)
 	  :documentation ,documentation
-	  :body ',forms
-	  :function (make-method-lambda
-		     ,generic-function-var
-		     (class-prototype
-		      (generic-function-method-class ,generic-function-var))
-		     '(lambda ,lambda-list
-		       ,@declarations
-		       ,@forms)
-		     nil))))))
-
+	  :function ,(make-method-lambda
+		      ;; FIXME: do this better.
+		      (class-prototype (find-class 'standard-generic-function))
+		      (class-prototype (find-class 'standard-method))
+		      `(lambda ,lambda-list
+			 ,@declarations
+			 ,@forms)
+		      nil))))))
