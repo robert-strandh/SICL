e9c248cb107dcfe68b49628a5683a0765ab11aae
Robert Strandh <robert.strandh@gmail.com>
2015-01-16 05:25:27 +0100

    Bug fixes.

--- Code/CLOS/defmethod-support.lisp
+++ Code/CLOS/defmethod-support.lisp
@@ -1,10 +1,10 @@
 (cl:in-package #:sicl-clos)
 
 (defun parse-defmethod (all-but-name)
-  (let* ((lambda-list-position (position-if #'listp rest))
-	 (qualifiers (subseq rest 0 lambda-list-position))
-	 (lambda-list (elt rest lambda-list-position))
-	 (body (subseq rest (1+ lambda-list-position)))
+  (let* ((lambda-list-position (position-if #'listp all-but-name))
+	 (qualifiers (subseq all-but-name 0 lambda-list-position))
+	 (lambda-list (elt all-but-name lambda-list-position))
+	 (body (subseq all-but-name (1+ lambda-list-position)))
 	 (parsed-lambda-list (parse-specialized-lambda-list lambda-list))
 	 (required (required parsed-lambda-list)))
     (multiple-value-bind (declarations documentation forms)
@@ -13,7 +13,9 @@
 	      (append (mapcar #'car required)
 		      (subseq lambda-list (length required)))
 	      (mapcar #'cadr required)
-	      args))))
+	      declarations
+	      documentation
+	      forms))))
 
 (defun canonicalize-specializer (specializer)
   (cond ((symbolp specializer)
