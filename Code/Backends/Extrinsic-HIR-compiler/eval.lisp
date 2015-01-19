e7db617761f2d192c6280f2a953ec423918f75c1
Robert Strandh <robert.strandh@gmail.com>
2015-01-19 16:14:32 +0100

    Removed one level of FUNCALL/LAMBDA since no local vars needed.

--- Code/Backends/Extrinsic-HIR-compiler/eval.lisp
+++ Code/Backends/Extrinsic-HIR-compiler/eval.lisp
@@ -8,19 +8,15 @@
   (let* ((cleavir-generate-ast:*compiler* 'cl:eval)
 	 (ast (cleavir-generate-ast:generate-ast form *environment*))
 	 (hir (cleavir-ast-to-hir:compile-toplevel ast))
-	 (lambda-expr `(lambda ()
-			 ,(translate hir *environment*)))
-	 (fun1 (compile nil lambda-expr))
-	 (fun2 (funcall fun1)))
-    (funcall fun2)))
+	 (lambda-expr (translate hir *environment*))
+	 (fun (compile nil lambda-expr)))
+    (funcall fun)))
 
 (defmethod cleavir-env:eval (form environment1 (environment2 environment))
   (let* ((cleavir-generate-ast:*compiler* 'cl:eval)
 	 (ast (cleavir-generate-ast:generate-ast form environment1))
 	 (hir (cleavir-ast-to-hir:compile-toplevel ast)))
     (funcall
-     (funcall
-      (compile
-       nil
-       `(lambda ()
-	  ,(translate hir environment2)))))))
+     (compile
+      nil
+      (translate hir environment2)))))
