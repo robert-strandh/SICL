c26b10fd4a26451b5e49b27014177f7a7d842822
Robert Strandh <robert.strandh@gmail.com>
2015-01-19 10:31:28 +0100

    Simplify debugging by creating intermediate variables.

--- Code/Backends/Extrinsic-HIR-compiler/eval.lisp
+++ Code/Backends/Extrinsic-HIR-compiler/eval.lisp
@@ -10,15 +10,14 @@
 (defun eval (form)
   (let* ((cleavir-generate-ast:*compiler* 'cl:eval)
 	 (ast (cleavir-generate-ast:generate-ast form *environment*))
-	 (hir (cleavir-ast-to-hir:compile-toplevel ast)))
-    (funcall
-     (funcall 
-      (compile
-       nil
-       `(lambda ()
-	  (let ((fdefinition *fdefinition*))
-	    (declare (ignorable fdefinition))
-	    ,(translate hir *environment*))))))))
+	 (hir (cleavir-ast-to-hir:compile-toplevel ast))
+	 (lambda-expr `(lambda ()
+			 (let ((fdefinition *fdefinition*))
+			   (declare (ignorable fdefinition))
+			   ,(translate hir *environment*))))
+	 (fun1 (compile nil lambda-expr))
+	 (fun2 (funcall fun1)))
+    (funcall fun2)))
 
 (defmethod cleavir-env:eval (form environment1 (environment2 environment))
   (let* ((cleavir-generate-ast:*compiler* 'cl:eval)
