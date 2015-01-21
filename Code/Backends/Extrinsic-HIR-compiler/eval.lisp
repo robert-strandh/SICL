c15009d0851e5afb288d0307870bb1ba1e334b0a
Robert Strandh <robert.strandh@gmail.com>
2015-01-21 04:16:01 +0100

    Make the two function have the same shape.

--- Code/Backends/Extrinsic-HIR-compiler/eval.lisp
+++ Code/Backends/Extrinsic-HIR-compiler/eval.lisp
@@ -15,8 +15,7 @@
 (defmethod cleavir-env:eval (form environment1 (environment2 environment))
   (let* ((cleavir-generate-ast:*compiler* 'cl:eval)
 	 (ast (cleavir-generate-ast:generate-ast form environment1))
-	 (hir (cleavir-ast-to-hir:compile-toplevel ast)))
-    (funcall
-     (compile
-      nil
-      (translate hir environment2)))))
+	 (hir (cleavir-ast-to-hir:compile-toplevel ast))
+	 (lambda-expr (translate hir environment2))
+	 (fun (compile nil lambda-expr)))
+    (funcall fun)))
