9a6d4fa2934bd626c242a8f7afdad6f214be150d
Robert Strandh <robert.strandh@gmail.com>
2015-01-21 10:46:12 +0100

    New component containing definition of function NSUBST-IF-NOT.

---
+++ Code/Cons/nsubst-if-not-defun.lisp
@@ -0,0 +1,50 @@
+(cl:in-package #:sicl-cons)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;; Function nsubst-if-not
+
+(defun |nsubst-if-not key=identity| (new predicate tree)
+  ;; Define a local function so as to avoid passing the new and
+  ;; predicate arguments to each recursive call.
+  (labels ((traverse (tree)
+             (cond ((not (funcall predicate (car tree)))
+                    (setf (car tree) new))
+                   ((atom (car tree))
+                    nil)
+                   (t
+                    (traverse (car tree))))
+             (cond ((not (funcall predicate (cdr tree)))
+                    (setf (cdr tree) new))
+                   ((atom (cdr tree))
+                    nil)
+                   (t
+                    (traverse (cdr tree))))))
+    (cond ((not (funcall predicate tree)) new)
+          ((atom tree) tree)
+          (t (traverse tree) tree))))
+
+(defun |nsubst-if-not key=other| (new predicate tree key)
+  ;; Define a local function so as to avoid passing the new and
+  ;; predicate arguments to each recursive call.
+  (labels ((traverse (tree)
+             (cond ((not (funcall predicate (funcall key (car tree))))
+                    (setf (car tree) new))
+                   ((atom (car tree))
+                    nil)
+                   (t
+                    (traverse (car tree))))
+             (cond ((not (funcall predicate (funcall key (cdr tree))))
+                    (setf (cdr tree) new))
+                   ((atom (cdr tree))
+                    nil)
+                   (t
+                    (traverse (cdr tree))))))
+    (cond ((not (funcall predicate (funcall key tree))) new)
+          ((atom tree) tree)
+          (t (traverse tree) tree))))
+
+(defun nsubst-if-not (new predicate tree &key key)
+  (if (null key)
+      (|nsubst-if-not key=identity| new predicate tree)
+      (|nsubst-if-not key=other| new predicate tree key)))
