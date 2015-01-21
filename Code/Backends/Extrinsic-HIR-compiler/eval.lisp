34be4a1cf9e893166394b06da5d57452f895ac62
Robert Strandh <robert.strandh@gmail.com>
2015-01-21 04:22:12 +0100

    Defined EVAL in terms of CLEAVIR-ENV:EVAL.

--- Code/Backends/Extrinsic-HIR-compiler/eval.lisp
+++ Code/Backends/Extrinsic-HIR-compiler/eval.lisp
@@ -4,14 +4,6 @@
 ;;;
 ;;; Main entry point.
 
-(defun eval (form)
-  (let* ((cleavir-generate-ast:*compiler* 'cl:eval)
-	 (ast (cleavir-generate-ast:generate-ast form *environment*))
-	 (hir (cleavir-ast-to-hir:compile-toplevel ast))
-	 (lambda-expr (translate hir *environment*))
-	 (fun (compile nil lambda-expr)))
-    (funcall fun)))
-
 (defmethod cleavir-env:eval (form environment1 (environment2 environment))
   (let* ((cleavir-generate-ast:*compiler* 'cl:eval)
 	 (ast (cleavir-generate-ast:generate-ast form environment1))
@@ -19,3 +11,6 @@
 	 (lambda-expr (translate hir environment2))
 	 (fun (compile nil lambda-expr)))
     (funcall fun)))
+
+(defun eval (form)
+  (cleavir-env:eval form *environment* *environment*))
