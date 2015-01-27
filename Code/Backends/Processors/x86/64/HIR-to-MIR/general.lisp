a8c91ebbb94edb9a289e1bf0035621719b2e28a1
Robert Strandh <robert.strandh@gmail.com>
2015-01-27 14:11:45 +0100

    Added methods on SPECIALIZE specialized to the FUNCALL and TAILCALL.

--- Code/Backends/Processors/x86/64/HIR-to-MIR/general.lisp
+++ Code/Backends/Processors/x86/64/HIR-to-MIR/general.lisp
@@ -1,6 +1,22 @@
 (cl:in-package #:sicl-hir-to-mir)
 
 (defmethod cleavir-ir:specialize
+    ((instruction cleavir-ir:funcall-instruction)
+     (implementation sicl-target-sicl:sicl)
+     (processor cleavir-processor-x86-64:x86-64)
+     os)
+  (declare (ignore os))
+  instruction)
+
+(defmethod cleavir-ir:specialize
+    ((instruction cleavir-ir:tailcall-instruction)
+     (implementation sicl-target-sicl:sicl)
+     (processor cleavir-processor-x86-64:x86-64)
+     os)
+  (declare (ignore os))
+  instruction)
+
+(defmethod cleavir-ir:specialize
     ((instruction cleavir-ir:eq-instruction)
      (implementation sicl-target-sicl:sicl)
      (processor cleavir-processor-x86-64:x86-64)
