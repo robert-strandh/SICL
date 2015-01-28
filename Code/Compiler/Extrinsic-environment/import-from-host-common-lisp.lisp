3f916bb7903fc98781c014afe0bb4e8dad949c9b
Robert Strandh <robert.strandh@gmail.com>
2015-01-28 14:00:59 +0100

    Added *PACKAGE* to imported variables.

--- Code/Compiler/Extrinsic-environment/import-from-host-common-lisp.lisp
+++ Code/Compiler/Extrinsic-environment/import-from-host-common-lisp.lisp
@@ -20,7 +20,7 @@
 (defun import-variables (environment)
   (let ((variables '(*standard-input* *standard-output* *error-output*
 		     *terminal-io* *trace-output* *query-io*
-		     *print-base* *read-base*)))
+		     *print-base* *read-base* *package*)))
     (loop for symbol in variables
 	  do (setf (sicl-env:special-variable symbol environment t)
 		   (cl:symbol-value symbol)))))
