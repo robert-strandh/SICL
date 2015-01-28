9906710d0d93dc00cdbdfa6a3cea465f91e87457
Robert Strandh <robert.strandh@gmail.com>
2015-01-28 13:59:48 +0100

    Import standard variables.

--- Code/Compiler/Extrinsic-environment/import-from-host-common-lisp.lisp
+++ Code/Compiler/Extrinsic-environment/import-from-host-common-lisp.lisp
@@ -17,8 +17,17 @@
 	  do (setf (sicl-env:find-class symbol environment)
 		   class)))
 
+(defun import-variables (environment)
+  (let ((variables '(*standard-input* *standard-output* *error-output*
+		     *terminal-io* *trace-output* *query-io*
+		     *print-base* *read-base*)))
+    (loop for symbol in variables
+	  do (setf (sicl-env:special-variable symbol environment t)
+		   (cl:symbol-value symbol)))))
+
 (defun import-from-host-common-lisp (environment)
   (import-special-operators environment)
   (import-nil-and-t environment)
-  (import-classes environment))
+  (import-classes environment)
+  (import-variables environment))
   
