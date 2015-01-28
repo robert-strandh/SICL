0e6e07bfbe863c17a1e5503dd6822e209da527b3
Robert Strandh <robert.strandh@gmail.com>
2015-01-28 13:55:27 +0100

    Renamed functions to better correspond to purpose.

--- Code/Compiler/Extrinsic-environment/import-from-host-common-lisp.lisp
+++ Code/Compiler/Extrinsic-environment/import-from-host-common-lisp.lisp
@@ -1,15 +1,15 @@
 (cl:in-package #:sicl-extrinsic-environment)
 
-(defun add-special-operators (environment)
+(defun import-special-operators (environment)
   (loop for symbol being each external-symbol in '#:common-lisp
 	when (special-operator-p symbol)
 	  do (setf (sicl-env:special-operator symbol environment) t)))
 
-(defun add-nil-and-t (environment)
+(defun import-nil-and-t (environment)
   (setf (sicl-env:constant-variable t environment) t)
   (setf (sicl-env:constant-variable nil environment) nil))
 
 (defun import-from-host-common-lisp (environment)
-  (add-special-operators environment)
-  (add-nil-and-t environment))
+  (import-special-operators environment)
+  (import-nil-and-t environment))
   
