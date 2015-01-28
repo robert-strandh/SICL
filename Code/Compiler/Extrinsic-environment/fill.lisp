db8ba3273a783268c3cb8b2ad9f02ee95caf0132
Robert Strandh <robert.strandh@gmail.com>
2015-01-28 09:22:26 +0100

    Added comment to function DEFINE-DEFMACRO.

--- Code/Compiler/Extrinsic-environment/fill.lisp
+++ Code/Compiler/Extrinsic-environment/fill.lisp
@@ -9,6 +9,16 @@
   (setf (sicl-env:constant-variable t environment) t)
   (setf (sicl-env:constant-variable nil environment) nil))
 
+;;; We need to start using DEFMACRO early on to define macros, and
+;;; since we don't already have it, we must create it "manually".
+;;; This version is incorrect, though, because it uses the host
+;;; compiler both to create the macro function for DEFMACRO (which is
+;;; fine) and for creating the macro function for the macros defined
+;;; by DEFMACRO (which is not fine).  As a result, the macros defined
+;;; by this version of DEFMACRO must be defined in the NULL lexical
+;;; environment.  Luckily, most macros are, and certainly the ones we
+;;; need to define with this version of DEFMACRO until we can replace
+;;; it with a native version.
 (defun define-defmacro (environment)
   (setf (sicl-env:macro-function 'defmacro environment)
 	(compile nil
