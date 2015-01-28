c79437ce2d843023cf5bcc74c932a8c8e6b59b52
Robert Strandh <robert.strandh@gmail.com>
2015-01-28 09:15:57 +0100

    Wrapped fill forms in functions to be called from INITIALIZE-INSTANCE.

--- Code/Compiler/Extrinsic-environment/fill.lisp
+++ Code/Compiler/Extrinsic-environment/fill.lisp
@@ -1,40 +1,48 @@
 (cl:in-package #:sicl-extrinsic-environment)
 
-(loop for symbol being each external-symbol in '#:common-lisp
-      when (special-operator-p symbol)
-	do (setf (sicl-env:special-operator symbol *environment*) t))
+(defun add-special-operators (environment)
+  (loop for symbol being each external-symbol in '#:common-lisp
+	when (special-operator-p symbol)
+	  do (setf (sicl-env:special-operator symbol environment) t)))
 
-(setf (sicl-env:constant-variable t *environment*) t)
-(setf (sicl-env:constant-variable nil *environment*) nil)
+(defun add-nil-and-t (environment)
+  (setf (sicl-env:constant-variable t environment) t)
+  (setf (sicl-env:constant-variable nil environment) nil))
 
-(setf (sicl-env:macro-function 'defmacro *environment*)
-      (compile nil
-	       (cleavir-code-utilities:parse-macro
-		'defmacro
-		'(name lambda-list &body body)
-		`((eval-when (:compile-toplevel :load-toplevel :execute)
-		    (setf (sicl-env:macro-function name *environment*)
-			  (compile nil
-				   (cleavir-code-utilities:parse-macro
-				    name
-				    lambda-list
-				    body))))))))
+(defun define-defmacro (environment)
+  (setf (sicl-env:macro-function 'defmacro environment)
+	(compile nil
+		 (cleavir-code-utilities:parse-macro
+		  'defmacro
+		  '(name lambda-list &body body)
+		  `((eval-when (:compile-toplevel :load-toplevel :execute)
+		      (setf (sicl-env:macro-function name environment)
+			    (compile nil
+				     (cleavir-code-utilities:parse-macro
+				      name
+				      lambda-list
+				      body)))))))))
 
-(setf (sicl-env:default-setf-expander *environment*)
-      (lambda (form)
-	(if (symbolp form)
-	    (let ((new (gensym)))
-	      (values '()
-		      '()
-		      `(,new)
-		      `(setq ,form ,new)
-		      form))
-	    (let ((temps (loop for arg in (rest form) collect (gensym)))
-		  (new (gensym)))
-	      (values temps
-		      (rest form)
-		      `(,new)
-		      `(funcall #'(setf ,(first form) ,new ,@temps))
-		      `(,(first form) ,@temps))))))
+(defun add-default-setf-expander (environment)
+  (setf (sicl-env:default-setf-expander environment)
+	(lambda (form)
+	  (if (symbolp form)
+	      (let ((new (gensym)))
+		(values '()
+			'()
+			`(,new)
+			`(setq ,form ,new)
+			form))
+	      (let ((temps (loop for arg in (rest form) collect (gensym)))
+		    (new (gensym)))
+		(values temps
+			(rest form)
+			`(,new)
+			`(funcall #'(setf ,(first form) ,new ,@temps))
+			`(,(first form) ,@temps)))))))
 
-		    
+(defun fill-environment (environment)
+  (add-special-operators environment)
+  (add-nil-and-t environment)
+  (define-defmacro environment)
+  (add-default-setf-expander environment))
