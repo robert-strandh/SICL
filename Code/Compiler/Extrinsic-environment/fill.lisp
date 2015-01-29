fe3616984646826a39bae0e21cf56d31457d3fe0
Robert Strandh <robert.strandh@gmail.com>
2015-01-29 08:00:34 +0100

    Call function that defines backquote macros.

--- Code/Compiler/Extrinsic-environment/fill.lisp
+++ Code/Compiler/Extrinsic-environment/fill.lisp
@@ -4,5 +4,6 @@
   (import-from-host-common-lisp environment)
   (import-from-sicl-global-environment environment)
   (import-loop-support environment)
+  (define-backquote-macros environment)
   (define-defmacro environment)
   (define-default-setf-expander environment))
