590e90378a397ba5742719df113fd125f769dd47
Robert Strandh <robert.strandh@gmail.com>
2015-01-30 14:43:36 +0100

    Added comment to loading of LAMBDA.LISP.

--- Code/Compiler/Extrinsic-environment/fill.lisp
+++ Code/Compiler/Extrinsic-environment/fill.lisp
@@ -8,4 +8,13 @@
   (define-defmacro environment)
   (define-in-package environment)
   (define-default-setf-expander environment)
+  ;; Load a file containing a definition of the macro LAMBDA.  This
+  ;; macro is particularly simple, so it doesn't really matter how it
+  ;; is expanded.  This is fortunate, because that the time this file
+  ;; is loaded, the definition of DEFMACRO is still one we created
+  ;; "manually" and which uses the host compiler to compile the macro
+  ;; function in the null lexical environment.  We define the macro
+  ;; LAMBDA before we redefine DEFMACRO as a target macro because
+  ;; PARSE-MACRO returns a LAMBDA form, so we need this macro in order
+  ;; to redefine DEFMACRO.
   (load-file "../../Evaluation-and-compilation/lambda.lisp" environment))
