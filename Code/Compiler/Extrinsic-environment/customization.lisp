d8ba1f139a3bc27a208962e333b5cfd975107281
Robert Strandh <robert.strandh@gmail.com>
2015-01-30 14:08:22 +0100

    Removed component that is no longer needed.

--- Code/Compiler/Extrinsic-environment/customization.lisp
+++
@@ -1,8 +0,0 @@
-(cl:in-package #:sicl-extrinsic-environment)
-
-(defclass environment (sicl-simple-environment:simple-environment)
-  ())
-
-(defmethod cleavir-env:eval (form environment1 (environment2 environment))
-  (let ((ast (cleavir-generate-ast:generate-ast form environment1)))
-    (cleavir-ast-interpreter:interpret ast)))
