fc2457596efcd99c8515b552144af39e0679d8e7
Robert Strandh <robert.strandh@gmail.com>
2015-01-31 10:00:03 +0100

    Load file containing definition of macro NTH-VALUE.

--- Code/Compiler/Extrinsic-environment/fill.lisp
+++ Code/Compiler/Extrinsic-environment/fill.lisp
@@ -61,4 +61,9 @@
   ;; Load a file containing the definition of the macro
   ;; MULTIPLE-VALUE-LIST.  This definition is needed, because it is
   ;; used in the expansion of the macro NTH-VALUE loaded below.
-  (load "../../Data-and-control-flow/multiple-value-list.lisp" environment))
+  (load "../../Data-and-control-flow/multiple-value-list.lisp" environment)
+  ;; Load a file containing the definition of the macro NTH-VALUE.
+  ;; This definition is needed by the function CONSTANTP which is
+  ;; loaded as part of the file standard-environment-functions.lisp
+  ;; loaded below.
+  (load "../../Data-and-control-flow/nth-value.lisp" environment))
