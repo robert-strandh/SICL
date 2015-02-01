5617ff387a3295515685c398507efee27cc62bd3
Robert Strandh <robert.strandh@gmail.com>
2015-02-01 08:15:32 +0100

    Wrap top-level ASTs in a big PROGN-AST.

--- Code/Compiler/Extrinsic-compiler/File-compiler/compile-file.lisp
+++ Code/Compiler/Extrinsic-compiler/File-compiler/compile-file.lisp
@@ -1,7 +1,8 @@
 (cl:in-package #:sicl-extrinsic-file-compiler)
 
 (defun ast-from-stream (stream)
-  (loop with eof = (list nil)
-	for form = (sicl-reader:read stream nil eof)
-	until (eq form eof)
-	collect form))
+  (apply #'cleavir-ast:make-progn-ast
+	 (loop with eof = (list nil)
+	       for form = (sicl-reader:read stream nil eof)
+	       until (eq form eof)
+	       collect form)))
