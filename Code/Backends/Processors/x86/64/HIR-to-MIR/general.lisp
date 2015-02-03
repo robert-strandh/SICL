56a68542076adaf13d0630fc6681d08b0f301601
Robert Strandh <robert.strandh@gmail.com>
2015-02-03 08:14:29 +0100

    Provide a method on SPECIALIZE specialied to SYMBOL-VALUE-INSTRUCTION.

--- Code/Backends/Processors/x86/64/HIR-to-MIR/general.lisp
+++ Code/Backends/Processors/x86/64/HIR-to-MIR/general.lisp
@@ -17,6 +17,14 @@
   instruction)
 
 (defmethod cleavir-ir:specialize
+    ((instruction cleavir-ir:fdefinition-instruction)
+     (implementation sicl-target-sicl:sicl)
+     (processor cleavir-processor-x86-64:x86-64)
+     os)
+  (declare (ignore os))
+  instruction)
+
+(defmethod cleavir-ir:specialize
     ((instruction cleavir-ir:tailcall-instruction)
      (implementation sicl-target-sicl:sicl)
      (processor cleavir-processor-x86-64:x86-64)
