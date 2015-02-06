c7f06fab4f8282baf702b33b002ac075d40deeb5
Robert Strandh <robert.strandh@gmail.com>
2015-02-06 16:47:05 +0100

    Added function HOIST-LOAD-TIME-VALUE.

--- Code/Cleavir/AST-transformations/hoist-load-time-value.lisp
+++ Code/Cleavir/AST-transformations/hoist-load-time-value.lisp
@@ -6,3 +6,11 @@
       (let ((children (cleavir-ast:children ast)))
 	(reduce #'append (mapcar #'find-load-time-value-asts children)
 		:from-end t))))
+
+(defun hoist-load-time-value (ast)
+  (let* ((load-time-value-asts (find-load-time-value-asts ast))
+	 (forms (mapcar #'cleavir-ast:form load-time-value-asts)))
+    (loop for ast in load-time-value-asts
+	  do (change-class ast 'cleavir-ast:lexical-ast :name (gensym)))
+    (cleavir-ast:make-top-level-function-ast
+     ast load-time-value-asts forms)))
