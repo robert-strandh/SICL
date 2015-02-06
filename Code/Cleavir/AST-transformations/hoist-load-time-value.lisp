d5fa2846258fe2e9fdfa496beed6a3f6bc3fe470
Robert Strandh <robert.strandh@gmail.com>
2015-02-06 16:20:32 +0100

    New function for finding all LOAD-TIME-VALUE-ASTs.

--- Code/Cleavir/AST-transformations/hoist-load-time-value.lisp
+++ Code/Cleavir/AST-transformations/hoist-load-time-value.lisp
@@ -1,1 +1,8 @@
 (cl:in-package #:cleavir-ast-transformations)
+
+(defun find-load-time-value-asts (ast)
+  (if (typep ast 'cleavir-ast:load-time-value-ast)
+      (list ast)
+      (let ((children (cleavir-ast:children ast)))
+	(reduce #'append (mapcar #'find-load-time-value-asts children)
+		:from-end t))))
