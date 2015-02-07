1b28b841c357c212137fcb7d92c67c1033aeef28
Robert Strandh <robert.strandh@gmail.com>
2015-02-07 09:31:17 +0100

    Make sure the computation terminates even though the AST has cycles.

--- Code/Cleavir/AST-transformations/hoist-load-time-value.lisp
+++ Code/Cleavir/AST-transformations/hoist-load-time-value.lisp
@@ -1,11 +1,17 @@
 (cl:in-package #:cleavir-ast-transformations)
 
 (defun find-load-time-value-asts (ast)
-  (if (typep ast 'cleavir-ast:load-time-value-ast)
-      (list ast)
-      (let ((children (cleavir-ast:children ast)))
-	(reduce #'append (mapcar #'find-load-time-value-asts children)
-		:from-end t))))
+  (let ((table (make-hash-table :test #'eq)))
+    (labels ((traverse (ast)
+	       (unless (gethash ast table)
+		 (setf (gethash ast table) t)
+		 (if (typep ast 'cleavir-ast:load-time-value-ast)
+		     (list ast)
+		     (let ((children (cleavir-ast:children ast)))
+		       (reduce #'append
+			       (mapcar #'traverse children)
+			       :from-end t))))))
+      (traverse ast))))
 
 (defun hoist-load-time-value (ast)
   (let* ((load-time-value-asts (find-load-time-value-asts ast))
