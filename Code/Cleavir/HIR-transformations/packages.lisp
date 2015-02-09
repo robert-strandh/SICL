bcb7a85be1638bb1dca486ebf04d87ae37483a8a
Robert Strandh <robert.strandh@gmail.com>
2015-02-09 06:39:24 +0100

    Export names of more entry points.

--- Code/Cleavir/HIR-transformations/packages.lisp
+++ Code/Cleavir/HIR-transformations/packages.lisp
@@ -4,6 +4,10 @@
   (:use #:common-lisp)
   (:export
    #:eliminate-typeq
+   #:type-inference
+   #:process-fdefinitions
+   #:process-fdefinition
+   #:process-captured-variables
    #:compute-ownerships
    #:segregate-lexicals
    #:hir-transformations
