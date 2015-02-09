eb2c462b8cd5a88b95b8c0e8ef019f42d3d71602
Robert Strandh <robert.strandh@gmail.com>
2015-02-09 04:57:38 +0100

    Added method on CONVERT-CONSTANT-TO-IMMEDIATE for new env.

--- Code/Compiler/Extrinsic-compiler/File-compiler/compile-file.lisp
+++ Code/Compiler/Extrinsic-compiler/File-compiler/compile-file.lisp
@@ -31,3 +31,15 @@
 
 (defclass environment (sicl-extrinsic-environment:environment)
   ())
+
+(defmethod cleavir-generate-ast:convert-constant-to-immediate
+    (constant (environment environment))
+  (cond ((and (typep constant 'integer)
+	      (<= #.(- (expt 2 30)) constant #.(1- (expt 2 30))))
+	 (* 2 constant))
+	((typep constant 'character)
+	 ;; FIXME: Currently, we depend on the host having the same
+	 ;; character encoding as the target.
+	 (+ #b11 (* 4 (char-code constant))))
+	(t
+	 nil)))
