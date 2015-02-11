e5afd92888dc381c1b09cfdc8b5107b3f350d2d6
Robert Strandh <robert.strandh@gmail.com>
2015-02-11 09:19:38 +0100

    Do not generate HIR for evaluating constants.

--- Code/Compiler/Extrinsic-environment/eval.lisp
+++ Code/Compiler/Extrinsic-environment/eval.lisp
@@ -5,9 +5,26 @@
 ;;; Main entry point.
 
 (defmethod cleavir-env:eval (form environment1 (environment2 environment))
-  (let* ((cleavir-generate-ast:*compiler* 'cl:eval)
-	 (ast (cleavir-generate-ast:generate-ast form environment1))
-	 (hir (cleavir-ast-to-hir:compile-toplevel ast))
-	 (lambda-expr (translate hir environment2))
-	 (fun (compile nil lambda-expr)))
-    (funcall fun)))
+  (cond ((and (consp form)
+	      (consp (cdr form))
+	      (null (cddr form))
+	      (eq (car form) 'quote))
+	 (cadr form))
+	((and (symbolp form)
+	      (nth-value 1 (sicl-global-environment:constant-variable
+			    form environment1)))
+	 (nth-value 0 (sicl-global-environment:constant-variable
+		       form environment1)))
+	((and (atom form) (not (symbolp form)))
+	 form)
+	(t
+	 (let* ((cleavir-generate-ast:*compiler* 'cl:eval)
+		(ast (cleavir-generate-ast:generate-ast form environment1))
+		(ast-bis (cleavir-ast-transformations:hoist-load-time-value ast))
+		(hir (cleavir-ast-to-hir:compile-toplevel ast-bis))
+		(lambda-expr (translate hir environment2))
+		(fun (compile nil lambda-expr))
+		(args (loop for arg in (cleavir-ir:forms hir)
+			    collect (cleavir-env:eval
+				     arg environment1 environment2))))
+	   (apply fun args)))))
