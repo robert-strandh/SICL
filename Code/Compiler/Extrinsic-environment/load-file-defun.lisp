aad837601a3dda030d235f92fe308ad7cff47fe8
Robert Strandh <robert.strandh@gmail.com>
2015-02-13 08:15:25 +0100

    At least for now, print the name of the file being loaded.

--- Code/Compiler/Extrinsic-environment/load-file-defun.lisp
+++ Code/Compiler/Extrinsic-environment/load-file-defun.lisp
@@ -1,6 +1,7 @@
 (cl:in-package #:sicl-extrinsic-environment)
 
 (defun load-file (filename environment)
+  (format *trace-output* "Loading file ~a~%" filename)
   (load-source-with-environments
    (asdf:system-relative-pathname :sicl-extrinsic-environment filename)
    environment
