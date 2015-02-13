fd459d09e3070834d53c20b4e5683837046f0009
Robert Strandh <robert.strandh@gmail.com>
2015-02-13 10:25:10 +0100

    Call FINISH-OUTPUT after writing trace message.

--- Code/Compiler/Extrinsic-environment/load-file-defun.lisp
+++ Code/Compiler/Extrinsic-environment/load-file-defun.lisp
@@ -2,6 +2,7 @@
 
 (defun load-file (filename environment)
   (format *trace-output* "Loading file ~a~%" filename)
+  (finish-output *trace-output*)
   (load-source-with-environments
    (asdf:system-relative-pathname :sicl-extrinsic-environment filename)
    environment
