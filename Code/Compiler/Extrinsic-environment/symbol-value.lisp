f8bae5343ebbcccda1520f9f499e40e5dbdeaf0a
Robert Strandh <robert.strandh@gmail.com>
2015-02-13 07:12:54 +0100

    Simplify signature of functions by making them do more look up in the environment.

--- Code/Compiler/Extrinsic-environment/symbol-value.lisp
+++ Code/Compiler/Extrinsic-environment/symbol-value.lisp
@@ -1,13 +1,15 @@
 (cl:in-package #:sicl-extrinsic-environment)
 
-(defun find-variable-entry (symbol dynamic-env)
-  (loop for entry in dynamic-env
+(defun find-variable-entry (symbol env)
+  (loop for entry in (dynamic-environment env)
 	do (when (and (typep entry 'variable-binding)
 		      (eq (symbol entry) symbol))
 	     (return entry))))
 
-(defun symbol-value (symbol dynamic-env unbound-value global-value-cell)
-  (let ((entry (find-variable-entry symbol dynamic-env)))
+(defun symbol-value (symbol env)
+  (let ((entry (find-variable-entry symbol env))
+	(unbound-value (sicl-global-environment:variable-unbound symbol env))
+	(global-value-cell (sicl-global-environment:variable-cell symbol env)))
     (if (null entry)
 	(if (eq (car global-value-cell) unbound-value)
 	    (error "unbound variable ~s" symbol)
@@ -16,8 +18,9 @@
 	    (error "unbound variable ~s" symbol)
 	    (value entry)))))
 
-(defun (setf symbol-value) (new-value symbol dynamic-env global-value-cell)
-  (let ((entry (find-variable-entry symbol dynamic-env)))
+(defun (setf symbol-value) (new-value symbol env)
+  (let ((entry (find-variable-entry symbol env))
+	(global-value-cell (sicl-global-environment:variable-cell symbol env)))
     (if (null entry)
 	(setf (car global-value-cell) new-value)
 	(setf (value entry) new-value))))
