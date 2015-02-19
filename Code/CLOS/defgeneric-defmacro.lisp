2d32e94bdbebe4b69e5a75ca8bb396855e255555
Robert Strandh <robert.strandh@gmail.com>
2015-02-19 11:34:08 +0100

    Distinguish actions at compile time and load time.

--- Code/CLOS/defgeneric-defmacro.lisp
+++ Code/CLOS/defgeneric-defmacro.lisp
@@ -5,15 +5,14 @@
 ;;; DEFGENERIC.
 
 ;;; FIXME: Add options and methods
-;;;
-;;; FIXME: At compile time, the expansion should set some information
-;;; in the compilation environment, such as the lambda list, etc.  At
-;;; load time it should call ENSURE-GENERIC-FUNCTION, but NOT with the
-;;; ENV environment, because the ENV environment is the compilation
-;;; environment.  Instead, it should use something like NIL or perhaps
-;;; SICL-GLOBAL-ENVIRONMENT:*GLOBAL-ENVIRONMENT*.
 (defmacro defgeneric (&environment env name lambda-list)
-  `(ensure-generic-function
-    ',name
-    :lambda-list ',lambda-list
-    :environment ,env))
+  (let* ((arg-type (cleavir-code-utilities:lambda-list-type-specifier lambda-list))
+	 (function-type `(function ,arg-type t)))
+  `(progn (eval-when (:compile-toplevel)
+	    (setf (sicl-global-environment:function-type ',name ,env)
+		  ',function-type))
+	  (eval-when (:load-toplevel :execute)
+	    (ensure-generic-function
+	     ',name
+	     :lambda-list ',lambda-list
+	     :environment sicl-global-environment:*global-environment*)))))
