6bd107d7f3b3069474ef624e36f43256acfad301
Robert Strandh <robert.strandh@gmail.com>
2015-02-19 08:57:31 +0100

    Added a FIXME comment.

--- Code/CLOS/defgeneric-defmacro.lisp
+++ Code/CLOS/defgeneric-defmacro.lisp
@@ -4,7 +4,14 @@
 ;;;
 ;;; DEFGENERIC.
 
-;;; FIXME: add options and methods
+;;; FIXME: Add options and methods
+;;;
+;;; FIXME: At compile time, the expansion should set some information
+;;; in the compilation environment, such as the lambda list, etc.  At
+;;; load time it should call ENSURE-GENERIC-FUNCTION, but NOT with the
+;;; ENV environment, because the ENV environment is the compilation
+;;; environment.  Instead, it should use something like NIL or perhaps
+;;; SICL-GLOBAL-ENVIRONMENT:*GLOBAL-ENVIRONMENT*.
 (defmacro defgeneric (&environment env name lambda-list)
   `(ensure-generic-function
     ',name
