37a05b034b60d41a9ba0c9e59f260e74a30ffcb9
Robert Strandh <robert.strandh@gmail.com>
2015-02-22 09:29:43 +0100

    Added condition reporter for condition INVALID-EVAL-WHEN-SITUATION.

--- Code/Cleavir/Generate-AST/condition-reporters-english.lisp
+++ Code/Cleavir/Generate-AST/condition-reporters-english.lisp
@@ -19,3 +19,14 @@
            but the following was found instead:~@
            ~s"
 	  (expr condition)))
+
+(defmethod cleavir-i18n:report-condition
+    ((condition invalid-eval-when-situation)
+     stream
+     (langauge cleavir-i18n:english))
+  (format stream
+	  "An EVAL-WHEN situation must be one of:~@
+           :COMPILE-TOPLEVEL, :LOAD-TOPLEVEL, :EXECUTE, COMPILE, LOAD, EVAL,~@
+           but the following was found instead:~@
+           ~s"
+	  (expr condition)))
