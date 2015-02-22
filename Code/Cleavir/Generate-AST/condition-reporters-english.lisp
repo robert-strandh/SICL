9d5c987a7a0fdf7e3c8c0f97d4917832843efd8e
Robert Strandh <robert.strandh@gmail.com>
2015-02-22 09:23:48 +0100

    Added condition reporter for condition BLOCK-NAME-MUST-BE-A-SYMBOL.

--- Code/Cleavir/Generate-AST/condition-reporters-english.lisp
+++ Code/Cleavir/Generate-AST/condition-reporters-english.lisp
@@ -1,1 +1,11 @@
 (cl:in-package #:cleavir-generate-ast)
+
+(defmethod cleavir-i18n:report-condition
+    ((condition block-name-must-be-a-symbol)
+     stream
+     (langauge cleavir-i18n:english))
+  (format stream
+	  "The name of a block must be a symbol,~@
+           but the following was found instead:~@
+           ~s"
+	  (expr condition)))
