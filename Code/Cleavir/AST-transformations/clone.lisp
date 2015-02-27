774396fa11b9af670cf53188d6c595cff446d35b
Robert Strandh <robert.strandh@gmail.com>
2015-02-27 09:17:40 +0100

    Complete rewrite of CLONE-AST.

--- Code/Cleavir/AST-transformations/clone.lisp
+++ Code/Cleavir/AST-transformations/clone.lisp
@@ -1,70 +1,26 @@
 (cl:in-package #:cleavir-ast-transformations)
 
-(defgeneric fixup (clone original dictionary))
-
-(defmethod fixup (clone original dictionary)
-  (reinitialize-instance
-   clone
-   :children (loop for child in (cleavir-ast:children original)
-		   collect (gethash child dictionary))))
-
 (defun clone-ast (ast)
   (let ((dictionary (make-hash-table :test #'eq)))
-    (labels ((traverse (node)
-	       (when (null (gethash node dictionary))
-		 (setf (gethash node dictionary)
-		       (make-instance (class-of node)))
-		 (mapc #'traverse (cleavir-ast:children node)))))
+    (labels
+	((traverse (node)
+	   (when (null (gethash node dictionary))
+	     (setf (gethash node dictionary)
+		   (apply #'make-instance (class-of node)
+			  (loop for (keyword reader)
+				  in (cleavir-io:save-info node)
+				collect keyword
+				collect (funcall reader node))))
+	     (mapc #'traverse (cleavir-ast:children node)))))
       (traverse ast))
-    (maphash (lambda (original clone)
-	       (fixup clone original dictionary))
-	     dictionary)))
-			
-(defmethod fixup :after ((clone cleavir-ast:constant-ast) original dictionary)
-  (declare (ignore dictionary))
-  (reinitialize-instance
-   clone
-   :value (cleavir-ast:value original)))
-
-(defmethod fixup :after ((clone cleavir-ast:lexical-ast) original dictionary)
-  (declare (ignore dictionary))
-  (reinitialize-instance
-   clone
-   :name (cleavir-ast:name original)))
-
-(defmethod fixup :after ((clone cleavir-ast:function-ast) original dictionary)
-  (let ((new-lambda-list
-	  (loop for item in (cleavir-ast:lambda-list original)
-		collect (cond ((member item lambda-list-keywords)
-			       item)
-			      ((and (consp item) (= (length item) 3))
-			       `(,(first item)
-				 ,(gethash (second item) dictionary)
-				 ,(gethash (third item) dictionary)))
-			      ((and (consp item) (= (length item) 2))
-			       `(,(gethash (second item) dictionary)
-				 ,(gethash (third item) dictionary)))
-			      (t
-			       (gethash item dictionary))))))
-    (reinitialize-instance
-     clone
-     :lambda-list new-lambda-list)))
-
-(defmethod fixup :after ((clone cleavir-ast:tag-ast) original dictionary)
-  (declare (ignore dictionary))
-  (reinitialize-instance
-   clone
-   :name (cleavir-ast:name original)))
-
-(defmethod fixup :after ((clone cleavir-ast:typeq-ast) original dictionary)
-  (declare (ignore dictionary))
-  (reinitialize-instance
-   clone
-   :type-specifier (cleavir-ast:type-specifier original)))
-
-(defmethod fixup :after ((clone cleavir-ast:load-time-value-ast)
-			 original dictionary)
-  (declare (ignore dictionary))
-  (reinitialize-instance
-   clone
-   :read-only-p (cleavir-ast:read-only-p original)))
+    (maphash
+     (lambda (key value)
+       (apply #'reinitialize-instance
+	      value
+	      (loop for (keyword reader) in (cleavir-io:save-info key)
+		    for new = (gethash (funcall reader key) dictionary)
+		    unless (null new)
+		      collect keyword
+		      and collect new)))
+     dictionary)
+    (gethash ast dictionary)))
