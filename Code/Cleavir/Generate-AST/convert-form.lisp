65a2e1be144d70099188c0251022ae4bce9c625b
Robert Strandh <robert.strandh@gmail.com>
2015-02-28 11:42:24 +0100

    Fixed typo in comment.

--- Code/Cleavir/Generate-AST/convert-form.lisp
+++ Code/Cleavir/Generate-AST/convert-form.lisp
@@ -36,7 +36,7 @@
     (if (subtypep t type)
 	;; The only way T can be a subtype of some other type is if
 	;; that other type is also T, so this is our way of testing
-	;; whether the type of hte variable is equivalent to T.  We
+	;; whether the type of the variable is equivalent to T.  We
 	;; use this information to avoid wrapping a THE-AST around the
 	;; variable.
 	(cleavir-env:identity info)
