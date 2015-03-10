dc14e4b8dd43aef5985128154a363b422b2e7283
Robert Strandh <robert.strandh@gmail.com>
2015-03-10 14:08:49 +0100

    Added a comment for each meter class.

--- Code/Cleavir/Meter/meter.lisp
+++ Code/Cleavir/Meter/meter.lisp
@@ -3,7 +3,20 @@
 (defgeneric reset (meter)
   (:method-combination progn))
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;; Class METER.
+;;;
+;;; This is the base class of all meters.
+
 (defclass meter () ())
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;; Class BASIC-METER.
+;;;
+;;; A meter class that counts the number of invocations and measures
+;;; the total CPU time for the invocations.
 
 (defclass basic-meter (meter)
   ((%cpu-time :initform 0 :accessor cpu-time)
