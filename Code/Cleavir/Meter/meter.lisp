96c967664992de8eb87420ef4995ace1614508d3
Robert Strandh <robert.strandh@gmail.com>
2015-03-10 14:00:51 +0100

    Added class BASIC-METER for CPU time and invocation count..

--- Code/Cleavir/Meter/meter.lisp
+++ Code/Cleavir/Meter/meter.lisp
@@ -1,3 +1,7 @@
 (cl:in-package #:cleavir-meter)
 
 (defclass meter () ())
+
+(defclass basic-meter (meter)
+  ((%cpu-time :initform 0 :accessor cpu-time)
+   (%invocation-count :initform 0 :accessor invocation-count)))
