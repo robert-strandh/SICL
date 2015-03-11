326ea95e89217a161d4d100254c7a0fcac274161
Robert Strandh <robert.strandh@gmail.com>
2015-03-11 08:31:06 +0100

    Use new AST mapping function for finding LOAD-TIME-VALUE-ASTs.

--- Code/Cleavir/AST-transformations/hoist-load-time-value.lisp
+++ Code/Cleavir/AST-transformations/hoist-load-time-value.lisp
@@ -1,17 +1,13 @@
 (cl:in-package #:cleavir-ast-transformations)
 
 (defun find-load-time-value-asts (ast)
-  (let ((table (make-hash-table :test #'eq)))
-    (labels ((traverse (ast)
-	       (unless (gethash ast table)
-		 (setf (gethash ast table) t)
-		 (if (typep ast 'cleavir-ast:load-time-value-ast)
-		     (list ast)
-		     (let ((children (cleavir-ast:children ast)))
-		       (reduce #'append
-			       (mapcar #'traverse children)
-			       :from-end t))))))
-      (traverse ast))))
+  (let ((result '()))
+    (cleavir-ast:map-ast-depth-first-preorder
+     (lambda (node)
+       (when (typep node 'cleavir-ast:load-time-value-ast)
+	 (push node result)))
+     ast)
+    result))
 
 (defun hoist-load-time-value (ast)
   (let* ((load-time-value-asts (find-load-time-value-asts ast))
