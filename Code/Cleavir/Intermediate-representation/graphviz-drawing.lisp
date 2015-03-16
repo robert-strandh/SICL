b65edac2623eb58cfdcd8f9bbbffebdfb1909ae6
Robert Strandh <robert.strandh@gmail.com>
2015-03-16 14:57:48 +0100

    Removed unused code.

--- Code/Cleavir/Intermediate-representation/graphviz-drawing.lisp
+++ Code/Cleavir/Intermediate-representation/graphviz-drawing.lisp
@@ -5,16 +5,3 @@
 ;;; Drawing a datum on a stream.
 
 (defgeneric draw-datum (datum stream))
-
-;;; During the drawing process, the value of this variable is a hash
-;;; table that contains data that have already been drawn. 
-(defparameter *datum-table* nil)
-
-(defun datum-id (datum)
-  (gethash datum *datum-table*))
-
-(defmethod draw-datum :around (datum stream)
-  (when (null (datum-id datum))
-    (setf (gethash datum *datum-table*) (gensym))
-    (call-next-method)))
-
