1abe36d911671e324aaa8be9de938490a9d93542
Robert Strandh <robert.strandh@gmail.com>
2015-03-19 17:20:16 +0100

    Remove function IMPORT-CLASSES.  No longer needed.

--- Code/Compiler/Extrinsic-environment/import-from-host-common-lisp.lisp
+++ Code/Compiler/Extrinsic-environment/import-from-host-common-lisp.lisp
@@ -1,13 +1,5 @@
 (cl:in-package #:sicl-extrinsic-environment)
 
-;;; Enter every Common Lisp class into the environment.
-(defun import-classes (environment)
-  (loop for symbol being each external-symbol in '#:common-lisp
-	for class = (find-class symbol nil)
-	unless (null class)
-	  do (setf (sicl-env:find-class symbol environment)
-		   class)))
-
 (defun import-variables (environment)
   (let ((variables '(*standard-input* *standard-output* *error-output*
 		     *terminal-io* *trace-output* *query-io*
@@ -17,6 +9,5 @@
 		   (cl:symbol-value symbol)))))
 
 (defun import-from-host-common-lisp (environment)
-  (import-classes environment)
   (import-variables environment))
   
