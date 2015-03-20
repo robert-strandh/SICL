f1a08b58d976399c17a40dc74bf983041211129f
Robert Strandh <robert.strandh@gmail.com>
2015-03-20 08:45:19 +0100

    Do not shadow CL symbols.

--- Code/Iteration/packages.lisp
+++ Code/Iteration/packages.lisp
@@ -1,6 +1,4 @@
 (cl:in-package #:common-lisp)
 
 (defpackage #:sicl-iteration
-  (:use #:common-lisp)
-  (:shadow #:dolist #:dotimes #:do #:do*)
-  (:export #:dolist #:dotimes #:do #:do*))
+  (:use #:common-lisp))
