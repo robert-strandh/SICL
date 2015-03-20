ba9669b3692f52c29471d3d99d8f479797e59de5
Robert Strandh <robert.strandh@gmail.com>
2015-03-20 10:36:21 +0100

    Load file containing definition of macros DO and DO*.

--- Code/Compiler/Extrinsic-environment/fill.lisp
+++ Code/Compiler/Extrinsic-environment/fill.lisp
@@ -91,4 +91,6 @@
   ;; Load a file containing the definition of the macro DOTIMES.
   (load-file "../../Iteration/dotimes-defmacro.lisp" environment)
   ;; Load a file containing the definition of the macro DOLIST.
-  (load-file "../../Iteration/dolist-defmacro.lisp" environment))
+  (load-file "../../Iteration/dolist-defmacro.lisp" environment)
+  ;; Load a file containing the definition of the macros DO and DO*.
+  (load-file "../../Iteration/do-dostar-defmacro.lisp" environment))
