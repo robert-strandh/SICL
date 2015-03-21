b09bff44e93adc45817181abaac7b80608e50946
Robert Strandh <robert.strandh@gmail.com>
2015-03-21 08:25:46 +0100

    Added condition for when a name should name a function but names a local macro.

--- Code/Cleavir/Generate-AST/conditions.lisp
+++ Code/Cleavir/Generate-AST/conditions.lisp
@@ -139,6 +139,15 @@
     (compilation-program-error)
   ())
 
+;;; This condition is signaled when a function name is encountered
+;;; during compilation in a context where the name must be that of a
+;;; local or a globall function, but instead the name has a definition
+;;; as a local macro in the environment in which the function name is
+;;; compiled.
+(define-condition function-name-names-local-macro
+    (compilation-program-error)
+  ())
+
 ;;; This condition is signaled by methods on CONVERT-SPECIAL,
 ;;; specialized to operators for which Cleavir does not provide a
 ;;; default method.
