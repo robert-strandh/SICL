8cd8b34bd2fc9a8dc14c78b797604f397f82c37f
Robert Strandh <robert.strandh@gmail.com>
2015-03-23 11:41:10 +0100

    Added definition of macro PROG*.

--- Code/Data-and-control-flow/prog-progstar-defmacro.lisp
+++ Code/Data-and-control-flow/prog-progstar-defmacro.lisp
@@ -7,3 +7,11 @@
        (let ,bindings
 	 ,@declarations
 	 (tagbody ,@items)))))
+
+(defmacro prog* (bindings &body body)
+  (multiple-value-bind (declarations items)
+      (cleavir-code-utilities:separate-ordinary-body body)
+    `(block nil
+       (let* ,bindings
+	 ,@declarations
+	 (tagbody ,@items)))))
