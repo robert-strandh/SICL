4ef73378d996571df23236796db91c2b052b42d1
Robert Strandh <robert.strandh@gmail.com>
2015-03-23 11:39:57 +0100

    New component containing definition of macro PROG.

---
+++ Code/Data-and-control-flow/prog-progstar-defmacro.lisp
@@ -0,0 +1,9 @@
+(cl:in-package #:sicl-data-and-control-flow)
+
+(defmacro prog (bindings &body body)
+  (multiple-value-bind (declarations items)
+      (cleavir-code-utilities:separate-ordinary-body body)
+    `(block nil
+       (let ,bindings
+	 ,@declarations
+	 (tagbody ,@items)))))
