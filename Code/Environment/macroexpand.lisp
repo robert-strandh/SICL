f9600eccd54153a2f12d65dbe171ab70e0b66364
Robert Strandh <robert.strandh@gmail.com>
2015-03-26 09:15:14 +0100

    Removed function MACROEXPAND-1.  Now defined differently and elsewhere.

--- Code/Environment/macroexpand.lisp
+++ Code/Environment/macroexpand.lisp
@@ -1,4 +1,4 @@
-(in-package #:sicl-global-environment)
+(in-package #:sicl-standard-environment-functions)
 
 ;;;; Macro expansion.
 
@@ -15,45 +15,6 @@
 
 (defmethod cleavir-env:macro-function (symbol (env environment))
   (macro-function symbol env))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;;
-;;; Function MACROEXPAND-1.
-;;;
-;;; This function returns two values, the expanded form and a boolean
-;;; indicating whether the form was expanded or not.  I can't think of
-;;; a reason for the existence of the second return value, because if
-;;; the form is a macro form, then the expander is called and the
-;;; second value is true.  If the form is not a macro form, then the
-;;; initial form is return and the second value is false.  If the
-;;; expander should ever return the same form, then MACROEXPAND-1 will
-;;; be called again (at least if it was called by MACROEXPAND) and we
-;;; would have an infinite computation.  Though I suppose that if the
-;;; expander did some non-functional stuff like consulting global
-;;; variables, then, it might be possible. 
-
-(defun macroexpand-1 (form &optional env)
-  (let ((expander nil))
-    (cond
-      ((and (consp form) (symbolp (car form)))
-       (setf expander (macro-function (car form) env)))
-      ((symbolp form)
-       (let ((entry (find-if (lambda (entry)
-			       (and (typep entry 'symbol-macro-entry)
-				    (eq (name entry) form)))
-			     (append env
-				     (symbol-macros *global-environment*)))))
-	 (if (null entry)
-	     nil
-	     (setf expander (definition entry)))))
-      (t nil))
-    (if (null expander)
-	(values form nil)
-	(values (funcall (coerce *macroexpand-hook* 'function)
-			 expander
-			 form
-			 env)
-		t))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;
