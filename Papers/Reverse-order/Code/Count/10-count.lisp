bbb0131f7f92e8ca567baf64e5630ab452ad62e1
Irene Durand <idurand@labri.fr>
2015-03-26 18:37:04 +0100

    homogenize compiler declarations

--- Papers/Reverse-order/Code/Count/10-count.lisp
+++ Papers/Reverse-order/Code/Count/10-count.lisp
@@ -1,19 +1,19 @@
 ;; For Irene's experiments.
 ;; reverse in an array on stack
-
 (defun count-from-end-with-length-10 (x list length)
-  (declare (type fixnum length) (optimize (speed 3) (compilation-speed 0)))
+  (declare (optimize (speed 3) (debug 0) (safety 0) (compilation-speed 0)))
+  (declare (type fixnum length))
   (flet ((count-from-array (x list len)
-		   (declare (type fixnum len) (optimize (speed 3) (compilation-speed 0)))
-		   (let ((v (make-array len)))
-			 (declare (dynamic-extent v))
-			 (loop ;; reverse list to array
-				   for e in list
-				   for i from (1- len) downto 0
-				   do (setf (aref v i) e))
-			 (loop for i from 0 below len
-				   when (eq x (aref v i))
-					 count 1))))
+	   (declare (type fixnum len) (optimize (speed 3) (compilation-speed 0)))
+	   (let ((v (make-array len)))
+	     (declare (dynamic-extent v))
+	     (loop ;; reverse list to array
+		for e in list
+		for i from (1- len) downto 0
+		do (setf (aref v i) e))
+	     (loop for i from 0 below len
+		when (eq x (aref v i))
+		count 1))))
     (macrolet ((divide (x rest len k)
 		 (let* ((n (ash 1 k))
 			(gensyms (loop repeat n collect (gensym)))
@@ -21,14 +21,14 @@
 		   `(let ((,f (ash len (- ,k)))
 			  (,(car gensyms) ,rest))
 		      (let* ,(loop
-			       for gensym1 in gensyms
-			       for gensym2 in (cdr gensyms)
-			       collect `(,gensym2 (nthcdr ,f ,gensym1)))
+				for gensym1 in gensyms
+				for gensym2 in (cdr gensyms)
+				collect `(,gensym2 (nthcdr ,f ,gensym1)))
 			(+ 
 			 (traverse ,x (nthcdr ,f ,(car (last gensyms))) (- ,len (ash ,f ,k)))
 			 ,@(loop
-			     for gensym in (reverse gensyms)
-			     collect `(traverse ,x ,gensym ,f))))))))
+			      for gensym in (reverse gensyms)
+			      collect `(traverse ,x ,gensym ,f))))))))
       (labels
 	  ((traverse (x rest len)
 	     (declare (type fixnum len))
@@ -41,4 +41,3 @@
 
 (defun reverse-count-10 (x list)
   (count-from-end-with-length-10 x list (length list)))
-
