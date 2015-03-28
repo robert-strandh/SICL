bf7718344e441faf6f22cfc55622a35ea4e5ca82
Robert Strandh <robert.strandh@gmail.com>
2015-03-28 12:03:53 +0100

    New condition for when PSETF has an odd number of arguments.

--- Code/Data-and-control-flow/conditions.lisp
+++ Code/Data-and-control-flow/conditions.lisp
@@ -3,3 +3,7 @@
 (define-condition odd-number-of-arguments-to-setf
     (program-error cleavir-i18n:condition)
   ((%form :initarg :form :reader form)))
+
+(define-condition odd-number-of-arguments-to-psetf
+    (program-error cleavir-i18n:condition)
+  ((%form :initarg :form :reader form)))
