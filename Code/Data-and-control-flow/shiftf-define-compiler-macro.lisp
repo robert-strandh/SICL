64629273d7604738902e5e9ef2f8b3809c7d8e96
Robert Strandh <robert.strandh@gmail.com>
2015-03-30 11:31:12 +0200

    Added compiler macro for SHIFTF with the purpose of checking syntax.

---
+++ Code/Data-and-control-flow/shiftf-define-compiler-macro.lisp
@@ -0,0 +1,11 @@
+(cl:in-package #:sicl-data-and-control-flow)
+
+(define-compiler-macro shiftf (&whole form)
+  (cond ((not (cleavir-code-utilities:proper-list-p form))
+	 (error 'cleavir-code-utilities:form-must-be-proper-list
+		:form form))
+	((< (length form) 3)
+	 (error 'too-few-arguments-to-shiftf
+		:form form))
+	(t
+	 form)))
