ea91fa87b9158b389bcfd6568f7b0734a564e771
Robert Strandh <robert.strandh@gmail.com>
2015-03-30 11:33:14 +0200

    Added condition reporter for new condition.

--- Code/Data-and-control-flow/condition-reporters-english.lisp
+++ Code/Data-and-control-flow/condition-reporters-english.lisp
@@ -4,7 +4,7 @@
     ((condition odd-number-of-arguments-to-setf)
      stream
      (langauge cleavir-i18n:english))
-  (format stream 
+  (format stream
 	  "An odd number of arguments was given to SETF~@
            in the following form:~@
            ~s"
@@ -14,7 +14,7 @@
     ((condition odd-number-of-arguments-to-psetf)
      stream
      (langauge cleavir-i18n:english))
-  (format stream 
+  (format stream
 	  "An odd number of arguments was given to PSETF~@
            in the following form:~@
            ~s"
@@ -24,8 +24,18 @@
     ((condition odd-number-of-arguments-to-psetq)
      stream
      (langauge cleavir-i18n:english))
-  (format stream 
+  (format stream
 	  "An odd number of arguments was given to PSETQ~@
+           in the following form:~@
+           ~s"
+          (form condition)))
+
+(defmethod cleavir-i18n:report-condition
+    ((condition too-few-arguments-to-shiftf)
+     stream
+     (langauge cleavir-i18n:english))
+  (format stream
+	  "Too few arguments were given to SHIFTF~@
            in the following form:~@
            ~s"
           (form condition)))
