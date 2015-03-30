434627be795939ece0d65793659c56873ab8090d
Robert Strandh <robert.strandh@gmail.com>
2015-03-30 16:22:04 +0200

    New component containing definition of macro WITH-OPEN-STREAM.

---
+++ Code/Stream/with-open-stream-defmacro.lisp
@@ -0,0 +1,9 @@
+(cl:in-package #:sicl-stream)
+
+(defmacro with-open-stream ((variable stream-form) &body body)
+  (multiple-value-bind (declarations forms)
+      (cleavir-code-utilities:separate-ordinary-body body)
+    `(let ((,variable ,stream-form))
+       ,@declarations
+       (unwind-protect (progn ,@forms)
+	 (close ,variable)))))
