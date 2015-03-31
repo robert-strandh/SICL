7914a80437365471a52a0dffc27d3bff55c6d97e
Robert Strandh <robert.strandh@gmail.com>
2015-03-31 16:19:55 +0200

    Break long lines.

--- Papers/Reverse-order/Code/Count/7-count.lisp
+++ Papers/Reverse-order/Code/Count/7-count.lisp
@@ -1,5 +1,6 @@
 (defun count-from-end-with-length-7 (x list length)
-  (declare (optimize (speed 3) (safety 0) (debug 0) (compilation-speed 0)))
+  (declare (optimize (speed 3) (safety 0) (debug 0) 
+           (compilation-speed 0)))
   (declare (type fixnum length))
   (labels (;; AUX1 is the recursive traversal 
 	   ;; by CDR.
