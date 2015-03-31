13df846d085c22d93446da79b510f2231f36831c
Robert Strandh <robert.strandh@gmail.com>
2015-03-31 08:48:11 +0200

    Break long lines to improve final layout.

--- Papers/Reverse-order/Code/Count/1-count.lisp
+++ Papers/Reverse-order/Code/Count/1-count.lisp
@@ -1,5 +1,7 @@
 (defun reverse-count-1 (x list)
-  (declare (optimize (speed 3) (debug 0) (safety 0) (compilation-speed 0)))
+  (declare (optimize
+	    (speed 3) (debug 0) (safety 0)
+	    (compilation-speed 0)))
   (loop
      for e in (reverse list)
      count (eq x e)))
