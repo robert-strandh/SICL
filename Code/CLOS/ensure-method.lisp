0764a74989f7027ec69cfad915326a1b795d446b
Robert Strandh <robert.strandh@gmail.com>
2015-04-01 09:00:51 +0200

    Added required parameter ENVIRONMENT to call SICL-ENVIRONMENT:FIND-CLASS.

--- Code/CLOS/ensure-method.lisp
+++ Code/CLOS/ensure-method.lisp
@@ -7,13 +7,16 @@
 ;;; This function is not required by the AMOP to exist, 
 
 (defun ensure-method (generic-function
+		      environment
 		      &rest keys
 		      &key specializers
 		      &allow-other-keys)
   ;; FIXME: Check that SPECIALIZERS is a proper list, and that
   ;; it contains only symbols and specializer metaobjects. 
   (let ((specs (loop for s in specializers
-		     collect (if (symbolp s) (find-class s) s)))
+		     collect (if (symbolp s)
+				 (sicl-environment:find-class s environment)
+				 s)))
 	(remaining-keys (copy-list keys)))
     (loop while (remf remaining-keys :specializers))
     (let ((method (apply #'make-instance
