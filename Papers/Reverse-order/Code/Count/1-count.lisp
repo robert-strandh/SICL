bf940991ec05334742fe1839c183b99ba9c6b6e6
Robert Strandh <robert.strandh@gmail.com>
2015-04-03 08:33:53 +0200

    Avoid newline after LOOP.

--- Papers/Reverse-order/Code/Count/1-count.lisp
+++ Papers/Reverse-order/Code/Count/1-count.lisp
@@ -1,7 +1,6 @@
 (defun reverse-count-1 (x list)
   (declare (optimize
-	    (speed 3) (debug 0) (safety 0)
-	    (compilation-speed 0)))
-  (loop
-     for e in (reverse list)
-     count (eq x e)))
+            (speed 3) (debug 0) (safety 0)
+            (compilation-speed 0)))
+  (loop for e in (reverse list)
+        count (eq x e)))
