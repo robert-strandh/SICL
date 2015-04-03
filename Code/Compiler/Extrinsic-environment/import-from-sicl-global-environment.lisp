d86ace7274d5b01600cd92d5c40c9905bf1af17a
Robert Strandh <robert.strandh@gmail.com>
2015-04-03 14:58:52 +0200

    Create new constant variable in addition to existing special variable.

--- Code/Compiler/Extrinsic-environment/import-from-sicl-global-environment.lisp
+++ Code/Compiler/Extrinsic-environment/import-from-sicl-global-environment.lisp
@@ -1,5 +1,9 @@
 (cl:in-package #:sicl-extrinsic-environment)
 
 (defun import-from-sicl-global-environment (environment)
+  ;; This variable is deprecated.  Uses of it should be replaced by
+  ;; uses of the constant sicl-env:+global-environment+
   (setf (sicl-env:special-variable 'sicl-env:*global-environment* environment t)
+	environment)
+  (setf (sicl-env:constant-variable 'sicl-env:+global-environment+ environment)
 	environment))
