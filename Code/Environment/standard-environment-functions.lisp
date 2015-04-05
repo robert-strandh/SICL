95a2c63e946b5af42028d1f1ee032ade85013be6
Robert Strandh <robert.strandh@gmail.com>
2015-04-05 08:37:58 +0200

    Removed definition of function (SETF MACRO-FUNCTION) now in a different component.

--- Code/Environment/standard-environment-functions.lisp
+++ Code/Environment/standard-environment-functions.lisp
@@ -48,24 +48,6 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;
-;;; Function (SETF MACRO-FUNCTION).
-;;;
-;;; The HyperSpec says that the consequences are undefined if a
-;;; non-nil environment is given.  We define those consequences to be
-;;; that the environment is considered to be a first-class global
-;;; environment.
-
-(defun (setf macro-function) (new-function symbol &optional environment)
-  (declare (type symbol symbol)
-	   (type function new-function))
-  (when (null environment)
-    (setf environment
-	  (load-time-value (sicl-genv:global-environment))))
-  (setf (sicl-genv:macro-function symbol environment)
-	new-function))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;;
 ;;; Function FBOUNDP.
 ;;;
 ;;; According to the HyperSpec, this function should return any true
