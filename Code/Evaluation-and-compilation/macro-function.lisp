64610e1eed570cf9ff530801752f12f47a7cc480
Robert Strandh <robert.strandh@gmail.com>
2015-04-05 08:14:09 +0200

    Improve definitions of functions MACRO-FUNCTION and (SETF MACRO-FUNCTION).

--- Code/Evaluation-and-compilation/macro-function.lisp
+++ Code/Evaluation-and-compilation/macro-function.lisp
@@ -1,10 +1,25 @@
 (cl:in-package #:sicl-evaluation-and-compilation)
 
 (defun macro-function (symbol &optional environment)
-  (cleavir-env:macro-function symbol (or environment *global-environment*)))
+  (cleavir-env:macro-function
+   symbol
+   (or environment
+       (load-time-value (sicl-genv:global-environment)))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;; Function (SETF MACRO-FUNCTION).
+;;;
+;;; The HyperSpec says that the consequences are undefined if a
+;;; non-nil environment is given.  We define those consequences to be
+;;; that the environment is considered to be a first-class global
+;;; environment.
 
 (defun (setf macro-function) (new-function symbol &optional environment)
-  (unless (null environment)
-    (error 'environment-must-be-omitted-or-nil))
-  (setf (sicl-env:macro-function symbol *global-environment*)
+  (declare (type symbol symbol)
+	   (type function new-function))
+  (when (null environment)
+    (setf environment
+	  (load-time-value (sicl-genv:global-environment))))
+  (setf (sicl-genv:macro-function symbol environment)
 	new-function))
