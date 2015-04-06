3e31c3d72a90acd3081ee7c92a975b32bef5c464
Robert Strandh <robert.strandh@gmail.com>
2015-04-06 18:10:07 +0200

    Do not shadow symbols from the COMMON-LISP package.

--- Code/Stream/packages.lisp
+++ Code/Stream/packages.lisp
@@ -1,11 +1,4 @@
 (cl:in-package #:common-lisp-user)
 
 (defpackage #:sicl-stream
-  (:use #:common-lisp)
-  (:shadow #:finish-output
-	   #:force-output
-	   #:clear-output)
-  (:export
-   #:finish-output
-   #:force-output
-   #:clear-output))
+  (:use #:common-lisp))
