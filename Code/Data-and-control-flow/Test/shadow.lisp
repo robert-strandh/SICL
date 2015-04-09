7be9fbce5dd96ebc5d921a88e0442887d236dc7d
Robert Strandh <robert.strandh@gmail.com>
2015-04-09 17:44:29 +0200

    File for shadowing CL symbols for the purpose of testing module.

---
+++ Code/Data-and-control-flow/Test/shadow.lisp
@@ -0,0 +1,17 @@
+(cl:in-package "sicl-data-and-control-flow")
+
+(shadow '(#:setf
+	  #:define-setf-expander
+	  #:multiple-value-list
+	  #:nth-value
+	  #:get-setf-expansion
+	  #:return
+	  #:prog1
+	  #:prog2
+	  #:prog
+	  #:prog*
+	  #:psetq
+	  #:psetf
+	  #:rotatef
+	  #:destructuring-bind
+	  #:shiftf))
