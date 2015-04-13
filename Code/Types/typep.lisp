ca5d0d4d25b556cee0c32109da7ff2b1a5f25aa4
Robert Strandh <robert.strandh@gmail.com>
2015-04-13 17:39:28 +0200

    Added method on TYPEP-COMPOUND specialized to (EQL 'RATIONAL).

--- Code/Types/typep.lisp
+++ Code/Types/typep.lisp
@@ -310,3 +310,35 @@
 	   (< object (car upper-bound)))
 	  (t
 	   t))))
+
+;;; Given a type specifier of the form (RATIONAL . REST), check whether
+;;; OBJECT is of that type in ENVIRONMENT.
+(defmethod typep-compound (object (head (eql 'rational)) rest environment)
+  (unless (rationalp object)
+    (return-from typep-compound nil))
+  ;; OBJECT is definitely an rational.
+  (when (null rest)
+    ;; the type specifier is (RATIONAL), so since OBJECT is an rational,
+    ;; we are done.
+    (return-from typep-compound t))
+  ;; the type specifier is (RATIONAL <lower-bound> . ...).
+  (let ((lower-bound (first rest)))
+    (cond ((rationalp lower-bound)
+	   (when (< object lower-bound)
+	     (return-from typep-compound nil)))
+	  ((consp lower-bound)
+	   (when (<= object (car lower-bound))
+	     (return-from typep-compound nil)))
+	  (t
+	   nil)))
+  (when (null (rest rest))
+    ;; the type specifier is (RATIONAL <lower-bound>), so since we have
+    ;; checked that the lower bound is OK, we are done.
+    (return-from typep-compound t))
+  (let ((upper-bound (second rest)))
+    (cond ((rationalp upper-bound)
+	   (<= object upper-bound))
+	  ((consp upper-bound)
+	   (< object (car upper-bound)))
+	  (t
+	   t))))
