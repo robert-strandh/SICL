f2a21b633a1b367b73319f91e0741653724657da
Robert Strandh <robert.strandh@gmail.com>
2015-04-14 11:32:35 +0200

    Added ordinary function for handling all float types.

--- Code/Types/typep.lisp
+++ Code/Types/typep.lisp
@@ -347,3 +347,33 @@
   (or (eq type1 type2)
       (and (subtypep type1 type2)
 	   (subtypep type2 type1))))
+
+(defun typep-compound-float (object head rest)
+  (unless (same-float-type-p (type-of object) head)
+    (return-from typep-compound-float nil))
+  ;; OBJECT is definitely a float of the right type.
+  (when (null rest)
+    ;; The type specifier is (...-FLOAT), so since OBJECT is a float
+    ;; of the right type, we are done.
+    (return-from typep-compound-float t))
+  ;; The type specifier is (...-FLOAT <lower-bound> . ...).
+  (let ((lower-bound (first rest)))
+    (cond ((floatp lower-bound)
+	   (when (< object lower-bound)
+	     (return-from typep-compound-float nil)))
+	  ((consp lower-bound)
+	   (when (<= object (car lower-bound))
+	     (return-from typep-compound-float nil)))
+	  (t
+	   nil)))
+  (when (null (rest rest))
+    ;; The type specifier is (...-FLOAT <lower-bound>), so since we
+    ;; have checked that the lower bound is OK, we are done.
+    (return-from typep-compound-float t))
+  (let ((upper-bound (second rest)))
+    (cond ((floatp upper-bound)
+	   (<= object upper-bound))
+	  ((consp upper-bound)
+	   (< object (car upper-bound)))
+	  (t
+	   t))))
