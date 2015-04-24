ffa07c4c281c83532e39d2022c15e0fa0c899acd
Robert Strandh <robert.strandh@gmail.com>
2015-04-24 17:05:39 +0200

    New function for swapping two blocks.

--- Papers/Merge-sort/Code/Huang-Langston/common.lisp
+++ Papers/Merge-sort/Code/Huang-Langston/common.lisp
@@ -1,1 +1,10 @@
 (cl:in-package #:huang-langston)
+
+;;; Exchange two blocks of length LENGTH.  The first block starts at
+;;; index START1 and the second block starts at index START2.  Blocks
+;;; do not overlap.
+(defun blockswap (vector start1 start2 length)
+  (loop for i from start1
+	for j from start2
+	repeat length
+	do (rotatef (aref vector i) (aref vector j))))
