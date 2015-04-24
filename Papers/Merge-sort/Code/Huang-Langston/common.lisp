8d110410c0adef626f891778e085f92fc77954bc
Robert Strandh <robert.strandh@gmail.com>
2015-04-24 17:06:24 +0200

    New function for rotating the elements of a block.

--- Papers/Merge-sort/Code/Huang-Langston/common.lisp
+++ Papers/Merge-sort/Code/Huang-Langston/common.lisp
@@ -16,3 +16,13 @@
 	for j downfrom (+ start length -1)
 	repeat (floor length 2)
 	do (rotatef (aref vector i) (aref vector j))))
+
+;;; Rotate a block with LENGTH elements starting at index START by
+;;; AMOUNT places to the left.  Let PREFIX be the AMOUNT elements
+;;; starting at START and the SUFFIX be the remaining elements.  The
+;;; method used is to reverse the prefix and the suffix and then to
+;;; reverse the entire block.
+(defun rotate-block (vector start length amount)
+  (reverse-block vector start amount)
+  (reverse-block vector (+ start amount) length)
+  (reverse-block vector start length))
