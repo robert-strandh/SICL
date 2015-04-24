38099d0c205475f7cdec93bf2a398e9241a02593
Robert Strandh <robert.strandh@gmail.com>
2015-04-24 17:06:09 +0200

    New function for reversing the elements of a block.

--- Papers/Merge-sort/Code/Huang-Langston/common.lisp
+++ Papers/Merge-sort/Code/Huang-Langston/common.lisp
@@ -8,3 +8,11 @@
 	for j from start2
 	repeat length
 	do (rotatef (aref vector i) (aref vector j))))
+
+;;; Reverse the elements of a block of length LENGTH starting at index
+;;; START.
+(defun reverse-block (vector start length)
+  (loop for i from start
+	for j downfrom (+ start length -1)
+	repeat (floor length 2)
+	do (rotatef (aref vector i) (aref vector j))))
