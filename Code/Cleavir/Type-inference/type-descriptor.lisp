241dd34f5f3b878aebe13882082b4fb8d598f3fb
Robert Strandh <robert.strandh@gmail.com>
2015-04-27 12:04:01 +0200

    Added comment, justifying the use of a canonical representation.

--- Code/Cleavir/Type-inference/type-descriptor.lisp
+++ Code/Cleavir/Type-inference/type-descriptor.lisp
@@ -1,1 +1,15 @@
 (cl:in-package #:cleavir-type-inference)
+
+;;;; We use a canonical representation of the type of a variable.
+;;;;
+;;;; Baker's paper on SUBTYPEP suggests that it is not necessary to
+;;;; canonicalize the representation of the type, and it is probably
+;;;; best not to do so when SUBTYPEP is called a single time at
+;;;; run-time.  However, in the type inferencer, the requirements are
+;;;; quite different.  We frequently need to compute the union,
+;;;; intersection, and difference of types, as well as many
+;;;; compositions of these operations.  In that situation it is
+;;;; probably better to use a canonical representation to avoid that
+;;;; the size of the representation grow after each operation.
+
+;;  LocalWords:  canonicalize inferencer
