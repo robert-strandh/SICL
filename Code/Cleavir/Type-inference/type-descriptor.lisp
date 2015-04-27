b6be66a0cc248d3a6f28d9611961fdae22eba0c3
Robert Strandh <robert.strandh@gmail.com>
2015-04-27 12:23:03 +0200

    Definition of the sub-type descriptor for integers.

--- Code/Cleavir/Type-inference/type-descriptor.lisp
+++ Code/Cleavir/Type-inference/type-descriptor.lisp
@@ -11,5 +11,37 @@
 ;;;; compositions of these operations.  In that situation it is
 ;;;; probably better to use a canonical representation to avoid that
 ;;;; the size of the representation grow after each operation.
+;;;;
+;;;; The canonical representation we use is not a valid Common Lisp
+;;;; type specifier.  For that reason, we call it a TYPE DESCRIPTOR
+;;;; instead.
+;;;;
+;;;; A type descriptor is either T, meaning the type T, or it is a
+;;;; list of sub-type descriptors.  The meaning of this list is the OR
+;;;; of the meaning of each element.  If the list is empty, i.e. NIL,
+;;;; it means the type NIL.  If a sub-type is omitted from the list,
+;;;; then objects described by that sub-type are not members of the
+;;;; type described by the type descriptor.
+;;;;
+;;;; Sub-type descriptors that are present in the type descriptor
+;;;; appear in a fixed, predefined order.  This order is defined by
+;;;; the order in which sub-type descriptors are defined below.
+;;;;
+;;;; INTEGER
+;;;;
+;;;;   This sub-type descriptor has the following form:
+;;;;
+;;;;     (INTEGER (l1 u1) (l2 u2) ... (ln un))
+;;;;
+;;;;   where li and ui are integers or the symbol *.  Only l1 and un
+;;;;   can be the symbol *.  The pair (li ui) designate a closed
+;;;;   interval of integers, where li is the least possible value and
+;;;;   ui is the largest possible value.  Furthermore for all i, if
+;;;;   both li and ui are integers, then i <= 1 <= n, li <= ui.  Also,
+;;;;   for all i such that 1 <= i < n, ui + 1 < li+1.  In other words,
+;;;;   the intervals are ordered and there is at least one integer
+;;;;   between each interval.  Notice that we do not allow the
+;;;;   notation (n) for the sub-type INTEGER because it can trivially
+;;;;   be replaced by n-1 or n+1 as needed.
 
 ;;  LocalWords:  canonicalize inferencer
