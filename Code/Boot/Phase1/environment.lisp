4119fb89c0307470c3251b90fea79c88d09d357f
Robert Strandh <robert.strandh@gmail.com>
2015-05-06 07:14:12 +0200

    Define :AFTER method on INITIALIZE-INSTANCE on phase 1 environment.

--- Code/Boot/Phase1/environment.lisp
+++ Code/Boot/Phase1/environment.lisp
@@ -3,3 +3,7 @@
 (defclass environment (sicl-simple-environment:simple-environment)
   ((%compilation-environment :initarg :compilation-environment
 			     :reader compilation-environment)))
+
+(defmethod initialize-instance :after
+    ((environment environment) &key compilation-environment)
+  (fill-environment compilation-environment environment))
