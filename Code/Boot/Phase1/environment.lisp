eb19615232d4aa50da6bcde3af6766edd0ff8270
Robert Strandh <robert.strandh@gmail.com>
2015-05-06 07:10:03 +0200

    Added slot for compilation environment.

--- Code/Boot/Phase1/environment.lisp
+++ Code/Boot/Phase1/environment.lisp
@@ -1,4 +1,5 @@
 (cl:in-package #:sicl-boot-phase1)
 
 (defclass environment (sicl-simple-environment:simple-environment)
-  ())
+  ((%compilation-environment :initarg :compilation-environment
+			     :reader compilation-environment)))
