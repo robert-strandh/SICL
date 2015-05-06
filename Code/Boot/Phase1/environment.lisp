4149d530f2f9fe63a6cdab334fe5f828bee7512d
Robert Strandh <robert.strandh@gmail.com>
2015-05-06 08:27:47 +0200

    Added initform to slot containing compilation environment.

--- Code/Boot/Phase1/environment.lisp
+++ Code/Boot/Phase1/environment.lisp
@@ -1,8 +1,10 @@
 (cl:in-package #:sicl-boot-phase1)
 
 (defclass environment (sicl-simple-environment:simple-environment)
-  ((%compilation-environment :initarg :compilation-environment
-			     :reader compilation-environment)))
+  ((%compilation-environment
+    :initarg :compilation-environment
+    :initform (make-instance 'sicl-extrinsic-environment:environment)
+    :reader compilation-environment)))
 
 (defmethod initialize-instance :after ((environment environment) &key)
   (fill-environment environment))
