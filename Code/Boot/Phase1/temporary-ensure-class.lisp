e6553be52b565ffd3f18f55b7776879560b66da1
Robert Strandh <robert.strandh@gmail.com>
2015-05-13 16:07:16 +0200

    Define ENSURE-CLASS to call host version of itself.

--- Code/Boot/Phase1/temporary-ensure-class.lisp
+++ Code/Boot/Phase1/temporary-ensure-class.lisp
@@ -1,4 +1,20 @@
 (cl:in-package #:sicl-clos)
 
-(defun ensure-class (name &rest arguments &key &allow-other-keys)
-  (apply #'ensure-class-using-class-null name arguments))
+(defun ensure-class (name
+		     &rest arguments
+		     &key direct-superclasses
+		     &allow-other-keys)
+  (let* ((generated-name (gensym))
+	 (environment (sicl-genv:global-environment))
+	 (new-arguments (copy-list arguments))
+	 (new-superclasses
+	   (loop for superclass-name in (remove t direct-superclasses)
+		 for class = (sicl-genv:find-class superclass-name environment)
+		 collect (class-name class))))
+    (loop while (remf new-arguments :direct-superclasses))
+    (let ((class (apply #'closer-mop:ensure-class
+			generated-name
+			:direct-superclasses new-superclasses
+			new-arguments)))
+      (setf (sicl-genv:find-class name environment) class)
+      class)))
