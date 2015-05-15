3c4e0eac872fb6a81d7eca88863cf093e654c2b6
Robert Strandh <robert.strandh@gmail.com>
2015-05-15 11:10:52 +0200

    Extracted definition of generic function SLOT-DEFINITION-INIFORM to a separate component.

---
+++ Code/CLOS/slot-definition-initform-defgeneric.lisp
@@ -0,0 +1,5 @@
+(cl:in-package #:sicl-clos)
+
+;;; For the specification of this generic function, see
+;;; http://metamodular.com/CLOS-MOP/slot-definition-initform.html
+(defgeneric slot-definition-initform (slot-definition))
--- Code/CLOS/slot-definition-readers-defgenerics.lisp
+++ Code/CLOS/slot-definition-readers-defgenerics.lisp
@@ -1,10 +1,6 @@
 (cl:in-package #:sicl-clos)
 
 ;;; For the specification of this generic function, see
-;;; http://metamodular.com/CLOS-MOP/slot-definition-initform.html
-(defgeneric slot-definition-initform (slot-definition))
-
-;;; For the specification of this generic function, see
 ;;; http://metamodular.com/CLOS-MOP/slot-definition-initfunction.html
 (defgeneric slot-definition-initfunction (slot-definition))
 
