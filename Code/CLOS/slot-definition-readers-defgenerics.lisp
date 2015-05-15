38885aa6451b7564f37d875f4958f596c389ac3e
Robert Strandh <robert.strandh@gmail.com>
2015-05-15 11:12:23 +0200

    Extracted definition of generic function SLOT-DEFINITION-INIFUNCTION to a separate component.

--- Code/CLOS/slot-definition-readers-defgenerics.lisp
+++ Code/CLOS/slot-definition-readers-defgenerics.lisp
@@ -1,10 +1,6 @@
 (cl:in-package #:sicl-clos)
 
 ;;; For the specification of this generic function, see
-;;; http://metamodular.com/CLOS-MOP/slot-definition-initfunction.html
-(defgeneric slot-definition-initfunction (slot-definition))
-
-;;; For the specification of this generic function, see
 ;;; http://metamodular.com/CLOS-MOP/slot-definition-name.html
 (defgeneric slot-definition-name (slot-definition))
 
