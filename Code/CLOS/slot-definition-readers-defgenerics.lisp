3340f346748cef4758146da2f8a3617395e9c734
Robert Strandh <robert.strandh@gmail.com>
2015-05-15 11:25:39 +0200

    Extracted definition of generic function SLOT-DEFINITION-WRITERS to a separate component.

--- Code/CLOS/slot-definition-readers-defgenerics.lisp
+++ Code/CLOS/slot-definition-readers-defgenerics.lisp
@@ -1,9 +1,5 @@
 (cl:in-package #:sicl-clos)
 
 ;;; For the specification of this generic function, see
-;;; http://metamodular.com/CLOS-MOP/slot-definition-writers.html
-(defgeneric slot-definition-writers (slot-definition))
-
-;;; For the specification of this generic function, see
 ;;; http://metamodular.com/CLOS-MOP/slot-definition-location.html
 (defgeneric slot-definition-location (slot-definition))
