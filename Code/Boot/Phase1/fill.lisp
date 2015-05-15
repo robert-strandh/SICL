91a7f87877d2cf0aaac5e29496dd09369fa482e8
Robert Strandh <robert.strandh@gmail.com>
2015-05-15 06:22:47 +0200

    Load a file containing the definition of the class STANDARD-SLOT-DEFINITION.

--- Code/Boot/Phase1/fill.lisp
+++ Code/Boot/Phase1/fill.lisp
@@ -32,6 +32,7 @@
   (ld "../../CLOS/standard-reader-method-defclass.lisp" environment)
   (ld "../../CLOS/standard-writer-method-defclass.lisp" environment)
   (ld "../../CLOS/slot-definition-defclass.lisp" environment)
+  (ld "../../CLOS/standard-slot-definition-defclass.lisp" environment)
   (ld "../../CLOS/specializer-defclass.lisp" environment)
   (ld "../../CLOS/eql-specializer-defclass.lisp" environment)
   (ld "../../CLOS/class-unique-number-defparameter.lisp" environment)
