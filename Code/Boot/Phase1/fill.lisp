72ba2b4dac5b24566b5ecbabdd9e28419a60ab0f
Robert Strandh <robert.strandh@gmail.com>
2015-05-15 06:23:45 +0200

    Load a file containing the definition of the class STANDARD-EFFECTIVE-SLOT-DEFINITION.

--- Code/Boot/Phase1/fill.lisp
+++ Code/Boot/Phase1/fill.lisp
@@ -34,6 +34,7 @@
   (ld "../../CLOS/slot-definition-defclass.lisp" environment)
   (ld "../../CLOS/standard-slot-definition-defclass.lisp" environment)
   (ld "../../CLOS/standard-direct-slot-definition-defclass.lisp" environment)
+  (ld "../../CLOS/standard-effective-slot-definition-defclass.lisp" environment)
   (ld "../../CLOS/specializer-defclass.lisp" environment)
   (ld "../../CLOS/eql-specializer-defclass.lisp" environment)
   (ld "../../CLOS/class-unique-number-defparameter.lisp" environment)
