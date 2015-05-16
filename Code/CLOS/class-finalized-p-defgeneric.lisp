ce79a9aa9ffbfd32d7253523e50d5f2edfa140f1
Robert Strandh <robert.strandh@gmail.com>
2015-05-16 08:51:21 +0200

    Extracted definition of generic function CLASS-FINALIZED-P to a separate component.

---
+++ Code/CLOS/class-finalized-p-defgeneric.lisp
@@ -0,0 +1,5 @@
+(cl:in-package #:sicl-clos)
+
+;;; For the specification of this generic function, see
+;;; http://metamodular.com/CLOS-MOP/class-finalized-p.html
+(defgeneric class-finalized-p (class))
--- Code/CLOS/class-readers-defgenerics.lisp
+++ Code/CLOS/class-readers-defgenerics.lisp
@@ -5,9 +5,5 @@
 ;;; Class CLASS.
 
 ;;; For the specification of this generic function, see
-;;; http://metamodular.com/CLOS-MOP/class-finalized-p.html
-(defgeneric class-finalized-p (class))
-
-;;; For the specification of this generic function, see
 ;;; http://metamodular.com/CLOS-MOP/class-prototype.html
 (defgeneric class-prototype (class))
