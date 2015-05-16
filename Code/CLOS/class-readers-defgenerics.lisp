f5fdd6ca683cc681bcb360ce44aee25bd415f4cf
Robert Strandh <robert.strandh@gmail.com>
2015-05-16 08:52:55 +0200

    Extracted definition of generic function CLASS-PROTOTYPE to a separate component.

--- Code/CLOS/class-readers-defgenerics.lisp
+++ Code/CLOS/class-readers-defgenerics.lisp
@@ -3,7 +3,3 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;
 ;;; Class CLASS.
-
-;;; For the specification of this generic function, see
-;;; http://metamodular.com/CLOS-MOP/class-prototype.html
-(defgeneric class-prototype (class))
