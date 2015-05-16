d4ff5c5e9905da2c786443e6caa859997893e6b7
Robert Strandh <robert.strandh@gmail.com>
2015-05-16 07:35:47 +0200

    Load a new component containing a temporary version of the class FUNCTION.

--- Code/Boot/Phase1/fill.lisp
+++ Code/Boot/Phase1/fill.lisp
@@ -47,4 +47,5 @@
   (ld "../../CLOS/standard-class-defclass.lisp" environment)
   (ld "../../CLOS/funcallable-standard-class-defclass.lisp" environment)
   (ld "../../CLOS/built-in-class-defclass.lisp" environment)
+  (ld "function-temporary-defclass.lisp" environment)
   (ld "../../Environment/standard-environment-functions.lisp" environment))
