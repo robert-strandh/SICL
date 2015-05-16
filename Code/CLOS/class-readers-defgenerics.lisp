e4effe72c3c129cfdfb942fbfff2648e9320f6eb
Robert Strandh <robert.strandh@gmail.com>
2015-05-16 08:50:06 +0200

    Extracted definition of generic function CLASS-PRECEDENCE-LIST to a separate component.

--- Code/CLOS/class-readers-defgenerics.lisp
+++ Code/CLOS/class-readers-defgenerics.lisp
@@ -5,10 +5,6 @@
 ;;; Class CLASS.
 
 ;;; For the specification of this generic function, see
-;;; http://metamodular.com/CLOS-MOP/class-precedence-list.html
-(defgeneric class-precedence-list (class))
-
-;;; For the specification of this generic function, see
 ;;; http://metamodular.com/CLOS-MOP/class-finalized-p.html
 (defgeneric class-finalized-p (class))
 
