51dac82d04d05fe68c99db59fb5fe91a59e7f5fe
Robert Strandh <robert.strandh@gmail.com>
2015-05-17 07:29:57 +0200

    New component containing definition of class for phase 2 of boot process.

---
+++ Code/Boot/Phase2/environment.lisp
@@ -0,0 +1,10 @@
+(cl:in-package #:sicl-boot-phase2)
+
+(defclass environment (sicl-extrinsic-environment:environment)
+  ((%phase1-environment
+    :initarg :phase1-environment
+    :initform (make-instance 'sicl-boot-phase1:environment)
+    :reader phase1-environment)))
+
+(defmethod initialize-instance :after ((environment environment) &key)
+  (fill-environment environment))
