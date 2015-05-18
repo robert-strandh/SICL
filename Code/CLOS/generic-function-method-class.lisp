533fa0b9915f78734a3b21e2b095ec29e7791712
Robert Strandh <robert.strandh@gmail.com>
2015-05-18 11:43:42 +0200

    Extracted definition of generic function GENERIC-FUNCTION-METHOD-CLASS to a separate component.

---
+++ Code/CLOS/generic-function-method-class.lisp
@@ -0,0 +1,5 @@
+(cl:in-package #:sicl-clos)
+
+;;; For the specification of this generic function, see
+;;; http://metamodular.com/CLOS-MOP/generic-function-method-class.html
+(defgeneric generic-function-method-class (generic-function))
--- Code/CLOS/generic-function-readers-defgenerics.lisp
+++ Code/CLOS/generic-function-readers-defgenerics.lisp
@@ -5,10 +5,6 @@
 ;;; http://metamodular.com/CLOS-MOP/readers-for-generic-function-metaobjects.html
 
 ;;; For the specification of this generic function, see
-;;; http://metamodular.com/CLOS-MOP/generic-function-method-class.html
-(defgeneric generic-function-method-class (generic-function))
-
-;;; For the specification of this generic function, see
 ;;; http://metamodular.com/CLOS-MOP/generic-function-method-combination.html
 (defgeneric generic-function-method-combination (generic-function))
 
