4e90c264d53ee13fe9b535c619715547a8757231
Robert Strandh <robert.strandh@gmail.com>
2015-05-21 07:37:42 +0200

    Define a subclass of the extrinsic environment to use as compilation environment.

--- Code/Boot/Phase1/environment.lisp
+++ Code/Boot/Phase1/environment.lisp
@@ -1,5 +1,14 @@
 (cl:in-package #:sicl-boot-phase1)
 
+;;; We define a subclass of the normal extrinsic environment.  This
+;;; version of the extrinsic environment is used as compilation
+;;; environment in phase 1 of bootstrapping.  By creating a special
+;;; version of it, we can define an :AFTER method on
+;;; INITIALIZE-INSTANCE where we can modify it to correspond to the
+;;; needs of bootstrapping.
+(defclass compilation-environment (sicl-extrinsic-environment:environment)
+  ())
+
 (defclass environment (sicl-extrinsic-environment:environment)
   ((%compilation-environment
     :initarg :compilation-environment
