4f0569e66a69f8e909b500a76757a96f43d493ad
Robert Strandh <robert.strandh@gmail.com>
2015-05-21 11:36:14 +0200

    Replace :AROUND methods by a :BEFORE and :AFTER methods on SHARED-INITIALIZE.

--- Code/CLOS/generic-function-initialization-defmethods.lisp
+++ Code/CLOS/generic-function-initialization-defmethods.lisp
@@ -11,49 +11,27 @@
   (apply #'initialize-instance-after-standard-generic-function-default
 	 generic-function initargs))
 
-(defmethod initialize-instance :around
+(defmethod shared-initialize :before
     ((generic-function generic-function)
+     slot-names
      &rest initargs
      &key
-       documentation
-       declarations
-       (lambda-list nil lambda-list-p)
-       (argument-precedence-order nil argument-precedence-order-p)
-       method-combination
-       (method-class (find-class 'standard-method))
-       name
      &allow-other-keys)
-  (check-documentation documentation)
-  (check-declarations declarations)
-  (check-method-combination method-combination)
-  (check-method-class method-class)
-  (if lambda-list-p
-      (let* ((parsed-lambda-list
-	       (parse-generic-function-lambda-list lambda-list))
-	     (required (required parsed-lambda-list)))
-	(if argument-precedence-order-p
-	    (check-argument-precedence-order argument-precedence-order required)
-	    (setf argument-precedence-order required))
-	(apply #'call-next-method
-	       generic-function
-	       :documentation documentation
-	       :declarations declarations
-	       :argument-precedence-order argument-precedence-order
-	       :specializer-profile (make-list (length required)
-					       :initial-element nil)
-	       :method-class method-class
-	       :name name
-	       initargs))
-      (if argument-precedence-order-p
-	  (error "when argument precedence order appears,~@
-                  so must lambda list")
-	(apply #'call-next-method
-	       generic-function
-	       :documentation documentation
-	       :declarations declarations
-	       :method-class method-class
-	       :name name
-	       initargs))))
+  (apply #'shared-initialize-before-generic-function
+	 generic-function
+	 slot-names
+	 initargs))
+
+(defmethod shared-initialize :after
+    ((generic-function generic-function)
+     slot-names
+     &rest initargs
+     &key
+     &allow-other-keys)
+  (apply #'shared-initialize-after-generic-function
+	 generic-function
+	 slot-names
+	 initargs))
 
 (defmethod reinitialize-instance :after
     ((generic-function standard-generic-function)
@@ -62,38 +40,3 @@
      &allow-other-keys)
   (apply #'reinitialize-instance-after-standard-generic-function-default
 	 generic-function initargs))
-
-(defmethod reinitialize-instance :around
-    ((generic-function generic-function)
-     &rest initargs
-     &key
-       documentation
-       declarations
-       (lambda-list nil lambda-list-p)
-       (argument-precedence-order nil argument-precedence-order-p)
-       method-combination
-       (method-class nil method-class-p)
-     &allow-other-keys)
-  (check-documentation documentation)
-  (check-declarations declarations)
-  (check-method-combination method-combination)
-  (when method-class-p
-    (check-method-class method-class))
-  (if lambda-list-p
-      (let* ((parsed-lambda-list
-	       (parse-generic-function-lambda-list lambda-list))
-	     (required (required parsed-lambda-list)))
-	(if argument-precedence-order-p
-	    (check-argument-precedence-order argument-precedence-order required)
-	    (setf argument-precedence-order required))
-	(apply #'call-next-method
-	       generic-function
-	       :argument-precedence-order argument-precedence-order
-	       :specializer-profile (make-list (length required)
-					       :initial-element nil)
-	       initargs))
-      (if argument-precedence-order-p
-	  (error "when argument precedence order appears,~@
-                  so must lambda list")
-	  (call-next-method))))
-
