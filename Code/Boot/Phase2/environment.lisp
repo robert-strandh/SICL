e35b7c424786614d3dfdac7312806762a19b7b95
Robert Strandh <robert.strandh@gmail.com>
2015-05-22 14:23:04 +0200

    New function for defining macro DEFMETHOD in phase 2 compilation environment.

--- Code/Boot/Phase2/environment.lisp
+++ Code/Boot/Phase2/environment.lisp
@@ -9,5 +9,37 @@
     :initform (make-instance 'sicl-boot-phase1:environment)
     :reader phase1-environment)))
 
+(defun define-defmethod (compilation-environment run-time-environment)
+  (setf (sicl-genv:macro-function 'defmethod compilation-environment)
+	(lambda (form environment)
+	  (declare (ignore environment))
+	  (let* ((name (second form))
+		 (rest (rest (rest form)))
+		 (lambda-list-position (position-if #'listp rest))
+		 (lambda-list (nth lambda-list-position rest))
+		 (qualifiers (subseq rest 0 (1- lambda-list-position)))
+		 (body (subseq rest lambda-list-position))
+		 (required-end-position
+		   (position-if (lambda (x) (member x lambda-list-keywords))
+				lambda-list))
+		 (required (if (null required-end-position)
+			       lambda-list
+			       (subseq lambda-list 0 required-end-position)))
+		 (remaining (if (null required-end-position)
+				'()
+				(subseq lambda-list required-end-position)))
+		 (new-required
+		   (loop for req in required
+			 collect
+			 (if (symbolp req)
+			     req
+			     (list (first req)
+				   (class-name
+				    (sicl-genv:find-class
+				     (second req)
+				     run-time-environment))))))
+		 (new-args (append new-required remaining)))
+	    `(eval '(defmethod ,name ,@qualifiers ,@new-args ,@body))))))
+
 (defmethod initialize-instance :after ((environment environment) &key)
   (fill-environment environment))
