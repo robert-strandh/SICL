aed7900ea17c17625af74e543b4614412090aefd
Robert Strandh <robert.strandh@gmail.com>
2015-05-22 14:22:30 +0200

    Added a separate compilation environment for phase 2.

--- Code/Boot/Phase2/environment.lisp
+++ Code/Boot/Phase2/environment.lisp
@@ -1,7 +1,10 @@
 (cl:in-package #:sicl-boot-phase2)
 
 (defclass environment (sicl-extrinsic-environment:environment)
-  ((%phase1-environment
+  ((%compilation-environment
+    :initarg :compilation-environment
+    :initform (make-instance 'sicl-extrinsic-environment:environment))
+   (%phase1-environment
     :initarg :phase1-environment
     :initform (make-instance 'sicl-boot-phase1:environment)
     :reader phase1-environment)))
