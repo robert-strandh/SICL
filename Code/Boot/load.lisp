0c7ec73de3567bdd0b113fd0234cf9c3bd759672
Robert Strandh <robert.strandh@gmail.com>
2015-05-27 17:14:12 +0200

    New component defining a function for loading code using two environments.

---
+++ Code/Boot/load.lisp
@@ -0,0 +1,9 @@
+(cl:in-package #:sicl-boot)
+
+(defun ld (filename environment1 environment2)
+  (format *trace-output* "Loading file ~a~%" filename)
+  (finish-output *trace-output*)
+  (sicl-extrinsic-environment:load-source-with-environments
+   (asdf:system-relative-pathname :sicl-boot-phase1 filename)
+   environment1
+   environment2))
--- Code/Boot/sicl-boot.asd
+++ Code/Boot/sicl-boot.asd
@@ -5,4 +5,5 @@
   :serial t
   :components
   ((:file "packages")
-   (:file "boot")))
+   (:file "boot")
+   (:file "load")))
