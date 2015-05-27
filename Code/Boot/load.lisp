9bff8d9604a78b17366a6f4fbc5fdbb60456684b
Robert Strandh <robert.strandh@gmail.com>
2015-05-27 17:38:55 +0200

    Load files relative to new boot system.

--- Code/Boot/load.lisp
+++ Code/Boot/load.lisp
@@ -4,6 +4,6 @@
   (format *trace-output* "Loading file ~a~%" filename)
   (finish-output *trace-output*)
   (sicl-extrinsic-environment:load-source-with-environments
-   (asdf:system-relative-pathname :sicl-boot-phase1 filename)
+   (asdf:system-relative-pathname :sicl-boot filename)
    environment1
    environment2))
