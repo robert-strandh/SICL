58ffac841819d360b5a07475e62166997ca85adb
Robert Strandh <robert.strandh@gmail.com>
2015-05-27 18:20:14 +0200

    Call phase 1 code.

--- Code/Boot/fill.lisp
+++ Code/Boot/fill.lisp
@@ -1,8 +1,7 @@
 (cl:in-package #:sicl-boot)
 
 (defun fill-boot (boot)
-  (declare (ignore boot))
-  ())
+  (fill1 boot))
 
 (defmethod initialize-instance :after ((boot boot) &key &allow-other-keys)
   (fill-boot boot))
