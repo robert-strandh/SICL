416fc204369add2dccc995b15d19756402e755d7
Robert Strandh <robert.strandh@gmail.com>
2015-05-28 17:51:11 +0200

    Load a file containing support code for ENSURE-GENERIC-FUNCTION-USING-CLASS.

--- Code/Boot/customize-r2.lisp
+++ Code/Boot/customize-r2.lisp
@@ -1,7 +1,9 @@
 (cl:in-package #:sicl-boot)
 
 (defun customize-r2 (boot)
-  (define-make-instance boot)
-  (define-direct-slot-definition-class boot)
-  (define-find-class boot))
-
+  (let ((c (c1 boot))
+	(r (r2 boot)))
+    (define-make-instance boot)
+    (define-direct-slot-definition-class boot)
+    (define-find-class boot)
+    (ld "../CLOS/ensure-generic-function-using-class-support.lisp" c r)))
