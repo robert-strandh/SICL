dc1287d5c30f4650d0917c78b2d01a34a5c6e087
Robert Strandh <robert.strandh@gmail.com>
2015-05-29 08:34:28 +0200

    Quote lambda list and name of generic function.

--- Code/Boot/customize-c2.lisp
+++ Code/Boot/customize-c2.lisp
@@ -10,9 +10,9 @@
 		  (method-class-name 'standard-method)
 		  (method-class (sicl-genv:find-class method-class-name r1))
 		  (gf (make-instance class
-			:name ,(second form)
-			:lambda-list ,(third form)
-			:argument-precedence-order ,(third form)
+			:name ',(second form)
+			:lambda-list ',(third form)
+			:argument-precedence-order ',(third form)
 			:declarations '()
 			:documentation nil
 			;; FIXME: supply a method-combination metaobject.
