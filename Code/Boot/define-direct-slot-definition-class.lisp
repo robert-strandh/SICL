dc8c44fcaa28f4d4d31bb3a8e2ceb7a1ff1e9898
Robert Strandh <robert.strandh@gmail.com>
2015-05-29 10:11:34 +0200

    Use package prefix for name of class STANDARD-DIRECT-SLOT-DEFINITION.

--- Code/Boot/define-direct-slot-definition-class.lisp
+++ Code/Boot/define-direct-slot-definition-class.lisp
@@ -4,4 +4,5 @@
   (setf (sicl-genv:fdefinition 'sicl-clos:direct-slot-definition-class (r2 boot))
 	(lambda (&rest arguments)
 	  (declare (ignore arguments))
-	  (sicl-genv:find-class 'standard-direct-slot-definition (r1 boot)))))
+	  (sicl-genv:find-class 'sicl-clos:standard-direct-slot-definition
+				(r1 boot)))))
