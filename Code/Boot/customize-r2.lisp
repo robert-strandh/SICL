2b9b04ba2c427feddf5740477399342dcad356ba
Robert Strandh <robert.strandh@gmail.com>
2015-05-29 10:32:42 +0200

    Define function VALIDATE-SUPERCLASS in run-time environment R2, always returning true.

--- Code/Boot/customize-r2.lisp
+++ Code/Boot/customize-r2.lisp
@@ -1,9 +1,14 @@
 (cl:in-package #:sicl-boot)
 
+(defun define-validate-superclass (boot)
+  (setf (sicl-genv:fdefinition 'sicl-clos:validate-superclass (r2 boot))
+	(constantly t)))
+
 (defun customize-r2 (boot)
   (let ((c (c1 boot))
 	(r (r2 boot)))
     (define-make-instance boot)
     (define-direct-slot-definition-class boot)
     (define-find-class boot)
+    (define-validate-superclass boot)
     (ld "../CLOS/ensure-generic-function-using-class-support.lisp" c r)))
