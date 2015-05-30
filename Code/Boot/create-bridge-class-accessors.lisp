044530c647188c2b0f0261b97ddd88609251588e
Robert Strandh <robert.strandh@gmail.com>
2015-05-30 13:23:31 +0200

    Added comments so group accessors.

--- Code/Boot/create-bridge-class-accessors.lisp
+++ Code/Boot/create-bridge-class-accessors.lisp
@@ -3,14 +3,17 @@
 (defun create-bridge-class-accessors (boot)
   (let ((c (c2 boot))
 	(r (r3 boot)))
+    ;; Accessors for generic function metaobjects.
     (ld "../CLOS/call-history-defgeneric.lisp" c r)
     (ld "../CLOS/setf-call-history-defgeneric.lisp" c r)
+    ;; Accessors for method metaobjects.
     (ld "../CLOS/method-function-defgeneric.lisp" c r)
     (ld "../CLOS/method-generic-function-defgeneric.lisp" c r)
     (ld "../CLOS/method-lambda-list-defgeneric.lisp" c r)
     (ld "../CLOS/method-qualifiers-defgeneric.lisp" c r)
     (ld "../CLOS/method-specializers-defgeneric.lisp" c r)
     (ld "../CLOS/accessor-method-slot-definition-defgeneric.lisp" c r)
+    ;; Accessors for slot definition metaobjects.
     (ld "../CLOS/slot-definition-name-defgeneric.lisp" c r)
     (ld "../CLOS/slot-definition-allocation-defgeneric.lisp" c r)
     (ld "../CLOS/slot-definition-type-defgeneric.lisp" c r)
@@ -20,6 +23,7 @@
     (ld "../CLOS/slot-definition-readers-defgeneric.lisp" c r)
     (ld "../CLOS/slot-definition-writers-defgeneric.lisp" c r)
     (ld "../CLOS/slot-definition-location-defgeneric.lisp" c r)
+    ;; Accessors for specializer metaobjects.
     (ld "../CLOS/specializer-direct-methods-defgeneric.lisp" c r)
     (ld "../CLOS/setf-specializer-direct-methods-defgeneric.lisp" c r)
     (ld "../CLOS/specializer-profile-defgeneric.lisp" c r)
@@ -35,5 +39,6 @@
     (ld "../CLOS/class-default-initargs-defgeneric.lisp" c r)
     (ld "../CLOS/class-finalized-p-defgeneric.lisp" c r)
     (ld "../CLOS/class-prototype-defgeneric.lisp" c r)
+    ;; General accessors.
     (ld "../CLOS/dependents-defgeneric.lisp" c r)
     (ld "../CLOS/setf-dependents-defgeneric.lisp" c r)))
