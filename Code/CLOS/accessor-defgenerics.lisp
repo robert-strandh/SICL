96589c8ed143cdc120bdae30ceca9d485cf3317f
Robert Strandh <robert.strandh@gmail.com>
2015-06-01 11:39:54 +0200

    Extracted definition of writer (SETF PRECEDENCE-LIST) to a separate component.

--- Code/CLOS/accessor-defgenerics.lisp
+++ Code/CLOS/accessor-defgenerics.lisp
@@ -8,10 +8,6 @@
 ;;;
 ;;; Accessors for specializer metaobjects other than classes.
 
-;;; This function is used by the class finalization protocol to set
-;;; the precedence list of the class.
-(defgeneric (setf precedence-list) (precedence-list class))
-
 ;;; For a slot with :ALLOCATION :CLASS, this function returns a CONS
 ;;; cell where the CAR is used to store the value of the slot.
 (defgeneric slot-definition-storage (direct-slot-definition))
