9c9fce31a9ce72b4b9600cd332a247fe1c4c26ef
Robert Strandh <robert.strandh@gmail.com>
2015-06-01 10:36:34 +0200

    Load a file containing the definition of the writer (SETF INSTANCE-SIZE).

--- Code/Boot/create-bridge-class-accessors.lisp
+++ Code/Boot/create-bridge-class-accessors.lisp
@@ -57,6 +57,7 @@
     (ld "../CLOS/class-prototype-defgeneric.lisp" c r)
     (ld "../CLOS/setf-class-prototype-defgeneric.lisp" c r)
     (ld "../CLOS/instance-size-defgeneric.lisp" c r)
+    (ld "../CLOS/setf-instance-size-defgeneric.lisp" c r)
     ;; General accessors.
     (ld "../CLOS/dependents-defgeneric.lisp" c r)
     (ld "../CLOS/setf-dependents-defgeneric.lisp" c r)
