21835b0a11fb8bac53f5113408ea455ea66657f6
Robert Strandh <robert.strandh@gmail.com>
2015-06-03 17:02:37 +0200

    Remove :AFTER methods.  What they did is now in the :AROUND method.

--- Code/CLOS/class-initialization-defmethods.lisp
+++ Code/CLOS/class-initialization-defmethods.lisp
@@ -1,10 +1,5 @@
 (cl:in-package #:sicl-clos)
 
-(defmethod initialize-instance :after
-    ((class regular-class) &rest initargs &key &allow-other-keys)
-  (apply #'initialize-instance-after-regular-class-default
-	 class initargs))
-
 (defmethod shared-initialize :around
     ((class real-class)
      slot-names
@@ -16,29 +11,3 @@
 	 class
 	 slot-names
 	 initargs))
-
-;;; According to the AMOP, calling initialize-instance on a built-in
-;;; class (i.e., on an instance of a subclass of the class
-;;; BUILT-IN-CLASS) signals an error, because built-in classes can not
-;;; be created by the user.  But during the bootstrapping phase we
-;;; need to create built-in classes.  We solve this problem by
-;;; removing this method once the bootstrapping phase is finished.
-;;;
-;;; We do not add readers and writers here, because we do it in
-;;; ENSURE-BUILT-IN-CLASS after we have finalized inheritance.  The
-;;; reason for that is that we then know the slot location.
-(defmethod initialize-instance :after
-    ((class built-in-class) &rest initargs &key &allow-other-keys)
-  (apply #'initialize-instance-after-built-in-class-default
-	 class initargs))
-
-;;; I don't know why this definition makes SBCL go into an infinite
-;;; recursion.
-;; (defmethod reinitialize-instance :after
-;;     ((class standard-class)
-;;      &rest args
-;;      &key
-;;      &allow-other-keys)
-;;   (map-dependents class
-;; 		  (lambda (dependent)
-;; 		    (apply #'update-dependent class dependent args))))
