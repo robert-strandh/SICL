05306e25bb0ef60d27d8cb8fd993c57f5e033863
Robert Strandh <robert.strandh@gmail.com>
2015-06-03 12:07:55 +0200

    Load a file containing the definition of the class STANDARD-OBJECT.

--- Code/Boot/create-bridge-classes.lisp
+++ Code/Boot/create-bridge-classes.lisp
@@ -3,4 +3,5 @@
 (defun create-bridge-classes (boot)
   (let ((c (c1 boot))
 	(r (r2 boot)))
-    (ld "../CLOS/t-defclass.lisp" c r)))
+    (ld "../CLOS/t-defclass.lisp" c r)
+    (ld "../CLOS/standard-object-defclass.lisp" c r)))
