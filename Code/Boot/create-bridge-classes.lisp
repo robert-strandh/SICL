bb8b203859885f3533d6588d307024e4ffe4d7a7
Robert Strandh <robert.strandh@gmail.com>
2015-06-04 09:56:07 +0200

    Load a file containing the definition of STANDARD-EFFECTIVE-SLOT-DEFINITION.

--- Code/Boot/create-bridge-classes.lisp
+++ Code/Boot/create-bridge-classes.lisp
@@ -12,6 +12,7 @@
     (ld "../CLOS/direct-slot-definition-defclass.lisp" c r)
     (ld "../CLOS/effective-slot-definition-defclass.lisp" c r)
     (ld "../CLOS/standard-direct-slot-definition-defclass.lisp" c r)
+    (ld "../CLOS/standard-effective-slot-definition-defclass.lisp" c r)
     (ld "../CLOS/specializer-defclass.lisp" c r)
     (ld "../CLOS/class-defclass.lisp" c r)
     (ld "../CLOS/forward-referenced-class-defclass.lisp" c r)
