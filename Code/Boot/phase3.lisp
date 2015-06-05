041bab29d537f2d7d478a9f07e9051e3689db182
Robert Strandh <robert.strandh@gmail.com>
2015-06-05 06:31:00 +0200

    Load file containing support code for class finalization.

--- Code/Boot/phase3.lisp
+++ Code/Boot/phase3.lisp
@@ -3,4 +3,5 @@
 (defun phase3 (boot)
   (let ((c (c1 boot))
 	(r (r2 boot)))
-    (define-effective-slot-definition-class boot)))
+    (define-effective-slot-definition-class boot)
+    (ld "../CLOS/class-finalization-support.lisp" c r)))
