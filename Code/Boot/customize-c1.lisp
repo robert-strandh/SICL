3b2ac327fb6b97d3083e0d69e9b9ce420082fc71
Robert Strandh <robert.strandh@gmail.com>
2015-06-19 17:45:30 +0200

    Added comment explaining the reason for defining DEFGENERIC this way.

--- Code/Boot/customize-c1.lisp
+++ Code/Boot/customize-c1.lisp
@@ -1,5 +1,17 @@
 (cl:in-package #:sicl-boot)
 
+;;; Define the macro DEFGENERIC in compile-time environment C1.  We
+;;; define it a bit differently from its usual definition.  Its main
+;;; purpose is to define a generic function in the run-time
+;;; environment R1.  However, before definining it, we remove the
+;;; existing generic function if it exists.  This way, we are sure to
+;;; get a fresh generic function, as opposed to one that happened to
+;;; have been imported from the host.  We must, of course, make sure
+;;; that we execute a DEFGENERIC form for a particular generic
+;;; function exactly once, but we can do that because we completely
+;;; master the boot process.  We also put the same definition in the
+;;; compile-time environment C1 so that the compiler sees it when
+;;; subsequent forms that use this function are compiled.
 (defun define-defgeneric-c1 (boot)
   (setf (sicl-genv:macro-function 'defgeneric (c1 boot))
 	(lambda (form environment)
