147f786c0639d6ce809625117bb6ac3bf2628054
Robert Strandh <robert.strandh@gmail.com>
2015-06-19 16:39:16 +0200

    Define a custom version of DEFGENERIC in compilation environment C1.

--- Code/Boot/customize-c1.lisp
+++ Code/Boot/customize-c1.lisp
@@ -1,5 +1,18 @@
 (cl:in-package #:sicl-boot)
 
+(defun define-defgeneric-c1 (boot)
+  (setf (sicl-genv:macro-function 'defgeneric (c1 boot))
+	(lambda (form environment)
+	  (declare (ignore environment))
+	  `(progn (sicl-genv:fmakunbound ',(car form) ,(r1 boot))
+		  (ensure-generic-function
+		   ',(car form)
+		   :name ',(car form)
+		   :lambda-list ',(cadr form))
+		  (setf (sicl-genv:fdefinition ',(car form) ,(c1 boot))
+			(sicl-genv:fdefinition ',(car form) ,(r1 boot)))))))
+
 (defun customize-c1 (boot)
   (message "Customizing compilation environment C1~%")
+  (define-defgeneric-c1 boot)
   (message "Finished customizing compilation environment C1~%"))
