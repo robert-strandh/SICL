3281b0e89f828f455ced38d832f4c389dd327c81
Robert Strandh <robert.strandh@gmail.com>
2015-06-19 11:10:55 +0200

    Add temporary definition of ENSURE-GENERIC-FUNCTION into R1.

--- Code/Boot/customize-r1.lisp
+++ Code/Boot/customize-r1.lisp
@@ -1,7 +1,17 @@
 (cl:in-package #:sicl-boot)
 
+(defun define-ensure-generic-function-r1 (boot)
+  (setf (sicl-genv:fdefinition 'ensure-generic-function (r1 boot))
+	(lambda (function-name &rest arguments)
+	  (if (sicl-genv:fboundp function-name (r1 boot))
+	      (sicl-genv:fdefinition function-name (r1 boot))
+	      (setf (sicl-genv:fdefinition function-name (r1 boot))
+		    (apply #'make-instance 'standard-generic-function
+			   arguments))))))
+
 (defun customize-r1 (boot)
   (let ((c (c1 boot))
 	(r (r1 boot)))
-    (message "Customizing run-time environment R2~%")
-    (message "Finished customizing run-time environment R2~%")))
+    (message "Customizing run-time environment R1~%")
+    (define-ensure-generic-function-r1 boot)
+    (message "Finished customizing run-time environment R1~%")))
