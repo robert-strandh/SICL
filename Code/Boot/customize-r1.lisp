9fc53cda297e377bc71f9fafd38858fe0be0a75c
Robert Strandh <robert.strandh@gmail.com>
2015-06-19 14:45:34 +0200

    Remove the :ENVIRONMENT keyword argument before calling MAKE-INSTANCE.

--- Code/Boot/customize-r1.lisp
+++ Code/Boot/customize-r1.lisp
@@ -3,11 +3,13 @@
 (defun define-ensure-generic-function-r1 (boot)
   (setf (sicl-genv:fdefinition 'ensure-generic-function (r1 boot))
 	(lambda (function-name &rest arguments)
-	  (if (sicl-genv:fboundp function-name (r1 boot))
-	      (sicl-genv:fdefinition function-name (r1 boot))
-	      (setf (sicl-genv:fdefinition function-name (r1 boot))
-		    (apply #'make-instance 'standard-generic-function
-			   arguments))))))
+	  (let ((args (copy-list arguments)))
+	    (loop while (remf args :environment))
+	    (if (sicl-genv:fboundp function-name (r1 boot))
+		(sicl-genv:fdefinition function-name (r1 boot))
+		(setf (sicl-genv:fdefinition function-name (r1 boot))
+		      (apply #'make-instance 'standard-generic-function
+			     args)))))))
 
 (defun customize-r1 (boot)
   (let ((c (c1 boot))
