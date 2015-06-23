1ba3bb478488d4b7656b9e9d8a95a15ef23e8a50
Robert Strandh <robert.strandh@gmail.com>
2015-06-23 06:12:24 +0200

    Define function in R1, but operate on R2.

--- Code/Boot/customize-r1.lisp
+++ Code/Boot/customize-r1.lisp
@@ -1,13 +1,21 @@
 (cl:in-package #:sicl-boot)
 
+;;; We define a special version of ENSURE-GENERIC-FUNCTION in the
+;;; run-time environment R1.  While the function is defined in the
+;;; run-time environment R1, it operates on the run-time environment
+;;; R2.  This version checks whether there is already a function named
+;;; FUNCTION-NAME in R2.  If so that function is returned, and it is
+;;; assumed to be a generic function.  If not, an instance of the host
+;;; class STANDARD-GENERIC-FUNCTION is created and associated with
+;;; FUNCTION-NAME in R2.
 (defun define-ensure-generic-function-r1 (boot)
   (setf (sicl-genv:fdefinition 'ensure-generic-function (r1 boot))
 	(lambda (function-name &rest arguments)
 	  (let ((args (copy-list arguments)))
 	    (loop while (remf args :environment))
-	    (if (sicl-genv:fboundp function-name (r1 boot))
-		(sicl-genv:fdefinition function-name (r1 boot))
-		(setf (sicl-genv:fdefinition function-name (r1 boot))
+	    (if (sicl-genv:fboundp function-name (r2 boot))
+		(sicl-genv:fdefinition function-name (r2 boot))
+		(setf (sicl-genv:fdefinition function-name (r2 boot))
 		      (apply #'make-instance 'standard-generic-function
 			     :name function-name
 			     args)))))))
