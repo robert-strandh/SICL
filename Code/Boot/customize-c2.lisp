67eaf71ed01b1dddb6edab71823ad598c5741d14
Robert Strandh <robert.strandh@gmail.com>
2015-06-23 16:29:27 +0200

    Moved and renamed function for definining DEFGENERIC in environment C2.

--- Code/Boot/customize-c2.lisp
+++ Code/Boot/customize-c2.lisp
@@ -1,35 +1,7 @@
 (cl:in-package #:sicl-boot)
 
-;;; The purpose of this function is to redefine the macro DEFGENERIC
-;;; in the compilation environment C2.  The new definition is only
-;;; used to create bridge-class accessors in run-time environment
-;;; during phase 2.  Therefore, we can process only the name and the
-;;; lambda-list of each DEFGENERIC form.  Furthermore, by passing all
-;;; the initialization arguments to MAKE-INSTANCE, we avoid having to
-;;; implement the generic-function initialization protocol in phase 2.
-(defun define-defgeneric (boot)
-  (setf (sicl-genv:macro-function 'defgeneric (c2 boot))
-	(lambda (form environment)
-	  (declare (ignore environment))
-	  `(let* ((r1 ,(r1 boot))
-		  (class-name 'standard-generic-function)
-		  (class (sicl-genv:find-class class-name r1))
-		  (method-class-name 'standard-method)
-		  (method-class (sicl-genv:find-class method-class-name r1))
-		  (gf (make-instance class
-			:name ',(second form)
-			:lambda-list ',(third form)
-			:argument-precedence-order ',(third form)
-			:declarations '()
-			:documentation nil
-			;; FIXME: supply a method-combination metaobject.
-			:method-combination nil
-			:method-class method-class)))
-	     (setf (sicl-genv:fdefinition ',(second form) ,(r3 boot)) gf)))))
-
 (defun customize-c2 (boot)
   (message "Customizing compilation environment C2~%")
-  (define-defgeneric boot)
   (message "Finished customizing compilation environment C2~%"))
 
 ;;  LocalWords:  accessors
--- Code/Boot/customize-environments.lisp
+++ Code/Boot/customize-environments.lisp
@@ -25,11 +25,39 @@
 		  (setf (sicl-genv:fdefinition ',(second form) ,(c1 boot))
 			(sicl-genv:fdefinition ',(second form) ,(r2 boot)))))))
 
+;;; The purpose of this function is to redefine the macro DEFGENERIC
+;;; in the compilation environment C2.  The new definition is only
+;;; used to create bridge-class accessors in run-time environment
+;;; during phase 2.  Therefore, we can process only the name and the
+;;; lambda-list of each DEFGENERIC form.  Furthermore, by passing all
+;;; the initialization arguments to MAKE-INSTANCE, we avoid having to
+;;; implement the generic-function initialization protocol in phase 2.
+(defun define-defgeneric-c2 (boot)
+  (setf (sicl-genv:macro-function 'defgeneric (c2 boot))
+	(lambda (form environment)
+	  (declare (ignore environment))
+	  `(let* ((r1 ,(r1 boot))
+		  (class-name 'standard-generic-function)
+		  (class (sicl-genv:find-class class-name r1))
+		  (method-class-name 'standard-method)
+		  (method-class (sicl-genv:find-class method-class-name r1))
+		  (gf (make-instance class
+			:name ',(second form)
+			:lambda-list ',(third form)
+			:argument-precedence-order ',(third form)
+			:declarations '()
+			:documentation nil
+			;; FIXME: supply a method-combination metaobject.
+			:method-combination nil
+			:method-class method-class)))
+	     (setf (sicl-genv:fdefinition ',(second form) ,(r3 boot)) gf)))))
+
 (defun customize-c1 (boot)
   (let ((c1 (c1 boot))
 	(r1 (r1 boot)))
     (message "Customizing environments~%")
     (define-defgeneric-c1 boot)
+    (define-defgeneric-c2 boot)
     (ld "../CLOS/make-method-lambda-support.lisp" c1 c1)
     (ld "../CLOS/make-method-lambda-defuns.lisp" c1 c1)
     (ld "../CLOS/ensure-method.lisp" c1 r1)
