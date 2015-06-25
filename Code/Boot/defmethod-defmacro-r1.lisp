a65214b2d517d056757a7cb2cc31c54cf9e5431a
Robert Strandh <robert.strandh@gmail.com>
2015-06-25 15:25:01 +0200

    Added comment explaining the special version of DEFMETHOD.

--- Code/Boot/defmethod-defmacro-r1.lisp
+++ Code/Boot/defmethod-defmacro-r1.lisp
@@ -1,5 +1,14 @@
 (cl:in-package #:sicl-clos)
 
+;;;; This definition of the macro DEFMETHOD is less general than the
+;;;; normal definition.  It is only guaranteed to work when the
+;;;; generic function is of type STANDARD-GENERIC-FUNCTION, and the
+;;;; method to be created can be of type STANDARD-METHOD.
+;;;;
+;;;; The advantage of this definition is that it does not require the
+;;;; full generality of MAKE-METHOD-LAMBDA.  Instead, it uses the
+;;;; default version of MAKE-METHOD-LAMBDA.
+
 (defmacro defmethod (function-name &rest rest)
   (multiple-value-bind
 	(qualifiers lambda-list specializers declarations documentation forms)
