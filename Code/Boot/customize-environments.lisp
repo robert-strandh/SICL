714128ac5796eb48cdbcf3d3cd1a12ea4539bc7e
Robert Strandh <robert.strandh@gmail.com>
2015-06-25 10:04:47 +0200

    Added comment explaining the need for a separate import of FUNCALLABLE-STANDARD-CLASS.

--- Code/Boot/customize-environments.lisp
+++ Code/Boot/customize-environments.lisp
@@ -185,6 +185,16 @@
 	(make-instance 'closer-mop:funcallable-standard-class
 	  :name (make-symbol (symbol-name '#:function)))))
 
+;;; This function defines class SICL-CLOS:FUNCALLABLE-STANDARD-CLASS
+;;; in the host environment to be the same as the host version of that
+;;; class.  It is needed because, although we import definitions in
+;;; the host environment that are associated with the the package
+;;; CLOSER-MOP, in fact those symbols do not have CLOSER-MOP as their
+;;; package.  Instead they are imported into the CLOSER-MOP package
+;;; from the corresponding host-specific package.  We should eliminate
+;;; the need for this function by not importing based on the package
+;;; of the symbols, but based on whether the symbols are accessible in
+;;; the CLOSER-MOP package.
 (defun define-funcallable-standard-class ()
   (setf (find-class 'sicl-clos:funcallable-standard-class)
 	(find-class 'closer-mop:funcallable-standard-class)))
