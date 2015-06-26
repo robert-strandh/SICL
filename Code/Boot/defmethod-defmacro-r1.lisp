513126408e44296358c7683d26328fa7d3ccb57b
Robert Strandh <robert.strandh@gmail.com>
2015-06-26 10:23:19 +0200

    Call TEMPORARY-ENSURE-METHOD rather than ENSURE-METHOD.

--- Code/Boot/defmethod-defmacro-r1.lisp
+++ Code/Boot/defmethod-defmacro-r1.lisp
@@ -13,22 +13,15 @@
   (multiple-value-bind
 	(qualifiers lambda-list specializers declarations documentation forms)
       (parse-defmethod rest)
-    (let ((generic-function-var (gensym)))
-      `(let* ((rt-env (load-time-value (sicl-genv:global-environment)))
-	      (,generic-function-var
-		(ensure-generic-function ',function-name
-					 :environment rt-env)))
-	 (ensure-method
-	  ,generic-function-var
-	  rt-env
-	  :lambda-list ',lambda-list
-	  :qualifiers ',qualifiers
-	  :specializers ,(canonicalize-specializers specializers)
-	  :documentation ,documentation
-	  :function
-	  ,(sicl-clos::make-method-lambda-default
-	    nil nil
-	    `(lambda ,lambda-list
-	       ,@declarations
-	       ,@forms)
-	    nil))))))
+    `(temporary-ensure-method
+      ',function-name
+      ',lambda-list
+      ',qualifiers
+      ,(canonicalize-specializers specializers)
+      ,documentation
+      ,(sicl-clos::make-method-lambda-default
+	nil nil
+	`(lambda ,lambda-list
+	   ,@declarations
+	   ,@forms)
+	nil))))
