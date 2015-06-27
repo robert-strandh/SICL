2a1ca1c076ca392d300decb2e396bca113e6307d
Robert Strandh <robert.strandh@gmail.com>
2015-06-27 15:08:54 +0200

    Remove obsolete component.

--- Code/Boot/Phase1-obsolete/defclass.lisp
+++
@@ -1,7 +0,0 @@
-(cl:in-package #:sicl-boot-phase1)
-
-(defmacro defclass (name superclasses slots &optional class-options)
-  (declare (ignore class-options))
-  (let ((supers (remove t superclasses)))
-    `(cl:defclass ,name ,supers ,slots)))
-
