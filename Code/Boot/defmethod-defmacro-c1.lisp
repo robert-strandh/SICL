aa4c9f46f0b4d7e9e89a065c5e21dd7c6f714e50
Robert Strandh <robert.strandh@gmail.com>
2015-06-27 10:37:03 +0200

    Explain why the temporary DEFMETHOD macro is written the way it is.

--- Code/Boot/defmethod-defmacro-c1.lisp
+++ Code/Boot/defmethod-defmacro-c1.lisp
@@ -8,6 +8,15 @@
 ;;;; The advantage of this definition is that it does not require the
 ;;;; full generality of MAKE-METHOD-LAMBDA.  Instead, it uses the
 ;;;; default version of MAKE-METHOD-LAMBDA.
+;;;;
+;;;; By using this version, we avoid calling CLASS-PROTOTYPE and
+;;;; GENERIC-FUNCTION-METHOD-CLASS.  These two functions pose a
+;;;; problem because if we were to call them here, they would have to
+;;;; be the functions defined by the host.  However, when we define
+;;;; the accessor generic functions for the MOP classes in environment
+;;;; R1, we also define those two functions to be the target versions,
+;;;; and the two versions can not exist simultaneously.  We avoid the
+;;;; problem by not needing the host versions of these functions.
 
 (defmacro defmethod (function-name &rest rest)
   (multiple-value-bind
