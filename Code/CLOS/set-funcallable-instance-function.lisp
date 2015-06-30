f38ac859697d96cfd7addad474443b112903ee1f
Robert Strandh <robert.strandh@gmail.com>
2015-06-30 13:50:50 +0200

    Remove obsolete component.

--- Code/CLOS/set-funcallable-instance-function.lisp
+++
@@ -1,6 +0,0 @@
-(cl:in-package #:sicl-clos)
-
-(defun set-funcallable-instance-function (generic-function function)
-  #+sbcl(sb-pcl:set-funcallable-instance-function generic-function function)
-  #-sbcl(error "Can't set funcallable instance function")
-  )
