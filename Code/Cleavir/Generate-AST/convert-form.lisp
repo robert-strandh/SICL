13b686e9a818e9fd758b35d08d839eaa233c27bd
Robert Strandh <robert.strandh@gmail.com>
2015-07-01 14:30:42 +0200

    Reorganize code slightly, and add comment to CONVERT-FUNCTION.

--- Code/Cleavir/Generate-AST/convert-form.lisp
+++ Code/Cleavir/Generate-AST/convert-form.lisp
@@ -136,10 +136,22 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;
-;;; Converting a compound form that calls a global function.
-;;; A global function can have  compiler macro associated with it.
+;;; Generic function CONVERT-FUNCTION.
+;;;
+;;; This generic function converts a function given by a description
+;;; in the form of an INFO instance.  That INFO instance was obtained
+;;; by calling FUNCTION-INFO with an environment and a function name.
+;;; The function signals an error if the INFO instance is not a
+;;; GLOBAL-FUNCTION-INFO or a LOCAL-FUNCTION-INFO.  Client code can
+;;; override the default behavior by adding methods to this function,
+;;; specialized to the particular system defined by that client code.
 
 (defgeneric convert-function (info env system))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;; Converting a compound form that calls a global function.
+;;; A global function can have  compiler macro associated with it.
 
 (defun make-call (info env arguments system)
   (let ((argument-asts (convert-sequence arguments env system))
