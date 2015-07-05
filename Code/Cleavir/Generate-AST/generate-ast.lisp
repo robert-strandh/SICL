e9d611194661e9dca62190f42880ac5be5868d15
Robert Strandh <robert.strandh@gmail.com>
2015-07-05 09:37:02 +0200

    Only evaluate at compile time if it is not one of the special cases.

--- Code/Cleavir/Generate-AST/generate-ast.lisp
+++ Code/Cleavir/Generate-AST/generate-ast.lisp
@@ -519,9 +519,21 @@
 	 (let ((info (variable-info environment form)))
 	   (convert-form form info environment system)))
 	((symbolp (car form))
+	 ;; Even if we are in COMPILE-TIME-TOO mode, at this point, we
+	 ;; do not know whether to evaluate the form at compile time,
+	 ;; simply because it might be a special form that is handled
+	 ;; specially.  So we must wait until we have more
+	 ;; information.
 	 (let ((info (function-info environment (car form))))
 	   (convert-form form info environment system)))
 	(t
+	 ;; The form must be a compound form where the CAR is a lambda
+	 ;; expression.  Evaluating such a form might have some
+	 ;; compile-time side effects, so we must check whether we are
+	 ;; in COMPILE-TIME-TOO mode, in which case we must evaluate
+	 ;; the form as well.
+	 (when (and *current-form-is-top-level-p* *compile-time-too*)
+	   (cleavir-env:eval form environment environment))
 	 (convert-lambda-call form environment system))))
 
 (defun generate-ast (form environment system)
@@ -587,6 +599,4 @@
   (declare (ignore system))
   (let ((*current-form-is-top-level-p* *subforms-are-top-level-p*)
 	(*subforms-are-top-level-p* nil))
-    (when (and *compile-time-too* *current-form-is-top-level-p*)
-      (cleavir-env:eval form environment environment ))
     (call-next-method)))
