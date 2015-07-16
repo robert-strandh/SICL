92909e7e4eb643febaf905574d79c90cb2de8ae2
Robert Strandh <robert.strandh@gmail.com>
2015-07-16 14:39:35 +0200

    Moved phase 1 customization to component dedicated to phase 1 processing.

--- Code/Boot/customize-environments.lisp
+++ Code/Boot/customize-environments.lisp
@@ -1,30 +1,5 @@
 (cl:in-package #:sicl-boot)
 
-;;; Define the macro DEFGENERIC in compile-time environment C1.  We
-;;; define it a bit differently from its usual definition.  Its main
-;;; purpose is to define a generic function in the run-time
-;;; environment R2.  However, before definining it, we remove the
-;;; existing generic function if it exists.  This way, we are sure to
-;;; get a fresh generic function, as opposed to one that happened to
-;;; have been imported from the host.  We must, of course, make sure
-;;; that we execute a DEFGENERIC form for a particular generic
-;;; function exactly once, but we can do that because we completely
-;;; master the boot process.  We also put the same definition in the
-;;; compile-time environment C1 so that the compiler sees it when
-;;; subsequent forms that use this function are compiled.
-(defun define-defgeneric-c1 (boot)
-  (setf (sicl-genv:macro-function 'defgeneric (c1 boot))
-	(lambda (form environment)
-	  (declare (ignore environment))
-	  `(progn (sicl-genv:fmakunbound ',(second form) ,(r2 boot))
-		  (setf (sicl-genv:fdefinition ',(second form) ,(r2 boot))
-			(ensure-generic-function
-			 ',(second form)
-			 :name ',(second form)
-			 :lambda-list ',(third form)))
-		  (setf (sicl-genv:fdefinition ',(second form) ,(c1 boot))
-			(sicl-genv:fdefinition ',(second form) ,(r2 boot)))))))
-
 ;;; The purpose of this function is to redefine the macro DEFGENERIC
 ;;; in the compilation environment C2.  The new definition is only
 ;;; used to create bridge-class accessors in run-time environment
@@ -266,7 +241,6 @@
   (let ((c1 (c1 boot))
 	(r1 (r1 boot)))
     (message "Customizing environments for phase 1~%")
-    (define-defgeneric-c1 boot)
     (define-temporary-ensure-method-c1-r1 boot)
     (define-class-t-phase1 r1)
     (define-class-function-r1 boot)
--- Code/Boot/phase1.lisp
+++ Code/Boot/phase1.lisp
@@ -10,33 +10,61 @@
 ;;;; environment contains the normal definition of DEFCLASS, which
 ;;;; expands to a call to ENSURE-CLASS.
 
+;;; Define the macro DEFGENERIC in compile-time environment C1.  We
+;;; define it a bit differently from its usual definition.  Its main
+;;; purpose is to define a generic function in the run-time
+;;; environment R2.  However, before definining it, we remove the
+;;; existing generic function if it exists.  This way, we are sure to
+;;; get a fresh generic function, as opposed to one that happened to
+;;; have been imported from the host.  We must, of course, make sure
+;;; that we execute a DEFGENERIC form for a particular generic
+;;; function exactly once, but we can do that because we completely
+;;; master the boot process.  We also put the same definition in the
+;;; compile-time environment C1 so that the compiler sees it when
+;;; subsequent forms that use this function are compiled.
+(defun define-defgeneric-phase1 (env)
+  (setf (sicl-genv:macro-function 'defgeneric env)
+	(lambda (form environment)
+	  (declare (ignore environment))
+	  `(progn (sicl-genv:fmakunbound ',(second form) ,env)
+		  (setf (sicl-genv:fdefinition ',(second form) ,env)
+			(ensure-generic-function
+			 ',(second form)
+			 :name ',(second form)
+			 :lambda-list ',(third form)))))))
+
+(defun customize-phase1 (env)
+  (define-defgeneric-phase1 env))
+
 (defun phase1 (boot)
   (let ((c1 (c1 boot))
-	(r1 (r1 boot)))
-    (ld "../CLOS/accessor-defgenerics.lisp" c1 r1)
-    (ld "../CLOS/standard-object-defclass.lisp" c1 r1)
-    (ld "../CLOS/metaobject-defclass.lisp" c1 r1)
-    (ld "../CLOS/method-defclass.lisp" c1 r1)
-    (ld "../CLOS/standard-method-defclass.lisp" c1 r1)
-    (ld "../CLOS/standard-accessor-method-defclass.lisp" c1 r1)
-    (ld "../CLOS/standard-reader-method-defclass.lisp" c1 r1)
-    (ld "../CLOS/standard-writer-method-defclass.lisp" c1 r1)
-    (ld "../CLOS/slot-definition-defclass.lisp" c1 r1)
-    (ld "../CLOS/standard-slot-definition-defclass.lisp" c1 r1)
-    (ld "../CLOS/direct-slot-definition-defclass.lisp" c1 r1)
-    (ld "../CLOS/effective-slot-definition-defclass.lisp" c1 r1)
-    (ld "../CLOS/standard-direct-slot-definition-defclass.lisp" c1 r1)
-    (ld "../CLOS/standard-effective-slot-definition-defclass.lisp" c1 r1)
-    (ld "../CLOS/specializer-defclass.lisp" c1 r1)
-    (ld "../CLOS/eql-specializer-defclass.lisp" c1 r1)
-    (ld "../CLOS/class-unique-number-defparameter.lisp" c1 r1)
-    (ld "../CLOS/class-defclass.lisp" c1 r1)
-    (ld "../CLOS/forward-referenced-class-defclass.lisp" c1 r1)
-    (ld "../CLOS/real-class-defclass.lisp" c1 r1)
-    (ld "../CLOS/regular-class-defclass.lisp" c1 r1)
-    (ld "../CLOS/standard-class-defclass.lisp" c1 r1)
-    (ld "../CLOS/funcallable-standard-class-defclass.lisp" c1 r1)
-    (ld "../CLOS/built-in-class-defclass.lisp" c1 r1)
-    (ld "../CLOS/funcallable-standard-object-defclass.lisp" c1 r1)
-    (ld "../CLOS/generic-function-defclass.lisp" c1 r1)
-    (ld "../CLOS/standard-generic-function-defclass.lisp" c1 r1)))
+	(r1 (r1 boot))
+	(r2 (r2 boot)))
+    (ld "../CLOS/accessor-defgenerics.lisp" c1 r2)
+    ;; (ld "../CLOS/standard-object-defclass.lisp" c1 r1)
+    ;; (ld "../CLOS/metaobject-defclass.lisp" c1 r1)
+    ;; (ld "../CLOS/method-defclass.lisp" c1 r1)
+    ;; (ld "../CLOS/standard-method-defclass.lisp" c1 r1)
+    ;; (ld "../CLOS/standard-accessor-method-defclass.lisp" c1 r1)
+    ;; (ld "../CLOS/standard-reader-method-defclass.lisp" c1 r1)
+    ;; (ld "../CLOS/standard-writer-method-defclass.lisp" c1 r1)
+    ;; (ld "../CLOS/slot-definition-defclass.lisp" c1 r1)
+    ;; (ld "../CLOS/standard-slot-definition-defclass.lisp" c1 r1)
+    ;; (ld "../CLOS/direct-slot-definition-defclass.lisp" c1 r1)
+    ;; (ld "../CLOS/effective-slot-definition-defclass.lisp" c1 r1)
+    ;; (ld "../CLOS/standard-direct-slot-definition-defclass.lisp" c1 r1)
+    ;; (ld "../CLOS/standard-effective-slot-definition-defclass.lisp" c1 r1)
+    ;; (ld "../CLOS/specializer-defclass.lisp" c1 r1)
+    ;; (ld "../CLOS/eql-specializer-defclass.lisp" c1 r1)
+    ;; (ld "../CLOS/class-unique-number-defparameter.lisp" c1 r1)
+    ;; (ld "../CLOS/class-defclass.lisp" c1 r1)
+    ;; (ld "../CLOS/forward-referenced-class-defclass.lisp" c1 r1)
+    ;; (ld "../CLOS/real-class-defclass.lisp" c1 r1)
+    ;; (ld "../CLOS/regular-class-defclass.lisp" c1 r1)
+    ;; (ld "../CLOS/standard-class-defclass.lisp" c1 r1)
+    ;; (ld "../CLOS/funcallable-standard-class-defclass.lisp" c1 r1)
+    ;; (ld "../CLOS/built-in-class-defclass.lisp" c1 r1)
+    ;; (ld "../CLOS/funcallable-standard-object-defclass.lisp" c1 r1)
+    ;; (ld "../CLOS/generic-function-defclass.lisp" c1 r1)
+    ;; (ld "../CLOS/standard-generic-function-defclass.lisp" c1 r1)
+    ))
