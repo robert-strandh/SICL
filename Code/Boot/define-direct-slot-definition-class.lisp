da03185d6c88e0489a566cc40a181275cebd39c8
Robert Strandh <robert.strandh@gmail.com>
2015-07-21 13:57:43 +0200

    Remove obsolete component.

--- Code/Boot/define-direct-slot-definition-class.lisp
+++
@@ -1,8 +0,0 @@
-(cl:in-package #:sicl-boot)
-
-(defun define-direct-slot-definition-class (boot)
-  (setf (sicl-genv:fdefinition 'sicl-clos:direct-slot-definition-class (r2 boot))
-	(lambda (&rest arguments)
-	  (declare (ignore arguments))
-	  (sicl-genv:find-class 'sicl-clos:standard-direct-slot-definition
-				(r1 boot)))))
