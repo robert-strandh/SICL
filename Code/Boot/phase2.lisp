94f181a4a87c4e0d510a1cf6d9b6df57189205c5
Robert Strandh <robert.strandh@gmail.com>
2015-07-24 13:18:20 +0200

    Define and call temporary function ALL-DESCENDANTS in phase 2.

--- Code/Boot/phase2.lisp
+++ Code/Boot/phase2.lisp
@@ -328,6 +328,10 @@
   (setf (sicl-genv:fdefinition 'sicl-clos::heap-instance-p env)
 	(constantly t)))
 
+(defun define-all-descendants-phase2 (env)
+  (setf (sicl-genv:fdefinition 'sicl-clos::all-descendents env)
+	(constantly t)))
+
 (defun phase2 (boot)
   (let ((r1 (r1 boot))
 	(r2 (r2 boot))
@@ -362,6 +366,7 @@
     (define-validate-superclass-phase2 r3)
     (define-check-direct-superclasses-phase2 r3)
     (define-heap-instance-p r3)
+    (define-all-descendants-phase2 r3)
     ;; (create-bridge-classes r3 r3)
     (message "End of phase 2~%")))
 
