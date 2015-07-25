c18167d578c4323048967eb3a04914a3456ec43f
Robert Strandh <robert.strandh@gmail.com>
2015-07-25 06:20:05 +0200

    Bind special variables before filling environments.

--- Code/Boot/fill.lisp
+++ Code/Boot/fill.lisp
@@ -7,4 +7,9 @@
   (phase3 boot))
 
 (defmethod initialize-instance :after ((boot boot) &key &allow-other-keys)
-  (fill-boot boot))
+  (let ((*pristine-env* (c1 boot))
+	(*phase1-mop-accessor-env* (r2 boot))
+	(*phase1-mop-class-env* (r1 boot))
+	(*phase2-mop-accessor-env* (r3 boot))
+	(*phase2-mop-class-env* (r2 boot)))
+    (fill-boot boot)))
