79010ca87aacf5a342fbcca8a9f096145dd9c620
Robert Strandh <robert.strandh@gmail.com>
2015-07-28 06:08:16 +0200

    Define a function for defining ALLOCATE-GENERAL-INSTANCE.

--- Code/Boot/phase3.lisp
+++ Code/Boot/phase3.lisp
@@ -4,6 +4,13 @@
   ((%class :initarg :class :accessor class)
    (%rack :initarg :rack :reader rack)))
 
+(defun define-allocate-general-instance (env)
+  (setf (sicl-genv:fdefinition 'sicl-clos:allocate-general-instance env)
+	(lambda (class size)
+	  (make-instance 'header
+	    :class class
+	    :rack (make-array size)))))
+
 (defun phase3 (boot)
   (let ((c (c1 boot))
 	(r (r2 boot)))
