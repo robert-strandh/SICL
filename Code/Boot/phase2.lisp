f430aab483898ba137c56b5ca16fbefe99982c21
Robert Strandh <robert.strandh@gmail.com>
2015-08-03 06:46:38 +0200

    Do not load support code for computing discriminating function in phase 2.

--- Code/Boot/phase2.lisp
+++ Code/Boot/phase2.lisp
@@ -378,7 +378,6 @@
     (define-all-descendants-phase2 r2)
     (define-reader-method-class-phase2 r2 r1)
     (define-writer-method-class-phase2 r2 r1)
-    ;; (ld "../CLOS/compute-discriminating-function-support.lisp" r3 r3)
     (define-add-method-phase2 r2 r2)
     (ld "../CLOS/class-unique-number-defparameter.lisp" r2 r2)
     (create-bridge-classes r2 r2)
