7f3a7d6f66a7eb83b06725285408b23883c857ee
Robert Strandh <robert.strandh@gmail.com>
2015-08-04 14:16:12 +0200

    Define :AROUND method on TWO-SUCCESSORS-TRANSFER.

--- Code/Cleavir/Type-inference/transfer.lisp
+++ Code/Cleavir/Type-inference/transfer.lisp
@@ -2,7 +2,7 @@
 
 (defgeneric one-successor-transfer (instruction input-bag))
 
-(defgeneric two-successors-treansfer (instruction input-bag))
+(defgeneric two-successors-transfer (instruction input-bag))
 
 (defmethod one-successor-transfer :around (instruction input-bag)
   (let* ((result (call-next-method))
@@ -21,9 +21,20 @@
 	  do (setf result (update output t result))
 	finally (return result)))
 
-(defmethod two-successors-treansfer (instruction input-bag)
+(defmethod two-successors-transfer (instruction input-bag)
   (loop with result = input-bag
 	for output in (cleavir-ir:outputs instruction)
 	when (typep output 'cleavir-ir:lexical-location)
 	  do (setf result (update output t result))
 	finally (return (values result result))))
+
+(defmethod two-successors-transfer :around (instruction input-bag)
+  (loop with results = (multiple-value-list (call-next-method))
+	for result in results
+	for successor in (cleavir-ir:successors instruction)
+	for key = (cons instruction successor)
+	for existing = (gethash key *dictionary*)
+	for filtered-result = (filter result existing)
+	unless (bag-equal filtered-result existing)
+	  do (push successor *work-list*)
+	     (setf (gethash key *dictionary*) filtered-result)))
