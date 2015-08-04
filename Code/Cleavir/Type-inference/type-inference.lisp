01e485f3f6816617057aea94b1cd332f4cad4c92
Robert Strandh <robert.strandh@gmail.com>
2015-08-04 14:54:50 +0200

    New function for computing initial dictionary.

--- Code/Cleavir/Type-inference/type-inference.lisp
+++ Code/Cleavir/Type-inference/type-inference.lisp
@@ -7,3 +7,27 @@
        (when (typep instruction 'cleavir-ir:enter-instruction)
 	 (push instruction result)))
      initial-instruction)))
+
+(defun compute-initial-dictionary (initial-instruction)
+  (flet ((variable-p (datum)
+	   (or (typep datum 'cleavir-ir:lexical-location)
+	       (typep datum 'cleavir-ir:values-location))))
+    (let ((liveness (cleavir-liveness:liveness
+		     initial-instruction
+		     #'cleavir-ir:successors
+		     #'cleavir-ir:predecessors
+		     #'cleavir-ir:inputs
+		     #'cleavir-ir:outputs
+		     #'variable-p))
+	  (result (make-hash-table :test #'equal)))
+      (cleavir-ir:map-instructions-arbitrary-order
+       (lambda (instruction)
+	 (loop for predecessor in (cleavir-ir:predecessors instruction)
+	       for key = (cons predecessor instruction)
+	       for live = (cleavir-liveness:live-before liveness instruction)
+	       do (loop for var in live
+			do (push (cons var t)
+				 (gethash (cons predecessor instruction)
+					  result)))))
+       initial-instruction)
+      result)))
