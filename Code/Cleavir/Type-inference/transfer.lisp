3b6490014251199000ab540252ed3e8b8115955b
Robert Strandh <robert.strandh@gmail.com>
2015-08-04 13:58:29 +0200

    Define generic function TWO-SUCCESSORS-TRANSFER and a default method.

--- Code/Cleavir/Type-inference/transfer.lisp
+++ Code/Cleavir/Type-inference/transfer.lisp
@@ -2,9 +2,18 @@
 
 (defgeneric one-successor-transfer (instruction input-bag))
 
+(defgeneric two-successors-treansfer (instruction input-bag))
+
 (defmethod one-successor-transfer (instruction input-bag)
   (loop with result = input-bag
 	for output in (cleavir-ir:outputs instruction)
 	when (typep output 'cleavir-ir:lexical-location)
 	  do (setf result (update output t result))
 	finally (return result)))
+
+(defmethod two-successors-treansfer (instruction input-bag)
+  (loop with result = input-bag
+	for output in (cleavir-ir:outputs instruction)
+	when (typep output 'cleavir-ir:lexical-location)
+	  do (setf result (update output t result))
+	finally (return (values result result))))
