46c8ff08f3a568dc382448ae26daf524cc63ca32
Robert Strandh <robert.strandh@gmail.com>
2015-08-04 14:40:50 +0200

    New function for computing initial work-list.

--- Code/Cleavir/Type-inference/type-inference.lisp
+++ Code/Cleavir/Type-inference/type-inference.lisp
@@ -1,1 +1,9 @@
 (cl:in-package #:cleavir-type-inference)
+
+(defun compute-initial-work-list (initial-instruction)
+  (let ((result '()))
+    (cleavir-ir:map-instructions-arbitrary-order
+     (lambda (instruction)
+       (when (typep instruction 'cleavir-ir:enter-instruction)
+	 (push instruction result)))
+     initial-instruction)))
