011a1893c7bdc95289b1742ebe3787056006a257
Robert Strandh <robert.strandh@gmail.com>
2015-08-04 13:56:18 +0200

    Rename in preparation for a TWO-SUCCESSORS-TRANSFER function.

--- Code/Cleavir/Type-inference/transfer.lisp
+++ Code/Cleavir/Type-inference/transfer.lisp
@@ -1,8 +1,8 @@
 (cl:in-package #:cleavir-type-inference)
 
-(defgeneric transfer (instruction input-bag))
+(defgeneric one-successor-transfer (instruction input-bag))
 
-(defmethod transfer (instruction input-bag)
+(defmethod one-successor-transfer (instruction input-bag)
   (loop with result = input-bag
 	for output in (cleavir-ir:outputs instruction)
 	when (typep output 'cleavir-ir:lexical-location)
