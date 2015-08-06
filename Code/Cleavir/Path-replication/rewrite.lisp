4a0a9d8ef92d45347b896ce277aa2dc2725a7dd7
Robert Strandh <robert.strandh@gmail.com>
2015-08-06 09:44:55 +0200

    New rewrite rule for moving instruction past its predecessors.

--- Code/Cleavir/Path-replication/rewrite.lisp
+++ Code/Cleavir/Path-replication/rewrite.lisp
@@ -3,7 +3,7 @@
 ;;; This transformation is used as a preparation for moving
 ;;; INSTRUCTION past its predecessors, because in order to do that,
 ;;; INSTRUCTION must have a single predecessor.  This function
-;;; replacates INSTRUCTION for each predecessor P.  It does that by
+;;; replicates INSTRUCTION for each predecessor P.  It does that by
 ;;; creating a replica RP of INSTRUCTION, then it replaces INSTRUCTION
 ;;; with RP in the successors of P.  Finally, it replaces INSTRUCTION
 ;;; by all the RPs in each successor of INSTRUCTION.
@@ -34,3 +34,24 @@
 			 else
 			   collect predecessor)))
     copies))
+
+;;; This transformation moves INSTRUCTION so that it precedes its
+;;; current predecessor.  INSTRUCTION must have a single predecessor.
+;;; It accomplishes the transformation by replicating its current
+;;; predecessor between itself and each of its current successors.
+(defun move-instruction (instruction)
+  (assert (= (length (cleavir-ir:predecessors instruction)) 1))
+  (loop with predecessor = (first (cleavir-ir:predecessors instruction))
+	for successors = (cleavir-ir:successors predecessor)
+	for remaining on (cleavir-ir:successors instruction)
+	for successor = (first remaining)
+	for copy = (make-instance (class-of predecessor)
+		     :predecessors (list instruction)
+		     :successors (substitute successor instruction successors)
+		     :inputs (cleavir-ir:inputs predecessor)
+		     :outputs (cleavir-ir:outputs predecessor))
+	do (setf (first remaining) copy)
+	   (nsubstitute copy instruction (cleavir-ir:predecessors successor))
+	   (loop for pred in (cleavir-ir:predecessors predecessor)
+		 do (nsubstitute instruction predecessor
+				 (cleavir-ir:successors pred)))))
