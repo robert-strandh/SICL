709527b195080bde5cf1f37b8c46c0c03ca7350f
Robert Strandh <robert.strandh@gmail.com>
2015-08-06 15:05:50 +0200

    Color new successors of moved instruction.

--- Code/Cleavir/Path-replication/path-replication.lisp
+++ Code/Cleavir/Path-replication/path-replication.lisp
@@ -1,5 +1,11 @@
 (cl:in-package #:cleavir-path-replication)
 
+(defun move-and-color (instruction red-instructions blue-instructions)
+  (move-instruction instruction)
+  (let ((successors (cleavir-ir:successors instruction)))
+    (setf (gethash (first successors) red-instructions) t)
+    (setf (gethash (second successors) blue-instructions) t)))
+
 ;;; Replicate all the instructions between DOMINATOR and INSTRUCTION.
 ;;; This transformation is accomplished by moving INSTRUCTION so that
 ;;; it precedes its current predecessors until it is precedes
@@ -12,7 +18,9 @@
 ;;; writes a lexical location that is an input of INSTRUCTION, then
 ;;; this transformation will also fail.
 (defun path-replication (instruction dominator)
-  (loop with work-list = (list instruction)
+  (loop with red-instructions = (make-hash-table :test #'eq)
+	with blue-instructions = (make-hash-table :test #'eq)
+	with work-list = (list instruction)
 	until (null work-list)
 	do (let* ((instruction (pop work-list))
 		  (predecessors (cleavir-ir:predecessors instruction)))
@@ -24,11 +32,15 @@
 		   ((eq (first predecessors) dominator)
 		    ;; If the sole predecessor is DOMINATOR, then we
 		    ;; move INSTRUCTION, and we are done.
-		    (move-instruction instruction))
+		    (move-and-color instruction
+				    red-instructions
+				    blue-instructions))
 		   (t
 		    ;; If the sole predecessor is some instruction
 		    ;; other than DOMINATOR, then we move INSTRUCTION,
 		    ;; but we put it back on WORK-LIST so that it is
 		    ;; processed again.
 		    (push instruction work-list)
-		    (move-instruction instruction))))))
+		    (move-and-color instruction
+				    red-instructions
+				    blue-instructions))))))
