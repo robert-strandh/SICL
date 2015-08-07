b939f17805790abd48807555a4ec268e5502e7ed
Robert Strandh <robert.strandh@gmail.com>
2015-08-07 07:27:00 +0200

    Added comments explaining the transformation.

--- Code/Cleavir/HIR-transformations/eliminate-load-time-value-inputs.lisp
+++ Code/Cleavir/HIR-transformations/eliminate-load-time-value-inputs.lisp
@@ -1,5 +1,23 @@
 (cl:in-package #:cleavir-hir-transformations)
 
+;;;; This transformation replaces every LOAD-TIME-VALUE-INPUT in the
+;;;; program by a lexical location.  A form is appended to the forms
+;;;; of the TOP-LEVEL-ENTER-INSTRUCTION for each LOAD-TIME-VALUE-INPUT
+;;;; and the lexical location is added to the outputs of the initial
+;;;; TOP-LEVEL-ENTER-INSTRUCTION.
+;;;;
+;;;; If the LOAD-TIME-VALUE-INPUT to be replaced has a form of type
+;;;; (QUOTE constant), then CONVERT-CONSTANT-TO-IMMEDIATE is called,
+;;;; passing the constant and the SYSTEM as arguments.  If that call
+;;;; returns an integer, then instead of adding a form to the initial
+;;;; instruction, this transformation replaces the input by an
+;;;; immediate input instead.  If that call returns NIL, the normal
+;;;; transformation is performed.
+;;;;
+;;;; Client code can add a method on CONVERT-CONSTANT-TO-IMMEDIATE
+;;;; that should then specialize on the second parameter, i.e. the
+;;;; SYSTEM.
+
 (defgeneric convert-constant-to-immediate (constant system))
 
 (defmethod convert-constant-to-immediate (constant system)
@@ -32,6 +50,7 @@
 	      (replace-load-time-value-with-immediate immediate input)))
 	(replace-load-time-value-with-lexical initial-instruction input))))
 
+;;; Main entry point.
 (defun eliminate-load-time-value-inputs (initial-instruction system)
   (assert (typep initial-instruction 'cleavir-ir:top-level-enter-instruction))
   (cleavir-ir:map-instructions-arbitrary-order
