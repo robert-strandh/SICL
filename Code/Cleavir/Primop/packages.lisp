a288cc25e55592c175f4f5ba8994312037bcaa51
Robert Strandh <robert.strandh@gmail.com>
2015-08-10 08:43:54 +0200

    Export symbols related to SHORT-FLOAT arithmetic.

--- Code/Cleavir/Primop/packages.lisp
+++ Code/Cleavir/Primop/packages.lisp
@@ -9,6 +9,18 @@
    #:car #:cdr #:rplaca #:rplacd
    #:fixnum-arithmetic #:fixnum-+ #:fixnum--
    #:fixnum-< #:fixnum-<= #:fixnum-> #:fixnum->= #:fixnum-=
+   #:short-float-add
+   #:short-float-sub
+   #:short-float-mul
+   #:short-float-div
+   #:short-float-less
+   #:short-float-not-greater
+   #:short-float-greater
+   #:short-float-not-less
+   #:short-float-equal
+   #:short-float-sin
+   #:short-float-cos
+   #:short-float-sqrt
    #:slot-read #:slot-write
    #:aref #:aset
    #:call-with-variable-bound
