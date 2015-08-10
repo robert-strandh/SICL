3d06a871ec38542579556650a4886568bf8801d9
Robert Strandh <robert.strandh@gmail.com>
2015-08-10 08:56:15 +0200

    Use more practical naming for binary operators.

--- Code/Arithmetic/arithmetic.lisp
+++ Code/Arithmetic/arithmetic.lisp
@@ -4,22 +4,22 @@
   (cond ((null args) 0)
 	;; FIXME: check that we have a number
 	((null (cdr args)) (car args))
-	(t (apply #'+ (binary-+ (car args) (cadr args)) (cddr args)))))
+	(t (apply #'+ (binary-add (car args) (cadr args)) (cddr args)))))
  
 (defun - (x &rest args)
   (cond ((null args) (negate x))
-	((null (cdr args)) (binary-- x (car args)))
-	(t (apply #'- (binary-- x (car args)) (cdr args)))))
+	((null (cdr args)) (binary-sub x (car args)))
+	(t (apply #'- (binary-sub x (car args)) (cdr args)))))
 
 (defun * (&rest args)
   (cond ((null args) 1)
 	;; FIXME: check that we have a number
 	((null (cdr args)) (car args))
-	(t (apply #'* (binary-* (car args) (cadr args)) (cddr args)))))
+	(t (apply #'* (binary-mul (car args) (cadr args)) (cddr args)))))
 
 (defun / (x &rest args)
   (cond ((null args) (invert x))
-	((null (cdr args)) (binary-/ x (car args)))
-	(t (apply #'/ (binary-/ x (car args)) (cdr args)))))
+	((null (cdr args)) (binary-div x (car args)))
+	(t (apply #'/ (binary-div x (car args)) (cdr args)))))
  
 
