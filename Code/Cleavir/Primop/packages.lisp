d806daccd9fd81cf528c20fa43ce5710cd7934e4
Robert Strandh <robert.strandh@gmail.com>
2015-08-10 08:45:32 +0200

    Export symbols related to SINGLE-FLOAT arithmetic.

--- Code/Cleavir/Primop/packages.lisp
+++ Code/Cleavir/Primop/packages.lisp
@@ -21,6 +21,18 @@
    #:short-float-sin
    #:short-float-cos
    #:short-float-sqrt
+   #:single-float-add
+   #:single-float-sub
+   #:single-float-mul
+   #:single-float-div
+   #:single-float-less
+   #:single-float-not-greater
+   #:single-float-greater
+   #:single-float-not-less
+   #:single-float-equal
+   #:single-float-sin
+   #:single-float-cos
+   #:single-float-sqrt
    #:slot-read #:slot-write
    #:aref #:aset
    #:call-with-variable-bound
