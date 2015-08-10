787b14298467bdd2f658bf09ca554c6c992052b7
Robert Strandh <robert.strandh@gmail.com>
2015-08-10 08:58:03 +0200

    Use new naming in compiler macros for arithmetic functions.

--- Code/Arithmetic/compiler-macros.lisp
+++ Code/Arithmetic/compiler-macros.lisp
@@ -4,59 +4,59 @@
   (cond ((null args) 0)
 	;; FIXME: check that we have a number
 	((null (cdr args)) (car args))
-	((null (cddr args)) `(binary-+ ,(car args) ,(cadr args)))
-	(t `(binary-+ (+ ,@(butlast args)) ,(car (last args))))))
+	((null (cddr args)) `(binary-add ,(car args) ,(cadr args)))
+	(t `(binary-add (+ ,@(butlast args)) ,(car (last args))))))
 
 ;;; FIXME: do this better by not having a required argument
 ;;; and instead reporting a compilation error when no
 ;;; arguments are given
 (define-compiler-macro - (x &rest args)
   (cond ((null args) `(negate ,x))
-	((null (cdr args)) `(binary-- ,x ,(car args)))
-	(t `(binary-- (- ,@(butlast args)) ,(car (last args))))))
+	((null (cdr args)) `(binary-sub ,x ,(car args)))
+	(t `(binary-sub (- ,@(butlast args)) ,(car (last args))))))
 
 (define-compiler-macro * (&rest args)
   (cond ((null args) 0)
 	;; FIXME: check that we have a number
 	((null (cdr args)) (car args))
-	((null (cddr args)) `(binary-* ,(car args) ,(cadr args)))
-	(t `(binary-* (* ,@(butlast args)) ,(car (last args))))))
+	((null (cddr args)) `(binary-mul ,(car args) ,(cadr args)))
+	(t `(binary-mul (* ,@(butlast args)) ,(car (last args))))))
 
 ;;; FIXME: do this better by not having a required argument
 ;;; and instead reporting a compilation error when no
 ;;; arguments are given
 (define-compiler-macro / (x &rest args)
   (cond ((null args) `(invert ,x))
-	((null (cdr args)) `(binary-/ ,x ,(car args)))
-	(t `(binary-/ (/ ,@(butlast args)) ,(car (last args))))))
+	((null (cdr args)) `(binary-div ,x ,(car args)))
+	(t `(binary-div (/ ,@(butlast args)) ,(car (last args))))))
 
 (define-compiler-macro < (x &rest args)
   (if (null args)
       t
-      `(and (binary-< ,x ,(car args))
+      `(and (binary-less ,x ,(car args))
 	    (< ,@args))))
 
 (define-compiler-macro <= (x &rest args)
   (if (null args)
       t
-      `(and (binary-<= ,x ,(car args))
+      `(and (binary-not-greater ,x ,(car args))
 	    (<= ,@args))))
 
 (define-compiler-macro > (x &rest args)
   (if (null args)
       t
-      `(and (binary-> ,x ,(car args))
+      `(and (binary-greater ,x ,(car args))
 	    (> ,@args))))
 
 (define-compiler-macro >= (x &rest args)
   (if (null args)
       t
-      `(and (binary->= ,x ,(car args))
+      `(and (binary-not-less ,x ,(car args))
 	    (>= ,@args))))
 
 (define-compiler-macro = (x &rest args)
   (if (null args)
       t
-      `(and (binary-= ,x ,(car args))
+      `(and (binary-equal ,x ,(car args))
 	    (= ,@args))))
 
