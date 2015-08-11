28bc20f38a92d92fda552bd074ca66a45041372c
Robert Strandh <robert.strandh@gmail.com>
2015-08-11 10:18:30 +0200

    New utility functions for testing equality of sets, partitions, equivalence classes.

--- Code/Cleavir/HIR-transformations/Equivalent-lexical-locations/equivalent-lexical-locations.lisp
+++ Code/Cleavir/HIR-transformations/Equivalent-lexical-locations/equivalent-lexical-locations.lisp
@@ -1,1 +1,11 @@
 (cl:in-package #:cleavir-equivalent-lexical-locations)
+
+(defun set-equality (set1 set2 test)
+  (and (null (set-difference set1 set2 :test test))
+       (null (set-difference set2 set1 :test test))))
+
+(defun class-equality (class1 class2)
+  (set-equality class1 class2 #'eq))
+
+(defun partition-equality (partition1 partition2)
+  (set-equality partition1 partition2 #'class-equality))
