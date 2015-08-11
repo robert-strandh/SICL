4b2fab459b31f8ddd21650a22a8834a0df5ea8a8
Robert Strandh <robert.strandh@gmail.com>
2015-08-11 10:18:59 +0200

    New function for computing a new partition on MEET.

--- Code/Cleavir/HIR-transformations/Equivalent-lexical-locations/equivalent-lexical-locations.lisp
+++ Code/Cleavir/HIR-transformations/Equivalent-lexical-locations/equivalent-lexical-locations.lisp
@@ -9,3 +9,18 @@
 
 (defun partition-equality (partition1 partition2)
   (set-equality partition1 partition2 #'class-equality))
+
+(defun update-for-meet (partition defined used)
+  (let* ((dclass (find defined partition :test #'member))
+	 (intermediate (cond ((null dclass)
+			      partition)
+			     ((= (length dclass) 2)
+			      (remove dclass partition :test #'eq))
+			     (t
+			      (cons (remove defined dclass :test #'eq)
+				    (remove dclass partition :test #'eq)))))
+	 (uclass (find used intermediate :test #'member)))
+    (if (null uclass)
+	(cons (list defined used) intermediate)
+	(cons (cons defined uclass)
+	      (remove uclass intermediate :test #'eq)))))
