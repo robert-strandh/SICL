6a25ca16748630aa641de86f4c8feebe29a44713
Robert Strandh <robert.strandh@gmail.com>
2015-08-13 06:53:19 +0200

    New component for measuring performance of module.

---
+++ Code/Cleavir/HIR-transformations/Equivalent-lexical-locations/meter.lisp
@@ -0,0 +1,6 @@
+(cl:in-package #:cleavir-equivalent-lexical-locations)
+
+(defparameter *equivalent-lexical-locations-meter*
+  (make-instance 'cleavir-meter:size-meter
+    :name "EQUIVALENT-LEXICAL-LOCATIONS-METER"))
+
