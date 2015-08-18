3a39585437fb15f1b7e593a85f43c3ea412b6682
Robert Strandh <robert.strandh@gmail.com>
2015-08-18 11:02:01 +0200

    Remove obsolete component.

--- Code/Boot/Phase2-obsolete/compile.lisp
+++
@@ -1,5 +0,0 @@
-(cl:in-package #:sicl-boot-phase2)
-
-(defun compile (dummy lambda-expression)
-  (declare (ignore dummy lambda-expression))
-  (sicl-boot-phase1:allocate-built-in-instance (find-bridge-class 'function)))
