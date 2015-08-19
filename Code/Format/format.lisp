f74b314b3cc2ba9ba7ac65df5f2b58de2059072e
Robert Strandh <robert.strandh@gmail.com>
2015-08-19 09:40:34 +0200

    Extracted definition of conditions to a separate component.

--- Code/Format/format.lisp
+++ Code/Format/format.lisp
@@ -48,91 +48,6 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;
-;;; Conditions
-
-;;; The base class of all format errors.
-
-(define-condition format-error (error) ())
-
-;;; This is the base class for all parse errors, i.e. error found
-;;; before we were able to construct a directive object at all.  All
-;;; we know is where the directive start (tilde-position), and the
-;;; control string.
-(define-condition directive-parse-error (format-error)
-  ((%control-string :initarg :control-string :reader control-string)
-   (%tilde-position :initarg :tilde-position :reader tilde-position)))
-
-(defun report-control-string-and-directive-start-position
-    (condition stream)
-  (format stream
-	  "In the control-string \"~a\",~%~
-           in the directive that starts at position ~a,~%"
-	  (control-string condition)
-	  (tilde-position condition)))
-
-(define-condition end-of-control-string-error (directive-parse-error)
-  ((%why :initarg :why :reader why))
-  (:report (lambda (condition stream)
-	     (report-control-string-and-directive-start-position condition stream)
-	     (format stream
-		     "~a, but reached the end of the control string"
-		     (why condition)))))
-
-(define-condition found-something-else-error (directive-parse-error)
-  ((%index :initarg :index :reader index)))
-
-(define-condition expected-integer-error (found-something-else-error)
-  ()
-  (:report
-   (lambda (condition stream)
-     (report-control-string-and-directive-start-position condition stream)
-     (format stream
-	     "expected an integer at index ~a,~% but found the character `~a' instead"
-	     (index condition)
-	     (char (control-string condition) (index condition))))))
-
-(define-condition expected-parameter-start (found-something-else-error)
-  ()
-  (:report
-   (lambda (condition stream)
-     (report-control-string-and-directive-start-position condition stream)
-     (format stream
-	     "expected one of ', +, -, or a decimal digit at index ~a,~%~
-              but found the character `~a' instead"
-	     (index condition)
-	     (char (control-string condition) (index condition))))))
-
-(define-condition two-identical-modifiers (found-something-else-error)
-  ()
-  (:report
-   (lambda (condition stream)
-     (report-control-string-and-directive-start-position condition stream)
-     (format stream
-	     "found two identical modifiers `~a' at index ~a"
-	     (char (control-string condition) (index condition))
-	     (index condition)))))
-
-(define-condition more-than-two-modifiers (found-something-else-error)
-  ()
-  (:report
-   (lambda (condition stream)
-     (report-control-string-and-directive-start-position condition stream)
-     (format stream
-	     "found a sequence of more than two modifiers at index ~a"
-	     (index condition)))))
-
-(define-condition unknown-format-directive (found-something-else-error)
-  ()
-  (:report
-   (lambda (condition stream)
-     (report-control-string-and-directive-start-position condition stream)
-     (format stream
-	     "unknown format directive `~a' at index ~a"
-	     (char (control-string condition) (index condition))
-	     (index condition)))))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;;
 ;;; Parsing a control string
 
 ;;; Parse a parameter.  This function is called only if a parameter
@@ -350,33 +265,6 @@
 ;;; by an eql-specialized method on this generic function.
 (defgeneric directive-subclass-name (directive-character directive))
 
-;;; The base class of all syntax errors.  When one of these is
-;;; signaled, we have correctly parsed the directive, so we know where
-;;; in the control string it starts and ends.  
-(define-condition directive-syntax-error (format-error)
-  ((%directive :initarg :directive :reader directive)))
-
-(defun report-control-string-and-directive-position (condition stream)
-  (with-accessors ((control-string control-string)
-		   (start start)
-		   (end end))
-    (directive condition)
-    (format stream
-	    "In the control-string \"~a\",~%in the directive that ~
-             starts at position ~a and ends at position ~a,~%"
-	    control-string
-	    start
-	    end)))
-
-(define-condition unknown-directive-character (directive-syntax-error)
-  ()
-  (:report
-   (lambda (condition stream)
-     (report-control-string-and-directive-position condition stream)
-     (format stream
-	     "unknown directive character: ~c~%"
-	     (directive-character (directive condition))))))
-
 ;;; For the default case, signal an error
 (defmethod directive-subclass-name (directive-character directive)
   (error 'unknown-directive-character
@@ -422,59 +310,6 @@
 ;;;
 ;;; Checking syntax, interpreting, and compiling directives.
 
-;;; More conditions.
-
-;;; FIXME, report the index
-(define-condition directive-takes-no-modifiers (directive-syntax-error)
-  ()
-  (:report
-   (lambda (condition stream)
-     (report-control-string-and-directive-position condition stream)
-     (format stream
-	     "found a modifier at index,~%but this ~
-              directive takes no modifiers"))))
-
-;;; FIXME, report the index
-(define-condition directive-takes-only-colon (directive-syntax-error)
-  ()
-  (:report
-   (lambda (condition stream)
-     (report-control-string-and-directive-position condition stream)
-     (format stream
-	     "found an at-sign at index,~%but this directive ~
-              takes only the colon modifier"))))
-
-;;; FIXME, report the index
-(define-condition directive-takes-only-at-sign (directive-syntax-error)
-  ()
-  (:report
-   (lambda (condition stream)
-     (report-control-string-and-directive-position condition stream)
-     (format stream
-	     "found a colon at index,~%but this directive ~
-              takes only the at-sign modifier"))))
-
-;;; FIXME, report the index
-(define-condition directive-takes-at-most-one-modifier (directive-syntax-error)
-  ()
-  (:report
-   (lambda (condition stream)
-     (report-control-string-and-directive-position condition stream)
-     (format stream
-	     "found both modifiers,~%but this directive ~
-              takes at most one modifier"))))
-
-(define-condition too-many-parameters (directive-syntax-error)
-  ((%at-most-how-many :initarg :at-most-how-many :reader at-most-how-many)
-   (%how-many-found :initarg :how-many-found :reader how-many-found))
-  (:report
-   (lambda (condition stream)
-     (report-control-string-and-directive-position condition stream)
-     (format stream
-	     "the directive takes at most ~a parameters,~%but ~a found"
-	     (at-most-how-many condition)
-	     (how-many-found condition)))))
-
 ;;; For certain common types used by FORMAT, return a string
 ;;; explaining in English what the type means.  For other
 ;;; types, return a string "an object of type <type>"
@@ -495,15 +330,6 @@
 	   (3 (format nil "an integer between ~d and ~d" (second type) (third type)))
 	   (t (format nil "an object of type ~s" type))))
 	(t (format nil "an object of type ~s" type))))
-
-(define-condition parameter-type-error (directive-syntax-error type-error)
-  ()
-  (:report
-   (lambda (condition stream)
-     (format stream
-	     "~a was required as parameter, but ~a was found"
-	     (type-name (type-error-expected-type condition))
-	     (type-error-datum condition)))))
 
 ;;; Specialize a directive according to a particular directive
 ;;; character.
@@ -556,28 +382,6 @@
 	       :directive directive
 	       :at-most-how-many (length parameter-specs)
 	       :how-many-found (length given-parameters))))))
-
-;;; Runtime conditions and macros
-
-(define-condition format-runtime-error (format-error) ())
-
-(define-condition no-more-arguments (format-runtime-error)
-  ;; maybe add the number of the argument that
-  ;; was accessed?
-  ()
-  (:report
-   (lambda (condition stream)
-     (declare (ignore condition))
-     (format stream "an attempt was made to access more arguments than available"))))
-
-(define-condition argument-type-error (format-runtime-error type-error)
-  ()
-  (:report
-   (lambda (condition stream)
-     (format stream
-	     "~a was required as argument, but ~a was found"
-	     (type-name (type-error-expected-type condition))
-	     (type-error-datum condition)))))
 
 ;;; Runtime environment
 
@@ -1486,34 +1290,6 @@
     ()
   (%function-name :accessor function-name))
 
-(define-condition too-many-package-markers (directive-syntax-error)
-  ()
-  (:report
-   (lambda (condition stream)
-     (report-control-string-and-directive-position condition stream)
-     (format stream "the function name contains too many package markers"))))
-
-(define-condition no-such-package (directive-syntax-error)
-  ()
-  (:report
-   (lambda (condition stream)
-     (report-control-string-and-directive-position condition stream)
-     (format stream "the named package does not exist"))))
-
-(define-condition no-such-symbol (directive-syntax-error)
-  ()
-  (:report
-   (lambda (condition stream)
-     (report-control-string-and-directive-position condition stream)
-     (format stream "a symbol with that name does not exist"))))
-
-(define-condition symbol-not-external (directive-syntax-error)
-  ()
-  (:report
-   (lambda (condition stream)
-     (report-control-string-and-directive-position condition stream)
-     (format stream "the symbol is not external in the package"))))
-
 (defmethod check-directive-syntax progn ((directive call-function-directive))
   ;; Check that there is at most one package marker in the function name.
   ;; Also, compute a symbol from the name.
@@ -1691,16 +1467,6 @@
 (define-directive #\* go-to-directive (named-parameters-directive at-most-one-modifier-mixin)
     ((param :type (integer 0))))
 
-(define-condition go-to-out-of-bounds (format-runtime-error)
-  ((%what-argument :initarg :what-argument :reader what-argument)
-   (%max-arguments :initarg :max-arguments :reader max-arguments))
-  (:report
-   (lambda (condition stream)
-     (format stream "an attempt was made to go to argument number ~d ~
-                     instead of one between 0 and ~d"
-	     (what-argument condition)
-	     (max-arguments condition)))))
-
 (define-format-directive-interpreter go-to-directive
   (cond (colonp
 	 ;; Back up in the list of arguments.
@@ -1778,53 +1544,6 @@
     ((param :type integer))
   (%clauses :accessor clauses)
   (%last-clause-is-default-p :initform nil :accessor last-clause-is-default-p))
-
-(define-condition modifier-and-parameter (directive-syntax-error)
-  ()
-  (:report
-   (lambda (condition stream)
-     (declare (ignore condition))
-     (format stream "a parameter can be used only of there are no modifiers"))))
-
-(define-condition illegal-clause-separators (directive-syntax-error)
-  ()
-  (:report
-   (lambda (condition stream)
-     (declare (ignore condition))
-     (format stream "at most the last clause separator can have ~
-                     a `:' modifier"))))
-
-(define-condition clause-separator-with-colon-modifier-not-allowed
-    (directive-syntax-error)
-  ()
-  (:report
-   (lambda (condition stream)
-     (declare (ignore condition))
-     (format stream "a default clause is incompatible with modifiers"))))
-
-(define-condition at-least-one-item-required (directive-syntax-error)
-  ()
-  (:report
-   (lambda (condition stream)
-     (declare (ignore condition))
-     (format stream "there must be at least one clause in a ~
-                     conditional directive"))))
-
-(define-condition colon-modifier-requires-two-clauses
-    (directive-syntax-error)
-  ()
-  (:report
-   (lambda (condition stream)
-     (declare (ignore condition))
-     (format stream "a colon modifier requires two clauses"))))
-
-(define-condition at-sign-modifier-requires-one-clause
-    (directive-syntax-error)
-  ()
-  (:report
-   (lambda (condition stream)
-     (declare (ignore condition))
-     (format stream "an at-sign modifier requires a single clause"))))
 
 (defmethod check-directive-syntax progn ((directive conditional-directive))
   ;; Check that, if a parameter is given, then there are
@@ -2279,17 +1998,6 @@
      (p2 :type integer)
      (p3 :type integer)))
 
-(define-condition parameter-omitted (directive-syntax-error)
-  ((%parameter1 :initarg :parameter1 :reader parameter1)
-   (%parameter2 :initarg :parameter2 :reader parameter2))
-  (:report
-   (lambda (condition stream)
-     (format stream
-	     "parameter number ~d was given, but parameter ~d ~
-              was omitted, which is not allowed"
-	     (parameter1 condition)
-	     (parameter2 condition)))))
-
 (defmethod check-directive-syntax progn
   ((directive circumflex-directive))
   (let ((parameters (given-parameters directive)))
@@ -2365,22 +2073,6 @@
 ;;;
 ;;; The main entry point
 
-(define-condition unmatched-directive (directive-syntax-error)
-  ()
-  (:report
-   (lambda (condition stream)
-     (report-control-string-and-directive-position condition stream)
-     (format stream
-	     "there is no matching directive"))))
-
-(define-condition nesting-violation (directive-syntax-error)
-  ()
-  (:report
-   (lambda (condition stream)
-     (report-control-string-and-directive-position condition stream)
-     (format stream
-	     "the directive is not nested properly"))))
-
 (defun structure-items (items end)
   (loop with result = '()
 	with first = (car items)
@@ -2429,14 +2121,6 @@
 		    (specialize-directive item)
 		    (check-directive-syntax item)
 		    (push item result))))))
-
-(define-condition invalid-destination (format-error)
-  ((%destination :initarg :destination :reader destination))
-  (:report
-   (lambda (condition stream)
-     (format stream
-	     "the object ~s is not a valid destination for a format operation"
-	     (destination condition)))))
 
 (defun interpret-items (items)
   (loop for item across items
