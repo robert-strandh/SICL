15dc5137816ab0db0c279c199f720505e65a0d2a
Robert Strandh <robert.strandh@gmail.com>
2015-08-21 09:09:23 +0200

    Extracted package definition to a separate component.

--- Code/Hash-tables/hash-tables.lisp
+++ Code/Hash-tables/hash-tables.lisp
@@ -1,15 +1,3 @@
-(defpackage #:sicl-hash-tables
-  (:use #:common-lisp)
-  ;; Shadow these for now.  Ultimately, import them with
-  ;; the rest of the CL package. 
-  (:shadow #:hash-table
-           #:make-hash-table #:hash-table-p
-           #:hash-table-count #:hash-table-rehash-threshold #:hash-table-rehash-size
-           #:hash-table-size #:hash-table-test
-           #:puthash #:gethash #:remhash #:clrhash
-           #:with-hash-table-iterator
-           #:maphash))
-
 (in-package #:sicl-hash-tables)
 
 (defstruct (hash-table (:constructor %make-hash-table)
---
+++ Code/Hash-tables/packages.lisp
@@ -0,0 +1,13 @@
+(cl:in-package #:common-lisp-user)
+
+(defpackage #:sicl-hash-tables
+  (:use #:common-lisp)
+  ;; Shadow these for now.  Ultimately, import them with
+  ;; the rest of the CL package. 
+  (:shadow #:hash-table
+           #:make-hash-table #:hash-table-p
+           #:hash-table-count #:hash-table-rehash-threshold #:hash-table-rehash-size
+           #:hash-table-size #:hash-table-test
+           #:puthash #:gethash #:remhash #:clrhash
+           #:with-hash-table-iterator
+           #:maphash))
