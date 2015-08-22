39341382b53d35459c4ef38666972f63baa937e2
Robert Strandh <robert.strandh@gmail.com>
2015-08-22 10:27:54 +0200

    Define special variable for holding constant designators.

--- Code/Cleavir/HIR-transformations/Simple-value-numbering/simple-value-numbering.lisp
+++ Code/Cleavir/HIR-transformations/Simple-value-numbering/simple-value-numbering.lisp
@@ -2,3 +2,9 @@
 
 ;;; We use instances of this class to designate values.
 (defclass value-designator () ())
+
+;;; This variable holds and EQ hash table mapping constants to value
+;;; designators.
+(defvar *constant-designators*)
+
+;;  LocalWords:  designator designators
