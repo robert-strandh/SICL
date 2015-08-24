ae838bca8313200c62d5287dd637cbee691a6bec
Robert Strandh <robert.strandh@gmail.com>
2015-08-24 06:19:50 +0200

    New function for adding an eqivalence to a partition.

--- Code/Cleavir/HIR-transformations/Simple-value-numbering/simple-value-numbering.lisp
+++ Code/Cleavir/HIR-transformations/Simple-value-numbering/simple-value-numbering.lisp
@@ -18,6 +18,10 @@
 (defun remove-location (location partition)
   (remove location partition :key #'car :test #'eq))
 
+(defun add-equivalence (location1 location2 partition)
+  (cons (cons location1 (cdr (assoc location2 partition :test #'eq)))
+	partition))
+
 (defun simple-value-numbering (initial-instruction)
   (declare (ignore initial-instruction))
   (let ((*constant-designators* (make-hash-table :test #'eq)))))
