c35755ceede0c5f96cabb6d4d9d78fca881328fd
Robert Strandh <robert.strandh@gmail.com>
2015-08-24 10:21:54 +0200

    Update PARTITION-INTERSECTION to account for new representation of entries.

--- Code/Cleavir/HIR-transformations/Simple-value-numbering/simple-value-numbering.lisp
+++ Code/Cleavir/HIR-transformations/Simple-value-numbering/simple-value-numbering.lisp
@@ -45,28 +45,29 @@
 
 ;;; Compute the intersection of two partitions.
 ;;;
-;;; If a location is present in a pair in only one of the partitions,
-;;; then keep that pair in the intersection.  If a location is present
-;;; in a pair in both partitions, and the two pairs have the same
-;;; value designator, then keep a single copy in the intersection.
-;;; Otherwise, i.e., if the location is present in a pair in both
-;;; partitions but with different value designators, then keep a pair
-;;; with the location in the result, but with NIL in place of the
-;;; value designator.
+;;; If a location is present in an entry in only one of the
+;;; partitions, then keep that entry in the intersection.  If a
+;;; location is present in an entry in both partitions, and the two
+;;; entries have the same value designator, then keep a single copy in
+;;; the intersection.  Otherwise, i.e., if the location is present in
+;;; an entry in both partitions but with different value designators,
+;;; then keep an entry with the location in the result, but with NIL in
+;;; place of the value designator.
 (defun partition-intersection (partition1 partition2)
   (let ((result partition1))
-    (loop for pair2 in partition2
-	  for (location . designator2) = pair2
-	  for pair1 = (assoc location result :test #'eq)
-	  for designator1 = (cdr pair1)
-	  do (cond ((null pair1)
-		    (push pair2 result))
-		   ((eq designator1 designator2)
-		    nil)
-		   (t
-		    (setf result
-			  (cons (cons location nil)
-				(remove pair1 result :test #'eq))))))
+    (loop for entry2 in partition2
+	  for location = (location entry2)
+	  for designator2 = (value entry2)
+	  for entry1 = (find location result :key #'location :test #'eq)
+	  do (if (null entry1)
+		 (push entry2 result)
+		 (let ((designator1 (value entry1)))
+		   (if (and (eq designator1 designator2)
+			    (eq (new-p entry1) (new-p entry2)))
+		       nil
+		       (setf result
+			     (cons (new-value location)
+				   (remove entry1 result :test #'eq)))))))
     result))
 
 ;;; Keep a pair only if the location is live.
