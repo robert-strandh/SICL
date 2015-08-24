8eb47cb7e18e9921c20d22f03ad26f184e68e862
Robert Strandh <robert.strandh@gmail.com>
2015-08-24 06:14:38 +0200

    Remove unique number.  We will use a different technique.

--- Code/Cleavir/HIR-transformations/Simple-value-numbering/simple-value-numbering.lisp
+++ Code/Cleavir/HIR-transformations/Simple-value-numbering/simple-value-numbering.lisp
@@ -1,16 +1,8 @@
 (cl:in-package #:cleavir-simple-value-numbering)
 
-;;; We attribute a unique number to each value designator.  That way,
-;;; we can keep the value designators in a partition sorted by the
-;;; unique number which makes operations on partitions faster.
-;;;
-;;; This variable should be bound to some small integer at the
-;;; beginning of processing.
-(defvar *unique-number*)
-
 ;;; We use instances of this class to designate values.
 (defclass value-designator ()
-  ((%unique-number :initform (incf *unique-number*) :reader unique-number)))
+  ())
 
 ;;; This variable holds and EQ hash table mapping constants to value
 ;;; designators.
@@ -25,7 +17,6 @@
 
 (defun simple-value-numbering (initial-instruction)
   (declare (ignore initial-instruction))
-  (let ((*unique-number* 0)
-	(*constant-designators* (make-hash-table :test #'eq)))))
+  (let ((*constant-designators* (make-hash-table :test #'eq)))))
 
 ;;  LocalWords:  designator designators
