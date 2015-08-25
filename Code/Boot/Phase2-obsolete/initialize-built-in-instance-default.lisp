90e73e4feb3b7675e2ff5c5d6ef5be6006a71358
Robert Strandh <robert.strandh@gmail.com>
2015-08-25 16:41:32 +0200

    Remove obsolete component.

--- Code/Boot/Phase2-obsolete/initialize-built-in-instance-default.lisp
+++
@@ -1,7 +0,0 @@
-(cl:in-package #:sicl-boot-phase2)
-
-(defparameter *initialize-built-in-instance-default*
-  #'sicl-boot-phase1:initialize-built-in-instance-default)
-
-(defun initialize-built-in-instance-default (&rest args)
-  (apply *initialize-built-in-instance-default* args))
