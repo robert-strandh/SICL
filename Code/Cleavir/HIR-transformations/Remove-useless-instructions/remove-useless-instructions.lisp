233970a1cb980fc022525ab5f094a62637b7f28c
Robert Strandh <robert.strandh@gmail.com>
2015-08-26 11:02:47 +0200

    Add default method on new generic function.

--- Code/Cleavir/HIR-transformations/Remove-useless-instructions/remove-useless-instructions.lisp
+++ Code/Cleavir/HIR-transformations/Remove-useless-instructions/remove-useless-instructions.lisp
@@ -2,6 +2,11 @@
 
 (defgeneric instruction-may-be-removed-p (instruction))
 
+(defmethod instruction-may-be-removed-p (instruction)
+  (and (= (length (cleavir-ir:successors instruction)) 1)
+       (loop for output in (cleavir-ir:outputs instruction)
+	     always (null (cleavir-ir:using-instructions output)))))
+
 (defun remove-useless-instructions (initial-instruction)
   (cleavir-ir:reinitialize-data initial-instruction)
   (let ((useless-instructions '()))
