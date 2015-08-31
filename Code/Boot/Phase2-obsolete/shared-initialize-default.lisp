85014a41498f736378fc478799ab65fee12961c9
Robert Strandh <robert.strandh@gmail.com>
2015-08-31 10:14:14 +0200

    Remove obsolete component.

--- Code/Boot/Phase2-obsolete/shared-initialize-default.lisp
+++
@@ -1,7 +0,0 @@
-(cl:in-package #:sicl-boot-phase2)
-
-(defparameter *shared-initialize-default*
-  #'sicl-boot-phase1:shared-initialize-default)
-
-(defun shared-initialize-default (&rest args)
-  (apply *shared-initialize-default* args))
