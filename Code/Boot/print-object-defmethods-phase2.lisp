41f742feb6b8a4f212cad5234863e8b5f49ce072
Robert Strandh <robert.strandh@gmail.com>
2015-09-03 11:31:31 +0200

    Add method on PRINT-OBJECT specialized to STANDARD-GENERIC-FUNCTION.

--- Code/Boot/print-object-defmethods-phase2.lisp
+++ Code/Boot/print-object-defmethods-phase2.lisp
@@ -9,3 +9,8 @@
   (format stream
 	  "#<Ersatz built-in class ~s>"
 	  (sicl-clos:class-name class)))
+
+(defmethod print-object ((function standard-generic-function) stream)
+  (format stream
+	  "#<Ersatz standard generic function ~s>"
+	  (sicl-clos:generic-function-name function)))
