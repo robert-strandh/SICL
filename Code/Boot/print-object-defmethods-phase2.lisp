4d79c34ab91d1a3a0b803372e0cbed4a2238eb93
Robert Strandh <robert.strandh@gmail.com>
2015-09-03 11:17:50 +0200

    Add method on PRINT-OBJECT specialized to BUILT-IN-CLASS.

--- Code/Boot/print-object-defmethods-phase2.lisp
+++ Code/Boot/print-object-defmethods-phase2.lisp
@@ -4,3 +4,8 @@
   (format stream
 	  "#<Ersatz standard class ~s>"
 	  (sicl-clos:class-name class)))
+
+(defmethod print-object ((class sicl-clos:built-in-class) stream)
+  (format stream
+	  "#<Ersatz built-in class ~s>"
+	  (sicl-clos:class-name class)))
