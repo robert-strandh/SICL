2abfa208bba020dd4f5d90cd91923f036a63befd
Robert Strandh <robert.strandh@gmail.com>
2015-09-07 12:10:39 +0200

    Use new function for loading file for computing discriminating TAGBODY.

--- Code/Boot/phase3.lisp
+++ Code/Boot/phase3.lisp
@@ -149,12 +149,12 @@
     (ld "../CLOS/discriminating-automaton.lisp" r3 r3)
     (define-general-instance-p-phase3 r3)
     (ldp "../CLOS/list-utilities.lisp" r2 r2)
+    (ldp "../CLOS/discriminating-tagbody.lisp" r3 r3)
     (handler-bind
 	((cleavir-env:no-function-info
 	   (lambda (condition)
 	     (declare (ignore condition))
 	   (invoke-restart 'cleavir-generate-ast:consider-global))))
-      (ld "../CLOS/discriminating-tagbody.lisp" r3 r3)
       (ld "../CLOS/compute-discriminating-function-support.lisp" r3 r3)
       (ld "../CLOS/compute-applicable-methods-support.lisp" r2 r2)
       (ld "../CLOS/satiation.lisp" r2 r2))
