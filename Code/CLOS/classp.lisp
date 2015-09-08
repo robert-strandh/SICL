8d438ea7ede3739514b3137343e3d3e55eaad5b5
Robert Strandh <robert.strandh@gmail.com>
2015-09-08 10:24:41 +0200

    Remove irrelevant comment.

--- Code/CLOS/classp.lisp
+++ Code/CLOS/classp.lisp
@@ -1,12 +1,5 @@
 (cl:in-package #:sicl-clos)
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;;
-;;; We need a function to determine whether some object is a class.
-;;; Since classes can be unnamed, we cannot consult our database of
-;;; classes.  Instead, we decide that something is a class if it is 
-;;; a subclass of the class CLASS.
-
 (defmethod classp (object)
   nil)
 
