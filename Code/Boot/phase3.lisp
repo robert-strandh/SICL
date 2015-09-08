759e0be29f5c9e3ce77c6e1ee0f8601501d2e8be
Robert Strandh <robert.strandh@gmail.com>
2015-09-08 10:41:21 +0200

    Load a file containing definition of methods on generic function CLASSP.

--- Code/Boot/phase3.lisp
+++ Code/Boot/phase3.lisp
@@ -160,6 +160,7 @@
     (ldp "../CLOS/discriminating-tagbody.lisp" r3 r3)
     (ld "../CLOS/compute-discriminating-function-support.lisp" r3 r3)
     (ld "../CLOS/classp-defgeneric.lisp" r1 r1)
+    (ld "../CLOS/classp-defmethods.lisp" r2 r2)
     (ld "../CLOS/compute-applicable-methods-support.lisp" r2 r2)
     (ld "../CLOS/compute-applicable-methods-defgenerics.lisp" r1 r1)
     (ld "../CLOS/compute-applicable-methods-defmethods.lisp" r2 r2)
