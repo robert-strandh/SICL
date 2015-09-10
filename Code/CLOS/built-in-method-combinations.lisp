9f9a7daba64191218977a8c4e9b8241457fc64e2
Robert Strandh <robert.strandh@gmail.com>
2015-09-10 09:27:40 +0200

    Added more built-in method combinations.

--- Code/CLOS/built-in-method-combinations.lisp
+++ Code/CLOS/built-in-method-combinations.lisp
@@ -1,3 +1,19 @@
 (cl:in-package #:sicl-clos)
 
 (define-method-combination progn :identity-with-one-argument t)
+
+(define-method-combination and :identity-with-one-argument t)
+
+(define-method-combination or :identity-with-one-argument t)
+
+(define-method-combination append :identity-with-one-argument nil)
+
+(define-method-combination nconc :identity-with-one-argument t)
+
+(define-method-combination list :identity-with-one-argument nil)
+
+(define-method-combination max :identity-with-one-argument t)
+
+(define-method-combination min :identity-with-one-argument t)
+
+(define-method-combination + :identity-with-one-argument t)
