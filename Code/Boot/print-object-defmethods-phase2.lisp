c76cad30d1f739d7cb53d3b98b108da39dad1056
Robert Strandh <robert.strandh@gmail.com>
2015-09-22 10:28:58 +0200

    Fix incorrect terminology in PRINT-OBJECT methods.

--- Code/Boot/print-object-defmethods-phase2.lisp
+++ Code/Boot/print-object-defmethods-phase2.lisp
@@ -2,15 +2,15 @@
 
 (defmethod print-object ((class standard-class) stream)
   (format stream
-	  "#<Ersatz standard class ~s>"
+	  "#<Bridge standard class ~s>"
 	  (sicl-clos:class-name class)))
 
 (defmethod print-object ((class sicl-clos:built-in-class) stream)
   (format stream
-	  "#<Ersatz built-in class ~s>"
+	  "#<Bridge built-in class ~s>"
 	  (sicl-clos:class-name class)))
 
 (defmethod print-object ((function standard-generic-function) stream)
   (format stream
-	  "#<Ersatz standard generic function ~s>"
+	  "#<Bridge standard generic function ~s>"
 	  (sicl-clos:generic-function-name function)))
