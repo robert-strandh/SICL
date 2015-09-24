e6ace16683a72fee5f38903f2c44389f74a32620
Robert Strandh <robert.strandh@gmail.com>
2015-09-24 09:47:54 +0200

    Load a file containing support code for adding and removing methods.

--- Code/Boot/phase3.lisp
+++ Code/Boot/phase3.lisp
@@ -192,6 +192,7 @@
     (ld "../CLOS/initialize-instance-defmethods.lisp" r3 r3)
     (finalize-all-classes-phase3 r2 r1 r2)
     (ld "../CLOS/invalidate-discriminating-function.lisp" r3 r3)
+    (ld "../CLOS/add-remove-method-support.lisp" r3 r3)
     (message "End of phase 3~%")))
 
 ;;  LocalWords:  metaobject
