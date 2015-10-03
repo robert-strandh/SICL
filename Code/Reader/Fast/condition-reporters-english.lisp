ece3eb905d153662b31c5c1a51ef9c3f1d054eea
Robert Strandh <robert.strandh@gmail.com>
2015-10-03 08:13:57 +0200

    Add method on REPORT-CONDITION specialized to UNMATCHED-RIGHT-PARENTHESIS.

--- Code/Reader/Fast/condition-reporters-english.lisp
+++ Code/Reader/Fast/condition-reporters-english.lisp
@@ -1,2 +1,7 @@
 (cl:in-package #:sicl-read)
 
+(defmethod cleavir-i18n:report-condition
+    ((condition unmatched-right-parenthesis)
+     stream
+     (language cleavir-i18n:english))
+  (format stream "Unmatched right parenthesis found."))
