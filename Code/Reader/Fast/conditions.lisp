a4d0a10c00fe7d325484508bc9551b3de6ae35c1
Robert Strandh <robert.strandh@gmail.com>
2015-10-03 08:10:12 +0200

    Make existing conditions use new class as superclass.

--- Code/Reader/Fast/conditions.lisp
+++ Code/Reader/Fast/conditions.lisp
@@ -3,7 +3,7 @@
 (define-condition sicl-reader-error (reader-error cleavir-i18n:condition)
   ())
 
-(define-condition unmatched-right-parenthesis (reader-error)
+(define-condition unmatched-right-parenthesis (sicl-reader-error)
   ()
   (:report
    (lambda (condition stream)
@@ -12,7 +12,7 @@
 
 ;;; This condition is signaled when a token consisting
 ;;; only of (unescaped) dots was found.
-(define-condition only-dots-in-token (reader-error)
+(define-condition only-dots-in-token (sicl-reader-error)
   ()
   (:report
    (lambda (condition stream)
@@ -29,7 +29,7 @@
      (format stream "A token consisting of a single dot was found ~@
                      in a context that does not permit such a token."))))
 
-(define-condition no-object-preceding-dot (reader-error)
+(define-condition no-object-preceding-dot (sicl-reader-error)
   ()
   (:report
    (lambda (condition stream)
@@ -37,7 +37,7 @@
      (format stream "A left parenthesis cannot be ~
                      immediately followed by a dot"))))
 
-(define-condition multiple-objects-following-dot (reader-error)
+(define-condition multiple-objects-following-dot (sicl-reader-error)
   ((%offending-expression
     :initarg :offending-expression
     :reader offending-expression))
@@ -47,7 +47,7 @@
                      inside a list was found: ~S."
 	     (offending-expression condition)))))
 
-(define-condition no-parameter-allowed (reader-error)
+(define-condition no-parameter-allowed (sicl-reader-error)
   ((%which-directive :initarg :which-directive :reader which-directive)
    (%parameter :initarg :parameter :reader parameter))
   (:report
@@ -56,10 +56,10 @@
 	     "The ~a directive does not take a numeric parameter"
 	     (which-directive condition)))))
 
-(define-condition unknown-character-name (reader-error)
+(define-condition unknown-character-name (sicl-reader-error)
   ((%name :initarg :name :reader name)))
 
-(define-condition invalid-character (reader-error)
+(define-condition invalid-character (sicl-reader-error)
   ((%char :initarg :char :reader char))
   (:report
    (lambda (condition stream)
