97d4e98e742538bebb0556f6c5d0723a09cf7165
Robert Strandh <robert.strandh@gmail.com>
2015-10-04 07:33:18 +0200

    Add method on REPORT-CONDITION specialized to ONLY-DOTS-IN-TOKEN.

--- Code/Reader/Fast/condition-reporters-english.lisp
+++ Code/Reader/Fast/condition-reporters-english.lisp
@@ -5,3 +5,9 @@
      stream
      (language cleavir-i18n:english))
   (format stream "Unmatched right parenthesis found."))
+
+(defmethod cleavir-i18n:report-condition
+    ((condition only-dots-in-token)
+     stream
+     (language cleavir-i18n:english))
+  (format stream "A token with only dots in it was found"))
