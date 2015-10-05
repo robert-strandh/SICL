f14d6bb786ec3b349bb55f370ce113904fc7e400
Robert Strandh <robert.strandh@gmail.com>
2015-10-05 10:33:26 +0200

    Add method on REPORT-CONDITION specialized to SINGLE-DOT-TOKEN.

--- Code/Reader/Fast/condition-reporters-english.lisp
+++ Code/Reader/Fast/condition-reporters-english.lisp
@@ -11,3 +11,10 @@
      stream
      (language cleavir-i18n:english))
   (format stream "A token with only dots in it was found"))
+
+(defmethod cleavir-i18n:report-condition
+    ((condition single-dot-token)
+     stream
+     (language cleavir-i18n:english))
+  (format stream "A token consisting of a single dot was found ~@
+                  in a context that does not permit such a token."))
