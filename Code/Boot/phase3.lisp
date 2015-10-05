944da9765aa35b5f3338cb7f0c78107dff2dbd08
Robert Strandh <robert.strandh@gmail.com>
2015-10-05 16:39:00 +0200

    Call function for satiating all generic functions in phase 3.

--- Code/Boot/phase3.lisp
+++ Code/Boot/phase3.lisp
@@ -243,6 +243,7 @@
     (ld "../CLOS/compute-effective-method-defmethods.lisp" r2 r2)
     (ld "../CLOS/compute-discriminating-function-defgenerics.lisp" r1 r1)
     (ld "../CLOS/compute-discriminating-function-defmethods.lisp" r2 r2)
+    (satiate-phase3 r3 r1 r2)
     (message "End of phase 3~%")))
 
 ;;  LocalWords:  metaobject
