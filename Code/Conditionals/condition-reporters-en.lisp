13def1f1fcdc101db0cbffe074f97224d3755a3b
Robert Strandh <robert.strandh@gmail.com>
2015-10-11 13:03:17 +0200

    Use explicit package marker with IN-PACKAGE.

--- Code/Conditionals/condition-reporters-en.lisp
+++ Code/Conditionals/condition-reporters-en.lisp
@@ -29,7 +29,7 @@
 ;;;; OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 ;;;; POSSIBILITY OF SUCH DAMAGE.
 
-(in-package :sicl-conditionals)
+(cl:in-package :sicl-conditionals)
 
 (defmethod cleavir-i18n:report-condition ((condition malformed-body)
 					  stream
