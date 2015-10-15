aec865de86a4f15fcb1f5591ba09e3ea9cb519a0
Robert Strandh <robert.strandh@gmail.com>
2015-10-15 09:56:09 +0200

    Supply ORIGIN keyword argument when creating BLOCK-AST.

--- Code/Cleavir/Generate-AST/convert-special.lisp
+++ Code/Cleavir/Generate-AST/convert-special.lisp
@@ -18,7 +18,8 @@
     ((symbol (eql 'block)) form env system)
   (db s (block name . body) form
     (declare (ignore block))
-    (let* ((ast (cleavir-ast:make-block-ast nil))
+    (let* ((origin (location form))
+	   (ast (cleavir-ast:make-block-ast nil :origin origin))
 	   (new-env (cleavir-env:add-block env name ast)))
       (setf (cleavir-ast:body-ast ast)
 	    (process-progn (convert-sequence body new-env system)))
