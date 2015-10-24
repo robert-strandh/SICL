0f20ca6bc044175f019a451f35bc30ab1d8423b4
Robert Strandh <robert.strandh@gmail.com>
2015-10-24 12:00:06 +0200

    Split function into two parts, one for lookup and one for the main work.

--- Code/CLOS/shared-initialize-support.lisp
+++ Code/CLOS/shared-initialize-support.lisp
@@ -8,32 +8,37 @@
 ;;; metaobject available, so we use the ...-USING-CLASS version
 ;;; directly.
 
+(defun shared-initialize-default-using-class-and-slots
+    (instance slot-names class slots &rest initargs)
+  (loop for slot in slots
+	do (multiple-value-bind (key value foundp)
+	       ;; Find the first key/value pair in initargs where the
+	       ;; key is one of the initargs of the slot.
+	       (get-properties initargs (slot-definition-initargs slot))
+	     (declare (ignore key))
+	     (if foundp
+		 ;; Found an explicit initarg in initargs.
+		 ;; Initialize the slot from its value.
+		 (setf (slot-value-using-class class instance slot)
+		       value)
+		 ;; No explicit initarg found.
+		 (when (and (not (slot-boundp-using-class class instance slot))
+			    (not (null (slot-definition-initfunction slot)))
+			    (or (eq slot-names t)
+				(member (slot-definition-name slot)
+					slot-names)))
+		   ;; Evaluate the initform by executing the initfunction.
+		   (setf (slot-value-using-class class instance slot)
+			 (funcall (slot-definition-initfunction slot)))))))
+  ;; Store the class slots in the instance so that we can update the instance
+  ;; when the class changes.
+  (setf (standard-instance-access instance +instance-slots-offset+)
+	slots)
+  instance)
+
 (defun shared-initialize-default
     (instance slot-names &rest initargs)
   (let* ((class (class-of instance))
 	 (slots (class-slots class)))
-    (loop for slot in slots
-	  do (multiple-value-bind (key value foundp)
-		 ;; Find the first key/value pair in initargs where the
-		 ;; key is one of the initargs of the slot. 
-		 (get-properties initargs (slot-definition-initargs slot))
-	       (declare (ignore key))
-	       (if foundp
-		   ;; Found an explicit initarg in initargs.
-		   ;; Initialize the slot from its value.
-		   (setf (slot-value-using-class class instance slot)
-			 value)
-		   ;; No explicit initarg found.  
-		   (when (and (not (slot-boundp-using-class class instance slot))
-			      (not (null (slot-definition-initfunction slot)))
-			      (or (eq slot-names t)
-				  (member (slot-definition-name slot)
-					  slot-names)))
-		     ;; Evaluate the initform by executing the initfunction. 
-		     (setf (slot-value-using-class class instance slot)
-			   (funcall (slot-definition-initfunction slot)))))))
-    ;; Store the class slots in the instance so that we can update the instance
-    ;; when the class changes.
-    (setf (standard-instance-access instance +instance-slots-offset+)
-	  slots))
-  instance)
+    (apply #'shared-initialize-default-using-class-and-slots
+	   instance slot-names class slots initargs)))
