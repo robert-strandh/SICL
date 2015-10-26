b58eccb9a153b1534d034ae906ce5c4cef107389
Robert Strandh <robert.strandh@gmail.com>
2015-10-26 10:56:30 +0100

    Define GENERAL-INSTANCE-ACCESS in R3 as well as R2.

--- Code/Boot/phase3.lisp
+++ Code/Boot/phase3.lisp
@@ -215,6 +215,7 @@
     (define-effective-slot-definition-class-phase3 r2 r1)
     (define-allocate-general-instance-phase3 r2)
     (define-general-instance-access-phase3 r2)
+    (define-general-instance-access-phase3 r3)
     (define-setf-general-instance-access-phase3 r2)
     (ld "../CLOS/class-unique-number-offset-defconstant.lisp" r2 r2)
     (ld "../CLOS/allocate-instance-support.lisp" r2 r2)
