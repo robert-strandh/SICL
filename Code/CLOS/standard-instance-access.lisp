b556e4cc08fb2147a1ccb5f61176ed5642349b58
Robert Strandh <robert.strandh@gmail.com>
2015-10-26 09:59:38 +0100

    Call GENERAL-INSTANCE-ACCESS accessor.

--- Code/CLOS/standard-instance-access.lisp
+++ Code/CLOS/standard-instance-access.lisp
@@ -3,7 +3,7 @@
 ;;; For the specification of this function, see
 ;;; http://metamodular.com/CLOS-MOP/standard-instance-access.html
 (defun standard-instance-access (instance location)
-  (slot-contents (heap-instance-slots instance) location))
+  (general-instance-access instance location))
 
 (defun (setf standard-instance-access) (new-value instance location)
-  (setf (slot-contents (heap-instance-slots instance) location) new-value))
+  (setf (general-instance-access instance location) new-value))
