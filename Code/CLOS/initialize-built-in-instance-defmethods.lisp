a44c07010c058c77f5208a4c03cafd0f6c8723da
Robert Strandh <robert.strandh@gmail.com>
2015-10-28 11:19:56 +0100

    Remove obsolete component.  We now use INITIALIZE-INSTANCE etc also on built-in instances.

--- Code/CLOS/initialize-built-in-instance-defmethods.lisp
+++
@@ -1,5 +0,0 @@
-(cl:in-package #:sicl-clos)
-
-(defmethod initialize-built-in-instance
-    (instance &rest initargs &key &allow-other-keys)
-  (apply #'initialize-built-in-instance-default instance initargs))
