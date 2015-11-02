54df6bb66dc22bd345d131efda208a1f3c2c4334
Robert Strandh <robert.strandh@gmail.com>
2015-11-02 09:21:13 +0100

    Update method on BLOCK-INFO according to new representation.

--- Code/Cleavir/Environment/new-info-methods.lisp
+++ Code/Cleavir/Environment/new-info-methods.lisp
@@ -702,26 +702,13 @@
 ;;; Finding info about a BLOCK is particularly easy because there can
 ;;; be no entries modifying the properties of the block. 
 
-;;; This method implements the action to take when the argument is a
-;;; BLOCK entry.
-(defmethod block-info ((environment block) symbol)
-  (if (eq symbol (name environment))
-      ;; We found a BLOCK entry with the same name, so we are done.
-      ;; Create and return a valid BLOCK-INFO instance for this
-      ;; environment.
-      (make-instance 'block-info
-	:name symbol
-	:identity (identity environment))
-      ;; We had a BLOCK entry, but it does not have the name that we
-      ;; are looking for.  Try the next entry in the environment.
-      (block-info (next environment) symbol)))
-
-;;; This method implements the action to take when the argument is an
-;;; ENTRY, but it is not a BLOCK entry.  We handle this situation by
-;;; just making a recursive call, passing the next entry in the
-;;; environment.
-(defmethod block-info ((environment entry) symbol)
-  (block-info (next environment) symbol))
+(defmethod block-info ((environment environment) symbol)
+  (loop for entry in (augmentations environment)
+	when (and (typep entry 'block)
+		  (eq symbol (name entry)))
+	  return (make-instance 'block-info
+		   :name symbol
+		   :identity (identity environment))))
 
 ;;; This method implements the action to take when the argument is the
 ;;; global environment.  We detect this situation by the fact that the
