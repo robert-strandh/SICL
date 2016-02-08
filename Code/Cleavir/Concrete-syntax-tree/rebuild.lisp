6472b744ebaf180c72ccaa84c6afee69acef3773
Robert Strandh <robert.strandh@gmail.com>
2016-02-08 16:25:58 +0100

    Improve comment.

--- Code/Cleavir/Concrete-syntax-tree/rebuild.lisp
+++ Code/Cleavir/Concrete-syntax-tree/rebuild.lisp
@@ -1,7 +1,7 @@
 (cl:in-package #:cleavir-cst)
 
-;;;; By REBUILDING a CST C, we mean taking a Common Lisp expression E
+;;;; By REBUILDING a CST, we mean taking a Common Lisp expression E
 ;;;; and building a CST from it.  To reconstruct the source
-;;;; information, an existing CST D is used.  E may contain
-;;;; sub-expressions from D, and in that case the source information
-;;;; in D is used for these sub-expressions.
+;;;; information, an existing CST C is used.  E may contain
+;;;; sub-expressions from C, and in that case the source information
+;;;; in C is used for these sub-expressions.
