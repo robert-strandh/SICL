b1a823f68dfe1ded980563cf5a27889e7df21c1e
Robert Strandh <robert.strandh@gmail.com>
2016-02-09 08:43:14 +0100

    New function for checking whether a CST represents a declaration.

--- Code/Cleavir/Concrete-syntax-tree/body.lisp
+++ Code/Cleavir/Concrete-syntax-tree/body.lisp
@@ -1,1 +1,10 @@
 (cl:in-package #:cleavir-cst)
+
+;;; Return true if and only if the concrete syntax tree CST represents
+;;; a DECLARE expression, i.e., that the expression represented is a
+;;; CONS with the symbol DECLARE in the CAR slot.  No attempt is made
+;;; to verify the syntax of the declare expression.
+(defun cst-is-declaration-p (cst)
+  (let ((expression (expression cst)))
+    (and (consp expression)
+	 (eq (car expression) 'declare))))
