ad20265389a923d6c0cdb3f031a84eed6c469ea1
Robert Strandh <robert.strandh@gmail.com>
2016-02-09 09:20:11 +0100

    New function for separating a function body into declarations, docstring, and code.

--- Code/Cleavir/Concrete-syntax-tree/body.lisp
+++ Code/Cleavir/Concrete-syntax-tree/body.lisp
@@ -40,3 +40,53 @@
 	;; DECLARE expressions.
 	(values children '())
 	(values (subseq children 0 pos) (subseq children pos)))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;; Separate a concrete syntax tree representing a function body (that
+;;; may contain declarations and/or a documentation string) into three
+;;; things:
+;;;
+;;;   * a list of concrete syntax trees representing the
+;;;     declarations,
+;;;
+;;;   * a concrete syntax tree representing a literal documentation
+;;;     string, or NIL if there is no documentation string in the
+;;;     body, and
+;;;
+;;;   * a list of concrete syntax trees representing the executable
+;;;     forms.
+;;;
+;;; Return the result as three values.
+;;;
+;;; If there are declarations after the first executable form (which
+;;; is a syntax error), then those declarations will be considered
+;;; part of the executable forms.
+;;;
+;;; If there is a literal string after the first executable form, then
+;;; that string is also considered an executable form.
+;;;
+;;; We assume that it has already been checked that the represented
+;;; body is a proper list.
+
+(defun separate-function-body (body-cst)
+  (let ((declaration-csts '())
+	(documentation-cst nil)
+	(form-csts '())
+	(children (children body-cst)))
+    (loop for (expr-cst . rest-csts) on children
+	  do (cond ((not (null form-csts))
+		    (push expr-cst form-csts))
+		   ((cst-is-declaration-p expr-cst)
+		    (push expr-cst declaration-csts))
+		   ((cst-is-literal-string-p expr-cst)
+		    (if (or (null rest-csts)
+			    (not (null documentation-cst))
+			    (not (null form-csts)))
+			(push expr-cst form-csts)
+			(setf documentation-cst expr-cst)))
+		   (t
+		    (push expr-cst form-csts))))
+    (values (nreverse declaration-csts)
+	    documentation-cst
+	    (nreverse form-csts))))
