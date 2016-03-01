2fb9392ea8643a6b7ffdebb0f29215e08d0e1af3
Robert Strandh <robert.strandh@gmail.com>
2016-03-01 10:50:42 +0100

    Define a class for representing a list of optional parameters.

--- Code/Cleavir/Concrete-syntax-tree/lambda-list.lisp
+++ Code/Cleavir/Concrete-syntax-tree/lambda-list.lisp
@@ -14,3 +14,13 @@
    ;; this slot contains NIL.
    (%supplied-p-parameter-cst :initform :supplied-p-parameter-cst
 			      :reader supplied-p-parameter-cst)))
+
+(defclass optional-parameters ()
+  (;; This slot contains a CST for the lambda-list keyword.
+   ;; Typically, this keyword will be &OPTIONAL, but client code may
+   ;; define other lambda-list keywords that work the same way as
+   ;; &OPTIONAL.
+   (%keyword-cst :initarg :keyword-cst :reader keyword-cst)
+   ;; This slot contains a list of instances of the class
+   ;; OPTIONAL-PARAMETER.
+   (%parameters :initarg :parameters :reader parameters)))
