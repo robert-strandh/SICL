0cf0a37bf87ed755e9104248086bd06a8b6b84e9
Robert Strandh <robert.strandh@gmail.com>
2016-03-01 10:45:14 +0100

    Define a class for representing a single optional parameter.

--- Code/Cleavir/Concrete-syntax-tree/lambda-list.lisp
+++ Code/Cleavir/Concrete-syntax-tree/lambda-list.lisp
@@ -1,2 +1,16 @@
 (cl:in-package #:cleavir-cst)
 
+(defclass optional-parameter ()
+  (;; This slot contains the CST for the obligatory variable of the
+   ;; optional parameter.
+   (%variable-cst :initarg :variable-cst :reader variable-cst)
+   ;; This slot contains a CST for the INIT-FORM of the optional
+   ;; parameter.  If no INIT-FORM was supplied, then this slot
+   ;; contains a CST for which both EXPRESSION and LOCATION return
+   ;; NIL.
+   (%init-form-cst :initarg :init-form-cst :reader init-form-cst)
+   ;; This slot contains a CST for the SUPPLIED-P-PARAMETER of the
+   ;; optional parameter.  If no SUPPLIED-P-PARAMETER was given, then
+   ;; this slot contains NIL.
+   (%supplied-p-parameter-cst :initform :supplied-p-parameter-cst
+			      :reader supplied-p-parameter-cst)))
