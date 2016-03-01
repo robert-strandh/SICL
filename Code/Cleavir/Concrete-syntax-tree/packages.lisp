5f356fc51c6fec82c5790ab3fe4d53a6ead52172
Robert Strandh <robert.strandh@gmail.com>
2016-03-01 11:04:12 +0100

    Export names of recently added classes and slot readers.

--- Code/Cleavir/Concrete-syntax-tree/packages.lisp
+++ Code/Cleavir/Concrete-syntax-tree/packages.lisp
@@ -7,4 +7,15 @@
    #:cst-is-declaration-p
    #:cst-is-literal-string-p
    #:separate-ordinary-body
-   #:separate-function-body))
+   #:separate-function-body
+   #:optional-or-keyword-parameter
+   #:variables-cst
+   #:init-form-cst
+   #:supplied-p-parameter-cst
+   #:optional-parameter
+   #:optional-parameters
+   #:keyword-cst
+   #:parameters
+   #:keyword-parameter
+   #:keyword-name-cst
+   #:keyword-parameters))
