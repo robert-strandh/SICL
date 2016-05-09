489e1467adaa1eabe1d0e2b117b34f05d133877c
Robert Strandh <robert.strandh@gmail.com>
2016-05-09 06:14:32 +0200

    Define root class for optimize ASTs.

--- Code/Cleavir/Abstract-syntax-tree/optimize-related-asts.lisp
+++ Code/Cleavir/Abstract-syntax-tree/optimize-related-asts.lisp
@@ -1,1 +1,19 @@
 (cl:in-package #:cleavir-ast)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;; Class OPTIMIZE-AST.
+
+(defclass optimize-ast (ast)
+  ((%child-ast :initarg :child-ast :reader child-ast)))
+
+(defun make-optimize-ast (child-ast &key origin)
+  (make-instance 'optimize-ast
+    :origin origin
+    :child-ast child-ast))
+
+(cleavir-io:define-save-info optimize-ast
+  (:child-ast child-ast))
+
+(defmethod children ((ast optimize-ast))
+  (list (child-ast ast)))
