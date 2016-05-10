9cf66605e6199c6994b5f3398e6a60309357fe2c
Robert Strandh <robert.strandh@gmail.com>
2016-05-10 07:09:21 +0200

    Define new class SPEED-AST subclass of OPTIMIZE-AST.

--- Code/Cleavir/Abstract-syntax-tree/optimize-related-asts.lisp
+++ Code/Cleavir/Abstract-syntax-tree/optimize-related-asts.lisp
@@ -14,3 +14,16 @@
 
 (defmethod children ((ast optimize-ast))
   (list (child-ast ast)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;; Class SPEED-AST.
+
+(defclass speed-ast (optimize-ast)
+  ())
+
+(defun make-speed-ast (child-ast value-ast &key origin)
+  (make-instance 'speed-ast
+    :origin origin
+    :child-ast child-ast
+    :value-asts value-ast))
