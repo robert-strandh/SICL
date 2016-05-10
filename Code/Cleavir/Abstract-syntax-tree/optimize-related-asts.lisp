7a61d51b426579c37e659e3fcab5f9b456df7f3e
Robert Strandh <robert.strandh@gmail.com>
2016-05-10 07:01:13 +0200

    Remove constructor for class OPTIMIZE-AST.

--- Code/Cleavir/Abstract-syntax-tree/optimize-related-asts.lisp
+++ Code/Cleavir/Abstract-syntax-tree/optimize-related-asts.lisp
@@ -8,11 +8,6 @@
   ((%child-ast :initarg :child-ast :reader child-ast)
    (%value-ast :initarg :value-ast :reader value-ast)))
 
-(defun make-optimize-ast (child-ast &key origin)
-  (make-instance 'optimize-ast
-    :origin origin
-    :child-ast child-ast))
-
 (cleavir-io:define-save-info optimize-ast
   (:child-ast child-ast)
   (:value-ast value-ast))
