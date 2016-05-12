1a65c3fc77b048db96c1e67e42da52f06690ee5f
Robert Strandh <robert.strandh@gmail.com>
2016-05-12 07:51:29 +0200

    Define method on COMPILE-AST specialized to DEBUG-AST.

--- Code/Cleavir/AST-to-HIR/compile-optimize-related-asts.lisp
+++ Code/Cleavir/AST-to-HIR/compile-optimize-related-asts.lisp
@@ -4,3 +4,8 @@
   (compile-ast (cleavir-ast:child-ast ast)
 	       (clone-context context
 			      :speed-value (cleavir-ast:value ast))))
+
+(defmethod compile-ast ((ast cleavir-ast:debug-ast) context)
+  (compile-ast (cleavir-ast:child-ast ast)
+	       (clone-context context
+			      :debug-value (cleavir-ast:value ast))))
