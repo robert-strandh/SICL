ffe48c2f4916451ba37b516a8ee218db25984012
Robert Strandh <robert.strandh@gmail.com>
2016-05-12 07:50:03 +0200

    Define method on COMPILE-AST specialized to SPEED-AST.

--- Code/Cleavir/AST-to-HIR/compile-optimize-related-asts.lisp
+++ Code/Cleavir/AST-to-HIR/compile-optimize-related-asts.lisp
@@ -1,1 +1,6 @@
 (cl:in-package #:cleavir-ast-to-hir)
+
+(defmethod compile-ast ((ast cleavir-ast:speed-ast) context)
+  (compile-ast (cleavir-ast:child-ast ast)
+	       (clone-context context
+			      :speed-value (cleavir-ast:value ast))))
