d8a5076cd7aad553e3ae6a64f1f205f50bb809dc
Robert Strandh <robert.strandh@gmail.com>
2016-05-13 17:24:23 +0200

    Define save info for new AST class.

--- Code/Cleavir/Abstract-syntax-tree/scope-related-asts.lisp
+++ Code/Cleavir/Abstract-syntax-tree/scope-related-asts.lisp
@@ -3,3 +3,7 @@
 (defclass scope-ast (ast)
   ((%child-ast :initarg :child-ast :reader child-ast)
    (%variable-ast :initarg :variable-ast :reader variable-ast)))
+
+(cleavir-io:define-save-info scope-ast
+  (:child-ast child-ast)
+  (:variable-ast variagble-ast))
