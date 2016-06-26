d4e3b97034320249b43cc3b64a5631f549f9d483
Robert Strandh <robert.strandh@gmail.com>
2016-06-26 06:13:36 +0200

    Specialize on language from ACCLIMATION.

--- Code/Cleavir/Code-utilities/condition-reporters-english.lisp
+++ Code/Cleavir/Code-utilities/condition-reporters-english.lisp
@@ -1,15 +1,15 @@
 (cl:in-package #:cleavir-code-utilities)
 
-(defmethod cleavir-i18n:report-condition
-    ((condition form-must-be-proper-list) stream (language cleavir-i18n:english))
+(defmethod acclimation:report-condition
+    ((condition form-must-be-proper-list) stream (language acclimation:english))
   (format stream "A form must be a proper list,~@
                   but the following was found instead:~@
                   ~s" (form condition)))
 
-(defmethod cleavir-i18n:report-condition
+(defmethod acclimation:report-condition
     ((condition invalid-number-of-arguments)
      stream
-     (language cleavir-i18n:english))
+     (language acclimation:english))
   (format stream
 	  "Invalid number of arguments.~@
            There must be at least ~d arguments.~%"
@@ -26,40 +26,40 @@
 ;;;
 ;;;  Lambda list conditions.
 
-(defmethod cleavir-i18n:report-condition
-    ((c lambda-list-must-be-list) stream (language cleavir-i18n:english))
+(defmethod acclimation:report-condition
+    ((c lambda-list-must-be-list) stream (language acclimation:english))
   (format stream
 	  "A lambda list must be a list.~@
            But the following was found instead:~@
            ~s"
 	  (code c)))
 
-(defmethod cleavir-i18n:report-condition
-    ((c lambda-list-must-not-be-circular) stream (language cleavir-i18n:english))
+(defmethod acclimation:report-condition
+    ((c lambda-list-must-not-be-circular) stream (language acclimation:english))
   (format stream
 	  "A lambda list must not be a circular list.~@
            But the following was found instead:~@
            ~s"
 	  (code c)))
 
-(defmethod cleavir-i18n:report-condition
-    ((c lambda-list-must-be-proper-list) stream (language cleavir-i18n:english))
+(defmethod acclimation:report-condition
+    ((c lambda-list-must-be-proper-list) stream (language acclimation:english))
   (format stream
 	  "This lambda list must be a proper list.~@
            But the following was found instead:~@
            ~s"
 	  (code c)))
 
-(defmethod cleavir-i18n:report-condition
-    ((c lambda-list-keyword-not-allowed) stream (language cleavir-i18n:english))
+(defmethod acclimation:report-condition
+    ((c lambda-list-keyword-not-allowed) stream (language acclimation:english))
   (format stream
 	  "Lambda list keyword ~s not allowed in this type of lambda list:~@
            ~s"
 	  (lambda-list-keyword c)
 	  (code c)))
 
-(defmethod cleavir-i18n:report-condition
-    ((c suspect-lambda-list-keyword) stream (language cleavir-i18n:english))
+(defmethod acclimation:report-condition
+    ((c suspect-lambda-list-keyword) stream (language acclimation:english))
   (format stream
 	  "Suspect lambda list keyword ~s will be treated as an ordinary symbol.~@
            In this lambda list:~@
@@ -67,26 +67,26 @@
 	  (lambda-list-keyword c)
 	  (code c)))
 
-(defmethod cleavir-i18n:report-condition
+(defmethod acclimation:report-condition
     ((c lambda-list-keyword-not-allowed-in-dotted-lambda-list)
      stream
-     (language cleavir-i18n:english))
+     (language acclimation:english))
   (format stream
 	  "Lambda list keyword ~s not allowed in a dotted lambda list:~@
            ~s"
 	  (lambda-list-keyword c)
 	  (code c)))
 
-(defmethod cleavir-i18n:report-condition
-    ((c multiple-occurrences-of-lambda-list-keyword) stream (language cleavir-i18n:english))
+(defmethod acclimation:report-condition
+    ((c multiple-occurrences-of-lambda-list-keyword) stream (language acclimation:english))
   (format stream
 	  "Lambda list keyword ~s appears multiple times in lambda list:~@
            ~s"
 	  (lambda-list-keyword c)
 	  (code c)))
 
-(defmethod cleavir-i18n:report-condition
-    ((c incorrect-keyword-order) stream (language cleavir-i18n:english))
+(defmethod acclimation:report-condition
+    ((c incorrect-keyword-order) stream (language acclimation:english))
   (format stream
 	  "Incorrect lambda list keyword order.~@
            The keyword ~s incorrectly appears before the keyword ~s in:~@
@@ -95,72 +95,72 @@
 	  (lambda-list-keyword2 c)
 	  (code c)))
 
-(defmethod cleavir-i18n:report-condition
-    ((c both-rest-and-body-occur-in-lambda-list) stream (language cleavir-i18n:english))
+(defmethod acclimation:report-condition
+    ((c both-rest-and-body-occur-in-lambda-list) stream (language acclimation:english))
   (format stream
 	  "Both &rest and &body may not occur in a lambda list.
            But they do in this lambda list:~@
            ~s"
 	  (code c)))
 
-(defmethod cleavir-i18n:report-condition
-    ((c rest/body-must-be-followed-by-variable) stream (language cleavir-i18n:english))
+(defmethod acclimation:report-condition
+    ((c rest/body-must-be-followed-by-variable) stream (language acclimation:english))
   (format stream
 	  "The lambda list keyword &rest or &body must be followed by a variable.~@
            But this is not the case in this lambda list:~@
            ~s"
 	  (code c)))
 
-(defmethod cleavir-i18n:report-condition
-    ((c atomic-lambda-list-tail-must-be-variable) stream (language cleavir-i18n:english))
+(defmethod acclimation:report-condition
+    ((c atomic-lambda-list-tail-must-be-variable) stream (language acclimation:english))
   (format stream
 	  "The atomic tail of a lambda list must be a variable.~@
            But this is not the case in this lambda list:~@
            ~s"
 	  (code c)))
 
-(defmethod cleavir-i18n:report-condition
-    ((c whole-must-be-followed-by-variable) stream (language cleavir-i18n:english))
+(defmethod acclimation:report-condition
+    ((c whole-must-be-followed-by-variable) stream (language acclimation:english))
   (format stream
 	  "The lambda list keyword &whole must be followed by a variable.~@
            But this is not the case in this lambda list:~@
            ~s"
 	  (code c)))
 
-(defmethod cleavir-i18n:report-condition
-    ((c whole-must-appear-first) stream (language cleavir-i18n:english))
+(defmethod acclimation:report-condition
+    ((c whole-must-appear-first) stream (language acclimation:english))
   (format stream
 	  "If &whole is used in a lambda list, it must appear first.~@
            But this is not the case in this lambda list:~@
            ~s"
 	  (code c)))
 
-(defmethod cleavir-i18n:report-condition
-    ((c whole-must-be-followed-by-variable) stream (language cleavir-i18n:english))
+(defmethod acclimation:report-condition
+    ((c whole-must-be-followed-by-variable) stream (language acclimation:english))
   (format stream
 	  "The lambda list keyword &whole must be followed by a variable.~@
            But this is not the case in this lambda list:~@
            ~s"
 	  (code c)))
 
-(defmethod cleavir-i18n:report-condition
-    ((c environment-must-be-followed-by-variable) stream (language cleavir-i18n:english))
+(defmethod acclimation:report-condition
+    ((c environment-must-be-followed-by-variable) stream (language acclimation:english))
   (format stream
 	  "The lambda list keyword &environment must be followed by a variable.~@
            But this is not the case in this lambda list:~@
            ~s"
 	  (code c)))
 
-(defmethod cleavir-i18n:report-condition
-    ((c environment-can-appear-at-most-once) stream (language cleavir-i18n:english))
+(defmethod acclimation:report-condition
+    ((c environment-can-appear-at-most-once) stream (language acclimation:english))
   (format stream
 	  "The lambda list keyword &environment can occur at most once in a lambda list.~@
            But it occurs several times in this lambda list:~@
            ~s"
 	  (code c)))
 
-(defmethod cleavir-i18n:report-condition
-    ((c malformed-specialized-required) stream (language cleavir-i18n:english))
+(defmethod acclimation:report-condition
+    ((c malformed-specialized-required) stream (language acclimation:english))
   (format stream
 	  "In this type of lambda list, a required parameter must~@
            have one of the following forms:~@
@@ -174,8 +174,8 @@
            ~s"
           (code c)))
 
-(defmethod cleavir-i18n:report-condition
-    ((c malformed-ordinary-optional) stream (language cleavir-i18n:english))
+(defmethod acclimation:report-condition
+    ((c malformed-ordinary-optional) stream (language acclimation:english))
   (format stream
 	  "In this type of lambda list, an item following the &optional~@
            lambda-list keyword must have one of the following forms:~@
@@ -189,8 +189,8 @@
            ~s"
           (code c)))
 
-(defmethod cleavir-i18n:report-condition
-    ((c malformed-defgeneric-optional) stream (language cleavir-i18n:english))
+(defmethod acclimation:report-condition
+    ((c malformed-defgeneric-optional) stream (language acclimation:english))
   (format stream
 	  "In this type of lambda list, an item following the &optional~@
            lambda-list keyword must have one of the following forms:~@
@@ -202,8 +202,8 @@
            ~s"
           (code c)))
 
-(defmethod cleavir-i18n:report-condition
-    ((c malformed-destructuring-optional) stream (language cleavir-i18n:english))
+(defmethod acclimation:report-condition
+    ((c malformed-destructuring-optional) stream (language acclimation:english))
   (format stream
 	  "In this type of lambda list, an item following the &optional~@
            lambda-list keyword must have one of the following forms:~@
@@ -217,8 +217,8 @@
            ~s"
           (code c)))
 
-(defmethod cleavir-i18n:report-condition
-    ((c malformed-ordinary-key) stream (language cleavir-i18n:english))
+(defmethod acclimation:report-condition
+    ((c malformed-ordinary-key) stream (language acclimation:english))
   (format stream
 	  "In this type of lambda list, an item following the &key~@
            lambda-list keyword must have one of the following forms:~@
@@ -235,8 +235,8 @@
            ~s"
           (code c)))
 
-(defmethod cleavir-i18n:report-condition
-    ((c malformed-defgeneric-key) stream (language cleavir-i18n:english))
+(defmethod acclimation:report-condition
+    ((c malformed-defgeneric-key) stream (language acclimation:english))
   (format stream
 	  "In this type of lambda list, an item following the &key~@
            lambda-list keyword must have one of the following forms:~@
@@ -249,8 +249,8 @@
            ~s"
           (code c)))
 
-(defmethod cleavir-i18n:report-condition
-    ((c malformed-destructuring-key) stream (language cleavir-i18n:english))
+(defmethod acclimation:report-condition
+    ((c malformed-destructuring-key) stream (language acclimation:english))
   (format stream
 	  "In this type of lambda list, an item following the &key~@
            lambda-list keyword must have one of the following forms:~@
@@ -268,8 +268,8 @@
            ~s"
           (code c)))
 
-(defmethod cleavir-i18n:report-condition
-    ((c malformed-aux) stream (language cleavir-i18n:english))
+(defmethod acclimation:report-condition
+    ((c malformed-aux) stream (language acclimation:english))
   (format stream
 	  "In a lambda list, an item following the &aux~@
            lambda-list keyword must have one of the following forms:~@
@@ -282,8 +282,8 @@
            ~s"
           (code c)))
 
-(defmethod cleavir-i18n:report-condition
-    ((c malformed-destructuring-tree) stream (language cleavir-i18n:english))
+(defmethod acclimation:report-condition
+    ((c malformed-destructuring-tree) stream (language acclimation:english))
   (format stream
 	  "A destructuring tree can only contain CONS cells and~@
            symbols that are also not names of contstants.~@
@@ -291,8 +291,8 @@
            ~s"
           (code c)))
 
-(defmethod cleavir-i18n:report-condition
-    ((c malformed-lambda-list-pattern) stream (language cleavir-i18n:english))
+(defmethod acclimation:report-condition
+    ((c malformed-lambda-list-pattern) stream (language acclimation:english))
   (format stream
 	  "A lambda-list pattern must be either a tree containing only~@
            CONS cells and symbols that are also not names of contstants,~@
@@ -301,8 +301,8 @@
            ~s"
           (code c)))
 
-(defmethod cleavir-i18n:report-condition
-    ((c required-must-be-variable) stream (language cleavir-i18n:english))
+(defmethod acclimation:report-condition
+    ((c required-must-be-variable) stream (language acclimation:english))
   (format stream
 	  "In this type of lambda list, the required parameter must~@
            be a variable which is also not the name of a constant.~@
@@ -314,8 +314,8 @@
 ;;;
 ;;;  Conditions for forms with BODY.
 
-(defmethod cleavir-i18n:report-condition
-    ((c ordinary-body-must-be-proper-list) stream (language cleavir-i18n:english))
+(defmethod acclimation:report-condition
+    ((c ordinary-body-must-be-proper-list) stream (language acclimation:english))
   (format stream
 	  "The BODY of a form such as LET, LET*, FLET, LABELS,~@
            or LOCALLY, must be a proper list.
@@ -323,8 +323,8 @@
            ~s"
           (body c)))
 
-(defmethod cleavir-i18n:report-condition
-    ((c function-body-must-be-proper-list) stream (language cleavir-i18n:english))
+(defmethod acclimation:report-condition
+    ((c function-body-must-be-proper-list) stream (language acclimation:english))
   (format stream
 	  "The BODY of a function must be a proper list.
            But the following was found instead:~@
