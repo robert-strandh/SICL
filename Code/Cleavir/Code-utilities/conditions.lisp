506064fddbeebb3adcd0ba2b01cb69f5a524c72e
Robert Strandh <robert.strandh@gmail.com>
2016-06-26 06:13:05 +0200

    Get superclass of conditions from ACCLIMATION.

--- Code/Cleavir/Code-utilities/conditions.lisp
+++ Code/Cleavir/Code-utilities/conditions.lisp
@@ -4,12 +4,12 @@
   ((%code :initarg :code :reader code)))
 
 (define-condition form-must-be-proper-list
-    (program-error cleavir-i18n:condition)
+    (program-error acclimation:condition)
   ((%form :initarg :form :reader form)))
 
 ;;; A max-argcount of NIL means no upper bound.
 (define-condition invalid-number-of-arguments 
-    (program-error cleavir-i18n:condition)
+    (program-error acclimation:condition)
   ((%form :initarg :form :reader form)
    (%min-argcount :initarg :min-argcount :reader min-argcount)
    (%max-argcount :initarg :max-argcount :reader max-argcount)))
@@ -17,15 +17,15 @@
 ;;; FIXME: improve these conditions!
 
 (define-condition too-few-arguments
-    (program-error cleavir-i18n:condition)
+    (program-error acclimation:condition)
   ())
 
 (define-condition too-many-arguments
-    (program-error cleavir-i18n:condition)
+    (program-error acclimation:condition)
   ())
 
 (define-condition odd-number-of-keyword-arguments
-    (program-error cleavir-i18n:condition)
+    (program-error acclimation:condition)
   ())
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -33,64 +33,64 @@
 ;;;  Lambda list conditions.
 
 (define-condition lambda-list-must-be-list
-    (program-error code-condition cleavir-i18n:condition)
+    (program-error code-condition acclimation:condition)
   ())
 
 (define-condition lambda-list-must-not-be-circular
-    (program-error code-condition cleavir-i18n:condition)
+    (program-error code-condition acclimation:condition)
   ())
 
 (define-condition lambda-list-must-be-proper-list
-    (program-error code-condition cleavir-i18n:condition)
+    (program-error code-condition acclimation:condition)
   ())
 
 (define-condition lambda-list-keyword-not-allowed
-    (program-error code-condition cleavir-i18n:condition)
+    (program-error code-condition acclimation:condition)
   ((%keyword :initarg :keyword :reader lambda-list-keyword)))
 
 (define-condition lambda-list-keyword-not-allowed-in-dotted-lambda-list
-    (program-error code-condition cleavir-i18n:condition)
+    (program-error code-condition acclimation:condition)
   ((%keyword :initarg :keyword :reader lambda-list-keyword)))
 
 (define-condition multiple-occurrences-of-lambda-list-keyword
-    (program-error code-condition cleavir-i18n:condition)
+    (program-error code-condition acclimation:condition)
   ((%keyword :initarg :keyword :reader lambda-list-keyword)))
 
 (define-condition incorrect-keyword-order
-    (program-error code-condition cleavir-i18n:condition)
+    (program-error code-condition acclimation:condition)
   ((%keyword1 :initarg :keyword1 :reader lambda-list-keyword1)
    (%keyword2 :initarg :keyword2 :reader lambda-list-keyword2)))
 
 (define-condition both-rest-and-body-occur-in-lambda-list
-    (program-error code-condition cleavir-i18n:condition)
+    (program-error code-condition acclimation:condition)
   ())
 
 (define-condition rest/body-must-be-followed-by-variable
-    (program-error code-condition cleavir-i18n:condition)
+    (program-error code-condition acclimation:condition)
   ())
 
 (define-condition atomic-lambda-list-tail-must-be-variable
-    (program-error code-condition cleavir-i18n:condition)
+    (program-error code-condition acclimation:condition)
   ())
 
 (define-condition whole-must-appear-first
-    (program-error code-condition cleavir-i18n:condition)
+    (program-error code-condition acclimation:condition)
   ())
 
 (define-condition whole-must-be-followed-by-variable
-    (program-error code-condition cleavir-i18n:condition)
+    (program-error code-condition acclimation:condition)
   ())
 
 (define-condition environment-must-be-followed-by-variable
-    (program-error code-condition cleavir-i18n:condition)
+    (program-error code-condition acclimation:condition)
   ())
 
 (define-condition environment-can-appear-at-most-once
-    (program-error code-condition cleavir-i18n:condition)
+    (program-error code-condition acclimation:condition)
   ())
 
 (define-condition malformed-specialized-required
-    (program-error code-condition cleavir-i18n:condition)
+    (program-error code-condition acclimation:condition)
   ())
 
 ;;; This condition is used to indicate that there is a malformed item
@@ -108,7 +108,7 @@
 ;;; where var and supplied-p-parameter are symbols that are not names
 ;;; of constants.
 (define-condition malformed-ordinary-optional
-    (program-error code-condition cleavir-i18n:condition)
+    (program-error code-condition acclimation:condition)
   ())
 
 ;;; This condition is used to indicate that there is a malformed item
@@ -120,7 +120,7 @@
 ;;;
 ;;; where var is a symbol that is not a name of a constant.
 (define-condition malformed-defgeneric-optional
-    (program-error code-condition cleavir-i18n:condition)
+    (program-error code-condition acclimation:condition)
   ())
 
 ;;; This condition is used to indicate that there is a malformed item
@@ -136,30 +136,30 @@
 ;;; where var and supplied-p-parameter are symbols that are not names
 ;;; of constants, and pattern is any destructuring pattern.
 (define-condition malformed-destructuring-optional
-    (program-error code-condition cleavir-i18n:condition)
+    (program-error code-condition acclimation:condition)
   ())
 
 (define-condition malformed-ordinary-key
-    (program-error code-condition cleavir-i18n:condition)
+    (program-error code-condition acclimation:condition)
   ())
 
 (define-condition malformed-defgeneric-key
-    (program-error code-condition cleavir-i18n:condition)
+    (program-error code-condition acclimation:condition)
   ())
 
 (define-condition malformed-destructuring-key
-    (program-error code-condition cleavir-i18n:condition)
+    (program-error code-condition acclimation:condition)
   ())
 
 (define-condition malformed-aux
-    (program-error code-condition cleavir-i18n:condition)
+    (program-error code-condition acclimation:condition)
   ())
 
 ;;; This condition is used to indicate that a destructuring tree
 ;;; contains some item other than a CONS cell or a symbol which is not
 ;;; also the name of a constant. 
 (define-condition malformed-destructuring-tree
-    (program-error code-condition cleavir-i18n:condition)
+    (program-error code-condition acclimation:condition)
   ())
 
 ;;; This condition is used to indicate that something that ought
@@ -167,17 +167,17 @@
 ;;; keywords in fact is something else, such as a constant or some
 ;;; illegal atomic object.
 (define-condition malformed-lambda-list-pattern
-    (program-error code-condition cleavir-i18n:condition)
+    (program-error code-condition acclimation:condition)
   ())
 
 ;;; This condition is used to indicate that in a (non destructuring)
 ;;; lambda list, the required parameter must be a variable
 (define-condition required-must-be-variable
-    (program-error code-condition cleavir-i18n:condition)
+    (program-error code-condition acclimation:condition)
   ())
 
 (define-condition suspect-lambda-list-keyword
-    (style-warning code-condition cleavir-i18n:condition)
+    (style-warning code-condition acclimation:condition)
   ((%keyword :initarg :keyword :reader lambda-list-keyword)))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -185,9 +185,9 @@
 ;;;  Conditions for forms with BODY.
 
 (define-condition ordinary-body-must-be-proper-list
-    (style-warning cleavir-i18n:condition)
+    (style-warning acclimation:condition)
   ((%body :initarg :body :reader body)))
 
 (define-condition function-body-must-be-proper-list
-    (style-warning cleavir-i18n:condition)
+    (style-warning acclimation:condition)
   ((%body :initarg :body :reader body)))
