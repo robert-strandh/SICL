ce28018b079f6a33e800b9a0ccc77c3219bfe66e
Daniel Kochmański <daniel@turtleware.eu>
2016-06-27 10:46:35 +0200

    internationalization: remove obsolete system

--- Code/Cleavir/Internationalization/condition.lisp
+++
@@ -1,8 +0,0 @@
-(cl:in-package #:cleavir-internationalization)
-
-(defgeneric report-condition (condition stream language))
-
-(define-condition condition ()
-  ()
-  (:report (lambda (condition stream)
-	     (report-condition condition stream (language *locale*)))))
--- Code/Cleavir/Internationalization/locale.lisp
+++
@@ -1,6 +0,0 @@
-(cl:in-package #:cleavir-internationalization)
-
-(defclass locale ()
-  ((%language :initarg :language :accessor language)))
-
-(defvar *locale*)
