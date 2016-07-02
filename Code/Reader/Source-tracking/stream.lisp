3dcc0d48e8b6469ea4ce7106eff37325dd2bc834
Robert Strandh <robert.strandh@gmail.com>
2016-07-02 17:15:52 +0200

    Define method on STREAM-READ-CHAR specialized to SOURCE-TRACKING-STREAM.

--- Code/Reader/Source-tracking/stream.lisp
+++ Code/Reader/Source-tracking/stream.lisp
@@ -17,3 +17,12 @@
 (defun make-source-tracking-stream (stream)
   (make-instance 'source-tracking-stream
     :original stream))
+
+(defmethod trivial-gray-streams:stream-read-char
+    ((stream source-tracking-stream))
+  (let ((char (trivial-gray-streams:stream-read-char (original stream))))
+    (vector-push-extend char (contents stream) 10)
+    (when (eql char #\Newline)
+      (setf (column stream) 0)
+      (incf (line stream)))
+    char))
