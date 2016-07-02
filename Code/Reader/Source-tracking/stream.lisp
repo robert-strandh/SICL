71fa7f60f6f04c5d84cf679fd398a87e26f40401
Robert Strandh <robert.strandh@gmail.com>
2016-07-02 17:03:41 +0200

    Create contents in :INITFORM.

--- Code/Reader/Source-tracking/stream.lisp
+++ Code/Reader/Source-tracking/stream.lisp
@@ -1,9 +1,14 @@
 (cl:in-package #:sicl-reader)
 
+(defun make-contents ()
+  (make-array 10
+	      :element-type 'character
+	      :adjustable t))
+
 (defclass source-tracking-stream
     (trivial-gray-streams:fundamental-character-input-stream)
   ((%original :initarg :original :reader original)
-   (%contents :initarg :contents :reader contents)
+   (%contents :initform (make-contents) :reader contents)
    (%index :initform 0 :accessor index)
    (%line :initform 0 :accessor line)
    (%column :initform 0 :accessor column)))
