7daaa03180168f4a06a543c44df062aa4e53b574
Robert Strandh <robert.strandh@gmail.com>
2016-07-03 09:45:37 +0200

    Use specific package and adjust references accordingly.

--- Code/Reader/Source-tracking/read.lisp
+++ Code/Reader/Source-tracking/read.lisp
@@ -1,15 +1,16 @@
-(cl:in-package #:sicl-reader)
+(cl:in-package #:sicl-source-tracking-reader)
 
 (defparameter *syntax-trees* '())
 
-(defun read (&optional
-	       (input-stream *standard-input*)
-	       (eof-error-p t)
-	       (eof-value nil)
-	       (recursive-p nil))
-  (let ((*preserve-whitespace* recursive-p)
+(defun sicl-reader:read
+    (&optional
+       (input-stream *standard-input*)
+       (eof-error-p t)
+       (eof-value nil)
+       (recursive-p nil))
+  (let ((sicl-reader::*preserve-whitespace* recursive-p)
 	(*syntax-trees* (cons (list nil) *syntax-trees*)))
-    (let ((result (read-common input-stream eof-error-p eof-value)))
+    (let ((result (sicl-reader::read-common input-stream eof-error-p eof-value)))
       (push result (cadr *syntax-trees*))
       result)))
 
