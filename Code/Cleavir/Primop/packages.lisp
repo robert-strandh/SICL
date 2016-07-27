6889b0f01492d8c386a611fcbb70049d9ac31cab
Robert Strandh <robert.strandh@gmail.com>
2016-07-27 07:01:09 +0200

    Export name of operations for accessing non-simple T arrays.

--- Code/Cleavir/Primop/packages.lisp
+++ Code/Cleavir/Primop/packages.lisp
@@ -65,6 +65,7 @@
    #:long-float-sqrt
    #:slot-read #:slot-write
    #:simple-t-aref #:simple-t-aset
+   #:intricate-t-aref #:intricate-t-aset
    #:bit-aref #:bit-aset
    #:short-float-aref #:short-float-aset
    #:call-with-variable-bound
