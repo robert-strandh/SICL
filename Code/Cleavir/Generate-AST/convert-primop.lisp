10dfabdb1d56a4b003712762355d7e6860e70221
Robert Strandh <robert.strandh@gmail.com>
2016-07-29 14:43:32 +0200

    Define methods on CONVERT-SPECIAL for simple double-float array access.

--- Code/Cleavir/Generate-AST/convert-primop.lisp
+++ Code/Cleavir/Generate-AST/convert-primop.lisp
@@ -659,3 +659,50 @@
      :index-ast (convert index env system)
      :object-ast (convert single-float env system)
      :origin origin)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;; Converting CLEAVIR-PRIMOP:SIMPLE-DOUBLE-FLOAT-AREF.
+;;;
+;;; This primitive operation is used in the implementation of the
+;;; Common Lisp function AREF.  The ARRAY argument is a form that must
+;;; evaluate to a simple array specialized to DOUBLE-FLOAT.  The INDEX
+;;; argument is a form that must evaluate to a fixnum.  It represents
+;;; a valid row-major index into ARRAY.
+;;;
+;;; This primitive operation returns the object at INDEX in ARRAY.
+
+(defmethod convert-special
+    ((symbol (eql 'cleavir-primop:simple-double-float-aref)) form env system)
+  (db origin (op array index) form
+    (declare (ignore op))
+    (make-instance 'cleavir-ast:simple-double-float-aref-ast
+     :array-ast (convert array env system)
+     :index-ast (convert index env system)
+     :origin origin)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;; Converting CLEAVIR-PRIMOP:SIMPLE-DOUBLE-FLOAT-ASET.
+;;;
+;;; This primitive operation is used in the implementation of the
+;;; Common Lisp function (SETF AREF).  The ARRAY argument is a form
+;;; that must evaluate to a simple array specialized to DOUBLE-FLOAT.
+;;; The INDEX argument is a form that must evaluate to a fixnum.  It
+;;; represents a valid row-major index into ARRAY.
+;;;
+;;; This primitive operation stores DOUBLE-FLOAT at INDEX in ARRAY.
+;;;
+;;; Forms using this primitive operation must occur in a context that
+;;; does not require a value, such as in a PROGN other than as the
+;;; last form.
+
+(defmethod convert-special
+    ((symbol (eql 'cleavir-primop:simple-double-float-aset)) form env system)
+  (db origin (op array index double-float) form
+    (declare (ignore op))
+    (make-instance 'cleavir-ast:simple-double-float-aset-ast
+     :array-ast (convert array env system)
+     :index-ast (convert index env system)
+     :object-ast (convert double-float env system)
+     :origin origin)))
