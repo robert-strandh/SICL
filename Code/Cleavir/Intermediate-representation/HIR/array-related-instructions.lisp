641ad2d99e52ba915ce142627973c711712e82c5
Robert Strandh <robert.strandh@gmail.com>
2016-07-31 14:53:19 +0200

    Define macro for simplified definition of instructions and their constructors.

--- Code/Cleavir/Intermediate-representation/HIR/array-related-instructions.lisp
+++ Code/Cleavir/Intermediate-representation/HIR/array-related-instructions.lisp
@@ -2,6 +2,65 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;
+;;; Macro DEFINE-ARRAY-INSTRUCTIONS.
+;;;
+;;; This macro defines four instructions and their associated
+;;; constructors: one instruction for reading an element of a simple
+;;; array, one instruction for writing an element of a simple array,
+;;; one instruction for reading an element of a non-simple array, one
+;;; instruction for writing an element of a non-simple array,
+(defmacro define-array-instructions
+    (simple-aref-instruction
+     simple-aref-constructor
+     simple-aset-instruction
+     simple-aset-constructor
+     non-simple-aref-instruction
+     non-simple-aref-constructor
+     non-simple-aset-instruction
+     non-simple-aset-constructor)
+  `(progn
+     (defclass ,simple-aref-instruction
+	 (instruction one-successor-mixin)
+       ())
+
+     (defun ,simple-aref-constructor (input1 input2 output successor)
+       (make-instance ',simple-aref-instruction
+	 :inputs (list input1 input2)
+	 :outputs (list output)
+	 :successors (list successor)))
+
+     (defclass ,simple-aset-instruction
+	 (instruction one-successor-mixin side-effect-mixin)
+       ())
+
+     (defun ,simple-aset-constructor (input1 input2 input3 successor)
+       (make-instance ',simple-aset-instruction
+	 :inputs (list input1 input2 input3)
+	 :outputs ()
+	 :successors (list successor)))
+
+     (defclass ,non-simple-aref-instruction
+	 (instruction one-successor-mixin)
+       ())
+
+     (defun ,non-simple-aref-constructor (input1 input2 output successor)
+       (make-instance ',non-simple-aref-instruction
+	 :inputs (list input1 input2)
+	 :outputs (list output)
+	 :successors (list successor)))
+
+     (defclass ,non-simple-aset-instruction
+	 (instruction one-successor-mixin side-effect-mixin)
+       ())
+
+     (defun ,non-simple-aset-constructor (input1 input2 input3 successor)
+       (make-instance ',non-simple-aset-instruction
+	 :inputs (list input1 input2 input3)
+	 :outputs ()
+	 :successors (list successor)))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
 ;;; Instruction SIMPLE-T-AREF-INSTRUCTION.
 ;;;
 ;;; This instruction takes two inputs.  The first input is assumed to
