5a902f694be00857b33e7d79f0ddfa237a6dab7d
Robert Strandh <robert.strandh@gmail.com>
2016-08-09 11:07:48 +0200

    Remove backend that is no longer needed.

--- Code/Backends/Extrinsic-HIR-interpreter/packages.lisp
+++
@@ -1,9 +0,0 @@
-(cl:in-package #:common-lisp-user)
-
-(defpackage #:sicl-extrinsic-hir-interpreter
-  (:use #:common-lisp)
-  (:shadow #:function
-	   #:symbol
-	   #:unwind-protect)
-  (:export
-   ))
