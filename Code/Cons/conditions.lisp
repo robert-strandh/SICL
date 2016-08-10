a73737771067cbf328e031ccf9f0fb945aee58da
Robert Strandh <robert.strandh@gmail.com>
2016-08-10 10:05:17 +0200

    New component containing definitions of conditions.

---
+++ Code/Cons/conditions.lisp
@@ -0,0 +1,57 @@
+(cl:in-package :sicl-cons)
+
+;;;; Copyright (c) 2008 - 2016
+;;;;
+;;;;     Robert Strandh (robert.strandh@gmail.com)
+;;;;
+;;;; All rights reserved.
+;;;;
+;;;; Redistribution and use in source and binary forms, with or
+;;;; without modification, are permitted provided that the following
+;;;; conditions are met:
+;;;;
+;;;; 1. Redistributions of source code must retain the above copyright
+;;;;    notice, this list of conditions and the following disclaimer.
+;;;; 2. Redistributions in binary form must reproduce the above
+;;;;    copyright notice, this list of conditions and the following
+;;;;    disclaimer in the documentation and/or other materials
+;;;;    provided with the distribution.
+;;;;
+;;;; THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
+;;;; CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+;;;; INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+;;;; MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+;;;; DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS
+;;;; BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+;;;; EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+;;;; TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+;;;; DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+;;;; ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+;;;; OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+;;;; OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+;;;; POSSIBILITY OF SUCH DAMAGE.
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;; Externally visible conditions
+
+;;; This condition is used by functions an macros that require 
+;;; some argument to be a nonnegative integer. 
+(define-condition must-be-nonnegative-integer
+    (type-error acclimation:condition)
+  ()
+  (:default-initargs :expected-type '(integer 0)))
+
+;;; This condition is used by functions and macros that require
+;;; some argument to be a cons cell.
+(define-condition must-be-cons
+    (type-error acclimation:condition)
+  ()
+  (:default-initargs :expected-type 'cons))
+
+;;; This condition is used by functions and macros that require
+;;; some argument to be a list (a cons or nil).
+(define-condition must-be-list
+    (type-error acclimation:condition)
+  ()
+  (:default-initargs :expected-type 'list))
