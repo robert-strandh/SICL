142b4cc2ffd7b12a1030a0966d294e637e46f598
Robert Strandh <robert.strandh@gmail.com>
2016-08-16 16:18:26 +0200

    Add tests for vector elements in reverse order.

--- Code/Sequences/Test/utilities.lisp
+++ Code/Sequences/Test/utilities.lisp
@@ -63,6 +63,69 @@
       (push index result))
     (assert (equal (reverse result) '()))))
 
+(defun relevant-elements-8 ()
+  (let ((v1 #(a b c))
+	(result '()))
+    (sicl-sequence::for-each-relevant-element
+	(element index v1 0 3 t)
+      (push element result)
+      (push index result))
+    (assert (equal (reverse result) '(c 2 b 1 a 0)))))
+
+(defun relevant-elements-9 ()
+  (let ((v1 #(a b c))
+	(result '()))
+    (sicl-sequence::for-each-relevant-element
+	(element index v1 0 2 t)
+      (push element result)
+      (push index result))
+    (assert (equal (reverse result) '(b 1 a 0)))))
+
+(defun relevant-elements-10 ()
+  (let ((v1 #(a b c))
+	(result '()))
+    (sicl-sequence::for-each-relevant-element
+	(element index v1 0 1 t)
+      (push element result)
+      (push index result))
+    (assert (equal (reverse result) '(a 0)))))
+
+(defun relevant-elements-11 ()
+  (let ((v1 #(a b c))
+	(result '()))
+    (sicl-sequence::for-each-relevant-element
+	(element index v1 0 0 t)
+      (push element result)
+      (push index result))
+    (assert (equal (reverse result) '()))))
+
+(defun relevant-elements-12 ()
+  (let ((v1 #(a b c))
+	(result '()))
+    (sicl-sequence::for-each-relevant-element
+	(element index v1 1 3 t)
+      (push element result)
+      (push index result))
+    (assert (equal (reverse result) '(c 2 b 1)))))
+
+(defun relevant-elements-13 ()
+  (let ((v1 #(a b c))
+	(result '()))
+    (sicl-sequence::for-each-relevant-element
+	(element index v1 2 3 t)
+      (push element result)
+      (push index result))
+    (assert (equal (reverse result) '(c 2)))))
+
+(defun relevant-elements-14 ()
+  (let ((v1 #(a b c))
+	(result '()))
+    (sicl-sequence::for-each-relevant-element
+	(element index v1 3 3 t)
+      (push element result)
+      (push index result))
+    (assert (equal (reverse result) '()))))
+
 (defun relevant-elements ()
   (relevant-elements-1)
   (relevant-elements-2)
@@ -70,4 +133,11 @@
   (relevant-elements-4)
   (relevant-elements-5)
   (relevant-elements-6)
-  (relevant-elements-7))
+  (relevant-elements-7)
+  (relevant-elements-8)
+  (relevant-elements-9)
+  (relevant-elements-10)
+  (relevant-elements-11)
+  (relevant-elements-12)
+  (relevant-elements-13)
+  (relevant-elements-14))
