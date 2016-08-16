e18defbc23ec98a9add946bc445a413c3310507d
Robert Strandh <robert.strandh@gmail.com>
2016-08-16 13:51:48 +0200

    Define function FIND-LIST as one special case of FIND.

--- Code/Sequences/find.lisp
+++ Code/Sequences/find.lisp
@@ -1,5 +1,14 @@
 (cl:in-package :sicl-sequence)
 
+(defun find-list (item list from-end test test-not start end key)
+  (declare (optimize (speed 3) (debug 0) (safety 3)))
+  (with-test-and-test-not (test test-not)
+    (with-from-end from-end
+      (for-each-relevant-cons (cons list start end from-end)
+	(let ((element (apply-key-function (car cons) key)))
+	  (when (satisfies-two-argument-test-p item element test test-not)
+	    (return-from find-list element)))))))
+
 (defun find-aux (item sequence from-end test test-not start end key)
   ;; (declare (optimize (debug 0) (speed 3) (safety 0))
   ;; 	   (type (and fixnum (integer 0)) start)
