8b064e9375910833742aa959caa490c6e5e2f412
Robert Strandh <robert.strandh@gmail.com>
2016-08-17 16:27:08 +0200

    Fixed remaining typos in LANGUAGE method parameter.

--- Code/Environment/condition-reporters-english.lisp
+++ Code/Environment/condition-reporters-english.lisp
@@ -1,13 +1,13 @@
 (cl:in-package #:sicl-standard-environment-functions)
 
 (defmethod acclimation:report-condition
-    ((condition no-such-class) stream (langauge acclimation:english))
+    ((condition no-such-class) stream (language acclimation:english))
   (format stream "There is no class named ~s" (name condition)))
 
 (defmethod acclimation:report-condition
     ((condition variables-must-be-proper-list)
      stream
-     (langauge acclimation:english))
+     (language acclimation:english))
   (format stream
 	  "The variables to be bound by MULTIPLE-VALUE-BIND must~@
            be a proper list, but the following was found instead:~@
@@ -17,7 +17,7 @@
 (defmethod acclimation:report-condition
     ((condition variable-must-be-symbol)
      stream
-     (langauge acclimation:english))
+     (language acclimation:english))
   (format stream
 	  "A variable to be bound by MULTIPLE-VALUE-BIND must~@
            be a symbol, but the following was found instead:~@
