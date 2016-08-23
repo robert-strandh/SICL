cff9829f218df928f54d063c6442afa21b62570b
Bike <aeshtaer@gmail.com>
2016-08-22 23:36:51 -0700

    type inference: add setf for arc-bag

--- Code/Cleavir/Type-inference/dictionary.lisp
+++ Code/Cleavir/Type-inference/dictionary.lisp
@@ -4,5 +4,7 @@
 
 (defun arc-bag (from to dictionary)
   (gethash (cons from to) dictionary))
+(defun (setf arc-bag) (new-value from to dictionary)
+  (setf (gethash (cons from to) dictionary) new-value))
 
 (defvar *work-list*)
