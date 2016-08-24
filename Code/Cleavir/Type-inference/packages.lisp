e14d2dddc7d5dbd963379d7bee704bcbe3708971
Bike <aeshtaer@gmail.com>
2016-08-23 21:57:26 -0700

    type inference: export more symbols

--- Code/Cleavir/Type-inference/packages.lisp
+++ Code/Cleavir/Type-inference/packages.lisp
@@ -2,4 +2,8 @@
 
 (defpackage #:cleavir-type-inference
   (:use #:common-lisp)
+  (:export #:approximate-type #:canonicalize-type
+	   #:top-p #:bottom-p
+	   #:binary-join #:binary-meet #:difference
+	   #:join #:meet)
   (:export #:infer-types #:arc-bag #:find-type))
