35dc10e4b7a2750418590ad75d92d228b18962eb
Bike <aeshtaer@gmail.com>
2016-09-09 17:42:27 -0700

    type inference: use join for multiple predecessors
    Previously an instruction with multiple predecessors would call its
    transfer function once for each predecessor, and possibly alter the bags
    for its successor arcs each time. This does not make sense. An example
    of invalid inference:
    
    (loop with x = (the fixnum y)
          do (if (consp x) (return nil) (setq x (the cons z))))
    
    Previously this would infer x to be a fixnum throughout the loop,
    because it happened to transfer that predecessor last. Needless to say
    this would not end well.
    
    Added an exported function INSTRUCTION-INPUT to make the join easy.

--- Code/Cleavir/Type-inference/dictionary.lisp
+++ Code/Cleavir/Type-inference/dictionary.lisp
@@ -12,4 +12,10 @@
 (defun (setf arc-bag) (new-value from to dictionary)
   (setf (gethash (cons from to) dictionary) new-value))
 
+(defun instruction-input (instruction dictionary)
+  (apply #'bag-join
+	 (mapcar (lambda (pred)
+		   (arc-bag pred instruction dictionary))
+		 (cleavir-ir:predecessors instruction))))
+
 (defvar *work-list*)
