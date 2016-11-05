8f52eb84bb8b4443e6d35c666bcead5996702398
Bike <aeshtaer@gmail.com>
2016-11-05 01:35:05 -0700

    compilation policy: more specific warning messages

--- Code/Cleavir/Compilation-policy/condition-reporters-english.lisp
+++ Code/Cleavir/Compilation-policy/condition-reporters-english.lisp
@@ -2,7 +2,7 @@
 
 (defmethod acclimation:report-condition
     ((condition bad-optimize-value) stream (language acclimation:english))
-  (format stream "Bad optimize value for ~s: ~s~@
+  (format stream "Ignoring bad optimize value for ~s: ~s~@
                   Expected ~s"
 	  (first (specifier condition))
 	  (second (specifier condition))
@@ -10,7 +10,7 @@
 
 (defmethod acclimation:report-condition
     ((condition unknown-optimize-quality) stream (language acclimation:english))
-  (format stream "Unknown OPTIMIZE quality ~s"
+  (format stream "Ignoring unknown OPTIMIZE quality ~s"
 	  (first (specifier condition))))
 
 (defmethod acclimation:report-condition
