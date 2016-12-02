9070a3945db0959c4c9f4f4f1581bb91b11dec8f
Robert Strandh <robert.strandh@gmail.com>
2016-12-02 09:04:54 +0100

    Define new function dictionary-from-cst.

--- Code/Cleavir/Concrete-syntax-tree/rebuild.lisp
+++ Code/Cleavir/Concrete-syntax-tree/rebuild.lisp
@@ -5,3 +5,18 @@
 ;;;; information, an existing CST C is used.  E may contain
 ;;;; sub-expressions from C, and in that case the source information
 ;;;; in C is used for these sub-expressions.
+
+;; (defun dictionary-from-cst (cst)
+;;   (let ((result (make-hash-table :test #'eq)))
+;;     (flet ((enter (expression location)
+;; 	     (cond ((atom expression)
+;; 		    (pushnew location (gethash expression result)))
+;; 		   ((null (nth-value 1 (gethash e result)))
+;; 		    (setf (gethash expression result) (list location)))
+;; 		   (t
+;; 		    nil))))
+;;       (labels ((aux (cst)
+;; 		 (let ((l (location cst))
+;; 		       (e (expression cst)))
+;; 		   (cond ((null (children cst))
+;; 			  (pushnew l (gethash e result)))
