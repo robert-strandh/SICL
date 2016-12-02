6ac02e9f1166d1676bb6f63986aa42c523c3affe
Robert Strandh <robert.strandh@gmail.com>
2016-12-02 09:10:22 +0100

    Ignore unused parameters.

--- Code/Cleavir/HIR-transformations/hir-transformations.lisp
+++ Code/Cleavir/HIR-transformations/hir-transformations.lisp
@@ -1,6 +1,8 @@
 (cl:in-package #:cleavir-hir-transformations)
 
-(defun hir-transformations (initial-instruction implementation processor os)
+(defun hir-transformations
+    (initial-instruction implementation &optional processor os)
+  (declare (ignore implementation processor os))
   (type-inference initial-instruction)
   (eliminate-typeq initial-instruction)
   (process-captured-variables initial-instruction))
