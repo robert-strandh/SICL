2cf0e3a4c704ff36195dad4168d77437f53b339a
Robert Strandh <robert.strandh@gmail.com>
2016-12-04 08:55:25 +0100

    Beginning of ASDF system definition for the FAST version of the sequence functions.

---
+++ Code/Sequences/sicl-sequence-fast.lisp
@@ -0,0 +1,14 @@
+(cl:in-package #:asdf-user)
+
+(defsystem :sicl-sequence-fast
+  :serial t
+  :components
+  ((:file "Common/packages")
+   (:file "Common/conditions")
+   (:file "Common/condition-reporters-en")
+   (:file "Common/docstrings-en")
+   (:file "Common/utilities")
+   (:file "Fast/utilities")
+   (:file "Fast/find")
+   (:file "Fast/position")
+   (:file "Fast/count")))
