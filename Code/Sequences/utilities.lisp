ed684b9caccdecb61de1d3f821f22b7128e50f4a
Robert Strandh <robert.strandh@gmail.com>
2016-12-06 06:43:55 +0100

    Remove code that is now in the common utilities.

--- Code/Sequences/utilities.lisp
+++ Code/Sequences/utilities.lisp
@@ -1,115 +1,5 @@
 (cl:in-package #:sicl-sequence)
 
-;;; Skip a prefix of a list and signal an error if the list is too
-;;; short, or if it is not a proper list.  Also check that start is a
-;;; nonnegative integer.
-(defun skip-to-start (name list start)
-  (let ((start-bis start)
-	(remaining list))
-    (loop until (zerop start-bis)
-	  until (atom remaining)
-	  do (setf remaining (cdr remaining))
-	     (decf start-bis)
-	  finally (when (and (atom remaining) (not (null remaining)))
-		    (error 'must-be-proper-list
-			   :name name
-			   :datum list))
-		  (when (plusp start-bis)
-		    (error 'invalid-start-index
-			   :name name
-			   :datum start
-			   :expected-type `(integer 0 ,(- start start-bis))
-			   :in-sequence list)))
-    remaining))
-
-;;; This function is called at the end of some list traversal
-;;; to make sure that the list is a proper list.
-(defun tail-must-be-proper-list (name list tail)
-  (when (and (atom tail) (not (null tail)))
-    (error 'must-be-proper-list
-	   :name name
-	   :datum list)))
-
-;;; This function is called at the end of some list traversal
-;;; to make sure that the list is a proper list, and to make sure
-;;; that end is a valid index.
-(defun tail-must-be-proper-list-with-end (name list tail end length)
-  (when (and (atom tail) (not (null tail)))
-    (error 'must-be-proper-list
-	   :name name
-	   :datum list))
-  (when (and (atom tail) (< length end))
-    (error 'invalid-end-index
-	   :name name
-	   :datum end
-	   :in-sequence list
-	   :expected-type `(integer 0 ,length))))
-
-;;; This function is used when the sequence is a vector of some kind
-;;; in order to verify that start and end are valid bounding indexes.
-;;; It has already been verified that start is a nonnegative integer.
-;;; FIXME: What do we know about end?
-(defun verify-bounding-indexes (name vector start end)
-  (let ((length (length vector)))
-    (when (> start length)
-      (error 'invalid-start-index
-	     :name name
-	     :datum start
-	     :expected-type `(integer 0 ,length)
-	     :in-sequence vector))
-    (unless (<= 0 end length)
-      (error 'invalid-end-index
-	     :name name
-	     :datum end
-	     :expected-type `(integer 0 ,length)
-	     :in-sequence vector))
-    (unless (<= start end)
-      (error 'end-less-than-start
-	     :name name
-	     :datum start
-	     :expected-type `(integer 0 ,end)
-	     :end-index end
-	     :in-sequence vector))))
-
-;;; This function is used to compute the length of the list
-;;; given a remainder and a start index.
-(defun compute-length-from-remainder (name list remainder start)
-  (loop for length from start
-	until (atom remainder)
-	do (setf remainder (cdr remainder))
-	finally (unless (null remainder)
-		  (error 'must-be-proper-list
-			 :name name
-			 :datum list))
-		(return length)))
-
-;;; This function is used to verify that the end sequence index
-;;; is valid, and that, if we reach the end of the list, it is 
-;;; a proper list.  The remainder of the list is returned.
-(defun verify-end-index (name list remainder start end)
-  (loop for length from start
-	until (or (atom remainder) (>= length end))
-	do (setf remainder (cdr remainder))
-	   finally (unless (or (null remainder) (consp remainder))
-		     (error 'must-be-proper-list
-			 :name name
-			 :datum list))
-		   (when (< length end)
-		     (error 'invalid-end-index
-			    :name name
-			    :datum end
-			    :expect-type `(integer 0 ,length)
-			    :in-sequence list))
-		   (return remainder)))
-
-(defmacro process-list-elements-from-start-without-end
-    ((list-form cons-var) &body body)
-  `(loop for ,cons-var = ,list-form then (cdr ,cons-var)
-	 while (consp ,cons-var)
-	 do ,@body
-	 finally (unless (null ,cons-var)
-		   (error "Not a proper list"))))
-
 ;;; This function calls KEY-FUNCTION with ELEMENT as the only
 ;;; argument.  It is semantically equivalent to FUNCALL except that
 ;;; only functions are allowed, and not any other function
@@ -245,21 +135,6 @@
 					 (,index-var ,iter-var))
 				     (declare (ignorable ,index-var))
 				     ,@body)))))))))
-
-(defmacro canonicalize-test-and-test-not (test-var test-not-var caller-name)
-  `(cond ((and (not (null ,test-var)) (not (null ,test-not-var)))
-	  (error 'both-test-and-test-not-given
-		 :name ',caller-name))
-	 ((null ,test-var)
-	  (cond ((null ,test-not-var)
-		 (setf ,test-var #'eql))
-		((not (functionp ,test-not-var))
-		 (setf ,test-not-var (fdefinition ,test-not-var)))
-		(t nil)))
-	 ((null ,test-not-var)
-	  (unless (functionp ,test-var)
-	    (setf ,test-var (fdefinition ,test-var))))
-	 (t nil)))
 
 (defmacro with-test-and-test-not ((test-var test-not-var) &body body)
   `(cond ((null ,test-not-var)
@@ -425,21 +300,6 @@
 	     ((same-type-p ,type-var '(complex long-float))
 	      ,@body)))))
 
-(defun verify-bounding-indices-list (start end)
-  (unless (typep start 'fixnum)
-    (error 'invalid-start-index-type
-	   :datum start
-	   :expected-type 'fixnum))
-  (unless (typep end '(or null fixnum))
-    (error 'invalid-end-index-type
-	   :datum start
-	   :expected-type '(or null fixnum)))
-  (when (and (typep end 'fixnum)
-	     (< end start))
-    (error 'end-less-than-start
-	   :datum end
-	   :expected-type `(integer ,start))))
-
 ;;; This macro is used when the sequence is a list in order to check
 ;;; that the START and END parameters are valid.  BODY is duplicated
 ;;; in two different contexts, according to the type of the END
@@ -452,27 +312,6 @@
 	      (locally (declare (type fixnum ,start-var)
 				(type null ,end-var))
 		,@body))))
-
-;;; This function is used when the sequence is a vector of some kind
-;;; in order to verify that start and end are valid bounding indexes.
-(defun verify-bounding-indices-vector (vector start end)
-  (let ((length (length vector)))
-    (when (> start length)
-      (error 'invalid-start-index
-	     :datum start
-	     :expected-type `(integer 0 ,length)
-	     :in-sequence vector))
-    (unless (<= 0 end length)
-      (error 'invalid-end-index
-	     :datum end
-	     :expected-type `(integer 0 ,length)
-	     :in-sequence vector))
-    (unless (<= start end)
-      (error 'end-less-than-start
-	     :datum start
-	     :expected-type `(integer 0 ,end)
-	     :end-index end
-	     :in-sequence vector))))
 
 ;;; This macro is used when the sequence is a vector in order to check
 ;;; that the START and END parameters are valid.  BODY is duplicated
