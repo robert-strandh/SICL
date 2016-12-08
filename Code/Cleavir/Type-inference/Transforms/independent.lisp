972bf1b74c2edd9ef3347fffe9f74ab82080cab3
Bike <aeshtaer@gmail.com>
2016-12-07 20:03:27 -0800

    add unused transform to mark non-returning calls

--- Code/Cleavir/Type-inference/Transforms/independent.lisp
+++ Code/Cleavir/Type-inference/Transforms/independent.lisp
@@ -18,6 +18,33 @@
     (mapc #'cleavir-ir:delete-instruction death-row))
   initial)
 
+;; make some HIR that will signal a type error, that DATUM
+;;  (a lexical location) is not of type EXPECTED.
+;; The instructions will be self-contained and have the right
+;;  predecessor, but changing the predecessor's successor must be
+;;  done elsewhere. (Because you're probably change-class-ing it.)
+(defun make-type-error (predecessor datum expected)
+  ;; FIXME: programmatic HIR could probably be made nicer.
+  (let* ((fdef (cleavir-ir:new-temporary))
+	 (fdefinition-instruction
+	   (cleavir-ir:make-fdefinition-instruction
+	    (cleavir-ir:make-load-time-value-input ''cl:error)
+	    fdef))
+	 (fnr-instruction
+	   (cleavir-ir:make-funcall-no-return-instruction
+	    (list fdef
+		  (cleavir-ir:make-load-time-value-input ''cl:type-error)
+		  (cleavir-ir:make-load-time-value-input '':datum)
+		  datum
+		  (cleavir-ir:make-load-time-value-input '':expected-type)
+		  (cleavir-ir:make-load-time-value-input
+		   `',expected)))))
+    (cleavir-ir:insert-instruction-before fdefinition-instruction
+					  fnr-instruction)
+    (setf (cleavir-ir:predecessors fdefinition-instruction)
+	  (list predecessor))
+    fdefinition-instruction))
+
 ;;; TODO: SBCL has a middle setting on its insert-type-checks that
 ;;; downgrades types into easily checkable ones like FIXNUM.
 ;;; So a sanity check but not wholly correct. Might be nice.
@@ -27,33 +54,14 @@
 ;;; maybe convert a THE instruction into a TYPEQ instruction,
 ;;; depending on policy
 (defun the->typeq (the-instruction)
-  (let ((policy (cleavir-ir:policy the-instruction)))
-    (when (cleavir-policy:policy-value policy 'insert-type-checks)
-      ;; FIXME: programmatic HIR could probably be made nicer.
-      (let* ((cleavir-ir:*policy* policy) ; copy from base instr
-	     (fdef (cleavir-ir:new-temporary))
-	     (fdefinition-instruction
-	       (cleavir-ir:make-fdefinition-instruction
-		(cleavir-ir:make-load-time-value-input ''cl:error)
-		fdef))
-	     (fnr-instruction
-	       (cleavir-ir:make-funcall-no-return-instruction
-		(list fdef
-		      (cleavir-ir:make-load-time-value-input ''cl:type-error)
-		      (cleavir-ir:make-load-time-value-input '':datum)
+  (change-class
+   the-instruction 'cleavir-ir:typeq-instruction
+   :successors (list (first
+		      (cleavir-ir:successors the-instruction))
+		     (make-type-error
+		      the-instruction
 		      (first (cleavir-ir:inputs the-instruction))
-		      (cleavir-ir:make-load-time-value-input '':expected-type)
-		      (cleavir-ir:make-load-time-value-input
-		       `',(cleavir-ir:value-type the-instruction))))))
-	(cleavir-ir:insert-instruction-before fdefinition-instruction
-					      fnr-instruction)
-	(setf (cleavir-ir:predecessors fdefinition-instruction)
-	      (list the-instruction))
-	(change-class
-	 the-instruction 'cleavir-ir:typeq-instruction
-	 :successors (list (first
-			    (cleavir-ir:successors the-instruction))
-			   fdefinition-instruction))))))
+		      (cleavir-ir:value-type the-instruction)))))
 
 ;; convert all of them. intended for safe code, before type inference.
 (defun thes->typeqs (initial)
@@ -66,3 +74,49 @@
     (mapc #'the->typeq thes)
     ;; we only added some branches, so we don't need to reinitialize etc.
     initial))
+
+;; convert FUNCALL to FUNCALL-NO-RETURN where possible
+;; if SAFE-P, cause a type error if they do return
+;; (which will still cut it off from successors. good, yes?)
+(defun mark-noreturns (initial safe-p)
+  ;; this doesn't actually use inferred type information.
+  ;; it COULD, but i expect that calling a variable function that
+  ;;  must not return is not super common.
+  ;; the main value of this transform is to chop impossible
+  ;;  execution paths; we want to do this as early as possible to
+  ;;  improve the performance of other analyses.
+  ;; there could be another stage after type inference to do it for
+  ;;  variable functions if necessary.
+  (let (thevalueses)
+    (cleavir-ir:map-instructions-arbitrary-order
+     (lambda (i)
+       (when (typep i 'cleavir-ir:the-values-instruction)
+	 (push i thevalueses))))
+    (dolist (i thevalueses)
+      (when (member-if (lambda (type) (subtypep type nil))
+		     (cleavir-ir:required-types i))
+	;; we have a noreturn, can proceed
+	(let ((call (first (cleavir-ir:predecessors i))))
+	  ;; this condition should be always true from AST-to-HIR,
+	  ;;  but we might as well check.
+	  ;; in english, the THE-VALUES is the direct successor of
+	  ;;  a FUNCALL and defines the types of its results.
+	  (when (and (= (length (cleavir-ir:predecessors i)) 1)
+		     (typep call 'cleavir-ir:funcall-instruction)
+		     (eq (first (cleavir-ir:inputs i))
+			 (first (cleavir-ir:outputs call))))
+	    ;; ok, the actual transform
+	    (if safe-p
+		;; FIXME: abstract this?
+		(setf (cleavir-ir:successors call)
+		      ;; FIXME: "expected a value of type NIL" is
+		      ;;  not a good error message.
+		      (list (make-type-error
+			     i (first (cleavir-ir:inputs i)) nil)))
+		(change-class
+		 i 'cleavir-ir:funcall-no-return-instruction
+		 :successors nil :outputs nil)))))))
+  ;; paths are dead. make sure the IR is consistent.
+  (cleavir-ir:set-predecessors initial)
+  (cleavir-ir:reinitialize-data initial)
+  initial)
