4ad00eb6e90a3af717ec9816db51c285c5fe9dd6
Robert Strandh <robert.strandh@gmail.com>
2017-01-12 13:13:55 +0100

    Wrap body of all functions in call to new macro WITH-KEY-FUNCTION.

--- Code/Sequences/Common/count.lisp
+++ Code/Sequences/Common/count.lisp
@@ -4,15 +4,16 @@
   (declare (optimize (speed 3) (debug 0) (safety 3)))
   (declare (type list list))
   (with-bounding-indices-list (start end)
-    (with-test-and-test-not (test test-not)
-      (with-from-end from-end
-	(let ((result 0))
-	  (declare (type fixnum result))
-	  (for-each-relevant-cons (cons index list start end from-end)
-	    (let ((element (apply-key-function (car cons) key)))
-	      (when (satisfies-two-argument-test-p item element test test-not)
-		(incf result))))
-	  (return-from count-list result))))))
+    (with-key-function key
+      (with-test-and-test-not (test test-not)
+	(with-from-end from-end
+	  (let ((result 0))
+	    (declare (type fixnum result))
+	    (for-each-relevant-cons (cons index list start end from-end)
+	      (let ((element (apply-key-function (car cons) key)))
+		(when (satisfies-two-argument-test-p item element test test-not)
+		  (incf result))))
+	    (return-from count-list result)))))))
 
 ;;; A version of COUNT, specialized to a vector.  ITEM is the item to
 ;;; count.  VECTOR is the vector to search.  FROM-END is a generalized
@@ -24,16 +25,17 @@
 (defun count-vector (item vector from-end test test-not start end key)
   (declare (optimize (speed 3) (debug 0) (safety 3)))
   (declare (type fixnum start end))
-  (with-test-and-test-not (test test-not)
-    (with-from-end from-end
-      (with-vector-type vector
-	(let ((result 0))
-	  (declare (type fixnum result))
-	  (for-each-relevant-element (e index vector start end from-end)
-	    (let ((element (apply-key-function e key)))
-	      (when (satisfies-two-argument-test-p item element test test-not)
-		(incf result))))
-	  (return-from count-vector result))))))
+  (with-key-function key
+    (with-test-and-test-not (test test-not)
+      (with-from-end from-end
+	(with-vector-type vector
+	  (let ((result 0))
+	    (declare (type fixnum result))
+	    (for-each-relevant-element (e index vector start end from-end)
+	      (let ((element (apply-key-function e key)))
+		(when (satisfies-two-argument-test-p item element test test-not)
+		  (incf result))))
+	    (return-from count-vector result)))))))
 
 (defun count-aux (item sequence from-end test test-not start end key)
   ;; (declare (optimize (debug 0) (speed 3) (safety 0))
@@ -60,51 +62,55 @@
 (defun count-if-list (predicate list from-end start end key)
   (declare (optimize (speed 3) (debug 0) (safety 3)))
   (declare (type function predicate))
-  (with-from-end from-end
-    (let ((result 0))
-      (declare (type fixnum result))
-      (for-each-relevant-cons (cons index list start end from-end)
-	(let ((element (apply-key-function (car cons) key)))
-	  (when (funcall predicate element)
-	    (incf result))))
-      (return-from count-if-list result))))
+  (with-key-function key
+    (with-from-end from-end
+      (let ((result 0))
+	(declare (type fixnum result))
+	(for-each-relevant-cons (cons index list start end from-end)
+	  (let ((element (apply-key-function (car cons) key)))
+	    (when (funcall predicate element)
+	      (incf result))))
+	(return-from count-if-list result)))))
 
 (defun count-if-vector (predicate vector from-end start end key)
   (declare (optimize (speed 3) (debug 0) (safety 3)))
   (declare (type fixnum start end))
   (declare (type function predicate))
-  (with-from-end from-end
-    (with-vector-type vector
-      (let ((result 0))
-	(declare (type fixnum result))
-	(for-each-relevant-element (e index vector start end from-end)
-	  (let ((element (apply-key-function e key)))
-	    (when (funcall predicate element)
-	      (incf result))))
-	(return-from count-if-vector result)))))
+  (with-key-function key
+    (with-from-end from-end
+      (with-vector-type vector
+	(let ((result 0))
+	  (declare (type fixnum result))
+	  (for-each-relevant-element (e index vector start end from-end)
+	    (let ((element (apply-key-function e key)))
+	      (when (funcall predicate element)
+		(incf result))))
+	  (return-from count-if-vector result))))))
 
 (defun count-if-not-list (predicate list from-end start end key)
   (declare (optimize (speed 3) (debug 0) (safety 3)))
   (declare (type function predicate))
-  (with-from-end from-end
-    (let ((result 0))
-      (declare (type fixnum result))
-      (for-each-relevant-cons (cons index list start end from-end)
-	(let ((element (apply-key-function (car cons) key)))
-	  (unless (funcall predicate element)
-	    (incf result))))
-      (return-from count-if-not-list result))))
+  (with-key-function key
+    (with-from-end from-end
+      (let ((result 0))
+	(declare (type fixnum result))
+	(for-each-relevant-cons (cons index list start end from-end)
+	  (let ((element (apply-key-function (car cons) key)))
+	    (unless (funcall predicate element)
+	      (incf result))))
+	(return-from count-if-not-list result)))))
 
 (defun count-if-not-vector (predicate vector from-end start end key)
   (declare (optimize (speed 3) (debug 0) (safety 3)))
   (declare (type fixnum start end))
   (declare (type function predicate))
-  (with-from-end from-end
-    (with-vector-type vector
-      (let ((result 0))
-	(declare (type fixnum result))
-	(for-each-relevant-element (e index vector start end from-end)
-	  (let ((element (apply-key-function e key)))
-	    (unless (funcall predicate element)
-	      (incf result))))
-	(return-from count-if-not-vector result)))))
+  (with-key-function key
+    (with-from-end from-end
+      (with-vector-type vector
+	(let ((result 0))
+	  (declare (type fixnum result))
+	  (for-each-relevant-element (e index vector start end from-end)
+	    (let ((element (apply-key-function e key)))
+	      (unless (funcall predicate element)
+		(incf result))))
+	  (return-from count-if-not-vector result))))))
