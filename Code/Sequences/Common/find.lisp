13901c14fe7622571a1670e9ed28faafb0c6636e
Robert Strandh <robert.strandh@gmail.com>
2017-01-12 13:07:17 +0100

    Wrap body of all functions in call to new macro WITH-KEY-FUNCTION.

--- Code/Sequences/Common/find.lisp
+++ Code/Sequences/Common/find.lisp
@@ -6,12 +6,13 @@
   (declare (optimize (speed 3) (debug 0) (safety 0)))
   (declare (type list list))
   (with-bounding-indices-list (start end)
-    (with-test-and-test-not (test test-not)
-      (with-from-end from-end
-	(for-each-relevant-cons (cons index list start end from-end)
-	  (let ((element (apply-key-function (car cons) key)))
-	    (when (satisfies-two-argument-test-p item element test test-not)
-	      (return-from find-list element))))))))
+    (with-key-function key
+      (with-test-and-test-not (test test-not)
+	(with-from-end from-end
+	  (for-each-relevant-cons (cons index list start end from-end)
+	    (let ((element (apply-key-function (car cons) key)))
+	      (when (satisfies-two-argument-test-p item element test test-not)
+		(return-from find-list element)))))))))
 
 ;;; A version of FIND, specialized to a vector.  ITEM is the item to
 ;;; find.  VECTOR is the vector to search.  FROM-END is a generalized
@@ -23,13 +24,14 @@
 (defun find-vector (item vector from-end test test-not start end key)
   (declare (optimize (speed 3) (debug 0) (safety 3)))
   (declare (type fixnum start end))
-  (with-test-and-test-not (test test-not)
-    (with-from-end from-end
-      (with-vector-type vector
-	(for-each-relevant-element (e index vector start end from-end)
-	  (let ((element (apply-key-function e key)))
-	    (when (satisfies-two-argument-test-p item element test test-not)
-	      (return-from find-vector element))))))))
+  (with-key-function key
+    (with-test-and-test-not (test test-not)
+      (with-from-end from-end
+	(with-vector-type vector
+	  (for-each-relevant-element (e index vector start end from-end)
+	    (let ((element (apply-key-function e key)))
+	      (when (satisfies-two-argument-test-p item element test test-not)
+		(return-from find-vector element)))))))))
 
 (defun find-aux (item sequence from-end test test-not start end key)
   ;; (declare (optimize (debug 0) (speed 3) (safety 0))
@@ -56,39 +58,43 @@
 (defun find-if-list (predicate list from-end start end key)
   (declare (optimize (speed 3) (debug 0) (safety 3)))
   (declare (type function predicate))
-  (with-from-end from-end
-    (for-each-relevant-cons (cons index list start end from-end)
-      (let ((element (apply-key-function (car cons) key)))
-	(when (funcall predicate element)
-	  (return-from find-if-list element))))))
+  (with-key-function key
+    (with-from-end from-end
+      (for-each-relevant-cons (cons index list start end from-end)
+	(let ((element (apply-key-function (car cons) key)))
+	  (when (funcall predicate element)
+	    (return-from find-if-list element)))))))
 
 (defun find-if-vector (predicate vector from-end start end key)
   (declare (optimize (speed 3) (debug 0) (safety 3)))
   (declare (type fixnum start end))
   (declare (type function predicate))
-  (with-from-end from-end
-    (with-vector-type vector
-      (for-each-relevant-element (e index vector start end from-end)
-	(let ((element (apply-key-function e key)))
-	  (when (funcall predicate element)
-	    (return-from find-if-vector element)))))))
+  (with-key-function key
+    (with-from-end from-end
+      (with-vector-type vector
+	(for-each-relevant-element (e index vector start end from-end)
+	  (let ((element (apply-key-function e key)))
+	    (when (funcall predicate element)
+	      (return-from find-if-vector element))))))))
 
 (defun find-if-not-list (predicate list from-end start end key)
   (declare (optimize (speed 3) (debug 0) (safety 3)))
   (declare (type function predicate))
-  (with-from-end from-end
-    (for-each-relevant-cons (cons index list start end from-end)
-      (let ((element (apply-key-function (car cons) key)))
-	(unless (funcall predicate element)
-	  (return-from find-if-not-list element))))))
+  (with-key-function key
+    (with-from-end from-end
+      (for-each-relevant-cons (cons index list start end from-end)
+	(let ((element (apply-key-function (car cons) key)))
+	  (unless (funcall predicate element)
+	    (return-from find-if-not-list element)))))))
 
 (defun find-if-not-vector (predicate vector from-end start end key)
   (declare (optimize (speed 3) (debug 0) (safety 3)))
   (declare (type fixnum start end))
   (declare (type function predicate))
-  (with-from-end from-end
-    (with-vector-type vector
-      (for-each-relevant-element (e index vector start end from-end)
-	(let ((element (apply-key-function e key)))
-	  (unless (funcall predicate element)
-	    (return-from find-if-not-vector element)))))))
+  (with-key-function key
+    (with-from-end from-end
+      (with-vector-type vector
+	(for-each-relevant-element (e index vector start end from-end)
+	  (let ((element (apply-key-function e key)))
+	    (unless (funcall predicate element)
+	      (return-from find-if-not-vector element))))))))
