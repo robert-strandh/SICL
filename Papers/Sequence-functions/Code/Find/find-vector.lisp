b6e170689adae1ce1221b6a90b9955eba67ce4a2
idurand <idurand@labri.fr>
2017-01-18 08:18:44 +0100

    several element-types and test-functions

--- Papers/Sequence-functions/Code/Find/find-vector.lisp
+++ Papers/Sequence-functions/Code/Find/find-vector.lisp
@@ -1,58 +1,7 @@
 (in-package :find)
 
-(deftype simple-byte-vector ()
-  '(simple-array (unsigned-byte)))
+ (defun find-vector-0 (item vector test)
+   (find item vector :test test))
 
-(defmacro with-vector-type (vector-var &body body)
-  `(macrolet ((vref (array index)
-                `(aref ,array ,index)))
-     (cond
-       ((typep ,vector-var 'simple-byte-vector)
-         (locally (declare (type simple-byte-vector ,vector-var))
-           ,@body))
-       ((typep ,vector-var 'simple-bit-vector)
-	(locally (declare (type simple-bit-vector ,vector-var))
-	  ,@body))
-       (t
-         (progn
-           ,@body)))))
-
- (defun find-vector-0 (item vector)
-   (find item vector :test #'eq))
-
- (defun find-vector-1 (item vector)
-   (sicl-sequence::find-vector item vector nil #'eq nil 0 (length vector) #'identity))
-
-;; (defun find-vector-1 (item vector)
-;;   (declare (optimize (speed 3) (debug 0) (safety 0)))
-;;   (loop for index from 0 below (length vector)
-;;         for element = (aref vector index)
-;;         when (eql item element)
-;;           return element))
-
-;; (defun find-vector-2 (item vector)
-;;   (declare (optimize (speed 3) (debug 0) (safety 0)))
-;;   (cond
-;;     ((typep vector 'simple-byte-vector)
-;;      (loop for index from 0 below (length vector)
-;; 	   for element = (aref vector index)
-;; 	   when (eql item element)
-;; 	     return element))
-;;     ((typep vector 'simple-bit-vector)
-;;      (loop for index from 0 below (length vector)
-;; 	   for element = (aref vector index)
-;; 	   when (eql item element)
-;; 	     return element))
-;;     (t
-;;      (loop for index from 0 below (length vector)
-;; 	   for element = (aref vector index)
-;; 	   when (eql item element)
-;; 	     return element))))
-
-;; (defun find-vector-4 (item vector)
-;;   (declare (optimize (speed 3) (debug 0) (safety 0)))
-;;   (with-vector-type vector
-;;     (loop for index from 0 below (length vector)
-;;           for element = (vref vector index)
-;;           when (eql item element)
-;;             return element)))
+ (defun find-vector-1 (item vector test)
+   (sicl-sequence::find-vector item vector nil test nil 0 (length vector) #'identity))
--- Papers/Sequence-functions/Code/Find/tests.lisp
+++ Papers/Sequence-functions/Code/Find/tests.lisp
@@ -1,24 +1,25 @@
 (in-package :find)
 
-(defvar *the-value* 1)
-(defvar *the-other-value* 0)
+(defun the-values (element-type)
+  (case element-type
+    (symbol  (values 'b 'a))
+    (char (values #\b #\a))
+    (t (values 1 0))))
 
-(setq *the-value* 1)
+(defun make-vector (n element-type)
+  (multiple-value-bind (the-value the-other-value) (the-values element-type)
+    (let ((vector (make-array n :element-type element-type :initial-element the-other-value)))
+      (setf (aref vector (1- n)) the-value)
+      vector)))
 
-(defun make-sbv (n &key (element-type '(unsigned-byte 8)))
-  (let ((vector
-	  (make-array n :element-type element-type)))
-    (setf (aref vector (1- n)) *the-value*)
-    vector))
+(defun adjust-vector (n vector)
+  (make-vector (+ (length vector) n) (array-element-type vector)))
 
-(defun adjust-sbv (n vector)
-  (make-sbv (+ (length vector) n) :element-type (array-element-type vector)))
+(defun test-call (find-fun the-value vector test)
+  (funcall find-fun the-value vector test))
 
-(defun test-call (find-fun vector)
-  (funcall find-fun *the-value* vector))
-
-(defun evaluate-test-call (find-fun vector times)
-  (evaluate-time (test-call find-fun vector) times))
+(defun evaluate-test-call (find-fun the-value vector test times)
+  (evaluate-time (test-call find-fun the-value vector test) times))
 
 (defun version-symbol (version-number)
   (find-symbol (format nil "FIND-VECTOR-~A" version-number)
@@ -27,75 +28,73 @@
 (defun version-symbols (version-numbers)
   (mapcar #'version-symbol version-numbers))
 
-(defun header (stream versions element-type)
+(defun header (stream versions element-type test-symbol)
   (format stream "Element-type: ~A~%" element-type)
-  (format stream "N~15T")
-  (format t "~{~14,A~}~%" versions)
+  (format stream "Test: ~A~%" test-symbol)
+  (format stream " N~15T")
+  (format stream "~{~14,A~}~%" versions)
   )
-  
-(defun compare-versions-to-stream (stream versions element-type start end &key (step 1) (times 1))
-  (header stream versions element-type)
+
+(defun compare-versions-to-stream (stream versions element-type test-symbol start end &key (step 1) (times 1))
+  (header stream versions element-type test-symbol)
   (loop
+    with test = (symbol-function test-symbol)
+    with the-value = (the-value element-type)
     for k from start to end by step
-    for vector = (make-sbv start :element-type element-type) 
-      then (adjust-sbv step vector)
+    for vector = (make-vector start element-type) 
+      then (adjust-vector step vector)
     do (format stream "~8D" k)
     do (when *trace* (format t "~D~14T" k))
     do (loop for version in (mapcar #'symbol-function versions)
-			 do (let ((version-time (evaluate-test-call version vector times)))
-			      (format stream "~14,5F" version-time)
-			      (when *trace*
-				(format t "~14,5F" version-time))
-			      )
+	     do (let ((version-time (evaluate-test-call version the-value vector test times)))
+		  (format stream "~14,5F" version-time)
+		  (when *trace*
+		    (format t "~14,5F" version-time))
+		  )
 	     finally (progn (format stream "~%")
 			    (when *trace* (format t "~%") (finish-output t))
 			    (finish-output stream)))))
 
-(defun compare-version-numbers-to-stream (stream version-numbers element-type start end &key (step 1) (times 1))
+(defun compare-version-numbers-to-stream (stream version-numbers element-type test-symbol start end &key (step 1) (times 1))
   (compare-versions-to-stream 
-   stream (version-symbols version-numbers)
+   stream
+   (version-symbols version-numbers)
    element-type
+   test-symbol
    start end :step step :times times))
 
-(defun compare-version-numbers-to-file (file version-numbers element-type start end &key (step 1) (times 1))
+(defun compare-version-numbers-to-file (file version-numbers element-type test-symbol start end &key (step 1) (times 1))
   (with-open-file (stream file :direction :output :if-does-not-exist :create :if-exists :supersede)
     (compare-version-numbers-to-stream
      stream
      version-numbers
-     element-type start end :step step :times times)))
+     element-type test-symbol start end :step step :times times)))
 
-(defvar *local*)
-(setq *local* nil)
-(defun make-test-filename (&optional (prefix ""))
-  (if *local* 
+(defun make-test-filename (element-type test-symbol &optional (prefix ""))
+  (format nil "~A:~A:~A:~A-~A-~A.res"
 	  prefix
-	  (format nil "~A:~A-~A-~A.res"
-			  prefix
-			  (cl:lisp-implementation-type)
-			  (cl:lisp-implementation-version) 
-			  (machine-version))))
-		  
-(defun compare-version-numbers (version-numbers element-type start end &key (step 1) (times 1))
-  (let ((name (format nil "v~A" (first version-numbers))))
-	(loop
-	  for n in (rest version-numbers)
-	  do (setq name (format nil "~A-v~A" name n)))
-	(let ((filename (make-test-filename name)))
-	  (format t "Ouput written to ~A~%" filename)
-	  (finish-output t)
-	  (compare-version-numbers-to-file filename version-numbers element-type start end :step step :times times))))
+	  element-type
+	  test-symbol
+	  (cl:lisp-implementation-type)
+	  (cl:lisp-implementation-version) 
+	  (machine-version)))
 
-(defun test-version-numbers (element-type &rest version-numbers)
-  (compare-version-numbers version-numbers element-type 10000 10000000 :step 100000 :times 2))
+(defun compare-version-numbers (version-numbers element-type test-symbol start end &key (step 1) (times 1))
+  (let ((name (format nil "v~A" (first version-numbers))))
+    (loop
+      for n in (rest version-numbers)
+      do (setq name (format nil "~A-v~A" name n)))
+    (let ((filename (make-test-filename element-type test-symbol name)))
+      (format t "Ouput written to ~A~%" filename)
+      (finish-output t)
+      (compare-version-numbers-to-file filename version-numbers element-type test-symbol start end :step step :times times))))
+
+(defun test-version-numbers (element-type test-symbol &rest version-numbers)
+  (compare-version-numbers version-numbers element-type test-symbol 10000 1000000 :step 100000 :times 2))
 
 (defun the-test ()
-  (test-version-numbers 0 1 7))
-
-(defun the-test2 ()
-  (test-version-numbers 7 11))
-
-;; CL-USER> (mouline-truc-versus-machin-one-file "v1-vs-v4" #'find-vector-1 #'reverse-count-4 1000 1000000 :step 1000 :times 30)
-;; 1000    0.00000    0.03333
-;; 2000    0.03333    0.06667
-;; 3000    0.06667    0.10000
-;; 4000    0.06667    0.13333
+  (test-version-numbers 'bit '= 0 1)
+  (test-version-numbers 'symbol 'eq 0 1)
+  (test-version-numbers 'char 'eql 0 1)
+  (test-version-numbers '(unsigned-byte 8) '= 0 1)
+  )
