6aa11fb7657074c975fe92e1e408de23a8ff4410
idurand <idurand@labri.fr>
2017-01-20 22:35:06 +0100

    debut de courbes - plein de choses a corriger

--- Papers/Sequence-functions/curves.sh
+++ Papers/Sequence-functions/curves.sh
@@ -15,33 +15,37 @@ TITLE="Comparison between two find functions (SBCL/SICL)"
 echo gp=$GP
 OUTPUT=eps
 
+SAVE=$IFS
+IFS='-'
+set
+DATATYPE=$1
+TEST=$2
+TYPE=$3
+KEY=$4
+IFS=$SAVE
+
 cat << _EOF_ > $GP
 set term $OUTPUT dashed
 set output "$NAME.$OUTPUT"
 set title "$TITLE"
 set key on inside center top
-set xlabel "List length"
+set xlabel "$DATATYPE length ($TEST $TYPE $KEY)"
 set ylabel "Time in seconds"
 _EOF_
 
 STRING="'"$NAME"'"
 echo STRING=$STRING
 
-SAVE-IFS=$IFS
-
-IFS='-'
-set
-TYPE=$0
-TEST=$1
-KEY=$2
-IFS=$SAVE-IFS
 
 echo plot '\' >> $GP
 
-N=2
-for i in $FIELDS ;
-do 
-	echo FIELD=$i N=$N
-	echo "    " $STRING using 1:$N title \'$i\' with lines ',\' >> $GP
-	N=$(expr $N + 1)
-done
+echo "    " $STRING using 1:2 title \'SBCL\' with lines ',\' >> $GP
+echo "    " $STRING using 1:3 title \'SICL\' with lines ',\' >> $GP
+
+# N=2
+# for i in $FIELDS ;
+# do 
+# 	echo FIELD=$i N=$N
+# 	echo "    " $STRING using 1:$N title \'$i\' with lines ',\' >> $GP
+# 	N=$(expr $N + 1)
+# done
