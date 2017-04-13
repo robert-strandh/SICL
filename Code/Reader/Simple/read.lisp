d4d145288f9b981e0e31e80d3522bb0239fd4cd2
Robert Strandh <robert.strandh@gmail.com>
2017-04-13 08:07:15 +0200

    Define function READ-PRESERVING-WHITESPACE.

--- Code/Reader/Simple/read.lisp
+++ Code/Reader/Simple/read.lisp
@@ -7,3 +7,12 @@
 	       (recursive-p nil))
   (let ((*preserve-whitespace* recursive-p))
     (read-common input-stream eof-error-p eof-value)))
+
+(defun read-preserving-whitespace (&optional
+                                     (input-stream *standard-input*)
+                                     (eof-error-p t)
+                                     (eof-value nil)
+                                     (recursive-p nil))
+  (declare (ignore recursive-p))
+  (let ((*preserve-whitespace* t))
+    (read-common input-stream eof-error-p eof-value)))
