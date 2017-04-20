3cd47ef7980d85604ee4a130ec2838e6f3c158a3
Robert Strandh <robert.strandh@gmail.com>
2017-04-20 06:42:01 +0200

    Untabify for better tool support.

--- Code/Character/character.lisp
+++ Code/Character/character.lisp
@@ -10,30 +10,30 @@
   (if (null (cdr characters))
       t
       (loop for rest = (cdr characters) then (cdr rest)
-	    while (consp rest)
-	    for char1 = (car characters) then char2
-	    for char2 = (car rest)
-	    unless (binary-char= char1 char2)
-	      return nil
-	    finally (return t))))
+            while (consp rest)
+            for char1 = (car characters) then char2
+            for char2 = (car rest)
+            unless (binary-char= char1 char2)
+              return nil
+            finally (return t))))
  
 (proclaim '(notinline char=))
 
 (define-compiler-macro char= (&whole form &rest arguments)
   (cond ((not (and (cleavir-code-utilities:proper-list-p arguments)
-	       (>= (length arguments) 1)))
-	 form)
-	((= (length arguments) 1)
-	 `(characterp ,(car arguments)))
-	(t (let* ((vars (loop for argument in arguments collect (gensym))))
-	     `(let ,(loop for var in vars
-			  for arg in arguments
-			  collect `(,var ,arg))
-		(and ,@(loop for rest = (cdr vars) then (cdr rest)
-			     while (consp rest)
-			     for var1 = (car vars) then var2
-			     for var2 = (car rest)
-			     collect `(binary-char= ,var1 ,var2))))))))
+               (>= (length arguments) 1)))
+         form)
+        ((= (length arguments) 1)
+         `(characterp ,(car arguments)))
+        (t (let* ((vars (loop for argument in arguments collect (gensym))))
+             `(let ,(loop for var in vars
+                          for arg in arguments
+                          collect `(,var ,arg))
+                (and ,@(loop for rest = (cdr vars) then (cdr rest)
+                             while (consp rest)
+                             for var1 = (car vars) then var2
+                             for var2 = (car rest)
+                             collect `(binary-char= ,var1 ,var2))))))))
  
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;
@@ -49,30 +49,30 @@
   (if (null (cdr characters))
       t
       (loop for rest = (cdr characters) then (cdr rest)
-	    while (consp rest)
-	    for char1 = (car characters) then char2
-	    for char2 = (car rest)
-	    unless (binary-char< char1 char2)
-	      return nil
-	    finally (return t))))
+            while (consp rest)
+            for char1 = (car characters) then char2
+            for char2 = (car rest)
+            unless (binary-char< char1 char2)
+              return nil
+            finally (return t))))
  
 (proclaim '(notinline char<))
 
 (define-compiler-macro char< (&whole form &rest arguments)
   (cond ((not (and (cleavir-code-utilities:proper-list-p arguments)
-	       (>= (length arguments) 1)))
-	 form)
-	((= (length arguments) 1)
-	 `(characterp ,(car arguments)))
-	(t (let* ((vars (loop for argument in arguments collect (gensym))))
-	     `(let ,(loop for var in vars
-			  for arg in arguments
-			  collect `(,var ,arg))
-		(and ,@(loop for rest = (cdr vars) then (cdr rest)
-			     while (consp rest)
-			     for var1 = (car vars) then var2
-			     for var2 = (car rest)
-			     collect `(binary-char< ,var1 ,var2))))))))
+               (>= (length arguments) 1)))
+         form)
+        ((= (length arguments) 1)
+         `(characterp ,(car arguments)))
+        (t (let* ((vars (loop for argument in arguments collect (gensym))))
+             `(let ,(loop for var in vars
+                          for arg in arguments
+                          collect `(,var ,arg))
+                (and ,@(loop for rest = (cdr vars) then (cdr rest)
+                             while (consp rest)
+                             for var1 = (car vars) then var2
+                             for var2 = (car rest)
+                             collect `(binary-char< ,var1 ,var2))))))))
  
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;
@@ -84,30 +84,30 @@
   (if (null (cdr characters))
       t
       (loop for rest = (cdr characters) then (cdr rest)
-	    while (consp rest)
-	    for char1 = (car characters) then char2
-	    for char2 = (car rest)
-	    unless (binary-char<= char1 char2)
-	      return nil
-	    finally (return t))))
+            while (consp rest)
+            for char1 = (car characters) then char2
+            for char2 = (car rest)
+            unless (binary-char<= char1 char2)
+              return nil
+            finally (return t))))
  
 (proclaim '(notinline char<=))
 
 (define-compiler-macro char<= (&whole form &rest arguments)
   (cond ((not (and (cleavir-code-utilities:proper-list-p arguments)
-	       (>= (length arguments) 1)))
-	 form)
-	((= (length arguments) 1)
-	 `(characterp ,(car arguments)))
-	(t (let* ((vars (loop for argument in arguments collect (gensym))))
-	     `(let ,(loop for var in vars
-			  for arg in arguments
-			  collect `(,var ,arg))
-		(and ,@(loop for rest = (cdr vars) then (cdr rest)
-			     while (consp rest)
-			     for var1 = (car vars) then var2
-			     for var2 = (car rest)
-			     collect `(binary-char<= ,var1 ,var2))))))))
+               (>= (length arguments) 1)))
+         form)
+        ((= (length arguments) 1)
+         `(characterp ,(car arguments)))
+        (t (let* ((vars (loop for argument in arguments collect (gensym))))
+             `(let ,(loop for var in vars
+                          for arg in arguments
+                          collect `(,var ,arg))
+                (and ,@(loop for rest = (cdr vars) then (cdr rest)
+                             while (consp rest)
+                             for var1 = (car vars) then var2
+                             for var2 = (car rest)
+                             collect `(binary-char<= ,var1 ,var2))))))))
  
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;
@@ -119,30 +119,30 @@
   (if (null (cdr characters))
       t
       (loop for rest = (cdr characters) then (cdr rest)
-	    while (consp rest)
-	    for char1 = (car characters) then char2
-	    for char2 = (car rest)
-	    unless (binary-char> char1 char2)
-	      return nil
-	    finally (return t))))
+            while (consp rest)
+            for char1 = (car characters) then char2
+            for char2 = (car rest)
+            unless (binary-char> char1 char2)
+              return nil
+            finally (return t))))
  
 (proclaim '(notinline char>))
 
 (define-compiler-macro char> (&whole form &rest arguments)
   (cond ((not (and (cleavir-code-utilities:proper-list-p arguments)
-	       (>= (length arguments) 1)))
-	 form)
-	((= (length arguments) 1)
-	 `(characterp ,(car arguments)))
-	(t (let* ((vars (loop for argument in arguments collect (gensym))))
-	     `(let ,(loop for var in vars
-			  for arg in arguments
-			  collect `(,var ,arg))
-		(and ,@(loop for rest = (cdr vars) then (cdr rest)
-			     while (consp rest)
-			     for var1 = (car vars) then var2
-			     for var2 = (car rest)
-			     collect `(binary-char> ,var1 ,var2))))))))
+               (>= (length arguments) 1)))
+         form)
+        ((= (length arguments) 1)
+         `(characterp ,(car arguments)))
+        (t (let* ((vars (loop for argument in arguments collect (gensym))))
+             `(let ,(loop for var in vars
+                          for arg in arguments
+                          collect `(,var ,arg))
+                (and ,@(loop for rest = (cdr vars) then (cdr rest)
+                             while (consp rest)
+                             for var1 = (car vars) then var2
+                             for var2 = (car rest)
+                             collect `(binary-char> ,var1 ,var2))))))))
  
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;
@@ -154,27 +154,27 @@
   (if (null (cdr characters))
       t
       (loop for rest = (cdr characters) then (cdr rest)
-	    while (consp rest)
-	    for char1 = (car characters) then char2
-	    for char2 = (car rest)
-	    unless (binary-char>= char1 char2)
-	      return nil
-	    finally (return t))))
+            while (consp rest)
+            for char1 = (car characters) then char2
+            for char2 = (car rest)
+            unless (binary-char>= char1 char2)
+              return nil
+            finally (return t))))
  
 (proclaim '(notinline char>=))
 
 (define-compiler-macro char>= (&whole form &rest arguments)
   (cond ((not (and (cleavir-code-utilities:proper-list-p arguments)
-	       (>= (length arguments) 1)))
-	 form)
-	((= (length arguments) 1)
-	 `(characterp ,(car arguments)))
-	(t (let* ((vars (loop for argument in arguments collect (gensym))))
-	     `(let ,(loop for var in vars
-			  for arg in arguments
-			  collect `(,var ,arg))
-		(and ,@(loop for rest = (cdr vars) then (cdr rest)
-			     while (consp rest)
-			     for var1 = (car vars) then var2
-			     for var2 = (car rest)
-			     collect `(binary-char>= ,var1 ,var2))))))))
+               (>= (length arguments) 1)))
+         form)
+        ((= (length arguments) 1)
+         `(characterp ,(car arguments)))
+        (t (let* ((vars (loop for argument in arguments collect (gensym))))
+             `(let ,(loop for var in vars
+                          for arg in arguments
+                          collect `(,var ,arg))
+                (and ,@(loop for rest = (cdr vars) then (cdr rest)
+                             while (consp rest)
+                             for var1 = (car vars) then var2
+                             for var2 = (car rest)
+                             collect `(binary-char>= ,var1 ,var2))))))))
