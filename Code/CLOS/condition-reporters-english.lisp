d959f4240b4fe98c646d089b1f36c15a9e47a67f
Robert Strandh <robert.strandh@gmail.com>
2017-04-29 16:56:34 +0200

    Define English language condition reporter for new condition.

--- Code/CLOS/condition-reporters-english.lisp
+++ Code/CLOS/condition-reporters-english.lisp
@@ -1,9 +1,19 @@
 (cl:in-package #:sicl-clos)
 
-(defmethod acclimation:report-condition ((c class-name-must-be-non-nil-symbol)
-			     stream
-			     (language acclimation:english))
+(defmethod acclimation:report-condition
+    ((c class-name-must-be-non-nil-symbol)
+     stream
+     (language acclimation:english))
   (format stream
 	  "A class name must be a non-nil symbol, but~@
            ~s was found."
 	  (type-error-datum c)))
+
+(defmethod acclimation:report-condition
+    ((c attempt-to-add-existing-subclass)
+     stream
+     (language acclimation:english))
+  (format stream
+          "Attempt to add existing subclass ~s as a subclass of ~s."
+	   (subclass c)
+           (superclass c)))
