e4cab8cb0e87ecf731d993bf620f5c0474c6fc3b
Robert Strandh <robert.strandh@gmail.com>
2017-04-29 16:55:57 +0200

    Define new condition ATTEMPT-TO-ADD-EXISTING-SUBCLASS.

--- Code/CLOS/conditions.lisp
+++ Code/CLOS/conditions.lisp
@@ -4,3 +4,8 @@
     (sicl-additional-conditions:sicl-type-error)
   ()
   (:default-initargs :type '(and symbol (not null))))
+
+(define-condition attempt-to-add-existing-subclass
+    (sicl-additional-conditions:sicl-error)
+  ((%subclass :initarg :subclass :reader subclass)
+   (%superclass :initarg :superclass :reader superclass)))
