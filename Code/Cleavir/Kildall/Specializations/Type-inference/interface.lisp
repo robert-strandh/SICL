1e44a0781d0b54296db0137147081bd83005cea4
Bike <aeshtaer@gmail.com>
2017-06-19 12:10:48 -0400

    Kildall type inference: export THES->TYPEQS instead of running unconditionally

--- Code/Cleavir/Kildall/Specializations/Type-inference/interface.lisp
+++ Code/Cleavir/Kildall/Specializations/Type-inference/interface.lisp
@@ -2,8 +2,6 @@
 
 (defun infer-types (initial-instruction environment
                     &key prune draw liveness)
-  ;; controlled by policy (see insert-type-checks.lisp)
-  (thes->typeqs initial-instruction environment)
   (let* ((s (make-instance 'type-inference :env environment))
          (d (cleavir-kildall:kildall s initial-instruction
                                      :liveness liveness)))
@@ -13,30 +11,5 @@
       (cleavir-kildall-graphviz:draw-flowchart-with-inputs
        initial-instruction draw s d))
     (when prune
-      (prune-typeqs initial-instruction s d)
-      (prune-eqs initial-instruction s d))
-    nil
-    #+(or)
-    (function-type initial-instruction s d)))
-
-#+(or)
-(defun function-type (enter specialization dictionary)
-  (let* ((return (cleavir-kildall:entry-return
-                  (cleavir-kildall:find-entry
-                   specialization enter
-                   :key #'cleavir-kildall:entry-enter)))
-         (return-descriptor
-           (cleavir-kildall:find-in-pool specialization
-            (first (cleavir-ir:inputs return))
-            (cleavir-kildall:instruction-pool return dictionary)))
-         (return-type
-           ;; This assumes that no unboxed descriptors, which have
-           ;; no corresponding type specifiers, are involved.
-           (cleavir-type-descriptors:descriptor->specifier
-            return-descriptor (environment specialization)))
-         (lambda-list
-           (loop for thing in (cleavir-ir:lambda-list enter)
-                 when (symbolp thing) ; lambda list keyword
-                   collect thing
-                 else collect t)))
-    `(function ,lambda-list ,return-type)))
+      (prune-typeqs initial-instruction s d))
+    initial-instruction))
