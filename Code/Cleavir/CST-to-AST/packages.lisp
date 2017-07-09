ab99a6dedbc10f60f2b8a88376e5222c8ce7d4b9
Robert Strandh <robert.strandh@gmail.com>
2017-07-09 14:23:58 +0200

    Export names of new functions.

--- Code/Cleavir/CST-to-AST/packages.lisp
+++ Code/Cleavir/CST-to-AST/packages.lisp
@@ -2,4 +2,7 @@
 
 (defpackage #:cleavir-cst-to-ast
   (:use #:common-lisp)
-  (:export #:cst-to-ast))
+  (:export #:cst-to-ast
+           #:convert-constant
+           #:convert-constant-to-immediate
+           #:convert-special))
