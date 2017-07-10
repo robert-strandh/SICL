91663bb58880c5861c73b5b02d4c27706a05a778
Robert Strandh <robert.strandh@gmail.com>
2017-07-10 16:48:34 +0200

    Add definitions of functions CONVERT-LAMBDA-CALL and CONVERT-CODE.

--- Code/Cleavir/CST-to-AST/generic-functions.lisp
+++ Code/Cleavir/CST-to-AST/generic-functions.lisp
@@ -5,3 +5,7 @@
 (defgeneric convert-cst (cst info environment system))
 
 (defgeneric convert-special (head cst environment system))
+
+(defgeneric convert-lambda-call (cst env system))
+
+(defgeneric convert-code (lambda-list body-cst env system &optional block-name))
