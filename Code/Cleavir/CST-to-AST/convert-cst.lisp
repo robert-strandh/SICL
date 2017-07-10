93317f14e33fe006aeafe8bbcaa539fa78061266
Robert Strandh <robert.strandh@gmail.com>
2017-07-10 16:47:12 +0200

    New component containing definitions of methods on CONVERT-CST.

---
+++ Code/Cleavir/CST-to-AST/convert-cst.lisp
@@ -0,0 +1,5 @@
+(cl:in-package #:cleavir-cst-to-ast)
+
+(defmethod convert-cst
+    (cst (info cleavir-env:special-operator-info) env system)
+  (convert-special (car (cst:raw cst)) cst env system))
