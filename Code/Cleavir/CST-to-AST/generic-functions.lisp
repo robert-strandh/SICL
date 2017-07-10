97d86abddad859830d924dc36146e64840e41a3c
Robert Strandh <robert.strandh@gmail.com>
2017-07-10 11:02:01 +0200

    Define generic function CONVERT.

--- Code/Cleavir/CST-to-AST/generic-functions.lisp
+++ Code/Cleavir/CST-to-AST/generic-functions.lisp
@@ -1,5 +1,7 @@
 (cl:in-package #:cleavir-cst-to-ast)
 
+(defgeneric convert (cst environment system))
+
 (defgeneric convert-cst (cst info environment system))
 
 (defgeneric convert-special (head cst environment system))
