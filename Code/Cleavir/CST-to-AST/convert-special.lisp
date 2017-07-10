fc23d98fa922c7ba1064f4c8aa26c6858ca3461e
Robert Strandh <robert.strandh@gmail.com>
2017-07-10 11:04:21 +0200

    Define method on CONVERT-SPECIAL for converting BLOCK.

--- Code/Cleavir/CST-to-AST/convert-special.lisp
+++ Code/Cleavir/CST-to-AST/convert-special.lisp
@@ -9,3 +9,18 @@
   (cst:db s (quote const) cst
     (declare (ignore quote))
     (convert-constant const env system)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;; Converting BLOCK.
+
+(defmethod convert-special
+    ((symbol (eql 'block)) cst env system)
+  (cst:db origin (block name-cst . body-cst) cst
+    (declare (ignore block))
+    (let* ((ast (cleavir-ast:make-block-ast nil :origin origin))
+           (name (cst:raw name-cst))
+	   (new-env (cleavir-env:add-block env name ast)))
+      (setf (cleavir-ast:body-ast ast)
+	    (process-progn (convert-sequence body-cst new-env system)))
+      ast)))
