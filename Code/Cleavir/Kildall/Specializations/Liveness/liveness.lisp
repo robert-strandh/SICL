1a627618b19b4e6a3b6c56240d7f25f1491516e9
Bike <aeshtaer@gmail.com>
2017-07-11 13:44:28 -0400

    Cleavir liveness: switch back to old implementation
    I rewrote liveness with bit vectors. Testing this in clasp had it working, but
    less efficient. Given that efficiency was the only reason for the rewrite this
    was rather disheartening.
    
    Particularly: using bit vectors means that no allocation has to be done in the
    main Kildall loop. That's cool, but it means that each instruction is associated
    with a bit vector (its pool) that has one bit for every variable in the graph.
    This means that the bulk of the space required is the number of instructions
    times the number of variables.
    
    This is contrasted with the old implementation that just collected lists of
    variables. Overall the bit vector implementation trades space for speed. I
    expected that the use of bits instead of object references would make the
    space usage tolerable, and perhaps even result in an overall gain, but I was
    mistaken.
    
    A few basic tests with Clasp showed that the slow-to-compile code would have a
    few hundred instructions (normal) and tens of thousands of variables (eh). This
    easily results in hundreds of KB of storage, if not megabytes. Importantly,
    large parts had to be accessed within the main loop all at once. Probably
    defeated processor caching.
    
    This effect was obvious and severe. The bit vector implementation was not just a
    little slower. I thought that the compiler hung.
    
    It's possible that bit vectors could actually be more efficient, but only if
    there are far less variables (in normal code), and even then it would still get
    bad on code with a lot of variables. The fact is that at any given instruction,
    there are almost always less than a dozen or two live variables, so a bit vector
    of all variables will be very sparse.
    
    There are possible improvements to be made with this implementation. Sets as
    lists is not great, lots of O(n^2). But it might not be worth improving, at
    least not right off.

--- Code/Cleavir/Kildall/Specializations/Liveness/liveness.lisp
+++ Code/Cleavir/Kildall/Specializations/Liveness/liveness.lisp
@@ -12,111 +12,32 @@
      cleavir-kildall:start-everywhere-mixin)
   ())
 
-(defvar *variables-hash*)
-
-(defun compute-variables-hash (initial-instruction)
-  (let ((id 0)
-        (result (make-hash-table :test #'eq)))
-    (flet ((try (datum)
-             (when (and (cleavir-ir:variable-p datum)
-                        (not (nth-value 1 (gethash datum result))))
-               (setf (gethash datum result) id)
-               (incf id))))
-      (cleavir-ir:map-instructions-arbitrary-order
-       (lambda (i)
-         (mapc #'try (cleavir-ir:inputs i))
-         (mapc #'try (cleavir-ir:outputs i)))
-       initial-instruction))
-    result))
-
 (defmethod cleavir-kildall:make-pool
     ((s liveness-traverse) instruction)
   (declare (ignore instruction))
-  (make-array (hash-table-count *variables-hash*)
-              :element-type 'bit :initial-element 0))
-
-(defmethod find-in-pool ((s liveness-traverse) variable pool)
-  (sbit pool (gethash variable *variables-hash*)))
-
-(defvar *input-table*)
-(defvar *output-table*)
-
-(defun make-pool-from-vars (vars)
-  (let ((pool (make-array (hash-table-count *variables-hash*)
-                          :element-type 'bit :initial-element 0)))
-    (loop for var in vars
-          do (setf (sbit pool (gethash var *variables-hash*))
-                   1))
-    pool))
-
-(defun initialize-tables (initial-instruction)
-  (cleavir-ir:map-instructions-arbitrary-order
-   (lambda (i)
-     (setf (gethash i *input-table*)
-           (make-pool-from-vars
-            (remove-if-not #'cleavir-ir:variable-p
-                           (cleavir-ir:inputs i)))
-           (gethash i *output-table*)
-           (make-pool-from-vars
-            (remove-if-not #'cleavir-ir:variable-p
-                           (cleavir-ir:outputs i)))))
-   initial-instruction))
-
-(defun bits->list (pool)
-  (loop for var being the hash-keys of *variables-hash*
-          using (hash-value index)
-        unless (zerop (sbit pool index))
-          collect var))
-
-(defvar *scratch-pool*) ; note each thread would need one of each.
-(defvar *subbit-pool*)
-
-(defmethod cleavir-kildall:kildall :around
-    ((s liveness-traverse) initial-instruction &key)
-  (let* ((*variables-hash*
-           (compute-variables-hash initial-instruction))
-         (*input-table* (make-hash-table :test #'eq))
-         (*output-table* (make-hash-table :test #'eq))
-         (*scratch-pool*
-           (make-array (hash-table-count *variables-hash*)
-                       :element-type 'bit :initial-element 0))
-         (*subbit-pool*
-           (make-array (hash-table-count *variables-hash*)
-                       :element-type 'bit :initial-element 0)))
-    (initialize-tables initial-instruction)
-    (let ((result (call-next-method)))
-      ;; convert to list-set.
-      (maphash (lambda (var pool)
-                 (setf (gethash var result)
-                       (bits->list pool)))
-               result)
-      result)))
+  nil)
 
 (defmacro do-all-predecessors ((name instruction) &body body)
   `(loop for ,name in (cleavir-ir:predecessors ,instruction)
          do (tagbody ,@body)))
 
-(defun subbitp (bv1 bv2)
-  ;; this seems like a dumb way to find zero-ness, but I think
-  ;; it's the only game in town for bit vectors?
-  (every #'zerop (bit-andc2 bv1 bv2 *subbit-pool*)))
-
 (defmethod cleavir-kildall:transfer
     ((s liveness-traverse) instruction)
-  (declare (optimize speed space))
-  (let ((pool
-          (cleavir-kildall:maybe-instruction-pool s instruction))
-        (input (gethash instruction *input-table*))
-        (output (gethash instruction *output-table*)))
-    (declare (type simple-bit-vector pool input output
-                   *scratch-pool*))
-    (bit-andc2 pool output *scratch-pool*)
-    (bit-ior *scratch-pool* input t) ; t = write into scratch
+  (let* ((pool (cleavir-kildall:maybe-instruction-pool s instruction))
+         (inputs
+           (remove-if-not #'cleavir-ir:variable-p
+                          (cleavir-ir:inputs instruction)))
+         (outputs
+           (remove-if-not #'cleavir-ir:variable-p
+                          (cleavir-ir:outputs instruction)))
+         ;; we could use nunion, but this is probably not
+         ;; going to be that fast anyway.
+         (new (union inputs (set-difference pool outputs))))
     (do-all-predecessors (pred instruction)
-      (let ((dest (cleavir-kildall:maybe-instruction-pool s pred)))
-        (declare (type simple-bit-vector dest))
-        (unless (subbitp *scratch-pool* dest)
-          (replace dest *scratch-pool*)
+      (let ((old (cleavir-kildall:maybe-instruction-pool s pred)))
+        (unless (subsetp new old)
+          (setf (cleavir-kildall:dictionary-pool pred)
+                (union new old))
           (cleavir-kildall:add-work pred))))))
 
 (defun liveness (initial-instruction)
@@ -125,7 +46,7 @@
 	 (before (make-hash-table :test #'eq)))
     (cleavir-ir:map-instructions-arbitrary-order
      (lambda (i)
-       (let* ((after-set (gethash i after))
+       (let* ((after-set (cleavir-kildall:instruction-pool i after))
               (before-set
                 (union (set-difference
                         after-set
@@ -141,4 +62,4 @@
   (gethash node (before liveness)))
 
 (defun live-after (liveness node)
-  (gethash node (after liveness)))
+  (cleavir-kildall:instruction-pool node (after liveness)))
