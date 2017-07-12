0783579dd869937938d7f3c3306e3290c1d2037d
Robert Strandh <robert.strandh@gmail.com>
2017-07-12 06:13:42 +0200

    Rename parameter and clarify in comment that the defintion is a CST.

--- Code/Cleavir/CST-to-AST/convert-special.lisp
+++ Code/Cleavir/CST-to-AST/convert-special.lisp
@@ -111,11 +111,12 @@
 ;;;
 ;;; Converting FLET.
 
-;;; Take an environment and a single function definition, and return a
-;;; new environment which is like the one passed as an argument except
-;;; that it has been augmented by the local function name.
-(defun augment-environment-from-fdef (environment definition)
-  (cst:db origin (name . rest) definition
+;;; Take an environment and the CST of a single function definition,
+;;; and return a new environment which is like the one passed as an
+;;; argument except that it has been augmented by the local function
+;;; name.
+(defun augment-environment-from-fdef (environment definition-cst)
+  (cst:db origin (name . rest) definition-cst
     (declare (ignore rest))
     (let* ((raw-name (cst:raw name))
            (var-ast (cleavir-ast:make-lexical-ast raw-name
