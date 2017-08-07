701e08675f00796ee0bcc13dba47a8759338a2c2
Robert Strandh <robert.strandh@gmail.com>
2017-08-07 09:23:00 +0200

    Remove obsolete function ENVIRONMENT-INFO.

--- Code/Cleavir/CST-to-AST/environment-query.lisp
+++ Code/Cleavir/CST-to-AST/environment-query.lisp
@@ -1,25 +1,5 @@
 (cl:in-package #:cleavir-cst-to-ast)
 
-(defun variable-info (environment symbol)
-  (let ((result (cleavir-env:variable-info environment symbol)))
-    (loop while (null result)
-	  do (restart-case (error 'cleavir-env:no-variable-info
-				  :name symbol)
-	       (consider-special ()
-		 :report (lambda (stream)
-			   (format stream "Consider the variable as special."))
-		 (return-from variable-info
-		   (make-instance 'cleavir-env:special-variable-info
-		     :name symbol)))
-	       (substitute (new-symbol)
-		 :report (lambda (stream)
-			   (format stream "Substitute a different name."))
-		 :interactive (lambda ()
-				(format *query-io* "Enter new name: ")
-				(list (read *query-io*)))
-		 (setq result (cleavir-env:variable-info environment new-symbol)))))
-    result))
-
 (defun function-info (environment function-name)
   (let ((result (cleavir-env:function-info environment function-name)))
     (loop while (null result)
