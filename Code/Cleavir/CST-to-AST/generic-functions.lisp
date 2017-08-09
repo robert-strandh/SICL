5364c94d55c4b318898a6bbf92b61a9e1562f504
Robert Strandh <robert.strandh@gmail.com>
2017-08-09 15:24:32 +0200

    Define generic function CONVERT-SPECIAL-BINDING.

--- Code/Cleavir/CST-to-AST/generic-functions.lisp
+++ Code/Cleavir/CST-to-AST/generic-functions.lisp
@@ -6,6 +6,9 @@
 
 (defgeneric convert-special (head cst environment system))
 
+(defgeneric convert-special-binding
+    (variable value-ast next-ast env system))
+
 (defgeneric convert-lambda-call (cst env system))
 
 (defgeneric convert-code (lambda-list body-cst env system &optional block-name))
