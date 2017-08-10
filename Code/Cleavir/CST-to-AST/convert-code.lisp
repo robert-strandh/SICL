156e7917ef6a98c5664fa1aa16836355587f5735
Robert Strandh <robert.strandh@gmail.com>
2017-08-10 08:59:16 +0200

    Explain the format of a lexical lambda list, and use four semicolons for general comments.

--- Code/Cleavir/CST-to-AST/convert-code.lisp
+++ Code/Cleavir/CST-to-AST/convert-code.lisp
@@ -1,26 +1,51 @@
 (cl:in-package #:cleavir-cst-to-ast)
 
-;;; The general method for processing the lambda list is as follows:
-;;; We use recursion to process the remaining lambda list.  Before the
-;;; recursive call, we add the current parameters to the environment
-;;; that we pass to the recursive call.  The call returns two values:
-;;; the AST that was built and a modified lambda list, containing the
-;;; lambda list keywords, and the lexical variables that were
-;;; introduced.  The exception is that processing &AUX entries does
-;;; not return any lambda list, because it will always be empty.
-;;;
-;;; The reason we do it this way is that, if a parameter turns out to
-;;; be a special variable, the entire rest of the lambda list and
-;;; function body must be executed with this variable bound.  The AST
-;;; configuration for expressing that situation is that the AST for
-;;; computing the rest of the lambda list and the body must be a child
-;;; of a BIND-AST that indicates that the special variable should be
-;;; bound.  This recursive method makes sure that the child exists
-;;; before the BIND-AST is created.
-;;;
-;;; The parameter DSPECS that is used in several functions is a list
-;;; of canonicalized declaration specifiers.  This list is used to
-;;; determine whether a variable is declared special.
+;;;; The general method for processing the lambda list is as follows:
+;;;; We use recursion to process the remaining lambda list.  Before
+;;;; the recursive call, we add the current parameters to the
+;;;; environment that we pass to the recursive call.  The call returns
+;;;; two values: the AST that was built and a modified lambda list,
+;;;; containing the lambda list keywords, and the lexical variables
+;;;; that were introduced.  The exception is that processing &AUX
+;;;; entries does not return any lambda list, because it will always
+;;;; be empty.
+;;;;
+;;;; The reason we do it this way is that, if a parameter turns out to
+;;;; be a special variable, the entire rest of the lambda list and
+;;;; function body must be executed with this variable bound.  The AST
+;;;; configuration for expressing that situation is that the AST for
+;;;; computing the rest of the lambda list and the body must be a
+;;;; child of a BIND-AST that indicates that the special variable
+;;;; should be bound.  This recursive method makes sure that the child
+;;;; exists before the BIND-AST is created.
+;;;;
+;;;; The parameter DSPECS that is used in several functions is a list
+;;;; of canonicalized declaration specifiers.  This list is used to
+;;;; determine whether a variable is declared special.
+
+;;;; Several functions in this system create a LEXICAL LAMBDA LIST.
+;;;; Such a lambda list is similar to an ordinary lambda list.  It can
+;;;; have required parameter, &OPTIONAL parameters, a &REST parameter,
+;;;; and &KEY parameters.  It can not have any &AUX parameters,
+;;;; though.  It can also have implementation-specific parameters.
+;;;; The parameters are different from those of an ordinary lambda
+;;;; list.  A required parameter is represented as a LEXICAL-AST
+;;;; corresponding to the variable of the parameter in the original
+;;;; lambda list.  The same thing is true for a &REST parameter.  An
+;;;; &OPTIONAL parameter is represented as a list of two LEXICAL-ASTs.
+;;;; The first AST of the list corresponds to the variable of the
+;;;; parameter in the original lambda list, and the second AST
+;;;; corresponds to a SUPPLIED-P parameter, whether the original
+;;;; lambda list had such a parameter or not.  We generate ASTs that
+;;;; test the SUPPLIED-P parameter and, if it is NIL, compute the
+;;;; value of the initialization form if the corresponding parameter.
+;;;; A &KEY parameter is represented as a list of three elements.  The
+;;;; first element is the keyword to be used to determine whether this
+;;;; parameter was given.  The remaining two elements play the same
+;;;; role as those of an &OPTIONAL parameter.  We are not concerned
+;;;; here with they way in which it is determined whether a particular
+;;;; &KEY parameter was given or not.  This logic is determined for
+;;;; each implementation.
 
 ;;; Process a single parameter.  This function first computes a new
 ;;; environment by augmenting ENVIRONMENT with information from
