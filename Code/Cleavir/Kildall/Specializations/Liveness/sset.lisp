0bc077310006c5fa09850d576051f6f65dbaec55
Bike <aeshtaer@gmail.com>
2017-08-16 11:19:16 -0400

    Cleavir liveness: Fix bugs in sset functions
    Some variables are declared as fixnums, so they can't be the result of
    (car nil).

--- Code/Cleavir/Kildall/Specializations/Liveness/sset.lisp
+++ Code/Cleavir/Kildall/Specializations/Liveness/sset.lisp
@@ -2,7 +2,7 @@
 
 ;;; An implementation of "small sets" as sorted lists.
 ;;; This code is important to performance, so it's written
-;;; a bit strangel.
+;;; a bit strangely.
 
 (defun sset-union (s1 s2)
   (declare (type list s1 s2)
@@ -22,12 +22,14 @@
                with x2 fixnum = (car s2)
                when (< x1 x2)
                  collect x1
-                 and do (setf s1 (cdr s1) x1 (car s1))
+                 and do (setf s1 (cdr s1))
                  and when (null s1) nconc s2 and do (loop-finish) end
+                 and do (setf x1 (car s1))
                else when (> x1 x2)
                       collect x2
-                      and do (setf s2 (cdr s2) x2 (car s2))
+                      and do (setf s2 (cdr s2))
                       and when (null s2) nconc s1 and do (loop-finish) end
+                      and do (setf x2 (car s2))
                else ; (= x1 x2), unlikeliest case
                  collect x1
                  and do (setf s1 (cdr s1) s2 (cdr s2))
@@ -46,11 +48,13 @@
                with x2 fixnum = (car s2)
                when (< x1 x2)
                  collect x1
-                 and do (setf s1 (cdr s1) x1 (car s1))
+                 and do (setf s1 (cdr s1))
                  and when (null s1) do (loop-finish) end
+                 and do (setf x1 (car s1))
                else when (> x1 x2)
-                      do (setf s2 (cdr s2) x2 (car s2))
+                      do (setf s2 (cdr s2))
                       and when (null s2) nconc s1 and do (loop-finish) end
+                      and do (setf x2 (car s2))
                else
                  do (setf s1 (cdr s1) s2 (cdr s2))
                  and when (null s1) do (loop-finish) end
@@ -68,8 +72,9 @@
                when (< x1 x2)
                  return nil
                when (> x1 x2)
-                 do (setf s2 (cdr s2) x2 (car s2))
+                 do (setf s2 (cdr s2))
                  and when (null s2) return nil end ; nothing left in s2 for x1 to be
+                 and do (setf x2 (car s2))
                else
                  do (setf s1 (cdr s1) s2 (cdr s2))
                  and when (null s1) return t end
