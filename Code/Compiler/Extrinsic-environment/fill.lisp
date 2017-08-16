c930d74b15b751d95331976997c16c37a3e5e4d4
Robert Strandh <robert.strandh@gmail.com>
2017-08-16 17:40:58 +0200

    Load file containing new macro.

--- Code/Compiler/Extrinsic-environment/fill.lisp
+++ Code/Compiler/Extrinsic-environment/fill.lisp
@@ -68,6 +68,8 @@
   ;; loaded as part of the file standard-environment-functions.lisp
   ;; loaded below.
   (load-file "../../Data-and-control-flow/nth-value.lisp" environment)
+  (load-file "../../Data-and-control-flow/multiple-value-call-defmacro.lisp"
+             environment)
   ;; Load a file containing the definition of macro DEFUN.
   (load-file "../../Environment/defun-defmacro.lisp" environment)
   ;; Load file containing definition of function GET-SETF-EXPANSION.
