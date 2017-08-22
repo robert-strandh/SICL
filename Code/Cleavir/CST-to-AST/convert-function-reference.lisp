2c58614f8c48836938ff97a2cc627d8b00ca8ad6
Robert Strandh <robert.strandh@gmail.com>
2017-08-22 07:39:27 +0200

    Define methods on CONVERT-FUNCTION-REFERENCE.

--- Code/Cleavir/CST-to-AST/convert-function-reference.lisp
+++ Code/Cleavir/CST-to-AST/convert-function-reference.lisp
@@ -1,1 +1,25 @@
 (cl:in-package #:cleavir-cst-to-ast)
+
+(defmethod convert-function-reference
+    ((info cleavir-env:global-function-info) env system)
+  (convert-global-function info (cleavir-env:global-environment env) system))
+
+(defmethod convert-function-reference
+    ((info cleavir-env:local-function-info) env system)
+  (declare (ignore env system))
+  (cleavir-env:identity info))
+
+(defmethod convert-function-reference
+    ((info cleavir-env:global-macro-info) env system)
+  (error 'function-name-names-global-macro
+	 :expr (cleavir-env:name info)))
+
+(defmethod convert-function-reference
+    ((info cleavir-env:local-macro-info) env system)
+  (error 'function-name-names-local-macro
+	 :expr (cleavir-env:name info)))
+
+(defmethod convert-function-reference
+    ((info cleavir-env:special-operator-info) env system)
+  (error 'function-name-names-special-operator
+	 :expr (cleavir-env:name info)))
