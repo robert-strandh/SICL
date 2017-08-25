1d53f437f4a87492bd9f2270cdc607abdd68c2eb
Robert Strandh <robert.strandh@gmail.com>
2017-08-25 17:48:23 +0200

    Factor slot into new superclass.

--- Code/Cleavir/Environment/conditions.lisp
+++ Code/Cleavir/Environment/conditions.lisp
@@ -1,17 +1,17 @@
 (cl:in-package #:cleavir-environment)
 
-(define-condition no-variable-info
+(define-condition no-info
     (program-error acclimation:condition)
   ((%name :initarg :name :reader name)))
 
-(define-condition no-function-info
-    (program-error acclimation:condition)
-  ((%name :initarg :name :reader name)))
+(define-condition no-variable-info (no-info)
+  ())
 
-(define-condition no-block-info
-    (program-error acclimation:condition)
-  ((%name :initarg :name :reader name)))
+(define-condition no-function-info (no-info)
+  ())
 
-(define-condition no-tag-info
-    (program-error acclimation:condition)
-  ((%name :initarg :name :reader name)))
+(define-condition no-block-info (no-info)
+  ())
+
+(define-condition no-tag-info (no-info)
+  ())
