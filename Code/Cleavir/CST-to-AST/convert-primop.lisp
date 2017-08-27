1949d66f8d8bfd6dd4169af7427371ee6b57be35
Robert Strandh <robert.strandh@gmail.com>
2017-08-27 09:59:55 +0200

    Define methods on CONVERT-SPECIAL specialized to fixnum comparison primops .

--- Code/Cleavir/CST-to-AST/convert-primop.lisp
+++ Code/Cleavir/CST-to-AST/convert-primop.lisp
@@ -49,7 +49,7 @@
   (cst:db origin (car-cst arg-cst) cst
     (declare (ignore car-cst))
     (cleavir-ast:make-car-ast (convert arg-cst env system)
-			      :origin origin)))
+                              :origin origin)))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;
@@ -60,7 +60,7 @@
   (cst:db origin (cdr-cst arg-cst) cst
     (declare (ignore cdr-cst))
     (cleavir-ast:make-cdr-ast (convert arg-cst env system)
-			      :origin origin)))
+                              :origin origin)))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;
@@ -71,8 +71,8 @@
   (cst:db origin (rplaca-cst arg1-cst arg2-cst) cst
     (declare (ignore rplaca-cst))
     (cleavir-ast:make-rplaca-ast (convert arg1-cst env system)
-				 (convert arg2-cst env system)
-				 :origin origin)))
+                                 (convert arg2-cst env system)
+                                 :origin origin)))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;
@@ -83,8 +83,8 @@
   (cst:db origin (rplacd-cst arg1-cst arg2-cst) cst
     (declare (ignore rplacd-cst))
     (cleavir-ast:make-rplacd-ast (convert arg1-cst env system)
-				 (convert arg2-cst env system)
-				 :origin origin)))
+                                 (convert arg2-cst env system)
+                                 :origin origin)))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;
@@ -95,9 +95,9 @@
   (cst:db origin (add-cst arg1-cst arg2-cst variable-cst) cst
     (declare (ignore add-cst))
     (cleavir-ast:make-fixnum-add-ast (convert arg1-cst env system)
-				     (convert arg2-cst env system)
-				     (convert variable-cst env system)
-				     :origin origin)))
+                                     (convert arg2-cst env system)
+                                     (convert variable-cst env system)
+                                     :origin origin)))
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -109,6 +109,58 @@
   (cst:db origin (sub-cst arg1-cst arg2-cst variable-cst) cst
     (declare (ignore sub-cst))
     (cleavir-ast:make-fixnum-sub-ast (convert arg1-cst env system)
-				     (convert arg2-cst env system)
-				     (convert variable-cst env system)
-				     :origin origin)))
+                                     (convert arg2-cst env system)
+                                     (convert variable-cst env system)
+                                     :origin origin)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;; Converting CLEAVIR-PRIMOP:FIXNUM-LESS.
+
+(defmethod convert-special
+    ((symbol (eql 'cleavir-primop:fixnum-less)) cst env system)
+  (cst:db origin (less-cst arg1-cst arg2-cst) cst
+    (declare (ignore less-cst))
+    (make-instance 'cleavir-ast:fixnum-less-ast
+      :arg1-ast (convert arg1-cst env system)
+      :arg2-ast (convert arg2-cst env system)
+      :origin origin)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;; Converting CLEAVIR-PRIMOP:FIXNUM-NOT-GREATER.
+
+(defmethod convert-special
+    ((symbol (eql 'cleavir-primop:fixnum-not-greater)) cst env system)
+  (cst:db origin (not-greater-cst arg1-cst arg2-cst) cst
+    (declare (ignore not-greater-cst))
+    (make-instance 'cleavir-ast:fixnum-not-greater-ast
+      :arg1-ast (convert arg1-cst env system)
+      :arg2-ast (convert arg2-cst env system)
+      :origin origin)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;; Converting CLEAVIR-PRIMOP:FIXNUM-GREATER.
+
+(defmethod convert-special
+    ((symbol (eql 'cleavir-primop:fixnum-greater)) cst env system)
+  (cst:db origin (greater-cst arg1-cst arg2-cst) cst
+    (declare (ignore greater-cst))
+    (make-instance 'cleavir-ast:fixnum-greater-ast
+      :arg1-ast (convert arg1-cst env system)
+      :arg2-ast (convert arg2-cst env system)
+      :origin origin)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;; Converting CLEAVIR-PRIMOP:FIXNUM-NOT-LESS.
+
+(defmethod convert-special
+    ((symbol (eql 'cleavir-primop:fixnum-not-less)) cst env system)
+  (cst:db origin (not-less-cst arg1-cst arg2-cst) cst
+    (declare (ignore not-less-cst))
+    (make-instance 'cleavir-ast:fixnum-not-less-ast
+      :arg1-ast (convert arg1-cst env system)
+      :arg2-ast (convert arg2-cst env system)
+      :origin origin)))
