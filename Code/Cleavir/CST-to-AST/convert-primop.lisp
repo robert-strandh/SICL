660410310092225ea3c8000b592ee58f89667bf8
Robert Strandh <robert.strandh@gmail.com>
2017-08-27 07:25:10 +0200

    Define method on CONVERT-SPECIAL specialized to primop EQ.

--- Code/Cleavir/CST-to-AST/convert-primop.lisp
+++ Code/Cleavir/CST-to-AST/convert-primop.lisp
@@ -13,3 +13,15 @@
   (cst:db origin (primop ast) cst
     (declare (ignore primop))
     ast))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;; Converting CLEAVIR-PRIMOP:EQ.
+
+(defmethod convert-special
+    ((symbol (eql 'cleavir-primop:eq)) cst env system)
+  (cst:db origin (eql-cst arg1-cst arg2-cst) cst
+    (declare (ignore eql-cst))
+    (cleavir-ast:make-eq-ast
+     (convert arg1-cst env system)
+     (convert arg2-cst env system))))
