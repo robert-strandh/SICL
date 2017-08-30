781e73ff6f0862a25d8cfc33c2e1f7a5895ead42
Robert Strandh <robert.strandh@gmail.com>
2017-08-30 11:42:10 +0200

    Try giving a full AST in source.

--- Code/Cleavir/CST-to-AST/Test/test.lisp
+++ Code/Cleavir/CST-to-AST/Test/test.lisp
@@ -1,4 +1,32 @@
 (cl:in-package #:cleavir-cst-to-ast-test)
 
 (defun ff ()
-  [cleavir-ast:function-ast])
+  [cleavir-ast:function-ast
+    :body-ast [cleavir-ast:progn-ast
+                :form-asts ([cleavir-ast:setq-ast
+                              :lhs-ast [cleavir-ast:lexical-ast
+                                         :name x
+                                         :origin nil
+                                         :policy nil]
+                              :origin 234
+                              :policy nil
+                              :value-ast [cleavir-ast:lexical-ast
+                                           :name #:|x|
+                                           :origin 234234
+                                           :policy nil]]
+                            [cleavir-ast:block-ast
+                              :body-ast [cleavir-ast:load-time-value-ast
+                                          :form nil
+                                          :origin nil
+                                          :policy nil
+                                          :read-only-p t]
+                              :origin nil
+                              :policy nil])
+                :origin nil
+                :policy nil]
+    :lambda-list ([cleavir-ast:lexical-ast
+                    :name #:|x|
+                    :origin 234234
+                    :policy nil])
+    :origin nil
+    :policy nil])
