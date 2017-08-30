1b1735685a44b0b6bc4323432f01e529eac4ed5a
Robert Strandh <robert.strandh@gmail.com>
2017-08-30 17:29:50 +0200

    Define function TEST1 containing first test case.

--- Code/Cleavir/CST-to-AST/Test/test.lisp
+++ Code/Cleavir/CST-to-AST/Test/test.lisp
@@ -1,32 +1,37 @@
 (cl:in-package #:cleavir-cst-to-ast-test)
 
-(defun ff ()
-  [cleavir-ast:function-ast
-    :body-ast [cleavir-ast:progn-ast
-                :form-asts ([cleavir-ast:setq-ast
-                              :lhs-ast [cleavir-ast:lexical-ast
-                                         :name x
-                                         :origin nil
-                                         :policy nil]
-                              :origin 234
-                              :policy nil
-                              :value-ast [cleavir-ast:lexical-ast
-                                           :name #:|x|
-                                           :origin 234234
-                                           :policy nil]]
-                            [cleavir-ast:block-ast
-                              :body-ast [cleavir-ast:load-time-value-ast
-                                          :form nil
-                                          :origin nil
-                                          :policy nil
-                                          :read-only-p t]
-                              :origin nil
-                              :policy nil])
-                :origin nil
-                :policy nil]
-    :lambda-list ([cleavir-ast:lexical-ast
-                    :name #:|x|
-                    :origin 234234
-                    :policy nil])
-    :origin nil
-    :policy nil])
+(defun test1 ()
+  (let* ((cst (cst:cst-from-expression '(function (lambda (x) x))))
+         (env (make-instance 'environment))
+         (ast1 (cleavir-cst-to-ast:cst-to-ast cst env nil))
+         (ast2 [cleavir-ast:function-ast
+                 :origin (0)
+                 :policy nil
+                 :body-ast [cleavir-ast:progn-ast
+                             :origin nil
+                             :policy nil
+                             :form-asts ([cleavir-ast:setq-ast
+                                           :origin 234
+                                           :policy nil
+                                           :lhs-ast [cleavir-ast:lexical-ast
+                                                      :name x
+                                                      :origin (0 1 2)
+                                                      :policy nil]
+                                           :value-ast [cleavir-ast:lexical-ast
+                                                        :name #:|x|
+                                                        :origin (0 1 1 1)
+                                                        :policy nil]]
+                                         [cleavir-ast:block-ast
+                                           :origin nil
+                                           :policy nil
+                                           :body-ast [cleavir-ast:load-time-value-ast
+                                                       :form nil
+                                                       :origin nil
+                                                       :policy nil
+                                                       :read-only-p t]])]
+                 :lambda-list ([cleavir-ast:lexical-ast
+                                 :name #:|x|
+                                 :origin (0 1 1 1)
+                                 :policy nil])]))
+    (assign-sources cst)
+    (ast-equal-p ast1 ast2)))
