5ca536477eebff80fac8e3bfa2a3e01e7fef4a32
Robert Strandh <robert.strandh@gmail.com>
2017-09-02 07:36:40 +0200

    Define test that calls a global function.

--- Code/Cleavir/CST-to-AST/Test/test.lisp
+++ Code/Cleavir/CST-to-AST/Test/test.lisp
@@ -50,3 +50,31 @@
     (assign-sources cst)
     (let ((ast1 (cleavir-cst-to-ast:cst-to-ast cst env nil)))
       (ast-equal-p ast1 ast2))))
+
+(defun test3 ()
+  (let* ((cst (cst:cst-from-expression '(car *special1*)))
+         (env (make-instance 'environment))
+         (ast2 [cleavir-ast:call-ast
+                 :origin nil
+                 :policy nil
+                 :callee-ast
+                 [cleavir-ast:fdefinition-ast
+                   :origin nil
+                   :policy nil
+                   :name-ast [cleavir-ast:load-time-value-ast
+                               :origin (0 0)
+                               :policy nil
+                               :read-only-p nil
+                               :form 'car]]
+                 :argument-asts
+                 ([cleavir-ast:symbol-value-ast
+                    :origin (0 1)
+                    :policy nil
+                    :symbol-ast [cleavir-ast:load-time-value-ast
+                                  :origin (0)
+                                  :policy nil
+                                  :read-only-p nil
+                                  :form '*special1*]])]))
+    (assign-sources cst)
+    (let ((ast1 (cleavir-cst-to-ast:cst-to-ast cst env nil)))
+      (ast-equal-p ast1 ast2))))
