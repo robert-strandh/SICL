8f552c1247bb34070120192d3e91cf573a9f4d15
Robert Strandh <robert.strandh@gmail.com>
2017-09-11 17:50:28 +0200

    Define method on CONVERT-SETQ specialized to CONSTANT-VARIABLE-INFO.

--- Code/Cleavir/CST-to-AST/convert-setq.lisp
+++ Code/Cleavir/CST-to-AST/convert-setq.lisp
@@ -7,3 +7,9 @@
 ;;; Recall that the SETQ-AST is a NO-VALUE-AST-MIXIN.  We must
 ;;; therefore make sure it is always compiled in a context where its
 ;;; value is not needed.  We do that by wrapping a PROGN around it.
+
+(defmethod convert-setq
+    (var-cst form-cst (info cleavir-env:constant-variable-info) env system)
+  (declare (ignore var-cst form-cst env system))
+  (error 'setq-constant-variable
+	 :expr (cleavir-env:name info)))
