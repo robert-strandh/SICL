f16da7d4313864d724bc58327f9d387735b9c9d3
Robert Strandh <robert.strandh@gmail.com>
2017-09-21 17:58:22 +0200

    Add &optional parameter ORIGIN to PROCESS-PROGN.

--- Code/Cleavir/CST-to-AST/process-progn.lisp
+++ Code/Cleavir/CST-to-AST/process-progn.lisp
@@ -6,7 +6,7 @@
 ;;; LOAD-TIME-VALUE-AST containing NIL in case the list of ASTs is
 ;;; NIL.
 
-(defun process-progn (asts)
+(defun process-progn (asts &optional origin)
   (if (null asts)
-      (cleavir-ast:make-load-time-value-ast 'nil t)
-      (cleavir-ast:make-progn-ast asts)))
+      (cleavir-ast:make-load-time-value-ast 'nil t :origin origin)
+      (cleavir-ast:make-progn-ast asts :origin origin)))
