0657ed11a2054078c09413186e497eccfa4576a0
Robert Strandh <robert.strandh@gmail.com>
2017-09-22 15:03:40 +0200

    Rename parameter to CONVERT-SEQUENCE for better consistency in naming.

--- Code/Cleavir/CST-to-AST/convert-sequence.lisp
+++ Code/Cleavir/CST-to-AST/convert-sequence.lisp
@@ -2,7 +2,7 @@
 
 ;;; Utility function for converting a sequence of CSTs, represented
 ;;; as a chain of CONS-CSTs terminated by a NULL-CST.
-(defun convert-sequence (csts environment system)
-  (loop for cst = csts then (cst:rest cst)
+(defun convert-sequence (sequence-cst environment system)
+  (loop for cst = sequence-cst then (cst:rest cst)
         until (cst:null cst)
 	collect (convert (cst:first cst) environment system)))
