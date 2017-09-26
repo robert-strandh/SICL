64d76e714391aed2cbd21b9689215778cad08ed0
Robert Strandh <robert.strandh@gmail.com>
2017-09-26 14:07:39 +0200

    Convert the form to a CST before calling CST-EVAL.

--- Code/Compiler/Extrinsic-environment/cst-repl.lisp
+++ Code/Compiler/Extrinsic-environment/cst-repl.lisp
@@ -9,7 +9,7 @@
 	 (when (equal form '(quit))
 	   (loop-finish))
 	 (let ((values (multiple-value-list
-			(cst-eval form env1 env2))))
+			(cst-eval (cst:cst-from-expression form) env1 env2))))
 	   (loop for value in values
 		 do (print value))
 	   (terpri)))))
