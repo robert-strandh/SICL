8a027730a96bb4f785b47245cc9291f30037c4e5
Robert Strandh <robert.strandh@gmail.com>
2017-09-28 14:08:14 +0200

    Improve code layout by introducing a temporary variable.

--- Code/Cleavir/CST-to-AST/convert-special-binding.lisp
+++ Code/Cleavir/CST-to-AST/convert-special-binding.lisp
@@ -13,15 +13,15 @@
 ;;; Lisp list of CSTs, and then we convert that to a CST.
 (defmethod convert-special-binding
     (variable-cst value-ast next-ast env system)
-  (let ((call-with-variable-bound-cst
-          (cst:cst-from-expression 'cleavir-primop:call-with-variable-bound))
-        (quoted-variable-cst
-          (cst:cstify `(,(cst:cst-from-expression 'quote)
-                        ,variable-cst))))
-    (convert (cst:cstify `(,call-with-variable-bound-cst
-                           ,quoted-variable-cst
-                           ,(cst:cst-from-expression
-                             `(cleavir-primop:ast ,value-ast))
-                           ,(cst:cst-from-expression
-                             `(lambda () (cleavir-primop:ast ,next-ast)))))
-             env system)))
+  (let* ((call-with-variable-bound-cst
+           (cst:cst-from-expression 'cleavir-primop:call-with-variable-bound))
+         (quoted-variable-cst
+           (cst:cstify `(,(cst:cst-from-expression 'quote)
+                         ,variable-cst)))
+         (new-cst (cst:cstify `(,call-with-variable-bound-cst
+                                ,quoted-variable-cst
+                                ,(cst:cst-from-expression
+                                  `(cleavir-primop:ast ,value-ast))
+                                ,(cst:cst-from-expression
+                                  `(lambda () (cleavir-primop:ast ,next-ast)))))))
+    (convert new-cst env system)))
