d569a08b0e502b31dfff804e5e53099d728b3609
Robert Strandh <robert.strandh@gmail.com>
2017-09-30 10:01:12 +0200

    Define test for primop RPLACD.

--- Code/Cleavir/CST-to-AST/Test/test.lisp
+++ Code/Cleavir/CST-to-AST/Test/test.lisp
@@ -1139,6 +1139,28 @@
     (let ((ast1 (cleavir-cst-to-ast:cst-to-ast cst env nil)))
       (assert (ast-equal-p ast1 ast2)))))
 
+(defun test53 ()
+  (let* ((cst (cst:cst-from-expression '(cleavir-primop:rplacd 1 2)))
+         (env (make-instance 'environment))
+         (ast2 [cleavir-ast:rplacd-ast
+                  :object-ast
+                  [cleavir-ast:load-time-value-ast
+                     :read-only-p t
+                     :form '2
+                     :policy nil
+                     :origin (0 2)]
+                  :cons-ast
+                  [cleavir-ast:load-time-value-ast
+                     :read-only-p t
+                     :form '1
+                     :policy nil
+                     :origin (0 1)]
+                  :policy nil
+                  :origin (0)]))
+    (assign-sources cst)
+    (let ((ast1 (cleavir-cst-to-ast:cst-to-ast cst env nil)))
+      (assert (ast-equal-p ast1 ast2)))))
+
 (defun test ()
   (test1)
   (test2)
@@ -1181,4 +1203,5 @@
   (test49)
   (test50)
   (test51)
-  (test52))
+  (test52)
+  (test53))
