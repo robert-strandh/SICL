113912fc75ee34ec44641efab56aaa7714651cc9
Robert Strandh <robert.strandh@gmail.com>
2017-10-05 13:55:58 +0200

    Define function CST-LOAD-FILE.

--- Code/Compiler/Extrinsic-environment/load-file-defun.lisp
+++ Code/Compiler/Extrinsic-environment/load-file-defun.lisp
@@ -7,3 +7,14 @@
    (asdf:system-relative-pathname :sicl-extrinsic-environment filename)
    environment
    environment))
+
+;;; This function is just like LOAD-FILE, except that it calls
+;;; CST-LOAD-SOURCE-WITH-ENVIRONMENT rather than
+;;; LOAD-SOURCE-WITH-ENVIRONMENTS.
+(defun cst-load-file (filename environment)
+  (format *trace-output* "CST Loading file ~a~%" filename)
+  (finish-output *trace-output*)
+  (cst-load-source-with-environments
+   (asdf:system-relative-pathname :sicl-extrinsic-environment filename)
+   environment
+   environment))
