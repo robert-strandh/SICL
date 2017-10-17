665f46a7f8fcc89d00b79dc640a779ca06163b4f
Robert Strandh <robert.strandh@gmail.com>
2017-10-17 14:05:04 +0200

    Remove obsolete code for checking syntax of MULTIPLE-VALUE-PROG1.

--- Code/Cleavir/CST-to-AST/check-special-form-syntax.lisp
+++ Code/Cleavir/CST-to-AST/check-special-form-syntax.lisp
@@ -202,8 +202,11 @@
 ;;; Checking MULTIPLE-VALUE-PROG1.
 
 (defmethod check-special-form-syntax ((operator (eql 'multiple-value-prog1)) cst)
-  (check-cst-proper-list cst 'form-must-be-proper-list)
-  (check-argument-count cst 1 nil))
+  ;; The code in this method has been moved to the corresponding
+  ;; method of convert-special.  Ultimately, the code of every method
+  ;; on CHECK-SPECIAL-FORM-SYNTAX will be moved.
+  (declare (ignore cst))
+  nil)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;
