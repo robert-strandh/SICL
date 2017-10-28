5de1fe75d7a16cdfea087f79a68fbd72565f17e5
Robert Strandh <robert.strandh@gmail.com>
2017-10-28 16:41:51 +0200

    Untabify for better tool support.

--- Code/Boot/define-find-class.lisp
+++ Code/Boot/define-find-class.lisp
@@ -2,8 +2,8 @@
 
 (defun define-find-class (boot)
   (setf (sicl-genv:fdefinition 'find-class (r2 boot))
-	(lambda (name-or-class &optional errorp environment)
-	  (declare (ignore errorp environment))
-	  (if (symbolp name-or-class)
-	      (sicl-genv:find-class name-or-class (r1 boot))
-	      name-or-class))))
+        (lambda (name-or-class &optional errorp environment)
+          (declare (ignore errorp environment))
+          (if (symbolp name-or-class)
+              (sicl-genv:find-class name-or-class (r1 boot))
+              name-or-class))))
