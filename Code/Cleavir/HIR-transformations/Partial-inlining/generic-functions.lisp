4ffcfcaaec60744c3d3b37636b264ea69086a4a8
Robert Strandh <robert.strandh@gmail.com>
2017-10-29 15:42:43 +0100

    Modify signature and add comment describing the function.

--- Code/Cleavir/HIR-transformations/Partial-inlining/generic-functions.lisp
+++ Code/Cleavir/HIR-transformations/Partial-inlining/generic-functions.lisp
@@ -1,4 +1,16 @@
 (cl:in-package #:cleavir-partial-inlining)
 
+;;; The first input to CALL-INSTRUCTION is a temporary that is the
+;;; output of ENCLOSE-INSTRUCTION, and ENCLOSE-INSTRUCTION has
+;;; ENTER-INSTRUCTION as its input.
+;;;
+;;; Furthermore, we know that no other call-instruction has the same
+;;; first input as CALL-INSTRUCTION, so we are free to mutate
+;;; CALL-INSTRUCTION and ENTER-INSTRUCTION to add more inputs and
+;;; outputs respectively.
+;;;
+;;; MAPPING is a mapping that takes an intruction or a datum in the
+;;; callee and maps it to an instruction or a datum in the caller,
+;;; provided such a correspondance has been added to the mapping.
 (defgeneric inline-line-instruction
-  (call-instruction enter-instruction translation))
+  (enclose-instruction call-instruction enter-instruction mapping))
