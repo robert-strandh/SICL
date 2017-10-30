bcc92d13f0c76197ca52d5bec9d375a0e0c02b4f
Robert Strandh <robert.strandh@gmail.com>
2017-10-30 04:29:13 +0100

    Improve comment to INLINE-ONE-INSTRUCTION.

--- Code/Cleavir/HIR-transformations/Partial-inlining/generic-functions.lisp
+++ Code/Cleavir/HIR-transformations/Partial-inlining/generic-functions.lisp
@@ -4,14 +4,21 @@
 ;;;
 ;;; Generic function INLINE-ONE-INSTRUCTION.
 
+;;; In the following description, the terms INPUT and OUTPUT refer to
+;;; data in HIR code.  If needed, we use the terms ARGUMENTS and
+;;; VALUES to refer to objects supplied to and received from function
+;;; calls.
+;;;
 ;;; The first input to CALL-INSTRUCTION is a temporary that is the
 ;;; output of ENCLOSE-INSTRUCTION, and ENCLOSE-INSTRUCTION has
-;;; ENTER-INSTRUCTION as its input.
+;;; ENTER-INSTRUCTION as its input.  Furthermore, ENCLOSE-INSTRUCTION
+;;; is the immediate predecessor of CALL-INSTRUCTION.
 ;;;
-;;; Furthermore, we know that no other call-instruction has the same
-;;; first input as CALL-INSTRUCTION, so we are free to mutate
-;;; CALL-INSTRUCTION and ENTER-INSTRUCTION to add more inputs and
-;;; outputs respectively.
+;;; We know that no other call-instruction has the same first input as
+;;; CALL-INSTRUCTION, so we are free to mutate CALL-INSTRUCTION and
+;;; ENTER-INSTRUCTION to add more inputs and outputs respectively.
+;;; When this is done, new inputs and outputs are added to the end of
+;;; the existing list.
 ;;;
 ;;; MAPPING is a mapping that takes an instruction or a datum in the
 ;;; callee and maps it to an instruction or a datum in the caller,
