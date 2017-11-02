75432d815a024d33e8499b9516e00f866ef29fff
Robert Strandh <robert.strandh@gmail.com>
2017-11-02 08:16:07 +0100

    Add keyword parameter SYSTEM to :AFTER method on INITIALIZE-INSTANCE.

--- Code/Compiler/Extrinsic-environment/initialization.lisp
+++ Code/Compiler/Extrinsic-environment/initialization.lisp
@@ -1,8 +1,8 @@
 (cl:in-package #:sicl-extrinsic-environment)
 
 (defmethod initialize-instance :after
-    ((object environment) &key &allow-other-keys)
-  (fill-environment object))
+    ((object environment) &key system &allow-other-keys)
+  (fill-environment object system))
 
 (defmethod initialize-instance :around
     ((object environment) &key (cache-p nil cache-p-p) &allow-other-keys)
