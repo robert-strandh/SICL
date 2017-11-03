8665cc530fffb3adc911060ea4a4f032a0af411e
Robert Strandh <robert.strandh@gmail.com>
2017-11-03 14:32:20 +0100

    Remove obsolete package SICL-COMPILER-TYPES.

--- Code/Compiler/packages.lisp
+++ Code/Compiler/packages.lisp
@@ -41,43 +41,6 @@
   (:shadowing-import-from #:sicl-global-environment
 			  #:proclaim))
 
-(defpackage #:sicl-compiler-types
-  (:use #:common-lisp)
-  (:export
-   #:type-descriptor-from-type
-   #:rational-descriptor
-   #:short-float-descriptor
-   #:single-float-descriptor
-   #:double-float-descriptor
-   #:long-float-descriptor
-   #:complex-descriptor
-   #:array-t-descriptor
-   #:array-single-float-descriptor
-   #:array-double-float-descriptor
-   #:array-character-descriptor
-   #:array-bit-descriptor
-   #:array-unsigned-byte-8-descriptor
-   #:array-unsigned-byte-32-descriptor
-   #:array-signed-byte-32-descriptor
-   #:array-unsigned-byte-64-descriptor
-   #:array-signed-byte-64-descriptor
-   #:null-descriptor
-   #:others-descriptor
-   #:type-descriptor-and
-   #:type-descriptor-or
-   #:type-descriptor-diff
-   #:type-descriptor
-   #:make-t-type-descriptor
-   #:make-t-type-map
-   #:type-map-or
-   #:type-map-and
-   #:type-map-equal
-   #:copy-type-map
-   #:split-type-map
-   #:impossible-type-map-p
-   ))
-
-
 (defpackage #:sicl-compiler
   (:use #:common-lisp)
   (:shadow #:compile-file)
