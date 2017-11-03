771720a33bc4c26569c82edc3ba7d09c26a55944
Robert Strandh <robert.strandh@gmail.com>
2017-11-03 15:02:51 +0100

    Remove obsolete definition(s) of the macro DEFTYPE.

--- Code/Compiler/types.lisp
+++ Code/Compiler/types.lisp
@@ -1,33 +1,5 @@
 (in-package #:sicl-global-environment)
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;;
-;;; Macro DEFTYPE.
-
-(defmacro deftype (name lambda-list &body body)
-  `(eval-when (:compile-toplevel :load-toplevel :execute)
-     (setf (type-function ',name)
-           ,(cleavir-code-utilities:parse-deftype
-             name
-             lambda-list
-             body))))
-
-;;; The next form requires some explanation.  In the native compiler,
-;;; the symbols defmacro and cl:defmacro are the same, so then this
-;;; next form only redefines the macro deftype.  In the cross
-;;; compiler, however, the two symbols are different.  The effect of
-;;; this form, then, is to define a host macro named
-;;; sicl-global-environment:deftype and which puts host functions
-;;; into the global SICL environment as type functions.
-(eval-when (:compile-toplevel :load-toplevel :execute)
-  (cl:defmacro deftype (name lambda-list &body body)
-    `(eval-when (:compile-toplevel :load-toplevel :execute)
-       (setf (type-function ',name)
-             ,(cleavir-code-utilities:parse-deftype
-               name
-               lambda-list
-               body)))))
-
 (deftype mod (n)
   `(integer 0 (,n)))
 
