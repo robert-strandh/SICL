d2c109eacfece7e9ddb2cc156d786e116a70be23
Robert Strandh <robert.strandh@gmail.com>
2017-11-29 07:07:29 +0100

    Import function SHARED-INITIALIZE from host.

--- Code/Boot/phase2.lisp
+++ Code/Boot/phase2.lisp
@@ -247,6 +247,7 @@
   (setf (sicl-genv:fdefinition 'sicl-clos::compute-discriminating-function env2)
         (lambda (generic-function) (declare (ignore generic-function)) nil))
   (ld "../CLOS/invalidate-discriminating-function.lisp" env2 env2)
+  (sicl-extrinsic-environment:import-function-from-host 'shared-initialize env2)
   (ld "../CLOS/generic-function-initialization-defmethods.lisp" env2 env2)
   ;; We must also make sure that DEFGENERIC is handled properly for
   ;; phase 2.
