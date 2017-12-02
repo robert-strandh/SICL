a9b72f1884f60f67f15da24aa308967fec73e51e
Robert Strandh <robert.strandh@gmail.com>
2017-12-02 16:47:38 +0100

    Call new function in phase 3.

--- Code/Boot/phase3.lisp
+++ Code/Boot/phase3.lisp
@@ -310,6 +310,7 @@
     (ld "../CLOS/slot-bound-using-index.lisp" r3 r3)
     (define-error-function 'reinitialize-instance r3)
     (define-error-function 'change-class r3)
+    (define-ensure-generic-function-phase3 r3)
     (ld "../CLOS/ensure-class-using-class-support.lisp" r3 r3)
     (message "End of phase 3~%")))
 
