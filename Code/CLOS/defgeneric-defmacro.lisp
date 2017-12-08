482e3c36b0c3b78b5731c5778f1e81ffcf68e149
Robert Strandh <robert.strandh@gmail.com>
2017-12-08 16:27:15 +0100

    Untabify for better tool support.

--- Code/CLOS/defgeneric-defmacro.lisp
+++ Code/CLOS/defgeneric-defmacro.lisp
@@ -10,14 +10,14 @@
                       &rest options-and-methods)
   (declare (ignore options-and-methods))
   (let* ((arg-type (cleavir-code-utilities:lambda-list-type-specifier lambda-list))
-	 (function-type `(function ,arg-type t)))
+         (function-type `(function ,arg-type t)))
   `(progn (eval-when (:compile-toplevel)
-	    (setf (sicl-global-environment:function-type ',name ,env)
-		  ',function-type))
-	  (eval-when (:load-toplevel :execute)
-	    (ensure-generic-function
-	     ',name
-	     :lambda-list ',lambda-list
-	     :environment
-	     (load-time-value
-	      (sicl-global-environment:global-environment)))))))
+            (setf (sicl-global-environment:function-type ',name ,env)
+                  ',function-type))
+          (eval-when (:load-toplevel :execute)
+            (ensure-generic-function
+             ',name
+             :lambda-list ',lambda-list
+             :environment
+             (load-time-value
+              (sicl-global-environment:global-environment)))))))
