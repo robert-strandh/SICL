cfd21ad815fcf62cc5cd9709347e827bd14e49ab
Robert Strandh <robert.strandh@gmail.com>
2017-12-08 16:27:43 +0100

    New component containing support code for macro DEFGENERIC.

---
+++ Code/CLOS/defgeneric-support.lisp
@@ -0,0 +1,17 @@
+(cl:in-package #:sicl-clos)
+
+(defun check-defgeneric-options-and-methods (options-and-methods)
+  (loop for option-or-method in options-and-methods
+        do (unless (consp option-or-method)
+             ;; FIXME: Define signal type and provide arguments.
+             (error 'option-or-method-must-be-non-empty-list))
+           (unless (member option-or-method '(:argument-precedence-order
+                                              declare
+                                              :documentation
+                                              :method-combination
+                                              :generic-function-class
+                                              :method-class
+                                              :method))
+             ;; FIXME: Define signal type.
+             (error 'unknown-defgeneric-option
+                    :option option-or-method))))
