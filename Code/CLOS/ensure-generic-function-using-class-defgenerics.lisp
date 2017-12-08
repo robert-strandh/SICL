b51c10d6795f1366406553283bd068824ca67b06
Robert Strandh <robert.strandh@gmail.com>
2017-12-08 13:17:20 +0100

    Add comment explaining the additional keyword argument ENVIRONMENT.

--- Code/CLOS/ensure-generic-function-using-class-defgenerics.lisp
+++ Code/CLOS/ensure-generic-function-using-class-defgenerics.lisp
@@ -2,6 +2,11 @@
 
 ;;; For the specification of this generic function, see
 ;;; http://metamodular.com/CLOS-MOP/ensure-generic-function-using-class.html
+;;;
+;;; We handle an additional keyword argument compared to the
+;;; specification, namely ENVIRONMENT.  It can be used to specify in
+;;; which the generic function is to be found or defined.  It defaults
+;;; to the environment in which this function was loaded.
 (defgeneric ensure-generic-function-using-class
     (generic-function
      function-name
