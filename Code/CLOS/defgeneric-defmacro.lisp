101dca27a163ead68231abc761b756ccb8fac14a
Robert Strandh <robert.strandh@gmail.com>
2017-12-13 17:32:06 +0100

    Fix bad indentation.

--- Code/CLOS/defgeneric-defmacro.lisp
+++ Code/CLOS/defgeneric-defmacro.lisp
@@ -11,13 +11,13 @@
   (declare (ignore options-and-methods))
   (let* ((arg-type (cleavir-code-utilities:lambda-list-type-specifier lambda-list))
          (function-type `(function ,arg-type t)))
-  `(progn (eval-when (:compile-toplevel)
-            (setf (sicl-global-environment:function-type ',name ,env)
-                  ',function-type))
-          (eval-when (:load-toplevel :execute)
-            (ensure-generic-function
-             ',name
-             :lambda-list ',lambda-list
-             :environment
-             (load-time-value
-              (sicl-global-environment:global-environment)))))))
+    `(progn (eval-when (:compile-toplevel)
+              (setf (sicl-global-environment:function-type ',name ,env)
+                    ',function-type))
+            (eval-when (:load-toplevel :execute)
+              (ensure-generic-function
+               ',name
+               :lambda-list ',lambda-list
+               :environment
+               (load-time-value
+                (sicl-global-environment:global-environment)))))))
