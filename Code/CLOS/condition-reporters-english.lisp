39d45619a961a1b598446a739fe98a56b94e76c0
Robert Strandh <robert.strandh@gmail.com>
2017-12-13 10:21:15 +0100

    Define method on REPORT-CONDITION specialized to condition for malformed option or method.

--- Code/CLOS/condition-reporters-english.lisp
+++ Code/CLOS/condition-reporters-english.lisp
@@ -69,3 +69,13 @@
   (format stream
           "Unable to compute the class precedence list of the class ~s"
           (offending-class c)))
+
+(defmethod acclimation:report-condition
+    ((c option-or-method-must-be-non-empty-list)
+     stream
+     (language acclimation:english))
+  (format stream
+          "Option or method must be a non-empty list,~@
+           but the following expression was found instead~@
+           ~s"
+          (expression c)))
