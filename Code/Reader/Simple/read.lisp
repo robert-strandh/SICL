a3e18dd6dbebb59daf79bc5b898006cf51921843
Robert Strandh <robert.strandh@gmail.com>
2017-12-25 09:53:33 +0100

    Define special variable *LABELS* for hash table used by #= and ##.

--- Code/Reader/Simple/read.lisp
+++ Code/Reader/Simple/read.lisp
@@ -1,18 +1,27 @@
 (cl:in-package #:sicl-reader)
 
+(defvar *labels*)
+
 (defun read (&optional
 	       (input-stream *standard-input*)
 	       (eof-error-p t)
 	       (eof-value nil)
 	       (recursive-p nil))
-  (let ((*preserve-whitespace* recursive-p))
-    (read-common input-stream eof-error-p eof-value)))
+  (if recursive-p
+      (let ((*preserve-whitespace* recursive-p))
+        (read-common input-stream eof-error-p eof-value))
+      (let ((*preserve-whitespace* recursive-p)
+            (*labels* (make-hash-table)))
+        (read-common input-stream eof-error-p eof-value))))
 
 (defun read-preserving-whitespace (&optional
                                      (input-stream *standard-input*)
                                      (eof-error-p t)
                                      (eof-value nil)
                                      (recursive-p nil))
-  (declare (ignore recursive-p))
-  (let ((*preserve-whitespace* t))
-    (read-common input-stream eof-error-p eof-value)))
+  (if recursive-p
+      (let ((*preserve-whitespace* t))
+        (read-common input-stream eof-error-p eof-value))
+      (let ((*preserve-whitespace* t)
+            (*labels* (make-hash-table)))
+        (read-common input-stream eof-error-p eof-value))))
