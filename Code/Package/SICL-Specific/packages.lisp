68516614bb22343199b25b717a925aeccf9aba93
Robert Strandh <robert.strandh@gmail.com>
2017-12-31 09:37:04 +0100

    Export standard symbols from the package SICL-PACKAGE.

--- Code/Package/SICL-Specific/packages.lisp
+++ Code/Package/SICL-Specific/packages.lisp
@@ -13,4 +13,27 @@
            #:package-error
            #:make-package
            #:intern
-           #:find-symbol)))
+           #:find-symbol))
+  (:export #:package
+           #:export
+           #:find-symbol
+           #:import
+           #:rename-package
+           #:shadow
+           #:shadowing-import
+           #:make-package
+           #:with-package-iterator
+           #:unexport
+           #:unintern
+           #:unuse-package
+           #:use-package
+           #:defpackage
+           #:do-symbols
+           #:do-external-symbols
+           #:intern
+           #:package-name
+           #:package-nicknames
+           #:package-shadowing-symbols
+           #:package-use-list
+           #:package-used-by-list
+           #:packagep))
