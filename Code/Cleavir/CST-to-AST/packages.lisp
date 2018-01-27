f433a3ce0b8dc29f64d9aae91fec73b80c8c539b
Robert Strandh <robert.strandh@gmail.com>
2018-01-27 09:28:04 +0100

    Export names of conditions.

--- Code/Cleavir/CST-to-AST/packages.lisp
+++ Code/Cleavir/CST-to-AST/packages.lisp
@@ -23,4 +23,42 @@
            #:process-parameters-in-group
            #:process-parameter
            #:items-from-parameter-group
-           #:consider-global))
+           #:consider-global
+           ;; Names of conditions.
+           #:run-time-program-error
+           #:incorrect-number-of-arguments
+           #:values-&rest-syntax
+           #:ignored-variable-referenced
+           #:block-name-must-be-a-symbol
+           #:form-must-be-proper-list
+           #:situations-must-be-proper-list
+           #:invalid-eval-when-situation
+           #:flet-functions-must-be-proper-list
+           #:local-function-definition-must-be-proper-list
+           #:lambda-must-be-proper-list
+           #:function-argument-must-be-function-name-or-lambda-expression
+           #:function-name-must-be-proper-function-name
+           #:let-or-let*-must-have-at-least-one-argument
+           #:bindings-must-be-proper-list
+           #:binding-must-be-symbol-or-list
+           #:binding-must-have-length-one-or-two
+           #:variable-must-be-a-symbol
+           #:read-only-p-must-be-boolean
+           #:block-name-unknown
+           #:setq-must-have-even-number-of-arguments
+           #:setq-var-must-be-symbol
+           #:setq-constant-variable
+           #:variable-name-unknown
+           #:function-name-unknown
+           #:function-name-names-global-macro
+           #:function-name-names-local-macro
+           #:function-name-names-special-operator
+           #:no-default-method
+           #:lambda-call-first-symbol-not-lambda
+           #:malformed-lambda-list
+           #:too-many-arguments-warning
+           #:too-many-arguments-style-warning
+           #:not-enough-arguments-warning
+           #:not-enough-arguments-style-warning
+           #:odd-keyword-portion-warning
+           #:odd-keyword-portion-style-warning))
