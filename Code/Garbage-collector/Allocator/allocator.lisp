eab0542621ed62fbb030a0e9c9bce88e098cfd4e
Robert Strandh <robert.strandh@gmail.com>
2018-07-08 07:05:15 +0200

    New functions for linking and unlinking chunks.

--- Code/Garbage-collector/Allocator/allocator.lisp
+++ Code/Garbage-collector/Allocator/allocator.lisp
@@ -30,3 +30,33 @@
   (assert (preceding-chunk-free-p chunk))
   (- chunk (preceding-chunk-size chunk)))
 
+(defun following-chunk (chunk)
+  (assert (zerop (mod chunk 8)))
+  (+ chunk (chunk-size chunk)))
+
+(defun chunk-free-p (chunk)
+  (assert (zerop (mod chunk 8)))
+  (preceding-chunk-free-p (following-chunk chunk)))
+
+(defun chunk-linked-p (chunk)
+  (assert (zerop (mod chunk 8)))
+  (and (not (zerop (sicl-gc-memory:memory-64 (chunk-prev-slot-address chunk))))
+       (not (zerop (sicl-gc-memory:memory-64 (chunk-next-slot-address chunk))))))
+
+(defun unlink-chunk (chunk)
+  (assert (zerop (mod chunk 8)))
+  (assert (chunk-linked-p chunk))
+  (let* ((pa (chunk-prev-slot-address chunk))
+         (p (sicl-gc-memory:memory-64 pa))
+         (na (chunk-next-slot-address chunk))
+         (n (sicl-gc-memory:memory-64 na)))
+    (setf (sicl-gc-memory:memory-64 p) n)
+    (setf (sicl-gc-memory:memory-64 n) p)
+    (setf (sicl-gc-memory:memory-64 pa) 0)
+    (setf (sicl-gc-memory:memory-64 na) 0)))
+
+(defun link-chunk-between (chunk pa na)
+  (setf (sicl-gc-memory:memory-64 (chunk-prev-slot-address chunk)) pa)
+  (setf (sicl-gc-memory:memory-64 (chunk-next-slot-address chunk)) na)
+  (setf (sicl-gc-memory:memory-64 pa) (chunk-prev-slot-address chunk))
+  (setf (sicl-gc-memory:memory-64 na) (chunk-next-slot-address chunk)))
