dfaf05e7a4bf5d2504d2a70f27f9ac78f216ff6d
Robert Strandh <robert.strandh@gmail.com>
2018-07-16 11:45:23 +0200

    No longer shadow any symbols from COMMON-LISP.

--- Code/Memory/packages.lisp
+++ Code/Memory/packages.lisp
@@ -2,9 +2,7 @@
 
 (defpackage #:sicl-memory
   (:use #:common-lisp)
-  (:shadow #:car #:cdr #:class)
-  (:export
-   #:load-byte #:store-byte
-   #:load-2-byte-word #:store-2-byte-word
-   #:load-4-byte-word #:store-4-byte-word
-   #:load-8-byte-word #:store-8-byte-word))
+  (:export #:load-byte #:store-byte
+           #:load-2-byte-word #:store-2-byte-word
+           #:load-4-byte-word #:store-4-byte-word
+           #:load-8-byte-word #:store-8-byte-word))
