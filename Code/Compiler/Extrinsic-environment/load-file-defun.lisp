c408793bad3dba3fdd560022737c450b50a43daa
Robert Strandh <robert.strandh@gmail.com>
2018-07-18 10:51:01 +0200

    Remove obsolete function LOAD-FILE.

--- Code/Compiler/Extrinsic-environment/load-file-defun.lisp
+++ Code/Compiler/Extrinsic-environment/load-file-defun.lisp
@@ -1,16 +1,5 @@
 (cl:in-package #:sicl-extrinsic-environment)
 
-(defun load-file (filename environment)
-  (format *trace-output* "Loading file ~a~%" filename)
-  (finish-output *trace-output*)
-  (load-source-with-environments
-   (asdf:system-relative-pathname :sicl-extrinsic-environment filename)
-   environment
-   environment))
-
-;;; This function is just like LOAD-FILE, except that it calls
-;;; CST-LOAD-SOURCE-WITH-ENVIRONMENT rather than
-;;; LOAD-SOURCE-WITH-ENVIRONMENTS.
 (defun cst-load-file (filename environment system)
   (format *trace-output* "CST Loading file ~a~%" filename)
   (finish-output *trace-output*)
