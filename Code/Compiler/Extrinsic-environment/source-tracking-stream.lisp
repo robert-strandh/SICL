b8e57383284635d9b46f3ba8b05b3d30f1ea82f4
Robert Strandh <robert.strandh@gmail.com>
2018-07-18 12:12:46 +0200

    Define method on SOURCE-POSITION of Eclector.

--- Code/Compiler/Extrinsic-environment/source-tracking-stream.lisp
+++ Code/Compiler/Extrinsic-environment/source-tracking-stream.lisp
@@ -58,3 +58,11 @@
   ((%lines :initarg :lines :reader lines)
    (%line-index :initarg :line-index :reader line-index)
    (%character-index :initarg :character-index :reader character-index)))
+
+;;; FIXME: Use SICL-specific client.
+(defmethod eclector.concrete-syntax-tree:source-position
+    ((stream source-tracking-stream) client)
+  (make-instance 'source-position
+    :lines (lines stream)
+    :line-index (current-line-index stream)
+    :character-index (current-character-index stream)))
