6d8db24be6c3edd64c634442c67dcbf9229fefe0
Charles Zhang <charleszhang99@yahoo.com>
2018-07-29 15:05:21 -0400

    Rewrite r-u-i to use worklists and eliminate catches

--- Code/Cleavir/HIR-transformations/Remove-useless-instructions/remove-useless-instructions.lisp
+++ Code/Cleavir/HIR-transformations/Remove-useless-instructions/remove-useless-instructions.lisp
@@ -2,10 +2,11 @@
 
 (defgeneric instruction-may-be-removed-p (instruction))
 
-(defmethod instruction-may-be-removed-p (instruction)
-  (and (= (length (cleavir-ir:successors instruction)) 1)
-       (loop for output in (cleavir-ir:outputs instruction)
-	     always (null (cleavir-ir:using-instructions output)))))
+(defmethod instruction-may-be-removed-p (instruction) t)
+
+(defmethod instruction-may-be-removed-p ((instruction cleavir-ir:eq-instruction)) nil)
+
+(defmethod instruction-may-be-removed-p ((instruction cleavir-ir:catch-instruction)) t)
 
 (defmethod instruction-may-be-removed-p
     ((instruction cleavir-ir:side-effect-mixin))
@@ -15,16 +16,44 @@
     ((instruction cleavir-ir:enter-instruction))
   nil)
 
+(defgeneric remove-instruction (instruction))
+
+(defmethod remove-instruction (instruction)
+  (error "remove-instruction is not implemented for this type of instruction."))
+
+(defmethod remove-instruction ((instruction cleavir-ir:one-successor-mixin))
+  (cleavir-ir:delete-instruction instruction))
+
+;; We can eliminate catch instructions when the continuation is unused.
+(defmethod remove-instruction ((instruction cleavir-ir:catch-instruction))
+  (cleavir-ir:bypass-instruction (first (cleavir-ir:successors instruction))
+                                 instruction))
+
+(defun remove-useless-instructions-with-worklist (initial-instruction worklist)
+   (let ((worklist worklist))
+    (loop (when (null worklist) (return))
+          (let ((instruction (pop worklist)))
+            (when (and (instruction-may-be-removed-p instruction)
+                       (every (lambda (output)
+                                (null (cleavir-ir:using-instructions output)))
+                              (cleavir-ir:outputs instruction)))
+              (dolist (output (cleavir-ir:outputs instruction))
+                (setf (cleavir-ir:defining-instructions output)
+                      (remove instruction (cleavir-ir:defining-instructions output))))
+              (dolist (input (cleavir-ir:inputs instruction))
+                (setf (cleavir-ir:using-instructions input)
+                      (remove instruction (cleavir-ir:using-instructions input)))
+                (dolist (defining-instruction (cleavir-ir:defining-instructions input))
+                  (push defining-instruction worklist)))
+              (remove-instruction instruction))))))
+
 (defun remove-useless-instructions (initial-instruction)
-  (cleavir-meter:with-meter (m *remove-useless-instructions-meter*)
-    (loop do (cleavir-ir:reinitialize-data initial-instruction)
-	     (let ((useless-instructions '()))
-	       (cleavir-ir:map-instructions-arbitrary-order
-		(lambda (instruction)
-		  (cleavir-meter:increment-size m)
-		  (when (instruction-may-be-removed-p instruction)
-		    (push instruction useless-instructions)))
-		initial-instruction)
-	       (when (null useless-instructions)
-		 (loop-finish))
-	       (mapc #'cleavir-ir:delete-instruction useless-instructions)))))
+  (remove-useless-instructions-with-worklist initial-instruction
+                                             (cleavir-ir:instructions-of-type initial-instruction t)))
+
+;;; An incremental version of remove-useless-instructions meant for
+;;; clients which know where in the graph a potentially useless
+;;; instruction has appeared.
+(defun remove-useless-instructions-from (initial-instruction instruction)
+  (remove-useless-instructions-with-worklist initial-instruction
+                                             (list instruction)))
