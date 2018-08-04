cf4444a5e0b5748d99d70dcad8958032b6e31201
Charles Zhang <charleszhang99@yahoo.com>
2018-08-04 14:48:09 -0400

    Convert LETs into assignments

--- Code/Cleavir/CST-to-AST/convert-let-and-letstar.lisp
+++ Code/Cleavir/CST-to-AST/convert-let-and-letstar.lisp
@@ -36,8 +36,40 @@
         until (cst:null remaining)
         do (check-binding (cst:first remaining))))
 
-;;; We convert a LET form CST by transforming it into an equivalent
-;;; LAMBDA form CST.
+(defun make-let-init-asts (initform-csts temp-asts idspecs environment system)
+  (loop for initform-cst in initform-csts
+        for converted = (convert initform-cst environment system)
+        for temp-ast in temp-asts
+        collect (cleavir-ast:make-setq-ast temp-ast converted)))
+
+;;; We convert a LET form CST by assignment.
+(defun process-remaining-let-bindings (bindings idspecs rdspecs body-forms-cst environment system)
+  (if (null bindings)
+      ;; We ran out of bindings.  We must build an AST for the body of
+      ;; the function.
+      (let ((new-env (augment-environment-with-declarations environment rdspecs)))
+	(process-progn (convert-sequence body-forms-cst new-env system)))
+      (destructuring-bind (variable-cst . lexical-ast) (first bindings)
+	(let* (;; We enter the new variable into the environment and
+	       ;; then we process remaining parameters and ultimately
+	       ;; the body of the function.
+	       (new-env (augment-environment-with-variable
+			 variable-cst (first idspecs) environment environment))
+	       ;; We compute the AST of the remaining computation by
+	       ;; recursively calling this same function with the
+	       ;; remaining bindings (if any) and the environment that
+	       ;; we obtained by augmenting the original one with the
+	       ;; parameter variable.
+	       (next-ast (process-remaining-let-bindings (rest bindings)
+							 (rest idspecs)
+							 rdspecs
+							 body-forms-cst
+							 new-env
+							 system)))
+	  ;; All that is left to do now, is to construct the AST to
+	  ;; return by using the new variable and the AST of the
+	  ;; remaining computation as components.
+	  (set-or-bind-variable variable-cst lexical-ast next-ast new-env system)))))
 
 (defmethod convert-let (cst environment system)
   (when (cst:null (cst:rest cst))
@@ -47,27 +79,35 @@
   (cst:db origin (let-cst bindings-cst . body-forms-cst) cst
     (declare (ignore let-cst))
     (check-bindings bindings-cst)
-    (let* ((binding-csts (cst:listify bindings-cst))
-           (variable-csts (loop for binding-cst in binding-csts
-                                collect (if (cst:atom binding-cst)
-                                            binding-cst
-                                            (cst:first binding-cst))))
-           (initform-csts (loop for binding-cst in binding-csts
-                                collect (if (or (cst:atom binding-cst)
-                                                (cst:null (cst:rest binding-cst)))
-                                            (cst:cst-from-expression 'nil)
-                                            (cst:second binding-cst))))
-           (lambda-form-cst
-             (make-instance 'cst:cons-cst
-               :raw `((lambda ,(mapcar #'cst:raw variable-csts)
-                        ,@(cst:raw body-forms-cst))
-                      ,(mapcar #'cst:raw initform-csts))
-               :source origin
-               :first (cst:cons (cst:cst-from-expression 'lambda)
-                                (cst:cons (cst:cstify variable-csts)
-                                          body-forms-cst))
-               :rest (cst:cstify initform-csts))))
-      (convert lambda-form-cst environment system))))
+    (multiple-value-bind (declarations-cst body-forms-cst)
+        (cst:separate-ordinary-body body-forms-cst)
+      (let* ((canonical-dspecs
+               (cst:canonicalize-declarations system declarations-cst))
+             (binding-csts (cst:listify bindings-cst))
+             (variable-csts (loop for binding-cst in binding-csts
+                                  collect (if (cst:atom binding-cst)
+                                              binding-cst
+                                              (cst:first binding-cst))))
+             (initform-csts (loop for binding-cst in binding-csts
+                                  collect (if (or (cst:atom binding-cst)
+                                                  (cst:null (cst:rest binding-cst)))
+                                              (cst:cst-from-expression 'nil)
+                                              (cst:second binding-cst))))
+             (temp-asts (loop repeat (length binding-csts)
+                              collect (cleavir-ast:make-lexical-ast (gensym)))))
+        (multiple-value-bind (idspecs rdspecs)
+            (itemize-declaration-specifiers (mapcar #'list variable-csts)
+                                            canonical-dspecs)
+          (process-progn
+           (append (make-let-init-asts initform-csts temp-asts
+                                       idspecs environment system)
+                   (list (process-remaining-let-bindings
+                          (mapcar #'cons variable-csts temp-asts)
+                          idspecs
+                          rdspecs
+                          body-forms-cst
+                          environment
+                          system)))))))))
 
 ;;; We convert a LET* form CST by transforming it into nested LET form
 ;;; CSTs and then converting those instead.  This is not trivial,
