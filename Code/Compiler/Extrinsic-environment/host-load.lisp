089793f3129e7ed3ef838b15b5f801374bd4d86f
Robert Strandh <robert.strandh@gmail.com>
2018-08-06 13:47:20 +0200

    Make directory relative to an ASDF system.

--- Code/Compiler/Extrinsic-environment/host-load.lisp
+++ Code/Compiler/Extrinsic-environment/host-load.lisp
@@ -1,7 +1,9 @@
 (cl:in-package #:sicl-extrinsic-environment)
 
-(defun host-load (filename)
-  (let ((*package* *package*))
+(defun host-load (relative-filename)
+  (let ((*package* *package*)
+        (filename (asdf:system-relative-pathname :sicl-extrinsic-environment
+                                                 relative-filename)))
     (with-open-file (stream filename :direction :input)
       (loop with eof-marker = (list nil)
             for form = (eclector.reader:read stream nil eof-marker)
