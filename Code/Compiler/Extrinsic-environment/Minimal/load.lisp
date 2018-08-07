bf33b84ec5560f1a1478909d58db8dcb302be625
Robert Strandh <robert.strandh@gmail.com>
2018-08-07 15:55:30 +0200

    Load relative to the SICL root directory.

--- Code/Compiler/Extrinsic-environment/Minimal/load.lisp
+++ Code/Compiler/Extrinsic-environment/Minimal/load.lisp
@@ -32,7 +32,7 @@
   (format *trace-output* "CST Loading file ~a~%" filename)
   (finish-output *trace-output*)
   (cst-load-source-with-environments
-   (asdf:system-relative-pathname :sicl-minimal-extrinsic-environment filename)
+   (asdf:system-relative-pathname '#:sicl filename)
    environment
    environment
    system))
