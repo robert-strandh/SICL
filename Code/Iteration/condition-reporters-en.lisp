6589d18dc7c277519a819894e0f51574e2f0aa01
Robert Strandh <robert.strandh@gmail.com>
2018-08-09 14:21:48 +0200

    Untabify for better tool support.

--- Code/Iteration/condition-reporters-en.lisp
+++ Code/Iteration/condition-reporters-en.lisp
@@ -8,87 +8,87 @@
 ;;; reporters.  I now think it is OK.  RS -- 2014-09-15.
 
 (defmethod acclimation:report-condition ((c malformed-binding-var)
-					  stream
-					  (language acclimation:english))
+                                          stream
+                                          (language acclimation:english))
   (format stream
-	  "In ~a (in the ~a package):~@
+          "In ~a (in the ~a package):~@
            The binding variable must be a symbol,~@
            but the following was given:~@
            ~s"
-	  (name c)
-	  (name-package (name c))
+          (name c)
+          (name-package (name c))
           (type-error-datum c)))
 
 (defmethod acclimation:report-condition ((c malformed-list-form)
-					  stream
-					  (language acclimation:english))
+                                          stream
+                                          (language acclimation:english))
   (format stream
-	  "In ~a (in the ~a package):~@
+          "In ~a (in the ~a package):~@
            The list form must be a list,~@
            but the following was given:~@
            ~s"
-	  (name c)
-	  (name-package (name c))
-	  (type-error-datum c)))
+          (name c)
+          (name-package (name c))
+          (type-error-datum c)))
 
 (defmethod acclimation:report-condition ((c malformed-count-form)
-					  stream
-					  (language acclimation:english))
+                                          stream
+                                          (language acclimation:english))
   (format stream
-	  "In ~a (in the ~a package):~@
+          "In ~a (in the ~a package):~@
            The count form must be a non-negative integer,~@
            but the following was given:~@
            ~s"
-	  (name c)
-	  (name-package (name c))
-	  (type-error-datum c)))
+          (name c)
+          (name-package (name c))
+          (type-error-datum c)))
 
 (defmethod acclimation:report-condition ((c malformed-body)
-					  stream
-					  (language acclimation:english))
+                                          stream
+                                          (language acclimation:english))
   (format stream
-	  "In ~a (in the ~a package):~@
+          "In ~a (in the ~a package):~@
            The body must be a proper list,~@
            but the following was given:~@
            ~s"
-	  (name c)
-	  (name-package (name c))
-	  (type-error-datum c)))
+          (name c)
+          (name-package (name c))
+          (type-error-datum c)))
 
 (defmethod acclimation:report-condition ((c malformed-variable-clauses)
-					  stream
-					  (language acclimation:english))
+                                          stream
+                                          (language acclimation:english))
   (format stream
-	  "In ~a (in the ~a package):~@
+          "In ~a (in the ~a package):~@
            Expected a proper list of variable clauses~@
            but the following was given:~@
            ~s"
-	  (name c)
-	  (name-package (name c))
-	  (type-error-datum c)))
+          (name c)
+          (name-package (name c))
+          (type-error-datum c)))
 
 (defmethod acclimation:report-condition ((c malformed-variable-clause)
-					  stream
-					  (language acclimation:english))
+                                          stream
+                                          (language acclimation:english))
   (format stream
-	  "In ~a (in the ~a package):~@
+          "In ~a (in the ~a package):~@
            Expected a variable clause of the form~@
            var, (var), (var init-form), or (var init-form step-form),~@
            but the following was given:~@
            ~s"
-	  (name c)
-	  (name-package (name c))
-	  (found c)))
+          (name c)
+          (name-package (name c))
+          (found c)))
 
 (defmethod acclimation:report-condition ((c malformed-end-test)
-					  stream
-					  (language acclimation:english))
+                                          stream
+                                          (language acclimation:english))
   (format stream
-	  "In ~a (in the ~a package):~@
+          "In ~a (in the ~a package):~@
            Expected an end test clause of the form~@
            (end-test result-form*),
            but the following was given:~@
            ~s"
-	  (name c)
-	  (name-package (name c))
-	  (found c)))
+          (name c)
+          (name-package (name c))
+          (found c)))
