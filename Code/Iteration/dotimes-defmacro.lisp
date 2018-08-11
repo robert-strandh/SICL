ca98e8e73a9fc9daf2b095d3f77d3f824f737c84
Robert Strandh <robert.strandh@gmail.com>
2018-08-11 14:10:19 +0200

    Remove expansion code and just call the expanders.

--- Code/Iteration/dotimes-defmacro.lisp
+++ Code/Iteration/dotimes-defmacro.lisp
@@ -1,32 +1,4 @@
 (cl:in-package #:sicl-iteration)
 
-;;; Macro DOTIMES.
-;;;
-;;; The spec says we have a choice between binding or assigning the
-;;; variable in each iteration.  We have chosen to bind the variable
-;;; once for the entire loop body.
-
 (defmacro dotimes ((var count-form &optional result-form) &body body)
-  ;; do some syntax checking
-  (binding-var-must-be-symbol 'dotimes var)
-  (count-form-must-be-nonnegative-integer 'dotimes count-form)
-  (body-must-be-proper-list 'dotimes body)
-  (multiple-value-bind (declarations forms)
-      (cleavir-code-utilities:separate-ordinary-body body)
-    (let ((start-tag (gensym))
-	  (end-tag (gensym))
-	  (count-var (gensym)))
-      `(let ((,count-var ,count-form)
-	     (,var 0))
-	 (declare (type unsigned-byte ,var))
-	 ,@declarations
-	 (block nil
-	   (tagbody
-	      ,start-tag
-	      (when (= ,var ,count-var)
-		(go ,end-tag))
-	      (tagbody ,@forms)
-	      (incf ,var)
-	      (go ,start-tag)
-	      ,end-tag)
-	   ,result-form)))))
+  (dotimes-expander var count-form result-form body))
