59378560dc04c4126317792365676225c8da7da9
Robert Strandh <robert.strandh@gmail.com>
2018-08-11 15:19:01 +0200

    Import package STANDARD-ENVIRONMENT-MACROS from host.

--- Code/Compiler/Extrinsic-environment/import-from-host.lisp
+++ Code/Compiler/Extrinsic-environment/import-from-host.lisp
@@ -34,6 +34,7 @@
                          sicl-cons
                          sicl-iteration
                          sicl-clos
+                         sicl-standard-environment-macros
                          sicl-environment)
         do (import-functions-from-package-symbols package environment))
   (loop for name in '(symbol-value
