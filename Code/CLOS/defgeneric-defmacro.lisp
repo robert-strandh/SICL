0ebb2d4ddead8a1884c0cce742e29da17e2da318
Robert Strandh <robert.strandh@gmail.com>
2018-08-12 10:57:59 +0200

    Move expander code for DEFGENERIC to support component.

--- Code/CLOS/defgeneric-defmacro.lisp
+++ Code/CLOS/defgeneric-defmacro.lisp
@@ -1,23 +1,6 @@
 (cl:in-package #:sicl-clos)
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;;
-;;; DEFGENERIC.
-
-;;; FIXME: Add options and methods
 (defmacro defgeneric (&environment env
                         name lambda-list
                       &rest options-and-methods)
-  (declare (ignore options-and-methods))
-  (let* ((arg-type (cleavir-code-utilities:lambda-list-type-specifier lambda-list))
-         (function-type `(function ,arg-type t)))
-    `(progn (eval-when (:compile-toplevel)
-              (setf (sicl-global-environment:function-type ',name ,env)
-                    ',function-type))
-            (eval-when (:load-toplevel :execute)
-              (ensure-generic-function
-               ',name
-               :lambda-list ',lambda-list
-               :environment
-               (load-time-value
-                (sicl-global-environment:global-environment)))))))
+  (defgeneric-expander env name lambda-list options-and-methods))
--- Code/CLOS/defgeneric-support.lisp
+++ Code/CLOS/defgeneric-support.lisp
@@ -26,3 +26,19 @@
   (loop for method in (initial-methods generic-function)
         do (remove-method generic-function method))
   (setf (initial-methods generic-function) '()))
+
+;;; FIXME: Add options and methods
+(defun defgeneric-expander (env name lambda-list options-and-methods)
+  (declare (ignore options-and-methods))
+  (let* ((arg-type (cleavir-code-utilities:lambda-list-type-specifier lambda-list))
+         (function-type `(function ,arg-type t)))
+    `(progn (eval-when (:compile-toplevel)
+              (setf (sicl-global-environment:function-type ',name ,env)
+                    ',function-type))
+            (eval-when (:load-toplevel :execute)
+              (ensure-generic-function
+               ',name
+               :lambda-list ',lambda-list
+               :environment
+               (load-time-value
+                (sicl-global-environment:global-environment)))))))
