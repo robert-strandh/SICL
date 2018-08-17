f72fd003d693cef0e8926a335c3194ac28e82fdc
Robert Strandh <robert.strandh@gmail.com>
2018-08-17 11:08:55 +0200

    Add comment explaining why the :AROUND method is trivial.

--- Code/CLOS/generic-function-initialization-defmethods.lisp
+++ Code/CLOS/generic-function-initialization-defmethods.lisp
@@ -1,5 +1,14 @@
 (cl:in-package #:sicl-clos)
 
+;;; This :AROUND method on SHARED-INITIALIZE is meant to implement the
+;;; generic-function initialization protocol as describe in the AMOP.
+;;; We define a trivial version that trampolines to an ordinary
+;;; function.  That way, during bootstrapping we can have the ordinary
+;;; function evaluated by the host and we can define special-purpose
+;;; versions of this :AROUND method.  Notice that the function
+;;; CALL-NEXT-METHOD is passed as an additional argument to the
+;;; ordinary function.
+
 (defmethod shared-initialize :around
     ((generic-function generic-function)
      slot-names
