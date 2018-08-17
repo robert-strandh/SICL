ea52951aaf53b5a25ff1a1b0898dc9f5c10c626d
Robert Strandh <robert.strandh@gmail.com>
2018-08-17 08:44:33 +0200

    Add comment explaining why the :AROUND method trampolines to an ordinary function.

--- Code/CLOS/class-initialization-defmethods.lisp
+++ Code/CLOS/class-initialization-defmethods.lisp
@@ -1,5 +1,13 @@
 (cl:in-package #:sicl-clos)
 
+;;; This :AROUND method on SHARED-INITIALIZE is meant to implement the
+;;; class-initialization protocol as describe in the AMOP.  We define
+;;; a trivial version that trampolines to an ordinary function.  That
+;;; way, during bootstrapping we can have the ordinary function
+;;; evaluated by the host and we can define special-purpose versions
+;;; of this :AROUND method.  Notice that the function CALL-NEXT-METHOD
+;;; is passed as an additional argument to the ordinary function.
+
 (defmethod shared-initialize :around
     ((class real-class)
      slot-names
