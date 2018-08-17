4046325ec2d549ec8a2898a6301b751603278716
Robert Strandh <robert.strandh@gmail.com>
2018-08-17 08:39:13 +0200

    Take environments as argumets.

--- Code/New-boot/Phase-1/boot-phase-1.lisp
+++ Code/New-boot/Phase-1/boot-phase-1.lisp
@@ -10,11 +10,9 @@
   (sicl-minimal-extrinsic-environment:import-function-from-host
    '(setf sicl-genv:special-variable) env))
 
-(defun boot-phase-1 ()
-  (let ((e1 (make-instance 'sicl-minimal-extrinsic-environment:environment))
-        (e2 (make-instance 'sicl-minimal-extrinsic-environment:environment)))
-    (import-more-to-env1 e1)
-    (sicl-minimal-extrinsic-environment:import-package-from-host 'sicl-clos e2)
-    (load-accessor-defgenerics e2)
-    (create-mop-classes-phase1 e1 e2)
-    (list e1 e2)))
+(defun boot-phase-1 (e1 e2)
+  (import-more-to-env1 e1)
+  (sicl-minimal-extrinsic-environment:import-package-from-host 'sicl-clos e2)
+  (load-accessor-defgenerics e2)
+  (create-mop-classes-phase1 e1 e2)
+  (list e1 e2))
