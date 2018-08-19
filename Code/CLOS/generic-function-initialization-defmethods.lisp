a14f38134f75a6f4648421c5fe97ae12b55f1c5b
Robert Strandh <robert.strandh@gmail.com>
2018-08-19 10:47:20 +0200

    Improve comment.

--- Code/CLOS/generic-function-initialization-defmethods.lisp
+++ Code/CLOS/generic-function-initialization-defmethods.lisp
@@ -1,13 +1,15 @@
 (cl:in-package #:sicl-clos)
 
 ;;; This :AROUND method on SHARED-INITIALIZE is meant to implement the
-;;; generic-function initialization protocol as describe in the AMOP.
+;;; generic-function initialization protocol as described in the AMOP.
 ;;; We define a trivial version that trampolines to an ordinary
 ;;; function.  That way, during bootstrapping we can have the ordinary
-;;; function evaluated by the host and we can define special-purpose
+;;; function executed by the host and we can define special-purpose
 ;;; versions of this :AROUND method.  Notice that the function
 ;;; CALL-NEXT-METHOD is passed as an additional argument to the
-;;; ordinary function.
+;;; ordinary function.  This is also the case of the function
+;;; INVALIDATE-DISCRIMINATING-FUNCTION.  This way, the ordinary
+;;; function does not have to refer to a globally defined function.
 
 (defmethod shared-initialize :around
     ((generic-function generic-function)
