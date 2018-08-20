1bf369323879002127f63cbdf957df636fa67732
Robert Strandh <robert.strandh@gmail.com>
2018-08-20 11:55:29 +0200

    Check that each specializer is either a symbol or a specializer metaobject.

--- Code/CLOS/ensure-method.lisp
+++ Code/CLOS/ensure-method.lisp
@@ -23,21 +23,28 @@
 ;;; compile-time environment) in which the call to DEFMETHOD is made.
 ;;;
 ;;; Since the environment might be a lexical environment or a global
-;;; environment, we use the function SICL-ENVIRONMENT:FIND-CLASS for
-;;; the lookup, because that function automatically finds the global
-;;; environment if it is given a lexical environment.
+;;; environment, we use the function
+;;; SICL-GLOBAL-ENVIRONMENT:FIND-CLASS for the lookup, because that
+;;; function automatically finds the global environment if it is given
+;;; a lexical environment.
+
+(defun make-specializer (specializer environment)
+  (cond ((symbolp specializer)
+         (sicl-genv:find-class s environment))
+        ((typep specializer 'specializer environment)
+         specializer)
+        (t
+         (error "Specializer must be symbol or specializer metaobject: ~s"
+                specializer))))
 
 (defun ensure-method (generic-function
 		      environment
 		      &rest keys
 		      &key specializers
 		      &allow-other-keys)
-  ;; FIXME: Check that SPECIALIZERS is a proper list, and that
-  ;; it contains only symbols and specializer metaobjects. 
+  ;; FIXME: Check that SPECIALIZERS is a proper list.
   (let ((specs (loop for s in specializers
-		     collect (if (symbolp s)
-				 (sicl-genv:find-class s environment)
-				 s)))
+		     collect (make-specializer s environment)))
 	(remaining-keys (copy-list keys)))
     (loop while (remf remaining-keys :specializers))
     (let ((method (apply #'make-instance
