ceb1141d60680032e105b2f2cb31effb9b639ce1
Robert Strandh <robert.strandh@gmail.com>
2018-08-23 16:09:51 +0200

    Define function BOOT-PHASE-3.

--- Code/New-boot/Phase-3/boot-phase-3.lisp
+++ Code/New-boot/Phase-3/boot-phase-3.lisp
@@ -1,1 +1,12 @@
 (cl:in-package #:sicl-new-boot-phase-3)
+
+(defclass environment (sicl-minimal-extrinsic-environment:environment)
+  ())
+
+(defun boot-phase-3 (boot)
+  (format *trace-output* "Start of phase 3~%")
+  (with-accessors ((e1 sicl-new-boot:e1)
+                   (e2 sicl-new-boot:e2)
+                   (e3 sicl-new-boot:e3)) boot
+    (change-class e3 'environment)
+    (load-file "CLOS/class-finalization-support.lisp" e2)))
