c9a86407dbd3b15d5f28964117415b93c49f6941
Robert Strandh <robert.strandh@gmail.com>
2018-08-23 16:19:31 +0200

    Define utility for importing several funtions from the host.

--- Code/New-boot/Phase-3/utilities.lisp
+++ Code/New-boot/Phase-3/utilities.lisp
@@ -2,3 +2,10 @@
 
 (defun load-file (file environment)
   (sicl-minimal-extrinsic-environment:cst-load-file file environment nil))
+
+(defun import-function-from-host (name environment)
+  (sicl-minimal-extrinsic-environment:import-function-from-host name environment))
+
+(defun import-functions-from-host (names environment)
+  (loop for name in names
+        do (import-function-from-host name environment)))
