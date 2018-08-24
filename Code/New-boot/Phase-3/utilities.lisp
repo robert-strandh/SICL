f8ce9eb0ec0a08c0c27ae4fba7cd0048b8df143a
Robert Strandh <robert.strandh@gmail.com>
2018-08-24 17:50:31 +0200

    Define function LOAD-FILE-PROTECTED.

--- Code/New-boot/Phase-3/utilities.lisp
+++ Code/New-boot/Phase-3/utilities.lisp
@@ -3,6 +3,23 @@
 (defun load-file (file environment)
   (sicl-minimal-extrinsic-environment:cst-load-file file environment nil))
 
+;;; Function LOAD-FILE-PROTECTED (protected loading).  It wraps the
+;;; loading of a file in a handler that invokes a restart that tells
+;;; the compiler to treat all undefined functions as if they were
+;;; global.  This function should only be used in exceptional cases
+;;; because it is better to find undefined functions at compile time
+;;; than at run time.  However, sometimes we need it.  In particular,
+;;; since Cleavir is currently not smart enough to recognize
+;;; self-recursive functions, we need this function in those
+;;; situations.
+(defun load-file-protected (file environment)
+  (handler-bind
+      ((cleavir-env:no-function-info
+	 (lambda (condition)
+	   (declare (ignore condition))
+	   (invoke-restart 'cleavir-cst-to-ast:consider-global))))
+    (load-file file environment)))
+
 (defun import-function-from-host (name environment)
   (sicl-minimal-extrinsic-environment:import-function-from-host name environment))
 
