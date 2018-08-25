d59468e6a27bae7a77e41156336f275fddb78bb6
Robert Strandh <robert.strandh@gmail.com>
2018-08-25 13:57:46 +0200

    Extract class finalization to a separate function.

--- Code/New-boot/Phase-3/boot-phase-3.lisp
+++ Code/New-boot/Phase-3/boot-phase-3.lisp
@@ -8,16 +8,10 @@
    (%rack :initarg :rack))
   (:metaclass closer-mop:funcallable-standard-class))
 
-(defun boot-phase-3 (boot)
-  (format *trace-output* "Start of phase 3~%")
+(defun activate-class-finalization (boot)
   (with-accessors ((e1 sicl-new-boot:e1)
                    (e2 sicl-new-boot:e2)
                    (e3 sicl-new-boot:e3)) boot
-    (change-class e3 'environment)
-    (import-functions-from-host
-     '(find reverse last remove-duplicates reduce mapcar union find-if-not
-       eql count)
-     e2)
     (setf (sicl-genv:special-variable
            'sicl-clos::*standard-direct-slot-definition* e2 t)
           (sicl-genv:find-class 'sicl-clos:standard-direct-slot-definition e1))
@@ -25,12 +19,24 @@
            'sicl-clos::*standard-effective-slot-definition* e2 t)
           (sicl-genv:find-class 'sicl-clos:standard-effective-slot-definition e1))
     (sicl-genv:fmakunbound 'sicl-clos:direct-slot-definition-class e2)
+    (import-functions-from-host
+     '(find reverse last remove-duplicates reduce
+       mapcar union find-if-not eql count)
+     e2)
     (load-file "CLOS/slot-definition-class-support.lisp" e2)
     (load-file "CLOS/slot-definition-class-defgenerics.lisp" e2)
     (load-file "CLOS/slot-definition-class-defmethods.lisp" e2)
     (load-file "CLOS/class-finalization-defgenerics.lisp" e2)
     (load-file "CLOS/class-finalization-support.lisp" e2)
-    (load-file "CLOS/class-finalization-defmethods.lisp" e2)
+    (load-file "CLOS/class-finalization-defmethods.lisp" e2)))
+
+(defun boot-phase-3 (boot)
+  (format *trace-output* "Start of phase 3~%")
+  (with-accessors ((e1 sicl-new-boot:e1)
+                   (e2 sicl-new-boot:e2)
+                   (e3 sicl-new-boot:e3)) boot
+    (change-class e3 'environment)
+    (activate-class-finalization boot)
     (setf (sicl-genv:fdefinition 'sicl-clos::allocate-general-instance e2)
           (lambda (class size)
             (make-instance 'header
