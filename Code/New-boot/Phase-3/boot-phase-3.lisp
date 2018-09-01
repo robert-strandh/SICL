48ffff8bcddb1d9f3e6171b6e6ae59a12bc59f91
Robert Strandh <robert.strandh@gmail.com>
2018-09-01 15:44:23 +0200

    Load a file containing definition of constant +INSTANCE-SLOTS-OFFSET+.

--- Code/New-boot/Phase-3/boot-phase-3.lisp
+++ Code/New-boot/Phase-3/boot-phase-3.lisp
@@ -243,7 +243,8 @@
     (load-file "CLOS/slot-bound-using-index.lisp" e2)
     (load-file "CLOS/slot-value-etc-defgenerics.lisp" e2)
     (load-file "CLOS/slot-value-etc-support.lisp" e2)
-    (load-file "CLOS/slot-value-etc-defmethods.lisp" e2)))
+    (load-file "CLOS/slot-value-etc-defmethods.lisp" e2)
+    (load-file "CLOS/instance-slots-offset-defconstant.lisp" e2)))
 
 (defun activate-defmethod-in-e3 (boot)
   (with-accessors ((e1 sicl-new-boot:e1)
