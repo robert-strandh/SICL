c1e94df224da70fd3468a103e17cc8d62bd5192a
Robert Strandh <robert.strandh@gmail.com>
2018-09-03 13:11:28 +0200

    Remove error case.  It is taken care of by the :REQUIRED T option.

--- Code/CLOS/standard-method-combination.lisp
+++ Code/CLOS/standard-method-combination.lisp
@@ -8,21 +8,18 @@
   (flet ((call-methods (methods)
            (loop for method in methods
                  collect `(call-method ,method))))
-    (if (null primary-methods)
-        '(lambda (&rest args)
-          (error "no primary method"))
-        (let ((all-but-around-form
-                (if (and (null before-methods)
-                         (null after-methods)
-                         (null (rest primary-methods)))
-                    `(call-method ,(first primary-methods))
-                    `(multiple-value-prog1
-                         (progn ,@(call-methods before-methods)
-                                (call-method ,(first primary-methods)
-                                             ,(rest primary-methods)))
-                       ,@(call-methods (reverse after-methods))))))
-          (if (null around-methods)
-              form
-              `(call-method ,(first around-methods)
-                            (,@(rest around-methods)
-                             (make-method ,all-but-around-form))))))))
+    (let ((all-but-around-form
+            (if (and (null before-methods)
+                     (null after-methods)
+                     (null (rest primary-methods)))
+                `(call-method ,(first primary-methods))
+                `(multiple-value-prog1
+                     (progn ,@(call-methods before-methods)
+                            (call-method ,(first primary-methods)
+                                         ,(rest primary-methods)))
+                   ,@(call-methods (reverse after-methods))))))
+      (if (null around-methods)
+          form
+          `(call-method ,(first around-methods)
+                        (,@(rest around-methods)
+                         (make-method ,all-but-around-form)))))))
