73c6154a4e79e4225f222709374e17b3651f51d7
Robert Strandh <robert.strandh@gmail.com>
2018-09-06 09:15:17 +0200

    Supply the variant determiner.

--- Code/Method-combination/long-form-expansion.lisp
+++ Code/Method-combination/long-form-expansion.lisp
@@ -1,8 +1,8 @@
 (cl:in-package #:sicl-method-combination)
 
-(defun long-form-lambda (lambda-list method-group-specifiers declarations body)
-  (let ((lambda-list-variables (lambda-list-variables lambda-list))
-        (method-list-var (gensym "method-list")))
+(defun long-form-lambda
+    (lambda-list-variables method-group-specifiers declarations body)
+  (let ((method-list-var (gensym "method-list")))
     `(lambda ,lambda-list-variables
        ,@declarations
        ,(wrap-body method-list-var method-group-specifiers body))))
@@ -11,12 +11,19 @@
 ;;; yet.
 (defun long-form-expander
     (environment name lambda-list method-group-specifiers body)
-  (multiple-value-bind (declarations documentation forms)
-      (cleavir-code-utilities:separate-function-body body)
-    `(setf (sicl-genv:find-method-combination-template ',name ,environment)
-           (make-instance 'method-combination-template
-             :name ',name
-             :documentation ,documentation
-             :effective-method-form-function
-             ,(long-form-lambda
-               lambda-list method-group-specifiers declarations forms)))))
+  (let ((lambda-list-variables (lambda-list-variables lambda-list)))
+    (multiple-value-bind (declarations documentation forms)
+        (cleavir-code-utilities:separate-function-body body)
+      `(setf (sicl-genv:find-method-combination-template ',name ,environment)
+             (make-instance 'method-combination-template
+               :name ',name
+               :documentation ,documentation
+               :variant-signature-determiner
+               (lambda ,lambda-list-variables
+                 (list ,@lambda-list-variables))
+               :effective-method-form-function
+               ,(long-form-lambda
+                 lambda-list-variables
+                 method-group-specifiers
+                 declarations
+                 forms))))))
