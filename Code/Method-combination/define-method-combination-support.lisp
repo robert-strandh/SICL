22dec85aaf89b2a562ac7ea8b5983811f24b5b19
Robert Strandh <robert.strandh@gmail.com>
2018-09-06 08:47:26 +0200

    Supply the code for the general expander.

--- Code/Method-combination/define-method-combination-support.lisp
+++ Code/Method-combination/define-method-combination-support.lisp
@@ -1,1 +1,14 @@
 (cl:in-package #:sicl-method-combination)
+
+(defun define-method-combination-expander (environment name &rest more-arguments)
+  (if (or (null more-arguments)
+          (and (symbolp (first more-arguments))
+               (not (null (first more-arguments)))))
+      (short-form-expander name more-arguments)
+      (destructuring-bind (lambda-list method-group-specifiers . body)
+          more-arguments
+        (long-form-expander environment
+                            name
+                            lambda-list
+                            method-group-specifiers
+                            body))))
