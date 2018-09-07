57bbeead7374210364114320262fafe97ca5b38c
Robert Strandh <robert.strandh@gmail.com>
2018-09-07 15:32:17 +0200

    New component containing environment-specific version of FIND-METHOD-COMBINATION.

---
+++ Code/Method-combination/find-method-combination.lisp
@@ -0,0 +1,17 @@
+(cl:in-package #:sicl-method-combination)
+
+(defun find-method-combination (name options environment)
+  (let ((template (sicl-genv:find-method-combination-template name environment)))
+    (when (null template)
+      (error "no such method combination ~s" name))
+    (let* ((variant-determiner (variant-signature-determiner template))
+           (variant-signature (apply variant-determiner options)))
+      (loop for variant in (variants template)
+            do (when (equal (sicl-clos:variant-signature variant)
+                            variant-signature)
+                 (return-from find-method-combination variant)))
+      (let ((new-variant (make-instance 'method-combination
+                           :variant-signature variant-signature
+                           :template template)))
+        (push new-variant (variants template))
+        new-variant))))
