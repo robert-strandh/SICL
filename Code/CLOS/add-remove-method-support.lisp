ca61832962c591e1c040b57726c4982ef1a3fb57
Robert Strandh <robert.strandh@gmail.com>
2018-09-07 15:37:28 +0200

    Untabify and remove useless whitespace.

--- Code/CLOS/add-remove-method-support.lisp
+++ Code/CLOS/add-remove-method-support.lisp
@@ -4,7 +4,7 @@
 ;;;; ADD-METHOD and REMOVE-METHOD.
 ;;;;
 ;;;; In this file, there are no definitions of generic functions, nor
-;;;; of any methods.  
+;;;; of any methods.
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;
@@ -14,23 +14,23 @@
 ;;; a list of specializers of a method.
 (defun update-specializer-profile (generic-function specializers)
   (setf (specializer-profile generic-function)
-	(loop with environment = (sicl-genv:global-environment)
+        (loop with environment = (sicl-genv:global-environment)
               with class-t = (sicl-genv:find-class 't environment)
               for specializer in specializers
-	      for p in (specializer-profile generic-function)
-	      collect (if (eq specializer class-t)
-			  p
-			  t))))
+              for p in (specializer-profile generic-function)
+              collect (if (eq specializer class-t)
+                          p
+                          t))))
 
 ;;; Compute a completely new specializer profile for a generic
 ;;; function.
 (defun compute-and-set-specializer-profile (generic-function)
   ;; Keep the length of the profile, but with all elements NIL.
   (setf (specializer-profile generic-function)
-	(make-list (length (specializer-profile generic-function))))
+        (make-list (length (specializer-profile generic-function))))
   (loop for method in (generic-function-methods generic-function)
-	for specializers = (method-specializers method)
-	do (update-specializer-profile generic-function specializers)))
+        for specializers = (method-specializers method)
+        do (update-specializer-profile generic-function specializers)))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;
@@ -47,22 +47,22 @@
 (defun add-method-default (generic-function method)
   (unless (null (method-generic-function method))
     (error "method is already associated with a generic-function ~s"
-	   method))
+           method))
   ;; FIXME: check that the lambda list are congruent
   ;;
   ;; See if GENERIC-FUNCTION has a method with the same specializers
   ;; and the same qualifiers as METHOD.  If such a method exists, it
   ;; must be removed before this method is added.
   (let ((method-to-remove
-	  (find-if (lambda (existing-method)
-		     ;; They must have the same qualifiers
-		     ;; and the same specializers.
-		     (and (null (set-exclusive-or
-				 (method-qualifiers method)
-				 (method-qualifiers existing-method)))
-			  (equal (method-specializers method)
-				 (method-specializers existing-method))))
-		   (generic-function-methods generic-function))))
+          (find-if (lambda (existing-method)
+                     ;; They must have the same qualifiers
+                     ;; and the same specializers.
+                     (and (null (set-exclusive-or
+                                 (method-qualifiers method)
+                                 (method-qualifiers existing-method)))
+                          (equal (method-specializers method)
+                                 (method-specializers existing-method))))
+                   (generic-function-methods generic-function))))
     (unless (null method-to-remove)
       (remove-method generic-function method-to-remove)))
   ;; Add this method to the set of methods of this generic function.
@@ -74,18 +74,18 @@
   (setf (method-generic-function method) generic-function)
   ;; Call ADD-DIRECT-METHOD for each of the specializers of METHOD.
   (loop for specializer in (method-specializers method)
-	do (add-direct-method specializer method))
+        do (add-direct-method specializer method))
   ;; In validate the current discriminating function so that it will
   ;; be recomputed next time the generic function is called.
   (invalidate-discriminating-function generic-function)
   ;; Update the dependents of GENERIC-FUNCTION.
   (map-dependents generic-function
-		  (lambda (dependent)
-		    (update-dependent generic-function
-				      dependent
-				      'add-method
-				      method))))
-  
+                  (lambda (dependent)
+                    (update-dependent generic-function
+                                      dependent
+                                      'add-method
+                                      method))))
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;
 ;;; Generic function REMOVE-METHOD.
@@ -101,12 +101,12 @@
 (defun remove-method-default (generic-function method)
   ;; Remove METHOD from the methods of GENERIC-FUNCTION.
   (setf (generic-function-methods generic-function)
-	(remove method (generic-function-methods generic-function)))
+        (remove method (generic-function-methods generic-function)))
   ;; Compute a new specializer profile for the generic function.
   (compute-and-set-specializer-profile generic-function)
   ;; Call REMOVE-DIRECT-METHOD for each of the specializers of METHOD.
   (loop for specializer in (method-specializers method)
-	do (remove-direct-method specializer method))
+        do (remove-direct-method specializer method))
   ;; Disassociate GENERIC-FUNCTION from METHOD.
   (setf (method-generic-function method) nil)
   ;; In validate the current discriminating function so that it will
@@ -114,8 +114,8 @@
   (invalidate-discriminating-function generic-function)
   ;; Update the dependents of GENERIC-FUNCTION.
   (map-dependents generic-function
-		  (lambda (dependent)
-		    (update-dependent generic-function
-				      dependent
-				      'remove-method
-				      method))))
+                  (lambda (dependent)
+                    (update-dependent generic-function
+                                      dependent
+                                      'remove-method
+                                      method))))
