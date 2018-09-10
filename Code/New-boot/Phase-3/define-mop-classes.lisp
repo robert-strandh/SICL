47de6277a6ec69a4f61ddb06783ae25851bfba6e
Robert Strandh <robert.strandh@gmail.com>
2018-09-10 10:21:46 +0200

    Define methods on TYPEP for CLASS and FUNCTION.

--- Code/New-boot/Phase-3/define-mop-classes.lisp
+++ Code/New-boot/Phase-3/define-mop-classes.lisp
@@ -1,5 +1,16 @@
 (cl:in-package #:sicl-new-boot-phase-3)
 
+(defmethod sicl-genv:typep
+    (object (type-specifier (eql 'function)) (environment environment))
+  (typep object 'function))
+
+;;; We only check for the type CLASS for reasons of catching errors,
+;;; but during bootstrapping, we completely control the arguments, so
+;;; we can simply return true here.
+(defmethod sicl-genv:typep
+    (object (type-specifier (eql 'class)) (environment environment))
+  t)
+
 (defun create-mop-classes (boot)
   (with-accessors ((e1 sicl-new-boot:e1)
                    (e2 sicl-new-boot:e2)
