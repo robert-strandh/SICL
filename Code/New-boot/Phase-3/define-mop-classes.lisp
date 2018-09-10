c869892cddddbde85e0a38e96f3100c37bbdd801
Robert Strandh <robert.strandh@gmail.com>
2018-09-10 15:57:14 +0200

    Define add-reader/writer-method in E3.

--- Code/New-boot/Phase-3/define-mop-classes.lisp
+++ Code/New-boot/Phase-3/define-mop-classes.lisp
@@ -11,6 +11,53 @@
     (object (type-specifier (eql 'class)) (environment environment))
   t)
 
+;;; This function defines functions ADD-READER-METHOD and
+;;; ADD-WRITER-METHOD in E3 that look up their generic functions in
+;;; E4.  They do not call ADD-METHOD, because ADD-METHOD is defined to
+;;; add methods to generic functions in E3.  The other defect these
+;;; functions have is that they do not set the specializer profile on
+;;; the generic function, so we have to do that manually in phase 4.
+(defun define-add-accessor-method (e2 e3 e4)
+  (setf (sicl-genv:fdefinition 'sicl-clos::add-reader-method e3)
+        (lambda (class function-name slot-definition)
+          (let* ((lambda-list '(object))
+                 (generic-function (sicl-genv:fdefinition function-name e4))
+                 (specializers (list class))
+                 (slot-name (slot-value slot-definition 'sicl-clos::%name))
+                 (method-function
+                   (compile nil `(lambda (arguments next-methods)
+                                   (declare (ignore next-methods))
+                                   (slot-value (car arguments) ',slot-name))))
+                 (method-class (sicl-genv:find-class
+                                'sicl-clos:standard-reader-method e2))
+                 (method (make-instance method-class
+                           :lambda-list lambda-list
+                           :qualifiers '()
+                           :specializers specializers
+                           :function method-function
+                           :slot-definition slot-definition)))
+            (push method (slot-value generic-function 'sicl-clos::%methods)))))
+  (setf (sicl-genv:fdefinition 'sicl-clos::add-writer-method e3)
+        (lambda (class function-name slot-definition)
+          (let* ((lambda-list '(object))
+                 (generic-function (sicl-genv:fdefinition function-name e4))
+                 (specializers (list (sicl-genv:find-class 't e3) class))
+                 (slot-name (slot-value slot-definition 'sicl-clos::%name))
+                 (method-function
+                   (compile nil `(lambda (arguments next-methods)
+                                   (declare (ignore next-methods))
+                                   (setf (slot-value (cadr arguments) ',slot-name)
+                                         (car arguments)))))
+                 (method-class (sicl-genv:find-class
+                                'sicl-clos:standard-writer-method e2))
+                 (method (make-instance method-class
+                           :lambda-list lambda-list
+                           :qualifiers '()
+                           :specializers specializers
+                           :function method-function
+                           :slot-definition slot-definition)))
+            (push method (slot-value generic-function 'sicl-clos::%methods))))))
+
 (defun create-mop-classes (boot)
   (with-accessors ((e1 sicl-new-boot:e1)
                    (e2 sicl-new-boot:e2)
@@ -30,4 +77,5 @@
     (load-file "CLOS/add-remove-direct-subclass-support.lisp" e3)
     (load-file "CLOS/add-remove-direct-subclass-defgenerics.lisp" e3)
     (load-file "CLOS/add-remove-direct-subclass-defmethods.lisp" e3)
+    (define-add-accessor-method e2 e3 e4)
     nil))
