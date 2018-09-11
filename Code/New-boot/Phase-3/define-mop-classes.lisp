9dbdf4ffa9b0712e93285a5d47d3096dc09899b3
Robert Strandh <robert.strandh@gmail.com>
2018-09-11 06:36:00 +0200

    Provide default list of superclasses if appropriate.

--- Code/New-boot/Phase-3/define-mop-classes.lisp
+++ Code/New-boot/Phase-3/define-mop-classes.lisp
@@ -73,7 +73,16 @@
                    &allow-other-keys)
             (let* ((metaclass-name (if metaclass-p metaclass 'standard-class))
                    (metaclass-class (sicl-genv:find-class metaclass-name e2))
-                   (superclasses (loop for name in direct-superclasses
+                   (superclass-names
+                     (if (null direct-superclasses)
+                         (cond ((eq metaclass-name 'standard-class)
+                                '(standard-object))
+                               ((eq metaclass-name
+                                    'sicl-clos:funcallable-standard-class)
+                                '(funcallable-standard-object))
+                               (t '()))
+                         direct-superclasses))
+                   (superclasses (loop for name in superclass-names
                                        collect (sicl-genv:find-class name e2)))
                    (remaining-keys (copy-list keys)))
               (loop while (remf remaining-keys :metaclass))
