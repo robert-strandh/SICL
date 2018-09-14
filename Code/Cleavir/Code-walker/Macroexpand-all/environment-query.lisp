6b34ea6714c7c7c9e9995027a875ddf91637a80d
Robert Strandh <robert.strandh@gmail.com>
2018-09-14 12:00:49 +0200

    Do not use restarts or signal errors for non-existing variables.

--- Code/Cleavir/Code-walker/Macroexpand-all/environment-query.lisp
+++ Code/Cleavir/Code-walker/Macroexpand-all/environment-query.lisp
@@ -2,44 +2,15 @@
 
 (defun variable-info (environment symbol)
   (let ((result (cleavir-env:variable-info environment symbol)))
-    (loop while (null result)
-	  do (restart-case (error 'cleavir-env:no-variable-info
-				  :name symbol)
-	       (consider-special ()
-		 :report (lambda (stream)
-			   (format stream "Consider the variable as special."))
-		 (return-from variable-info
-		   (make-instance 'cleavir-env:special-variable-info
-		     :name symbol)))
-	       (substitute (new-symbol)
-		 :report (lambda (stream)
-			   (format stream "Substitute a different name."))
-		 :interactive (lambda ()
-				(format *query-io* "Enter new name: ")
-				(list (read *query-io*)))
-		 (setq result (cleavir-env:variable-info environment new-symbol)))))
-    result))
+    (if (null result)
+        (make-instance 'cleavir-env:special-variable-info :name symbol)
+        result)))
 
 (defun function-info (environment function-name)
   (let ((result (cleavir-env:function-info environment function-name)))
-    (loop while (null result)
-	  do (restart-case (error 'cleavir-env:no-function-info
-				  :name function-name)
-	       (consider-global ()
-		 :report (lambda (stream)
-			   (format stream
-				   "Treat it as the name of a global function."))
-		 (return-from function-info
-		   (make-instance 'cleavir-env:global-function-info
-		     :name function-name)))
-	       (substitute (new-function-name)
-		 :report (lambda (stream)
-			   (format stream "Substitute a different name."))
-		 :interactive (lambda ()
-				(format *query-io* "Enter new name: ")
-				(list (read *query-io*)))
-		 (setq result (cleavir-env:function-info environment new-function-name)))))
-    result))
+    (if (null result)
+        (make-instance 'cleavir-env:global-function-info :name function-name)
+        result)))
 
 (defun block-info (environment block-name)
   (let ((result (cleavir-env:block-info environment block-name)))
