9b76e699daf1fa1391bdab5d01819e875c3489d8
Robert Strandh <robert.strandh@gmail.com>
2018-09-16 10:00:44 +0200

    New component containing definition of primary method on FIND-METHOD-COMBINATION.

---
+++ Code/CLOS/find-method-combination-defmethods.lisp
@@ -0,0 +1,10 @@
+(cl:in-package #:sicl-clos)
+
+(defmethod find-method-combination
+    ((generic-function standard-generic-function)
+     method-combination-type-name
+     method-combination-options)
+  (sicl-method-combination:find-method-combination
+   method-combination-type-name
+   method-combination-options
+   (sicl-genv:global-environment)))
