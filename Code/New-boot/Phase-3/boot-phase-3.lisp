041ee955e7a995bdd1eeda0751c25d579febe69c
Robert Strandh <robert.strandh@gmail.com>
2018-09-21 17:55:39 +0200

    Extract code for satiating a single function to a separate function.

--- Code/New-boot/Phase-3/boot-phase-3.lisp
+++ Code/New-boot/Phase-3/boot-phase-3.lisp
@@ -60,27 +60,24 @@
     (load-file "CLOS/allocate-instance-support.lisp" e2)
     (load-file "CLOS/allocate-instance-defmethods.lisp" e2)))
 
+(defun satiate-function (function e2)
+  (funcall (sicl-genv:fdefinition
+            'sicl-clos::compute-and-set-specializer-profile e2)
+           function (sicl-genv:find-class 't e2))
+  (funcall (sicl-genv:fdefinition 'sicl-clos::satiate-generic-function e2)
+           function))
+
 (defun satiate-all-functions (e1 e2 e3)
   (format *trace-output* "Satiating all functions.~%")
   (do-all-symbols (var)
     (when (and (sicl-genv:fboundp var e3)
                (eq (class-of (sicl-genv:fdefinition var e3))
                    (sicl-genv:find-class 'standard-generic-function e1)))
-      (funcall (sicl-genv:fdefinition
-                'sicl-clos::compute-and-set-specializer-profile e2)
-               (sicl-genv:fdefinition var e3)
-               (sicl-genv:find-class 't e2))
-      (funcall (sicl-genv:fdefinition 'sicl-clos::satiate-generic-function e2)
-               (sicl-genv:fdefinition var e3)))
+      (satiate-function (sicl-genv:fdefinition var e3) e2))
     (when (and (sicl-genv:fboundp `(setf ,var) e3)
                (eq (class-of (sicl-genv:fdefinition `(setf ,var) e3))
                    (sicl-genv:find-class 'standard-generic-function e1)))
-      (funcall (sicl-genv:fdefinition
-                'sicl-clos::compute-and-set-specializer-profile e2)
-               (sicl-genv:fdefinition `(setf ,var) e3)
-               (sicl-genv:find-class 't e2))
-      (funcall (sicl-genv:fdefinition 'sicl-clos::satiate-generic-function e2)
-               (sicl-genv:fdefinition `(setf ,var) e3))))
+      (satiate-function (sicl-genv:fdefinition `(setf ,var) e3) e2)))
   (format *trace-output* "Done satiating all functions.~%"))
 
 (defun activate-generic-function-invocation (boot)
