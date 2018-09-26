9f12ea7de830dd51fb1ae8ae675bf01a66a3aada
Robert Strandh <robert.strandh@gmail.com>
2018-09-26 13:42:06 +0200

    Remove obsolete code.

--- Code/Cleavir/Intermediate-representation/HIR-to-MIR/general.lisp
+++ Code/Cleavir/Intermediate-representation/HIR-to-MIR/general.lisp
@@ -1,24 +1,4 @@
-(cl:in-package #:cleavir-ir)
+(cl:in-package #:cleavir-hir-to-mir)
 
-(defgeneric specialize (instruction implementation processor os))
+(defgeneric specialize (client instruction))
 
-(defun hir-to-mir (initial-instruction implementation processor os)
-  (let ((all-instructions '()))
-    ;; Gather up all instructions in a list.
-    (map-instructions
-     (lambda (instruction)
-       (push instruction all-instructions))
-     initial-instruction)
-    ;; Appply specialize to all instructions in the list.
-    (loop for instruction in all-instructions
-	  do (specialize instruction implementation processor os))))
-
-(defmethod specialize :around (instruction implementation processor os)
-  (declare (ignorable implementation processor os))
-  (let ((replacement (call-next-method)))
-    (unless (eq instruction replacement)
-      (setf (cleavir-ir:predecessors replacement)
-	    (cleavir-ir:predecessors instruction))
-      (loop for predecessor in (cleavir-ir:predecessors instruction)
-	    do (nsubstitute replacement instruction
-			    (cleavir-ir:successors predecessor))))))
