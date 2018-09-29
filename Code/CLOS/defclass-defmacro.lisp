afce2cfc9e11e76366a8176e73185876763628b7
Robert Strandh <robert.strandh@gmail.com>
2018-09-29 08:58:12 +0200

    Untabify for better tool support.

--- Code/CLOS/defclass-defmacro.lisp
+++ Code/CLOS/defclass-defmacro.lisp
@@ -1,7 +1,7 @@
 (cl:in-package #:sicl-clos)
 
 (defmacro defclass (name
-		    superclass-names
-		    slot-specifiers
-		    &rest options)
+                    superclass-names
+                    slot-specifiers
+                    &rest options)
   (defclass-expander name superclass-names slot-specifiers options))
