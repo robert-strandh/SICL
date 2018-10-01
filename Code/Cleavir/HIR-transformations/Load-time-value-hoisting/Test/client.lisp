5f5a5eb7826275e9c7e19d35f3000ab02304bbdf
Marco Heisig <marco.heisig@fau.de>
2018-10-01 20:22:34 +0200

    Avoid circular definitions in the load time value test suite.

--- Code/Cleavir/HIR-transformations/Load-time-value-hoisting/Test/client.lisp
+++ Code/Cleavir/HIR-transformations/Load-time-value-hoisting/Test/client.lisp
@@ -3,14 +3,72 @@
 ;;; In this file, we define an example client for the load-time-value
 ;;; hoisting machinery.  We use this client for testing, but it is also an
 ;;; illustration of how to design a custom client.
-;;;
+
 (defvar *environment*
   (make-instance 'sicl-extrinsic-environment:environment))
 
 (defclass client ()
   ((%environment
+    :initform *environment*
     :initarg :environment
     :reader environment)))
+
+;;; This client uses a SICL extrinsic environment for compilation.  To
+;;; avoid circular definitions, we must ensure that calls to
+;;; SICL-GENV:FUNCTION-CELL of the symbols CL:INTERN, CL:MAKE-STRING and
+;;; SICL-GENV:FUNCTION-CELL itself are handled specially.  We do this by
+;;; detecting these cases in HIR-FROM-FROM and by returning a custom
+;;; expansion for each such case.
+
+(defvar *make-string*)
+
+(defvar *cons*)
+
+(defvar *find-package*)
+
+(defvar *intern*)
+
+(defvar *function-cell*)
+
+;; Create a HIR thunk that, when called, will call the function stored in
+;; LEXICAL-LOCATION with the given input data ARGUMENTS.
+(defun hir-funcall (lexical-location &rest arguments)
+  (let ((values-location (cleavir-ir:make-values-location)))
+    (cleavir-ir:make-enter-instruction
+     '()
+     :successor
+     (cleavir-ir:make-funcall-instruction
+      (list* lexical-location arguments)
+      (list values-location)
+      (cleavir-ir:make-return-instruction
+       (list values-location))))))
+
+(defmethod cleavir-load-time-value-hoisting:hir-from-form :around
+    (form (client client))
+  (cond
+    ((eq (first form) '%make-string)
+     (apply #'hir-funcall
+            *make-string*
+            (loop for character in (rest form)
+                  collect
+                  (cleavir-ir:make-constant-input character))))
+    ((eq (first form) '%cons)
+     (hir-funcall *cons*))
+    ((eq (first form) '%find-package)
+     (hir-funcall
+      *find-package*
+      (cleavir-ir:make-constant-input (second form))))
+    ((eq (first form) '%intern)
+     (hir-funcall
+      *intern*
+      (cleavir-ir:make-constant-input (second form))
+      (cleavir-ir:make-constant-input (third form))))
+    ((eq (first form) 'sicl-genv:function-cell)
+     (hir-funcall
+      *function-cell*
+      (cleavir-ir:make-constant-input (second (second form)))))
+    (t
+     (call-next-method))))
 
 (defmethod cleavir-load-time-value-hoisting:hir-from-form
     (form (client client))
@@ -28,12 +86,27 @@
    (environment client)))
 
 (defmethod cleavir-load-time-value-hoisting:make-load-form-using-client
-    ((symbol symbol) (client client))
-  42)
+    ((string string) (client client))
+  (values
+   `(%make-string
+     ,@(loop for index below (length string)
+             collect (row-major-aref string index)))))
 
 (defmethod cleavir-load-time-value-hoisting:make-load-form-using-client
-    ((string string) (client client))
-  1337)
+    ((package package) (client client))
+  `(%find-package ,(package-name package)))
+
+(defmethod cleavir-load-time-value-hoisting:make-load-form-using-client
+    ((symbol symbol) (client client))
+  `(%intern ,(symbol-name symbol)
+            ,(symbol-package symbol)))
+
+(defmethod cleavir-load-time-value-hoisting:make-load-form-using-client
+    ((cons cons) (client client))
+  (values
+   `(%cons)
+   `(setf (car ,cons) ',(car cons)
+          (cdr ,cons) ',(cdr cons))))
 
 (defmethod cleavir-load-time-value-hoisting:simplify-datum
     ((constant-input cleavir-ir:constant-input) (client client))
@@ -48,52 +121,8 @@
          (change-to-immediate-input (+ (ash (char-int value) 2) 2)))
         (t nil)))))
 
-;;; This client uses a SICL extrinsic environment for compilation.  In this
-;;; environment, references to functions and variables are replaced by
-;;; something like
-;;;
-;;; (car (load-time-value (sicl-genv:function-cell 'NAME environment)))
-;;;
-;;; Now we have a problem, because we cannot use sicl-genv:function-cell to
-;;; obtain its own function cell.
-
-(defparameter *car-lexical-location*
-  (cleavir-ir:make-lexical-location 'car))
-
-(defparameter *function-cell-lexical-location*
-  (cleavir-ir:make-lexical-location 'sicl-genv:function-cell))
-
-(defparameter *global-environment-lexical-location*
-  (cleavir-ir:make-lexical-location 'sicl-genv:function-cell))
-
-(defun make-lexical-location-thunk (lexical-location)
-  (cleavir-ir:make-enter-instruction
-   '()
-   :successor
-   (cleavir-ir:make-return-instruction
-    (list lexical-location))))
-
 (defmethod cleavir-load-time-value-hoisting:scan-hir :around
     ((enter-instruction cleavir-ir:enter-instruction) client)
   (let ((cleavir-ir:*policy* (cleavir-ir:policy enter-instruction)))
     (call-next-method)))
 
-(defmethod cleavir-load-time-value-hoisting:hir-from-form :around
-    (form (client client))
-  (cond
-    ;; FIXME: This cascade of equal tests is neither fast, nor pretty.  But
-    ;; it will do for testing.
-    ((equal form '(sicl-global-environment:function-cell
-                   'car
-                   (sicl-global-environment:global-environment)))
-     (make-lexical-location-thunk *car-lexical-location*))
-    ((equal form '(sicl-global-environment:function-cell
-                   'sicl-global-environment:function-cell
-                   (sicl-global-environment:global-environment)))
-     (make-lexical-location-thunk *function-cell-lexical-location*))
-    ((equal form '(sicl-global-environment:function-cell
-                   'sicl-global-environment:global-environment
-                   (sicl-global-environment:global-environment)))
-     (make-lexical-location-thunk *global-environment-lexical-location*))
-    (t
-     (call-next-method))))
--- Code/Cleavir/HIR-transformations/Load-time-value-hoisting/Test/test.lisp
+++ Code/Cleavir/HIR-transformations/Load-time-value-hoisting/Test/test.lisp
@@ -1,10 +1,22 @@
 (cl:in-package #:cleavir-load-time-value-hoisting-test)
 
-(defun hoisted-hir-from-form (lambda-form)
+(defun hoisted-hir-from-form (form)
   (let* ((environment *environment*)
          (system (make-instance 'client
-                   :environment environment)))
-    (cleavir-load-time-value-hoisting:hoist-load-time-values
-     (cleavir-ast-to-hir:compile-toplevel-unhoisted
-      (cleavir-generate-ast:generate-ast lambda-form environment system))
-     system)))
+                   :environment environment))
+         (*make-string*
+           (cleavir-ir:make-lexical-location '%make-string))
+         (*cons*
+           (cleavir-ir:make-lexical-location '%cons))
+         (*find-package*
+           (cleavir-ir:make-lexical-location '%find-package))
+         (*intern*
+           (cleavir-ir:make-lexical-location '%intern))
+         (*function-cell*
+           (cleavir-ir:make-lexical-location 'sicl-genv:function-cell)))
+    (let ((hir (cleavir-ast-to-hir:compile-toplevel-unhoisted
+                (cleavir-generate-ast:generate-ast
+                 `(lambda () ,form) environment system))))
+      (cleavir-load-time-value-hoisting:hoist-load-time-values hir system)
+      (cleavir-ir:reinitialize-data hir)
+      hir)))
