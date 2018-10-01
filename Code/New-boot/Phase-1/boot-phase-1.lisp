5794ec6c950bf84f79243ada916e57c8ffb11023
Robert Strandh <robert.strandh@gmail.com>
2018-10-01 04:17:38 +0200

    Use new utilities to simplify code.

--- Code/New-boot/Phase-1/boot-phase-1.lisp
+++ Code/New-boot/Phase-1/boot-phase-1.lisp
@@ -1,19 +1,17 @@
 (cl:in-package #:sicl-new-boot-phase-1)
 
 (defun import-more-to-env1 (env)
-  (sicl-minimal-extrinsic-environment:import-package-from-host 'sicl-clos env)
-  (sicl-minimal-extrinsic-environment:import-function-from-host
-   'sicl-clos:defclass-expander  env)
+  (import-package-from-host 'sicl-clos env)
+  (import-function-from-host 'sicl-clos:defclass-expander env)
   ;; This function is needed because we define a special variable
   ;; *CLASS-UNIQUE-NUMBER* that is used in the :INITFORM of the 
   ;; corresponding slot in the class CLASS.
-  (sicl-minimal-extrinsic-environment:import-function-from-host
-   '(setf sicl-genv:special-variable) env))
+  (import-function-from-host '(setf sicl-genv:special-variable) env))
 
 (defun boot-phase-1 (boot)
   (with-accessors ((e1 sicl-new-boot:e1) (e2 sicl-new-boot:e2)) boot
     (import-more-to-env1 e1)
-    (sicl-minimal-extrinsic-environment:import-package-from-host 'sicl-clos e2)
+    (import-package-from-host 'sicl-clos e2)
     (load-accessor-defgenerics e2)
     (create-mop-classes-phase1 e1 e2)
     (list e1 e2)))
