9be38bf96ae4ecb6165c867ebb530f3d4c670045
Robert Strandh <robert.strandh@gmail.com>
2018-10-04 04:48:40 +0200

    Import function SICL-GENV:FIND-CLASS from the host.

--- Code/New-boot/Phase-0/boot-phase-0.lisp
+++ Code/New-boot/Phase-0/boot-phase-0.lisp
@@ -1,5 +1,13 @@
 (cl:in-package #:sicl-new-boot-phase-0)
 
+(defun import-from-host (boot)
+  (with-accessors ((e0 sicl-new-boot:e0)) boot
+    (import-package-from-host 'sicl-clos e0)
+    (import-functions-from-host
+     '(sicl-genv:find-class)
+     e0)))
+
 (defun boot-phase-0 (boot)
+  (import-from-host boot)
   (with-accessors ((e0 sicl-new-boot:e0) (e1 sicl-new-boot:e1)) boot
-    (import-package-from-host 'sicl-clos e0)))
+    nil))
