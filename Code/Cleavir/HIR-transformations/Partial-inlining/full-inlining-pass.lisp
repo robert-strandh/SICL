84c9de5178e8ab1de5b7cb3028a9c666020de838
Bike <aeshtaer@gmail.com>
2018-10-04 17:25:13 -0400

    Partial inlining: Avoid RUI- delete ourselves

--- Code/Cleavir/HIR-transformations/Partial-inlining/full-inlining-pass.lisp
+++ Code/Cleavir/HIR-transformations/Partial-inlining/full-inlining-pass.lisp
@@ -1,13 +1,32 @@
 (in-package #:cleavir-partial-inlining)
 
+(defun instruction-and-all-users (instruction)
+  (loop with all = (list instruction)
+        with worklist = (cleavir-ir:outputs instruction)
+        do (if (null worklist)
+               (return all)
+               (let ((work (pop worklist)))
+                 (loop for next in (cleavir-ir:using-instructions work)
+                       do (pushnew next all :test #'eq)
+                          (etypecase next
+                            (cleavir-ir:assignment-instruction
+                             (push (first (cleavir-ir:outputs next)) worklist))
+                            ;; FIXME: cleavir-ir:call-instruction
+                            ((or cleavir-ir:funcall-instruction
+                                 cleavir-ir:multiple-value-call-instruction))))))))
+
+(defun delete-enclose (enclose-instruction)
+  (mapc #'cleavir-ir:delete-instruction (instruction-and-all-users enclose-instruction)))
+
 ;; FIXME: move? finesse?
 (defun all-parameters-required-p (enter)
   (every (lambda (param) (typep param 'cleavir-ir:lexical-location))
          (cleavir-ir:lambda-list enter)))
 
 ;; get one potential inline that can be done, or else NIL.
-;; An inline here is a list (enter call uniquep), where uniquep expresses whether the function
-;; is not used for anything but this call.
+;; An inline here is a list (enter call enclose enclose-unique-p enter-unique-p)
+;; enclose-unique-p is true iff the enclose is only used for this call (and thus can be deleted)
+;; enter-unique-p is true iff the enter instruction is the CODE of only this enclose.
 (defun one-potential-inline (initial-instruction)
   (let ((dag (cleavir-hir-transformations:build-function-dag initial-instruction))
         (destinies (cleavir-hir-transformations:compute-destinies initial-instruction)))
@@ -21,7 +40,9 @@
                      (loop with enclose = (cleavir-hir-transformations:enclose-instruction node)
                            with result
                            with enclose-destinies = (gethash enclose destinies)
-                           with unique-p = (unique-p enter enclose-destinies)
+                           with enclose-unique-p = (= (length enclose-destinies) 1)
+                           with enter-unique-p
+                           = (= (length (gethash enter (cleavir-hir-transformations:dag-nodes dag))) 1)
                            for caller in enclose-destinies
                            for call-owner = (gethash caller *instruction-ownerships*)
                            when (eq caller :escape)
@@ -29,13 +50,8 @@
                            when (parent-node-p node call-owner) ; recursive
                              return nil
                            ;; We're all good, but keep looking through for escapes and recursivity.
-                           do (setf result (list enter caller unique-p))
+                           do (setf result (list enter caller enclose enclose-unique-p enter-unique-p))
                            finally (return-from one-potential-inline result)))))
-               (unique-p (enter enclose-destinies)
-                 ;; Return T iff the ENTER is only used for one call, which allows most copying
-                 ;; during the inline process to be skipped.
-                 (and (= (length enclose-destinies) 1)
-                      (= (length (gethash enter (cleavir-hir-transformations:dag-nodes dag))) 1)))
                (parent-node-p (parent enter)
                  ;; parent is a node (i.e. enclose), enter is an enter instruction
                  ;; we return T iff the enter is enclosed by a node that has parent
@@ -63,7 +79,11 @@
         with *location-ownerships* = (cleavir-hir-transformations:compute-location-owners initial-instruction)
         for inline = (one-potential-inline initial-instruction)
         until (null inline)
-        do (destructuring-bind (enter call uniquep) inline
-             (inline-function initial-instruction call enter (make-hash-table :test #'eq) :uniquep uniquep))
+        do (destructuring-bind (enter call enclose enclose-unique-p enter-unique-p) inline
+             (inline-function initial-instruction call enter (make-hash-table :test #'eq) :uniquep uniquep)
+             (when enclose-unique-p
+               ;; enclose was only used once, and we've just eliminated that - delete it.
+               (delete-enclose enclose)))
            (cleavir-ir:set-predecessors initial-instruction)
+           (cleavir-ir:reinitialize-data initial-instruction)))
            (cleavir-remove-useless-instructions:remove-useless-instructions initial-instruction)))
