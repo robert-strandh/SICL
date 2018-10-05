062bab58fcfd986d6d92969dad25f1649b309979
Robert Strandh <robert.strandh@gmail.com>
2018-10-05 06:23:14 +0200

    Export names of new utilities.

--- Code/New-boot/packages.lisp
+++ Code/New-boot/packages.lisp
@@ -3,8 +3,8 @@
 (defpackage #:sicl-new-boot
   (:use #:common-lisp)
   (:export #:boot
-           #:e0
-           #:e1
-           #:e2
-           #:e3
-           #:e4))
+           #:e0 #:e1 #:e2 #:e3 #:e4
+           #:load-file
+           #:import-function-from-host
+           #:import-functions-from-host
+           #:import-package-from-host))
