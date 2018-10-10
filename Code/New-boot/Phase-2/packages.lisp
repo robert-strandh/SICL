2907f599ad4dbdf1c7e960747ed78a0ddc9bf8d7
Robert Strandh <robert.strandh@gmail.com>
2018-10-10 07:54:37 +0200

    Import utilities from package SICL-NEW-BOOT rather then redefining them.

--- Code/New-boot/Phase-2/packages.lisp
+++ Code/New-boot/Phase-2/packages.lisp
@@ -2,4 +2,11 @@
 
 (defpackage #:sicl-new-boot-phase-2
   (:use #:common-lisp)
+  (:import-from #:sicl-new-boot
+                #:load-file
+                #:load-file-protected
+                #:import-function-from-host
+                #:import-functions-from-host
+                #:import-package-from-host
+                #:import-class-from-host)
   (:export #:boot-phase-2))
