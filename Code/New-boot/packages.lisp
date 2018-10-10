08512756fd6a629c3dcd89fd89797e13d34a14e9
Robert Strandh <robert.strandh@gmail.com>
2018-10-10 07:25:33 +0200

    Export name of new function.

--- Code/New-boot/packages.lisp
+++ Code/New-boot/packages.lisp
@@ -5,6 +5,7 @@
   (:export #:boot
            #:e0 #:e1 #:e2 #:e3 #:e4
            #:load-file
+           #:load-file-protected
            #:import-function-from-host
            #:import-functions-from-host
            #:import-package-from-host))
