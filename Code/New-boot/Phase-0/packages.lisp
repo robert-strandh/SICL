abb932a6825197a208a1b67bb8d7dc8db31cd3dd
Robert Strandh <robert.strandh@gmail.com>
2018-10-10 06:49:31 +0200

    Import utililities rather than redefining them.

--- Code/New-boot/Phase-0/packages.lisp
+++ Code/New-boot/Phase-0/packages.lisp
@@ -2,4 +2,9 @@
 
 (defpackage #:sicl-new-boot-phase-0
   (:use #:common-lisp)
+  (:import-from #:sicl-new-boot
+                #:load-file
+                #:import-function-from-host
+                #:import-functions-from-host
+                #:import-package-from-host)
   (:export #:boot-phase-0))
--- Code/New-boot/Phase-0/utilities.lisp
+++ Code/New-boot/Phase-0/utilities.lisp
@@ -1,14 +1,1 @@
 (cl:in-package #:sicl-new-boot-phase-0)
-
-(defun load-file (file environment)
-  (sicl-minimal-extrinsic-environment:cst-load-file file environment nil))
-
-(defun import-function-from-host (name environment)
-  (sicl-minimal-extrinsic-environment:import-function-from-host name environment))
-
-(defun import-functions-from-host (names environment)
-  (loop for name in names
-        do (import-function-from-host name environment)))
-
-(defun import-package-from-host (name environment)
-  (sicl-minimal-extrinsic-environment:import-package-from-host name environment))
