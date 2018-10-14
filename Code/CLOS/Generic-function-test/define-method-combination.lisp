2869d91c1063844bd49946588387fbafb68e5036
Robert Strandh <robert.strandh@gmail.com>
2018-10-14 07:01:21 +0200

    Remove obsolete code.

--- Code/CLOS/Generic-function-test/define-method-combination.lisp
+++
@@ -1,4 +0,0 @@
-(cl:in-package #:sicl-clos)
-
-(defmacro define-method-combination (name &rest more-arguments)
-  (sicl-method-combination::define-method-combination-expander *env* name more-arguments))
--- Code/CLOS/Generic-function-test/my-defmethod-defmacro.lisp
+++
@@ -1,4 +0,0 @@
-(cl:in-package #:sicl-clos)
-
-(defmacro my-defmethod (function-name &rest rest)
-  (defmethod-expander function-name rest))
