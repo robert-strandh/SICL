d36509946d72862c42dc97619046c302cda456e8
Robert Strandh <robert.strandh@gmail.com>
2018-10-16 11:38:38 +0200

    Export name of new environment.

--- Code/New-boot/packages.lisp
+++ Code/New-boot/packages.lisp
@@ -3,6 +3,7 @@
 (defpackage #:sicl-new-boot
   (:use #:common-lisp)
   (:export #:boot
+           #:environment
            #:e0 #:e1 #:e2 #:e3 #:e4
            #:load-file
            #:load-file-protected
