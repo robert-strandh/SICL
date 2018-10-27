c6d77625776e6deed0ecccb12238c5fd041211ae
Robert Strandh <robert.strandh@gmail.com>
2018-10-27 13:21:18 +0200

    Define more methods on PRINT-OBJECT for bridge classes and generic functions.

--- Code/New-boot/Phase-2/define-methods-on-print-object.lisp
+++ Code/New-boot/Phase-2/define-methods-on-print-object.lisp
@@ -1,5 +1,13 @@
 (cl:in-package #:sicl-clos)
 
 (defmethod print-object ((object standard-generic-function) stream)
-  (format stream "<standard generic function named ~s>"
+  (format stream "<E3 Bridge generic function ~s>"
           (generic-function-name object)))
+
+(defmethod print-object ((object standard-class) stream)
+  (format stream "<E2 Bridge standard-class ~s>"
+          (class-name object)))
+
+(defmethod print-object ((object funcallable-standard-class) stream)
+  (format stream "<E2 Bridge funcallable-standard-class ~s>"
+          (class-name object)))
