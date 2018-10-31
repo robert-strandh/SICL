44a1c56dfa6c0d640fe6afe740d532a1782355b6
Robert Strandh <robert.strandh@gmail.com>
2018-10-31 05:51:43 +0100

    Abstract out functions for providing individual functionality.

--- Code/New-boot/Phase-3/enable-generic-function-invocation-in-e3.lisp
+++ Code/New-boot/Phase-3/enable-generic-function-invocation-in-e3.lisp
@@ -1,29 +1,38 @@
 (cl:in-package #:sicl-new-boot-phase-3)
 
+;;; SUB-SPECIALIZER-P ca calls CLASS-PRECEDENCE-LIST to obtain the
+;;; class precedence list of an argument passed to a generic function.
+;;; Then it calls POSITION to determine which of two classes comes
+;;; first in that precedence list.
+;;;
+;;; SUB-SPECIALIZER-P is called by COMPUTE-APPLICABLE-METHODS
+;;; (indirectly) to determine which is two methods is more specific.
+(defun define-sub-specializer-p (boot)
+  (with-accessors ((e3 sicl-new-boot:e3)) boot
+    (import-function-from-host 'position e3)
+    (load-file "New-boot/Phase-2/sub-specializer-p.lisp" e3)))
+
+;;; COMPUTE-APPLICABLE-METHODS calls MAPCAR (indirectly) in order to
+;;; get the class of each of the arguments passed to a generic
+;;; function.  It calls SORT to sort the applicable methods in order
+;;; from most specific to least specific.  EQL is called to compare
+;;; the object of an EQL specializer to an argument passed to a
+;;; generic function.
+(defun define-compute-applicable-methods (boot)
+  (with-accessors ((e3 sicl-new-boot:e3)) boot
+    (import-functions-from-host '(sort mapcar eql) e3)
+    (load-file "CLOS/compute-applicable-methods-support.lisp" e3)
+    (load-file "CLOS/compute-applicable-methods-defgenerics.lisp" e3)
+    (load-file "CLOS/compute-applicable-methods-defmethods.lisp" e3)))
+    
+
 (defun enable-generic-function-invocation (boot)
   (with-accessors ((e2 sicl-new-boot:e2)
                    (e3 sicl-new-boot:e3)) boot
     (load-file "CLOS/classp-defgeneric.lisp" e3)
     (load-file "CLOS/classp-defmethods.lisp" e3)
-    ;; SUB-SPECIALIZER-P ca calls CLASS-PRECEDENCE-LIST to obtain the
-    ;; class precedence list of an argument passed to a generic
-    ;; function.  Then it calls POSITION to determine which of two
-    ;; classes comes first in that precedence list.
-    (import-function-from-host 'position e3)
-    ;; SUB-SPECIALIZER-P is called (indirectly) by
-    ;; COMPUTE-APPLICABLE-METHODS to determine which is two methods is
-    ;; more specific.
-    (load-file "New-boot/Phase-2/sub-specializer-p.lisp" e3)
-    ;; COMPUTE-APPLICABLE-METHODS (indirectly) calls MAPCAR in order
-    ;; to get the class of each of the arguments passed to a generic
-    ;; function.  It calls SORT to sort the applicable methods in
-    ;; order from most specific to least specific.  EQL is called to
-    ;; compare the object of an EQL specializer to an argument passed
-    ;; to a generic function.
-    (import-functions-from-host '(sort mapcar eql) e3)
-    (load-file "CLOS/compute-applicable-methods-support.lisp" e3)
-    (load-file "CLOS/compute-applicable-methods-defgenerics.lisp" e3)
-    (load-file "CLOS/compute-applicable-methods-defmethods.lisp" e3)
+    (define-sub-specializer-p boot)
+    (define-compute-applicable-methods boot)
     ;; COMPUTE-EFFECTIVE-METHOD calls several functions.  For each
     ;; method that is passed to it as an argument, it calls
     ;; METHOD-QUALIFIERS.  It calls TEMPLATE on the method combination
