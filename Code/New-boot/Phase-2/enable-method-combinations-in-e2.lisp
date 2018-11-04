91e5e7f6d93f429bbfb33da71ebbf19a03df65a5
Robert Strandh <robert.strandh@gmail.com>
2018-11-04 09:44:44 +0100

    Reorganize code so that it resembles similar code in phase 3.

--- Code/New-boot/Phase-2/enable-method-combinations-in-e2.lisp
+++ Code/New-boot/Phase-2/enable-method-combinations-in-e2.lisp
@@ -12,6 +12,13 @@
      e2)
     (import-class-from-host 'sicl-method-combination:method-combination-template
      e1)
+    (import-functions-from-host
+     '(sicl-method-combination:effective-method-form-function
+       sicl-method-combination::variant-signature-determiner
+       sicl-method-combination::variants
+       (setf sicl-method-combination::variants))
+     e2)
+    (load-file "Method-combination/find-method-combination.lisp" e2)
     ;; The standard method combination uses LOOP to traverse the list
     ;; of methods, so we need to import LIST-CAR and LIST-CDR from the
     ;; LOOP package.
@@ -20,12 +27,5 @@
     ;; the order of invocation of the :AFTER methods.
     (import-function-from-host 'reverse e2)
     (load-file "CLOS/standard-method-combination.lisp" e2)
-    (import-functions-from-host
-     '(sicl-method-combination:effective-method-form-function
-       sicl-method-combination::variant-signature-determiner
-       sicl-method-combination::variants
-       (setf sicl-method-combination::variants))
-     e2)
-    (load-file "Method-combination/find-method-combination.lisp" e2)
     (load-file "CLOS/find-method-combination-defgenerics.lisp" e2)
     (load-file "CLOS/find-method-combination-defmethods.lisp" e2)))
