9c20fe991089fd1e17058095d0e51c58c0742925
Robert Strandh <robert.strandh@gmail.com>
2018-11-05 17:27:23 +0100

    Import package SICL-PACKAGE to all environments.

--- Code/New-boot/boot.lisp
+++ Code/New-boot/boot.lisp
@@ -12,7 +12,8 @@
   (setf (sicl-genv:special-variable '*trace-output* object t) *trace-output*)
   (import-package-from-host 'sicl-clos object)
   (import-package-from-host 'sicl-cons object)
-  (import-package-from-host 'sicl-method-combination object))
+  (import-package-from-host 'sicl-method-combination object)
+  (import-package-from-host 'sicl-package object))
 
 (defclass boot ()
   ((%e0 :initarg :e0 :accessor e0)
