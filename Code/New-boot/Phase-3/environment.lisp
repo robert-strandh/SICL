a0eec19ae9d92dfe643c480b91b7db0e31841dd1
Robert Strandh <robert.strandh@gmail.com>
2018-11-07 17:42:01 +0100

    Add comment defining the nature of objects in environment E3.

--- Code/New-boot/Phase-3/environment.lisp
+++ Code/New-boot/Phase-3/environment.lisp
@@ -1,5 +1,13 @@
 (cl:in-package #:sicl-new-boot-phase-3)
 
+;;; Environment E3.
+;;;
+;;; In this environment, generic functions are bridge generic
+;;; functions of type STANDARD-GENERIC-FUNCTION.  Methods on these
+;;; generic functions are bridge methods of type STANDARD-METHOD.
+;;;
+;;; Classes in this environment are ersatz classes.
+
 (defclass environment (sicl-new-boot:environment)
   ())
 
