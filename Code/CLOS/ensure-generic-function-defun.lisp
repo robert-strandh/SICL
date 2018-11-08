f0db34be11da8cc341eb7ae3913ee0d7ce4fb55c
Robert Strandh <robert.strandh@gmail.com>
2018-11-08 17:04:14 +0100

    Introduce a variant of ENSURE-GENERIC-FUNCTION that calls functions in SICL-GENV.

---
+++ Code/CLOS/ensure-generic-function-defun.lisp
@@ -0,0 +1,24 @@
+(cl:in-package #:sicl-clos)
+
+;;; For the specification of this function, see
+;;; http://metamodular.com/CLOS-MOP/ensure-generic-function.html
+;;;
+;;; This function is also specified in the CLHS.
+;;; http://www.lispworks.com/documentation/HyperSpec/Body/f_ensure.htm#ensure-generic-function
+
+(defun ensure-generic-function
+    (name
+     &rest keys
+     &key (environment (sicl-genv:global-environment))
+     &allow-other-keys)
+  (let ((generic-function
+	  (if (sicl-genv:fboundp name environment)
+	      (let ((fun (sicl-genv:fdefinition name environment)))
+		(if (typep fun 'generic-function)
+		    fun
+		    (error 'type-error
+			   :datum fun
+			   :expected-type '(or null generic-function))))
+	      nil)))
+    (apply #'ensure-generic-function-using-class
+	   generic-function name keys)))
