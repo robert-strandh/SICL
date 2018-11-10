af8592a3474f801020e571234aaaa9b934470fa6
Robert Strandh <robert.strandh@gmail.com>
2018-11-10 13:07:25 +0100

    Use production version of DEFMETHOD-EXPANDER.

--- Code/New-boot/Phase-3/enable-defmethod-in-e3.lisp
+++ Code/New-boot/Phase-3/enable-defmethod-in-e3.lisp
@@ -10,35 +10,6 @@
                 (t
                  specializer)))))
 
-(defun define-defmethod-expander (boot)
-  (with-accessors ((e2 sicl-new-boot:e2)
-                   (e3 sicl-new-boot:e3)) boot
-    (setf (sicl-genv:fdefinition 'sicl-clos:defmethod-expander e3)
-          (lambda (ct-env function-name rest)
-            (multiple-value-bind
-                  (qualifiers required remaining
-                   specializers declarations documentation forms)
-                (sicl-clos::parse-defmethod rest)
-              (let* ((lambda-list (append required remaining))
-                     (generic-function-var (gensym)))
-                `(let* ((,generic-function-var
-                          (ensure-generic-function ',function-name :environment ,ct-env)))
-                   (sicl-clos:ensure-method
-                    ,generic-function-var
-                    :lambda-list ',lambda-list
-                    :qualifiers ',qualifiers
-                    :specializers ,(sicl-clos::canonicalize-specializers specializers)
-                    :documentation ,documentation
-                    :function
-                    ,(funcall
-                      (sicl-genv:fdefinition 'sicl-clos:make-method-lambda e3)
-                      nil
-                      nil
-                      `(lambda ,lambda-list
-                         ,@declarations
-                         ,@forms)
-                      nil)))))))))
-
 (defun enable-defmethod-in-e3 (boot)
   (with-accessors ((e2 sicl-new-boot:e2)
                    (e3 sicl-new-boot:e3)) boot
@@ -58,5 +29,16 @@
           (sicl-genv:fdefinition 'sicl-clos::ensure-method e2))
     (setf (sicl-genv:fdefinition 'sicl-clos::ensure-method e2)
           (sicl-genv:fdefinition 'temp e2))
-    (define-defmethod-expander boot)
+    (setf (sicl-genv:fdefinition 'sicl-clos::create-method-lambda e3)
+          (lambda (function lambda-expression environment)
+            ( sicl-clos::make-method-lambda-default
+              function nil lambda-expression environment)))
+    (import-functions-from-host
+     '(mapcar subseq 1+ elt position-if
+       sicl-genv:fdefinition sicl-genv:fboundp
+       cleavir-code-utilities:parse-specialized-lambda-list
+       cleavir-code-utilities:separate-function-body
+       cleavir-code-utilities:required)
+     e3)
+    (load-file "CLOS/defmethod-support.lisp" e3)
     (load-file "CLOS/defmethod-defmacro.lisp" e3)))
