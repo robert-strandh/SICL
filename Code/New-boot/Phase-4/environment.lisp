8a6d00b0d1c9280be72482e3a221d5f809b0c45d
Robert Strandh <robert.strandh@gmail.com>
2018-11-15 17:07:47 +0100

    Define methods on TYPEP, required to load MOP class definitions.

--- Code/New-boot/Phase-4/environment.lisp
+++ Code/New-boot/Phase-4/environment.lisp
@@ -12,10 +12,10 @@
 (defclass environment (sicl-new-boot:environment)
   ())
 
-;; (defmethod sicl-genv:typep
-;;     (object (type-specifier (eql 'function)) (environment environment))
-;;   (typep object 'function))
+(defmethod sicl-genv:typep
+    (object (type-specifier (eql 'function)) (environment environment))
+  (typep object 'function))
 
-;; (defmethod sicl-genv:typep
-;;     (object (type-specifier (eql 'class)) (environment environment))
-;;   (not (symbolp object)))
+(defmethod sicl-genv:typep
+    (object (type-specifier (eql 'class)) (environment environment))
+  (not (symbolp object)))
