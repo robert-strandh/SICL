f6e6eda0ee14edd85a8ec1cee89fa28bb3bd6e3e
Robert Strandh <robert.strandh@gmail.com>
2018-11-15 17:09:06 +0100

    Move utilities for interactive development to parent directory and environment.

--- Code/New-boot/Phase-3/utilities.lisp
+++ Code/New-boot/Phase-3/utilities.lisp
@@ -1,21 +1,2 @@
 (cl:in-package #:sicl-new-boot-phase-3)
 
-;;; Some utilities for making interactive queries easier.
-
-(define-symbol-macro ee0 (sicl-new-boot:e0 *b*))
-(define-symbol-macro ee1 (sicl-new-boot:e1 *b*))
-(define-symbol-macro ee2 (sicl-new-boot:e2 *b*))
-(define-symbol-macro ee3 (sicl-new-boot:e3 *b*))
-(define-symbol-macro ee4 (sicl-new-boot:e4 *b*))
-
-(defun fd (function-name environment)
-  (sicl-genv:fdefinition function-name environment))
-
-(defun fbp (function-name environment)
-  (sicl-genv:fboundp function-name environment))
-
-(defun fc (class-name environment)
-  (sicl-genv:find-class class-name environment))
-
-(defun repl (environment)
-  (sicl-minimal-extrinsic-environment::repl environment environment nil))
