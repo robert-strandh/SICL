4a39a1480a52ccf08afc962238fef6cc4e5f0443
Robert Strandh <robert.strandh@gmail.com>
2018-11-18 06:24:32 +0100

    Extract definition of function STAMP to a separate component.

--- Code/CLOS/compute-discriminating-function-support.lisp
+++ Code/CLOS/compute-discriminating-function-support.lisp
@@ -203,15 +203,6 @@
 ;;;   6. If the call in step 4 returns an empty list, then call
 ;;;      NO-APPLICABLE-METHOD.
 
-;;; The implementation of this function is not complete.  Furthermore,
-;;; this is probably not a good location for it.
-(defun stamp (instance)
-  (if (general-instance-p instance)
-      (general-instance-access instance 0)
-      ;; For now, anything else is considered to be an instance of
-      ;; class T, and we know that T has unique number 0.
-      0))
-
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;
 ;;; How we make slot accessors fast.
---
+++ Code/CLOS/stamp-defun.lisp
@@ -0,0 +1,10 @@
+(cl:in-package #:sicl-clos)
+
+;;; The implementation of this function is not complete.  Furthermore,
+;;; this is probably not a good location for it.
+(defun stamp (instance)
+  (if (general-instance-p instance)
+      (general-instance-access instance 0)
+      ;; For now, anything else is considered to be an instance of
+      ;; class T, and we know that T has unique number 0.
+      0))
