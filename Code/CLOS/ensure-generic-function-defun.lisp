63370a7b092984903410ddd636bf107d305b343f
Robert Strandh <robert.strandh@gmail.com>
2018-11-19 06:51:56 +0100

    Untabify for better tool support.

--- Code/CLOS/ensure-generic-function-defun.lisp
+++ Code/CLOS/ensure-generic-function-defun.lisp
@@ -12,13 +12,13 @@
      &key (environment (sicl-genv:global-environment))
      &allow-other-keys)
   (let ((generic-function
-	  (if (sicl-genv:fboundp name environment)
-	      (let ((fun (sicl-genv:fdefinition name environment)))
-		(if (typep fun 'generic-function)
-		    fun
-		    (error 'type-error
-			   :datum fun
-			   :expected-type '(or null generic-function))))
-	      nil)))
+          (if (sicl-genv:fboundp name environment)
+              (let ((fun (sicl-genv:fdefinition name environment)))
+                (if (typep fun 'generic-function)
+                    fun
+                    (error 'type-error
+                           :datum fun
+                           :expected-type '(or null generic-function))))
+              nil)))
     (apply #'ensure-generic-function-using-class
-	   generic-function name keys)))
+           generic-function name keys)))
