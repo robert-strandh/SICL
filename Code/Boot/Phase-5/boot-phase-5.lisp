e65fd7f57d89119df20d47d7d353ee7f89dd8c85
Robert Strandh <robert.strandh@gmail.com>
2018-11-21 15:12:53 +0100

    Call COMPUTE-DISCRIMINATING-FUNCTION in E4 once.

--- Code/Boot/Phase-5/boot-phase-5.lisp
+++ Code/Boot/Phase-5/boot-phase-5.lisp
@@ -1,4 +1,9 @@
 (cl:in-package #:sicl-boot-phase-5)
 
 (defun boot-phase-5 (boot)
-  boot)
+  (with-accessors ((e3 sicl-boot:e3)
+                   (e4 sicl-boot:e4)
+                   (e5 sicl-boot:e5))
+      boot
+    (funcall (sicl-genv:fdefinition 'sicl-clos:compute-discriminating-function e4)
+	     (sicl-genv:fdefinition 'class-name e5))))
