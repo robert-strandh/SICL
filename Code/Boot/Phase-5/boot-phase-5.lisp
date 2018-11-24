5c1f7f74dedd392d0b497607f5f7ddd525cc6305
Robert Strandh <robert.strandh@gmail.com>
2018-11-24 13:35:41 +0100

    Print message to indicate start of phase 5.

--- Code/Boot/Phase-5/boot-phase-5.lisp
+++ Code/Boot/Phase-5/boot-phase-5.lisp
@@ -1,6 +1,7 @@
 (cl:in-package #:sicl-boot-phase-5)
 
 (defun boot-phase-5 (boot)
+  (format *trace-output* "Start of phase 5~%")
   (with-accessors ((e3 sicl-boot:e3)
                    (e4 sicl-boot:e4)
                    (e5 sicl-boot:e5))
