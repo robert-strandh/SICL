0b398cd99780d58fd31c814a969ad60d44b54e22
Robert Strandh <robert.strandh@gmail.com>
2018-11-27 16:13:18 +0100

    Call function for definining slot-value setter.

--- Code/Boot/Phase-5/boot-phase-5.lisp
+++ Code/Boot/Phase-5/boot-phase-5.lisp
@@ -6,5 +6,6 @@
                    (e4 sicl-boot:e4)
                    (e5 sicl-boot:e5))
       boot
+    (define-set-slot-value boot)
     (funcall (sicl-genv:fdefinition 'sicl-clos:compute-discriminating-function e4)
 	     (sicl-genv:fdefinition 'class-name e5))))
