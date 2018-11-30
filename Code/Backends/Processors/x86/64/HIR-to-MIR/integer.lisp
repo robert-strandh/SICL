5c136c911d04dc9b1efc52779b0d3505e74dcab0
Robert Strandh <robert.strandh@gmail.com>
2018-11-30 13:37:47 +0100

    Modify integer-related translators to conform to new protocol.

--- Code/Backends/Processors/x86/64/HIR-to-MIR/integer.lisp
+++ Code/Backends/Processors/x86/64/HIR-to-MIR/integer.lisp
@@ -1,21 +1,36 @@
 (cl:in-package #:sicl-hir-to-mir)
 
-(defmethod hir-to-mir ((client sicl-client:sicl-x86-64)
-                       (instruction cleavir-ir:fixnum-add-instruction))
-  (change-class instruction 'cleavir-ir:signed-add-instruction))
+(defmethod translate ((client sicl-client:sicl-x86-64)
+                      (instruction cleavir-ir:fixnum-add-instruction))
+  (make-instance 'cleavir-ir:signed-add-instruction
+    :inputs (cleavir-ir:inputs instruction)
+    :outputs (cleavir-ir:outputs instruction)
+    :successors (cleavir-ir:successors instruction)))
 
-(defmethod hir-to-mir ((client sicl-client:sicl-x86-64)
-                       (instruction cleavir-ir:fixnum-sub-instruction))
-  (change-class instruction 'cleavir-ir:signed-sub-instruction))
+(defmethod translate ((client sicl-client:sicl-x86-64)
+                      (instruction cleavir-ir:fixnum-sub-instruction))
+  (make-instance 'cleavir-ir:signed-sub-instruction
+    :inputs (cleavir-ir:inputs instruction)
+    :outputs (cleavir-ir:outputs instruction)
+    :successors (cleavir-ir:successors instruction)))
 
-(defmethod hir-to-mir ((client sicl-client:sicl-x86-64)
-                       (instruction cleavir-ir:fixnum-less-instruction))
-  (change-class instruction 'cleavir-ir:signed-less-instruction))
+(defmethod translate ((client sicl-client:sicl-x86-64)
+                      (instruction cleavir-ir:fixnum-less-instruction))
+  (make-instance 'cleavir-ir:signed-less-instruction
+    :inputs (cleavir-ir:inputs instruction)
+    :outputs (cleavir-ir:outputs instruction)
+    :successors (cleavir-ir:successors instruction)))
 
-(defmethod hir-to-mir ((client sicl-client:sicl-x86-64)
-                       (instruction cleavir-ir:fixnum-not-greater-instruction))
-  (change-class instruction 'cleavir-ir:signed-not-greater-instruction))
+(defmethod translate ((client sicl-client:sicl-x86-64)
+                      (instruction cleavir-ir:fixnum-not-greater-instruction))
+  (make-instance 'cleavir-ir:signed-not-greater-instruction
+    :inputs (cleavir-ir:inputs instruction)
+    :outputs (cleavir-ir:outputs instruction)
+    :successors (cleavir-ir:successors instruction)))
 
-(defmethod hir-to-mir ((client sicl-client:sicl-x86-64)
-                       (instruction cleavir-ir:fixnum-equal-instruction))
-  (change-class instruction 'cleavir-ir:equal-instruction))
+(defmethod translate ((client sicl-client:sicl-x86-64)
+                      (instruction cleavir-ir:fixnum-equal-instruction))
+  (make-instance 'cleavir-ir:equal-instruction
+    :inputs (cleavir-ir:inputs instruction)
+    :outputs (cleavir-ir:outputs instruction)
+    :successors (cleavir-ir:successors instruction)))
