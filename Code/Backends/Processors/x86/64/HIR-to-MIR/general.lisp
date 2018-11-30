0d5498d7fe71f342d03d0b671a6854a625b1bdb6
Robert Strandh <robert.strandh@gmail.com>
2018-11-30 13:26:52 +0100

    Change protocol and rename main generic function.

--- Code/Backends/Processors/x86/64/HIR-to-MIR/general.lisp
+++ Code/Backends/Processors/x86/64/HIR-to-MIR/general.lisp
@@ -1,14 +1,35 @@
 (cl:in-package #:sicl-hir-to-mir)
 
-(defgeneric hir-to-mir (client instruction))
+(defgeneric translate (client instruction))
 
-(defmethod hir-to-mir ((client sicl-client:sicl-x86-64) instruction)
-  nil)
+(defun clone-instruction (instruction)
+  (make-instance (class-of instruction)
+    :inputs (cleavir-ir:inputs instruction)
+    :outputs (cleavir-ir:outputs instruction)
+    :successors (cleavir-ir:successors instruction)))
 
-(defmethod hir-to-mir ((client sicl-client:sicl-x86-64)
-                       (instruction cleavir-ir:enter-instruction))
-  nil)
+(defvar *translation-table*)
 
-(defmethod hir-to-mir ((client sicl-client:sicl-x86-64)
-                       (instruction cleavir-ir:eq-instruction))
-  (change-class instruction 'cleavir-ir:equal-instruction))
+(defmethod translate ((client sicl-client:sicl-x86-64) instruction)
+  (clone-instruction instruction))
+
+(defmethod translate ((client sicl-client:sicl-x86-64)
+                      (instruction cleavir-ir:enter-instruction))
+  (clone-instruction instruction))
+
+(defmethod translate ((client sicl-client:sicl-x86-64)
+                      (instruction cleavir-ir:eq-instruction))
+  (clone-instruction instruction))
+
+(defmethod translate :around ((client sicl-client:sicl-x86-64) instruction)
+  (when (null (gethash instruction *translation-table*))
+    (let ((translation (call-next-method)))
+      (setf (gethash instruction *translation-table*) translation))))
+
+(defgeneric hir-to-mir (client initial-instruction))
+
+(defmethod hir-to-mir ((client sicl-client:sicl-x86-64) initial-instruction)
+  (let ((*translation-table* (make-hash-table :test #'eq)))
+    (cleavir-ir:map-instructions-arbitrary-order
+     (lambda (instruction) (translate client instruction))
+     initial-instruction)))
