bc782e6dd39af682c8607ddd80a5703464730231
Robert Strandh <robert.strandh@gmail.com>
2018-11-30 13:56:12 +0100

    Modify general-instance-related translators to conform to new protocol.

--- Code/Backends/Processors/x86/64/HIR-to-MIR/general-instance.lisp
+++ Code/Backends/Processors/x86/64/HIR-to-MIR/general-instance.lisp
@@ -1,88 +1,46 @@
 (cl:in-package #:sicl-hir-to-mir)
 
-(defmethod hir-to-mir ((client sicl-client:sicl-x86-64)
-                       (instruction cleavir-ir:slot-read-instruction))
-  (let* ((inputs (cleavir-ir:inputs instruction))
-         (object-location (first inputs))
-         (slot-number (second inputs))
-         (outputs (cleavir-ir:outputs instruction))
-         (result-location (first outputs))
-         (successors (cleavir-ir:successors instruction))
-         (successor (first successors))
-         (origin (cleavir-ir:origin instruction))
-         (policy (cleavir-ir:policy instruction))
-         (d2 (make-instance 'cleavir-ir:lexical-location
-               :name '#:slot-address))
-         (d1 (make-instance 'cleavir-ir:lexical-location
-               :name '#:rack-address))
-         (i3 (make-instance 'cleavir-ir:memref2-instruction
-               :origin origin
-               :policy policy
-               :offset -7
-               :inputs (list d2)
-               :outputs (list result-location)))
-         (i2 (make-instance 'cleavir-ir:unsigned-add-instruction
-               :origin origin
-               :policy policy
-               :inputs (list d1 slot-number)
-               :outputs (list d2)
-               :successors (list i3)))
-         (i1 (make-instance 'cleavir-ir:memref2-instruction
-               :origin origin
-               :policy policy
-               :offset 3
-               :inputs (list object-location)
-               :outputs (list d1)
-               :successors (list i2))))
-    (setf (cleavir-ir:predecessors i3) (list i2))
-    (setf (cleavir-ir:predecessors i2) (list i1))
-    (setf (cleavir-ir:predecessors i1) (list instruction))
-    (cleavir-ir:insert-instruction-before i3 successor)
-    (setf (cleavir-ir:predecessors successor)
-          (remove instruction (cleavir-ir:predecessors successor)))
-    (change-class instruction 'cleavir-ir:nop-instruction
-                  :successors (list i1))))
+(defmethod translate ((client sicl-client:sicl-x86-64)
+                      (instruction cleavir-ir:slot-read-instruction))
+  (let ((rack-address (make-instance 'cleavir-ir:lexical-location
+                        :name '#:rack-address))
+        (slot-address (make-instance 'cleavir-ir:lexical-location
+                        :name '#:slot-address)))
+    (multiple-value-bind (object-location slot-number)
+        (cleavir-ir:inputs instruction)
+      (make-instance 'cleavir-ir:memref2-instruction
+        :offset 3
+        :inputs (list object-location)
+        :outputs (list rack-address)
+        :successors
+        (list (make-instance 'cleavir-ir:unsigned-add-instruction
+                :inputs (list rack-address slot-number)
+                :outputs (list slot-address)
+                :successors
+                (list (make-instance 'cleavir-ir:memref2-instruction
+                        :offset -7
+                        :inputs (list slot-address)
+                        :outputs (cleavir-ir:outputs instruction)
+                        :successors (cleavir-ir:successors instruction)))))))))
 
-(defmethod hir-to-mir ((client sicl-client:sicl-x86-64)
-                       (instruction cleavir-ir:slot-write-instruction))
-  (let* ((inputs (cleavir-ir:inputs instruction))
-         (object-location (first inputs))
-         (slot-number (second inputs))
-         (value-location (third inputs))
-         (outputs (cleavir-ir:outputs instruction))
-         (result-location (first outputs))
-         (successors (cleavir-ir:successors instruction))
-         (successor (first successors))
-         (origin (cleavir-ir:origin instruction))
-         (policy (cleavir-ir:policy instruction))
-         (d2 (make-instance 'cleavir-ir:lexical-location
-               :name '#:slot-address))
-         (d1 (make-instance 'cleavir-ir:lexical-location
-               :name '#:rack-address))
-         (i3 (make-instance 'cleavir-ir:memset2-instruction
-               :origin origin
-               :policy policy
-               :offset -7
-               :inputs (list d2 value-location)
-               :outputs (list result-location)))
-         (i2 (make-instance 'cleavir-ir:unsigned-add-instruction
-               :origin origin
-               :policy policy
-               :inputs (list d1 slot-number)
-               :outputs (list d2)
-               :successors (list i3)))
-         (i1 (make-instance 'cleavir-ir:memset2-instruction
-               :origin origin
-               :policy policy
-               :offset 3
-               :inputs (list object-location)
-               :outputs (list d1)
-               :successors (list i2))))
-    (setf (cleavir-ir:predecessors i3) (list i2))
-    (setf (cleavir-ir:predecessors i2) (list i1))
-    (setf (cleavir-ir:predecessors i1) (list instruction))
-    (cleavir-ir:insert-instruction-before i3 successor)
-    (setf (cleavir-ir:predecessors successor)
-          (remove instruction (cleavir-ir:predecessors successor)))
-    (change-class instruction 'cleavir-ir:nop-instruction
-                  :successors (list i1))))
+(defmethod translate ((client sicl-client:sicl-x86-64)
+                      (instruction cleavir-ir:slot-write-instruction))
+  (let ((rack-address (make-instance 'cleavir-ir:lexical-location
+                        :name '#:rack-address))
+        (slot-address (make-instance 'cleavir-ir:lexical-location
+                        :name '#:slot-address)))
+    (multiple-value-bind (object-location slot-number value-location)
+        (cleavir-ir:inputs instruction)
+      (make-instance 'cleavir-ir:memref2-instruction
+        :offset 3
+        :inputs (list object-location)
+        :outputs (list rack-address)
+        :successors
+        (list (make-instance 'cleavir-ir:unsigned-add-instruction
+                :inputs (list rack-address slot-number)
+                :outputs (list slot-address)
+                :successors
+                (list (make-instance 'cleavir-ir:memset2-instruction
+                        :offset -7
+                        :inputs (list slot-address value-location)
+                        :successors (cleavir-ir:successors instruction)))))))))
