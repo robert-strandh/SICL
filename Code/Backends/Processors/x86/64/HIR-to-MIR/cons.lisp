96fedf3bb3a1b5080c6646bf49d09ebccacb3250
Robert Strandh <robert.strandh@gmail.com>
2018-11-30 13:34:47 +0100

    Modify CONS-related translators to conform to new protocol.

--- Code/Backends/Processors/x86/64/HIR-to-MIR/cons.lisp
+++ Code/Backends/Processors/x86/64/HIR-to-MIR/cons.lisp
@@ -1,21 +1,33 @@
 (cl:in-package #:sicl-hir-to-mir)
 
-(defmethod hir-to-mir ((client sicl-client:sicl-x86-64)
-                       (instruction cleavir-ir:car-instruction))
-  (change-class instruction 'cleavir-ir:memref2-instruction
-                :offset -1))
+(defmethod translate ((client sicl-client:sicl-x86-64)
+                      (instruction cleavir-ir:car-instruction))
+  (make-instance 'cleavir-ir:memref2-instruction
+    :offset -1
+    :inputs (cleavir-ir:inputs instruction)
+    :outputs (cleavir-ir:outputs instruction)
+    :successors (cleavir-ir:successors instruction)))
 
-(defmethod hir-to-mir ((client sicl-client:sicl-x86-64)
-                       (instruction cleavir-ir:cdr-instruction))
-  (change-class instruction 'cleavir-ir:memref2-instruction
-                :offset 7))
+(defmethod translate ((client sicl-client:sicl-x86-64)
+                      (instruction cleavir-ir:cdr-instruction))
+  (make-instance 'cleavir-ir:memref2-instruction
+    :offset 7
+    :inputs (cleavir-ir:inputs instruction)
+    :outputs (cleavir-ir:outputs instruction)
+    :successors (cleavir-ir:successors instruction)))
 
-(defmethod hir-to-mir ((client sicl-client:sicl-x86-64)
-                       (instruction cleavir-ir:rplaca-instruction))
-  (change-class instruction 'cleavir-ir:memset2-instruction
-                :offset -1))
+(defmethod translate ((client sicl-client:sicl-x86-64)
+                      (instruction cleavir-ir:rplaca-instruction))
+  (make-instance 'cleavir-ir:memset2-instruction
+    :offset -1
+    :inputs (cleavir-ir:inputs instruction)
+    :outputs (cleavir-ir:outputs instruction)
+    :successors (cleavir-ir:successors instruction)))
 
-(defmethod hir-to-mir ((client sicl-client:sicl-x86-64)
-                       (instruction cleavir-ir:rplacd-instruction))
-  (change-class instruction 'cleavir-ir:memset2-instruction
-                :offset 7))
+(defmethod translate ((client sicl-client:sicl-x86-64)
+                      (instruction cleavir-ir:rplacd-instruction))
+  (make-instance 'cleavir-ir:memset2-instruction
+    :offset 7
+    :inputs (cleavir-ir:inputs instruction)
+    :outputs (cleavir-ir:outputs instruction)
+    :successors (cleavir-ir:successors instruction)))
