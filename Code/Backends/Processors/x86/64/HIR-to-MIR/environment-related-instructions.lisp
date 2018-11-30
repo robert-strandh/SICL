668e8ac40493e8c4e064bae7a3d7753b5b47a09c
Robert Strandh <robert.strandh@gmail.com>
2018-11-30 13:39:50 +0100

    Modify environment-related translators to conform to new protocol.

--- Code/Backends/Processors/x86/64/HIR-to-MIR/environment-related-instructions.lisp
+++ Code/Backends/Processors/x86/64/HIR-to-MIR/environment-related-instructions.lisp
@@ -1,11 +1,17 @@
 (cl:in-package #:sicl-hir-to-mir)
 
-(defmethod hir-to-mir ((client sicl-client:sicl-x86-64)
-                       (instruction cleavir-ir:read-cell-instruction))
-  (change-class instruction 'cleavir-ir:memref2-instruction
-                :offset -1))
+(defmethod translate ((client sicl-client:sicl-x86-64)
+                      (instruction cleavir-ir:read-cell-instruction))
+  (make-instance 'cleavir-ir:memref2-instruction
+    :offset -1
+    :inputs (cleavir-ir:inputs instruction)
+    :outputs (cleavir-ir:outputs instruction)
+    :successors (cleavir-ir:successors instruction)))
 
-(defmethod hir-to-mir ((client sicl-client:sicl-x86-64)
-                       (instruction cleavir-ir:write-cell-instruction))
-  (change-class instruction 'cleavir-ir:memset2-instruction
-                :offset -1))
+(defmethod translate ((client sicl-client:sicl-x86-64)
+                      (instruction cleavir-ir:write-cell-instruction))
+  (make-instance 'cleavir-ir:memset2-instruction
+    :offset -1
+    :inputs (cleavir-ir:inputs instruction)
+    :outputs (cleavir-ir:outputs instruction)
+    :successors (cleavir-ir:successors instruction)))
