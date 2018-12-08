2c23e140c5f75912defa4a330646e12b09125c7a
Robert Strandh <robert.strandh@gmail.com>
2018-12-08 14:17:14 +0100

    Untabify for better tool support.

--- Code/CLOS/ensure-method.lisp
+++ Code/CLOS/ensure-method.lisp
@@ -4,7 +4,7 @@
 ;;;
 ;;; ENSURE-METHOD.
 ;;;
-;;; This function is not required by the AMOP to exist, 
+;;; This function is not required by the AMOP to exist,
 ;;;
 ;;; One can think of this function as a convenience function that
 ;;; combines a call to MAKE-INSTANCE with a call to ADD-METHOD.  A
@@ -23,14 +23,14 @@
 ;;; compile-time environment) in which the call to DEFMETHOD is made.
 
 (defun ensure-method (generic-function
-		      &rest keys
-		      &key specializers
-		      &allow-other-keys)
+                      &rest keys
+                      &key specializers
+                      &allow-other-keys)
   (unless (cleavir-code-utilities:proper-list-p specializers)
     (error "Specializers must be a proper list: ~s" specializers))
   (let ((specs (loop for s in specializers
-		     collect (make-specializer s)))
-	(remaining-keys (copy-list keys)))
+                     collect (make-specializer s)))
+        (remaining-keys (copy-list keys)))
     (loop while (remf remaining-keys :specializers))
     (let ((method (make-method-for-generic-function
                    generic-function specs remaining-keys)))
