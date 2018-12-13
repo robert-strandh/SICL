eae0a7788a12919c62341d221050cbd0b77de50e
Robert Strandh <robert.strandh@gmail.com>
2018-12-13 11:52:10 +0100

    New file containing method on PRINT-OBJECT specialized to STANDARD-OBJECT.

---
+++ Code/Printer/print-object-defmethod-standard-object.lisp
@@ -0,0 +1,5 @@
+(cl:in-package #:sicl-printer)
+
+(defmethod print-object ((object standard-object) stream)
+  (print-unreadable-object (object stream :type t :identity t)
+    (declare (ignorable object stream))))
