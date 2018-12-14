8894f38a870efa622d82e203406a62ae3875527e
Robert Strandh <robert.strandh@gmail.com>
2018-12-14 14:59:33 +0100

    New file containing method on PRINT-OBJECT specialized to FUNCTION.

---
+++ Code/Printer/print-object-defmethod-function.lisp
@@ -0,0 +1,5 @@
+(cl:in-package #:sicl-printer)
+
+(defmethod print-object ((object function) stream)
+  (print-unreadable-object (object stream :type t :identity t)
+    (declare (ignorable object stream))))
