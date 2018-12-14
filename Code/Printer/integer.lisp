93b52f980e4e3d353914b5f95b39e9cc1893472f
Robert Strandh <robert.strandh@gmail.com>
2018-12-14 14:58:58 +0100

    Determine whether radix should be printed, and if so, how.

--- Code/Printer/integer.lisp
+++ Code/Printer/integer.lisp
@@ -19,14 +19,29 @@
                    (princ (schar digits integer) stream)
                    (let* ((length/2 (floor length 2))
                           (divisor (expt base length/2)))
-                     (lower (floor integer divisor) (- length length/2))
-                     (lower (mod integer divisor) length/2)))))
+                     (multiple-value-bind (quotient remainder)
+                         (floor integer divisor)
+                       (lower quotient  (- length length/2))
+                       (lower remainder length/2))))))
       (upper integer))))
 
 (defun print-integer (integer base stream)
+  ;; Determine whether a radix prefix should be printed, and if so,
+  ;; which one.
+  (when *print-radix*
+    (princ (case *print-base*
+             (2 "#b")
+             (8 "#o")
+             (10 "")
+             (16 "#x")
+             (otherwise (format nil "#~dr" *print-base*)))
+           stream))
   (cond ((zerop integer)
          (princ #\0 stream))
         ((minusp integer)
          (print-positive-integer (- integer) base stream))
         (t
-         (print-positive-integer integer base stream))))
+         (print-positive-integer integer base stream)))
+  ;; Determine whether a trailing dot should be printed.
+  (when (and *print-radix* (= *print-base* 10))
+    (princ #\. stream)))
