8bc4b88fb428eea0bc6dc36312c1131679096e78
Robert Strandh <robert.strandh@gmail.com>
2018-12-14 15:00:29 +0100

    New component containing definitions of printer-related special variables.

---
+++ Code/Printer/variables.lisp
@@ -0,0 +1,31 @@
+(cl:in-package #:sicl-printer)
+
+(defvar *print-array t)
+
+(defvar *print-base* 10)
+
+(defvar *print-case* :upcase)
+
+(defvar *print-radix* nil)
+
+(defvar *print-circle* nil)
+
+(defvar *print-escape* t)
+
+(defvar *print-gensym* t)
+
+(defvar *print-length* nil)
+
+(defvar *print-level* nil)
+
+(defvar *print-lines* nil)
+
+(defvar *print-miser-width* nil)
+
+(defvar *print-pretty* t)
+
+(defvar *print-radix* nil)
+
+(defvar *print-readably* nil)
+
+(defvar *print-right-margin* nil)
