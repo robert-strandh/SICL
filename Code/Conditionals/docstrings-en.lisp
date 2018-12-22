05a72ee48d8b507adbe655c66ebe04941e515721
Robert Strandh <robert.strandh@gmail.com>
2018-12-22 16:17:58 +0100

    Remove useless whitespace at end of line.

--- Code/Conditionals/docstrings-en.lisp
+++ Code/Conditionals/docstrings-en.lisp
@@ -2,16 +2,16 @@
 ;;;;
 ;;;;     Robert Strandh (robert.strandh@gmail.com)
 ;;;;
-;;;; all rights reserved. 
+;;;; all rights reserved.
 ;;;;
-;;;; Permission is hereby granted to use this software for any 
+;;;; Permission is hereby granted to use this software for any
 ;;;; purpose, including using, modifying, and redistributing it.
 ;;;;
 ;;;; The software is provided "as-is" with no warranty.  The user of
-;;;; this software assumes any responsibility of the consequences. 
+;;;; this software assumes any responsibility of the consequences.
 
 ;;;; This file is part of the conditionals module of the SICL project.
-;;;; See the file SICL.text for a description of the project. 
+;;;; See the file SICL.text for a description of the project.
 ;;;; See the file conditionals.text for a description of the module.
 
 (in-package #:sicl-conditionals)
