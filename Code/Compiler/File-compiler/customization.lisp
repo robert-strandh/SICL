a4c6ff369d7c56dad0835768fb8f472e0fc6d85a
Robert Strandh <robert.strandh@gmail.com>
2018-12-23 08:30:51 +0100

    Remove customization for FDEFINITION.

--- Code/Compiler/File-compiler/customization.lisp
+++ Code/Compiler/File-compiler/customization.lisp
@@ -1,24 +1,5 @@
 (cl:in-package #:sicl-file-compiler)
 
-;;; When we are asked to compile the name of a global function, by
-;;; default Cleavir generates an FDEFINITION-AST taking the function
-;;; name as an input.  For SICL, we do not want that.  Instead we want
-;;; it to generate an access to the CAR of the global function cell
-;;; that contains the function.  And we want the function cell to be
-;;; accessed at load time.
-(defmethod cleavir-generate-ast:convert-global-function
-    ((info cleavir-env:global-function-info)
-     (env sicl-genv:environment)
-     system)
-  (declare (ignore system))
-  (cleavir-ast:make-car-ast
-   (cleavir-ast:make-load-time-value-ast
-    `(sicl-genv:function-cell
-      ',(cleavir-env:name info)
-      (sicl-genv:global-environment))
-    ;; The cell is not read-only.
-    nil)))
-
 ;;; For this environment, whenever Cleavir asks for information about
 ;;; a function name and that function name is a symbol in the
 ;;; CLEAVIR-PRIMOP package, we want to return an object of type
