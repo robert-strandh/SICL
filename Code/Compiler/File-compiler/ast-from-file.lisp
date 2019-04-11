02694f28db9d3f3f27fdb0be0e3f1d7067b66fe4
Robert Strandh <robert.strandh@gmail.com>
2019-04-11 16:08:27 +0200

    Use instance of specific client for CST-TO-AST.

--- Code/Compiler/File-compiler/ast-from-file.lisp
+++ Code/Compiler/File-compiler/ast-from-file.lisp
@@ -1,15 +1,16 @@
 (cl:in-package #:sicl-file-compiler)
 
-(defun ast-from-stream (stream environment system)
+(defun ast-from-stream (stream environment)
   (cleavir-ast:make-progn-ast
    (loop with cleavir-generate-ast:*compiler* = 'compile-file
+         with client = (make-instance 'sicl-file-compiler)
          with eof = (list nil)
          for cst = (eclector.concrete-syntax-tree:cst-read stream nil eof)
          until (eq cst eof)
-         collect (cleavir-cst-to-ast:cst-to-ast cst environment system))
+         collect (cleavir-cst-to-ast:cst-to-ast cst environment client))
    :origin nil
    :policy nil))
 
-(defun ast-from-file (filename environment system)
+(defun ast-from-file (filename environment)
   (with-open-file (stream filename :direction :input)
-    (ast-from-stream stream environment system)))
+    (ast-from-stream stream environment)))
