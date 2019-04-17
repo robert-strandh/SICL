c1f71ff035389f0db32ddbcd0e1abfd2647053d3
Robert Strandh <robert.strandh@gmail.com>
2019-04-17 16:52:37 +0200

    Move mixin classes to a different component.

--- Code/Cleavir2/Abstract-syntax-tree/ast.lisp
+++ Code/Cleavir2/Abstract-syntax-tree/ast.lisp
@@ -36,7 +36,56 @@
                          :initarg :dynamic-environment
                          :accessor dynamic-environment)))
 
-;;; Policies must be saved
 (cleavir-io:define-save-info ast
   (:dynamic-environment dynamic-environment))
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;; Mixin classes.
+
+;;; This class is used as a superclass for ASTs that produce Boolean
+;;; results, so are mainly used as the TEST-AST of an IF-AST.
+(defclass boolean-ast-mixin () ())
+
+;;; This class is used as a superclass for ASTs that produce no value
+;;; and that must be compiled in a context where no value is required.
+(defclass no-value-ast-mixin () ())
+
+;;; This class is used as a superclass for ASTs that produce a single
+;;; value that is not typically not just a Boolean value.
+(defclass one-value-ast-mixin () ())
+
+;;; This class is used as a superclass for ASTs that have no side
+;;; effect.
+(defclass side-effect-free-ast-mixin () ())
+
+;;; This class is used as a superclass for ASTs that output a dynamic
+;;; environment.
+(defclass dynamic-environment-output-ast-mixin ()
+  ((%dynenv-out :initarg :dynamic-environment-out
+                :accessor dynamic-environment-out-ast)))
+
+;;; FIXME: It would be nice if this could have a method
+;;; for CHILDREN as well.
+(cleavir-io:define-save-info dynamic-environment-output-ast-mixin
+  (:dynamic-environment-out dynamic-environment-out-ast))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;; Predicate to test whether an AST is side-effect free.
+;;;
+;;; For instances of SIDE-EFFECT-FREE-AST-MIXIN, this predicate always
+;;; returns true.  For others, it has a default method that returns
+;;; false.  Implementations may add a method on some ASTs such as
+;;; CALL-AST that return true only if a particular call is side-effect
+;;; free.
+
+(defgeneric side-effect-free-p (ast))
+
+(defmethod side-effect-free-p (ast)
+  (declare (ignore ast))
+  nil)
+
+(defmethod side-effect-free-p ((ast side-effect-free-ast-mixin))
+  (declare (ignorable ast))
+  t)
