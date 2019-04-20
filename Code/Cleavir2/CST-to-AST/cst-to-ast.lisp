2c520c231888db4fc8e8da7d558a1ae72a5c2ea7
Robert Strandh <robert.strandh@gmail.com>
2019-04-20 11:35:24 +0200

    Make CLIENT the first parameter of CST-TO-AST.

--- Code/Cleavir2/CST-to-AST/cst-to-ast.lisp
+++ Code/Cleavir2/CST-to-AST/cst-to-ast.lisp
@@ -7,10 +7,10 @@
 ;;; is the source location of that CST.
 (defvar *origin*)
 
-(defun cst-to-ast (cst lexical-environment client
-                       &optional (cleavir-ast:*dynamic-environment*
-                                  (make-instance 'cleavir-ast:lexical-ast
-                                   :name '#:unused-dynamic-environment)))
+(defun cst-to-ast (client cst lexical-environment
+                   &optional (cleavir-ast:*dynamic-environment*
+                              (make-instance 'cleavir-ast:lexical-ast
+                                :name '#:unused-dynamic-environment)))
   (let ((*subforms-are-top-level-p* t)
 	(*compile-time-too* nil))
     (convert client cst lexical-environment)))
