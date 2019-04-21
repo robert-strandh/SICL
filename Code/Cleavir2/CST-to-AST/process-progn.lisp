56d0e681268503b99b9e1b760eddcadf0792b624
Robert Strandh <robert.strandh@gmail.com>
2019-04-21 17:24:27 +0200

    Correct incorrect comment.

--- Code/Cleavir2/CST-to-AST/process-progn.lisp
+++ Code/Cleavir2/CST-to-AST/process-progn.lisp
@@ -2,9 +2,8 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;
-;;; Turn a list of ASTs into either a PROGN-AST or a
-;;; LOAD-TIME-VALUE-AST containing NIL in case the list of ASTs is
-;;; NIL.
+;;; Turn a list of ASTs into either a PROGN-AST or a CONSTANT-AST
+;;; containing NIL in case the list of ASTs is NIL.
 
 (defun process-progn (asts)
   (if (null asts)
