a8f513865b02606e12a43a680aa07579e42358d7
Robert Strandh <robert.strandh@gmail.com>
2019-04-21 17:28:37 +0200

    Add FIXME remark to incorrecct comment.

--- Code/Cleavir2/CST-to-AST/set-or-bind-variable.lisp
+++ Code/Cleavir2/CST-to-AST/set-or-bind-variable.lisp
@@ -1,15 +1,18 @@
 (cl:in-package #:cleavir-cst-to-ast)
 
-;;; ENV is an environment that is known to contain information about
-;;; the variable VARIABLE, but we don't know whether it is special or
-;;; lexical.  VALUE-AST is an AST that computes the value to be given
-;;; to VARIABLE.  NEXT-AST is an AST that represents the computation
-;;; to take place after the variable has been given its value.  If the
-;;; variable is special, this function creates a BIND-AST with
-;;; NEXT-AST as its body.  If the variable is lexical, this function
-;;; creates a PROGN-AST with two ASTs in it.  The first one is a
-;;; SETQ-AST that assigns the value to the variable, and the second
-;;; one is the NEXT-AST.
+;;; FIXME: this comment is incorrect.  NEXT-AST has been replaced by
+;;; NEXT-THUNK.
+
+;;; LEXICAL-ENVIRONMENT is an environment that is known to contain
+;;; information about the variable VARIABLE, but we don't know whether
+;;; it is special or lexical.  VALUE-AST is an AST that computes the
+;;; value to be given to VARIABLE.  NEXT-AST is an AST that represents
+;;; the computation to take place after the variable has been given
+;;; its value.  If the variable is special, this function creates a
+;;; BIND-AST with NEXT-AST as its body.  If the variable is lexical,
+;;; this function creates a PROGN-AST with two ASTs in it.  The first
+;;; one is a SETQ-AST that assigns the value to the variable, and the
+;;; second one is the NEXT-AST.
 
 (defun set-or-bind-variable (client
                              variable-cst
