7d009ed3fecb35d7d8b050c3627bd03bd54e216b
Robert Strandh <robert.strandh@gmail.com>
2019-04-23 07:40:45 +0200

    Get rid of CALL-WITH-VARIABLE-BOUND in favor of generating a BIND-AST.

--- Code/Cleavir2/CST-to-AST/convert-special-binding.lisp
+++ Code/Cleavir2/CST-to-AST/convert-special-binding.lisp
@@ -1,36 +1,18 @@
 (cl:in-package #:cleavir-cst-to-ast)
 
-;;; We need to build a CST corresponding to the following expression:
-;;;
-;;; `(cleavir-primop:call-with-variable-bound
-;;;   ',variable
-;;;   (cleavir-primop:ast ,value-ast)
-;;;   (lambda () (cleavir-primop:ast ,next-ast)))
-;;;
-;;; The problem here is that we don't have VARIABLE, only the
-;;; corresponding CST, and we do not want to lose the source
-;;; information of the variable.  So we construct an ordinary Common
-;;; Lisp list of CSTs, and then we convert that to a CST.
-(defmethod convert-special-binding
-    (client variable-cst value-ast next-thunk lexical-environment dynamic-environment-ast)
-  (let* ((call-with-variable-bound-cst
-           (cst:cst-from-expression 'cleavir-primop:call-with-variable-bound))
-         (quoted-variable-cst
-           (cst:cstify `(,(cst:cst-from-expression 'quote)
-                         ,variable-cst)))
-         (new-cst (cst:cstify `(,call-with-variable-bound-cst
-                                ,quoted-variable-cst
-                                ,(cst:cst-from-expression
-                                  `(cleavir-primop:ast ,value-ast))
-                                ,(cst:cst-from-expression
-                                  `(cleavir-primop:ast
-                                    ,(let* ((dynamic-environment-out-ast
-                                              (make-instance 'cleavir-ast:lexical-ast
-                                               :name '#:cwvb-dynamic-environment))
-                                            (next-ast (funcall next-thunk)))
-                                       (make-instance 'cleavir-ast:function-ast
-                                         :body-ast next-ast
-                                         :lambda-list nil
-                                         :dynamic-environment-ast dynamic-environment-ast
-                                         :dynamic-environment-out dynamic-environment-out-ast))))))))
-    (convert client new-cst lexical-environment dynamic-environment-ast)))
+(defmethod convert-special-binding (client
+                                    variable-cst
+                                    value-ast
+                                    next-thunk
+                                    lexical-environment
+                                    dynamic-environment-ast)
+  (let ((dynamic-environment-out-ast
+          (make-instance 'cleavir-ast:lexical-ast :name '#:bound-env)))
+    (make-instance 'cleavir-ast:bind-ast
+      :name-ast (convert client
+                 variable-cst
+                 lexical-environment
+                 dynamic-environment-ast)
+      :value-ast value-ast
+      :body-ast (funcall next-thunk) ; FIXME: supply argument once implemented.
+      :dynamic-environment-out dynamic-environment-out-ast)))
