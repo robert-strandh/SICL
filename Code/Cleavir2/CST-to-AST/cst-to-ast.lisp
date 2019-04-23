e966d139e42861f42939090302a7dd8c2b6baffd
Robert Strandh <robert.strandh@gmail.com>
2019-04-23 05:54:27 +0200

    Use an :INITFORM rather than an :AFTER method on INITIALIZE-INSTANCE.

--- Code/Cleavir2/CST-to-AST/cst-to-ast.lisp
+++ Code/Cleavir2/CST-to-AST/cst-to-ast.lisp
@@ -1,12 +1,9 @@
 (cl:in-package #:cleavir-cst-to-ast)
 
 (stealth-mixin:define-stealth-mixin ast-mixin () cleavir-ast:ast
-  ((%origin :initarg :origin :reader origin)))
+  ((%origin :initfform *origin* :initarg :origin :reader origin)))
 
 (defun cst-to-ast (client cst lexical-environment)
   (let ((*subforms-are-top-level-p* t)
 	(*compile-time-too* nil))
     (convert client cst lexical-environment nil)))
-
-(defmethod initialize-instance :after ((object ast-mixin) &key)
-  (reinitialize-instance object :origin *origin*))
