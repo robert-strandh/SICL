b9a75f7ba0992043285bb15331ede365dfd716d9
Robert Strandh <robert.strandh@gmail.com>
2019-04-25 07:31:04 +0200

    Define function for computing colors of dynamic environment ASTs.

--- Code/Cleavir2/Abstract-syntax-tree/Visualizer/color.lisp
+++ Code/Cleavir2/Abstract-syntax-tree/Visualizer/color.lisp
@@ -7,3 +7,25 @@
 
 (defmethod background-color ((ast cleavir-ast:constant-ast))
   clim:+pink+)
+
+(defun compute-dynamic-environment-colors (ast)
+  (let ((visited (make-hash-table :test #'eq))
+        (colors (make-hash-table :test #'eq))
+        (choices (list clim:+red+ clim:+blue+ clim:+green+
+                       clim:+cyan+ clim:+magenta+ clim:+gold+)))
+    ;; Turn the choices into a circular list.
+    (setf (cdr (last choices)) choices)
+    (flet ((assign-color (dynamic-environment-ast)
+             (when (null (gethash dynamic-environment-ast colors))
+               (setf (gethash dynamic-environment-ast colors)
+                     (pop choices)))))
+      (labels ((visit (ast)
+                 (unless (gethash ast visited)
+                   (when (typep ast 'cleavir-ast:dynamic-environment-input-ast-mixin)
+                     (assign-color (cleavir-ast:dynamic-environment-input-ast ast)))
+                   (when (typep ast 'cleavir-ast:dynamic-environment-output-ast-mixin)
+                     (assign-color (cleavir-ast:dynamic-environment-output-ast ast)))
+                   (loop for child in (cleavir-ast:children ast)
+                         do (visit child)))))
+        (visit ast)))
+    colors))
