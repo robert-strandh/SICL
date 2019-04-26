51f2ea57ae0684d7e2b979b1fff51ff36c79cc2a
Robert Strandh <robert.strandh@gmail.com>
2019-04-26 10:50:10 +0200

    Define function for finding all FDEFINITION-INSTRUCTIONs.

--- Code/Compiler/HIR-transformations/hoist-fdefinitions.lisp
+++ Code/Compiler/HIR-transformations/hoist-fdefinitions.lisp
@@ -1,5 +1,14 @@
 (cl:in-package #:sicl-hir-transformations)
 
+(defun find-fdefinitions (top-level-enter-instruction)
+  (let ((result '()))
+    (cleavir-ir:map-instructions-arbitrary-order
+     (lambda (instruction)
+       (when (typep instruction 'cleavir-ir:fdefinition-instruction)
+         (push instruction result)))
+     top-level-enter-instruction)
+    result))
+
 (defun hoist-fdefinitions (top-level-enter-instruction)
   (declare (ignore top-level-enter-instruction))
   nil)
