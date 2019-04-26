21d1cfdbb721c9ebf2ef23dead9a6425c041e6d8
Robert Strandh <robert.strandh@gmail.com>
2019-04-26 11:30:06 +0200

    Define function for transforming a single FDEFINITION-INSTRUCTION.

--- Code/Compiler/HIR-transformations/hoist-fdefinitions.lisp
+++ Code/Compiler/HIR-transformations/hoist-fdefinitions.lisp
@@ -1,6 +1,28 @@
 (cl:in-package #:sicl-hir-transformations)
 
-(defun find-fdefinitions (top-level-enter-instruction)
+(defclass find-function-cell-instruction (cleavir-ir:instruction)
+  ((%name :initarg :name :reader name)))
+
+(defun transform-fdefinition-instruction (top-level-enter-instruction
+                                          fdefinition-instruction)
+  (let* ((variable (first (cleavir-ir:inputs fdefinition-instruction)))
+         (assignment-instruction (first (cleavir-ir:defining-instructions variable)))
+         (constant-input (first (cleavir-ir:inputs assignment-instruction)))
+         (function-name (cleavir-ir:value constant-input))
+         (dynamic-environment-location
+           (cleavir-ir:dynamic-environment-output top-level-enter-instruction))
+         (new (make-instance 'find-function-cell-instruction
+                :name function-name
+                :inputs '()
+                :output variable
+                :dynamic-environment-location dynamic-environment-location)))
+    (change-class assignment-instruction 'cleavir-ir:nop-instruction
+                  :inputs '()
+                  :outputs '())
+    (change-class fdefinition-instruction 'cleavir-ir:car-instruction)
+    (cleavir-ir:insert-instruction-after new top-level-enter-instruction)))
+
+(defun find-fdefinition-instructions (top-level-enter-instruction)
   (let ((result '()))
     (cleavir-ir:map-instructions-arbitrary-order
      (lambda (instruction)
@@ -10,5 +32,8 @@
     result))
 
 (defun hoist-fdefinitions (top-level-enter-instruction)
-  (declare (ignore top-level-enter-instruction))
-  nil)
+  (let ((fdefinition-instructions
+          (find-fdefinition-instructions top-level-enter-instruction)))
+    (loop for fdefinition-instruction in fdefinition-instructions
+          do (transform-fdefinition-instruction top-level-enter-instruction
+                                                fdefinition-instruction))))
