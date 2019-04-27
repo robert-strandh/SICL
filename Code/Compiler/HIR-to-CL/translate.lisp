cd7d419f61ad73b7979c80667f3563e5a5206a82
Robert Strandh <robert.strandh@gmail.com>
2019-04-27 09:48:30 +0200

    Define method on TRANSLATE specialized to ASSIGNMENT-INSTRUCTION.

--- Code/Compiler/HIR-to-CL/translate.lisp
+++ Code/Compiler/HIR-to-CL/translate.lisp
@@ -10,6 +10,16 @@
         (reduce #'append
                 (mapcar #'translate (cleavir-ir:successors instruction)))))
 
+(defmethod translate ((instruction cleavir-ir:assignment-instruction))
+  (let* ((input (first (cleavir-ir:inputs instruction)))
+         (output (first (cleavir-ir:outputs instruction)))
+         (successor (first (cleavir-ir:successors instruction))))
+    (cons `(setq ,(cleavir-ir:name output)
+                 ,(if (typep input 'cleavir-ir:constant-input)
+                      `',(cleavir-ir:value input)
+                      (cleavir-ir:name input)))
+          (translate successor))))
+
 (defmethod translate :around (instruction)
   (let ((tag (gethash instruction *visited*)))
     (if (null tag)
