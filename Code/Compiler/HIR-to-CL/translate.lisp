9c222fa94b9c8e8302449038b89b7ae0c4765fbc
Robert Strandh <robert.strandh@gmail.com>
2019-04-27 09:41:14 +0200

    Define an :AROUND method on TRANSLATE that checks for instructions already visited.

--- Code/Compiler/HIR-to-CL/translate.lisp
+++ Code/Compiler/HIR-to-CL/translate.lisp
@@ -3,3 +3,10 @@
 (defvar *visited*)
 
 (defgeneric translate (instruction))
+
+(defmethod translate :around (instruction)
+  (let ((tag (gethash instruction *visited*)))
+    (if (null tag)
+        (progn (setf (gethash instruction *visited*) (gensym))
+               (call-next-method))
+        `((go ,tag)))))
