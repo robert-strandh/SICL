510c0082843e48e6f9a0c06241e3f9a41d6b8a7b
Robert Strandh <robert.strandh@gmail.com>
2019-04-27 09:42:00 +0200

    Define temporary default method on TRANSLATE.

--- Code/Compiler/HIR-to-CL/translate.lisp
+++ Code/Compiler/HIR-to-CL/translate.lisp
@@ -4,6 +4,12 @@
 
 (defgeneric translate (instruction))
 
+;;; FIXME: Remove this method later.
+(defmethod translate (instruction)
+  (cons `(invalid ,instruction)
+        (reduce #'append
+                (mapcar #'translate (cleavir-ir:successors instruction)))))
+
 (defmethod translate :around (instruction)
   (let ((tag (gethash instruction *visited*)))
     (if (null tag)
