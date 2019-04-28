baf5c053a2b790778756fbebe3665a5daed2ff96
Robert Strandh <robert.strandh@gmail.com>
2019-04-28 15:57:44 +0200

    Add slot BLOCK-NAME to be used with RETURN-INSTRUCTIONs.

--- Code/Compiler/HIR-to-CL/context.lisp
+++ Code/Compiler/HIR-to-CL/context.lisp
@@ -2,4 +2,5 @@
 
 (defclass context ()
   ((%visited :initform (make-hash-table :test #'eq) :reader visited)
-   (%values-location :initform (gensym) :reader values-location)))
+   (%values-location :initform (gensym) :reader values-location)
+   (%block-name :initform (gensym) :reader block-name)))
