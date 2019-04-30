cbf0c72e03b206def9d49a2f3987ea867d52e96d
Robert Strandh <robert.strandh@gmail.com>
2019-04-30 16:28:08 +0200

    Define and use function for creating top-level LET bindings for code entrypoints.

--- Code/Compiler/HIR-to-CL/hir-to-cl.lisp
+++ Code/Compiler/HIR-to-CL/hir-to-cl.lisp
@@ -1,9 +1,15 @@
 (cl:in-package #:sicl-hir-to-cl)
 
+(defun make-code-bindings (initial-instruction context)
+  (let ((enter-instructions (sort-functions initial-instruction)))
+    (loop for enter-instruction in (butlast enter-instructions)
+          collect `(,(gethash enter-instruction (function-names context))
+                    ,(translate-enter-instruction enter-instruction context)))))
+
 (defun hir-to-cl (initial-instruction)
   (let ((enter-instructions (sort-functions initial-instruction))
         (context (make-instance 'context)))
     (loop for enter-instruction in (butlast enter-instructions)
           do (setf (gethash enter-instruction (function-names context))
-                   (gensym "function")))
-    (translate-enter-instruction initial-instruction context)))
+                   (gensym "code")))
+    (make-code-bindings initial-instruction context)))
