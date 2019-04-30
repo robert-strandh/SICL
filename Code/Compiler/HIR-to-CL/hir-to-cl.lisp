39b5dec92d022689933a756aa2c596e2dbdba5b2
Robert Strandh <robert.strandh@gmail.com>
2019-04-30 13:52:40 +0200

    Add each ENTER-INSTRUCTION except the top-level one into the function table.

--- Code/Compiler/HIR-to-CL/hir-to-cl.lisp
+++ Code/Compiler/HIR-to-CL/hir-to-cl.lisp
@@ -1,5 +1,9 @@
 (cl:in-package #:sicl-hir-to-cl)
 
 (defun hir-to-cl (initial-instruction)
-  (let ((context (make-instance 'context)))
+  (let ((enter-instructions (sort-functions initial-instruction))
+        (context (make-instance 'context)))
+    (loop for enter-instruction in (butlast enter-instructions)
+          do (setf (gethash enter-instruction (function-names context))
+                   (gensym "function")))
     (translate-enter-instruction initial-instruction context)))
