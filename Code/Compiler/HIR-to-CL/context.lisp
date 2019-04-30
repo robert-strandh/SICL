8f4252dd4cd2cb55d0506320dfb5121c94c031db
Robert Strandh <robert.strandh@gmail.com>
2019-04-30 13:46:34 +0200

    Define slots containing names of environment access functions.

--- Code/Compiler/HIR-to-CL/context.lisp
+++ Code/Compiler/HIR-to-CL/context.lisp
@@ -3,4 +3,16 @@
 (defclass context ()
   ((%visited :initform (make-hash-table :test #'eq) :reader visited)
    (%values-location :initform (gensym "values") :reader values-location)
-   (%block-name :initform (gensym "block") :reader block-name)))
+   (%block-name :initform (gensym "block") :reader block-name)
+   (%enclose-function-var
+    :initform (gensym "enclose-function")
+    :reader enclose-function-var)
+   (%make-cell-function-var
+    :initform (gensym "make-cell-function")
+    :reader make-cell-function-var)
+   (%fetch-function-var
+    :initform (gensym "fetch-function")
+    :reader fetch-function-var)
+   (%static-env-function-var
+    :initform (gensym "static-env-function")
+    :reader static-env-function-var)))
