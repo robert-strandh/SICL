4093678056bf209f16c7a47c53226e822d468e74
Robert Strandh <robert.strandh@gmail.com>
2019-05-01 17:36:44 +0200

    Remove obsolete slots.

--- Code/Compiler/HIR-to-CL/context.lisp
+++ Code/Compiler/HIR-to-CL/context.lisp
@@ -5,15 +5,6 @@
    (%function-names :initform (make-hash-table :test #'eq) :reader function-names)
    (%values-location :initform (gensym "values") :reader values-location)
    (%block-name :initform (gensym "block") :reader block-name)
-   (%enclose-function-var
-    :initform (gensym "enclose-function")
-    :reader enclose-function-var)
-   (%make-cell-function-var
-    :initform (gensym "make-cell-function")
-    :reader make-cell-function-var)
-   (%fetch-function-var
-    :initform (gensym "fetch-function")
-    :reader fetch-function-var)
    (%static-env-function-var
     :initform (gensym "static-env-function")
     :reader static-env-function-var)))
