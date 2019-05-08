c74dfc132affbb37a100308a2aec32271e99eacd
Robert Strandh <robert.strandh@gmail.com>
2019-05-08 08:31:02 +0200

    Make default method on TRANSLATE-LAST-INSTRUCTION handle dynamic environment popping.

--- Code/Compiler/HIR-to-CL/translate-basic-block.lisp
+++ Code/Compiler/HIR-to-CL/translate-basic-block.lisp
@@ -1,14 +1,34 @@
 (cl:in-package #:sicl-hir-to-cl)
 
-(defgeneric translate-last-instruction (client instruction context))
+(defgeneric translate-last-instruction
+    (client instruction context dynamic-environment-stack))
 
-(defmethod translate-last-instruction (client instruction context)
-  (append (translate client instruction context)
-          '()))
+;;; Default method on TRANSLATE-LAST-INSTRUCTION.  It is used when the
+;;; last instruction is a simple instruction with a single successor
+;;; that happened to be the last instruction of a basic block because
+;;; its single successor (which must be the LEADER of a basic block)
+;;; has multiple predecessors.
+(defmethod translate-last-instruction
+    (client instruction context dynamic-environment-stack)
+  (let* ((successor (first (cleavir-ir:successors instruction)))
+         (successor-block (basic-block-of-leader successor))
+         (tag (tag-of-basic-block successor-block))
+         (dynamic-environment-location
+           (cleavir-ir:dynamic-environment-location successor)))
+    (append (translate client instruction context)
+            (let ((pos (position dynamic-environment-location
+                                 dynamic-environment-stack)))
+              (loop repeat pos
+                    collect `(pop *dynamic-environment*)))
+            `((go ,tag)))))
 
-(defun translate-basic-block (client basic-block context)
+(defun translate-basic-block
+    (client basic-block context dynamic-environment-stack)
   (let* ((instructions (instructions basic-block))
          (last-instruction (first (last instructions))))
     (append (loop for instruction in (butlast instructions)
                   append (translate client instruction context))
-            (translate-last-instruction client last-instruction context))))
+            (translate-last-instruction client
+                                        last-instruction
+                                        context
+                                        dynamic-environment-stack))))
