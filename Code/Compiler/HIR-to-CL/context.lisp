3341e38c7a87e46420981f0b0abcce5f5d0424ca
Robert Strandh <robert.strandh@gmail.com>
2019-05-08 08:04:26 +0200

    Define special variable to hold a hash table mapping leaders to basic blocks.

--- Code/Compiler/HIR-to-CL/context.lisp
+++ Code/Compiler/HIR-to-CL/context.lisp
@@ -16,16 +16,27 @@
 
 (defvar *dynamic-environment-staack* '())
 
-;;; When a single function is translated, this special variable holds
-;;; an EQ hash table.  Each key in that hash table is a lexical
-;;; location of that single function, corresponding to a dynamic
-;;; environment.  The value is a list of basic blocks of that function
-;;; that are executed in the corresponding dynamic environment at run
-;;; time.
+;;; When a single function is translated, the value of this special
+;;; variable is an EQ hash table.  Each key in that hash table is a
+;;; lexical location of that single function, corresponding to a
+;;; dynamic environment.  The value is a list of basic blocks of that
+;;; function that are executed in the corresponding dynamic
+;;; environment at run time.
 (defvar *basic-blocks-in-dynamic-environment*)
 
 ;;; Given a lexical location corresponding to a dynamic environment,
-;;; return a list of basic blocks that will be executed in teh
+;;; return a list of basic blocks that will be executed in the
 ;;; corresponding dynamic environment at run time.
 (defun basic-blocks-in-dynamic-environment (dynamic-environment-location)
   (gethash dynamic-environment-location *basic-blocks-in-dynamic-environment*))
+
+;;; When a single function is translated, the value of this special
+;;; variable is n EQ hash table.  Each key in that hash table is a
+;;; LEADER, i.e. the first instruction of some basic block.  The value
+;;; is the basic block having that leader.
+(defvar *basic-block-of-leader*)
+
+;;; Given a LEADER, i.e. the first instruction of some basic block,
+;;; return the basic block having that instruction as its leader.
+(defun basic-block-of-leader (leader)
+  (gethash leader *basic-block-of-leader*))
