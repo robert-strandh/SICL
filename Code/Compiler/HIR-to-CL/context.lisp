53f01797ffd327461ff81f5ebbc11b20fd4832bd
Robert Strandh <robert.strandh@gmail.com>
2019-05-08 10:14:58 +0200

    Remove unused slot VISITED from context.

--- Code/Compiler/HIR-to-CL/context.lisp
+++ Code/Compiler/HIR-to-CL/context.lisp
@@ -1,8 +1,7 @@
 (cl:in-package #:sicl-hir-to-cl)
 
 (defclass context ()
-  ((%visited :initform (make-hash-table :test #'eq) :reader visited)
-   (%function-names :initform (make-hash-table :test #'eq) :reader function-names)
+  ((%function-names :initform (make-hash-table :test #'eq) :reader function-names)
    (%values-location :initform (gensym "values") :reader values-location)
    (%block-name :initform (gensym "block") :reader block-name)
    (%static-env-function-var
