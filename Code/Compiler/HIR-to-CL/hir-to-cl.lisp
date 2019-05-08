bcd31170f8f4144281eacc28cabce73d830d096c
Robert Strandh <robert.strandh@gmail.com>
2019-05-08 11:26:16 +0200

    Translate the top-level ENTER instructions in a way similar to the nested ones.

--- Code/Compiler/HIR-to-CL/hir-to-cl.lisp
+++ Code/Compiler/HIR-to-CL/hir-to-cl.lisp
@@ -7,12 +7,30 @@
                     ,(translate-enter-instruction client enter-instruction context)))))
 
 (defun hir-to-cl (client initial-instruction)
-  (let ((enter-instructions (sort-functions initial-instruction))
-        (context (make-instance 'context))
-        (lexical-locations (find-lexical-locations initial-instruction))
-        (successor (first (cleavir-ir:successors initial-instruction)))
-        (*static-environment-variable* (gensym "static-environment"))
-        (*top-level-function-parameter* (gensym "function-cell")))
+  (let* ((enter-instructions (sort-functions initial-instruction))
+         (context (make-instance 'context))
+         (lexical-locations (find-lexical-locations initial-instruction))
+         (successor (first (cleavir-ir:successors initial-instruction)))
+         (*static-environment-variable* (gensym "static-environment"))
+         (*top-level-function-parameter* (gensym "function-cell"))
+         (basic-blocks (compute-basic-blocks initial-instruction))
+         (*dynamic-environment-of-basic-block* (make-hash-table :test #'eq))
+         (*basic-blocks-in-dynamic-environment* (make-hash-table :test #'eq))
+         (*basic-block-of-leader* (make-hash-table :test #'eq))
+         (*tag-of-basic-block* (make-hash-table :test #'eq)))
+    (loop for basic-block in basic-blocks
+          for leader = (first (instructions basic-block))
+          for dynamic-environment-location
+            = (cleavir-ir:dynamic-environment-location leader)
+          do (setf (gethash basic-block *dynamic-environment-of-basic-block*)
+                   dynamic-environment-location)
+             (push basic-block
+                   (gethash dynamic-environment-location
+                            *basic-blocks-in-dynamic-environment*))
+             (setf (gethash leader *basic-block-of-leader*)
+                   basic-block)
+             (setf (gethash basic-block *tag-of-basic-block*)
+                   (gensym)))
     (loop for enter-instruction in (butlast enter-instructions)
           do (setf (gethash enter-instruction (function-names context))
                    (gensym "code")))
@@ -30,7 +48,19 @@
                                (first (cleavir-ir:outputs initial-instruction)))
                              ,*static-environment-variable*))
          (block ,(block-name context)
-           (tagbody ,@(translate client successor context)))))))
+           (tagbody (go ,(tag-of-basic-block (basic-block-of-leader successor)))
+              ,@(loop with dynamic-environment-location
+                        = (cleavir-ir:dynamic-environment-location successor)
+                      with basic-blocks
+                        = (basic-blocks-in-dynamic-environment
+                           dynamic-environment-location)
+                      for basic-block in basic-blocks
+                      collect (tag-of-basic-block basic-block)
+                      append (translate-basic-block
+                              client
+                              basic-block
+                              context
+                              (list dynamic-environment-location)))))))))
 
 (defmethod translate
     (client (instruction sicl-hir-transformations::find-function-cell-instruction) context)
