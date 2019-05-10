db8d6702bdc1926b028cfca0012b7b742da72027
Robert Strandh <robert.strandh@gmail.com>
2019-05-10 14:25:48 +0200

    Define function for unwinding the dynamic environment.

--- Code/Compiler/HIR-to-CL/dynamic-environment.lisp
+++ Code/Compiler/HIR-to-CL/dynamic-environment.lisp
@@ -19,3 +19,27 @@
 
 (defclass unwind-protect-entry (entry)
   ((%thunk :initarg :thunk :reader thunk)))
+
+(defun find-entry (identifier)
+  (loop for entry in *dynamic-environment*
+        when (and (typep entry 'block/tagbody-entry)
+                  (eq (identifier entry) identifier))
+          return entry))
+
+(defun unwind (identifier continuation)
+  (let ((entry (find-entry identifier)))
+    (cond ((null entry)
+           (error "Attempt to unwind to a non-existing exit point."))
+          ((not (valid-p entry))
+           (error "Attempt to unwind to an expired exit point."))
+          (t
+           (loop for temp in *dynamic-environment*
+                 until (eq temp entry)
+                 when (typep temp 'exit-point)
+                   do (setf (valid-p temp) nil))
+           (loop for top = (first *dynamic-environment*)
+                 until (eq top entry)
+                 when (typep top 'unwind-protect-entry)
+                   do (funcall (thunk entry))
+                 do (pop *dynamic-environment*))
+           (throw (identifier (pop *dynamic-environment*)) continuation)))))
