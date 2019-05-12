adcb17d7c45db48e239e53ab40739959f3ce31ef
Robert Strandh <robert.strandh@gmail.com>
2019-05-12 10:01:16 +0200

    Convert SYMBOL-VALUE and SET-SYMBOL-VALUE to function calls.

--- Code/Compiler/AST-to-HIR/ast-to-hir.lisp
+++ Code/Compiler/AST-to-HIR/ast-to-hir.lisp
@@ -14,6 +14,8 @@
                         :dynamic-environment-input-ast nil
                         :dynamic-environment-output-ast dynamic-environment-output-ast))
          (hir (cleavir-ast-to-hir:compile-toplevel-unhoisted wrapped-ast)))
+    (sicl-hir-transformations:convert-symbol-value hir)
+    (sicl-hir-transformations:convert-set-symbol-value hir)
     (sicl-hir-transformations:hoist-fdefinitions hir)
     (cleavir-hir-transformations::process-captured-variables hir)
     hir))
