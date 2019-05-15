2f3b3e7890ca64d526c81626a313cc57efcc425e
Robert Strandh <robert.strandh@gmail.com>
2019-05-15 20:59:16 +0200

    Muffle warnings when loading code with custom loader.

--- Code/Compiler/Extrinsic-environment/Alternative/host-load.lisp
+++ Code/Compiler/Extrinsic-environment/Alternative/host-load.lisp
@@ -6,11 +6,17 @@
 ;;; occurrences of the backquote reader macro character to generate
 ;;; macro calls to the Eclector version of the corresponding macros.
 
+(defun quiet-warning-handler (condition)
+  (let ((restart (find-restart 'muffle-warning condition)))
+    (unless (null restart)
+      (invoke-restart restart))))
+
 (defun host-load (relative-filename)
   (let ((*package* *package*)
         (filename (asdf:system-relative-pathname '#:sicl relative-filename)))
-    (with-open-file (stream filename :direction :input)
-      (loop with eof-marker = (list nil)
-            for form = (eclector.reader:read stream nil eof-marker)
-            until (eq form eof-marker)
-            do (eval form)))))
+    (handler-bind ((warning #'quiet-warning-handler))
+      (with-open-file (stream filename :direction :input)
+        (loop with eof-marker = (list nil)
+              for form = (eclector.reader:read stream nil eof-marker)
+              until (eq form eof-marker)
+              do (eval form))))))
