4486fd30a72a7c5dd1ea04afdf93e6e6c0c66195
Robert Strandh <robert.strandh@gmail.com>
2019-05-17 14:50:00 +0200

    Use CLONE-AST where possible.

--- Code/Cleavir2/AST-to-HIR/compile-fixnum-related-asts.lisp
+++ Code/Cleavir2/AST-to-HIR/compile-fixnum-related-asts.lisp
@@ -5,167 +5,157 @@
 ;;; Compile a FIXNUM-ADD-AST.
 
 (defmethod compile-ast ((ast cleavir-ast:fixnum-add-ast) context)
-  (with-accessors ((results results)
-                   (successors successors))
+  (assert-context ast context 0 2)
+  (let ((temp1 (make-temp))
+        (temp2 (make-temp))
+        (result (find-or-create-location (cleavir-ast:variable-ast ast))))
+    (compile-ast
+     (cleavir-ast:arg1-ast ast)
+     (clone-context
       context
-    (assert-context ast context 0 2)
-    (let ((temp1 (make-temp))
-          (temp2 (make-temp))
-          (result (find-or-create-location (cleavir-ast:variable-ast ast))))
+      :result temp1
+      :successor
       (compile-ast
-       (cleavir-ast:arg1-ast ast)
-       (context (list temp1)
-                (list (compile-ast
-                       (cleavir-ast:arg2-ast ast)
-                       (context (list temp2)
-                                (list (make-instance 'cleavir-ir:fixnum-add-instruction
-                                       :inputs (list temp1 temp2)
-                                       :output result
-                                       :successors successors))
-                                (invocation context))))
-                (invocation context))))))
+       (cleavir-ast:arg2-ast ast)
+       (clone-context
+        context
+        :result temp2
+        :successor
+        (make-instance 'cleavir-ir:fixnum-add-instruction
+          :inputs (list temp1 temp2)
+          :output result
+          :successors (successors context))))))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;
 ;;; Compile a FIXNUM-SUB-AST.
 
 (defmethod compile-ast ((ast cleavir-ast:fixnum-sub-ast) context)
-  (with-accessors ((results results)
-                   (successors successors))
+  (assert-context ast context 0 2)
+  (let ((temp1 (make-temp))
+        (temp2 (make-temp))
+        (result (find-or-create-location (cleavir-ast:variable-ast ast))))
+    (compile-ast
+     (cleavir-ast:arg1-ast ast)
+     (clone-context
       context
-    (assert-context ast context 0 2)
-    (let ((temp1 (make-temp))
-          (temp2 (make-temp))
-          (result (find-or-create-location (cleavir-ast:variable-ast ast))))
-      (compile-ast
-       (cleavir-ast:arg1-ast ast)
-       (context (list temp1)
-                (list (compile-ast
-                       (cleavir-ast:arg2-ast ast)
-                       (context (list temp2)
-                                (list (make-instance 'cleavir-ir:fixnum-sub-instruction
-                                       :inputs (list temp1 temp2)
-                                       :output result
-                                       :successors successors))
-                                (invocation context))))
-                (invocation context))))))
+      :result  temp1
+      :successor (compile-ast
+                  (cleavir-ast:arg2-ast ast)
+                  (clone-context
+                   context
+                   :result temp2
+                   :successor (make-instance 'cleavir-ir:fixnum-sub-instruction
+                                :inputs (list temp1 temp2)
+                                :output result
+                                :successors (successors context))))))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;
 ;;; Compile a FIXNUM-LESS-AST.
 
 (defmethod compile-ast ((ast cleavir-ast:fixnum-less-ast) context)
-  (with-accessors ((results results)
-                   (successors successors))
+  (assert-context ast context 0 2)
+  (let ((temp1 (make-temp))
+        (temp2 (make-temp)))
+    (compile-ast
+     (cleavir-ast:arg1-ast ast)
+     (clone-context
       context
-    (assert-context ast context 0 2)
-    (let ((temp1 (make-temp))
-          (temp2 (make-temp)))
-      (compile-ast
-       (cleavir-ast:arg1-ast ast)
-       (context (list temp1)
-                (list (compile-ast
-                       (cleavir-ast:arg2-ast ast)
-                       (context
-                        (list temp2)
-                        (list (make-instance 'cleavir-ir:fixnum-less-instruction
-                               :inputs (list temp1 temp2)
-                               :successors successors))
-                        (invocation context))))
-                (invocation context))))))
+      :result temp1
+      :successor (compile-ast
+                  (cleavir-ast:arg2-ast ast)
+                  (clone-context
+                   context
+                   :result temp2
+                   :successor (make-instance 'cleavir-ir:fixnum-less-instruction
+                                :inputs (list temp1 temp2)
+                                :successors (successors context))))))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;
 ;;; Compile a FIXNUM-NOT-GREATER-AST.
 
 (defmethod compile-ast ((ast cleavir-ast:fixnum-not-greater-ast) context)
-  (with-accessors ((results results)
-                   (successors successors))
+  (assert-context ast context 0 2)
+  (let ((temp1 (make-temp))
+        (temp2 (make-temp)))
+    (compile-ast
+     (cleavir-ast:arg1-ast ast)
+     (clone-context
       context
-    (assert-context ast context 0 2)
-    (let ((temp1 (make-temp))
-          (temp2 (make-temp)))
-      (compile-ast
-       (cleavir-ast:arg1-ast ast)
-       (context (list temp1)
-                (list (compile-ast
-                       (cleavir-ast:arg2-ast ast)
-                       (context
-                        (list temp2)
-                        (list (make-instance 'cleavir-ir:fixnum-not-greater-instruction
-                               :inputs (list temp1 temp2)
-                               :successors successors))
-                        (invocation context))))
-                (invocation context))))))
+      :result temp1
+      :successor (compile-ast
+                  (cleavir-ast:arg2-ast ast)
+                  (clone-context
+                   context
+                   :result temp2
+                   :successor (make-instance 'cleavir-ir:fixnum-not-greater-instruction
+                                :inputs (list temp1 temp2)
+                                :successors (successors context))))))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;
 ;;; Compile a FIXNUM-GREATER-AST.
 
 (defmethod compile-ast ((ast cleavir-ast:fixnum-greater-ast) context)
-  (with-accessors ((results results)
-                   (successors successors))
+  (assert-context ast context 0 2)
+  (let ((temp1 (make-temp))
+        (temp2 (make-temp)))
+    (compile-ast
+     (cleavir-ast:arg1-ast ast)
+     (clone-context
       context
-    (assert-context ast context 0 2)
-    (let ((temp1 (make-temp))
-          (temp2 (make-temp)))
-      (compile-ast
-       (cleavir-ast:arg1-ast ast)
-       (context (list temp1)
-                (list (compile-ast
-                       (cleavir-ast:arg2-ast ast)
-                       (context
-                        (list temp2)
-                        (list (make-instance 'cleavir-ir:fixnum-less-instruction
-                               :inputs (list temp2 temp1)
-                               :successors successors))
-                        (invocation context))))
-                (invocation context))))))
+      :result temp1
+      :successor (compile-ast
+                  (cleavir-ast:arg2-ast ast)
+                  (clone-context
+                   context
+                   :result temp2
+                   :successor (make-instance 'cleavir-ir:fixnum-less-instruction
+                                :inputs (list temp2 temp1)
+                                :successors (successors context))))))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;
 ;;; Compile a FIXNUM-NOT-LESS-AST.
 
 (defmethod compile-ast ((ast cleavir-ast:fixnum-not-less-ast) context)
-  (with-accessors ((results results)
-                   (successors successors))
+  (assert-context ast context 0 2)
+  (let ((temp1 (make-temp))
+        (temp2 (make-temp)))
+    (compile-ast
+     (cleavir-ast:arg1-ast ast)
+     (clone-context
       context
-    (assert-context ast context 0 2)
-    (let ((temp1 (make-temp))
-          (temp2 (make-temp)))
-      (compile-ast
-       (cleavir-ast:arg1-ast ast)
-       (context (list temp1)
-                (list (compile-ast
-                       (cleavir-ast:arg2-ast ast)
-                       (context
-                        (list temp2)
-                        (list (make-instance 'cleavir-ir:fixnum-not-greater-instruction
-                               :inputs (list temp2 temp1)
-                               :successors successors))
-                        (invocation context))))
-                (invocation context))))))
+      :result temp1
+      :successor (compile-ast
+                  (cleavir-ast:arg2-ast ast)
+                  (clone-context
+                   context
+                   :result temp2
+                   :successor (make-instance 'cleavir-ir:fixnum-not-greater-instruction
+                                :inputs (list temp2 temp1)
+                                :successors (successors context))))))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;
 ;;; Compile a FIXNUM-EQUAL-AST.
 
 (defmethod compile-ast ((ast cleavir-ast:fixnum-equal-ast) context)
-  (with-accessors ((results results)
-                   (successors successors))
+  (assert-context ast context 0 2)
+  (let ((temp1 (make-temp))
+        (temp2 (make-temp)))
+    (compile-ast
+     (cleavir-ast:arg1-ast ast)
+     (clone-context
       context
-    (assert-context ast context 0 2)
-    (let ((temp1 (make-temp))
-          (temp2 (make-temp)))
-      (compile-ast
-       (cleavir-ast:arg1-ast ast)
-       (context (list temp1)
-                (list (compile-ast
-                       (cleavir-ast:arg2-ast ast)
-                       (context
-                        (list temp2)
-                        (list (make-instance 'cleavir-ir:fixnum-equal-instruction
-                               :inputs (list temp2 temp1)
-                               :successors successors))
-                        (invocation context))))
-                (invocation context))))))
+      :result temp1
+      :successor  (compile-ast
+                   (cleavir-ast:arg2-ast ast)
+                   (clone-context
+                    context
+                    :result temp2
+                    :successor (make-instance 'cleavir-ir:fixnum-equal-instruction
+                                 :inputs (list temp2 temp1)
+                                 :successors (successors context))))))))
