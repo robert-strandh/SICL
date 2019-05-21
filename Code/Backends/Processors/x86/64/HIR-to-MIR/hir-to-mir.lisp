f089ede1b6077c8ecf299413be3f0727f1189588
Robert Strandh <robert.strandh@gmail.com>
2019-05-21 17:19:00 +0200

    Replace the original instruction by the translation in the graph.

--- Code/Backends/Processors/x86/64/HIR-to-MIR/hir-to-mir.lisp
+++ Code/Backends/Processors/x86/64/HIR-to-MIR/hir-to-mir.lisp
@@ -2,8 +2,15 @@
 
 (defgeneric hir-to-mir (client initial-instruction))
 
+(defun replace-instruction (client instruction)
+  (let ((replacement (translate client instruction)))
+    (unless (eq replacement instruction)
+      (loop for predecessor in (cleavir-ir:predecessors instruction)
+            do (nsubstitute replacement instruction
+                            (cleavir-ir:successors predecessor))))))
+
 (defmethod hir-to-mir ((client sicl-client:sicl-x86-64) initial-instruction)
   (let ((*translation-table* (make-hash-table :test #'eq)))
     (cleavir-ir:map-instructions-arbitrary-order
-     (lambda (instruction) (translate client instruction))
+     (lambda (instruction) (replace-instruction client instruction))
      initial-instruction)))
