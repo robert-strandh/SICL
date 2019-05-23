98ca51fc74f9afe7172caa9166cf6ec37229f5a6
Robert Strandh <robert.strandh@gmail.com>
2019-05-23 15:15:58 +0200

    Untabify for better tool support.

--- Code/Cleavir2/CST-to-AST/environment-query.lisp
+++ Code/Cleavir2/CST-to-AST/environment-query.lisp
@@ -3,34 +3,34 @@
 (defun function-info (lexical-environment function-name)
   (let ((result (cleavir-env:function-info lexical-environment function-name)))
     (loop while (null result)
-	  do (restart-case (error 'cleavir-env:no-function-info
-				  :name function-name)
-	       (consider-global ()
-		 :report (lambda (stream)
-			   (format stream
-				   "Treat it as the name of a global function."))
-		 (return-from function-info
-		   (make-instance 'cleavir-env:global-function-info
-		     :name function-name)))
-	       (substitute (new-function-name)
-		 :report (lambda (stream)
-			   (format stream "Substitute a different name."))
-		 :interactive (lambda ()
-				(format *query-io* "Enter new name: ")
-				(list (read *query-io*)))
-		 (setq result (cleavir-env:function-info lexical-environment new-function-name)))))
+          do (restart-case (error 'cleavir-env:no-function-info
+                                  :name function-name)
+               (consider-global ()
+                 :report (lambda (stream)
+                           (format stream
+                                   "Treat it as the name of a global function."))
+                 (return-from function-info
+                   (make-instance 'cleavir-env:global-function-info
+                     :name function-name)))
+               (substitute (new-function-name)
+                 :report (lambda (stream)
+                           (format stream "Substitute a different name."))
+                 :interactive (lambda ()
+                                (format *query-io* "Enter new name: ")
+                                (list (read *query-io*)))
+                 (setq result (cleavir-env:function-info lexical-environment new-function-name)))))
     result))
 
 (defun tag-info (lexical-environment tag-name)
   (let ((result (cleavir-env:tag-info lexical-environment tag-name)))
     (loop while (null result)
-	  do (restart-case (error 'cleavir-env:no-tag-info
-				  :name tag-name)
-	       (substitute (new-tag-name)
-		 :report (lambda (stream)
-			   (format stream "Substitute a different name."))
-		 :interactive (lambda ()
-				(format *query-io* "Enter new name: ")
-				(list (read *query-io*)))
-		 (setq result (cleavir-env:tag-info lexical-environment new-tag-name)))))
+          do (restart-case (error 'cleavir-env:no-tag-info
+                                  :name tag-name)
+               (substitute (new-tag-name)
+                 :report (lambda (stream)
+                           (format stream "Substitute a different name."))
+                 :interactive (lambda ()
+                                (format *query-io* "Enter new name: ")
+                                (list (read *query-io*)))
+                 (setq result (cleavir-env:tag-info lexical-environment new-tag-name)))))
     result))
