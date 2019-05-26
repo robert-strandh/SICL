9e1c082e838c587366f1ac4fce979720900114e5
Robert Strandh <robert.strandh@gmail.com>
2019-05-26 15:23:46 +0200

    Redefine EVAL as an ordinary function calling CST-EVAL.

--- Code/Compiler/HIR-to-CL/Test/eval.lisp
+++ Code/Compiler/HIR-to-CL/Test/eval.lisp
@@ -1,13 +1,5 @@
 (cl:in-package #:sicl-hir-to-cl-test)
 
-(defgeneric eval (client form environment))
-
-(defmethod eval (client form environment)
-  (let* ((cst (cst:cst-from-expression form))
-         (ast (let ((cleavir-cst-to-ast::*origin* nil))
-                (cleavir-cst-to-ast:cst-to-ast client cst environment)))
-         (hir (sicl-ast-to-hir:ast-to-hir ast))
-         (cl (sicl-hir-to-cl:hir-to-cl client hir))
-         (fun (compile nil cl))
-         (sicl-hir-to-cl::*dynamic-environment* '()))
-    (funcall fun (sicl-hir-to-cl::function-finder environment))))
+(defun eval (client form environment)
+  (let ((cst (cst:cst-from-expression form)))
+    (sicl-hir-to-cl:cst-eval client cst environment)))
