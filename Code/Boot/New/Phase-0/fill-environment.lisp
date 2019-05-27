4ef1b238fe6fc0860ca55ea3fbd906f6ed947162
Robert Strandh <robert.strandh@gmail.com>
2019-05-27 09:55:13 +0200

    Call function for defining backquote macro from environment fill function.

--- Code/Boot/New/Phase-0/fill-environment.lisp
+++ Code/Boot/New/Phase-0/fill-environment.lisp
@@ -3,4 +3,5 @@
 (defun fill-environment (client environment)
   (declare (ignore client))
   (import-from-host environment)
-  (define-defmacro environment))
+  (define-defmacro environment)
+  (define-backquote-macros environment))
