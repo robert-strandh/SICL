de7b84eca65b07e3e9fe51b2c84ba72650d9e83a
Robert Strandh <robert.strandh@gmail.com>
2019-05-28 15:27:09 +0200

    Use the environment from the lambda list.

--- Code/Evaluation-and-compilation/defmacro-defmacro.lisp
+++ Code/Evaluation-and-compilation/defmacro-defmacro.lisp
@@ -1,6 +1,6 @@
 (cl:in-package #:sicl-evaluation-and-compilation)
 
-(defmacro defmacro (name lambda-list &body body)
+(defmacro defmacro (&environment environment name lambda-list &body body)
   `(eval-when (:compile-toplevel :load-toplevel :execute)
-     (setf (sicl-env:macro-function ',name sicl-env:*global-environment*)
+     (setf (sicl-env:macro-function ',name ,environment)
 	   ,(cleavir-code-utilities:parse-macro name lambda-list body))))
