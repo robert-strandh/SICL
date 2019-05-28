0ee2c43629419010a645bc2c1badd579ddc0d70c
Robert Strandh <robert.strandh@gmail.com>
2019-05-28 16:14:33 +0200

    Define macro DEFMACRO in the global environment.

--- Code/Evaluation-and-compilation/defmacro-defmacro.lisp
+++ Code/Evaluation-and-compilation/defmacro-defmacro.lisp
@@ -1,6 +1,6 @@
 (cl:in-package #:sicl-evaluation-and-compilation)
 
-(defmacro defmacro (&environment environment name lambda-list &body body)
+(defmacro defmacro (name lambda-list &body body)
   `(eval-when (:compile-toplevel :load-toplevel :execute)
-     (setf (sicl-env:macro-function ',name ,environment)
+     (setf (sicl-env:macro-function ',name (sicl-genv:global-environment))
 	   ,(cleavir-code-utilities:parse-macro name lambda-list body))))
