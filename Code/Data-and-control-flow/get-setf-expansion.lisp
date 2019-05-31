4b07dc50bd0ab13e8c5b8a6e2710d6aa741f6fe5
Robert Strandh <robert.strandh@gmail.com>
2019-05-31 07:16:27 +0200

    Replace reference to package CLEAVIR-ENV by reference to package TRUCLER.

--- Code/Data-and-control-flow/get-setf-expansion.lisp
+++ Code/Data-and-control-flow/get-setf-expansion.lisp
@@ -1,5 +1,5 @@
 (in-package #:sicl-data-and-control-flow)
 
 (defun get-setf-expansion (place &optional environment)
-  (let ((global-env (cleavir-env:global-environment environment)))
+  (let ((global-env (trucler:global-environment environment)))
     (sicl-global-environment:get-setf-expansion place global-env)))
