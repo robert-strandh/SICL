409924a787d43d7d0959dcaf427c290428aeb015
Robert Strandh <robert.strandh@gmail.com>
2019-06-01 16:20:46 +0200

    Call GLOBAL-ENVIRONMENT in TRUCLER-REFERENCE.

--- Code/Data-and-control-flow/get-setf-expansion.lisp
+++ Code/Data-and-control-flow/get-setf-expansion.lisp
@@ -1,5 +1,7 @@
 (in-package #:sicl-data-and-control-flow)
 
 (defun get-setf-expansion (place &optional environment)
-  (let ((global-env (trucler:global-environment environment)))
+  (let ((global-env (if (null environment)
+                        (sicl-genv:global-environment)
+                        (trucler-reference:global-environment environment))))
     (sicl-global-environment:get-setf-expansion place global-env)))
