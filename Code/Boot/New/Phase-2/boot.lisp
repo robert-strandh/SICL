c09ac903fcfc0db15d60d98967aebf83c9d92b30
Robert Strandh <robert.strandh@gmail.com>
2019-06-04 17:27:35 +0200

    Call functions defined in recently added components.

--- Code/Boot/New/Phase-2/boot.lisp
+++ Code/Boot/New/Phase-2/boot.lisp
@@ -1,6 +1,10 @@
 (cl:in-package #:sicl-boot-phase-2)
 
 (defun boot (boot)
+  (format *trace-output* "Start phase 2~%")
   (with-accessors ((e2 sicl-boot:e2) (e3 sicl-boot:e3)) boot
     (change-class e2 'environment)
-    (enable-defgeneric e3)))
+    (set-up-environments boot)
+    (enable-defgeneric e3)
+    (load-accessor-defgenerics e3)
+    (enable-defclass boot)))
