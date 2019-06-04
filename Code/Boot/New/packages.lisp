da74501cc39aed44755cd282f5569eb606c648aa
Robert Strandh <robert.strandh@gmail.com>
2019-06-04 13:18:20 +0200

    Export names of new utility functions and of BOOT class and its accessors.

--- Code/Boot/New/packages.lisp
+++ Code/Boot/New/packages.lisp
@@ -2,4 +2,9 @@
 
 (defpackage #:sicl-boot
   (:use #:common-lisp)
-  (:export #:environment))
+  (:export #:environment
+           #:boot
+           #:e0 #:e1 #:e2 #:e3 #:e4 #:e5 #:e6
+           #:import-function-from-host
+           #:import-package-from-host
+           #:import-class-from-host))
