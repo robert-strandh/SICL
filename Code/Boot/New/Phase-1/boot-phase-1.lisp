6ee1347bd163d561d5f2bf883f0fa4cd6ad04e70
Robert Strandh <robert.strandh@gmail.com>
2019-06-04 13:19:13 +0200

    Define metaclass for MOP classes in environment E2.

--- Code/Boot/New/Phase-1/boot-phase-1.lisp
+++ Code/Boot/New/Phase-1/boot-phase-1.lisp
@@ -1,1 +1,27 @@
 (cl:in-package #:sicl-boot-phase-1)
+
+;;; This class will be the metaclass of all the MOP classes defined in
+;;; E2.  We define it as a subclass of the host
+;;; FUNCALLABLE-STANDARD-CLASS so that instances of it can be used as
+;;; functions, and so that we avoid any problems with incompatible
+;;; superclasses and metaclasses.
+;;;
+;;; Furthermore, this class will be the specializer of the :AROUND
+;;; method on SHARED-INITIALIZE that handles the class-initialization
+;;; protocol.  Recall that this :AROUND method handles the definitions
+;;; of slot readers and slot writers defined in class slots.
+;;; Therefore, that aspect of the host class-initialization protocol
+;;; will not be invoked.
+(defclass funcallable-standard-class
+    (closer-mop:funcallable-standard-class)
+  ())
+
+(defmethod print-object ((object funcallable-standard-class) stream)
+  (format stream "<E2 host class ~a>" (class-name object)))
+
+;;; This method is apparently necessary so that we are allowed to
+;;; make instances of our new class.
+(defmethod closer-mop:validate-superclass
+    ((class funcallable-standard-class)
+     (superclass closer-mop:funcallable-standard-class))
+  t)
