e533d0dd65bec8f46a5a72f98db035323ff5771e
Robert Strandh <robert.strandh@gmail.com>
2019-06-04 14:26:37 +0200

    Print phase start to *TRACE-OUTPUT*.

--- Code/Boot/New/Phase-0/boot.lisp
+++ Code/Boot/New/Phase-0/boot.lisp
@@ -1,6 +1,7 @@
 (cl:in-package #:sicl-boot-phase-0)
 
 (defun boot (boot)
+  (format *trace-output* "Start phase 0~%")
   (let ((environment (make-instance 'environment))
         (client (make-instance 'client)))
     (setf (sicl-boot:e0 boot) environment)
