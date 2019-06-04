df14a1ef245bc70f62a4d717127aa2b236a84fc5
Robert Strandh <robert.strandh@gmail.com>
2019-06-04 11:22:03 +0200

    Inherit from SICL-BOOT:ENVIRONMENT instead of directly from alternative environment.

--- Code/Boot/New/Phase-0/environment.lisp
+++ Code/Boot/New/Phase-0/environment.lisp
@@ -1,7 +1,4 @@
 (cl:in-package #:sicl-boot-phase-0)
 
-(defclass environment (sicl-alternative-extrinsic-environment:environment)
+(defclass environment (sicl-boot:environment)
   ())
-
-(defmethod trucler-reference:global-environment ((environment environment))
-  environment)
