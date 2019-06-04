4794387d8e306b98e9e0d2d4be482dbdaef5e4cd
Robert Strandh <robert.strandh@gmail.com>
2019-06-04 13:42:05 +0200

    Define a parameter holding the boot class instance.

--- Code/Boot/New/Phase-0/boot.lisp
+++ Code/Boot/New/Phase-0/boot.lisp
@@ -1,7 +1,8 @@
 (cl:in-package #:sicl-boot-phase-0)
 
-(defun boot ()
+(defun boot (boot)
   (let ((environment (make-instance 'environment))
         (client (make-instance 'client)))
+    (setf (sicl-boot:e0 boot) environment)
     (fill-environment client environment)
     (compile-files client environment)))
