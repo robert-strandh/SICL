af97b21a8d1840a85742fc1ba2ef37f5850fe906
Robert Strandh <robert.strandh@gmail.com>
2019-06-04 14:53:01 +0200

    Create all the environments when the BOOT class is instantiated.

--- Code/Boot/New/boot.lisp
+++ Code/Boot/New/boot.lisp
@@ -1,7 +1,14 @@
 (cl:in-package #:sicl-boot)
 
 (defun boot ()
-  (let ((boot (make-instance 'boot)))
+  (let ((boot (make-instance 'boot
+                :e0 (make-instance 'environment)
+                :e1 (make-instance 'environment)
+                :e2 (make-instance 'environment)
+                :e3 (make-instance 'environment)
+                :e4 (make-instance 'environment)
+                :e5 (make-instance 'environment)
+                :e6 (make-instance 'environment))))
     (sicl-boot-phase-0:boot boot)
     (sicl-boot-phase-1:boot boot)
     boot))
