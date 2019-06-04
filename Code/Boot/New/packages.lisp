c2ade670004d2a6ac6771bbf6317bac8fe0066dd
Robert Strandh <robert.strandh@gmail.com>
2019-06-04 17:34:24 +0200

    Export name of function LOAD-FASL.

--- Code/Boot/New/packages.lisp
+++ Code/Boot/New/packages.lisp
@@ -7,4 +7,5 @@
            #:e0 #:e1 #:e2 #:e3 #:e4 #:e5 #:e6
            #:import-function-from-host
            #:import-package-from-host
-           #:import-class-from-host))
+           #:import-class-from-host
+           #:load-fasl))
