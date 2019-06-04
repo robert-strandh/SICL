7771c8dd7fd2ab4d71dfc590ca1818eef04c5263
Robert Strandh <robert.strandh@gmail.com>
2019-06-04 15:35:26 +0200

    Call function for enabling definition of generic function from main entry point.

--- Code/Boot/New/Phase-2/boot.lisp
+++ Code/Boot/New/Phase-2/boot.lisp
@@ -1,5 +1,6 @@
 (cl:in-package #:sicl-boot-phase-2)
 
 (defun boot (boot)
-  (with-accessors ((e2 sicl-boot:e2)) boot
-    (change-class e2 'environment)))
+  (with-accessors ((e2 sicl-boot:e2) (e3 sicl-boot:e3)) boot
+    (change-class e2 'environment)
+    (enable-defgeneric e3)))
