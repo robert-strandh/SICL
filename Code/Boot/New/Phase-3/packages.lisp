4db6c3f5262adf02dce0bfd07bf35198ec988b4c
Robert Strandh <robert.strandh@gmail.com>
2019-06-05 15:00:37 +0200

    Import functions LOAD-FASL and IMPORT-FUNCTION-FROM-HOST from SICL-BOOT.

--- Code/Boot/New/Phase-3/packages.lisp
+++ Code/Boot/New/Phase-3/packages.lisp
@@ -2,4 +2,7 @@
 
 (defpackage #:sicl-boot-phase-3
   (:use #:common-lisp)
+  (:import-from #:sicl-boot
+                #:load-fasl
+                #:import-function-from-host)
   (:export #:boot))
