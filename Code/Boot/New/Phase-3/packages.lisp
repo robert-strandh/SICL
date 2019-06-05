462c29ad6f9aa914dd271ca33ef1dddb8d9fe600
Robert Strandh <robert.strandh@gmail.com>
2019-06-05 17:10:46 +0200

    Import function IMPORT-FUNCTIONS-FROM-HOST from SICL-BOOT.

--- Code/Boot/New/Phase-3/packages.lisp
+++ Code/Boot/New/Phase-3/packages.lisp
@@ -4,5 +4,6 @@
   (:use #:common-lisp)
   (:import-from #:sicl-boot
                 #:load-fasl
-                #:import-function-from-host)
+                #:import-function-from-host
+                #:import-functions-from-host)
   (:export #:boot))
