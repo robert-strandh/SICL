47aa267462ba42fb21cccf604ee9fbed714f0933
Robert Strandh <robert.strandh@gmail.com>
2019-06-05 16:33:07 +0200

    Call function for defining MAKE-INSTANCE.

--- Code/Boot/New/Phase-3/boot.lisp
+++ Code/Boot/New/Phase-3/boot.lisp
@@ -4,4 +4,5 @@
   (format *trace-output* "Start phase 3~%")
   (with-accessors ((e2 sicl-boot:e2) (e3 sicl-boot:e3)) boot
     (change-class e3 'environment)
-    (set-up-environments boot)))
+    (set-up-environments boot)
+    (define-make-instance boot)))
