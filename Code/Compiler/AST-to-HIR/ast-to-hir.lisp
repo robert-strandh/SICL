c7385b35e0c68d6f612d0fc86d67362a75dd0c52
Robert Strandh <robert.strandh@gmail.com>
2019-06-05 10:19:32 +0200

    Hoist LOAD-TIME-VALUE-ASTs before wrapping the result.

--- Code/Compiler/AST-to-HIR/ast-to-hir.lisp
+++ Code/Compiler/AST-to-HIR/ast-to-hir.lisp
@@ -6,9 +6,10 @@
   ;; name, returns the function cell from the environment the code
   ;; will be loaded into.
   (let* ((cleavir-cst-to-ast::*origin* nil)
+         (hoisted-ast (cleavir-ast-transformations:hoist-load-time-value ast))
          (wrapped-ast (make-instance 'cleavir-ast:function-ast
                         :lambda-list '()
-                        :body-ast ast))
+                        :body-ast hoisted-ast))
          (hir (cleavir-ast-to-hir:compile-toplevel-unhoisted wrapped-ast)))
     (sicl-hir-transformations:convert-symbol-value hir)
     (sicl-hir-transformations:convert-set-symbol-value hir)
