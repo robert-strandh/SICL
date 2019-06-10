6d91ec3ac0d84c2a1f679dcb6ffd88f08d7b9676
Robert Strandh <robert.strandh@gmail.com>
2019-06-10 10:10:48 +0200

    Provide method on CLEAVIR-CST-TO-AST:EVAL.

--- Code/Boot/New/Phase-0/eval.lisp
+++ Code/Boot/New/Phase-0/eval.lisp
@@ -2,3 +2,7 @@
 
 (defmethod cleavir-cst-to-ast:cst-eval ((client client) cst environment)
   (sicl-hir-to-cl:cst-eval client cst environment))
+
+(defmethod cleavir-cst-to-ast:eval ((client client) form environment)
+  (let ((cst (cst:cst-from-expression form)))
+    (sicl-hir-to-cl:cst-eval client cst environment)))
