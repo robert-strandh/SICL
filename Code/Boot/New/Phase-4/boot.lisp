981056df33a04792d4894394ee3cc99ab8fe73d2
Robert Strandh <robert.strandh@gmail.com>
2019-06-11 15:33:30 +0200

    Remove useless accessor.

--- Code/Boot/New/Phase-4/boot.lisp
+++ Code/Boot/New/Phase-4/boot.lisp
@@ -3,8 +3,7 @@
 (defun boot (boot)
   (format *trace-output* "Start of phase 4~%")
   (with-accessors ((e3 sicl-boot:e3)
-                   (e4 sicl-boot:e4)
-                   (e5 sicl-boot:e6))
+                   (e4 sicl-boot:e4))
       boot
     (change-class e4 'environment)
     (enable-class-finalization boot)
