c90fcf7cc0f3f2f5c162deb6ef1f2ddaaaa42e60
Robert Strandh <robert.strandh@gmail.com>
2019-06-13 11:27:02 +0200

    Rename variable LEXICAL-ENVIRONMENT to ENVIRONMENT.

--- Code/Cleavir2/CST-to-AST/convert-setq.lisp
+++ Code/Cleavir2/CST-to-AST/convert-setq.lisp
@@ -9,23 +9,23 @@
 ;;; value is not needed.  We do that by wrapping a PROGN around it.
 
 (defmethod convert-setq
-    (client var-cst form-cst (info trucler:constant-variable-description) lexical-environment)
-  (declare (ignore var-cst lexical-environment client))
+    (client var-cst form-cst (info trucler:constant-variable-description) environment)
+  (declare (ignore var-cst environment client))
   (error 'setq-constant-variable :cst form-cst))
 
 (defmethod convert-setq
-    (client var-cst form-cst (info trucler:lexical-variable-description) lexical-environment)
+    (client var-cst form-cst (info trucler:lexical-variable-description) environment)
   (process-progn 
    (list (cleavir-ast:make-ast 'cleavir-ast:setq-ast
           :lhs-ast (trucler:identity info)
-          :value-ast (convert client form-cst lexical-environment))
+          :value-ast (convert client form-cst environment))
          (trucler:identity info))))
 
 (defmethod convert-setq
-    (client var-cst form-cst (info trucler:symbol-macro-description) lexical-environment)
+    (client var-cst form-cst (info trucler:symbol-macro-description) environment)
   (let* ((expansion (trucler:expansion info))
          (expander (symbol-macro-expander expansion))
-         (expanded-variable (expand-macro expander var-cst lexical-environment))
+         (expanded-variable (expand-macro expander var-cst environment))
          (expanded-cst (cst:reconstruct expanded-variable var-cst client))
          (origin (cst:source var-cst)))
     (setf (cst:source expanded-cst) origin)
@@ -33,7 +33,7 @@
              (cst:cons (make-atom-cst 'setf origin)
                        (cst:list expanded-cst form-cst)
                        :source origin)
-             lexical-environment)))
+             environment)))
 
 (defmethod convert-setq-special-variable
     (client var-cst form-ast info global-env)
@@ -51,17 +51,17 @@
            temp))))
 
 (defmethod convert-setq
-    (client var-cst form-cst (info trucler:special-variable-description) lexical-environment)
-  (let ((global-env (trucler:global-environment client lexical-environment)))
+    (client var-cst form-cst (info trucler:special-variable-description) environment)
+  (let ((global-env (trucler:global-environment client environment)))
     (convert-setq-special-variable client
                                    var-cst
-                                   (convert client form-cst lexical-environment)
+                                   (convert client form-cst environment)
                                    info
                                    global-env)))
 
-(defun convert-elementary-setq (client var-cst form-cst lexical-environment)
+(defun convert-elementary-setq (client var-cst form-cst environment)
   (let* ((symbol (cst:raw var-cst))
-         (info (trucler:describe-variable client lexical-environment symbol)))
+         (info (trucler:describe-variable client environment symbol)))
     (loop while (null info)
           do (restart-case (error 'trucler:no-variable-description
                                   :name symbol
@@ -85,5 +85,5 @@
                  :interactive (lambda ()
                                 (format *query-io* "Enter new name: ")
                                 (list (read *query-io*)))
-                 (setq info (trucler:describe-variable client lexical-environment new-symbol)))))
-    (convert-setq client var-cst form-cst info lexical-environment)))
+                 (setq info (trucler:describe-variable client environment new-symbol)))))
+    (convert-setq client var-cst form-cst info environment)))
