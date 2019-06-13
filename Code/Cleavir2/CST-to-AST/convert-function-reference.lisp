26d72d0333766dc7e115ac2b6b96a0381df0fcad
Robert Strandh <robert.strandh@gmail.com>
2019-06-13 07:06:29 +0200

    Improve code layout.

--- Code/Cleavir2/CST-to-AST/convert-function-reference.lisp
+++ Code/Cleavir2/CST-to-AST/convert-function-reference.lisp
@@ -1,48 +1,33 @@
 (cl:in-package #:cleavir-cst-to-ast)
 
-(defmethod convert-global-function-reference (client
-                                              cst
-                                              info
-                                              global-env)
-  (declare (ignore global-env))
+(defmethod convert-global-function-reference (client cst info global-environment)
+  (declare (ignore global-environment))
   (let* ((*origin* (cst:source cst))
          (name-ast (cleavir-ast:make-ast 'cleavir-ast:constant-ast
                      :value (trucler:name info))))
     (cleavir-ast:make-ast 'cleavir-ast:fdefinition-ast
       :name-ast name-ast)))
 
-(defmethod convert-function-reference (client
-                                       cst
-                                       (info trucler:global-function-description)
-                                       lexical-environment)
-  (convert-global-function-reference client
-                                     cst
-                                     info
-                                     (trucler:global-environment client lexical-environment)))
+(defmethod convert-function-reference
+    (client cst (info trucler:global-function-description) environment)
+  (convert-global-function-reference
+   client cst info (trucler:global-environment client environment)))
 
-(defmethod convert-function-reference (client
-                                       cst
-                                       (info trucler:local-function-description)
-                                       lexical-environment)
-  (declare (ignore client lexical-environment))
+(defmethod convert-function-reference
+    (client cst (info trucler:local-function-description) environment)
+  (declare (ignore client environment))
   (trucler:identity info))
 
-(defmethod convert-function-reference (client
-                                       cst
-                                       (info trucler:global-macro-description)
-                                       lexical-environment)
+(defmethod convert-function-reference
+    (client cst (info trucler:global-macro-description) environment)
   (error 'function-name-names-global-macro :cst cst))
 
-(defmethod convert-function-reference (client
-                                       cst
-                                       (info trucler:local-macro-description)
-                                       lexical-environment)
+(defmethod convert-function-reference
+    (client cst (info trucler:local-macro-description) environment)
   (error 'function-name-names-local-macro :cst cst))
 
-(defmethod convert-function-reference (client
-                                       cst
-                                       (info trucler:special-operator-description)
-                                       lexical-environment)
+(defmethod convert-function-reference
+    (client cst (info trucler:special-operator-description) environment)
   (error 'function-name-names-special-operator :cst cst))
 
 ;;; These are used by (foo ...) forms.
@@ -51,42 +36,29 @@
 ;;; of type UNDEFINED-FUNCTION, allowing an fboundp check to be skipped.
 ;;; Other than the inlining, they by default have the same behavior.
 
-(defmethod convert-called-function-reference (client
-                                              cst
-                                              info
-                                              lexical-environment)
+(defmethod convert-called-function-reference (client cst info environment)
   (when (not (eq (trucler:inline info) 'cl:notinline))
     (let ((ast (trucler:inline-data info)))
       (when ast
         (return-from convert-called-function-reference
           ;; The AST must be cloned because hoisting is destructive.
           (cleavir-ast-transformations:clone-ast ast)))))
-  (convert-global-function-reference client
-                                     cst
-                                     info
-                                     (trucler:global-environment client lexical-environment)))
+  (convert-global-function-reference
+   client cst info (trucler:global-environment client environment)))
 
-(defmethod convert-called-function-reference (client
-                                              cst
-                                              (info trucler:local-function-description)
-                                              lexical-environment)
-  (declare (ignore client lexical-environment))
+(defmethod convert-called-function-reference
+    (client cst (info trucler:local-function-description) environment)
+  (declare (ignore client environment))
   (trucler:identity info))
 
-(defmethod convert-called-function-reference (client
-                                              cst
-                                              (info trucler:global-macro-description)
-                                              lexical-environment)
+(defmethod convert-called-function-reference
+    (client cst (info trucler:global-macro-description) environment)
   (error 'function-name-names-global-macro :cst cst))
 
-(defmethod convert-called-function-reference (client
-                                              cst
-                                              (info trucler:local-macro-description)
-                                              lexical-environment)
+(defmethod convert-called-function-reference
+    (client cst (info trucler:local-macro-description) environment)
   (error 'function-name-names-local-macro :cst cst))
 
-(defmethod convert-called-function-reference (client
-                                              cst
-                                              (info trucler:special-operator-description)
-                                              lexical-environment)
+(defmethod convert-called-function-reference
+    (client cst (info trucler:special-operator-description) environment)
   (error 'function-name-names-special-operator :cst cst))
