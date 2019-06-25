e78b7c4c8c3cc8d70b80251b3db63f9bf93fbbad
Robert Strandh <robert.strandh@gmail.com>
2019-06-25 11:11:17 +0200

    Remove obsolete version of function GET-PROPERTIES.

--- Code/Cons-high/cons-high.lisp
+++ Code/Cons-high/cons-high.lisp
@@ -4003,26 +4003,3 @@
 				 ,writer-form))
 		 ,value-var)
 	      `(getf ,reader-form ,indicator-var ,default)))))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;;
-;;; Function get-properties
-
-(defun get-properties (plist indicator-list)
-  (unless (typep plist 'list)
-    (error 'must-be-property-list
-	   :datum plist
-	   :name 'getf))
-  (loop for rest on plist by #'cddr
-	do (unless (consp (cdr rest))
-	     (error 'must-be-property-list
-		    :datum plist
-		    :name 'getf))
-	   (let ((temp (member (car rest) indicator-list :test #'eq)))
-	     (unless (null temp)
-	       (return-from get-properties (values (car temp) (cadr rest) rest))))
-	finally (unless (null rest)
-		  (error 'must-be-property-list
-			 :datum plist
-			 :name 'getf))
-		(return (values nil nil nil))))
