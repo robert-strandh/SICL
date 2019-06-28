ed178d92b71f62fc8fbc9577ebce8f235ba84ad8
Robert Strandh <robert.strandh@gmail.com>
2019-06-28 10:00:54 +0200

    Define a version of LOAD-FASL that loads host FASL instead of SICL FASL.

--- Code/Boot/New/utilities.lisp
+++ Code/Boot/New/utilities.lisp
@@ -1,15 +1,26 @@
 (cl:in-package #:sicl-boot)
 
+;; (defun load-fasl (relative-pathname environment)
+;;   (format *trace-output* "Loading file ~s~%" relative-pathname)
+;;   (let* ((prefixed (concatenate 'string "ASTs/" relative-pathname))
+;;          (pathname (asdf:system-relative-pathname '#:sicl-new-boot prefixed))
+;;          (ast (cleavir-io:read-model pathname '(v0)))
+;;          (hir (sicl-ast-to-hir:ast-to-hir ast))
+;;          (cl (sicl-hir-to-cl:hir-to-cl nil hir))
+;;          (fun (compile nil cl))
+;;          (sicl-hir-to-cl:*dynamic-environment* '()))
+;;     (funcall fun (sicl-hir-to-cl:make-function-cell-finder environment))))
+
+(defvar *top-level-function*)
+
 (defun load-fasl (relative-pathname environment)
   (format *trace-output* "Loading file ~s~%" relative-pathname)
-  (let* ((prefixed (concatenate 'string "ASTs/" relative-pathname))
+  (let* ((prefixed (concatenate 'string "Host-FASLs/" relative-pathname))
          (pathname (asdf:system-relative-pathname '#:sicl-new-boot prefixed))
-         (ast (cleavir-io:read-model pathname '(v0)))
-         (hir (sicl-ast-to-hir:ast-to-hir ast))
-         (cl (sicl-hir-to-cl:hir-to-cl nil hir))
-         (fun (compile nil cl))
          (sicl-hir-to-cl:*dynamic-environment* '()))
-    (funcall fun (sicl-hir-to-cl:make-function-cell-finder environment))))
+    (load pathname)
+    (funcall *top-level-function*
+             (sicl-hir-to-cl:make-function-cell-finder environment))))
 
 (defun import-function-from-host (name environment)
   (setf (sicl-genv:fdefinition name environment)
