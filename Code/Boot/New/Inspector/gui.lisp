43c04f4d857ea71bac1bc071652f0179e3851981
Robert Strandh <robert.strandh@gmail.com>
2019-06-30 07:35:31 +0200

    Pass initarg :BOOT when application frame is made.

--- Code/Boot/New/Inspector/gui.lisp
+++ Code/Boot/New/Inspector/gui.lisp
@@ -6,7 +6,7 @@
   (:panes (application
            :application
            :scroll-bars nil
-           :display-function 'display-object
+           :display-function 'display-stack-top
            :text-style (clim:make-text-style :sans-serif :roman 12))
           (interactor :interactor :scroll-bars nil))
   (:layouts (default (clim:vertically (:width 1200 :height 900)
@@ -15,7 +15,8 @@
 
 (defun inspect (object boot &key new-process-p)
   (let ((frame (clim:make-application-frame 'inspector
-                  :object-stack (list object))))
+                 :object-stack (list object)
+                 :boot boot)))
     (flet ((run ()
              (clim:run-frame-top-level frame)))
       (if new-process-p
