1b14ddebb733091d5529dd1b002167d5185f3445
Robert Strandh <robert.strandh@gmail.com>
2019-07-01 14:42:23 +0200

    Add host object types NUMBER, CHARACTER, STRING, SYMBOL, and PACKAGE.

--- Code/Boot/New/Inspector/display-object.lisp
+++ Code/Boot/New/Inspector/display-object.lisp
@@ -9,3 +9,18 @@
 
 (defmethod display-object (object frame pane)
   (format pane "~a" (short-description object)))
+
+(defmethod display-object ((object number) frame pane)
+  (format pane "A host number~%~s~%" object))
+
+(defmethod display-object ((object character) frame pane)
+  (format pane "A host character~%~s~%" object))
+
+(defmethod display-object ((object string) frame pane)
+  (format pane "A host string~%~s~%" object))
+
+(defmethod display-object ((object symbol) frame pane)
+  (format pane "A host symbol~%~s~%" object))
+
+(defmethod display-object ((object package) frame pane)
+  (format pane "A host package~%~s~%" object))
