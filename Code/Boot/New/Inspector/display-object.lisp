dedf258fcb52b5365f0c1cc31d039bdbb4fab17c
Robert Strandh <robert.strandh@gmail.com>
2019-07-01 15:02:35 +0200

    Define method on display-object for displaying lists.

--- Code/Boot/New/Inspector/display-object.lisp
+++ Code/Boot/New/Inspector/display-object.lisp
@@ -24,3 +24,40 @@
 
 (defmethod display-object ((object package) frame pane)
   (format pane "A host package~%~s~%" object))
+
+(defun display-element (element index frame pane)
+  (declare (ignore frame))
+  (format pane "~s: ~s~%" index (short-description element)))
+
+(defun display-proper-list (object frame pane)
+  (format pane "A host proper list~%")
+  (loop for element in object
+        for i from 0
+        do (display-element element i frame pane)))
+
+(defun display-circular-list (object frame pane)
+  (format pane "A host circular list~%")
+  (loop with table = (make-hash-table :test #'eq)
+        for remaining = object then (cdr remaining)
+        for i from 0
+        until (gethash remaining table)
+        do (setf (gethash remaining table) t)
+           (display-element (car remaining) i frame pane)))
+
+(defun display-dotted-list (object frame pane)
+  (format pane "A host dotted list~%")
+  (loop for remaining = object then (cdr remaining)
+        for i from 0
+        until (atom remaining)
+        do (display-element (car remaining) i frame pane)
+        finally
+           (format pane " .~%")
+           (display-element remaining (1+ i) frame pane)))
+
+(defmethod display-object ((object cons) frame pane)
+  (cond ((proper-list-p object)
+         (display-proper-list object frame pane))
+        ((circular-list-p object)
+         (display-circular-list object frame pane))
+        (t
+         (display-dotted-list object frame pane))))
