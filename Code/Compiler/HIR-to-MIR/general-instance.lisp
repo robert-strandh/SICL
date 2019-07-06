8034df0a0489ad43fa4cbea91c124ff4b087da61
Robert Strandh <robert.strandh@gmail.com>
2019-07-06 11:23:45 +0200

    Define generic function and method for processing the SLOT-WRITE-INSTRUCTION.

--- Code/Compiler/HIR-to-MIR/general-instance.lisp
+++ Code/Compiler/HIR-to-MIR/general-instance.lisp
@@ -34,3 +34,24 @@
      (when (typep instruction 'cleavir-ir:slot-read-instruction)
        (process-slot-read-instruction client instruction )))
    initial-instruction))
+
+(defgeneric process-slot-write-instruction (client instruction))
+
+(defmethod  process-slot-write-instruction (client instruction)
+  (let ((slot-address (make-instance 'cleavir-ir:lexical-location
+                        :name '#:slot-address)))
+    (destructuring-bind (object-location slot-number value-location)
+        (cleavir-ir:inputs instruction)
+      (find-slot
+       object-location slot-number slot-address
+       (list (make-instance 'cleavir-ir:memset2-instruction
+               :offset -7
+               :inputs (list slot-address value-location)
+               :successors (cleavir-ir:successors instruction)))))))
+
+(defun process-slot-write-instructions (client initial-instruction)
+  (cleavir-ir:map-instructions-arbitrary-order
+   (lambda (instruction)
+     (when (typep instruction 'cleavir-ir:slot-write-instruction)
+       (process-slot-write-instruction client instruction )))
+   initial-instruction))
