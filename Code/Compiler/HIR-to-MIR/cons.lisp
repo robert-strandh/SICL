85d6bc09a6f57715635a41238926d051d85783b7
Robert Strandh <robert.strandh@gmail.com>
2019-07-06 11:58:38 +0200

    Define methods on PROCESS-INSTRUCTION for CDR, RPLACA, and RPLACD.

--- Code/Compiler/HIR-to-MIR/cons.lisp
+++ Code/Compiler/HIR-to-MIR/cons.lisp
@@ -7,3 +7,27 @@
                 :inputs (cleavir-ir:inputs instruction)
                 :outputs (cleavir-ir:outputs instruction)
                 :successors (cleavir-ir:successors instruction)))
+
+(defmethod process-instruction (client
+                                (instruction cleavir-ir:cdr-instruction))
+  (change-class instruction 'cleavir-ir:memref2-instruction
+                :offset 7
+                :inputs (cleavir-ir:inputs instruction)
+                :outputs (cleavir-ir:outputs instruction)
+                :successors (cleavir-ir:successors instruction)))
+
+(defmethod process-instruction (client
+                                (instruction cleavir-ir:rplaca-instruction))
+  (change-class instruction 'cleavir-ir:memset2-instruction
+                :offset -1
+                :inputs (cleavir-ir:inputs instruction)
+                :outputs (cleavir-ir:outputs instruction)
+                :successors (cleavir-ir:successors instruction)))
+
+(defmethod process-instruction (client
+                                (instruction cleavir-ir:rplacd-instruction))
+  (change-class instruction 'cleavir-ir:memset2-instruction
+                :offset 7
+                :inputs (cleavir-ir:inputs instruction)
+                :outputs (cleavir-ir:outputs instruction)
+                :successors (cleavir-ir:successors instruction)))
