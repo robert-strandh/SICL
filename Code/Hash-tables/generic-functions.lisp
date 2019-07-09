f4ef58d4e21e1a38e5761143a3de53409568732c
Robert Strandh <robert.strandh@gmail.com>
2019-07-09 11:31:11 +0200

    New component containing definition of generic functions on hash tables.

---
+++ Code/Hash-tables/generic-functions.lisp
@@ -0,0 +1,27 @@
+(cl:in-package #:sicl-hash-table)
+
+(defgeneric hash-table-p (object)
+  (:method (object) nil))
+
+(defgeneric hash-table-count (hash-table))
+
+(defgeneric hash-table-rehash-size (hash-table))
+
+(defgeneric hash-table-rehash-threshold (hash-table))
+
+(defgeneric hash-table-size (hash-table))
+
+(defgeneric hash-table-test (hash-table))
+
+(defgeneric gethash (key hash-table &optional default))
+
+(defgeneric (setf gethash) (new-value key hash-table &optional default))
+
+(defgeneric remhash (key hash-table))
+
+(defgeneric maphash (function hash-table))
+
+(defgeneric clrhash (hash-table))
+
+
+
