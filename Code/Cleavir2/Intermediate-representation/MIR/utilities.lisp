b4ea9545acea75d0cc0201e8e152f86920caf862
Robert Strandh <robert.strandh@gmail.com>
2019-07-10 17:04:18 +0200

    Define utility function BOTH, NONE, and BOTH-OR-NONE.

--- Code/Cleavir2/Intermediate-representation/MIR/utilities.lisp
+++ Code/Cleavir2/Intermediate-representation/MIR/utilities.lisp
@@ -1,2 +1,10 @@
 (cl:in-package #:cleavir-ir)
 
+(defun both (x y)
+  (and (not (null x)) (not (null y))))
+
+(defun none (x y)
+  (and (null x) (null y)))
+
+(defun both-or-none (x y)
+  (or (both x y) (none x y)))
