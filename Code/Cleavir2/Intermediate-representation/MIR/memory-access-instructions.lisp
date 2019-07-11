ee0e34e39b61c040727f888c025437a28b5d31bc
Robert Strandh <robert.strandh@gmail.com>
2019-07-11 10:54:40 +0200

    Define :AROUND method on SHARED-INITIALIZE specialized to MEMSET2-INSTRUCTION.

--- Code/Cleavir2/Intermediate-representation/MIR/memory-access-instructions.lisp
+++ Code/Cleavir2/Intermediate-representation/MIR/memory-access-instructions.lisp
@@ -92,6 +92,20 @@
 (defclass memset2-instruction (instruction one-successor-mixin)
   ((%offset :initarg :offset :reader offset)))
 
+(defmethod shared-initialize :around
+    ((instruction memset2-instruction) slot-names
+     &key
+       inputs base-address offset value
+       outputs output
+       successors successor)
+  (assert (all-or-none base-address offset value))
+  (assert (null outputs))
+  (assert (null output))
+  (let ((inputs (combine inputs base-address offset value))
+        (successors (if (null successor) successors (list successor))))
+    (call-next-method instruction slot-names
+                      :inputs inputs :outputs outputs :successors successors)))
+
 (defun make-memset2-instruction (base-address offset value &optional successor)
   (make-instance 'memset2-instruction
     :inputs (list base-address value)
