a56af11cc7664302058dcaa2503954b4d4c8f1e3
Robert Strandh <robert.strandh@gmail.com>
2019-07-11 10:41:18 +0200

    Fix incorrect comments.

--- Code/Cleavir2/Intermediate-representation/MIR/memory-access-instructions.lisp
+++ Code/Cleavir2/Intermediate-representation/MIR/memory-access-instructions.lisp
@@ -28,10 +28,12 @@
 ;;;
 ;;; MIR instruction MEMREF2-INSTRUCTION
 ;;;
-;;; This instruction loads a memory location.  It takes a single input
-;;; containing the address of the word to load.  It has a single
-;;; output which is set to the contents of the memory location at the
-;;; address specified by the input and the offset added together.
+;;; This instruction loads a memory location.  It takes two inputs.
+;;; The first input is the base-address of a location in memory.  The
+;;; second input is an offset to be added to the base address.  It has
+;;; a single output which is set to the contents of the memory
+;;; location at the address specified by the input and the offset
+;;; added together.
 
 (defclass memref2-instruction (instruction one-successor-mixin)
   ((%offset :initarg :offset :reader offset)))
@@ -55,7 +57,7 @@
 ;;;
 ;;; This instruction stores an item in a memory location.  It takes
 ;;; two inputs.  The first input is the address of a location in
-;;; memory.  The second input is the item to be stored in that
+;;; memory.  The second input is the value to be stored in that
 ;;; location.
 
 (defclass memset1-instruction (instruction one-successor-mixin)
@@ -81,8 +83,9 @@
 ;;; MIR instruction MEMSET2-INSTRUCTION
 ;;;
 ;;; This instruction stores an item in a memory location.  It takes
-;;; two inputs.  The first input is the address of a location in
-;;; memory.  The second input is the item to be stored in that
+;;; three inputs.  The first input is the base-address of a location
+;;; in memory.  The second input is an offset to be added to the base
+;;; address.  The third input is the value to be stored in that
 ;;; location.  This instruction adds the input address to the offset
 ;;; and stores the input in the resulting address.
 
