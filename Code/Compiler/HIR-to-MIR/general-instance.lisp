1eeac3eea221176eb42abf84c3aa0f9fc56b610e
Robert Strandh <robert.strandh@gmail.com>
2019-07-11 16:36:31 +0200

    Rename variables holding locations for better clarity.

--- Code/Compiler/HIR-to-MIR/general-instance.lisp
+++ Code/Compiler/HIR-to-MIR/general-instance.lisp
@@ -1,43 +1,43 @@
 (cl:in-package #:sicl-hir-to-mir)
 
 (defun find-slot (object-location slot-number slot-address instruction)
-  (let ((rack-address (make-instance 'cleavir-ir:lexical-location
-                        :name '#:rack-address)))
+  (let ((rack-location (make-instance 'cleavir-ir:lexical-location
+                        :name '#:rack-location)))
     (cleavir-ir:insert-instruction-before
      (make-instance 'cleavir-ir:memref2-instruction
        :offset 3
        :inputs (list object-location)
-       :outputs (list rack-address)
+       :outputs (list rack-location)
        :successors (list instruction))
      instruction)
     (cleavir-ir:insert-instruction-before
      (make-instance 'cleavir-ir:unsigned-add-instruction
-       :inputs (list rack-address slot-number)
-       :outputs (list slot-address)
+       :inputs (list rack-location slot-number)
+       :outputs (list slot-location)
        :successors (list instruction))
      instruction)))
 
 (defmethod  process-instruction (client
                                  (instruction cleavir-ir:slot-read-instruction))
-  (let ((slot-address (make-instance 'cleavir-ir:lexical-location
-                        :name '#:slot-address)))
+  (let ((slot-location (make-instance 'cleavir-ir:lexical-location
+                        :name '#:slot-location)))
     (destructuring-bind (object-location slot-number)
         (cleavir-ir:inputs instruction)
-      (find-slot object-location slot-number slot-address instruction)
+      (find-slot object-location slot-number slot-location instruction)
       (change-class instruction 'cleavir-ir:memref2-instruction
                     :offset -7
-                    :inputs (list slot-address)
+                    :inputs (list slot-location)
                     :outputs (cleavir-ir:outputs instruction)
                     :successors (cleavir-ir:successors instruction)))))
 
 (defmethod  process-instruction (client
                                  (instruction cleavir-ir:slot-write-instruction))
-  (let ((slot-address (make-instance 'cleavir-ir:lexical-location
-                        :name '#:slot-address)))
+  (let ((slot-location (make-instance 'cleavir-ir:lexical-location
+                        :name '#:slot-location)))
     (destructuring-bind (object-location slot-number value-location)
         (cleavir-ir:inputs instruction)
-      (find-slot object-location slot-number slot-address instruction)
+      (find-slot object-location slot-number slot-location instruction)
       (change-class instruction 'cleavir-ir:memset2-instruction
                     :offset -7
-                    :inputs (list slot-address value-location)
+                    :inputs (list slot-location value-location)
                     :successors (cleavir-ir:successors instruction)))))
