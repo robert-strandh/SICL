3c73e90eed7c7c50253809366d5774cc706ea731
Robert Strandh <robert.strandh@gmail.com>
2019-07-13 11:35:17 +0200

    Modify processing of CAR-INSTRUCTION to use an explicit subtraction.

--- Code/Compiler/HIR-to-MIR/cons.lisp
+++ Code/Compiler/HIR-to-MIR/cons.lisp
@@ -2,11 +2,19 @@
 
 (defmethod process-instruction (client
                                 (instruction cleavir-ir:car-instruction))
-  (change-class instruction 'cleavir-ir:memref2-instruction
-                :offset -1
-                :inputs (cleavir-ir:inputs instruction)
-                :outputs (cleavir-ir:outputs instruction)
-                :successors (cleavir-ir:successors instruction)))
+  (let ((offset-input (make-instance 'cleavir-ir:immediate-input :value 1))
+        (raw-pointer-location (make-instance 'cleavir-ir:raw-integer :size 64)))
+    (cleavir-ir:insert-instruction-before
+     (make-instance 'cleavir-ir:unsigned-sub-instruction
+       :minuend (first (cleavir-ir:inputs instruction))
+       :subtrahend offset-input
+       :output raw-pointer-location
+       :successor instruction)
+     instruction)
+    (change-class instruction 'cleavir-ir:memref1-instruction
+                  :input raw-pointer-location
+                  :outputs (cleavir-ir:outputs instruction)
+                  :successors (cleavir-ir:successors instruction))))
 
 (defmethod process-instruction (client
                                 (instruction cleavir-ir:cdr-instruction))
