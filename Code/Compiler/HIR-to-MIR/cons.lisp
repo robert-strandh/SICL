326977c22f8cbf7abc704624ed5d55e825edfea9
Robert Strandh <robert.strandh@gmail.com>
2019-07-13 12:41:25 +0200

    Use new utility function to process RPLACA/RPLACD-INSTRUCTION.

--- Code/Compiler/HIR-to-MIR/cons.lisp
+++ Code/Compiler/HIR-to-MIR/cons.lisp
@@ -20,16 +20,20 @@
 
 (defmethod process-instruction (client
                                 (instruction cleavir-ir:rplaca-instruction))
-  (change-class instruction 'cleavir-ir:memset2-instruction
-                :offset -1
-                :inputs (cleavir-ir:inputs instruction)
-                :outputs (cleavir-ir:outputs instruction)
-                :successors (cleavir-ir:successors instruction)))
+  (let* ((input (first (cleavir-ir:inputs instruction)))
+         (raw-pointer-location (raw-pointer-from-tagged-pointer instruction input -1))
+         (value-location (second (cleavir-ir:inputs instruction))))
+    (change-class instruction 'cleavir-ir:memset1-instruction
+                  :inputs (list raw-pointer-location value-location)
+                  :outputs (cleavir-ir:outputs instruction)
+                  :successors (cleavir-ir:successors instruction))))
 
 (defmethod process-instruction (client
                                 (instruction cleavir-ir:rplacd-instruction))
-  (change-class instruction 'cleavir-ir:memset2-instruction
-                :offset 7
-                :inputs (cleavir-ir:inputs instruction)
-                :outputs (cleavir-ir:outputs instruction)
-                :successors (cleavir-ir:successors instruction)))
+  (let* ((input (first (cleavir-ir:inputs instruction)))
+         (raw-pointer-location (raw-pointer-from-tagged-pointer instruction input 7))
+         (value-location (second (cleavir-ir:inputs instruction))))
+    (change-class instruction 'cleavir-ir:memset1-instruction
+                  :inputs (list raw-pointer-location value-location)
+                  :outputs (cleavir-ir:outputs instruction)
+                  :successors (cleavir-ir:successors instruction))))
