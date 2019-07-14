1b5a333ae88a11f3b708b7706cbe801fc60ad27f
Robert Strandh <robert.strandh@gmail.com>
2019-07-14 09:40:52 +0200

    Use abstraction FIND-RACK in array processing code.

--- Code/Compiler/HIR-to-MIR/array.lisp
+++ Code/Compiler/HIR-to-MIR/array.lisp
@@ -1,15 +1,7 @@
 (cl:in-package #:sicl-hir-to-mir)
 
 (defun find-element (array-location index size element-address-location instruction)
-  (let ((rack-location (make-instance 'cleavir-ir:lexical-location
-                         :name '#:rack-address)))
-    (cleavir-ir:insert-instruction-before
-     (make-instance 'cleavir-ir:memref2-instruction
-       :offset 3
-       :inputs (list array-location)
-       :outputs (list rack-location)
-       :successors (list instruction))
-     instruction)
+  (let ((rack-location (find-rack instruction array-location)))
     (cleavir-ir:insert-instruction-before
      (make-instance 'cleavir-ir:unsigned-add-instruction
        :inputs (list rack-location (* index size))
