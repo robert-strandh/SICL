8e0f5cff4055073662b7b5feee31cf7eb27a75d1
Robert Strandh <robert.strandh@gmail.com>
2019-07-17 14:17:37 +0200

    Define function for boxing an unsigned integer.

--- Code/Compiler/HIR-to-MIR/boxing.lisp
+++ Code/Compiler/HIR-to-MIR/boxing.lisp
@@ -1,2 +1,15 @@
 (cl:in-package #:sicl-hir-to-mir)
 
+(defun box-unsigned-integer (instruction)
+  (let ((word-location (make-instance 'cleavir-ir:raw-integer :size 64))
+        (shift-count-input (make-instance 'cleavir-ir:immediate-input :value 1)))
+    (cleavir-ir:insert-instruction-before
+     (make-instance 'cleavir-ir:assignment-instruction
+       :inputs (cleavir-ir:inputs instruction)
+       :output word-location
+       :successor instruction)
+     instruction)
+    (change-class instruction 'cleavir-ir:shift-left-instruction
+                  :shifted-input word-location
+                  :shift-count shift-count-input
+                  :outputs (cleavir-ir:outputs instruction))))
