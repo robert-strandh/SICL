424fcb336381c52b540a6d46e9493e131ef63299
Robert Strandh <robert.strandh@gmail.com>
2019-07-17 09:13:36 +0200

    Remove incorrect function for finding an element of an array.

--- Code/Compiler/HIR-to-MIR/array.lisp
+++ Code/Compiler/HIR-to-MIR/array.lisp
@@ -1,14 +1,5 @@
 (cl:in-package #:sicl-hir-to-mir)
 
-(defun find-element (array-location index size element-address-location instruction)
-  (let ((rack-location (find-rack instruction array-location)))
-    (cleavir-ir:insert-instruction-before
-     (make-instance 'cleavir-ir:unsigned-add-instruction
-       :inputs (list rack-location (* index size))
-       :outputs (list element-address-location)
-       :successors (list instruction))
-     instruction)))
-
 (defun shift-count (element-type)
   (cond ((equal element-type 't) 3)
         ((equal element-type 'character) 3)
