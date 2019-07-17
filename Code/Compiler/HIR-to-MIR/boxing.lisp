169d0b6ccd019048281c5d0be46a67987e5828ee
Robert Strandh <robert.strandh@gmail.com>
2019-07-17 14:50:03 +0200

    Define function for unboxing a single float.

--- Code/Compiler/HIR-to-MIR/boxing.lisp
+++ Code/Compiler/HIR-to-MIR/boxing.lisp
@@ -83,3 +83,17 @@
     (change-class instruction 'cleavir-ir:assignment-instruction
                   :input word-location
                   :outputs (cleavir-ir:outputs instruction))))
+
+(defun unbox-single-float (instruction)
+  (let ((word-location (make-instance 'cleavir-ir:raw-integer :size 64))
+        (shift-count-input (make-instance 'cleavir-ir:immediate-input :value 32)))
+    (cleavir-ir:insert-instruction-before
+     (make-instance 'cleavir-ir:arithmetic-shift-right-instruction
+       :shifted-input (first (cleavir-ir:inputs instruction))
+       :shift-count shift-count-input
+       :output word-location
+       :successor instruction)
+     instruction)
+    (change-class instruction 'cleavir-ir:assignment-instruction
+                  :input word-location
+                  :outputs (cleavir-ir:outputs instruction))))
