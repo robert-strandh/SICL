3859770f4f4b892a88d62d4085442c80d4e7ce78
Robert Strandh <robert.strandh@gmail.com>
2019-07-17 14:24:36 +0200

    Define function for boxing an a single float.

--- Code/Compiler/HIR-to-MIR/boxing.lisp
+++ Code/Compiler/HIR-to-MIR/boxing.lisp
@@ -13,3 +13,25 @@
                   :shifted-input word-location
                   :shift-count shift-count-input
                   :outputs (cleavir-ir:outputs instruction))))
+
+(defun box-single-float (instruction)
+  (let ((word-location-1 (make-instance 'cleavir-ir:raw-integer :size 64))
+        (word-location-2 (make-instance 'cleavir-ir:raw-integer :size 64))
+        (shift-count-input (make-instance 'cleavir-ir:immediate-input :value 32))
+        (tag-input (make-instance 'cleavir-ir:immediate-input :value #b10011)))
+    (cleavir-ir:insert-instruction-before
+     (make-instance 'cleavir-ir:assignment-instruction
+       :inputs (cleavir-ir:inputs instruction)
+       :output word-location-1
+       :successor instruction)
+     instruction)
+    (cleavir-ir:insert-instruction-before
+     (make-instance 'cleavir-ir:shift-left-instruction
+       :shifted-input word-location-1
+       :shift-count shift-count-input
+       :output word-location-2
+       :successor instruction)
+     instruction)
+    (change-class instruction 'cleavir-ir:bitwise-or-instruction
+                  :inputs (list word-location-2 tag-input)
+                  :outputs (cleavir-ir:outputs instruction))))
