93cf4456d6625e17622039757558b7f19caf6e49
Robert Strandh <robert.strandh@gmail.com>
2019-07-25 14:06:44 +0200

    Define instruction class FIXNUM-DIVIDE-INSTRUCTION.

--- Code/Cleavir2/Intermediate-representation/HIR/fixnum-related-instructions.lisp
+++ Code/Cleavir2/Intermediate-representation/HIR/fixnum-related-instructions.lisp
@@ -34,3 +34,16 @@
 
 (defclass fixnum-equal-instruction (instruction multiple-successors-mixin)
   ())
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;; Instruction FIXNUM-DIVIDE-INSTRUCTION.
+;;;
+;;; This instruction takes two inputs, each of which must be a fixnum.
+;;; It has two outputs, both fixnums.  The first output contains the
+;;; quotient between the two inputs, and the second output contains
+;;; the remainder.  The slot ROUNDING-MODE one of the symbols
+;;; CL:FLOOR, CL:CEILING, CL:TRUNCATE, and CL:ROUND.
+
+(defclass fixnum-divide-instruction (instruction one-successors-mixin)
+  ((%rounding-mode :initarg :rounding-mode :reader rounding-mode)))
