979ae2cc9e94153890841ac88625adf56be184a0
Robert Strandh <robert.strandh@gmail.com>
2019-08-06 13:35:56 +0200

    Define main entry point function.

--- Code/Compiler/HIR-transformations/Argument-processing/process-parameters.lisp
+++ Code/Compiler/HIR-transformations/Argument-processing/process-parameters.lisp
@@ -43,4 +43,107 @@
               (values (subseq lambda-list (1+ key-position))
                       t
                       nil))))))
-                
+
+(defun process-parameters (enter-instruction)
+  (let* ((lambda-list (cleavir-ir:lambda-list enter-instruction))
+         (required-parameters (extract-required-parameters lambda-list))
+         (optional-parameters (extract-optional-parameters lambda-list))
+         (rest-parameter (extract-rest-parameter lambda-list))
+         (successor (first (cleavir-ir:successors enter-instruction)))
+         (no-more-arguments-branch successor)
+         (more-arguments-branch successor)
+         (argument-count-location
+           (make-instance 'cleavir-ir:lexical-location :name 'argument-count))
+         (error-function-location
+           (make-instance 'cleavir-ir:lexical-location :name 'error-function))
+         (dynamic-environment-location
+           (cleavir-ir:dynamic-environment-location enter-instruction)))
+    (setf (cleavir-ir:predecessors successor) '())
+    (multiple-value-bind (key-parameters key-p allow-other-keys-p)
+        (extract-key-parameters lambda-list)
+      (when key-p
+        (multiple-value-bind (first last)
+            (initialize-keyword-parameters-to-nil
+             (mapcar #'rest key-parameters) dynamic-environment-location)
+          (setf (cleavir-ir:successors last)
+                (list no-more-arguments-branch))
+          (setf no-more-arguments-branch first))
+        (multiple-value-bind (first last)
+            (process-keyword-arguments
+             key-parameters
+             argument-count-location
+             dynamic-environment-location
+             error-function-location
+             (+ (length required-parameters) (length optional-parameters))
+             allow-other-keys-p)
+          (setf (cleavir-ir:successors last)
+                (list more-arguments-branch))
+          (setf more-arguments-branch first)))
+      (unless (null rest-parameter)
+        (multiple-value-bind (first last)
+            (create-rest-parameter
+             argument-count-location
+             rest-parameter
+             dynamic-environment-location
+             (+ (length required-parameters) (length optional-parameters)))
+          (setf (cleavir-ir:successors last)
+                (list more-arguments-branch))
+          (setf more-arguments-branch first)))
+      (multiple-value-bind (first no-more-last more-last)
+          (initialize-optional-parameters
+           optional-parameters
+           argument-count-location
+           dynamic-environment-location
+           (+ (length required-parameters) (length optional-parameters)))
+        (setf (cleavir-ir:successors no-more-last)
+              (list no-more-arguments-branch))
+        (setf (cleavir-ir:successors more-last)
+              (list more-arguments-branch))
+        (setf more-arguments-branch first))
+      (unless (null rest-parameter)
+        (setf more-arguments-branch
+              (make-instance 'cleavir-ir:assignment-instruction
+                :input (make-instance 'cleavir-ir:constant-input :value nil)
+                :output rest-parameter
+                :successor more-arguments-branch
+                :dynamic-environment-location dynamic-environment-location)))
+      (multiple-value-bind (first last)
+          (initialize-required-parameters
+           required-parameters dynamic-environment-location)
+        (setf (cleavir-ir:successors last)
+              (list more-arguments-branch))
+        (setf more-arguments-branch first))
+      (when (and (null rest-parameter) (null key-p))
+        (multiple-value-bind (first last)
+            (check-maximum-argument-count
+             argument-count-location
+             (+ (length required-parameters) (length optional-parameters))
+             dynamic-environment-location
+             error-function-location)
+          (setf (cleavir-ir:successors last)
+                (list more-arguments-branch))
+          (setf more-arguments-branch first)))
+      (unless (null required-parameters)
+        (multiple-value-bind (first last)
+            (check-minimum-argument-count
+             argument-count-location
+             (length required-parameters)
+             dynamic-environment-location
+             error-function-location)
+          (setf (cleavir-ir:successors last)
+                (list more-arguments-branch))
+          (setf more-arguments-branch first)))
+      (setf more-arguments-branch
+            (make-instance 'cleavir-ir:compute-argument-count-instruction
+              :inputs '()
+              :output argument-count-location
+              :successor more-arguments-branch
+              :dynamic-environment-location dynamic-environment-location))
+      (setf more-arguments-branch
+            (make-instance 'cleavir-ir:fdefinition-instruction
+              :input (make-instance 'cleavir-ir:constant-input :value 'error)
+              :output error-function-location
+              :successor more-arguments-branch
+              :dynamic-environment-location dynamic-environment-location))
+      (setf (cleavir-ir:successors enter-instruction)
+            (list more-arguments-branch)))))
