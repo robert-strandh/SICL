6c63a5b85fab1c3f3f5e6404d228c97529dbbc87
Robert Strandh <robert.strandh@gmail.com>
2019-08-09 20:39:12 +0200

    Export only the name of the main entry point.

--- Code/Compiler/HIR-transformations/Argument-processing/packages.lisp
+++ Code/Compiler/HIR-transformations/Argument-processing/packages.lisp
@@ -2,6 +2,4 @@
 
 (defpackage #:sicl-argument-processing
   (:use #:common-lisp)
-  (:export #:check-minimum-argument-count
-           #:check-maximum-argument-count
-           #:initialize-required-parameters))
+  (:export #:process-parameters))
