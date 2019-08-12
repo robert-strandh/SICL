72372a234fe532ec44e08df4e2e9ec4cafbd2e05
Robert Strandh <robert.strandh@gmail.com>
2019-08-12 15:47:16 +0200

    Add a fixed location for NIL in the static environment.

--- Code/Compiler/HIR-to-MIR/variables.lisp
+++ Code/Compiler/HIR-to-MIR/variables.lisp
@@ -1,9 +1,11 @@
 (cl:in-package #:sicl-hir-to-mir)
 
-(defparameter *fixed-position-count* 3)
+(defparameter *fixed-position-count* 4)
 
 (defparameter *code-object-index* 0)
 
 (defparameter *enclose-function-cell-index* 1)
 
 (defparameter *cons-function-cell-index* 2)
+
+(defparameter *nil-index* 3)
