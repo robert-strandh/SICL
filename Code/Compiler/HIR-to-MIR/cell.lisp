f78659b7d40f3264cac1a481ddbc66b833c2f5ea
Robert Strandh <robert.strandh@gmail.com>
2019-08-12 16:49:36 +0200

    Turn READ/WRITE-CELL into CAR/RPLACA.

--- Code/Compiler/HIR-to-MIR/cell.lisp
+++ Code/Compiler/HIR-to-MIR/cell.lisp
@@ -1,17 +1,23 @@
 (cl:in-package #:sicl-hir-to-mir)
 
-(defmethod process-instruction (client
-                                (instruction cleavir-ir:read-cell-instruction))
-  (change-class instruction 'cleavir-ir:memref2-instruction
-                :offset -1
-                :inputs (cleavir-ir:inputs instruction)
-                :outputs (cleavir-ir:outputs instruction)
-                :successors (cleavir-ir:successors instruction)))
+(defun process-read-cell-instruction (read-cell-instruction)
+  (change-class read-cell-instruction 'car-instruction))
 
-(defmethod process-instruction (client
-                                (instruction cleavir-ir:write-cell-instruction))
-  (change-class instruction 'cleavir-ir:memset2-instruction
-                :offset -1
-                :inputs (cleavir-ir:inputs instruction)
-                :outputs (cleavir-ir:outputs instruction)
-                :successors (cleavir-ir:successors instruction)))
+(defun process-read-cell-instructions (top-level-enter-instruction)
+  (cleavir-ir:map-instructions-with-owner
+   (lambda (instruction owner)
+     (when (and (typep instruction 'cleavir-ir:read-cell-instruction)
+                (not (eq owner top-level-enter-instruction)))
+       (process-read-cell-instructions instruction)))
+   top-level-enter-instruction))
+
+(defun process-write-cell-instruction (write-cell-instruction)
+  (change-class write-cell-instruction 'rplaca-instruction))
+
+(defun process-write-cell-instructions (top-level-enter-instruction)
+  (cleavir-ir:map-instructions-with-owner
+   (lambda (instruction owner)
+     (when (and (typep instruction 'cleavir-ir:write-cell-instruction)
+                (not (eq owner top-level-enter-instruction)))
+       (process-write-cell-instructions instruction)))
+   top-level-enter-instruction))
