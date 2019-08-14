5ada63b7f404d848ba77b089b662bbbcd6534d1a
Robert Strandh <robert.strandh@gmail.com>
2019-08-14 09:29:12 +0200

    Add package prefix to instruction class names.

--- Code/Compiler/HIR-transformations/eliminate-cell-access.lisp
+++ Code/Compiler/HIR-transformations/eliminate-cell-access.lisp
@@ -1,23 +1,23 @@
 (cl:in-package #:sicl-hir-transformations)
 
 (defun eliminate-read-cell-instruction (read-cell-instruction)
-  (change-class read-cell-instruction 'car-instruction))
+  (change-class read-cell-instruction 'cleavir-ir:car-instruction))
 
 (defun eliminate-read-cell-instructions (top-level-enter-instruction)
   (cleavir-ir:map-instructions-with-owner
    (lambda (instruction owner)
      (when (and (typep instruction 'cleavir-ir:read-cell-instruction)
                 (not (eq owner top-level-enter-instruction)))
-       (eliminate-read-cell-instructions instruction)))
+       (eliminate-read-cell-instruction instruction)))
    top-level-enter-instruction))
 
 (defun eliminate-write-cell-instruction (write-cell-instruction)
-  (change-class write-cell-instruction 'rplaca-instruction))
+  (change-class write-cell-instruction 'cleavir-ir:rplaca-instruction))
 
 (defun eliminate-write-cell-instructions (top-level-enter-instruction)
   (cleavir-ir:map-instructions-with-owner
    (lambda (instruction owner)
      (when (and (typep instruction 'cleavir-ir:write-cell-instruction)
                 (not (eq owner top-level-enter-instruction)))
-       (eliminate-write-cell-instructions instruction)))
+       (eliminate-write-cell-instruction instruction)))
    top-level-enter-instruction))
