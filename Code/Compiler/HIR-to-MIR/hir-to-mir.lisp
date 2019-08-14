a29a3d9401605bca0d30eba1cfbb0dcc27ef3807
Robert Strandh <robert.strandh@gmail.com>
2019-08-14 07:23:16 +0200

    Define main entry point.

--- Code/Compiler/HIR-to-MIR/hir-to-mir.lisp
+++ Code/Compiler/HIR-to-MIR/hir-to-mir.lisp
@@ -1,7 +1,16 @@
 (cl:in-package #:sicl-hir-to-mir)
 
-(defun process-instructions (client initial-instruction)
-  (cleavir-ir:map-instructions-arbitrary-order
-   (lambda (instruction)
-     (process-instruction client instruction))
-   initial-instruction))
+(defun process-instructions (client top-level-enter-instruction)
+  (cleavir-ir:map-instructions-with-owner
+   (lambda (instruction owner)
+     (unless (eq owner top-level-enter-instruction)
+       (process-instruction client instruction)))
+   top-level-enter-instruction))
+
+(defun hir-to-mir (client top-level-enter-instruction)
+  (process-enclose-instructions top-level-enter-instruction)
+  (process-create-cell-instruction top-level-enter-instruction)
+  (process-read-cell-instruction top-level-enter-instruction)
+  (process-write-cell-instruction top-level-enter-instruction)
+  (process-fetch top-level-enter-instruction)
+  (process-instructions client top-level-enter-instruction))
