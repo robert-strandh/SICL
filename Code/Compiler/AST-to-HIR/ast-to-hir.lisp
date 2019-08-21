4f64ca72b235fe08405bf641ce63113caeab6c2b
Robert Strandh <robert.strandh@gmail.com>
2019-08-21 14:55:46 +0200

    Call ELIMINATE-CREATE-CELL-INSTRUCTIONS after closure conversion.

--- Code/Compiler/AST-to-HIR/ast-to-hir.lisp
+++ Code/Compiler/AST-to-HIR/ast-to-hir.lisp
@@ -20,6 +20,7 @@
     (sicl-hir-transformations:convert-set-symbol-value hir)
     (sicl-hir-transformations:hoist-fdefinitions hir)
     (cleavir-hir-transformations::process-captured-variables hir)
+    (sicl-hir-transformations:eliminate-create-cell-instructions hir)
     (sicl-hir-transformations:eliminate-read-cell-instructions hir)
     (sicl-hir-transformations:eliminate-write-cell-instructions hir)
     hir))
