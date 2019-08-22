0e9ee257ae9892a952eae5e013d21ed542ad35f9
Robert Strandh <robert.strandh@gmail.com>
2019-08-22 07:37:14 +0200

    Export name of main entry point of new component.

--- Code/Compiler/HIR-transformations/packages.lisp
+++ Code/Compiler/HIR-transformations/packages.lisp
@@ -6,5 +6,6 @@
            #:convert-symbol-value
            #:convert-set-symbol-value
            #:eliminate-create-cell-instructions
+           #:eliminate-fetch-instructions
            #:eliminate-read-cell-instructions
            #:eliminate-write-cell-instructions))
