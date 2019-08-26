db40d8ff1c35ddc16fd4d79ed953d94dfb82d198
Robert Strandh <robert.strandh@gmail.com>
2019-08-26 16:18:20 +0200

    Improve layout of file comment.

--- Code/Cleavir2/HIR-transformations/function-dag.lisp
+++ Code/Cleavir2/HIR-transformations/function-dag.lisp
@@ -2,18 +2,18 @@
 
 ;;;; In this file we define functions for building and traversing a
 ;;;; FUNCTION DAG.  Such a DAG defines the nesting of functions in a
-;;;; program.  A node (other than the root) of the DAG contains a
-;;;; slot holding an ENCLOSE-INSTRUCTION.  Each node contains a list
-;;;; of CHILDREN that are also DAG nodes.  The children of a node N
+;;;; program.  A node (other than the root) of the DAG contains a slot
+;;;; holding an ENCLOSE-INSTRUCTION.  Each node contains a list of
+;;;; CHILDREN that are also DAG nodes.  The children of a node N
 ;;;; (other than the root) containing some ENCLOSE-INSTRUCTION E are
 ;;;; nodes that contain the ENCLOSE-INSTRUCTIONs owned by the CODE of
 ;;;; (i.e., the ENTER-INSTRUCTION associated with) E.  The children of
 ;;;; the root node are nodes containing the ENCLOSE-INSTRUCTIONs owned
 ;;;; by the ENTER-INSTRUCTION INITIAL-INSTRUCTION.  In addition to a
 ;;;; list of children, the root node also contains an EQ hash table
-;;;; that maps ENTER-INSTRUCTIONs to DAG nodes such that the DAG
-;;;; node is either the root, or it contains the ENCLOSE-INSTRUCTION
-;;;; that the ENTER-INSTRUCTION is associated with.
+;;;; that maps ENTER-INSTRUCTIONs to DAG nodes such that the DAG node
+;;;; is either the root, or it contains the ENCLOSE-INSTRUCTION that
+;;;; the ENTER-INSTRUCTION is associated with.
 
 (defgeneric enter-instruction (node))
 
