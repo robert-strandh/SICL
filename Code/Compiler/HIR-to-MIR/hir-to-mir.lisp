e4f5db6d548c103a259b40311527b5bbe8d5e990
Robert Strandh <robert.strandh@gmail.com>
2019-08-27 18:14:35 +0200

    Eliminate unnecessary indirection.

--- Code/Compiler/HIR-to-MIR/hir-to-mir.lisp
+++ Code/Compiler/HIR-to-MIR/hir-to-mir.lisp
@@ -1,11 +1,8 @@
 (cl:in-package #:sicl-hir-to-mir)
 
-(defun process-instructions (client top-level-enter-instruction)
+(defun hir-to-mir (client top-level-enter-instruction)
   (cleavir-ir:map-instructions-with-owner
    (lambda (instruction owner)
      (unless (eq owner top-level-enter-instruction)
        (process-instruction client instruction)))
    top-level-enter-instruction))
-
-(defun hir-to-mir (client top-level-enter-instruction)
-  (process-instructions client top-level-enter-instruction))
