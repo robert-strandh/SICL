f06ac8998fc5e807bed3e2306cd4aabb08e00ed6
Robert Strandh <robert.strandh@gmail.com>
2019-08-27 16:36:15 +0200

    Replace constant intputs by lexical variable assigned by top-level function.

--- Code/Compiler/HIR-transformations/hoist-constant-inputs.lisp
+++ Code/Compiler/HIR-transformations/hoist-constant-inputs.lisp
@@ -16,16 +16,38 @@
   (< (char-code constant) #.(expt 2 29)))
 
 (defun hoist-constant-inputs (initial-instruction)
-  (with-accessors ((constants constants))
-      initial-instruction
-    (cleavir-ir:map-instructions-arbitrary-order
-     (lambda (instruction)
-       (loop for input in (cleavir-ir:inputs instruction)
-             do (when (and (typep input 'cleavir-ir:constant-input)
-                           (not (trivial-constant-p (cleavir-ir:value input))))
-                  (let* ((value (cleavir-ir:value input))
-                         (pos (position value constants)))
-                    (if (null pos)
+  (let ((locations '())
+        (dynamic-environment-location
+          (cleavir-ir:dynamic-environment-output initial-instruction))
+        (static-environment-location
+          (cleavir-ir:static-environment initial-instruction)))
+    (with-accessors ((constants constants))
+        initial-instruction
+      (cleavir-ir:map-instructions-arbitrary-order
+       (lambda (instruction)
+         (loop for remaining = (cleavir-ir:inputs instruction) then (rest remaining)
+               for input = (first remaining)
+               until (null remaining)
+               do (when (and (typep input 'cleavir-ir:constant-input)
+                             (not (trivial-constant-p (cleavir-ir:value input))))
+                    (let* ((value (cleavir-ir:value input))
+                           (pos (position value constants)))
+                      (when (null pos)
+                        (setf pos (length constants))
                         (setf constants (append constants (list value)))
-                        nil)))))
-     initial-instruction)))
+                        (let ((location (make-instance 'cleavir-ir:lexical-location
+                                          :name (gensym))))
+                          (setf locations (append locations (list location)))
+                          (cleavir-ir:insert-instruction-after
+                           (make-instance 'cleavir-ir:fetch-instruction
+                             :inputs (list static-environment-location
+                                           (make-instance 'cleavir-ir:constant-input
+                                             :value pos))
+                             :output location
+                             :dynamic-environment-location
+                             dynamic-environment-location)
+                           initial-instruction)))
+                      (setf (first remaining)
+                            (nth pos locations))))))
+       initial-instruction)))
+  (cleavir-ir:reinitialize-data initial-instruction))
