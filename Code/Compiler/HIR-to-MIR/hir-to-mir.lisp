b7cb877701ddf20d858592d4503cb822ff606a46
Robert Strandh <robert.strandh@gmail.com>
2019-08-28 14:05:21 +0200

    Call new function for eliminating enclose-instructions.

--- Code/Compiler/HIR-to-MIR/hir-to-mir.lisp
+++ Code/Compiler/HIR-to-MIR/hir-to-mir.lisp
@@ -1,6 +1,7 @@
 (cl:in-package #:sicl-hir-to-mir)
 
 (defun hir-to-mir (client top-level-enter-instruction)
+  (eliminate-enclose-instructions client top-level-enter-instruction)
   (cleavir-ir:map-instructions-with-owner
    (lambda (instruction owner)
      (unless (eq owner top-level-enter-instruction)
