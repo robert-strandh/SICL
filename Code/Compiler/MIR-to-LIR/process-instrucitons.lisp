d0882d1ce5b767ce849b4f69f112d37ad88e659d
Robert Strandh <robert.strandh@gmail.com>
2019-08-29 16:08:20 +0200

    New component for processing existing instructions.

---
+++ Code/Compiler/MIR-to-LIR/process-instrucitons.lisp
@@ -0,0 +1,37 @@
+(cl:in-package #:sicl-mir-to-lir)
+
+(defgeneric process-instruction (instruction lexical-locations))
+
+(defmethod process-instruction
+    (instruction cleavir-ir:assignment-instruction lexical-locations)
+  (let ((dynamic-environment-location
+          (cleavir-ir:dynamic-environment-location instruction))
+        (input (first (cleavir-ir:inputs instruction)))
+        (output (first (cleavir-ir:outputs instruction))))
+    (cond ((and (typep input 'cleavir-ir:register-location)
+                (typep output 'cleavir-ir:register-location))
+           ;; Leave it as it is
+           nil)
+          ((and (or (typep input 'cleavir-ir:register-location)
+                    (typep input 'cleavir-ir:immediate-input))
+                (typep output 'cleavir-ir:lexical-location))
+           (let ((immediate-input
+                   (make-instance 'cleavir-ir:immediate-input
+                     :value (+ (gethash output lexical-locations) 8))))
+             (cleavir-ir:insert-instruction-before
+              (make-instance 'cleavir-ir:assignment-instruction
+                :input *rbp*
+                :output *r11*
+                :dynamic-environment-location dynamic-environment-location)
+              instruction)
+             (cleavir-ir:insert-instruction-before
+              (make-instance 'cleavir-ir:unsigned-sub-instruction
+                :inputs (list *r11* immediate-input)
+                :output *r11*
+                :dynamic-environment-location dynamic-environment-location)
+              instruction)
+             (change-class instruction
+                           'cleavir-ir:memset1-instruction
+                           :inputs (list *r11* input)
+                           :outputs '())))
+          (t (error "Can't handle input ~s and output ~s" input output)))))
--- Code/Compiler/MIR-to-LIR/sicl-mir-to-lir.asd
+++ Code/Compiler/MIR-to-LIR/sicl-mir-to-lir.asd
@@ -1,7 +1,8 @@
 (cl:in-package #:asdf-user)
 
 (defsystem #:sicl-mir-to-lir
-  :depends-on (#:cleavir2-lir)
+  :depends-on (#:cleavir2-lir
+               #:cleavir2-hir)
   :serial t
   :components
   ((:file "packages")
