589de62b5924a58a2d0134dceacc0c38930ed386
Robert Strandh <robert.strandh@gmail.com>
2019-08-31 12:12:35 +0200

    Define an additional top-level translator for file compilation.

--- Code/Compiler/HIR-to-CL/packages.lisp
+++ Code/Compiler/HIR-to-CL/packages.lisp
@@ -3,7 +3,10 @@
 (defpackage #:sicl-hir-to-cl
   (:use #:common-lisp)
   (:export #:hir-to-cl
+           #:hir-to-cl-for-file-compilation
            #:cst-eval
            #:fill-environment
            #:make-function-cell-finder
-           #:*dynamic-environment*))
+           #:*dynamic-environment*
+           #:*top-level-function*
+           #:*constants*))
