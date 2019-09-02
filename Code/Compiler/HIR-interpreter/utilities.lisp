9557039d0002f1ccf337c9e1bc28a07d9073704d
Robert Strandh <robert.strandh@gmail.com>
2019-09-02 11:35:53 +0200

    Define generic function INTERPRET-INSTRUCTION and function INTERPRET-INSTRUCTIONS.

--- Code/Compiler/HIR-interpreter/utilities.lisp
+++ Code/Compiler/HIR-interpreter/utilities.lisp
@@ -4,3 +4,9 @@
   (if (typep input 'cleavir-ir:constant-input)
       (cleavir-ir:value input)
       (gethash input lexical-environment)))
+
+(defgeneric interpret-instruction (client instruction lexical-environment))
+
+(defun interpret-instructions (client initial-instruction lexical-environment)
+  (loop for instruction = initial-instruction
+          then (interpret-instruction client instruction lexical-environment)))
