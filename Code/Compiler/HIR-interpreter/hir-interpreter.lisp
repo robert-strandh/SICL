08f9faa903c592e9dfb19c2307d20e2246467259
Robert Strandh <robert.strandh@gmail.com>
2019-09-04 12:32:49 +0200

    Define main entry point.

--- Code/Compiler/HIR-interpreter/hir-interpreter.lisp
+++ Code/Compiler/HIR-interpreter/hir-interpreter.lisp
@@ -1,1 +1,23 @@
 (cl:in-package #:sicl-hir-interpreter)
+
+(defun interpret-hir (client enter-instruction lexical-environment)
+  (let ((instruction (first (cleavir-ir:successors enter-instruction))))
+    (setf *previous-instruction* instruction)
+    (loop do (setf instruction
+                   (interpret-instruction client instruction lexical-environment)))))
+
+(defun hir-to-host-function (client enter-instruction)
+  (lambda (arguments static-environment dynamic-environment)
+    (let ((lexical-environment (make-hash-table :test #'eq))
+          (static-environment-output
+            (cleavir-ir:static-environment enter-instruction))
+          (dynamic-environment-output
+            (cleavir-ir:dynamic-environment-output enter-instruction)))
+      (setf (gethash static-environment-output lexical-environment)
+            static-environment)
+      (setf (gethash dynamic-environment-output lexical-environment)
+            dynamic-environment)
+      (setf (gethash 'arguments lexical-environment)
+            arguments)
+      (catch 'return
+        (interpret-hir client enter-instruction lexical-environment)))))
