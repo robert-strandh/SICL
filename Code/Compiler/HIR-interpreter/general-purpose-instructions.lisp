2b37c0a0c24b6fdc1a5dd7ac91b7776375020029
Robert Strandh <robert.strandh@gmail.com>
2019-09-04 12:46:06 +0200

    Define method on interpret-instruction specialized to FUNCALL-INSTRUCTION.

--- Code/Compiler/HIR-interpreter/general-purpose-instructions.lisp
+++ Code/Compiler/HIR-interpreter/general-purpose-instructions.lisp
@@ -6,3 +6,13 @@
      lexical-environment)
   (throw 'return
     (apply #'values *global-values-location*)))
+
+(defmethod interpret-instruction
+    (client
+     (instruction cleavir-ir:funcall-instruction)
+     lexical-environment)
+  (let* ((inputs (cleavir-ir:inputs instruction))
+         (input-values (loop for input in inputs
+                             collect (input-value input lexical-environment))))
+    (setf *global-values-location*
+          (multiple-value-list (apply #'funcall input-values)))))
