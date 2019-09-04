bcaf2e93a32f26fc0938e10cfd3eb0a90fe35b68
Robert Strandh <robert.strandh@gmail.com>
2019-09-04 13:02:58 +0200

    Define methods on INTERPRET-INSTRUCTION specialized to several comparison instructions.

--- Code/Compiler/HIR-interpreter/general-purpose-instructions.lisp
+++ Code/Compiler/HIR-interpreter/general-purpose-instructions.lisp
@@ -27,3 +27,44 @@
     (setf (gethash output lexical-environment)
           (input-value input lexical-environment)))
   (first (cleavir-ir:successors instruction)))
+
+(defmethod interpret-instruction
+    (client
+     (instruction cleavir-ir:eq-instruction)
+     lexical-environment)
+  (let* ((inputs (cleavir-ir:inputs instruction))
+         (successors (cleavir-ir:successors instruction)))
+    (if (eq (input-value (first inputs) lexical-environment)
+            (input-value (second inputs) lexical-environment))
+        (first successors)
+        (second successors))))
+
+(defmethod interpret-instruction
+    (client
+     (instruction cleavir-ir:consp-instruction)
+     lexical-environment)
+  (let* ((input (first (cleavir-ir:inputs instruction)))
+         (successors (cleavir-ir:successors instruction)))
+    (if (consp (input-value input lexical-environment))
+        (first successors)
+        (second successors))))
+
+(defmethod interpret-instruction
+    (client
+     (instruction cleavir-ir:fixnump-instruction)
+     lexical-environment)
+  (let* ((input (first (cleavir-ir:inputs instruction)))
+         (successors (cleavir-ir:successors instruction)))
+    (if (typep (input-value input lexical-environment) 'fixnum)
+        (first successors)
+        (second successors))))
+
+(defmethod interpret-instruction
+    (client
+     (instruction cleavir-ir:characterp-instruction)
+     lexical-environment)
+  (let* ((input (first (cleavir-ir:inputs instruction)))
+         (successors (cleavir-ir:successors instruction)))
+    (if (characterp (input-value input lexical-environment))
+        (first successors)
+        (second successors))))
