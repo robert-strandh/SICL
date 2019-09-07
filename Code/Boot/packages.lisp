0e5c830e20527b1d7f441e35f3632fc91f611b34
Robert Strandh <robert.strandh@gmail.com>
2019-09-07 16:03:39 +0200

    Export name of class CLIENT.

--- Code/Boot/packages.lisp
+++ Code/Boot/packages.lisp
@@ -3,6 +3,7 @@
 (defpackage #:sicl-boot
   (:use #:common-lisp)
   (:export #:environment
+           #:client
            #:boot
            #:e0 #:e1 #:e2 #:e3 #:e4 #:e5 #:e6
            #:import-function-from-host
