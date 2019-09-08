675293fb106fd8d462e1a4249f300edcaf605e80
Robert Strandh <robert.strandh@gmail.com>
2019-09-08 07:10:27 +0200

    Untabify for better tool support.

--- Code/Evaluation-and-compilation/defmacro-defmacro.lisp
+++ Code/Evaluation-and-compilation/defmacro-defmacro.lisp
@@ -3,4 +3,4 @@
 (defmacro defmacro (name lambda-list &body body)
   `(eval-when (:compile-toplevel :load-toplevel :execute)
      (setf (sicl-env:macro-function ',name (sicl-genv:global-environment))
-	   ,(cleavir-code-utilities:parse-macro name lambda-list body))))
+           ,(cleavir-code-utilities:parse-macro name lambda-list body))))
