06cc085d375a60aafd50641f93d72c68febddf5b
Robert Strandh <robert.strandh@gmail.com>
2019-09-10 13:59:05 +0200

    Remove obsolete system and its components.

--- Code/Compiler/File-compiler/client.lisp
+++
@@ -1,3 +0,0 @@
-(cl:in-package #:sicl-file-compiler)
-
-(defclass sicl-file-compiler () ())
--- Code/Compiler/File-compiler/packages.lisp
+++
@@ -1,5 +0,0 @@
-(cl:in-package #:common-lisp-user)
-
-(defpackage #:sicl-file-compiler
-  (:use #:common-lisp)
-  (:export #:ast-from-file))
