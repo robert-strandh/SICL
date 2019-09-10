679fba65f54513ea68f442a2362448c1bc3ba54d
Robert Strandh <robert.strandh@gmail.com>
2019-09-10 13:19:04 +0200

    Remove obsolete system and its components.

--- Code/Reader/Source-tracking/location.lisp
+++
@@ -1,7 +0,0 @@
-(cl:in-package #:sicl-source-tracking-reader)
-
-(defclass location ()
-  ((%start-line :initarg :start-line :reader start-line)
-   (%start-column :initarg :start-column :reader start-column)
-   (%end-line :initarg :end-line :reader end-line)
-   (%end-column :initarg :end-column :reader end-column)))
--- Code/Reader/Source-tracking/packages.lisp
+++
@@ -1,5 +0,0 @@
-(cl:in-package #:common-lisp-user)
-
-(defpackage #:sicl-source-tracking-reader
-  (:use #:common-lisp)
-  (:export))
