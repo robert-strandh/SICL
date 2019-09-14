cd491bd111db5445cb48691f05df04a0e299de3d
Robert Strandh <robert.strandh@gmail.com>
2019-09-14 07:06:34 +0200

    Fix typo.

--- Code/Compiler/HIR-to-MIR/generic-functions.lisp
+++ Code/Compiler/HIR-to-MIR/generic-functions.lisp
@@ -1,6 +1,6 @@
 (cl:in-package #:sicl-hir-to-mir)
 
-(defgeneric process-instruciton (client instruction))
+(defgeneric process-instruction (client instruction))
 
 (defmethod process-instruction (client instruction)
   (declare (ignore client instruction))
