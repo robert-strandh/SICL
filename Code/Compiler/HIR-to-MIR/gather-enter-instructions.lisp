c0ba7ab9d103b2e8618985bb899f196e4a2aec9c
Robert Strandh <robert.strandh@gmail.com>
2019-09-14 07:57:03 +0200

    Find the children of and ENTER by getting the CODE of ENCLOSE-INSTRUCTIONs.

--- Code/Compiler/HIR-to-MIR/gather-enter-instructions.lisp
+++ Code/Compiler/HIR-to-MIR/gather-enter-instructions.lisp
@@ -3,8 +3,10 @@
 ;;; Collect a list of ENTER-INSTRUCTIONs such that a parent
 ;;; ENTER-INSTRUCTION precedes all of its children in the list.
 (defun gather-enter-instructions (enter-instruction)
-  (let ((children (cleavir-ir:local-instructions-of-type
-                   enter-instruction 'cleavir-ir:enter-instruction)))
+  (let ((enclose-instructions
+          (cleavir-ir:local-instructions-of-type
+           enter-instruction 'cleavir-ir:enclose-instruction)))
     (cons enter-instruction
-          (loop for child in children
+          (loop for enclose-instruction in enclose-instructions
+                for child = (cleavir-ir:code enclose-instruction)
                 append (gather-enter-instructions child)))))
