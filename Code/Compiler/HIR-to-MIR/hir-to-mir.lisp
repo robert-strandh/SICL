3b17bc200f5c64d8c343e3eca48d1414b2d4f21c
Robert Strandh <robert.strandh@gmail.com>
2019-09-14 07:07:50 +0200

    Split the HIR program into individual functions before processing.

--- Code/Compiler/HIR-to-MIR/hir-to-mir.lisp
+++ Code/Compiler/HIR-to-MIR/hir-to-mir.lisp
@@ -1,9 +1,14 @@
 (cl:in-package #:sicl-hir-to-mir)
 
+(defun convert-one-function (client enter-instruction)
+  (eliminate-enclose-instructions client enter-instruction)
+  (cleavir-ir:map-local-instructions
+   (lambda (instruction)
+     (process-instruction client instruction))
+   enter-instruction))
+
 (defun hir-to-mir (client top-level-enter-instruction)
-  (eliminate-enclose-instructions client top-level-enter-instruction)
-  (cleavir-ir:map-instructions-with-owner
-   (lambda (instruction owner)
-     (unless (eq owner top-level-enter-instruction)
-       (process-instruction client instruction)))
-   top-level-enter-instruction))
+  (let ((enter-instructions
+          (gather-enter-instructions top-level-enter-instruction)))
+    (loop for enter-instruction in enter-instructions
+          collect (convert-one-function client enter-instruction))))
