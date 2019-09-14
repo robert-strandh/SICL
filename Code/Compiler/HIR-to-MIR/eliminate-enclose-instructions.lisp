ce1d7ee23ab72c1b31cbe5cfb8d8d674c114325d
Robert Strandh <robert.strandh@gmail.com>
2019-09-14 07:07:27 +0200

    Eliminate ENCLOSE for a signle function only.

--- Code/Compiler/HIR-to-MIR/eliminate-enclose-instructions.lisp
+++ Code/Compiler/HIR-to-MIR/eliminate-enclose-instructions.lisp
@@ -3,38 +3,40 @@
 (defclass entry-point-input (cleavir-ir:immediate-input)
   ((%enter-instruction :initarg :enter-instruction :reader enter-instruction)))
 
-(defun eliminate-enclose-instructions (client top-level-enter-instruction)
+;;; Replace each ENTER instruction that is an immediate child of
+;;; ENTER-INSTRUCTION with an AREF-instruction.
+(defun eliminate-enclose-instructions (client enter-instruction)
   (declare (ignore client))
-  (cleavir-ir:map-instructions-with-owner
-   (lambda (instruction owner)
-     (when (typep instruction 'cleavir-ir:enclose-instruction)
-       (let ((static-environment-location
-               (cleavir-ir:static-environment owner))
-             (enclose-function-lexical-location
-               (make-instance 'cleavir-ir:lexical-location
-                 :name (gensym "enclose-function")))
-             (static-input-1
-               (make-instance 'cleavir-ir:constant-input
-                 :value 1))
-             (entry-point-input
-               (make-instance 'entry-point-input
-                 :value 0
-                 :enter-instruction (cleavir-ir:code instruction))))
-         (cleavir-ir:insert-instruction-before
-          (make-instance 'cleavir-ir:aref-instruction
-            :boxed-p t
-            :simple-p t
-            :element-type t
-            :inputs (list static-environment-location static-input-1)
-            :output enclose-function-lexical-location)
-          instruction)
-         (cleavir-ir:insert-instruction-after
-          (make-instance 'cleavir-ir:multiple-to-fixed-instruction
-            :output (first (cleavir-ir:outputs instruction)))
-          instruction)
-         (change-class instruction
-                       'cleavir-ir:funcall-instruction
-                       :inputs (list* enclose-function-lexical-location
-                                      entry-point-input
-                                      (cleavir-ir:inputs instruction))))))
-   top-level-enter-instruction))
+  (let ((static-environment-location
+          (cleavir-ir:static-environment enter-instruction)))
+    (cleavir-ir:map-local-instructions
+     (lambda (instruction)
+       (when (typep instruction 'cleavir-ir:enclose-instruction)
+         (let ((enclose-function-lexical-location
+                 (make-instance 'cleavir-ir:lexical-location
+                   :name (gensym "enclose-function")))
+               (static-input-1
+                 (make-instance 'cleavir-ir:constant-input
+                   :value 1))
+               (entry-point-input
+                 (make-instance 'entry-point-input
+                   :value 0
+                   :enter-instruction (cleavir-ir:code instruction))))
+           (cleavir-ir:insert-instruction-before
+            (make-instance 'cleavir-ir:aref-instruction
+              :boxed-p t
+              :simple-p t
+              :element-type t
+              :inputs (list static-environment-location static-input-1)
+              :output enclose-function-lexical-location)
+            instruction)
+           (cleavir-ir:insert-instruction-after
+            (make-instance 'cleavir-ir:multiple-to-fixed-instruction
+              :output (first (cleavir-ir:outputs instruction)))
+            instruction)
+           (change-class instruction
+                         'cleavir-ir:funcall-instruction
+                         :inputs (list* enclose-function-lexical-location
+                                        entry-point-input
+                                        (cleavir-ir:inputs instruction))))))
+     enter-instruction)))
