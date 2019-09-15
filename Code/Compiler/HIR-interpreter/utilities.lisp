7f15efcf00d503926b5ca86c8173770a07e77e11
Robert Strandh <robert.strandh@gmail.com>
2019-09-15 08:54:01 +0200

    Define functions LEXICAL-VALUE and (SETF LEXICAL-VALUE).

--- Code/Compiler/HIR-interpreter/utilities.lisp
+++ Code/Compiler/HIR-interpreter/utilities.lisp
@@ -1,9 +1,18 @@
 (cl:in-package #:sicl-hir-interpreter)
 
+(defun lexical-value (name lexical-environment)
+  (multiple-value-bind (value present-p)
+      (gethash name lexical-environment)
+    ;; (assert present-p)
+    value))
+
+(defun (setf lexical-value) (value name lexical-environment)
+  (setf (gethash name lexical-environment) value))
+
 (defun input-value (input lexical-environment)
   (if (typep input 'cleavir-ir:constant-input)
       (cleavir-ir:value input)
-      (gethash input lexical-environment)))
+      (lexical-value input lexical-environment)))
 
 (defgeneric interpret-instruction (client instruction lexical-environment))
 
