68f2fd6ccf80ca0397419f21100a82b5fa844683
Robert Strandh <robert.strandh@gmail.com>
2019-09-15 12:10:07 +0200

    Hoist an FDEFINITION-INSTRUCTION by inserting a FUNCALL-INSTRUCTION to argument 0.

--- Code/Compiler/HIR-transformations/hoist-fdefinitions.lisp
+++ Code/Compiler/HIR-transformations/hoist-fdefinitions.lisp
@@ -3,32 +3,47 @@
 (defclass find-function-cell-instruction (cleavir-ir:instruction)
   ((%name :initarg :name :reader name)))
 
+(defun insert-find-function-cell (function-name output successor)
+  (let ((zero (make-instance 'cleavir-ir:constant-input :value 0))
+        (function-name-input
+          (make-instance 'cleavir-ir:constant-input
+            :value function-name))
+        (find-function-cell-function-location
+          (make-instance 'cleavir-ir:lexical-location
+            :name (gensym "find-function-cell-function"))))
+    (cleavir-ir:insert-instruction-before
+     (make-instance 'cleavir-ir:argument-instruction
+       :input zero
+       :output find-function-cell-function-location)
+     successor)
+    (cleavir-ir:insert-instruction-before
+     (make-instance 'cleavir-ir:funcall-instruction
+       :inputs (list find-function-cell-function-location function-name-input))
+     successor)
+    (cleavir-ir:insert-instruction-before
+     (make-instance 'cleavir-ir:multiple-to-fixed-instruction
+       :output output)
+     successor)))
+
 (defun transform-fdefinition-instruction
     (top-level-enter-instruction fdefinition-instruction table)
-  (let ((input (first (cleavir-ir:inputs fdefinition-instruction))))
+  (let ((input (first (cleavir-ir:inputs fdefinition-instruction)))
+               (successor (first (cleavir-ir:successors top-level-enter-instruction))))
     (if (typep input 'cleavir-ir:lexical-location)
         (let* ((assignment-instruction (first (cleavir-ir:defining-instructions input)))
                (constant-input (first (cleavir-ir:inputs assignment-instruction)))
-               (function-name (cleavir-ir:value constant-input))
-               (new (make-instance 'find-function-cell-instruction
-                      :name function-name
-                      :inputs '()
-                      :output input)))
+               (function-name (cleavir-ir:value constant-input)))
+          (insert-find-function-cell function-name input successor)
           (change-class assignment-instruction 'cleavir-ir:nop-instruction
                         :inputs '()
                         :outputs '())
-          (change-class fdefinition-instruction 'cleavir-ir:car-instruction)
-          (cleavir-ir:insert-instruction-after new top-level-enter-instruction))
-        (let* ((function-name (cleavir-ir:value input)))
+          (change-class fdefinition-instruction 'cleavir-ir:car-instruction))
+        (let ((function-name (cleavir-ir:value input)))
           (if (null (gethash function-name table))
-              (let* ((temp (cleavir-ast-to-hir:make-temp))
-                     (new (make-instance 'find-function-cell-instruction
-                            :name function-name
-                            :inputs '()
-                            :output temp)))
+              (let ((temp (cleavir-ast-to-hir:make-temp)))
+                (insert-find-function-cell function-name temp successor)
                 (change-class fdefinition-instruction 'cleavir-ir:car-instruction
                               :inputs (list temp))
-                (cleavir-ir:insert-instruction-after new top-level-enter-instruction)
                 (setf (gethash function-name table) temp))
               (change-class fdefinition-instruction 'cleavir-ir:car-instruction
                             :inputs (list (gethash function-name table))))))))
