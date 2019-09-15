2736d566ad7784118ad82c499a0fc94e52fa2d83
Robert Strandh <robert.strandh@gmail.com>
2019-09-15 12:34:14 +0200

    Catch attempt to let instructions pass, that should be processed.

--- Code/Compiler/HIR-to-MIR/generic-functions.lisp
+++ Code/Compiler/HIR-to-MIR/generic-functions.lisp
@@ -2,6 +2,30 @@
 
 (defgeneric process-instruction (client instruction))
 
+(defparameter *unprocessed-instruction-types*
+  '(or
+    cleavir-ir:enter-instruction
+    cleavir-ir:nop-instruction
+    cleavir-ir:assignment-instruction
+    sicl-ast-to-hir:breakpoint-instruction
+    cleavir-ir:funcall-instruction
+    cleavir-ir:return-instruction
+    cleavir-ir:multiple-to-fixed-instruction
+    cleavir-ir:fixed-to-multiple-instruction
+    cleavir-ir:argument-instruction
+    cleavir-ir:compute-argument-count-instruction
+    cleavir-ir:unreachable-instruction
+    cleavir-ir:catch-instruction
+    cleavir-ir:unwind-instruction
+    cleavir-ir:bind-instruction
+    cleavir-ir:eq-instruction
+    cleavir-ir:multiple-value-call-instruction
+    cleavir-ir:save-values-instruction
+    cleavir-ir:restore-values-instruction
+    ;; FIXME: this instruction should be processed
+    cleavir-ir:fixnum-divide-instruction))
+
 (defmethod process-instruction (client instruction)
-  (declare (ignore client instruction))
-  nil)
+  (declare (ignore client))
+  (unless (typep instruction *unprocessed-instruction-types*)
+    (error "Don't know how to handle instruction ~s" instruction)))
