bbc086a992cff8ded324afafc4d6545d0049f150
Robert Strandh <robert.strandh@gmail.com>
2019-09-17 13:40:29 +0200

    Untabify for better tool support.

--- Code/CLOS/generic-function-initialization-support.lisp
+++ Code/CLOS/generic-function-initialization-support.lisp
@@ -5,11 +5,11 @@
   (unless (cleavir-code-utilities:proper-list-p argument-precedence-order)
     (error "argument-precedence-order must be a proper list"))
   (unless (and (= (length argument-precedence-order)
-		  (length required-parameters))
-	       (null (set-difference argument-precedence-order
-				     required-parameters))
-	       (null (set-difference required-parameters
-				     argument-precedence-order)))
+                  (length required-parameters))
+               (null (set-difference argument-precedence-order
+                                     required-parameters))
+               (null (set-difference required-parameters
+                                     argument-precedence-order)))
     (error "argument precedence order must be a permutation~@
             of the required parameters")))
 
@@ -54,33 +54,33 @@
   (check-method-class method-class)
   (if lambda-list-p
       (let* ((parsed-lambda-list
-	       (cleavir-code-utilities:parse-generic-function-lambda-list
-		lambda-list))
-	     (required (cleavir-code-utilities:required parsed-lambda-list)))
-	(if argument-precedence-order-p
-	    (check-argument-precedence-order argument-precedence-order required)
-	    (setf argument-precedence-order required))
-	(apply call-next-method
-	       generic-function
-	       slot-names
-	       :documentation documentation
-	       :declarations declarations
-	       :argument-precedence-order argument-precedence-order
-	       :specializer-profile (make-list (length required)
-					       :initial-element nil)
-	       :method-class method-class
-	       :name name
-	       initargs))
+               (cleavir-code-utilities:parse-generic-function-lambda-list
+                lambda-list))
+             (required (cleavir-code-utilities:required parsed-lambda-list)))
+        (if argument-precedence-order-p
+            (check-argument-precedence-order argument-precedence-order required)
+            (setf argument-precedence-order required))
+        (apply call-next-method
+               generic-function
+               slot-names
+               :documentation documentation
+               :declarations declarations
+               :argument-precedence-order argument-precedence-order
+               :specializer-profile (make-list (length required)
+                                               :initial-element nil)
+               :method-class method-class
+               :name name
+               initargs))
       (if argument-precedence-order-p
-	  (error "when argument precedence order appears,~@
+          (error "when argument precedence order appears,~@
                   so must lambda list")
-	  (apply call-next-method
-		 generic-function
-		 slot-names
-		 :documentation documentation
-		 :declarations declarations
-		 :method-class method-class
-		 :name name
-		 initargs)))
+          (apply call-next-method
+                 generic-function
+                 slot-names
+                 :documentation documentation
+                 :declarations declarations
+                 :method-class method-class
+                 :name name
+                 initargs)))
   (funcall invalidate-discriminating-function generic-function)
   generic-function)
