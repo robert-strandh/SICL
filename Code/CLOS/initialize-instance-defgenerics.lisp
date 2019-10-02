e82dda216eef0db7cc7f6eb4a9f2ce61fb24c9db
Robert Strandh <robert.strandh@gmail.com>
2019-10-02 08:49:34 +0200

    Improve code layout.

--- Code/CLOS/initialize-instance-defgenerics.lisp
+++ Code/CLOS/initialize-instance-defgenerics.lisp
@@ -2,6 +2,5 @@
 
 ;;; For the specification of this generic function, see
 ;;; http://www.lispworks.com/documentation/HyperSpec/Body/f_init_i.htm#initialize-instance
-(defgeneric initialize-instance (instance
-				 &rest initargs
-				 &key &allow-other-keys))
+(defgeneric initialize-instance
+    (instance &rest initargs &key &allow-other-keys))
