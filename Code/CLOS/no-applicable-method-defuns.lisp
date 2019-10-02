ca7257d759cb47717f1e883bf8adc10703029fdb
Robert Strandh <robert.strandh@gmail.com>
2019-10-02 09:07:24 +0200

    Remove obsolete component.

--- Code/CLOS/no-applicable-method-defuns.lisp
+++
@@ -1,5 +0,0 @@
-(cl:in-package #:sicl-clos)
-
-(defun no-applicable-method (generic-function &rest function-arguments)
-  (error "no applicable method on generic function ~s with arguments: ~s"
-	 generic-function function-arguments))
