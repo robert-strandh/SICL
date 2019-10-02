074c18c827eaf0a3f5f838047b26a122b7a176c8
Robert Strandh <robert.strandh@gmail.com>
2019-10-02 08:56:52 +0200

    Improve code layout.

--- Code/CLOS/initialize-instance-defmethods.lisp
+++ Code/CLOS/initialize-instance-defmethods.lisp
@@ -1,6 +1,5 @@
 (cl:in-package #:sicl-clos)
 
-(defmethod initialize-instance ((instance standard-object)
-				&rest initargs
-				&key &allow-other-keys)
+(defmethod initialize-instance
+    ((instance standard-object) &rest initargs &key &allow-other-keys)
   (apply #'initialize-instance-default instance initargs))
