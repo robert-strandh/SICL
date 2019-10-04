4e9bb8c16fbc6711ac1de7565c0cd87417a3e12a
Robert Strandh <robert.strandh@gmail.com>
2019-10-04 14:35:21 +0200

    Compile file containing definition of function LIST*.

--- Code/Boot/Phase-0/compile-files.lisp
+++ Code/Boot/Phase-0/compile-files.lisp
@@ -37,6 +37,7 @@
     (cf "Cons/accessor-defuns.lisp")
     (cf "Cons/cxr.lisp")
     (cf "Cons/setf-cxr.lisp")
+    (cf "Cons/list-star-defun.lisp")
     (cf "Sequence/sequence-defclass.lisp")
     (cf "Package-and-symbol/symbol-defclass.lisp")
     (cf "Arithmetic/number-defclass.lisp")
