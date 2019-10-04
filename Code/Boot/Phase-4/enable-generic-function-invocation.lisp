a24e054a57a404cb4dcc82703f20856a65eefd43
Robert Strandh <robert.strandh@gmail.com>
2019-10-04 08:42:14 +0200

    Load SICL versions of list accessor functions into E4 rather than importing them from the host.

--- Code/Boot/Phase-4/enable-generic-function-invocation.lisp
+++ Code/Boot/Phase-4/enable-generic-function-invocation.lisp
@@ -37,7 +37,9 @@
   ;; one with a particular name.  SUBSEQ is used to extract the
   ;; required arguments from a list of all the arguments to a
   ;; generic function.
-  (import-functions-from-host '(list* car cadr caddr cdddr find subseq) e4)
+  (import-functions-from-host '(list* find subseq) e4)
+  (load-fasl "Cons/accessor-defuns.fasl" e4)
+  (load-fasl "Cons/cxr.fasl" e4)
   (load-fasl "CLOS/compute-discriminating-function-support.fasl" e4)
   (import-functions-from-host
    '(sicl-clos::add-path
