bd4dbdff846c11feb0d64ab14c8f565233e75de6
Robert Strandh <robert.strandh@gmail.com>
2019-10-05 11:47:07 +0200

    Remove obsolete definition of function APPEND.

--- Code/Cons-high/cons-high.lisp
+++ Code/Cons-high/cons-high.lisp
@@ -18,62 +18,6 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;
-;;; Function append
-
-;;; We need the append function in macro expanders in this module
-;;; so we define it early.  
-
-;;; It used to be the case that the append function was defined
-;;; in terms of (loop ... append ...), but it turns out that
-;;; some implementations have buggy versions of (loop ... append ...) 
-;;; so it is better to just define it in terms of a lower-level
-;;; construct.  Doing it this way allows for implementations with 
-;;; a buggy loop to still use this module without first replacing
-;;; loop by ours. 
-
-(eval-when (:compile-toplevel :load-toplevel :execute)
-  (defun append (&rest lists)
-    (if (null lists)
-	;; The standard doesn't say explicity that calling append with
-	;; zero arguments is allowed and returns nil, but one of the
-	;; examples on the HyperSpec page for append suggests that. 
-	'()
-	;; We need to copy every list except the last one.  Also,
-	;; every list except the last one must be a proper list.  In
-	;; order to avoid making a test in each iteration of the loop,
-	;; we use a sentinel, which is a cons cell whose cdr contains
-	;; the real result that we return in the end. 
-	(let* ((sentinel (list nil))
-	       ;; The variable last points to the last cons cell of the
-	       ;; resulting list to be accumulated. 
-	       (last sentinel))
-	  (loop until (null (cdr lists))
-		do (loop with list = (car lists)
-			 do (cond ((consp list)
-				   ;; There are more cells, copy the first one.
-				   (setf (cdr last) (list (car list)))
-				   (setf last (cdr last))
-				   (setf list (cdr list)))
-				  ((null list)
-				   ;; The list is a proper list and we reached the
-				   ;; end of it.
-				   (loop-finish))
-				  (t
-				   ;; The list is a dotted list
-				   (error 'must-be-proper-list
-					  :datum (car lists)
-					  :name 'append))))
-		   ;; We are through with the first list in lists
-		   (setf lists (cdr lists)))
-	  ;; When we get here, there is only one list left in lists.
-	  ;; And in fact, it doesn't have to be a list at all. 
-	  ;; Attach it to the end of what we have accumulated so far.
-	  (setf (cdr last) (car lists))
-	  ;; Skip the sentinel and return the rest. 
-	  (cdr sentinel)))))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;;
 ;;; Function mapcar
 ;;;
 ;;; The compiler macro is defined later because it uses the append
