53fdee0c248fbaa96d44b2180967ba2177948f3a
Robert Strandh <robert.strandh@gmail.com>
2019-10-06 15:45:16 +0200

    Move definition of function NTH to a separate component.

--- Code/Cons-high/cons-high.lisp
+++ Code/Cons-high/cons-high.lisp
@@ -37,25 +37,6 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;
-;;; Function nth
-
-(defun nth (n list)
-  (unless (typep n '(integer 0))
-    (error 'must-be-nonnegative-integer
-	   :datum n
-	   :name 'nthcdr))
-  (loop until (zerop n)
-	until (atom list)
-	do (decf n)
-	do (setf list (cdr list)))
-  (when (not (listp list))
-    (error 'must-be-list
-	   :datum list
-	   :name 'nth))
-  (car list))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;;
 ;;; Setf expander and setf function for nth
 
 (eval-when (:compile-toplevel :load-toplevel :execute)
