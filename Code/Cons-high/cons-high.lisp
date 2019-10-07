374cb882ad12e822c12e7a20aa6a7111af85cd28
Robert Strandh <robert.strandh@gmail.com>
2019-10-07 11:47:56 +0200

    Remove obsolete code for macros WITH-PROPER-LIST-RESTS/ELEMENTS.

--- Code/Cons-high/cons-high.lisp
+++ Code/Cons-high/cons-high.lisp
@@ -37,32 +37,6 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;
-;;; Some helper macros
-
-(defmacro with-proper-list-rests ((rest-var list function-name) &body body)
-  (let ((list-var (gensym)))
-    `(loop with ,list-var = ,list
-	   for ,rest-var on ,list-var
-	   do ,@body
-	   finally (unless (null ,rest-var)
-		     (error 'must-be-proper-list
-			    :datum ,list-var
-			    :name ,function-name)))))
-
-(defmacro with-proper-list-elements ((element-var list function-name) &body body)
-  (let ((list-var (gensym))
-	(rest-var (gensym)))
-    `(loop with ,list-var = ,list
-	   for ,rest-var on ,list-var
-	   for ,element-var = (car ,rest-var)
-	   do ,@body
-	   finally (unless (null ,rest-var)
-		     (error 'must-be-proper-list
-			    :datum ,list-var
-			    :name ,function-name)))))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;;
 ;;; Function member
 
 (defun |member test=eq key=identity| (name item list)
