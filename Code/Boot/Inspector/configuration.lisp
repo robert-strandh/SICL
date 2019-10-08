3c919a73319d48556a02d9bc79b1f233d78527fa
Robert Strandh <robert.strandh@gmail.com>
2019-10-08 16:09:48 +0200

    Configure the presentation of collapsed ersatz objects.

--- Code/Boot/Inspector/configuration.lisp
+++ Code/Boot/Inspector/configuration.lisp
@@ -13,3 +13,26 @@
           (sicl-simple-environment::unbound object))
          (format stream "UNBOUND"))
         (t (format stream "FUNCTION"))))
+
+(defun object-type (object)
+  (let ((class-of-object (slot-value object 'sicl-boot-phase-3::%class)))
+    (if (typep class-of-object 'sicl-boot-phase-3::header)
+        'pure-ersatz 'impure-ersatz)))
+
+(defun object-type-name (object)
+  (ecase (object-type object)
+    (pure-ersatz "Ersatz (pure)")
+    (impure-ersatz "Ersatz (impure)")))
+
+(defun class-name-of-object (object)
+  (let ((class-of-object (slot-value object 'sicl-boot-phase-3::%class)))
+    (if (typep class-of-object 'sicl-boot-phase-3::header)
+        (funcall (sicl-genv:fdefinition 'class-name (sicl-boot::e4 *boot*)) class-of-object)
+        (funcall (sicl-genv:fdefinition 'class-name (sicl-boot::e3 *boot*)) class-of-object))))
+
+(defmethod clouseau:inspect-object-using-state
+    ((object sicl-boot-phase-3::header)
+     (state clouseau:inspected-object)
+     (style (eql :collapsed))
+     (stream t))
+  (format stream "~a [~a]" (class-name-of-object object) (object-type-name object)))
