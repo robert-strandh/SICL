74857bf2e85f412b2b4a60816e99192512600584
Robert Strandh <robert.strandh@gmail.com>
2019-10-09 08:21:11 +0200

    Move definition of SETF expander for GETF to a separate component.

--- Code/Cons-high/cons-high.lisp
+++ Code/Cons-high/cons-high.lisp
@@ -34,29 +34,3 @@
   (if (eql n 1)
       `(last-1 ,list)
       form))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;;
-;;; Setf expander for getf
-
-(define-setf-expander getf (place indicator &optional default &environment env)
-  (let ((indicator-var (gensym))
-	(default-var (gensym))
-	(value-var (gensym)))
-    (multiple-value-bind (vars vals store-vars writer-form reader-form)
-	(get-setf-expansion place env)
-      (values (append vars (list indicator-var default-var))
-	      (append vals (list indicator default))
-	      (list value-var)
-	      `(let ((,default-var ,default-var))
-		 (declare (ignore ,default-var))
-		 (loop for rest on ,reader-form by #'cddr
-		       when (eq (car rest) ,indicator-var)
-			 do (setf (cadr rest) ,value-var)
-			    (return nil)
-		       finally (let ((,(car store-vars)
-				      (list* ,indicator-var ,value-var ,reader-form)))
-				 ,writer-form))
-		 ,value-var)
-	      `(getf ,reader-form ,indicator-var ,default)))))
-[
