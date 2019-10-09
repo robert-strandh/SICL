3ab92b8ab8fec461dc6c10860b5b4756fae34a3e
Robert Strandh <robert.strandh@gmail.com>
2019-10-09 08:15:20 +0200

    Move definition of function COPY-ALIST to a separate component.

--- Code/Cons-high/cons-high.lisp
+++ Code/Cons-high/cons-high.lisp
@@ -45,33 +45,6 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;
-;;; Function copy-alist
-
-;;; The HyperSpec says that the argument is an alist, and 
-;;; in those cases, whenever the type doesn't correspond, it
-;;; is legitimate to signal an error.  However, for copy-alist,
-;;; the HyperSpec also says that any object that is referred to
-;;; directly or indirectly is still shared betwee the argument
-;;; and the resulting list, which suggests that any element that
-;;; is not a cons should just be included in the resulting
-;;; list as well.  And that is what we are doing. 
-
-;;; We use (loop for ... on ...) because it uses ATOM to test for
-;;; the end of the list.  Then we check that the atom is really 
-;;; null, and if not, signal a type error.
-
-(defun copy-alist (alist)
-  (loop for remaining on alist
-	collect (if (consp (car remaining))
-		    (cons (caar remaining) (cdar remaining))
-		    (car remaining))
-	finally (unless (null remaining)
-		  (error 'must-be-proper-list
-			 :datum alist
-			 :name 'copy-alist))))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;;
 ;;; Function tailp
 
 ;;; We could use (loop for ... on ...) here for consistency. 
