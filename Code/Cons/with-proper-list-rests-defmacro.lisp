88ef53a4a1c78887e9f0911d5af4c4ce6d4dfa41
Robert Strandh <robert.strandh@gmail.com>
2019-10-13 09:43:17 +0200

    Traverse the list using FOR ... = ... THEN instead of FOR ... ON.

--- Code/Cons/with-proper-list-rests-defmacro.lisp
+++ Code/Cons/with-proper-list-rests-defmacro.lisp
@@ -3,7 +3,8 @@
 (defmacro with-proper-list-rests ((rest-var list function-name) &body body)
   (let ((list-var (gensym)))
     `(loop with ,list-var = ,list
-           for ,rest-var on ,list-var
+           for ,rest-var = ,list-var then (rest ,rest-var)
+           until (atom ,rest-var)
            do ,@body
            finally (unless (null ,rest-var)
                      (error 'must-be-proper-list
