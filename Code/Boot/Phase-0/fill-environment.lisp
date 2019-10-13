feda71962e969fa6658b78257485f32c9475bf84
Robert Strandh <robert.strandh@gmail.com>
2019-10-13 10:50:25 +0200

    Load a file containing the definition of the macro WITH-PROPER-LIST-ELEMENTS.

--- Code/Boot/Phase-0/fill-environment.lisp
+++ Code/Boot/Phase-0/fill-environment.lisp
@@ -119,6 +119,10 @@
     ;; WITH-PROPER-LIST-RESTS used by the functions MEMBER, MEMBER-IF,
     ;; and MEMBER-IF-NOT.
     (ld "Cons/with-proper-list-rests-defmacro.lisp")
+    ;; Load a file containing the definition of the macro
+    ;; WITH-PROPER-LIST-ELEMENTS used by several functions such as
+    ;; SET-DIFFERENCE, UNION, etc.
+    (ld "Cons/with-proper-list-elements-defmacro.lisp")
     ;; Load a file containing the definition of the macro DEFCLASS.
     (ld "CLOS/defclass-defmacro.lisp")
     ;; Load a file containing the definition of the macro DEFGENERIC.
