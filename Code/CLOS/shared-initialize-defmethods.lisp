685c0d9f2ace478215564685d3461439ea41a493
Robert Strandh <robert.strandh@gmail.com>
2019-10-14 19:58:46 +0200

    Improve code layout.

--- Code/CLOS/shared-initialize-defmethods.lisp
+++ Code/CLOS/shared-initialize-defmethods.lisp
@@ -1,6 +1,5 @@
 (cl:in-package #:sicl-clos)
 
-(defmethod shared-initialize ((instance standard-object)
-			      slot-names
-			      &rest initargs)
+(defmethod shared-initialize
+    ((instance standard-object) slot-names &rest initargs)
   (apply #'shared-initialize-default instance slot-names initargs))
