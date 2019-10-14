7eb648297a0a19e782f408526d93e6505955a8b3
Robert Strandh <robert.strandh@gmail.com>
2019-10-14 07:08:35 +0200

    Remove components containing code for obsolete function METHOD-COMBINATION-COMPUTE-EFFECTIVE-METHOD.

--- Code/CLOS/method-combination-compute-effective-method-defmethods.lisp
+++
@@ -1,7 +0,0 @@
-(cl:in-package #:sicl-clos)
-
-(defmethod method-combination-compute-effective-method
-    (method-combination methods)
-  ;; FIXME: specialize to standard-method-combination
-  (declare (ignore method-combination))
-  (compute-effective-method-default methods))
--- Code/CLOS/method-combination-compute-effective-method-defuns.lisp
+++
@@ -1,6 +0,0 @@
-(cl:in-package #:sicl-clos)
-
-(defun method-combination-compute-effective-method
-    (method-combination methods)
-  (declare (ignore method-combination))
-  (compute-effective-method-default methods))
