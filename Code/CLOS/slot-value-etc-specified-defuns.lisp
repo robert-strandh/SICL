35d8fb89291d2dafcac7a32638c51c8cc1156248
Robert Strandh <robert.strandh@gmail.com>
2019-10-14 20:14:30 +0200

    Introduce a macro to capture commonalities between functions.

--- Code/CLOS/slot-value-etc-specified-defuns.lisp
+++ Code/CLOS/slot-value-etc-specified-defuns.lisp
@@ -1,34 +1,24 @@
 (cl:in-package #:sicl-clos)
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;;
-;;; SLOT-EXISTS-P.
-
-(defun slot-exists-p (object slot-name)
-  (not (null (find-slot object slot-name))))
+(defmacro with-existing-slot
+    ((object-var slot-name-var slot-var operation) &body body)
+  `(let ((,slot-var (find-slot ,object-var slot-name-var)))
+     (if (null ,slot-var)
+         (slot-missing (class-of ,object-var) ,slot-name-var ',operation)
+         (progn ,@body))))
 
 (defun slot-value (object slot-name)
-  (let ((slot (find-slot object slot-name))
-	(class (class-of object)))
-    (if (null slot)
-	(slot-missing class object slot-name 'slot-value)
-	(slot-value-using-class class object slot))))
+  (with-existing-slot (object slot-name slot slot-value)
+    (slot-value-using-class (class-of object) class object slot)))
 
 (defun (setf slot-value) (new-value object slot-name)
-  (let ((slot (find-slot object slot-name))
-	(class (class-of object)))
-    (if (null slot)
-	(slot-missing class object slot-name 'slot-value)
-	(setf (slot-value-using-class class object slot) new-value))))
+  (with-existing-slot (object slot-name slot setf)
+    (setf (slot-value-using-class (class-of object) object slot) new-value)))
 
 (defun slot-boundp (object slot-name)
-  (let ((slot (find-slot object slot-name))
-	(class (class-of object)))
-    (slot-boundp-using-class class object slot)))
+  (with-existing-slot (object slot-name slot slot-boundp)
+    (slot-boundp-using-class (class-of object) object slot)))
 
 (defun slot-makunbound (object slot-name)
-  (let ((slot (find-slot object slot-name))
-	(class (class-of object)))
-    (if (null slot)
-	(slot-missing class object slot-name 'slot-makunbound)
-	(slot-makunbound-using-class class object slot))))
+  (with-existing-slot (object slot-name slot slot-makunbound)
+    (slot-makunbound-using-class (class-of object) object slot)))
