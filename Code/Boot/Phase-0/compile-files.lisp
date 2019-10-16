3b2e1208cb6d20dde284720d6741b3d675383e7a
Robert Strandh <robert.strandh@gmail.com>
2019-10-16 20:48:13 +0200

    Compile file containing definition of macro DEFUN.

--- Code/Boot/Phase-0/compile-files.lisp
+++ Code/Boot/Phase-0/compile-files.lisp
@@ -194,5 +194,6 @@
     (cf "Data-and-control-flow/setf.lisp")
     (cf "Data-and-control-flow/get-setf-expansion.lisp")
     (cf "Data-and-control-flow/eq-defun.lisp")
+    (cf "Data-and-control-flow/defun-defmacro.lisp")
     (cf "Conditionals/macros.lisp")
     (cf "Boot/Phase-3/sub-specializer-p.lisp")))
