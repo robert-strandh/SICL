a1f681f59763687358cf25264d63d1340eeb214d
Robert Strandh <robert.strandh@gmail.com>
2019-10-16 20:48:39 +0200

    Load FASL file containing definition of macro DEFUN into E5.

--- Code/Boot/Phase-6/load-data-and-control-flow-code.lisp
+++ Code/Boot/Phase-6/load-data-and-control-flow-code.lisp
@@ -1,4 +1,5 @@
 (cl:in-package #:sicl-boot-phase-6)
 
 (defun load-data-and-control-flow-code (e5)
-  (load-fasl "Data-and-control-flow/eq-defun.fasl" e5))
+  (load-fasl "Data-and-control-flow/eq-defun.fasl" e5)
+  (load-fasl "Data-and-control-flow/defun-defmacro.fasl" e5))
