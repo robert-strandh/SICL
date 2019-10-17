9c00efc80bb40e86f5ba48d30b8fcf44761d4b83
Robert Strandh <robert.strandh@gmail.com>
2019-10-17 12:59:36 +0200

    Load a file containing definitions of macro RETURN.

--- Code/Boot/Phase-6/load-data-and-control-flow-code.lisp
+++ Code/Boot/Phase-6/load-data-and-control-flow-code.lisp
@@ -11,4 +11,5 @@
   (load-fasl "Data-and-control-flow/functionp-defmethods.fasl" e5)
   (load-fasl "Data-and-control-flow/defconstant-defmacro.fasl" e5)
   (load-fasl "Data-and-control-flow/defparameter-defmacro.fasl" e5)
-  (load-fasl "Data-and-control-flow/defvar-defmacro.fasl" e5))
+  (load-fasl "Data-and-control-flow/defvar-defmacro.fasl" e5)
+  (load-fasl "Data-and-control-flow/return-defmacro.fasl" e5))
