8c6c80c28d37334b6ec201864a152b1b33bcc95f
Robert Strandh <robert.strandh@gmail.com>
2019-10-17 08:08:37 +0200

    Load files containing definitions of functions FDEFINITION and (SETF FDEFINITION).

--- Code/Boot/Phase-6/load-data-and-control-flow-code.lisp
+++ Code/Boot/Phase-6/load-data-and-control-flow-code.lisp
@@ -2,4 +2,6 @@
 
 (defun load-data-and-control-flow-code (e5)
   (load-fasl "Data-and-control-flow/eq-defun.fasl" e5)
-  (load-fasl "Data-and-control-flow/defun-defmacro.fasl" e5))
+  (load-fasl "Data-and-control-flow/defun-defmacro.fasl" e5)
+  (load-fasl "Data-and-control-flow/fdefinition-defun.fasl" e5)
+  (load-fasl "Data-and-control-flow/setf-fdefinition-defun.fasl" e5))
