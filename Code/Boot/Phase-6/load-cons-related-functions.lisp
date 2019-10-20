0adc89dfc0b76d7775b9a2274d79644fc26a5089
Robert Strandh <robert.strandh@gmail.com>
2019-10-20 18:08:03 +0200

    Load a file containing the definition of the function INTERSECTION.

--- Code/Boot/Phase-6/load-cons-related-functions.lisp
+++ Code/Boot/Phase-6/load-cons-related-functions.lisp
@@ -5,6 +5,7 @@
   (load-fasl "Cons/list-defun.fasl" e5)
   (load-fasl "Cons/list-star-defun.fasl" e5)
   (load-fasl "Cons/make-list-defun.fasl" e5)
+  (load-fasl "Cons/intersection-defun.fasl" e5)
   (load-fasl "Cons/set-difference-defun.fasl" e5)
   (load-fasl "Cons/endp-defun.fasl" e5)
   (load-fasl "Cons/member-defun.fasl" e5)
