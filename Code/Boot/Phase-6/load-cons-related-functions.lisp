35899eaa88552957fa591c7784f56df364de78dc
Robert Strandh <robert.strandh@gmail.com>
2019-10-21 10:35:56 +0200

    Load a file containing the definition of the function MAPL.

--- Code/Boot/Phase-6/load-cons-related-functions.lisp
+++ Code/Boot/Phase-6/load-cons-related-functions.lisp
@@ -27,4 +27,5 @@
   (load-fasl "Cons/mapcar-defun.fasl" e5)
   (load-fasl "Cons/mapcan-defun.fasl" e5)
   (load-fasl "Cons/mapc-defun.fasl" e5)
-  (load-fasl "Cons/mapcon-defun.fasl" e5))
+  (load-fasl "Cons/mapcon-defun.fasl" e5)
+  (load-fasl "Cons/mapl-defun.fasl" e5))
