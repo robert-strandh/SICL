42da95eb3547b0d0d05182e79f7ef3219a096d76
Robert Strandh <robert.strandh@gmail.com>
2019-10-21 07:57:30 +0200

    Compile a file containing the definition of the function MAPC.

--- Code/Boot/Phase-0/compile-files.lisp
+++ Code/Boot/Phase-0/compile-files.lisp
@@ -43,6 +43,7 @@
     (cf "Cons/list-length-defun.lisp")
     (cf "Cons/mapcar-defun.lisp")
     (cf "Cons/mapcan-defun.lisp")
+    (cf "Cons/mapc-defun.lisp")
     (cf "Cons/member-defun.lisp")
     (cf "Cons/intersection-defun.lisp")
     (cf "Cons/set-difference-defun.lisp")
