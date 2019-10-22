cec754a9a9dcd3de333c11ad304312405441b08f
Robert Strandh <robert.strandh@gmail.com>
2019-10-22 06:46:48 +0200

    Extract function DESIGNATED-LIST-OF-SYMBOLS to a separate component.

--- Code/Package-and-symbol/functions.lisp
+++ Code/Package-and-symbol/functions.lisp
@@ -6,22 +6,6 @@
 (defun proper-list-p (list)
   (integerp (ignore-errors (list-length list))))
 
-;;; Recall that a designator for a list of symbols is either a non-nil
-;;; symbol (denoting a singleton list whose element is is that non-nil
-;;; symbol, or a proper list of symbols, denoting itself.  Thus the
-;;; symbol NIL denotes the empty list of symbols.
-;;; FIXME: check for conflicts
-(defun designated-list-of-symbols (designator)
-  (cond ((null designator) '())
-        ((symbolp designator) (list designator))
-        ((and (proper-list-p designator)
-              (every #'symbolp designator))
-         designator)
-        (t
-         (error 'not-a-valid-designator-for-list-of-symbols
-                :exptected-type 'designator-for-list-of-symbols
-                :datum designator))))
-
 (defun export (symbols-designator &optional package-designator *package*)
   (let ((package (package-designator-to-package package-designator))
         (symbols (designated-list-of-symbols symbols-designator)))
