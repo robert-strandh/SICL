60ce2bd362d29447aae14b6dc1104c9b0a620a1b
Robert Strandh <robert.strandh@gmail.com>
2019-10-22 06:31:36 +0200

    Extract macro DO-EXTERNAL-SYMBOLS to a separate component.

--- Code/Package-and-symbol/functions.lisp
+++ Code/Package-and-symbol/functions.lisp
@@ -4,28 +4,6 @@
 (defparameter *package* nil)
 
 ;;; FIXME: check syntax better
-(defmacro do-external-symbols ((symbol-variable
-				&optional
-				  (package-form '*package*)
-				  (result-form 'nil))
-			       &body body)
-  (let ((function-name (gensym))
-	(package-var (gensym))
-	(remaining-body body)
-	(declarations '()))
-    (loop while (eq (caar remaining-body) 'declare)
-	  do (push (pop remaining-body) declarations))
-    `(block nil
-       (let ((,function-name (lambda (,symbol-variable)
-			       (locally ,@(reverse declarations)
-				 (tagbody
-				    ,@remaining-body))))
-	     (,package-var (package-designator-to-package ,package-form)))
-	 (mapc ,function-name
-	       (external-symbols ,package-var)))
-       ,result-form)))
-
-;;; FIXME: check syntax better
 (defmacro do-symbols ((symbol-variable
 		       &optional
 			 (package-form '*package*)
