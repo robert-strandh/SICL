314bfb847ec207ee3b156b2595fdd939b7ca35c9
Robert Strandh <robert.strandh@gmail.com>
2019-10-22 06:49:33 +0200

    Extract function EXPORT to a separate component.

--- Code/Package-and-symbol/functions.lisp
+++ Code/Package-and-symbol/functions.lisp
@@ -2,10 +2,3 @@
 
 ;;; FIXME: it should be the CL-USER package.
 (defparameter *package* nil)
-
-(defun export (symbols-designator &optional package-designator *package*)
-  (let ((package (package-designator-to-package package-designator))
-        (symbols (designated-list-of-symbols symbols-designator)))
-    (loop for symbol in symbols
-          do (export-one-symbol symbol package)))
-  t)
