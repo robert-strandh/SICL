13e49f901ef9837bb2843425d9a1694216f1d9c2
Robert Strandh <robert.strandh@gmail.com>
2019-10-22 06:23:10 +0200

    Extract function MAKE-PACKAGE to a separate component.

--- Code/Package-and-symbol/functions.lisp
+++ Code/Package-and-symbol/functions.lisp
@@ -1,36 +1,5 @@
 (cl:in-package #:sicl-package)
 
-;;; FIXME: signal correctable errors.
-;;; FIXME: check that nicknames is a proper list of string designators
-;;; FIXME: check that use is a proper list of package designators.
-(defun make-package (name &key nicknames use)
-  (declare (type string-designator name))
-  (let ((existing-package (find-package name)))
-    (loop until (null existing-package)
-          do (restart-case (error 'package-already-exists
-                                  :name existing-package)
-               (force (stuff)
-                 :report (lambda (stream)
-                           (format stream
-                                   "Replace the existing package."))
-                 (setf existing-package nil))
-               (change-name (new-package-name)
-                 :report (lambda (stream)
-                           (format stream
-                                   "Make a package with a different name."))
-                 :interactive (lambda ()
-                                (format *query-io* "Enter new name: ")
-                                (list (read *query-io*)))
-                 (setf existing-package (find-package new-package-name))))))
-  (loop for rest = nicknames then (cdr rest)
-	while (consp rest)
-	do (assert (null (find-package (car rest)))))
-  (let ((package (make-instance 'package)))
-    (setf (name package) (string name))
-    (setf (nicknames package) (copy-list nicknames))
-    (use-package use package)
-    package))
-
 (defun package-name (package-designator)
   (let ((package (package-designator-to-package package-designator)))
     (name package)))
