acc6bd3e4cfe718e2819f02d95356372b0d1a94d
Robert Strandh <robert.strandh@gmail.com>
2019-10-23 07:29:43 +0200

    Provide partial documentation string for EXPORT.

--- Code/Package-and-symbol/documentation-strings-english.lisp
+++ Code/Package-and-symbol/documentation-strings-english.lisp
@@ -1,4 +1,21 @@
 (in-package #:sicl-package)
 
-;;placeholder
-(+ 2 3)
+(eval-when (:compile-toplevel :load-toplevel :execute)
+  (defun fmt (&rest args)
+    (apply #'format nil args)))
+
+;;; Create documentation for a function.
+(defun fundoc (name string)
+  (setf (documentation name 'function) string)
+  (setf (documentation (fdefinition name) 'function)
+        (documentation name 'function)))
+
+;;; FIXME: Add more text.
+(fundoc 'export
+        (fmt "Lambda list: (symbols &optional package)~@
+              SYMBOLS is a designator for a list of symbols.~@
+              PACKAGE is a package designnator.~@
+              The default value for PACKAGE is the current package.~@
+              This function makes the designated symbols external~@
+              in PACKAGE.~@
+              "))
