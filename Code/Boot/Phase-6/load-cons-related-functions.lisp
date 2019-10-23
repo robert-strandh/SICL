52fabda7bb3254fada23f1338d7323f01c3bf6d7
Robert Strandh <robert.strandh@gmail.com>
2019-10-23 07:31:48 +0200

    Load a file containing the definition of the function NSUBST-IF.

--- Code/Boot/Phase-6/load-cons-related-functions.lisp
+++ Code/Boot/Phase-6/load-cons-related-functions.lisp
@@ -39,4 +39,5 @@
   (load-fasl "Cons/mapl-defun.fasl" e5)
   (load-fasl "Cons/maplist-defun.fasl" e5)
   (load-fasl "Cons/nsublis-defun.fasl" e5)
-  (load-fasl "Cons/nsubst-defun.fasl" e5))
+  (load-fasl "Cons/nsubst-defun.fasl" e5)
+  (load-fasl "Cons/nsubst-if-defun.fasl" e5))
