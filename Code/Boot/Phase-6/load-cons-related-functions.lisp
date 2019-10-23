f50c641ba774ca8132067e23da73ce00f9540bb5
Robert Strandh <robert.strandh@gmail.com>
2019-10-23 10:16:31 +0200

    Load a file containing the definition of the function SUBST.

--- Code/Boot/Phase-6/load-cons-related-functions.lisp
+++ Code/Boot/Phase-6/load-cons-related-functions.lisp
@@ -47,6 +47,7 @@
   (load-fasl "Cons/maplist-defun.fasl" e5)
   (load-fasl "Cons/sublis-defun.fasl" e5)
   (load-fasl "Cons/nsublis-defun.fasl" e5)
+  (load-fasl "Cons/subst-defun.fasl" e5)
   (load-fasl "Cons/nsubst-defun.fasl" e5)
   (load-fasl "Cons/nsubst-if-defun.fasl" e5)
   (load-fasl "Cons/nsubst-if-not-defun.fasl" e5)
