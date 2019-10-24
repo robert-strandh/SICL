5e11ee43d31d142116b4125869b85be961703ba5
Robert Strandh <robert.strandh@gmail.com>
2019-10-24 10:22:41 +0200

    Extract compiler macro for function CHAR> to a separate component.

--- Code/Character/char-greaterp-1-define-compiler-macro.lisp
+++ Code/Character/char-greaterp-1-define-compiler-macro.lisp
@@ -1,25 +1,6 @@
 (cl:in-package #:sicl-character)
- 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;;
-;;; Function CHAR>=
 
-(defun char>= (&rest characters)
-  (when (null characters)
-    (error 'program-error))
-  (if (null (cdr characters))
-      t
-      (loop for rest = (cdr characters) then (cdr rest)
-            while (consp rest)
-            for char1 = (car characters) then char2
-            for char2 = (car rest)
-            unless (binary-char>= char1 char2)
-              return nil
-            finally (return t))))
- 
-(proclaim '(notinline char>=))
-
-(define-compiler-macro char>= (&whole form &rest arguments)
+(define-compiler-macro char> (&whole form &rest arguments)
   (cond ((not (and (cleavir-code-utilities:proper-list-p arguments)
                (>= (length arguments) 1)))
          form)
@@ -33,4 +14,4 @@
                              while (consp rest)
                              for var1 = (car vars) then var2
                              for var2 = (car rest)
-                             collect `(binary-char>= ,var1 ,var2))))))))
+                             collect `(binary-char> ,var1 ,var2))))))))
