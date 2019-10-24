35b460f99dc693b8b3fce229fa4ef3ac2621b482
Robert Strandh <robert.strandh@gmail.com>
2019-10-24 10:38:20 +0200

    Extract function CHAR>= to a separate component.

--- Code/Character/character.lisp
+++ Code/Character/character.lisp
@@ -1,23 +1,4 @@
 (cl:in-package #:sicl-character)
- 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;;
-;;; Function CHAR>=
-
-(defun char>= (&rest characters)
-  (when (null characters)
-    (error 'program-error))
-  (if (null (cdr characters))
-      t
-      (loop for rest = (cdr characters) then (cdr rest)
-            while (consp rest)
-            for char1 = (car characters) then char2
-            for char2 = (car rest)
-            unless (binary-char>= char1 char2)
-              return nil
-            finally (return t))))
- 
-(proclaim '(notinline char>=))
 
 (define-compiler-macro char>= (&whole form &rest arguments)
   (cond ((not (and (cleavir-code-utilities:proper-list-p arguments)
