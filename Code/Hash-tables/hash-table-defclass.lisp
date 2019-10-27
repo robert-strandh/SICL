17dde564b32c40ef4c33dfaec6b52d4041174aae
Robert Strandh <robert.strandh@gmail.com>
2019-10-27 15:47:19 +0100

    Add slots for REHASH-SIZE and REHASH-THRESHOLD.

--- Code/Hash-tables/hash-table-defclass.lisp
+++ Code/Hash-tables/hash-table-defclass.lisp
@@ -1,5 +1,7 @@
 (cl:in-package #:sicl-hash-table)
 
 (defclass hash-table (t)
-  ((%test :initarg :test :reader test))
+  ((%test :initarg :test :reader hash-table-test)
+   (%rehash-size :initarg :rehash-size :reader hash-table-rehash-size)
+   (%rehash-threshold :initarg :rehash-threshold :reader hash-table-rehash-threshold))
   (:metaclass built-in-class))
