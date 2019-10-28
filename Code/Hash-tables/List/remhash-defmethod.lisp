8ae8434463f23ea62c082d14d3d1b9a541060f08
Robert Strandh <robert.strandh@gmail.com>
2019-10-28 11:21:38 +0100

    Bug fix: include omitted instance argument to WITH-ACCESSORS.

--- Code/Hash-tables/List/remhash-defmethod.lisp
+++ Code/Hash-tables/List/remhash-defmethod.lisp
@@ -3,10 +3,11 @@
 (defmethod remhash (key (hash-table list-hash-table))
   (with-accessors ((contents contents)
                    (test hash-table-test))
-      (let ((position (position key contents :key #'car :test test)))
-        (if (null position)
-            nil
-            (progn (setf contents
-                         (nconc (subseq contents 0 position)
-                                (nthcdr (1+ position) contents)))
-                   t)))))
+      hash-table
+    (let ((position (position key contents :key #'car :test test)))
+      (if (null position)
+          nil
+          (progn (setf contents
+                       (nconc (subseq contents 0 position)
+                              (nthcdr (1+ position) contents)))
+                 t)))))
