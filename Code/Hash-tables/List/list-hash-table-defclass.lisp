faa28bb6fe892948c6008d0ac6700d07c8e24764
Robert Strandh <robert.strandh@gmail.com>
2019-10-28 11:19:25 +0100

    Fix typo.

--- Code/Hash-tables/List/list-hash-table-defclass.lisp
+++ Code/Hash-tables/List/list-hash-table-defclass.lisp
@@ -1,6 +1,6 @@
 (cl:in-package #:sicl-list-hash-table)
 
 (defclass list-hash-table (hash-table)
-  ((%size :initform 0 :initarg :size :acessor size :reader hash-table-size)
+  ((%size :initform 0 :initarg :size :accessor size :reader hash-table-size)
    (%contents :initform '() :accessor contents))
   (:metaclass built-in-class))
