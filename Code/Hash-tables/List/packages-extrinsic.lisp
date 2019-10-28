c13f09f4fb0adcb898284ea41f395622ee1246f3
Robert Strandh <robert.strandh@gmail.com>
2019-10-28 11:21:02 +0100

    New component containing definition of extrinsic package for the list-hash-table system.

---
+++ Code/Hash-tables/List/packages-extrinsic.lisp
@@ -0,0 +1,14 @@
+(cl:in-package #:common-lisp-user)
+
+(defpackage #:sicl-list-hash-table
+  (:shadowing-import-from
+   #:sicl-hash-table
+   #:hash-table
+   #:make-hash-table #:hash-table-p
+   #:hash-table-count #:hash-table-rehash-threshold #:hash-table-rehash-size
+   #:hash-table-size #:hash-table-test
+   #:puthash #:gethash #:remhash #:clrhash
+   #:with-hash-table-iterator
+   #:maphash)
+  (:use #:common-lisp)
+  (:export #:sicl-list-hash-table))
