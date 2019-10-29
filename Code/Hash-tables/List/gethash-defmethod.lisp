aca9434234d9eb177b04179c5ff7cb711c5409a2
Robert Strandh <robert.strandh@gmail.com>
2019-10-29 11:22:14 +0100

    Bug fix: supply the hash table to WITH-ACCESSORS.

--- Code/Hash-tables/List/gethash-defmethod.lisp
+++ Code/Hash-tables/List/gethash-defmethod.lisp
@@ -1,9 +1,11 @@
 (cl:in-package #:sicl-list-hash-table)
 
 (defmethod gethash (key (hash-table list-hash-table) &optional default)
+  (declare (ignore default))
   (with-accessors ((contents contents)
                    (test hash-table-test))
-      (let ((entry (assoc key contents :key #'car :test test)))
-        (if (null entry)
-            (values nil nil)
-            (values (cdr entry) t)))))
+      hash-table
+    (let ((entry (assoc key contents :key #'car :test test)))
+      (if (null entry)
+          (values nil nil)
+          (values (cdr entry) t)))))
