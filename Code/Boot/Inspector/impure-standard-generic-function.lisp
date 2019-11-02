59fccbb042d165e0ed06e364bbbc83ba3c17be3d
Robert Strandh <robert.strandh@gmail.com>
2019-11-02 09:17:48 +0100

    Inspect teh slots of an impure standard generic function.

--- Code/Boot/Inspector/impure-standard-generic-function.lisp
+++ Code/Boot/Inspector/impure-standard-generic-function.lisp
@@ -29,7 +29,8 @@
      (state  inspected-impure-standard-generic-function)
      (style  (eql :expanded-body))
      (stream t))
-  (let ((e4 (sicl-boot::e4 *boot*)))
+  (let ((e4 (sicl-boot::e4 *boot*))
+        (e3 (sicl-boot::e3 *boot*)))
     (clouseau:with-preserved-cursor-x (stream)
       (clim:formatting-table (stream)
         (clouseau:format-place-row
@@ -51,5 +52,15 @@
          'clouseau:pseudo-place
          (funcall (sicl-genv:fdefinition 'sicl-clos:generic-function-lambda-list e4)
                   object)
-         :label "Lambda list")))))
+         :label "Lambda list")
+        (loop for slot in (aref (rack-of-object object) 1)
+              for slot-name = (funcall (sicl-genv:fdefinition 'sicl-clos:slot-definition-name e3) slot)
+              for slot-location = (funcall (sicl-genv:fdefinition 'sicl-clos:slot-definition-location e3) slot)
+              for slot-value = (aref (rack-of-object object) slot-location)
+              do (clouseau:format-place-row
+                  stream
+                  object
+                  'clouseau:pseudo-place
+                  slot-value
+                  :label (symbol-name slot-name)))))))
 
