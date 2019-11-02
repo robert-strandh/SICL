b202cca48fdcda0261f2e0e3acd68301d7eefc05
Robert Strandh <robert.strandh@gmail.com>
2019-11-02 08:05:07 +0100

    Simplify code by eliminating unnecessary lexical variables.

--- Code/Boot/Inspector/impure-standard-generic-function.lisp
+++ Code/Boot/Inspector/impure-standard-generic-function.lisp
@@ -29,10 +29,13 @@
      (state  inspected-impure-standard-generic-function)
      (style  (eql :expanded-body))
      (stream t))
-  (let* ((name-function (sicl-genv:fdefinition 'sicl-clos:generic-function-name (sicl-boot::e4 *boot*)))
-         (name (funcall name-function object)))
-    (clouseau:with-preserved-cursor-x (stream)
-      (clim:formatting-table (stream)
-        (clouseau:format-place-row
-         stream object 'clouseau:pseudo-place name :label "Name")))))
+  (clouseau:with-preserved-cursor-x (stream)
+    (clim:formatting-table (stream)
+      (clouseau:format-place-row
+       stream
+       object
+       'clouseau:pseudo-place
+       (funcall (sicl-genv:fdefinition 'sicl-clos:generic-function-name (sicl-boot::e4 *boot*))
+                object)
+       :label "Name"))))
 
