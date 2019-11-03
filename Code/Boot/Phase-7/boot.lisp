14b7e229ded471a8b77225f72bbd2b0a9a3364c4
Robert Strandh <robert.strandh@gmail.com>
2019-11-03 14:21:49 +0100

    Update greeting to reflect new phase number.

--- Code/Boot/Phase-7/boot.lisp
+++ Code/Boot/Phase-7/boot.lisp
@@ -1,7 +1,7 @@
 (cl:in-package #:sicl-boot-phase-7)
 
 (defun boot (boot)
-  (format *trace-output* "Start of phase 6~%")
+  (format *trace-output* "Start of phase 7~%")
   (with-accessors ((e5 sicl-boot:e5))
       boot
     (load-data-and-control-flow-code e5)
