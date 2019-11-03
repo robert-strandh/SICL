5bf8cc548fc1303ca5762b4afefa19bc1b85b004
Robert Strandh <robert.strandh@gmail.com>
2019-11-03 09:08:05 +0100

    Remove code that is inappropriate for this class.

--- Code/Boot/Inspector/impure-method-combination.lisp
+++ Code/Boot/Inspector/impure-method-combination.lisp
@@ -7,10 +7,7 @@
      (stream t))
   (clim:with-drawing-options (stream :ink clim:+red+)
     (format stream
-            "Impure method combination ~s"
-            (funcall (sicl-genv:fdefinition 'class-name
-                                            (sicl-boot::e4 *boot*))
-                     object))))
+            "Impure method combination")))
 
 (defmethod clouseau:inspect-object-using-state
     ((object t)
@@ -19,10 +16,7 @@
      (stream t))
   (clim:with-drawing-options (stream :ink clim:+red+)
     (format stream
-            "Impure method combination ~s"
-            (funcall (sicl-genv:fdefinition 'class-name
-                                            (sicl-boot::e4 *boot*))
-                     object))))
+            "Impure method combination")))
 
 (defmethod clouseau:inspect-object-using-state
     ((object t)
@@ -30,6 +24,7 @@
      (style  (eql :expanded-body))
      (stream t))
   (let ((e4 (sicl-boot::e4 *boot*)))
+    (declare (ignore e4))
     (clouseau:with-preserved-cursor-x (stream)
       (clim:formatting-table (stream)
         (clouseau:format-place-row
@@ -38,11 +33,4 @@
          'clouseau:pseudo-place
          (aref (rack-of-object object) 0)
          :label "Stamp")
-        (clouseau:format-place-row
-         stream
-         object
-         'clouseau:pseudo-place
-         (funcall (sicl-genv:fdefinition 'class-name e4)
-                  object)
-         :label "Name")
         (present-impure-object-slots object stream)))))
