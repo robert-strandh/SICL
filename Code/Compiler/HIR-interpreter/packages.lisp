168d6bb720627c049365901de2318bdf5218bb1b
Robert Strandh <robert.strandh@gmail.com>
2019-11-05 07:32:07 +0100

    Export names of call entries, accessors, and special variable.

--- Code/Compiler/HIR-interpreter/packages.lisp
+++ Code/Compiler/HIR-interpreter/packages.lisp
@@ -5,6 +5,10 @@
   (:export #:cst-eval
            #:top-level-hir-to-host-function
            #:*dynamic-environment*
+           #:call-stack-entry
+           #:origin
+           #:arguments
+           #:*call-stack*
            #:enclose
            #:fill-environment
            #:make-function-cell-finder))
