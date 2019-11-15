65ea2ca2b66fe7aa2ca54799ad1d876647c679cf
Robert Strandh <robert.strandh@gmail.com>
2019-11-15 15:21:18 +0100

    Make RATIO a standard class and inherit from STANDARD-OBJECT.

--- Code/Arithmetic/ratio-defclass.lisp
+++ Code/Arithmetic/ratio-defclass.lisp
@@ -1,6 +1,5 @@
 (cl:in-package #:sicl-arithmetic)
 
-(defclass ratio (rational)
+(defclass ratio (rational standard-object)
   ((%numerator :initarg :numerator :reader numerator)
-   (%denominator :initarg :denominator :reader denominator))
-  (:metaclass built-in-class))
+   (%denominator :initarg :denominator :reader denominator)))
