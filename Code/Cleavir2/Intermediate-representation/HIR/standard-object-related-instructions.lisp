7913cbc2861fcf076107996ae8da41b0c2442b83
Robert Strandh <robert.strandh@gmail.com>
2019-11-17 15:55:28 +0100

    Define instruction classes CELL-READ-INSTRUCTION and CELL-WRITE-INSTRUCTION.

--- Code/Cleavir2/Intermediate-representation/HIR/standard-object-related-instructions.lisp
+++ Code/Cleavir2/Intermediate-representation/HIR/standard-object-related-instructions.lisp
@@ -5,8 +5,8 @@
 ;;; Instruction SLOT-READ-INSTRUCTION.
 ;;;
 ;;; This instruction takes two inputs.  The first input is assumed
-;;; to be a standard instance.  The second is assumed to be a FIXNUM
-;;; and represents the index in the instance of the slot to be read.
+;;; to be a standard object.  The second is assumed to be a FIXNUM
+;;; and represents the index in the object of the slot to be read.
 ;;; This instruction produces a single output, which is the contents
 ;;; of the SLOT.
 
@@ -18,11 +18,38 @@
 ;;; Instruction SLOT-WRITE-INSTRUCTION
 ;;;
 ;;; This instruction takes three inputs.  The first input is assumed
-;;; to be a standard instance.  The second is assumed to be a FIXNUM
-;;; and represents the index in the instance of the slot to be
+;;; to be a standard object.  The second is assumed to be a FIXNUM
+;;; and represents the index in the object of the slot to be
 ;;; written.  The third is the value to write.  This instruction
 ;;; replaces the contents of the slot with the new value.
 
 (defclass slot-write-instruction
+    (instruction one-successor-mixin side-effect-mixin)
+  ())
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;; Instruction CELL-READ-INSTRUCTION.
+;;;
+;;; This instruction takes two inputs.  The first input is assumed
+;;; to be a standard object.  The second is assumed to be a FIXNUM
+;;; and represents the index in the object of the cell to be read.
+;;; This instruction produces a single output, which is the contents
+;;; of the CELL.
+
+(defclass cell-read-instruction (instruction one-successor-mixin)
+  ())
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;; Instruction CELL-WRITE-INSTRUCTION
+;;;
+;;; This instruction takes three inputs.  The first input is assumed
+;;; to be a standard object.  The second is assumed to be a FIXNUM
+;;; and represents the index in the object of the cell to be
+;;; written.  The third is the value to write.  This instruction
+;;; replaces the contents of the cell with the new value.
+
+(defclass cell-write-instruction
     (instruction one-successor-mixin side-effect-mixin)
   ())
