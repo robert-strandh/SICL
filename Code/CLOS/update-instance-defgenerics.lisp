62ce00284da8c4f6adcabcb9a0534faeb8834736
Robert Strandh <robert.strandh@gmail.com>
2019-11-18 16:02:09 +0100

    Improve layout of lambda list.

--- Code/CLOS/update-instance-defgenerics.lisp
+++ Code/CLOS/update-instance-defgenerics.lisp
@@ -2,16 +2,16 @@
 
 ;;; For the specification of this generic function, see
 ;;; http://www.lispworks.com/documentation/HyperSpec/Body/f_update.htm#update-instance-for-different-class
-(defgeneric update-instance-for-different-class (previous
-						 current
-						 &rest initargs
-						 &key &allow-other-keys))
+(defgeneric update-instance-for-different-class
+    (previous current
+     &rest initargs
+     &key
+     &allow-other-keys))
 
 ;;; For the specification of this generic function, see
 ;;; http://www.lispworks.com/documentation/HyperSpec/Body/f_upda_1.htm#update-instance-for-redefined-class
-(defgeneric update-instance-for-redefined-class (instance
-						 added-slots
-						 discarded-slots
-						 property-list
-						 &rest initargs
-						 &key &allow-other-keys))
+(defgeneric update-instance-for-redefined-class
+    (instance added-slots discarded-slots property-list
+     &rest initargs
+     &key
+     &allow-other-keys))
