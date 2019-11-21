543382b51fba797c951dc2d67dada36d8cd92a07
Robert Strandh <robert.strandh@gmail.com>
2019-11-21 15:38:37 +0100

    Use new primop to access the stamp of a standard object.

--- Code/CLOS/stamp-defun.lisp
+++ Code/CLOS/stamp-defun.lisp
@@ -4,7 +4,7 @@
 ;;; this is probably not a good location for it.
 (defun stamp (instance)
   (if (general-instance-p instance)
-      (general-instance-access instance 0)
+      (cleavir-primop:nook-read instance 0)
       ;; For now, anything else is considered to be an instance of
       ;; class T, and we know that T has unique number 0.
       0))
