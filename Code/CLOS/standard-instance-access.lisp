52bafab3f56c8199f870842c60100a2393baa1ca
Robert Strandh <robert.strandh@gmail.com>
2019-11-21 15:25:29 +0100

    Use new primops to implement STANDARD-INSTANCE-ACCESS and (SETF STANDARD-INSTANCE-ACCESS).

--- Code/CLOS/standard-instance-access.lisp
+++ Code/CLOS/standard-instance-access.lisp
@@ -3,7 +3,8 @@
 ;;; For the specification of this function, see
 ;;; http://metamodular.com/CLOS-MOP/standard-instance-access.html
 (defun standard-instance-access (instance location)
-  (general-instance-access instance location))
+  (cleavir-primop:nook-read instance location))
 
 (defun (setf standard-instance-access) (new-value instance location)
-  (setf (general-instance-access instance location) new-value))
+  (cleavir-primop:nook-write instance location new-value)
+  new-value)
