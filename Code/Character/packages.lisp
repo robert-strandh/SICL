f3d9b9670c60f44fe4592220f572f63dabc96604
Robert Strandh <robert.strandh@gmail.com>
2019-11-22 07:45:17 +0100

    Do not shadow standard symbols in intrinsic package definition.

--- Code/Character/packages.lisp
+++ Code/Character/packages.lisp
@@ -1,39 +1,4 @@
 (cl:in-package #:common-lisp-user)
 
 (defpackage #:sicl-character
-  (:use #:common-lisp)
-  (:shadow
-   . #1=(#:character
-	 #:characterp
-	 #:base-char
-	 #:standard-char
-	 #:extended-char
-	 #:alpha-char-p
-	 #:alphanumericp
-	 #:digit-char-p
-	 #:graphic-char-p
-	 #:standard-char-p
-	 #:char-upcase
-	 #:char-downcase
-	 #:upper-case-p
-	 #:lower-case-p
-	 #:both-case-p
-	 #:char=
-	 #:char/=
-	 #:char<
-	 #:char>
-	 #:char<=
-	 #:char>=
-	 #:char-equal
-	 #:char-not-equal
-	 #:char-lessp
-	 #:char-greaterp
-	 #:char-not-greaterp
-	 #:char-not-lessp
-	 #:char-code
-	 #:char-int
-	 #:code-char
-	 #:char-code-limit
-	 #:char-name
-	 #:name-char))
-  (:export . #1#))
+  (:use #:common-lisp))
