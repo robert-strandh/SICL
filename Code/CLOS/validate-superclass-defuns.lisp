8e3f0f17bdc8795233be52d4de1a8b1bf9baf5a6
Robert Strandh <robert.strandh@gmail.com>
2019-11-23 13:49:57 +0100

    Remove obsolete components.

--- Code/CLOS/validate-superclass-defuns.lisp
+++
@@ -1,4 +0,0 @@
-(cl:in-package #:sicl-clos)
-
-(defun validate-superclass (class superclass)
-  (validate-superclass-default class superclass))
--- Code/CLOS/validate-superclass-support.lisp
+++
@@ -1,8 +0,0 @@
-(cl:in-package #:sicl-clos)
-
-(defun validate-superclass-default (class superclass)
-  (let ((c1 (class-of class))
-        (c2 (class-of superclass))
-        (built-in-class (find-class 'built-in-class)))
-    (or (eq c1 built-in-class)
-        (not (eq c2 built-in-class)))))
