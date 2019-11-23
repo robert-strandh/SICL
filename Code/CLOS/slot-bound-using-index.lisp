a31f2495049975a6cd1f53bfc93c0437905c76b6
Robert Strandh <robert.strandh@gmail.com>
2019-11-23 11:04:00 +0100

    Untabify for better tool support.

--- Code/CLOS/slot-bound-using-index.lisp
+++ Code/CLOS/slot-bound-using-index.lisp
@@ -2,7 +2,7 @@
 
 (defun slot-boundp-using-index (general-instance index)
   (not (eq (cleavir-primop:nook-read general-instance index)
-	   +unbound-slot-value+)))
+           +unbound-slot-value+)))
 
 (defun slot-makunbound-using-index (instance index)
   (cleavir-primop:nook-write instance index +unbound-slot-value+)
