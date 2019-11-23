c19852e2a60d0ad8461caf6d1ab7d4e54b07d0dc
Robert Strandh <robert.strandh@gmail.com>
2019-11-23 11:04:43 +0100

    Improve code layout.

--- Code/CLOS/shared-initialize-defgenerics.lisp
+++ Code/CLOS/shared-initialize-defgenerics.lisp
@@ -2,7 +2,5 @@
 
 ;;; For the specification of this generic function, see
 ;;; http://www.lispworks.com/documentation/HyperSpec/Body/f_shared.htm#shared-initialize
-(defgeneric shared-initialize (instance
-			       slot-names
-			       &rest initargs
-			       &key &allow-other-keys))
+(defgeneric shared-initialize
+    (instance slot-names &rest initargs &key &allow-other-keys))
