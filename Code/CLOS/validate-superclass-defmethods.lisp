c074059d5b5c6a56cb3772cd3a6512d02a0a83c8
Robert Strandh <robert.strandh@gmail.com>
2019-11-23 13:54:16 +0100

    Define methods to return Boolean values directly.

--- Code/CLOS/validate-superclass-defmethods.lisp
+++ Code/CLOS/validate-superclass-defmethods.lisp
@@ -1,4 +1,14 @@
 (cl:in-package #:sicl-clos)
 
+;;; The argument precedence order is (SUPERCLASS CLASS).  Therefore,
+;;; when both arguments are built-in classes, the third method is more
+;;; specific than the second, and we return T.
+
 (defmethod validate-superclass ((class class) (superclass class))
-  (validate-superclass-default class superclass))
+  t)
+
+(defmethod validate-superclass ((class built-in-class) (superclass class))
+  nil)
+
+(defmethod validate-superclass ((class class) (superclass built-in-class))
+  t)
