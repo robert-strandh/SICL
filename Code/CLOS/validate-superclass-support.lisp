a225352fff4c83cb43114b127595b006ea3cdf18
Robert Strandh <robert.strandh@gmail.com>
2019-11-23 10:59:32 +0100

    Reimplement VALIDATE-SUPERCLASS to correspond to SICL restrictions.

--- Code/CLOS/validate-superclass-support.lisp
+++ Code/CLOS/validate-superclass-support.lisp
@@ -1,12 +1,8 @@
 (cl:in-package #:sicl-clos)
 
-;;; FIXME: this code is almost certainly wrong.
 (defun validate-superclass-default (class superclass)
-  (or (eq superclass *t*)
-      (let ((c1 (class-of class))
-            (c2 (class-of superclass)))
-        (or (eq c1 c2)
-            (and (eq c1 *standard-class*)
-                 (eq c2 *funcallable-standard-class*))
-            (and (eq c1 *funcallable-standard-class*)
-                 (eq c2 *standard-class*))))))
+  (let ((c1 (class-of class))
+        (c2 (class-of superclass))
+        (built-in-class (find-class 'built-in-class)))
+    (or (eq c1 built-in-class)
+        (not (eq c2 built-in-class)))))
