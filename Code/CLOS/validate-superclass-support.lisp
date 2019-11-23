f98de97330d342c30cf5c53bc3f046f6dbc427f5
Robert Strandh <robert.strandh@gmail.com>
2019-11-23 10:53:19 +0100

    Add FIXME comment.

--- Code/CLOS/validate-superclass-support.lisp
+++ Code/CLOS/validate-superclass-support.lisp
@@ -1,11 +1,12 @@
 (cl:in-package #:sicl-clos)
 
+;;; FIXME: this code is almost certainly wrong.
 (defun validate-superclass-default (class superclass)
   (or (eq superclass *t*)
       (let ((c1 (class-of class))
-	    (c2 (class-of superclass)))
-	(or (eq c1 c2)
-	    (and (eq c1 *standard-class*)
-		 (eq c2 *funcallable-standard-class*))
-	    (and (eq c1 *funcallable-standard-class*)
-		 (eq c2 *standard-class*))))))
+            (c2 (class-of superclass)))
+        (or (eq c1 c2)
+            (and (eq c1 *standard-class*)
+                 (eq c2 *funcallable-standard-class*))
+            (and (eq c1 *funcallable-standard-class*)
+                 (eq c2 *standard-class*))))))
