32a187ed742b2ae470379e6bdca81f1d742c7595
Robert Strandh <robert.strandh@gmail.com>
2019-11-23 11:06:10 +0100

    Improve code layout.

--- Code/CLOS/reinitialize-instance-defgenerics.lisp
+++ Code/CLOS/reinitialize-instance-defgenerics.lisp
@@ -2,6 +2,5 @@
 
 ;;; For the specification of this generic function, see
 ;;; http://www.lispworks.com/documentation/HyperSpec/Body/f_reinit.htm#reinitialize-instance
-(defgeneric reinitialize-instance (instance
-				   &rest initargs
-				   &key &allow-other-keys))
+(defgeneric reinitialize-instance
+    (instance &rest initargs &key &allow-other-keys))
