df434b51c41ae32ca3eaaf99f5b152c776b2b61d
Robert Strandh <robert.strandh@gmail.com>
2019-11-25 15:44:02 +0100

    New component containing definition of class VECTOR.

---
+++ Code/Array/vector-defclass.lisp
@@ -0,0 +1,10 @@
+(cl:in-package #:sicl-array)
+
+;;; All our vectors have a slot for the fill pointer.  When a vector
+;;; is created without a fill pointer, we set the slot to NIL.
+
+(defclass vector (array sequence)
+  ((%fill-pointer
+    :initform nil
+    :initarg :fill-pinter
+    :accessor vector-fill-pointer)))
