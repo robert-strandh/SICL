78e3afdb70e68965ee423d89e4b88651944c64b3
Robert Strandh <robert.strandh@gmail.com>
2019-11-26 15:39:49 +0100

    New component containing definitions of methods on generic function ARRAY-ROW-MAJOR-INDEX.

---
+++ Code/Array/array-row-major-index-defmethods.lisp
@@ -0,0 +1,22 @@
+(cl:in-package #:sicl-array)
+
+(defmethod array-row-major-index (object &rest subscripts)
+  (error 'argument-to-array-row-major-index-must-be-an-array
+         :datum object
+         :expected-type 'array))
+
+(defmethod array-row-major-index ((array array) &rest subscripts)
+  (let ((dimensions (array-dimensions array)))
+    (unless (= (length dimensions) (length subscripts))
+      (error 'number-of-indices-must-equal-array-rank
+             :subscripts subscripts
+             :array array))
+    (loop for index in subscripts
+          for dimension in dimensions
+          for index-number from 0
+          for result = index then (+ (* result dimension) index)
+          unless (and (integerp index) (>= index 0) (< index dimension))
+            do (error 'index-must-be-non-negative-and-less-than-dimension
+                      :array array
+                      :index-number index-number)
+          finally (return result))
