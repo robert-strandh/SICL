14095d4159864a7f46f714e31909295406126f41
Robert Strandh <robert.strandh@gmail.com>
2019-11-26 16:06:48 +0100

    Define reporter for conditions to be signaled when number of indices is not equal to array rank.

--- Code/Array/condition-reporters-english.lisp
+++ Code/Array/condition-reporters-english.lisp
@@ -12,6 +12,22 @@
           (type-error-datum condition)))
 
 (defmethod acclimation:report-condition
+  ((condition number-of-indices-must-equal-array-rank)
+   stream
+     (language acclimation:english))
+  (format stream
+          "The following indices:~@
+           ~s~@
+           were given in order to access the array:~@
+           ~s~.@
+           However, the number of indices given is ~d~@
+           whereas the randk of the array is ~d."
+          (indices condition)
+          (given-array condition)
+          (length (indices condition))
+          (array-rank (given-array condition))))
+
+(defmethod acclimation:report-condition
     ((condition index-must-be-non-negative-and-less-than-dimension)
      stream
      (language acclimation:english))
