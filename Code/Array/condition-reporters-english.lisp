06c9503fb61a87fe4fb7f3aa30c011fb3a68fe41
Robert Strandh <robert.strandh@gmail.com>
2019-11-26 14:15:09 +0100

    New component containing English language condition reporters.

---
+++ Code/Array/condition-reporters-english.lisp
@@ -0,0 +1,13 @@
+(cl:in-package #:sicl-array)
+
+(defmethod acclimation:report-condition
+    ((condition argument-to-array-displacement-must-be-an-array)
+     stream
+     (language acclimation:english))
+  (format stream
+	  "An object of type ~s was expected,~@
+           but the following object was found instead~@
+           ~s"
+          (type-error-expected-type condition)
+          (type-error-datum condition)))
+
