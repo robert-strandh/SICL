52d194ec9ed7cc16626e583d9af2f379309f4d0a
Robert Strandh <robert.strandh@gmail.com>
2019-11-26 16:06:11 +0100

    Remove obsolete condition reporters.

--- Code/Array/condition-reporters-english.lisp
+++ Code/Array/condition-reporters-english.lisp
@@ -1,29 +1,7 @@
 (cl:in-package #:sicl-array)
 
 (defmethod acclimation:report-condition
-    ((condition argument-to-array-element-type-must-be-an-array)
-     stream
-     (language acclimation:english))
-  (format stream
-	  "An object of type ~s was expected,~@
-           but the following object was found instead~@
-           ~s"
-          (type-error-expected-type condition)
-          (type-error-datum condition)))
-
-(defmethod acclimation:report-condition
-    ((condition argument-to-array-displacement-must-be-an-array)
-     stream
-     (language acclimation:english))
-  (format stream
-	  "An object of type ~s was expected,~@
-           but the following object was found instead~@
-           ~s"
-          (type-error-expected-type condition)
-          (type-error-datum condition)))
-
-(defmethod acclimation:report-condition
-    ((condition argument-to-array-row-major-index-must-be-an-array)
+    ((condition object-must-be-an-array)
      stream
      (language acclimation:english))
   (format stream
