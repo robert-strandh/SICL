968058e6edee92d76774ccbbf9d4a00e76b7819b
Robert Strandh <robert.strandh@gmail.com>
2019-11-26 15:25:13 +0100

    Define reporter for class of conditions to be signaled when indices and rank do not correspond.

--- Code/Array/condition-reporters-english.lisp
+++ Code/Array/condition-reporters-english.lisp
@@ -21,3 +21,14 @@
            ~s"
           (type-error-expected-type condition)
           (type-error-datum condition)))
+
+(defmethod acclimation:report-condition
+    ((condition argument-to-array-row-major-index-must-be-an-array)
+     stream
+     (language acclimation:english))
+  (format stream
+	  "An object of type ~s was expected,~@
+           but the following object was found instead~@
+           ~s"
+          (type-error-expected-type condition)
+          (type-error-datum condition)))
