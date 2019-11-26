3380cb9160d1dbf220c5cb81c1028195ee282f12
Robert Strandh <robert.strandh@gmail.com>
2019-11-26 16:05:52 +0100

    Merge similar condition classes into a single one.

--- Code/Array/conditions.lisp
+++ Code/Array/conditions.lisp
@@ -1,14 +1,6 @@
 (cl:in-package #:sicl-array)
 
-(define-condition argument-to-array-displacement-must-be-an-array
-    (type-error acclimation:condition)
-  ())
-
-(define-condition argument-to-array-element-type-must-be-an-array
-    (type-error acclimation:condition)
-  ())
-
-(define-condition argument-to-array-row-major-index-must-be-an-array
+(define-condition object-must-be-an-array
     (type-error acclimation:condition)
   ())
 
