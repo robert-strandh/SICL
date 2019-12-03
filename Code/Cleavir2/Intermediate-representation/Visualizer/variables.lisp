ffa927eb7d9b341a13bd716f245dbf81920fa50d
Robert Strandh <robert.strandh@gmail.com>
2019-12-03 13:12:23 +0100

    Increase horizontal spacing.

--- Code/Cleavir2/Intermediate-representation/Visualizer/variables.lisp
+++ Code/Cleavir2/Intermediate-representation/Visualizer/variables.lisp
@@ -10,10 +10,10 @@
 
 (define-symbol-macro datum-width (round (+ *base-width* 4)))
 
-(define-symbol-macro horizontal-node-separation (round (* 2.5 *base-width*)))
+(define-symbol-macro horizontal-node-separation (round (* 6 *base-width*)))
 
 (define-symbol-macro vertical-rack-separation (round (* 5 *base-height*)))
 
-(define-symbol-macro horizontal-column-separation (round (* 3.5 *base-width*)))
+(define-symbol-macro horizontal-column-separation (round (* 6 *base-width*)))
 
 (define-symbol-macro vertical-node-separation (round (* 3 *base-height*)))
