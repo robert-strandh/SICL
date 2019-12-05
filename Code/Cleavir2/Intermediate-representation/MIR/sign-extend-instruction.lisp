5212fe14f5578e6965dc0e3dfa4e28c898940aca
Robert Strandh <robert.strandh@gmail.com>
2019-12-05 15:49:17 +0100

    Use new macro with sign-extend instructions.

--- Code/Cleavir2/Intermediate-representation/MIR/sign-extend-instruction.lisp
+++ Code/Cleavir2/Intermediate-representation/MIR/sign-extend-instruction.lisp
@@ -3,18 +3,8 @@
 (defclass sign-extend-instruction (instruction one-successor-mixin)
   ())
 
-(defmethod shared-initialize :around
-    ((instruction sign-extend-instruction) slot-names
-     &rest keys
-     &key
-       inputs input
-       outputs output
-       successors successor)
-  (let ((inputs (if (null input) inputs (list input)))
-        (outputs (if (null output) outputs (list output)))
-        (successors (if (null successor) successors (list successor))))
-    (apply #'call-next-method instruction slot-names
-           :inputs inputs
-           :outputs outputs
-           :successors successors
-           keys)))
+(normalize-arguments
+ sign-extend-instruction
+ (input)
+ (output)
+ (successor))
