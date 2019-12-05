cc39001fcdbcb40753956b9bee2b2ab39613f4e6
Robert Strandh <robert.strandh@gmail.com>
2019-12-05 12:19:27 +0100

    Define a macro for normalizing arguments.

--- Code/Cleavir2/Intermediate-representation/MIR/integer-arithmetic-instructions.lisp
+++ Code/Cleavir2/Intermediate-representation/MIR/integer-arithmetic-instructions.lisp
@@ -1,5 +1,74 @@
 (cl:in-package #:cleavir-ir)
 
+(defmacro normalize-arguments
+    (instruction-class-name input-names output-names successor-names)
+  `(defmethod shared-initialize :around
+     ((instruction ,instruction-class-name)
+      slot-names
+      &rest keys
+      &key
+      (inputs nil inputs-p) ,@input-names
+      (outputs nil outputs-p) ,@output-names
+      (successors nil successors-p) ,@successor-names)
+     (declare (ignore inputs outputs successors))
+     (cond ((or ,@input-names)
+            (assert (and ,@input-names))
+            (assert (not inputs-p))
+            (cond ((or ,@output-names)
+                   (assert (and ,@output-names))
+                   (assert (not outputs-p))
+                   (cond ((or ,@successor-names)
+                          (assert (and ,@successor-names))
+                          (assert (not successors-p))
+                          (apply #'call-next-method instruction slot-names
+                                 :inputs (list ,@input-names)
+                                 :outputs (list ,@output-names)
+                                 :successors (list ,@successor-names)
+                                 keys))
+                         (t
+                          (apply #'call-next-method instruction slot-names
+                                 :inputs (list ,@input-names)
+                                 :outputs (list ,@output-names)
+                                 keys))))
+                  (t
+                   (cond ((or ,@successor-names)
+                          (assert (and ,@successor-names))
+                          (assert (not successors-p))
+                          (apply #'call-next-method instruction slot-names
+                                 :inputs (list ,@input-names)
+                                 :successors (list ,@successor-names)
+                                 keys))
+                         (t
+                          (apply #'call-next-method instruction slot-names
+                                 :inputs (list ,@input-names)
+                                 keys))))))
+           (t
+            (cond ((or ,@output-names)
+                   (assert (and ,@output-names))
+                   (assert (not outputs-p))
+                   (cond ((or ,@successor-names)
+                          (assert (and ,@successor-names))
+                          (assert (not successors-p))
+                          (apply #'call-next-method instruction slot-names
+                                 :outputs (list ,@output-names)
+                                 :successors (list ,@successor-names)
+                                 keys))
+                         (t
+                          (apply #'call-next-method instruction slot-names
+                                 :outputs (list ,@output-names)
+                                 keys))))
+                  (t
+                   (cond ((or ,@successor-names)
+                          (assert (and ,@successor-names))
+                          (assert (not successors-p))
+                          (apply #'call-next-method instruction slot-names
+                                 :inputs (list ,@input-names)
+                                 :successors (list ,@successor-names)
+                                 keys))
+                         (t
+                          (apply #'call-next-method instruction slot-names
+                                 keys)))))))))
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;
 ;;; MIR instruction SIGNED-ADD-INSTRUCTION
@@ -107,23 +176,11 @@
 (defclass unsigned-add-instruction (instruction multiple-successors-mixin)
   ())
 
-(defmethod shared-initialize :around
-    ((instruction unsigned-add-instruction) slot-names
-     &rest keys
-     &key
-       inputs augend addend
-       outputs output
-       successors normal-successor carry-successor)
-  (assert (all-or-none augend addend))
-  (assert (all-or-none normal-successor carry-successor))
-  (let ((inputs (combine inputs augend addend))
-        (outputs (if (null output) outputs (list output)))
-        (successors (combine successors normal-successor carry-successor)))
-    (apply #'call-next-method instruction slot-names
-           :inputs inputs
-           :outputs outputs
-           :successors successors
-           keys)))
+(normalize-arguments
+ unsigned-add-instruction
+ (augend addend)
+ (output)
+ (normal-successor carry-successor))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;
