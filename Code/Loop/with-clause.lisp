2ddfd692933635667f560f526c95f0afb6b1143d
Robert Strandh <robert.strandh@gmail.com>
2019-12-06 16:44:43 +0100

    Untabify and remove unnecessary whitespace at end of line.

--- Code/Loop/with-clause.lisp
+++ Code/Loop/with-clause.lisp
@@ -2,13 +2,13 @@
 ;;;;
 ;;;;     Robert Strandh (robert.strandh@gmail.com)
 ;;;;
-;;;; all rights reserved. 
+;;;; all rights reserved.
 ;;;;
-;;;; Permission is hereby granted to use this software for any 
+;;;; Permission is hereby granted to use this software for any
 ;;;; purpose, including using, modifying, and redistributing it.
 ;;;;
 ;;;; The software is provided "as-is" with no warranty.  The user of
-;;;; this software assumes any responsibility of the consequences. 
+;;;; this software assumes any responsibility of the consequences.
 
 (cl:in-package #:sicl-loop)
 
@@ -19,7 +19,7 @@
 ;;;
 ;;; The syntax of a with-clause is:
 ;;;
-;;;    with-clause ::= WITH var1 [type-spec] [= form1] 
+;;;    with-clause ::= WITH var1 [type-spec] [= form1]
 ;;;                    {AND var2 [type-spec] [= form2]}*
 ;;;
 ;;; where var1 and var2 are destructuring variable specifiers
@@ -31,17 +31,17 @@
 ;;; done sequentially, so that variables created in one with-clause
 ;;; can be used in the forms of subsequent with-clauses.  If parallel
 ;;; creation of variables is wanted, then the with-clause can be
-;;; followed by one or more and-clauses. 
+;;; followed by one or more and-clauses.
 ;;;
 ;;; The (destructuring) type specifier is optional.  If no type
-;;; specifier is given, it is as if t was given. 
+;;; specifier is given, it is as if t was given.
 ;;;
 ;;; The initialization form is optional.  If there is a corresponding
 ;;; type specifier for a variable, but no initialization form, then
 ;;; the variable is initialized to a value that is appropriate for the
 ;;; type.  In particular, for the type t the value is nil, for the
 ;;; type number, the value is 0, and for the type float, the value is
-;;; 0.0.  
+;;; 0.0.
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;
@@ -67,8 +67,8 @@
   (multiple-value-bind (temp-vars dictionary)
       (fresh-variables (var-spec clause))
     (reinitialize-instance clause
-			   :temp-vars temp-vars
-			   :dictionary dictionary)))
+                           :temp-vars temp-vars
+                           :dictionary dictionary)))
 
 (defclass with-subclause-no-form (with-subclause) ())
 
@@ -82,12 +82,12 @@
 
 (defmethod bound-variables ((clause with-clause))
   (reduce #'append
-	  (mapcar #'bound-variables (subclauses clause))
-	  :from-end t))
+          (mapcar #'bound-variables (subclauses clause))
+          :from-end t))
 
 (defmethod bound-variables ((subclause with-subclause))
   (mapcar #'car
-	  (extract-variables (var-spec subclause) nil)))
+          (extract-variables (var-spec subclause) nil)))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;
@@ -97,50 +97,50 @@
 ;;; We try this parser first.
 (define-parser with-subclause-type-1-parser
   (consecutive (lambda (var-spec type-spec = form)
-		 (declare (ignore =))
-		 (make-instance 'with-subclause-with-form
-		   :var-spec var-spec
-		   :type-spec type-spec
-		   :form form))
-	       ;; Accept anything for now.  Analyze later. 
-	       'anything-parser
-	       'optional-type-spec-parser
-	       (keyword-parser '=)
-	       'anything-parser))
+                 (declare (ignore =))
+                 (make-instance 'with-subclause-with-form
+                   :var-spec var-spec
+                   :type-spec type-spec
+                   :form form))
+               ;; Accept anything for now.  Analyze later.
+               'anything-parser
+               'optional-type-spec-parser
+               (keyword-parser '=)
+               'anything-parser))
 
 ;;; Parser for var [type-spec]
 (define-parser with-subclause-type-2-parser
   (consecutive (lambda (var-spec type-spec)
-		 (make-instance 'with-subclause-no-form
-		   :var-spec var-spec
-		   :type-spec type-spec))
-	       ;; Accept anything for now.  Analyze later. 
-	       'anything-parser
-	       'optional-type-spec-parser))
+                 (make-instance 'with-subclause-no-form
+                   :var-spec var-spec
+                   :type-spec type-spec))
+               ;; Accept anything for now.  Analyze later.
+               'anything-parser
+               'optional-type-spec-parser))
 
 ;;; Parser for any type of with subclause without the leading keyword
 (define-parser with-subclause-no-keyword-parser
   (alternative 'with-subclause-type-1-parser
-	       'with-subclause-type-2-parser))
+               'with-subclause-type-2-parser))
 
 ;;; Parser for the with subclause starting with the AND keyword.
 (define-parser with-subclause-and-parser
   (consecutive (lambda (and subclause)
-		 (declare (ignore and))
-		 subclause)
-	       (keyword-parser 'and)
-	       'with-subclause-no-keyword-parser))
+                 (declare (ignore and))
+                 subclause)
+               (keyword-parser 'and)
+               'with-subclause-no-keyword-parser))
 
 ;;; Parser for a with clause
 (define-parser with-clause-parser
   (consecutive (lambda (with first rest)
-		 (declare (ignore with))
-		 (make-instance 'with-clause
-		   :subclauses (cons first rest)))
-	       (keyword-parser 'with)
-	       'with-subclause-no-keyword-parser
-	       (repeat* #'list
-			'with-subclause-and-parser)))
+                 (declare (ignore with))
+                 (make-instance 'with-clause
+                   :subclauses (cons first rest)))
+               (keyword-parser 'with)
+               'with-subclause-no-keyword-parser
+               (repeat* #'list
+                        'with-subclause-and-parser)))
 
 (add-clause-parser 'with-clause-parser)
 
@@ -164,14 +164,14 @@
 
 (defmethod wrap-subclause ((subclause with-subclause-no-form) inner-form)
   (let* ((vars-and-types
-	   (extract-variables (var-spec subclause) (type-spec subclause)))
-	 (vars-and-values
-	   (loop for (var type) in vars-and-types
-		 collect (list var
-			       (case type
-				 (fixnum 0)
-				 (float 0.0)
-				 (t nil))))))
+           (extract-variables (var-spec subclause) (type-spec subclause)))
+         (vars-and-values
+           (loop for (var type) in vars-and-types
+                 collect (list var
+                               (case type
+                                 (fixnum 0)
+                                 (float 0.0)
+                                 (t nil))))))
     `(let ,vars-and-values
        ,inner-form)))
 
@@ -181,4 +181,4 @@
 
 (defmethod declarations ((clause with-subclause))
   (reduce #'append (mapcar #'declarations (subclauses clause))
-	  :from-end t))
+          :from-end t))
