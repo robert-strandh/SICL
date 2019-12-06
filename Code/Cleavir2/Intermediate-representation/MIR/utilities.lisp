d3ac03aeb5d86eda96bb3441d48822971411b1b0
Robert Strandh <robert.strandh@gmail.com>
2019-12-06 16:55:31 +0100

    Add comment to macro NORMALIZE-ARGUMENTS.

--- Code/Cleavir2/Intermediate-representation/MIR/utilities.lisp
+++ Code/Cleavir2/Intermediate-representation/MIR/utilities.lisp
@@ -15,6 +15,35 @@
       combination
       (copy-list arguments)))
 
+;;; This macro looks a bit messy.  The reason for that is that I could
+;;; not figure out a better way of accomplishing what it does.
+;;;
+;;; Essentially, we want to simulate the possibility of giving keyword
+;;; aguments naming individual inputs, outputs, and successors when an
+;;; instruction is created or when it has its class changed.  The best
+;;; way of doing that seemed to have an :AROUND method on
+;;; SHARED-INITIALIZE that checks whether one of these individual
+;;; keyword arguments has been given, and if so, translates them to a
+;;; list of inputs, outputs, or successors, before calling
+;;; CALL-NEXT-METHOD.
+;;;
+;;; Things get complicated though, because if neither (say) any
+;;; individual input name was given, NOR the :INPUTS keyword argument,
+;;; then CALL-NEXT-METHOD should not be given an explicit :INPUTS,
+;;; so as to preserve the existing slot value.
+;;;
+;;; The best thing I could think of was to distinguish eight cases
+;;; depending on whether any individual input argument, any individual
+;;; output argument, and any individual successor was given, and for
+;;; each of the eight cases, call CALL-NEXT-METHOD differently.
+;;;
+;;; Also we do some error checking.  If any individual keyword
+;;; argument was given, then they must all be given, and the aggregate
+;;; one must not be given.
+;;;
+;;; Although this macro is a bit messy, it saves a lot of code
+;;; elsewhere.  So until I can think of a better way of doing it, I
+;;; still think it is a win.
 (defmacro normalize-arguments
     (instruction-class-name input-names output-names successor-names)
   `(defmethod shared-initialize :around
