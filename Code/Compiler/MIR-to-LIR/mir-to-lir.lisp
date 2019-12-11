e1174b01f9dd9009c0c48622bd1994f508ce7298
Robert Strandh <robert.strandh@gmail.com>
2019-12-11 17:46:22 +0100

    Define embryonic method on main entry point.

--- Code/Compiler/MIR-to-LIR/mir-to-lir.lisp
+++ Code/Compiler/MIR-to-LIR/mir-to-lir.lisp
@@ -1,3 +1,23 @@
 (cl:in-package #:sicl-mir-to-lir)
 
-(defgeneric mir-to-lir (client lir))
+(defgeneric mir-to-lir (client mir))
+
+(defun enclose-funcall-p (instruction)
+  (and (typep instruction 'cleavir-ir:funcall-instruction)
+       (>= (length (cleavir-ir:inputs instruction)) 2)
+       (typep (second (cleavir-ir:inputs instruction))
+              'sicl-hir-to-mir::entry-point-input)))
+
+(defmethod mir-to-lir (client mir)
+  (let ((worklist (list mir))
+        (all-instructions '()))
+    (loop until (null worklist)
+          do (cleavir-ir:map-instructions-arbitrary-order
+              (lambda (instruction)
+                (push instruction all-instructions)
+                (when (enclose-funcall-p instruction)
+                  (push (sicl-hir-to-mir::enter-instruction
+                         (second (cleavir-ir:inputs instruction)))
+                        worklist)))
+              (pop worklist)))
+    (length all-instructions)))
