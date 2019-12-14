78704f8f9c594daadc0b013f57fba6562376bf04
Robert Strandh <robert.strandh@gmail.com>
2019-12-14 14:10:19 +0100

    Remove methods specialized to comparison and assignment instructions.

--- Code/Compiler/MIR-to-LIR/process-instructions.lisp
+++ Code/Compiler/MIR-to-LIR/process-instructions.lisp
@@ -6,36 +6,6 @@
   (declare (ignore lexical-locations))
   (error "Don't know how to process instruction ~s" instruction))
 
-(defmethod process-instruction
-    ((instruction cleavir-ir:assignment-instruction) lexical-locations)
-  (let ((input (first (cleavir-ir:inputs instruction)))
-        (output (first (cleavir-ir:outputs instruction))))
-    (cond ((and (typep input 'cleavir-ir:register-location)
-                (typep output 'cleavir-ir:register-location))
-           ;; Leave it as it is
-           nil)
-          ((and (or (typep input 'cleavir-ir:register-location)
-                    (typep input 'cleavir-ir:immediate-input))
-                (typep output 'cleavir-ir:lexical-location))
-           (let ((immediate-input
-                   (make-instance 'cleavir-ir:immediate-input
-                     :value (+ (gethash output lexical-locations) 8))))
-             (cleavir-ir:insert-instruction-before
-              (make-instance 'cleavir-ir:assignment-instruction
-                :input *rbp*
-                :output *r11*)
-              instruction)
-             (cleavir-ir:insert-instruction-before
-              (make-instance 'cleavir-ir:unsigned-sub-instruction
-                :inputs (list *r11* immediate-input)
-                :output *r11*)
-              instruction)
-             (change-class instruction
-                           'cleavir-ir:memset1-instruction
-                           :inputs (list *r11* input)
-                           :outputs '())))
-          (t (error "Can't handle input ~s and output ~s" input output)))))
-
 (defun insert-memref-before
     (instruction lexical-location register lexical-locations)
   (let ((immediate-input
@@ -77,25 +47,3 @@
        :input *rbp*
        :output *r11*)
      instruction)))
-
-(defun process-comparison (instruction lexical-locations)
-  (destructuring-bind (input1 input2)
-      (cleavir-ir:inputs instruction)
-    (when (typep input1 'cleavir-ir:lexical-location)
-      (insert-memref-before input1 *r11* input1 lexical-locations)
-      (setf (first (cleavir-ir:inputs instruction)) *r11*))
-    (when (typep input2 'cleavir-ir:lexical-location)
-      (insert-memref-before input1 *r12* input1 lexical-locations)
-      (setf (second (cleavir-ir:inputs instruction)) *r12*))))
-
-(defmethod process-instruction
-    ((instruction cleavir-ir:unsigned-less-instruction) lexical-locations)
-  (process-comparison instruction lexical-locations))
-
-(defmethod process-instruction
-    ((instruction cleavir-ir:unsigned-not-greater-instruction) lexical-locations)
-  (process-comparison instruction lexical-locations))
-
-(defmethod process-instruction
-    ((instruction cleavir-ir:eq-instruction) lexical-locations)
-  (process-comparison instruction lexical-locations))
