7c25136c20e3dd1cfa40547bd3cc9d19fb26ab9a
Robert Strandh <robert.strandh@gmail.com>
2019-12-14 14:37:31 +0100

    Define default method on PROCESS-INSTRUCTION.

--- Code/Compiler/MIR-to-LIR/process-instructions.lisp
+++ Code/Compiler/MIR-to-LIR/process-instructions.lisp
@@ -2,10 +2,6 @@
 
 (defgeneric process-instruction (instruction lexical-locations))
 
-(defmethod process-instruction (instruction lexical-locations)
-  (declare (ignore lexical-locations))
-  (error "Don't know how to process instruction ~s" instruction))
-
 (defun insert-memref-before
     (instruction lexical-location register lexical-locations)
   (let ((immediate-input
@@ -47,3 +43,28 @@
        :input *rbp*
        :output *r11*)
      instruction)))
+
+(defmethod process-instruction (instruction lexical-locations)
+  (loop for inputs = (cleavir-ir:inputs instruction)
+          then (rest inputs)
+        for register in (list *r11* *r12*)
+        until (null inputs)
+        unless (or (typep (first inputs) 'cleavir-ir:register-location)
+                   (typep (first inputs) 'cleavir-ir:immediate-input))
+          do (insert-memref-before
+              instruction
+              (first inputs)
+              register
+              lexical-locations)
+             (setf (first inputs) register))
+  (loop for outputs = (cleavir-ir:outputs instruction)
+          then (rest outputs)
+        for register in (list *r11* *r12*)
+        until (null outputs)
+        unless (typep (first outputs) 'cleavir-ir:register-location)
+          do (insert-memset-after
+              instruction
+              register
+              (first outputs)
+              lexical-locations)
+             (setf (first outputs) register)))
