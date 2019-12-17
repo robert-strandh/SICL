9b495f84e04e748e8cc4fa21117406243bd63abd
Robert Strandh <robert.strandh@gmail.com>
2019-12-17 15:38:24 +0100

    New component containing instructions for parsing return values.

---
+++ Code/Cleavir2/Intermediate-representation/HIR/return-value-related-instructions.lisp
@@ -0,0 +1,33 @@
+(cl:in-package #:cleavir-ir)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;; Instruction COMPUTE-RETURN-VALUE-COUNT-INSTRUCTION.
+;;;
+;;; This instruction can be used by implementation that would like to
+;;; make return-value parsing explicitly represented in HIR code.  It
+;;; has no input, and it has a single output, which is a fixnum
+;;; containing the number of return values returned from a function
+;;; call.
+
+(defclass compute-return-value-count-instruction
+    (instruction one-successor-mixin)
+  ())
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;; Instruction RETURN-VALUE-INSTRUCTION.
+;;;
+;;; This instruction can be used by implementation that would like to
+;;; make return-value parsing explicitly represented in HIR code.  It
+;;; has a single input, which must contain a non-negative fixnum.  It
+;;; has a single output, which is the return value with the index
+;;; indicated by the input.  The first return value has index 0.  The
+;;; input must have a value that is strictly less than the argument
+;;; count as computed by the instruction
+;;; COMPUTE-RETURN-VALUE-COUNT-INSTRUCTION, except that in
+;;; implementations that always return NIL in the first return-value
+;;; position, an input value of 0 is always valid.
+
+(defclass return-value-instruction (instruction one-successor-mixin)
+  ())
