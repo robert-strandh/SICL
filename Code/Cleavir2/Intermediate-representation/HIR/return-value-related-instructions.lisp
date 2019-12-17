2dd3d2d98fb8c042a0cf9d42b49a7920bbaabe39
Robert Strandh <robert.strandh@gmail.com>
2019-12-17 15:48:43 +0100

    Improve comments.

--- Code/Cleavir2/Intermediate-representation/HIR/return-value-related-instructions.lisp
+++ Code/Cleavir2/Intermediate-representation/HIR/return-value-related-instructions.lisp
@@ -1,14 +1,21 @@
 (cl:in-package #:cleavir-ir)
 
+;;;; These instruction can be used by implementation that would like
+;;;; to make return-value parsing explicitly represented in HIR code.
+;;;;
+;;;; Such an implementation would translate the
+;;;; MULTIPLE-TO-FIXED-INSTRUCTION to tests comparing the value
+;;;; computed by the COMPUTE-RETURN-VALUE-COUNT-INSTRUCTION to fixnums
+;;;; between 0 and the number of fixed outputs of the
+;;;; MULTIPLE-TO-FIXED-INSTRUCTION.
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;
 ;;; Instruction COMPUTE-RETURN-VALUE-COUNT-INSTRUCTION.
 ;;;
-;;; This instruction can be used by implementation that would like to
-;;; make return-value parsing explicitly represented in HIR code.  It
-;;; has no input, and it has a single output, which is a fixnum
-;;; containing the number of return values returned from a function
-;;; call.
+;;; This instruction has no input, and it has a single output, which
+;;; is a fixnum containing the number of return values returned from a
+;;; function call.
 
 (defclass compute-return-value-count-instruction
     (instruction one-successor-mixin)
@@ -18,13 +25,11 @@
 ;;;
 ;;; Instruction RETURN-VALUE-INSTRUCTION.
 ;;;
-;;; This instruction can be used by implementation that would like to
-;;; make return-value parsing explicitly represented in HIR code.  It
-;;; has a single input, which must contain a non-negative fixnum.  It
-;;; has a single output, which is the return value with the index
-;;; indicated by the input.  The first return value has index 0.  The
-;;; input must have a value that is strictly less than the argument
-;;; count as computed by the instruction
+;;; This instruction has a single input, which must contain a
+;;; non-negative fixnum.  It has a single output, which is the return
+;;; value with the index indicated by the input.  The first return
+;;; value has index 0.  The input must have a value that is strictly
+;;; less than the argument count as computed by the instruction
 ;;; COMPUTE-RETURN-VALUE-COUNT-INSTRUCTION, except that in
 ;;; implementations that always return NIL in the first return-value
 ;;; position, an input value of 0 is always valid.
