e6a77861b68b553df68a27fbe9f506651cb3cdc6
Robert Strandh <robert.strandh@gmail.com>
2019-12-21 14:30:35 +0100

    New component containing code for expanding funcall instructions.

---
+++ Code/Compiler/HIR-to-MIR/expand-funcall-instructions.lisp
@@ -0,0 +1,43 @@
+(cl:in-package #:sicl-hir-to-mir)
+
+(defun expand-one-funcall-instruction (instruction)
+  (let ((entry-point-offset-input
+          (make-instance 'cleavir-ir:constant-input :value 2))
+        (entry-point-location
+          (make-instance 'cleavir-ir:lexical-location
+            :name "ENTRY"))
+        (static-environment-offset-input
+          (make-instance 'cleavir-ir:constant-input :value 3))
+        (static-environment-location
+          (make-instance 'cleavir-ir:lexical-location
+            :name "ENV"))
+        (dynamic-environment-location
+          (cleavir-ir:dynamic-environment-location instruction))
+        (function-location (first (cleavir-ir:inputs instruction))))
+    (cleavir-ir:insert-instruction-before
+     (make-instance 'cleavir-ir:nook-read-instruction
+       :inputs (list function-location entry-point-offset-input)
+       :output entry-point-location)
+     instruction)
+    (cleavir-ir:insert-instruction-before
+     (make-instance 'cleavir-ir:nook-read-instruction
+       :inputs (list function-location static-environment-offset-input)
+       :output static-environment-location)
+     instruction)
+    (setf (cleavir-ir:inputs instruction)
+          (list* entry-point-location
+                 static-environment-location
+                 dynamic-environment-location
+                 (rest (cleavir-ir:inputs instruction))))))
+
+(defun expand-funcall-instructions (top-level-enter-instruction)
+  (let ((funcall-instructions '()))
+    (cleavir-ir:map-instructions-arbitrary-order
+     (lambda (instruction)
+       (when (typep instruction 'cleavir-ir:funcall-instruction)
+         (push instruction funcall-instructions)))
+     top-level-enter-instruction)
+    (loop for instruction in funcall-instructions
+          do (expand-one-funcall-instruction instruction))))
+              
+           
