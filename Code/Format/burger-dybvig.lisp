b79b6261b94b0c3765010ce2123698685e96de41
Robert Strandh <robert.strandh@gmail.com>
2019-12-22 17:11:12 +0100

    Untabify and remove useless whitespace at end of lines.

--- Code/Format/burger-dybvig.lisp
+++ Code/Format/burger-dybvig.lisp
@@ -1,5 +1,5 @@
 ;;; First some background.
-;;; 
+;;;
 ;;; Let us say we are dealing with IEEE 754-2008 binary64 floats to
 ;;; get a more concrete example.  Such a number uses 1-bit field
 ;;; containing the representation of the sign of the number, a 52 bit
@@ -15,7 +15,7 @@
 ;;; seen as nonnegative integers.  For instance, the relation between
 ;;; s and S is that when s is +, S is 0 and when s is - then S is 1.
 ;;; The other two are more complicated as explained below.
-;;; 
+;;;
 ;;; There are many possible combinations of mantissa and exponent for
 ;;; a given number, but the IEEE standard imposes a canonical
 ;;; representation in the memory of the computer for a given number.
@@ -42,7 +42,7 @@
 ;;; and E = 2046 and therefore e = 1022.
 ;;;
 ;;; The Common Lisp function decode-float returns m, e, and s as
-;;; described above when the number is represented in normalized form. 
+;;; described above when the number is represented in normalized form.
 ;;;
 ;;; When the absolute value of a number is less than 2^-1022, the
 ;;; representation is switched to something called "denormalized".
@@ -57,9 +57,9 @@
 ;;;
 ;;; Positive and negative 0 is represented with M = 0, and E = 0.
 ;;; This is a natural extension of the denormalized form.  The Common
-;;; Lisp function decode-float returns 0 for m in this case. 
+;;; Lisp function decode-float returns 0 for m in this case.
 ;;;
-;;; Positive and negative infinites are represented with M = 0 and 
+;;; Positive and negative infinites are represented with M = 0 and
 ;;; E = 2047.
 
 ;;; Consider again the normalized representation.  Whereas the number
@@ -90,7 +90,7 @@
 ;;; generate the same number as the one we started with.  By an
 ;;; "accurate floating-point read routine" we mean one that generates
 ;;; the floating-point value that is closest to the exact (rational)
-;;; value represented by the printed representation. 
+;;; value represented by the printed representation.
 
 ;;; The main idea in the paper by Burger and Dybvig is to use exact
 ;;; rational arithmetic to determine how close the printed
@@ -153,7 +153,7 @@
 ;;; independently of this scaling.  However, adding and subtracting 1
 ;;; from m is no longer valid if m is scaled.  But if we compute
 ;;; (m + 1) * 2^e as v + 2^e and (m - 1) * 2^e as v - 2^e, we are
-;;; independent of any scaling between m and e. 
+;;; independent of any scaling between m and e.
 
 ;;; The second problem we have is that the test to determine the
 ;;; special case for v- depends on the which floating-point
@@ -165,17 +165,17 @@
 ;;;
 ;;; if 2^(e-1) is too small, i.e, v - 2^(e-1) would give the same number
 ;;; back if converted to a float, then use 2^e instead.  So we simply
-;;; try with 2^(e-1) first, and if that doesn't work, use 2^e instead. 
-;;; This method is independent of the precision of the floating-point 
+;;; try with 2^(e-1) first, and if that doesn't work, use 2^e instead.
+;;; This method is independent of the precision of the floating-point
 ;;; number, and depends only on IEEE-style representation of normalized
-;;; and denormalized numbers. 
+;;; and denormalized numbers.
 
 ;;; Once we have v+ and v- as above, we can compute the average between
 ;;; v- and v and call it low, and the average between v and v+ and call
 ;;; it high.  If we have a sequence of decimal digits that represent a
 ;;; rational value greater than low and smaller than high, then we know
 ;;; that an accurate floating-point reading routine will read it back
-;;; to the exact same value as v. 
+;;; to the exact same value as v.
 
 ;;; The following functions needs to make some assumption about the
 ;;; way floats are represented.  We shall assume IEEE-style
@@ -183,8 +183,8 @@
 ;;; as described above.
 
 ;;; Given a floating-point number x, compute its successor.
-;;; The number x must be strictly smaller than the maximum 
-;;; floating-point number, and strictly greater than 0. 
+;;; The number x must be strictly smaller than the maximum
+;;; floating-point number, and strictly greater than 0.
 (defun successor (x)
   (multiple-value-bind (m e)
       (decode-float x)
@@ -197,81 +197,81 @@
 (defun predecessor (x)
   (let ((p (float-precision x)))
     (multiple-value-bind (m e)
-	(decode-float x)
+        (decode-float x)
       (declare (ignore m))
       (if (and (= (decode-float x) 0.5)
-	       (/= (- x (scale-float (float 1 x) (- e 1 p))) x))
-	  (- x (scale-float (float 1 x) (- e 1 p)))
-	  (- x (scale-float (float 1 x) (- e p)))))))
+               (/= (- x (scale-float (float 1 x) (- e 1 p))) x))
+          (- x (scale-float (float 1 x) (- e 1 p)))
+          (- x (scale-float (float 1 x) (- e p)))))))
 
 ;;; This function goes through all single floats and checks that for a
 ;;; given float x, the successor of the predecessor x is x, as defined
-;;; by the two precedging functions. 
+;;; by the two precedging functions.
 (defun test-all ()
   (loop for x = most-positive-single-float then y
-	for y = (predecessor x)
-	for i from 0
-	until (= y 0)
-	do (when (zerop (mod i 1000000))
-	     (print x *trace-output*)
-	     (finish-output *trace-output*))
-	do (assert (= x (successor y)))))
+        for y = (predecessor x)
+        for i from 0
+        until (= y 0)
+        do (when (zerop (mod i 1000000))
+             (print x *trace-output*)
+             (finish-output *trace-output*))
+        do (assert (= x (successor y)))))
 
-;;; Given a rational r, return the smallest integer k such that 
+;;; Given a rational r, return the smallest integer k such that
 ;;; r < 10^k.  We use the floating-point logarithmic function
 ;;; to find an approximate value of k, then we find the exact
 ;;; one by a small search around the appriximation.
 (defun scale (r)
   (let* ((try (1- (ceiling (log r 10))))
-	 (expt (expt 10 try)))
+         (expt (expt 10 try)))
     (loop while (<= r expt)
-	  do (decf try)
-	     (setf expt (/ expt 10)))
+          do (decf try)
+             (setf expt (/ expt 10)))
     (loop until (<= r expt)
-	  do (incf try)
-	     (setf expt (* expt 10)))
+          do (incf try)
+             (setf expt (* expt 10)))
     try))
 
 ;;; This is a trivial and inefficient implementation of
 ;;; the first algorithm in the Burger-Dybvig paper.  It is used
 ;;; for testing-purposes only.  It returns two values, a list
-;;; of numbers that represent the individual digits of the result, 
-;;; and the scale factor. 
+;;; of numbers that represent the individual digits of the result,
+;;; and the scale factor.
 
 (defun burger-dybvig-1 (x)
   (let* ((v (rational x))
-	 (v- (rational (predecessor x)))
-	 (v+ (rational (successor x)))
-	 (low (/ (+ v v-) 2))
-	 (high (/ (+ v v+) 2))
-	 (scale (scale high)))
+         (v- (rational (predecessor x)))
+         (v+ (rational (successor x)))
+         (low (/ (+ v v-) 2))
+         (high (/ (+ v v+) 2))
+         (scale (scale high)))
     (loop with value = 0
-	  with result = '()
-	  for factor = 1/10 then (/ factor 10)
-	  for q = (/ v (expt 10 scale)) then (- (* q 10) (floor (* q 10)))
-	  for d = (floor (* q 10))
-	  do (let ((low-out (* (+ value (* factor d)) (expt 10 scale)))
-		   (high-out (* (+ value (* factor (1+ d))) (expt 10 scale))))
-	       (cond ((and (> low-out low)
-			   (>= high-out high))
-		      (push d result) (loop-finish))
-		     ((and (<= low-out low)
-			   (< high-out high))
-		      (push (1+ d) result) (loop-finish))
-		     ((and (> low-out low)
-			   (< high-out high))
-		      (cond ((< (abs (- v low-out))
-				(abs (- v high-out)))
-			     (push d result))
-			    ((< (abs (- v high-out))
-				(abs (- v low-out)))
-			     (push (1+ d) result))
-			    (t ;; beak the tie
-			     (push (1+ d) result)))
-		      (loop-finish))
-		     (t
-		      (push d result) (incf value (* factor d)))))
-	  finally (return (values (nreverse result) scale)))))
+          with result = '()
+          for factor = 1/10 then (/ factor 10)
+          for q = (/ v (expt 10 scale)) then (- (* q 10) (floor (* q 10)))
+          for d = (floor (* q 10))
+          do (let ((low-out (* (+ value (* factor d)) (expt 10 scale)))
+                   (high-out (* (+ value (* factor (1+ d))) (expt 10 scale))))
+               (cond ((and (> low-out low)
+                           (>= high-out high))
+                      (push d result) (loop-finish))
+                     ((and (<= low-out low)
+                           (< high-out high))
+                      (push (1+ d) result) (loop-finish))
+                     ((and (> low-out low)
+                           (< high-out high))
+                      (cond ((< (abs (- v low-out))
+                                (abs (- v high-out)))
+                             (push d result))
+                            ((< (abs (- v high-out))
+                                (abs (- v low-out)))
+                             (push (1+ d) result))
+                            (t ;; beak the tie
+                             (push (1+ d) result)))
+                      (loop-finish))
+                     (t
+                      (push d result) (incf value (* factor d)))))
+          finally (return (values (nreverse result) scale)))))
 
 ;;; This function uses a very direct method to generate floating
 ;;; point digits.  It uses floating-point arithmetic, so it may
@@ -284,56 +284,56 @@
 ;;; enough for some people.  For that reason, we might suggest
 ;;; an extension to the CL standard where a special variable
 ;;; is used to control whether fast printing should be used
-;;; instead. 
+;;; instead.
 (defun generate-digits-using-fp-arithmetic (x)
   (let* ((v- (predecessor x))
-	 (v+ (successor x))
-	 (s (scale (/ (+ (rational x) (rational v+)) 2)))
-	 (scale (expt 10 (1- s))) 
-	 (d- (- (/ (/ (- x v-) 2d0) scale)))
-	 (d+ (/ (/ (- v+ x) 2d0) scale))
-	 (scaled (/ x scale)))
+         (v+ (successor x))
+         (s (scale (/ (+ (rational x) (rational v+)) 2)))
+         (scale (expt 10 (1- s)))
+         (d- (- (/ (/ (- x v-) 2d0) scale)))
+         (d+ (/ (/ (- v+ x) 2d0) scale))
+         (scaled (/ x scale)))
     (values (loop with result = '()
-		  do (let ((digit (floor scaled)))
-		       (cond ((< (- scaled digit) d+)
-			      (push digit result)
-			      (loop-finish))
-			     ((> (- scaled (1+ digit)) d-)
-			      (push (1+ digit) result)
-			      (loop-finish))
-			     (t
-			      (push digit result)
-			      (setf scaled (* 10 (- scaled digit)))
-			      (setf d- (* d- 10))
-			      (setf d+ (* d+ 10)))))
-		  finally (return (nreverse result)))
-	    s)))
+                  do (let ((digit (floor scaled)))
+                       (cond ((< (- scaled digit) d+)
+                              (push digit result)
+                              (loop-finish))
+                             ((> (- scaled (1+ digit)) d-)
+                              (push (1+ digit) result)
+                              (loop-finish))
+                             (t
+                              (push digit result)
+                              (setf scaled (* 10 (- scaled digit)))
+                              (setf d- (* d- 10))
+                              (setf d+ (* d+ 10)))))
+                  finally (return (nreverse result)))
+            s)))
 
 (defun int-1 (x)
   (let* ((v- (predecessor x))
-	 (v+ (successor x))
-	 (s (scale (/ (+ (rational x) (rational v+)) 2)))
-	 (scale (expt 10 (1- s))) 
-	 (d- (- (/ (/ (rational (- x v-)) 2) scale)))
-	 (d+ (/ (/ (rational (- v+ x)) 2) scale))
-	 (scaled (/ (rational x) scale)))
+         (v+ (successor x))
+         (s (scale (/ (+ (rational x) (rational v+)) 2)))
+         (scale (expt 10 (1- s)))
+         (d- (- (/ (/ (rational (- x v-)) 2) scale)))
+         (d+ (/ (/ (rational (- v+ x)) 2) scale))
+         (scaled (/ (rational x) scale)))
     (values (loop with result = '()
-		  do (let ((digit (floor scaled)))
-		       (cond ((< (- scaled digit) d+)
-			      (push digit result)
-			      (loop-finish))
-			     ((> (- scaled (1+ digit)) d-)
-			      (push (1+ digit) result)
-			      (loop-finish))
-			     (t
-			      (push digit result)
-			      (setf scaled (* 10 (- scaled digit)))
-			      (setf d- (* d- 10))
-			      (setf d+ (* d+ 10)))))
-		  finally (return (nreverse result)))
-	    s)))
+                  do (let ((digit (floor scaled)))
+                       (cond ((< (- scaled digit) d+)
+                              (push digit result)
+                              (loop-finish))
+                             ((> (- scaled (1+ digit)) d-)
+                              (push (1+ digit) result)
+                              (loop-finish))
+                             (t
+                              (push digit result)
+                              (setf scaled (* 10 (- scaled digit)))
+                              (setf d- (* d- 10))
+                              (setf d+ (* d+ 10)))))
+                  finally (return (nreverse result)))
+            s)))
 
-;;; This is a direct implementation of the second algorithm of the 
+;;; This is a direct implementation of the second algorithm of the
 ;;; Burger & Dybvig paper.  It is not modeled after their Scheme code,
 ;;; but reimplements the algorithm they present in Common Lisp.
 (defun burger-dybvig-2 (x)
@@ -345,66 +345,66 @@
       (decf e diff))
     (let (r s m+ m-)
       (if (>= e 0)
-	  (progn (if (= (decode-float x) 0.5)
-		     (setf m- (expt 2 e)
-			   m+ (* m- 2)
-			   s 4
-			   r (* f m+  2))
-		     (setf m- (expt 2 e)
-			   m+ m-
-			   s 2
-			   r (* f m+ 2))))
-	  (progn (if (and (= (decode-float x) 0.5)
-			  (= (float-precision x)
-			     (float-precision (predecessor x))))
-		     (setf m- 1
-			   m+ 2
-			   s (* (expt 2 (- 1 e)) 2)
-			   r (* f 4))
-		     (setf m- 1
-			   m+ 1
-			   s (* (expt 2 (- e)) 2)
-			   r (* f 2)))))
+          (progn (if (= (decode-float x) 0.5)
+                     (setf m- (expt 2 e)
+                           m+ (* m- 2)
+                           s 4
+                           r (* f m+  2))
+                     (setf m- (expt 2 e)
+                           m+ m-
+                           s 2
+                           r (* f m+ 2))))
+          (progn (if (and (= (decode-float x) 0.5)
+                          (= (float-precision x)
+                             (float-precision (predecessor x))))
+                     (setf m- 1
+                           m+ 2
+                           s (* (expt 2 (- 1 e)) 2)
+                           r (* f 4))
+                     (setf m- 1
+                           m+ 1
+                           s (* (expt 2 (- e)) 2)
+                           r (* f 2)))))
       (let ((k (scale (/ (+ r m+) s))))
-	(if (>= k 0)
-	    (setf s (* s (expt 10 k)))
-	    (let ((coeff (expt 10 (- k))))
-	      (setf r (* r coeff)
-		    m+ (* m+ coeff)
-		    m- (* m- coeff))))
-	(loop with result = '()
-	      do (multiple-value-bind (quotient remainder)
-		     (floor (* r 10) s)
-		   (setf r remainder
-			 m+ (* m+ 10)
-			 m- (* m- 10))
-		   (if (and (>= r m-) (<= (+ r m+) s))
-		       (push quotient result)
-		       (progn (push (+ quotient
-				       (if (< r m-)
-					   (if (> (+ r m+) s)
-					       ;; break the tie
-					       (if (< (* 2 r) s) 0 1)
-					       0)
-					   1))
-				    result)
-			      (loop-finish))))
-	      finally (return (values (nreverse result) k)))))))
+        (if (>= k 0)
+            (setf s (* s (expt 10 k)))
+            (let ((coeff (expt 10 (- k))))
+              (setf r (* r coeff)
+                    m+ (* m+ coeff)
+                    m- (* m- coeff))))
+        (loop with result = '()
+              do (multiple-value-bind (quotient remainder)
+                     (floor (* r 10) s)
+                   (setf r remainder
+                         m+ (* m+ 10)
+                         m- (* m- 10))
+                   (if (and (>= r m-) (<= (+ r m+) s))
+                       (push quotient result)
+                       (progn (push (+ quotient
+                                       (if (< r m-)
+                                           (if (> (+ r m+) s)
+                                               ;; break the tie
+                                               (if (< (* 2 r) s) 0 1)
+                                               0)
+                                           1))
+                                    result)
+                              (loop-finish))))
+              finally (return (values (nreverse result) k)))))))
 
 ;;; Test that the two implemetations above give the same result
 ;;; for all single floats.  Running this test may take a few days
 ;;; on a good 64-bit machine.
 (defun test-dybvig-2 (start)
   (loop for x = start then (predecessor x)
-	for i from 0
-	until (= x 0)
-	do (when (zerop (mod i 1000000))
-	     (format *trace-output* "~s~%" x)
-	     (finish-output *trace-output*))
-	do (multiple-value-bind (d1 k1)
-	       (burger-dybvig-1 x)
-	     (multiple-value-bind (d2 k2)
-		 (burger-dybvig-2 x)
-	       (when (not (and (equal d1 d2)
-			       (= k1 k2)))
-		 (format *trace-output* "no: ~s~%" x))))))
+        for i from 0
+        until (= x 0)
+        do (when (zerop (mod i 1000000))
+             (format *trace-output* "~s~%" x)
+             (finish-output *trace-output*))
+        do (multiple-value-bind (d1 k1)
+               (burger-dybvig-1 x)
+             (multiple-value-bind (d2 k2)
+                 (burger-dybvig-2 x)
+               (when (not (and (equal d1 d2)
+                               (= k1 k2)))
+                 (format *trace-output* "no: ~s~%" x))))))
