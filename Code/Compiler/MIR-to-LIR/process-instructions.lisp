86983154ccbf305420beda4fe50dc51d9552d2df
Robert Strandh <robert.strandh@gmail.com>
2019-12-29 12:59:14 +0100

    Do not alter the outputs of the ENTER-INSTRUCTION.

--- Code/Compiler/MIR-to-LIR/process-instructions.lisp
+++ Code/Compiler/MIR-to-LIR/process-instructions.lisp
@@ -2,6 +2,9 @@
 
 (defgeneric process-instruction (instruction lexical-locations))
 
+(defmethod process-instruction (instruction lexical-locations)
+  nil)
+
 (defun insert-memref-before
     (instruction lexical-location register lexical-locations)
   (let ((immediate-input
@@ -44,27 +47,28 @@
        :output *r11*)
      instruction)))
 
-(defmethod process-instruction (instruction lexical-locations)
-  (loop for inputs = (cleavir-ir:inputs instruction)
-          then (rest inputs)
-        for register in (list *r11* *r12*)
-        until (null inputs)
-        unless (or (typep (first inputs) 'cleavir-ir:register-location)
-                   (typep (first inputs) 'cleavir-ir:immediate-input))
-          do (insert-memref-before
-              instruction
-              (first inputs)
-              register
-              lexical-locations)
-             (setf (first inputs) register))
-  (loop for outputs = (cleavir-ir:outputs instruction)
-          then (rest outputs)
-        for register in (list *r11* *r12*)
-        until (null outputs)
-        unless (typep (first outputs) 'cleavir-ir:register-location)
-          do (insert-memset-after
-              instruction
-              register
-              (first outputs)
-              lexical-locations)
-             (setf (first outputs) register)))
+(defmethod process-instruction :before (instruction lexical-locations)
+  (unless (typep instruction 'cleavir-ir:enter-instruction)
+    (loop for inputs = (cleavir-ir:inputs instruction)
+            then (rest inputs)
+          for register in (list *r11* *r12*)
+          until (null inputs)
+          unless (or (typep (first inputs) 'cleavir-ir:register-location)
+                     (typep (first inputs) 'cleavir-ir:immediate-input))
+            do (insert-memref-before
+                instruction
+                (first inputs)
+                register
+                lexical-locations)
+               (setf (first inputs) register))
+    (loop for outputs = (cleavir-ir:outputs instruction)
+            then (rest outputs)
+          for register in (list *r11* *r12*)
+          until (null outputs)
+          unless (typep (first outputs) 'cleavir-ir:register-location)
+            do (insert-memset-after
+                instruction
+                register
+                (first outputs)
+                lexical-locations)
+               (setf (first outputs) register))))
