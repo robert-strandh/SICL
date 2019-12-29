5c80123377ad68025d6bf8cf8e1693130b3874d3
Robert Strandh <robert.strandh@gmail.com>
2019-12-29 16:27:02 +0100

    Export names of special variables holding registers.

--- Code/Compiler/MIR-to-LIR/packages.lisp
+++ Code/Compiler/MIR-to-LIR/packages.lisp
@@ -3,4 +3,21 @@
 (defpackage #:sicl-mir-to-lir
   (:use #:common-lisp)
   (:export
-   #:mir-to-lir))
+   #:mir-to-lir
+   #:*rax*
+   #:*rax*
+   #:*rbx*
+   #:*rcx*
+   #:*rdx*
+   #:*rsp*
+   #:*rbp*
+   #:*rsi*
+   #:*rdi*
+   #:*r8*
+   #:*r9*
+   #:*r10*
+   #:*r11*
+   #:*r12*
+   #:*r13*
+   #:*r14*
+   #:*r15*))
