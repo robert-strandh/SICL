4a10a076201174fc50cacead1578681d15d87465
Robert Strandh <robert.strandh@gmail.com>
2019-12-29 16:26:38 +0100

    Define method on TRANSLATE-INSTRUCTION for ASSIGNMENT-INSTRUCTION.

--- Code/Compiler/Code-generation/translate-instruction.lisp
+++ Code/Compiler/Code-generation/translate-instruction.lisp
@@ -2,3 +2,9 @@
 
 (defgeneric translate-instruction (instruction))
 
+(defmethod translate-instruction
+    ((instruction cleavir-ir:assignment-instruction))
+  (make-instance 'cluster:code-command
+    :mnemonic "MOV"
+    :operands (list (translate-datum (first (cleavir-ir:outputs instruction)))
+                    (translate-datum (first (cleavir-ir:inputs instruction))))))
