c57e750a98cfe912b4035524a543dfef65b35eab
Robert Strandh <robert.strandh@gmail.com>
2019-12-29 16:33:10 +0100

    Define method on TRANSLATE-INSTRUCTION for MEMREF1-INSTRUCTION.

--- Code/Compiler/Code-generation/translate-instruction.lisp
+++ Code/Compiler/Code-generation/translate-instruction.lisp
@@ -6,5 +6,18 @@
     ((instruction cleavir-ir:assignment-instruction))
   (make-instance 'cluster:code-command
     :mnemonic "MOV"
-    :operands (list (translate-datum (first (cleavir-ir:outputs instruction)))
-                    (translate-datum (first (cleavir-ir:inputs instruction))))))
+    :operands
+    (list
+     (translate-datum (first (cleavir-ir:outputs instruction)))
+     (translate-datum (first (cleavir-ir:inputs instruction))))))
+
+(defmethod translate-instruction
+    ((instruction cleavir-ir:memref1-instruction))
+  (make-instance 'cluster:code-command
+    :mnemonic "MOV"
+    :operands
+    (list
+     (translate-datum (first (cleavir-ir:outputs instruction)))
+     (make-instance 'cluster:memory-operand
+       :base-register (translate-datum (first (clevir-ir:inputs instruction)))
+       :size 64))))
