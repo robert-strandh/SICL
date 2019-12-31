89fb71a744049dcdcf096dd45b7a459de6f2df07
Robert Strandh <robert.strandh@gmail.com>
2019-12-31 18:32:41 +0100

    Take into account arguments on the stack.

--- Code/Compiler/MIR-to-LIR/funcall.lisp
+++ Code/Compiler/MIR-to-LIR/funcall.lisp
@@ -1,6 +1,25 @@
 (cl:in-package #:sicl-mir-to-lir)
 
-(defun do-arguments (instruction)
+(defun load-lexical (lexical-location into-register instruction lexical-locations)
+  (cleavir-ir:insert-instruction-before
+   (make-instance 'cleavir-ir:assignment-instruction
+     :input *rbp*
+     :output into-register)
+   instruction)
+  (cleavir-ir:insert-instruction-before
+   (make-instance 'cleavir-ir:unsigned-sub-instruction
+     :minuend into-register
+     :subtrahend (make-instance 'cleavir-ir:immediate-input
+                   :value (* 8 (1+ (gethash lexical-location lexical-locations))))
+     :output into-register)
+   instruction)
+  (cleavir-ir:insert-instruction-before
+   (make-instance 'cleavir-ir:memref1-instruction
+     :address into-register
+     :output into-register)
+   instruction))
+
+(defun do-arguments (instruction lexical-locations)
   (let ((inputs (cleavir-ir:inputs instruction)))
     (when (> (- (length inputs) 3) 5)
       (loop for argument in (reverse (subseq inputs (+ 3 5)))
@@ -9,19 +28,30 @@
                 (make-instance 'cleavir-ir:unsigned-sub-instruction
                   :inputs (list *rsp* constant-8)
                   :output *rsp*)
+                instruction)
+               (when (typep argument 'cleavir-ir:lexical-location)
+                 (load-lexical argument *r11* instruction lexical-locations))
+               (cleavir-ir:insert-instruction-before
+                (make-instance 'cleavir-ir:memset1-instruction
+                  :address *rsp*
+                  :value (if (typep argument 'cleavir-ir:lexical-location)
+                             *r11*
+                             argument))
                 instruction)))
     (loop for argument in (subseq inputs 3 (min (+ 3 5) (length inputs)))
           for register in (list *rdi* *rsi* *rdx* *rcx* *r8*)
-          do (cleavir-ir:insert-instruction-before
-              (make-instance 'cleavir-ir:assignment-instruction
-                :input argument
-                :output register)
-              instruction))))
+          do (if (typep argument 'cleavir-ir:lexical-location)
+                 (load-lexical argument register instruction lexical-locations)
+                 (cleavir-ir:insert-instruction-before
+                  (make-instance 'cleavir-ir:assignment-instruction
+                    :input argument
+                    :output register)
+                  instruction)))))
 
 (defmethod process-instruction
     ((instruction cleavir-ir:funcall-instruction)
      lexical-locations)
-  (do-arguments instruction)
+  (do-arguments instruction lexical-locations)
   (let ((inputs (cleavir-ir:inputs instruction)))
      ;; Store the static environment to R10.
     (cleavir-ir:insert-instruction-before
