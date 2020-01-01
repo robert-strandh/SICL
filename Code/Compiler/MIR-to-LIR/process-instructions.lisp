9f4eb0303702f0f752151088ae782ace3ccbf5ee
Robert Strandh <robert.strandh@gmail.com>
2020-01-01 13:31:02 +0100

    Factor code for coputing the address of a lexical location.

--- Code/Compiler/MIR-to-LIR/process-instructions.lisp
+++ Code/Compiler/MIR-to-LIR/process-instructions.lisp
@@ -5,47 +5,54 @@
 (defmethod process-instruction (instruction lexical-locations)
   nil)
 
-(defun insert-memref-before
-    (instruction lexical-location register lexical-locations)
-  (let ((immediate-input
-          (make-instance 'cleavir-ir:immediate-input
-            :value (+ (gethash lexical-location lexical-locations) 8))))
-    (cleavir-ir:insert-instruction-before
-     (make-instance 'cleavir-ir:assignment-instruction
-       :input *rbp*
-       :output *r11*)
-     instruction)
-    (cleavir-ir:insert-instruction-before
-     (make-instance 'cleavir-ir:unsigned-sub-instruction
-       :inputs (list *r11* immediate-input)
-       :output *r11*)
-     instruction)
-    (cleavir-ir:insert-instruction-before
-     (make-instance 'cleavir-ir:memref1-instruction
-       :input *r11*
-       :output register)
-     instruction)))
-
-(defun insert-memset-after
-    (instruction register lexical-location lexical-locations)
+;;; Return a list of instructions that, when executed, loads the
+;;; address of LEXICAL-LOCATION into TO-REGISTER.
+(defun load-address-of-lexical-location
+    (lexical-location
+     to-register
+     lexical-locations)
   (let ((immediate-input
           (make-instance 'cleavir-ir:immediate-input
             :value (* (1+ (gethash lexical-location lexical-locations)) 8))))
-    (cleavir-ir:insert-instruction-after
-     (make-instance 'cleavir-ir:memset1-instruction
-       :inputs (list *r11* register)
-       :output register)
-     instruction)
-    (cleavir-ir:insert-instruction-after
-     (make-instance 'cleavir-ir:unsigned-sub-instruction
-       :inputs (list *r11* immediate-input)
-       :output *r11*)
-     instruction)
-    (cleavir-ir:insert-instruction-after
-     (make-instance 'cleavir-ir:assignment-instruction
-       :input *rbp*
-       :output *r11*)
-     instruction)))
+    (list (make-instance 'cleavir-ir:assignment-instruction
+            :input *rbp*
+            :output to-register)
+          (make-instance 'cleavir-ir:unsigned-sub-instruction
+            :inputs (list to-register immediate-input)
+            :output to-register))))
+
+(defun insert-memref-before
+    (instruction
+     from-lexical-location
+     to-register
+     scratch-register
+     lexical-locations)
+  (let ((load-instructions
+          (load-address-of-lexical-location
+           from-lexical-location scratch-register lexical-locations)))
+    (loop for load-instruction in load-instructions
+          do (cleavir-ir:insert-instruction-before load-instruction instruction)))
+  (cleavir-ir:insert-instruction-before
+   (make-instance 'cleavir-ir:memref1-instruction
+     :input scratch-register
+     :output to-register)
+   instruction))
+
+(defun insert-memset-after
+    (instruction
+     from-register
+     to-lexical-location
+     scratch-register
+     lexical-locations)
+  (cleavir-ir:insert-instruction-after
+   (make-instance 'cleavir-ir:memset1-instruction
+     :inputs (list scratch-register from-register))
+   instruction)
+  (let ((load-instructions
+          (load-address-of-lexical-location
+           to-lexical-location scratch-register lexical-locations)))
+    (loop for load-instruction in (reverse load-instructions)
+          do (cleavir-ir:insert-instruction-after load-instruction instruction))))
 
 (defmethod process-instruction :before (instruction lexical-locations)
   (unless (typep instruction 'cleavir-ir:enter-instruction)
@@ -59,6 +66,7 @@
                 instruction
                 (first inputs)
                 register
+                *r11*
                 lexical-locations)
                (setf (first inputs) register))
     (loop for outputs = (cleavir-ir:outputs instruction)
@@ -70,5 +78,6 @@
                 instruction
                 register
                 (first outputs)
+                *r11*
                 lexical-locations)
                (setf (first outputs) register))))
