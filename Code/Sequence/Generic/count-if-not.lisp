0f21e31d4bcfd747ceba71e5d0f8f820e7f2e5bf
Marco Heisig <marco.heisig@fau.de>
2020-01-02 11:53:02 +0100

    Implement count, count-if and count-if-not.

--- Code/Sequence/Generic/count-if-not.lisp
+++ Code/Sequence/Generic/count-if-not.lisp
@@ -1,1 +1,24 @@
 (cl:in-package #:sicl-sequence)
+
+(defmethod count-if-not (predicate (datum t) &key &allow-other-keys)
+  (error 'must-be-sequence
+         :datum datum))
+
+(defmethod count-if-not (predicate (list list) &key from-end start end key)
+  (let ((count 0))
+    (with-predicate (predicate predicate)
+      (with-key-function (key key)
+        (for-each-relevant-cons (cons index list start end from-end)
+          (unless (predicate (key (car cons)))
+            (incf count)))))
+    count))
+
+(replicate-for-each-relevant-vectoroid #1=#:vectoroid
+  (defmethod count-if-not (predicate (vectoroid #1#) &key from-end start end key)
+    (let ((count 0))
+      (with-predicate (predicate predicate)
+        (with-key-function (key key)
+          (for-each-relevant-element (element index vectoroid start end from-end)
+            (unless (predicate (key element))
+              (incf count)))))
+      count)))
--- Code/Sequence/Generic/count.lisp
+++ Code/Sequence/Generic/count.lisp
@@ -1,1 +1,24 @@
 (cl:in-package #:sicl-sequence)
+
+(defmethod count (item (datum t) &key &allow-other-keys)
+  (error 'must-be-sequence
+         :datum datum))
+
+(defmethod count (item (list list) &key from-end start end key test test-not)
+  (let ((count 0))
+    (with-test-function (test test test-not)
+      (with-key-function (key key)
+        (for-each-relevant-cons (cons index list start end from-end)
+          (when (test item (key (car cons)))
+            (incf count)))))
+    count))
+
+(replicate-for-each-relevant-vectoroid #1=#:vectoroid
+  (defmethod count (item (vectoroid #1#) &key from-end start end key test test-not)
+    (let ((count 0))
+      (with-test-function (test test test-not)
+        (with-key-function (key key)
+          (for-each-relevant-element (element index vectoroid start end from-end)
+            (when (test item (key element))
+              (incf count)))))
+      count)))
