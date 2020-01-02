30e5c9c5fb213390f9cebd2a2015552bb4bf2c97
Marco Heisig <marco.heisig@fau.de>
2020-01-02 12:07:48 +0100

    Implement find-if and find-if-not.

--- Code/Sequence/Generic/find-if-not.lisp
+++ Code/Sequence/Generic/find-if-not.lisp
@@ -1,2 +1,21 @@
 (cl:in-package #:sicl-sequence)
 
+(defmethod find-if-not (predicate (datum t) &key &allow-other-keys)
+  (error 'must-be-sequence
+         :datum datum))
+
+(defmethod find-if-not (predicate (list list) &key from-end start end key)
+  (with-predicate (predicate predicate)
+    (with-key-function (key key)
+      (for-each-relevant-cons (cons index list start end from-end)
+        (let ((element (car cons)))
+          (unless (predicate (key element))
+            (return-from find-if-not element)))))))
+
+(replicate-for-each-relevant-vectoroid #1=#:vectoroid
+  (defmethod find-if-not (predicate (vectoroid #1#) &key from-end start end key)
+    (with-predicate (predicate predicate)
+      (with-key-function (key key)
+        (for-each-relevant-element (element index vectoroid start end from-end)
+          (unless (predicate (key element))
+            (return-from find-if-not element)))))))
