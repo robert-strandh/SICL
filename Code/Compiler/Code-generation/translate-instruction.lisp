861ad92988809184e7b21a985443e6ae674d8062
Robert Strandh <robert.strandh@gmail.com>
2020-01-04 17:05:17 +0100

    Define method on TRANSLATE-INSTRUCTION for UNSIGNED-SUBINSTRUCTION.

--- Code/Compiler/Code-generation/translate-instruction.lisp
+++ Code/Compiler/Code-generation/translate-instruction.lisp
@@ -120,3 +120,24 @@
                 :operands
                 (list
                  (find-instruction-label (second (cleavir-ir:outputs instruction)))))))))
+
+(defmethod translate-instruction
+    ((instruction cleavir-ir:unsigned-sub-instruction))
+  (assert (eq (first (cleavir-ir:inputs instruction))
+              (first (cleavir-ir:outputs instruction))))
+  (let ((successors (cleavir-ir:successors instruction))
+        (add (make-instance 'cluster:code-command
+               :mnemonic "SUB"
+               :operands
+               (list
+                (translate-datum (first (cleavir-ir:inputs instruction)))
+                (translate-datum (second (cleavir-ir:inputs instruction)))))))
+    (if (or (= (length successors) 1)
+            (eq (first successors) (second successors)))
+        add
+        (list add
+              (make-instance 'cluster:code-command
+                :mnemonic "JCF"
+                :operands
+                (list
+                 (find-instruction-label (second (cleavir-ir:outputs instruction)))))))))
