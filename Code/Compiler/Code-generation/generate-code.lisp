9f463cdb8ae467a77cf8a861fd5be4820888d0be
Robert Strandh <robert.strandh@gmail.com>
2020-01-04 16:36:37 +0100

    Provide body of main entry.

--- Code/Compiler/Code-generation/generate-code.lisp
+++ Code/Compiler/Code-generation/generate-code.lisp
@@ -1,5 +1,16 @@
 (cl:in-package #:sicl-code-generation)
 
+(defvar *labels*)
+
+(defun find-instruction-label (instruction)
+  (gethash instruction *labels*))
+
 (defun generate-code (lir)
-  (declare (ignore lir))
-  nil)
+  (let* ((instructions (linearize-lir lir))
+         (*labels* (create-instruction-labels instructions)))
+    (loop for instruction in instructions
+          for translation = (translate-instruction instruction)
+          if (listp translation)
+            append translation
+          else
+            collect translation)))
