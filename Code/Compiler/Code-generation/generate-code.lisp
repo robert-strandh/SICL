2cbb4d4ad86817a185f1989058a2748523c27e52
Robert Strandh <robert.strandh@gmail.com>
2020-01-04 18:05:14 +0100

    Collect labels as well as instructions.

--- Code/Compiler/Code-generation/generate-code.lisp
+++ Code/Compiler/Code-generation/generate-code.lisp
@@ -9,7 +9,10 @@
   (let* ((instructions (linearize-lir lir))
          (*labels* (create-instruction-labels instructions)))
     (loop for instruction in instructions
+          for label = (find-instruction-label instruction)
           for translation = (translate-instruction instruction)
+          unless (null label)
+            collect label
           if (listp translation)
             append translation
           else
