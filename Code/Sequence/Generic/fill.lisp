5185e6591567f539b831a027e8029615ee2709a8
Marco Heisig <marco.heisig@fau.de>
2020-01-06 19:28:01 +0100

    Have fill return the modified sequence.

--- Code/Sequence/Generic/fill.lisp
+++ Code/Sequence/Generic/fill.lisp
@@ -6,9 +6,11 @@
 
 (defmethod fill ((list list) item &key (start 0) end)
   (for-each-relevant-cons (cons index list start end)
-    (setf (car cons) item)))
+    (setf (car cons) item))
+  list)
 
 (replicate-for-each-relevant-vectoroid #1=#:vectoriod
   (defmethod fill ((vectoroid #1#) item &key (start 0) end)
     (for-each-relevant-element (element index vectoroid start end)
-      (setf element item))))
+      (setf element item))
+    vectoroid))
