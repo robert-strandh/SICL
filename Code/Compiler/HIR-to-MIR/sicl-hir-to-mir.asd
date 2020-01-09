59fc5a8b62f72ec6c208a339e1a5974c4154d483
Robert Strandh <robert.strandh@gmail.com>
2020-01-09 14:43:56 +0100

    New component containing constants for indexes into the static environment.

--- Code/Compiler/HIR-to-MIR/sicl-hir-to-mir.asd
+++ Code/Compiler/HIR-to-MIR/sicl-hir-to-mir.asd
@@ -6,6 +6,7 @@
   :serial t
   :components
   ((:file "packages")
+   (:file "static-environment")
    (:file "generic-functions")
    (:file "variables")
    (:file "expand-funcall-instructions")
---
+++ Code/Compiler/HIR-to-MIR/static-environment.lisp
@@ -0,0 +1,11 @@
+(cl:in-package #:sicl-hir-to-mir)
+
+(defparameter +code-object-index+ 0)
+
+(defparameter +enclose-function-index+ 1)
+
+(defparameter +cons-function-index+ 2)
+
+(defparameter +nil-index+ 3)
+
+(defparameter +first-constant-index+ 4)
