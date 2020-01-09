baae1b2e61b43ecf5f8130bbfad23f5f2fc6f29f
Robert Strandh <robert.strandh@gmail.com>
2020-01-09 15:00:14 +0100

    Export names of constant indexes into the static environment.

--- Code/Compiler/HIR-to-MIR/packages.lisp
+++ Code/Compiler/HIR-to-MIR/packages.lisp
@@ -4,4 +4,9 @@
   (:use #:common-lisp)
   (:export #:hir-to-mir
            #:entry-point-input
-           #:enter-instruction))
+           #:enter-instruction
+           +code-object-index+
+           +enclose-function-index+
+           +cons-function-index+
+           +nil-index+
+           +first-constant-index+))
