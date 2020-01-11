be654ad4ea5c202511ab156007c4a27e9bdd58f3
Marco Heisig <marco.heisig@fau.de>
2020-01-11 18:05:16 +0100

    Provide implementations of LENGTH and ELT for lists.

--- Code/Sequence/Generic/elt.lisp
+++ Code/Sequence/Generic/elt.lisp
@@ -5,7 +5,19 @@
          :datum datum))
 
 (defmethod elt ((list list) index)
-  (cl:elt list index))
+  (check-type index array-index)
+  (labels ((find-elt (rest counter)
+             (if (atom rest)
+                 (if (null rest)
+                     (error 'invalid-sequence-index
+                            :datum list
+                            :expected-type `(integer 0 ,(- index counter)))
+                     (error 'must-be-proper-list
+                            :datum list))
+                 (if (zerop counter)
+                     (car rest)
+                     (find-elt (cdr rest) (1- counter))))))
+    (find-elt list index)))
 
 (defmethod elt ((vector vector) index)
   (cl:elt vector index))
--- Code/Sequence/Generic/length.lisp
+++ Code/Sequence/Generic/length.lisp
@@ -5,7 +5,14 @@
          :datum datum))
 
 (defmethod length ((list list))
-  (cl:length list))
+  (labels ((len (rest counter)
+             (if (atom rest)
+                 (if (null rest)
+                     counter
+                     (error 'must-be-proper-list
+                            :datum list))
+                 (len (cdr rest) (1+ counter)))))
+    (len list 0)))
 
 (defmethod length ((vector vector))
   (cl:length vector))
