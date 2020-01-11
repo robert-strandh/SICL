837eda46023a238356ffd1bbff684b3f8efa4b2c
Robert Strandh <robert.strandh@gmail.com>
2020-01-11 16:45:59 +0100

    New system SICL-COMPILER-BASE with its own package definition.

---
+++ Code/Compiler/packages.lisp
@@ -0,0 +1,5 @@
+(cl:in-package #:common-lisp-user)
+
+(defpackage #:sicl-compiler
+  (:use #:common-lisp)
+  (:export))
---
+++ Code/Compiler/sicl-compiler-base.asd
@@ -0,0 +1,5 @@
+(cl:in-package #:asdf-user)
+
+(defsystem #:sicl-compiler-base
+  :components
+  ((:file "packages")))
