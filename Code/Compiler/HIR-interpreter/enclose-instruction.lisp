ab4ef6f942f279698ef145e6359e792815999b4e
Robert Strandh <robert.strandh@gmail.com>
2020-01-12 10:38:39 +0100

    Use symbolic constant for indices of the static environment.

--- Code/Compiler/HIR-interpreter/enclose-instruction.lisp
+++ Code/Compiler/HIR-interpreter/enclose-instruction.lisp
@@ -8,9 +8,11 @@
          (output (first (cleavir-ir:outputs instruction)))
          (enter (cleavir-ir:code instruction)))
     (setf (lexical-value output lexical-environment)
-          (apply (aref (lexical-value 'static-environment lexical-environment) 1)
+          (apply (aref (lexical-value 'static-environment lexical-environment)
+                       sicl-compiler:+enclose-function-index+)
                  (hir-to-host-function client enter)
-                 (aref (lexical-value 'static-environment lexical-environment) 0)
+                 (aref (lexical-value 'static-environment lexical-environment)
+                       sicl-compiler:+code-object-index+)
                  (loop for input in inputs
                        collect (input-value input lexical-environment)))))
   (first (cleavir-ir:successors instruction)))
