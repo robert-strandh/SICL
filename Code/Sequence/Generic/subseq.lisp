390c2ac4a1192a19c5b99cccd071433a0f013f48
Marco Heisig <marco.heisig@fau.de>
2020-01-14 19:43:42 +0100

    Implement subseq.

--- Code/Sequence/Generic/subseq.lisp
+++ Code/Sequence/Generic/subseq.lisp
@@ -1,1 +1,21 @@
 (cl:in-package #:sicl-sequence)
+
+(defmethod subseq ((datum t) start &optional end)
+  (declare (ignore start end))
+  (error 'must-be-sequence
+         :datum datum))
+
+(defmethod subseq ((list list) start &optional end)
+  (sicl-utilities:with-collectors ((result collect))
+    (let ((read (make-sequence-reader
+                 list start end nil
+                 (lambda () (return-from subseq (result))))))
+      (loop (collect (funcall read))))))
+
+(replicate-for-each-relevant-vectoroid #1=#:vectoroid
+  (defmethod subseq ((vector #1#) start &optional end)
+    (multiple-value-bind (start end)
+        (canonicalize-start-and-end vector (length vector) start end)
+      (replace
+       (make-sequence-like vector (- end start))
+       vector :start2 start :end2 end))))
