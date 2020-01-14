841246a708428eac08eddbb145db24e4faf91f65
Marco Heisig <marco.heisig@fau.de>
2020-01-14 09:15:19 +0100

    Fix a bug in REPLACE.

--- Code/Sequence/Generic/replace.lisp
+++ Code/Sequence/Generic/replace.lisp
@@ -30,7 +30,7 @@
         (loop (funcall dst (funcall src)))))))
 
 (replicate-for-all-compatible-vectoroids #1=#:vectoroid-1 #2=#:vectoroid-2
-  (defmethod replace ((vectoroid-1 #1#) (vectoroid-2 #2#) &key start1 end1 start2 end2)
+  (defmethod replace ((vectoroid-1 #1#) (vectoroid-2 #2#) &key (start1 0) end1 (start2 0) end2)
     (block nil
       (let ((dst (make-vector-writer vectoroid-1 start1 end1 nil (lambda () (return vectoroid-1))))
             (src (make-vector-reader vectoroid-2 start2 end2 nil (lambda () (return vectoroid-1)))))
