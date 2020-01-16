1ad4461c3bc959dcf64e8569d43fe08a8367ecd2
Marco Heisig <marco.heisig@fau.de>
2020-01-16 19:18:48 +0100

    Provide a better implementation of COPY-SEQ.

--- Code/Sequence/Generic/copy-seq.lisp
+++ Code/Sequence/Generic/copy-seq.lisp
@@ -5,8 +5,15 @@
          :datum datum))
 
 (defmethod copy-seq ((list list))
-  (subseq list 0))
+  (sicl-utilities:with-collectors ((result collect))
+    (do ((rest list (cdr rest)))
+        ((atom rest)
+         (unless (null rest)
+           (error 'must-be-proper-list
+                  :datum list))
+         (result))
+      (collect (car rest)))))
 
 (replicate-for-each-relevant-vectoroid #1=#:vectoroid
-  (defmethod copy-seq ((vectoroid #1#))
-    (subseq vectoroid 0)))
+  (defmethod copy-seq ((vector #1#))
+    (make-sequence-like vector (length vector) :initial-contents vector)))
