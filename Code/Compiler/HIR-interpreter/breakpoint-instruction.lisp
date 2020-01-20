01875738c399a7f533d7e0a3229a1dadf841f4e1
Robert Strandh <robert.strandh@gmail.com>
2020-01-20 15:52:34 +0100

    Temporarily comment out calls to Clordane.

--- Code/Compiler/HIR-interpreter/breakpoint-instruction.lisp
+++ Code/Compiler/HIR-interpreter/breakpoint-instruction.lisp
@@ -5,6 +5,6 @@
      (instruction sicl-compiler:breakpoint-instruction)
      lexical-environment)
   (declare (ignore client lexical-environment))
-  (clordane::show (sicl-compiler:debug-information instruction))
-  (clordane::wait)
+  ;; (clordane::show (sicl-compiler:debug-information instruction))
+  ;; (clordane::wait)
   (first (cleavir-ir:successors instruction)))
