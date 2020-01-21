8511e6a05071e6229017e6a617e6df98e769734d
Robert Strandh <robert.strandh@gmail.com>
2020-01-21 10:47:14 +0100

    Remove blank line at end of file.

--- Code/Compiler/HIR-transformations/preprocess-catch.lisp
+++ Code/Compiler/HIR-transformations/preprocess-catch.lisp
@@ -10,10 +10,10 @@
     ((instruction cleavir-ir:catch-instruction))
   (let* ((make-instance-constant-input
            (make-instance 'cleavir-ir:constant-input
-             :value 'make-instance))
+             :value 'sicl-hir-interpreter:augment-with-block/tagbody-entry))
          (make-instance-location
            (make-instance 'cleavir-ir:lexical-location
-             :name "make-instance-function")))
+             :name "augmentation-function")))
     (cleavir-ir:insert-instruction-before
      (make-instance 'cleavir-ir:fdefinition-instruction
        :input make-instance-constant-input
@@ -26,4 +26,3 @@
     (top-level-enter-instruction)
   (cleavir-ir:map-instructions-arbitrary-order
    #'preprocess-catch-instruction top-level-enter-instruction))
-
