b7d8a9ef72aa56c08adc52f4c5e962f532e2bc31
Robert Strandh <robert.strandh@gmail.com>
2020-02-19 18:03:57 +0100

    New system for run-time functionality.

---
+++ Code/Compiler/Run-time/packages.lisp
@@ -0,0 +1,5 @@
+(cl:in-package #:common-lisp-user)
+
+(defpackage #:sicl-run-time
+  (:use #:common-lisp)
+  (:export #:augment-with-block/tagbody-entry))
---
+++ Code/Compiler/Run-time/sicl-run-time.asd
@@ -0,0 +1,6 @@
+(cl:in-package #:asdf-user)
+
+(defsystem #:sicl-run-time
+  :serial t
+  :components
+  ((:file "packages")))
