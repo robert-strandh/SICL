131b68bb83577e4207d7c7ed49be07a91fd8ec8f
Robert Strandh <robert.strandh@gmail.com>
2020-02-22 16:10:31 +0100

    Export names of environment classes and their accessors.

--- Code/Compiler/Run-time/packages.lisp
+++ Code/Compiler/Run-time/packages.lisp
@@ -2,4 +2,11 @@
 
 (defpackage #:sicl-run-time
   (:use #:common-lisp)
-  (:export #:augment-with-block/tagbody-entry))
+  (:export #:*dynamic-environment*
+           #:block/tagbody-entry
+           #:stack-pointer
+           #:frame-pointer
+           #:special-variable-entry
+           #:name
+           #:value
+           #:augment-with-block/tagbody-entry))
