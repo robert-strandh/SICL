c784258a05b31bed5303f6bf285e126b644b2200
Robert Strandh <robert.strandh@gmail.com>
2020-02-26 16:08:07 +0100

    Supply methods on CLEAVIR-IO:DEFINE-SAVE-INFO and CLEAVIR-AST:CHILDREN.

--- Code/Compiler/Abstract-syntax-tree/dynamic-environment-ast.lisp
+++ Code/Compiler/Abstract-syntax-tree/dynamic-environment-ast.lisp
@@ -3,3 +3,8 @@
 (defclass dynamic-environment-ast
     (cleavir-ast:ast cleavir-ast:one-value-ast-mixin)
   ())
+
+(cleavir-io:define-save-info dynamic-environment-ast)
+
+(defmethod cleavir-ast:children ((ast dynamic-environment-ast))
+  '())
--- Code/Compiler/Abstract-syntax-tree/stack-asts.lisp
+++ Code/Compiler/Abstract-syntax-tree/stack-asts.lisp
@@ -4,6 +4,16 @@
     (cleavir-ast:ast cleavir-ast:one-value-ast-mixin)
   ())
 
+(cleavir-io:define-save-info caller-stack-pointer-ast)
+
+(defmethod cleavir-ast:children ((ast caller-stack-pointer-ast))
+  '())
+
 (defclass caller-frame-pointer-ast
     (cleavir-ast:ast cleavir-ast:one-value-ast-mixin)
   ())
+
+(cleavir-io:define-save-info caller-frame-pointer-ast)
+
+(defmethod cleavir-ast:children ((ast caller-frame-pointer-ast))
+  '())
