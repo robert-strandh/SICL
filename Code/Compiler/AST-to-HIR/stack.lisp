aede7b3d74129aa42df710e43a5c32f23cdfbbbc
Robert Strandh <robert.strandh@gmail.com>
2020-02-28 16:15:40 +0100

    Add correct package prefix to name of generic function COMPILE-AST.

--- Code/Compiler/AST-to-HIR/stack.lisp
+++ Code/Compiler/AST-to-HIR/stack.lisp
@@ -1,6 +1,6 @@
 (cl:in-package #:sicl-ast-to-hir)
 
-(defmethod compile-ast
+(defmethod cleavir-ast-to-hir:compile-ast
     (client (ast sicl-ast:caller-stack-pointer-ast) context)
   (cleavir-ast-to-hir:assert-context ast context 1 1)
   (make-instance 'sicl-ir:caller-stack-pointer-instruction
@@ -8,7 +8,7 @@
     :output (first (cleavir-ast-to-hir:results context))
     :successor (first (cleavir-ast-to-hir:successors context))))
 
-(defmethod compile-ast
+(defmethod cleavir-ast-to-hir:compile-ast
     (client (ast sicl-ast:caller-frame-pointer-ast) context)
   (cleavir-ast-to-hir:assert-context ast context 1 1)
   (make-instance 'sicl-ir:caller-frame-pointer-instruction
