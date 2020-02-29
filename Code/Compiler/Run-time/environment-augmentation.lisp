0ea0c1781ead102fe3b679e6f8ad93ee328ae580
Robert Strandh <robert.strandh@gmail.com>
2020-02-29 11:00:10 +0100

    Rewrite augmentation functions to use new primops for stack/frame pointer.

--- Code/Compiler/Run-time/environment-augmentation.lisp
+++ Code/Compiler/Run-time/environment-augmentation.lisp
@@ -1,16 +1,14 @@
 (cl:in-package #:sicl-run-time)
 
-;;; This version is meant to be used with the HIR interpreter
 (defun augment-with-block/tagbody-entry ()
-  (let ((transfer-tag (list nil))
-        (abandon-tag (list nil)))
-    (values (cons (make-instance 'block/tagbody-entry
-                    :frame-pointer abandon-tag)
-                  *dynamic-environment*)
-            transfer-tag)))
+  (cons (make-instance 'block/tagbody-entry
+          :stack-pointer (sicl-primop:caller-stack-pointer)
+          :frame-pointer (sicl-primop:caller-frame-pointer)
+          :continuation (list nil))
+        (sicl-primop:dynamic-environment)))
 
 (defun augment-with-special-variable-entry (name value)
   (cons (make-instance 'special-variable-entry
           :name name
           :value value)
-        *dynamic-environment*))
+        (sicl-primop:dynamic-environment)))
