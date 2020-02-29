bcf6a59d7ed686a90d33567f53d51e1078b33428
Robert Strandh <robert.strandh@gmail.com>
2020-02-29 14:59:30 +0100

    Define AST class ESTABLISH-STACK-FRAME-AST.

--- Code/Compiler/Abstract-syntax-tree/stack-asts.lisp
+++ Code/Compiler/Abstract-syntax-tree/stack-asts.lisp
@@ -1,5 +1,9 @@
 (cl:in-package #:sicl-ast)
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;; CALLER-STACK-POINTER-AST
+
 (defclass caller-stack-pointer-ast
     (cleavir-ast:ast cleavir-ast:one-value-ast-mixin)
   ())
@@ -9,6 +13,10 @@
 (defmethod cleavir-ast:children ((ast caller-stack-pointer-ast))
   '())
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;; CALLER-FRAME-POINTER-AST
+
 (defclass caller-frame-pointer-ast
     (cleavir-ast:ast cleavir-ast:one-value-ast-mixin)
   ())
@@ -17,3 +25,18 @@
 
 (defmethod cleavir-ast:children ((ast caller-frame-pointer-ast))
   '())
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;; ESTABLISH-STACK-FRAME-AST
+
+(defclass establish-stack-frame-ast
+    (cleavir-ast:ast cleavir-ast:no-value-ast-mixin)
+  ((%stack-pointer-ast :initarg :stack-pointer-ast :reader stack-pointer-ast)
+   (%frame-pointer-ast :initarg :frame-pointer-ast :reader frame-pointer-ast)))
+
+(cleavir-io:define-save-info establish-stack-frame-ast)
+
+(defmethod cleavir-ast:children ((ast establish-stack-frame-ast))
+  (list (stack-pointer-ast ast)
+        (frame-pointer-ast ast)))
