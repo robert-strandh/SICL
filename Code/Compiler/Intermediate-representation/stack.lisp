2d1786e2ba57dba14c2c46763c10bc9b9501e79c
Robert Strandh <robert.strandh@gmail.com>
2020-02-29 16:04:37 +0100

    Define instruction class ESTABLISH-STACK-FRAME-INSTRUCTION.

--- Code/Compiler/Intermediate-representation/stack.lisp
+++ Code/Compiler/Intermediate-representation/stack.lisp
@@ -15,3 +15,11 @@
 (defclass caller-frame-pointer-instruction
     (cleavir-ir:instruction cleavir-ir:one-successor-mixin)
   ())
+
+;;; This instruction establishes a stack frame by setting the stack
+;;; pointer and the frame pointer according to the inputs.
+;;;
+;;; It has two inputs, no outputs, and a single successor.
+(defclass establish-stack-frame-instruction
+    (cleavir-ir:instruction cleavir-ir:one-successor-mixin)
+  ())
