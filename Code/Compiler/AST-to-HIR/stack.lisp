44bcb0b0f820d64e55ffe010fb78b198e774a9f5
Robert Strandh <robert.strandh@gmail.com>
2020-02-29 16:05:06 +0100

    Define method on COMPILE-AST for ESTABLISH-STACK-FRAME-AST.

--- Code/Compiler/AST-to-HIR/stack.lisp
+++ Code/Compiler/AST-to-HIR/stack.lisp
@@ -15,3 +15,26 @@
     :inputs '()
     :output (first (cleavir-ast-to-hir:results context))
     :successor (first (cleavir-ast-to-hir:successors context))))
+
+(defmethod cleavir-ast-to-hir:compile-ast
+    (client (ast sicl-ast:establish-stack-frame-ast) context)
+  (cleavir-ast-to-hir:assert-context ast context 0 1)
+  (let ((temp1 (cleavir-ir:new-temporary))
+        (temp2 (cleavir-ir:new-temporary)))
+    (cleavir-ast-to-hir:compile-ast
+     client
+     (sicl-ast:stack-pointer-ast ast)
+     (cleavir-ast-to-hir:clone-context
+      context
+      :result temp1
+      :successor
+      (cleavir-ast-to-hir:compile-ast
+       client
+       (sicl-ast:frame-pointer-ast ast)
+       (cleavir-ast-to-hir:clone-context
+        context
+        :result temp2
+        :successor
+        (make-instance 'sicl-ir:establish-stack-frame-instruction
+          :inputs (list temp1 temp2)
+          :successor (first (cleavir-ast-to-hir:successors context)))))))))
