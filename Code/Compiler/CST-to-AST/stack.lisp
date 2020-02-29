5e14a954dcafdb2720746d74cf6f18ec67799577
Robert Strandh <robert.strandh@gmail.com>
2020-02-29 15:45:51 +0100

    Define method on CONVERT-SPECIAL for primop ESTABLISH-STACK-FRAME.

--- Code/Compiler/CST-to-AST/stack.lisp
+++ Code/Compiler/CST-to-AST/stack.lisp
@@ -11,3 +11,13 @@
   (declare (ignore client environment))
   (cleavir-cst-to-ast:check-simple-primop-syntax cst 0)
   (cleavir-ast:make-ast 'sicl-ast:caller-frame-pointer-ast))
+
+(defmethod cleavir-cst-to-ast:convert-special
+    (client (symbol (eql 'sicl-primop:establish-stack-frame)) cst environment)
+  (cst:db origin (esf-cst stack-pointer-cst frame-pointer-cst) cst
+    (declare (ignore esf-cst))
+    (cleavir-ast:make-ast 'sicl-ast:establish-stack-frame-ast
+      :stack-pointer-ast
+      (cleavir-cst-to-ast:convert client stack-pointer-cst environment)
+      :frame-pointer-ast
+      (cleavir-cst-to-ast:convert client frame-pointer-cst environment))))
