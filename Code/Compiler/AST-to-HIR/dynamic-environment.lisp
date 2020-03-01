9f98aaceaf95787841fd6554270da020155aca28
Robert Strandh <robert.strandh@gmail.com>
2020-03-01 14:04:12 +0100

    Define method on COMPILE-AST for WITH-DYNAMIC-ENVIRONMENT-AST.

--- Code/Compiler/AST-to-HIR/dynamic-environment.lisp
+++ Code/Compiler/AST-to-HIR/dynamic-environment.lisp
@@ -7,3 +7,22 @@
     :inputs '()
     :output (first (cleavir-ast-to-hir:results context))
     :successor (first (cleavir-ast-to-hir:successors context))))
+
+(defmethod cleavir-ast-to-hir:compile-ast
+    (client (ast sicl-ast:with-dynamic-environment-ast) context)
+  (cleavir-ast-to-hir:assert-context ast context nil 1)
+  (let ((new-dynamic-environment-location
+          (cleavir-ir:make-lexical-location "denv")))
+    (cleavir-ast-to-hir:compile-ast
+     client
+     (sicl-ast:dynamic-environment-ast ast)
+     (cleavir-ast-to-hir:clone-context
+      context
+      :result new-dynamic-environment-location
+      :successor
+      (cleavir-ast-to-hir:compile-ast
+       client
+       (sicl-ast:body-ast ast)
+       (cleavir-ast-to-hir:clone-context
+        context
+        :dynamic-environment-location new-dynamic-environment-location))))))
