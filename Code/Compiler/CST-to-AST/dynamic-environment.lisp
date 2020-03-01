9e832c4ed9456158c47dc32ce610e3a53588ab47
Robert Strandh <robert.strandh@gmail.com>
2020-03-01 13:24:22 +0100

    Define method on CONVERT-SPECIAL for WITH-DYNAMIC-ENVIRONMENT primop.

--- Code/Compiler/CST-to-AST/dynamic-environment.lisp
+++ Code/Compiler/CST-to-AST/dynamic-environment.lisp
@@ -5,3 +5,15 @@
   (declare (ignore client environment))
   (cleavir-cst-to-ast:check-simple-primop-syntax cst 0)
   (cleavir-ast:make-ast 'sicl-ast:dynamic-environment-ast))
+
+(defmethod cleavir-cst-to-ast:convert-special
+    (client (symbol (eql 'sicl-primop:with-dynamic-environment)) cst environment)
+  (cst:db origin (wde-cst dynamic-environment-cst . body-csts) cst
+    (declare (ignore wde-cst))
+    (cleavir-ast:make-ast 'sicl-ast:with-dynamic-environment-ast
+      :dynamic-environment-ast
+      (cleavir-cst-to-ast:convert client dynamic-environment-cst environment)
+      :body-ast
+      (cleavir-cst-to-ast:process-progn
+       (loop for cst in body-csts
+             collect (cleavir-cst-to-ast:convert client cst environment))))))
