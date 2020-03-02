e329a3d79245bf9b17ff6a61cf3121423a8a7fd7
Marco Heisig <marco.heisig@fau.de>
2020-03-03 00:20:09 +0100

    Use SEAL-DOMAIN instead of SEAL.

--- Code/Sequence/Generic/seal-auxiliary-generic-functions.lisp
+++ Code/Sequence/Generic/seal-auxiliary-generic-functions.lisp
@@ -1,4 +1,4 @@
 (cl:in-package #:sicl-sequence)
 
-(seal make-sequence-reader)
-(seal make-sequence-writer)
+(seal-domain make-sequence-reader '(vector t t t t))
+(seal-domain make-sequence-writer '(list t t t t))
