34adb23d837d560c7271aa84b13d28c1f6946f60
Robert Strandh <robert.strandh@gmail.com>
2020-03-07 16:52:14 +0100

    Define function for creating the incrementation instruction.

--- Code/Compiler/HIR-transformations/eliminate-append-values.lisp
+++ Code/Compiler/HIR-transformations/eliminate-append-values.lisp
@@ -15,3 +15,11 @@
       :output cell-location
       :successor successor)))
 
+(defun make-incrementation-instruction
+    (successor index-location dynamic-environment-location)
+  (make-instance 'cleavir-ir:unsigned-add-instruction
+    :augend index-location
+    :addend (make-instance 'cleavir-ir:constant-input
+              :value 1)
+    :dynamic-environment-location dynamic-environment-location
+    :successor successor))
