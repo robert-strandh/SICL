303f0fe04faf4b2895ca9d87742613aa24d6cf1e
Robert Strandh <robert.strandh@gmail.com>
2020-03-15 11:31:00 +0100

    Define method on TRANSLATE-INSTRUCTION for the SIGNED-ADD-INSTRUCTION.

--- Code/Compiler/Code-generation/translate-instruction.lisp
+++ Code/Compiler/Code-generation/translate-instruction.lisp
@@ -109,6 +109,27 @@
     :operands '()))
 
 (defmethod translate-instruction
+    ((instruction cleavir-ir:signed-add-instruction))
+  (assert (eq (first (cleavir-ir:inputs instruction))
+              (first (cleavir-ir:outputs instruction))))
+  (let ((successors (cleavir-ir:successors instruction))
+        (add (make-instance 'cluster:code-command
+               :mnemonic "ADD"
+               :operands
+               (list
+                (translate-datum (first (cleavir-ir:inputs instruction)))
+                (translate-datum (second (cleavir-ir:inputs instruction)))))))
+    (if (or (= (length successors) 1)
+            (eq (first successors) (second successors)))
+        add
+        (list add
+              (make-instance 'cluster:code-command
+                :mnemonic "JOF"
+                :operands
+                (list
+                 (find-instruction-label (second (cleavir-ir:successors instruction)))))))))
+
+(defmethod translate-instruction
     ((instruction cleavir-ir:unsigned-add-instruction))
   (assert (eq (first (cleavir-ir:inputs instruction))
               (first (cleavir-ir:outputs instruction))))
