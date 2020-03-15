0c5518baf0114d2d74be638c07d838046ba291c9
Robert Strandh <robert.strandh@gmail.com>
2020-03-15 16:47:37 +0100

    Define methods on PROCESS-INSTRUCTION for MEMREF2 and MEMSET2.

--- Code/Compiler/MIR-to-LIR/memory.lisp
+++ Code/Compiler/MIR-to-LIR/memory.lisp
@@ -40,3 +40,44 @@
        *rax*
        lexical-locations)
       (setf (second inputs) *rax*))))
+
+(defmethod process-instruction
+    ((instruction cleavir-ir:memref2-instruction) lexical-locations)
+  (let ((inputs (cleavir-ir:inputs instruction))
+        (outputs (cleavir-ir:outputs instruction)))
+    (when (lexical-p (first inputs))
+      (insert-memref-before
+       instruction
+       (first inputs)
+       *r11*
+       *rax*
+       lexical-locations)
+      (setf (first inputs) *r11*))
+    (when (lexical-p (first outputs))
+      (insert-memset-after
+       instruction
+       *r11*
+       (first outputs)
+       *rax*
+       lexical-locations)
+      (setf (first outputs) *r11*))))
+
+(defmethod process-instruction
+    ((instruction cleavir-ir:memset2-instruction) lexical-locations)
+  (let ((inputs (cleavir-ir:inputs instruction)))
+    (when (lexical-p (first inputs))
+      (insert-memref-before
+       instruction
+       (first inputs)
+       *r11*
+       *rax*
+       lexical-locations)
+      (setf (first inputs) *r11*))
+    (when (lexical-p (third inputs))
+      (insert-memref-before
+       instruction
+       (third inputs)
+       *rax*
+       *rax*
+       lexical-locations)
+      (setf (third inputs) *rax*))))
