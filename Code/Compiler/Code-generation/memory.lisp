899487599e97a4841a5933e9875c080f5fa81a37
Robert Strandh <robert.strandh@gmail.com>
2020-03-15 16:01:17 +0100

    Define methods on TRANSLATE-INSTRUCTION for MEMREF2 and MEMSET2 instructions.

--- Code/Compiler/Code-generation/memory.lisp
+++ Code/Compiler/Code-generation/memory.lisp
@@ -21,3 +21,35 @@
        :base-register (translate-datum (first (cleavir-ir:inputs instruction)))
        :size 64)
      (translate-datum (second (cleavir-ir:inputs instruction))))))
+
+(defmethod translate-instruction
+    ((instruction cleavir-ir:memref2-instruction))
+  (destructuring-bind (base-register-input offset-input)
+      (cleavir-ir:inputs instruction)
+    ;; We only handle immediate offsets right now.
+    (assert (typep offset-input 'cleavir-ir:immediate-input))
+    (make-instance 'cluster:code-command
+      :mnemonic "MOV"
+      :operands
+      (list
+       (translate-datum (first (cleavir-ir:outputs instruction)))
+       (make-instance 'cluster:memory-operand
+         :base-register (translate-datum base-register-input)
+         :displacement (cleavir-ir:value offset-input)
+         :size 64)))))
+
+(defmethod translate-instruction
+    ((instruction cleavir-ir:memset2-instruction))
+  (destructuring-bind (base-register-input offset-input value-input)
+      (cleavir-ir:inputs instruction)
+    ;; We only handle immediate offsets right now.
+    (assert (typep offset-input 'cleavir-ir:immediate-input))
+    (make-instance 'cluster:code-command
+      :mnemonic "MOV"
+      :operands
+      (list
+       (make-instance 'cluster:memory-operand
+         :base-register (translate-datum base-register-input)
+         :displacement (cleavir-ir:value offset-input)
+         :size 64)
+       (translate-datum value-input)))))
