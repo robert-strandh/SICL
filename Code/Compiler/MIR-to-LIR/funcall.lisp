09cb6f40b3129697aef72dc71c5d746148e8c164
Robert Strandh <robert.strandh@gmail.com>
2020-03-17 14:51:08 +0100

    Use new instruction PUSH, and eliminate call to LOAD-LEXICAL.

--- Code/Compiler/MIR-to-LIR/funcall.lisp
+++ Code/Compiler/MIR-to-LIR/funcall.lisp
@@ -22,31 +22,30 @@
 (defun do-arguments (instruction lexical-locations)
   (let ((inputs (cleavir-ir:inputs instruction)))
     (when (> (- (length inputs) 3) 5)
+      ;; Make room for the return address
+      (cleavir-ir:insert-instruction-before
+       (make-instance 'sicl-ir:push-instruction
+         :input (make-instance 'cleavir-ir:immediate-input
+                  :value 0))
+       instruction)
       (loop for argument in (reverse (subseq inputs (+ 3 5)))
             for constant-8 = (make-instance 'cleavir-ir:immediate-input :value 8)
-            do (cleavir-ir:insert-instruction-before
-                (make-instance 'cleavir-ir:unsigned-sub-instruction
-                  :inputs (list *rsp* constant-8)
-                  :output *rsp*)
-                instruction)
-               (when (lexical-p argument)
-                 (load-lexical argument *r11* instruction lexical-locations))
+            do (insert-memref-before
+                instruction
+                argument
+                *r11*
+                lexical-locations)
                (cleavir-ir:insert-instruction-before
-                (make-instance 'cleavir-ir:memset1-instruction
-                  :address *rsp*
-                  :value (if (lexical-p argument)
-                             *r11*
-                             argument))
+                (make-instance 'sicl-ir:push-instruction
+                  :input *r11*)
                 instruction)))
     (loop for argument in (subseq inputs 3 (min (+ 3 5) (length inputs)))
           for register in (list *rdi* *rsi* *rdx* *rcx* *r8*)
-          do (if (lexical-p argument)
-                 (load-lexical argument register instruction lexical-locations)
-                 (cleavir-ir:insert-instruction-before
-                  (make-instance 'cleavir-ir:assignment-instruction
-                    :input argument
-                    :output register)
-                  instruction)))))
+          do (insert-memref-before
+              instruction
+              argument
+              register
+              lexical-locations))))
 
 (defun process-funcall (instruction lexical-locations)
   (do-arguments instruction lexical-locations)
