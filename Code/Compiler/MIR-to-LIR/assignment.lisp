1db3b2992f796972babf5f115991679ae426a3fb
Robert Strandh <robert.strandh@gmail.com>
2020-03-18 08:17:30 +0100

    Shuffle code.

--- Code/Compiler/MIR-to-LIR/assignment.lisp
+++ Code/Compiler/MIR-to-LIR/assignment.lisp
@@ -2,15 +2,18 @@
 
 (defmethod process-instruction
     ((instruction cleavir-ir:assignment-instruction) lexical-locations)
-  (when (lexical-p (first (cleavir-ir:inputs instruction)))
-    (insert-memref-before
-     instruction
-     (first (cleavir-ir:inputs instruction))
-     *r11*
-     lexical-locations)
-    (setf (cleavir-ir:inputs instruction) (list *r11*)))
   (insert-memset-after
    instruction
    *r11*
    (first (cleavir-ir:outputs instruction))
-   lexical-locations))
+   lexical-locations)
+  (if (lexical-p (first (cleavir-ir:inputs instruction)))
+      (progn (insert-memref-before
+              instruction
+              (first (cleavir-ir:inputs instruction))
+              *r11*
+              lexical-locations)
+             (change-class instruction 'cleavir-ir:nop-instruction
+                           :inputs '()
+                           :outputs '()))
+      (setf (cleavir-ir:outputs instruction) (list *r11*))))
