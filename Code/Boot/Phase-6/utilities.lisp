053364d7c5cc41f7ff3ca62b2f18a264d14cceda
Robert Strandh <robert.strandh@gmail.com>
2020-03-18 14:58:18 +0100

    Call the main entry point of the code-generation phase.

--- Code/Boot/Phase-6/utilities.lisp
+++ Code/Boot/Phase-6/utilities.lisp
@@ -13,6 +13,7 @@
     (sicl-hir-transformations:eliminate-append-values-instructions hir2)
     (sicl-hir-to-mir:hir-to-mir client hir2)
     (sicl-mir-to-lir:mir-to-lir client hir2)
+    (sicl-code-generation:generate-code hir2)
     (funcall fun
              (sicl-hir-interpreter:make-function-cell-finder global-environment)
              (apply #'vector
