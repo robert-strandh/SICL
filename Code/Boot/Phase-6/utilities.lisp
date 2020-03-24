75ac64059effb33caf4ee36950ed794bc2322d1e
Robert Strandh <robert.strandh@gmail.com>
2020-03-24 16:16:51 +0100

    Create a code object and pass it in the static environment.

--- Code/Boot/Phase-6/utilities.lisp
+++ Code/Boot/Phase-6/utilities.lisp
@@ -13,31 +13,38 @@
     (sicl-hir-transformations:eliminate-append-values-instructions hir2)
     (sicl-hir-to-mir:hir-to-mir client hir2)
     (sicl-mir-to-lir:mir-to-lir client hir2)
-    (sicl-code-generation:generate-code hir2)
-    (funcall fun
-             (sicl-hir-interpreter:make-function-cell-finder global-environment)
-             (apply #'vector
-                    nil ; Ultimately, replace with code object.
-                    (labels ((enclose (entry-point code-object &rest static-environment-values)
-                               (let* ((static-environment
-                                        (apply #'vector
-                                               code-object
-                                               #'enclose
-                                               #'cons
-                                               nil
-                                               static-environment-values))
-                                      (closure (funcall (sicl-genv:fdefinition 'make-instance sicl-boot:*e5*)
-                                              'function
-                                              :environment static-environment)))
-                                 (closer-mop:set-funcallable-instance-function
-                                  closure
-                                  (lambda (&rest args)
-                                    (funcall entry-point
-                                             args
-                                             static-environment
-                                             sicl-run-time:*dynamic-environment*)))
-                                 closure)))
-                      #'enclose)
-                    #'cons
-                    nil
-                    (sicl-hir-transformations:constants hir)))))
+    (multiple-value-bind (instructions label-map)
+        (sicl-code-generation:generate-code hir2)
+      (declare (ignore label-map))
+      (funcall fun
+               (sicl-hir-interpreter:make-function-cell-finder global-environment)
+               (apply #'vector
+                      (funcall (sicl-genv:fdefinition 'make-instance sicl-boot:*e5*)
+                               'sicl-compiler:code-object
+                               :instructions instructions
+                               :frame-maps nil
+                               :callee-saves-register-maps nil
+                               :callee-saves-stack-maps nil)
+                      (labels ((enclose (entry-point code-object &rest static-environment-values)
+                                 (let* ((static-environment
+                                          (apply #'vector
+                                                 code-object
+                                                 #'enclose
+                                                 #'cons
+                                                 nil
+                                                 static-environment-values))
+                                        (closure (funcall (sicl-genv:fdefinition 'make-instance sicl-boot:*e5*)
+                                                          'function
+                                                          :environment static-environment)))
+                                   (closer-mop:set-funcallable-instance-function
+                                    closure
+                                    (lambda (&rest args)
+                                      (funcall entry-point
+                                               args
+                                               static-environment
+                                               sicl-run-time:*dynamic-environment*)))
+                                   closure)))
+                        #'enclose)
+                      #'cons
+                      nil
+                      (sicl-hir-transformations:constants hir))))))
