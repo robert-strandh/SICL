c77b8576f66f62cc6df5b7d1173f9c3fe40414a2
Robert Strandh <robert.strandh@gmail.com>
2020-03-26 17:32:38 +0100

    Add :ACCESSOR ENVIRONMENT to slot definition.

--- Code/CLOS/funcallable-standard-object-defclass.lisp
+++ Code/CLOS/funcallable-standard-object-defclass.lisp
@@ -37,5 +37,5 @@
 
 (defclass funcallable-standard-object (standard-object function)
   ((%entry-point :initarg :entry-point :accessor entry-point)
-   (%environment :initform nil :initarg :environment))
+   (%environment :initform nil :initarg :environment :accessor environment))
   (:metaclass funcallable-standard-class))
