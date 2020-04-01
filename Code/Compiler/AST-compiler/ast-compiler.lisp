efedc88956821cea6420561e4789eea1d69ac028
Robert Strandh <robert.strandh@gmail.com>
2020-04-01 15:27:30 +0200

    Fill the resulting code object with all information avaliable.

--- Code/Compiler/AST-compiler/ast-compiler.lisp
+++ Code/Compiler/AST-compiler/ast-compiler.lisp
@@ -1,16 +1,19 @@
 (cl:in-package #:sicl-compiler)
 
-(defun compile-ast (client ast environment)
-  (declare (ignore environment)) ; for now
-  (let ((hir (sicl-ast-to-hir:ast-to-hir client ast)))
-    (sicl-hir-transformations:eliminate-append-values-instructions hir)
-    (sicl-hir-to-mir:hir-to-mir client hir)
-    (sicl-mir-to-lir:mir-to-lir client hir)
+(defun compile-ast (client ast)
+  (let ((hir1 (sicl-ast-to-hir:ast-to-hir client ast))
+        (hir2 (sicl-ast-to-hir:ast-to-hir client ast)))
+    (sicl-hir-transformations:eliminate-append-values-instructions hir2)
+    (sicl-hir-to-mir:hir-to-mir client hir2)
+    (sicl-mir-to-lir:mir-to-lir client hir2)
     (multiple-value-bind (instructions label-map)
-        (cluster:assemble (sicl-code-generation:generate-code hir))
+        (cluster:assemble (sicl-code-generation:generate-code hir2))
       (declare (ignore label-map))
       (make-instance 'code-object
         :instructions instructions
         :frame-maps nil
         :callee-saves-register-maps nil
-        :callee-saved-stack-maps nil))))
+        :callee-saves-stack-maps nil
+        :constants (sicl-hir-transformations:constants hir1)
+        :function-names (sicl-hir-transformations:function-names hir1)
+        :hir hir1))))
