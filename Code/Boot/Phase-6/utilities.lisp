44aa67544e944151d536d5a40c96cd11698725fa
Robert Strandh <robert.strandh@gmail.com>
2020-04-02 09:23:39 +0200

    Call TIE-CODE-OBJECT in the SICL-BOOT package.

--- Code/Boot/Phase-6/utilities.lisp
+++ Code/Boot/Phase-6/utilities.lisp
@@ -1,45 +1,5 @@
 (cl:in-package #:sicl-boot-phase-6)
 
-(defun tie-code-object (client code-object e1 e2)
-  (let* ((hir (sicl-compiler:hir code-object))
-         (constants (sicl-compiler:constants code-object))
-         (function-names (sicl-compiler:function-names code-object))
-         (fun (sicl-hir-interpreter:top-level-hir-to-host-function client hir))
-         (sicl-run-time:*dynamic-environment* '()))
-    (funcall fun
-             (apply #'vector
-                    (funcall (sicl-genv:fdefinition 'make-instance e2)
-                             'sicl-compiler:code-object
-                             :instructions (sicl-compiler:instructions code-object)
-                             :frame-maps nil
-                             :callee-saves-register-maps nil
-                             :callee-saves-stack-maps nil)
-                    (labels ((enclose (entry-point code-object &rest static-environment-values)
-                               (let* ((static-environment
-                                        (apply #'vector
-                                               code-object
-                                               #'enclose
-                                               #'cons
-                                               nil
-                                               static-environment-values))
-                                      (closure (funcall (sicl-genv:fdefinition 'make-instance e2)
-                                                        'sicl-clos:simple-function
-                                                        :environment static-environment)))
-                                 (closer-mop:set-funcallable-instance-function
-                                  closure
-                                  (lambda (&rest args)
-                                    (funcall entry-point
-                                             args
-                                             static-environment
-                                             sicl-run-time:*dynamic-environment*)))
-                                 closure)))
-                      #'enclose)
-                    #'cons
-                    nil
-                    (append (loop for name in function-names
-                                  collect (sicl-genv:function-cell name e1))
-                            constants)))))
-
 (defun load-fasl (relative-pathname global-environment)
   (format *trace-output* "Loading file ~s~%" relative-pathname)
   (let* ((client (make-instance 'sicl-boot:client))
@@ -47,4 +7,4 @@
          (pathname (asdf:system-relative-pathname '#:sicl-boot prefixed))
          (ast (cleavir-io:read-model pathname '(v0)))
          (code-object (sicl-compiler:compile-ast client ast)))
-    (tie-code-object client code-object global-environment sicl-boot:*e5*)))
+    (sicl-boot:tie-code-object client code-object global-environment sicl-boot:*e5*)))
