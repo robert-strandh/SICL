fa5dde43a7b8f2ae7e487dec00ff7a7071c8c44f
Robert Strandh <robert.strandh@gmail.com>
2020-04-02 08:59:04 +0200

    Express LOAD-FASL in terms of COMPILE-AST and TIE-CODE-OBJECT.

--- Code/Boot/Phase-6/utilities.lisp
+++ Code/Boot/Phase-6/utilities.lisp
@@ -38,7 +38,7 @@
                     nil
                     (append (loop for name in function-names
                                   collect (sicl-genv:function-cell name e1))
-                            constants)))))))
+                            constants)))))
 
 (defun load-fasl (relative-pathname global-environment)
   (format *trace-output* "Loading file ~s~%" relative-pathname)
@@ -46,47 +46,5 @@
          (prefixed (concatenate 'string "ASTs/" relative-pathname))
          (pathname (asdf:system-relative-pathname '#:sicl-boot prefixed))
          (ast (cleavir-io:read-model pathname '(v0)))
-         (hir (sicl-ast-to-hir:ast-to-hir client ast))
-         (hir2 (sicl-ast-to-hir:ast-to-hir client ast))
-         (fun (sicl-hir-interpreter:top-level-hir-to-host-function client hir))
-         (sicl-run-time:*dynamic-environment* '()))
-    (sicl-hir-transformations:eliminate-append-values-instructions hir2)
-    (sicl-hir-to-mir:hir-to-mir client hir2)
-    (sicl-mir-to-lir:mir-to-lir client hir2)
-    (multiple-value-bind (instructions label-map)
-        (cluster:assemble (sicl-code-generation:generate-code hir2))
-      (declare (ignore label-map))
-      (funcall fun
-               (apply #'vector
-                      (funcall (sicl-genv:fdefinition 'make-instance sicl-boot:*e5*)
-                               'sicl-compiler:code-object
-                               :instructions instructions
-                               :frame-maps nil
-                               :callee-saves-register-maps nil
-                               :callee-saves-stack-maps nil)
-                      (labels ((enclose (entry-point code-object &rest static-environment-values)
-                                 (let* ((static-environment
-                                          (apply #'vector
-                                                 code-object
-                                                 #'enclose
-                                                 #'cons
-                                                 nil
-                                                 static-environment-values))
-                                        (closure (funcall (sicl-genv:fdefinition 'make-instance sicl-boot:*e5*)
-                                                          'sicl-clos:simple-function
-                                                          :environment static-environment)))
-                                   (closer-mop:set-funcallable-instance-function
-                                    closure
-                                    (lambda (&rest args)
-                                      (funcall entry-point
-                                               args
-                                               static-environment
-                                               sicl-run-time:*dynamic-environment*)))
-                                   closure)))
-                        #'enclose)
-                      #'cons
-                      nil
-                      (append (loop with names = (sicl-hir-transformations:function-names hir)
-                                    for name in names
-                                    collect (sicl-genv:function-cell name global-environment))
-                              (sicl-hir-transformations:constants hir)))))))
+         (code-object (sicl-compiler:compile-ast client ast)))
+    (tie-code-object client code-object global-environment sicl-boot:*e5*)))
