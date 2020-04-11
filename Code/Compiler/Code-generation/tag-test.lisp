974f4b9f6f0978ef16740b102b0acb7faf8458b5
Robert Strandh <robert.strandh@gmail.com>
2020-04-11 08:11:38 +0200

    Add method on translation functions for the SINGLE-FLOAT-P-INSTRUCTION.

--- Code/Compiler/Code-generation/tag-test.lisp
+++ Code/Compiler/Code-generation/tag-test.lisp
@@ -53,3 +53,23 @@
   (append
    (translate-simple-instruction instruction)
    (compute-branches instruction next "JNE" "JE")))
+
+(defmethod translate-simple-instruction
+    ((instruction cleavir-ir:single-float-p-instruction))
+  (list
+   (make-instance 'cluster:code-command
+     :mnemonic "AND"
+     :operands
+     (list (translate-datum (first (cleavir-ir:inputs instruction)))
+           (make-instance 'cluster:immediate-operand :value #b11111)))
+   (make-instance 'cluster:code-command
+     :mnemonic "CMP"
+     :operands
+     (list (translate-datum (first (cleavir-ir:inputs instruction)))
+           (make-instance 'cluster:immediate-operand :value #b10011)))))
+
+(defmethod translate-branch-instruction
+    ((instruction cleavir-ir:single-float-p-instruction) next)
+  (append
+   (translate-simple-instruction instruction)
+   (compute-branches instruction next "JNE" "JE")))
