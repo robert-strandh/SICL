f242162a251fbad0e5220bff14b52889d402c416
Robert Strandh <robert.strandh@gmail.com>
2020-04-11 08:40:59 +0200

    Define methods on PROCESS-INSTRUCTION for remaining tag-test instructions.

--- Code/Compiler/MIR-to-LIR/tag-test.lisp
+++ Code/Compiler/MIR-to-LIR/tag-test.lisp
@@ -15,3 +15,27 @@
    (first (cleavir-ir:inputs instruction))
    *r11*
    lexical-locations))
+
+(defmethod process-instruction
+    ((instruction cleavir-ir:characterp-instruction) lexical-locations)
+  (insert-memref-before
+   instruction
+   (first (cleavir-ir:inputs instruction))
+   *r11*
+   lexical-locations))
+
+(defmethod process-instruction
+    ((instruction cleavir-ir:single-float-p-instruction) lexical-locations)
+  (insert-memref-before
+   instruction
+   (first (cleavir-ir:inputs instruction))
+   *r11*
+   lexical-locations))
+
+(defmethod process-instruction
+    ((instruction cleavir-ir:standard-object-p-instruction) lexical-locations)
+  (insert-memref-before
+   instruction
+   (first (cleavir-ir:inputs instruction))
+   *r11*
+   lexical-locations))
