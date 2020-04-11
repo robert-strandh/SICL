5c621197fad51721ab36a67aed67e0bd75735f80
Robert Strandh <robert.strandh@gmail.com>
2020-04-11 07:52:59 +0200

    Add method on translation functions for the CONSP-INSTRUCTION.

--- Code/Compiler/Code-generation/tag-test.lisp
+++ Code/Compiler/Code-generation/tag-test.lisp
@@ -13,3 +13,23 @@
   (cons
    (translate-simple-instruction instruction)
    (compute-branches instruction next "JC" "JNC")))
+
+(defmethod translate-simple-instruction
+    ((instruction cleavir-ir:consp-instruction))
+  (list
+   (make-instance 'cluster:code-command
+     :mnemonic "AND"
+     :operands
+     (list (translate-datum (first (cleavir-ir:inputs instruction)))
+           (make-instance 'cluster:immediate-operand :value #b111)))
+   (make-instance 'cluster:code-command
+     :mnemonic "CMP"
+     :operands
+     (list (translate-datum (first (cleavir-ir:inputs instruction)))
+           (make-instance 'cluster:immediate-operand :value #b001)))))
+
+(defmethod translate-branch-instruction
+    ((instruction cleavir-ir:consp-instruction) next)
+  (append
+   (translate-simple-instruction instruction)
+   (compute-branches instruction next "JNE" "JE")))
