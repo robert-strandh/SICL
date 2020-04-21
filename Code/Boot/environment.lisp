a3faaf17bf381980b07d9367dc3e064da6459264
Robert Strandh <robert.strandh@gmail.com>
2020-04-21 18:17:51 +0200

    Initialize the CLIENT of the environment.

--- Code/Boot/environment.lisp
+++ Code/Boot/environment.lisp
@@ -1,7 +1,8 @@
 (cl:in-package #:sicl-boot)
 
 (defclass environment (sicl-extrinsic-environment:environment)
-  ())
+  ()
+  (:default-initargs :client (make-instance 'client)))
 
 (defmethod trucler-reference:global-environment ((environment environment))
   environment)
