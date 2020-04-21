315c3f7c85633d9d23260e0c72be2c3a6e944185
Robert Strandh <robert.strandh@gmail.com>
2020-04-21 17:19:27 +0200

    Eliminate references to constant variable +GLOBAL-ENVIRONMENT+.

--- Code/Environment/packages.lisp
+++ Code/Environment/packages.lisp
@@ -86,9 +86,6 @@
    #:special-operator
    #:get-setf-expansion
    #:find-method-combination-template
-   ;; This special variable is deprecated, it should replaced by the
-   ;; constant variable +global-environment+
-   #:+global-environment+
    #:global-environment
    #:declaration
    #:declarations
