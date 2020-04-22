fafc891a742fd2adb2c165aaca92a90cb2227fdd
Robert Strandh <robert.strandh@gmail.com>
2020-04-22 10:41:06 +0200

    Define SICL-GENV:GLOBAL-ENVIRONMENT in the host.

--- Code/Boot/Phase-0/boot.lisp
+++ Code/Boot/Phase-0/boot.lisp
@@ -1,5 +1,14 @@
 (cl:in-package #:sicl-boot-phase-0)
 
+;;; During bootstrapping, we compile a certain number of files
+;;; containing DEFUN forms.  The macro expander for DEFUN is executed
+;;; by the host, because it itself is defined using DEFUN.  That
+;;; microsecond calls SICL-GENV:GLOBAL-ENVIRONMENT, so that function
+;;; must be defined in the host.  For that reason, we define it here.
+(defun sicl-genv:global-environment (&optional environment)
+  (assert (typep environment 'environment))
+  environment)
+
 (defun boot (boot)
   (format *trace-output* "Start phase 0~%")
   (with-accessors ((e0 sicl-boot:e0)) boot
