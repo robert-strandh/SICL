b2a83336f78a3e1d18b8035a68f16b8d79f6eaf2
Robert Strandh <robert.strandh@gmail.com>
2020-04-24 16:34:08 +0200

    Delete obsolete component.

--- Code/CLOS/add-method-to-generic-function-defun.lisp
+++
@@ -1,6 +0,0 @@
-(cl:in-package #:sicl-clos)
-
-;;; This function is a simple indirection for ADD-METHOD.  It is used
-;;; during bootstrapping.
-(defun add-method-to-generic-function (generic-function method)
-  (add-method generic-function method))
