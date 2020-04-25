2b5c8aa17205121837bbeb4fb034e246df83b7ca
Robert Strandh <robert.strandh@gmail.com>
2020-04-25 05:46:58 +0200

    Remove incorrect component.

--- Code/CLOS/find-class-defgenerics.lisp
+++
@@ -1,9 +0,0 @@
-(cl:in-package #:sicl-clos)
-
-;;; For the specification of this generic function, see
-;;; http://www.lispworks.com/documentation/HyperSpec/Body/f_find_c.htm#find-class
-(defgeneric find-class (symbol &optional errorp environment))
-
-;;; For the specification of this generic function, see
-;;; http://www.lispworks.com/documentation/HyperSpec/Body/f_find_c.htm#find-class
-(defgeneric (setf find-class) (new-class symbol &optional errorp environment))
