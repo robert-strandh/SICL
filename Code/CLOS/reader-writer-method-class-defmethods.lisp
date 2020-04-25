ab69153f21b525c49dcaca9eae607c3b99df7a6e
Robert Strandh <robert.strandh@gmail.com>
2020-04-25 06:04:46 +0200

    Add comment explaining why we extract bodies of default methods into separate functions.

--- Code/CLOS/reader-writer-method-class-defmethods.lisp
+++ Code/CLOS/reader-writer-method-class-defmethods.lisp
@@ -1,5 +1,12 @@
 (cl:in-package #:sicl-clos)
 
+;;; We extract the body of the default methods into separate functions
+;;; in a separate file.  During bootstrapping, the generic functions
+;;; need to be tied to and present in an environment En, but the
+;;; classes need to be looked up in the environment En-1.  We
+;;; accomplish this effect by having READER-METHOD-CLASS-DEFAULT and
+;;; WRITER-METHOD-CLASS-DEFAULT tied to En-1 and present in En.
+
 (defmethod reader-method-class
     ((class regular-class)
      (direct-slot standard-direct-slot-definition)
