9e3148ed15dea8cbcf400bb7068f90f9f01582c8
Robert Strandh <robert.strandh@gmail.com>
2020-04-30 08:35:43 +0200

    Add accessor and move call to FILL-ENVIRONMENT.

--- Code/Boot/Phase-3/set-up-environments.lisp
+++ Code/Boot/Phase-3/set-up-environments.lisp
@@ -1,13 +1,14 @@
 (cl:in-package #:sicl-boot-phase-3)
 
 (defun set-up-environments (boot)
-  (with-accessors ((e3 sicl-boot:e3)
+  (with-accessors ((e2 sicl-boot:e2)
+                   (e3 sicl-boot:e3)
                    (e4 sicl-boot:e4))
       boot
+    (sicl-hir-interpreter:fill-environment e4)
     (import-function-from-host 'funcall e4)
     (import-function-from-host '(setf sicl-genv:function-lambda-list) e4)
     (import-function-from-host '(setf sicl-genv:function-type) e4)
-    (sicl-hir-interpreter:fill-environment e4)
     (import-function-from-host 'list e3)
     (import-function-from-host 'null e3)
     (import-function-from-host 'append e3)
