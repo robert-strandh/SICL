15042e366ac6c417c79ba09208fb08b5da4a04f3
Robert Strandh <robert.strandh@gmail.com>
2020-04-30 08:43:17 +0200

    Collapse several calls to IMPORT-FUNCTION-... into one call to IMPORT-FUNCTIONS-...

--- Code/Boot/Phase-3/set-up-environments.lisp
+++ Code/Boot/Phase-3/set-up-environments.lisp
@@ -6,10 +6,15 @@
                    (e4 sicl-boot:e4))
       boot
     (sicl-hir-interpreter:fill-environment e4)
-    (import-function-from-host 'funcall e4)
-    (import-function-from-host '(setf sicl-genv:function-lambda-list) e4)
-    (import-function-from-host '(setf sicl-genv:function-type) e4)
-    (import-function-from-host 'list e3)
-    (import-function-from-host 'null e3)
-    (import-function-from-host 'append e3)
-    (import-function-from-host '(setf sicl-genv:special-variable) e3)))
+    (import-functions-from-host
+     '()
+     e2)
+    (import-functions-from-host
+     '((setf sicl-genv:special-variable)
+       list null append)
+     e3)
+    (import-functions-from-host
+     '(funcall
+       (setf sicl-genv:function-lambda-list)
+       (setf sicl-genv:function-type))
+     e4)))
