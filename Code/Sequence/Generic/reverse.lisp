c994fbdea1ca48576b377df03e290f3da5409ac1
Marco Heisig <marco.heisig@fau.de>
2020-05-01 14:50:01 +0200

    Implement reverse.

--- Code/Sequence/Generic/reverse.lisp
+++ Code/Sequence/Generic/reverse.lisp
@@ -1,2 +1,19 @@
 (cl:in-package #:sicl-sequence)
 
+(defmethod reverse ((list list))
+  (let ((result '()))
+    (dolist (elt list result)
+      (push elt result))))
+
+(seal-domain #'reverse '(list))
+
+(replicate-for-each-relevant-vectoroid #1=#:vectoroid
+  (defmethod reverse ((vectoroid #1#))
+    (let* ((n (length vectoroid))
+           (result (make-array n :element-type (array-element-type vectoroid))))
+      (loop for index below n do
+        (setf (aref result index)
+              (aref vectoroid (- n index 1))))
+      result)))
+
+(seal-domain #'reverse '(vector))
