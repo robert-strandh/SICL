99d4037878caff1ff292bf9e787065fe752a7983
Marco Heisig <marco.heisig@fau.de>
2020-05-01 16:25:55 +0200

    Implement position, position-if and position-if-not.

--- Code/Sequence/Generic/position-if.lisp
+++ Code/Sequence/Generic/position-if.lisp
@@ -1,1 +1,21 @@
 (cl:in-package #:sicl-sequence)
+
+(defmethod position-if (predicate (list list) &key from-end (start 0) end key)
+  (with-predicate (predicate predicate)
+    (with-key-function (key key)
+      (for-each-relevant-cons (cons index list start end from-end)
+        (let ((element (car cons)))
+          (when (predicate (key element))
+            (return-from position-if index)))))))
+
+(seal-domain #'position-if '(t list))
+
+(replicate-for-each-relevant-vectoroid #1=#:vector
+  (defmethod position-if (predicate (vector #1#) &key from-end (start 0) end key)
+    (with-predicate (predicate predicate)
+      (with-key-function (key key)
+        (for-each-relevant-element (element index vector start end from-end)
+          (when (predicate (key element))
+            (return-from position-if index)))))))
+
+(seal-domain #'position-if '(t vector))
