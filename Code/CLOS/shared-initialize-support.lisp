9f66e8102981b9bcb0216d21e596086b693ace5c
Robert Strandh <robert.strandh@gmail.com>
2020-05-03 12:23:09 +0200

    Call DEFAULT versions of SLOT-VALUE/BOUNDP-USING-CLASS.

--- Code/CLOS/shared-initialize-support.lisp
+++ Code/CLOS/shared-initialize-support.lisp
@@ -12,17 +12,17 @@
                (if foundp
                    ;; Found an explicit initarg in initargs.
                    ;; Initialize the slot from its value.
-                   (setf (slot-value-using-class class instance slot)
+                   (setf (slot-value-using-class-default class instance slot)
                          value)
                    ;; No explicit initarg found.
-                   (when (and (not (slot-boundp-using-class class instance slot))
+                   (when (and (not (slot-boundp-using-class-default class instance slot))
                               (not (null (slot-definition-initfunction slot)))
                               (or (eq slot-names t)
                                   (member (slot-definition-name slot)
                                           slot-names)))
                      ;; Evaluate the initform by executing the
                      ;; initfunction.
-                     (setf (slot-value-using-class class instance slot)
+                     (setf (slot-value-using-class-default class instance slot)
                            (funcall (slot-definition-initfunction slot)))))))
     ;; Store the class slots in the instance so that we can update the
     ;; instance when the class changes.
