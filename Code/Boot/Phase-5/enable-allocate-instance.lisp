085919874bb8872ad4182f890dc208622ec8c2bb
Robert Strandh <robert.strandh@gmail.com>
2020-05-04 17:38:49 +0200

    Rename parameter from a phase-specific name to a phase-neutral one.

--- Code/Boot/Phase-5/enable-allocate-instance.lisp
+++ Code/Boot/Phase-5/enable-allocate-instance.lisp
@@ -1,12 +1,12 @@
 (cl:in-package #:sicl-boot-phase-5)
 
-(defun enable-allocate-instance (e4)
-  (setf (sicl-genv:fdefinition 'sicl-clos::allocate-general-instance e4)
+(defun enable-allocate-instance (ea)
+  (setf (sicl-genv:fdefinition 'sicl-clos::allocate-general-instance ea)
         (lambda (class size)
           (make-instance 'sicl-boot::header
             :class class
             :rack (make-array size :initial-element 10000000))))
-  (load-fasl "CLOS/stamp-offset-defconstant.fasl" e4)
-  (load-fasl "CLOS/allocate-instance-defgenerics.fasl" e4)
-  (load-fasl "CLOS/allocate-instance-support.fasl" e4)
-  (load-fasl "CLOS/allocate-instance-defmethods.fasl" e4))
+  (load-fasl "CLOS/stamp-offset-defconstant.fasl" ea)
+  (load-fasl "CLOS/allocate-instance-defgenerics.fasl" ea)
+  (load-fasl "CLOS/allocate-instance-support.fasl" ea)
+  (load-fasl "CLOS/allocate-instance-defmethods.fasl" ea))
--- Code/Boot/Phase-6/enable-allocate-instance.lisp
+++ Code/Boot/Phase-6/enable-allocate-instance.lisp
@@ -1,12 +1,12 @@
 (cl:in-package #:sicl-boot-phase-6)
 
-(defun enable-allocate-instance (e5)
-  (setf (sicl-genv:fdefinition 'sicl-clos::allocate-general-instance e5)
+(defun enable-allocate-instance (ea)
+  (setf (sicl-genv:fdefinition 'sicl-clos::allocate-general-instance ea)
         (lambda (class size)
           (make-instance 'sicl-boot::header
             :class class
             :rack (make-array size :initial-element 10000000))))
-  (load-fasl "CLOS/stamp-offset-defconstant.fasl" e5)
-  (load-fasl "CLOS/allocate-instance-defgenerics.fasl" e5)
-  (load-fasl "CLOS/allocate-instance-support.fasl" e5)
-  (load-fasl "CLOS/allocate-instance-defmethods.fasl" e5))
+  (load-fasl "CLOS/stamp-offset-defconstant.fasl" ea)
+  (load-fasl "CLOS/allocate-instance-defgenerics.fasl" ea)
+  (load-fasl "CLOS/allocate-instance-support.fasl" ea)
+  (load-fasl "CLOS/allocate-instance-defmethods.fasl" ea))
