111800159e0979a693c11783875707e787b1e0b5
Robert Strandh <robert.strandh@gmail.com>
2020-05-05 16:58:59 +0200

    Satiate all generic functions in environment E5.

--- Code/Boot/Phase-7/boot.lisp
+++ Code/Boot/Phase-7/boot.lisp
@@ -1,5 +1,24 @@
 (cl:in-package #:sicl-boot-phase-7)
 
+(defun satiate-generic-functions (e3 e4 e5)
+  (flet ((satiate (function)
+           (funcall (sicl-genv:fdefinition 'sicl-clos::satiate-generic-function e4)
+                    function)))
+    (let ((name-fun (sicl-genv:fdefinition 'sicl-clos:generic-function-name e4)))
+      (do-all-symbols (var)
+        (when (sicl-genv:fboundp var e5)
+          (let ((fun (sicl-genv:fdefinition var e5)))
+            (when (and (typep fun 'sicl-boot::header)
+                       (eq (slot-value fun 'sicl-boot::%class)
+                           (sicl-genv:find-class 'standard-generic-function e3)))
+              (format *trace-output*
+                      "Satiating ~s~%"
+                      (funcall name-fun fun))
+              (satiate fun))))))))
+
 (defun boot (boot)
-  (declare (ignore boot))
-  nil)
+  (with-accessors ((e3 sicl-boot:e3)
+                   (e4 sicl-boot:e4)
+                   (e5 sicl-boot:e5))
+      boot
+    (satiate-generic-functions e3 e4 e5)))
