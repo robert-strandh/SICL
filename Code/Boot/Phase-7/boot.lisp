283930e9dc814a5af4ab4182923e7af44f74b8e3
Robert Strandh <robert.strandh@gmail.com>
2020-05-07 05:51:34 +0200

    Call individual functions to accomplish the phase.

--- Code/Boot/Phase-7/boot.lisp
+++ Code/Boot/Phase-7/boot.lisp
@@ -19,6 +19,10 @@
 (defun boot (boot)
   (with-accessors ((e3 sicl-boot:e3)
                    (e4 sicl-boot:e4)
-                   (e5 sicl-boot:e5))
+                   (e5 sicl-boot:e5)
+                   (e6 sicl-boot:e6))
       boot
-    (satiate-generic-functions e3 e4 e5)))
+    (satiate-generic-functions e3 e4 e5)
+    (patch-classes e4 e5)
+    (patch-generic-functions e3 e4 e5)
+    (move-functions e5 e6)))
