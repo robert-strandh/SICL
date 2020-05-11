e5b4598c2447a31f9d36895e1a9f1c5223c65ab2
Robert Strandh <robert.strandh@gmail.com>
2020-05-11 15:37:32 +0200

    Disable code generation during experimentation with bootstrapping.

--- Code/Compiler/AST-compiler/ast-compiler.lisp
+++ Code/Compiler/AST-compiler/ast-compiler.lisp
@@ -2,12 +2,13 @@
 
 (defun compile-ast (client ast)
   (let ((hir1 (sicl-ast-to-hir:ast-to-hir client ast))
-        (hir2 (sicl-ast-to-hir:ast-to-hir client ast)))
-    (sicl-hir-transformations:eliminate-append-values-instructions hir2)
-    (sicl-hir-to-mir:hir-to-mir client hir2)
-    (sicl-mir-to-lir:mir-to-lir client hir2)
+        #+(or)(hir2 (sicl-ast-to-hir:ast-to-hir client ast)))
+    ;; (sicl-hir-transformations:eliminate-append-values-instructions hir2)
+    ;; (sicl-hir-to-mir:hir-to-mir client hir2)
+    ;; (sicl-mir-to-lir:mir-to-lir client hir2)
     (multiple-value-bind (instructions label-map)
-        (cluster:assemble (sicl-code-generation:generate-code hir2))
+        (values nil nil)
+        ;; (cluster:assemble (sicl-code-generation:generate-code hir2))
       (declare (ignore label-map))
       (make-instance 'code-object
         :instructions instructions
