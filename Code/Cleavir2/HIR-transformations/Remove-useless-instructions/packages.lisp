dde7f004c7b401af006502c525d856dc472c2643
Robert Strandh <robert.strandh@gmail.com>
2020-05-11 18:35:24 +0200

    Import system for removing useless instructions from Cleavir 1.

---
+++ Code/Cleavir2/HIR-transformations/Remove-useless-instructions/packages.lisp
@@ -0,0 +1,7 @@
+(cl:in-package #:common-lisp-user)
+
+(defpackage #:cleavir-remove-useless-instructions
+  (:use #:common-lisp)
+  (:export #:remove-useless-instructions
+           #:remove-useless-instructions-from
+	   #:instruction-may-be-removed-p))
