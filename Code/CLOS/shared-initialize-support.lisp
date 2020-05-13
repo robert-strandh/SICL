db46a7e1c3bd260b94c9166d0a3dd61da6666a4e
Robert Strandh <robert.strandh@gmail.com>
2020-05-13 14:56:27 +0200

    Temporarily remove call to MULTIPLE-VALUE-BIND.

--- Code/CLOS/shared-initialize-support.lisp
+++ Code/CLOS/shared-initialize-support.lisp
@@ -4,11 +4,25 @@
     (instance slot-names class &rest initargs)
   (let ((slots (class-slots class)))
     (loop for slot in slots
-          do (multiple-value-bind (key value foundp)
-                 ;; Find the first key/value pair in initargs where
-                 ;; the key is one of the initargs of the slot.
-                 (get-properties initargs (slot-definition-initargs slot))
-               (declare (ignore key))
+          do ;; (multiple-value-bind (key value foundp)
+             ;;     ;; Find the first key/value pair in initargs where
+             ;;     ;; the key is one of the initargs of the slot.
+             ;;     (get-properties initargs (slot-definition-initargs slot))
+             ;;   (declare (ignore key))
+             ;;   (format *trace-output* "here 3~%")
+             ;; The following loop replaces the commented-out code
+             ;; above.  The reason is that MUTLIPE-VALUE-BIND
+             ;; currently expands to MUTLIPLE-VALUE-CALL which in turn
+             ;; creates a closure, and allocating a closure triggers
+             ;; the object-initialization protocol, which lands us in
+             ;; an infinite recursion.
+             (let ((value nil)
+                   (foundp nil))
+               (loop with slot-initargs = (slot-definition-initargs slot)
+                     for (i v) on initargs by #'cddr
+                     when (member i slot-initargs)
+                       do (setf value v foundp t)
+                          (loop-finish))
                (if foundp
                    ;; Found an explicit initarg in initargs.
                    ;; Initialize the slot from its value.
