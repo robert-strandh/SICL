7df5dfaa8f3e921a66d069cf4982e0d86d2dfcad
Robert Strandh <robert.strandh@gmail.com>
2020-05-15 17:27:15 +0200

    Copy definition of LOAD-FASL into LOAD-FASL-1.

--- Code/Boot/utilities.lisp
+++ Code/Boot/utilities.lisp
@@ -63,6 +63,28 @@
                                   collect (sicl-genv:function-cell name global-environment))
                             (sicl-hir-transformations:constants hir))))))
 
+(defun load-fasl-1 (relative-pathname global-environment)
+  (format *trace-output* "Loading file ~s~%" relative-pathname)
+  (let* ((client (make-instance 'client))
+         (prefixed (concatenate 'string "ASTs/" relative-pathname))
+         (pathname (asdf:system-relative-pathname '#:sicl-boot prefixed))
+         (ast (cleavir-io:read-model pathname '(v0)))
+         (hir (sicl-ast-to-hir:ast-to-hir client ast))
+         ;; (hir2 (sicl-ast-to-hir:ast-to-hir client ast))
+         (fun (sicl-hir-interpreter:top-level-hir-to-host-function client hir))
+         (sicl-run-time:*dynamic-environment* '()))
+    ;; (sicl-hir-to-mir:hir-to-mir client hir2)
+    (funcall fun
+             (apply #'vector
+                    nil ; Ultimately, replace with code object.
+                    #'sicl-hir-interpreter:enclose
+                    #'cons
+                    nil
+                    (append (loop with names = (sicl-hir-transformations:function-names hir)
+                                  for name in names
+                                  collect (sicl-genv:function-cell name global-environment))
+                            (sicl-hir-transformations:constants hir))))))
+
 (defun import-function-from-host (name environment)
   (setf (sicl-genv:fdefinition name environment)
         (fdefinition name)))
