cf819ae5002b5bbacfaa869e3b7164510e73c52e
Robert Strandh <robert.strandh@gmail.com>
2020-05-16 07:55:32 +0200

    Define LOAD-FASL to FUNCALL the version in the environment in question.

--- Code/Boot/utilities.lisp
+++ Code/Boot/utilities.lisp
@@ -42,26 +42,8 @@
                             constants)))))
 
 (defun load-fasl (relative-pathname global-environment)
-  (format *trace-output* "Loading file ~s~%" relative-pathname)
-  (let* ((client (make-instance 'client))
-         (prefixed (concatenate 'string "ASTs/" relative-pathname))
-         (pathname (asdf:system-relative-pathname '#:sicl-boot prefixed))
-         (ast (cleavir-io:read-model pathname '(v0)))
-         (hir (sicl-ast-to-hir:ast-to-hir client ast))
-         ;; (hir2 (sicl-ast-to-hir:ast-to-hir client ast))
-         (fun (sicl-hir-interpreter:top-level-hir-to-host-function client hir))
-         (sicl-run-time:*dynamic-environment* '()))
-    ;; (sicl-hir-to-mir:hir-to-mir client hir2)
-    (funcall fun
-             (apply #'vector
-                    nil ; Ultimately, replace with code object.
-                    #'sicl-hir-interpreter:enclose
-                    #'cons
-                    nil
-                    (append (loop with names = (sicl-hir-transformations:function-names hir)
-                                  for name in names
-                                  collect (sicl-genv:function-cell name global-environment))
-                            (sicl-hir-transformations:constants hir))))))
+  (funcall (sicl-genv:fdefinition 'sicl-boot:load-fasl global-environment)
+           relative-pathname))
 
 (defun load-fasl-1 (relative-pathname global-environment)
   (format *trace-output* "Loading file ~s~%" relative-pathname)
