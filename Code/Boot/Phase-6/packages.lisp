a8f01023cfa1e94923731c4f51c0f6067b3d88c1
Robert Strandh <robert.strandh@gmail.com>
2020-05-16 07:56:55 +0200

    Import LOAD-FASL from SICL-BOOT.

--- Code/Boot/Phase-6/packages.lisp
+++ Code/Boot/Phase-6/packages.lisp
@@ -3,6 +3,7 @@
 (defpackage #:sicl-boot-phase-6
   (:use #:common-lisp)
   (:import-from #:sicl-boot
+                #:load-fasl
                 #:import-function-from-host
                 #:import-functions-from-host)
   (:export #:boot))
