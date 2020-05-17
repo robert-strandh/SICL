23461df8dc8b8bceb244dfa989efcae41980ef9c
Robert Strandh <robert.strandh@gmail.com>
2020-05-17 09:29:35 +0200

    Bug fix,  Load the file with the .fasl extension.

--- Code/Boot/load-source.lisp
+++ Code/Boot/load-source.lisp
@@ -2,6 +2,9 @@
 
 (defun load-source (relative-pathname environment)
   (compile-file (sicl-genv:client *e0*) relative-pathname *e0*)
-  (load-fasl relative-pathname environment))
+  (let* ((dot-pos (position #\. relative-pathname))
+         (prefix (subseq relative-pathname 0 dot-pos))
+         (relative-fasl-pathname (concatenate 'string prefix ".fasl")))
+    (load-fasl relative-fasl-pathname environment)))
 
                 
