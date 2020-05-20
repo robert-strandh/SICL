a488593c26e59653c7fd09b7bbd70a1b81c43167
Robert Strandh <robert.strandh@gmail.com>
2020-05-20 07:50:03 +0200

    Check whether a class is already finalized before calling FINALIZE-INHERITANCE.

--- Code/Boot/finalize-all-classes.lisp
+++ Code/Boot/finalize-all-classes.lisp
@@ -2,10 +2,13 @@
 
 (defun finalize-all-classes (ea)
   (format *trace-output* "Finalizing all classes.~%")
-  (let* ((finalization-function
+  (let* ((finalized-p-function
+           (sicl-genv:fdefinition 'sicl-clos:class-finalized-p ea))
+         (finalization-function
            (sicl-genv:fdefinition 'sicl-clos:finalize-inheritance ea)))
     (do-all-symbols (var)
       (let ((class (sicl-genv:find-class var ea)))
-        (unless (null class)
+        (unless (or (null class)
+                    (funcall finalized-p-function class))
           (funcall finalization-function class)))))
   (format *trace-output* "Done finalizing all classes.~%"))
