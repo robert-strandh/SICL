d19c6ae058fcbdec7a507bf6cce4e59c48dace15
Robert Strandh <robert.strandh@gmail.com>
2020-05-22 13:23:32 +0200

    Add slot NAME to the ENVIRONMENT class, and print it in PRINT-OBJECT method.

--- Code/Boot/environment.lisp
+++ Code/Boot/environment.lisp
@@ -1,8 +1,12 @@
 (cl:in-package #:sicl-boot)
 
 (defclass environment (sicl-extrinsic-environment:environment)
-  ()
+  ((%name :initarg :name :reader name))
   (:default-initargs :client (make-instance 'client)))
+
+(defmethod print-object ((object environment) stream)
+  (print-unreadable-object (object stream)
+    (format stream "Environment ~a" (name object))))
 
 (defmethod initialize-instance :after ((env environment) &key)
   (setf (sicl-genv:fdefinition 'sicl-genv:global-environment env)
