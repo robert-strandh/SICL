5d1a358ca98777cd2b48acc8a5ec30b0d9a6eeec
Robert Strandh <robert.strandh@gmail.com>
2020-05-25 11:12:37 +0200

    Fix incorrect name and use REDUCE for the implementation.

--- Code/Arithmetic/minus-defun.lisp
+++ Code/Arithmetic/minus-defun.lisp
@@ -1,9 +1,6 @@
 (cl:in-package #:sicl-arithmetic)
 
-(defun / (number &rest subtrahends)
+(defun - (number &rest subtrahends)
   (if (null subtrahends)
       (- 0 number)
-      (loop with result = number
-            for subtrahend in subtrahends
-            do (setf result (binary-minus result subtrahend))
-            finally (return result))))
+      (reduce #'binary-minus subtrahends :initial-value number)))
