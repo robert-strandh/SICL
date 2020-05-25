34a051182f7445fa4e7f39a6eaeea6c79ac42d48
Robert Strandh <robert.strandh@gmail.com>
2020-05-25 11:13:05 +0200

    Use REDUCE in the implementation.

--- Code/Arithmetic/plus-defun.lisp
+++ Code/Arithmetic/plus-defun.lisp
@@ -1,14 +1,4 @@
 (cl:in-package #:sicl-arithmetic)
 
 (defun + (&rest arguments)
-  (cond ((null arguments) 0)
-        ((null (rest arguments))
-         (let ((argument (first arguments)))
-           (if (numberp argument)
-               argument
-               (error 'argument-to-plus-must-be-a-number))))
-        (t
-         (loop with result = (first arguments)
-               for argument in (rest arguments)
-               do (setf result (binary-plus result argument))
-               finally (return result)))))
+  (reduce #'binary-plus arguments :initial-value 0))
