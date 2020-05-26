be15939b8de80dc8b01664d7348a1e6a22c287c3
Robert Strandh <robert.strandh@gmail.com>
2020-05-26 14:55:58 +0200

    Fix incorrect indentation.

--- Code/Boot/load-source.lisp
+++ Code/Boot/load-source.lisp
@@ -4,11 +4,11 @@
   (let* ((dot-pos (position #\. relative-pathname))
          (prefix (subseq relative-pathname 0 dot-pos))
          (relative-fasl-pathname (concatenate 'string prefix ".fasl")))
-  (let* ((boot-relative-fasl-pathname
-           (concatenate 'string "Boot/ASTs/" relative-fasl-pathname))
-         (fasl-pathname
-           (asdf:system-relative-pathname '#:sicl boot-relative-fasl-pathname)))
-    (if (probe-file fasl-pathname)
-        (format *trace-output* "Reusing existing FASL.~%")
-        (compile-file (sicl-genv:client *e0*) relative-pathname *e0*)))
+    (let* ((boot-relative-fasl-pathname
+             (concatenate 'string "Boot/ASTs/" relative-fasl-pathname))
+           (fasl-pathname
+             (asdf:system-relative-pathname '#:sicl boot-relative-fasl-pathname)))
+      (if (probe-file fasl-pathname)
+          (format *trace-output* "Reusing existing FASL.~%")
+          (compile-file (sicl-genv:client *e0*) relative-pathname *e0*)))
     (load-fasl relative-fasl-pathname environment)))
