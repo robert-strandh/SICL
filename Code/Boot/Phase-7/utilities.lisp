b711dafb933567ac67746dbf14a1f86b644d8beb
Robert Strandh <robert.strandh@gmail.com>
2020-05-27 05:56:47 +0200

    Load the file with .fasl extension.

--- Code/Boot/Phase-7/utilities.lisp
+++ Code/Boot/Phase-7/utilities.lisp
@@ -1,8 +1,11 @@
 (cl:in-package #:sicl-boot-phase-7)
 
 (defun load-source (relative-pathname environment)
-  (sicl-boot:compile-file
-   relative-pathname
-   (sicl-genv:client sicl-boot:*e0*)
-   environment)
-  (load-fasl relative-pathname environment))
+  (let* ((dot-pos (position #\. relative-pathname))
+         (prefix (subseq relative-pathname 0 dot-pos))
+         (relative-fasl-pathname (concatenate 'string prefix ".fasl")))
+    (sicl-boot:compile-file
+     (sicl-genv:client sicl-boot:*e0*)
+     relative-pathname
+     environment)
+    (load-fasl relative-fasl-pathname environment)))
