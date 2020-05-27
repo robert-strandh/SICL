105cc9742a8b6ae654f8124d439cd53bdd9bd154
Robert Strandh <robert.strandh@gmail.com>
2020-05-27 08:02:47 +0200

    Define function for enabling macros in environment E5, and enable macro DECLAIM.

--- Code/Boot/Phase-7/enable-compilation.lisp
+++ Code/Boot/Phase-7/enable-compilation.lisp
@@ -4,5 +4,21 @@
   (do-symbols (symbol (find-package '#:cleavir-primop))
     (setf (sicl-genv:special-operator symbol environment) t)))
 
+;;; We import most of the expander functions from the host at first.
+;;; The reason is that expander function might need some of the
+;;; macros, and the dependency might be circular.  So the strategy is
+;;; to first import the expanders, then make all the macros work, and
+;;; when everything works, load the expanders normally.
+(defun enable-macros (environment)
+  ;; Enable DECLAIM.
+  ;; FIXME: define PROCLAIM better.
+  (setf (sicl-genv:fdefinition 'proclaim environment)
+        (lambda (&rest arguments)
+          (format *trace-output* "args: ~s~%" arguments)))
+  (sicl-boot:import-function-from-host
+   'sicl-evaluation-and-compilation:declaim-expander environment)
+  (sicl-boot:load-source "Evaluation-and-compilation/declaim-defmacro.lisp" environment))
+
 (defun enable-compilation (environment)
-  (enable-special-operators environment))
+  (enable-special-operators environment)
+  (enable-macros environment))
