cc1a2aefc2e923527f01dde7440a2fabcb6ebc18
Robert Strandh <robert.strandh@gmail.com>
2020-05-27 09:11:11 +0200

    No longer load support code for macro DEFMETHOD.

--- Code/Boot/Phase-7/boot.lisp
+++ Code/Boot/Phase-7/boot.lisp
@@ -40,6 +40,4 @@
     (sicl-boot:load-source "Cons/cxr.lisp" e5)
     (sicl-boot:load-source "Cons/setf-cxr.lisp" e5)
     (sicl-boot:load-source "Cons/getf-defun.lisp" e5)
-    (sicl-boot:load-source "CLOS/defmethod-support.lisp" e5)
-    (sicl-boot:load-source "CLOS/defmethod-defmacro.lisp" e5)
     (sicl-boot:load-source "Cons/member-if-not-defun.lisp" e5)))
