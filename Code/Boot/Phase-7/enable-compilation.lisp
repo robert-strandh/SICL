0e1021457f05bb1f2bef023243904bee3016cf1d
Robert Strandh <robert.strandh@gmail.com>
2020-05-28 14:36:08 +0200

    Remove package prefix from LOAD-SOURCE.

--- Code/Boot/Phase-7/enable-compilation.lisp
+++ Code/Boot/Phase-7/enable-compilation.lisp
@@ -31,46 +31,46 @@
 ;;; some of the code again here.
 (defun enable-macros (environment)
   ;; Enable DEFMACRO.
-  (sicl-boot:load-source "Evaluation-and-compilation/defmacro-defmacro.lisp" environment)
+  (load-source "Evaluation-and-compilation/defmacro-defmacro.lisp" environment)
   ;; Enable LAMBDA.
   ;; There is no independent expander for LAMBDA, because it is so
   ;; simple.
-  (sicl-boot:load-source "Evaluation-and-compilation/lambda.lisp" environment)
+  (load-source "Evaluation-and-compilation/lambda.lisp" environment)
   ;; Enable MULTIPLE-VALUE-BIND.
   ;; There is no independent expander for MULTIPLE-VALUE-BIND.,
   ;; because it is so simple.
-  (sicl-boot:load-source "Environment/multiple-value-bind.lisp" environment)
+  (load-source "Environment/multiple-value-bind.lisp" environment)
   ;; Enable MULTIPLE-VALUE-LIST.
   ;; There is no independent expander for MULTIPLE-VALUE-LIST.,
   ;; because it is so simple.
-  (sicl-boot:load-source "Data-and-control-flow/multiple-value-list-defmacro.lisp" environment)
+  (load-source "Data-and-control-flow/multiple-value-list-defmacro.lisp" environment)
   ;; Enable MULTIPLE-VALUE-CALL.
   ;; There is no independent expander for MULTIPLE-VALUE-CALL.,
   ;; because it is so simple.
-  (sicl-boot:load-source "Data-and-control-flow/multiple-value-call-defmacro.lisp" environment)
+  (load-source "Data-and-control-flow/multiple-value-call-defmacro.lisp" environment)
   ;; Enable LOOP.
   (sicl-boot:import-functions-from-host
    '(sicl-loop:expand-body sicl-loop::list-car sicl-loop::list-cdr)
      environment)
   ;; There is no independent expander for LOOP, because it is so
   ;; simple.
-  (sicl-boot:load-source "Loop/loop-defmacro.lisp" environment)
+  (load-source "Loop/loop-defmacro.lisp" environment)
   ;; Enable RETURN.
   ;; There is no independent expander for RETURN, because it is so
   ;; simple.
-  (sicl-boot:load-source "Data-and-control-flow/return-defmacro.lisp" environment)
+  (load-source "Data-and-control-flow/return-defmacro.lisp" environment)
   ;; Enable PROG1 and PROG2.
   ;; There are no independent expanders for PROG1 and PROG2, because
   ;; they are so simple.
-  (sicl-boot:load-source "Data-and-control-flow/prog1-prog2-defmacro.lisp" environment)
+  (load-source "Data-and-control-flow/prog1-prog2-defmacro.lisp" environment)
   ;; Enable PROG and PROG*.
   ;; There are no independent expanders for PROG and PROG*, because
   ;; they are so simple.
-  (sicl-boot:load-source "Data-and-control-flow/prog-progstar-defmacro.lisp" environment)
+  (load-source "Data-and-control-flow/prog-progstar-defmacro.lisp" environment)
   ;; enable INCF and DECF.
   ;; There are no independent expanders for INCF and DECF, because
   ;; they are so simple.
-  (sicl-boot:load-source "Arithmetic/incf-decf-defmacro.lisp" environment)
+  (load-source "Arithmetic/incf-decf-defmacro.lisp" environment)
   ;; Enable DECLAIM.
   ;; FIXME: define PROCLAIM better.
   (setf (sicl-genv:fdefinition 'proclaim environment)
@@ -78,7 +78,7 @@
           (format *trace-output* "args: ~s~%" arguments)))
   (sicl-boot:import-function-from-host
    'sicl-evaluation-and-compilation:declaim-expander environment)
-  (sicl-boot:load-source "Evaluation-and-compilation/declaim-defmacro.lisp" environment)
+  (load-source "Evaluation-and-compilation/declaim-defmacro.lisp" environment)
   ;; Enable OR, AND, WHEN, UNLESS, COND,
   ;; CASE, ECASE, CCASE, TYPECASE, ETYPECASE, CTYPECASE.
   ;; There are no independent expanders for WHEN and UNLESS, because
@@ -93,43 +93,43 @@
      sicl-conditionals:etypecase-expander
      sicl-conditionals:ctypecase-expander)
    environment)
-  (sicl-boot:load-source "Conditionals/macros.lisp" environment)
+  (load-source "Conditionals/macros.lisp" environment)
   ;; Load a file containing the definition of GET-SETF-EXPANSION.  We
   ;; can't use the host version of this function, because it accesses
   ;; the global environment for SETF expanders.
-  (sicl-boot:load-source "Data-and-control-flow/get-setf-expansion-defun.lisp" environment)
+  (load-source "Data-and-control-flow/get-setf-expansion-defun.lisp" environment)
   ;; Enable PUSH and POP.
   ;; We can't use the host version of the expanders for these macros,
   ;; because they call GET-SETF-EXPANSION at macro expansion time.
-  (sicl-boot:load-source "Cons/push-pop-support.lisp" environment)
-  (sicl-boot:load-source "Cons/push-pop-defmacro.lisp" environment)
+  (load-source "Cons/push-pop-support.lisp" environment)
+  (load-source "Cons/push-pop-defmacro.lisp" environment)
   ;; Enable WITH-ACCESSORS
   ;; There is no independent expander for WITH-ACCESSORS, because it
   ;; is so simple.
-  (sicl-boot:load-source "CLOS/with-accessors-defmacro.lisp" environment)
+  (load-source "CLOS/with-accessors-defmacro.lisp" environment)
   ;; Enable QUASIQUOTE for Eclector
   (setf (sicl-genv:macro-function 'eclector.reader::quasiquote environment)
         (macro-function 'eclector.reader::quasiquote))
   ;; Enable DEFUN.
   (sicl-boot:import-function-from-host
    'sicl-data-and-control-flow:defun-expander environment)
-  (sicl-boot:load-source "Data-and-control-flow/defun-defmacro.lisp" environment)
+  (load-source "Data-and-control-flow/defun-defmacro.lisp" environment)
   ;; Enable DEFGENERIC.
   (sicl-boot:import-function-from-host
    'sicl-clos:defgeneric-expander environment)
-  (sicl-boot:load-source "CLOS/defgeneric-defmacro.lisp" environment)
+  (load-source "CLOS/defgeneric-defmacro.lisp" environment)
   ;; Enable DEFCLASS.
   (sicl-boot:import-function-from-host
    'sicl-clos:defclass-expander environment)
-  (sicl-boot:load-source "CLOS/defclass-defmacro.lisp" environment)
-  (sicl-boot:load-source "CLOS/find-or-create-generic-function.lisp" environment)
+  (load-source "CLOS/defclass-defmacro.lisp" environment)
+  (load-source "CLOS/find-or-create-generic-function.lisp" environment)
   ;; FIXME: load a file with the definition instead of defining it here.
   (setf (sicl-genv:fdefinition 'sicl-clos::find-accessor-method-class environment)
         (sicl-genv:fdefinition 'sicl-clos::find-class environment))
   ;; Enable DEFMETHOD.
   (sicl-boot:import-function-from-host
    'sicl-clos:defmethod-expander environment)
-  (sicl-boot:load-source "CLOS/defmethod-defmacro.lisp" environment))
+  (load-source "CLOS/defmethod-defmacro.lisp" environment))
 
 (defun enable-compilation (environment)
   (enable-special-operators environment)
--- Code/Boot/Phase-7/load-make-instance.lisp
+++ Code/Boot/Phase-7/load-make-instance.lisp
@@ -2,6 +2,6 @@
 
 ;;; FIXME: use a version of LOAD-FASL that creates machine code.
 (defun load-make-instance (e5)
-  (sicl-boot:load-source "CLOS/make-instance-support.lisp" e5)
-  (sicl-boot:load-source "CLOS/make-instance-defgenerics.lisp" e5)
-  (sicl-boot:load-source "CLOS/make-instance-defmethods.lisp" e5))
+  (load-source "CLOS/make-instance-support.lisp" e5)
+  (load-source "CLOS/make-instance-defgenerics.lisp" e5)
+  (load-source "CLOS/make-instance-defmethods.lisp" e5))
