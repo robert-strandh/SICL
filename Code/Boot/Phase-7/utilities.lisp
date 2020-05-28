4c8b5392f8e83232bd754121a06b9eef2f4c1b67
Robert Strandh <robert.strandh@gmail.com>
2020-05-28 14:35:32 +0200

    Import function LOAD-SOURCE from SICL-BOOT rather than defining it here.

--- Code/Boot/Phase-7/utilities.lisp
+++ Code/Boot/Phase-7/utilities.lisp
@@ -1,11 +1,1 @@
 (cl:in-package #:sicl-boot-phase-7)
-
-(defun load-source (relative-pathname environment)
-  (let* ((dot-pos (position #\. relative-pathname))
-         (prefix (subseq relative-pathname 0 dot-pos))
-         (relative-fasl-pathname (concatenate 'string prefix ".fasl")))
-    (sicl-boot:compile-file
-     (sicl-genv:client sicl-boot:*e0*)
-     relative-pathname
-     environment)
-    (load-fasl relative-fasl-pathname environment)))
