baf048c24f4cca0aca7a198133ab95fedf9bd717
Robert Strandh <robert.strandh@gmail.com>
2020-05-28 13:48:05 +0200

    Announce the start of the phase.

--- Code/Boot/Phase-7/boot.lisp
+++ Code/Boot/Phase-7/boot.lisp
@@ -5,6 +5,7 @@
   (slot-value object 'sicl-boot::%class))
 
 (defun boot (boot)
+  (format *trace-output* "Start of phase 7~%")
   (with-accessors ((e3 sicl-boot:e3)
                    (e4 sicl-boot:e4)
                    (e5 sicl-boot:e5)
