17c432cb287399de1cfa32279834c49782e3ed93
Robert Strandh <robert.strandh@gmail.com>
2020-05-30 10:05:00 +0200

    Extract general methods on TYPEP-COMPOUND to a separate component.

--- Code/Types/Typep/typep-compound.lisp
+++ Code/Types/Typep/typep-compound.lisp
@@ -1,18 +1,5 @@
 (cl:in-package #:sicl-type)
 
-(defun proper-list-p (object)
-  (integerp (ignore-errors (list-length object))))
-
-(defmethod typep-compound :before (object head rest)
-  (assert (proper-list-p rest)))
-
-;;; Given a type specifier that is illegal in the context of TYPEP,
-;;; such as VALUES or FUNCTION, signal an error to that effect.
-(defmethod typep-compound (object head rest)
-  (declare (ignore object))
-  (error "Compound type specifier is illegal for typep: ~s."
-	 (cons head rest)))
-
 (defmethod typep-compound (object (head (eql 'and)) rest)
   (loop for type-spec in rest
 	always (generic-typep object type-spec)))
