9a71c9bb4153a7eaf9ff7c9a15db9da0fff5800d
Robert Strandh <robert.strandh@gmail.com>
2020-05-30 09:57:53 +0200

    Remove comment and improve code by dispatching on the type specifier argument.

--- Code/Types/Typep/generic-typep.lisp
+++ Code/Types/Typep/generic-typep.lisp
@@ -1,9 +1,7 @@
 (cl:in-package #:sicl-type)
 
-;;; We try not to allocate any memory in TYPEP.  To avoid it as much
-;;; as possible, we do not start by expanding the type desriptor.
+(defmethod generic-typep (object (type-specifier cons))
+  (typep-compound object (first type-specifier) (rest type-specifier)))
 
-(defmethod generic-typep (object type-specifier environment)
-  (if (symbolp type-specifier)
-      (typep-atomic object type-specifier environment)
-      (typep-compound object type-specifier environment)))
+(defmethod generic-typep (object type-specifier)
+  (typep-atomic object type-specifier))
