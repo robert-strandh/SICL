b1ad02c8c8b429b2894d7d3640d66dbfea33f838
Robert Strandh <robert.strandh@gmail.com>
2020-06-05 18:19:41 +0200

    Load a file containing definition of class LIST-HASH-TABLE.

--- Code/Boot/Phase-8/load-hash-table-functionality.lisp
+++ Code/Boot/Phase-8/load-hash-table-functionality.lisp
@@ -2,5 +2,6 @@
 
 (defun load-hash-table-functionality (e5)
   (load-source "Hash-tables/hash-table-defclass.lisp" e5)
+  (load-source "Hash-tables/List/list-hash-table-defclass.lisp" e5)
   (load-source "Data-and-control-flow/equalp-defgeneric.lisp" e5)
   (load-source "Hash-tables/generic-functions.lisp" e5))
