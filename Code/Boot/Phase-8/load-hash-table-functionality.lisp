10b95cb16d66d9c7e0e7ab468aaa59dd088efa36
Robert Strandh <robert.strandh@gmail.com>
2020-06-05 17:42:30 +0200

    Load a file containing definitions of generic functions on hash tables.

--- Code/Boot/Phase-8/load-hash-table-functionality.lisp
+++ Code/Boot/Phase-8/load-hash-table-functionality.lisp
@@ -1,4 +1,6 @@
 (cl:in-package #:sicl-boot-phase-8)
 
 (defun load-hash-table-functionality (e5)
-  (load-source "Hash-tables/hash-table-defclass.lisp" e5))
+  (load-source "Hash-tables/hash-table-defclass.lisp" e5)
+  (load-source "Data-and-control-flow/equalp-defgeneric.lisp" e5)
+  (load-source "Hash-tables/generic-functions.lisp" e5))
