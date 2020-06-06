7a432b0a82f3ed93bc1990ba09ad164ea12993db
Marco Heisig <marco.heisig@fau.de>
2020-06-06 21:19:34 +0200

    Improve correctness and performance of REPLACE.
    - Correct handling of the case when both sequences are EQ.
    
    - Enable method inlining.
    
    - Use DO instead of (LOOP REPEAT ...), because the former allows for a
      crucial type annotation on the loop limit.

--- Code/Sequence/replace.lisp
+++ Code/Sequence/replace.lisp
@@ -1,40 +1,68 @@
 (cl:in-package #:sicl-sequence)
 
 (defmethod replace ((list1 list) (list2 list) &key (start1 0) end1 (start2 0) end2)
+  (declare (method-properties inlineable))
   (multiple-value-bind (start1 end1)
       (canonicalize-start-and-end list1 start1 end1)
     (multiple-value-bind (start2 end2)
         (canonicalize-start-and-end list2 start2 end2)
-      (loop repeat (min (- end1 start1) (- end2 start2))
-            for cons1 on (nthcdr start1 list1)
-            for cons2 on (nthcdr start2 list2)
-            do (setf (car cons1) (car cons2)))
-      list1)))
+      ;; We have to be careful to correctly handle the cases where LIST1
+      ;; and LIST2 are identical.  Depending on the relative order of the
+      ;; source and the target range, it may be necessary to copy the
+      ;; source elements.
+      (multiple-value-bind (rest1 rest2)
+          (if (eq list1 list2)
+              (cond ((= start1 start2)
+                     (return-from replace list1))
+                    ((> end2 start1 start2)
+                     (let ((tmp (nthcdr start2 list1)))
+                       (values
+                        (nthcdr (- start1 start2) tmp)
+                        (subseq tmp 0 (- end2 start2)))))
+                    ((< start1 start2)
+                     (let ((tmp (nthcdr start1 list1)))
+                       (values
+                        tmp
+                        (nthcdr (- start2 start1) tmp)))))
+              (values
+               (nthcdr start1 list1)
+               (nthcdr start2 list2)))
+        (do ((limit (min (- end1 start1) (- end2 start2)))
+             (counter 0 (1+ counter))
+             (cons1 rest1 (cdr cons1))
+             (cons2 rest2 (cdr cons2)))
+            ((= counter limit) list1)
+          (declare (array-length counter limit))
+          (setf (car cons1) (car cons2)))))))
 
 (seal-domain #'replace '(list list))
 
 (replicate-for-each-vector-class #1=#:vector-class
   (defmethod replace ((list list) (vector #1#) &key (start1 0) end1 (start2 0) end2)
+    (declare (method-properties inlineable))
     (multiple-value-bind (start1 end1)
         (canonicalize-start-and-end list start1 end1)
       (multiple-value-bind (start2 end2)
           (canonicalize-start-and-end vector start2 end2)
-        (loop repeat (min (- end1 start1) (- end2 start2))
-              for index from start2
-              for cons on (nthcdr start1 list)
-              do (setf (car cons) (elt vector index)))
-        list)))
+        (do ((limit (min (- end1 start1) (- end2 start2)))
+             (counter 0 (1+ counter))
+             (cons (nthcdr start1 list) (cdr cons)))
+            ((= counter limit) list)
+          (declare (array-length counter limit))
+          (setf (car cons) (elt vector (+ start2 counter)))))))
 
   (defmethod replace ((vector #1#) (list list) &key (start1 0) end1 (start2 0) end2)
+    (declare (method-properties inlineable))
     (multiple-value-bind (start1 end1)
         (canonicalize-start-and-end vector start1 end1)
       (multiple-value-bind (start2 end2)
           (canonicalize-start-and-end list start2 end2)
-        (loop repeat (min (- end1 start1) (- end2 start2))
-              for index from start1
-              for cons on (nthcdr start2 list)
-              do (setf (elt vector index) (car cons)))
-        vector))))
+        (do ((limit (min (- end1 start1) (- end2 start2)))
+             (counter 0 (1+ counter))
+             (cons (nthcdr start2 list) (cdr cons)))
+            ((= counter limit) vector)
+          (declare (array-length counter limit))
+          (setf (elt vector (+ start1 counter)) (car cons)))))))
 
 (seal-domain #'replace '(list vector))
 
@@ -42,14 +70,34 @@
 
 (replicate-for-all-compatible-vector-classes #1=#:vector-class-1 #2=#:vector-class-2
   (defmethod replace ((vector1 #1#) (vector2 #2#) &key (start1 0) end1 (start2 0) end2)
+    (declare (method-properties inlineable))
     (multiple-value-bind (start1 end1)
         (canonicalize-start-and-end vector1 start1 end1)
       (multiple-value-bind (start2 end2)
           (canonicalize-start-and-end vector2 start2 end2)
-        (loop repeat (min (- end1 start1) (- end2 start2))
-              for index1 from start1
-              for index2 from start2
-              do (setf (elt vector1 index1) (elt vector2 index2)))
-        vector1))))
+        (if (and (eq vector1 vector2)
+                 (if (= start1 start2) (return-from replace vector1) t)
+                 (> end2 start1 start2))
+            ;; When we are reading and writing from the same array, and
+            ;; when the destination precedes the source, we have to be
+            ;; careful not to overwrite values that will be read later.  We
+            ;; achieve this by copying from end to start.
+            (if (= start1 start2)
+                ;; If the source and target vectors and the designated
+                ;; ranges are identical, we don't need to copy at all.
+                vector1
+                (do ((counter (min (- end1 start1) (- end2 start2)) (1- counter)))
+                    ((zerop counter) vector1)
+                  (declare (array-length counter))
+                  (setf (elt vector1 (+ start1 counter -1))
+                        (elt vector1 (+ start2 counter -1)))))
+            ;; If we are dealing with distinct vectors, or if the source
+            ;; precedes the destination, we copy from start to end.
+            (do ((limit (min (- end1 start1) (- end2 start2)))
+                 (counter 0 (1+ counter)))
+                ((= counter limit) vector1)
+              (declare (array-length counter limit))
+              (setf (elt vector1 (+ start1 counter))
+                    (elt vector2 (+ start2 counter)))))))))
 
 (seal-domain #'replace '(vector vector))
