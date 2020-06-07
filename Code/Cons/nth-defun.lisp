6dd3397b6fe8bff7bdc28cd7a6c20cd43d4e1697
Robert Strandh <robert.strandh@gmail.com>
2020-06-07 13:13:51 +0200

    Temporarily disable type checks.

--- Code/Cons/nth-defun.lisp
+++ Code/Cons/nth-defun.lisp
@@ -1,16 +1,16 @@
 (cl:in-package #:sicl-cons)
 
 (defun nth (n list)
-  (unless (typep n '(integer 0))
-    (error 'must-be-nonnegative-integer
-           :datum n
-           :name 'nthcdr))
+  ;; (unless (typep n '(integer 0))
+  ;;   (error 'must-be-nonnegative-integer
+  ;;          :datum n
+  ;;          :name 'nthcdr))
   (loop until (zerop n)
         until (atom list)
         do (decf n)
         do (setf list (cdr list)))
-  (when (not (listp list))
-    (error 'must-be-list
-           :datum list
-           :name 'nth))
+  ;; (when (not (listp list))
+  ;;   (error 'must-be-list
+  ;;          :datum list
+  ;;          :name 'nth))
   (car list))
