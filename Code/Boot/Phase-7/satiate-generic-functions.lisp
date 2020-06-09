903a70dff8433df34b94426e41f92468ba01e4e2
Robert Strandh <robert.strandh@gmail.com>
2020-06-09 13:13:13 +0200

    Call function for finding all generic functions.

--- Code/Boot/Phase-7/satiate-generic-functions.lisp
+++ Code/Boot/Phase-7/satiate-generic-functions.lisp
@@ -14,21 +14,14 @@
               (push fun result))))))
     result))
 
+(defun satiate-generic-function (function e4)
+  (format *trace-output*
+          "Satiating ~s~%"
+          (funcall (sicl-genv:fdefinition 'sicl-clos:generic-function-name e4)
+                   function))
+  (funcall (sicl-genv:fdefinition 'sicl-clos::satiate-generic-function e4)
+           function))
+
 (defun satiate-generic-functions (e3 e4 e5)
-  (flet ((satiate (function)
-           (funcall (sicl-genv:fdefinition 'sicl-clos::satiate-generic-function e4)
-                    function)))
-    (let ((name-fun (sicl-genv:fdefinition 'sicl-clos:generic-function-name e4))
-          (table (make-hash-table :test #'eq)))
-      (do-all-symbols (var)
-        (unless (gethash var table)
-          (setf (gethash var table) t)
-          (when (sicl-genv:fboundp var e5)
-            (let ((fun (sicl-genv:fdefinition var e5)))
-              (when (and (typep fun 'sicl-boot::header)
-                         (eq (slot-value fun 'sicl-boot::%class)
-                             (sicl-genv:find-class 'standard-generic-function e3)))
-                (format *trace-output*
-                        "Satiating ~s~%"
-                        (funcall name-fun fun))
-                (satiate fun)))))))))
+  (loop for function in (find-all-generic-functions e3 e5)
+        do (satiate-generic-function function e4)))
