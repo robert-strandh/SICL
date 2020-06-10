9b7fdf1af3dd28610a244155d5608d4215ce3e18
Robert Strandh <robert.strandh@gmail.com>
2020-06-10 08:55:10 +0200

    Define method on COMPILE-AST specialized to SINGLE-FLOAT-AST.

--- Code/Cleavir2/AST-to-HIR/compile-simple-float-related-asts.lisp
+++ Code/Cleavir2/AST-to-HIR/compile-simple-float-related-asts.lisp
@@ -1,5 +1,23 @@
 (in-package #:cleavir-ast-to-hir)
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;; Compile a SINGLE-FLOAT-P-AST.
+
+(defmethod compile-ast (client (ast cleavir-ast:single-float-p-ast) context)
+  (assert-context ast context 0 2)
+  (let ((temp (make-temp)))
+    (compile-ast
+     client
+     (cleavir-ast:object-ast ast)
+     (clone-context
+      context
+      :result temp
+      :successor
+      (make-instance 'cleavir-ir:single-float-p-instruction
+        :input temp
+        :successors (successors context))))))
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;
 ;;; Utility function for compiling a list of ASTs that are arguments
