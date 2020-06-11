3905f05f9baf8a809ca6e93d528033d4b7d4eff0
Robert Strandh <robert.strandh@gmail.com>
2020-06-11 18:15:40 +0200

    Rewrite method on INTERPRET-INSTRUCTION specialized to NOOK-READ-INSTRUCTION.

--- Code/Boot/Phase-3/HIR-interpreter-configuration.lisp
+++ Code/Boot/Phase-3/HIR-interpreter-configuration.lisp
@@ -6,15 +6,30 @@
      lexical-environment)
   (destructuring-bind (object-input index-input)
       (cleavir-ir:inputs instruction)
-    (let ((object
-            (sicl-hir-interpreter:input-value object-input lexical-environment))
-          (index
-            (sicl-hir-interpreter:input-value index-input lexical-environment))
-          (output (first (cleavir-ir:outputs instruction))))
+    (let* ((standard-object-input-value
+             (sicl-hir-interpreter:input-value object-input lexical-environment))
+           (index-input-value
+             (sicl-hir-interpreter:input-value index-input lexical-environment))
+           (output (first (cleavir-ir:outputs instruction))))
       (setf (sicl-hir-interpreter:lexical-value output lexical-environment)
-            (aref (slot-value object 'sicl-boot::%rack) index))))
+            (if (zerop index-input-value)
+                ;; Then the stamp is asked for
+                (flet ((unique-number (class-name)
+                         (funcall (sicl-genv:fdefinition
+                                   'sicl-clos::unique-number sicl-boot:*e3*)
+                                  (sicl-genv:find-class class-name sicl-boot:*e3*))))
+                  (cond ((null standard-object-input-value)
+                         (unique-number 'null))
+                        ((symbolp standard-object-input-value)
+                         (unique-number 'symbol))
+                        ((typep standard-object-input-value 'sicl-boot::header)
+                         (let ((rack (slot-value standard-object-input-value 'sicl-boot::%rack)))
+                           (aref rack 0)))
+                        (t (error "Can't deal with object ~s"
+                                  standard-object-input-value))))
+                (let ((rack (slot-value standard-object-input-value 'sicl-boot::%rack)))
+                  (aref rack index-input-value))))))
   (first (cleavir-ir:successors instruction)))
-
 
 (defmethod sicl-hir-interpreter:interpret-instruction
     ((client sicl-boot:client)
