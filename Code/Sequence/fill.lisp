86909597c4867dabfa5f9b78a2b49a2104880e28
Marco Heisig <marco.heisig@fau.de>
2020-06-12 18:43:35 +0200

    Declare sicl-sequence:fill as inlineable.

--- Code/Sequence/fill.lisp
+++ Code/Sequence/fill.lisp
@@ -1,6 +1,7 @@
 (cl:in-package #:sicl-sequence)
 
 (defmethod fill ((list list) item &key (start 0) end)
+  (declare (method-properties inlineable))
   (for-each-relevant-cons (cons index list start end)
     (setf (car cons) item))
   list)
@@ -9,6 +10,7 @@
 
 (replicate-for-each-vector-class #1=#:vector-class
   (defmethod fill ((vector #1#) item &key (start 0) end)
+    (declare (method-properties inlineable))
     (for-each-relevant-element (element index vector start end)
       (setf element item))
     vector))
