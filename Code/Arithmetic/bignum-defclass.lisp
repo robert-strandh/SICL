85019382e5de181c6a794cbb736cc2166368da4e
Robert Strandh <robert.strandh@gmail.com>
2020-06-13 16:55:28 +0200

    Add STANDARD-OBJECT as an explicit superclass.

--- Code/Arithmetic/bignum-defclass.lisp
+++ Code/Arithmetic/bignum-defclass.lisp
@@ -1,6 +1,9 @@
 (cl:in-package #:sicl-arithmetic)
 
-(defclass bignum (integer)
+;;; We must explicitly add STANDARD-OBJECT as a superclass since the
+;;; presence of INTEGER as a superclass prevents the automatic
+;;; inclusion of STANDARD-OBJECT as a superclass.
+(defclass bignum (integer standard-object)
   ((%sign-and-digit-count
     :initarg :sign-and-digit-count
     :reader sign-and-digit-count)))
