03176025389e9413051f7e7cb2c7535d7b29097d
Robert Strandh <robert.strandh@gmail.com>
2020-06-13 11:56:03 +0200

    Supply integer default initargs to avoid floating-point arithmetic.

--- Code/Hash-tables/List/list-hash-table-defclass.lisp
+++ Code/Hash-tables/List/list-hash-table-defclass.lisp
@@ -2,6 +2,7 @@
 
 (defclass list-hash-table (hash-table)
   ((%size :initform 0 :initarg :size :accessor size :reader hash-table-size)
-   (%contents :initform '() :accessor contents)))
+   (%contents :initform '() :accessor contents))
+  (:default-initargs :rehash-size 1 :rehash-threshold 1))
 
 (setf sicl-hash-table:*default-hash-table-class* (find-class 'list-hash-table))
