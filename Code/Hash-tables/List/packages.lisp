0a100f425ffb995320cb087922a60f9a534023da
Robert Strandh <robert.strandh@gmail.com>
2020-06-13 11:28:58 +0200

    Import names of internal functions from package SICL-HASH-TABLE.

--- Code/Hash-tables/List/packages.lisp
+++ Code/Hash-tables/List/packages.lisp
@@ -1,4 +1,8 @@
 (cl:in-package #:common-lisp-user)
 
 (defpackage #:sicl-list-hash-table
-  (:use #:common-lisp))
+  (:use #:common-lisp)
+  (:import-from
+   #:sicl-hash-table
+   #:make-hash-table-iterator
+   #:%hash-table-test))
