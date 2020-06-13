2150bcea78f4add97b55233f60a2664588b80ae8
Robert Strandh <robert.strandh@gmail.com>
2020-06-13 16:23:54 +0200

    Load file containing definition of MAKE-HASH-TABLE.

--- Code/Boot/Phase-8/load-hash-table-functionality.lisp
+++ Code/Boot/Phase-8/load-hash-table-functionality.lisp
@@ -4,6 +4,7 @@
   (load-source "Hash-tables/hash-table-defclass.lisp" e5)
   (load-source "Data-and-control-flow/equalp-defgeneric.lisp" e5)
   (load-source "Hash-tables/generic-functions.lisp" e5)
+  (load-source "Hash-tables/make-hash-table.lisp" e5)
   (load-source "Hash-tables/List/list-hash-table-defclass.lisp" e5)
   (load-source "Hash-tables/List/gethash-defmethod.lisp" e5)
   (load-source "Hash-tables/List/setf-gethash-defmethod.lisp" e5)
