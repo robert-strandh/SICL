524d01c353b29d560a47f0918bb25eb123da1285
Marco Heisig <marco.heisig@fau.de>
2020-06-14 20:23:30 +0200

    Fix a bug in map-into for vectors with a fill pointer.
    This bug was triggered when a vector with a fill pointer occurs both as the
    target and as a source of map-into.

--- Code/Sequence/map-into.lisp
+++ Code/Sequence/map-into.lisp
@@ -15,20 +15,20 @@
 
 (replicate-for-each-vector-class #1=#:vector-class
   (defmethod map-into ((vector #1#) function &rest sequences)
-    (when (array-has-fill-pointer-p vector)
-      (setf (fill-pointer vector)
-            (array-total-size vector)))
     (let ((function (function-designator-function function))
           (index 0))
       (declare (vector-length index))
-      (flet ((fn (&rest arguments)
-               (unless (array-in-bounds-p vector index)
-                 (return-from map-into vector))
-               (setf (elt vector index)
-                     (apply function arguments))
-               (incf index)))
-        (declare (dynamic-extent #'fn))
-        (apply #'map-for-effect #'fn sequences))
+      (block nil
+        (flet ((fn (&rest arguments)
+                 (unless (array-in-bounds-p vector index)
+                   (return))
+                 ;; We have to use AREF instead of ELT here, because only the
+                 ;; former ignores the fill-pointer.
+                 (setf (aref vector index)
+                       (apply function arguments))
+                 (incf index)))
+          (declare (dynamic-extent #'fn))
+          (apply #'map-for-effect #'fn sequences)))
       (when (array-has-fill-pointer-p vector)
         (setf (fill-pointer vector) index))
       vector)))
