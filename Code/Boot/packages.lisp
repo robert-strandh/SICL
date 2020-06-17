ae438daf6e8dd546b88c876d7a99456fdb79a1f8
Robert Strandh <robert.strandh@gmail.com>
2020-06-17 10:59:34 +0200

    No longer export name of removed function.

--- Code/Boot/packages.lisp
+++ Code/Boot/packages.lisp
@@ -10,7 +10,6 @@
            #:*e0* #:*e1* #:*e2* #:*e3* #:*e4* #:*e5* #:*e6* #:*e7*
            #:import-function-from-host
            #:import-functions-from-host
-           #:import-package-from-host
            #:import-class-from-host
            #:tie-code-object
            #:load-fasl
