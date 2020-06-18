50d0ff4bfc38b4065ca9ed95559f863a4cac9a57
Robert Strandh <robert.strandh@gmail.com>
2020-06-18 14:06:10 +0200

    Rewrite to use FLET and hash table of external symbols.

--- Code/Package-and-symbol/do-external-symbols-defmacro.lisp
+++ Code/Package-and-symbol/do-external-symbols-defmacro.lisp
@@ -1,23 +1,24 @@
 (cl:in-package #:sicl-package)
 
 ;;; FIXME: check syntax better
-(defmacro do-external-symbols ((symbol-variable
-				&optional
-				  (package-form '*package*)
-				  (result-form 'nil))
-			       &body body)
+(defmacro do-external-symbols
+    ((symbol-variable
+      &optional
+        (package-form '*package*)
+        (result-form 'nil))
+     &body body)
   (let ((function-name (gensym))
-	(package-var (gensym))
-	(remaining-body body)
-	(declarations '()))
-    (loop while (eq (caar remaining-body) 'declare)
-	  do (push (pop remaining-body) declarations))
-    `(block nil
-       (let ((,function-name (lambda (,symbol-variable)
-			       (locally ,@(reverse declarations)
-				 (tagbody
-				    ,@remaining-body))))
-	     (,package-var (package-designator-to-package ,package-form)))
-	 (mapc ,function-name
-	       (external-symbols ,package-var)))
-       ,result-form)))
+        (package-var (gensym))
+        (remaining-body body)
+        (declarations '()))
+    (multiple-value-bind (declarations body-forms)
+        (cleavir-code-utilities:separate-ordinary-body body)
+      `(block nil
+         (flet ((,function-name (,symbol-variable)
+                  (locally ,@declarations
+                    (tagbody
+                       ,@body-forms))))
+           (let ((,package-var (package-designator-to-package ,package-form)))
+             (loop for symbol being each hash-value of (external-symbols ,package-var)
+                   do (,funcation-name symbol))))
+         ,result-form))))
