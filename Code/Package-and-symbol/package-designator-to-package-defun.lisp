09fd2c49007b45f417b443e93d070621f6be94a5
Robert Strandh <robert.strandh@gmail.com>
2020-06-19 17:16:36 +0200

    Untabify.

--- Code/Package-and-symbol/package-designator-to-package-defun.lisp
+++ Code/Package-and-symbol/package-designator-to-package-defun.lisp
@@ -2,8 +2,8 @@
 
 (defun package-designator-to-package (package-designator)
   (cond ((packagep package-designator)
-	 package-designator)
-	((typep package-designator 'string-designator)
-	 (find-package package-designator))
-	(t
-	 (error 'not-a-package-designator :datum package-designator))))
+         package-designator)
+        ((typep package-designator 'string-designator)
+         (find-package package-designator))
+        (t
+         (error 'not-a-package-designator :datum package-designator))))
