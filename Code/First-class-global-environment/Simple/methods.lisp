b18a65f5b093bbe9e53e1c9a8622478d13ba8ea1
Robert Strandh <robert.strandh@gmail.com>
2020-06-29 16:59:30 +0200

    Untabify.

--- Code/First-class-global-environment/Simple/methods.lisp
+++ Code/First-class-global-environment/Simple/methods.lisp
@@ -3,14 +3,14 @@
 ;;; Utility function for associating a name with a function.
 (defun add-function-name (function function-name env)
   (pushnew function-name
-	   (gethash function (function-names env))
-	   :test #'equal))
+           (gethash function (function-names env))
+           :test #'equal))
 
 ;;; Utility function for disassociating a name from a function.
 (defun remove-function-name (function function-name env)
   (setf (gethash function (function-names env))
-	(remove function-name (gethash function (function-names env))
-		:test #'equal))
+        (remove function-name (gethash function (function-names env))
+                :test #'equal))
   (when (null (gethash function (function-names env)))
     (remhash function (function-names env))))
 
@@ -20,17 +20,17 @@
 (defmethod sicl-genv:fboundp (function-name (env simple-environment))
   (let ((entry (find-function-entry env function-name)))
     (and (not (null entry))
-	 (or (not (eq (car (function-cell entry))
-		      (unbound entry)))
-	     (not (null (macro-function entry)))
-	     (not (null (special-operator entry)))))))
+         (or (not (eq (car (function-cell entry))
+                      (unbound entry)))
+             (not (null (macro-function entry)))
+             (not (null (special-operator entry)))))))
 
 (defmethod sicl-genv:fmakunbound (function-name (env simple-environment))
   (let ((entry (find-function-entry env function-name)))
     (unless (null entry)
       (remove-function-name (car (function-cell entry)) function-name env)
       (setf (car (function-cell entry))
-	    (unbound entry))
+            (unbound entry))
       (setf (macro-function entry) nil)
       (setf (special-operator entry) nil)
       (setf (type entry) t)
@@ -39,17 +39,17 @@
 (defmethod sicl-genv:special-operator (function-name (env simple-environment))
   (let ((entry (find-function-entry env function-name)))
     (if (null entry)
-	nil
-	(special-operator entry))))
+        nil
+        (special-operator entry))))
 
 (defmethod (setf sicl-genv:special-operator)
     (new-definition function-name (env simple-environment))
   (let ((entry (find-function-entry env function-name)))
     (cond ((and (null entry) (null new-definition))
-	   nil)
-	  ((null entry)
-	   (setf entry (ensure-function-entry env function-name))
-	   (setf (special-operator entry) new-definition))
+           nil)
+          ((null entry)
+           (setf entry (ensure-function-entry env function-name))
+           (setf (special-operator entry) new-definition))
           ((not (eq (car (function-cell entry))
                     (unbound entry)))
            (error 'redefinition :name function-name
@@ -57,22 +57,22 @@
           ((not (null (macro-function entry)))
            (error 'redefinition :name function-name
                                 :oldtype :macro :newtype :special-operator))
-	  (t
-	   (setf (special-operator entry) new-definition)))))
+          (t
+           (setf (special-operator entry) new-definition)))))
 
 (defmethod sicl-genv:fdefinition (function-name (env simple-environment))
   (let ((entry (find-function-entry env function-name)))
     (cond ((null entry)
-	   (error 'undefined-function :name function-name))
-	  ((not (eq (car (function-cell entry))
-		    (unbound entry)))
-	   (car (function-cell entry)))
-	  ((not (null (macro-function entry)))
-	   `(cl:macro-function ,(macro-function entry)))
-	  ((not (null (special-operator entry)))
-	   `(cl:special (special-operator entry)))
-	  (t
-	   (error 'undefined-function :name function-name)))))
+           (error 'undefined-function :name function-name))
+          ((not (eq (car (function-cell entry))
+                    (unbound entry)))
+           (car (function-cell entry)))
+          ((not (null (macro-function entry)))
+           `(cl:macro-function ,(macro-function entry)))
+          ((not (null (special-operator entry)))
+           `(cl:special (special-operator entry)))
+          (t
+           (error 'undefined-function :name function-name)))))
 
 (defmethod (setf sicl-genv:fdefinition)
     (new-definition function-name (env simple-environment))
@@ -93,8 +93,8 @@
 (defmethod sicl-genv:macro-function (symbol (env simple-environment))
   (let ((entry (find-function-entry env symbol)))
     (if (null entry)
-	nil
-	(macro-function entry))))
+        nil
+        (macro-function entry))))
 
 (defmethod (setf sicl-genv:macro-function)
     (new-definition function-name (env simple-environment))
@@ -110,60 +110,60 @@
 (defmethod sicl-genv:compiler-macro-function
     (function-name (env simple-environment))
   (let ((entry (assoc function-name (compiler-macro-expanders env)
-		      :test #'equal)))
+                      :test #'equal)))
     (if (null entry)
-	nil
-	(cdr entry))))
+        nil
+        (cdr entry))))
 
 (defmethod (setf sicl-genv:compiler-macro-function)
     (new-definition function-name (env simple-environment))
   (assert (or (functionp new-definition) (null new-definition)))
   (if (null new-definition)
       (setf (compiler-macro-expanders env)
-	    (remove function-name (compiler-macro-expanders env)
-		    :key #'car :test #'equal))
+            (remove function-name (compiler-macro-expanders env)
+                    :key #'car :test #'equal))
       (let ((entry (assoc function-name (compiler-macro-expanders env)
-			  :test #'equal)))
-	(if (null entry)
-	    (push (cons function-name new-definition)
-		  (compiler-macro-expanders env))
-	    (setf (cdr entry) new-definition))))
+                          :test #'equal)))
+        (if (null entry)
+            (push (cons function-name new-definition)
+                  (compiler-macro-expanders env))
+            (setf (cdr entry) new-definition))))
   new-definition)
 
 (defmethod sicl-genv:function-type (function-name (env simple-environment))
   (let ((entry (find-function-entry env function-name)))
     (if (or (null entry)
-	    (not (null (special-operator entry)))
-	    (not (null (macro-function entry))))
-	nil
-	(type entry))))
+            (not (null (special-operator entry)))
+            (not (null (macro-function entry))))
+        nil
+        (type entry))))
 
 (defmethod (setf sicl-genv:function-type)
     (new-type function-name (env simple-environment))
   (let ((entry (ensure-function-entry env function-name)))
     (cond ((not (null (special-operator entry)))
-	   (error 'attempt-to-proclaim-type-of-special-operator
-		  :name  function-name))
-	  ((not (null (macro-function entry)))
-	   (error 'attempt-to-proclaim-type-of-macro
-		  :name  function-name))
-	  (t
-	   (setf (type entry) new-type)))))
+           (error 'attempt-to-proclaim-type-of-special-operator
+                  :name  function-name))
+          ((not (null (macro-function entry)))
+           (error 'attempt-to-proclaim-type-of-macro
+                  :name  function-name))
+          (t
+           (setf (type entry) new-type)))))
 
 (defmethod sicl-genv:function-inline (function-name (env simple-environment))
   (let ((entry (ensure-function-entry env function-name)))
     (if (null entry)
-	(error 'undefined-function :name function-name)
-	(inline entry))))
+        (error 'undefined-function :name function-name)
+        (inline entry))))
 
 (defmethod (setf sicl-genv:function-inline)
     (new-inline function-name (env simple-environment))
   (assert (member new-inline '(nil cl:inline cl:notinline)))
   (let ((entry (find-function-entry env function-name)))
     (if (or (null entry)
-	    (eq (car (function-cell entry)) (unbound entry)))
-	(error 'undefined-function :name function-name)
-	(setf (inline entry) new-inline))))
+            (eq (car (function-cell entry)) (unbound entry)))
+        (error 'undefined-function :name function-name)
+        (setf (inline entry) new-inline))))
 
 (defmethod sicl-genv:function-cell (function-name (env simple-environment))
   (let ((entry (ensure-function-entry env function-name)))
@@ -192,8 +192,8 @@
 (defmethod sicl-genv:function-ast (function-name (env simple-environment))
   (let ((entry (find-function-entry env function-name)))
     (if (null entry)
-	nil
-	(ast entry))))
+        nil
+        (ast entry))))
 
 (defmethod (setf sicl-genv:function-ast)
     (new-ast function-name (env simple-environment))
@@ -210,8 +210,8 @@
 (defmethod sicl-genv:boundp (symbol (env simple-environment))
   (let ((entry (find-variable-entry env symbol)))
     (and (not (null entry))
-	 (or (not (null (macro-function entry)))
-	     (not (eq (car (value-cell entry)) (unbound env)))))))
+         (or (not (null (macro-function entry)))
+             (not (eq (car (value-cell entry)) (unbound env)))))))
 
 (defmethod sicl-genv:makunbound (symbol (env simple-environment))
   (let ((entry (find-variable-entry env symbol)))
@@ -226,8 +226,8 @@
 (defmethod sicl-genv:constant-variable (symbol (env simple-environment))
   (let ((entry (find-variable-entry env symbol)))
     (if (or (null entry) (not (constantp entry)))
-	(values nil nil)
-	(values (car (value-cell entry)) t))))
+        (values nil nil)
+        (values (car (value-cell entry)) t))))
 
 (defmethod (setf sicl-genv:constant-variable)
     (value symbol (env simple-environment))
@@ -240,23 +240,23 @@
            (error 'redefinition
                   :name symbol
                   :oldtype :special-variable :newtype :constant))
-	  ((and (constantp entry)
-		(not (eql (car (value-cell entry)) value)))
+          ((and (constantp entry)
+                (not (eql (car (value-cell entry)) value)))
            (error 'constant-redefinition
                   :name symbol
                   :old (car (value-cell entry)) :new value))
-	  (t
-	   (setf (constantp entry) t)
-	   (setf (car (value-cell entry)) value)))))
+          (t
+           (setf (constantp entry) t)
+           (setf (car (value-cell entry)) value)))))
 
 (defmethod sicl-genv:special-variable (symbol (env simple-environment))
   (let ((entry (find-variable-entry env symbol)))
     (values (if (or (null entry)
-		    (not (specialp entry))
-		    (eq (car (value-cell entry)) (unbound env)))
-		nil
-		(car (value-cell entry)))
-	    (and (not (null entry)) (specialp entry)))))
+                    (not (specialp entry))
+                    (eq (car (value-cell entry)) (unbound env)))
+                nil
+                (car (value-cell entry)))
+            (and (not (null entry)) (specialp entry)))))
 
 (defmethod (setf sicl-genv:special-variable)
     (value symbol (env simple-environment) initialize-p)
@@ -268,16 +268,16 @@
            (error 'redefinition
                   :name symbol
                   :oldtype :symbol-macro :newtype :special-variable))
-	  (t
-	   (setf (specialp entry) t)
-	   (when initialize-p
-	     (setf (car (value-cell entry)) value))))))
+          (t
+           (setf (specialp entry) t)
+           (when initialize-p
+             (setf (car (value-cell entry)) value))))))
 
 (defmethod sicl-genv:symbol-macro (symbol (env simple-environment))
   (let ((entry (find-variable-entry env symbol)))
     (if (or (null entry) (null (macro-function entry)))
-	(values nil nil)
-	(values (macro-function entry) (expansion entry)))))
+        (values nil nil)
+        (values (macro-function entry) (expansion entry)))))
 
 (defmethod (setf sicl-genv:symbol-macro)
     (expansion symbol (env simple-environment))
@@ -290,25 +290,25 @@
            (error 'redefinition
                   :name symbol
                   :oldtype :constant :newtype :symbol-macro))
-	  (t
-	   (setf (expansion entry) expansion)
-	   (setf (macro-function entry)
-		 (lambda (form environment)
-		   (declare (ignore form environment))
-		   expansion))))))
+          (t
+           (setf (expansion entry) expansion)
+           (setf (macro-function entry)
+                 (lambda (form environment)
+                   (declare (ignore form environment))
+                   expansion))))))
 
 (defmethod sicl-genv:variable-type (symbol (env simple-environment))
   (let ((entry (find-variable-entry env symbol)))
     (if (null entry)
-	t
-	(type entry))))
+        t
+        (type entry))))
 
 (defmethod (setf sicl-genv:variable-type)
     (new-type symbol (env simple-environment))
   (let ((entry (ensure-variable-entry env symbol)))
     (if (constantp entry)
         (error 'attempt-to-proclaim-type-of-constant :name symbol)
-	(setf (type entry) new-type))))
+        (setf (type entry) new-type))))
 
 (defmethod sicl-genv:variable-cell (symbol (env simple-environment))
   (let ((entry (ensure-variable-entry env symbol)))
@@ -333,8 +333,8 @@
     (new-expander symbol (env simple-environment))
   (let ((association (assoc symbol (setf-expanders env) :test #'eq)))
     (if (null association)
-	(push (cons symbol new-expander) (setf-expanders env))
-	(setf (cdr association) new-expander))))
+        (push (cons symbol new-expander) (setf-expanders env))
+        (setf (cdr association) new-expander))))
 
 (defmethod sicl-genv:default-setf-expander ((env simple-environment))
   (default-setf-expander env))
@@ -350,8 +350,8 @@
     (new-expander symbol (env simple-environment))
   (let ((association (assoc symbol (type-expanders env) :test #'eq)))
     (if (null association)
-	(push (cons symbol new-expander) (type-expanders env))
-	(setf (cdr association) new-expander))))
+        (push (cons symbol new-expander) (type-expanders env))
+        (setf (cdr association) new-expander))))
 
 (defmethod sicl-genv:find-package (name (env simple-environment))
   (values (gethash name (packages env))))
--- Code/First-class-global-environment/Simple/packages.lisp
+++ Code/First-class-global-environment/Simple/packages.lisp
@@ -3,8 +3,8 @@
 (defpackage #:sicl-simple-environment
   (:use #:common-lisp)
   (:shadow #:type
-	   #:inline #:notinline
-	   #:constantp
-	   #:macro-function #:compiler-macro-function
+           #:inline #:notinline
+           #:constantp
+           #:macro-function #:compiler-macro-function
            #:undefined-function)
   (:export #:simple-environment))
