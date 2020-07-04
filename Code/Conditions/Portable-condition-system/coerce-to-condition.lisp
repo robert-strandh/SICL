016c6692a763dab45e79eae7f84e7603fbba4632
Robert Strandh <robert.strandh@gmail.com>
2020-07-04 09:42:54 +0200

    Remove remaining documentation strings.

--- Code/Conditions/Portable-condition-system/coerce-to-condition.lisp
+++ Code/Conditions/Portable-condition-system/coerce-to-condition.lisp
@@ -19,26 +19,19 @@
   datum)
 
 (defmethod coerce-to-condition ((datum symbol) arguments default-type name)
-  "Calls MAKE-CONDITION on the provided symbol and arguments."
   (apply #'make-condition datum arguments))
 
 (defmethod coerce-to-condition ((datum string) arguments default-type name)
-  "Makes a SIMPLE-CONDITION of the provided DEFAULT-TYPE, using the DATUM string
-as its format control and ARGUMENTS as its format arguments."
   (make-condition default-type
                   :format-control datum
                   :format-arguments arguments))
 
 (defmethod coerce-to-condition ((datum function) arguments default-type name)
-  "Makes a SIMPLE-CONDITION of the provided DEFAULT-TYPE, using the DATUM
-function as its format control and ARGUMENTS as format arguments."
   (make-condition default-type
                   :format-control datum
                   :format-arguments arguments))
 
 (defmethod coerce-to-condition (datum arguments default-type name)
-  "Signals an error that the provided datum is not coercable to a condition
-object."
   (error 'simple-type-error
          :datum datum
          :expected-type '(or condition symbol function string)
