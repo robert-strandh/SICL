101e9a0a490bd5f20764770a26b38d903bc5f72e
Robert Strandh <robert.strandh@gmail.com>
2020-07-05 14:28:39 +0200

    New component containing definition of RESTART as a standard class.

---
+++ Code/Conditions/Portable-condition-system/restart-defclass.lisp
@@ -0,0 +1,31 @@
+(cl:in-package #:portable-condition-system)
+
+:;; Definition of RESTART as a standard class.
+
+(defclass restart ()
+  ((%name
+    :initarg :name
+    :initform (error "NAME required")
+    :reader restart-name)
+   (%function
+    :initarg :function
+    :initform (error "FUNCTION required")
+    :reader restart-function)
+   (%report-function
+    :initarg :report-function
+    :initform nil
+    :reader restart-report-function)
+   (%interactive-function
+    :initarg :interactive-function
+    :initform nil
+    :reader restart-interactive-function)
+   (%test-function
+    :initarg :test-function
+    :initform (constantly t)
+    :reader restart-test-function)
+   (%associated-conditions
+    :initform '()
+    :accessor restart-associated-conditions)))
+
+(defun make-restart (&rest arguments)
+  (apply #'make-instance 'restart arguments))
