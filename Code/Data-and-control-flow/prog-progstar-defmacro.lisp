b3aaccdfb9b8741a479e441c3d28871ad8a09b6e
Robert Strandh <robert.strandh@gmail.com>
2020-07-07 17:10:27 +0200

    Untabify.

--- Code/Data-and-control-flow/prog-progstar-defmacro.lisp
+++ Code/Data-and-control-flow/prog-progstar-defmacro.lisp
@@ -5,13 +5,13 @@
       (cleavir-code-utilities:separate-ordinary-body body)
     `(block nil
        (let ,bindings
-	 ,@declarations
-	 (tagbody ,@items)))))
+         ,@declarations
+         (tagbody ,@items)))))
 
 (defmacro prog* (bindings &body body)
   (multiple-value-bind (declarations items)
       (cleavir-code-utilities:separate-ordinary-body body)
     `(block nil
        (let* ,bindings
-	 ,@declarations
-	 (tagbody ,@items)))))
+         ,@declarations
+         (tagbody ,@items)))))
