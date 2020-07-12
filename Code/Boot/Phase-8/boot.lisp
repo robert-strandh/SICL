dc586be4ddf3a3cd21dd39c7fd394db734240736
Robert Strandh <robert.strandh@gmail.com>
2020-07-12 10:44:38 +0200

    Load files containing definition of macro DESTRUCTURING-BIND.

--- Code/Boot/Phase-8/boot.lisp
+++ Code/Boot/Phase-8/boot.lisp
@@ -31,4 +31,6 @@
     (load-source "Iteration/do-dostar-support.lisp" e5)
     (load-source "Iteration/do-dostar-defmacro.lisp" e5)
     (import-function-from-host 'cleavir-code-utilities:separate-ordinary-body e5)
+    (load-source "Data-and-control-flow/destructuring-bind-support.lisp" e5)
+    (load-source "Data-and-control-flow/destructuring-bind-defmacro.lisp" e5)
     (load-asdf-system-components '#:sicl-conditions e5)))
