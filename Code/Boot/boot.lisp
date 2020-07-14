ec7b6d4a9e30647ddfe12fc4e1390c650969633e
Robert Strandh <robert.strandh@gmail.com>
2020-07-14 10:06:09 +0200

    Replace call so MAKE-PACKAGE by a call to HOST-LOAD with the package definition file.

--- Code/Boot/boot.lisp
+++ Code/Boot/boot.lisp
@@ -23,8 +23,8 @@
      (asdf:system-relative-pathname '#:sicl-boot "ASTs/")
      :validate t
      :if-does-not-exist :ignore)
-    (when (null (find-package '#:sicl-array))
-      (make-package '#:sicl-array :use (list (find-package '#:common-lisp))))
+    ;; FIXME: move host-load from phase 0 to main directory.
+    (sicl-boot-phase-0::host-load "Array/packages.lisp")
     (sicl-boot-phase-0:boot boot)
     (sicl-boot-phase-1:boot boot)
     (sicl-boot-phase-2:boot boot)
