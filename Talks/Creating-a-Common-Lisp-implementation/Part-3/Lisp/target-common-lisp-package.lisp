750748050d51145741d2d1257c559ffa6817f81d
Robert Strandh <robert.strandh@gmail.com>
2020-07-24 14:44:47 +0200

    Import lambda-list keywords and special operators from host.

--- Talks/Creating-a-Common-Lisp-implementation/Part-3/Lisp/target-common-lisp-package.lisp
+++ Talks/Creating-a-Common-Lisp-implementation/Part-3/Lisp/target-common-lisp-package.lisp
@@ -2,6 +2,14 @@
 
 (defpackage #:target-common-lisp
   (:use)
+  (:import-from #:common-lisp
+                .
+                #.(let ((result '()))
+                    (do-external-symbols (symbol (find-package '#:common-lisp))
+                      (when (or (member symbol lambda-list-keywords)
+                                (special-operator-p symbol))
+                        (push (symbol-name symbol) result)))
+                    result))
   (:export
    .
    #.(let ((result '()))
