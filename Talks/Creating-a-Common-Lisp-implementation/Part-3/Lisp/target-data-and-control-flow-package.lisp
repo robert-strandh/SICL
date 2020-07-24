dd45338e64f544602352094a559abcaa16137ae0
Robert Strandh <robert.strandh@gmail.com>
2020-07-24 14:43:18 +0200

    New components containing defintions of packages DATA-ENAND-CONTROL-FLOW, and EVALUATION-AND-COMPILATION.

---
+++ Talks/Creating-a-Common-Lisp-implementation/Part-3/Lisp/target-data-and-control-flow-package.lisp
@@ -0,0 +1,4 @@
+(cl:in-package #:common-lisp-user)
+
+(defpackage #:target-data-and-control-flow
+  (:use #:target-common-lisp))
---
+++ Talks/Creating-a-Common-Lisp-implementation/Part-3/Lisp/target-evaluation-and-compilation-package.lisp
@@ -0,0 +1,5 @@
+(cl:in-package #:common-lisp-user)
+
+(defpackage #:target-evaluation-and-compilation
+  (:use #:target-common-lisp))
+               
