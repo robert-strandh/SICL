3116c65147d033faa9aa7155ebe75680d72735ed
Robert Strandh <robert.strandh@gmail.com>
2020-07-25 13:34:29 +0200

    Introduce restarts for returning to SICL top level and for quitting SICL.

--- Code/Boot/repl.lisp
+++ Code/Boot/repl.lisp
@@ -1,12 +1,18 @@
 (cl:in-package #:sicl-boot)
 
 (defun repl (e5)
-  (loop with client = (make-instance 'sicl-boot:client)
-        do (format t "SICL> ")
-           (finish-output *standard-output*)
-           (let* ((form (eclector.reader:read))
-                  (values (multiple-value-list (cleavir-cst-to-ast:eval client form e5))))
-             (loop for value in values
-                   do (print value))
-             (format t "~%")
-             (finish-output *standard-output*))))
+  (with-simple-restart (quit "Quit SICL")
+    (let ((client (make-instance 'sicl-boot:client)))
+      (loop (restart-case
+                (progn (format t "SICL> ")
+                       (finish-output *standard-output*)
+                       (let* ((form (eclector.reader:read))
+                              (values (multiple-value-list
+                                       (cleavir-cst-to-ast:eval client form e5))))
+                         (loop for value in values
+                               do (print value))
+                         (format t "~%")
+                         (finish-output *standard-output*)))
+              (abort ()
+                :report "Return to SICL top level"
+                nil))))))
