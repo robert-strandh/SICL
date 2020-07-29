552b8dbe312783de8e2c07bea908a1beac16ce32
Robert Strandh <robert.strandh@gmail.com>
2020-07-29 18:23:38 +0200

    Import function FINISH-OUTPUT from the host.

--- Code/Boot/Phase-8/boot.lisp
+++ Code/Boot/Phase-8/boot.lisp
@@ -46,5 +46,6 @@
     (load-source "Data-and-control-flow/constantly-defun.lisp" e5)
     (setf (sicl-genv:special-variable '*debug-io* e5 t)
           *debug-io*)
+    (import-function-from-host 'finish-output e5)
     (load-asdf-system-components '#:sicl-conditions e5)
     (load-source "CLOS/conditions.lisp" e5)))
