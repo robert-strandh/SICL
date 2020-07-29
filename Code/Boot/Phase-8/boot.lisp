149bb3724245087f61641088680c1e10e21d5553
Robert Strandh <robert.strandh@gmail.com>
2020-07-29 18:06:14 +0200

    Load a file containing definition of function CONSTANTLY.

--- Code/Boot/Phase-8/boot.lisp
+++ Code/Boot/Phase-8/boot.lisp
@@ -43,5 +43,6 @@
     (import-function-from-host 'trucler:macro-function e5)
     (load-source "Evaluation-and-compilation/macroexpand-1-defun.lisp" e5)
     (load-source "Evaluation-and-compilation/macroexpand-defun.lisp" e5)
+    (load-source "Data-and-control-flow/constantly-defun.lisp" e5)
     (load-asdf-system-components '#:sicl-conditions e5)
     (load-source "CLOS/conditions.lisp" e5)))
