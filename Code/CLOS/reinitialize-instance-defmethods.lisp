2859971054e27722bc8f29617afca99b667794d4
Robert Strandh <robert.strandh@gmail.com>
2020-08-02 11:23:22 +0200

    Add :AROUND method on REINITIALIZE-INSTANCE specialized to REAL-CLASS.

--- Code/CLOS/reinitialize-instance-defmethods.lisp
+++ Code/CLOS/reinitialize-instance-defmethods.lisp
@@ -3,3 +3,10 @@
 (defmethod reinitialize-instance
     ((instance standard-object) &rest initargs &key &allow-other-keys)
   (apply #'reinitialize-instance-default instance initargs))
+
+(defmethod reinitialize-instance :around
+    ((class real-class) &rest initargs &key &allow-other-keys)
+  (apply #'reinitialize-instance-around-real-class-default
+         #'call-next-method
+         class
+         initargs))
--- Code/CLOS/reinitialize-instance-support.lisp
+++ Code/CLOS/reinitialize-instance-support.lisp
@@ -6,3 +6,33 @@
   ;; i.e., only assign values to slots that have explicit
   ;; initialization arguments in initargs. 
   (apply #'shared-initialize instance () initargs))
+
+(defun reinitialize-instance-around-real-class-default
+    (call-next-method
+     class
+     &rest initargs
+     &key (direct-superclasses '() direct-superclasses-p)
+     &allow-other-keys)
+  (let ((existing-direct-superclasses (class-direct-superclasses class)))
+    (check-direct-superclasses class direct-superclasses)
+    (when direct-superclasses-p
+      (when (null direct-superclasses)
+        (setf direct-superclasses (default-superclasses class)))
+      (loop with difference = (set-difference existing-direct-superclasses
+                                              direct-superclasses)
+            for removed-superclass in difference
+            do (remove-direct-subclass removed-superclass class)))
+    (if direct-superclasses-p
+        (apply call-next-method
+               class
+               :direct-superclasses direct-superclasses
+               initargs)
+        (apply call-next-method
+               class
+               initargs))
+    (when direct-superclasses-p
+      (loop with difference = (set-difference direct-superclasses
+                                              existing-direct-superclasses)
+            for added-superclass in difference
+            do (add-direct-subclass added-superclass class))))
+  class)
