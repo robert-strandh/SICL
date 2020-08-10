1ce6ce820a9df63b12b2fe088a14a55d39853c11
Robert Strandh <robert.strandh@gmail.com>
2020-08-10 10:42:26 +0200

    Define instruction classes CHAR-CODE-INSTRUCTION and CODE-CHAR-INSTRUCTION.

--- Code/Cleavir2/Intermediate-representation/HIR/character-related-instructions.lisp
+++ Code/Cleavir2/Intermediate-representation/HIR/character-related-instructions.lisp
@@ -10,3 +10,25 @@
 
 (defclass characterp-instruction (instruction multiple-successors-mixin)
   ())
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;; Instruction CHAR-CODE-INSTRUCTION.
+;;;
+;;; This instruction is used to convert a character to its code.
+;;; It has one input which must be a character.  It has one output
+;;; which is a fixnum representing the code of that character.
+
+(defclass char-code-instruction (instruction one-successors-mixin)
+  ())
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;; Instruction CODE-CHAR-INSTRUCTION.
+;;;
+;;; This instruction is used to convert a character code to the
+;;; character with that code.  It has one input which must be a
+;;; fixnum.  It has one output which is a character.
+
+(defclass code-char-instruction (instruction one-successors-mixin)
+  ())
