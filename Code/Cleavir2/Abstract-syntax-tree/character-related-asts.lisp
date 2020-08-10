7f89f389b25f5fed1e093603749d4d0761e9bd1f
Robert Strandh <robert.strandh@gmail.com>
2020-08-10 10:43:05 +0200

    Define ast classes CHAR-CODE-AST and CODE-CHAR-AST.

--- Code/Cleavir2/Abstract-syntax-tree/character-related-asts.lisp
+++ Code/Cleavir2/Abstract-syntax-tree/character-related-asts.lisp
@@ -16,3 +16,33 @@
 
 (defmethod children ((ast characterp-ast))
   (list (object-ast ast)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;; Class CHAR-CODE-AST.
+;;;
+;;; This AST can be used to convert a character to its code.
+
+(defclass char-code-ast (ast one-value-ast-mixin side-effect-free-ast-mixin)
+  ((%char-ast :initarg :char-ast :reader char-ast)))
+
+(cleavir-io:define-save-info char-code-ast
+  (:char-ast char-ast))
+
+(defmethod children ((ast char-code-ast))
+  (list (char-ast ast)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;; Class CODE-CHAR-AST.
+;;;
+;;; This AST can be used to convert a character code to a character.
+
+(defclass code-char-ast (ast one-value-ast-mixin side-effect-free-ast-mixin)
+  ((%code-ast :initarg :code-ast :reader code-ast)))
+
+(cleavir-io:define-save-info code-char-ast
+  (:code-ast code-ast))
+
+(defmethod children ((ast code-char-ast))
+  (list (code-ast ast)))
