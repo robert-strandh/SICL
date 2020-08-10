f890b91cd2c70155be2814c222649bd48ba81711
Robert Strandh <robert.strandh@gmail.com>
2020-08-10 11:16:39 +0200

    Defin interpreter methods for CHAR-CODE and CODE-CHAR.

--- Code/Compiler/HIR-interpreter/character-related-instructions.lisp
+++ Code/Compiler/HIR-interpreter/character-related-instructions.lisp
@@ -9,3 +9,27 @@
     (if (characterp (input-value input lexical-environment))
         (first successors)
         (second successors))))
+
+(defmethod interpret-instruction
+    (client
+     (instruction cleavir-ir:char-code-instruction)
+     lexical-environment)
+  (let* ((input-value (input-value (first (cleavir-ir:inputs instruction))
+                                   lexical-environment))
+         (output (first (cleavir-ir:outputs instruction)))
+         (successor (first (cleavir-ir:successors instruction))))
+    (setf (lexical-value output lexical-environment)
+          (char-code input-value))
+    successor))
+
+(defmethod interpret-instruction
+    (client
+     (instruction cleavir-ir:code-char-instruction)
+     lexical-environment)
+  (let* ((input-value (input-value (first (cleavir-ir:inputs instruction))
+                                   lexical-environment))
+         (output (first (cleavir-ir:outputs instruction)))
+         (successor (first (cleavir-ir:successors instruction))))
+    (setf (lexical-value output lexical-environment)
+          (code-char input-value))
+    successor))
