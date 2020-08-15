cbb001abcaff78a4c268b612d950b52cf3806274
Henry Harrington <henry.harrington@gmail.com>
2020-08-15 19:35:44 +0100

    defstruct: Update the intrinsic structure package

--- Code/Structure/packages.lisp
+++ Code/Structure/packages.lisp
@@ -1,4 +1,14 @@
 (cl:in-package #:common-lisp-user)
 
 (defpackage #:sicl-structure
-  (:use #:common-lisp))
+  (:use #:common-lisp)
+  (:local-nicknames (#:mop #:sicl-clos))
+  (:export #:defstruct
+           #:copy-structure
+           #:structure-class
+           #:structure-object
+
+           #:structure-slot-definition
+           #:structure-slot-definition-read-only
+           #:structure-direct-slot-definition
+           #:structure-effective-slot-definition))
