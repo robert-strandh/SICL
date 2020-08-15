74a405b060369cf1fd7cf2b7718c9a5b555baba8
Henry Harrington <henry.harrington@gmail.com>
2020-08-15 15:25:42 +0100

    defstruct: Track if a structure has a standard constructor.
    This is required for reading the #S syntax

--- Code/Structure/structure-class-defclass-extrinsic.lisp
+++ Code/Structure/structure-class-defclass-extrinsic.lisp
@@ -4,4 +4,6 @@
 ;;; definition is that this definition inherits from standard-class, not
 ;;; sicl-clos:regular-class.
 (defclass structure-class (standard-class)
-  ())
+  ((%has-standard-constructor :initarg :has-standard-constructor
+                              :reader has-standard-constructor))
+  (:default-initargs :has-standard-constructor nil))
