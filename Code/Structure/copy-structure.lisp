10d04cf1bec202f2476f397efa6e6e7fc12fc47c
Henry Harrington <henry.harrington@gmail.com>
2020-08-15 13:52:07 +0100

    defstruct: Copy-structure shouldn't copy unbound slots.

--- Code/Structure/copy-structure.lisp
+++ Code/Structure/copy-structure.lisp
@@ -6,6 +6,7 @@
          (new-object (allocate-instance class)))
     (dolist (slot (closer-mop:class-slots class))
       ;; Use s-v-u-c here to avoid the extra slot lookup.
-      (setf (closer-mop:slot-value-using-class class new-object slot)
-            (closer-mop:slot-value-using-class class object slot)))
+      (when (closer-mop:slot-boundp-using-class class object slot)
+        (setf (closer-mop:slot-value-using-class class new-object slot)
+              (closer-mop:slot-value-using-class class object slot))))
     new-object))
