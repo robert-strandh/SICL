652567a6450b38244f9231b895b3018d7f16db19
Robert Strandh <robert.strandh@gmail.com>
2020-08-17 10:20:40 +0200

    Load files contining definitions of functions EVENP and ODDP.

--- Code/Boot/Phase-8/load-arithmetic-functions.lisp
+++ Code/Boot/Phase-8/load-arithmetic-functions.lisp
@@ -39,5 +39,7 @@
   (load-source "Arithmetic/ceiling-defun.lisp" e5)
   (load-source "Arithmetic/truncate-defun.lisp" e5)
   (load-source "Arithmetic/round-defun.lisp" e5)
+  (load-source "Arithmetic/evenp-defun.lisp" e5)
+  (load-source "Arithmetic/oddp-defun.lisp" e5)
   (load-source "Arithmetic/allocate-instance-defmethod-bignum.lisp" e5)
   (load-source "Arithmetic/initialize-instance-defmethod-after-bignum.lisp" e5))
