7779618fbdbd2901812ace4420601516ade8336f
Robert Strandh <robert.strandh@gmail.com>
2020-08-19 15:46:03 +0200

    Handle negative arguments.

--- Code/Arithmetic/binary-multiply-defmethods.lisp
+++ Code/Arithmetic/binary-multiply-defmethods.lisp
@@ -1,9 +1,30 @@
 (cl:in-package #:sicl-arithmetic)
 
 (defmethod binary-multiply ((multiplier fixnum) (multiplicand fixnum))
-  (multiple-value-bind (most-significant least-significant)
-      (cleavir-primop:fixnum-multiply multiplier multiplicand)
-    (if (zerop most-significant)
-        least-significant
-        ;; FIXME: create a bignum once we know how to do that.
-        (error 'create-a-bignum))))
+  (if (minusp multiplier)
+      (if (minusp multiplicand)
+          (multiple-value-bind (most-significant least-significant)
+              (cleavir-primop:fixnum-multiply (- multiplier) (- multiplicand))
+            (if (zerop most-significant)
+                least-significant
+                ;; FIXME: create a bignum once we know how to do that.
+                (error 'create-a-bignum)))
+          (multiple-value-bind (most-significant least-significant)
+              (cleavir-primop:fixnum-multiply (- multiplier) multiplicand)
+            (if (zerop most-significant)
+                (- least-significant)
+                ;; FIXME: create a bignum once we know how to do that.
+                (error 'create-a-bignum))))
+      (if (minusp multiplicand)
+          (multiple-value-bind (most-significant least-significant)
+              (cleavir-primop:fixnum-multiply multiplier (- multiplicand))
+            (if (zerop most-significant)
+                (- least-significant)
+                ;; FIXME: create a bignum once we know how to do that.
+                (error 'create-a-bignum)))
+          (multiple-value-bind (most-significant least-significant)
+              (cleavir-primop:fixnum-multiply multiplier multiplicand)
+            (if (zerop most-significant)
+                least-significant
+                ;; FIXME: create a bignum once we know how to do that.
+                (error 'create-a-bignum))))))
