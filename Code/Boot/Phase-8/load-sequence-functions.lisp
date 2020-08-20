3a26a767b97d1c60bb08f904fd2886498f5709bd
Robert Strandh <robert.strandh@gmail.com>
2020-08-20 17:22:11 +0200

    Load a file containing definitions of condition types for sequence functions.

--- Code/Boot/Phase-8/load-sequence-functions.lisp
+++ Code/Boot/Phase-8/load-sequence-functions.lisp
@@ -3,4 +3,5 @@
 (defun load-sequence-functions (e5)
   (load-source "Array/array-total-size-limit-defconstant.lisp" e5)
   (load-source "Sequence/types.lisp" e5)
+  (load-source "Sequence/conditions.lisp" e5)
   (load-source "Sequence/with-cons-iterator.lisp" e5))
