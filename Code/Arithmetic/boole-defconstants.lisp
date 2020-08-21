1e865c090ddd4212eba2ba9ef1c404ed3025bbb7
Robert Strandh <robert.strandh@gmail.com>
2020-08-21 17:11:37 +0200

    New component containing definitions of constant variables BOOLE-...

---
+++ Code/Arithmetic/boole-defconstants.lisp
@@ -0,0 +1,33 @@
+(cl:in-package #:sicl-arithmetic)
+
+(defconstant boole-1 0)
+
+(defconstant boole-2 1)
+
+(defconstant boole-and 2)
+
+(defconstant boole-andc1 3)
+
+(defconstant boole-andc2 4)
+
+(defconstant boole-c1 5)
+
+(defconstant boole-c2 6)
+
+(defconstant boole-clr 7)
+
+(defconstant boole-eqv 8)
+
+(defconstant boole-ior 9)
+
+(defconstant boole-nand 10)
+
+(defconstant boole-nor 11)
+
+(defconstant boole-orc1 12)
+
+(defconstant boole-orc2 13)
+
+(defconstant boole-set 14)
+
+(defconstant boole-xor 15)
