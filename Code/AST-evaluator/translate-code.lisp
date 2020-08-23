1a4ff49a8257f386be78bcbd258cb0c3192c25ef
Robert Strandh <robert.strandh@gmail.com>
2020-08-23 14:53:05 +0200

    Initialize the lexical environment before calling TRANSLATE-AST.

--- Code/AST-evaluator/translate-code.lisp
+++ Code/AST-evaluator/translate-code.lisp
@@ -1,10 +1,13 @@
 (cl:in-package #:sicl-ast-evaluator)
 
-(defclass client () ())
+(defclass client (trucler-reference:client)
+  ())
 
 (defun translate-code (code environment)
   (let* ((cst (cst:cst-from-expression code))
          (client (make-instance 'client))
          (ast (cleavir-cst-to-ast:cst-to-ast
-               client cst environment)))
-    (translate-ast ast environment '())))
+               client cst environment))
+         (table (make-hash-table :test #'eq))
+         (lexical-environment (list table)))
+    (translate-ast ast environment lexical-environment)))
