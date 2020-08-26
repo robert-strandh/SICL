944b7b98c44c5d9842660feaa4dd9a9255573e20
Robert Strandh <robert.strandh@gmail.com>
2020-08-26 15:44:04 +0200

    Define function SICL-ENVIRONMENT:GLOBAL-ENVIRONMENT in the run-time environment.

--- Code/AST-evaluator/fill-environment.lisp
+++ Code/AST-evaluator/fill-environment.lisp
@@ -139,6 +139,28 @@
              (format *trace-output* "Loading file ~a~%" relative-file-name)
              (load-file relative-file-name environment)))
       (host-load "Evaluation-and-compilation/packages.lisp")
+      (host-load "Data-and-control-flow/packages.lisp")
+      (host-load "Environment/environment-package.lisp")
+      ;; This function is used by macros in order to find the current
+      ;; global environment.  If no argument is given, the run-time
+      ;; environment (or startup environment) is returned.  If a macro
+      ;; supplies an argument, and then it will typically be the
+      ;; environment given to it by the &ENVIRONMENT parameter, then
+      ;; the compilation environment is returned.  Macros use this
+      ;; function to find information that is truly global, and that
+      ;; Trucler does not manage, such as compiler macros. type
+      ;; definitions, SETF expanders, etc.
+      (setf (env:fdefinition
+             client
+             environment
+             ;; There has got to be an easier way to define the
+             ;; package so that it exists before this system is
+             ;; compiled.
+             (intern (symbol-name '#:global-environment) '#:sicl-environment))
+            (lambda (&optional env)
+              (if (null env)
+                  environment
+                  (trucler:global-environment client env))))
       ;; Load a file containing a definition of the macro LAMBDA.
       ;; This macro is particularly simple, so it doesn't really
       ;; matter how it is expanded.  This is fortunate, because at the
