21c1c29145d2f3f06b4b387fc58cb3b32a9c6f50
Robert Strandh <robert.strandh@gmail.com>
2020-08-27 16:58:19 +0200

    Untabify.

--- Code/Data-and-control-flow-Clostrum/setf-defmacro.lisp
+++ Code/Data-and-control-flow-Clostrum/setf-defmacro.lisp
@@ -2,23 +2,23 @@
 
 (defmacro setf (&whole form &environment env place new-value-form &rest more-pairs)
   (cond ((null more-pairs)
-	 (multiple-value-bind (variables
-			       values
-			       store-variables
-			       writer-form
-			       reader-form)
-	     (get-setf-expansion place env)
-	   (declare (ignore reader-form))
-	   `(let* ,(mapcar #'list variables values)
-	      ;; Optimize a bit when there is only one store variable.
-	      ,(if (= 1 (length store-variables))
-		   `(let ((,(first store-variables) ,new-value-form))
-		      ,writer-form)
-		   `(multiple-value-bind ,store-variables
-			,new-value-form
-		      ,writer-form)))))
-	((not (null (cdr more-pairs)))
-	 `(progn (setf ,place ,new-value-form)
-		 (setf ,@more-pairs)))
-	(t
-	 (error 'odd-number-of-arguments-to-setf :form form))))
+         (multiple-value-bind (variables
+                               values
+                               store-variables
+                               writer-form
+                               reader-form)
+             (get-setf-expansion place env)
+           (declare (ignore reader-form))
+           `(let* ,(mapcar #'list variables values)
+              ;; Optimize a bit when there is only one store variable.
+              ,(if (= 1 (length store-variables))
+                   `(let ((,(first store-variables) ,new-value-form))
+                      ,writer-form)
+                   `(multiple-value-bind ,store-variables
+                        ,new-value-form
+                      ,writer-form)))))
+        ((not (null (cdr more-pairs)))
+         `(progn (setf ,place ,new-value-form)
+                 (setf ,@more-pairs)))
+        (t
+         (error 'odd-number-of-arguments-to-setf :form form))))
