c142c97f9b8012257d639467d98fbce7cfb1b3b1
Robert Strandh <robert.strandh@gmail.com>
2020-08-27 09:31:45 +0200

    Make package-local nickname ENV refer to package SICL-ENVIRONMENT rather than CLOSTRUM.

--- Code/AST-evaluator/packages.lisp
+++ Code/AST-evaluator/packages.lisp
@@ -3,6 +3,7 @@
 (defpackage #:sicl-ast-evaluator
   (:use #:common-lisp)
   (:shadow #:symbol-value #:eval)
-  (:local-nicknames (#:env #:clostrum)
+  (:local-nicknames (#:env #:sicl-environment)
+                    (#:client #:sicl-client)
                     (#:ast #:cleavir-ast))
   (:export))
