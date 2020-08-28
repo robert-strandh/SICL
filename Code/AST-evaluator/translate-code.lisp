0f578099fef7e4925f2022b32a0fb008ce683369
Robert Strandh <robert.strandh@gmail.com>
2020-08-28 18:20:18 +0200

    Turn errors for unknown functions and variables into warnings.

--- Code/AST-evaluator/translate-code.lisp
+++ Code/AST-evaluator/translate-code.lisp
@@ -1,9 +1,25 @@
 (cl:in-package #:sicl-ast-evaluator)
 
+(defun cst-to-ast (client cst environment)
+   (handler-bind
+       ((trucler:no-function-description
+          (lambda (condition)
+            (warn "Unknown function ~s" (trucler:name condition))
+            (invoke-restart 'cleavir-cst-to-ast:consider-global)))
+        (trucler:no-variable-description
+          (lambda (condition)
+            (warn "Unknown variable ~s" (trucler:name condition))
+            (invoke-restart 'cleavir-cst-to-ast:consider-special)))
+        (cleavir-cst-to-ast::encapsulated-condition
+          (lambda (condition)
+            (declare (ignore condition))
+            (invoke-restart 'cleavir-cst-to-ast:signal-original-condition))))
+     (cleavir-cst-to-ast:cst-to-ast
+      client cst environment)))
+
 (defun translate-code (cst environment)
   (let* ((client (env:client environment))
-         (ast (cleavir-cst-to-ast:cst-to-ast
-               client cst environment))
+         (ast (cst-to-ast client cst environment))
          (table (make-hash-table :test #'eq))
          (lexical-environment (list table)))
     (let ((*run-time-environment-name* (gensym)))
