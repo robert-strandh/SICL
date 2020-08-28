d6d1743f90c26f15e9c7fb190423d94d50427186
Robert Strandh <robert.strandh@gmail.com>
2020-08-28 17:17:50 +0200

    Do not call SETF, since it is not defined yet.

--- Code/Data-and-control-flow-Clostrum/defun-support.lisp
+++ Code/Data-and-control-flow-Clostrum/defun-support.lisp
@@ -11,18 +11,20 @@
          (eval-when (:compile-toplevel)
            (let* ((,env-var (sicl-environment:global-environment ,environment))
                   (,client-var (sicl-environment:client ,env-var)))
-             (setf (sicl-environment:function-description ,client-var ,env-var ',name)
-                   (make-instance 'sicl-environment:simple-function-description
-                     :lambda list ',lambda-list))))
+             (funcall #'(setf sicl-environment:function-description)
+                      (make-instance 'sicl-environment:simple-function-description
+                        :lambda list ',lambda-list)
+                      ,client-var ,env-var ',name)))
          (eval-when (:load-toplevel :execute)
            (let* ((,env-var (sicl-environment:global-environment))
                   (,client-var (sicl-environment:client ,env-var)))
-             (setf (sicl-environment:fdefinition ,client-var ,env-var ',name)
-                   (lambda ,lambda-list
-                     ,@declarations
-                     ,@(if (null documentation)
-                           '()
-                           (list documentation))
-                     (block ,(if (symbolp name) name (second name))
-                       ,@forms))))
+             (funcall #'(setf sicl-environment:fdefinition)
+                      (lambda ,lambda-list
+                        ,@declarations
+                        ,@(if (null documentation)
+                              '()
+                              (list documentation))
+                        (block ,(if (symbolp name) name (second name))
+                          ,@forms))
+                      ,client-var ,env-var ',name))
            ',name)))))
