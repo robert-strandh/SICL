9e395507a06d110d4c35f4122b80da1c71188066
Robert Strandh <robert.strandh@gmail.com>
2020-08-30 10:37:37 +0200

    Use the SETF macro to add function information and to define the function at load time.

--- Code/Data-and-control-flow-Clostrum/defun-support.lisp
+++ Code/Data-and-control-flow-Clostrum/defun-support.lisp
@@ -11,20 +11,20 @@
          (eval-when (:compile-toplevel)
            (let* ((,env-var (sicl-environment:global-environment ,environment))
                   (,client-var (sicl-environment:client ,env-var)))
-             (funcall #'(setf sicl-environment:function-description)
-                      (make-instance 'sicl-environment:simple-function-description
-                        :lambda list ',lambda-list)
-                      ,client-var ,env-var ',name)))
+             (setf (sicl-environment:function-description
+                    ,client-var ,env-var ',name)
+                   (make-instance 'sicl-environment:simple-function-description
+                     :lambda list ',lambda-list))))
          (eval-when (:load-toplevel :execute)
            (let* ((,env-var (sicl-environment:global-environment))
                   (,client-var (sicl-environment:client ,env-var)))
-             (funcall #'(setf sicl-environment:fdefinition)
-                      (lambda ,lambda-list
-                        ,@declarations
-                        ,@(if (null documentation)
-                              '()
-                              (list documentation))
-                        (block ,(if (symbolp name) name (second name))
-                          ,@forms))
-                      ,client-var ,env-var ',name))
+             (setf (sicl-environment:fdefinition
+                    ,client-var ,env-var ',name)
+                   (lambda ,lambda-list
+                     ,@declarations
+                     ,@(if (null documentation)
+                           '()
+                           (list documentation))
+                     (block ,(if (symbolp name) name (second name))
+                       ,@forms))))
            ',name)))))
