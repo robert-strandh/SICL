b42ac6e31638c2c3cf8c247e20011e6525c132ea
Robert Strandh <robert.strandh@gmail.com>
2020-09-02 09:33:27 +0200

    Export names of utility functions for RESTART-CASE.

--- Code/Conditions/packages.lisp
+++ Code/Conditions/packages.lisp
@@ -7,4 +7,9 @@
    #:make-handler-case-without-no-error-case
    #:make-handler-case-with-no-error-case
    #:restart-bind-transform-binding
+   #:restart-case-make-restart-binding
+   #:restart-case-make-restart-case
+   #:restart-case-signaling-form-p
+   #:restart-case-expand-signaling-form
+   #:restart-case-parse-case
    #:condition-class))
