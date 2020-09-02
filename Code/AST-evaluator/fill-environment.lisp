7b004d472cd111f990eb9a84e7c7499f3ca1690d
Robert Strandh <robert.strandh@gmail.com>
2020-09-02 08:40:25 +0200

    Load a file containing definition of macro CHECK-TYPE.

--- Code/AST-evaluator/fill-environment.lisp
+++ Code/AST-evaluator/fill-environment.lisp
@@ -364,4 +364,5 @@
       (import-function
        client environment 'sicl-conditions:define-condition-expander)
       (ld "Conditions/define-condition-defmacro.lisp")
-      (ld "Conditions/assert-defmacro.lisp"))))
+      (ld "Conditions/assert-defmacro.lisp")
+      (ld "Conditions/check-type-defmacro.lisp"))))
