de2f303e18a65029d63dd1b17ad113cc6decd622
Robert Strandh <robert.strandh@gmail.com>
2020-09-04 16:52:55 +0200

    Use variable *DYNAMIC-ENVIRONMENT* from module SICL-RUN-TIME.

--- Code/AST-evaluator/run-time.lisp
+++ Code/AST-evaluator/run-time.lisp
@@ -1,13 +1,11 @@
 (cl:in-package #:sicl-ast-evaluator)
 
-(defparameter *dynamic-environment* '())
-
 (defclass special-variable-entry ()
   ((%name :initarg :name :reader name)
    (%value :initarg :value :accessor value)))
 
 (defun symbol-value (name global-environment)
-  (loop for entry in *dynamic-environment*
+  (loop for entry in sicl-run-time:*dynamic-environment*
         when (and (typep entry 'special-variable-entry)
                   (eq name (name entry)))
           return (value entry)
@@ -16,7 +14,7 @@
                          (env:client global-environment) global-environment name))))
 
 (defun (setf symbol-value) (value name global-environment)
-  (loop for entry in *dynamic-environment*
+  (loop for entry in sicl-run-time:*dynamic-environment*
         when (and (typep entry 'special-variable-entry)
                   (eq name (name entry)))
           do (setf (value entry) value)
@@ -27,11 +25,11 @@
                 (return value)))
 
 (defmacro with-variable-bound ((name-form value-form) &body body)
-  `(let ((*dynamic-environment*
+  `(let ((sicl-run-time:*dynamic-environment*
            (cons (make-instance 'special-variable-entry
                    :name ,name-form
                    :value ,value-form)
-                 *dynamic-environment*)))
+                 sicl-run-time:*dynamic-environment*)))
      ,@body))
 
 (defclass exit-point-entry ()
@@ -39,31 +37,31 @@
    (%validp :initform t :accessor validp)))
 
 (defmacro with-exit-point ((name) &body body)
-  `(let ((*dynamic-environment*
+  `(let ((sicl-run-time:*dynamic-environment*
            (cons (make-instance 'exit-point-entry
                    :name ',name)
-                 *dynamic-environment*)))
+                 sicl-run-time:*dynamic-environment*)))
      ,@body))
 
 (defclass unwind-protect-entry ()
   ((%thunk :initarg :thunk :reader thunk)))
 
 (defmacro with-unwind-protect ((thunk-form) &body body)
-  `(let ((*dynamic-environment*
+  `(let ((sicl-run-time:*dynamic-environment*
            (cons (make-instance unwind-protect-entry
                    :thunk ,thunk-form)
-                 *dynamic-environment*)))
+                 sicl-run-time:*dynamic-environment*)))
      ,@body))
 
 (defun unwind (name)
-  (let ((exit-point-entry (loop for entry in *dynamic-environment*
+  (let ((exit-point-entry (loop for entry in sicl-run-time:*dynamic-environment*
                                 when (and (typep entry 'exit-point-entry)
                                           (eq (name entry) name))
                                   return entry)))
     (when (null exit-point-entry)
       (error 'attempt-to-exit-to-an-invalid-exit-point))
-    (loop until (eq exit-point-entry (first *dynamic-environment*))
-          do (let ((entry (pop *dynamic-environment*)))
+    (loop until (eq exit-point-entry (first sicl-run-time:*dynamic-environment*))
+          do (let ((entry (pop sicl-run-time:*dynamic-environment*)))
                (when (typep entry 'unwind-protect-entry)
                  (funcall (thunk entry)))))))
 
