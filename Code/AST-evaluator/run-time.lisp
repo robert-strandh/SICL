cfdf567a423ae7eda14e4046330eea2731f31bd6
Robert Strandh <robert.strandh@gmail.com>
2020-09-04 17:09:29 +0200

    Use class BLOCK/TAGBODY-ENTRY from module SICL-RUN-TIME.

--- Code/AST-evaluator/run-time.lisp
+++ Code/AST-evaluator/run-time.lisp
@@ -28,14 +28,12 @@
                  sicl-run-time:*dynamic-environment*)))
      ,@body))
 
-(defclass exit-point-entry ()
-  ((%name :initarg :name :reader name)
-   (%validp :initform t :accessor validp)))
-
 (defmacro with-exit-point ((name) &body body)
   `(let ((sicl-run-time:*dynamic-environment*
-           (cons (make-instance 'exit-point-entry
-                   :name ',name)
+           (cons (make-instance 'sicl-run-time:block/tagbody-entry
+                   :stack-pointer nil
+                   :frame-pointer nil
+                   :identifier ',name)
                  sicl-run-time:*dynamic-environment*)))
      ,@body))
 
@@ -47,13 +45,15 @@
      ,@body))
 
 (defun unwind (name)
-  (let ((exit-point-entry (loop for entry in sicl-run-time:*dynamic-environment*
-                                when (and (typep entry 'exit-point-entry)
-                                          (eq (name entry) name))
-                                  return entry)))
-    (when (null exit-point-entry)
+  (let ((sicl-run-time:block/tagbody-entry
+          (loop for entry in sicl-run-time:*dynamic-environment*
+                when (and (typep entry 'sicl-run-time:block/tagbody-entry)
+                          (eq (name entry) name))
+                  return entry)))
+    (when (null sicl-run-time:block/tagbody-entry)
       (error 'attempt-to-exit-to-an-invalid-exit-point))
-    (loop until (eq exit-point-entry (first sicl-run-time:*dynamic-environment*))
+    (loop until (eq sicl-run-time:block/tagbody-entry
+                    (first sicl-run-time:*dynamic-environment*))
           do (let ((entry (pop sicl-run-time:*dynamic-environment*)))
                (when (typep entry 'sicl-run-time:unwind-protect-entry)
                  (funcall (thunk entry)))))))
