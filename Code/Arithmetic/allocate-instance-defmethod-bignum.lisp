317d6d27e6b3d43e3724ad2590633615830bcfc4
Robert Strandh <robert.strandh@gmail.com>
2020-09-17 10:46:48 +0200

    Remove obsolete file containing definition of method on ALLOCATE-INSTANCE for bignum.

--- Code/Arithmetic/allocate-instance-defmethod-bignum.lisp
+++
@@ -1,4 +0,0 @@
-(cl:in-package #:sicl-arithmetic)
-
-(defmethod allocate-instance ((class (eql (find-class 'bignum))) &key limb-count)
-  (sicl-clos:allocate-instance-common class (+ 2 limb-count)))
