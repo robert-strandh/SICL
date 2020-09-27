99aee07fc70a3049d29f0904c005325873a272ef
Robert Strandh <robert.strandh@gmail.com>
2020-09-27 15:24:20 +0200

    Call GET-SETF-EXPANSION rather than the equivalent environment function.

--- Code/Data-and-control-flow-Clostrum/setf-defmacro.lisp
+++ Code/Data-and-control-flow-Clostrum/setf-defmacro.lisp
@@ -2,23 +2,21 @@
 
 (defmacro setf (&whole form &environment env place new-value-form &rest more-pairs)
   (cond ((null more-pairs)
-         (let* ((global-env (sicl-environment:global-environment env))
-                (client (sicl-environment:client global-env)))
-           (multiple-value-bind (variables
-                                 values
-                                 store-variables
-                                 writer-form
-                                 reader-form)
-               (sicl-environment:get-setf-expansion client env place)
-             (declare (ignore reader-form))
-             `(let* ,(mapcar #'list variables values)
-                ;; Optimize a bit when there is only one store variable.
-                ,(if (= 1 (length store-variables))
-                     `(let ((,(first store-variables) ,new-value-form))
-                        ,writer-form)
-                     `(multiple-value-bind ,store-variables
-                          ,new-value-form
-                        ,writer-form))))))
+         (multiple-value-bind (variables
+                               values
+                               store-variables
+                               writer-form
+                               reader-form)
+             (get-setf-expansion place env)
+           (declare (ignore reader-form))
+           `(let* ,(mapcar #'list variables values)
+              ;; Optimize a bit when there is only one store variable.
+              ,(if (= 1 (length store-variables))
+                   `(let ((,(first store-variables) ,new-value-form))
+                      ,writer-form)
+                   `(multiple-value-bind ,store-variables
+                        ,new-value-form
+                      ,writer-form)))))
         ((not (null (cdr more-pairs)))
          `(progn (setf ,place ,new-value-form)
                  (setf ,@more-pairs)))
