f610b051166f2df6be61f43d73b61c7cc87bcae7
Robert Strandh <robert.strandh@gmail.com>
2020-10-03 17:58:12 +0200

    Remove copyright notice.

--- Code/Loop/clause.lisp
+++ Code/Loop/clause.lisp
@@ -1,15 +1,3 @@
-;;;; Copyright (c) 2008, 2009, 2010, 2011, 2012, 2013, 2014
-;;;;
-;;;;     Robert Strandh (robert.strandh@gmail.com)
-;;;;
-;;;; all rights reserved. 
-;;;;
-;;;; Permission is hereby granted to use this software for any 
-;;;; purpose, including using, modifying, and redistributing it.
-;;;;
-;;;; The software is provided "as-is" with no warranty.  The user of
-;;;; this software assumes any responsibility of the consequences. 
-
 (cl:in-package #:sicl-loop)
 
 ;;;; The terminology used here is that of the BNF grammar in the
