9e05780aebcca851703a66bf4b770ebccf82ee5f
Robert Strandh <robert.strandh@gmail.com>
2020-10-10 06:32:18 +0200

    Remove copyright notice.

--- Code/Loop/termination-test-clause.lisp
+++ Code/Loop/termination-test-clause.lisp
@@ -1,15 +1,3 @@
-;;;; Copyright (c) 2015
-;;;;
-;;;;     Robert Strandh (robert.strandh@gmail.com)
-;;;;
-;;;; all rights reserved. 
-;;;;
-;;;; Permission is hereby granted to use this software for any 
-;;;; purpose, including using, modifying, and redistributing it.
-;;;;
-;;;; The software is provided "as-is" with no warranty.  The user of
-;;;; this software assumes any responsibility of the consequences. 
-
 (cl:in-package #:sicl-loop)
 
 ;;; We define a class that is the root class of all termination-test
