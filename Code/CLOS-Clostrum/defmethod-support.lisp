fcdc7b44be0bebbc7e86c37ca01b2bf15ceddd61
Robert Strandh <robert.strandh@gmail.com>
2020-10-11 15:03:36 +0200

    Remove expander code, now directly in the DEFMETHOD body.

--- Code/CLOS-Clostrum/defmethod-support.lisp
+++ Code/CLOS-Clostrum/defmethod-support.lisp
@@ -1,30 +1,5 @@
 (cl:in-package #:sicl-clos)
 
-;;; At the moment, this macro does not have any compile-time side
-;;; effects.
-
-;;; CT-ENV is the compile-time environment in which the call to
-;;; DEFMETHOD is compiled.  We check the compile-time environment to
-;;; see whether there is a generic function named FUNCTION-NAME in it.
-;;; If there is such a generic function, it is used as the first
-;;; argument to MAKE-METHOD-LAMBDA, and the CLASS-PROTOTYPE of the
-;;; GENERIC-FUNCTION-METHOD-CLASS of that function is used as the
-;;; second argument to MAKE-METHOD-LAMBDA.  If no such generic
-;;; function is found, we use the CLASS-PROTOTYPE of the class named
-;;; STANDARD-GENERIC-FUNCTION in CT-ENV as the first argument to
-;;; MAKE-METHOD-LAMBDA, and we use the CLASS-PROTOTYPE of the class
-;;; named STANDARD-METHOD in CT-ENV as the second argument to
-;;; MAKE-METHOD-LAMBDA.
-;;;
-;;; The expansion of this macro calls the SICL-specific function
-;;; ENSURE-METHOD.  The first argument to ENSURE-METHOD is the generic
-;;; function named FUNCTION-NAME in the run-time environment (and not
-;;; in the compile-time environment) in which the expansion is
-;;; evaluated.  The second argument to ENSURE-METHOD is the run-time
-;;; environment itself.  The function ENSURE-METHOD uses the
-;;; environment argument to take symbols naming specializers and to
-;;; look up the corresponding classes.
-
 (defun parse-defmethod (all-but-name)
   (let* ((lambda-list-position (position-if #'listp all-but-name))
          (qualifiers (subseq all-but-name 0 lambda-list-position))
@@ -45,47 +20,13 @@
 
 (defun canonicalize-specializer (specializer)
   (cond ((symbolp specializer)
-         `',specializer)
+         `(find-class ',specializer))
         ((and (consp specializer)
               (consp (cdr specializer))
               (null (cddr specializer)))
-         `(make-instance 'eql-specializer :object ,(cadr specializer)))
+         `(make-instance 'eql-specializer :object ,(second specializer)))
         (t
          (error 'malformed-specializer :specializer specializer))))
 
 (defun canonicalize-specializers (specializers)
-  `(list ,@(mapcar #'canonicalize-specializer specializers)))
-
-;;; FIXME: What we should really do here is to call
-;;; TRUCLER:DESCRIBE-FUNCTION to determine the GENERIC-FUNCTION-CLASS
-;;; and the METHOD-CLASS, so that we can call MAKE-METHOD-LAMBDA with
-;;; the correct arguments.  However, Trucler does not provide this
-;;; information (yet).  Perhaps we need to create a subclass of the
-;;; class TRUCLER:GENERIC-FUNCTION-DESCRIPTION that contains all the
-;;; information.
-
-(defun defmethod-expander (ct-env function-name rest)
-  (multiple-value-bind
-        (qualifiers required remaining specializers declarations documentation forms)
-      (parse-defmethod rest)
-    (let* ((lambda-list (append required remaining))
-           (generic-function-var (gensym)))
-      `(let* ((rt-env (sicl-environment:global-environment))
-              (,generic-function-var
-                (ensure-generic-function ',function-name :environment rt-env)))
-         (ensure-method
-          ,generic-function-var
-          :lambda-list ',lambda-list
-          :qualifiers ',qualifiers
-          :specializers ,(canonicalize-specializers specializers)
-          :documentation ,documentation
-          :function
-          ,(create-method-lambda
-            function-name
-            `(lambda ,lambda-list
-               ,@declarations
-               (block ,(if (consp function-name)
-                           (second function-name)
-                           function-name)
-                 ,@forms))
-            ct-env))))))
+  (mapcar #'canonicalize-specializer specializers))
