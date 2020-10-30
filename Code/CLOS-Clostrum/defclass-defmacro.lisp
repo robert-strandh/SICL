e73f19f5c743f0d865693177fba7057aafd82f4b
Robert Strandh <robert.strandh@gmail.com>
2020-10-30 18:06:58 +0100

    Remove obsolete components.

--- Code/CLOS-Clostrum/defclass-defmacro.lisp
+++
@@ -1,8 +0,0 @@
-(cl:in-package #:sicl-clos)
-
-(defmacro defclass (&environment environment
-                      name
-                      superclass-names
-                      slot-specifiers
-                    &rest options)
-  (defclass-expander name superclass-names slot-specifiers options environment))
--- Code/CLOS-Clostrum/defgeneric-defmacro.lisp
+++
@@ -1,6 +0,0 @@
-(cl:in-package #:sicl-clos)
-
-(defmacro defgeneric (&environment environment
-                        name lambda-list
-                      &rest options-and-methods)
-  (defgeneric-expander name lambda-list options-and-methods environment))
