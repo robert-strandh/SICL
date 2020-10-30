509aa72e2bea529b6392621b8fa9dca96d96669f
Robert Strandh <robert.strandh@gmail.com>
2020-10-30 10:51:51 +0100

    Remove incorrect IGNORE declarations.

--- Code/CLOS/class-readers-forward-referenced-class-defmethods.lisp
+++ Code/CLOS/class-readers-forward-referenced-class-defmethods.lisp
@@ -1,15 +1,12 @@
 (cl:in-package #:sicl-clos)
 
 (defmethod class-default-initargs ((class forward-referenced-class))
-  (declare (ignore class))
   (error 'attempt-to-access-default-initargs-of-forward-referenced-class
          :offending-class class))
 
 (defmethod class-precedence-list ((class forward-referenced-class))
-  (declare (ignore class))
   (error 'attempt-to-access-precedence-list-of-forward-referenced-class
          :offending-class class))
 
 (defmethod class-slots ((class forward-referenced-class))
-  (declare (ignore class))
   (error 'attempt-to-access-effective-slots-of-forward-referenced-class))
