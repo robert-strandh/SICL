124c780fb2d518d4889acfe140cc4070dc52d247
Robert Strandh <robert.strandh@gmail.com>
2020-10-30 10:53:46 +0100

    Call indirect function for accessing the function cell.

--- Code/Compiler/HIR-evaluator-Clostrum/cst-eval.lisp
+++ Code/Compiler/HIR-evaluator-Clostrum/cst-eval.lisp
@@ -4,12 +4,15 @@
 
 (defmethod cst-eval (client cst environment)
   (let* ((global-environment (trucler:global-environment client environment))
-         (client (env:client global-environment))
+         (client (sicl-environment:client global-environment))
          (ast (let ((cleavir-cst-to-ast::*origin* nil))
                 (cleavir-cst-to-ast:cst-to-ast client cst global-environment)))
          (hir (sicl-ast-to-hir:ast-to-hir client ast))
          (fun (top-level-hir-to-host-function client hir))
-         (sicl-run-time:*dynamic-environment* '()))
+         (sicl-run-time:*dynamic-environment* '())
+         (function-cell-function
+           (sicl-environment:fdefinition
+            client global-environment 'sicl-data-and-control-flow:function-cell)))
     (funcall fun
              (apply #'vector
                     nil ; Ultimately, replace with code object.
@@ -19,5 +22,5 @@
                     nil
                     (append (loop with names = (sicl-hir-transformations:function-names hir)
                                   for name in names
-                                  collect (env:function-cell client global-environment name))
+                                  collect (funcall function-cell-function name))
                             (sicl-hir-transformations:constants hir))))))
