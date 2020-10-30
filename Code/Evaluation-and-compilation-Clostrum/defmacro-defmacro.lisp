9c926fc031261f3a5aa016b02dbf8be1ae4b22bb
Robert Strandh <robert.strandh@gmail.com>
2020-10-30 10:57:29 +0100

    Simplify code for macro DEFMACRO.

--- Code/Evaluation-and-compilation-Clostrum/defmacro-defmacro.lisp
+++ Code/Evaluation-and-compilation-Clostrum/defmacro-defmacro.lisp
@@ -1,19 +1,9 @@
 (cl:in-package #:sicl-evaluation-and-compilation)
 
-(defmacro defmacro (&environment environment name lambda-list &body body)
-  (let ((compilation-environment
-          (sicl-environment:global-environment environment))
+(defmacro defmacro (name lambda-list &body body)
+  (let (;; (compilation-environment
+        ;;  (sicl-environment:global-environment environment))
         (expansion
           (cleavir-code-utilities:parse-macro name lambda-list body)))
-    `(progn (eval-when (:compile-toplevel)
-              (setf (sicl-environment:macro-function
-                     (sicl-environment:client ,compilation-environment)
-                     ,compilation-environment
-                     ',name)
-                    ,expansion))
-            (eval-when (:load-toplevel :execute)
-              (setf (sicl-environment:macro-function
-                     (sicl-environment:client (sicl-environment:global-environment))
-                     (sicl-environment:global-environment)
-                     ',name)
-                    ,expansion)))))
+    `(eval-when (:compile-toplevel :load-toplevel :execute)
+       (setf (macro-function ',name) ,expansion))))
