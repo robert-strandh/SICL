7cdeddfcca679c5f9814013ca500ff0d2c233357
Robert Strandh <robert.strandh@gmail.com>
2020-10-30 15:28:02 +0100

    Replace obsolete function MAKE-INSTANCE-TEMP by MAKE-INSTANCE.

--- Code/CLOS/make-instance-defmethods.lisp
+++ Code/CLOS/make-instance-defmethods.lisp
@@ -1,24 +1,13 @@
 (cl:in-package #:sicl-clos)
 
-;;; Because of the way bootstrapping works, we must unfortunately give
-;;; a temporary name to MAKE-INSTANCE.  The basic reason is that,
-;;; during bootstrapping, we must have a special version of it so that
-;;; it can call both ALLOCATE-INSTANCE and INITIALIZE-INSTANCE.  But
-;;; then, we can not redefine it later by loading FASL files, because
-;;; creating generic functions requires a working version of
-;;; MAKE-INSTANCE.  Our solution is to define MAKE-INSTANCE as a
-;;; generic function, but with a different name.  Then, once we have a
-;;; circular graph of CLOS metaobjects, we simply rename it, which
-;;; does not require any object creation.
-
-(defmethod make-instance-temp ((class symbol) &rest initargs)
+(defmethod make-instance ((class symbol) &rest initargs)
   (apply #'make-instance (find-class class) initargs))
 
 ;;; The HyperSpec only recognizes methods on MAKE-INSTANCE specialized
 ;;; to SYMBOL and STANDARD-CLASS.  However, the AMOP clearly says
 ;;; that ENSURE-GENERIC-FUNCTION calls MAKE-INSTANCE, and the default
 ;;; class that is instantiated is STANDARD-GENERIC-FUNCTION.  But
-;;; STANDARD-GENERIC-FUNCTION is not and instance of STANDARD-CLASS,
+;;; STANDARD-GENERIC-FUNCTION is not an instance of STANDARD-CLASS,
 ;;; and instead of FUNCALLABLE-STANDARD-CLASS.
 ;;;
 ;;; We have two possible ways of solving this conflict.  Way number 1
@@ -34,5 +23,5 @@
 ;;; that the method is specialized to CLASS.  So for instance, it is
 ;;; possible to call (MAKE-INSTANCE 'SYMBOL) in SBCL.  It does not
 ;;; complain in MAKE-INSTANCE, but in ALLOCATE-INSTANCE.
-(defmethod make-instance-temp ((class regular-class) &rest initargs)
+(defmethod make-instance ((class regular-class) &rest initargs)
   (apply #'make-instance-default class initargs))
