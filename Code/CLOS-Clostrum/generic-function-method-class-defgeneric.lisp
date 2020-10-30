8cc656bb3ac67130f021065d3a1b60e20b97b337
Robert Strandh <robert.strandh@gmail.com>
2020-10-30 17:54:33 +0100

    Remove obsolete components.

--- Code/CLOS-Clostrum/generic-function-method-class-defgeneric.lisp
+++
@@ -1,5 +0,0 @@
-(cl:in-package #:sicl-clos)
-
-;;; For the specification of this generic function, see
-;;; http://metamodular.com/CLOS-MOP/generic-function-method-class.html
-(defgeneric generic-function-method-class (generic-function))
--- Code/CLOS-Clostrum/make-method-lambda-defgenerics.lisp
+++
@@ -1,6 +0,0 @@
-(cl:in-package #:sicl-clos)
-
-;;; For the specification of this generic function, see
-;;; http://metamodular.com/CLOS-MOP/make-method-lambda.html
-(defgeneric make-method-lambda
-    (generic-function method lambda-expression environment))
