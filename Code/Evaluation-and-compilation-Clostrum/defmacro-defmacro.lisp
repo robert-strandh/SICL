f939d3a7cde843eaac56ed1f094aa46fd781b8f5
Robert Strandh <robert.strandh@gmail.com>
2020-10-31 07:51:46 +0100

    Remove obsolete component.

--- Code/Evaluation-and-compilation-Clostrum/defmacro-defmacro.lisp
+++
@@ -1,7 +0,0 @@
-(cl:in-package #:sicl-evaluation-and-compilation)
-
-(defmacro defmacro (name lambda-list &body body)
-  (let ((expansion
-          (cleavir-code-utilities:parse-macro name lambda-list body)))
-    `(eval-when (:compile-toplevel :load-toplevel :execute)
-       (setf (macro-function ',name) ,expansion))))
