5197db7a568d1bc988266e63cea4fdd20b46bbda
Robert Strandh <robert.strandh@gmail.com>
2020-10-31 07:59:16 +0100

    Replace reference to system SICL-GLOBAL-ENVIRONMENT by reference to system SICL-ENVIRONMENT.

--- Code/Evaluation-and-compilation/macroexpand-1-defun.lisp
+++ Code/Evaluation-and-compilation/macroexpand-1-defun.lisp
@@ -17,7 +17,7 @@
 ;;; variables, then, it might be possible. 
 
 (defun macroexpand-1
-    (form &optional (env (sicl-global-environment:global-environment)))
+    (form &optional (env (sicl-environment:global-environment)))
   (let ((expander nil))
     (cond ((symbolp form)
 	   (let ((expansion (trucler:symbol-macro-expansion form env)))
