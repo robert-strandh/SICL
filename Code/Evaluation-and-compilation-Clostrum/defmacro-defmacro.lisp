3530b93b182e2df6e17d634cb36f6dc958b09318
Robert Strandh <robert.strandh@gmail.com>
2020-10-31 07:49:16 +0100

    Remove commented-out code.

--- Code/Evaluation-and-compilation-Clostrum/defmacro-defmacro.lisp
+++ Code/Evaluation-and-compilation-Clostrum/defmacro-defmacro.lisp
@@ -1,9 +1,7 @@
 (cl:in-package #:sicl-evaluation-and-compilation)
 
 (defmacro defmacro (name lambda-list &body body)
-  (let (;; (compilation-environment
-        ;;  (sicl-environment:global-environment environment))
-        (expansion
+  (let ((expansion
           (cleavir-code-utilities:parse-macro name lambda-list body)))
     `(eval-when (:compile-toplevel :load-toplevel :execute)
        (setf (macro-function ',name) ,expansion))))
