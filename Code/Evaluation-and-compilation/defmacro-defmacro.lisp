e74947e6bca20759e383896c2a8c23dffb75dacd
Robert Strandh <robert.strandh@gmail.com>
2020-10-31 07:50:58 +0100

    No longer refer directly to global environment functions.

--- Code/Evaluation-and-compilation/defmacro-defmacro.lisp
+++ Code/Evaluation-and-compilation/defmacro-defmacro.lisp
@@ -1,6 +1,7 @@
 (cl:in-package #:sicl-evaluation-and-compilation)
 
 (defmacro defmacro (name lambda-list &body body)
-  `(eval-when (:compile-toplevel :load-toplevel :execute)
-     (setf (sicl-env:macro-function ',name (sicl-genv:global-environment))
-           ,(cleavir-code-utilities:parse-macro name lambda-list body))))
+  (let ((expansion
+          (cleavir-code-utilities:parse-macro name lambda-list body)))
+    `(eval-when (:compile-toplevel :load-toplevel :execute)
+       (setf (macro-function ',name) ,expansion))))
