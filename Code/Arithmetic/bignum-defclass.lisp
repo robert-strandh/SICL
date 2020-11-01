fc04ca733b7844e1fb9ab580a0cc3de2765f75ff
Robert Strandh <robert.strandh@gmail.com>
2020-11-01 10:20:47 +0100

    Define subclasses POSITIVE-BIGNUM and NEGATIVE-BIGNUM of class BIGNUM and change accessor names.

--- Code/Arithmetic/bignum-defclass.lisp
+++ Code/Arithmetic/bignum-defclass.lisp
@@ -1,9 +1,17 @@
 (cl:in-package #:sicl-arithmetic)
 
+(defgeneric limb-count (bignum))
+
+(defgeneric (setf limb-count) (limb-count bignum))
+
 ;;; We must explicitly add STANDARD-OBJECT as a superclass since the
 ;;; presence of INTEGER as a superclass prevents the automatic
 ;;; inclusion of STANDARD-OBJECT as a superclass.
 (defclass bignum (integer standard-object)
-  ((%sign-and-limb-count
+  ((%limb-count
     :initarg :limb-count
-    :accessor sign-and-limb-count)))
+    :accessor limb-count)))
+
+(defclass positive-bignum (bignum) ())
+
+(defclass negative-bignum (bignum) ())
