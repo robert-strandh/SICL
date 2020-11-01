ea6895573a33bf573c8e6347673b69696dc679a4
Robert Strandh <robert.strandh@gmail.com>
2020-11-01 10:21:38 +0100

    Bug fix and adapt to new BIGNUM subclasses.

--- Code/Arithmetic/convert-fixnum-to-bignum-defun.lisp
+++ Code/Arithmetic/convert-fixnum-to-bignum-defun.lisp
@@ -1,13 +1,12 @@
 (cl:in-package #:sicl-arithmetic)
 
 (defun convert-fixnum-to-bignum (fixnum)
-  (let ((bignum (make-instance 'bignum :limb-count 2))
+  (let ((bignum (make-instance (if (minusp fixnum)
+                                   'positive-fixnum
+                                   'negative-fixnum)
+                  :limb-count 2))
         (least-significant-limb
           (logand fixnum #.(- (expt 2 62) 1))))
     (cleavir-primop:nook-write bignum 3 least-significant-limb)
-    (cleavir-primop:nook-write bignum 4 1))
-  (unless (minusp fixnum)
-    (setf (sign-and-limb-count bignum) -2)))
-
-        
-      
+    (cleavir-primop:nook-write bignum 4 1)
+    bignum))
