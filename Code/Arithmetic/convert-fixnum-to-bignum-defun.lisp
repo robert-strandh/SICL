7bc3b446c98d184600cc5c1742fd7a66ab503c58
Robert Strandh <robert.strandh@gmail.com>
2020-11-01 10:41:43 +0100

    Add comment explaining the function.

--- Code/Arithmetic/convert-fixnum-to-bignum-defun.lisp
+++ Code/Arithmetic/convert-fixnum-to-bignum-defun.lisp
@@ -1,5 +1,13 @@
 (cl:in-package #:sicl-arithmetic)
 
+;;; When two fixnums are added and there is an overflow, the result is
+;;; a fixnum that is negative when the addition of two positive
+;;; fixnums resulted in the overflow, and that is positive when the
+;;; addition of two negative fixnums resulted in undeflow.  This
+;;; function takes such a resulting fixnum and converts it to a bignum
+;;; with the correct sign and magnitude according to the sign and
+;;; magnitude of the resulting fixnum.
+
 (defun convert-fixnum-to-bignum (fixnum)
   (let ((bignum (make-instance (if (minusp fixnum)
                                    'positive-fixnum
