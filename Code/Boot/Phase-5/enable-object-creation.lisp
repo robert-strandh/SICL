46a84e78e590b1fdf43bbd5f3c71d7340cd8dbc5
Robert Strandh <robert.strandh@gmail.com>
2020-11-02 11:08:53 +0100

    Load files containing definition of ALLOCATE-INSTANCE.

--- Code/Boot/Phase-5/enable-object-creation.lisp
+++ Code/Boot/Phase-5/enable-object-creation.lisp
@@ -1,6 +1,6 @@
 (cl:in-package #:sicl-boot-phase-5)
 
-(defun enable-object-creation (e5)
+(defun enable-object-allocation (e5)
   (setf (env:fdefinition (env:client e5) e5 'sicl-clos::allocate-general-instance)
         (lambda (class size)
           (make-instance 'sicl-boot:header
@@ -9,4 +9,10 @@
   (load-source-file "CLOS/stamp-offset-defconstant.lisp" e5)
   (load-source-file "CLOS/effective-slot-definition-class-support.lisp" e5)
   (load-source-file "CLOS/effective-slot-definition-class-defgeneric.lisp" e5)
-  (load-source-file "CLOS/effective-slot-definition-class-defmethods.lisp" e5))
+  (load-source-file "CLOS/effective-slot-definition-class-defmethods.lisp" e5)
+  (load-source-file "CLOS/allocate-instance-support.lisp" e5)
+  (load-source-file "CLOS/allocate-instance-defgenerics.lisp" e5)
+  (load-source-file "CLOS/allocate-instance-defmethods.lisp" e5))
+
+(defun enable-object-creation (e5)
+  (enable-object-allocation e5))
