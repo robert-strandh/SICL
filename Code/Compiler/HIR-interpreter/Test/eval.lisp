802fe4f535b6271114b2284d59a000dda52c6dc9
Robert Strandh <robert.strandh@gmail.com>
2020-11-04 10:48:29 +0100

    Remove obsolete module.

--- Code/Compiler/HIR-interpreter/Test/eval.lisp
+++
@@ -1,5 +0,0 @@
-(cl:in-package #:sicl-hir-interpreter-test)
-
-(defun eval (client form environment)
-  (let ((cst (cst:cst-from-expression form)))
-    (sicl-hir-interpreter:cst-eval client cst environment)))
