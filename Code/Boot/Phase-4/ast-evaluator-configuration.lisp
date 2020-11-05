7f4eb36cf08ed3d714ec2fb11022e77a97692e90
Robert Strandh <robert.strandh@gmail.com>
2020-11-05 16:07:13 +0100

    New component containing code for configuring the AST evaluator.

---
+++ Code/Boot/Phase-4/ast-evaluator-configuration.lisp
@@ -0,0 +1,42 @@
+(cl:in-package #:sicl-boot-phase-4)
+
+(defmethod sicl-ast-evaluator:translate-ast
+    ((client client)
+     (ast cleavir-ast:standard-object-p-ast)
+     lexical-environment)
+  `(let ((object ,(sicl-ast-evaluator:translate-ast
+                   client
+                   (cleavir-ast:object-ast ast)
+                   lexical-environment)))
+     (not (or (floatp object)
+              (characterp object)
+              (typep object 'fixnum)
+              (consp object)))))
+
+(defmethod sicl-ast-evaluator:translate-ast
+    ((client client)
+     (ast cleavir-ast:standard-object-class-of-ast)
+     lexical-environment)
+  `(funcall ,(lambda (object)
+               (cond ((typep object 'sicl-boot::header)
+                      (slot-value object 'sicl-boot::%class))
+                     ((null object)
+                      (env:find-class client (e4 client) 'null))
+                     ((symbolp object)
+                      (env:find-class client (e4 client) 'symbol))
+                     (t
+                      (error "Class of ~s asked for in E4" object))))
+            ,(sicl-ast-evaluator:translate-ast
+              client
+              (cleavir-ast:standard-object-ast ast)
+              lexical-environment)))
+
+(defmethod sicl-ast-evaluator:translate-ast
+    ((client client)
+     (ast cleavir-ast:single-float-p-ast)
+     lexical-environment)
+  `(let ((object ,(sicl-ast-evaluator:translate-ast
+                   client
+                   (cleavir-ast:object-ast ast)
+                   lexical-environment)))
+     (floatp object)))
