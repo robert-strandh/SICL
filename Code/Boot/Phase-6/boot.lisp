cc80f9f72e4e8529b6d5dfbcdd4326683a6366d8
Robert Strandh <robert.strandh@gmail.com>
2020-11-06 18:28:14 +0100

    Load file containing definition of function CLASS-OF.

--- Code/Boot/Phase-6/boot.lisp
+++ Code/Boot/Phase-6/boot.lisp
@@ -5,6 +5,7 @@
   (with-accessors ((e4 sicl-boot:e4)
                    (e5 sicl-boot:e5))
       boot
+    (load-source-file "CLOS/class-of-defun.lisp" e5)
     (enable-slot-value e5)
     (enable-object-creation e5)
     (copy-classes e4 e5)))
