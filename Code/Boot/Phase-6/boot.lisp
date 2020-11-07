f111de1948554e9652981349f8ac01ba10f9e3a2
Robert Strandh <robert.strandh@gmail.com>
2020-11-07 11:14:13 +0100

    Call main entry point of new components from main boot procedure.

--- Code/Boot/Phase-6/boot.lisp
+++ Code/Boot/Phase-6/boot.lisp
@@ -5,7 +5,9 @@
   (with-accessors ((e4 sicl-boot:e4)
                    (e5 sicl-boot:e5))
       boot
+    (copy-classes e4 e5)
     (load-source-file "CLOS/class-of-defun.lisp" e5)
     (enable-slot-value e5)
     (enable-object-creation e5)
-    (copy-classes e4 e5)))
+    (enable-method-combinations e5)
+    (enable-generic-function-creation e5)))
