31d231b5c586b94eee219ee36812dfdd4a158381
Robert Strandh <robert.strandh@gmail.com>
2020-11-08 17:43:57 +0100

    New component for enabling object creation in E4.

---
+++ Code/Boot/Phase-5/enable-object-creation.lisp
@@ -0,0 +1,23 @@
+(cl:in-package #:sicl-boot-phase-5)
+
+(defun enable-object-allocation (e4)
+  (setf (env:fdefinition (env:client e4) e4 'sicl-clos::allocate-general-instance)
+        (lambda (class size)
+          (make-instance 'sicl-boot:header
+            :class class
+            :rack (make-array size :initial-element 10000000))))
+  (load-source-file "CLOS/stamp-offset-defconstant.lisp" e4)
+  (load-source-file "CLOS/effective-slot-definition-class-support.lisp" e4)
+  (load-source-file "CLOS/effective-slot-definition-class-defgeneric.lisp" e4)
+  (load-source-file "CLOS/effective-slot-definition-class-defmethods.lisp" e4)
+  ;; These were already loaded in phase 3 because they were needed for
+  ;; the finalization of built-in classes.
+  ;; (load-source-file "CLOS/class-finalization-defgenerics.lisp" e4)
+  ;; (load-source-file "CLOS/class-finalization-defmethods.lisp" e4)
+  (load-source-file "CLOS/allocate-instance-support.lisp" e4)
+  (load-source-file "CLOS/allocate-instance-defgenerics.lisp" e4)
+  (load-source-file "CLOS/allocate-instance-defmethods.lisp" e4))
+
+(defun enable-object-creation (e4 e5)
+  (declare (ignore e5))
+  (enable-object-allocation e4))
