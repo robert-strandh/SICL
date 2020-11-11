56f09e1f65f7e692984c0f49d1201c6754ee454c
Robert Strandh <robert.strandh@gmail.com>
2020-11-11 09:17:35 +0100

    Replace SBCL-specific low-level output by new SICL-POSIX version.

--- Code/Stream/unix-byte-stream.lisp
+++ Code/Stream/unix-byte-stream.lisp
@@ -1,6 +1,6 @@
 (cl:in-package #:sicl-stream)
 
-(defclass unix-byte-stream ()
+(defclass unix-byte-output-stream ()
   ((%file-descriptor :initarg :file-descriptor :reader file-descriptor)
    (%buffer :initform (make-array 256
 				  :element-type '(unsigned-byte 8)
@@ -8,25 +8,15 @@
 	    :initarg :buffer
 	    :accessor buffer)))
 
-(defclass sbcl-unix-byte-stream (unix-byte-stream)
-  ((%fd-stream :initarg :fd-stream :reader fd-stream)))
-
-(defmethod initialize-instance :after ((instance sbcl-unix-byte-stream) &key)
-  (reinitialize-instance
-   instance
-   :fd-stream (sb-sys:make-fd-stream (file-descriptor instance)
-				     :output t
-				     :element-type '(unsigned-byte 8))))
-
-(defmethod finish-output ((stream sbcl-unix-byte-stream))
-  (write-sequence (buffer stream) (fd-stream stream)
-		  :end (fill-pointer (buffer stream)))
-  (cl:finish-output (fd-stream stream))
+(defmethod finish-output ((stream unix-byte-output-stream))
+  (sicl-posix-high:write
+   (file-descriptor stream)
+   (buffer stream)
+   :end (fill-pointer (buffer stream)))
   (setf (fill-pointer (buffer stream)) 0))
 
-(defmethod write-byte (integer (stream unix-byte-stream))
+(defmethod write-byte (integer (stream unix-byte-output-stream))
   (when (= (fill-pointer (buffer stream))
 	   (array-total-size (buffer stream)))
     (finish-output stream))
   (vector-push integer (buffer stream)))
-
