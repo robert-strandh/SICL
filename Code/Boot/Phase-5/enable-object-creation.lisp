24a94c31eed3651e0e9dbd7cb4abd88f53e4b03b
Robert Strandh <robert.strandh@gmail.com>
2020-11-15 09:11:12 +0100

    Only enable object allocation, but not initialization or MAKE-INSTANCE in E4.

--- Code/Boot/Phase-5/enable-object-creation.lisp
+++ Code/Boot/Phase-5/enable-object-creation.lisp
@@ -18,43 +18,45 @@
   (load-source-file "CLOS/allocate-instance-defgenerics.lisp" e4)
   (load-source-file "CLOS/allocate-instance-defmethods.lisp" e4))
 
-(defun enable-object-initialization (e4 e5)
-  (setf (env:constant-variable (env:client e4) e4 'sicl-clos::+unbound-slot-value+)
-        10000000)
-  (load-source-file "CLOS/slot-bound-using-index.lisp" e4)
-  (load-source-file "CLOS/standard-instance-access.lisp" e4)
-  (import-functions-from-host '(slot-unbound) e4)
-  (with-intercepted-function-cells
-      (e4
-       (class-of
-        (list (lambda (object)
-                (slot-value object 'sicl-boot::%class)))))
-    (load-source-file "CLOS/slot-value-etc-support.lisp" e4))
-  (load-source-file "CLOS/instance-slots-offset-defconstant.lisp" e4)
-  (load-source-file "CLOS/shared-initialize-support.lisp" e4)
-  (load-source-file "CLOS/shared-initialize-defgenerics.lisp" e5)
-  (with-intercepted-function-cells
-      (e5
-       (sicl-clos::shared-initialize-default-using-class
-        (env:function-cell
-         (env:client e4) e4 'sicl-clos::shared-initialize-default-using-class)))
-    (load-source-file "CLOS/shared-initialize-defmethods.lisp" e5))
-  (load-source-file "CLOS/initialize-instance-support.lisp" e5)
-  (load-source-file "CLOS/initialize-instance-defgenerics.lisp" e5)
-  (load-source-file "CLOS/initialize-instance-defmethods.lisp" e5))
+;; (defun enable-object-initialization (e4 e5)
+;;   (setf (env:constant-variable (env:client e4) e4 'sicl-clos::+unbound-slot-value+)
+;;         10000000)
+;;   (load-source-file "CLOS/slot-bound-using-index.lisp" e4)
+;;   (load-source-file "CLOS/standard-instance-access.lisp" e4)
+;;   (import-functions-from-host '(slot-unbound) e4)
+;;   (with-intercepted-function-cells
+;;       (e4
+;;        (class-of
+;;         (list (lambda (object)
+;;                 (slot-value object 'sicl-boot::%class)))))
+;;     (load-source-file "CLOS/slot-value-etc-support.lisp" e4))
+;;   (load-source-file "CLOS/instance-slots-offset-defconstant.lisp" e4)
+;;   (load-source-file "CLOS/shared-initialize-support.lisp" e4)
+;;   (load-source-file "CLOS/shared-initialize-defgenerics.lisp" e5)
+;;   (with-intercepted-function-cells
+;;       (e5
+;;        (sicl-clos::shared-initialize-default-using-class
+;;         (env:function-cell
+;;          (env:client e4) e4 'sicl-clos::shared-initialize-default-using-class)))
+;;     (load-source-file "CLOS/shared-initialize-defmethods.lisp" e5))
+;;   (load-source-file "CLOS/initialize-instance-support.lisp" e5)
+;;   (load-source-file "CLOS/initialize-instance-defgenerics.lisp" e5)
+;;   (load-source-file "CLOS/initialize-instance-defmethods.lisp" e5))
 
-(defun enable-make-instance (e4 e5)
-  (with-intercepted-function-cells
-      (e4
-       (find-class
-        (env:function-cell (env:client e4) e4 'find-class))
-       (initialize-instance
-        (env:function-cell (env:client e5) e5 'initialize-instance)))
-    (load-source-file "CLOS/make-instance-support.lisp" e4))
-  (load-source-file "CLOS/make-instance-defgenerics.lisp" e4)
-  (load-source-file "CLOS/make-instance-defmethods.lisp" e4))
+;; (defun enable-make-instance (e4 e5)
+;;   (with-intercepted-function-cells
+;;       (e4
+;;        (find-class
+;;         (env:function-cell (env:client e4) e4 'find-class))
+;;        (initialize-instance
+;;         (env:function-cell (env:client e5) e5 'initialize-instance)))
+;;     (load-source-file "CLOS/make-instance-support.lisp" e4))
+;;   (load-source-file "CLOS/make-instance-defgenerics.lisp" e4)
+;;   (load-source-file "CLOS/make-instance-defmethods.lisp" e4))
 
 (defun enable-object-creation (e4 e5)
+  (declare (ignore e5))
   (enable-object-allocation e4)
-  (enable-object-initialization e4 e5)
-  (enable-make-instance e4 e5))
+  (define-error-functions '(make-instance) e4))
+  ;; (enable-object-initialization e4 e5)
+  ;; (enable-make-instance e4 e5))
