02cc227fea1a0b880ea4135b06d2b0f546c360e4
Robert Strandh <robert.strandh@gmail.com>
2020-11-16 16:07:09 +0100

    Remove obsolete component.

--- Code/CLOS/find-accessor-method-class-defun.lisp
+++
@@ -1,7 +0,0 @@
-(cl:in-package #:sicl-clos)
-
-;;; This definition should be loaded into the final system.  During
-;;; bootstrapping, however, we define it differently, because the
-;;; class needs to be found in a different environment.
-(defun define-accessor-method-class (class-name)
-  (find-class class-name))
