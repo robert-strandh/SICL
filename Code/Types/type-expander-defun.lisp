852406bd1a98fd4431ba3e735f3606dbf495b533
Robert Strandh <robert.strandh@gmail.com>
2020-11-18 09:57:48 +0100

    Add comment explaining why the function is defined that way.

--- Code/Types/type-expander-defun.lisp
+++ Code/Types/type-expander-defun.lisp
@@ -1,5 +1,16 @@
 (cl:in-package #:sicl-type)
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;; Function TYPE-EXPANDER.
+;;;
+;;; This function is defined the way it is because we want it to be
+;;; defined in an environment where we will ultimately undefine
+;;; functions with names in the SICL-ENVIRONMENT package.  So the
+;;; function SICL-ENVIRONMENT:TYPE-EXPANDER and the CLIENT object of
+;;; the environment are closed over at load time so that they can be
+;;; undefined after this definition has been loaded.
+
 (setf (fdefinition 'type-expander)
       (load-time-value
        (let* ((environment (sicl-environment:global-environment))
