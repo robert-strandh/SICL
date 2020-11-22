1576adc70eb7adff70c5d30c76ed06e006240271
Robert Strandh <robert.strandh@gmail.com>
2020-11-22 16:46:51 +0100

    Remove obsolete component.

--- Code/CLOS/make-method-for-generic-function.lisp
+++
@@ -1,7 +0,0 @@
-(cl:in-package #:sicl-clos)
-
-(defun make-method-for-generic-function (generic-function specializers keys)
-  (apply #'make-instance
-         (generic-function-method-class generic-function)
-         :specializers specializers
-         keys))
