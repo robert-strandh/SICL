5e514717c39359fca7331220c6b05e8c5f801eb0
Robert Strandh <robert.strandh@gmail.com>
2020-11-23 16:34:15 +0100

    Rewrite function to call indirect environment functions.

--- Code/CLOS/ensure-generic-function-defun.lisp
+++ Code/CLOS/ensure-generic-function-defun.lisp
@@ -9,19 +9,31 @@
 (defun ensure-generic-function
     (name
      &rest keys
-     &key environment
      &allow-other-keys)
-  (let* ((global-environment (sicl-environment:global-environment environment))
-         (client (sicl-environment:client global-environment))
-         (generic-function
-           (if (sicl-environment:fboundp client global-environment name)
-               (let ((fun (sicl-environment:fdefinition
-                           client global-environment name)))
-                 (if (typep fun 'generic-function)
-                     fun
-                     (error 'type-error
-                            :datum fun
-                            :expected-type '(or null generic-function))))
-               nil)))
+  (let ((generic-function
+          (if (fboundp name)
+              (if (consp name)
+                  ;; Then it must be a function and not a macro or a
+                  ;; special operator.  It could, of course be an
+                  ;; ordinary function.  In that case, we signal an
+                  ;; error in the method specialized to FUNCTION on
+                  ;; ENSURE-GENERIC-FUNCTION-USING-CLASS.
+                  (fdefinition name)
+                  ;; If it is not a CONS, then it might be a macro or
+                  ;; a special operator, so we must check for that
+                  ;; possibility.
+                  (if (and (null (macro-function name))
+                           (not (special-operator-p name)))
+                      ;; Again, it must be a function.
+                      (fdefinition name)
+                      ;; It is fbound, but it is either a macro or a
+                      ;; special operator.  The standard says that we
+                      ;; must signal a type error then.
+                      (error 'type-error
+                             :expected-type 'generic-function
+                             :datum (fdefinition name))))
+              ;; It is not fbound, so we return NIL so that we can
+              ;; pass it to ENSURE-GENERIC-FUNCTION-USING-CLASS.
+              nil)))
     (apply #'ensure-generic-function-using-class
            generic-function name keys)))
