851f3d72f6c4071099173df2f771757df0b30fce
Robert Strandh <robert.strandh@gmail.com>
2020-11-24 11:19:45 +0100

    Remove temporary override for CLASS-OF and CLASS-PRECEDENCE-LIST.

--- Code/Boot/Phase-2/enable-typep.lisp
+++ Code/Boot/Phase-2/enable-typep.lisp
@@ -2,13 +2,7 @@
 
 (defun enable-typep (e2)
   (define-error-functions '(typep symbol-package find-package) e2)
-  (with-intercepted-function-cells
-      (e2
-       (sicl-clos:class-precedence-list
-        (list #'closer-mop:class-precedence-list))
-       (class-of
-        (list #'class-of)))
-    (load-source-file "Types/Typep/typep-atomic.lisp" e2)
-    (load-source-file "Types/Typep/typep-compound-integer.lisp" e2)
-    (load-source-file "Types/Typep/typep-compound.lisp" e2)
-    (load-source-file "Types/Typep/typep.lisp" e2)))
+  (load-source-file "Types/Typep/typep-atomic.lisp" e2)
+  (load-source-file "Types/Typep/typep-compound-integer.lisp" e2)
+  (load-source-file "Types/Typep/typep-compound.lisp" e2)
+  (load-source-file "Types/Typep/typep.lisp" e2))
