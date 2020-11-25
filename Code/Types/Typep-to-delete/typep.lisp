53429c1d092aa614b76f135a2d26c3b42a9f74e3
Robert Strandh <robert.strandh@gmail.com>
2020-11-25 17:16:23 +0100

    Remove obsolete components.

--- Code/Types/Typep-to-delete/typep.lisp
+++
@@ -1,6 +0,0 @@
-(cl:in-package #:sicl-type)
-
-(defun typep (object type-specifier)
-  (if (consp type-specifier)
-      (typep-compound object type-specifier)
-      (typep-atomic object type-specifier)))
