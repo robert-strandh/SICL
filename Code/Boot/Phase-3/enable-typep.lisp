08ea3dcc772c671549b40efdb5b40d2ffe8902ab
Robert Strandh <robert.strandh@gmail.com>
2020-11-27 11:47:07 +0100

    Override references to CLASS-PRECEDENCE-LIST.

--- Code/Boot/Phase-3/enable-typep.lisp
+++ Code/Boot/Phase-3/enable-typep.lisp
@@ -1,10 +1,14 @@
 (cl:in-package #:sicl-boot-phase-3)
 
-(defun enable-typep (e3)
+(defun enable-typep (e2 e3)
   ;; FIXME: define functions SYMBOL-PACKAGE and FIND-PACKAGE in the
   ;; environment so that they exist at this point.
   (define-error-functions '(typep symbol-package find-package) e3)
-  (load-source-file "Types/Typep/typep-atomic.lisp" e3)
+  (with-intercepted-function-cells
+      (e3
+       (sicl-clos:class-precedence-list
+        (env:function-cell (env:client e2) e2 'sicl-clos:class-precedence-list)))
+    (load-source-file "Types/Typep/typep-atomic.lisp" e3))
   (load-source-file "Types/Typep/typep-compound-integer.lisp" e3)
   (load-source-file "Types/Typep/typep-compound.lisp" e3)
   (load-source-file "Types/Typep/typep.lisp" e3))
