6c5bbdd36d3d8a4fd1bbece1493f7f08941f7157
Robert Strandh <robert.strandh@gmail.com>
2020-11-27 17:58:36 +0100

    Call ENABLE-DEFCLASS from main phase entry point.

--- Code/Boot/Phase-3/prepare-next-phase.lisp
+++ Code/Boot/Phase-3/prepare-next-phase.lisp
@@ -40,6 +40,5 @@
     (load-source-file "CLOS/generic-function-initialization-defmethods.lisp" e3))
   (enable-defgeneric e2 e3 e4)
   (enable-defmethod e2 e3 e4)
-  ;; (enable-defclass e2 e3 e4))
-  (finalize-classes e3)
-  )
+  (enable-defclass e2 e3 e4)
+  (finalize-classes e3))
