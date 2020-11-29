67f7f1ef09a40bb9d23c202f3faf865ff6ad27ee
Robert Strandh <robert.strandh@gmail.com>
2020-11-29 09:03:43 +0100

    New component containing code for defining TYPEP in environment E4.

---
+++ Code/Boot/Phase-4/enable-typep.lisp
@@ -0,0 +1,14 @@
+(cl:in-package #:sicl-boot-phase-4)
+
+(defun enable-typep (e3 e4)
+  ;; FIXME: define functions SYMBOL-PACKAGE and FIND-PACKAGE in the
+  ;; environment so that they exist at this point.
+  (define-error-functions '(typep symbol-package find-package) e4)
+  (with-intercepted-function-cells
+      (e4
+       (sicl-clos:class-precedence-list
+        (env:function-cell (env:client e3) e3 'sicl-clos:class-precedence-list)))
+    (load-source-file "Types/Typep/typep-atomic.lisp" e4))
+  (load-source-file "Types/Typep/typep-compound-integer.lisp" e4)
+  (load-source-file "Types/Typep/typep-compound.lisp" e4)
+  (load-source-file "Types/Typep/typep.lisp" e4))
