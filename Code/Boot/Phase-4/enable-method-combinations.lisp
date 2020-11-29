77b1bbcb65e0f1b9b77dbc8e7eff5f8e94501a4a
Robert Strandh <robert.strandh@gmail.com>
2020-11-29 09:02:59 +0100

    Make function-cell overrides more idiomatic.

--- Code/Boot/Phase-4/enable-method-combinations.lisp
+++ Code/Boot/Phase-4/enable-method-combinations.lisp
@@ -10,28 +10,20 @@
   (with-intercepted-function-cells
       (e4
        (make-instance
-           (list (lambda (class-or-name &rest initargs)
-                   (let ((class (if (symbolp class-or-name)
-                                    (env:find-class (env:client e3) e3 class-or-name)
-                                    class-or-name)))
-                     (apply (env:fdefinition (env:client e3) e3 'make-instance)
-                            class initargs))))))
+        (env:function-cell (env:client e3) e3 'make-instance))
+       (sicl-method-combination:find-method-combination-template
+        (env:function-cell
+         (env:client e5) e5 'sicl-method-combination:find-method-combination-template)))
     (load-source-file "Method-combination/find-method-combination.lisp" e4))
   (load-source-file "Method-combination/define-method-combination-defmacro.lisp" e4)
   (with-intercepted-function-cells
       (e4
        (make-instance
-           (list (lambda (class-or-name &rest initargs)
-                   (let ((class (if (symbolp class-or-name)
-                                    (env:find-class (env:client e3) e3 class-or-name)
-                                    class-or-name)))
-                     (apply (env:fdefinition (env:client e3) e3 'make-instance)
-                            class initargs)))))
-       (find-class (list (lambda (x) (env:find-class (env:client e3) e3 x)))))
+        (env:function-cell (env:client e3) e3 'make-instance))
+       ((setf sicl-method-combination:find-method-combination-template)
+        (env:function-cell
+         (env:client e5) e5
+         '(setf sicl-method-combination:find-method-combination-template))))
     (load-source-file "CLOS/standard-method-combination.lisp" e4))
-  (setf (env:fdefinition (env:client e5) e5 'sicl-clos:find-method-combination)
-        (lambda (generic-function name arguments)
-          (declare (ignore generic-function))
-          (funcall (env:fdefinition
-                    (env:client e4) e4 'sicl-method-combination:find-method-combination)
-                   name arguments))))
+  (load-source-file "CLOS/find-method-combination-defgenerics.lisp" e4)
+  (load-source-file "CLOS/find-method-combination-defmethods.lisp" e4))
