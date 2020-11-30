1989ca9f56874378a16a848e3000ad85d060752d
Robert Strandh <robert.strandh@gmail.com>
2020-11-30 17:54:59 +0100

    Use production version of ENSURE-METHOD rather than a bootstrapping-specific version.

--- Code/Boot/Phase-4/enable-defmethod.lisp
+++ Code/Boot/Phase-4/enable-defmethod.lisp
@@ -1,54 +1,19 @@
 (cl:in-package #:sicl-boot-phase-4)
 
-(defun canonicalize-specializers (specializers e4)
-  (loop for specializer in specializers
-        collect (cond ((symbolp specializer)
-                       (env:find-class (env:client e4) e4 specializer))
-                      (t
-                       specializer))))
-
 (defun define-ensure-method (e3 e4 e5)
-  (setf (env:fdefinition (env:client e5) e5 'sicl-clos:ensure-method)
-        (lambda (name
-                 &key
-                   method-class
-                   lambda-list
-                   qualifiers
-                   specializers
-                   function
-                   (slot-definition nil slot-definition-p)
-                 &allow-other-keys)
-          (assert (or (symbolp name) (consp name)))
-          (let* ((ensure-gf (env:fdefinition (env:client e5) e5 'ensure-generic-function))
-                 (add-method (env:fdefinition (env:client e4) e4 'add-method))
-                 (make-instance (env:fdefinition (env:client e3) e3 'make-instance))
-                 (method-class
-                   (if (symbolp method-class)
-                       (env:find-class (env:client e3) e3 method-class)
-                       method-class))
-                 (generic-function
-                   (funcall ensure-gf name
-                            :lambda-list lambda-list
-                            :generic-function-class 'standard-generic-function
-                            :method-class method-class))
-                 (method
-                   (if slot-definition-p
-                       (funcall
-                        make-instance method-class
-                        :lambda-list lambda-list
-                        :qualifiers qualifiers
-                        :specializers (canonicalize-specializers specializers e4)
-                        :slot-definition slot-definition
-                        :function function)
-                       (funcall
-                        make-instance
-                        method-class
-                        :lambda-list lambda-list
-                        :qualifiers qualifiers
-                        :specializers (canonicalize-specializers specializers e4)
-                        :function function))))
-            (funcall add-method generic-function method)
-            method))))
+  (setf (env:special-variable (env:client e5) e5 'lambda-list-keywords t)
+        '(&optional &reest &body &key &allow-other-keys &aux &whole &environment))
+  (load-source-file "CLOS/lambda-list-functions.lisp" e5)
+  (let ((client (env:client e4)))
+    (with-intercepted-function-cells
+        (e5
+         (typep
+          (env:function-cell client e4 'typep))
+         (add-method
+          (env:function-cell client e4 'add-method))
+         (make-instance
+          (env:function-cell client e3 'make-instance)))
+      (load-source-file "CLOS/ensure-method-defun.lisp" e5))))
 
 (defun enable-defmethod (e3 e4 e5)
   (define-generic-function-class-names e5)
@@ -73,12 +38,7 @@
   (load-source-file "CLOS/add-remove-method-defgenerics.lisp" e4)
   (with-intercepted-function-cells
       (e4
-       (find-class
-        (list (lambda (name)
-                (assert (eq name 't))
-                (env:find-class (env:client e4) e4 't))))
-       (slot-boundp
-        (env:function-cell (env:client e3) e3 'slot-boundp)))
+       (env:function-cell (env:client e3) e3 'slot-boundp))
     (load-source-file "CLOS/add-remove-method-support.lisp" e4))
   (load-source-file "CLOS/add-remove-method-defmethods.lisp" e4)
   (load-source-file "CLOS/defmethod-support.lisp" e5)
