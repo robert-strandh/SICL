7b5adee9d20aa54772f6a8e34a2193b95431b7c5
Robert Strandh <robert.strandh@gmail.com>
2020-12-05 13:55:39 +0100

    Near-complete rewrite to take advantage of compile-time computations.

--- Code/CLOS/compute-effective-method-support.lisp
+++ Code/CLOS/compute-effective-method-support.lisp
@@ -1,32 +1,27 @@
 (cl:in-package #:sicl-clos)
 
 (defun wrap-make-method-form
-    (form arguments-var arguments-var-p next-methods-var)
-  (let* ((arguments-var-list
-           (if arguments-var-p `(,arguments-var) '())))
-    `(lambda (,@arguments-var-list ,next-methods-var)
-       ,@(if arguments-var-p
-             '()
-             `((declare (ignore ,arguments-var))))
-       (flet ((next-method-p ()
-                (not (null ,next-methods-var)))
-              (call-next-method (&rest ,arguments-var)
-                (funcall (method-function (first ,next-methods-var))
-                         ,arguments-var
-                         (rest ,next-methods-var))))
-         ,form))))
+    (form arguments-var next-methods-var)
+  `(lambda (,arguments-var ,next-methods-var)
+     (flet ((next-method-p ()
+              (not (null ,next-methods-var)))
+            (call-next-method (&rest arguments)
+              (funcall (method-function (first ,next-methods-var))
+                       (if (null arguments)
+                           ,arguments-var
+                           arguments)
+                       (rest ,next-methods-var))))
+       ,form)))
 
-(defun wrap-in-call-method-macrolet (form arguments-var next-methods-var)
+(defun wrap-in-call-method-macrolet (form arguments-var)
   `(macrolet ((call-method (method &optional next-method-list)
                 (cond ((and (consp method)
                             (eq (first method) 'make-method)
                             (null (rest (rest method))))
-                       `(funcall ,(wrap-make-method-form
-                                   (second method)
-                                   ',arguments-var
+                       `(funcall ,(compile
                                    nil
-                                   ',next-methods-var)
-                                 (list ,@next-method-list)))
+                                   `(lambda ()
+                                      ,(wrap-in-call-method-macrolet (second method) arguments-var)))))
                       ((not (consp method))
                        `(funcall ,(method-function method)
                                  ,',arguments-var
@@ -34,32 +29,28 @@
                       (t (error "Malformed argument to CALL-METHOD ~s" method)))))
      ,form))
 
-(defun wrap-in-make-method-macrolet
-    (form method-class arguments-var next-methods-var)
+(defun wrap-in-make-method-macrolet (form method-class)
   `(macrolet ((make-method (make-method-form)
-                (make-instance ,method-class
-                  :qualifiers '()
-                  :lambda-list '()
-                  :specializers '()
-                  :function
-                  (lambda (,arguments-var ,next-methods-var)
-                    ,(wrap-in-call-method-macrolet
-                      (wrap-make-method-form 'make-method-form
-                                             arguments-var
-                                             t
-                                             next-methods-var)
-                      arguments-var
-                      next-methods-var)))))
+                (let ((arguments-var (gensym))
+                      (next-methods-var (gensym)))
+                  (make-instance ,method-class
+                    :qualifiers '()
+                    :lambda-list '()
+                    :specializers '()
+                    :function
+                    (compile nil
+                     `(lambda (,arguments-var ,next-methods-var)
+                        (declare (ignorable ,arguments-var))
+                        (declare (ignore ,next-methods-var))
+                        ,(wrap-in-call-method-macrolet make-method-form arguments-var)))))))
      ,form))
 
 (defun wrap-method-combination-form (form method-class)
-  (let ((arguments-var (gensym "ARGUMENTS-"))
-        (next-methods-var (gensym "NEXT-METHODS-")))
+  (let ((arguments-var (gensym "ARGUMENTS-")))
     `(lambda (,arguments-var)
        ,(wrap-in-call-method-macrolet
-         (wrap-in-make-method-macrolet
-          form method-class arguments-var next-methods-var)
-         arguments-var next-methods-var))))
+         (wrap-in-make-method-macrolet form method-class)
+         arguments-var))))
 
 (defun compute-effective-method-default
     (generic-function method-combination methods)
@@ -70,5 +61,6 @@
          (variant-signature (variant-signature method-combination))
          (function (sicl-method-combination:effective-method-form-function template))
          (form (apply function method-qualifier-pairs variant-signature))
-         (method-class (generic-function-method-class generic-function)))
-    (wrap-method-combination-form form method-class)))
+         (method-class (generic-function-method-class generic-function))
+         (result (wrap-method-combination-form form method-class)))
+    result))
