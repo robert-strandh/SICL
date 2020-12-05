f3d243e5ec3160be73ce29e55b58c81b25a231f6
Robert Strandh <robert.strandh@gmail.com>
2020-12-05 17:21:12 +0100

    Remove obsolete (comented-out) code and associated comment.

--- Code/Boot/Phase-4/boot.lisp
+++ Code/Boot/Phase-4/boot.lisp
@@ -9,12 +9,6 @@
       boot
     (change-class e4 'environment
                   :client (make-instance 'client :e4 e4))
-    ;; Define CLASS-OF temporarily here.  A production version will be
-    ;; loaded once we have enough classes in E4 to be able to return
-    ;; classes for FIXNUM, SINGLE-FLOAT, CHARACTER, and CONS.
-    ;; (setf (env:fdefinition (env:client e4) e4 'class-of)
-    ;;       (lambda (object)
-    ;;         (slot-value object 'sicl-boot::%class)))
     (sicl-boot:create-accessor-defgenerics e4)
     (sicl-boot:create-mop-classes e4)
     ;; (load-source-file "CLOS/class-readers-forward-referenced-class-defmethods.lisp" e4)
