366a642adcc26af4647b6a82d278653f1261b464
Robert Strandh <robert.strandh@gmail.com>
2020-12-09 17:57:55 +0100

    Remove function-cell overrides so that references are all to Ersatz (E5).

--- Code/Boot/Phase-5/enable-defgeneric.lisp
+++ Code/Boot/Phase-5/enable-defgeneric.lisp
@@ -17,13 +17,9 @@
   (load-source-file "CLOS/compute-effective-method-defgenerics.lisp" e5)
   (load-source-file "CLOS/compute-effective-method-defmethods.lisp" e5))
 
-(defun define-compute-discriminating-function (e4 e5)
+(defun define-compute-discriminating-function (e5)
   (load-source-file "CLOS/compute-discriminating-function-defgenerics.lisp" e5)
-  (with-intercepted-function-cells
-      (e5
-       (class-of (env:function-cell (env:client e4) e4 'class-of))
-       (make-instance (env:function-cell (env:client e4) e4 'make-instance)))
-    (load-source-file "CLOS/maybe-replace-method.lisp" e5))
+  (load-source-file "CLOS/maybe-replace-method.lisp" e5)
   (with-intercepted-function-cells
       (e5
        ;; FIXME: this one should also assign the slots in the
@@ -39,21 +35,14 @@
   (load-source-file "CLOS/compute-discriminating-function-support-c.lisp" e5)
   (load-source-file "CLOS/compute-discriminating-function-defmethods.lisp" e5))
 
-(defun enable-compute-discriminating-function (e4 e5)
+(defun enable-compute-discriminating-function (e5)
   (define-classp e5)
   (define-sub-specializer-p e5)
   (define-compute-applicable-methods e5)
   (define-compute-effective-method e5)
-  (define-compute-discriminating-function e4 e5))
+  (define-compute-discriminating-function e5))
 
-(defun enable-defgeneric (e4 e5)
-  (with-intercepted-function-cells
-      (e5
-       (make-instance (env:function-cell (env:client e4) e4 'make-instance))
-       (sicl-clos:class-precedence-list
-        (env:function-cell (env:client e4) e4 'sicl-clos:class-precedence-list))
-       (sicl-clos:class-prototype
-        (env:function-cell (env:client e4) e4 'sicl-clos:class-prototype)))
-    (load-source-file "CLOS/ensure-generic-function-using-class-support.lisp" e5))
+(defun enable-defgeneric (e5)
+  (load-source-file "CLOS/ensure-generic-function-using-class-support.lisp" e5)
   (load-source-file "CLOS/ensure-generic-function-using-class-defgenerics.lisp" e5)
   (load-source-file "CLOS/ensure-generic-function-using-class-defmethods.lisp" e5))
