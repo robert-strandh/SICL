a76da688068597bd3c167d2e368486d25d85ccde
Robert Strandh <robert.strandh@gmail.com>
2020-12-09 10:40:03 +0100

    Remove unused argument to PREPARE-NEXT-PHASE.

--- Code/Boot/Phase-4/boot.lisp
+++ Code/Boot/Phase-4/boot.lisp
@@ -13,4 +13,4 @@
     (sicl-boot:create-mop-classes e4)
     ;; (load-source-file "CLOS/class-readers-forward-referenced-class-defmethods.lisp" e4)
     ;; (load-source-file "CLOS/class-readers-defmethods-before.lisp" e4)
-    (prepare-next-phase e2 e3 e4 e5)))
+    (prepare-next-phase e3 e4 e5)))
