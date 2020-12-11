9c1dba6bc60750a755ff40857cb38e6ef5100fcb
Robert Strandh <robert.strandh@gmail.com>
2020-12-11 16:20:07 +0100

    Fix incorrect names and parameter orders of methods.

--- Code/Stream/unix-byte-stream.lisp
+++ Code/Stream/unix-byte-stream.lisp
@@ -7,15 +7,15 @@
 (defclass unix-byte-output-stream (stream buffered-output-stream-mixin)
   ((%file-descriptor :initarg :file-descriptor :reader file-descriptor)))
 
-(defmethod finish-output ((stream unix-byte-output-stream))
+(defmethod stream-finish-output ((stream unix-byte-output-stream))
   (sicl-posix-high:write
    (file-descriptor stream)
    (buffer stream)
    :end (fill-pointer (buffer stream)))
   (setf (fill-pointer (buffer stream)) 0))
 
-(defmethod write-byte (integer (stream unix-byte-output-stream))
+(defmethod stream-write-byte ((stream unix-byte-output-stream) integer)
   (when (= (fill-pointer (buffer stream))
 	   (array-total-size (buffer stream)))
-    (finish-output stream))
+    (stream-finish-output stream))
   (vector-push integer (buffer stream)))
