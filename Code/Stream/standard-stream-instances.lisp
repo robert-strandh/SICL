e9af7bf415af6406e52ffdcea35975386757cf2a
Robert Strandh <robert.strandh@gmail.com>
2020-12-11 16:20:55 +0100

    Create stream instance for *STANDARD-OUTPUT*.

--- Code/Stream/standard-stream-instances.lisp
+++ Code/Stream/standard-stream-instances.lisp
@@ -5,6 +5,10 @@
     :file-descriptor 1
     :buffer
     (make-instance 'sicl-array:vector-unsigned-byte-8
+      :dimensions '(256)
       :fill-pointer 0
       :additional-space 32)))
 
+(defparameter *standard-output*
+  (make-instance 'character-to-binary-output-stream
+    :binary-stream *unix-standard-output*))
