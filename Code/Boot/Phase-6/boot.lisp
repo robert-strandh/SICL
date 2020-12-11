6a19d8c6f78d102bfe38d92ee1b86ecad7356030
Robert Strandh <robert.strandh@gmail.com>
2020-12-11 09:54:02 +0100

    Replace call to satiation by call to enable printing.

--- Code/Boot/Phase-6/boot.lisp
+++ Code/Boot/Phase-6/boot.lisp
@@ -6,9 +6,4 @@
                    (e4 sicl-boot:e4)
                    (e5 sicl-boot:e5))
       boot
-    (load-source-file "CLOS/standard-instance-access.lisp" e4)
-    (define-error-functions
-        '(sicl-clos::all-descendants sicl-clos::cartesian-product)
-        e4)
-    (load-source-file "CLOS/satiation.lisp" e4)
-    (satiate-generic-functions-1 e3 e4 e5)))
+    (enable-printing e5)))
