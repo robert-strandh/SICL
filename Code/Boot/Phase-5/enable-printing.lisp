ed53144e72568068e2e9d80e69729e5b1e5768b1
Robert Strandh <robert.strandh@gmail.com>
2020-12-11 09:29:41 +0100

    Define function for defining funtion SICL-POSIX-HIGH:WRITE in Ersatz.

--- Code/Boot/Phase-5/enable-printing.lisp
+++ Code/Boot/Phase-5/enable-printing.lisp
@@ -1,4 +1,28 @@
 (cl:in-package #:sicl-boot-phase-5)
 
+(defun define-posix-write (e5)
+  (let* ((client (env:client e5))
+         (aref-function (env:fdefinition client e5 'aref))
+         (array-total-size-function
+           (env:fdefinition client e5 'array-total-size))
+         (posix-package (find-package '#:sicl-posix-high))
+         (symbol-name (symbol-name '#:write)))
+    (when (null posix-package)
+      (setf posix-package (make-package '#:sicl-posix-high :use '(#:common-lisp)))
+      (export (intern symbol-name posix-package) posix-package))
+    (let ((write-symbol (find-symbol symbol-name posix-package)))
+      (setf (env:fdefinition (env:client e5) e5 write-symbol)
+            (lambda (file-descriptor vector &key (start 0) (end nil))
+              (let ((stream (ecase file-descriptor
+                              (1 *standard-output*)
+                              (2 *error-output*)))
+                    (end (if (null end)
+                             (funcall array-total-size-function vector)
+                             end)))
+                (loop for index from start below end
+                      for element = (funcall aref-function vector index)
+                      do (write-char (code-char element) stream))
+                (finish-output stream)))))))
+
 (defun enable-printing (e5)
   (load-source-file "Printer/variables.lisp" e5))
