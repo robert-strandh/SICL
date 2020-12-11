9ff299578fee2d19329e5c63d1e14dd5233997a4
Robert Strandh <robert.strandh@gmail.com>
2020-12-11 09:54:24 +0100

    Call main boot entry point in phase 6.

--- Code/Boot/boot.lisp
+++ Code/Boot/boot.lisp
@@ -17,5 +17,5 @@
     (sicl-boot-phase-3:boot boot)
     (sicl-boot-phase-4:boot boot)
     (sicl-boot-phase-5:boot boot)
-    ;; (sicl-boot-phase-6:boot boot)
+    (sicl-boot-phase-6:boot boot)
     boot))
