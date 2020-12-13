3453ce09410b072292dd06668c7bd4f9e8df00af
Robert Strandh <robert.strandh@gmail.com>
2020-12-13 09:50:25 +0100

    New component containing definitions of functions SYMBOL-VALUE, (SETF SYMBOL-VALUE), and MAKUNBOUND

---
+++ Code/Package-and-symbol/symbol-value-etc-defuns.lisp
@@ -0,0 +1,18 @@
+(cl:in-package #:sicl-symbol)
+
+(let ((variable-cell-function #'variable-cell))
+  (defun symbol-value (symbol)
+    (let ((cell (funcall variable-cell-function symbol)))
+      (sicl-run-time:symbol-value symbol cell)))
+
+  (defun (setf symbol-value) (new-value symbol)
+    (let ((cell (funcall variable-cell-function symbol)))
+      (setf (sicl-run-time:symbol-value symbol cell)
+            new-value)))
+
+  (defun makunbound (symbol)
+    (let ((cell (funcall variable-cell-function symbol)))
+      (sicl-run-time:makunbound symbol cell))))
+    
+    
+  
