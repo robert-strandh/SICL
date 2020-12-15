c8462f37a54e2714592c584c55b0e77480f5fbef
Robert Strandh <robert.strandh@gmail.com>
2020-12-15 17:30:06 +0100

    Define accessor using DEFGENERIC.

--- Code/Structure/structure-class-defclass.lisp
+++ Code/Structure/structure-class-defclass.lisp
@@ -1,5 +1,7 @@
 (cl:in-package #:sicl-structure)
 
+(defgeneric has-standard-constructor (structure-class))
+
 (defclass structure-class (sicl-clos:regular-class)
   ((%has-standard-constructor :initarg :has-standard-constructor
                               :reader has-standard-constructor))
