e256766cd286a510142528b5922d860ef595c6f1
Robert Strandh <robert.strandh@gmail.com>
2020-12-16 10:58:02 +0100

    Add definition of standard function BOUNDP.

--- Code/Package-and-symbol/symbol-value-etc-defuns.lisp
+++ Code/Package-and-symbol/symbol-value-etc-defuns.lisp
@@ -10,9 +10,10 @@
       (setf (sicl-run-time:symbol-value symbol cell)
             new-value)))
 
+  (defun boundp (symbol)
+    (let ((cell (funcall variable-cell-function symbol)))
+      (sicl-run-time:boundp symbol cell)))
+
   (defun makunbound (symbol)
     (let ((cell (funcall variable-cell-function symbol)))
       (sicl-run-time:makunbound symbol cell))))
-    
-    
-  
