6bf91ba6fc49c3aa09a97749b1eaa2f2c401e5e0
Robert Strandh <robert.strandh@gmail.com>
2020-12-23 09:48:37 +0100

    Add slot containing slot location or NIL.

--- Code/CLOS/standard-accessor-method-defclass.lisp
+++ Code/CLOS/standard-accessor-method-defclass.lisp
@@ -35,8 +35,14 @@
 ;;;
 ;;; Class STANDARD-ACCESSOR-METHOD.
 
+(defgeneric slot-location (standard-accessor-method))
+
 (defclass standard-accessor-method (standard-method)
   ((%slot-definition 
     :initarg :slot-definition
     :type direct-slot-definition
-    :accessor accessor-method-slot-definition)))
+    :accessor accessor-method-slot-definition)
+   (%slot-location
+    :initarg :slot-location
+    :initform nil
+    :reader slot-location)))
