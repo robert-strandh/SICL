217e4a675b8757800cef2a50d58b39bdb8654ccb
Robert Strandh <robert.strandh@gmail.com>
2020-12-23 09:44:43 +0100

    Provide default method on UPDATE-INSTANCE-FOR-DIFFERENT-CLASS.

--- Code/CLOS/update-instance-defgenerics.lisp
+++ Code/CLOS/update-instance-defgenerics.lisp
@@ -8,6 +8,20 @@
      &key
      &allow-other-keys))
 
+(defmethod update-instance-for-different-class
+    ((previous standard-object) (current standard-object) &rest initargs)
+  (let ((from-class-slots (class-slots (class-of previous)))
+        (to-class-slots (class-slots (class-of current)))
+        (added-slot-names '()))
+    (loop for to-slot in to-class-slots
+          for name = (slot-definition-name to-slot)
+          do (when (and (eq (slot-definition-allocation to-slot) :instance)
+                        (null (find name from-class-slots
+                                    :test #'eq
+                                    :key #'slot-definition-name)))
+               (push name added-slot-names)))
+    (apply #'shared-initialize current added-slot-names initargs)))
+
 ;;; For the specification of this generic function, see
 ;;; http://www.lispworks.com/documentation/HyperSpec/Body/f_upda_1.htm#update-instance-for-redefined-class
 (defgeneric update-instance-for-redefined-class
