198b42cef03bc4caf9f2164b75b02e668517289a
Robert Strandh <robert.strandh@gmail.com>
2020-12-26 11:01:02 +0100

    Load method-combination expanders rather than having the host evaluate them.

--- Code/Boot/Phase-5/enable-method-combinations.lisp
+++ Code/Boot/Phase-5/enable-method-combinations.lisp
@@ -2,9 +2,15 @@
 
 (defun enable-method-combinations (e5)
   (import-functions-from-host
-   '(sicl-method-combination:define-method-combination-expander
+   '(;; sicl-method-combination:define-method-combination-expander
+     sicl-method-combination::lambda-list-variables
      sicl-loop::list-car sicl-loop::list-cdr)
    e5)
+  (load-source-file "Method-combination/method-group-specifier.lisp" e5)
+  (load-source-file "Method-combination/method-discriminator.lisp" e5)
+  (load-source-file "Method-combination/long-form-expansion.lisp" e5)
+  (load-source-file "Method-combination/short-form-expansion.lisp" e5)
+  (load-source-file "Method-combination/define-method-combination-support.lisp" e5)
   (load-source-file "Method-combination/accessor-defgenerics.lisp" e5)
   (load-source-file "Method-combination/method-combination-template-defclass.lisp" e5)
   (load-source-file "Method-combination/find-method-combination.lisp" e5)
