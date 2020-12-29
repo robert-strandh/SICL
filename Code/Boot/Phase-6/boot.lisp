be550a433fbc4b93606a1d83b6f0a36e786333c3
Robert Strandh <robert.strandh@gmail.com>
2020-12-29 16:28:11 +0100

    Call function for enabling DEFTYPE from main entry point of phase 6.

--- Code/Boot/Phase-6/boot.lisp
+++ Code/Boot/Phase-6/boot.lisp
@@ -15,5 +15,6 @@
     (load-source-file "CLOS/update-instance-defgenerics.lisp" e5)
     (load-source-file "CLOS/change-class-defgenerics.lisp" e5)
     (load-source-file "CLOS/built-in-method-combinations.lisp" e5)
+    (enable-deftype e5)
     (enable-printing e5)
     (enable-conditions e5)))
