cde11838d7f419fa4f605ce77753218ce34cde5c
Robert Strandh <robert.strandh@gmail.com>
2020-12-31 16:04:26 +0100

    Update definition according to Clostrum-based global environments.

--- Code/Data-and-control-flow/define-setf-expander.lisp
+++ Code/Data-and-control-flow/define-setf-expander.lisp
@@ -1,7 +1,5 @@
 (cl:in-package #:sicl-data-and-control-flow)
 
 (defmacro define-setf-expander (&environment env name lambda-list &body body)
-  `(let ((expander ,(cleavir-code-utilities:parse-macro name lambda-list body))
-	 (global-env ,(sicl-genv:global-environment env)))
-     (setf (sicl-global-environment:setf-expander ',name global-env)
-	   expander)))
+  `(let ((expander ,(cleavir-code-utilities:parse-macro name lambda-list body)))
+     (setf (setf-expander ',name) expander)))
