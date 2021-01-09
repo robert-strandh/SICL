e9e6d67a827c7640fd0431f9f0035592e6dd22f4
Robert Strandh <robert.strandh@gmail.com>
2021-01-09 15:06:16 +0100

    Update defininition for Clostrum-based environments.

--- Code/Data-and-control-flow/fboundp-defun.lisp
+++ Code/Data-and-control-flow/fboundp-defun.lisp
@@ -1,6 +1,18 @@
 (cl:in-package #:sicl-data-and-control-flow)
 
-(defun fboundp (function-name)
-  (sicl-global-environment:fboundp
-   function-name
-   (sicl-global-environment:global-environment)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;; Function FBOUNDP.
+;;;
+;;; According to the HyperSpec, this function should return any true
+;;; value if the name is fbound in the global environment.  From the
+;;; glossary, we learn that "fbound" means that the name has a
+;;; definition as either a function, a macro, or a special operator in
+;;; the global environment.
+
+(let* ((environment (sicl-environment:global-environment))
+       (client (sicl-environment:client environment))
+       (fboundp (fdefinition 'sicl-environment:fboundp)))
+  (defun fboundp (function-name)
+    (funcall fboundp client environment function-name)))
