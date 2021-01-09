9e524d2cd077a8787e05ced5debe7145ff0ba673
Robert Strandh <robert.strandh@gmail.com>
2021-01-09 15:25:38 +0100

    Update defininition for Clostrum-based environments.

--- Code/Data-and-control-flow/fmakunbound-defun.lisp
+++ Code/Data-and-control-flow/fmakunbound-defun.lisp
@@ -1,6 +1,8 @@
 (cl:in-package #:sicl-data-and-control-flow)
 
-(defun fmakunbound (function-name)
-  (sicl-global-environment:fmakunbound
-   function-name
-   (sicl-global-environment:global-environment)))
+(let* ((environment (sicl-environment:global-environment))
+       (client (sicl-environment:client environment))
+       (fmakunbound (fdefinition 'sicl-environment:fmakunbound)))
+  (defun fmakunbound (function-name)
+    (funcall fmakunbound client environment function-name)
+    function-name))
