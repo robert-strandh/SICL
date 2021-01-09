3bc4dc4a782e779163361dca76a3de8ee3b5b2c3
Robert Strandh <robert.strandh@gmail.com>
2021-01-09 16:09:49 +0100

    Update defininition for Clostrum-based environments.

--- Code/Data-and-control-flow/setf-fdefinition-defun.lisp
+++ Code/Data-and-control-flow/setf-fdefinition-defun.lisp
@@ -1,7 +1,8 @@
 (cl:in-package #:sicl-data-and-control-flow)
 
-(defun (setf fdefinition) (new-definition function-name)
-  (setf (sicl-global-environment:fdefinition
-	 function-name
-	 (sicl-global-environment:global-environment))
-	new-definition))
+(let* ((environment (sicl-environment:global-environment))
+       (client (sicl-environment:client environment))
+       (setf-fdefinition (fdefinition '(setf sicl-environment:fdefinition))))
+  (defun (setf fdefinition) (new-definition function-name)
+    (funcall setf-fdefinition new-definition client environment function-name)
+    new-definition))
