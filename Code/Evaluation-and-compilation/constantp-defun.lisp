7f8fdbb0255fa698523480fc88b481f144ce578a
Robert Strandh <robert.strandh@gmail.com>
2021-01-10 09:23:41 +0100

    Update definition of function CONSTANTP to access environment only at load time.

--- Code/Evaluation-and-compilation/constantp-defun.lisp
+++ Code/Evaluation-and-compilation/constantp-defun.lisp
@@ -1,13 +1,14 @@
 (cl:in-package #:sicl-evaluation-and-compilation)
 
-(defun constantp (form &optional environment)
-  (let* ((global-environment (sicl-environment:global-environment environment))
-         (client (sicl-environment:client global-environment)))
+(let* ((global-environment (sicl-environment:global-environment environment))
+       (client (sicl-environment:client global-environment))
+       (constant-variable-function (fdefinition 'sicl-environment:constant-variable)))
+  (defun constantp (form &optional environment)
     (or (and (not (symbolp form))
              (not (consp form)))
         (keywordp form)
         (and (symbolp form)
-             (nth-value 1 (sicl-environment:constant-variable
-                           client global-environment form)))
+             (nth-value 1 (funcall constant-variable-function
+                                   client global-environment form)))
         (and (consp form)
              (eq (car form) 'quote)))))
