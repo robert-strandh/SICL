61d0fc0d804c312850d736ff1f005877a1fd1671
Robert Strandh <robert.strandh@gmail.com>
2021-01-12 17:29:40 +0100

    Remove obsolete component.

--- Code/Environment/declaim.lisp
+++
@@ -1,7 +0,0 @@
-(cl:in-package #:sicl-global-environment)
-
-(defmacro declaim (&rest declaration-specifiers)
-  `(eval-when (:compile-toplevel :load-toplevel :execute)
-     (progn ,@(mapcar #'(lambda (declaration-specifier)
-			  `(proclaim ',declaration-specifier))
-		      declaration-specifiers))))
