3abec8e2ddc1e6d4865784e45eb67e72e1032847
Robert Strandh <robert.strandh@gmail.com>
2021-01-13 14:34:04 +0100

    Remove obsolete definition of macro DEFCONSTANT.

--- Code/Environment/macro-support.lisp
+++ Code/Environment/macro-support.lisp
@@ -29,29 +29,6 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;
-;;; Macro DEFCONSTANT.
-;;;
-;;; The HyperSpec says that we have a choice as to whether the
-;;; initial-value form is evaluated at compile-time, at load-time, or
-;;; both, but that in either case, the compiler must recognize the
-;;; name as a constant variable.  We have chosen to evaluate it both
-;;; at compile-time and at load-time.  We evaluate it at compile time
-;;; so that successive references to the variable can be replaced by
-;;; the value.
-;;;
-;;; This is not the final version of the macro.  For one thing, we
-;;; need to handle the optional DOCUMENTATION argument.
-
-(defun defconstant-expander (name initial-value documentation)
-  (declare (ignore documentation))
-  `(eval-when (:compile-toplevel :load-toplevel :execute)
-     (setf (sicl-genv:constant-variable
-            ',name
-            (load-time-value (sicl-genv:global-environment)))
-           ,initial-value)))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;;
 ;;; Macro DEFVAR.
 ;;;
 ;;; The HyperSpec says that when DEFVAR is processed as a top-level
--- Code/Environment/standard-environment-macros.lisp
+++ Code/Environment/standard-environment-macros.lisp
@@ -29,24 +29,6 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;
-;;; Macro DEFCONSTANT.
-;;;
-;;; The HyperSpec says that we have a choice as to whether the
-;;; initial-value form is evaluated at compile-time, at load-time, or
-;;; both, but that in either case, the compiler must recognize the
-;;; name as a constant variable.  We have chosen to evaluate it both
-;;; at compile-time and at load-time.  We evaluate it at compile time
-;;; so that successive references to the variable can be replaced by
-;;; the value.
-;;;
-;;; This is not the final version of the macro.  For one thing, we
-;;; need to handle the optional DOCUMENTATION argument.
-
-(defmacro defconstant (name initial-value &optional documentation)
-  (defconstant-expander name initial-value documentation))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;;
 ;;; Macro DEFVAR.
 ;;;
 ;;; The HyperSpec says that when DEFVAR is processed as a top-level
