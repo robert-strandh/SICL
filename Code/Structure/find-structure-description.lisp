fbdf398c5c3a841ddd4a2e885b70c5440c4d80c4
Robert Strandh <robert.strandh@gmail.com>
2021-01-14 16:06:29 +0100

    Update code for Clustrum-based environments.

--- Code/Structure/find-structure-description.lisp
+++ Code/Structure/find-structure-description.lisp
@@ -1,13 +1,30 @@
 (cl:in-package #:sicl-structure)
 
-(defun find-structure-description (name &optional (errorp t) environment)
-  (let* ((global-environment (sicl-genv:global-environment environment))
-         (description (sicl-genv:structure-description name global-environment)))
-    (if (and (null description) errorp)
-	(error 'undefined-structure-description :name name)
-	description)))
+(let* ((environment (sicl-environment:global-environment))
+       (client (sicl-environment:client environment))
+       (global-environment-function
+         (sicl-environment:fdefinition
+          client environment 'sicl-environment:global-environment))
+       (structure-description-function
+         (sicl-environment:fdefinition
+          client environment 'sicl-environment:structure-description))
+       (setf-structure-description-function
+         (sicl-environment:fdefinition
+          client environment '(setf sicl-environment:structure-description))))
+  (defun find-structure-description (name &optional (errorp t) environment)
+    (let* ((global-environment
+             (funcall global-environment-function environment))
+           (description
+             (funcall structure-description-function
+                      name global-environment)))
+      (if (and (null description) errorp)
+          (error 'undefined-structure-description :name name)
+          description)))
 
-(defun (setf find-structure-description) (new-value name &optional (errorp t) environment)
-  (declare (ignore errorp))
-  (let ((global-environment (sicl-genv:global-environment environment)))
-    (setf (sicl-genv:structure-description name global-environment) new-value)))
+  (defun (setf find-structure-description)
+      (new-value name &optional (errorp t) environment)
+    (declare (ignore errorp))
+    (let ((global-environment
+            (funcall global-environment-function environment)))
+      (funcall setf-structure-description-function
+               new-value name global-environment-function))))
