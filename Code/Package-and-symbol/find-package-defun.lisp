619e67563c6f3b3c470bf89c6c904e700315aaed
Robert Strandh <robert.strandh@gmail.com>
2021-01-14 15:01:15 +0100

    Adapt code for use with Clostrum.

--- Code/Package-and-symbol/find-package-defun.lisp
+++ Code/Package-and-symbol/find-package-defun.lisp
@@ -1,6 +1,10 @@
 (cl:in-package #:sicl-package)
 
-(defun find-package (name)
-  (if (typep name 'package)
-      name
-      (sicl-genv:find-package (string name) (sicl-genv:global-environment))))
+(let* ((environment (sicl-environment:global-environment))
+       (client (sicl-environment:client environment))
+       (find-package-function
+         (sicl-environment:fdefinition client environment 'find-package)))
+  (defun find-package (name)
+    (if (typep name 'package)
+        name
+        (funcall find-package-function client environment name))))
