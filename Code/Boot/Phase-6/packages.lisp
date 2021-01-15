ecb0c0da770b4b16c72eebbb8e1a3b0e334a7b42
Robert Strandh <robert.strandh@gmail.com>
2021-01-15 15:46:44 +0100

    Import name of function LOAD-ASDF-SYSTEM from package SICL-BOOT.

--- Code/Boot/Phase-6/packages.lisp
+++ Code/Boot/Phase-6/packages.lisp
@@ -7,5 +7,6 @@
                 #:import-functions-from-host
                 #:define-error-functions
                 #:load-source-file
+                #:load-asdf-system
                 #:with-intercepted-function-cells)
   (:export #:boot))
