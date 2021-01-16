7a62e53801a9e127927ad3402933211e2f844a77
Robert Strandh <robert.strandh@gmail.com>
2021-01-16 10:18:56 +0100

    Import functions LOG and SQRT from the host.

--- Code/Boot/environment.lisp
+++ Code/Boot/environment.lisp
@@ -178,6 +178,7 @@
      plusp minusp integerp numberp
      logand logior logxor ash integer-length
      zerop evenp max expt abs ldb byte random
+     log sqrt
      ;; CONSes
      cons list list* append nconc make-list copy-list subst
      consp atom listp null endp
