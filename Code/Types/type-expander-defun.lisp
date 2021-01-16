d7f672d57fa14421b707e054ff653812a55bbd3f
Robert Strandh <robert.strandh@gmail.com>
2021-01-16 09:54:33 +0100

    Rewrite in a more idiomatic style.

--- Code/Types/type-expander-defun.lisp
+++ Code/Types/type-expander-defun.lisp
@@ -11,11 +11,10 @@
 ;;; the environment are closed over at load time so that they can be
 ;;; undefined after this definition has been loaded.
 
-(setf (fdefinition 'type-expander)
-      (load-time-value
-       (let* ((environment (sicl-environment:global-environment))
-              (client (sicl-environment:client environment))
-              (type-expander
-                (fdefinition 'sicl-environment:type-expander)))
-         (lambda (type-descriptor)
-           (funcall type-expander client environment type-descriptor)))))
+(let* ((environment (sicl-environment:global-environment))
+       (client (sicl-environment:client environment))
+       (type-expander-function
+         (sicl-environment:fdefinition
+          client environment 'sicl-environment:type-expander)))
+  (defun type-expander (type-descriptor)
+    (funcall type-expander-function client environment type-descriptor)))
