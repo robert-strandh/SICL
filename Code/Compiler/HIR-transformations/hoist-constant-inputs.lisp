77a5ea48fe006c97fa2a71cf7775f621fedd673e
Robert Strandh <robert.strandh@gmail.com>
2021-01-31 16:08:27 +0100

    Add comment.

--- Code/Compiler/HIR-transformations/hoist-constant-inputs.lisp
+++ Code/Compiler/HIR-transformations/hoist-constant-inputs.lisp
@@ -17,6 +17,36 @@
 (defmethod trivial-constant-p ((constant character))
   (< (char-code constant) #.(expt 2 29)))
 
+;;; This function should be renamed.  It used to be the case that
+;;; constants were hoisted to the top level so that they became part
+;;; of the static environment of the top-level function.  Then,
+;;; constants used by nested functions were transmitted through the
+;;; static environments when closures for those nested functions were
+;;; created.  But we changed the handling of constants.
+;;;
+;;; They will now be allocated in a vector on the global heap.  Since
+;;; the global heap does not move objects, the distance between a
+;;; particular constant and the instruction that needs it does not
+;;; change.  So we can use PC-relative addressing to load the
+;;; constant.  This way is now reflected in HIR by the use of the
+;;; LOAD-CONSTANT-INSTRUCTION.
+;;;
+;;; We make the LOCATION-INFO of the LOAD-CONSTANT-INSTRUCTION a CONS
+;;; of two things.  The first thing is the position of the constant in
+;;; what will ultimately become the vector, but during HIR
+;;; transformations, it is a list contained in the
+;;; TOP-LEVEL-ENTER-INSTRUCTION.  This position information is used
+;;; when HIR is transformed into MIR, LIR, etc.  The second thing is
+;;; the constant itself, and it is used only by the HIR evaluator.
+;;;
+;;; The idea is to handle LOAD-TIME-VALUE in a similar way, but we
+;;; haven't worked out the details yet.  Again a
+;;; LOAD-CONSTANT-INSTRUCTION would be used, but constant to load
+;;; would be computed by the top-level function and inserted into the
+;;; vector.  An alternative would be to treat all non-trivial
+;;; constants as LOAD-TIME-VALUEs.  The difference then would be that
+;;; true constants would then be created at load time, rather than
+;;; when the AST is read by the reader.
 (defun hoist-constant-inputs (initial-instruction)
   (with-accessors ((constants constants)
                    (function-names function-names))
