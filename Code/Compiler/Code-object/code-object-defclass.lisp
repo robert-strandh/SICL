9e0f2f9cc82a1cf0e023ca2127bd773f3c609a9c
Robert Strandh <robert.strandh@gmail.com>
2021-02-02 11:25:02 +0100

    Move definitions of slot accessors to the component containing the definition of the class.

--- Code/Compiler/Code-object/code-object-defclass.lisp
+++ Code/Compiler/Code-object/code-object-defclass.lisp
@@ -1,14 +1,42 @@
 (cl:in-package #:sicl-compiler)
 
+(defgeneric instructions (code-object))
+
+(defgeneric (setf instructions) (instructions code-object))
+
+(defgeneric frame-maps (code-object))
+
+(defgeneric (setf frame-maps) (frame-maps code-object))
+
+(defgeneric callee-saves-register-maps (code-object))
+
+(defgeneric (setf callee-saves-register-maps) (maps code-object))
+
+(defgeneric callee-saves-stack-maps (code-object))
+
+(defgeneric (setf callee-saves-stack-maps) (maps code-object))
+
+(defgeneric constants (code-object))
+
+(defgeneric (setf constants) (constants code-object))
+
+(defgeneric function-names (code-object))
+
+(defgeneric (setf function-names) (function-names code-object))
+
+(defgeneric ir (code-object))
+
+(defgeneric (setf ir) (ir code-object))
+
 (defclass code-object ()
-  ((%instructions :initarg :instructions :reader instructions)
-   (%frame-maps :initarg :frame-maps :reader frame-maps)
+  ((%instructions :initarg :instructions :accessor instructions)
+   (%frame-maps :initarg :frame-maps :accessor frame-maps)
    (%callee-saves-register-maps
     :initarg :callee-saves-register-maps
-    :reader callee-saves-register-maps)
+    :accessor callee-saves-register-maps)
    (%callee-saves-stack-maps
     :initarg :callee-saves-stack-maps
-    :reader callee-saves-stack-maps)
+    :accessor callee-saves-stack-maps)
    (%constants :initform '() :initarg :constants :accessor constants)
-   (%function-names :initarg :function-names :reader function-names)
-   (%hir :initarg :hir :reader hir)))
+   (%function-names :initarg :function-names :accessor function-names)
+   (%ir :initarg :ir :accessor ir)))
--- Code/Compiler/Code-object/generic-functions.lisp
+++ Code/Compiler/Code-object/generic-functions.lisp
@@ -1,17 +1,2 @@
 (cl:in-package #:sicl-compiler)
 
-(defgeneric instructions (code-object))
-
-(defgeneric frame-maps (code-object))
-
-(defgeneric callee-saves-register-maps (code-object))
-
-(defgeneric callee-saves-stack-maps (code-object))
-
-(defgeneric constants (code-object))
-
-(defgeneric (setf constants) (constants code-object))
-
-(defgeneric function-names (code-object))
-
-(defgeneric hir (code-object))
