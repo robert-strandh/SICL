cb622dea7a447e6cf5aa7617f6bc830c6eba560e
Robert Strandh <robert.strandh@gmail.com>
2021-02-04 10:39:05 +0100

    Call new versions of functions HOIST-FDEFINITIONS and PROCESS-CONSTANTS.

--- Code/Compiler/AST-compiler/ast-compiler.lisp
+++ Code/Compiler/AST-compiler/ast-compiler.lisp
@@ -19,10 +19,10 @@
     (sicl-hir-transformations:preprocess-multiple-value-call-instructions hir)
     (sicl-hir-transformations:preprocess-unwind-instructions hir)
     (sicl-hir-transformations:find-named-call-instructions  hir)
-    (sicl-hir-transformations:hoist-fdefinitions hir)
+    (hoist-fdefinitions code-object)
     (sicl-hir-transformations:eliminate-fixed-to-multiple-instructions hir)
     (sicl-hir-transformations:eliminate-multiple-to-fixed-instructions hir)
-    (sicl-hir-transformations:process-constant-inputs hir)
+    (process-constant-inputs code-object)
     (cleavir-hir-transformations::process-captured-variables hir)
     ;; Replacing aliases does not appear to have a great effect when
     ;; code generation is disabled.  Try removing this commented line
