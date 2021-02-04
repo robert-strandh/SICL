b60d80cd142f39f69f749674fb99fe63b20b3fea
Robert Strandh <robert.strandh@gmail.com>
2021-02-04 18:18:59 +0100

    Fill function-cell cells of call sites when tying.

--- Code/Compiler/AST-compiler/tie-code-object.lisp
+++ Code/Compiler/AST-compiler/tie-code-object.lisp
@@ -6,6 +6,12 @@
          (function-cell-function
            (sicl-environment:fdefinition
             client environment 'sicl-data-and-control-flow:function-cell)))
+    (loop for call-site in (call-sites code-object)
+          for instruction = (instruction call-site)
+          for cell = (sicl-ir:function-cell-cell instruction)
+          for name = (name call-site)
+          do (setf (car cell)
+                   (funcall function-cell-function name)))
     (funcall (hir-thunks code-object)
              (apply #'vector
                     ;; FIXME: remove this element.
