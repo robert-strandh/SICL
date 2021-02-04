052c70922c9f4ec1fa3d30668ed7e8339073eb30
Robert Strandh <robert.strandh@gmail.com>
2021-02-04 16:31:56 +0100

    Change definition of class and add comment explaining the change.

--- Code/Compiler/Intermediate-representation/named-call-instruction.lisp
+++ Code/Compiler/Intermediate-representation/named-call-instruction.lisp
@@ -1,9 +1,26 @@
 (cl:in-package #:sicl-ir)
 
-;;; This instruction class adds a function cell slot to the ordinary
-;;; NAMED-CALL-INSTRUCTION for the purpose of the HIR evaluator.  When
-;;; the code is tied to an environment, we go through all the
+;;; This instruction class adds a slot to the ordinary
+;;; NAMED-CALL-INSTRUCTION for the purpose of the HIR evaluator.  This
+;;; slot serves the purpose of a double indirection.
+;;;
+;;; When the HIR evaluator processes instructions, the code object has
+;;; not yet been tied to any environment, so the function cells are
+;;; unknown.  If we stored the function cell directly in this
+;;; instruction, the HIR evaluator would have to close over the
+;;; instruction, which is not desirable, because the instruction will
+;;; change when HIR is transformed to MIR, LIR, etc.  So instead we
+;;; store a CONS cell in the instruction, and the CAR of that CONS
+;;; cell will ultimately contain the function cell, i.e. after the
+;;; code object has been tied.  The HIR evaluator closes over this
+;;; cell.
+;;;
+;;; So when we establish call sites, we find all the
 ;;; NAMED-CALL-INSTRUCTIONs, change their classes to this one, and
-;;; store the function cell corresponding to the name in the new slot.
+;;; store a CONS cell in the new slot.  When the code object is tied
+;;; to the environment, we store the function cell in the CAR of this
+;;; cell.
 (defclass named-call-instruction (cleavir-ir:named-call-instruction)
-  ((%function-cell :initarg :function-cell :reader function-cell)))
+  ((%function-cell-cell
+    :initarg :function-cell-cell
+    :reader function-cell-cell)))
