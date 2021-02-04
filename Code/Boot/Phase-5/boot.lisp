bf3290e16f40042490291806b60eccd786fb24ff
Robert Strandh <robert.strandh@gmail.com>
2021-02-04 10:40:29 +0100

    Remove unused function AST-EVAL.

--- Code/Boot/Phase-5/boot.lisp
+++ Code/Boot/Phase-5/boot.lisp
@@ -1,25 +1,5 @@
 (cl:in-package #:sicl-boot-phase-5)
 
-(defun ast-eval (ast client environment)
-  (let* ((global-environment (trucler:global-environment client environment))
-         (hir (sicl-ast-to-hir:ast-to-hir client ast))
-         (fun (sicl-hir-evaluator:top-level-hir-to-host-function client hir))
-         (sicl-run-time:*dynamic-environment* '())
-         (function-cell-function
-           (sicl-environment:fdefinition
-            client global-environment 'sicl-data-and-control-flow:function-cell)))
-    (funcall fun
-             (apply #'vector
-                    nil ; Ultimately, replace with code object.
-                    #'sicl-hir-evaluator:enclose
-                    #'sicl-hir-evaluator:initialize-closure
-                    #'cons
-                    nil
-                    (append (loop with names = (sicl-hir-transformations:function-names hir)
-                                  for name in names
-                                  collect (funcall function-cell-function name))
-                            (sicl-hir-transformations:constants hir))))))
-
 (defun boot (boot)
   (format *trace-output* "Start phase 5~%")
   (with-accessors ((e0 sicl-boot:e0)
