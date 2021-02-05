2bb6dfc5850f75ba5a6844d26278cf0f3b704c45
Robert Strandh <robert.strandh@gmail.com>
2021-02-05 17:55:27 +0100

    Update comment to reflect new way of handling named calls.

--- Code/Compiler/HIR-to-MIR/augment-catch-instruction.lisp
+++ Code/Compiler/HIR-to-MIR/augment-catch-instruction.lisp
@@ -1,14 +1,11 @@
 (cl:in-package #:sicl-hir-to-mir)
 
 ;;; We want to handle the CATCH-INSTRUCTION in a way similar to the
-;;; way we handle the FUNCALL-INSTRUCTION.  But in HIR, the HIR
-;;; interpreter makes it hard to do so, because the CATCH-INSTRUCTION
-;;; has to do weird things with CL:CATCH.  In HIR, we were able to add
-;;; an input to the CATCH-INSTRUCTION, containing a run-time function
-;;; SICL-RUN-TIME:AUGMENT-WITH-BLOCK/TAGBODY-ENTRY.  We used the
-;;; FDEFINITION-INSTRUCTION to find that function, and that
-;;; FDEFINITION-INSTRUCTION was then hoisted, so that the function is
-;;; now fetched from the static environment.
+;;; way we handle the NAMED-CALL-INSTRUCTION.  But in HIR, the HIR
+;;; evaluator makes it hard to do so, because the CATCH-INSTRUCTION
+;;; has to do weird things with CL:CATCH.  In HIR, we were able to
+;;; establish the CATCH-INSTRUCTION as a call site, calling the
+;;; function SICL-RUN-TIME:AUGMENT-WITH-BLOCK/TAGBODY-ENTRY.
 ;;;
 ;;; The function SICL-RUN-TIME:AUGMENT-WITH-BLOCK/TAGBODY-ENTRY
 ;;; returns two values, corresponding to the outputs of the
@@ -17,7 +14,7 @@
 ;;;
 ;;; However, we were not able to add RETURN-VALUE-INSTRUCTIONs
 ;;; following the CATCH-INSTRUCTION, simply because the HIR
-;;; interpreter, when interpreting the CATCH-INSTRUCTION, needs the
+;;; evaluator, when executing the CATCH-INSTRUCTION, needs the
 ;;; continuation to be available, so it would be too late go create it
 ;;; as output of a RETURN-VALUE-INSTRUCTION.  It is convenient if
 ;;; those RETURN-VALUE-INSTRUCTIONs exist when we turn MIR into LIR.
@@ -26,10 +23,10 @@
 ;;;
 ;;; So we remove the outputs of the CATCH-INSTRUCTION and attach them
 ;;; to the RETURN-VALUE-INSTRUCTIONs instead, and we insert the
-;;; RETURN-VALUE-INSTRUCTIONs in the first child of the
+;;; RETURN-VALUE-INSTRUCTIONs in the first successor of the
 ;;; CATCH-INSTRUCTION.  But we do not turn the CATCH-INSTRUCTION into
 ;;; a FUNCALL-INSTRUCTION, because we still want to keep the
-;;; additional children of the CATCH-INSTRUCTION.
+;;; additional successors of the CATCH-INSTRUCTION.
 
 (defmethod process-instruction
     (client (instruction cleavir-ir:catch-instruction))
