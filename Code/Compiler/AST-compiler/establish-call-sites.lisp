8f24aecc4fd586e6770a07d9ef04cc48591badda
Robert Strandh <robert.strandh@gmail.com>
2021-02-05 15:46:25 +0100

    Bug fix: use TYPECASE for each CLEAVIR-IR instruction type.

--- Code/Compiler/AST-compiler/establish-call-sites.lisp
+++ Code/Compiler/AST-compiler/establish-call-sites.lisp
@@ -3,17 +3,13 @@
 (defun establish-call-sites (code-object)
   (cleavir-ir:map-instructions-arbitrary-order
    (lambda (instruction)
-     (when (typep instruction 'sicl-ir:named-call-mixin)
-       (let ((function-cell-cell (list nil))
-             name)
-         (etypecase instruction
-           (cleavir-ir:named-call-instruction
-            (setf name (cleavir-ir:callee-name instruction))
-            (change-class instruction
-                          'sicl-ir:named-call-instruction
-                          :function-cell-cell function-cell-cell)))
-         (push (make-instance 'call-site
-                 :name name
-                 :instruction instruction)
-               (call-sites code-object)))))
+     (typecase instruction
+       (cleavir-ir:named-call-instruction
+        (change-class instruction
+                      'sicl-ir:named-call-instruction
+                      :function-cell-cell (list nil))
+        (push (make-instance 'call-site
+                :name (cleavir-ir:callee-name instruction)
+                :instruction instruction)
+              (call-sites code-object)))))
    (ir code-object)))
