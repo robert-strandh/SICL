eb74004fe21ed0fefa2ddcabb35fc432452c279b
Robert Strandh <robert.strandh@gmail.com>
2021-02-06 11:45:51 +0100

    Establish call sites for BIND-INSTRUCTIONs.

--- Code/Compiler/AST-compiler/establish-call-sites.lisp
+++ Code/Compiler/AST-compiler/establish-call-sites.lisp
@@ -19,5 +19,13 @@
         (push (make-instance 'call-site
                 :name 'sicl-run-time:augment-with-block/tagbody-entry
                 :instruction instruction)
+              (call-sites code-object)))
+       (cleavir-ir:bind-instruction
+        (change-class instruction
+                      'sicl-ir:bind-instruction
+                      :function-cell-cell (list nil))
+        (push (make-instance 'call-site
+                :name 'sicl-run-time:augment-with-special-variable-entry
+                :instruction instruction)
               (call-sites code-object)))))
    (ir code-object)))
