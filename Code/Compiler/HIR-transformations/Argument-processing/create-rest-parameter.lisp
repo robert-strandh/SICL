b11941e4912ea84e30f2a8e8461d497e88572063
Robert Strandh <robert.strandh@gmail.com>
2021-02-07 10:47:27 +0100

    Use new instruction class NAMED-CALL-INSTRUCTION to call function CONS.

--- Code/Compiler/HIR-transformations/Argument-processing/create-rest-parameter.lisp
+++ Code/Compiler/HIR-transformations/Argument-processing/create-rest-parameter.lisp
@@ -4,11 +4,9 @@
                               rest-parameter-location
                               dynamic-environment-location
                               first-index)
-  (let ((cons-location (make-instance 'cleavir-ir:lexical-location :name (gensym "cons")))
-        (argument-location (make-instance 'cleavir-ir:lexical-location :name (gensym "argument")))
+  (let ((argument-location (make-instance 'cleavir-ir:lexical-location :name (gensym "argument")))
         (index-location (make-instance 'cleavir-ir:lexical-location :name (gensym "index")))
         (constant-1-input (make-instance 'cleavir-ir:constant-input :value 1))
-        (constant-cons-input (make-instance 'cleavir-ir:constant-input :value 'cons))
         (first-index-input (make-instance 'cleavir-ir:constant-input :value first-index))
         (nop (make-instance 'cleavir-ir:nop-instruction
                :dynamic-environment-location dynamic-environment-location)))
@@ -23,9 +21,9 @@
               :successor first
               :dynamic-environment-location dynamic-environment-location))
       (setf first
-            (make-instance 'cleavir-ir:funcall-instruction
-              :inputs
-              (list cons-location argument-location rest-parameter-location)
+            (make-instance 'cleavir-ir:named-call-instruction
+              :callee-name 'cons
+              :inputs (list argument-location rest-parameter-location)
               :successor first
               :dynamic-environment-location dynamic-environment-location))
       (setf first
@@ -46,11 +44,5 @@
               :inputs (list argument-count-location constant-1-input)
               :output index-location
               :successors (list first first)
-              :dynamic-environment-location dynamic-environment-location))
-      (setf first
-            (make-instance 'cleavir-ir:fdefinition-instruction
-              :input constant-cons-input
-              :output cons-location
-              :successor first
               :dynamic-environment-location dynamic-environment-location))
       (values first nop))))
