7a3db978cc318461be8578ef8c55791c75cb0fca
Robert Strandh <robert.strandh@gmail.com>
2021-02-08 09:27:58 +0100

    Remove obsolete system COMPILATION-POLICY.

--- Code/Cleavir/Compilation-policy/packages.lisp
+++
@@ -1,9 +0,0 @@
-(cl:in-package #:common-lisp-user)
-
-(defpackage #:cleavir-compilation-policy
-  (:use #:common-lisp)
-  (:nicknames #:cleavir-policy)
-  (:export #:policy-value)
-  (:export #:policy-qualities #:define-cleavir-policy-quality)
-  (:export #:optimize-value #:normalize-optimize)
-  (:export #:compute-policy #:compute-policy-quality))
--- Code/Cleavir/Compilation-policy/policy.lisp
+++
@@ -1,5 +0,0 @@
-(cl:in-package #:cleavir-compilation-policy)
-
-(defun policy-value (policy quality)
-  ;; policies are assumed to be complete
-  (cdr (assoc quality policy)))
