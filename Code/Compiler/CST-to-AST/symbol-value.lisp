5ebf98a26712f8c190c73b55911d9159f4c5d839
Robert Strandh <robert.strandh@gmail.com>
2021-02-09 16:32:53 +0100

    Add FIXME comment,

--- Code/Compiler/CST-to-AST/symbol-value.lisp
+++ Code/Compiler/CST-to-AST/symbol-value.lisp
@@ -1,5 +1,12 @@
 (cl:in-package #:sicl-cst-to-ast)
 
+;;;; FIXME: Since the name of the symbol is known, there should be a
+;;;; LOAD-TIME-VALUE that accesses the variable cell, so that we don't
+;;;; have to do a hash-table lookup at run-time.  So, instead of
+;;;; calling CL:SYMBOL-VALUE, we should call SICL-SYMBOL:VARIABLE-CELL
+;;;; at load time, and SICL-RUN-TIME:SYMBOL-VALUE at run time, passing
+;;;; it the variable cell.
+
 (defmethod cleavir-cst-to-ast:convert-special-variable
     ((client sicl-client:sicl) cst info global-env)
   (make-instance 'cleavir-ast:named-call-ast
