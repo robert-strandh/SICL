f73c73abd8c176199626d9319d134c37cd233507
Robert Strandh <robert.strandh@gmail.com>
2021-02-09 16:28:42 +0100

    Use new AST class NAMED-CALL-AST to call SYMBOL-VALUE and (SETF SYMBOL-VALUE).

--- Code/Compiler/CST-to-AST/symbol-value.lisp
+++ Code/Compiler/CST-to-AST/symbol-value.lisp
@@ -2,10 +2,8 @@
 
 (defmethod cleavir-cst-to-ast:convert-special-variable
     ((client sicl-client:sicl) cst info global-env)
-  (make-instance 'cleavir-ast:call-ast
-    :callee-ast (cleavir-ast:make-ast 'cleavir-ast:fdefinition-ast
-                  :name-ast (cleavir-ast:make-ast 'cleavir-ast:constant-ast
-                              :value 'symbol-value))
+  (make-instance 'cleavir-ast:named-call-ast
+    :callee-name 'symbol-value
     :argument-asts
     (list
      (cleavir-ast:make-ast 'cleavir-ast:constant-ast
@@ -13,10 +11,8 @@
 
 (defmethod cleavir-cst-to-ast:convert-setq-special-variable
     ((client sicl-client:sicl) var-cst form-ast info global-env)
-  (make-instance 'cleavir-ast:call-ast
-    :callee-ast (cleavir-ast:make-ast 'cleavir-ast:fdefinition-ast
-                  :name-ast (cleavir-ast:make-ast 'cleavir-ast:constant-ast
-                              :value '(setf symbol-value)))
+  (make-instance 'cleavir-ast:named-call-ast
+    :callee-name '(setf symbol-value)
     :argument-asts
     (list
      form-ast
