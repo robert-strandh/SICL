c1e81ab966affc3882fb6c74b0561d867339777f
Robert Strandh <robert.strandh@gmail.com>
2021-02-09 16:46:49 +0100

    No longer export name of symbol that is now unused.

--- Code/Compiler/HIR-transformations/packages.lisp
+++ Code/Compiler/HIR-transformations/packages.lisp
@@ -14,8 +14,6 @@
            #:process-constant-inputs
            #:eliminate-fixed-to-multiple-instructions
            #:eliminate-multiple-to-fixed-instructions
-           #:preprocess-unwind-instructions
            #:preprocess-initialize-values-instructions
            #:preprocess-multiple-value-call-instructions
-           #:preprocess-unwind-instructions
            #:eliminate-append-values-instructions))
