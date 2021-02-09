ed9d3ec78bc66a22626108e9fdd65745938b4469
Robert Strandh <robert.strandh@gmail.com>
2021-02-09 14:58:30 +0100

    No longer export names of now unused symbols.

--- Code/Compiler/HIR-evaluator/packages.lisp
+++ Code/Compiler/HIR-evaluator/packages.lisp
@@ -11,8 +11,6 @@
            #:*call-stack*
            #:enclose
            #:initialize-closure
-           #:symbol-value-function
-           #:set-symbol-value-function
            #:fill-environment
            #:instruction-thunk
            #:make-thunk
