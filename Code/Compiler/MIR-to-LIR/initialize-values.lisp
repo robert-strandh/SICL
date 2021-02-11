113291b9208bc178febd1314d1d130afb1e9f468
Robert Strandh <robert.strandh@gmail.com>
2021-02-11 10:15:06 +0100

    Remove obsolete component.

--- Code/Compiler/MIR-to-LIR/initialize-values.lisp
+++
@@ -1,5 +0,0 @@
-(cl:in-package #:sicl-mir-to-lir)
-
-(defmethod process-instruction
-    ((instruction cleavir-ir:initialize-values-instruction) lexical-locations)
-  (process-funcall instruction lexical-locations))
