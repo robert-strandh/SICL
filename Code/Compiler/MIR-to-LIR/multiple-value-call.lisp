f40bb868adff38718f81bd1b9cbf0aeaa499f402
Robert Strandh <robert.strandh@gmail.com>
2021-02-11 18:13:50 +0100

    Remove obsolete component.

--- Code/Compiler/MIR-to-LIR/multiple-value-call.lisp
+++
@@ -1,5 +0,0 @@
-(cl:in-package #:sicl-mir-to-lir)
-
-(defmethod process-instruction
-    ((instruction cleavir-ir:multiple-value-call-instruction) lexical-locations)
-  (process-funcall instruction lexical-locations))
