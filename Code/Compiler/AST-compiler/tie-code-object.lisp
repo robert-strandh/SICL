637cea4dba74b078a97aecd374fb0da236076f5c
Robert Strandh <robert.strandh@gmail.com>
2021-02-12 09:43:34 +0100

    Fill in the function-cell cell only for NAMED-CALL-INSTRUCTIONs.

--- Code/Compiler/AST-compiler/tie-code-object.lisp
+++ Code/Compiler/AST-compiler/tie-code-object.lisp
@@ -8,10 +8,11 @@
             client environment 'sicl-data-and-control-flow:function-cell)))
     (loop for call-site in (call-sites code-object)
           for instruction = (instruction call-site)
-          for cell = (sicl-ir:function-cell-cell instruction)
-          for name = (name call-site)
-          do (setf (car cell)
-                   (funcall function-cell-function name)))
+          when (typep instruction 'sicl-ir:named-call-instruction)
+            do (let ((cell (sicl-ir:function-cell-cell instruction))
+                     (name (name call-site)))
+                 (setf (car cell)
+                       (funcall function-cell-function name))))
     (funcall (hir-thunks code-object)
              (apply #'vector
                     ;; FIXME: remove this element.
