fd618badcb2f2386016393bcd33381f55f9a6890
Robert Strandh <robert.strandh@gmail.com>
2021-02-13 15:46:47 +0100

    Call function INITIALIZE-CLOSURE directly, rather than accessing it in the static environment.

--- Code/Compiler/HIR-evaluator/initialize-closure-instruction.lisp
+++ Code/Compiler/HIR-evaluator/initialize-closure-instruction.lisp
@@ -4,15 +4,13 @@
     (client
      (instruction cleavir-ir:initialize-closure-instruction)
      lexical-environment)
-  (let* ((static-environment-lref (ensure-lref 'static-environment lexical-environment))
-         (inputs (cleavir-ir:inputs instruction))
+  (let* ((inputs (cleavir-ir:inputs instruction))
          (closure-lref (ensure-lref (first inputs) lexical-environment))
          (input-lrefs
            (loop for input in (rest inputs)
                  collect (ensure-lref input lexical-environment))))
     (make-thunk (client instruction lexical-environment)
-      (apply (aref (lref static-environment-lref)
-                   sicl-compiler:+initialize-closure-function-index+)
+      (apply #'initialize-closure
              (lref closure-lref)
              (loop for input-lref in input-lrefs
                    collect (lref input-lref)))
