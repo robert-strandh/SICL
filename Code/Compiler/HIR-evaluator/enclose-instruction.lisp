9787e2fb6b2eef8b32a341dbaea11d5d0ac2af4a
Robert Strandh <robert.strandh@gmail.com>
2021-02-13 16:38:14 +0100

    Simplify by eliminating references to the static environment.

--- Code/Compiler/HIR-evaluator/enclose-instruction.lisp
+++ Code/Compiler/HIR-evaluator/enclose-instruction.lisp
@@ -4,9 +4,7 @@
     (client
      (instruction cleavir-ir:enclose-instruction)
      lexical-environment)
-  (let* ((static-environment-lref
-           (ensure-lref 'static-environment lexical-environment))
-         (enter-fn
+  (let* ((enter-fn
            #'dummy-enter-fn)
          (initializer
            (cleavir-ir:initializer instruction))
@@ -16,13 +14,11 @@
                0
                (length (rest (cleavir-ir:inputs initializer))))))
     (prog1 (make-thunk (client instruction lexical-environment :inputs 0 :outputs 1)
-             (let ((static-environment (lref static-environment-lref)))
-               (setf (output 0)
-                     (enclose enter-fn
-                              nil
-                              env-length
-                              (lexical-locations)))
-               (successor 0)))
+             (setf (output 0)
+                   (enclose enter-fn
+                            env-length
+                            (lexical-locations)))
+             (successor 0))
       (setf enter-fn
             (hir-to-host-function client (cleavir-ir:code instruction) lexical-environment)))))
 
