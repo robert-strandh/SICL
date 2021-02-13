7bafe84841105e05898a2a3f2c06962670ad2f8d
Robert Strandh <robert.strandh@gmail.com>
2021-02-13 16:39:57 +0100

    Remove obsolete constants.

--- Code/Compiler/static-environment.lisp
+++ Code/Compiler/static-environment.lisp
@@ -1,9 +1,3 @@
 (cl:in-package #:sicl-compiler)
 
-(defparameter +code-object-index+ 0)
-
-(defparameter +cons-function-index+ 3)
-
-(defparameter +nil-index+ 4)
-
 (defparameter +first-constant-index+ 5)
