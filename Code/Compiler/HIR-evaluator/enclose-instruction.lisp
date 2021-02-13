5bdcde8435fe0d8b8fe305e3542f38b8efb14e13
Robert Strandh <robert.strandh@gmail.com>
2021-02-13 16:19:31 +0100

    Call ENCLOSE directly rather than through the static environment.

--- Code/Compiler/HIR-evaluator/enclose-instruction.lisp
+++ Code/Compiler/HIR-evaluator/enclose-instruction.lisp
@@ -18,8 +18,7 @@
     (prog1 (make-thunk (client instruction lexical-environment :inputs 0 :outputs 1)
              (let ((static-environment (lref static-environment-lref)))
                (setf (output 0)
-                     (funcall (aref static-environment sicl-compiler:+enclose-function-index+)
-                              enter-fn
+                     (enclose enter-fn
                               (aref static-environment sicl-compiler:+code-object-index+)
                               env-length
                               (lexical-locations)))
