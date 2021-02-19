1b545c5c1e730bb87d89803acd85d900b5e91483
Robert Strandh <robert.strandh@gmail.com>
2021-02-19 10:38:45 +0100

    Determine shift count for type SINGLE-FLOAT.

--- Code/Compiler/HIR-to-MIR/array.lisp
+++ Code/Compiler/HIR-to-MIR/array.lisp
@@ -4,6 +4,7 @@
   (cond ((equal element-type 't) 3)
         ((equal element-type 'character) 3)
         ((equal element-type 'double-float) 3)
+        ((equal element-type 'single-float) 2)
         ((equal element-type '(signed-byte 8)) 0)
         ((equal element-type '(unsigned-byte 8)) 0)
         ((equal element-type '(signed-byte 16)) 1)
