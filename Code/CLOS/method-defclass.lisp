f8a17f0c233e28c617b140ce7bd263ca4c36d6ff
Robert Strandh <robert.strandh@gmail.com>
2021-02-26 20:16:54 +0100

    Provide DEFGENERIC forms for slot accessor METHOD-GENERIC-FUNCTION.

--- Code/CLOS/method-defclass.lisp
+++ Code/CLOS/method-defclass.lisp
@@ -4,6 +4,14 @@
 ;;; http://metamodular.com/CLOS-MOP/method-function.html
 (defgeneric method-function (method))
 
+;;; For the specification of this generic function, see
+;;; http://metamodular.com/CLOS-MOP/method-generic-function.html
+(defgeneric method-generic-function (method))
+
+;;; This function is called by ADD-METHOD and REMOVE-METHOD to assign
+;;; the generic function to which the method is associated.
+(defgeneric (setf method-generic-function) (generic-function method))
+
 (defclass method (metaobject)
   ((%function 
     :initarg :function 
