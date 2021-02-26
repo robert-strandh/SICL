565e97832a2a4b84db41bd43e8ce710a6fde5a74
Robert Strandh <robert.strandh@gmail.com>
2021-02-26 20:18:49 +0100

    Remove obsolete components.

--- Code/CLOS/setf-method-generic-function-defgeneric.lisp
+++
@@ -1,5 +0,0 @@
-(cl:in-package #:sicl-clos)
-
-;;; This function is called by ADD-METHOD and REMOVE-METHOD to assign
-;;; the generic function to which the method is associated.
-(defgeneric (setf method-generic-function) (generic-function method))
