f4b1586172d90bb963966be8467f1652019b3190
Robert Strandh <robert.strandh@gmail.com>
2021-02-27 17:59:06 +0100

    Remove obsolete component.

--- Code/CLOS/method-lambda-list-defgeneric.lisp
+++
@@ -1,5 +0,0 @@
-(cl:in-package #:sicl-clos)
-
-;;; For the specification of this generic function, see
-;;; http://metamodular.com/CLOS-MOP/method-lambda-list.html
-(defgeneric method-lambda-list (method))
