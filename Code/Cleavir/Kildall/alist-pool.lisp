b6f11d6274c5645dc7650dd2bd331f75e456d6c6
Robert Strandh <robert.strandh@gmail.com>
2021-02-27 12:10:46 +0100

    Improve code layout.

--- Code/Cleavir/Kildall/alist-pool.lisp
+++ Code/Cleavir/Kildall/alist-pool.lisp
@@ -5,21 +5,21 @@
 ;;; move? not sure
 (defgeneric instruction-variables (specialization instruction))
 
-(defmethod make-pool ((specialization alist-pool-mixin)
-                      instruction)
+(defmethod make-pool
+    ((specialization alist-pool-mixin) instruction)
   (loop for variable
           in (instruction-variables specialization instruction)
         collect (cons variable (object1 specialization variable))))
 
-(defmethod find-in-pool ((specialization alist-pool-mixin)
-                         variable pool)
+(defmethod find-in-pool
+    ((specialization alist-pool-mixin) variable pool)
   (let ((pair (assoc variable pool :test #'eq)))
-    (if pair
-        (cdr pair)
-        (object1 specialization variable))))
+    (if (null pair)
+        (object1 specialization variable)
+        (cdr pair))))
 
-(defmethod map-into-pool ((specialization alist-pool-mixin)
-                          function pool)
+(defmethod map-into-pool
+    ((specialization alist-pool-mixin) function pool)
   (loop for pair in pool
         do (setf (cdr pair)
                  (funcall function (car pair) (cdr pair)))))
