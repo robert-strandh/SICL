f5210853d780d41fde9869dacba9aedc3549a696
Robert Strandh <robert.strandh@gmail.com>
2021-02-27 12:04:28 +0100

    Improve layout of comment.

--- Code/Cleavir/Kildall/packages.lisp
+++ Code/Cleavir/Kildall/packages.lisp
@@ -1,7 +1,11 @@
 (cl:in-package #:common-lisp-user)
 
 ;;;; Implementation of Kildall's "Algorithm A":
-;;;; G.A. Kildall, "A Unified Approach to Global Program Optimization." Proceedings of the First ACM Symposium on Principles of Programming Languages,194-206, 1973.
+;;;;
+;;;; G.A. Kildall, "A Unified Approach to Global Program
+;;;; Optimization." Proceedings of the First ACM Symposium on
+;;;; Principles of Programming Languages,194-206, 1973.
+;;;;
 ;;;; It's a very general algorithm for optimization information on
 ;;;; program structures like HIR.
 ;;;; See liveness.lisp for an example.
