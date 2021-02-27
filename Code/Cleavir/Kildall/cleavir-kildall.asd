a0065cc68b1ba835c2b8e9437da7b4c985b3f214
Robert Strandh <robert.strandh@gmail.com>
2021-02-27 12:12:50 +0100

    Move comment explaining the purpose of the system to the ASDF system file.

--- Code/Cleavir/Kildall/cleavir-kildall.asd
+++ Code/Cleavir/Kildall/cleavir-kildall.asd
@@ -1,5 +1,15 @@
 (cl:in-package #:asdf-user)
 
+;;;; Implementation of Kildall's "Algorithm A":
+;;;;
+;;;; G.A. Kildall, "A Unified Approach to Global Program
+;;;; Optimization." Proceedings of the First ACM Symposium on
+;;;; Principles of Programming Languages,194-206, 1973.
+;;;;
+;;;; It's a very general algorithm for optimization information on
+;;;; program structures like HIR.
+;;;; See liveness.lisp for an example.
+
 (defsystem :cleavir-kildall
   :depends-on (:cleavir-hir)
   :serial t
--- Code/Cleavir/Kildall/packages.lisp
+++ Code/Cleavir/Kildall/packages.lisp
@@ -1,15 +1,5 @@
 (cl:in-package #:common-lisp-user)
 
-;;;; Implementation of Kildall's "Algorithm A":
-;;;;
-;;;; G.A. Kildall, "A Unified Approach to Global Program
-;;;; Optimization." Proceedings of the First ACM Symposium on
-;;;; Principles of Programming Languages,194-206, 1973.
-;;;;
-;;;; It's a very general algorithm for optimization information on
-;;;; program structures like HIR.
-;;;; See liveness.lisp for an example.
-
 (defpackage #:cleavir-kildall
   (:use #:cl)
   (:export #:make-pool)
