6b3bc45e536b898939185618d73d56aab6fbe3bf
Robert Strandh <robert.strandh@gmail.com>
2021-03-02 06:25:16 +0100

    Remove obsolete component.

--- Code/CLOS/generic-function-name-defgeneric.lisp
+++
@@ -1,5 +0,0 @@
-(cl:in-package #:sicl-clos)
-
-;;; For the specification of this generic function, see
-;;; http://metamodular.com/CLOS-MOP/generic-function-name.html
-(defgeneric generic-function-name (generic-function))
