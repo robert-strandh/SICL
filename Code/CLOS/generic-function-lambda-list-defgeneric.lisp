044f4e736e154dc245e00ee5cfbf91380405ba93
Robert Strandh <robert.strandh@gmail.com>
2021-03-04 14:53:55 +0100

    Remove obsolete component containing DEFGENERIC form for slot reader GENERIC-FUNCTION-LAMBDA-LIST.

--- Code/CLOS/generic-function-lambda-list-defgeneric.lisp
+++
@@ -1,5 +0,0 @@
-(cl:in-package #:sicl-clos)
-
-;;; For the specification of this generic function, see
-;;; http://metamodular.com/CLOS-MOP/generic-function-lambda-list.html
-(defgeneric generic-function-lambda-list (generic-function))
