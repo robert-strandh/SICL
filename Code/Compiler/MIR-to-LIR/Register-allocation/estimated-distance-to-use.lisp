ce790578010cbdf2d095ee887b07db6e583bb492
Robert Strandh <robert.strandh@gmail.com>
2021-03-05 12:27:36 +0100

    Define generic function COMPUTE-NEW-OUTPUT-POOL and its methods.

--- Code/Compiler/MIR-to-LIR/Register-allocation/estimated-distance-to-use.lisp
+++ Code/Compiler/MIR-to-LIR/Register-allocation/estimated-distance-to-use.lisp
@@ -8,8 +8,25 @@
                    do (setf (gethash instruction input-pool)
                             (add-variable input (gethash instruction input-pool))))))
 
-;;; Compute the input estimated distance to use as a function of 
-(defgeneric handle-instruction (instruction input-pool output-pool back-arcs))
+(defgeneric compute-new-output-pool (instruction input-pool back-arcs))
+
+(defmethod compute-new-output-pool (instruction input-pool back-arcs)
+  (let ((successors (cleavir-ir:successors instruction)))
+    (ecase (length successors)
+      (1 (gethash (first successors) input-pool))
+      (2 (destructuring-bind (first second) successors
+           (pool-meet (if (gethash first back-arcs)
+                          (if (gethash second back-arcs) 1/2 9/10)
+                          (if (gethash second back-arcs) 1/10 1/2))
+                      (gethash first input-pool)
+                      (gethash second input-pool)))))))
+
+(defgeneric handle-instruction
+    (instruction input-pool output-pool back-arcs))
+
+(defmethod compute-new-output-pool
+    ((instruction cleavir-ir:unwind-instruction) input-pool back-arcs)
+  (gethash (first (cleavir-ir:successors instruction)) input-pool))
 
 ;;; Compute estimated distance to use for a single function, defined
 ;;; by an ENTER-INSTRUCTION.
