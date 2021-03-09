3009b9d5392460c9430480dc0878a896682edbd3
Robert Strandh <robert.strandh@gmail.com>
2021-03-09 14:06:14 +0100

    Remove obsolete components containing DEFGENERIC forms for slot accessors CALL-HISTORY.

--- Code/CLOS/call-history-defgeneric.lisp
+++
@@ -1,5 +0,0 @@
-(cl:in-package #:sicl-clos)
-
-;;; This function returns the call history of the generic function.
-;;; FIXME: say more.
-(defgeneric call-history (generic-function))
--- Code/CLOS/setf-call-history-defgeneric.lisp
+++
@@ -1,5 +0,0 @@
-(cl:in-package #:sicl-clos)
-
-;;; This function sets the call history of the generic function.
-;;; FIXME: say more.
-(defgeneric (setf call-history) (new-call-history generic-function))
