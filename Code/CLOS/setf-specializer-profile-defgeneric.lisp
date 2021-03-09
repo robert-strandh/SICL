c2eb060c67e2e37ccad7088fb8f77ce82beabe55
Robert Strandh <robert.strandh@gmail.com>
2021-03-09 14:08:58 +0100

    Remove obsolete components containing DEFGENERIC forms for slot accessors SPECIALIZER-PROFILE.

--- Code/CLOS/setf-specializer-profile-defgeneric.lisp
+++
@@ -1,5 +0,0 @@
-(cl:in-package #:sicl-clos)
-
-;;; This function sets the specializer profile of the generic function.
-;;; FIXME: say more.
-(defgeneric (setf specializer-profile) (new-specializer-profile generic-function))
--- Code/CLOS/specializer-profile-defgeneric.lisp
+++
@@ -1,5 +0,0 @@
-(cl:in-package #:sicl-clos)
-
-;;; This function returns the specializer profile of the generic function.
-;;; FIXME: say more.
-(defgeneric specializer-profile (generic-function))
