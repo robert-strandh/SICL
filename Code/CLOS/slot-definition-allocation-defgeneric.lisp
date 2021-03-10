f40dccedc3dfd932b8b520177b56480922276953
Robert Strandh <robert.strandh@gmail.com>
2021-03-10 16:36:31 +0100

    Remove obsolete components containing DEFGENERIC form for slot accessor SLOT-DEFINITION-ALLOCATION.

--- Code/CLOS/slot-definition-allocation-defgeneric.lisp
+++
@@ -1,5 +0,0 @@
-(cl:in-package #:sicl-clos)
-
-;;; For the specification of this generic function, see
-;;; http://metamodular.com/CLOS-MOP/slot-definition-allocation.html
-(defgeneric slot-definition-allocation (slot-definition))
