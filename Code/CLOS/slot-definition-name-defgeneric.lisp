8c14e13da12bf2af1b9ec42089f7b8f664f8bde7
Robert Strandh <robert.strandh@gmail.com>
2021-03-10 16:34:17 +0100

    Remove obsolete components containing DEFGENERIC form for slot accessor SLOT-DEFINITION-NAME.

--- Code/CLOS/slot-definition-name-defgeneric.lisp
+++
@@ -1,5 +0,0 @@
-(cl:in-package #:sicl-clos)
-
-;;; For the specification of this generic function, see
-;;; http://metamodular.com/CLOS-MOP/slot-definition-name.html
-(defgeneric slot-definition-name (slot-definition))
