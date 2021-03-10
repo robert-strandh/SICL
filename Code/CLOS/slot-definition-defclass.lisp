b751b5d915585bd0dc40305cb4d56b6708ebeca6
Robert Strandh <robert.strandh@gmail.com>
2021-03-10 16:32:51 +0100

    Include DEFGENERIC forms for slot accessors SLOT-DEFINITION-NAME.

--- Code/CLOS/slot-definition-defclass.lisp
+++ Code/CLOS/slot-definition-defclass.lisp
@@ -1,5 +1,9 @@
 (cl:in-package #:sicl-clos)
 
+;;; For the specification of this generic function, see
+;;; http://metamodular.com/CLOS-MOP/slot-definition-name.html
+(defgeneric slot-definition-name (slot-definition))
+
 (defclass slot-definition (metaobject)
   ((%name 
     :initarg :name
