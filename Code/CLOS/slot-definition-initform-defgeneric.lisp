24d6fda22ecee5c22fd45018b0ffbe2e328877ef
Robert Strandh <robert.strandh@gmail.com>
2021-03-11 09:33:51 +0100

    Remove obsolete component containing DEFGENERIC form for slot accessor SLOT-DEFINITION-INITFORM.

--- Code/CLOS/slot-definition-initform-defgeneric.lisp
+++
@@ -1,5 +0,0 @@
-(cl:in-package #:sicl-clos)
-
-;;; For the specification of this generic function, see
-;;; http://metamodular.com/CLOS-MOP/slot-definition-initform.html
-(defgeneric slot-definition-initform (slot-definition))
