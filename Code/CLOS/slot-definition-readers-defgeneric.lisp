c8f88c07ae92e4a7b6d1046bcda4ef7124816cc4
Robert Strandh <robert.strandh@gmail.com>
2021-03-12 15:51:26 +0100

    Remove obsolete component containing DEFGENERIC form for slot accessor SLOT-DEFINITION-READERS.

--- Code/CLOS/slot-definition-readers-defgeneric.lisp
+++
@@ -1,5 +0,0 @@
-(cl:in-package #:sicl-clos)
-
-;;; For the specification of this generic function, see
-;;; http://metamodular.com/CLOS-MOP/slot-definition-readers.html
-(defgeneric slot-definition-readers (slot-definition))
