4fba59fd9c76092d3eb01a411d379466a2c2ee23
Robert Strandh <robert.strandh@gmail.com>
2021-03-13 16:28:42 +0100

    Return the first unassigned register rather than a list of all of them.

--- Code/Compiler/MIR-to-LIR/Register-allocation/allocate-register.lisp
+++ Code/Compiler/MIR-to-LIR/Register-allocation/allocate-register.lisp
@@ -1,7 +1,10 @@
 (cl:in-package #:sicl-register-allocation)
 
-(defun unassigned-registers (arrangement candidates)
+;;; Find the first unassigned register.  If all registers are
+;;; assigned, return NIL.
+(defun find-unassigned-register (arrangement candidates)
   (loop for candidate in candidates
         unless (member candidate (attributions arrangement)
                        :test #'eq :key #'register)
-          collect candidate))
+          return candiate
+        finally (return nil)))
