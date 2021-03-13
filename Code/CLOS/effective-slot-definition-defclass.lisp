cfadb23e4645ce5574c4a38001ad460a43b417fb
Robert Strandh <robert.strandh@gmail.com>
2021-03-13 14:56:22 +0100

    Include DEFGENERIC forms for slot accessors SLOT-DEFINITION-LOCATION.

--- Code/CLOS/effective-slot-definition-defclass.lisp
+++ Code/CLOS/effective-slot-definition-defclass.lisp
@@ -1,5 +1,11 @@
 (cl:in-package #:sicl-clos)
 
+;;; For the specification of this generic function, see
+;;; http://metamodular.com/CLOS-MOP/slot-definition-location.html
+(defgeneric slot-definition-location (slot-definition))
+
+(defgeneric (setf slot-definition-location) (new-location slot-definition))
+
 (defclass effective-slot-definition (slot-definition)
   ((%location 
     :initform nil
