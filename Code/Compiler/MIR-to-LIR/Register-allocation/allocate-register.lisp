68ae2d5af8f47d86278d313c81a206006eddf73e
Robert Strandh <robert.strandh@gmail.com>
2021-03-13 16:52:08 +0100

    Define function for allocating a register out of a list of candidates.

--- Code/Compiler/MIR-to-LIR/Register-allocation/allocate-register.lisp
+++ Code/Compiler/MIR-to-LIR/Register-allocation/allocate-register.lisp
@@ -6,7 +6,7 @@
   (loop for candidate in candidates
         unless (member candidate (attributions arrangement)
                        :test #'eq :key #'register)
-          return candiate
+          return candidate
         finally (return nil)))
 
 (defun extract-locations (arrangement candidates)
@@ -18,3 +18,20 @@
 (defun extract-pool-items (pool locations)
   (loop for location in locations
         collect (find location pool :test #'eq :key #'lexical-location)))
+
+(defun allocate-register
+    (predecessor instruction pool arrangement candidates)
+  (let ((unassigned (find-unassigned-register arrangement candidates)))
+    (if (not (null unassigned))
+        unassigned
+        (let* ((locations (extract-locations arrangement candidates))
+               (pool-items (extract-pool-items pool locations))
+               (sorted-pool-items (sort pool-items #'> :key #'distance))
+               (location (lexical-location (first sorted-pool-items)))
+               (attribution (find location (attributions arrangement)
+                                  :test #'eq :key #'lexical-location))
+               (register (register attribution)))
+          (values register
+                  (if (null (stack-slot attribution))
+                      (spill predecessor instruction register)
+                      predecessor))))))
