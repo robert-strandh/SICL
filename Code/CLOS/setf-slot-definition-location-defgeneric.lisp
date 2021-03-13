8c27eb590979230b883b3b7233fbfe3e41655992
Robert Strandh <robert.strandh@gmail.com>
2021-03-13 14:57:20 +0100

    Remove obsolete components containing DEFGENERIC forms for slot accessors SLOT-DEFINITION-LOCATION.

--- Code/CLOS/setf-slot-definition-location-defgeneric.lisp
+++
@@ -1,3 +0,0 @@
-(cl:in-package #:sicl-clos)
-
-(defgeneric (setf slot-definition-location) (new-location slot-definition))
--- Code/CLOS/slot-definition-location-defgeneric.lisp
+++
@@ -1,5 +0,0 @@
-(cl:in-package #:sicl-clos)
-
-;;; For the specification of this generic function, see
-;;; http://metamodular.com/CLOS-MOP/slot-definition-location.html
-(defgeneric slot-definition-location (slot-definition))
