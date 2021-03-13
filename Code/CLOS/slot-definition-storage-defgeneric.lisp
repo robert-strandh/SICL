b2d51429a33141cc85c4247bdcd9c629925cac08
Robert Strandh <robert.strandh@gmail.com>
2021-03-13 14:54:08 +0100

    Remove obsolete component containing DEFGENERIC form for slot accessor SLOT-DEFINITION-STORAGE.

--- Code/CLOS/slot-definition-storage-defgeneric.lisp
+++
@@ -1,5 +0,0 @@
-(cl:in-package #:sicl-clos)
-
-;;; For a slot with :ALLOCATION :CLASS, this function returns a CONS
-;;; cell where the CAR is used to store the value of the slot.
-(defgeneric slot-definition-storage (direct-slot-definition))
