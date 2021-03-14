ad7abacc225acacea9b19f0341f8621f197cab26
Robert Strandh <robert.strandh@gmail.com>
2021-03-14 10:14:23 +0100

    No longer load a files containing DEFGENERIC forms for slot accessors SPECIALIZER-DIRECT-METHODS.

--- Code/Boot/create-accessor-generic-functions.lisp
+++ Code/Boot/create-accessor-generic-functions.lisp
@@ -9,8 +9,6 @@
 (defun create-accessor-defgenerics (ea)
   (flet ((ld (path)
            (load-source-file path ea)))
-    (ld "CLOS/specializer-direct-methods-defgeneric.lisp")
-    (ld "CLOS/setf-specializer-direct-methods-defgeneric.lisp")
     (ld "CLOS/unique-number-defgeneric.lisp")
     (ld "CLOS/class-name-defgeneric.lisp")
     (ld "CLOS/class-direct-subclasses-defgeneric.lisp")
