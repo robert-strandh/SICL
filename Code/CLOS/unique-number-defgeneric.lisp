ad1cbdf1ff317fde930f503481b18d24277652ce
Robert Strandh <robert.strandh@gmail.com>
2021-03-14 10:17:36 +0100

    Remove obsolete components containing DEFGENERIC form for slot reader UNIQUE-NUMBER.

--- Code/CLOS/unique-number-defgeneric.lisp
+++
@@ -1,5 +0,0 @@
-(cl:in-package #:sicl-clos)
-
-;;; This function returns the unique number of the class, assigned
-;;; when the class is initialized or reinitialized.
-(defgeneric unique-number (class))
