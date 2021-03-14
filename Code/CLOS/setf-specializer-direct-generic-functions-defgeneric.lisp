451042e391ffbd3038b2e24f67d0c842db172ce2
Robert Strandh <robert.strandh@gmail.com>
2021-03-14 10:12:27 +0100

    Remove obsolete components containing DEFGENERIC forms for slot accessors SPECIALIZER-DIRECT-GENERIC-FUNCTIONS.

--- Code/CLOS/setf-specializer-direct-generic-functions-defgeneric.lisp
+++
@@ -1,7 +0,0 @@
-(cl:in-package #:sicl-clos)
-
-;;; This function is called by ADD-DIRECT-METHOD and
-;;; REMOVE-DIRECT-METHOD so change the list of generic functions
-;;; having SPECIALIZER as a specializer.
-(defgeneric (setf specializer-direct-generic-functions)
-    (new-generic-functions specializer))
--- Code/CLOS/specializer-direct-generic-functions-defgeneric.lisp
+++
@@ -1,5 +0,0 @@
-(cl:in-package #:sicl-clos)
-
-;;; For the specification of this generic function, see
-;;; http://metamodular.com/CLOS-MOP/specializer-direct-generic-functions.html
-(defgeneric specializer-direct-generic-functions (specializer))
