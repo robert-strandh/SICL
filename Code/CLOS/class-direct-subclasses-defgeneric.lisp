f1f58ac4e8e17d521529862ab5f5569a87d72786
Robert Strandh <robert.strandh@gmail.com>
2021-03-15 05:21:06 +0100

    Remove obsolete components containing DEFGENERIC forms for slot accessor CLASS-DIRECT-SUBCLASSES.

--- Code/CLOS/class-direct-subclasses-defgeneric.lisp
+++
@@ -1,5 +0,0 @@
-(cl:in-package #:sicl-clos)
-
-;;; For the specification of this generic function, see
-;;; http://metamodular.com/CLOS-MOP/class-direct-subclasses.html
-(defgeneric class-direct-subclasses (class))
--- Code/CLOS/setf-class-direct-subclasses-defgeneric.lisp
+++
@@ -1,6 +0,0 @@
-(cl:in-package #:sicl-clos)
-
-;;; The functions ADD-DIRECT-SUBCLASS and REMOVE-DIRECT-SUBCLASS are
-;;; used to update the direct subclasses of a class, so they call this
-;;; function.
-(defgeneric (setf class-direct-subclasses) (direct-subclassees class))
