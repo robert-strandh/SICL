a086438d7f9ccb44f1ede12741e8b3db0bcf4c23
Robert Strandh <robert.strandh@gmail.com>
2021-03-15 06:19:07 +0100

    Remove obsolete component containing DEFGENERIC form for slot reader CLASS-DIRECT-SUPERCLASSES.

--- Code/CLOS/class-direct-superclasses-defgeneric.lisp
+++
@@ -1,5 +0,0 @@
-(cl:in-package #:sicl-clos)
-
-;;; For the specification of this generic function, see
-;;; http://metamodular.com/CLOS-MOP/class-direct-superclasses.html
-(defgeneric class-direct-superclasses (class))
