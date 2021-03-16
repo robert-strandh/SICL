b02ee51b0a1687e7a6589520168377b69e593244
Robert Strandh <robert.strandh@gmail.com>
2021-03-16 11:01:34 +0100

    Remove obsolete component containing DEFGENERIC form for slot reader CLASS-DIRECT-DEFAULT-INITARGS.

--- Code/CLOS/class-direct-default-initargs-defgeneric.lisp
+++
@@ -1,5 +0,0 @@
-(cl:in-package #:sicl-clos)
-
-;;; For the specification of this generic function, see
-;;; http://metamodular.com/CLOS-MOP/class-direct-default-initargs.html
-(defgeneric class-direct-default-initargs (class))
