18982e43d0b3c54aa5325e1e6cbf87f3b8090bdb
Robert Strandh <robert.strandh@gmail.com>
2021-03-16 10:58:18 +0100

    Remove obsolete component containing DEFGENERIC form for slot reader CLASS-DIRECT-SLOTS.

--- Code/CLOS/class-direct-slots-defgeneric.lisp
+++
@@ -1,5 +0,0 @@
-(cl:in-package #:sicl-clos)
-
-;;; For the specification of this generic function, see
-;;; http://metamodular.com/CLOS-MOP/class-direct-slots.html
-(defgeneric class-direct-slots (class))
