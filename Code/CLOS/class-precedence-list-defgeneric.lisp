99f6efa8693f7d6f77da7e376707010d9207f790
Robert Strandh <robert.strandh@gmail.com>
2021-03-17 10:59:53 +0100

    Remove obsolete component containing DEFGENERIC form for slot reader CLASS-PRECEDENCE-LIST.

--- Code/CLOS/class-precedence-list-defgeneric.lisp
+++
@@ -1,5 +0,0 @@
-(cl:in-package #:sicl-clos)
-
-;;; For the specification of this generic function, see
-;;; http://metamodular.com/CLOS-MOP/class-precedence-list.html
-(defgeneric class-precedence-list (class))
