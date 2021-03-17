615e4b2d534c98de4c7f89d217d7c5cc7984c23f
Robert Strandh <robert.strandh@gmail.com>
2021-03-17 10:53:34 +0100

    Remove obsolete component containing DEFGENERIC form for slot reader CLASS-FINALIZED-P.

--- Code/CLOS/class-finalized-p-defgeneric.lisp
+++
@@ -1,5 +0,0 @@
-(cl:in-package #:sicl-clos)
-
-;;; For the specification of this generic function, see
-;;; http://metamodular.com/CLOS-MOP/class-finalized-p.html
-(defgeneric class-finalized-p (class))
