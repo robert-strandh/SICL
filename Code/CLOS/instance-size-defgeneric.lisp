b683161e573fabe88b0b6228f149efa9423cfccf
Robert Strandh <robert.strandh@gmail.com>
2021-03-18 06:25:09 +0100

    Remove obsolete components containing DEFGENERIC forms for slot accessor INSTANCE-SIZE.

--- Code/CLOS/instance-size-defgeneric.lisp
+++
@@ -1,6 +0,0 @@
-(cl:in-package #:sicl-clos)
-
-;;; This function is used by ALLOCATE-INSTANCE and
-;;; ALLOCATE-BUILT-IN-INSTANCE to determine the size of the instance
-;;; to allocate.
-(defgeneric instance-size (class))
--- Code/CLOS/setf-instance-size-defgeneric.lisp
+++
@@ -1,6 +0,0 @@
-(cl:in-package #:sicl-clos)
-
-;;; This function is used during class finalization, when the
-;;; effective slots are known, and it is therefore also known what
-;;; size the instances of this class should have.
-(defgeneric (setf instance-size) (new-size class))
