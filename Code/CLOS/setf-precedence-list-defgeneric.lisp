9ba96219a9f6d58aa4931a92f779dac7d7a10ccd
Robert Strandh <robert.strandh@gmail.com>
2021-03-18 06:22:01 +0100

    Remove obsolete components containing DEFGENERIC forms for slot accessor PRECEDENCE-LIST.

--- Code/CLOS/setf-precedence-list-defgeneric.lisp
+++
@@ -1,5 +0,0 @@
-(cl:in-package #:sicl-clos)
-
-;;; This function is used by the class finalization protocol to set
-;;; the precedence list of the class.
-(defgeneric (setf precedence-list) (precedence-list class))
