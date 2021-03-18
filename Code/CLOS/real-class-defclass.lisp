010e7910cec69a717c57edf7ad385c50c250cbd7
Robert Strandh <robert.strandh@gmail.com>
2021-03-18 06:20:53 +0100

    Include DEFGENERIC forms for slot accessors PRECEDENCE-LIST.

--- Code/CLOS/real-class-defclass.lisp
+++ Code/CLOS/real-class-defclass.lisp
@@ -8,6 +8,22 @@
 ;;; http://metamodular.com/CLOS-MOP/class-precedence-list.html
 (defgeneric class-precedence-list (class))
 
+;;; This function is similar to CLASS-PRECEDENCE-LIST, except that
+;;; CLASS-PRECEDENCE-LIST is specified to signal an error when the
+;;; class is not finalized, which we accomplish by using a :BEFORE
+;;; method.  However, during class finalization, we need to access the
+;;; class precedence list in the two steps following its computation.
+;;; Since at that point the class is not yet finalized, those two
+;;; steps can not call CLASS-PRECEDENCE-LIST.  Our solution is to
+;;; define an alternative reader for the same slot, named
+;;; PRECEDENCE-LIST, and which does not signal an error if the class
+;;; is not finalized.
+(defgeneric precedence-list (class))
+
+;;; This function is used by the class finalization protocol to set
+;;; the precedence list of the class.
+(defgeneric (setf precedence-list) (precedence-list class))
+
 (defclass real-class (class)
   ((%direct-default-initargs
     :initarg :direct-default-initargs
