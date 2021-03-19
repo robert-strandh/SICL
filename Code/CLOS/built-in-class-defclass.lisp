a732452bdf7eab736216b8629654c7c94d4de385
Robert Strandh <robert.strandh@gmail.com>
2021-03-19 11:01:06 +0100

    Include DEFMETHOD forms specialized to BUILT-IN-CLASS.

--- Code/CLOS/built-in-class-defclass.lisp
+++ Code/CLOS/built-in-class-defclass.lisp
@@ -18,3 +18,21 @@
    (%effective-slots
     :initform '()
     :reader class-slots)))
+
+;;; The AMOP says that CLASS-DIRECT-SLOTS should return the empty list
+;;; for a built-in class.
+(defmethod class-direct-slots ((class built-in-class))
+  (declare (ignore class))
+  '())
+
+;;; The AMOP says that CLASS-SLOTS should return the empty list for a
+;;; built-in class.
+(defmethod class-slots ((class built-in-class))
+  (declare (ignore class))
+  '())
+
+;;; The AMOP says that CLASS-DEFAULT-INITARGS should return the empty
+;;; list for a built-in class.
+(defmethod class-default-initargs ((class built-in-class))
+  (declare (ignore class))
+  '())
