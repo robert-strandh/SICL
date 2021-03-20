3e372e30ca074b06d4e01950d8c7f64e85e080d1
Robert Strandh <robert.strandh@gmail.com>
2021-03-20 16:28:37 +0100

    Define function UNATTRIBUTE-REGISTER.

--- Code/Compiler/MIR-to-LIR/Register-allocation/spill-unspill.lisp
+++ Code/Compiler/MIR-to-LIR/Register-allocation/spill-unspill.lisp
@@ -21,6 +21,7 @@
          (available-stack-slot (position 0 stack-map))
          (new-length (+ length (if (null available-stack-slot) 1 0)))
          (new-stack-map (make-array new-length :element-type 'bit)))
+    (assert (null (stack-slot selected-attribution)))
     (replace new-stack-map stack-map)
     (when (null available-stack-slot)
       (setf available-stack-slot length))
@@ -37,6 +38,42 @@
            (new-instruction (make-instance 'cleavir-ir:assignment-instruction
                               :input lexical-location
                               :output lexical-location)))
+      (cleavir-ir:insert-instruction-between
+       new-instruction predecessor instruction)
+      (setf (input-arrangement new-instruction) arrangement
+            (output-arrangement new-instruction) new-arrangement)
+      new-instruction)))
+
+;;; By UNATTRIBUTE, we mean to remove a particular register R from an
+;;; attribution that contains both R and a valid stack slot, so that
+;;; after the operation, the lexical location of the attribution is
+;;; present only on the stack.  We make this change in attribution
+;;; explicit in the MIR code as a NOP instruction I.  The input
+;;; arrangement if I contains an attribution with both R and a valid
+;;; stack slot in it.  The output arrangement is similar to the input
+;;; arrangement, but with the attribution corresponding to the lexical
+;;; location, having the register value NIL.
+(defun unattribute-register (predecessor instruction register)
+  (let* ((arrangement (output-arrangement predecessor))
+         (attributions (attributions arrangement))
+         (selected-attribution
+           (find register attributions :test #'eq :key #'register))
+         (remaining-attributions
+           (remove selected-attribution attributions :test #'eq))
+         (lexical-location (lexical-location selected-attribution))
+         (stack-slot (stack-slot selected-attribution))
+         (stack-map (stack-map arrangement)))
+    (assert (not (null stack-slot)))
+    (let* ((new-attribution
+             (make-instance 'attribution
+               :lexical-location lexical-location
+               :register nil
+               :stack-slot stack-slot))
+           (new-arrangement (make-instance 'arrangement
+                              :stack-map stack-map
+                              :attributions
+                              (cons new-attribution remaining-attributions)))
+           (new-instruction (make-instance 'cleavir-ir:nop-instruction)))
       (cleavir-ir:insert-instruction-between
        new-instruction predecessor instruction)
       (setf (input-arrangement new-instruction) arrangement
