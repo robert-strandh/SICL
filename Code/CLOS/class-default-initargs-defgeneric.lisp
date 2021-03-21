aaa08f53e15a86c51f1f24d2ef7e32c1c392af11
Robert Strandh <robert.strandh@gmail.com>
2021-03-21 16:51:02 +0100

    Remove obsolete components containing DEFGENERIC forms for slot accessor CLASS-DEFAULT-INITARGS.

--- Code/CLOS/class-default-initargs-defgeneric.lisp
+++
@@ -1,5 +0,0 @@
-(cl:in-package #:sicl-clos)
-
-;;; For the specification of this generic function, see
-;;; http://metamodular.com/CLOS-MOP/class-default-initargs.html
-(defgeneric class-default-initargs (class))
--- Code/CLOS/setf-class-default-initargs-defgeneric.lisp
+++
@@ -1,5 +0,0 @@
-(cl:in-package #:sicl-clos)
-
-;;; This function is used by the class finalization protocol to set
-;;; the default initargs of the class. 
-(defgeneric (setf class-default-initargs) (default-initargs class))
