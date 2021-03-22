c241b917cbd2a939a0f4d2fa6cc8474b7013b42e
Robert Strandh <robert.strandh@gmail.com>
2021-03-22 17:01:35 +0100

    Define default method on COMPUTE-INPUT-ARRANGEMENT.

--- Code/Compiler/MIR-to-LIR/Register-allocation/allocate-registers-for-instructions.lisp
+++ Code/Compiler/MIR-to-LIR/Register-allocation/allocate-registers-for-instructions.lisp
@@ -8,3 +8,9 @@
 (defmethod allocate-registers-for-instruction (predecessor instruction)
   (let ((new-predecessor (compute-input-arrangement predecessor instruction)))
     (compute-output-arrangement new-predecessor instruction)))
+
+(defmethod compute-input-arrangement (predecessor instruction)
+  (let ((result predecessor))
+    (loop for input in (cleavir-ir:inputs instruction)
+          when (typep input 'cleavir-ir:lexical-location)
+            do (setf result (ensure-in-register input result instruction)))))
