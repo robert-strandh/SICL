ba190cf1c032636ed938a1322af7916b3062e588
Robert Strandh <robert.strandh@gmail.com>
2021-03-27 17:06:07 +0100

    Define function UPDATE-ARRANGEMENT-FOR-NEW-DEFINITION.

--- Code/Compiler/MIR-to-LIR/Register-allocation/arrangement.lisp
+++ Code/Compiler/MIR-to-LIR/Register-allocation/arrangement.lisp
@@ -45,3 +45,43 @@
 
 (defun (setf output-arrangement) (arrangement instruction)
   (setf (gethash instruction *output-arrangements*) arrangement))
+
+(defun update-arrangement-for-new-definition
+    (arrangement lexical-location register-number)
+  (with-arrangement-parts (stack-map register-map attributions arrangement)
+    (let ((attribution (find lexical-location attributions
+                             :test #'eq :key #'lexical-location)))
+      (cond ((null attribution)
+             (let ((new-register-map
+                     (reserve-register register-map register-number))
+                   (new-attribution (make-instance 'attribution
+                                      :lexical-location lexical-location
+                                      :stack-slot nil
+                                      :register-number register-number)))
+               (assert (null (find register-number attributions
+                                   :test #'eql :key #'register-number)))
+               (make-instance 'arrangement
+                 :stack-map stack-map
+                 :register-map new-register-map
+                 :attributions (cons new-attribution attributions))))
+            ((null (stack-slot attribution))
+             (assert (eql register-number (register-number attribution)))
+             ;; The existing arrangement is perfect.  Just return it.
+             arrangement)
+            (t
+             (assert (eql register-number (register-number attribution)))
+             ;; We must create a new arrangement that is similar to
+             ;; the one we got, except that the attribution for
+             ;; LEXICAL-LOCATION must have a stack slot that is NIL.
+             (let ((residual-attributions
+                     (remove attribution attributions :test #'eq))
+                   (new-stack-map (copy-stack-map stack-map))
+                   (new-attribution (make-instance 'attribution
+                                      :lexical-location lexical-location
+                                      :stack-slot nil
+                                      :register-number register-number)))
+               (free-stack-slot new-stack-map (stack-slot attribution))
+               (make-instance 'arrangement
+                 :stack-map new-stack-map
+                 :register-map register-map
+                 :attributions (cons new-attribution residual-attributions))))))))
