23f058e152d7f1324e1c653b0790bf1c86350d62
Robert Strandh <robert.strandh@gmail.com>
2021-03-27 14:10:48 +0100

    Introduced XMM registers and defined abstraction for defining a register.

--- Code/Compiler/MIR-to-LIR/Register-allocation/registers.lisp
+++ Code/Compiler/MIR-to-LIR/Register-allocation/registers.lisp
@@ -1,66 +1,51 @@
 (cl:in-package #:sicl-register-allocation)
 
-(defparameter *rax*
-  (make-instance 'cleavir-ir:register-location :name "RAX"))
+(defmacro make-register (variable name)
+  `(defparameter ,variable
+     (make-instance 'cleavir-ir:register-location :name ,name)))
 
-(defparameter *rbx*
-  (make-instance 'cleavir-ir:register-location :name "RBX"))
-
-(defparameter *rcx*
-  (make-instance 'cleavir-ir:register-location :name "RCX"))
-
-(defparameter *rdx*
-  (make-instance 'cleavir-ir:register-location :name "RDX"))
-
-(defparameter *rsp*
-  (make-instance 'cleavir-ir:register-location :name "RSP"))
-
-(defparameter *rbp*
-  (make-instance 'cleavir-ir:register-location :name "RBP"))
-
-(defparameter *rsi*
-  (make-instance 'cleavir-ir:register-location :name "RSI"))
-
-(defparameter *rdi*
-  (make-instance 'cleavir-ir:register-location :name "RDI"))
-
-(defparameter *r8*
-  (make-instance 'cleavir-ir:register-location :name "R8"))
-
-(defparameter *r9*
-  (make-instance 'cleavir-ir:register-location :name "R9"))
-
-(defparameter *r10*
-  (make-instance 'cleavir-ir:register-location :name "R10"))
-
-(defparameter *r11*
-  (make-instance 'cleavir-ir:register-location :name "R11"))
-
-(defparameter *r12*
-  (make-instance 'cleavir-ir:register-location :name "R12"))
-
-(defparameter *r13*
-  (make-instance 'cleavir-ir:register-location :name "R13"))
-
-(defparameter *r14*
-  (make-instance 'cleavir-ir:register-location :name "R14"))
-
-(defparameter *r15*
-  (make-instance 'cleavir-ir:register-location :name "R15"))
+(make-register *rax "RAX")
+(make-register *rax* "RAX")
+(make-register *rbx* "RBX")
+(make-register *rcx* "RCX")
+(make-register *rdx* "RDX")
+(make-register *rsp* "RSP")
+(make-register *rbp* "RBP")
+(make-register *rsi* "RSI")
+(make-register *rdi* "RDI")
+(make-register *r8* "R8")
+(make-register *r9* "R9")
+(make-register *r10* "R10")
+(make-register *r11* "R11")
+(make-register *r12* "R12")
+(make-register *r13* "R13")
+(make-register *r14* "R14")
+(make-register *r15* "R15")
+(make-register *xmm0* "XMM0")
+(make-register *xmm1* "XMM1")
+(make-register *xmm2* "XMM2")
+(make-register *xmm3* "XMM3")
+(make-register *xmm4* "XMM4")
+(make-register *xmm5* "XMM5")
+(make-register *xmm6* "XMM6")
+(make-register *xmm7* "XMM7")
 
 (defparameter *registers*
   (vector *rax* *rbx* *rcx* *rdx* *rsp* *rbp* *rsi* *rdi*
-          *r8* *r9* *r10* *r11* *r12* *r13* *r14* *r15*))
+          *r8* *r9* *r10* *r11* *r12* *r13* *r14* *r15*
+          *xmm0* *xmm1* *xmm2* *xmm3* *xmm4* *xmm5* *xmm6* *xmm7* ))
 
-(defparameter *caller-saves* #*1011001111110000)
+(defparameter *caller-saves* #*101100111111000000000000)
 
-(defparameter *callee-saves* #*0100000000001111)
+(defparameter *callee-saves* #*010000000000111100000000)
+
+(defparameter *xmm* #*000000000000000011111111)
 
 (defun register-number-in-map-p (register-number register-map)
   (not (zerop (bit register-map register-number))))
 
 (defun make-empty-register-map ()
-  (make-array 16 :element-type 'bit :initial-element 0))
+  (make-array 24 :element-type 'bit :initial-element 0))
 
 (defun mark-register (register-map register-number)
   (setf (bit register-map register-number) 1))
