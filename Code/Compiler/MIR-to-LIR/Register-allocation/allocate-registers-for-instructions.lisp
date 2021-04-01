199baf93de205c353ecc688356ab10acb303e073
Robert Strandh <robert.strandh@gmail.com>
2021-04-01 15:34:43 +0200

    Rename generic function to better reflect its purpose.

--- Code/Compiler/MIR-to-LIR/Register-allocation/allocate-registers-for-instructions.lisp
+++ Code/Compiler/MIR-to-LIR/Register-allocation/allocate-registers-for-instructions.lisp
@@ -2,15 +2,15 @@
 
 (defgeneric allocate-registers-for-instruction (predecessor instruction))
 
-(defgeneric compute-input-arrangement (predecessor instruction))
+(defgeneric process-inputs (predecessor instruction))
 
 (defgeneric compute-output-arrangement (predecessor instruction))
 
 (defmethod allocate-registers-for-instruction (predecessor instruction)
-  (let ((new-predecessor (compute-input-arrangement predecessor instruction)))
+  (let ((new-predecessor (process-inputs predecessor instruction)))
     (compute-output-arrangement new-predecessor instruction)))
 
-(defmethod compute-input-arrangement (predecessor instruction)
+(defmethod process-inputs (predecessor instruction)
   (let ((result predecessor))
     (loop for input in (cleavir-ir:inputs instruction)
           when (typep input 'cleavir-ir:lexical-location)
