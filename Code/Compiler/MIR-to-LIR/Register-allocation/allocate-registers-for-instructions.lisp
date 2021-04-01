f6c3b3f3b2cbe79adadc003b09425fce6eea9bf2
Robert Strandh <robert.strandh@gmail.com>
2021-04-01 18:10:47 +0200

    Redesign contract for PROCESS-OUTPUTS to only insert predecessors.

--- Code/Compiler/MIR-to-LIR/Register-allocation/allocate-registers-for-instructions.lisp
+++ Code/Compiler/MIR-to-LIR/Register-allocation/allocate-registers-for-instructions.lisp
@@ -77,30 +77,10 @@
 ;;; that the first input is dead after the instruction.  That fact
 ;;; makes it possible to use the same register for the output as for
 ;;; the first input.
-(defmethod comput-output-arrangement
+(defmethod process-outputs
     (predecessor (instruction cleavir-ir:binary-operation-mixin))
-  (let ((first-input (first (cleavir-ir:inputs instruction)))
-        (output (first (cleavir-ir:outputs instruction))))
-    (assert (not (eq first-input output)))
-    (with-arrangement-parts
-        (stack-map register-map attributions (output-arrangement instruction))
-      (let* ((input-arrangement (input-arrangement instruction))
-             (new-register-map (copy-register-map register-map))
-             (attribution
-               (find first-input (attributions input-arrangement)
-                     :test #'eq :key #'lexical-location))
-             (register-number (register-number attribution))
-             (new-attribution
-               (make-instance 'attribution
-                 :lexical-location output
-                 :stack-slot nil
-                 :register-number register-number)))
-        (reserve-register new-register-map register-number)
-        (setf (output-arrangement instruction)
-              (make-instance 'arrangement
-                :stack-map stack-map
-                :register-map new-register-map
-                :attributions (cons new-attribution attributions)))))))
+  (declare (ignore predecessor))
+  nil)
 
 ;;; All the following methods are for instructions that will turn into
 ;;; named calls, so the call-site manager will access the arguments
@@ -153,47 +133,23 @@
   (declare (ignore predecessor))
   nil)
 
-(defun allocate-and-attribute-new-register
-    (predecessor instruction lexical-location)
-  (let* ((input-pool (input-pool instruction))
-         (output-pool (output-pool instruction))
-         (output-arrangement (output-arrangement instruction))
-         (candidates (determine-candidates lexical-location output-pool)))
-    (multiple-value-bind (new-predecessor register-number)
-        (allocate-register predecessor instruction input-pool candidates)
-      (declare (ignore new-predecessor))
-      (setf (output-arrangement instruction)
-            (update-arrangement-for-new-definition
-             output-arrangement lexical-location register-number)))))
-
-;;;
-(defun allocate-or-reuse
-    (predecessor instruction input-attribution output-location pool)
-  (let ((input-location (lexical-location input-attribution)))
-    (if (or (eq input-location output-location)
-            (not (member input-location pool
-                         :test #'eq :key #'lexical-location)))
-        ;; Then we can reuse the same register
-        (setf (output-arrangement instruction)
-              (update-arrangement-for-new-definition
-               (output-arrangement instruction)
-               output-location
-               (register-number input-attribution)))
-        ;; Otherwise, we need to allocate a new register and
-        ;; attribute it to the output location.
-        (allocate-and-attribute-new-register
-         predecessor instruction output-location))))
+(defun ensure-unattributed (predecessor instruction lexical-location)
+  (let* ((pool (output-pool predecessor))
+         (candidates (determine-candidates lexical-location pool)))
+    (ensure-unattributed-register predecessor instruction pool candidates)))
 
 (defun process-outputs-default (predecessor instruction)
   (let* ((input (first (cleavir-ir:inputs instruction)))
-         (input-arrangement (input-arrangement instruction))
-         (input-attributions (attributions input-arrangement))
-         (input-attribution (find input input-attributions
-                                  :test #'eq :key #'lexical-location))
          (output (first (cleavir-ir:outputs instruction)))
          (pool (output-pool instruction)))
-    (allocate-or-reuse
-     predecessor instruction input-attribution output pool)))
+    ;; We do not need to allocate a new register if either the output
+    ;; and the input are the same or the input is dead after
+    ;; INSTRUCTION.
+    (unless (and (typep input 'cleavir-ir:lexical-location)
+                 (or (eq input output)
+                     (not (member input pool
+                                  :test #'eq :key #'lexical-location))))
+      (ensure-unattributed predecessor instruction output))))
 
 (defmethod process-outputs
     (predecessor (instruction cleavir-ir:memref1-instruction))
@@ -209,7 +165,7 @@
 
 (defmethod process-outputs
     (predecessor (instruction cleavir-ir:compute-argument-count-instruction))
-  (allocate-and-attribute-new-register
+  (ensure-unattributed
    predecessor instruction (first (cleavir-ir:outputs instruction))))
 
 (defmethod process-outputs
@@ -230,7 +186,7 @@
 
 (defmethod process-outputs
     (predecessor (instruction cleavir-ir:load-constant-instruction))
-  (allocate-and-attribute-new-register
+  (ensure-unattributed
    predecessor instruction (first (cleavir-ir:outputs instruction))))
 
 (defun allocate-registers-for-instructions (mir)
