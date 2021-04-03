137bf5c4ffce8f3f183df13161bd2062e18c2cbd
Robert Strandh <robert.strandh@gmail.com>
2021-04-03 09:27:29 +0200

    Define method on COMPUTE-OUTPUT-ARRANGEMENT specialized to INITIALIZE-RETURN-VALUES-INSTRUCTION.

--- Code/Compiler/MIR-to-LIR/Register-allocation/allocate-registers-for-instructions.lisp
+++ Code/Compiler/MIR-to-LIR/Register-allocation/allocate-registers-for-instructions.lisp
@@ -282,6 +282,12 @@
         (filter-arrangement
          (input-arrangement instruction) (output-pool instruction))))
 
+(defmethod compute-output-arrangement
+    ((instruction cleavir-ir:initialize-return-values-instruction))
+  (setf (output-arrangement instruction)
+        (filter-arrangement
+         (input-arrangement instruction) (output-pool instruction))))
+
 (defun compute-output-arrangement-no-inputs (instruction)
   (let* ((input-arrangement (input-arrangement instruction))
          (pool (output-pool instruction))
