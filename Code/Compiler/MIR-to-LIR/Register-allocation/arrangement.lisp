0228beab60e448b302ece4b88dedb79b36db3af6
Robert Strandh <robert.strandh@gmail.com>
2021-04-05 08:37:26 +0200

    New component containing definitions of accessors INPUT/OUTPUT-ARRANGEMENT.

---
+++ Code/Compiler/MIR-to-LIR/Register-allocation/arrangement.lisp
@@ -0,0 +1,26 @@
+(cl:in-package #:sicl-register-allocation)
+
+(defvar *input-arrangements*)
+
+(defun input-arrangement-p (instruction)
+  (nth-value 1 (gethash instruction *input-arrangements*)))
+
+(defun input-arrangement (instruction)
+  (multiple-value-bind (value value-p)
+      (gethash instruction *input-arrangements*)
+    (assert value-p)
+    value))
+
+(defun (setf input-arrangement) (arrangement instruction)
+  (setf (gethash instruction *input-arrangements*) arrangement))
+
+(defvar *output-arrangements*)
+
+(defun output-arrangement (instruction)
+  (multiple-value-bind (value value-p)
+      (gethash instruction *output-arrangements*)
+    (assert value-p)
+    value))
+
+(defun (setf output-arrangement) (arrangement instruction)
+  (setf (gethash instruction *output-arrangements*) arrangement))
--- Code/Compiler/MIR-to-LIR/Register-allocation/sicl-register-allocation.asd
+++ Code/Compiler/MIR-to-LIR/Register-allocation/sicl-register-allocation.asd
@@ -1,6 +1,7 @@
 (cl:in-package #:asdf-user)
 
 (defsystem #:sicl-register-allocation
+  :depeends-on (#:sicl-register-arrangement)
   :serial t
   :components
   ((:file "packages")
@@ -12,6 +13,7 @@
    (:file "estimated-distance-to-use")
    (:file "registers")
    (:file "stack-map")
+   (:file "arrangement")
    (:file "spill-unspill")
    (:file "allocate-register")
    (:file "allocate-registers-for-instructions")
