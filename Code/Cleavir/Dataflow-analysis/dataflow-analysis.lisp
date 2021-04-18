907f785f8b094d91395f367aacfde05fedfab903
Robert Strandh <robert.strandh@gmail.com>
2021-04-18 15:20:22 +0200

    Untabify and eliminate whitespace at end of line.

--- Code/Cleavir/Dataflow-analysis/dataflow-analysis.lisp
+++ Code/Cleavir/Dataflow-analysis/dataflow-analysis.lisp
@@ -21,50 +21,50 @@
   (;; This hash table maps each instruction to its corresponding
    ;; operation
    (%operations :initform (make-hash-table :test #'eq)
-		:initarg :operations
-		:accessor operations)))
+                :initarg :operations
+                :accessor operations)))
 
 (defun propagage-output (output operations)
   (let ((instruction (instruction (operation output)))
-	(datum (datum output))
-	(work-list '())
-	(on-work-list (make-hash-table :test #'eq)))
+        (datum (datum output))
+        (work-list '())
+        (on-work-list (make-hash-table :test #'eq)))
     (flet ((possibly-add-to-work-list (instruction)
-	     (unless (gethash instruction on-work-list)
-	       (setf (gethash instruction on-work-list) t)
-	       (push instruction work-list))))
+             (unless (gethash instruction on-work-list)
+               (setf (gethash instruction on-work-list) t)
+               (push instruction work-list))))
       (loop for successor in (cleavir-ir:successors instruction)
-	    do (possibly-add-to-work-list successor))
+            do (possibly-add-to-work-list successor))
       (loop until (null work-list)
-	    do (let* ((instruction (pop work-list))
-		      (operation (gethash instruction operations))
-		      (input (find datum (inputs operation)
-				   :key #'car :test #'eq)))
-		 (unless (null input)
-		   (pushnew output (outputs input)))
-		 (unless (member datum (cleavir-ir:outputs instruction)
-				 :test #'eq)
-		   (loop for successor in (cleavir-ir:successors instruction)
-			 do (possibly-add-to-work-list successor))))))))
+            do (let* ((instruction (pop work-list))
+                      (operation (gethash instruction operations))
+                      (input (find datum (inputs operation)
+                                   :key #'car :test #'eq)))
+                 (unless (null input)
+                   (pushnew output (outputs input)))
+                 (unless (member datum (cleavir-ir:outputs instruction)
+                                 :test #'eq)
+                   (loop for successor in (cleavir-ir:successors instruction)
+                         do (possibly-add-to-work-list successor))))))))
 
 (defun dataflow-analysis (initial-instruction)
   (let ((result (make-instance 'dataflow)))
     (cleavir-ir:map-instructions-arbitrary-order
      (lambda (instruction)
        (let* ((operation (make-instance 'operation
-			   :instruction instruction))
-	      (inputs (loop for input in (cleavir-ir:inputs instruction)
-			    collect (make-instance 'input
-				      :operation operation
-				      :datum input)))
-	      (outputs (loop for output in (cleavir-ir:outputs instruction)
-			     collect (make-instance 'output
-				       :operation operation
-				       :datum output))))
-	 (reinitialize-instance operation
-				:inputs inputs
-				:outputs outputs)
-	 (setf (gethash instruction (operations result))
-	       operation)))
+                           :instruction instruction))
+              (inputs (loop for input in (cleavir-ir:inputs instruction)
+                            collect (make-instance 'input
+                                      :operation operation
+                                      :datum input)))
+              (outputs (loop for output in (cleavir-ir:outputs instruction)
+                             collect (make-instance 'output
+                                       :operation operation
+                                       :datum output))))
+         (reinitialize-instance operation
+                                :inputs inputs
+                                :outputs outputs)
+         (setf (gethash instruction (operations result))
+               operation)))
      initial-instruction)
     result))
--- Code/Cleavir/Dominance/test-dominance.lisp
+++ Code/Cleavir/Dominance/test-dominance.lisp
@@ -16,145 +16,145 @@
 ;;; dominators of N.
 (defun slow-dominators (start-node successor-fun)
   (let* ((preorder (depth-first-search-preorder start-node successor-fun))
-	 (dominators (let ((table (make-hash-table :test #'eq)))
-		       (loop for node in preorder
-			     do (setf (gethash node table) preorder))
-		       table)))
+         (dominators (let ((table (make-hash-table :test #'eq)))
+                       (loop for node in preorder
+                             do (setf (gethash node table) preorder))
+                       table)))
     (flet ((successors (node)
-	     (funcall successor-fun node))
-	   (dominators (node)
-	     (gethash node dominators))
-	   ((setf dominators) (new-dominators node)
-	     (setf (gethash node dominators) new-dominators)))
+             (funcall successor-fun node))
+           (dominators (node)
+             (gethash node dominators))
+           ((setf dominators) (new-dominators node)
+             (setf (gethash node dominators) new-dominators)))
       (loop for node in preorder
-	    do (labels ((traverse (n path)
-			  (cond ((eq n node)
-				 (setf (dominators node)
-				       (intersection (dominators node)
-						     (cons n path)
-						     :test #'eq)))
-				((member n path :test #'eq)
-				 nil)
-				(t
-				 (loop for succ in (successors n)
-				       do (traverse succ (cons n path)))))))
-		 (traverse start-node '()))))
+            do (labels ((traverse (n path)
+                          (cond ((eq n node)
+                                 (setf (dominators node)
+                                       (intersection (dominators node)
+                                                     (cons n path)
+                                                     :test #'eq)))
+                                ((member n path :test #'eq)
+                                 nil)
+                                (t
+                                 (loop for succ in (successors n)
+                                       do (traverse succ (cons n path)))))))
+                 (traverse start-node '()))))
     dominators))
 
 (defun test-one-chart (start-node)
   (let ((d1 (cleavir-dominance:dominance-tree start-node #'successors))
-	(d2 (slow-dominators start-node #'successors))
-	(preorder (depth-first-search-preorder start-node #'successors)))
+        (d2 (slow-dominators start-node #'successors))
+        (preorder (depth-first-search-preorder start-node #'successors)))
     (loop for node in preorder
-	  do (unless (and (null (set-difference (gethash node d1)
-						(gethash node d2)))
-			  (null (set-difference (gethash node d2)
-						(gethash node d1))))
-	       (return (values node d1 d2))))))
+          do (unless (and (null (set-difference (gethash node d1)
+                                                (gethash node d2)))
+                          (null (set-difference (gethash node d2)
+                                                (gethash node d1))))
+               (return (values node d1 d2))))))
 
 (defun test-dominance (&optional (n 10000))
   (loop repeat n
-	do (let ((f (cleavir-test-utilities:random-flow-chart)))
-	     (when (< (cleavir-utilities:count-nodes
-		       f #'cleavir-test-utilities:successors)
-		      50)
-	       (test-one-chart f)))))
+        do (let ((f (cleavir-test-utilities:random-flow-chart)))
+             (when (< (cleavir-utilities:count-nodes
+                       f #'cleavir-test-utilities:successors)
+                      50)
+               (test-one-chart f)))))
 
 (defun draw-dominance-tree (dominance-tree filename)
   (with-open-file (stream filename
-			  :direction :output
-			  :if-exists :supersede)
+                          :direction :output
+                          :if-exists :supersede)
     (format stream "digraph G {~%   ordering = in;~%")
     ;; Draw all the nodes first.
     (loop for node being each hash-key of dominance-tree
-	  do (format stream "   ~a [label = \"~a\"];~%"
-		     (name node) (name node)))
+          do (format stream "   ~a [label = \"~a\"];~%"
+                     (name node) (name node)))
     ;; Now draw all the arcs.
     (loop for node being each hash-key of dominance-tree
-	  do (let ((idom (cleavir-dominance:immediate-dominator
-			  dominance-tree node)))
-	       (unless (null idom)
-		 (format stream "   ~a -> ~a;~%"
-			 (name node) (name idom)))))
+          do (let ((idom (cleavir-dominance:immediate-dominator
+                          dominance-tree node)))
+               (unless (null idom)
+                 (format stream "   ~a -> ~a;~%"
+                         (name node) (name idom)))))
     (format stream "}~%")))
 
 (defun draw-dominance-frontiers
     (start-node successor-fun dominance-frontiers filename)
   (flet ((successors (node)
-	   (funcall successor-fun node)))
+           (funcall successor-fun node)))
     (let ((table (make-hash-table :test #'eq)))
       (with-open-file (stream filename
-			      :direction :output
-			      :if-exists :supersede)
-	(format stream "digraph G {~% ordering = out;~%")
-	(labels ((draw-node (node)
-		   (unless (gethash node table)
-		     (setf (gethash node table) t)
-		     (loop for succ in (successors node)
-			   do (draw-node succ))
-		     (format stream
-			     "   ~a [label = \"~a\"];~%"
-			     (name node) (name node))
-		     (loop for succ in (successors node)
-			   do (format stream
-				      "   ~a -> ~a;~%"
-				      (name node)
-				      (name succ))))))
-	  (draw-node start-node))
-	(loop for node being each hash-key of dominance-frontiers
-	      do (let ((f (gethash node dominance-frontiers)))
-		   (loop for ff in f
-			 do (format stream
-				    "   ~a -> ~a [style = bold, color = red];"
-				    (name node) (name ff)))))
-	(format stream "}~%")))))
+                              :direction :output
+                              :if-exists :supersede)
+        (format stream "digraph G {~% ordering = out;~%")
+        (labels ((draw-node (node)
+                   (unless (gethash node table)
+                     (setf (gethash node table) t)
+                     (loop for succ in (successors node)
+                           do (draw-node succ))
+                     (format stream
+                             "   ~a [label = \"~a\"];~%"
+                             (name node) (name node))
+                     (loop for succ in (successors node)
+                           do (format stream
+                                      "   ~a -> ~a;~%"
+                                      (name node)
+                                      (name succ))))))
+          (draw-node start-node))
+        (loop for node being each hash-key of dominance-frontiers
+              do (let ((f (gethash node dominance-frontiers)))
+                   (loop for ff in f
+                         do (format stream
+                                    "   ~a -> ~a [style = bold, color = red];"
+                                    (name node) (name ff)))))
+        (format stream "}~%")))))
     
 ;;; The dominance frontier of a node X is defined to be the set of all
 ;;; nodes Y such that 1: there exists a predecessor Z of Y such that X
 ;;; dominates Z, and 2: X does not strictly dominate Y.
 (defun slow-dominance-frontiers (start-node successor-fun)
   (let ((predecessor-fun (cleavir-utilities:predecessor-function
-			  start-node successor-fun))
-	(dominance-tree (cleavir-dominance:dominance-tree
-			 start-node successor-fun))
-	(preorder (cleavir-utilities:depth-first-search-preorder
-		   start-node successor-fun))
-	(result (make-hash-table :test #'eq)))
+                          start-node successor-fun))
+        (dominance-tree (cleavir-dominance:dominance-tree
+                         start-node successor-fun))
+        (preorder (cleavir-utilities:depth-first-search-preorder
+                   start-node successor-fun))
+        (result (make-hash-table :test #'eq)))
     (flet ((predecessors (node)
-	     (funcall predecessor-fun node))
-	   (dominates (n1 n2)
-	     (member n1 (cleavir-dominance:dominators
-			 dominance-tree n2)))
-	   (strictly-dominates (n1 n2)
-	     (member n1 (cleavir-dominance:strict-dominators
-			 dominance-tree n2))))	     
+             (funcall predecessor-fun node))
+           (dominates (n1 n2)
+             (member n1 (cleavir-dominance:dominators
+                         dominance-tree n2)))
+           (strictly-dominates (n1 n2)
+             (member n1 (cleavir-dominance:strict-dominators
+                         dominance-tree n2))))
       (loop for x in preorder
-	    do (loop for y in preorder
-		     do (when (and (some (lambda (z)
-					   (dominates x z))
-					 (predecessors y))
-				   (not (strictly-dominates x y)))
-			  (push y (gethash x result))))))
+            do (loop for y in preorder
+                     do (when (and (some (lambda (z)
+                                           (dominates x z))
+                                         (predecessors y))
+                                   (not (strictly-dominates x y)))
+                          (push y (gethash x result))))))
     result))
     
 (defun test-dominance-frontiers-one-flow-chart (start-node successor-fun)
   (flet ((same-set-p (s1 s2)
-	   (and (null (set-difference s1 s2 :test #'eq))
-		(null (set-difference s2 s1 :test #'eq)))))
+           (and (null (set-difference s1 s2 :test #'eq))
+                (null (set-difference s2 s1 :test #'eq)))))
     (let ((f1 (cleavir-dominance:dominance-frontiers
-	       start-node successor-fun))
-	  (f2 (slow-dominance-frontiers
-	       start-node successor-fun)))
+               start-node successor-fun))
+          (f2 (slow-dominance-frontiers
+               start-node successor-fun)))
       (loop for node being each hash-key of f1
-	    do (unless (same-set-p (gethash node f1) (gethash node f2))
-		 (throw 'discrepancy (values start-node f1 f2))))
+            do (unless (same-set-p (gethash node f1) (gethash node f2))
+                 (throw 'discrepancy (values start-node f1 f2))))
       (loop for node being each hash-key of f2
-	    do (unless (same-set-p (gethash node f1) (gethash node f2))
-		 (throw 'discrepancy (values start-node f1 f2)))))))
+            do (unless (same-set-p (gethash node f1) (gethash node f2))
+                 (throw 'discrepancy (values start-node f1 f2)))))))
 
 (defun test-dominance-frontiers (&optional (n 10000))
   (catch 'discrepancy
     (loop repeat n
-	  do (test-dominance-frontiers-one-flow-chart
-	      (cleavir-test-utilities:random-flow-chart)
-	      #'cleavir-test-utilities:successors))))
+          do (test-dominance-frontiers-one-flow-chart
+              (cleavir-test-utilities:random-flow-chart)
+              #'cleavir-test-utilities:successors))))
