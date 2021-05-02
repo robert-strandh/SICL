b5e80d3e82ec41d26a4594e8fa45be599a008eb0
Robert Strandh <robert.strandh@gmail.com>
2021-05-02 16:47:18 +0200

    Define English-language condition reporter for new condition type.

--- Code/Array/condition-reporters-english.lisp
+++ Code/Array/condition-reporters-english.lisp
@@ -58,3 +58,19 @@
           (given-array condition)
           (type-error-datum condition)
           (array-total-size (given-array condition))))
+
+(defmethod acclimation:report-condition
+    ((condition incompatible-type-for-new-element)
+     stream
+     (language acclimation:english))
+  (format stream
+          "The new element to be stored in the array is:~@
+           ~s of type ~s,~@
+           but the element type of the array to store it in:
+           ~s~@
+           is ~s,~@
+           which is incompatible with the type of the new element."
+          (type-error-datum condition)
+          (type-of (type-error-datum condition))
+          (given-array condition)
+          (type-error-expected-type condition)))
