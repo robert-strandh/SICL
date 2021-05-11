24d6205b0b4ef5853abcd6cebf2fb2ba4f1ab791
Hayley Patton <theemacsshibe@gmail.com>
2021-05-11 13:02:59 +1000

    Convert STANDARD-OBJECT-CLASS-OF-INSTRUCTIONs in HIR-to-MIR

--- Code/Compiler/HIR-to-MIR/standard-object.lisp
+++ Code/Compiler/HIR-to-MIR/standard-object.lisp
@@ -1,6 +1,6 @@
 (cl:in-package #:sicl-hir-to-mir)
 
-(defmethod  process-instruction
+(defmethod process-instruction
     (client (instruction cleavir-ir:nook-read-instruction) code-object)
   (destructuring-bind (object-location slot-number-location)
       (cleavir-ir:inputs instruction)
@@ -14,7 +14,7 @@
       (change-class instruction 'cleavir-ir:memref1-instruction
                     :address slot-location))))
 
-(defmethod  process-instruction
+(defmethod process-instruction
     (client (instruction cleavir-ir:nook-write-instruction) code-object)
   (destructuring-bind (object-location slot-number-location value-location)
       (cleavir-ir:inputs instruction)
@@ -28,3 +28,15 @@
       (change-class instruction 'cleavir-ir:memset1-instruction
                     :address slot-location
                     :value value-location))))
+
+(defmethod process-instruction
+    (client
+     (instruction cleavir-ir:standard-object-class-of-instruction)
+     code-object)
+  (destructuring-bind (object-location)
+      (cleavir-ir:inputs instruction)
+    (change-class instruction
+                  'cleavir-ir:memref2-instruction
+                  :base-address object-location
+                  :offset (make-instance 'cleavir-ir:immediate-input
+                                         :value -5))))
