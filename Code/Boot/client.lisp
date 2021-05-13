480dab927116cebb2f91b5fba7c07cdff597508f
Robert Strandh <robert.strandh@gmail.com>
2021-05-13 16:49:58 +0200

    Make CLIENT a subclass of OPERATION.

--- Code/Boot/client.lisp
+++ Code/Boot/client.lisp
@@ -1,5 +1,27 @@
 (cl:in-package #:sicl-boot)
 
-(defclass client (sicl-client:sicl)
-  ((%environment :initarg :environment :reader environment)))
+;;; An instance of this class is used both as the CLIENT argument in
+;;; operations that require such an argument, and as an ASDF operation
+;;; object to pass to ASDF:OPERATE.
+(defclass client (sicl-client:sicl asdf:operation)
+  ((%environment :initarg :environment :reader environment)
+   (asdf:sideway-operation :initform 'client :allocation :class)))
 
+(defun make-client (class &rest initargs)
+  (let ((client (asdf:make-operation class)))
+    (apply #'reinitialize-instance client initargs)
+    client))
+
+(defgeneric loaded-files (environment))
+
+(defgeneric (setf loaded-files) (loaded-files environment))
+
+(defmethod asdf:perform ((operation client) component)
+  (format *trace-output*
+          "Not acting upon component ~s~%"
+          component))
+
+(defmethod asdf:perform ((operation client) (component asdf/system:system))
+  (format *trace-output*
+          "Done loading system ~s~%"
+          component))
