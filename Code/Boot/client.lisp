0e8331f8393c87093b0289caf221ad584e965c3e
Robert Strandh <robert.strandh@gmail.com>
2021-05-16 10:36:36 +0200

    Make LOAD-OP a separate ASDF operation class, no longer CLIENT.

--- Code/Boot/client.lisp
+++ Code/Boot/client.lisp
@@ -1,26 +1,22 @@
 (cl:in-package #:sicl-boot)
 
-;;; An instance of this class is used both as the CLIENT argument in
-;;; operations that require such an argument, and as an ASDF operation
-;;; object to pass to ASDF:OPERATE.
-(defclass client
-    (sicl-client:sicl asdf/action:downward-operation asdf/action:selfward-operation)
-  ((%environment :initarg :environment :reader environment)
-   (asdf:selfward-operation :initform '(prepare-op) :allocation :class)))
+(defclass client (sicl-client:sicl)
+  ((%environment :initarg :environment :reader environment)))
+
+(defclass load-op (asdf/action:downward-operation asdf/action:selfward-operation)
+  ((asdf:selfward-operation :initform '(prepare-op) :allocation :class)))
 
 (defclass prepare-op (asdf/action:upward-operation asdf/action:sideway-operation)
-  ((asdf:sideway-operation :initform 'client :allocation :class)))
+  ((asdf:sideway-operation :initform 'load-op :allocation :class)))
 
 (defun make-client (class &rest initargs)
-  (let ((client (asdf:make-operation class)))
-    (apply #'reinitialize-instance client initargs)
-    client))
+  (apply #'make-instance class initargs))
 
 (defgeneric loaded-files (environment))
 
 (defgeneric (setf loaded-files) (loaded-files environment))
 
-(defmethod asdf:perform ((operation client) component)
+(defmethod asdf:perform ((operation load-op) component)
   (format *trace-output*
           "Not acting upon component ~s~%"
           component))
@@ -28,13 +24,13 @@
 (defvar *environment*)
 
 (defmethod asdf:perform
-    ((operation client) (component asdf/lisp-action:cl-source-file))
+    ((operation load-op) (component asdf/lisp-action:cl-source-file))
   (format *trace-output* "Loading ~s~%" component)
   (load-source-file-common
    (asdf/component:component-pathname component)
    *environment*))
 
-(defmethod asdf:perform ((operation client) (component asdf/system:system))
+(defmethod asdf:perform ((operation load-op) (component asdf/system:system))
   (format *trace-output*
           "Done loading system ~s~%"
           component))
@@ -56,13 +52,13 @@
       (values nil nil)))
 
 (defmethod asdf/action:compute-action-stamp
-    (plan (operation client) component &key just-done)
+    (plan (operation load-op) component &key just-done)
   (if just-done
       (values (get-universal-time) t)
       (values nil nil)))
 
 (defmethod asdf/action:compute-action-stamp
-    (plan (operation client) (component asdf/lisp-action:cl-source-file) &key just-done)
+    (plan (operation load-op) (component asdf/lisp-action:cl-source-file) &key just-done)
   (if just-done
       (values (get-universal-time) t)
       (let* ((files (loaded-files *environment*))
