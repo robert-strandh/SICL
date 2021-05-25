ca0355d3db9de4ca65933afd703d7766516a907b
Robert Strandh <robert.strandh@gmail.com>
2021-05-25 18:02:35 +0200

    Improve indentation.

--- Code/Cons/pushnew-defmacro.lisp
+++ Code/Cons/pushnew-defmacro.lisp
@@ -1,15 +1,17 @@
 (cl:in-package #:sicl-cons)
 
-(defmacro pushnew (item place
-		   &environment environment
-		   &rest args
-		   &key
-		   (key nil key-p)
-		   (test nil test-p)
-		   (test-not nil test-not-p))
+(defmacro pushnew
+    (item place
+     &environment environment
+     &rest args
+     &key
+       (key nil key-p)
+       (test nil test-p)
+       (test-not nil test-not-p))
   (let* ((global-env (sicl-environment:global-environment environment))
          (client (sicl-environment:client global-env)))
-    (pushnew-expander client item place environment args
-                      key key-p
-                      test test-p
-                      test-not test-not-p)))
+    (pushnew-expander
+     client item place environment args
+     key key-p
+     test test-p
+     test-not test-not-p)))
