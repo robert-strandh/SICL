4a53ffadc3547e6328651e88f1284671e04541dc
Robert Strandh <robert.strandh@gmail.com>
2021-05-26 16:47:13 +0200

    Untabify, and add FIXME comment suggesting a rewrite.

--- Code/Cons/subsetp-defun.lisp
+++ Code/Cons/subsetp-defun.lisp
@@ -1,5 +1,8 @@
 (cl:in-package #:sicl-cons)
 
+;;; FIXME: rewrite this function to use the same techniqe
+;;; as is used by the sequence functions.
+
 (defun |subsetp key=identity test=eql| (name list1 list2)
   (with-proper-list-elements (element list1 name)
     (unless (|member test=eql key=identity| name element list2)
@@ -54,7 +57,7 @@
       (setf (gethash element table) t))
     (with-proper-list-elements (element list1 name)
       (unless (gethash element table)
-	(return-from |subsetp key=identity test=eq hash| nil))))
+        (return-from |subsetp key=identity test=eq hash| nil))))
   t)
 
 (defun |subsetp key=identity test=eql hash| (name list1 list2)
@@ -63,7 +66,7 @@
       (setf (gethash element table) t))
     (with-proper-list-elements (element list1 name)
       (unless (gethash element table)
-	(return-from |subsetp key=identity test=eql hash| nil))))
+        (return-from |subsetp key=identity test=eql hash| nil))))
   t)
 
 (defun |subsetp key=identity test=equal hash| (name list1 list2)
@@ -72,7 +75,7 @@
       (setf (gethash element table) t))
     (with-proper-list-elements (element list1 name)
       (unless (gethash element table)
-	(return-from |subsetp key=identity test=equal hash| nil))))
+        (return-from |subsetp key=identity test=equal hash| nil))))
   t)
 
 (defun |subsetp key=identity test=equalp hash| (name list1 list2)
@@ -81,7 +84,7 @@
       (setf (gethash element table) t))
     (with-proper-list-elements (element list1 name)
       (unless (gethash element table)
-	(return-from |subsetp key=identity test=equalp hash| nil))))
+        (return-from |subsetp key=identity test=equalp hash| nil))))
   t)
 
 (defun |subsetp key=other test=eq hash| (name list1 list2 key)
@@ -90,7 +93,7 @@
       (setf (gethash (funcall key element) table) t))
     (with-proper-list-elements (element list1 name)
       (unless (gethash (funcall key element) table)
-	(return-from |subsetp key=other test=eq hash| nil))))
+        (return-from |subsetp key=other test=eq hash| nil))))
   t)
 
 (defun |subsetp key=other test=eql hash| (name list1 list2 key)
@@ -99,7 +102,7 @@
       (setf (gethash (funcall key element) table) t))
     (with-proper-list-elements (element list1 name)
       (unless (gethash (funcall key element) table)
-	(return-from |subsetp key=other test=eql hash| nil))))
+        (return-from |subsetp key=other test=eql hash| nil))))
   t)
 
 (defun |subsetp key=other test=equal hash| (name list1 list2 key)
@@ -108,7 +111,7 @@
       (setf (gethash (funcall key element) table) t))
     (with-proper-list-elements (element list1 name)
       (unless (gethash (funcall key element) table)
-	(return-from |subsetp key=other test=equal hash| nil))))
+        (return-from |subsetp key=other test=equal hash| nil))))
   t)
 
 (defun |subsetp key=other test=equalp hash| (name list1 list2 key)
@@ -117,60 +120,60 @@
       (setf (gethash (funcall key element) table) t))
     (with-proper-list-elements (element list1 name)
       (unless (gethash (funcall key element) table)
-	(return-from |subsetp key=other test=equalp hash| nil))))
+        (return-from |subsetp key=other test=equalp hash| nil))))
   t)
 
 (defun subsetp (list1 list2
-		&key key (test nil test-given) (test-not nil test-not-given))
+                &key key (test nil test-given) (test-not nil test-not-given))
   (when (and test-given test-not-given)
     (error 'both-test-and-test-not-given :name 'subsetp))
   (let ((use-hash (> (* (length list1) (length list2)) 1000)))
     (if key
-	(if test-given
-	    (cond ((or (eq test #'eq) (eq test 'eq))
-		   (if use-hash
-		       (|subsetp key=other test=eq hash| 'subsetp list1 list2 key)
-		       (|subsetp key=other test=eq| 'subsetp list1 list2 key)))
-		  ((or (eq test #'eql) (eq test 'eql))
-		   (if use-hash
-		       (|subsetp key=other test=eql hash| 'subsetp list1 list2 key)
-		       (|subsetp key=other test=eql| 'subsetp list1 list2 key)))
-		  ((or (eq test #'equal) (eq test 'equal))
-		   (if use-hash
-		       (|subsetp key=other test=equal hash| 'subsetp list1 list2 key)
-		       (|subsetp key=other test=other| 'subsetp list1 list2 key #'equal)))
-		  ((or (eq test #'equalp) (eq test 'equalp))
-		   (if use-hash
-		       (|subsetp key=other test=equalp hash| 'subsetp list1 list2 key)
-		       (|subsetp key=other test=other| 'subsetp list1 list2 key #'equalp)))
-		  (t
-		   (|subsetp key=other test=other| 'subsetp list1 list2 key test)))
-	    (if test-not-given
-		(|subsetp key=other test-not=other| 'subsetp list1 list2 key test-not)
-		(if use-hash
-		    (|subsetp key=other test=eql hash| 'subsetp list1 list2 key)
-		    (|subsetp key=other test=eql| 'subsetp list1 list2 key))))
-	(if test-given
-	    (cond ((or (eq test #'eq) (eq test 'eq))
-		   (if use-hash
-		       (|subsetp key=identity test=eq hash| 'subsetp list1 list2)
-		       (|subsetp key=identity test=eq| 'subsetp list1 list2)))
-		  ((or (eq test #'eql) (eq test 'eql))
-		   (if use-hash
-		       (|subsetp key=identity test=eql hash| 'subsetp list1 list2)
-		       (|subsetp key=identity test=eql| 'subsetp list1 list2)))
-		  ((or (eq test #'equal) (eq test 'equal))
-		   (if use-hash
-		       (|subsetp key=identity test=equal hash| 'subsetp list1 list2)
-		       (|subsetp key=identity test=other| 'subsetp list1 list2 #'equal)))
-		  ((or (eq test #'equalp) (eq test 'equalp))
-		   (if use-hash
-		       (|subsetp key=identity test=equalp hash| 'subsetp list1 list2)
-		       (|subsetp key=identity test=other| 'subsetp list1 list2 #'equalp)))
-		  (t
-		   (|subsetp key=identity test=other| 'subsetp list1 list2 test)))
-	    (if test-not-given
-		(|subsetp key=identity test-not=other| 'subsetp list1 list2 test-not)
-		(if use-hash
-		    (|subsetp key=identity test=eql hash| 'subsetp list1 list2)
-		    (|subsetp key=identity test=eql| 'subsetp list1 list2)))))))
+        (if test-given
+            (cond ((or (eq test #'eq) (eq test 'eq))
+                   (if use-hash
+                       (|subsetp key=other test=eq hash| 'subsetp list1 list2 key)
+                       (|subsetp key=other test=eq| 'subsetp list1 list2 key)))
+                  ((or (eq test #'eql) (eq test 'eql))
+                   (if use-hash
+                       (|subsetp key=other test=eql hash| 'subsetp list1 list2 key)
+                       (|subsetp key=other test=eql| 'subsetp list1 list2 key)))
+                  ((or (eq test #'equal) (eq test 'equal))
+                   (if use-hash
+                       (|subsetp key=other test=equal hash| 'subsetp list1 list2 key)
+                       (|subsetp key=other test=other| 'subsetp list1 list2 key #'equal)))
+                  ((or (eq test #'equalp) (eq test 'equalp))
+                   (if use-hash
+                       (|subsetp key=other test=equalp hash| 'subsetp list1 list2 key)
+                       (|subsetp key=other test=other| 'subsetp list1 list2 key #'equalp)))
+                  (t
+                   (|subsetp key=other test=other| 'subsetp list1 list2 key test)))
+            (if test-not-given
+                (|subsetp key=other test-not=other| 'subsetp list1 list2 key test-not)
+                (if use-hash
+                    (|subsetp key=other test=eql hash| 'subsetp list1 list2 key)
+                    (|subsetp key=other test=eql| 'subsetp list1 list2 key))))
+        (if test-given
+            (cond ((or (eq test #'eq) (eq test 'eq))
+                   (if use-hash
+                       (|subsetp key=identity test=eq hash| 'subsetp list1 list2)
+                       (|subsetp key=identity test=eq| 'subsetp list1 list2)))
+                  ((or (eq test #'eql) (eq test 'eql))
+                   (if use-hash
+                       (|subsetp key=identity test=eql hash| 'subsetp list1 list2)
+                       (|subsetp key=identity test=eql| 'subsetp list1 list2)))
+                  ((or (eq test #'equal) (eq test 'equal))
+                   (if use-hash
+                       (|subsetp key=identity test=equal hash| 'subsetp list1 list2)
+                       (|subsetp key=identity test=other| 'subsetp list1 list2 #'equal)))
+                  ((or (eq test #'equalp) (eq test 'equalp))
+                   (if use-hash
+                       (|subsetp key=identity test=equalp hash| 'subsetp list1 list2)
+                       (|subsetp key=identity test=other| 'subsetp list1 list2 #'equalp)))
+                  (t
+                   (|subsetp key=identity test=other| 'subsetp list1 list2 test)))
+            (if test-not-given
+                (|subsetp key=identity test-not=other| 'subsetp list1 list2 test-not)
+                (if use-hash
+                    (|subsetp key=identity test=eql hash| 'subsetp list1 list2)
+                    (|subsetp key=identity test=eql| 'subsetp list1 list2)))))))
