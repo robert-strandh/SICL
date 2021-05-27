0f0aed15d162177f1bc82a07e0d38c906b3b8f00
Robert Strandh <robert.strandh@gmail.com>
2021-05-27 17:19:53 +0200

    Untabify and remove useless blank lines.

--- Code/Cleavir/Def-use-chains/def-use-chains.lisp
+++ Code/Cleavir/Def-use-chains/def-use-chains.lisp
@@ -6,20 +6,18 @@
 ;;; definition is used.
 (defun def-use-chains (start-node successor-fun in-fun out-fun)
   (let ((reaching-definitions
-	  (cleavir-reaching-definitions:reaching-definitions
-	   start-node successor-fun out-fun))
-	(def-use-chains (make-hash-table :test #'eq)))
+          (cleavir-reaching-definitions:reaching-definitions
+           start-node successor-fun out-fun))
+        (def-use-chains (make-hash-table :test #'eq)))
     (cleavir-utilities:map-nodes
      start-node
      successor-fun
      (lambda (node)
        (loop for reaching in (gethash node reaching-definitions)
-	     do (when (member (cdr reaching) (funcall in-fun node))
-		  (push node (gethash reaching def-use-chains))))))
+             do (when (member (cdr reaching) (funcall in-fun node))
+                  (push node (gethash reaching def-use-chains))))))
     (let ((result '()))
       (maphash (lambda (definition nodes)
-		 (push (cons definition nodes) result))
-	       def-use-chains)
+                 (push (cons definition nodes) result))
+               def-use-chains)
       result)))
-
-	       
