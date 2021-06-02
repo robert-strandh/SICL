b6b1a627a5df42ce04264f3e57183b4afe8e6e60
Robert Strandh <robert.strandh@gmail.com>
2021-06-02 10:17:05 +0200

    Define accessor FUNCALLABLE-STANDARD-INSTANCE-ACCESS.

--- Code/CLOS/standard-instance-access.lisp
+++ Code/CLOS/standard-instance-access.lisp
@@ -8,3 +8,12 @@
 (defun (setf standard-instance-access) (new-value instance location)
   (cleavir-primop:nook-write instance location new-value)
   new-value)
+
+;;; For the specification of this function, see
+;;; http://metamodular.com/CLOS-MOP/funcallable-standard-instance-access.html
+(defun funcallable-standard-instance-access (instance location)
+  (cleavir-primop:nook-read instance location))
+
+(defun (setf funcallable-standard-instance-access) (new-value instance location)
+  (cleavir-primop:nook-write instance location new-value)
+  new-value)
