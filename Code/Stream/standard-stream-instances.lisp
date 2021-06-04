c31f2e7ce80e45358c509f1eaaed4c472eb4cc8d
Robert Strandh <robert.strandh@gmail.com>
2021-06-04 15:25:47 +0200

    Define input streams *unix-standard-input* and *standard-input*.

--- Code/Stream/standard-stream-instances.lisp
+++ Code/Stream/standard-stream-instances.lisp
@@ -12,3 +12,16 @@
 (defparameter *standard-output*
   (make-instance 'character-to-binary-output-stream
     :binary-stream *unix-standard-output*))
+
+(defparameter *unix-standard-input*
+  (make-instance 'unix-byte-input-stream
+    :file-descriptor 0
+    :buffer
+    (make-instance 'sicl-array:vector-unsigned-byte-8
+      :dimensions '(256)
+      :fill-pointer 0
+      :additional-space 32)))
+
+(defparameter *standard-input*
+  (make-instance 'binary-to-character-input-stream
+    :binary-stream *unix-standard-input*))
