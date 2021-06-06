7c10626cbc0a49b8b448da9f14240c0beb3d1d2f
Robert Strandh <robert.strandh@gmail.com>
2021-06-06 15:31:39 +0200

    New file containing embryonic code for loading Eclector into e5.

---
+++ Code/Boot/Phase-6/load-eclector.lisp
@@ -0,0 +1,9 @@
+(cl:in-package #:sicl-boot-phase-6)
+
+(defun load-eclector (e5)
+  ;; Eclector uses EVAL in some compiler macros to evaluate some
+  ;; Boolean arguments, but it is applied only to contstants
+  ;; so we can use the host EVAL.
+  (import-functions-from-host '(eval) e5)
+  (let ((*features* '(:sicl)))
+    (load-asdf-system '#:eclector e5)))
