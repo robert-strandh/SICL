b0e261df523cb8ea4b034ecc523d5ddd2550f3e6
Robert Strandh <robert.strandh@gmail.com>
2021-06-07 11:22:13 +0200

    Define accessor generic functions in the same file as the class.

--- Code/Arithmetic/complex-defclass.lisp
+++ Code/Arithmetic/complex-defclass.lisp
@@ -1,5 +1,9 @@
 (cl:in-package #:sicl-arithmetic)
 
+(defgeneric realpart (number))
+
+(defgeneric imagpart (number))
+
 (defclass complex (number standard-object)
   ((%realpart :initarg :realpart :reader realpart)
    (%imagpart :initarg :imagpart :reader imagpart)))
