7b693a8969ca70149ba206bb2131c41d869df91c
Robert Strandh <robert.strandh@gmail.com>
2021-06-10 10:03:31 +0200

    Load new file contining definition of constant CHAR-CODE-LIMIT.

--- Code/Boot/Phase-6/load-ctype.lisp
+++ Code/Boot/Phase-6/load-ctype.lisp
@@ -1,7 +1,8 @@
 (cl:in-package #:sicl-boot-phase-6)
 
 (defun load-ctype (e5)
-  (import-functions-from-host '(map substitute) e5)
+  (import-functions-from-host '(map substitute functionp) e5)
   (load-source-file "Arithmetic/realp-defgeneric.lisp" e5)
   (load-source-file "Arithmetic/rationalp-defgeneric.lisp" e5)
-  (load-source-file "Arithmetic/floatp-defgeneric.lisp" e5))
+  (load-source-file "Arithmetic/floatp-defgeneric.lisp" e5)
+  (load-source-file "Character/char-code-limit-defconstant.lisp" e5))
