1ab460620d309ccbd6fa85bb73c35eced051f86c
Robert Strandh <robert.strandh@gmail.com>
2021-06-11 15:22:32 +0200

    Load new file containing definition of function MAKE-ARRAY into e5.

--- Code/Boot/Phase-6/load-eclector.lisp
+++ Code/Boot/Phase-6/load-eclector.lisp
@@ -1,6 +1,7 @@
 (cl:in-package #:sicl-boot-phase-6)
 
 (defun load-eclector (e5)
+  (load-source-file "Array/make-array.lisp" e5)
   ;; Eclector uses EVAL in some compiler macros to evaluate some
   ;; Boolean arguments, but it is applied only to contstants
   ;; so we can use the host EVAL.
