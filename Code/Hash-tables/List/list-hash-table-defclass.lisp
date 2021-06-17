655dd75d4e49592812b7786829446694d3096582
Robert Strandh <robert.strandh@gmail.com>
2021-06-17 14:36:53 +0200

    Increase the default values to satisfy assertion.

--- Code/Hash-tables/List/list-hash-table-defclass.lisp
+++ Code/Hash-tables/List/list-hash-table-defclass.lisp
@@ -3,6 +3,6 @@
 (defclass list-hash-table (hash-table)
   ((%size :initform 0 :initarg :size :accessor size :reader hash-table-size)
    (%contents :initform '() :accessor contents))
-  (:default-initargs :rehash-size 1 :rehash-threshold 1))
+  (:default-initargs :rehash-size 2 :rehash-threshold 2))
 
 (setf sicl-hash-table:*default-hash-table-class* (find-class 'list-hash-table))
