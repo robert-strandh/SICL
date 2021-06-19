80924719a4cee81da00579446dc5eb88cc16b58b
Robert Strandh <robert.strandh@gmail.com>
2021-06-19 16:35:17 +0200

    Remove more redundant loads.

--- Code/Boot/Phase-5/enable-generic-function-creation.lisp
+++ Code/Boot/Phase-5/enable-generic-function-creation.lisp
@@ -1,35 +1,14 @@
 (cl:in-package #:sicl-boot-phase-5)
 
 (defun enable-generic-function-initialization (e5)
-  (load-source-file "CLOS/discriminating-automaton.lisp" e5)
-  (define-error-functions
-      '(sicl-clos::compute-test-tree)
-      e5)
-  (load-source-file "CLOS/discriminating-tagbody.lisp" e5)
-  (define-error-functions
-      '(sicl-clos::make-cdr)
-      e5)
-  (load-source-file "CLOS/classp-defgeneric.lisp" e5)
-  (load-source-file "CLOS/classp-defmethods.lisp" e5)
-  (load-source-file "CLOS/sub-specializer-p.lisp" e5)
-  (load-source-file "CLOS/compute-applicable-methods-defgenerics.lisp" e5)
-  (load-source-file "CLOS/compute-applicable-methods-support.lisp" e5)
-  (load-source-file "CLOS/compute-applicable-methods-defmethods.lisp" e5)
-  (load-source-file "CLOS/compute-effective-method-defgenerics.lisp" e5)
-  (load-source-file "CLOS/compute-effective-method-support.lisp" e5)
-  (load-source-file "CLOS/compute-effective-method-defmethods.lisp" e5)
   (load-source-file "CLOS/no-applicable-method-defgenerics.lisp" e5)
   (load-source-file "CLOS/no-applicable-method-defmethods.lisp" e5)
-  (load-source-file "CLOS/compute-discriminating-function-defgenerics.lisp" e5)
   (with-intercepted-function-cells
       (e5
        ;; FIXME: this one should also assign the slots in the
        ;; function object.
        (sicl-clos:set-funcallable-instance-function
         (list #'sicl-host-mop:set-funcallable-instance-function)))
-    (load-source-file "CLOS/compute-discriminating-function-support.lisp" e5)
-    (load-source-file "CLOS/compute-discriminating-function-support-c.lisp" e5)
-    (load-source-file "CLOS/compute-discriminating-function-defmethods.lisp" e5)
     (load-source-file "CLOS/invalidate-discriminating-function.lisp" e5))
   (import-functions-from-host
    '(cleavir-code-utilities:parse-generic-function-lambda-list
