6b9f89bd3940179fbe6a2eeb6d165ed341f526b6
Robert Strandh <robert.strandh@gmail.com>
2021-06-24 16:49:04 +0200

    Call function PREPARE-THIS-PHASE first in main entry point.

--- Code/Boot/Phase-6/boot.lisp
+++ Code/Boot/Phase-6/boot.lisp
@@ -30,6 +30,7 @@
                    (e4 sicl-boot:e4)
                    (e5 sicl-boot:e5))
       boot
+    (prepare-this-phase e3 e4 e5)
     (load-source-file "String/make-string-defun.lisp" e5)
     (setf *symbol-names* (make-hash-table :test #'eq))
     (load-asdf-system '#:sicl-filename e5)
