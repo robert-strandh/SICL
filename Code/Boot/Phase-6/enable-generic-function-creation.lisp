cbc0635525bf8a68008d4d08c10eca173bfa8e37
Robert Strandh <robert.strandh@gmail.com>
2021-06-25 10:43:21 +0200

    Move definition of IMPORT-FUNCTIONS-FROM-HOST-INTO-E5.

--- Code/Boot/Phase-6/enable-generic-function-creation.lisp
+++ Code/Boot/Phase-6/enable-generic-function-creation.lisp
@@ -1,18 +1,5 @@
 (cl:in-package #:sicl-boot-phase-6)
 
-(defun import-functions-from-host-into-e5
-    (names e3 e5)
-  (loop with e3-client = (env:client e3)
-        with make-instance = (env:fdefinition e3-client e3 'make-instance)
-        with e5-client = (env:client e5)
-        for name in names
-        for host-function = (fdefinition name)
-        for sicl-function = (funcall make-instance 'sicl-clos:simple-function)
-        do (setf (sicl-boot:original-function sicl-function) host-function)
-           (sicl-host-mop:set-funcallable-instance-function
-            sicl-function host-function)
-           (setf (env:fdefinition e5-client e5 name) sicl-function)))
-
 (defun enable-generic-function-initialization (e3 e5)
   (load-source-file "CLOS/no-applicable-method-defgenerics.lisp" e5)
   (load-source-file "CLOS/no-applicable-method-defmethods.lisp" e5)
