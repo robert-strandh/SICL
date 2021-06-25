5c63a9dda18b0af2cc89c2df7da8f251511f339f
Robert Strandh <robert.strandh@gmail.com>
2021-06-25 10:50:46 +0200

    Move definition of COMPILE in E5 to phase 6.

--- Code/Boot/Phase-5/prepare-next-phase.lisp
+++ Code/Boot/Phase-5/prepare-next-phase.lisp
@@ -4,18 +4,4 @@
   (load-source-file "CLOS/class-of-defun.lisp" e5)
   (enable-typep e3 e5)
   (load-source-file "Types/type-of-defgeneric.lisp" e5)
-  (enable-object-creation e3 e5)
-  (setf (env:fdefinition (env:client e5) e5 'compile)
-        (lambda (x lambda-expression)
-          (assert (null x))
-          (assert (and (consp lambda-expression) (eq (first lambda-expression) 'lambda)))
-          (let* ((cst (cst:cst-from-expression lambda-expression))
-                 (ast (cleavir-cst-to-ast:cst-to-ast (env:client e5) cst e5)))
-            (with-intercepted-function-cells
-                (e5
-                 (make-instance
-                  (env:function-cell (env:client e3) e3 'make-instance))
-                 (sicl-clos:method-function
-                  (env:function-cell (env:client e4) e4 'sicl-clos:method-function)))
-              (funcall (env:fdefinition (env:client e5) e5 'sicl-boot:ast-eval)
-                       ast))))))
+  (enable-object-creation e3 e5))
--- Code/Boot/Phase-6/prepare-this-phase.lisp
+++ Code/Boot/Phase-6/prepare-this-phase.lisp
@@ -45,6 +45,20 @@
   (finish-output *trace-output*))
 
 (defun prepare-this-phase (e3 e4 e5)
+  (setf (env:fdefinition (env:client e5) e5 'compile)
+        (lambda (x lambda-expression)
+          (assert (null x))
+          (assert (and (consp lambda-expression) (eq (first lambda-expression) 'lambda)))
+          (let* ((cst (cst:cst-from-expression lambda-expression))
+                 (ast (cleavir-cst-to-ast:cst-to-ast (env:client e5) cst e5)))
+            (with-intercepted-function-cells
+                (e5
+                 (make-instance
+                  (env:function-cell (env:client e3) e3 'make-instance))
+                 (sicl-clos:method-function
+                  (env:function-cell (env:client e4) e4 'sicl-clos:method-function)))
+              (funcall (env:fdefinition (env:client e5) e5 'sicl-boot:ast-eval)
+                       ast)))))
   (enable-array-access e5)
   (enable-method-combinations e3 e5)
   (enable-compute-discriminating-function e5)
