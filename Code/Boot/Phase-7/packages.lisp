605bb283e42ddd5911696f6b1661f5217be21f0d
Robert Strandh <robert.strandh@gmail.com>
2021-06-26 16:12:36 +0200

    No longer import name of unused function IMPORT-FUNCTIONS-FROM-HOST.

--- Code/Boot/Phase-7/packages.lisp
+++ Code/Boot/Phase-7/packages.lisp
@@ -4,7 +4,6 @@
   (:use #:common-lisp)
   (:local-nicknames (#:env #:sicl-environment))
   (:import-from #:sicl-boot
-                #:import-functions-from-host
                 #:define-error-functions
                 #:load-source-file
                 #:with-intercepted-function-cells)
