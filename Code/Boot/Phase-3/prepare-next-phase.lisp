b50e61845ecab0c0b076d168327f53f7b7085f11
Robert Strandh <robert.strandh@gmail.com>
2021-06-26 08:06:51 +0200

    Move call to ENABLE-DEFGENERIC and more to phase 4.

--- Code/Boot/Phase-3/prepare-next-phase.lisp
+++ Code/Boot/Phase-3/prepare-next-phase.lisp
@@ -10,29 +10,4 @@
   (load-source-file "Types/type-of-defgeneric.lisp" e3)
   (enable-object-creation e2 e3)
   (enable-method-combinations e2 e3 e4)
-  (setf (env:special-operator (env:client e4) e4 'cleavir-primop:multiple-value-call) t)
-  (enable-compute-discriminating-function e2 e3 e4)
-  (load-source-file "CLOS/defgeneric-support.lisp" e4)
-  (load-source-file "CLOS/invalidate-discriminating-function.lisp" e3)
-  ;; These two definitions exist so that COMPILE will not signal an
-  ;; error that they are undefined.
-  (setf (env:fdefinition (env:client e3) e3 'slot-value)
-        (lambda (object slot-name)
-          (error "SLOT-VALUE called with ~s and ~s" object slot-name)))
-  (setf (env:fdefinition (env:client e3) e3 '(setf slot-value))
-        (lambda (value object slot-name)
-          (error "(SETF SLOT-VALUE) called with ~s, ~s and ~s" value object slot-name)))
-  (import-functions-from-host
-   '(shared-initialize initialize-instance)
-   e3)
-  (import-functions-from-host
-   '(cleavir-code-utilities:parse-generic-function-lambda-list
-     cleavir-code-utilities:required)
-   e3)
-  (load-source-file "CLOS/generic-function-initialization-support.lisp" e3)
-  (with-intercepted-function-cells
-      (e3
-       (sicl-clos:method-function
-        (env:function-cell (env:client e2) e2 'sicl-clos:method-function)))
-    (load-source-file "CLOS/generic-function-initialization-defmethods.lisp" e3))
-  (enable-defgeneric e2 e3 e4))
+  (setf (env:special-operator (env:client e4) e4 'cleavir-primop:multiple-value-call) t))
