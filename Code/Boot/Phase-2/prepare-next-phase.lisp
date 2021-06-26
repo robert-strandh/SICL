4189e85e231f12cdbd8a9a4b3f0bd1d69a2c5afe
Robert Strandh <robert.strandh@gmail.com>
2021-06-26 10:24:33 +0200

    Remove obsolete component.

--- Code/Boot/Phase-2/prepare-next-phase.lisp
+++
@@ -1,8 +0,0 @@
-(cl:in-package #:sicl-boot-phase-2)
-
-(defun prepare-next-phase (boot)
-  (with-accessors ((e1 sicl-boot:e1)
-                   (e2 sicl-boot:e2)
-                   (e3 sicl-boot:e3))
-      boot
-    ))
