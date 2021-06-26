fb3cc224b1f8c3cf74f85a87b5305312457697bc
Robert Strandh <robert.strandh@gmail.com>
2021-06-26 10:10:58 +0200

    Move call to ENABLE-DEFGENERIC to phase 3.

--- Code/Boot/Phase-2/prepare-next-phase.lisp
+++ Code/Boot/Phase-2/prepare-next-phase.lisp
@@ -9,5 +9,4 @@
      '(reinitialize-instance)
      e3)
     (enable-typep e2)
-    (enable-object-creation e1 e2)
-    (enable-defgeneric e1 e2 e3)))
+    (enable-object-creation e1 e2)))
--- Code/Boot/Phase-3/prepare-this-phase.lisp
+++ Code/Boot/Phase-3/prepare-this-phase.lisp
@@ -1,6 +1,7 @@
 (cl:in-package #:sicl-boot-phase-3)
 
 (defun prepare-this-phase (e1 e2 e3)
+  (enable-defgeneric e1 e2 e3)
   (enable-class-initialization e3)
   (enable-defclass e1 e2 e3)
   (enable-defmethod e1 e2 e3))
