b5b602c728cd9c18238f3054c0535bb92bb113ca
Robert Strandh <robert.strandh@gmail.com>
2021-06-27 15:51:52 +0200

    Remove obsolete files.

--- Code/CLOS/reader-writer-method-class-defgenerics.lisp
+++
@@ -1,9 +0,0 @@
-(cl:in-package #:sicl-clos)
-
-;;; For the specification of this generic function, see
-;;; http://metamodular.com/CLOS-MOP/reader-method-class.html
-(defgeneric reader-method-class (class direct-slot &rest initargs))
-
-;;; For the specification of this generic function, see
-;;; http://metamodular.com/CLOS-MOP/writer-method-class.html
-(defgeneric writer-method-class (class direct-slot &rest initargs))
--- Code/CLOS/reader-writer-method-class-defuns.lisp
+++
@@ -1,7 +0,0 @@
-(cl:in-package #:sicl-clos)
-
-(defun reader-method-class (class direct-slot &rest initargs)
-  (apply #'reader-method-class-default class direct-slot initargs))
-
-(defun writer-method-class (class direct-slot &rest initargs)
-  (apply #'writer-method-class-default class direct-slot initargs))
