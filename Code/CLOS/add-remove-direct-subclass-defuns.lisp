c4702b6b590646471fda5aa1fb808b25e3c2e1f3
Robert Strandh <robert.strandh@gmail.com>
2021-06-27 14:55:32 +0200

    Remove obsolete component.

--- Code/CLOS/add-remove-direct-subclass-defuns.lisp
+++
@@ -1,7 +0,0 @@
-(cl:in-package #:sicl-clos)
-
-(defun add-direct-subclass (superclass subclass)
-  (add-direct-subclass-default superclass subclass))
-
-(defun remove-direct-subclass (superclass subclass)
-  (remove-direct-subclass-default superclass subclass))
