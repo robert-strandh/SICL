b0b6465f51c0066908dfa65c82903313376a1d60
Robert Strandh <robert.strandh@gmail.com>
2021-06-27 16:09:05 +0200

    Remove obsolete files.

--- Code/CLOS/direct-slot-definition-class-defgeneric.lisp
+++
@@ -1,5 +0,0 @@
-(cl:in-package #:sicl-clos)
-
-;;; For the specification of this generic function, see
-;;; http://metamodular.com/CLOS-MOP/direct-slot-definition-class.html
-(defgeneric direct-slot-definition-class (class &rest initargs))
--- Code/CLOS/direct-slot-definition-class-support.lisp
+++
@@ -1,5 +0,0 @@
-(cl:in-package #:sicl-clos)
-
-(defun direct-slot-definition-class-default (class &rest initargs)
-  (declare (ignore class initargs))
-  (find-class 'standard-direct-slot-definition))
