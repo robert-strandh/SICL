0de61f577f31d7e9048ac444bfdc031c12fcb212
Robert Strandh <robert.strandh@gmail.com>
2021-06-27 16:12:29 +0200

    Remove obsolete methods.

--- Code/CLOS/reader-writer-method-class.lisp
+++ Code/CLOS/reader-writer-method-class.lisp
@@ -11,26 +11,12 @@
   (declare (ignore class direct-slot initargs))
   (find-class 'standard-reader-method))
 
-(defmethod reader-method-class
-    ((class built-in-class)
-     (direct-slot standard-direct-slot-definition)
-     &rest initargs)
-  (declare (ignore class direct-slot initargs))
-  (find-class 'standard-reader-method))
-
 ;;; For the specification of this generic function, see
 ;;; http://metamodular.com/CLOS-MOP/writer-method-class.html
 (defgeneric writer-method-class (class direct-slot &rest initargs))
 
 (defmethod writer-method-class
     ((class regular-class)
-     (direct-slot standard-direct-slot-definition)
-     &rest initargs)
-  (declare (ignore class direct-slot initargs))
-  (find-class 'standard-writer-method))
-
-(defmethod writer-method-class
-    ((class built-in-class)
      (direct-slot standard-direct-slot-definition)
      &rest initargs)
   (declare (ignore class direct-slot initargs))
