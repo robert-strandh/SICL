d74de6c3dec0c3cf88e55845e3ad0a93e4faf75a
Robert Strandh <robert.strandh@gmail.com>
2021-06-27 14:56:33 +0200

    Remove obsolete component.

--- Code/CLOS/compute-effective-method-defuns.lisp
+++
@@ -1,5 +0,0 @@
-(cl:in-package #:sicl-clos)
-
-(defun compute-effective-method (generic-function method-combination methods)
-  (declare (ignore generic-function method-combination))
-  (compute-effective-method-default methods))
