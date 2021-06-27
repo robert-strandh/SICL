18640edfe9e6f584a82fd7a685d338db3b54e34f
Robert Strandh <robert.strandh@gmail.com>
2021-06-27 15:33:50 +0200

    Remove obsolete files.

--- Code/CLOS/add-remove-direct-subclass-defgenerics.lisp
+++
@@ -1,9 +0,0 @@
-(cl:in-package #:sicl-clos)
-
-;;; For the specification of this generic function, see
-;;; http://metamodular.com/CLOS-MOP/add-direct-subclass.html
-(defgeneric add-direct-subclass (superclass subclass))
-
-;;; For the specification of this generic function, see
-;;; http://metamodular.com/CLOS-MOP/remove-direct-subclass.html
-(defgeneric remove-direct-subclass (superclass subclass))
--- Code/CLOS/add-remove-direct-subclass-defmethods.lisp
+++
@@ -1,7 +0,0 @@
-(cl:in-package #:sicl-clos)
-
-(defmethod add-direct-subclass ((superclass class) (subclass class))
-  (add-direct-subclass-default superclass subclass))
-
-(defmethod remove-direct-subclass ((superclass class) (subclass class))
-  (remove-direct-subclass-default superclass subclass))
