5432e892d53b8fe786b69b57fea9110cd3cf02c0
Robert Strandh <robert.strandh@gmail.com>
2021-06-29 17:05:55 +0200

    Remove obsolete files.

--- Code/CLOS/find-method-defgenerics.lisp
+++
@@ -1,6 +0,0 @@
-(cl:in-package #:sicl-clos)
-
-;;; For the specification of this generic function, see
-;;; http://www.lispworks.com/documentation/HyperSpec/Body/f_find_m.htm#find-method
-(defgeneric find-method
-    (generic-function method-qualifiers specializers &optional errorp))
