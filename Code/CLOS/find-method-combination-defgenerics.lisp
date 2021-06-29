7629580fcdc03d7057dcb66502c3d2165e8475e8
Robert Strandh <robert.strandh@gmail.com>
2021-06-29 17:21:47 +0200

    Remove obsolete files.

--- Code/CLOS/find-method-combination-defgenerics.lisp
+++
@@ -1,6 +0,0 @@
-(cl:in-package #:sicl-clos)
-
-;;; For the specification of this generic function, see
-;;; http://metamodular.com/CLOS-MOP/find-method-combination.html
-(defgeneric find-method-combination
-    (generic-function method-combination-type-name method-combination-options))
--- Code/CLOS/find-method-combination-defmethods.lisp
+++
@@ -1,9 +0,0 @@
-(cl:in-package #:sicl-clos)
-
-(defmethod find-method-combination
-    ((generic-function standard-generic-function)
-     method-combination-type-name
-     method-combination-options)
-  (sicl-method-combination:find-method-combination
-   method-combination-type-name
-   method-combination-options))
