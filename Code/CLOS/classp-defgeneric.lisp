5cb78a329a24fd9495c3d1f7f6ea0b5f3be7150f
Robert Strandh <robert.strandh@gmail.com>
2021-06-29 17:00:18 +0200

    Remove obsolete files.

--- Code/CLOS/classp-defgeneric.lisp
+++
@@ -1,4 +0,0 @@
-(cl:in-package #:sicl-clos)
-
-;;; Return true if and only if OBJECT is a class.
-(defgeneric classp (object))
--- Code/CLOS/classp-defmethods.lisp
+++
@@ -1,7 +0,0 @@
-(cl:in-package #:sicl-clos)
-
-(defmethod classp (object)
-  nil)
-
-(defmethod classp ((object class))
-  t)
