46842fd0eb8ee93f2f10b97d568b2cea9515fe86
Robert Strandh <robert.strandh@gmail.com>
2021-06-29 16:52:53 +0200

    Remove obsolete files.

--- Code/CLOS/no-applicable-method-defgenerics.lisp
+++
@@ -1,5 +0,0 @@
-(cl:in-package #:sicl-clos)
-
-;;; For the specification of this generic function, see
-;;; http://www.lispworks.com/documentation/HyperSpec/Body/f_no_app.htm#no-applicable-method
-(defgeneric no-applicable-method (generic-function &rest function-arguments))
--- Code/CLOS/no-applicable-method-defmethods.lisp
+++
@@ -1,6 +0,0 @@
-(cl:in-package #:sicl-clos)
-
-(defmethod no-applicable-method
-    ((generic-function generic-function) &rest function-arguments)
-  (error "no applicable method on generic function ~s with arguments: ~s"
-         generic-function function-arguments))
