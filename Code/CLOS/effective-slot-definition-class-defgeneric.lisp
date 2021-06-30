975533409e6cb9b0fd324f1acd91fb3bcb8c7d6a
Robert Strandh <robert.strandh@gmail.com>
2021-06-30 10:24:38 +0200

    Remove obsolete files.

--- Code/CLOS/effective-slot-definition-class-defgeneric.lisp
+++
@@ -1,5 +0,0 @@
-(cl:in-package #:sicl-clos)
-
-;;; For the specification of this generic function, see
-;;; http://metamodular.com/CLOS-MOP/effective-slot-definition-class.html
-(defgeneric effective-slot-definition-class (class &rest initargs))
--- Code/CLOS/effective-slot-definition-class-support.lisp
+++
@@ -1,5 +0,0 @@
-(cl:in-package #:sicl-clos)
-
-(defun effective-slot-definition-class-default (class &rest initargs)
-  (declare (ignore class initargs))
-  (find-class 'standard-effective-slot-definition))
