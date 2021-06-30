e9c6a2584b16fa12bda15523a0fe6d9d8004a811
Robert Strandh <robert.strandh@gmail.com>
2021-06-30 11:10:34 +0200

    Remove obsolete files.

--- Code/CLOS/validate-superclass-defgenerics.lisp
+++
@@ -1,6 +0,0 @@
-(cl:in-package #:sicl-clos)
-
-;;; For the specification of this generic function, see
-;;; http://metamodular.com/CLOS-MOP/validate-superclass.html
-(defgeneric validate-superclass (class superclass)
-  (:argument-precedence-order superclass class))
