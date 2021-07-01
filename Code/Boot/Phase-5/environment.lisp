e80bb9696811beebb571cc52670de15d06ed2a8c
Robert Strandh <robert.strandh@gmail.com>
2021-07-01 15:09:34 +0200

    Add slot containing slot reader SICL-CLOS:ENVIRONMENT.

--- Code/Boot/Phase-5/environment.lisp
+++ Code/Boot/Phase-5/environment.lisp
@@ -3,4 +3,11 @@
 (defclass environment (sicl-boot:environment)
   ())
 
-(defclass client (sicl-boot:client) ())
+(defclass client (sicl-boot:client)
+  (;; This slot contains the slot accessor SICL-CLOS:ENVIRONMENT
+   ;; which, when given an ersatz instance of the SICL class
+   ;; FUNCALLABLE-STANDARD-OBJECT, returns the static environment of
+   ;; that function.
+   (%static-environment-function
+    :initarg :static-environment-function
+    :accessor static-environment-function)))
