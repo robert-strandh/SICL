eee3c87c3cc0b8c96a5c027158444f619073eefd
Robert Strandh <robert.strandh@gmail.com>
2021-07-02 10:37:53 +0200

    Remove obsolete files.

--- Code/CLOS/class-prototype-defgeneric.lisp
+++
@@ -1,5 +0,0 @@
-(cl:in-package #:sicl-clos)
-
-;;; For the specification of this generic function, see
-;;; http://metamodular.com/CLOS-MOP/class-prototype.html
-(defgeneric class-prototype (class))
--- Code/CLOS/setf-class-prototype-defgeneric.lisp
+++
@@ -1,4 +0,0 @@
-(cl:in-package #:sicl-clos)
-
-;;; This function sets the class prototype of the class.
-(defgeneric (setf class-prototype) (prototype class))
