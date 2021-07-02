d32daac1685729806fd67efa1c550fd7efbb48cd
Robert Strandh <robert.strandh@gmail.com>
2021-07-02 16:58:56 +0200

    Improve function conversion by using MAP-DEFINED-FUNCTIONS.

--- Code/Boot/Phase-6/convert-functions.lisp
+++ Code/Boot/Phase-6/convert-functions.lisp
@@ -6,34 +6,12 @@
     (sicl-host-mop:set-funcallable-instance-function wrapper function)
     wrapper))
 
-;;; FIXME: Figure out a better way to write this function.
-(defun host-function-p (name client e5)
-  (if (symbolp name)
-      (if (and (env:fboundp client e5 name)
-               (null (env:special-operator client e5 name))
-               (null (env:macro-function client e5 name)))
-          (let ((result (env:fdefinition client e5 name)))
-            (if (typep result 'sicl-boot:header)
-                nil
-                result))
-          nil)
-      (if (env:fboundp client e5 name)
-          (let ((result (env:fdefinition client e5 name)))
-            (if (typep result 'sicl-boot:header)
-                nil
-                result))
-          nil)))
-
 (defun convert-functions (e5)
   (let* ((client (env:client e5))
          (make-instance (env:fdefinition client e5 'make-instance)))
-    (do-all-symbols (symbol)
-      (let ((maybe-function (host-function-p symbol client e5)))
-        (unless (null maybe-function)
-          (setf (env:fdefinition client e5 symbol)
-                (wrap-function maybe-function make-instance))))
-      (let ((maybe-function (host-function-p `(setf ,symbol) client e5)))
-        (unless (null maybe-function)
-          (setf (env:fdefinition client e5 `(setf ,symbol))
-                (wrap-function maybe-function make-instance)))))))
-
+    (env:map-defined-functions
+     client e5
+     (lambda (name function)
+       (unless (typep function 'sicl-boot:header)
+         (setf (env:fdefinition client e5 name)
+               (wrap-function function make-instance)))))))
