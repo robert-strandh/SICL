1e998d533dd6fc486feec38c583bb33a3fc98cdf
Robert Strandh <robert.strandh@gmail.com>
2021-07-04 05:47:03 +0200

    Simplify the memory module considerably.

--- Code/Memory/image.lisp
+++ Code/Memory/image.lisp
@@ -6,14 +6,6 @@
 ;;; address bits so that a directory always has 256 entries.  A page
 ;;; stores the equivalent of 256 bytes, or 32 8-byte words.  
 ;;;
-;;; The reason we make the memory contain words instead of bytes is
-;;; that we want to "cheat" in that we temporarily want to store host
-;;; objects as well as target objects in the memory.  When a word of
-;;; memory contains an integer, then it is always a non-negative
-;;; integer, and its value is always between 0 and 2^64 - 1.  Such a
-;;; word of memory always denotes a target object.  When a host object
-;;; is stored, then it is never an integer.
-;;;
 ;;; Word accesses are always word aligned.  We allow for accesses to
 ;;; 4-byte words, 2-byte words, and individual bytes.  A 4-byte words
 ;;; is aligned on a 4-byte boundary, and a 2-byte word is aligned on a
@@ -49,56 +41,37 @@
       (setf (aref directory index) (make-page)))
     (aref directory index)))
 
-(defun load-byte (address)
-  (let((page (find-page-of-address address))
-       (index (ldb (byte 5 3) address)))
-    (ldb (byte 8 (* 8 (ldb (byte 3 0) address))) (aref page index))))
+(defun split-address (address)
+  (values (find-page-of-address address)
+          (ldb (byte 5 3) address)
+          (ldb (byte 3 0) address)))
 
-(defun store-byte (value address)
-  (check-type value (unsigned-byte 8))
-  (let* ((page (find-page-of-address address))
-	 (index (ldb (byte 5 3) address)))
-    (setf (ldb (byte 8 (* 8 (ldb (byte 3 0) address))) (aref page index))
-	  value)))
+(defun check-address-and-size (address size)
+  (check-type size (member 8 16 32 64))
+  (assert (zerop (mod address (ash size -3)))))
 
-(defun load-8-byte-word (address)
-  (assert (zerop (ldb (byte 3 0) address)))
-  (let ((page (find-page-of-address address))
-	(index (ldb (byte 5 3) address)))
-    (aref page index)))
-  
-(defun store-8-byte-word (value address)
-  (assert (zerop (ldb (byte 3 0) address)))
-  (when (integerp value)
-    (check-type value (mod #.(expt 2 64))))
-  (let ((page (find-page-of-address address))
-	(index (ldb (byte 5 3) address)))
-    (setf (aref page index) value)))
+(defun load-unsigned (address size)
+  (check-address-and-size address size)
+  (multiple-value-bind (page page-index position)
+      (split-address address)
+    (ldb (byte size position) (aref page page-index))))
 
-(defun load-4-byte-word (address)
-  (assert (zerop (ldb (byte 2 0) address)))
-  (let((page (find-page-of-address address))
-       (index (ldb (byte 5 3) address)))
-    (ldb (byte 32 (* 32 (ldb (byte 1 2) address))) (aref page index))))
+(defun load-signed (address size)
+  (let ((unsigned (load-unsigned address size)))
+    (if (>= unsigned (ash 1 (1- size)))
+        (- unsigned (ash 1 size))
+        unsigned)))
 
-(defun store-4-byte-word (value address)
-  (assert (zerop (ldb (byte 2 0) address)))
-  (check-type value (mod #.(expt 2 32)))
-  (let((page (find-page-of-address address))
-       (index (ldb (byte 5 3) address)))
-    (setf (ldb (byte 32 (* 32 (ldb (byte 1 2) address))) (aref page index))
-	  value)))
+(defun store-unsigned (value address size)
+  (check-address-and-size address size)
+  (multiple-value-bind (page page-index position)
+      (split-address address)
+    (setf (ldb (byte size position) (aref page page-index))
+          value)))
 
-(defun load-2-byte-word (address)
-  (assert (zerop (ldb (byte 1 0) address)))
-  (let((page (find-page-of-address address))
-       (index (ldb (byte 5 3) address)))
-    (ldb (byte 16 (* 16 (ldb (byte 2 1) address))) (aref page index))))
-
-(defun store-2-byte-word (value address)
-  (assert (zerop (ldb (byte 1 0) address)))
-  (check-type value (mod #.(expt 2 16)))
-  (let((page (find-page-of-address address))
-       (index (ldb (byte 5 3) address)))
-    (setf (ldb (byte 16 (* 16 (ldb (byte 2 1) address))) (aref page index))
-	  value)))
+(defun store-signed (value address size)
+  (let ((unsigned
+          (if (minusp value)
+              (+ value (ash 1 size))
+              value)))
+    (store-unsigned unsigned address size)))
--- Code/Memory/packages.lisp
+++ Code/Memory/packages.lisp
@@ -2,7 +2,7 @@
 
 (defpackage #:sicl-memory
   (:use #:common-lisp)
-  (:export #:load-byte #:store-byte
-           #:load-2-byte-word #:store-2-byte-word
-           #:load-4-byte-word #:store-4-byte-word
-           #:load-8-byte-word #:store-8-byte-word))
+  (:export #:load-unsigned
+           #:store-unsigned
+           #:load-signed
+           #:store-signed))
