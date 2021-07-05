f03f9ca0071a96a82fa3f3016c52d53c44f8918d
Robert Strandh <robert.strandh@gmail.com>
2021-07-05 16:28:38 +0200

    No longer load obsolete file with definitition of VARIANT-SIGNATURE.

--- Code/Boot/create-accessor-generic-functions.lisp
+++ Code/Boot/create-accessor-generic-functions.lisp
@@ -17,7 +17,6 @@
     (ld "CLOS/setf-dependents-defgeneric.lisp")
     (ld "CLOS/accessor-method-slot-definition-defgeneric.lisp")
     (ld "CLOS/setf-accessor-method-slot-definition-defgeneric.lisp")
-    (ld "CLOS/variant-signature-defgeneric.lisp")
     (ld "CLOS/template-defgeneric.lisp")
     (ld "Arithmetic/numerator-defgeneric.lisp")
     (ld "Arithmetic/denominator-defgeneric.lisp")
