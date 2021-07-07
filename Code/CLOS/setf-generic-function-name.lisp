c68a730a3e9c6888d8f341afabfefb6fd63876d9
Robert Strandh <robert.strandh@gmail.com>
2021-07-07 16:45:02 +0200

    Remove incorrect and obsolete comment.

--- Code/CLOS/setf-generic-function-name.lisp
+++ Code/CLOS/setf-generic-function-name.lisp
@@ -1,10 +1,5 @@
 (cl:in-package #:sicl-clos)
 
-;;; Contrary to appearance, this function is not a slot writer.
-;;; Instead, according to the AMOP, this function should call
-;;; REINITIALIZE-INSTANCE with three arguments: CLASS, :NAME, and
-;;; NEW-NAME.
-;;;
 ;;; For the specification of this generic function, see
 ;;; http://metamodular.com/CLOS-MOP/setf-generic-function-name.html
 (defgeneric (setf generic-function-name) (new-name generic-function))
