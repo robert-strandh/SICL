4e679ae8e639de9c8fec6239c593fb7be67091e3
Robert Strandh <robert.strandh@gmail.com>
2021-07-07 16:49:04 +0200

    Provide default method for function (SETF CLASS-NAME).

--- Code/CLOS/setf-class-name.lisp
+++ Code/CLOS/setf-class-name.lisp
@@ -8,3 +8,6 @@
 ;;; For the specification of this generic function, see
 ;;; http://metamodular.com/CLOS-MOP/setf-class-name.html
 (defgeneric (setf class-name) (new-name class))
+
+(defmethod (setf class-name) (new-name (class class))
+  (reinitialize-instance class :name new-name))
