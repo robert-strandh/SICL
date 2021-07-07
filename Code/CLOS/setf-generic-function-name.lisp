6365be5a33cfb7d8cbc16436b3b6c1e373caec94
Robert Strandh <robert.strandh@gmail.com>
2021-07-07 16:44:06 +0200

    Provide default method for function (SETF GENERIC-FUNCTION-NAME).

--- Code/CLOS/setf-generic-function-name.lisp
+++ Code/CLOS/setf-generic-function-name.lisp
@@ -8,3 +8,7 @@
 ;;; For the specification of this generic function, see
 ;;; http://metamodular.com/CLOS-MOP/setf-generic-function-name.html
 (defgeneric (setf generic-function-name) (new-name generic-function))
+
+(defmethod (setf generic-function-name)
+    (new-name (generic-function generic-function))
+  (reinitialize-instance generic-function :name new-name))
