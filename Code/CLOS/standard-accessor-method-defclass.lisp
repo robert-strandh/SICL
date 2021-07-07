d6fdb98df1eab94ea8f4641a987ad9942eb8e9f1
Robert Strandh <robert.strandh@gmail.com>
2021-07-07 16:32:35 +0200

    Include DEFGENERIC forms for slot accessor.

--- Code/CLOS/standard-accessor-method-defclass.lisp
+++ Code/CLOS/standard-accessor-method-defclass.lisp
@@ -4,6 +4,13 @@
 ;;;
 ;;; Class STANDARD-ACCESSOR-METHOD.
 
+;;; For the specification of this generic function, see
+;;; http://metamodular.com/CLOS-MOP/accessor-method-slot-definition.html
+(defgeneric accessor-method-slot-definition (accessor-method))
+
+(defgeneric (setf accessor-method-slot-definition)
+    (slot-definition accessor-method))
+
 (defgeneric slot-location (standard-accessor-method))
 
 (defclass standard-accessor-method (standard-method)
