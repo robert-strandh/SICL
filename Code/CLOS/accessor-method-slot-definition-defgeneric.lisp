6a2f08553729dcefdcd6cdfe8c513839db59eb96
Robert Strandh <robert.strandh@gmail.com>
2021-07-07 16:34:37 +0200

    Remove obsolete files.

--- Code/CLOS/accessor-method-slot-definition-defgeneric.lisp
+++
@@ -1,5 +0,0 @@
-(cl:in-package #:sicl-clos)
-
-;;; For the specification of this generic function, see
-;;; http://metamodular.com/CLOS-MOP/accessor-method-slot-definition.html
-(defgeneric accessor-method-slot-definition (accessor-method))
--- Code/CLOS/setf-accessor-method-slot-definition-defgeneric.lisp
+++
@@ -1,3 +0,0 @@
-(cl:in-package #:sicl-clos)
-
-(defgeneric (setf accessor-method-slot-definition) (slot-definition accessor-method))
