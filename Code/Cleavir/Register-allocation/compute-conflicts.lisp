8076eac044add16d7215979bc799033ee7375b62
Robert Strandh <robert.strandh@gmail.com>
2021-07-09 18:44:40 +0200

    Untabify.

--- Code/Cleavir/Register-allocation/compute-conflicts.lisp
+++ Code/Cleavir/Register-allocation/compute-conflicts.lisp
@@ -17,40 +17,40 @@
 
 (defun same-conflict-p (c1 c2)
   (or (and (eq (car c1) (car c2))
-	   (eq (cdr c1) (cdr c2)))
+           (eq (cdr c1) (cdr c2)))
       (and (eq (car c1) (cdr c2))
-	   (eq (cdr c1) (car c2)))))
+           (eq (cdr c1) (car c2)))))
 
 (defun conflicts-instruction (instruction liveness)
   (loop with outputs = (cleavir-ir:outputs instruction)
-	for output in outputs
-	append (when (typep output 'cleavir-ir:lexical-location)
-		 (loop for live in (cleavir-liveness:live-after
-				    liveness instruction)
-		       when (typep live 'cleavir-ir:lexical-location)
-			 collect (cons output live)))))
+        for output in outputs
+        append (when (typep output 'cleavir-ir:lexical-location)
+                 (loop for live in (cleavir-liveness:live-after
+                                    liveness instruction)
+                       when (typep live 'cleavir-ir:lexical-location)
+                         collect (cons output live)))))
 
 (defun compute-conflicts (initial-instruction)
   (let ((conflicts '())
-	(table (make-hash-table :test #'eq))
-	(liveness (cleavir-liveness:liveness
-		   initial-instruction
-		   #'cleavir-ir:successors
-		   #'cleavir-ir:predecessors
-		   (lambda (instruction)
-		     (remove-if-not
-		      (lambda (input)
-			(typep input 'cleavir-ir:lexical-location))
-		      (cleavir-ir:inputs instruction))) 
-		   (lambda (instruction)
-		     (remove-if-not
-		      (lambda (output)
-			(typep output 'cleavir-ir:lexical-location))
-		      (cleavir-ir:outputs instruction))))))
+        (table (make-hash-table :test #'eq))
+        (liveness (cleavir-liveness:liveness
+                   initial-instruction
+                   #'cleavir-ir:successors
+                   #'cleavir-ir:predecessors
+                   (lambda (instruction)
+                     (remove-if-not
+                      (lambda (input)
+                        (typep input 'cleavir-ir:lexical-location))
+                      (cleavir-ir:inputs instruction))) 
+                   (lambda (instruction)
+                     (remove-if-not
+                      (lambda (output)
+                        (typep output 'cleavir-ir:lexical-location))
+                      (cleavir-ir:outputs instruction))))))
     (labels ((traverse (instruction)
-	       (unless (gethash instruction table)
-		 (setf (gethash instruction table) t)
-		 (setf conflicts
-		       (append (conflicts-instruction instruction liveness))))))
+               (unless (gethash instruction table)
+                 (setf (gethash instruction table) t)
+                 (setf conflicts
+                       (append (conflicts-instruction instruction liveness))))))
       (traverse initial-instruction))
     (remove-duplicates conflicts :test #'same-conflict-p)))
