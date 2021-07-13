75a64c68e465061c75353db29ea557f8c109737f
Robert Strandh <robert.strandh@gmail.com>
2021-07-13 17:11:43 +0200

    New file containing definition of ADUSTABLE-ARRAY-P.

---
+++ Code/Array/adjustable-array-p.lisp
@@ -0,0 +1,12 @@
+(cl:in-package #:sicl-array)
+
+(defgeneric adjustable-array-p (array))
+
+(defmethod adjustable-array-p (array)
+  (error 'type-error
+         :datum array
+         :expected-type 'array))
+
+;;; FIXME: Are displaced arrays adjustable? 
+(defmethod adjustable-array-p ((array array))
+  t)
