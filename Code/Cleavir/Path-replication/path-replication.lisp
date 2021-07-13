98dc580e961292ee0a6ad175c4b6ef30ec6ba3a7
Robert Strandh <robert.strandh@gmail.com>
2021-07-13 16:53:50 +0200

    Untabify.

--- Code/Cleavir/Path-replication/path-replication.lisp
+++ Code/Cleavir/Path-replication/path-replication.lisp
@@ -19,28 +19,28 @@
 ;;; this transformation will also fail.
 (defun path-replication (instruction dominator)
   (loop with red-instructions = (make-hash-table :test #'eq)
-	with blue-instructions = (make-hash-table :test #'eq)
-	with work-list = (list instruction)
-	until (null work-list)
-	do (let* ((instruction (pop work-list))
-		  (predecessors (cleavir-ir:predecessors instruction)))
-	     (assert (not (null predecessors)))
-	     (cond ((> (length predecessors) 1)
-		    (setf work-list
-			  (append (replicate-instruction instruction)
-				  work-list)))
-		   ((eq (first predecessors) dominator)
-		    ;; If the sole predecessor is DOMINATOR, then we
-		    ;; move INSTRUCTION, and we are done.
-		    (move-and-color instruction
-				    red-instructions
-				    blue-instructions))
-		   (t
-		    ;; If the sole predecessor is some instruction
-		    ;; other than DOMINATOR, then we move INSTRUCTION,
-		    ;; but we put it back on WORK-LIST so that it is
-		    ;; processed again.
-		    (push instruction work-list)
-		    (move-and-color instruction
-				    red-instructions
-				    blue-instructions))))))
+        with blue-instructions = (make-hash-table :test #'eq)
+        with work-list = (list instruction)
+        until (null work-list)
+        do (let* ((instruction (pop work-list))
+                  (predecessors (cleavir-ir:predecessors instruction)))
+             (assert (not (null predecessors)))
+             (cond ((> (length predecessors) 1)
+                    (setf work-list
+                          (append (replicate-instruction instruction)
+                                  work-list)))
+                   ((eq (first predecessors) dominator)
+                    ;; If the sole predecessor is DOMINATOR, then we
+                    ;; move INSTRUCTION, and we are done.
+                    (move-and-color instruction
+                                    red-instructions
+                                    blue-instructions))
+                   (t
+                    ;; If the sole predecessor is some instruction
+                    ;; other than DOMINATOR, then we move INSTRUCTION,
+                    ;; but we put it back on WORK-LIST so that it is
+                    ;; processed again.
+                    (push instruction work-list)
+                    (move-and-color instruction
+                                    red-instructions
+                                    blue-instructions))))))
--- Code/Cleavir/Path-replication/rewrite.lisp
+++ Code/Cleavir/Path-replication/rewrite.lisp
@@ -16,24 +16,24 @@
 ;;; required.
 (defun replicate-instruction (instruction)
   (let ((copies
-	  (loop for predecessor in (cleavir-ir:predecessors instruction)
-		for successors = (cleavir-ir:successors predecessor)
-		for copy = (make-instance (class-of instruction)
-			     :policy (cleavir-ir:policy instruction)
-			     :predecessors (list predecessor)
-			     :successors (cleavir-ir:successors instruction)
-			     :inputs (cleavir-ir:inputs instruction)
-			     :outputs (cleavir-ir:outputs instruction))
-		do (nsubstitute copy instruction successors)
-		collect copy)))
+          (loop for predecessor in (cleavir-ir:predecessors instruction)
+                for successors = (cleavir-ir:successors predecessor)
+                for copy = (make-instance (class-of instruction)
+                             :policy (cleavir-ir:policy instruction)
+                             :predecessors (list predecessor)
+                             :successors (cleavir-ir:successors instruction)
+                             :inputs (cleavir-ir:inputs instruction)
+                             :outputs (cleavir-ir:outputs instruction))
+                do (nsubstitute copy instruction successors)
+                collect copy)))
     (loop for successor in (cleavir-ir:successors instruction)
-	  for predecessors = (cleavir-ir:predecessors successor)
-	  do (setf (cleavir-ir:predecessors successor)
-		   (loop for predecessor in predecessors
-			 if (eq predecessor instruction)
-			   append copies
-			 else
-			   collect predecessor)))
+          for predecessors = (cleavir-ir:predecessors successor)
+          do (setf (cleavir-ir:predecessors successor)
+                   (loop for predecessor in predecessors
+                         if (eq predecessor instruction)
+                           append copies
+                         else
+                           collect predecessor)))
     copies))
 
 ;;; This rewrite rule moves INSTRUCTION so that it precedes its
@@ -43,22 +43,22 @@
 (defun move-instruction (instruction)
   (assert (= (length (cleavir-ir:predecessors instruction)) 1))
   (loop with predecessor = (first (cleavir-ir:predecessors instruction))
-	for successors = (cleavir-ir:successors predecessor)
-	for remaining on (cleavir-ir:successors instruction)
-	for successor = (first remaining)
-	for copy = (make-instance (class-of predecessor)
-		     :predecessors (list instruction)
-		     :successors (substitute successor instruction successors)
-		     :inputs (cleavir-ir:inputs predecessor)
-		     :outputs (cleavir-ir:outputs predecessor))
-	do (assert (null (intersection (cleavir-ir:outputs predecessor)
-				       (cleavir-ir:inputs instruction)
-				       :test #'eq)))
-	   (setf (first remaining) copy)
-	   (nsubstitute copy instruction (cleavir-ir:predecessors successor))
-	   (loop for pred in (cleavir-ir:predecessors predecessor)
-		 do (nsubstitute instruction predecessor
-				 (cleavir-ir:successors pred)))))
+        for successors = (cleavir-ir:successors predecessor)
+        for remaining on (cleavir-ir:successors instruction)
+        for successor = (first remaining)
+        for copy = (make-instance (class-of predecessor)
+                     :predecessors (list instruction)
+                     :successors (substitute successor instruction successors)
+                     :inputs (cleavir-ir:inputs predecessor)
+                     :outputs (cleavir-ir:outputs predecessor))
+        do (assert (null (intersection (cleavir-ir:outputs predecessor)
+                                       (cleavir-ir:inputs instruction)
+                                       :test #'eq)))
+           (setf (first remaining) copy)
+           (nsubstitute copy instruction (cleavir-ir:predecessors successor))
+           (loop for pred in (cleavir-ir:predecessors predecessor)
+                 do (nsubstitute instruction predecessor
+                                 (cleavir-ir:successors pred)))))
 
 ;;; This rewrite rule shortcuts every non-black predecessor P of
 ;;; INSTRUCTION, so that instead of having INSTRUCTiON as its
@@ -66,30 +66,30 @@
 ;;; INSTRUCTION as its new successor.
 (defun shortcut-predecessors (instruction red-instructions blue-instructions)
   (let* ((predecessors (cleavir-ir:predecessors instruction))
-	 (successors (cleavir-ir:successors instruction))
-	 (red-successor (first successors))
-	 (blue-successor (second successors))
-	 (red-predecessors (loop for predecessor in predecessors
-				 when (gethash predecessor red-instructions)
-				   collect predecessor))
-	 (blue-predecessors (loop for predecessor in predecessors
-				  when (gethash predecessor blue-instructions)
-				    collect predecessor)))
+         (successors (cleavir-ir:successors instruction))
+         (red-successor (first successors))
+         (blue-successor (second successors))
+         (red-predecessors (loop for predecessor in predecessors
+                                 when (gethash predecessor red-instructions)
+                                   collect predecessor))
+         (blue-predecessors (loop for predecessor in predecessors
+                                  when (gethash predecessor blue-instructions)
+                                    collect predecessor)))
     ;; Shortcut red predecessors to go to the red successor.
     (loop for predecessor in red-predecessors
-	  do (nsubstitute red-successor instruction
-			  (cleavir-ir:successors predecessor))
-	     (pushnew predecessor (cleavir-ir:predecessors red-successor)
-		      :test #'eq))
+          do (nsubstitute red-successor instruction
+                          (cleavir-ir:successors predecessor))
+             (pushnew predecessor (cleavir-ir:predecessors red-successor)
+                      :test #'eq))
     ;; Shortcut blue predecessors to go to the blue successor.
     (loop for predecessor in blue-predecessors
-	  do (nsubstitute blue-successor instruction
-			  (cleavir-ir:successors predecessor))
-	     (pushnew predecessor (cleavir-ir:predecessors blue-successor)
-		      :test #'eq))
+          do (nsubstitute blue-successor instruction
+                          (cleavir-ir:successors predecessor))
+             (pushnew predecessor (cleavir-ir:predecessors blue-successor)
+                      :test #'eq))
     ;; Keep only the black predecessors of INSTRUCTION.
     (setf (cleavir-ir:predecessors instruction)
-	  (loop for predecessor in predecessors
-		unless (or (gethash predecessor red-instructions)
-			   (gethash predecessor blue-instructions))
-		  collect predecessor))))
+          (loop for predecessor in predecessors
+                unless (or (gethash predecessor red-instructions)
+                           (gethash predecessor blue-instructions))
+                  collect predecessor))))
