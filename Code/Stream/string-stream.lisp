e8f49ca14a38a8ef482cecb95c881cb20f6fbc73
Robert Strandh <robert.strandh@gmail.com>
2021-07-14 17:42:53 +0200

    Add slots to STRING-INPUT-STREAM for START and END indices.

--- Code/Stream/string-stream.lisp
+++ Code/Stream/string-stream.lisp
@@ -5,17 +5,26 @@
 (defclass string-stream (fundamental-character-input-stream)
   ((%string :initarg :string :reader stream-string)))
 
+(defgeneric start (stream))
+
+(defgeneric end (stream))
+
 (defgeneric index (stream))
 
 (defclass string-input-stream (string-stream)
-  ((%index :initform 0 :accessor index)))
+  ((%start :initarg :start :accessor start)
+   (%end :initarg :end :accessor end)
+   (%index :initform 0 :accessor index)))
 
 (defmethod stream-read-char ((stream string-input-stream))
-  (prog1 (aref (stream-string stream) (index stream))
-    (incf (index stream))))
+  (if (>= (index stream) (end stream))
+      :eof
+      (prog1 (aref (stream-string stream) (index stream))
+        (incf (index stream)))))
 
 (defmethod stream-unread-char
     ((stream string-input-stream) (character character))
+  (assert (> (index stream) (start stream)))
   (decf (index stream))
   (assert (eql (aref (stream-string stream) (index stream))
                character)))
