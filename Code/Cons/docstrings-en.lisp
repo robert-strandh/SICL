f56a85cad4a7ed283ec72ed57d24d83d9cd474d1
Robert Strandh <robert.strandh@gmail.com>
2021-07-16 15:54:20 +0200

    Remove copyright notice.

--- Code/Cons/docstrings-en.lisp
+++ Code/Cons/docstrings-en.lisp
@@ -1,21 +1,5 @@
 (cl:in-package #:sicl-cons-high)
 
-;;;; Copyright (c) 2008 - 2013
-;;;;
-;;;;     Robert Strandh (robert.strandh@gmail.com)
-;;;;
-;;;; all rights reserved. 
-;;;;
-;;;; Permission is hereby granted to use this software for any 
-;;;; purpose, including using, modifying, and redistributing it.
-;;;;
-;;;; The software is provided "as-is" with no warranty.  The user of
-;;;; this software assumes any responsibility of the consequences. 
-
-;;;; This file is part of the cons-high module of the SICL project.
-;;;; See the file SICL.text for a description of the project. 
-;;;; See the file cons-high.text for a description of the module.
-
 (eval-when (:compile-toplevel :load-toplevel :execute)
   (defun fmt (&rest args)
     (apply #'format nil args)))
