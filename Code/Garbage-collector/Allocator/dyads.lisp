0c738403b991b8d09f094895aafcd02fd339525e
Robert Strandh <robert.strandh@gmail.com>
2021-07-18 15:49:49 +0200

    New file for manging dyad space in global heap.

---
+++ Code/Garbage-collector/Allocator/dyads.lisp
@@ -0,0 +1,20 @@
+(cl:in-package #:sicl-allocator)
+
+(defparameter *dyads-start* (ash 1 20))
+
+(defparameter *dyads-count* (ash 1 25))
+
+(defparameter *dyads-end* (+ *dyads-start* (* 16 *dyads-count*)))
+
+(defparameter *free-list* 0)
+
+(defparameter *next-dyad* *dyads-start*)
+
+(defun allocate-dyad ()
+  (if (zerop *free-list*)
+      (if (= *next-dyad* *dyads-end*)
+          (error 'storage-condition)
+          (prog1 *next-dyad* (incf *next-dyad* 16)))
+      (prog1 *free-list*
+        (setf *free-list*
+              (sicl-memory:memory-unsigned *free-list* 64)))))
