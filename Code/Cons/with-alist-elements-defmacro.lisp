bc9296a1e06759e85b16367c655a4e162d7da3f4
Robert Strandh <robert.strandh@gmail.com>
2021-07-19 18:09:13 +0200

    Untabify.

--- Code/Cons/with-alist-elements-defmacro.lisp
+++ Code/Cons/with-alist-elements-defmacro.lisp
@@ -4,16 +4,16 @@
   ;; We can use for ... on, because it uses atom to test the end
   ;; of the list
   (let ((remaining (gensym))
-	(alist-var (gensym)))
+        (alist-var (gensym)))
     `(loop with ,alist-var = ,alist
-	   for ,remaining on ,alist-var
-	   do (let ((,element-var (car ,remaining)))
-		(cond ((consp ,element-var)
-		       ,@body)
-		      ((null ,element-var)
-		       nil)
-		      (t
-		       (error 'must-be-list
-			      :datum ,element-var
-			      :name ',function-name))))
-	   finally (return nil))))
+           for ,remaining on ,alist-var
+           do (let ((,element-var (car ,remaining)))
+                (cond ((consp ,element-var)
+                       ,@body)
+                      ((null ,element-var)
+                       nil)
+                      (t
+                       (error 'must-be-list
+                              :datum ,element-var
+                              :name ',function-name))))
+           finally (return nil))))
