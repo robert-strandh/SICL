43a290b26da75bcff50329393d1aa64404ed385d
Robert Strandh <robert.strandh@gmail.com>
2021-07-19 16:01:25 +0200

    Define function (SETF %FIND-PACKAGE)

--- Code/Package-and-symbol/find-package-defun.lisp
+++ Code/Package-and-symbol/find-package-defun.lisp
@@ -3,8 +3,15 @@
 (let* ((environment (sicl-environment:global-environment))
        (client (sicl-environment:client environment))
        (find-package-function
-         (sicl-environment:fdefinition client environment 'find-package)))
+         (sicl-environment:fdefinition
+          client environment 'sicl-environment:find-package))
+       (setf-find-package-function
+         (sicl-environment:fdefinition
+          client environment '(setf sicl-environment:find-package))))
   (defun find-package (name)
     (if (typep name 'package)
         name
-        (funcall find-package-function client environment name))))
+        (funcall find-package-function client environment name)))
+  (defun (setf %find-package) (package name)
+    (funcall setf-find-package-function
+             package client environment name)))
