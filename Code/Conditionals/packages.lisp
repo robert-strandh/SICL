16170af08158436bd0e50ce73aea2112adc91136
Robert Strandh <robert.strandh@gmail.com>
2021-07-22 14:19:30 +0200

    Untabify and remove copyright notice.

--- Code/Conditionals/packages.lisp
+++ Code/Conditionals/packages.lisp
@@ -1,47 +1,12 @@
-;;;; Copyright (c) 2008 - 2018
-;;;;
-;;;;     Robert Strandh (robert.strandh@gmail.com)
-;;;;
-;;;; All rights reserved.
-;;;;
-;;;; Redistribution and use in source and binary forms, with or
-;;;; without modification, are permitted provided that the following
-;;;; conditions are met:
-;;;;
-;;;; 1. Redistributions of source code must retain the above copyright
-;;;;    notice, this list of conditions and the following disclaimer.
-;;;; 2. Redistributions in binary form must reproduce the above
-;;;;    copyright notice, this list of conditions and the following
-;;;;    disclaimer in the documentation and/or other materials
-;;;;    provided with the distribution.
-;;;;
-;;;; THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
-;;;; CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
-;;;; INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
-;;;; MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-;;;; DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS
-;;;; BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
-;;;; EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
-;;;; TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-;;;; DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
-;;;; ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
-;;;; OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
-;;;; OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-;;;; POSSIBILITY OF SUCH DAMAGE.
-
-;;;; This file is part of the conditionals module of the SICL project.
-;;;; See the file SICL.text for a description of the project. 
-;;;; See the file conditionals.text for a description of the module.
-
 (defpackage #:sicl-conditionals
   (:use #:common-lisp #:cleavir-code-utilities)
   (:export #:or #:or-expander
            #:and #:and-expander
            #:when #:unless
            #:cond #:cond-expander
-	   #:case #:case-expander
+           #:case #:case-expander
            #:ccase #:ccase-expander
            #:ecase #:ecase-expander
-	   #:typecase #:typecase-expander
+           #:typecase #:typecase-expander
            #:ctypecase #:ctypecase-expander
            #:etypecase #:etypecase-expander))
