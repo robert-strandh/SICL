046b5b6929b29b8dfea726ec83c4fe7ec74b59fe
Robert Strandh <robert.strandh@gmail.com>
2021-07-23 15:45:25 +0200

    Extract code for importing a single symbol to a separate function.

--- Code/Package-and-symbol/import-defun.lisp
+++ Code/Package-and-symbol/import-defun.lisp
@@ -1,6 +1,9 @@
 (cl:in-package #:sicl-package)
 
-(defun import (symbol &optional (package *package*))
+(defun import-one-symbol (symbol package)
+  (setf (gethash (symbol-name symbol) (internal-symbols packae))
+        symbol)))
+  
+(defun import (symbols &optional (package *package*))
   (loop for symbol in symbols
-        do (setf (gethash (symbol-name symbol) (internal-symbols packae))
-                 symbol)))
+        do (import-one-symbol symbol package)))
