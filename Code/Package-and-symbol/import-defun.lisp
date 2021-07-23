fba111aae3696f7cb07c092d2fb102286adb8595
Robert Strandh <robert.strandh@gmail.com>
2021-07-23 15:52:21 +0200

    Turn the package designator into a package.

--- Code/Package-and-symbol/import-defun.lisp
+++ Code/Package-and-symbol/import-defun.lisp
@@ -1,9 +1,10 @@
 (cl:in-package #:sicl-package)
 
 (defun import-one-symbol (symbol package)
-  (setf (gethash (symbol-name symbol) (internal-symbols packae))
+  (setf (gethash (symbol-name symbol) (internal-symbols package))
         symbol)))
-  
-(defun import (symbols &optional (package *package*))
-  (loop for symbol in symbols
+
+(defun import (symbols &optional (package-designator *package*))
+  (loop with package = (package-designator-to-package package-designator)
+        for symbol in symbols
         do (import-one-symbol symbol package)))
