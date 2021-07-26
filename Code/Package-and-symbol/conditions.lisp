11565635eace12c289319b69711cbcb5c0c3b8b1
Robert Strandh <robert.strandh@gmail.com>
2021-07-26 17:28:18 +0200

    Remove unused condition type.

--- Code/Package-and-symbol/conditions.lisp
+++ Code/Package-and-symbol/conditions.lisp
@@ -1,9 +1,5 @@
 (in-package #:sicl-package)
 
-(define-condition package-error
-    (error acclimation:condition)
-  ())
-
 (define-condition not-a-package-designator
     (type-error acclimation:condition)
   ()
