38a02b0ef3c24ced728da615c97b8ac43478507a
Robert Strandh <robert.strandh@gmail.com>
2021-07-27 15:02:57 +0200

    Include definition of DESIGNATED-LIST-OF-SYMBOLS.

--- Code/Package-and-symbol/utilities.lisp
+++ Code/Package-and-symbol/utilities.lisp
@@ -84,3 +84,18 @@
              (when present-p
                (return-from find-inherited-symbol symbol used-package))))
   (values nil nil))
+
+;;; Recall that a designator for a list of symbols is either a non-nil
+;;; symbol (denoting a singleton list whose element is is that non-nil
+;;; symbol, or a proper list of symbols, denoting itself.  Thus the
+;;; symbol NIL denotes the empty list of symbols.
+(defun designated-list-of-symbols (designator)
+  (cond ((null designator) '())
+        ((symbolp designator) (list designator))
+        ((and (proper-list-p designator)
+              (every #'symbolp designator))
+         designator)
+        (t
+         (error 'not-a-valid-designator-for-list-of-symbols
+                :exptected-type 'designator-for-list-of-symbols
+                :datum designator))))
