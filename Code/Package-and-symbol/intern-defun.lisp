b2235bce4d0834978928f15d7ce6d082fbe76ee8
Robert Strandh <robert.strandh@gmail.com>
2021-07-28 16:50:34 +0200

    Use a generic function for manpulating home package of symbols.

--- Code/Package-and-symbol/intern-defun.lisp
+++ Code/Package-and-symbol/intern-defun.lisp
@@ -1,12 +1,16 @@
 (cl:in-package #:sicl-package)
 
+(defgeneric home-package (symbol))
+
+(defgeneric (setf home-package) (new-home-package symbol))
+
 (defun intern (symbol-name &optional (package-designator *package*))
   (let ((package (package-designator-to-package package-designator)))
     (multiple-value-bind (symbol-or-nil status)
         (find-symbol symbol-name package)
       (if (null status)
           (let ((new-symbol (make-symbol symbol-name)))
-            (setf (sicl-symbol:package new-symbol) package)
+            (setf (home-package new-symbol) package)
             (setf (gethash
                    symbol-name
                    (if (eq package (find-package '#:keyword))
