dda06edd3ae7e1285bcb8ee3a4b85c7b129336e3
Robert Strandh <robert.strandh@gmail.com>
2021-07-31 14:44:05 +0200

    Use new variable to eliminate duplicate string designators.

--- Code/Package-and-symbol/packages-extrinsic.lisp
+++ Code/Package-and-symbol/packages-extrinsic.lisp
@@ -2,53 +2,5 @@
 
 (defpackage sicl-package
   (:use #:common-lisp)
-  (:shadow
-   #:package
-   #:packagep
-   #:*package*
-   #:package-name
-   #:package-nicknames
-   #:package-shadowing-symbols
-   #:package-use-list
-   #:package-used-by-list
-   #:package-error
-   #:package-error-package
-   #:rename-package
-   #:make-package
-   #:import
-   #:intern
-   #:unintern
-   #:find-symbol
-   #:export
-   #:unexport
-   #:shadow
-   #:shadowing-import
-   #:unuse-package
-   #:use-package
-   #:do-symbols
-   #:do-external-symbols)
-  (:export #:package
-           #:*package*
-           #:export
-           #:find-symbol
-           #:shadow
-           #:shadowing-import
-           #:make-package
-           #:with-package-iterator
-           #:unexport
-           #:unintern
-           #:unuse-package
-           #:use-package
-           #:defpackage
-           #:do-symbols
-           #:do-external-symbols
-           #:intern
-           #:unintern
-           #:package-name
-           #:package-nicknames
-           #:package-shadowing-symbols
-           #:package-use-list
-           #:package-used-by-list
-           #:packagep
-           #:package-error
-           #:package-error-package))
+  (:shadow . #.asdf-user:*sicl-package-string-designators*)
+  (:export . #.asdf-user:*sicl-package-string-designators*))
