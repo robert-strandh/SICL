0dbc758bb56c2576fc12d2802b49eed86fefe811
Robert Strandh <robert.strandh@gmail.com>
2021-08-01 14:56:15 +0200

    Reove definition of package SICL-SYMBOL, now in separate module.

--- Code/Package-and-symbol/packages.lisp
+++ Code/Package-and-symbol/packages.lisp
@@ -4,24 +4,3 @@
   (:use #:common-lisp)
   (:shadow #:find-package)
   (:export . #.asdf-user:*sicl-package-string-designators*))
-
-(defpackage #:sicl-symbol
-  (:use #:common-lisp)
-  (:shadow
-   #:package
-   .
-   #+sicl-host-test (#:symbol)
-   #-sicl-host-test ())
-  (:export #:symbol
-           #:symbolp
-           #:keyword
-           #:keywordp
-           #:symbol-name
-           #:package
-           #:symbol-package
-           #:make-symbol
-           #:copy-symbol
-           #:gensym
-           #:*gensym-counter*
-           #:gentemp
-           #:variable-cell))
