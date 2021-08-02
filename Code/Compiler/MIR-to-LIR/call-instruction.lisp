883e094ef0753b47be83a9cb8de1e917bbbb580b
Hayley Patton <theemacsshibe@gmail.com>
2021-08-02 13:30:42 +1000

    Emit code for an unnamed call with no more than 5 arguments.

--- Code/Compiler/MIR-to-LIR/call-instruction.lisp
+++ Code/Compiler/MIR-to-LIR/call-instruction.lisp
@@ -9,6 +9,11 @@
 ;;; previous instructions, so we are left to generate code for the
 ;;; rest of the process.
 
+;;; TODO: put these magic numbers into another system.
+(defconstant +rack-prefix-size+ 2)
+(defconstant +rack-tag+ 7)
+(defconstant +standard-object-tag+ 5)
+
 (defmethod finish-lir-for-instruction
     ((instruction cleavir-ir:funcall-instruction))
   (destructuring-bind (function &rest arguments)
@@ -25,12 +30,14 @@
                 instruction
                 predecessor)
                (setf predecessor instruction)))
-        ;; 1. Load the function into some register. We need to get at
-        ;; the rack.
-        #+(or)
+        ;; 1. Load the rack of the function into a register.
         (emit
-         (x86-64:load-from-stack-instruction
-          (cleavir-ir:offset function) function-register))
+         (x86-64:load-from-location-instruction function function-register))
+        (emit
+         (make-instance 'cleavir-ir:memref2-instruction
+           :inputs (list function-register
+                         (- 8 +standard-object-tag+))
+           :outputs (list function-register)))
         (cond
           ((<= argument-count (length x86-64:*argument-registers*))
            ;; 2. Store the arguments in RDI, RSI, RDX, RCX, and R8.
@@ -38,8 +45,7 @@
                  for register in x86-64:*argument-registers*
                  do (x86-64:load-from-location-instruction argument
                                                            register))
-           ;; 3. Store the argument count in R9 as a fixnum.
-           ;; (Does this really have to be boxed though?)
+           ;; 3. Store the argument count in R9.
            (emit
             (make-instance 'cleavir-ir:assignment-instruction
               :inputs (list (make-instance 'cleavir-ir:immediate-input
@@ -50,8 +56,16 @@
            ;; CLOS/funcallable-standard-object-defclass.lisp the
            ;; environment is in the second slot of a
            ;; funcallable-standard-object.
+           (emit
+            (make-instance 'cleavir-ir:memref2-instruction
+              :inputs (list function-register
+                            (- (* 8 (+ +rack-prefix-size+ 1))
+                               +rack-tag+))
+              :outputs (list x86-64:*r10*)))
            ;; 5. Push the value of RBP on the stack.
-           
+           (emit
+            (make-instance 'sicl-ir:push-instruction
+              :inputs (list x86-64:*rbp*)))
            ;; 6. Copy the value of RSP into RBP.
            (emit
             (make-instance 'cleavir-ir:assignment-instruction
@@ -59,7 +73,12 @@
               :outputs (list x86-64:*rbp*)))
            ;; 7. Load the entry point address of the callee into an
            ;; available scratch register, typically RAX.
-
+           (emit
+            (make-instance 'cleavir-ir:memref2-instruction
+              :inputs (list function-register
+                            (- (* 8 (+ +rack-prefix-size+ 0))
+                               +rack-tag+))
+              :outputs (list x86-64:*rax*)))
            ;; 8. Use the CALL instruction with that register as an argument.
            ;; We will just reuse this CALL instruction.
            (setf (cleavir-ir:inputs instruction)
