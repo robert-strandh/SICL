315c6bbbe439e8859f9b759216a6fb1669331e2f
Robert Strandh <robert.strandh@gmail.com>
2021-08-11 17:29:12 +0200

    Complete rewrite of lambda-list destructuring code.

--- Code/Cleavir/Code-utilities/new-destructuring.lisp
+++ Code/Cleavir/Code-utilities/new-destructuring.lisp
@@ -5,90 +5,205 @@
        (not (null (first remaining)))
        (eq (first (first remaining)) lambda-list-keyword)))
 
-(defun handle-aux (remaining body)
-  (if (first-group-is remaining '&aux)
-      (values (rest remaining)
-              `(let* ,(rest (first remaining))
-                 ,body))
-      (values remaining body)))
+(defun handle-required
+    (required variable canonicalized-lambda-list invoking-form-variable)
+  (let ((bindings '())
+        (ignored-variables '())
+        (not-enough-arguments-form
+          `(error 'too-few-arguments
+                  :lambda-list
+                  ',(reduce #'append canonicalized-lambda-list)
+                  :invoking-form ,invoking-form-variable)))
+    (loop for pattern in required
+          do (if (symbolp pattern)
+                 (progn (push `(,pattern (if (null ,variable)
+                                             ,not-enough-arguments-form
+                                             (first ,variable)))
+                              bindings)
+                        (push `(,variable (rest ,variable))
+                              bindings))
+                 (let ((temp (gensym)))
+                   (push `(,temp (if (null ,variable)
+                                     ,not-enough-arguments-form
+                                     (first ,variable)))
+                         bindings)
+                   (push `(,variable (rest ,variable))
+                         bindings)
+                   (multiple-value-bind
+                         (nested-bindings nested-ignored-variables)
+                       (new-destructure-lambda-list pattern temp)
+                     (setf bindings
+                           (append nested-bindings bindings))
+                     (setf ignored-variables
+                           (append nested-ignored-variables ignored-variables))))))
+    (values bindings ignored-variables)))
 
-(defun new-destructure-lambda-list (canonicalized-lambda-list variable body)
-  (let ((remaining (reverse canonicalized-lambda-list))
-        (result body)
-        (allow-other-keys nil))
-    (when (first-group-is remaining '&aux)
-      (setf result
-            `(let* ,(rest (pop remaining))
-               ,result)))
-    (when (first-group-is remaining '&allow-other-keys)
-      (setf allow-other-keys t)
-      (pop remaining))
-    (when (first-group-is remaining '&key)
-      (let* ((group (pop remaining))
-             (keywords (mapcar #'caar (rest group))))
-        (loop for ((keyword var) default supplied-p)
-                in (reverse (rest group))
-              for temp = (gensym)
-              do (setf result
-                       `(let ((,var (if (null ,temp)
-                                        ,default
-                                        (second ,temp))))
-                          ,result))
-                 (unless (null supplied-p)
-                   (setf result
-                         `(let ((,supplied-p (not (null ,temp))))
-                            ,result)))
-                 (setf result
-                       `(let ((,temp (member ,keyword ,variable :test #'eq)))
-                          ,result)))
-        (unless allow-other-keys
-          (let ((temp (gensym)))
-            (setf result
-                  `(progn (unless (getf ,variable :allow-other-keys)
-                            (loop for ,temp in ,variable (by #'cddr)
-                                  unless (member ,temp ',keywords)
-                                    do (error "Invalid keyword: ~s" ,temp)))
-                          ,result))))
-        (setf result
-              `(if (oddp (length ,variable))
-                   (error "Odd number of keywords/arguments: ~s"
-                          ,variable)
-                   ,result))))
+(defun handle-optional (optional variable)
+  (let ((bindings '()))
+    (loop for (var default supplied-p) in optional
+          do (unless (null supplied-p)
+               (push `(,supplied-p (not (null ,variable)))
+                     bindings))
+             (push `(,var (if (null ,variable)
+                              ,default
+                              (pop ,variable)))
+                   bindings))
+    bindings))
+
+(defun handle-rest/body
+    (pattern variable invoking-form-variable)
+  (let ((bindings '())
+        (ignored-variables '()))
+    (if (symbolp pattern)
+        (push `(,pattern ,variable) bindings)
+        (let ((temp (gensym)))
+          (push `(,temp ,variable)
+                bindings)
+          (multiple-value-bind (nested-bindings nested-ignored-variables)
+              (new-destructure-lambda-list pattern invoking-form-variable)
+            (setf bindings
+                  (append nested-bindings bindings))
+            (setf ignored-variables
+                  (append nested-ignored-variables ignored-variables)))))
+    (values bindings ignored-variables)))
+
+(defun handle-key
+    (key variable canonicalized-lambda-list invoking-form-variable allow-other-keys)
+  (let* ((bindings '())
+         (ignored-variables '())
+         (keywords (mapcar #'caar key))
+         (odd-number-of-keyword-arguments-form
+           `(error 'odd-number-of-keyword-arguments
+                   :lambda-list
+                   ',(reduce #'append canonicalized-lambda-list)
+                   :invoking-form ,invoking-form-variable))
+         (check-keywords-form
+           (let ((temp (gensym)))
+             `(let ((,temp ,variable))
+                (tagbody
+                 again
+                   (if (null temp) (go out))
+                   (if (not (member (first temp)
+                                    '(:allow-other-keys ,@keywords)
+                                    :test #'eq))
+                       (error 'invalid-keyword
+                              :lambda-list
+                              ',(reduce #'append canonicalized-lambda-list)
+                              :invoking-form ,invoking-form-variable)
+                       (progn (setf ,temp (cddr ,temp))
+                              (go again)))
+                 out)))))
+    (let ((ignored (gensym)))
+      (push ignored ignored-variables)
+      (push `(,ignored (if (oddp (length ,variable))
+                           ,odd-number-of-keyword-arguments-form))
+            bindings))
+    (unless allow-other-keys
+      (let ((ignored (gensym)))
+        (push ignored ignored-variables)
+        (push `(,ignored (if (not (getf ,variable :allow-other-keys))
+                             ,check-keywords-form))
+              bindings)))
+    (loop for ((keyword var) default supplied-p) in key
+          for temp1 = (gensym)
+          for temp2 = (gensym)
+          do (push `(,temp1 (list nil)) bindings)
+             (push `(,temp2 (getf ,variable ,keyword ,temp1))
+                   bindings)
+             (if (null supplied-p)
+                 nil
+                 (push `(,supplied-p (not (eq ,temp2 ,temp1)))
+                       bindings))
+             (push `(,var (if (eq ,temp2 ,temp1)
+                              ,default
+                              ,temp2))
+                   bindings))
+    (values bindings ignored-variables)))
+
+(defun new-destructure-lambda-list
+    (canonicalized-lambda-list variable invoking-form-variable)
+  (let* ((remaining canonicalized-lambda-list)
+         (bindings '())
+         (ignored-variables '()))
+    (unless (or (null remaining)
+                (member (first (first remaining)) (intrinsic-keywords)
+                        :test #'eq))
+      (multiple-value-bind (nested-bindings nested-ignored-variables)
+          (handle-required
+           (pop remaining) variable canonicalized-lambda-list invoking-form-variable)
+        (setf bindings
+              (append nested-bindings bindings))
+        (setf ignored-variables
+              (append nested-ignored-variables ignored-variables))))
+    (when (first-group-is remaining '&optional)
+      (setf bindings
+            (append (handle-optional (rest (pop remaining)) variable))))
     (when (or (first-group-is remaining '&rest)
               (first-group-is remaining '&body))
-      (let ((pattern (second (pop remaining))))
-        (setf result
-              (if (symbolp pattern)
-                  `(let (,pattern ,variable)
-                     ,result)
-                  (let ((temp (gensym)))
-                    `(let ((,temp ,variable))
-                       ,(new-destructure-lambda-list
-                         pattern temp result)))))))
-    (when (first-group-is remaining '&optional)
-      (loop for (var default supplied-p) in (reverse (rest (pop remaining)))
-            do (setf result
-                     `(let ((,var (if (null ,variable)
-                                      ,default
-                                      (pop ,variable))))
-                        ,result))
-               (unless (null supplied-p)
-                 (setf result
-                       `(let ((,supplied-p (not (null ,variable))))
-                          ,result)))))
-    (loop for pattern in (reverse (pop remaining))
-          do (setf result
-                   `(if (null ,variable)
-                        (error "Not enough arguments.")
-                        ,(if (symbolp pattern)
-                             `(let ((,pattern (pop ,variable)))
-                                ,result)
-                             (let ((temp (gensym)))
-                               `(let ((,temp (pop ,variable)))
-                                  ,(new-destructure-lambda-list
-                                    pattern temp result)))))))
-    (when (first-group-is remaining '&whole)
-      (setf result
-            `(let ((,(second (first remaining)) ,variable))
-               ,result)))
-    result))
+      (multiple-value-bind (nested-bindings nested-ignored-variables)
+          (handle-rest/body
+           (second (pop remaining)) variable invoking-form-variable)
+        (setf bindings
+              (append nested-bindings bindings))
+        (setf ignored-variables
+              (append nested-ignored-variables ignored-variables))))
+
+    (when (first-group-is remaining '&key)
+      (let* ((group (pop remaining))
+             (allow-other-keys
+               (if (first-group-is remaining '&allow-other-keys)
+                   (progn (pop remaining) t)
+                   nil)))
+        (multiple-value-bind (nested-bindings nested-ignored-variables)
+            (handle-key
+             (rest group)
+             variable
+             canonicalized-lambda-list
+             invoking-form-variable
+             allow-other-keys)
+          (setf bindings
+                (append nested-bindings bindings))
+          (setf ignored-variables
+                (append nested-ignored-variables ignored-variables)))))
+    (when (first-group-is remaining '&aux)
+      (setf bindings
+            (append (reverse (rest (pop remaining))) bindings)))
+    (values bindings ignored-variables)))
+
+(defun new-parse-macro (name lambda-list body &optional environment)
+  (declare (ignore name environment)) ; For now.
+  (let* ((canonicalized-lambda-list
+           (canonicalize-macro-lambda-list lambda-list))
+         (environment-group
+           (extract-named-group canonicalized-lambda-list '&environment))
+         (environment-parameter
+           (if (null environment-group) (gensym) (second environment-group)))
+         (whole-group
+           (extract-named-group canonicalized-lambda-list '&whole))
+         (whole-parameter
+           (if (null whole-group) (gensym) (second whole-group)))
+         (remaining
+           (remove '&environment
+                   (remove '&whole canonicalized-lambda-list
+                           :key #'first :test #'eq)
+                   :key #'first :test #'eq))
+         (args-var (gensym)))
+    (multiple-value-bind (declarations documentation forms)
+        (separate-function-body body)
+      (multiple-value-bind (bindings ignored-variables)
+          (new-destructure-lambda-list remaining args-var whole-parameter)
+        `(lambda (,whole-parameter ,environment-parameter)
+           ,@(if (null documentation) '() (list documentation))
+           ;; If the lambda list does not contain &environment, then
+           ;; we IGNORE the GENSYMed parameter to avoid warnings.
+           ;; If the lambda list does contain &environment, we do
+           ;; not want to make it IGNORABLE because we would want a
+           ;; warning if it is not used then.
+           ,@(if (null environment-group)
+                 `((declare (ignore ,environment-parameter)))
+                 `())
+           (let ((,args-var (rest ,whole-parameter)))
+             (let* ,(reverse bindings)
+               (declare (ignore ,@ignored-variables))
+               ,@declarations
+               ,@forms)))))))
