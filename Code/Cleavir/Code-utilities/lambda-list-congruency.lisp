66a102e1f8b4fefe7f3ec3dd0ba9129e7d9e1ec3
Robert Strandh <robert.strandh@gmail.com>
2021-08-12 15:52:43 +0200

    Update congruency code for new lambda list canonicalizer.

--- Code/Cleavir/Code-utilities/lambda-list-congruency.lisp
+++ Code/Cleavir/Code-utilities/lambda-list-congruency.lisp
@@ -38,52 +38,72 @@
 ;;; 6. The use of &aux need not be consistent across methods.
 
 ;;; Check rule number 1.
-(defun congruent-required-p (lambda-list-1 lambda-list-2)
-  (= (length (required lambda-list-1))
-     (length (required lambda-list-2))))
+(defun congruent-required-p
+    (canonicalized-lambda-list-1 canonicalized-lambda-list-2)
+  (= (length (extract-required canonicalized-lambda-list-1))
+     (length (extract-required canonicalized-lambda-list-2))))
 
 ;;; Check rule number 2.
-(defun congruent-optionals-p (lambda-list-1 lambda-list-2)
-  (or (and (eq (optionals lambda-list-1) :none)
-           (eq (optionals lambda-list-2) :none))
-      (and (listp (optionals lambda-list-1))
-           (listp (optionals lambda-list-2))
-           (= (length (optionals lambda-list-1))
-              (length (optionals lambda-list-2))))))
+(defun congruent-optionals-p
+    (canonicalized-lambda-list-1 canonicalized-lambda-list-2)
+  (let ((optional1
+          (extract-named-group canonicalized-lambda-list-1 '&optional))
+        (optional2
+          (extract-named-group canonicalized-lambda-list-2 '&optional)))
+    (= (length optional1) (length  optional2))))
 
 ;;; Check rule number 3.
-(defun congruent-key-rest-p (lambda-list-1 lambda-list-2)
-  (or (and (eq (rest-body lambda-list-1) :none)
-           (eq (keys lambda-list-1) :none)
-           (eq (rest-body lambda-list-2) :none)
-           (eq (keys lambda-list-2) :none))
-      (and (or (not (eq (rest-body lambda-list-1) :none))
-               (not (eq (keys lambda-list-1) :none)))
-           (or (not (eq (rest-body lambda-list-2) :none))
-               (not (eq (keys lambda-list-2) :none))))))
+(defun congruent-key-rest-p
+    (canonicalized-lambda-list-1 canonicalized-lambda-list-2)
+  (let ((rest1
+          (extract-named-group canonicalized-lambda-list-1 '&rest))
+        (rest2
+          (extract-named-group canonicalized-lambda-list-2 '&rest))
+        (key1
+          (extract-named-group canonicalized-lambda-list-1 '&key))
+        (key2
+          (extract-named-group canonicalized-lambda-list-2 '&key)))
+    (or (and (null rest1) (null key1) (null rest2) (null key2))
+        (and (or (not (null rest1)) (not (null key1)))
+             (or (not (null rest2)) (not (null key2)))))))
 
 (defun same-keys-accepted-p
-    (generic-function-lambda-list method-lambda-list)
-  (or (eq (keys generic-function-lambda-list) :none)
-      (null (set-exclusive-or
-             (keys generic-function-lambda-list)
-             (keys method-lambda-list)
-             :test #'eq
-             :key #'caar))
-      (not (eq (allow-other-keys method-lambda-list) :none))
-      (and (not (eq (rest-body method-lambda-list) :none))
-           (eq (keys method-lambda-list) :none))))
+    (canonicalized-generic-function-lambda-list canonicalized-method-lambda-list)
+  (let ((generic-function-key
+          (extract-named-group
+           canonicalized-generic-function-lambda-list '&key)))
+    (or (null generic-function-key)
+        (let ((method-key
+                (extract-named-group
+                 canonicalized-method-lambda-list '&key))
+              (method-allow-other-keys
+                (extract-named-group
+                 canonicalized-method-lambda-list '&allow-other-keys))
+              (method-rest
+                (extract-named-group
+                 canonicalized-method-lambda-list '&allow-other-keys)))
+          (or (not (null method-allow-other-keys))
+              (and (not (null method-rest))
+                   (null method-key))
+              (loop with method-keywords = (mapcar #'caar (cdr method-key))
+                    for key-parameter in (rest generic-function-key)
+                    for keyword = (caar key-parameter)
+                    always (member keyword method-keywords :test #'eq)))))))
 
 (defun lambda-lists-congruent-p
-    (generic-function-lambda-list method-lambda-list)
+    (canonicalized-generic-function-lambda-list canonicalized-method-lambda-list)
   (and (congruent-required-p
-        generic-function-lambda-list method-lambda-list)
+        canonicalized-generic-function-lambda-list
+        canonicalized-method-lambda-list)
        (congruent-optionals-p
-        generic-function-lambda-list method-lambda-list)
+        canonicalized-generic-function-lambda-list
+        canonicalized-method-lambda-list)
        (congruent-key-rest-p
-        generic-function-lambda-list method-lambda-list)
+        canonicalized-generic-function-lambda-list
+        canonicalized-method-lambda-list)
        (same-keys-accepted-p
-        generic-function-lambda-list method-lambda-list)))
+        canonicalized-generic-function-lambda-list
+        canonicalized-method-lambda-list)))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;
@@ -96,29 +116,18 @@
 ;;; options creates a generic function, and if the lambda list for the
 ;;; method mentions keyword arguments, the lambda list of the generic
 ;;; function will mention &key (but no keyword arguments).
-;;;
-;;; We return 2 values, the unparsed and the parsed lambda list.
 
-(defun generate-congruent-lambda-list (method-lambda-list)
-  (let* ((parsed-lambda-list
-           (make-instance
-            'lambda-list
-            :required (required method-lambda-list)
-            :optionals (optionals method-lambda-list)
-            :rest (rest-body method-lambda-list)
-            :keys (if (eq (keys method-lambda-list) :none)
-                      :none
-                      '())))
-         (unparsed-lambda-list
-           `(,(required parsed-lambda-list)
-             ,@(if (eq (optionals parsed-lambda-list) :none)
-                   '()
-                   `(&optional ,@(optionals parsed-lambda-list)))
-             ,@(if (eq (rest-body parsed-lambda-list) :none)
-                   '()
-                   `(&rest ,@(rest-body parsed-lambda-list)))
-             ,@(if (eq (keys parsed-lambda-list) :none)
-                   '()
-                   `(&key)))))
-    (values unparsed-lambda-list
-            parsed-lambda-list)))
+(defun generate-congruent-lambda-list (canonicalized-method-lambda-list)
+  (let ((required
+          (extract-required canonicalized-method-lambda-list))
+        (optional
+          (extract-named-group canonicalized-method-lambda-list '&optional))
+        (key
+          (extract-named-group canonicalized-method-lambda-list '&key)))
+    `(,@required
+      ,@(if (null optional)
+            '()
+            `((&optional ,@(mapcar #'car optional))))
+      ,@(if (null key)
+            '()
+            '((&key))))))
