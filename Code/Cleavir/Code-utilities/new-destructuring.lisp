70f4bf582dece40369896f30712941053e260a7b
Robert Strandh <robert.strandh@gmail.com>
2021-08-12 14:58:46 +0200

    Add comments.

--- Code/Cleavir/Code-utilities/new-destructuring.lisp
+++ Code/Cleavir/Code-utilities/new-destructuring.lisp
@@ -1,10 +1,66 @@
 (cl:in-package #:cleavir-code-utilities)
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;; Function DESTRUCTURE-LAMBDA-LIST.
+;;;
+;;; Destructuring a tree according to a lambda list.
+;;;
+;;; The destructuring itself is typically done when a macro function
+;;; is run, and the purpose is to take the macro form apart and assign
+;;; parts of it to the parameters of the lambda list of the macro.
+;;;
+;;; The function DESTRUCTURE-LAMBDA-LIST generates the code for doing
+;;; the destrucuring.  It is typically run by the expansion of
+;;; DEFMACRO.  Recall that DEFMACRO must take the definition of a
+;;; macro, in particular its lambda list, and generate a macro
+;;; function.  The macro function takes the macro form as input and
+;;; generates the expanded form.  Destructuring is done by a LET*
+;;; form, and this code generates the bindings of that LET* form.
+;;;
+;;; It would have been more elegant to generate nested LET
+;;; expressions, rather than a single LET*, because there are some
+;;; arbitrary forms that need to be evaluated in between bindings, and
+;;; those forms would fit more naturally into the body of a LET.  With
+;;; a single LET* those forms must be part of the binding form of the
+;;; LET*, and there is not always an obvious lexical variable to bind
+;;; the result to.  So we must generate new variables and then ignore
+;;; them in the LET* body.  But we do it this way because the DEFMACRO
+;;; form may contain declarations that mention the variables in the
+;;; DEFMACRO lambda list, and with nested LET expressions, some of
+;;; those variables would then be introduced in a LET expression that
+;;; is not the innermost one.  We could handle some such cases with
+;;; LOCALLY, but IGNORE declarations result in warnings in some
+;;; implementations.
+;;;
+;;; So, the bindings that we create will contain generated variables
+;;; that are not used in the body of the macro definition, and we want
+;;; them to be declared IGNORE.  For that reason,
+;;; DESTRUCTURE-LAMBDA-LIST returns two values: the bindings mentioned
+;;; above, and a list of variables to declare IGNORE in the beginning
+;;; of the body of the macro function.
+;;;
+;;; The bindings return by DESTRUCTURE-LAMBDA-LIST and its subroutines
+;;; are in the reverse order compared to the order it which they
+;;; should appear in the expanded expression.  We do it this way in
+;;; order to avoid too much consing.
+;;;
+;;; We assume that the lambda-list is syntactically correct.  It
+;;; should be, because this function takes as input not the raw lambda
+;;; list, but a canonicalized lambda list that has already been
+;;; checked for errors.
+
+;;; Given a list of the remaining groups of a lambda list, return true
+;;; if and only the list is not empty, and the first group of the list
+;;; starts with LAMBDA-LIST-KEYWORD.
 (defun first-group-is (remaining lambda-list-keyword)
   (and (not (null remaining))
        (not (null (first remaining)))
        (eq (first (first remaining)) lambda-list-keyword)))
 
+;;; Destructure a list of required parameters.  A required parameter
+;;; may be a variable or a pattern.  Return a list of bindings and a
+;;; list of variables to ignore.
 (defun handle-required
     (required variable canonicalized-lambda-list invoking-form-variable)
   (let ((bindings '())
@@ -38,6 +94,7 @@
                            (append nested-ignored-variables ignored-variables))))))
     (values bindings ignored-variables)))
 
+;;; Destructure an optional parameter.  Return a list of bindings.
 (defun handle-optional (optional variable)
   (let ((bindings '()))
     (loop for (var default supplied-p) in optional
@@ -54,6 +111,9 @@
                    bindings))
     bindings))
 
+;;; Destructure a &REST or &BODY parameter which can be a variable or
+;;; a pattern.  Return a list of bindings and a list of variables to
+;;; ignore.
 (defun handle-rest/body
     (pattern variable invoking-form-variable)
   (let ((bindings '())
@@ -71,6 +131,8 @@
                   (append nested-ignored-variables ignored-variables)))))
     (values bindings ignored-variables)))
 
+;;; Destructure a list of &KEY parameters.  Return a list of bindings
+;;; and a list of variables to ignore.
 (defun handle-key
     (key variable canonicalized-lambda-list invoking-form-variable allow-other-keys)
   (let* ((bindings '())
@@ -124,6 +186,10 @@
                               ,temp2))
                    bindings))
     (values bindings ignored-variables)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;; DESTRUCTURE-LAMBDA-LIST
 
 (defun new-destructure-lambda-list
     (canonicalized-lambda-list variable invoking-form-variable)
