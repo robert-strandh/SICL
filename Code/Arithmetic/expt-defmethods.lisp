1593dea13e3be6d4d7c353eed85f2b7323325988
Bike <aeshtaer@gmail.com>
2021-08-17 13:08:12 -0400

    Add basic definitions for elementary ratio arithmetic

--- Code/Arithmetic/expt-defmethods.lisp
+++ Code/Arithmetic/expt-defmethods.lisp
@@ -10,3 +10,19 @@
               (power power (ash power -1))
               (result 1 (if (oddp power) (* result base) result)))
              ((zerop power result))))))
+
+(defmethod expt ((base ratio) (power integer))
+  (cond ((zerop power) 1)
+        ((= power 1) base)
+        (t
+         (flet (;; In the below, we know num and den are coprime, so we
+                ;; build a ratio directly rather than divide.
+                (%ratio (num den)
+                  (make-instance 'ratio :numerator num :denominator den)))
+           (let ((num (numerator base)) (den (denominator base)))
+             (if (minusp power)
+                 (let ((npower (- power)))
+                   (cond ((= num 1) (expt den npower))
+                         ((= num -1) (expt (- den) npower))
+                         (t (%ratio (expt den npower) (expt num npower)))))
+                 (%ratio (expt num npower) (expt den npower))))))))
