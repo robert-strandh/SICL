45a009c3d946d30dc9e3109463220d335ef2db2b
Bike <aeshtaer@gmail.com>
2021-08-17 17:30:17 -0400

    Define comparisons for ratios

--- Code/Arithmetic/binary-equal-defmethods.lisp
+++ Code/Arithmetic/binary-equal-defmethods.lisp
@@ -2,3 +2,12 @@
 
 (defmethod binary-equal ((x fixnum) (y fixnum))
   (if (cleavir-primop:fixnum-equal x y) t nil))
+
+(defmethod binary-equal ((x fixnum) (y bignum)) nil)
+(defmethod binary-equal ((x bignum) (y fixnum)) nil)
+
+(defmethod binary-equal ((x integer) (y ratio)) nil)
+(defmethod binary-equal ((x ratio) (y integer)) nil)
+(defmethod binary-equal ((x ratio) (y ratio))
+  (and (binary-equal (numerator x) (numerator y))
+       (binary-equal (denominator x) (denominator y))))
--- Code/Arithmetic/binary-less-defmethods.lisp
+++ Code/Arithmetic/binary-less-defmethods.lisp
@@ -2,3 +2,13 @@
 
 (defmethod binary-less ((x fixnum) (y fixnum))
   (if (cleavir-primop:fixnum-less x y) t nil))
+
+(defmethod binary-less ((x ratio) (y integer))
+  (binary-less (floor (numerator x) (denominator x)) y))
+
+(defmethod binary-less ((x integer) (y ratio))
+  (binary-less x (ceiling (numerator y) (denominator y))))
+
+(defmethod binary-less ((x ratio) (y ratio))
+  (binary-less (* (numerator x) (denominator y))
+               (* (numerator y) (denominator x))))
