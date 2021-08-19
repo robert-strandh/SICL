e700daa4a6f663594d3ef7b4a8aaa344cecbde6b
Robert Strandh <robert.strandh@gmail.com>
2021-08-19 11:20:58 +0200

    No longer pass CLIENT and no longer access it in the environment.

--- Code/Cons/remf-defmacro.lisp
+++ Code/Cons/remf-defmacro.lisp
@@ -1,6 +1,5 @@
 (cl:in-package #:sicl-cons)
 
 (defmacro remf (&environment environment place indicator)
-  (let* ((global-env (sicl-environment:global-environment environment))
-         (client (sicl-environment:client global-env)))
-    (remf-expander client environment place indicator)))
+  (let ((global-env (sicl-environment:global-environment environment)))
+    (remf-expander environment place indicator)))
