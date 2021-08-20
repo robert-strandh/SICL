21a1e108a6aedd428e40c35b3694f7aba207ae77
Robert Strandh <robert.strandh@gmail.com>
2021-08-20 11:18:01 +0200

    Define TYPE-EXPANDER rather than loading a file with definition.

--- Code/Boot/Phase-6/enable-typep.lisp
+++ Code/Boot/Phase-6/enable-typep.lisp
@@ -1,10 +1,12 @@
 (cl:in-package #:sicl-boot-phase-6)
 
 (defun enable-typep (e5)
-  (load-source-file "Types/type-expander-defun.lisp" e5)  
+  (let ((client (env:client e5)))
+    (setf (env:fdefinition client e5 'sicl-type:type-expander)
+          (lambda (type-descriptor)
+            (env:type-expander client e5 type-descriptor))))
   (define-error-functions '(typep sicl-type::typep-atomic) e5)
   (load-source-file "Types/Typep/typep-atomic.lisp" e5)
   (load-source-file "Types/Typep/typep-compound-integer.lisp" e5)
   (load-source-file "Types/Typep/typep-compound.lisp" e5)
   (load-source-file "Types/Typep/typep.lisp" e5))
-
