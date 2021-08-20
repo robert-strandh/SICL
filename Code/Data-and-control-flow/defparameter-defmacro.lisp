b56d0dec8eb4668846cea12c8b10a416f1eda925
Robert Strandh <robert.strandh@gmail.com>
2021-08-20 10:17:05 +0200

    Use SICL-specific version of accessor VARIABLE-DESCRIPTION.

--- Code/Data-and-control-flow/defparameter-defmacro.lisp
+++ Code/Data-and-control-flow/defparameter-defmacro.lisp
@@ -4,14 +4,11 @@
     (&environment environment name initial-value &optional documentation)
   ;; FIXME: handle the documentation.
   (declare (ignore documentation))
-  (let ((env-var (gensym))
-        (client-var (gensym)))
+  (let ((env-var (gensym)))
     `(progn
        (eval-when (:compile-toplevel)
-         (let* ((,env-var (sicl-environment:global-environment ,environment))
-                (,client-var (sicl-environment:client ,env-var)))
-           (setf (sicl-environment:variable-description
-                  ,client-var ,env-var ',name)
+         (let* ((,env-var (sicl-environment:global-environment ,environment)))
+           (setf (sicl-environment:variable-description ,env-var ',name)
                  (make-instance 'sicl-environment:special-variable-description))))
        (eval-when (:load-toplevel :execute)
          (setf (special-variable ',name nil) nil)
