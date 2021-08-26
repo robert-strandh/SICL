1d687082fe4da28a7e978b51787023bec3be6b89
Robert Strandh <robert.strandh@gmail.com>
2021-08-26 14:55:06 +0200

    Keep only main package, and do not shadow CL symbols.

--- Code/Format/packages.lisp
+++ Code/Format/packages.lisp
@@ -1,24 +1,3 @@
-;;; The symbols that are shadowed from the COMMON-LISP package
-;;; are also symbols that we want to export.  To avoid repeating 
-;;; that list of symbols, we use the reader macros #= and ##. 
-;;; It is interesting to note that we would normally write 
-;;; (:shadow <string1> <string2> ...), but in order to put a 
-;;; reader label on the list (<string1> <string2> ...) we need to
-;;; express that as (:shadow . (<string1> <string2> ...)) instead. 
 (defpackage #:sicl-format
-    (:use #:cl)
-  (:shadow #:type . #1=(#:format))
-  (:export . #1#))
-
-;;; In the test package, we want to import all symbols of the
-;;; COMMON-LISP package, except the ones that were shadowed in
-;;; the package that we are testing.  We do that by programatically
-;;; getting the list of those symbols using the function 
-;;; package-shadowing symbols.  But since defpackage doesn't evaluate
-;;; the symbols in the :shadowing-import-from list, we need to 
-;;; have the reader produce that list, hence the use of the #. reader
-;;; macro and again of the consing dot followed by a list. 
-(defpackage #:sicl-format-test
-    (:shadowing-import-from #:sicl-format .
-                            #.(package-shadowing-symbols '#:sicl-format))
-    (:use #:sicl-format #:cl #:lisp-unit))
+  (:use #:cl)
+  (:export))
