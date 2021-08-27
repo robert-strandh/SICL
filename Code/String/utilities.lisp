21357af649e745199a7f931901c04c8f2f2059da
Robert Strandh <robert.strandh@gmail.com>
2021-08-27 16:00:45 +0200

    Define utility functino EXTRACT-INTERVAL.

--- Code/String/utilities.lisp
+++ Code/String/utilities.lisp
@@ -1,5 +1,15 @@
 (cl:in-package #:sicl-string)
 
+;;; Create a fresh string containing the characters of STRING in the
+;;; interval between START and END.
+(defun extract-interval (string start end)
+  (let ((result (make-string (- end start))))
+    (loop for source-index from start below end
+          for destination-index from 0
+          do (setf (char result destination-index)
+                   (char string source-index)))
+    result))
+
 (defun check-bounding-indices (string start end)
   (let ((length (length string)))
     (unless (typep start `(integer 0 ,length))
