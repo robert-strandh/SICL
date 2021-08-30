559f69cf1f12d0323fefb3b92e08c9378e9c8b4d
Robert Strandh <robert.strandh@gmail.com>
2021-08-30 17:45:49 +0200

    Load source file containing array-limit constants.

--- Code/Boot/Phase-6/enable-array-access.lisp
+++ Code/Boot/Phase-6/enable-array-access.lisp
@@ -1,6 +1,7 @@
 (cl:in-package #:sicl-boot-phase-6)
 
 (defun enable-array-access (e5)
+  (load-source-file "Array/array-limits-defconstants.lisp" e5)
   (load-source-file "Array/array-dimension-defun.lisp" e5)
   (load-source-file "Array/array-total-size-defun.lisp" e5)
   (load-source-file "Array/fill-pointer.lisp" e5)
