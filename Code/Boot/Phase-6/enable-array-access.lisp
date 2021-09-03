304c0910ed9289cc7ead4547891c06c334042c17
Robert Strandh <robert.strandh@gmail.com>
2021-09-03 11:26:41 +0200

    Load file containing definition of ARRAY-HAS-FILL-POINTER-P.

--- Code/Boot/Phase-6/enable-array-access.lisp
+++ Code/Boot/Phase-6/enable-array-access.lisp
@@ -11,4 +11,5 @@
   (load-source-file "Array/row-major-aref-defmethods.lisp" e5)
   (load-source-file "Array/aref-defuns.lisp" e5)
   (load-source-file "Array/array-rank-defun.lisp" e5)
-  (load-source-file "Array/adjustable-array-p.lisp" e5))
+  (load-source-file "Array/adjustable-array-p.lisp" e5)
+  (load-source-file "Array/array-has-fill-pointer-p.lisp" e5))
