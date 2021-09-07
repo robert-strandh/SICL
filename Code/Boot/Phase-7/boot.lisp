96ce467bef560b34681b09b592540d34a6aa0042
Robert Strandh <robert.strandh@gmail.com>
2021-09-07 16:07:24 +0200

    Move loading of funtions REALP and RATIONALP to phase 7.

--- Code/Boot/Phase-7/boot.lisp
+++ Code/Boot/Phase-7/boot.lisp
@@ -3,6 +3,8 @@
 (defun boot (boot)
   (with-accessors ((e5 sicl-boot:e5))
       boot
+    (load-source-file "Arithmetic/realp-defgeneric.lisp" e5)
+    (load-source-file "Arithmetic/rationalp-defgeneric.lisp" e5)
     (load-asdf-system '#:sicl-arithmetic-defuns e5)
     (load-source-file "Cons/accessor-defuns.lisp" e5)
     (load-asdf-system '#:sicl-cons-defuns e5)))
