b98c9c0d2a3969ecf3e170b3c44f33aaec98ade6
Robert Strandh <robert.strandh@gmail.com>
2021-09-09 18:20:53 +0200

    Remove obsolete systems, now supplied by Eclector.

--- Code/Readtable/Simple/packages.lisp
+++
@@ -1,6 +0,0 @@
-(cl:in-package #:common-lisp-user)
-
-(defpackage #:sicl-simple-readtable
-  (:use #:common-lisp)
-  (:shadow #:readtable)
-  (:export #:readtable))
--- Code/Readtable/sicl-readtable.asd
+++
@@ -1,7 +0,0 @@
-(cl:in-package #:asdf-user)
-
-(defsystem :sicl-readtable
-  :serial t
-  :components
-  ((:file "packages")
-   (:file "generic-functions")))
