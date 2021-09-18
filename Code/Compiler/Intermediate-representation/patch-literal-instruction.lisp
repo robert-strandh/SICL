b8ad07b99f1e6bbb5dd5f58a7e23033d83fa2106
Robert Strandh <robert.strandh@gmail.com>
2021-09-18 14:57:43 +0200

    Modify the PATCH-LITERAL-INSTRUCTION to take three inputs.

--- Code/Compiler/Intermediate-representation/patch-literal-instruction.lisp
+++ Code/Compiler/Intermediate-representation/patch-literal-instruction.lisp
@@ -1,21 +1,25 @@
 (cl:in-package #:sicl-ir)
 
+;;; This instruction takes three inputs.  The first input is the
+;;; object computed at load time and which will become a literal
+;;; object at run time.  The second input is an index into the code
+;;; vector where the object should be stored for use by an instruction
+;;; that loads an immediate object from the instruction stream.  The
+;;; third input is an index into the vector of constants where the
+;;; object should be stored so that it can be traced by the garbage
+;;; collector to keep it live.
+;;;
+;;; The slot contains a CONS cell that is shared by the corresponding
+;;; LOAD-LITERAL instruction.  The HIR evaluator evaluates this
+;;; instruction by storing the first input in the CAR of the CONS
+;;; cell, and by calling the function
+;;; SICL-RUN-TIME:RESOLVE-LOAD-TIME-VALUE with the three inputs as
+;;; arguments.
+
 (defclass patch-literal-instruction
     (cleavir-ir:instruction
      cleavir-ir:one-successor-mixin
      cleavir-ir:side-effect-mixin)
   (;; This slot holds a CONS cell that is shared with the LOAD-LITERAL
    ;; instruction.
-   (%literal-cell :initarg :literal-cell :reader literal-cell)
-   ;; This slot holds a CONS cell that (in its CAR) holds an index
-   ;; into the code vector where the object created by the load-time
-   ;; action must be stored.
-   (%code-vector-index-cell
-    :initform (list nil)
-    :reader code-vector-index-cell)
-   ;; This slot holds a CONS cell that (in its CAR) holds an index
-   ;; into the literals vector where the object created by the
-   ;; load-time action must be stored.
-   (%literals-vector-index-cell
-    :initform (list nil)
-    :reader literals-vector-index-cell)))
+   (%literal-cell :initarg :literal-cell :reader literal-cell)))
