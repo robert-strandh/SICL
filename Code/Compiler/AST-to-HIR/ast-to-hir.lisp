7caa1f10a8a8e482d19d8b036e7211bd7a6f042b
Robert Strandh <robert.strandh@gmail.com>
2021-09-21 13:57:47 +0200

    Bind *GENSYM-COUNTER* to 0 and fix incorrect indentation.

--- Code/Compiler/AST-to-HIR/ast-to-hir.lisp
+++ Code/Compiler/AST-to-HIR/ast-to-hir.lisp
@@ -1,8 +1,9 @@
 (cl:in-package #:sicl-ast-to-hir)
 
 (defun ast-to-hir (client ast)
-    (eliminate-fdefinition-asts ast)
-  (let ((cleavir-cst-to-ast::*origin* nil))
+  (eliminate-fdefinition-asts ast)
+  (let ((cleavir-cst-to-ast::*origin* nil)
+        (*gensym-counter* 0))
     (multiple-value-bind (hoisted-ast load-time-value-count)
         (hoist-load-time-value ast)
       (let* ((wrapped-ast (make-instance 'cleavir-ast:function-ast
