23ceb46af2a306ef6d7d5a06c4c1acedb108dd9d
Robert Strandh <robert.strandh@gmail.com>
2021-09-24 17:22:41 +0200

    Allocate labels for additional successors of CATCH-INSTRUCTIONs.

--- Code/Compiler/Code-generation/labels.lisp
+++ Code/Compiler/Code-generation/labels.lisp
@@ -3,7 +3,11 @@
 (defun create-instruction-labels (instructions)
   (loop with table = (make-hash-table :test #'eq)
         for (instruction next) on instructions
-        do (when (or (typep next 'cleavir-ir:enter-instruction)
+        do (when (typep instruction 'cleavir-ir:catch-instruction)
+             (loop for successor in (rest (cleavir-ir:successors instruction))
+                   do (setf (gethash successor table)
+                            (make-instance 'cluster:label))))
+           (when (or (typep next 'cleavir-ir:enter-instruction)
                      (and (not (null next))
                           (let ((predecessors (cleavir-ir:predecessors next)))
                             (or (> (length predecessors) 1)
