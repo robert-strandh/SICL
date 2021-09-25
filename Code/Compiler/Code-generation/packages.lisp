6a9fa4e172be1236bb27da8eb9ee14b084a91b5e
Robert Strandh <robert.strandh@gmail.com>
2021-09-25 13:55:07 +0200

    Introduce package-local nicknames.

--- Code/Compiler/Code-generation/packages.lisp
+++ Code/Compiler/Code-generation/packages.lisp
@@ -2,4 +2,6 @@
 
 (defpackage #:sicl-code-generation
   (:use #:common-lisp)
+  (:local-nicknames (#:arr #:sicl-register-arrangement)
+                    (#:x86-64 #:sicl-x86-64-registers))
   (:export #:generate-code))
