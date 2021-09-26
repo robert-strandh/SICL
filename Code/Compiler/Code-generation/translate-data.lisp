a3afd1affa6a69b94dda4f1fdc5b486e28528b90
Robert Strandh <robert.strandh@gmail.com>
2021-09-26 12:11:21 +0200

    Make TRANSLATE-DATUM generic, and add method for EFFECTIVE-ADDRESS.

--- Code/Compiler/Code-generation/translate-data.lisp
+++ Code/Compiler/Code-generation/translate-data.lisp
@@ -1,9 +1,20 @@
 (cl:in-package #:sicl-code-generation)
 
-(defun translate-datum (datum)
-  (if (typep datum 'cleavir-ir:immediate-input)
-      (make-instance 'cluster:immediate-operand
-        :value (cleavir-ir:value datum))
-      (make-instance 'cluster:gpr-operand
-        :code-number (x86-64::register-number datum)
-        :size 64)))
+(defgeneric translate-datum (datum))
+
+(defmethod translate-datum ((datum cleavir-ir:immediate-input))
+  (make-instance 'cluster:immediate-operand
+    :value (cleavir-ir:value datum)))
+
+(defmethod translate-datum ((datum cleavir-ir:register-location))
+  (make-instance 'cluster:gpr-operand
+    :code-number (x86-64::register-number datum)
+    :size 64))
+
+(defmethod translate-datum ((datum sicl-ir:effective-address))
+  (make-instance 'cluster:memory-operand
+    :base-register (sicl-ir::base datum)
+    :index-register (sicl-ir::offset datum)
+    :scale (sicl-ir::scale datum)
+    :displacement (sicl-ir::displacement datum)
+    :size 64))
