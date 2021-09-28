6cf968a8fcbaed956a718acb7b3660aeb0978fd4
Robert Strandh <robert.strandh@gmail.com>
2021-09-28 11:52:29 +0200

    Handle regsiter offsets as well as constant offsets.

--- Code/Compiler/Code-generation/memory.lisp
+++ Code/Compiler/Code-generation/memory.lisp
@@ -26,32 +26,41 @@
     ((instruction cleavir-ir:memref2-instruction))
   (destructuring-bind (base-register-input offset-input)
       (cleavir-ir:inputs instruction)
-    ;; We only handle immediate offsets right now.
-    (assert (typep offset-input 'cleavir-ir:immediate-input))
     (make-instance 'cluster:code-command
       :mnemonic "MOV"
       :operands
       (list
        (translate-datum (first (cleavir-ir:outputs instruction)))
-       (make-instance 'cluster:memory-operand
-         :base-register (translate-datum base-register-input)
-         :displacement (cleavir-ir:value offset-input)
-         :size 64)))))
+       (if (typep offset-input 'cleavir-ir:immediate-input)
+           (make-instance 'cluster:memory-operand
+             :base-register (translate-datum base-register-input)
+             :displacement (cleavir-ir:value offset-input)
+             :size 64)
+           (make-instance 'cluster:memory-operand
+             :base-register (translate-datum base-register-input)
+             :index-register (translate-datum offset-input)
+             :scale 8
+             :size 64))))))
+
 
 (defmethod translate-simple-instruction
     ((instruction cleavir-ir:memset2-instruction))
   (destructuring-bind (base-register-input offset-input value-input)
       (cleavir-ir:inputs instruction)
-    ;; We only handle immediate offsets right now.
-    (assert (typep offset-input 'cleavir-ir:immediate-input))
     (make-instance 'cluster:code-command
       :mnemonic "MOV"
       :operands
       (list
-       (make-instance 'cluster:memory-operand
-         :base-register (translate-datum base-register-input)
-         :displacement (cleavir-ir:value offset-input)
-         :size 64)
+       (if (typep offset-input 'cleavir-ir:immediate-input)
+           (make-instance 'cluster:memory-operand
+             :base-register (translate-datum base-register-input)
+             :displacement (cleavir-ir:value offset-input)
+             :size 64)
+           (make-instance 'cluster:memory-operand
+             :base-register (translate-datum base-register-input)
+             :index-register (translate-datum offset-input)
+             :scale 8
+             :size 64))
        (translate-datum value-input)))))
 
 (defmethod translate-simple-instruction
