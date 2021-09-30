d93df5bb24e6c04209117784e88ff66450976299
Robert Strandh <robert.strandh@gmail.com>
2021-09-30 15:32:50 +0200

    Fix incorrect comment and incorrect third argument register.

--- Code/POSIX/Low/Linux/write.lisp
+++ Code/POSIX/Low/Linux/write.lisp
@@ -5,11 +5,11 @@
    ;; We are given the file descriptor as a fixnum in the first
    ;; argument, so we start by turning it into a native integer.
    (unbox-fixnum *rdi*)
-   ;; Set RDI to a pointer to the first element of the buffer.
+   ;; Set RSI to a pointer to the first element of the buffer.
    (rack-from-standard-object *rsi* *rsi*)
    (first-vector-element-from-rack *rsi*)
    ;; We are given the count as a fixnum, so unbox it.
-   (unbox-fixnum *rsi*)
+   (unbox-fixnum *rdx*)
    (syscall 1)
    (adjust-return-value)
    (return-to-caller)))
