23433a566934ca3be6dfd5b37b247b86e0b515a2
Robert Strandh <robert.strandh@gmail.com>
2021-10-01 06:18:10 +0200

    Signal appropriate conditions.

--- Code/POSIX/High/write.lisp
+++ Code/POSIX/High/write.lisp
@@ -4,7 +4,36 @@
   (let ((end (if (null end) (length vector) end)))
     (multiple-value-bind (effective-count error)
         (low:write file-descriptor vector start end)
-      ;; for now, don't handle errors
-      (assert (null error))
-      effective-count)))
-
+      (ecase error
+        (0 effective-count)
+        ((#.sicl-posix-low:+eagain+
+          #+(or)#.sicl-posix-low:+ewouldblock+)
+         (error 'write-would-block
+                :file-descriptor file-descriptor))
+        (#.sicl-posix-low:+ebadf+
+         (error 'invalid-file-descriptor
+                :file-descriptor file-descriptor))
+        (#.sicl-posix-low:+edestaddrreq+
+         (error 'destination-address-not-set
+                :file-descriptor file-descriptor))
+        (#.sicl-posix-low:+edquot+
+         (error 'write-exceeds-quota
+                :file-descriptor file-descriptor))
+        (#.sicl-posix-low:+efbig+
+         (error 'write-exceeds-max-size
+                :file-descriptor file-descriptor))
+        (#.sicl-posix-low:+einval+
+         (error 'invalid-value
+                :file-descriptor file-descriptor))
+        (#.sicl-posix-low:+eio+
+         (error 'low-level-io-error
+                :file-descriptor file-descriptor))
+        (#.sicl-posix-low:+enospc+
+         (error 'no-space-on-device
+                :file-descriptor file-descriptor))
+        (#.sicl-posix-low:+eperm+
+         (error 'file-sealed
+                :file-descriptor file-descriptor))
+        (#.sicl-posix-low:+epipe+
+         (error 'pipe-closed
+                :file-descriptor file-descriptor))))))
