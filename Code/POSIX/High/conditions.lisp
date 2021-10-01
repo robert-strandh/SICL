d8c766dd2c19c9aff699947f6679aad4b7e78e6c
Robert Strandh <robert.strandh@gmail.com>
2021-10-01 06:17:32 +0200

    Define conditions required by the write system call.

--- Code/POSIX/High/conditions.lisp
+++ Code/POSIX/High/conditions.lisp
@@ -1,1 +1,44 @@
 (cl:in-package #:sicl-posix-high)
+
+(define-condition file-descriptor-mixin ()
+  ((%file-descriptor :initarg :file-descriptor :reader file-descriptor)))
+
+(define-condition write-would-block
+    (error file-descriptor-mixin)
+  ())
+
+(define-condition invalid-file-descriptor
+    (error file-descriptor-mixin)
+  ())
+
+(define-condition destination-address-not-set
+    (error file-descriptor-mixin)
+  ())
+
+(define-condition write-exceeds-quota
+    (error file-descriptor-mixin)
+  ())
+
+(define-condition write-exceeds-max-size
+    (error file-descriptor-mixin)
+  ())
+
+(define-condition invalid-value
+    (error file-descriptor-mixin)
+  ())
+
+(define-condition low-level-io-error
+    (error file-descriptor-mixin)
+  ())
+
+(define-condition no-space-on-device
+    (error file-descriptor-mixin)
+  ())
+
+(define-condition file-sealed
+    (error file-descriptor-mixin)
+  ())
+
+(define-condition pipe-closed
+    (error file-descriptor-mixin)
+  ())
