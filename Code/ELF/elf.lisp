5fe843dfbcb33689effd3339cbc0946dd5eecc4f
Robert Strandh <robert.strandh@gmail.com>
2021-10-02 10:32:51 +0200

    Add more slots and use new simplified store functions.

--- Code/ELF/elf.lisp
+++ Code/ELF/elf.lisp
@@ -3,6 +3,16 @@
 (defparameter *file-class*
   '((1 . :32-bit) (2 . :64-bit)))
 
+(defun encode (keyword dictionary)
+  (let ((entry (rassoc keyword dictionary)))
+    (assert (not (null entry)))
+    (car entry)))
+
+(defun decode (code dictionary)
+  (let ((entry (assoc code dictionary)))
+    (assert (not (null entry)))
+    (cdr entry)))
+
 (defparameter *data-encoding*
   '((1 . :little-endian) (2 . :big-endian)))
 
@@ -30,6 +40,28 @@
     (#x11 . :cloudabi)
     (#x12 . :stratus-technologies-openvos)))
 
+(defparameter *file-type*
+  '((#x00 . :no-type)
+    (#x01 . :relocatable)
+    (#x02 . :executable)
+    (#x03 . :shared-object)
+    (#x04 . :core)
+    (#xfe00 . :low-os)
+    (#xfeff . :high-os)
+    (#xff00 . :low-process)
+    (#xffff . :high-process)))
+
+(defparameter *machine*
+  '((#x00 . :no-specific-instruction-set)
+    ;; etc.
+    (#x02 . :sparc)
+    ;; etc.
+    (#x3e . :amd-x86-64)
+    ;; etc.
+    (#xf3 . :risc-v)
+    ;; etc.
+    ))
+
 (defclass elf ()
   ((%file-class
     :initarg :file-class
@@ -45,6 +77,34 @@
     :accessor os/abi-identification)
    (%abi-version
     :initarg :abi-version
-    :accessor abi-version))
-  
-   
+    :accessor abi-version)
+   (%file-type
+    :initarg :file-type
+    :accessor file-type)
+   (%machine
+    :initarg :machine
+    :accessor machine)
+   (%entry-point-address
+    :initarg :entry-point-address
+    :accessor entry-point-address)))
+
+(defun store (elf)
+  (let* ((bytes (make-array 100 :element-type '(unsigned-byte 8)))
+         (pos (make-instance 'vector-position :bytes bytes))
+         (encoding (data-encoding elf)))
+    (store-byte 0 pos)
+    (store-byte (char-code #\E) pos)
+    (store-byte (char-code #\L) pos)
+    (store-byte (char-code #\F) pos)
+    (store-byte (encode (file-class elf) *file-class*) pos)
+    (store-byte (encode encoding *data-encoding*) pos)
+    (store-byte (encode (file-version elf) *file-version*) pos)
+    (store-byte (encode (os/abi-identification elf) *os/abi-identification*) pos)
+    (store-byte (abi-version elf) pos)
+    (loop repeat 8 do (store-byte 0 pos))
+    (store-value (encode (file-type elf) *file-type*) 16 pos encoding)
+    (store-value (encode (machine elf) *machine*) 16 pos encoding)
+    (store-value (encode (file-version elf) *file-version*) 32 pos encoding)
+    (store-value (entry-point-address elf) 64 pos encoding)
+    bytes))
+    
