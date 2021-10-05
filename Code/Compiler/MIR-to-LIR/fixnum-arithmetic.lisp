0cc379cdc70abf457913175d9971a595b412ba72
Hayley Patton <theemacsshibe@gmail.com>
2021-10-05 15:53:43 +1100

    Make sure the dividend of a FIXNUM-DIVIDE instruction is in RAX, and zero out RDX too.

--- Code/Compiler/MIR-to-LIR/fixnum-arithmetic.lisp
+++ Code/Compiler/MIR-to-LIR/fixnum-arithmetic.lisp
@@ -12,6 +12,7 @@
     ((instruction cleavir-ir:fixnum-multiply-instruction))
   (destructuring-bind (input1 input2)
       (cleavir-ir:inputs instruction)
+    ;; Make sure the first input is in RAX.
     (unless (eq input1 x86-64:*rax*)
       (cleavir-ir:insert-instruction-before
        (make-instance 'cleavir-ir:assignment-instruction
@@ -41,6 +42,24 @@
 
 (defmethod finish-lir-for-instruction
     ((instruction cleavir-ir:fixnum-divide-instruction))
+  ;; Make sure the dividend is in RAX.
+  (destructuring-bind (dividend divisor)
+      (cleavir-ir:inputs instruction)
+    (unless (eq dividend x86-64:*rax*)
+      (cleavir-ir:insert-instruction-before
+       (make-instance 'cleavir-ir:assignment-instruction
+         :inputs (list dividend)
+         :outputs (list x86-64:*rax*))
+       instruction)
+      (setf (cleavir-ir:inputs instruction)
+            (list x86-64:*rax* divisor))))
+  ;; Zero out RDX before performing the division, as it is interpreted
+  ;; as the high 64 bits of a 128-bit dividend.
+  (cleavir-ir:insert-instruction-before
+   (make-instance 'cleavir-ir:assignment-instruction
+     :inputs (list (cleavir-ir:make-immediate-input 0))
+     :outputs (list x86-64:*rdx*))
+   instruction)
   (let ((quotient (first (cleavir-ir:outputs instruction))))
     (cleavir-ir:insert-instruction-after
      (make-instance 'cleavir-ir:shift-left-instruction
