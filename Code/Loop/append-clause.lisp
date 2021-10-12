b998413701eda08e278f0973204072dcfd856d6e
Robert Strandh <robert.strandh@gmail.com>
2021-10-12 11:54:23 +0200

    Update BODY-FORM to be more conforming.

--- Code/Loop/append-clause.lisp
+++ Code/Loop/append-clause.lisp
@@ -76,15 +76,61 @@
 
 (defmethod body-form ((clause append-form-clause) end-tag)
   (declare (ignore end-tag))
-  `(if (null ,*list-tail-accumulation-variable*)
-       (progn (setq ,*accumulation-variable*
-                    (copy-list ,(form clause)))
-              (setq ,*list-tail-accumulation-variable*
-                    (last ,*accumulation-variable*)))
-       (progn (rplacd ,*list-tail-accumulation-variable*
-                      (copy-list ,(form clause)))
-              (setq ,*list-tail-accumulation-variable*
-                    (last ,*list-tail-accumulation-variable*)))))
+  `(if (null ,*accumulation-variable*)
+       ;; If the accumulation variable is NIL, then so is the tail
+       ;; variable.  We leave the tail variable as NIL so as to
+       ;; indicate that every CONS cell in the list starting at the
+       ;; accumulation variable must be copied whenever yet more CONS
+       ;; cells are attached at the end.
+       (setq ,*accumulation-variable* ,(form clause))
+       ;; If the accumulation variable is not NIL, then the tail
+       ;; variable may or may not be NIL.
+       (progn 
+         ;; If the tail variable is NIL, then every CONS cell in the
+         ;; list starting at the accumulation variable must be copied,
+         ;; and we know that there is at least one.  So we can
+         ;; eliminate this special case by copying the first CONS
+         ;; cell, and setting the tail variable to point to it.  We
+         ;; could call COPY-LIST and then LAST, but then we would
+         ;; traverse the list twice, so we do it with a loop instead.
+         (when (null ,*list-tail-accumulation-variable*)
+           (setf ,*accumulation-variable*
+                 (cons (car ,*accumulation-variable*)
+                       (cdr ,*accumulation-variable*)))
+           (setf ,*list-tail-accumulation-variable*
+                 ,*accumulation-variable*))
+         ;; Now, whether the tail variable was initially NIL or not,
+         ;; now it no longer is.  And every CONS cell after the one
+         ;; that the tail variable points to must be copied.
+         (tagbody
+          again
+            (if (atom (cdr ,*list-tail-accumulation-variable*))
+                ;; We have copied all the CONS cells that had to be
+                ;; copied.
+                (go out)
+                ;; Otherwise, we copy the CONS cell pointed to by the
+                ;; CDR of the tail variable and advance the tail
+                ;; variable by one position.
+                (progn (setf (cdr ,*list-tail-accumulation-variable*)
+                             (cons (cadr ,*list-tail-accumulation-variable*)
+                                   (cddr ,*list-tail-accumulation-variable*)))
+                       (setf ,*list-tail-accumulation-variable*
+                             (cdr ,*list-tail-accumulation-variable*))
+                       (go again)))
+          out)
+         ;; When we come here, every CONS cell after the one that the
+         ;; tail variable points to has been copied, and the tail
+         ;; variable points to the last CONS cell in the list.  It
+         ;; remains to attach the new list to the end.  And we leave
+         ;; the tail variable where it is, indicating that the CONS
+         ;; cells of the newly attached list must be copied whenever
+         ;; yet more cells are attached at the end.
+         (if (null (cdr ,*list-tail-accumulation-variable*))
+             (setf (cdr ,*list-tail-accumulation-variable*)
+                   ,(form clause))
+             (error 'type-error
+                    :datum (cdr ,*list-tail-accumulation-variable*)
+                    :expected-type 'null)))))
 
 (defmethod body-form ((clause append-form-into-clause) end-tag)
   (declare (ignore end-tag))
--- Code/Loop/collect-clause.lisp
+++ Code/Loop/collect-clause.lisp
@@ -76,15 +76,64 @@
 
 (defmethod body-form ((clause collect-form-clause) end-tag)
   (declare (ignore end-tag))
-  `(if (null ,*list-tail-accumulation-variable*)
+  `(if (null ,*accumulation-variable*)
+       ;; If the accumulation variable is NIL, then so is the tail
+       ;; variable.  Then just allocate a new CONS cell containing the
+       ;; value of the form, and set bot the accumulation variable and
+       ;; the tail variable to point to it.
        (progn (setq ,*list-tail-accumulation-variable*
-                    (list ,(form clause)))
+                    (cons ,(form clause) nil))
               (setq ,*accumulation-variable*
                     ,*list-tail-accumulation-variable*))
-       (progn (rplacd ,*list-tail-accumulation-variable*
-                      (list ,(form clause)))
-              (setq ,*list-tail-accumulation-variable*
-                    (cdr ,*list-tail-accumulation-variable*)))))
+       ;; If the accumulation variable is not NIL, then the tail
+       ;; variable may or may not be NIL.
+       (progn 
+         ;; If the tail variable is NIL, then every CONS cell in the
+         ;; list starting at the accumulation variable must be copied,
+         ;; and we know that there is at least one.  So we can
+         ;; eliminate this special case by copying the first CONS
+         ;; cell, and setting the tail variable to point to it.  We
+         ;; could call COPY-LIST and then LAST, but then we would
+         ;; traverse the list twice, so we do it with a loop instead.
+         (when (null ,*list-tail-accumulation-variable*)
+           (setf ,*accumulation-variable*
+                 (cons (car ,*accumulation-variable*)
+                       (cdr ,*accumulation-variable*)))
+           (setf ,*list-tail-accumulation-variable*
+                 ,*accumulation-variable*))
+         ;; Now, whether the tail variable was initially NIL or not,
+         ;; now it no longer is.  And every CONS cell after the one
+         ;; that the tail variable points to must be copied.
+         (tagbody
+          again
+            (if (atom (cdr ,*list-tail-accumulation-variable*))
+                ;; We have copied all the CONS cells that had to be
+                ;; copied.
+                (go out)
+                ;; Otherwise, we copy the CONS cell pointed to by the
+                ;; CDR of the tail variable and advance the tail
+                ;; variable by one position.
+                (progn (setf (cdr ,*list-tail-accumulation-variable*)
+                             (cons (cadr ,*list-tail-accumulation-variable*)
+                                   (cddr ,*list-tail-accumulation-variable*)))
+                       (setf ,*list-tail-accumulation-variable*
+                             (cdr ,*list-tail-accumulation-variable*))
+                       (go again)))
+          out)
+         ;; When we come here, every CONS cell after the one that the
+         ;; tail variable points to has been copied, and the tail
+         ;; variable points to the last CONS cell in the list.  It
+         ;; remains to attach a new CONS cell containing the value of
+         ;; the form and to advance the tail variable to point to that
+         ;; new CONS cell.
+         (if (null (cdr ,*list-tail-accumulation-variable*))
+             (progn (setf (cdr ,*list-tail-accumulation-variable*)
+                          (cons ,(form clause) nil))
+                    (setf ,*list-tail-accumulation-variable*
+                          (cdr ,*list-tail-accumulation-variable*)))
+             (error 'type-error
+                    :datum (cdr ,*list-tail-accumulation-variable*)
+                    :expected-type 'null)))))
 
 (defmethod body-form ((clause collect-form-into-clause) end-tag)
   (declare (ignore end-tag))
