54b8b84b5f985de86ea5cc498d0676bebca6bbb4
Robert Strandh <robert.strandh@gmail.com>
2021-10-12 14:30:02 +0200

    Replace duplicate code by call to utility function COPY-CONS-CELLS.

--- Code/Loop/collect-clause.lisp
+++ Code/Loop/collect-clause.lisp
@@ -88,38 +88,8 @@
        ;; If the accumulation variable is not NIL, then the tail
        ;; variable may or may not be NIL.
        (progn
-         ;; If the tail variable is NIL, then every CONS cell in the
-         ;; list starting at the accumulation variable must be copied,
-         ;; and we know that there is at least one.  So we can
-         ;; eliminate this special case by copying the first CONS
-         ;; cell, and setting the tail variable to point to it.  We
-         ;; could call COPY-LIST and then LAST, but then we would
-         ;; traverse the list twice, so we do it with a loop instead.
-         (when (null ,*list-tail-accumulation-variable*)
-           (setf ,*accumulation-variable*
-                 (cons (car ,*accumulation-variable*)
-                       (cdr ,*accumulation-variable*)))
-           (setf ,*list-tail-accumulation-variable*
-                 ,*accumulation-variable*))
-         ;; Now, whether the tail variable was initially NIL or not,
-         ;; now it no longer is.  And every CONS cell after the one
-         ;; that the tail variable points to must be copied.
-         (tagbody
-          again
-            (if (atom (cdr ,*list-tail-accumulation-variable*))
-                ;; We have copied all the CONS cells that had to be
-                ;; copied.
-                (go out)
-                ;; Otherwise, we copy the CONS cell pointed to by the
-                ;; CDR of the tail variable and advance the tail
-                ;; variable by one position.
-                (progn (setf (cdr ,*list-tail-accumulation-variable*)
-                             (cons (cadr ,*list-tail-accumulation-variable*)
-                                   (cddr ,*list-tail-accumulation-variable*)))
-                       (setf ,*list-tail-accumulation-variable*
-                             (cdr ,*list-tail-accumulation-variable*))
-                       (go again)))
-          out)
+         ,(copy-cons-cells
+           *accumulation-variable* *list-tail-accumulation-variable*)
          ;; When we come here, every CONS cell after the one that the
          ;; tail variable points to has been copied, and the tail
          ;; variable points to the last CONS cell in the list.  It
