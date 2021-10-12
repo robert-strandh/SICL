8038f0aac85af72f322a7cc81d8d4845cb3b1343
Robert Strandh <robert.strandh@gmail.com>
2021-10-12 14:29:03 +0200

    Define utility function COPY-CONS-CELLS.

--- Code/Loop/utilities.lisp
+++ Code/Loop/utilities.lisp
@@ -3,8 +3,8 @@
 ;;; Loop keywords are symbols, but they are not recognized by symbol
 ;;; identity as is usually the case, but instead by their names.  The
 ;;; HyperSpec doesn't say what function to use for comparing the
-;;; names.  We assume string= here, meaning that the names are case 
-;;; sensitive. 
+;;; names.  We assume string= here, meaning that the names are case
+;;; sensitive.
 (defun symbol-equal (symbol1 symbol2)
   (and (symbolp symbol1)
        (symbolp symbol2)
@@ -88,3 +88,50 @@
                       (extract-aux (cdr d-var-spec) (cdr d-type-spec))))))
       (extract-aux d-var-spec d-type-spec)
       result)))
+
+;;; This function is used in the list accumulation clauses COLLECT,
+;;; APPEND, and NCONC.  The idea is that CONS cells in a suffix of the
+;;; accumulated list must be copied, because they were attached by the
+;;; APPEND clause, and so they weren't copied then, but if more CONS
+;;; cells must be attached, then they do have to be copied in order
+;;; that semantics be preserved.  When LIST-TAIL-ACCUMULATION-VARIABLE
+;;; points to a CONS cell (say, C), then this suffix consists of all
+;;; the CONS cells in the list pointed to by the CDR of C.  When the
+;;; value of LIST-TAIL-ACCUMULATION-VARIABLE is NIL, then the suffix
+;;; consists of all the CONS cell accumulated so far, and they make up
+;;; the list that is the value of ACCUMULATION-VARIABLE.
+(defun copy-cons-cells
+    (accumulation-variable list-tail-accumulation-variable)
+  `(progn
+     ;; If the tail variable is NIL, then every CONS cell in the
+     ;; list starting at the accumulation variable must be copied,
+     ;; and we know that there is at least one.  So we can
+     ;; eliminate this special case by copying the first CONS
+     ;; cell, and setting the tail variable to point to it.  We
+     ;; could call COPY-LIST and then LAST, but then we would
+     ;; traverse the list twice, so we do it with a loop instead.
+     (when (null ,list-tail-accumulation-variable)
+       (setf ,accumulation-variable
+             (cons (car ,accumulation-variable)
+                   (cdr ,accumulation-variable)))
+       (setf ,list-tail-accumulation-variable
+             ,accumulation-variable))
+     ;; Now, whether the tail variable was initially NIL or not,
+     ;; now it no longer is.  And every CONS cell after the one
+     ;; that the tail variable points to must be copied.
+     (tagbody
+      again
+        (if (atom (cdr ,list-tail-accumulation-variable))
+            ;; We have copied all the CONS cells that had to be
+            ;; copied.
+            (go out)
+            ;; Otherwise, we copy the CONS cell pointed to by the
+            ;; CDR of the tail variable and advance the tail
+            ;; variable by one position.
+            (progn (setf (cdr ,list-tail-accumulation-variable)
+                         (cons (cadr ,list-tail-accumulation-variable)
+                               (cddr ,list-tail-accumulation-variable)))
+                   (setf ,list-tail-accumulation-variable
+                         (cdr ,list-tail-accumulation-variable))
+                   (go again)))
+      out)))
