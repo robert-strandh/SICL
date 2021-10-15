4413380440f672253428b85c1643656f84977a8a
Moonchild <elronnd@elronnd.net>
2021-10-14 22:22:42 -0700

    Disallow creating characters with surrogate codepoints

--- Code/Character/code-char-defun.lisp
+++ Code/Character/code-char-defun.lisp
@@ -1,8 +1,6 @@
 (cl:in-package #:sicl-character)
 
 (defun code-char (code)
-  (if (typep code '(integer 0 #xffffff))
-      (cleavir-primop:code-char code)
-      (error 'type-error
-             :datum code
-             :expected-type '(integer 0 #xffffff))))
+  (check-type code `(integer 0 ,(1- char-code-limit)))
+  (and (not (<= #xd800 code #xdfff))
+       (cleavir-primop:code-char code)))
