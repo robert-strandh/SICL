541fb78e8f72686f3b2aa0052c641cb149666d33
Robert Strandh <robert.strandh@gmail.com>
2021-10-16 14:48:44 +0200

    Add method on trace-prefix for unspecialized arrays.

--- Code/Boot/pointer2.lisp
+++ Code/Boot/pointer2.lisp
@@ -12,6 +12,16 @@
 ;;; This method is invoked for most standard objects.  In such an
 ;;; object every word in the rack contains a Lisp object, so we return
 ;;; the instance size of the class, plus 2 for the stamp and the list
-;;; of effective slots.
+;;; of effective slots.  This method is also used for specialized
+;;; arrays, because the instance size of a specialized array reflects
+;;; the rack prefix that contains Lisp objects.
 (defmethod trace-prefix ((object standard-object))
   (+ (sicl-clos:instance-size (class-of object)) 2))
+
+;;; This method is invoked for unspecialized arrays, including simple
+;;; vectors.  For such objects, every word in the rack contains a Lisp
+;;; object.
+(defmethod trace-prefix ((object sicl-array:array-t))
+  (+ (sicl-clos:instance-size (class-of object))
+     (array-total-size object)
+     2))
