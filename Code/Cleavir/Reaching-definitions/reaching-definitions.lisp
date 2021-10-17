83557ff4ada5ab441b8e8fb8932502b1f6def1ed
Robert Strandh <robert.strandh@gmail.com>
2021-10-17 15:03:55 +0200

    Untabify and remove redundant blank lines.

--- Code/Cleavir/Reaching-definitions/reaching-definitions.lisp
+++ Code/Cleavir/Reaching-definitions/reaching-definitions.lisp
@@ -17,19 +17,18 @@
 ;;; by fixpoint iteration, starting from the initial node and
 ;;; following successors until a fixpoint is reached.  
 
-
 (defun same-set-p (set1 set2)
   (and (subsetp set1 set2 :test #'eq)
        (subsetp set2 set1 :test #'eq)))
 
 (defun b-to-a (node btable atable definitions)
   (let* ((before (gethash node btable))
-	 (stripped (remove-if (lambda (x)
-				(member (cdr x) definitions
-					:test #'eq :key #'cdr))
-			      before)))
+         (stripped (remove-if (lambda (x)
+                                (member (cdr x) definitions
+                                        :test #'eq :key #'cdr))
+                              before)))
     (setf (gethash node atable)
-	  (append definitions stripped))))
+          (append definitions stripped))))
 
 ;;; This function can handle any concept of NODE and VARIABLE as long
 ;;; as they can be compared with EQ.
@@ -38,39 +37,39 @@
 ;;; value is a list of definitions that reach that node.
 (defun reaching-definitions (start-node successor-fun out-fun)
   (let ((predecessor-fun (cleavir-utilities:predecessor-function
-			   start-node successor-fun)))
+                           start-node successor-fun)))
     (flet ((successors (node)
-	     (funcall successor-fun node))
-	   (predecessors (node)
-	     (funcall predecessor-fun node))
-	   (outputs (node)
-	     (funcall out-fun node)))
+             (funcall successor-fun node))
+           (predecessors (node)
+             (funcall predecessor-fun node))
+           (outputs (node)
+             (funcall out-fun node)))
       (let ((definitions (make-hash-table :test #'eq)))
-	;; Collect all definitions.  We create a definition once, so
-	;; that even though it is a CONS cell, we can compare two
-	;; defintions using EQ.
-	(cleavir-utilities:map-nodes
-	 start-node successor-fun
-	 (lambda (node)
-	   (loop for out in (outputs node)
-		 do (push (cons node out)
-			  (gethash node definitions)))))
-	(flet ((definitions (node)
-		 (gethash node definitions)))
-	  (let ((btable (make-hash-table :test #'eq))
-		(atable (make-hash-table :test #'eq)))
-	    (labels ((traverse (node)
-		       (multiple-value-bind (value present-p)
-			   (gethash node btable)
-			 (let* ((new (apply
-				      #'concatenate 'list
-				      (mapcar (lambda (n) (gethash n atable))
-					      (predecessors node))))
-				(unique (remove-duplicates new :test #'eq)))
-			   (when (or (not present-p)
-				     (not (same-set-p value unique)))
-			     (setf (gethash node btable) unique)
-			     (b-to-a node btable atable (definitions node))
-			     (mapc #'traverse (successors node)))))))
-	      (traverse start-node))
-	    btable))))))
+        ;; Collect all definitions.  We create a definition once, so
+        ;; that even though it is a CONS cell, we can compare two
+        ;; defintions using EQ.
+        (cleavir-utilities:map-nodes
+         start-node successor-fun
+         (lambda (node)
+           (loop for out in (outputs node)
+                 do (push (cons node out)
+                          (gethash node definitions)))))
+        (flet ((definitions (node)
+                 (gethash node definitions)))
+          (let ((btable (make-hash-table :test #'eq))
+                (atable (make-hash-table :test #'eq)))
+            (labels ((traverse (node)
+                       (multiple-value-bind (value present-p)
+                           (gethash node btable)
+                         (let* ((new (apply
+                                      #'concatenate 'list
+                                      (mapcar (lambda (n) (gethash n atable))
+                                              (predecessors node))))
+                                (unique (remove-duplicates new :test #'eq)))
+                           (when (or (not present-p)
+                                     (not (same-set-p value unique)))
+                             (setf (gethash node btable) unique)
+                             (b-to-a node btable atable (definitions node))
+                             (mapc #'traverse (successors node)))))))
+              (traverse start-node))
+            btable))))))
--- Code/Cleavir/Reaching-definitions/test-reaching-definitions.lisp
+++ Code/Cleavir/Reaching-definitions/test-reaching-definitions.lisp
@@ -17,51 +17,49 @@
 (defun nodes-reached-by-definition (node var successor-fun output-fun)
   (let ((table (make-hash-table :test #'eq)))
     (labels ((traverse (node path)
-	       (if (member node path :test #'eq)
-		   nil
-		   (unless (gethash node table)
-		     (setf (gethash node table) t)
-		     (unless (member var (funcall output-fun node)
-				     :test #'eq)
-		       (loop for succ in (funcall successor-fun node)
-			     do (traverse succ (cons node path))))))))
+               (if (member node path :test #'eq)
+                   nil
+                   (unless (gethash node table)
+                     (setf (gethash node table) t)
+                     (unless (member var (funcall output-fun node)
+                                     :test #'eq)
+                       (loop for succ in (funcall successor-fun node)
+                             do (traverse succ (cons node path))))))))
       (loop for succ in (funcall successor-fun node)
-	    do (traverse succ '())))
+            do (traverse succ '())))
     table))
-		 
+                 
 (defun test-reaching-definitions-on-one-graph
     (start-node successor-fun output-fun)
   (let ((reaching-definitions
-	  (cleavir-reaching-definitions:reaching-definitions
-	   start-node successor-fun output-fun)))
+          (cleavir-reaching-definitions:reaching-definitions
+           start-node successor-fun output-fun)))
     (cleavir-utilities:map-nodes
      start-node
      successor-fun
      (lambda (node)
        (loop for var in (funcall output-fun node)
-	     do (let ((nodes-reached (nodes-reached-by-definition
-				      node var successor-fun output-fun)))
-		  (loop for n being each hash-key of nodes-reached
-			do (assert (member (cons node var)
-					   (gethash n reaching-definitions)
-					   :test #'equal)))
-		  (loop for n being each hash-key of reaching-definitions
-			do (if (gethash n nodes-reached)
-			       (assert (member
-					(cons node var)
-					(gethash n reaching-definitions)
-					:test #'equal))
-			       (assert (not (member
-					     (cons node var)
-					     (gethash n reaching-definitions)
-					     :test #'equal)))))))))))
+             do (let ((nodes-reached (nodes-reached-by-definition
+                                      node var successor-fun output-fun)))
+                  (loop for n being each hash-key of nodes-reached
+                        do (assert (member (cons node var)
+                                           (gethash n reaching-definitions)
+                                           :test #'equal)))
+                  (loop for n being each hash-key of reaching-definitions
+                        do (if (gethash n nodes-reached)
+                               (assert (member
+                                        (cons node var)
+                                        (gethash n reaching-definitions)
+                                        :test #'equal))
+                               (assert (not (member
+                                             (cons node var)
+                                             (gethash n reaching-definitions)
+                                             :test #'equal)))))))))))
 
 (defun test-reaching-definitions (&optional (n 10000))
   (loop repeat n
-	do (multiple-value-bind (start-node successor-fun in-fun out-fun)
-	       (cleavir-test-utilities:random-flow-chart)
-	     (declare (ignore in-fun))
-	     (test-reaching-definitions-on-one-graph
-	      start-node successor-fun out-fun))))
-	     
-  
+        do (multiple-value-bind (start-node successor-fun in-fun out-fun)
+               (cleavir-test-utilities:random-flow-chart)
+             (declare (ignore in-fun))
+             (test-reaching-definitions-on-one-graph
+              start-node successor-fun out-fun))))
