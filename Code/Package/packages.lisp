b3e45b7f704f9ef98e3d5ae012272fec917050f5
Robert Strandh <robert.strandh@gmail.com>
2021-10-20 15:35:32 +0200

    Do not shadow symbols naming standard functions.

--- Code/Package/packages.lisp
+++ Code/Package/packages.lisp
@@ -2,7 +2,4 @@
 
 (defpackage sicl-package
   (:use #:common-lisp)
-  (:shadow #:find-package
-           #:delete-package
-           #:make-symbol)
   (:export . #.asdf-user:*sicl-package-string-designators*))
