5fae0e5a36b97f681e2e8c8b33c18c0d6850930e
Robert Strandh <robert.strandh@gmail.com>
2021-10-21 16:30:12 +0200

    Load system CLOSER-MOP into E5.

--- Code/Boot/Phase-6/boot.lisp
+++ Code/Boot/Phase-6/boot.lisp
@@ -92,4 +92,5 @@
     (sicl-boot:import-functions-from-host '(get-universal-time) e5)
     (load-asdf-system '#:sicl-random-intrinsic e5)
     (setf (env:special-variable (env:client e5) e5 '*features* t)
-          '(:sicl))))
+          '(:sicl))
+    (load-asdf-system '#:closer-mop e5)))
