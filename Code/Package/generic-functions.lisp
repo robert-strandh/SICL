ae4604e27b86c955d7a27d7d0ed0808a7404a83a
Robert Strandh <robert.strandh@gmail.com>
2021-10-24 14:30:49 +0200

    Remove obsolete definitions of generic functions.

--- Code/Package/generic-functions.lisp
+++ Code/Package/generic-functions.lisp
@@ -1,9 +1,5 @@
 (cl:in-package #:sicl-package)
 
-(defgeneric home-package (symbol))
-
 (defgeneric (setf home-package) (new-home-package symbol))
-
-(defgeneric make-symbol (name))
 
 (defgeneric delete-package (package-designator))
