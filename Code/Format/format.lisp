3121743f450175e0c691dfb4e0e59c73832155e4
Robert Strandh <robert.strandh@gmail.com>
2021-10-25 18:01:36 +0200

    Turn comments into complete sentences.

--- Code/Format/format.lisp
+++ Code/Format/format.lisp
@@ -1659,27 +1659,27 @@
 
 (define-format-directive-interpreter newline-directive
   (cond (colonp
-         ;; remove the newline but print the following whitespace
+         ;; Remove the newline but print the following whitespace.
          (let ((start (1+ (position #\Newline control-string :start start))))
            (loop for index from start below end
                  do (write-char (char control-string index) *destination*))))
         (at-signp
-         ;; print the newline, but remove the following whitespace
+         ;; Print the newline, but remove the following whitespace.
          (write-char #\Newline *destination*))
         (t
-         ;; ignore both the newline and the following whitespace
+         ;; Ignore both the newline and the following whitespace.
          nil)))
 
 (define-format-directive-compiler newline-directive
   (cond (colonp
-         ;; remove the newline but print the following whitespace
+         ;; Remove the newline but print the following whitespace.
          `(let ((start (1+ (position #\Newline control-string :start start))))
             (write-string ,(subseq control-string start end) *destination*)))
         (at-signp
-         ;; print the newline, but remove the following whitespace
+         ;; Print the newline, but remove the following whitespace.
          `(write-char #\Newline *destination*))
         (t
-         ;; ignore both the newline and the following whitespace
+         ;; Ignore both the newline and the following whitespace.
          nil)))
 
 ;;; The reason we define this function is that the ~? directive
