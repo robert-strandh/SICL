062146d6c43418c8204ab0ec090634641fd36d1a
Robert Strandh <robert.strandh@gmail.com>
2021-11-06 10:12:02 +0100

    Add comment explaining the purpose of eliminating FDEFINITIION-ASTs.

--- Code/Compiler/AST-to-HIR/eliminate-fdefinition-asts.lisp
+++ Code/Compiler/AST-to-HIR/eliminate-fdefinition-asts.lisp
@@ -9,6 +9,19 @@
      ast)
     result))
 
+;;; when CST-to-AST detects a global function reference in a FUNCTION
+;;; special form, it creates an FDEFINITION-AST containing a
+;;; LITERAL-AST in the NAME-AST slot.  As source code, it would look
+;;; like (FDEFINITION 'NAME).  We change that so that it, as source
+;;; code, looks like (CAR (LOAD-TIME-VALUE (FUNCTION-CELL 'NAME))).
+;;; In other words, the function cell is requested from the
+;;; environment at load time and then becomes a literal value at run
+;;; time.  So at run time, only a CAR-INSTRUCTION is executed in order
+;;; to obtain the function object.
+;;;
+;;; Notice that the FDEFINITION-AST is not created as a result of a
+;;; global function reference in a call position of a compound form.
+;;; Then, instead a NAMED-CALL-AST is created.
 (defun eliminate-fdefinition-asts (ast)
   (let ((fdefinition-asts (find-fdefinition-asts ast)))
     (loop for ast in fdefinition-asts
