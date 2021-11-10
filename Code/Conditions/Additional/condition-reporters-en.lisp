a6c4ca133d16e0e210776cf5ee3c01641701e2b2
Robert Strandh <robert.strandh@gmail.com>
2021-11-10 15:57:55 +0100

    Remove copyright notice.

--- Code/Conditions/Additional/condition-reporters-en.lisp
+++ Code/Conditions/Additional/condition-reporters-en.lisp
@@ -1,17 +1,5 @@
 (cl:in-package #:sicl-additional-conditions)
 
-;;;; Copyright (c) 2008, 2009, 2010, 2012, 2015
-;;;;
-;;;;     Robert Strandh (robert.strandh@gmail.com)
-;;;;
-;;;; all rights reserved. 
-;;;;
-;;;; Permission is hereby granted to use this software for any 
-;;;; purpose, including using, modifying, and redistributing it.
-;;;;
-;;;; The software is provided "as-is" with no warranty.  The user of
-;;;; this software assumes any responsibility of the consequences. 
-
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;
 ;;; Runtime conditions.
