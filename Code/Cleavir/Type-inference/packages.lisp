4b0b73276945e4ba73b72c87d230a98f671ec444
Robert Strandh <robert.strandh@gmail.com>
2021-11-12 16:53:18 +0100

    Untabify and remove unnecessary :EXPORT options.

--- Code/Cleavir/Type-inference/packages.lisp
+++ Code/Cleavir/Type-inference/packages.lisp
@@ -3,18 +3,18 @@
 (defpackage #:cleavir-type-inference
   (:use #:common-lisp)
   (:export #:approximate-type #:canonicalize-type
-	   #:top-p #:bottom-p
-	   #:binary-join #:binary-meet #:difference
-	   #:join #:meet
-	   #:descriptor-box #:descriptor-unbox)
-  (:export #:approximate-values #:values-nth #:values-rest-p
-	   #:values-top-p #:values-bottom-p
-	   #:values-required #:values-required-count
-	   #:values-binary-meet #:values-binary-join
-	   #:values-meet #:values-join
-	   #:values-top #:values-bottom
-	   #:values-descriptor->type)
-  (:export #:infer-types #:arc-bag
-	   #:instruction-input #:find-type)
-  (:export #:type-missing
-	   #:type-missing-location #:type-missing-bag))
+           #:top-p #:bottom-p
+           #:binary-join #:binary-meet #:difference
+           #:join #:meet
+           #:descriptor-box #:descriptor-unbox
+           #:approximate-values #:values-nth #:values-rest-p
+           #:values-top-p #:values-bottom-p
+           #:values-required #:values-required-count
+           #:values-binary-meet #:values-binary-join
+           #:values-meet #:values-join
+           #:values-top #:values-bottom
+           #:values-descriptor->type
+           #:infer-types #:arc-bag
+           #:instruction-input #:find-type
+           #:type-missing
+           #:type-missing-location #:type-missing-bag))
