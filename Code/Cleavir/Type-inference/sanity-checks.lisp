d25f905e8074ae0a370b3719ee1fde71debaa977
Robert Strandh <robert.strandh@gmail.com>
2021-11-12 16:53:47 +0100

    Untabify and improve comment.

--- Code/Cleavir/Type-inference/sanity-checks.lisp
+++ Code/Cleavir/Type-inference/sanity-checks.lisp
@@ -1,13 +1,13 @@
 (cl:in-package #:cleavir-type-inference)
 
-;;; things we track.
+;;; Things we track.
 (deftype typed-location ()
   '(or cleavir-ir:lexical-location cleavir-ir:values-location))
 
 (defun valid-bag-p (bag)
   (and (every (lambda (restriction)
-		(and (consp restriction)
-		     (typep (first restriction) 'typed-location)))
-	      bag)
+                (and (consp restriction)
+                     (typep (first restriction) 'typed-location)))
+              bag)
        (= (length bag)
-	  (length (remove-duplicates bag :test #'eq :key #'first)))))
+          (length (remove-duplicates bag :test #'eq :key #'first)))))
