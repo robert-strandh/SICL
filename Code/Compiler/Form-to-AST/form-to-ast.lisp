f015a42088857d2ba61c590e7128ef8c68a81778
Robert Strandh <robert.strandh@gmail.com>
2021-11-15 13:54:55 +0100

    Add keyword parameter FILE-COMPILATION-SEMANTICS.

--- Code/Compiler/Form-to-AST/form-to-ast.lisp
+++ Code/Compiler/Form-to-AST/form-to-ast.lisp
@@ -1,5 +1,7 @@
 (cl:in-package #:sicl-form-to-ast)
 
-(defun form-to-ast (client form environment)
+(defun form-to-ast (client form environment &key file-compilation-semantics)
   (let ((cst (cst:cst-from-expression form)))
-    (cleavir-cst-to-ast:cst-to-ast client cst environment)))
+    (cleavir-cst-to-ast:cst-to-ast
+     client cst environment
+     :file-compilation-semantics file-compilation-semantics)))
