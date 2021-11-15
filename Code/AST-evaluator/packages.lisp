898cc77cf87987400ed036d75f711604095ad83d
Robert Strandh <robert.strandh@gmail.com>
2021-11-15 17:28:02 +0100

    No longer shadow SYMBOL-VALUE.

--- Code/AST-evaluator/packages.lisp
+++ Code/AST-evaluator/packages.lisp
@@ -2,7 +2,7 @@
 
 (defpackage #:sicl-ast-evaluator
   (:use #:common-lisp)
-  (:shadow #:symbol-value #:eval)
+  (:shadow #:eval)
   (:local-nicknames (#:env #:sicl-environment)
                     (#:client #:sicl-client)
                     (#:ast #:cleavir-ast))
