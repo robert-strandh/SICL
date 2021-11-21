a6fbbe95d88529d1a6f5652b7f58b8aba12029cb
Robert Strandh <robert.strandh@gmail.com>
2021-11-21 14:42:40 +0100

    Return NIL as should be the case.

--- Code/Cleavir/CST-to-AST/trivial-constant-p.lisp
+++ Code/Cleavir/CST-to-AST/trivial-constant-p.lisp
@@ -1,8 +1,5 @@
 (cl:in-package #:cleavir-cst-to-ast)
 
 (defmethod trivial-constant-p (client object)
-  ;; FIXME: This method should always return NIL.  We return T at the
-  ;; moment because this effectively disables the code for hoisting
-  ;; constant during file compilation.  This is only a kludge that should
-  ;; be removed as soon as the hoisting code has been tested thoroughly.
-  t)
+  (declare (ignorable client object))
+  nil)
