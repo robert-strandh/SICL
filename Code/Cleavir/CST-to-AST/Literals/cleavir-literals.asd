b84b808d3dd18449bfe1b5d117b8e7c68a1dd658
Robert Strandh <robert.strandh@gmail.com>
2021-11-24 17:39:44 +0100

    New system for handling compile-time literals.

---
+++ Code/Cleavir/CST-to-AST/Literals/cleavir-literals.asd
@@ -0,0 +1,6 @@
+(cl:in-package #:asdf-user)
+
+(defsystem cleavir-literals
+  :serial t
+  :components
+  ((:file "packages")))
---
+++ Code/Cleavir/CST-to-AST/Literals/packages.lisp
@@ -0,0 +1,5 @@
+(cl:in-package #:common-lisp-user)
+
+(defpackage #:cleavir-literals
+  (:use #:common-lisp)
+  (:export #:make-load-form-using-client))
