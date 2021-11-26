a3b49856d6149671e5cdbcef4c5540098758e54a
Robert Strandh <robert.strandh@gmail.com>
2021-11-26 15:05:00 +0100

    Define generic function ALLOCATE-LEXICAL-LOCATION.

--- Code/Cleavir/CST-to-AST/Literals/generic-functions.lisp
+++ Code/Cleavir/CST-to-AST/Literals/generic-functions.lisp
@@ -3,3 +3,10 @@
 (defgeneric make-load-form-using-client (client object environment))
 
 (defgeneric load-time-literal (client object environment))
+
+;;; This generic function is called in order for client code to
+;;; allocate a lexical location in which the value of the creation
+;;; form will be stored.  Client code must define a method on this
+;;; generic function that returns a fresh lexical location that
+;;; corresponds to how client code translates code.
+(defgeneric allocate-lexical-location (client environment))
