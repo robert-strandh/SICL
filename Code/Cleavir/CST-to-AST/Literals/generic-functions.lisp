e307023d130a5fa1cba279854547dc05c0cba7e3
Robert Strandh <robert.strandh@gmail.com>
2021-11-27 10:01:46 +0100

    Define generic function CONVERT-FORM.

--- Code/Cleavir/CST-to-AST/Literals/generic-functions.lisp
+++ Code/Cleavir/CST-to-AST/Literals/generic-functions.lisp
@@ -7,9 +7,16 @@
 ;;; This generic function is called in order for client code to
 ;;; allocate a lexical location in which the value of the creation
 ;;; form will be stored.  Client code must define a method on this
-;;; generic function that returns a fresh lexical location that
+;;; generic function, that returns a fresh lexical location that
 ;;; corresponds to how client code translates code.
 (defgeneric allocate-lexical-location (client environment))
+
+;;; This generic function is called in order for client code to
+;;; convert a form to the notiation that the client uses for this
+;;; purpose.  Client code must define a method on this generic
+;;; function, that returns an object that represents the result of the
+;;; conversion.
+(defgeneric convert-form (client form environment))
 
 ;;; Client code calls this generic function when the entire
 ;;; compilation unit has been processed.
