697450bb03e3052fef0b9eb7011d72473f337394
Robert Strandh <robert.strandh@gmail.com>
2021-11-29 17:15:18 +0100

    Define method on CONVERT-CREATION-FORM.

--- Code/Boot/cleavir-configuration.lisp
+++ Code/Boot/cleavir-configuration.lisp
@@ -4,9 +4,15 @@
     ((client client) environment)
   (cleavir-ast:make-ast 'cleavir-ast:lexical-ast :name (gensym)))
 
-(defmethod cleavir-literals:convert-form
+(defmethod cleavir-literals:convert-initialization-form
     ((client client) form environment)
   (cleavir-cst-to-ast:convert client form environment))
+
+(defmethod cleavir-literals:convert-creation-form
+    ((client client) form lexical-ast environment)
+  (cleavir-ast:make-ast 'cleavir-ast:lexical-bind-ast
+    :value-ast (cleavir-cst-to-ast:convert client form environment)
+    :lexical-variable-ast lexical-ast))
 
 (defmethod cleavir-cst-to-ast:trivial-constant-p
     ((client client) object)
