1fd65a85e0bc28c65bb4c77a2df0d9f469b357dd
Robert Strandh <robert.strandh@gmail.com>
2021-12-01 15:34:39 +0100

    Call FINALIZE-LITERALS from new system CLEAVIR-LITERALS.

--- Code/Cleavir/CST-to-AST/cst-to-ast.lisp
+++ Code/Cleavir/CST-to-AST/cst-to-ast.lisp
@@ -12,10 +12,10 @@
         (*use-file-compilation-semantics-p* file-compilation-semantics))
     (if (not *use-file-compilation-semantics-p*)
         (convert client cst environment)
-        (let ((*similarity-table* (make-similarity-table))
-              (*prologue* '()))
+        (cleavir-literals:with-fresh-similarity-table
           (let ((top-level-ast (convert client cst environment)))
             (cleavir-ast:make-ast 'cleavir-ast:progn-ast
               :origin (origin top-level-ast)
-              :form-asts (append (reverse *prologue*)
-                                 (list top-level-ast))))))))
+              :form-asts
+              (append (cleavir-literals:finalize-literals client environment)
+               (list top-level-ast))))))))
