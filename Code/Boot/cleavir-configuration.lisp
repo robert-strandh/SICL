24b3cf62d7184efd85541ea9dff42eb781f5e566
Robert Strandh <robert.strandh@gmail.com>
2021-12-02 16:07:50 +0100

    Pass a CST rather than a form to CST-TO-AST:CONVERT.

--- Code/Boot/cleavir-configuration.lisp
+++ Code/Boot/cleavir-configuration.lisp
@@ -6,13 +6,16 @@
 
 (defmethod cleavir-literals:convert-initialization-form
     ((client client) form environment)
-  (cleavir-cst-to-ast:convert client form environment))
+  (let ((cst (cst:cst-from-expression form)))
+    (cleavir-cst-to-ast:convert client cst environment)))
 
 (defmethod cleavir-literals:convert-creation-form
     ((client client) form lexical-ast environment)
-  (cleavir-ast:make-ast 'cleavir-ast:lexical-bind-ast
-    :value-ast (cleavir-cst-to-ast:convert client form environment)
-    :lexical-variable-ast lexical-ast))
+  (let ((cst (cst:cst-from-expression form)))
+    (cleavir-ast:make-ast 'cleavir-ast:lexical-bind-ast
+      :origin nil
+      :value-ast (cleavir-cst-to-ast:convert client cst environment)
+      :lexical-variable-ast lexical-ast)))
 
 (defmethod cleavir-cst-to-ast:trivial-constant-p
     ((client client) object)
