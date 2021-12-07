00839990a1258528bb65128878b94e6880582d97
Robert Strandh <robert.strandh@gmail.com>
2021-12-07 14:04:12 +0100

    Near-complete rewrite.

--- Code/Cleavir/AST-transformations/Function-tree/function-tree.lisp
+++ Code/Cleavir/AST-transformations/Function-tree/function-tree.lisp
@@ -1,21 +1,20 @@
 (cl:in-package #:cleavir-ast-function-tree)
 
-(defclass parent-information ()
+(defclass lexical-location-occurrence ()
   ((%parent :initarg :parent :reader parent)
-   (%child-number :initarg :child-number :reader child-number)))
+   (%child-number :initarg :child-number :reader child-number)
+   (%node :initarg :node :reader node)))
 
-(defun compute-parents (ast)
-  (let ((result (make-hash-table :test #'eq)))
-    (cleavir-ast:map-ast-depth-first-preorder
-     (lambda (ast)
-       (loop for child in (cleavir-ast:children ast)
-             for i from 0
-             do (push (make-instance 'parent-information
-                        :parent ast
-                        :child-number i)
-                      (gethash child result))))
-     ast)
-    result))
+(defun make-lexical-location-occurrence (parent child-number node)
+  (make-instance 'lexical-location-occurrence
+    :parent parent
+    :child-number child-number
+    :node node))
+
+(defclass lexical-location-information ()
+  ((%definition :initarg :definition :reader definition)
+   (%assignments :initform '() :accessor assignments)
+   (%uses :initform '() :accessor uses)))
 
 (defclass node ()
   ((%function-ast :initarg :function-ast :reader function-ast)
@@ -25,24 +24,50 @@
    (%used-lexicals :initform '() :accessor used-lexicals)
    (%assigned-lexicals :initform '() :accessor assigned-lexicals)))
 
+(defvar *lexical-locations*)
+
+(defun add-definition (lexical-location parent child-number node)
+  (setf (gethash lexical-location *lexical-locations*)
+        (make-instance 'lexical-location-information
+          :definition
+          (make-lexical-location-occurrence parent child-number node))))
+
+(defun add-assignment (lexical-location parent child-number node)
+  (push (make-lexical-location-occurrence parent child-number node)
+        (assignments (gethash lexical-location *lexical-locations*))))
+
+(defun add-use (lexical-location parent child-number node)
+  (push (make-lexical-location-occurrence parent child-number node)
+        (uses (gethash lexical-location *lexical-locations*))))
+
 (defvar *visited*)
 
-(defgeneric traverse (client ast root-node))
+(defgeneric traverse (client parent child child-number root-node))
 
-(defmethod traverse :around (client ast root-node)
-  (unless (gethash ast *visited*)
-    (setf (gethash ast *visited*) t)
-    (call-next-method)))
+(defun traverse-children (client parent root-node)
+  (loop for child in (cleavir-ast:children parent)
+        for child-number from 0
+        do (traverse client parent child child-number root-node)))
 
-(defmethod traverse (client ast root-node)
-  (loop for child in (cleavir-ast:children ast)
-        do (traverse client child root-node)))
+(defmethod traverse :around (client parent child child-number root-node)
+  (let ((key (cons parent child)))
+    (unless (gethash key *visited*)
+      (setf (gethash key *visited*) t)
+      (call-next-method))))
 
-(defmethod traverse (client (ast cleavir-ast:function-ast) root-node)
+(defmethod traverse (client parent child child-number root-node)
+  (traverse-children client child root-node))
+
+(defmethod traverse
+    (client parent (child cleavir-ast:lexical-ast) child-number root-node)
+  (add-use child parent child-number root-node))
+
+(defmethod traverse
+    (client (parent cleavir-ast:function-ast) child child-number root-node)
   (let ((node (make-instance 'node
-                :function-ast ast
+                :function-ast parent
                 :parent root-node))
-        (lambda-list (cleavir-ast:lambda-list ast)))
+        (lambda-list (cleavir-ast:lambda-list parent)))
     (tagbody
      required
        ;; It is possible that LAMBDA-LIST is empty, or that it starts
@@ -56,9 +81,8 @@
                (&rest (pop lambda-list) (go rest))
                (&key (pop lambda-list) (go key))
                (t
-                ;; We have a required parameter.  Push it onto the
-                ;; list of defined lexicals for this node.
-                (push next (defined-lexicals node))
+                ;; We have a required parameter.  Add it as a definition.
+                (add-definition next parent -1 node)
                 ;; Process remaining required parameters if any.
                 (go required)))))
      optional
@@ -75,10 +99,9 @@
                (&key (pop lambda-list) (go key))
                (t
                 ;; We have an optional parameter represented as a list
-                ;; of two LEXICAL-ASTs.  Push them onto the list of
-                ;; defined lexicals for this node.
-                (push (first next) (defined-lexicals node))
-                (push (second next) (defined-lexicals node))
+                ;; of two LEXICAL-ASTs.  Add them as definitions.
+                (add-definition (first next) parent -1 node)
+                (add-definition (second next) parent -1 node)
                 ;; Process remaining optional parameters if any.
                 (go optional)))))
      rest
@@ -104,34 +127,71 @@
                (&allow-other-keys (go out))
                (t
                 ;; We have a keyword parameter in the form of a list
-                ;; containing a symbol and two LEXICAL-ASTs.  Push the
-                ;; LEXICAL-ASTs onto the list of defined lexicals for
-                ;; this node.
-                (push (second next) (defined-lexicals node))
-                (push (third next) (defined-lexicals node))
+                ;; containing a symbol and two LEXICAL-ASTs.  Add the
+                ;; LEXICAL-ASTs as definitions.
+                (add-definition (second next) parent -1 node)
+                (add-definition (third next) parent -1 node)
                 ;; Process remaining keyword parameters if any.
                 (go key)))))
      out)
     (push node (children root-node))
-    (traverse client (cleavir-ast:body-ast ast) node)))
+    (if (typep child 'cleavir-ast:lexical-ast)
+        (add-use child parent 0 node)
+        (traverse-children client child node))))
 
-(defmethod traverse (client (ast cleavir-ast:lexical-bind-ast) root-node)
-  (push (cleavir-ast:lexical-variable-ast ast)
-        (defined-lexicals root-node))
-  (traverse client (cleavir-ast:value-ast ast) root-node))
+(defmethod traverse
+    (client
+     (parent cleavir-ast:lexical-bind-ast)
+     child
+     (child-number (eql 1))
+     root-node)
+  (traverse-children client child root-node))
 
-(defmethod traverse (client (ast cleavir-ast:setq-ast) root-node)
-  (push (cleavir-ast:lexical-variable-ast ast)
-        (assigned-lexicals root-node))
-  (traverse client (cleavir-ast:value-ast ast) root-node))
+(defmethod traverse
+    (client
+     (parent cleavir-ast:lexical-bind-ast)
+     (child cleavir-ast:lexical-ast)
+     (child-number (eql 0))
+     root-node)
+  (add-definition child parent child-number root-node))
 
-(defmethod traverse (client (ast cleavir-ast:lexical-ast) root-node)
-  (push ast (used-lexicals root-node)))
+(defmethod traverse
+    (client
+     (parent cleavir-ast:lexical-bind-ast)
+     (child cleavir-ast:lexical-ast)
+     (child-number (eql 1))
+     root-node)
+  (add-use child parent child-number root-node))
+
+(defmethod traverse
+    (client
+     (parent cleavir-ast:setq-ast)
+     child
+     (child-number (eql 1))
+     root-node)
+  (traverse-children client child root-node))
+
+(defmethod traverse
+    (client
+     (parent cleavir-ast:setq-ast)
+     (child cleavir-ast:lexical-ast)
+     (child-number (eql 0))
+     root-node)
+  (add-assignment child parent child-number root-node))
+
+(defmethod traverse
+    (client
+     (parent cleavir-ast:setq-ast)
+     (child cleavir-ast:lexical-ast)
+     (child-number (eql 1))
+     root-node)
+  (add-use child parent child-number root-node))
 
 (defun function-tree (client ast)
   (let ((root-node (make-instance 'node
                      :function-ast nil
                      :parent nil))
-        (*visited* (make-hash-table :test #'eq)))
-    (traverse client ast root-node)
-    root-node))
+        (*lexical-locations* (make-hash-table :test #'eq))
+        (*visited* (make-hash-table :test #'equal)))
+    (traverse client nil ast -1 root-node)
+    (values root-node *lexical-locations*)))
