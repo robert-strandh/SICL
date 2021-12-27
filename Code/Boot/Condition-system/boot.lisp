dab24114b929f1473be99299cdad83816ba6276b
Robert Strandh <robert.strandh@gmail.com>
2021-12-27 17:53:29 +0100

    Define function for defining ENSURE-GENERIC-FUNCTION.

--- Code/Boot/Condition-system/boot.lisp
+++ Code/Boot/Condition-system/boot.lisp
@@ -1,5 +1,15 @@
 (cl:in-package #:sicl-boot-condition-system)
 
+(defun define-ensure-generic-function (environment)
+  (let* ((client (env:client environment))
+         (make-instance (env:fdefinition client environment 'make-instance)))
+    (setf (env:fdefinition  client environment 'ensure-generic-function)
+          (lambda (function-name &key lambda-list &allow-other-keys)
+            (funcall make-instance 'standard-generic-function
+                     :name function-name
+                     :lambda-list lambda-list)))))
+
 (defun boot (boot)
-  (declare (ignore boot))
-  nil)
+  (let* ((client (make-instance 'client))
+         (environment (make-instance 'environment :client client)))
+    (define-ensure-generic-function environment)))
