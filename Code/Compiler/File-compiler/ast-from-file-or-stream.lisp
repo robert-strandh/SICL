9f65220a2623f7413c334c49860acec15c08eb28
Robert Strandh <robert.strandh@gmail.com>
2022-03-18 18:02:26 +0100

    New file for turning source code into an AST.

---
+++ Code/Compiler/File-compiler/ast-from-file-or-stream.lisp
@@ -0,0 +1,25 @@
+(cl:in-package #:sicl-compiler)
+
+(defun read-cst (input-stream eof-marker)
+  (eclector.concrete-syntax-tree:read input-stream nil eof-marker))
+
+(defun cst-to-ast (client cst compilation-environment)
+  (cleavir-cst-to-ast:cst-to-ast
+   client cst compilation-environment :file-compilation-semantics t))
+
+(defun ast-from-stream (client input-stream run-time-environment)
+  (let* ((compilation-environment
+           (sicl-environment:make-environment-for-file-compilation
+            run-time-environment))
+         (*package* *package*)
+         (asts
+           (loop with eof-marker = input-stream
+                 for cst = (read-cst input-stream eof-marker)
+                 until (eq cst eof-marker)
+                 collect (cst-to-ast client cst compilation-environment))))
+    asts))
+          
+(defun ast-from-file (client absolute-pathname run-time-environment)
+  (sicl-source-tracking:with-source-tracking-stream-from-file
+      (input-stream absolute-pathname)
+    (ast-from-stream client input-stream run-time-environment)))
