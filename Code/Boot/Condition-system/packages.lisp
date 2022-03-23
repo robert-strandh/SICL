1a83154c786f70d4b66e6dd50e64df94daa3f3b1
Robert Strandh <robert.strandh@gmail.com>
2022-03-23 06:46:18 +0100

    Import names of certain function from the BOOT package.

--- Code/Boot/Condition-system/packages.lisp
+++ Code/Boot/Condition-system/packages.lisp
@@ -2,5 +2,8 @@
 
 (defpackage #:sicl-boot-condition-system
   (:use #:common-lisp)
+  (:import-from #:sicl-boot
+                #:import-functions-from-host
+                #:define-error-functions)
   (:local-nicknames (#:env #:sicl-environment))
   (:export #:boot))
