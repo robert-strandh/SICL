1b7ab3befd75beed00a2fc920792868b2203b455
Robert Strandh <robert.strandh@gmail.com>
2022-03-25 14:49:19 +0100

    Provide constructor for CLASS-DESCRIPTION.

--- Code/Environment-Clostrum/class-description.lisp
+++ Code/Environment-Clostrum/class-description.lisp
@@ -8,6 +8,12 @@
    (%superclass-names :initarg :superclass-names :reader superclass-names)
    (%metaclass-name :initarg :metaclass-name :reader metaclass-name)))
 
+(defun make-class-description (name superclass-names metaclass-name)
+  (make-instance 'class-description
+    :name name
+    :superclass-names superclass-names
+    :metaclass-name metaclass-name))
+
 (defgeneric class-description (environment class-name))
 
 (defmethod class-description (environment class-name)
