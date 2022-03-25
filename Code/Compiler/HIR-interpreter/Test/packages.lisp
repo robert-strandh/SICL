64bc0bbe8422c69a57460dcdab4d44b3181f0f6b
Robert Strandh <robert.strandh@gmail.com>
2022-03-25 06:40:23 +0100

    Remove unused system.

--- Code/Compiler/HIR-interpreter/Test/packages.lisp
+++
@@ -1,6 +0,0 @@
-(cl:in-package #:common-lisp-user)
-
-(defpackage #:sicl-hir-interpreter-test
-  (:use #:common-lisp)
-  (:shadow #:eval)
-  (:export #:test))
--- Code/Compiler/HIR-interpreter/call-stack.lisp
+++
@@ -1,7 +0,0 @@
-(cl:in-package #:sicl-hir-interpreter)
-
-(defparameter *call-stack* '())
-
-(defclass call-stack-entry ()
-  ((%origin :initarg :origin :reader origin)
-   (%arguments :initarg :arguments :reader arguments)))
