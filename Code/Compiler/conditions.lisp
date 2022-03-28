a0e58c08f18333b8f06c4a46226557075702d4f2
Robert Strandh <robert.strandh@gmail.com>
2022-03-28 17:11:22 +0200

    Define condition types COMPILE-TIME-WARNING and NAME-MIXIN.

--- Code/Compiler/conditions.lisp
+++ Code/Compiler/conditions.lisp
@@ -1,1 +1,10 @@
 (cl:in-package #:sicl-compiler)
+
+(define-condition compile-time-warning (warning)
+  ((%source-location
+    :initform nil
+    :initarg :source-location
+    :reader source-location)))
+
+(define-condition name-mixin ()
+  ((%name :initarg :name :reader name)))
