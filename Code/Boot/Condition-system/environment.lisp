6afc051d41ffde625a4bb73a59611ab2e921cc7d
Robert Strandh <robert.strandh@gmail.com>
2022-04-12 14:48:26 +0200

    Add slot for storing a base environment.

--- Code/Boot/Condition-system/environment.lisp
+++ Code/Boot/Condition-system/environment.lisp
@@ -1,7 +1,7 @@
 (cl:in-package #:sicl-boot-condition-system)
 
 (defclass environment (sicl-boot:environment)
-  ())
+  ((%base :initarg :base :reader base)))
 
 (defclass client (sicl-boot:client)
   ())
