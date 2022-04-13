032ca399371b3383069fc1bf7f3e1dadd27985a2
Robert Strandh <robert.strandh@gmail.com>
2022-04-13 14:53:06 +0200

    Implement ECS as a delta environment of E5.

--- Code/Boot/Condition-system/environment.lisp
+++ Code/Boot/Condition-system/environment.lisp
@@ -9,3 +9,45 @@
 (defmethod env:fboundp ((client client) (environment environment) function-name)
   (or (call-next-method)
       (env:fboundp client (base environment) function-name)))
+
+(defmethod trucler:describe-function
+    ((client client) (environment environment) function-name)
+  (let ((result (call-next-method)))
+    (if (null result)
+        (let* ((base (base environment))
+               (base-client (env:client base)))
+          (trucler:describe-function base-client base function-name))
+        result)))
+
+(defmethod env:fdefinition
+    ((client client) (environment environment) function-name)
+  (let ((result (call-next-method)))
+    (if (null result)
+        (let* ((base (base environment))
+               (base-client (env:client base)))
+          (env:fdefinition base-client base function-name))
+        result)))
+
+(defmethod env:macro-function
+    ((client client) (environment environment) function-name)
+  (let ((result (call-next-method)))
+    (if (null result)
+        (let* ((base (base environment))
+               (base-client (env:client base)))
+          (env:macro-function base-client base function-name))
+        result)))
+
+(defmethod env:function-cell
+    ((client client) (environment environment) function-name)
+  (let* ((base (base environment))
+         (base-client (env:client base)))
+    (if (null (env:fdefinition base-client environment function-name))
+        (env:function-cell base-client base function-name)
+        (call-next-method))))
+
+(defmethod (setf env:fdefinition)
+    (new-value (client client) (environment environment) function-name)
+  (let* ((base (base environment))
+         (base-client (env:client base)))
+    (setf (env:fdefinition base-client base function-name)
+          new-value)))
