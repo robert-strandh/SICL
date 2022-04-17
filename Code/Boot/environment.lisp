403efcd7d3340a600ba3d6e69d2543dbe67dc026
Robert Strandh <robert.strandh@gmail.com>
2022-04-17 16:29:32 +0200

    Remove import of function COUNT-IF, not needed during bootstrapping.

--- Code/Boot/environment.lisp
+++ Code/Boot/environment.lisp
@@ -229,7 +229,7 @@
      ;; Types and classes
      coerce
      ;; Sequence
-     elt length count count-if reverse position
+     elt length count reverse position
      ;; For debugging
      format
      ;; Conditions
