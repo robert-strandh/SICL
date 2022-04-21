2d11bf8de5b8ce4243ecdd055e4df84c07ca0904
Robert Strandh <robert.strandh@gmail.com>
2022-04-21 09:51:13 +0200

    Remove NCONC, unused at bootstrapping time.

--- Code/Boot/environment.lisp
+++ Code/Boot/environment.lisp
@@ -197,7 +197,7 @@
 (defun import-standard-functions (environment)
   (import-functions-from-host
    '(;; CONSes
-     cons list list* append nconc
+     cons list list* append
      consp atom listp null endp
      car cdr caar cadr cdar cddr
      nthcdr
