b02826f56eadc75871972efa5e1434f91133984e
Robert Strandh <robert.strandh@gmail.com>
2022-04-21 08:57:29 +0200

    Move import of COPY-LIST to where it is needed.

--- Code/Boot/environment.lisp
+++ Code/Boot/environment.lisp
@@ -197,7 +197,7 @@
 (defun import-standard-functions (environment)
   (import-functions-from-host
    '(;; CONSes
-     cons list list* append nconc make-list copy-list
+     cons list list* append nconc make-list
      consp atom listp null endp
      car cdr caar cadr cdar cddr
      nthcdr
