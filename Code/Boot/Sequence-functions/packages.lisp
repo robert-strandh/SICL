9cf76757e299fd68d0f5555ecd8516ee4a370a0c
Robert Strandh <robert.strandh@gmail.com>
2022-04-23 14:55:37 +0200

    Import LOAD-SOURCE-FILE from SICL-BOOT.

--- Code/Boot/Sequence-functions/packages.lisp
+++ Code/Boot/Sequence-functions/packages.lisp
@@ -3,6 +3,7 @@
 (defpackage #:sicl-boot-sequence-functions
   (:use #:common-lisp)
   (:import-from #:sicl-boot
+                #:load-source-file
                 #:import-functions-from-host
                 #:define-error-functions)
   (:local-nicknames (#:env #:sicl-environment))
