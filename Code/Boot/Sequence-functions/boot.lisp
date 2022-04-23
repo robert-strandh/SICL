866f92221f3be1810ff565acdd902ad58ba0a9b0
Robert Strandh <robert.strandh@gmail.com>
2022-04-23 14:56:18 +0200

    Remove function that are no longer needed, and calls to them.

--- Code/Boot/Sequence-functions/boot.lisp
+++ Code/Boot/Sequence-functions/boot.lisp
@@ -1,15 +1,5 @@
 (cl:in-package #:sicl-boot-sequence-functions)
 
-(defun pre-fill-environment (e5 e)
-  (sicl-boot:copy-macro-functions e5 e))
-
-(defun define-ast-eval (esf)
-  (setf (env:fdefinition (env:client esf) esf 'sicl-boot:ast-eval)
-        (lambda (ast)
-          (let* ((client (env:client esf))
-                 (code-object (sicl-compiler:compile-ast client ast)))
-            (sicl-compiler:tie-code-object code-object esf)))))
-
 (defun invoke-with-modified-e5 (e5 esf thunk)
   (let (;; Save the existing function (SETF FDEFINITION) in E5.
         (setf-fdefinition
@@ -36,7 +26,9 @@
   (with-accessors ((e5 sicl-boot:e5))
       boot
     (let* ((client (make-instance 'client))
-           (esf (make-instance 'environment :client client)))
-      (define-ast-eval esf)
-      (pre-fill-environment e5 esf)
+           (esf (make-instance 'environment
+                  :client client
+                  :name "ESF")))
+      (with-modified-e5 (e5 esf)
+        nil)
       esf)))
