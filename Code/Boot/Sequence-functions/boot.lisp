41bf272099a51e38534c72453d4e47a7b58ac58c
Robert Strandh <robert.strandh@gmail.com>
2022-04-30 13:57:27 +0200

    Delete obsolete code.

--- Code/Boot/Sequence-functions/boot.lisp
+++ Code/Boot/Sequence-functions/boot.lisp
@@ -2,74 +2,6 @@
 
 (defvar *esf*)
 
-(defun invoke-with-modified-e5 (e5 esf thunk)
-  (let (;; Save the existing E5 function (SETF FDEFINITION).
-        (setf-fdefinition
-          (env:fdefinition (env:client e5) e5 '(setf fdefinition)))
-        ;; Save the existing E5 function FDEFINITION.
-        (fdefinition
-          (env:fdefinition (env:client e5) e5 'fdefinition))
-        ;; Save the existing E5 function FBOUNDP.
-        (fboundp
-          (env:fdefinition (env:client e5) e5 'fboundp)))
-    ;; Modify the function (SETF FDEFINITION) in E5 so that it sets
-    ;; the definition in ESF instead.
-    (setf (env:fdefinition (env:client e5) e5 '(setf fdefinition))
-          (lambda (new-function name)
-            (setf (env:fdefinition (env:client esf) esf name)
-                  new-function)))
-    ;; Modify the function FDEFINITION in E5 so that it looks in ESF
-    ;; first.
-    (setf (env:fdefinition (env:client e5) e5 'fdefinition)
-          (lambda (name)
-            (if (null (env:fdefinition (env:client esf) esf name))
-                (if (member name *sequence-function-names* :test #'equal)
-                    ;; If the name is not FBOUNDP in ESF, and the name
-                    ;; is that of one of the sequence functions, then
-                    ;; signal an error because this should not happen, since
-                    ;; client code should have called FBOUNDP first.
-                    (error "Attempt to get the FDEFINITION of ~s" name)
-                    ;; Otherwise, return the FDEFINITION of the name in E5.
-                    (funcall fdefinition name))
-                ;; If the name is FBOUNDP in ESF then return the
-                ;; definition of the name in ESF.
-                (env:fdefinition (env:client esf) esf name))))
-    ;; Modify the function FBOUNDP in E5 so that it looks in ESF first.
-    (setf (env:fdefinition (env:client e5) e5 'fboundp)
-          (lambda (name)
-            (or
-             ;; If the name is FBOUNDP in ESF it is considered FBOUNDP
-             ;; in E5.
-             (not (null (env:fdefinition (env:client esf) esf name)))
-             (and
-              ;; If the name is not FBOUNDP in ESF and the name is
-              ;; that of one of the sequence functions, then it is not
-              ;; considered FBOUNDP in E5.
-              (not (member name *sequence-function-names* :test #'equal))
-              ;; Otherwise, it is FBOUNDP if it is FBOUNDP in E5.
-              (funcall fboundp name)))))
-    (unwind-protect
-         (funcall thunk)
-      ;; Restore the saved version of (SETF FDEFINITION) in E5.
-      (setf (env:fdefinition (env:client e5) e5 '(setf fdefinition))
-            setf-fdefinition)
-      ;; Restore the saved version of FDEFINITION in E5.
-      (setf (env:fdefinition (env:client e5) e5 'fdefinition)
-            fdefinition)
-      ;; Restore the saved version of FBOUNDP in E5.
-      (setf (env:fdefinition (env:client e5) e5 'fboundp)
-            fboundp))))
-
-;; (defmacro with-modified-e5 ((e5 esf) &body body)
-;;   `(invoke-with-modified-e5
-;;     ,e5 ,esf
-;;     (lambda ()
-;;       ,@body)))
-
-(defmacro with-modified-e5 ((e5 esf) &body body)
-  (declare (ignore e5 esf))
-  `(progn ,@body))
-
 (defun boot (boot)
   (with-accessors ((e5 sicl-boot:e5))
       boot
@@ -88,7 +20,6 @@
             nil)
       (setf (env:find-class e5-client esf 'standard-generic-function)
             (env:find-class e5-client e5 'standard-generic-function))
-      (with-modified-e5 (e5 esf)
-        (ensure-asdf-system-using-client client e5 '#:fast-generic-functions)
-        (ensure-asdf-system-using-client client e5 '#:sicl-sequence-for-sicl-boot))
+      (ensure-asdf-system-using-client client e5 '#:fast-generic-functions)
+      (ensure-asdf-system-using-client client e5 '#:sicl-sequence-for-sicl-boot)
       esf)))
