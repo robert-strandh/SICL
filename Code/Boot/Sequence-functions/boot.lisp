93d0be9eb9f1b4c3ab4d45e6801df93dec21df45
Robert Strandh <robert.strandh@gmail.com>
2022-05-01 14:59:20 +0200

    Import more functions from the host.

--- Code/Boot/Sequence-functions/boot.lisp
+++ Code/Boot/Sequence-functions/boot.lisp
@@ -17,6 +17,9 @@
          notevery
          plusp
          logand
+         subst
+         fourth fifth
+         nthcdr integer-length ash logior  ; temporary
          cleavir-code-utilities:parse-destructuring-bind)
        e5)
       (setf (env:compiler-macro-function client e5 'format)
