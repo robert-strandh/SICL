8b85df19ec17a9f87f6c991ed03df1f785dc761f
Robert Strandh <robert.strandh@gmail.com>
2022-05-01 08:34:44 +0200

    Import LOAD-SOURCE-FILE-USING-CLIENT from SICL-BOOT.

--- Code/Boot/Sequence-functions/packages.lisp
+++ Code/Boot/Sequence-functions/packages.lisp
@@ -4,6 +4,7 @@
   (:use #:common-lisp)
   (:import-from #:sicl-boot
                 #:load-source-file
+                #:load-source-file-using-client
                 #:ensure-asdf-system
                 #:ensure-asdf-system-using-client
                 #:import-functions-from-host
