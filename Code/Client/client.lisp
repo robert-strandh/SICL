0624f49101cb14146367394f6dde4843b520a11f
Robert Strandh <robert.strandh@gmail.com>
2022-05-03 13:18:33 +0200

    Introduce a special variable *CLIENT*.

--- Code/Client/client.lisp
+++ Code/Client/client.lisp
@@ -6,3 +6,8 @@
 (defclass x86-64 () ())
 
 (defclass sicl-x86-64 (sicl x86-64) ())
+
+;;; This special variable should be bound whenever some processing is
+;;; needed that requires a client object.  Typically the bootstrapping
+;;; procedure will bind it, and so will the top-level REPL.
+(defvar *client*)
