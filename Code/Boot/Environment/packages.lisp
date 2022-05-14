0c807ecb3427af3d5d1d258ced601699d3a12192
Robert Strandh <robert.strandh@gmail.com>
2022-05-14 11:12:14 +0200

    New system for booting environment functions.

---
+++ Code/Boot/Environment/packages.lisp
@@ -0,0 +1,13 @@
+(cl:in-package #:common-lisp-user)
+
+(defpackage #:sicl-boot-environment
+  (:use #:common-lisp)
+  (:import-from #:sicl-boot
+                #:load-source-file
+                #:load-source-file-using-client
+                #:ensure-asdf-system
+                #:ensure-asdf-system-using-client
+                #:import-functions-from-host
+                #:define-error-functions)
+  (:local-nicknames (#:env #:sicl-environment))
+  (:export #:boot))
