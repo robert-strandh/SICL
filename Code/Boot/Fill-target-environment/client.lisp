4981d2bb2443cb0400eab1d58680218bc95f6fe8
Robert Strandh <robert.strandh@gmail.com>
2022-05-17 15:50:15 +0200

    Add slot containing target version of ENV:FDEFINITION.

--- Code/Boot/Fill-target-environment/client.lisp
+++ Code/Boot/Fill-target-environment/client.lisp
@@ -1,6 +1,15 @@
 (cl:in-package #:sicl-boot-fill-target-environment)
 
 (defclass client (sicl-client:sicl)
-  ((%target-environment
+  (;; This slot contains a target environment represented as an ersatz
+   ;; object.
+   (%target-environment
     :initarg :target-environment
-    :reader target-environment)))
+    :reader target-environment)
+   ;; This slot contains the target function ENV:FDEFINITION, We can
+   ;; pass the target environment in the previous slot to this
+   ;; function and get access to any function in the target
+   ;; environment.
+   (%target-fdefinition
+    :initarg :target-fdefinition
+    :reader target-fdefinition)))
