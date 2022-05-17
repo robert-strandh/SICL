b9c9de32730517f6285f694834ecd3699ff31cf9
Robert Strandh <robert.strandh@gmail.com>
2022-05-17 15:31:52 +0200

    Define slot in client class for holding target environment.

--- Code/Boot/Fill-target-environment/client.lisp
+++ Code/Boot/Fill-target-environment/client.lisp
@@ -1,4 +1,6 @@
 (cl:in-package #:sicl-boot-fill-target-environment)
 
 (defclass client (sicl-client:sicl)
-  ())
+  ((%target-environment
+    :initarg :target-environment
+    :reader target-environment)))
