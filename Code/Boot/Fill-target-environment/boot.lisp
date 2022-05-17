83d9064448c426c9d639a2f0667718b1a2dd7338
Robert Strandh <robert.strandh@gmail.com>
2022-05-17 15:36:46 +0200

    Allocate a target environment and put it in the client object.

--- Code/Boot/Fill-target-environment/boot.lisp
+++ Code/Boot/Fill-target-environment/boot.lisp
@@ -3,6 +3,10 @@
 (defun boot (boot)
   (with-accessors ((e5 sicl-boot:e5))
       boot
-    (let* ((client (make-instance 'client))
+    (let* ((client sicl-client:*client*)
+           (make-instance (env:fdefinition client e5 'make-instance))
+           (target-environment (funcall make-instance 'env:run-time-environment))
+           (client (make-instance 'client
+                     :target-environment target-environment))
            (sicl-client:*client* client))
-      nil)))
+      client)))
