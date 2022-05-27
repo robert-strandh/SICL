56e6722b4a8ae5e4588d86e74f3b3a5607bd0704
Robert Strandh <robert.strandh@gmail.com>
2022-05-27 17:22:40 +0200

    Call SHARED-INITIALIZE directly, rather than intermediate function.

--- Code/CLOS/initialize-instance-defmethods.lisp
+++ Code/CLOS/initialize-instance-defmethods.lisp
@@ -1,9 +1,14 @@
 (cl:in-package #:sicl-clos)
 
+;; In each of these methods, we call shared-initialize with a
+;; slot-list of t, meaning all slots, i.e., for every slot that is not
+;; explicitly initialized and which is unbound, evaluate its initform
+;; if it has one.
+
 (defmethod initialize-instance
     ((instance standard-object) &rest initargs &key &allow-other-keys)
-  (apply #'initialize-instance-default instance initargs))
+  (apply #'shared-initialize instance t initargs))
 
 (defmethod initialize-instance
     ((instance function) &rest initargs &key &allow-other-keys)
-  (apply #'initialize-instance-default instance initargs))
+  (apply #'shared-initialize instance t initargs))
