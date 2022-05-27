954a7ce6ae447e8ad4560b93f73825930b629c52
Robert Strandh <robert.strandh@gmail.com>
2022-05-27 17:26:51 +0200

    Delete obsolete files.

--- Code/CLOS/initialize-instance-defuns.lisp
+++
@@ -1,4 +0,0 @@
-(cl:in-package #:sicl-clos)
-
-(defun initialize-instance (instance &rest initargs &key &allow-other-keys)
-  (apply #'initialize-instance-default instance initargs))
--- Code/CLOS/initialize-instance-support.lisp
+++
@@ -1,8 +0,0 @@
-(cl:in-package #:sicl-clos)
-
-(defun initialize-instance-default
-    (instance &rest initargs &key &allow-other-keys)
-  ;; Call shared-initialize with a slot-list of t, meaning all slots,
-  ;; i.e., for every slot that is not explicitly initialized and which
-  ;; is unbound, evaluate its initform if it has one. 
-  (apply #'shared-initialize instance t initargs))
