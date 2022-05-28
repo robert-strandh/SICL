9ef9b9708a382fdc982de635c1d84527485e7c6f
Robert Strandh <robert.strandh@gmail.com>
2022-05-28 10:11:33 +0200

    Incorporate method code rather than calling a separate function.

--- Code/CLOS/make-instance-defmethods.lisp
+++ Code/CLOS/make-instance-defmethods.lisp
@@ -23,5 +23,28 @@
 ;;; that the method is specialized to CLASS.  So for instance, it is
 ;;; possible to call (MAKE-INSTANCE 'SYMBOL) in SBCL.  It does not
 ;;; complain in MAKE-INSTANCE, but in ALLOCATE-INSTANCE.
+
+;;; The AMOP says that ALLOCATE-INSTANCE checks whether the class is
+;;; finalized, and if not, calls FINALIZE-INHERITANCE.  However, the
+;;; INITARGS received by ALLOCATE-INSTANCE should be the defaulted
+;;; initargs, and computing the defaulted initargs requires the class
+;;; to be finalized.  A peek at PCL shows that the class is finalized
+;;; in MAKE-INSTANCE, before ALLOCATE-INSTANCE is called, which makes
+;;; more sense.
+
+;;; FIXME: check validity also for generic functions
+
 (defmethod make-instance ((class regular-class) &rest initargs)
-  (apply #'make-instance-default class initargs))
+  (unless (class-finalized-p class)
+    (finalize-inheritance class))
+  ;; FIXME: check shape of initargs (proper, length is even, etc.).
+  (let ((defaulted-initargs initargs))
+    (loop with default = (load-time-value (list nil))
+          for (name form thunk) in (class-default-initargs class)
+          do (when (eq (getf initargs name default) default)
+               (setf defaulted-initargs
+                     (append defaulted-initargs
+                             (list name (funcall thunk))))))
+    (let ((instance (apply #'allocate-instance class defaulted-initargs)))
+      (apply #'initialize-instance instance defaulted-initargs)
+      instance)))
