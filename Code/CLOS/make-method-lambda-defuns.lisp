cc2c72ecddb68811f7714a8130b7553d86c9f129
Robert Strandh <robert.strandh@gmail.com>
2022-05-29 17:04:06 +0200

    Delete obsolete file.

--- Code/CLOS/make-method-lambda-defuns.lisp
+++
@@ -1,6 +0,0 @@
-(cl:in-package #:sicl-clos)
-
-(defun make-method-lambda
-    (generic-function method lambda-expression environment)
-  (make-method-lambda-default
-   generic-function method lambda-expression environment))
