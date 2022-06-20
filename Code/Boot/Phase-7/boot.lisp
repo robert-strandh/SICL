944ff235a86e943a41665011cc670b3ab150383b
Robert Strandh <robert.strandh@gmail.com>
2022-06-20 17:59:45 +0200

    Remove old code and replace with creation of E*.

--- Code/Boot/Phase-7/boot.lisp
+++ Code/Boot/Phase-7/boot.lisp
@@ -3,11 +3,10 @@
 (defclass client (sicl-boot:client) ())
 
 (defun boot (boot)
-  (with-accessors ((e5 sicl-boot:e5))
+  (with-accessors ((e5 sicl-boot:e5)
+                   (e* sicl-boot:e*))
       boot
     (let ((sicl-client:*client* (make-instance 'client)))
-      (load-source-file "Arithmetic/realp-defgeneric.lisp" e5)
-      (load-source-file "Arithmetic/rationalp-defgeneric.lisp" e5)
-      (ensure-asdf-system '#:sicl-arithmetic-defuns e5)
-      (load-source-file "Cons/accessor-defuns.lisp" e5)
-      (ensure-asdf-system '#:sicl-cons-defuns e5))))
+      (setf e*
+            (funcall (env:fdefinition sicl-client:*client* e5 'make-instance)
+                     'env:run-time-environment)))))
