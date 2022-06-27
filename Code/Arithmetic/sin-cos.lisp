397723bf1ed904f51e4afe6dec9b249ca150f1c5
Robert Strandh <robert.strandh@gmail.com>
2022-06-27 18:11:48 +0200

    Add lots more comments.

--- Code/Arithmetic/sin-cos.lisp
+++ Code/Arithmetic/sin-cos.lisp
@@ -5,8 +5,33 @@
 ;;; computations made here are much better done on the binary
 ;;; representation of the floating-point numbers.
 
-;;; We use the SIN and COS functions to fill the tables, but that is
-;;; obviously wrong.  Some other methods must be used.
+;;; The basic idea is as follows.  We use the well known identity:
+;;; (= (SIN (+ X Y) (+ (* (SIN X) (COS Y)) (* (COS X) (SIN Y)))))
+
+;;; X is called the BASE ANGLE and Y is called the SMALL ANGLE.  We
+;;; take an arbitrary angle (<= 0 ANGLE (/ PI 4)) and we split it so
+;;; that X is a multiple of an integer fraction of a radian.  The
+;;; integer is a power of 2, so in our case, the base angle is a
+;;; multiple of 1/128 of a radian.  The base angle can be determined
+;;; from the floating-point value of the angle by the use of integer
+;;; bit manipulation of the components of the value.  The difference
+;;; between the original angle and X is then small.  The values of
+;;; (SIN X) and (COS X) are kept in two tables that are relatively
+;;; small, and one can hope that they will be in the cache if an
+;;; application uses the SIN and COS functions frequently.  The values
+;;; for (SIN Y) and (COS Y) are computed using an approximation
+;;; polynomial.  Here, I just use the first few terms of the Taylor
+;;; expansion, but that's just because I haven't yet studied how to
+;;; get something optimal.  It might also be better to increase the
+;;; size of the tables if that means a lower degree of the
+;;; approximation polynomials.
+
+;;; The total cost for the formula above is 2 multiplications and 1
+;;; addition.  To that should be added the computation of the
+;;; approximation polynomials.  In this experiment, the approximations
+;;; cost 4 multiplications and 2 or 3 additions, but we may be able to
+;;; do better with lower-degree polynomials.  So we have a total of 6
+;;; multiplications and 3 or 4 additions.
 
 (defparameter *significant-bits* 7)
 
@@ -33,7 +58,8 @@
      (float *radian-fractions-count* 1d0)))
 
 ;;; This table contains values for (SIN X) where X is a base angle
-;;; between 0 and pi/4.
+;;; between 0 and pi/4.  We currently use the host SIN function to
+;;; fill the table, but that is obviously going to change.
 (defparameter *sin-table*
   (let ((result (make-array *table-length* :element-type 'double-float)))
     (loop for i from 0 below *table-length*
@@ -43,7 +69,8 @@
     result))
 
 ;;; This table contains values for (COS X) where X is a base angle
-;;; between 0 and pi/4.
+;;; between 0 and pi/4.  We currently use the host COS function to
+;;; fill the table, but that is obviously going to change.
 (defparameter *cos-table*
   (let ((result (make-array *table-length* :element-type 'double-float)))
     (loop for i from 0 below *table-length*
@@ -72,9 +99,7 @@
          (t2 (- (/ 2d0) (* x2 t1))))
     (- 1d0 (* x2 t2))))
 
-;;; Compute (SIN X) where X is between 0 and pi/4.  We use the
-;;; identity (= (SIN (+ X Y) (+ (* (SIN X) (COS Y)) (* (COS X) (SIN Y)))))
-;;; X will be the base angle and Y a small angle.
+;;; Compute (SIN X) where X is between 0 and pi/4.
 (defun dsin (angle)
   (let* ((radian-fraction-number (radian-fraction-number angle))
          (base-angle (base-angle radian-fraction-number))
