f83ba78d0a81b1043d3aa97bcf4fe99643513837
Robert Strandh <robert.strandh@gmail.com>
2022-06-28 14:14:59 +0200

    Include DEFGENERIC forms for slot readers.

--- Code/Arithmetic/ratio-defclass.lisp
+++ Code/Arithmetic/ratio-defclass.lisp
@@ -1,5 +1,9 @@
 (cl:in-package #:sicl-arithmetic)
 
+(defgeneric numerator (ratio))
+
+(defgeneric denominator (ratio))
+
 (defclass ratio (rational standard-object)
   ((%numerator :initarg :numerator :reader numerator)
    (%denominator :initarg :denominator :reader denominator)))
