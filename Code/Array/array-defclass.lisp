7affa97553134ef8b1ec7fb9ab013c5413783e4c
Robert Strandh <robert.strandh@gmail.com>
2022-06-28 14:37:47 +0200

    Include DEFGENERIC form for slot reader ARRAY-DIMENSIONS.

--- Code/Array/array-defclass.lisp
+++ Code/Array/array-defclass.lisp
@@ -1,5 +1,9 @@
 (cl:in-package #:sicl-array)
 
+;;; This generic function is a reader for a slot that stores the list
+;;; of dimensions in the rack.
+(defgeneric array-dimensions (array))
+
 ;;; ARRAY is a standard class, so STANDARD-OBJECT will automatically
 ;;; be included as a superclass.
 (defclass array ()
