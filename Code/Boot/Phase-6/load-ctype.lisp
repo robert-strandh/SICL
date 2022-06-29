56833ff73b373bc98bf79b8c85c1a5887f551b75
Robert Strandh <robert.strandh@gmail.com>
2022-06-29 16:12:46 +0200

    No longer load files that are needed only before run time.

--- Code/Boot/Phase-6/load-ctype.lisp
+++ Code/Boot/Phase-6/load-ctype.lisp
@@ -6,8 +6,6 @@
   (sicl-boot:import-functions-from-host
    '(realp rationalp)
    e5)
-  (load-source-file "Arithmetic/floatp-defgeneric.lisp" e5)
-  (load-source-file "Arithmetic/upgraded-complex-part-type-defun.lisp" e5)
   (load-source-file "Character/char-code-limit-defconstant.lisp" e5)
   (load-source-file "Cons/ldiff-defun.lisp" e5)
   (let ((*features* '(:sicl))) (ensure-asdf-system '#:ctype e5)))
