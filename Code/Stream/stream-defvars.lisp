dba7ca4110fec984a5a90839754f6e167bb53985
Robert Strandh <robert.strandh@gmail.com>
2022-07-02 10:59:20 +0200

    New file containing simple DEFVAR forms for stream variables.

---
+++ Code/Stream/stream-defvars.lisp
@@ -0,0 +1,21 @@
+(cl:in-package #:sicl-stream)
+
+(defvar *unix-standard-output*)
+
+(defvar *standard-output*)
+
+(defvar *trace-output*)
+
+(defvar *unix-error-output*)
+
+(defvar *error-output*)
+
+(defvar *unix-standard-input*)
+
+(defvar *standard-input*)
+
+(defvar *terminal-io*)
+
+(defvar *query-io*)
+
+(defvar *debug-io*)
