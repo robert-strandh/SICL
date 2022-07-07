d0090ce0d8ebd5b9a0a3fe490644776ca9e3d32d
Robert Strandh <robert.strandh@gmail.com>
2022-07-07 09:33:54 +0200

    Remove unused import of APPLY.

--- Code/Boot/environment.lisp
+++ Code/Boot/environment.lisp
@@ -194,7 +194,7 @@
 (defun import-standard-functions (environment)
   (import-functions-from-host
    '(;; Data and control flow
-     apply not eq eql equal values constantly identity every
+     not eq eql equal values constantly identity every
      ;; Evaluation and compilation
      constantp
      ;; Symbols
