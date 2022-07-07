f3b96bd49e9b6ec78c3b0c39aa3ecff5541dd168
Robert Strandh <robert.strandh@gmail.com>
2022-07-07 16:25:31 +0200

    Remove obsolete file.

--- Code/Environment-Clostrum/temporary-fix.lisp
+++
@@ -1,6 +0,0 @@
-(cl:in-package #:sicl-environment)
-
-(defmethod (setf fdefinition) :after
-    (new-definition client (environment base-run-time-environment) name)
-  (setf (car (function-cell (client environment) environment name))
-        new-definition))
