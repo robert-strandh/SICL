0787594eeb4999a5f02d388902c1eee50313c1b6
Robert Strandh <robert.strandh@gmail.com>
2022-07-11 10:11:20 +0200

    No longer explicitly import REALP and RATIONALP to environment E5.

--- Code/Boot/Phase-6/load-ctype.lisp
+++ Code/Boot/Phase-6/load-ctype.lisp
@@ -3,9 +3,6 @@
 (defun load-ctype (e5)
   (load-source-file "Types/typexpand-defun.lisp" e5)
   (load-source-file "Arithmetic/type-definitions.lisp" e5)
-  (sicl-boot:import-functions-from-host
-   '(realp rationalp)
-   e5)
   (load-source-file "Character/char-code-limit-defconstant.lisp" e5)
   (load-source-file "Cons/ldiff-defun.lisp" e5)
   (let ((*features* '(:sicl))) (ensure-asdf-system '#:ctype e5)))
