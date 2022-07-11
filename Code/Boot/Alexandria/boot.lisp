07e5b9fbbaf71d150e9e6c04b581097190deb6de
Robert Strandh <robert.strandh@gmail.com>
2022-07-11 18:01:27 +0200

    Use macro WITH-TEMPORARY-FUNCTION-IMPORTS to simplify function BOOT.

--- Code/Boot/Alexandria/boot.lisp
+++ Code/Boot/Alexandria/boot.lisp
@@ -18,28 +18,6 @@
       boot
     (let* ((client (make-instance 'client))
            (sicl-client:*client* client))
-      (let (functions)
-        (unwind-protect
-             (progn
-               ;; Access the current definitions in E5 of the
-               ;; functions that we need to be imported from the host
-               ;; during bootstrapping of Alexandria, and store them
-               ;; in the lexical variable FUNCTIONS in the same order
-               ;; as their names appear in *HOST-FUNCTION-NAMES*.
-               ;; Note that the current definition of some function
-               ;; could be NIL, which means it is undefined in E5.
-               ;; What then happens is that we restore it to be
-               ;; undefined after bootstrapping Alexandria.
-               (setf functions
-                     (loop for name in *host-function-names*
-                           collect (env:fdefinition client e5 name)))
-               ;; Next, import the functions named in
-               ;; *HOST-FUNCTION-NAMES* into E5.
-               (import-functions-from-host *host-function-names* e5)
-               ;; Now, it should be safe to load Alexandria.
-               (ensure-asdf-system-using-client client e5 '#:alexandria))
-          ;; Restore the original definitions in E5 of the functions
-          ;; named in *HOST-FUNCTION-NAMES*.
-          (loop for name in *host-function-names*
-                for function in functions
-                do (setf (env:fdefinition client e5 name) function)))))))
+      (with-temporary-function-imports
+          client e5 *host-function-names*
+        (ensure-asdf-system-using-client client e5 '#:alexandria)))))
