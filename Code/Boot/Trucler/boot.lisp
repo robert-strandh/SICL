430c5ff7dcf5bd199b3a52b4a0e3b1365f60f833
Robert Strandh <robert.strandh@gmail.com>
2022-07-11 17:53:40 +0200

    Move new macro to the Boot directory to simplify sharing.

--- Code/Boot/Trucler/boot.lisp
+++ Code/Boot/Trucler/boot.lisp
@@ -2,39 +2,6 @@
 
 (defclass client (sicl-boot:client) ())
 
-(defmacro with-temporary-function-imports
-    (client-form environment-form names-form &body body)
-  (let ((client-var (gensym))
-        (environment-var (gensym))
-        (names-var (gensym)))
-    `(let ((,client-var ,client-form)
-           (,names-var ,names-form)
-           (,environment-var ,environment-form)
-           functions)
-       (unwind-protect
-            (progn
-              ;; Access the current definitions of the functions that
-              ;; we need to be imported from the host, and store them
-              ;; in the lexical variable FUNCTIONS in the same order
-              ;; as their names appear in NAMES-FORM.  Note that the
-              ;; current definition of some function could be NIL,
-              ;; which means it is undefined in the environment.  What
-              ;; then happens is that we restore it to be undefined at
-              ;; the end.
-              (setf functions
-                    (loop for name in ,names-var
-                          collect
-                          (env:fdefinition ,client-var ,environment-var  name)))
-              ;; Next, import the functions.
-              (import-functions-from-host ,names-var ,environment-var)
-              ;; Execute the body in the new context.
-              ,@body)
-         ;; Restore the original definitions of the functions.
-         (loop for name in ,names-var
-               for function in functions
-               do (setf (env:fdefinition ,client-var ,environment-var name)
-                        function))))))
-
 (defun boot (boot)
   (with-accessors ((e5 sicl-boot:e5))
       boot
--- Code/Boot/utilities.lisp
+++ Code/Boot/utilities.lisp
@@ -220,3 +220,36 @@
 (defun set-up-client ()
   (setf *client* (env:client *e5*))
   (setf sicl-client:*client* *client*))
+
+(defmacro with-temporary-function-imports
+    (client-form environment-form names-form &body body)
+  (let ((client-var (gensym))
+        (environment-var (gensym))
+        (names-var (gensym)))
+    `(let ((,client-var ,client-form)
+           (,names-var ,names-form)
+           (,environment-var ,environment-form)
+           functions)
+       (unwind-protect
+            (progn
+              ;; Access the current definitions of the functions that
+              ;; we need to be imported from the host, and store them
+              ;; in the lexical variable FUNCTIONS in the same order
+              ;; as their names appear in NAMES-FORM.  Note that the
+              ;; current definition of some function could be NIL,
+              ;; which means it is undefined in the environment.  What
+              ;; then happens is that we restore it to be undefined at
+              ;; the end.
+              (setf functions
+                    (loop for name in ,names-var
+                          collect
+                          (env:fdefinition ,client-var ,environment-var  name)))
+              ;; Next, import the functions.
+              (import-functions-from-host ,names-var ,environment-var)
+              ;; Execute the body in the new context.
+              ,@body)
+         ;; Restore the original definitions of the functions.
+         (loop for name in ,names-var
+               for function in functions
+               do (setf (env:fdefinition ,client-var ,environment-var name)
+                        function))))))
