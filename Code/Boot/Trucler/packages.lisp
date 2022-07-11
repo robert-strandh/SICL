4c68ca5bedeeafe00ef004ab6d1b89cd6833b1b3
Robert Strandh <robert.strandh@gmail.com>
2022-07-11 17:54:36 +0200

    Import name of new macro WITH-TEMPORARY-FUNCTION-IMPORTS.

--- Code/Boot/Trucler/packages.lisp
+++ Code/Boot/Trucler/packages.lisp
@@ -8,6 +8,7 @@
                 #:ensure-asdf-system
                 #:ensure-asdf-system-using-client
                 #:import-functions-from-host
+                #:with-temporary-function-imports
                 #:define-error-functions)
   (:local-nicknames (#:env #:sicl-environment))
   (:export #:boot))
