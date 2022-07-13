3feda13924e5338eecf978c2773f59192b421bdf
Robert Strandh <robert.strandh@gmail.com>
2022-07-13 17:34:38 +0200

    No longer explicitly IMPORT DEFINE-METHOD-COMBINATION-EXPANDER.

--- Code/Boot/Phase-4/import-from-host.lisp
+++ Code/Boot/Phase-4/import-from-host.lisp
@@ -1,10 +1,6 @@
 (cl:in-package #:sicl-boot-phase-4)
 
 (defun import-misc (e4)
-  (import-functions-from-host
-   '(sicl-method-combination:define-method-combination-expander
-     coerce)
-   e4)
   (setf (env:macro-function (env:client e4) e4 'defpackage)
         (lambda (form env)
           (declare (ignore env))
