54bb5b591cde4c2d3ce8f70e017a25e727923b5d
Robert Strandh <robert.strandh@gmail.com>
2022-07-17 17:11:09 +0200

    Provide omitted quote.

--- Code/Symbol/type-proclamations.lisp
+++ Code/Symbol/type-proclamations.lisp
@@ -1,4 +1,4 @@
 (cl:in-package #:sicl-symbol)
 
-(proclaim (ftype (function (&optional (or string (integer 0))) symbol)
-                 gensym))
+(proclaim '(ftype (function (&optional (or string (integer 0))) symbol)
+            gensym))
