8b35f1f3aa02823cf52afb7687aa02b6da8d0981
Robert Strandh <robert.strandh@gmail.com>
2022-07-20 17:43:37 +0200

    Add type proclamation for POSITION.

--- Code/Sequence/type-proclamations.lisp
+++ Code/Sequence/type-proclamations.lisp
@@ -10,3 +10,14 @@
                              (:test-not function))
                    (integer 0))
             count))
+
+(proclaim '(ftype (function (t sequence
+                             &key
+                             (:from-end t)
+                             (:start (integer 0))
+                             (:end (or (nil (integer 0))))
+                             (:key function)
+                             (:test function)
+                             (:test-not function))
+                   (or null (integer 0)))
+            position))
