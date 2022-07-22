a3189338986bd05ec8ba38cd123054a30b9401c2
Robert Strandh <robert.strandh@gmail.com>
2022-07-22 14:47:03 +0200

    Provid type proclamations for MAKUNBOUND, BOUNDP and THROW.

--- Code/Compiler/Run-time/type-proclamations.lisp
+++ Code/Compiler/Run-time/type-proclamations.lisp
@@ -3,3 +3,9 @@
 (proclaim '(ftype (function (symbol cons) t) symbol-value))
 
 (proclaim '(ftype (function (t symbol cons) t) (setf symbol-value)))
+
+(proclaim '(ftype (function (symbol cons) t) makunbound))
+
+(proclaim '(ftype (function (symbol cons) t) boundp))
+
+(proclaim '(ftype (function ((or symbol integer) list) t) throw))
