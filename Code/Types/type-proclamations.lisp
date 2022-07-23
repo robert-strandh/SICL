f8671db15ffc466d54ea0dc7e88323b6d8600187
Robert Strandh <robert.strandh@gmail.com>
2022-07-23 09:51:01 +0200

    Provide type proclamation for COERCE.

--- Code/Types/type-proclamations.lisp
+++ Code/Types/type-proclamations.lisp
@@ -1,7 +1,8 @@
 (cl:in-package #:sicl-type)
 
 ;;; FIXME: try defining some more specific types
-(declaim (ftype (function (t t &optional t) (member t nil))
-		typep))
+(proclaim '(ftype (function (t t &optional t) (member t nil))
+            typep))
 
-
+(proclaim '(ftype (function (t t) t)
+            coerce))
