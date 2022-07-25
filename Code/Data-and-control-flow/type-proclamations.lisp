49fb229b24aecc2f2eea94638eccdf803471b61d
Robert Strandh <robert.strandh@gmail.com>
2022-07-25 09:40:01 +0200

    Use DECLAIM rather than PROCLAIM and add proclamation for VALUES.

--- Code/Data-and-control-flow/type-proclamations.lisp
+++ Code/Data-and-control-flow/type-proclamations.lisp
@@ -1,9 +1,11 @@
 (cl:in-package #:sicl-data-and-control-flow)
 
-(proclaim '(ftype (function (t) (or t nil)) not))
+(declaim (ftype (function (t) (or t nil)) not))
 
-(proclaim '(ftype (function (t t) (or t nil))
-            eq eql equal))
+(declaim (ftype (function (t t) (or t nil))
+                eq eql equal))
 
-(proclaim '(ftype (function (function sequence &rest sequence) t)
-            every some notevery notany))
+(declaim (ftype (function (function sequence &rest sequence) t)
+                every some notevery notany))
+
+(declaim (ftype (function (&rest t) t) values))
