1843c539536b6a7a32d8bfe96ee776e3b347b018
Robert Strandh <robert.strandh@gmail.com>
2022-07-25 10:18:36 +0200

    Use DECLAIM rather than PROCLAIM.

--- Code/Compiler/Run-time/type-proclamations.lisp
+++ Code/Compiler/Run-time/type-proclamations.lisp
@@ -1,11 +1,11 @@
 (cl:in-package #:sicl-run-time)
 
-(proclaim '(ftype (function (symbol cons) t) symbol-value))
+(declaim (ftype (function (symbol cons) t) symbol-value))
 
-(proclaim '(ftype (function (t symbol cons) t) (setf symbol-value)))
+(declaim (ftype (function (t symbol cons) t) (setf symbol-value)))
 
-(proclaim '(ftype (function (symbol cons) t) makunbound))
+(declaim (ftype (function (symbol cons) t) makunbound))
 
-(proclaim '(ftype (function (symbol cons) t) boundp))
+(declaim (ftype (function (symbol cons) t) boundp))
 
-(proclaim '(ftype (function ((or symbol integer) list) t) throw))
+(declaim (ftype (function ((or symbol integer) list) t) throw))
