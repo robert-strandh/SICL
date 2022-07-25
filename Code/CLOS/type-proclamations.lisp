7ea6c59a24c5f2cdc359961f4ce1f21f6b06df7d
Robert Strandh <robert.strandh@gmail.com>
2022-07-25 10:18:10 +0200

    Use DECLAIM rather than PROCLAIM.

--- Code/CLOS/type-proclamations.lisp
+++ Code/CLOS/type-proclamations.lisp
@@ -1,9 +1,9 @@
 (cl:in-package #:sicl-clos)
 
-(proclaim '(ftype (function (list)
-                   ;; FIXME: be more specific!
-                   (values t t t t t t t))
-            parse-defmethod))
+(declaim (ftype (function (list)
+                          ;; FIXME: be more specific!
+                          (values t t t t t t t))
+                parse-defmethod))
 
-(proclaim '(ftype (function (list) list)
-            canonicalize-specializers))
+(declaim (ftype (function (list) list)
+                canonicalize-specializers))
