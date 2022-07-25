eb93f5f04afae0b3317d19b4d68c8e21acef913a
Robert Strandh <robert.strandh@gmail.com>
2022-07-25 10:49:29 +0200

    Add type proclamations for MAKE-INSTANCE and NO-APPLICABLE-METHOD.

--- Code/CLOS/type-proclamations.lisp
+++ Code/CLOS/type-proclamations.lisp
@@ -7,3 +7,10 @@
 
 (declaim (ftype (function (list) list)
                 canonicalize-specializers))
+
+(declaim (ftype (function ((or symbol class)
+                           &key &allow-other-keys))
+                make-instance))
+
+(declaim (ftype (function (t &rest t) t)
+                no-applicable-method))
