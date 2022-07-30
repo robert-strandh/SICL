092f7610502bd02185bf1db6f7e6a956c62f7680
Robert Strandh <robert.strandh@gmail.com>
2022-07-30 13:40:00 +0200

    Rename lexical variable to better reflect contents.

--- Code/Compiler/AST-compiler/ast-compiler.lisp
+++ Code/Compiler/AST-compiler/ast-compiler.lisp
@@ -1,22 +1,22 @@
 (cl:in-package #:sicl-compiler)
 
 (defun compile-ast (client ast)
-  (multiple-value-bind (hir constants)
+  (multiple-value-bind (ir constants)
       (sicl-ast-to-hir:ast-to-hir client ast)
     (let ((code-object
             (make-instance 'code-object
               :constants constants
-              :ir hir)))
+              :ir ir)))
       (establish-call-sites code-object)
       (let ((hir-thunks
-              (sicl-hir-evaluator:top-level-hir-to-host-function client hir)))
-        (sicl-hir-transformations:eliminate-append-values-instructions hir)
+              (sicl-hir-evaluator:top-level-hir-to-host-function client ir)))
+        (sicl-hir-transformations:eliminate-append-values-instructions ir)
         (sicl-hir-to-mir:hir-to-mir client code-object)
-        (sicl-mir-to-lir:mir-to-lir client hir)
+        (sicl-mir-to-lir:mir-to-lir client ir)
         ;; Not sure why this one is necessary.  Sometime before this
         ;; stage, there is an instruction I1 that has a successor I2,
         ;; but I1 is not a predecessor of I2.
-        (cleavir-ir:set-predecessors hir)
-        (sicl-code-generation:generate-code hir)
-        (cluster:assemble (sicl-code-generation:generate-code hir))
+        (cleavir-ir:set-predecessors ir)
+        (sicl-code-generation:generate-code ir)
+        (cluster:assemble (sicl-code-generation:generate-code ir))
         (values code-object hir-thunks)))))
