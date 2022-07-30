8dfb9d2a397c8138c96c7a0118aa16c8001d8a2b
Robert Strandh <robert.strandh@gmail.com>
2022-07-30 14:10:12 +0200

    Rename lexical variable and simplify code.

--- Code/Compiler/HIR-to-MIR/hir-to-mir.lisp
+++ Code/Compiler/HIR-to-MIR/hir-to-mir.lisp
@@ -10,11 +10,10 @@
           do (process-instruction client instruction))))
 
 (defun hir-to-mir (client code-object)
-  (let ((top-level-enter-instruction (sicl-compiler:ir code-object))
+  (let ((ir (sicl-compiler:ir code-object))
         (*literals* (sicl-compiler:literals code-object)))
-    (expand-funcall-instructions top-level-enter-instruction)
-    (let ((enter-instructions
-            (gather-enter-instructions top-level-enter-instruction)))
-      (loop for enter-instruction in enter-instructions
-            collect (convert-one-function client enter-instruction))
-      top-level-enter-instruction)))
+    (expand-funcall-instructions ir)
+    (loop with enter-instructions = (gather-enter-instructions ir)
+          for enter-instruction in enter-instructions
+          collect (convert-one-function client enter-instruction))
+    ir))
