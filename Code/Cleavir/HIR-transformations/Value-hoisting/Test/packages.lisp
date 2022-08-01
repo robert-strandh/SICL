ab9ac4652f7baea6e376412c4efefd14f1e92519
Robert Strandh <robert.strandh@gmail.com>
2022-08-01 10:34:22 +0200

    Remove unused system for value hoisting.

--- Code/Cleavir/HIR-transformations/Value-hoisting/Test/packages.lisp
+++
@@ -1,5 +0,0 @@
-(cl:in-package #:common-lisp-user)
-
-(defpackage #:cleavir-value-hoisting-test
-  (:use #:common-lisp)
-  (:export))
--- Code/Cleavir/HIR-transformations/Value-hoisting/hir-from-form.lisp
+++
@@ -1,4 +0,0 @@
-(cl:in-package #:cleavir-value-hoisting)
-
-(defmethod hir-from-form (client form environment)
-  )
