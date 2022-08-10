7609ea4aa8f2429bffa793f06fd03f0a5987ce8f
Robert Strandh <robert.strandh@gmail.com>
2022-08-10 15:57:50 +0200

    Define generic function and default method ADD-CALL-SITE.

--- Code/Environment-Clostrum/function-entry.lisp
+++ Code/Environment-Clostrum/function-entry.lisp
@@ -7,3 +7,9 @@
 (stealth-mixin:define-stealth-mixin
     function-entry () clostrum-basic::function-entry
   ((%call-sites :initform '() :accessor call-sites)))
+
+(defgeneric add-call-site (client environment call-site name))
+
+(defmethod add-call-site (client environment call-site name)
+  (let ((entry (clostrum-basic::ensure-function-entry name environment)))
+    (push call-site (call-sites entry))))
--- Code/Environment-Clostrum/packages.lisp
+++ Code/Environment-Clostrum/packages.lisp
@@ -53,6 +53,7 @@
            #:map-defined-classes
            #:map-defined-method-combination-templates
            #:make-environment-for-file-compilation
+           #:add-call-site
            .
            #.(loop for symbol being each external-symbol in '#:clostrum
                    unless (member symbol '(clostrum:run-time-environment
