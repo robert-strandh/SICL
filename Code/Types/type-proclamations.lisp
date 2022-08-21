29ac23afb21a97f8131e8d538789500f8bd93af0
Robert Strandh <robert.strandh@gmail.com>
2022-08-21 15:22:17 +0200

    Use DECLAIM rather than PROCLAIM.

--- Code/Types/type-proclamations.lisp
+++ Code/Types/type-proclamations.lisp
@@ -1,8 +1,8 @@
 (cl:in-package #:sicl-type)
 
 ;;; FIXME: try defining some more specific types
-(proclaim '(ftype (function (t t &optional t) (member t nil))
-            typep))
+(declaim (ftype (function (t t &optional t) (member t nil))
+                typep))
 
-(proclaim '(ftype (function (t t) t)
-            coerce))
+(declaim (ftype (function (t t) t)
+                coerce))
