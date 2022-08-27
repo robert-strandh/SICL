d7a9c5fd5f0244f9c8ba9fa8b5524559cda77b21
Robert Strandh <robert.strandh@gmail.com>
2022-08-27 10:16:49 +0200

    Define new condition type.

--- Code/Evaluation-and-compilation/conditions.lisp
+++ Code/Evaluation-and-compilation/conditions.lisp
@@ -3,3 +3,14 @@
 (define-condition environment-must-be-omitted-or-nil
     (error acclimation:condition)
   ((%environment :initarg :environment :reader environment)))
+
+(define-condition load-time-value-read-only-p-not-evaluated
+    (style-warning)
+  ((%code :initarg :code :reader code))
+  (:report (lambda (condition stream)
+             (format stream
+                     "The second (optional) argument (read-only-p)~@
+                      is not evaluated, so a boolean value (T or NIL)~@
+                      was expected. But the following was found instead:~@
+                      ~s"
+                     (code condition)))))
