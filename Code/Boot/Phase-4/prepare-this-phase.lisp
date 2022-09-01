4960db17b39a8735c5c8f7145a0f06011fecd616
Robert Strandh <robert.strandh@gmail.com>
2022-09-01 14:37:20 +0200

    Load new system of proclamations related to arrays.

--- Code/Boot/Phase-4/prepare-this-phase.lisp
+++ Code/Boot/Phase-4/prepare-this-phase.lisp
@@ -27,6 +27,7 @@
   (ensure-asdf-system '#:sicl-clos-type-proclamations e4)
   (ensure-asdf-system '#:sicl-string-type-proclamations e4)
   (ensure-asdf-system '#:cleavir-code-utilities-type-proclamations e4)
+  (ensure-asdf-system '#:sicl-array-proclamations e4)
   (load-source-file "CLOS/class-of-defun.lisp" e3)
   (enable-typep e2 e3)
   (load-source-file "Types/type-of-defgeneric.lisp" e3)
