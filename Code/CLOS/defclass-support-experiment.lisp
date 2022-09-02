9398a578f9d9b06afb65bd39910cebb9367cafa4
Robert Strandh <robert.strandh@gmail.com>
2022-09-02 15:10:05 +0200

    Define a class for the architecture-builder protocol.

--- Code/CLOS/defclass-support-experiment.lisp
+++ Code/CLOS/defclass-support-experiment.lisp
@@ -3,3 +3,8 @@
 ;;; This file contains an experiment that uses the s-expression-syntax
 ;;; library to parse DEFCLASS.
 
+(defclass defclass-form ()
+  (%name :accessor name)
+  (%superclasses :initform '() :accessor superclasses)
+  (%slot-descriptions :initform '() :accessor slot-descriptions)
+  (%default-initform
