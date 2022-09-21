886ec3b39e5c8c5fbe1f568a2d7df7f51e16959a
Robert Strandh <robert.strandh@gmail.com>
2022-09-21 16:00:44 +0200

    Supply missing condition reporter.

--- Code/CLOS/conditions.lisp
+++ Code/CLOS/conditions.lisp
@@ -45,7 +45,12 @@
 
 (define-condition attempt-to-access-prototype-of-unfinalized-class
     (error)
-  ((%offending-class :initarg :offending-class :reader offending-class)))
+  ((%offending-class :initarg :offending-class :reader offending-class))
+  (:report (lambda (condition stream)
+             (format stream
+                     "Attempt to access the prototype list of the class ~s~@
+                      which has not yet been finalized."
+                     (offending-class condition)))))
 
 (define-condition attempt-to-access-precedence-list-of-unfinalized-class
     (error)
