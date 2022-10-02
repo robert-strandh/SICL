fee7e7d71081aba814f2c0d96e9c534678cd8aca
Robert Strandh <robert.strandh@gmail.com>
2022-10-02 16:50:30 +0200

    Remove invocation of reader macro, and reformat source code.

--- Code/Conditions/Portable-condition-system/docstrings-english-coerce-to-condition.lisp
+++ Code/Conditions/Portable-condition-system/docstrings-english-coerce-to-condition.lisp
@@ -1,9 +1,9 @@
 (cl:in-package #:portable-condition-system)
 
 (setf (documentation 'coerce-to-condition 'function)
-      #.(format
-         nil
-         "Attempts to coerce the provided arguments into a condition
+      (format
+       nil
+       "Attempts to coerce the provided arguments into a condition
 object. The DEFAULT-TYPE argument describes the default condition type that
 should be created if no condition type can be inferred from DATUM; the NAME
 argument is the name of the coercing operator and is used during invalid
@@ -16,10 +16,10 @@ coercions to properly report the error."))
                           (find-class 't)
                           (find-class 't)))
        t)
-      #.(format
-         nil
-         "Returns the condition object that was passed to the function.~@
-          If arguments are non-NIL, signals a continuable error."))
+      (format
+       nil
+       "Returns the condition object that was passed to the function.~@
+        If arguments are non-NIL, signals a continuable error."))
 
 (setf (documentation
        (find-method #'coerce-to-condition '()
@@ -28,9 +28,9 @@ coercions to properly report the error."))
                           (find-class 't)
                           (find-class 't)))
        t)
-      #.(format
-         nil
-         "Calls MAKE-CONDITION on the provided symbol and arguments."))
+      (format
+       nil
+       "Calls MAKE-CONDITION on the provided symbol and arguments."))
 
 (setf (documentation
        (find-method #'coerce-to-condition '()
@@ -39,11 +39,11 @@ coercions to properly report the error."))
                           (find-class 't)
                           (find-class 't)))
        t)
-      #.(format
-         nil
-         "Makes a SIMPLE-CONDITION of the provided DEFAULT-TYPE,~@
-          using the DATUM string as its format control and ARGUMENTS~@
-          as its format arguments."))
+      (format
+       nil
+       "Makes a SIMPLE-CONDITION of the provided DEFAULT-TYPE,~@
+        using the DATUM string as its format control and ARGUMENTS~@
+        as its format arguments."))
 
 (setf (documentation
        (find-method #'coerce-to-condition '()
@@ -52,11 +52,11 @@ coercions to properly report the error."))
                           (find-class 't)
                           (find-class 't)))
        t)
-      #.(format
-         nil
-         "Makes a SIMPLE-CONDITION of the provided DEFAULT-TYPE,~@
-          using the DATUM function as its format control and ARGUMENTS~@
-          as format arguments."))
+      (format
+       nil
+       "Makes a SIMPLE-CONDITION of the provided DEFAULT-TYPE,~@
+        using the DATUM function as its format control and ARGUMENTS~@
+        as format arguments."))
 
 (setf (documentation
        (find-method #'coerce-to-condition '()
@@ -65,7 +65,7 @@ coercions to properly report the error."))
                           (find-class 't)
                           (find-class 't)))
        t)
-      #.(format
-         nil
-         "Signals an error that the provided datum is not coercable~@
-          to a condition object."))
+      (format
+       nil
+       "Signals an error that the provided datum is not coercable~@
+        to a condition object."))
