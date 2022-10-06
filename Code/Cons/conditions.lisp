1d113b3e9ac510c9882fcea68e3fe7d556b88048
Robert Strandh <robert.strandh@gmail.com>
2022-10-06 17:07:31 +0200

    Move condition reporter to DEFINE-CONDITION form.

--- Code/Cons/conditions.lisp
+++ Code/Cons/conditions.lisp
@@ -88,6 +88,12 @@
 (define-condition must-be-proper-or-dotted-list
     (type-error acclimation:condition)
   ()
+  (:report (lambda (condition stream)
+             (format stream
+                     "A proper or dotted list was required,~@
+                      but the following was given:~@
+                      ~s"
+                     (type-error-datum condition))))
   (:default-initargs :expected-type 'list))
 
 ;;; This condition is used by functions and macros that require
