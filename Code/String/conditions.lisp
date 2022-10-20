b2540dddf1380e781c11a128700bea2c2fac8fc8
Robert Strandh <robert.strandh@gmail.com>
2022-10-20 16:24:08 +0200

    Move condition reporter for INVALID-BOUNDING-INDICES.

--- Code/String/conditions.lisp
+++ Code/String/conditions.lisp
@@ -54,4 +54,19 @@
     (error acclimation:condition)
   ((%target :initarg :target :reader target)
    (%start :initarg start :reader start)
-   (%end :initarg end :reader end)))
+   (%end :initarg end :reader end))
+  (:report (lambda (condition stream)
+             (format stream
+                     "In order for START and END to be valid bounding indices,~@
+                      START must between 0 and the length of the string, and~@
+                      END must be between 0 and the length of the string or NIL,~@
+                      and START must be less than or equal to END.~@
+                      But the following values of START and END were found:~@
+                      ~s, ~s~@
+                      For the string:~@
+                      ~s~@
+                      And the length of that string is ~s."
+                     (start condition)
+                     (end condition)
+                     (target condition)
+                     (length (target condition))))))
