f218cc6e0aab795e4889a6302c9bd64b6dd98885
Robert Strandh <robert.strandh@gmail.com>
2022-10-26 12:02:17 +0200

    Make CLONEABLE-MIXIN a superclass of SOURCE-POSITION.

--- Code/Source-tracking/source-tracking.lisp
+++ Code/Source-tracking/source-tracking.lisp
@@ -2,7 +2,7 @@
 
 ;;; Our source location contains the vector of lines of the entire
 ;;; file, a line index, and a character index within the line.
-(defclass source-position ()
+(defclass source-position (cleavir-io:cloneable-mixin)
   ((%lines :initarg :lines :reader lines)
    (%line-index :initarg :line-index :reader line-index)
    (%character-index :initarg :character-index :reader character-index)))
