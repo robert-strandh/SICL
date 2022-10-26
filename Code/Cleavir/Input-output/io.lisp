6d35bc0545485a2da7866aeb49bba6283ffa62eb
Robert Strandh <robert.strandh@gmail.com>
2022-10-26 18:12:49 +0200

    Replace call to SAVE-INFO by call to CLONE-INFORMATION.

--- Code/Cleavir/Input-output/io.lisp
+++ Code/Cleavir/Input-output/io.lisp
@@ -29,7 +29,8 @@
       (let ((*package* (find-package '#:keyword)))
         (pprint-logical-block (stream nil :prefix "[" :suffix "]")
           (format stream "~s ~2i" (class-name (class-of object)))
-          (loop for (initarg reader) in (reverse (save-info object))
+          (loop for (initarg reader)
+                  in (reverse (clonedijk:clone-information object))
                 do (format stream "~_~s ~_~W "
                            initarg (funcall reader object)))))
       (call-next-method)))
