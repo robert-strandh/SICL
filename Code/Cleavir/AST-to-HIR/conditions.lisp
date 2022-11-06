12bb53e940aa279f0ca8a70beda984484fd722ab
Robert Strandh <robert.strandh@gmail.com>
2022-11-06 17:26:53 +0100

    Move condition reporter to DEFINE-CONDITION form.

--- Code/Cleavir/AST-to-HIR/conditions.lisp
+++ Code/Cleavir/AST-to-HIR/conditions.lisp
@@ -8,7 +8,24 @@
    ;; The AST needs this many results and this many successors.
    ;; NIL is don't-care
    (%ast-results :initarg :ast-results :reader ast-results)
-   (%ast-successors :initarg :ast-successors :reader ast-successors)))
+   (%ast-successors :initarg :ast-successors :reader ast-successors))
+  (:report (lambda (condition stream)
+             (with-accessors ((ast ast) (expected-results ast-results)
+                              (expected-successors ast-successors)
+                              (context miscontext-context))
+                 condition
+               (format stream
+                       "Error during AST-TO-HIR:~@
+                        Found ~a with ~@[~d results ~]~@[~d successors~] in context where ~@[~d results ~]~@[~d successors ~]were expected.~@
+                        This is probably caused by a bug in GENERATE-AST."
+                       ast
+                       (if expected-results
+                           (length (results context))
+                           nil)
+                       (if expected-successors
+                           (length (successors context))
+                           nil)
+                       expected-results expected-successors)))))
 
 ;;; FIXME: Check that no results are required when AST-RESULTS is NIL.
 
