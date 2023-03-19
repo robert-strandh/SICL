ef1b4b5b2084dbcac2d60629518110c5a8db3c56
Robert Strandh <robert.strandh@gmail.com>
2023-03-19 15:51:56 +0100

    Define package-local nickname for ICONOCLAST-BUILDER.

--- Code/Future-CST-to-AST/packages.lisp
+++ Code/Future-CST-to-AST/packages.lisp
@@ -3,5 +3,5 @@
 (defpackage #:sicl-future-cst-to-ast
   (:use #:common-lisp)
   (:shadow #:eval)
-  (:local-nicknames (#:ico #:iconoclast))
+  (:local-nicknames (#:ico #:iconoclast) (#:bld #:iconoclast-builder))
   (:export #:cst-to-ast))
