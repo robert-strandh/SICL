cc4ec36ddb957f9614198b6b56c3e3fb0d767155
Robert Strandh <robert.strandh@gmail.com>
2023-03-20 17:26:15 +0100

    Use new macro WITH-BUILDER-COMPONENTS in method on FINISH-NODE.

--- Code/Future-CST-to-AST/progn.lisp
+++ Code/Future-CST-to-AST/progn.lisp
@@ -4,8 +4,9 @@
     ((builder builder)
      (kind t)
      (ast ico:progn-ast))
-  (reinitialize-instance ast
-    :form-asts
-    (loop for body-ast in (ico:form-asts ast)
-          for cst in (ico:origin body-ast)
-          collect (convert (client builder) cst (environment builder)))))
+  (with-builder-components (builder client environment)
+    (reinitialize-instance ast
+      :form-asts
+      (loop for body-ast in (ico:form-asts ast)
+            for cst in (ico:origin body-ast)
+            collect (convert client cst environment)))))
