63f860bada1cdeb133b2bb59aa4b6005fdc531ba
Robert Strandh <robert.strandh@gmail.com>
2023-03-20 06:17:47 +0100

    Create functions and macros for convenient manipulation of builders.

--- Code/Future-CST-to-AST/builder.lisp
+++ Code/Future-CST-to-AST/builder.lisp
@@ -5,9 +5,23 @@
 (defgeneric client (builder))
 
 (defclass builder (bld:builder)
-  ((%environment
-    :initarg :environment
-    :reader environment)
-   (%client
+  ((%client
     :initarg :client
-    :reader client)))
+    :reader client)
+   (%environment
+    :initarg :environment
+    :reader environment)))
+
+(defun make-builder (client environment)
+  (make-instance 'builder
+    :client client
+    :environment environment))
+
+(defun builder-components (builder)
+  (values (client builder) (environment builder)))
+
+(defmacro with-builder-components
+    ((builder-form client-name environment-name) &body body)
+  `(multiple-value-bind (,client-name ,environment-name)
+       ,builder-form
+     ,@body))
