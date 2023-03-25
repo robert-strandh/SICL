a3f86495e3e61bb7e7bb1536e833f57fd541b3fd
Robert Strandh <robert.strandh@gmail.com>
2023-03-25 17:12:39 +0100

    Bug fix, and update according to recent changes in Iconoclast.

--- Code/Future-CST-to-AST/let.lisp
+++ Code/Future-CST-to-AST/let.lisp
@@ -9,13 +9,17 @@
     (loop for binding-ast in (ico:variable-binding-asts ast)
           for cst = (ico:origin (ico:form-ast binding-ast))
           do (reinitialize-instance binding-ast
-               :form-ast (convert client environment)))
+               :form-ast (convert client cst environment)))
     (let ((body-environment environment))
       (loop for binding-ast in (ico:variable-binding-asts ast)
-            for variable-ast = (ico:variable-ast binding-ast)
+            for variable-name-ast = (ico:variable-name-ast binding-ast)
             do (setf environment
                      (augment-environment-with-variable
-                      client variable-ast '() body-environment environment)))
+                      client
+                      variable-name-ast
+                      '()
+                      body-environment
+                      environment)))
       (reinitialize-instance ast
         :form-asts
         (loop for body-ast in (ico:form-asts ast)
