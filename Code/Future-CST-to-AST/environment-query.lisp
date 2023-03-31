77a9188b975c61b33f3cdbd9a2bb96d525cbf880
Robert Strandh <robert.strandh@gmail.com>
2023-03-31 17:17:29 +0200

    Rename variables to avoid references to CST.

--- Code/Future-CST-to-AST/environment-query.lisp
+++ Code/Future-CST-to-AST/environment-query.lisp
@@ -1,18 +1,20 @@
 (cl:in-package #:sicl-future-cst-to-ast)
 
-(defun describe-function (client environment function-name-cst)
-  (let* ((function-name (c:raw function-name-cst))
-         (result (trucler:describe-function client environment function-name)))
+(defun describe-function (client environment cooked-function-name)
+  (let* ((raw-function-name (c:raw cooked-function-name))
+         (result (trucler:describe-function
+                  client environment raw-function-name)))
     (loop while (null result)
           do (restart-case (error "No function description for ~s"
-                                  function-name)
+                                  raw-function-name)
                (consider-global ()
                  :report (lambda (stream)
                            (format stream
                                    "Treat it as the name of a global function."))
                  (return-from describe-function
                    (make-instance 'trucler:global-function-description
-                     :name function-name)))
+                     :origin (c:origin cooked-function-name)
+                     :name raw-function-name)))
                (substitute (new-function-name)
                  :report (lambda (stream)
                            (format stream "Substitute a different name."))
