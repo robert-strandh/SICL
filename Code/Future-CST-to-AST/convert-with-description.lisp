eb90e7cca22f598154eff42b197a0381f4c86b31
Robert Strandh <robert.strandh@gmail.com>
2023-04-01 16:47:44 +0200

    Define method for converting a call to a global macro.

--- Code/Future-CST-to-AST/convert-with-description.lisp
+++ Code/Future-CST-to-AST/convert-with-description.lisp
@@ -68,3 +68,51 @@
     (setf (c:origin cooked-expanded-form) (c:origin cooked-form))
     (with-preserved-toplevel-ness
       (convert client cooked-expanded-form environment))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;; Converting a compound form that calls a global macro.
+;;; A global macro can have a compiler macro associated with it.
+
+(defmethod convert-with-description
+    (client
+     cooked-form
+     (info trucler:global-macro-description) environment)
+  (let ((compiler-macro (trucler:compiler-macro info))
+        (expander (trucler:expander info)))
+    (with-preserved-toplevel-ness
+      (if (null compiler-macro)
+          ;; There is no compiler macro, so we just apply the macro
+          ;; expander, and then convert the resulting form.
+          (let* ((raw-expanded-form
+                   (expand-macro expander cooked-form environment))
+                 (cooked-expanded-form
+                   (c:reconstruct raw-expanded-form cooked-form)))
+            (setf (c:origin cooked-expanded-form) (c:origin cooked-form))
+            (convert client cooked-expanded-form environment))
+          ;; There is a compiler macro, so we must see whether it will
+          ;; accept or decline.
+          (let ((raw-expanded-form
+                  (expand-compiler-macro
+                   compiler-macro cooked-form environment)))
+            (if (eq (c:raw cooked-form) raw-expanded-form)
+                ;; If the two are EQ, this means that the compiler macro
+                ;; declined.  Then we appply the macro function, and
+                ;; then convert the resulting form, just like we did
+                ;; when there was no compiler macro present.
+                (let* ((raw-expanded-form
+                         (expand-macro expander cooked-form environment))
+                       (cooked-expanded-form
+                         (c:reconstruct raw-expanded-form cooked-form)))
+                  (setf (c:origin cooked-expanded-form)
+                        (c:origin cooked-form))
+                  (convert client cooked-expanded-form environment))
+                ;; If the two are not EQ, this means that the compiler
+                ;; macro replaced the original form with a new form.
+                ;; This new form must then again be converted without
+                ;; taking into account the real macro expander.
+                (let ((cooked-expanded-form
+                        (c:reconstruct raw-expanded-form cooked-form)))
+                  (setf (c:origin cooked-expanded-form)
+                        (c:origin cooked-form))
+                  (convert client cooked-expanded-form environment))))))))
