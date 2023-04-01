0cbb6e4e83d74742fdb46a76e625f4e39163f525
Robert Strandh <robert.strandh@gmail.com>
2023-04-01 10:52:16 +0200

    Define extensions (SETF ORIGIN) and RECONSTRUCT.

--- Code/Future-CST-to-AST/Extended-Clearcut/extensions.lisp
+++ Code/Future-CST-to-AST/Extended-Clearcut/extensions.lisp
@@ -14,3 +14,37 @@
 
 (defun origin (expression)
   (origin* clearcut:*client* expression))
+
+(defgeneric (setf origin*) (new-origin client expression))
+
+(defmethod (setf origin*)
+    (new-origin
+     (client clearcut-implementation-s-expression:client)
+     expression)
+  nil)
+
+(defmethod (setf origin*)
+    (new-origin
+     (client clearcut-implementation-concrete-syntax-tree:client)
+     expression)
+  (setf (cst:source expression) new-origin))
+
+(defun (setf origin) (new-origin expression)
+  (setf (origin* expression) new-origin))
+
+(defgeneric reconstruct* (client new-form old-cooked-form))
+
+(defmethod reconstruct*
+  ((client clearcut-implementation-s-expression:client)
+   new-form
+   old-cooked-form)
+  new-form)
+
+(defmethod reconstruct*
+  ((client clearcut-implementation-concrete-syntax-tree:client)
+   new-form
+   old-cooked-form)
+  (cst:reconstruct client new-form old-cooked-form))
+
+(defun reconstruct (new-form old-cooked-form)
+  (reconstruct* clearcut:*client* new-form old-cooked-form))
