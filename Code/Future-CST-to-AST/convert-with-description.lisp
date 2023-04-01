c5cb794b8417a6e8b0d11c66bb4990f4080e3d14
Robert Strandh <robert.strandh@gmail.com>
2023-04-01 15:49:44 +0200

    Define method for converting a call to a local macro.

--- Code/Future-CST-to-AST/convert-with-description.lisp
+++ Code/Future-CST-to-AST/convert-with-description.lisp
@@ -42,3 +42,29 @@
      environment)
   (let ((builder (make-builder client environment)))
     (s-expression-syntax:parse builder t cooked-form)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;; Converting a compound form that calls a local macro.
+;;; A local macro can not have a compiler macro associated with it.
+;;;
+;;; If we found a local macro in ENVIRONMENT, it means that
+;;; ENVIRONMENT is not the global environment.  And it must be
+;;; the same kind of agumentation environment that was used when the
+;;; local macro was created by the use of MACROLET.  Therefore, the
+;;; expander should be able to handle being passed the same kind of
+;;; environment.
+
+(defmethod convert-with-description
+    (client
+     cooked-form
+     (info trucler:local-macro-description)
+     environment)
+  (let* ((expander (trucler:expander info))
+         (raw-expanded-form
+           (expand-macro expander cooked-form environment))
+         (cooked-expanded-form
+           (c:reconstruct raw-expanded-form cooked-form)))
+    (setf (c:origin cooked-expanded-form) (c:origin cooked-form))
+    (with-preserved-toplevel-ness
+      (convert client cooked-expanded-form environment))))
