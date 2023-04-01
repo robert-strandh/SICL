ead934b9c7f96f990e347e9f1099773e72931961
Robert Strandh <robert.strandh@gmail.com>
2023-04-01 10:35:03 +0200

    Fix comment and rename variables.

--- Code/Future-CST-to-AST/convert.lisp
+++ Code/Future-CST-to-AST/convert.lisp
@@ -2,25 +2,27 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;
-;;; CONVERT is responsible for converting a concrete syntax tree to an
-;;; abstract syntax tree.
+;;; CONVERT is responsible for converting a cooked form to an abstract
+;;; syntax tree.
 
-(defmethod convert (client cooked-expression environment)
-  (let ((form (c:raw cooked-expression)))
-    (cond ((or (and (not (consp form)) (not (symbolp form)))
-               (keywordp form)
-               (member form '(t nil)))
-           (convert-constant client cooked-expression environment))
-          ((symbolp form)
-           (convert-variable client cooked-expression environment))
-          ((symbolp (car form))
+(defmethod convert (client cooked-form environment)
+  (let ((raw-form (c:raw cooked-form)))
+    (cond ((or (and (not (consp raw-form)) (not (symbolp raw-form)))
+               (keywordp raw-form)
+               (member raw-form '(t nil)))
+           (convert-constant client cooked-form environment))
+          ((symbolp raw-form)
+           (convert-variable client cooked-form environment))
+          ((symbolp (car raw-form))
            ;; Even if we are in COMPILE-TIME-TOO mode, at this point, we
            ;; do not know whether to evaluate the form at compile time,
            ;; simply because it might be a special form that is handled
            ;; specially.  So we must wait until we have more
            ;; information.
-           (let ((info (describe-function client environment (c:first cooked-expression))))
-             (convert-with-description client cooked-expression info environment)))
+           (let* ((operator (c:first cooked-form))
+                  (info (describe-function client environment operator)))
+             (convert-with-description
+              client cooked-form info environment)))
           (t
            ;; The form must be a compound form where the CAR is a lambda
            ;; expression.  Evaluating such a form might have some
@@ -28,10 +30,10 @@
            ;; in COMPILE-TIME-TOO mode, in which case we must evaluate
            ;; the form as well.
            (when (and *current-form-is-top-level-p* *compile-time-too*)
-             (eval-cooked client cooked-expression environment))
-           (convert-lambda-call client cooked-expression environment)))))
+             (eval-cooked client cooked-form environment))
+           (convert-lambda-call client cooked-form environment)))))
 
-(defmethod convert :around (client cooked-expression environment)
+(defmethod convert :around (client cooked-form environment)
   #+sbcl(declare (sb-ext:muffle-conditions sb-ext:compiler-note))
   (restart-case
       ;; We bind these only here so that if a restart is invoked,
@@ -39,16 +41,16 @@
       ;; (i.e., the ones outside our binding)
       (let ((*current-form-is-top-level-p* *subforms-are-top-level-p*)
             (*subforms-are-top-level-p* nil)
-            (*origin* (c:origin cooked-expression)))
+            (*origin* (c:origin cooked-form)))
         (call-next-method))
     (continue ()
       :report "Replace with call to ERROR."
       (convert client
                (c:cook
                 `(error 'run-time-program-error
-                        :expr ',(c:raw cooked-expression)
-                        :origin ',(c:origin cooked-expression)))
+                        :expr ',(c:raw cooked-form)
+                        :origin ',(c:origin cooked-form)))
                environment))
-    (substitute-expression (cooked-expression)
-      :report "Compile the given expression in place of the problematic one."
-      (convert client cooked-expression environment))))
+    (substitute-form (cooked-form)
+      :report "Compile the given form in place of the problematic one."
+      (convert client cooked-form environment))))
