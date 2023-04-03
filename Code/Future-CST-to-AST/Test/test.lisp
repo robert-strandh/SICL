2893bc133d687073c2fa82dff90cad573b3a1fd8
Robert Strandh <robert.strandh@gmail.com>
2023-04-03 11:33:58 +0200

    Define the initial environment to be wrapped in a reference env.

--- Code/Future-CST-to-AST/Test/test.lisp
+++ Code/Future-CST-to-AST/Test/test.lisp
@@ -1,8 +1,10 @@
 (cl:in-package #:sicl-expression-to-ast)
 
 (defun test (form)
-  (let ((clearcut:*client*
-          (make-instance 'clearcut-implementation-s-expression:client))
-        (environment
-          (make-instance 'environment)))
-    (expression-to-ast nil form environment)))
+  (let* ((clearcut:*client*
+           (make-instance 'clearcut-implementation-s-expression:client))
+         (client (make-instance 'client))
+         (global-environment (make-instance 'environment))
+         (environment (make-instance 'trucler-reference:environment
+                        :global-environment global-environment)))
+    (expression-to-ast client form environment)))
