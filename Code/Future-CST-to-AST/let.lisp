a4965e7e70b23b12c59792f37028908e5d8ce256
Robert Strandh <robert.strandh@gmail.com>
2023-04-04 11:28:54 +0200

    Handle special variables.

--- Code/Future-CST-to-AST/let.lisp
+++ Code/Future-CST-to-AST/let.lisp
@@ -1,31 +1,45 @@
 (cl:in-package #:sicl-expression-to-ast)
 
-;;; FIXME: handle declarations.
+;;; FIXME: handle more declarations
 (defmethod abp:finish-node
     ((builder builder)
      (kind t)
      (ast ico:let-ast))
-  (with-builder-components (builder client environment)
-    ;; Start by converting the initialization arguments of
-    ;; each binding, using the original builder.
-    (loop for binding-ast in (ico:variable-binding-asts ast)
-          for form-ast = (ico:form-ast binding-ast)
-          do (reinitialize-instance binding-ast
-               :form-ast (convert-ast builder form-ast)))
-    (let ((body-environment environment))
-      (loop for binding-ast in (ico:variable-binding-asts ast)
-            for variable-name-ast = (ico:variable-name-ast binding-ast)
-            do (change-class variable-name-ast
-                             'ico:variable-definition-ast)
-               (setf body-environment
-                     (augment-environment-with-variable
+  (with-accessors ((binding-asts ico:variable-binding-asts)
+                   (declaration-asts ico:declaration-asts))
+      ast
+    (with-builder-components (builder client environment)
+      ;; Start by converting the initialization arguments of each
+      ;; binding, using the original builder.
+      (loop for binding-ast in binding-asts
+            for form-ast = (ico:form-ast binding-ast)
+            do (reinitialize-instance binding-ast
+                 :form-ast (convert-ast builder form-ast)))
+      ;; Next, compute a new environment to use for the body forms
+      (let ((body-environment environment))
+        (loop for binding-ast in binding-asts
+              for variable-name-ast = (ico:variable-name-ast binding-ast)
+              for variable-name = (ico:name variable-name-ast)
+              do (multiple-value-bind (special-p globally-special-p)
+                     (variable-is-special-p
                       client
+                      environment
                       variable-name-ast
-                      '()
-                      body-environment
-                      environment)))
-      (let ((new-builder (make-builder client body-environment)))
-        (reinitialize-instance ast
-          :form-asts
-          (loop for body-ast in (ico:form-asts ast)
-                collect (convert-ast new-builder body-ast)))))))
+                      declaration-asts)
+                   (change-class variable-name-ast
+                                 (if special-p
+                                     'ico:special-variable-bound-ast
+                                     'ico:variable-definition-ast))
+               (setf body-environment
+                     (if special-p
+                         (unless globally-special-p
+                           (trucler:add-special-variable
+                            client environment variable-name))
+                         (trucler:add-lexical-variable
+                          client environment
+                          variable-name variable-name-ast)))))
+        (let ((new-builder (make-builder client body-environment)))
+          (reinitialize-instance ast
+            :form-asts
+            (loop for body-ast in (ico:form-asts ast)
+                  collect (convert-ast new-builder body-ast))))))))
