277b318d7849ade7d51f715588e937bd2e029b25
Robert Strandh <robert.strandh@gmail.com>
2023-05-04 14:52:12 +0200

    Fix incorrect indentation.

--- Code/Cons/remf-support.lisp
+++ Code/Cons/remf-support.lisp
@@ -9,31 +9,31 @@
                     `((,(first vars) ,(first vals))))
               (,(first store-vars) ,reader-form)
               (,indicator-var ,indicator))
-       (cond ((atom ,(first store-vars))
-              (if (null ,(first store-vars))
-                  nil
-                  (error 'must-be-plist :datum ',store-vars
-                                        :expected-type 'list)))
-             ((eq (car ,(first store-vars)) ,indicator-var)
-              (cond ((atom (cdr ,(first store-vars)))
-                     (error 'must-be-plist :datum ',store-vars
-                                           :expected-type 'list))
-                    (t
-                     (setq ,(first store-vars)
-                           (cddr ,(first store-vars)))
-                     ,writer-form
-                     t)))
-             (t
-              (loop for rest = (cdr ,(first store-vars)) then (cddr rest)
-                    until (atom (cdr rest))
-                    do (cond ((atom (cddr rest))
-                              (error 'must-be-plist :datum ',store-vars
-                                                    :expected-type 'list))
-                             ((eq (cadr rest) ,indicator-var)
-                              (setf (cdr rest) (cdddr rest))
-                              (return t))
-                             (t
-                              nil))
-                    finally (unless (null (cdr rest))
-                              (error 'must-be-plist :datum ',store-vars
-                                                    :expected-type 'list)))))))))
+         (cond ((atom ,(first store-vars))
+                (if (null ,(first store-vars))
+                    nil
+                    (error 'must-be-plist :datum ',store-vars
+                                          :expected-type 'list)))
+               ((eq (car ,(first store-vars)) ,indicator-var)
+                (cond ((atom (cdr ,(first store-vars)))
+                       (error 'must-be-plist :datum ',store-vars
+                                             :expected-type 'list))
+                      (t
+                       (setq ,(first store-vars)
+                             (cddr ,(first store-vars)))
+                       ,writer-form
+                       t)))
+               (t
+                (loop for rest = (cdr ,(first store-vars)) then (cddr rest)
+                      until (atom (cdr rest))
+                      do (cond ((atom (cddr rest))
+                                (error 'must-be-plist :datum ',store-vars
+                                                      :expected-type 'list))
+                               ((eq (cadr rest) ,indicator-var)
+                                (setf (cdr rest) (cdddr rest))
+                                (return t))
+                               (t
+                                nil))
+                      finally (unless (null (cdr rest))
+                                (error 'must-be-plist :datum ',store-vars
+                                                      :expected-type 'list)))))))))
