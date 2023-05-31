24f41e870037711cd044436da6de6704a2c5bd2f
Robert Strandh <robert.strandh@gmail.com>
2023-05-31 15:57:53 +0200

    Change package prefix to reflect new package for FORM-AST.

--- Code/Future-CST-to-AST/convert-ast.lisp
+++ Code/Future-CST-to-AST/convert-ast.lisp
@@ -1,6 +1,6 @@
 (cl:in-package #:sicl-expression-to-ast)
 
 (defmethod convert-ast
-    (builder (ast bld:unparsed-form-ast))
+    (builder (ast ico:unparsed-form-ast))
   (with-builder-components (builder client environment)
-    (convert client (bld:form ast) environment)))
+    (convert client (ico:form ast) environment)))
