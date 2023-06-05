78e3a7af7d9706aa9e3bf3ea0eff564d5b9b91f3
Robert Strandh <robert.strandh@gmail.com>
2023-06-05 15:43:38 +0200

    Define entry points for s-expression and CST.

--- Code/Future-CST-to-AST/expression-to-ast.lisp
+++ Code/Future-CST-to-AST/expression-to-ast.lisp
@@ -6,3 +6,21 @@
         (*compile-time-too* nil)
         (*use-file-compilation-semantics-p* file-compilation-semantics))
     (convert client cooked-expression environment)))
+
+(defun s-expression-to-ast
+    (s-expression environment &key file-compilation-semantics)
+  (let ((client
+          (make-instance 'clearcut-implementation-s-expression:client)))
+    (let ((c:*client* client))
+      (expression-to-ast
+       client s-expression environment
+       :file-compilation-semantics file-compilation-semantics))))
+
+(defun cst-to-ast
+    (s-expression environment &key file-compilation-semantics)
+  (let ((client
+          (make-instance 'clearcut-implementation-concrete-syntax-tree:client)))
+    (let ((c:*client* client))
+      (expression-to-ast
+       client s-expression environment
+       :file-compilation-semantics file-compilation-semantics))))
