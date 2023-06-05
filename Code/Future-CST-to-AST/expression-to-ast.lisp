95ecd789d0f66a1c367303ddc3a23b5fd226ea7d
Robert Strandh <robert.strandh@gmail.com>
2023-06-05 16:20:13 +0200

    Define client classes for the two new entry points.

--- Code/Future-CST-to-AST/expression-to-ast.lisp
+++ Code/Future-CST-to-AST/expression-to-ast.lisp
@@ -7,19 +7,27 @@
         (*use-file-compilation-semantics-p* file-compilation-semantics))
     (convert client cooked-expression environment)))
 
+(defclass s-expression-client
+    (clearcut-implementation-s-expression:client
+     trucler-reference:client)
+  ())
+
 (defun s-expression-to-ast
     (s-expression environment &key file-compilation-semantics)
-  (let ((client
-          (make-instance 'clearcut-implementation-s-expression:client)))
+  (let ((client (make-instance 's-expression-client)))
     (let ((c:*client* client))
       (expression-to-ast
        client s-expression environment
        :file-compilation-semantics file-compilation-semantics))))
 
+(defclass cst-client
+    (clearcut-implementation-concrete-syntax-tree:client
+     trucler-reference:client)
+  ())
+
 (defun cst-to-ast
     (s-expression environment &key file-compilation-semantics)
-  (let ((client
-          (make-instance 'clearcut-implementation-concrete-syntax-tree:client)))
+  (let ((client (make-instance 'cst-client)))
     (let ((c:*client* client))
       (expression-to-ast
        client s-expression environment
