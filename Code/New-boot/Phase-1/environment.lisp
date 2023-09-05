059f01100e0a53ec0d53db52795d3e0c024bc0a0
Robert Strandh <robert.strandh@gmail.com>
2023-09-05 10:11:09 +0200

    Define class CLIENT and function CREATE-ENVIRONMENT.

--- Code/New-boot/Phase-1/environment.lisp
+++ Code/New-boot/Phase-1/environment.lisp
@@ -1,2 +1,8 @@
 (cl:in-package #:sicl-new-boot)
 
+(defclass client (trucler-reference:client)
+  ())
+
+(defun create-environment ()
+  (let ((environment (cb:create-environment)))
+    environment))
