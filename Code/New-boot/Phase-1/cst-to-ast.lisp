344145729ccfa8bfa2a7ea74d69bb37ca9f7606f
Robert Strandh <robert.strandh@gmail.com>
2023-09-28 12:29:38 +0200

    Bind COMMON-MACRO-DEFINITONS:*CLIENT* before macro expansions.

--- Code/New-boot/Phase-1/cst-to-ast.lisp
+++ Code/New-boot/Phase-1/cst-to-ast.lisp
@@ -1,4 +1,5 @@
 (cl:in-package #:sicl-new-boot)
 
 (defun cst-to-ast (client cst environment)
-  (cb:cst-to-ast client cst environment))
+  (let ((cmd:*client* client))
+    (cb:cst-to-ast client cst environment)))
