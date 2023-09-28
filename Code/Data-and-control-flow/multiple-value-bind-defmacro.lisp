687a4bb8d7b45594eb0b3e93cfc545ccfe1c9646
Robert Strandh <robert.strandh@gmail.com>
2023-09-28 10:09:09 +0200

    Remove obsolete comment.

--- Code/Data-and-control-flow/multiple-value-bind-defmacro.lisp
+++ Code/Data-and-control-flow/multiple-value-bind-defmacro.lisp
@@ -6,10 +6,6 @@
 ;;;
 ;;; We define this macro pretty much exactly as the HyperSpec says in
 ;;; the "Notes:" section on the page describing MULTIPLE-VALUE-BIND.  
-;;;
-;;; Later, we will optimize a bit, either by recognizing this
-;;; particular expansion when we compile MULIPLE-VALUE-CALL, or by
-;;; handling MULTIPLE-VALUE-BIND as a special operator. 
 
 (defmacro multiple-value-bind (variables values-form &body body)
   (let ((rest-variable (gensym)))
