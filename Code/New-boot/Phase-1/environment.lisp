9d78c107b05048eedeb969d893006eb7ed9b5d50
Robert Strandh <robert.strandh@gmail.com>
2023-09-30 10:18:50 +0200

    Make ECLECTOR.CONCRETE-SYNTAX-TREE:CST-CLIENT a superclass of CLIENT

--- Code/New-boot/Phase-1/environment.lisp
+++ Code/New-boot/Phase-1/environment.lisp
@@ -1,6 +1,7 @@
 (cl:in-package #:sicl-new-boot-phase-1)
 
-(defclass client (cb:macro-transforming-client)
+(defclass client (cb:macro-transforming-client
+                  eclector.concrete-syntax-tree:cst-client)
   ())
 
 (defun create-environment ()
