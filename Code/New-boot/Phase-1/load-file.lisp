9fe99ba4e127db1b5c7ed4cc922c3766174cb8d0
Robert Strandh <robert.strandh@gmail.com>
2023-09-30 11:09:52 +0200

    New component containing code for loading a file.

---
+++ Code/New-boot/Phase-1/load-file.lisp
@@ -0,0 +1,14 @@
+(cl:in-package #:sicl-new-boot-phase-1)
+
+(defun load-stream (client stream environment)
+  (let ((eclector.base:*client* client)
+        (abp:*builder* (cb::make-builder client environment)))
+    (loop with eof = (list nil)
+          for cst = (eclector.concrete-syntax-tree:read stream nil eof)
+          until (eq cst eof)
+          do (let ((ast (cst-to-ast client cst environment)))
+               (cbae:eval-ast client ast environment)))))
+
+(defun load-file (client filename environment)
+  (with-open-file (stream filename :direction :input)
+    (load-stream client stream environment)))
--- Code/New-boot/Phase-1/sicl-new-boot-phase-1.asd
+++ Code/New-boot/Phase-1/sicl-new-boot-phase-1.asd
@@ -12,4 +12,5 @@
    (:file "configuration")
    (:file "import-from-host")
    (:file "define-environment-functions")
-   (:file "cst-to-ast")))
+   (:file "cst-to-ast")
+   (:file "load-file")))
