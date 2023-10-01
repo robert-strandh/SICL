50c22b28f43834fb6460a50897f438c737348bf1
Robert Strandh <robert.strandh@gmail.com>
2023-10-01 15:41:38 +0200

    Define method on ECLECOR.READER:INTERPRET-SYMBOL for string.

--- Code/New-boot/Phase-1/package-programming.lisp
+++ Code/New-boot/Phase-1/package-programming.lisp
@@ -49,3 +49,44 @@
        (setf (gethash symbol *symbol-package*) *current-package*)
        (parcl:add-internal-symbol client *current-package* symbol)
        symbol))))
+
+;;; This method is applicable when Eclector sees a symbol with one or
+;;; two package markers, If it has a single package marker, then
+;;; INTERNP is false, and we should just find the symbol and return
+;;; it, and error if not.  If it has two package markers, then INTERNP
+;;; is true, and we should intern it no matter what.  If
+;;; PACKAGE-INDICATOR is either "COMMON-LISP" or "KEYWORD", then we
+;;; let Eclector do it's thing.
+(defmethod eclector.reader:interpret-symbol
+    ((client client)
+     input-stream
+     (package-indicator string)
+     symbol-name
+     internp)
+  (declare (ignore input-stream))
+  (if (or (string= package-indicator "COMMON-LISP")
+          (string= package-indicator "CL")
+          (string= package-indicator "KEYWORD"))
+      (call-next-method)
+      (multiple-value-bind (package existsp)
+          (gethash package-indicator *packages*)
+        (unless existsp
+          (error "No package named ~s" package-indicator))
+        (multiple-value-bind (symbol existsp)
+            (parcl:find-external-symbol client package symbol-name)
+          (when existsp
+            (return-from eclector.reader:interpret-symbol symbol)))
+        (multiple-value-bind (symbol existsp)
+            (parcl:find-internal-symbol client package symbol-name)
+          (when existsp
+            (if internp
+                (return-from eclector.reader:interpret-symbol symbol)
+                (error "Symbol ~a is not external in package ~a"
+                       symbol-name (parcl:name client package)))))
+        (if internp
+            (let ((symbol (make-symbol symbol-name)))
+              (setf (gethash symbol *symbol-package*) package)
+              (parcl:add-internal-symbol client package symbol)
+              symbol)
+            (error "Symbol ~a does not exist in package ~a"
+                   symbol-name (parcl:name client package))))))
