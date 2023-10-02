ea3b62369bd13d48c9100de8f8557c6753d46897
Robert Strandh <robert.strandh@gmail.com>
2023-10-02 06:08:08 +0200

    Make PACL-CLASS:CLIENT a superclass of CLIENT.

--- Code/New-boot/Phase-1/environment.lisp
+++ Code/New-boot/Phase-1/environment.lisp
@@ -1,7 +1,8 @@
 (cl:in-package #:sicl-new-boot-phase-1)
 
 (defclass client (cb:macro-transforming-client
-                  eclector.concrete-syntax-tree:cst-client)
+                  eclector.concrete-syntax-tree:cst-client
+                  parcl-class:client)
   ())
 
 (defun create-environment ()
