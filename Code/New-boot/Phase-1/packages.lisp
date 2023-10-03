7300a436198e1909007f8d0aabb7d15fbbd44716
Robert Strandh <robert.strandh@gmail.com>
2023-10-03 14:21:34 +0200

    Change package-local nickname for CLOSTRUM to CLO.

--- Code/New-boot/Phase-1/packages.lisp
+++ Code/New-boot/Phase-1/packages.lisp
@@ -4,7 +4,7 @@
   (:use #:common-lisp)
   (:local-nicknames
    (#:cb #:common-boot)
-   (#:env #:clostrum)
+   (#:clo #:clostrum)
    (#:cbae #:common-boot-ast-evaluator)
    (#:cmd #:common-macro-definitions)
    (#:abp #:architecture.builder-protocol))
