ec54f254062f8fc0cf118f804c35243469bb1c1d
Robert Strandh <robert.strandh@gmail.com>
2023-10-15 14:18:59 +0200

    Simplifiy code and provide methods for DEFGENERIC.

--- Code/New-boot/Phase-1/macro-programming.lisp
+++ Code/New-boot/Phase-1/macro-programming.lisp
@@ -1,5 +1,7 @@
 (cl:in-package #:sicl-new-boot-phase-1)
 
+;;; DEFCLASS programming.
+
 (defmethod cmd:defclass-compile-time-action
   ((client client)
    name
@@ -12,6 +14,22 @@
           superclass-names
           metaclass-name))
 
+(defun transform-name (name)
+  (intern (string-downcase (symbol-name name))
+          (find-package "SICL-NEW-BOOT-PHASE-1")))
+
+(defun transform-slot-spec
+    (&rest arguments &key readers writers &allow-other-keys)
+  (let ((result arguments)
+        (new-readers (loop for reader in readers
+                           collect  (transform-name reader)))
+        (new-writers (loop for writer in writers
+                           collect
+                           `(setf ,(transform-name (second writer))))))
+    (remf result :readers)
+    (remf result :writers)
+    `(,@result :readers ,new-readers ,new-writers)))
+
 (defmethod cmd:ensure-class
     ((client client)
      name
@@ -19,10 +37,44 @@
      direct-slot-specs
      options
      environment)
-  (format *trace-output*
-          "run time: ~s ~s ~s ~s ~s~%"
-          name
-          superclass-names
-          direct-slot-specs
-          options
-          environment))
+  (closer-mop:ensure-class
+   (transform-name name)
+   :name name
+   :direct-superclasses (mapcar #'transform-name superclass-names)
+   :direct-slots (mapcar (lambda (slot-spec)
+                           (apply #'transform-slot-spec slot-spec))
+                         direct-slot-specs)
+   :metaclass 'closer-mop:funcallable-standard-class))
+
+;;; DEFGENERIC programming.
+
+(defmethod cmd:defgeneric-compile-time-action
+    ((client client)
+     name
+     lambda-list
+     argument-precedence-order
+     generic-function-class-name
+     method-class-name
+     method-combination-name
+     method-combination-arguments
+     documentation-option
+     environment)
+  nil)
+
+(defmethod cmd:ensure-generic-function
+    ((client client)
+     name
+     lambda-list
+     argument-precedence-order
+     generic-function-class-name
+     method-class-name
+     method-combination-name
+     method-combination-arguments
+     documentation-option
+     environment)
+  (ensure-generic-function
+   (transform-name name)
+   :lambda-list lambda-list
+   :argument-precedence-order argument-precedence-order
+   :generic-function-class generic-function-class-name
+   :method-class method-class-name))
