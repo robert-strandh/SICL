1dd7654179bc3075b0ec147dd197fe6825058be8
Robert Strandh <robert.strandh@gmail.com>
2023-10-15 17:49:10 +0200

    New component containing shared version of LOAD-SOURCE-FILE.

---
+++ Code/New-boot/Shared/load-source-file.lisp
@@ -0,0 +1,16 @@
+(cl:in-package #:sicl-new-boot)
+
+(defun load-stream (client stream environment)
+  (let ((eclector.base:*client* client)
+        (abp:*builder* (cb::make-builder client environment)))
+    (loop with eof = (list nil)
+          for cst = (eclector.concrete-syntax-tree:read stream nil eof)
+          until (eq cst eof)
+          do (let ((ast (cst-to-ast client cst environment)))
+               (cbae:eval-ast client ast environment)))))
+
+(defun load-source-file (client filename environment)
+  (format *trace-output* "Loading ~s into ~s~%"
+          filename environment)
+  (with-open-file (stream filename :direction :input)
+    (load-stream client stream environment)))
