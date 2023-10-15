5b6d0f64711f49711b1b2d6fc6515f21a2c6f6d3
Robert Strandh <robert.strandh@gmail.com>
2023-10-15 17:50:46 +0200

    Add several local nicknames, and export ENSURE-ASDF-SYSTEM.

--- Code/New-boot/Shared/packages.lisp
+++ Code/New-boot/Shared/packages.lisp
@@ -1,4 +1,11 @@
 (cl:in-package #:common-lisp-user)
 
 (defpackage #:sicl-new-boot
-  (:use #:common-lisp))
+  (:use #:common-lisp)
+  (:local-nicknames
+   (#:abp #:architecture.builder-protocol)
+   (#:cmd #:common-macro-definitions)
+   (#:cb #:common-boot)
+   (#:cbae #:common-boot-ast-evaluator))
+  (:export
+   #:ensure-asdf-system))
