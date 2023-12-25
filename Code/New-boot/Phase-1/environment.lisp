1da8f56252fafa2d42ccf03308038e865ee48407
Robert Strandh <robert.strandh@gmail.com>
2023-12-25 10:29:06 +0100

    Remove obsolete class ENVIRONMENT.

--- Code/New-boot/Phase-1/environment.lisp
+++ Code/New-boot/Phase-1/environment.lisp
@@ -1,8 +1,5 @@
 (cl:in-package #:sicl-new-boot-phase-1)
 
-(defclass environment (common-boot:environment)
-  ())
-
 (defun create-environment (client)
   (let ((environment (cb:create-environment)))
     (change-class (trucler:global-environment client environment)
