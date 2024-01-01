f6e8481592dc777682b5e4ddb7272e3efbf4db3d
Robert Strandh <robert.strandh@gmail.com>
2024-01-01 15:48:51 +0100

    Catch warnings of undefined functions.

--- Code/New-boot/Shared/load-source-file.lisp
+++ Code/New-boot/Shared/load-source-file.lisp
@@ -2,14 +2,21 @@
 
 (defun load-stream (client stream environment)
   (let ((eclector.base:*client* client)
-        (abp:*builder* (cb::make-builder client environment)))
-    (loop with eof = (list nil)
-          for cst = (eclector.concrete-syntax-tree:read stream nil eof)
-          until (eq cst eof)
-          do (let* ((ast (cst-to-ast client cst environment))
-                    (top-level-function 
-                      (cbae:compile-ast client ast environment)))
-               (funcall top-level-function)))))
+        (abp:*builder* (cb::make-builder client environment))
+        (undefined-functions '()))
+    (handler-bind
+        ((common-boot:no-function-description
+           (lambda (condition)
+             (push condition undefined-functions))))
+      (loop with eof = (list nil)
+            for cst = (eclector.concrete-syntax-tree:read stream nil eof)
+            until (eq cst eof)
+            do (let* ((ast (cst-to-ast client cst environment))
+                      (top-level-function 
+                        (cbae:compile-ast client ast environment)))
+                 (funcall top-level-function))))
+    (loop for undefined-function in undefined-functions
+          do (warn undefined-function))))
 
 (defun load-source-file (client filename environment)
   (format *trace-output* "Loading ~s into ~s~%"
