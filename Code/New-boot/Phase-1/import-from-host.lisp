fa7fbf5004c923a2526f35cd2a2b925e90ab4b8a
Robert Strandh <robert.strandh@gmail.com>
2024-01-20 11:14:09 +0100

    Remove names already processed by shared IMPORT-FROM-HOST.

--- Code/New-boot/Phase-1/import-from-host.lisp
+++ Code/New-boot/Phase-1/import-from-host.lisp
@@ -1,47 +1,7 @@
 (cl:in-package #:sicl-new-boot-phase-1)
 
 (defparameter *host-function-names*
-  '(;; Arithmetic
-    + - * / floor ceiling 1+ 1- = /= < > <= >= max min evenp oddp
-    zerop plusp minusp
-    numberp integerp random
-    ;; Conses
-    cons list list* null endp
-    car cdr caar cadr cdar cddr
-    caaar caadr cadar caddr cdaar cdadr cddar cdddr
-    cddadr
-    first second third fourth fifth sixth seventh eighth ninth tenth
-    rest last butlast nth nthcdr append getf
-    consp listp atom member mapcar mapc set-difference set-exclusive-or
-    assoc ldiff tailp list-length make-list union
-    adjoin
-    ;; Sequences
-    elt subseq reduce length reverse nreverse
-    count count-if count-if-not
-    find find-if find-if-not
-    position position-if position-if-not
-    remove remove-if remove-if-not
-    delete delete-if delete-if-not
-    remove-duplicates sort
-    ;; Symbols
-    gensym symbolp symbol-name
-    ;; Strings
-    string stringp
-    ;; Characters
-    char
-    ;; Conditions
-    error warn break assert
-    ;; Evaluation and compilation
-    constantp
-    ;; Data and control flow
-    values not funcall apply eq eql equal identity functionp every
-    ;; Printer
-    format
-    ;; Environment
-    documentation
-    ;; Hash tables
-    make-hash-table gethash hash-table-count
-    ;; Objects
+  '(;; Objects
     initialize-instance reinitialize-instance shared-initialize
     slot-boundp slot-value allocate-instance class-of))
 
