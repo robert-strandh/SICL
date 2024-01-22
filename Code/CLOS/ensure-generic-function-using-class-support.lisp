9c7b6f93c3a820df8004858ab7a0b3f2b98d785d
Robert Strandh <robert.strandh@gmail.com>
2024-01-22 06:43:54 +0100

    Factor some code.

--- Code/CLOS/ensure-generic-function-using-class-support.lisp
+++ Code/CLOS/ensure-generic-function-using-class-support.lisp
@@ -1,5 +1,29 @@
 (cl:in-package #:sicl-clos)
 
+(defun canonicalize-generic-function-class (generic-function-class)
+  (cond ((symbolp generic-function-class)
+         (find-class generic-function-class t environment))
+        ((member (find-class 'generic-function)
+                 (class-precedence-list generic-function-class))
+         generic-function-class)
+        (t
+         (error 'generic-function-class-must-be-class-or-name
+                :object generic-function-class))))
+
+(defun canonicalize-method-class (method-class)
+  (cond ((symbolp method-class)
+         (find-class method-class t environment))
+        ((member (find-class 'method) (class-precedence-list method-class))
+         method-class)
+        (t
+         (error "method class must be a class or a name"))))
+
+(defun canonicalize-keyword-arguments (keyword-arguments)
+  (let ((result (copy-list keyword-arguments)))
+    (loop while (remf remaining-keys :generic-function-class))
+    (loop while (remf remaining-keys :environment))
+    result))
+
 (defun ensure-generic-function-using-class-null
     (generic-function
      function-name
@@ -13,26 +37,10 @@
        (method-combination nil method-combination-p)
      &allow-other-keys)
   (declare (ignore generic-function))
-  (cond ((symbolp generic-function-class)
-         (setf generic-function-class
-               (find-class generic-function-class t environment)))
-        ((member (find-class 'generic-function)
-                 (class-precedence-list generic-function-class))
-         nil)
-        (t
-         (error 'generic-function-class-must-be-class-or-name
-                :object generic-function-class)))
+  (setf generic-function-class
+        (canonicalize-generic-function-class generic-function-class))
   (when method-class-p
-    (cond ((symbolp method-class)
-           (let ((class (find-class method-class t environment)))
-             (when (null class)
-               (error 'no-such-method-class :class-name method-class))
-             (setf method-class class)))
-          ((member (find-class 'method)
-                   (class-precedence-list method-class))
-           nil)
-          (t
-           (error "method class must be a class or a name"))))
+    (setf method-class (canonicalize-method-class method-class)))
   (unless method-combination-p
     ;; Neither the Common Lisp standard nor the AMOP indicates where
     ;; this keyword argument is defaulted, but it has to be here,
@@ -43,9 +51,8 @@
     (let ((proto (class-prototype generic-function-class)))
       (setf method-combination
             (find-method-combination proto 'standard '()))))
-  (let ((remaining-keys (copy-list all-keyword-arguments)))
-    (loop while (remf remaining-keys :generic-function-class))
-    (loop while (remf remaining-keys :environment))
+  (let ((remaining-keys
+          (canonicalize-keyword-arguments all-keyword-arguments)))
     (if method-class-p
         (apply #'make-instance generic-function-class
                ;; The AMOP does
@@ -70,31 +77,15 @@
        (method-class nil method-class-p)
      &allow-other-keys)
   (declare (ignore function-name))
-  (cond ((symbolp generic-function-class)
-         (setf generic-function-class
-               (find-class generic-function-class t environment)))
-        ((member (find-class 'generic-function)
-                 (class-precedence-list generic-function-class))
-         nil)
-        (t
-         (error "generic function class must be a class or a name")))
+  (setf generic-function-class
+        (canonicalize-generic-function-class generic-function-class))
   (unless (eq generic-function-class (class-of generic-function))
     (error "classes don't agree ~s and ~s of ~s"
            generic-function-class (class-of generic-function) generic-function))
   (when method-class-p
-    (cond ((symbolp method-class)
-           (let ((class (find-class method-class t environment)))
-             (when (null class)
-               (error "no such method class ~s" method-class))
-             (setf method-class class)))
-          ((member (find-class 'method)
-                   (class-precedence-list method-class))
-           nil)
-          (t
-           (error "method class must be a class or a name"))))
-  (let ((remaining-keys (copy-list all-keyword-arguments)))
-    (loop while (remf remaining-keys :generic-function-class))
-    (loop while (remf remaining-keys :environment))
+    (setf method-class (canonicalize-method-class method-class)))
+  (let ((remaining-keys
+          (canonicalize-keyword-arguments all-keyword-arguments)))
     (if method-class-p
         (apply #'reinitialize-instance generic-function
                :method-class method-class
