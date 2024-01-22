fd080fe00bd643c4e795ab7c8c33ffcde76253cf
Robert Strandh <robert.strandh@gmail.com>
2024-01-22 17:22:13 +0100

    Incorporate definition of generic function.

--- Code/CLOS/ensure-generic-function-using-class-defmethods.lisp
+++ Code/CLOS/ensure-generic-function-using-class-defmethods.lisp
@@ -1,5 +1,27 @@
 (cl:in-package #:sicl-clos)
 
+;;; For the specification of this generic function, see
+;;; http://metamodular.com/CLOS-MOP/ensure-generic-function-using-class.html
+;;;
+;;; We handle an additional keyword argument compared to the
+;;; specification, namely ENVIRONMENT.  It can be used to specify in
+;;; which the generic function is to be found or defined.  It defaults
+;;; to the environment in which this function was loaded.
+(defgeneric ensure-generic-function-using-class
+    (generic-function
+     function-name
+     &key
+       argument-precedence-order
+       declarations
+       documentation
+       generic-function-class
+       lambda-list
+       method-class
+       method-combination
+       name
+       environment
+     &allow-other-keys))
+
 (defun canonicalize-generic-function-class (generic-function-class)
   (cond ((symbolp generic-function-class)
          (find-class generic-function-class t environment))
