34b02c4bb5048fd8f7e777967acb9a4faf79e89d
Robert Strandh <robert.strandh@gmail.com>
2024-01-28 13:59:01 +0100

    Initialize environment and client.

--- Code/New-boot/Phase-2/boot.lisp
+++ Code/New-boot/Phase-2/boot.lisp
@@ -1,4 +1,14 @@
 (cl:in-package #:sicl-new-boot-phase-2)
 
 (defun boot (boot)
-  boot)
+  (let* ((client (make-instance 'client))
+         (environment (create-environment client))
+         (global-environment
+           (trucler:global-environment client environment)))
+    (sb:define-package-functions client global-environment)
+    (setf (sb:e1 boot) global-environment)
+    (reinitialize-instance client
+      :environment global-environment)
+    (sb:define-backquote-macros client global-environment)
+    (sb:import-from-host client global-environment)
+    boot)
