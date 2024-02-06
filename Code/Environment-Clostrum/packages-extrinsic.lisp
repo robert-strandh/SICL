af73204d57744689ec5272062db0cb199d4d5322
Robert Strandh <robert.strandh@gmail.com>
2024-02-06 15:38:21 +0100

    New system for creating an extrinsic package.

---
+++ Code/Environment-Clostrum/packages-extrinsic.lisp
@@ -0,0 +1,14 @@
+(cl:in-package #:common-lisp-user)
+
+(defpackage #:sicl-environment
+  (:use #:common-lisp)
+  (:local-nicknames (#:clo #:clostrum))
+  (:shadow
+   . #1= (#:symbol-function
+          #:fdefinition
+          #:find-class
+          #:find-package
+          #:fboundp
+          #:fmakunbound))
+  (:export
+   . #1#))
---
+++ Code/Environment-Clostrum/sicl-environment-packages-extrinsic.asd
@@ -0,0 +1,6 @@
+(cl:in-package #:asdf-user)
+
+(defsystem "sicl-environment-packages-extrinsic"
+  :serial t
+  :components
+  ((:file "packages-extrinsic")))
