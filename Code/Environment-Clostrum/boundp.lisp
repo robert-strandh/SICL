36550326075610cdfc8f6245558a5a43392e779d
Robert Strandh <robert.strandh@gmail.com>
2024-02-07 07:41:59 +0100

    Do not check for CELL being NIL as it is unnecessary.

--- Code/Environment-Clostrum/boundp.lisp
+++ Code/Environment-Clostrum/boundp.lisp
@@ -1,7 +1,8 @@
 (cl:in-package #:sicl-environment)
 
 (defun boundp (name)
+  (unless (symbolp name)
+    (error 'type-error :datum name :expected-type 'symbol))
   (let ((cell (clostrum-sys:variable-cell *client* *environment* name)))
-    (when (null cell)
-      (setf cell (cons nil nil)))
+    ;; CELL might be NIL, but RT:BOUNDP can deal with that.
     (rt:boundp name cell rt:*dynamic-environment*)))
