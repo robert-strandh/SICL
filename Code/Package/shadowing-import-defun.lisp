e9869e95440224870cc0654034fb10118a451737
Robert Strandh <robert.strandh@gmail.com>
2024-02-08 07:13:20 +0100

    Call Parcl SHADOWING-IMPORT.

--- Code/Package/shadowing-import-defun.lisp
+++ Code/Package/shadowing-import-defun.lisp
@@ -4,15 +4,4 @@
   (let ((package (package-designator-to-package package-designator))
         (symbols (designated-list-of-symbols symbols-designator)))
     (loop for symbol in symbols
-          for name = (symbol-name symbol)
-          for cell = (member name (shadowing-symbols package)
-                             :key #'symbol-name
-                             :test #'equal)
-          do (remhash name (external-symbols package))
-             (remhash name (internal-symbols package))
-             (setf (gethash name (external-symbols package))
-                   symbol)
-             (setf (gethash name (internal-symbols package))
-                   symbol)
-             (unless (null cell)
-               (setf (car cell) symbol)))))
+          do (parcl:shadowing-import env:*client* package symbol))))
