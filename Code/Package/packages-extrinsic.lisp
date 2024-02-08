cd90c985cafdd0cebbd1578c9d3687df0b299dc0
Robert Strandh <robert.strandh@gmail.com>
2024-02-08 06:47:04 +0100

    Introduce package-local nickname ENV for SICL-ENVIRONMENT.

--- Code/Package/packages-extrinsic.lisp
+++ Code/Package/packages-extrinsic.lisp
@@ -2,5 +2,6 @@
 
 (defpackage sicl-package
   (:use #:common-lisp)
+  (:local-nicknames (#:env #:sicl-environment))
   (:shadow . #.asdf-user:*sicl-package-string-designators*)
   (:export . #.asdf-user:*sicl-package-string-designators*))
