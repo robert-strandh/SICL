53f811204e68e6604e76bd549c037baab608df68
Robert Strandh <robert.strandh@gmail.com>
2024-02-08 07:10:33 +0100

    Call Parcl USED-BY-LIST.

--- Code/Package/package-used-by-list-defun.lisp
+++ Code/Package/package-used-by-list-defun.lisp
@@ -2,4 +2,4 @@
 
 (defun package-used-by-list (package-designator)
   (let ((package (package-designator-to-package package-designator)))
-    (copy-list (used-by-list package))))
+    (parcl:used-by-list env:*client* package)))
