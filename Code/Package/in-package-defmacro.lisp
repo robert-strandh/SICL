2718bf1d7f941179cd63c87998f35522c95f4785
Robert Strandh <robert.strandh@gmail.com>
2024-02-09 07:06:26 +0100

    Remove definition of IN-PACKAGE now in Common Macros.

--- Code/Package/in-package-defmacro.lisp
+++
@@ -1,5 +0,0 @@
-(cl:in-package #:sicl-package)
-
-(defmacro in-package (string-designator)
-  `(eval-when (:compile-toplevel :load-toplevel :execute)
-     (setq *package* (find-package ',string-designator))))
