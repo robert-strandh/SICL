f05469fff2e1c04faca777c5c5f31a92fe448c69
Robert Strandh <robert.strandh@gmail.com>
2024-02-09 16:37:46 +0100

    Define package-local nickname ENV for SICL-ENVIRONMENT.

--- Code/New-boot/Phase-1/packages.lisp
+++ Code/New-boot/Phase-1/packages.lisp
@@ -3,6 +3,7 @@
 (defpackage #:sicl-new-boot-phase-1
   (:use #:common-lisp)
   (:local-nicknames
+   (#:env #:sicl-environment)
    (#:sb #:sicl-new-boot)
    (#:cb #:common-boot)
    (#:clo #:clostrum)
