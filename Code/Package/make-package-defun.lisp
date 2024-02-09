4c5110b4f0d64ff025dfd5f1bc5365bff4354f42
Robert Strandh <robert.strandh@gmail.com>
2024-02-09 07:02:30 +0100

    Call Parcl MAKE-PACKAGE to create the package.

--- Code/Package/make-package-defun.lisp
+++ Code/Package/make-package-defun.lisp
@@ -9,27 +9,9 @@
            :datum use))
   (let* ((name-string (string name))
          (nickname-strings (mapcar #'string nicknames))
-         (all-name-strings (cons name-string nickname-strings))
-         (existing-packages
-           (loop for name in all-name-strings
-                 for package = (find-package name)
-                 unless (null package)
-                   collect package)))
-    (loop until (null existing-packages)
-          do (restart-case (error 'package-already-exists
-                                  :packages existing-packages)
-               (force ()
-                 :report (lambda (stream)
-                           (format stream
-                                   "Replace the existing packages."))
-                 (mapc #'delete-package existing-packages)
-                 (setf existing-packages '()))))
-    (let ((package (make-instance 'package
-                     :name name-string
-                     :nicknames nickname-strings
-                     ;; FIXME: create with a use list of '() and then
-                     ;; import and check for errors.
-                     :use-list use)))
-      (loop for name in all-name-strings
-            do (setf (%find-package name) package))
-      package)))
+         (used-packages (mapcar #'package-designator-to-package use))
+         (result (parcl:make-package env*client* name-string)))
+    (setf (parcl:nicknames env:*client* result) nickname-strings)
+    (loop for name-string in (cons name-string nickname-strings)
+          do (setf (env:find-package name-string) result))
+    result))
