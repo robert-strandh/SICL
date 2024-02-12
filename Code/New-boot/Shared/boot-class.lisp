e6926f8f411e9c373387fff289951ba8138fa2ee
Robert Strandh <robert.strandh@gmail.com>
2024-02-12 15:21:15 +0100

    Add slots for the clients in different phases.

--- Code/New-boot/Shared/boot-class.lisp
+++ Code/New-boot/Shared/boot-class.lisp
@@ -2,5 +2,8 @@
 
 (defclass boot ()
   ((%e1 :accessor e1)
+   (%c1 :accessor c1)
    (%e2 :accessor e2)
-   (%e3 :accessor e3)))
+   (%c2 :accessor c2)
+   (%e3 :accessor e3)
+   (%c3 :accessor c3)))
