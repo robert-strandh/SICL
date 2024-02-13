b9cca5b272f5c20d5d8da083bf20c8d6201d01df
Robert Strandh <robert.strandh@gmail.com>
2024-02-13 17:54:49 +0100

    Add keyword argument ENVIRONMENT.

--- Code/Environment-Clostrum/fboundp.lisp
+++ Code/Environment-Clostrum/fboundp.lisp
@@ -1,4 +1,8 @@
 (cl:in-package #:sicl-environment)
 
-(defun fboundp (function-name)
-  (clo:fboundp *client* *environment* function-name))
+;;; I am not sure whether the standard allows the implementation to
+;;; add keyword arguments to standard functions that don't already
+;;; have some.  But for now, I am going to assume that it is allowed.
+
+(defun fboundp (function-name &key (environment *environment*))
+  (clo:fboundp *client* environment function-name))
--- Code/Environment-Clostrum/fdefinition.lisp
+++ Code/Environment-Clostrum/fdefinition.lisp
@@ -1,8 +1,13 @@
 (cl:in-package #:sicl-environment)
 
-(defun fdefinition (function-name)
-  (clo:fdefinition *client* *environment* function-name))
+;;; I am not sure whether the standard allows the implementation to
+;;; add keyword arguments to standard functions that don't already
+;;; have some.  But for now, I am going to assume that it is allowed.
 
-(defun (setf fdefinition) (new-definition function-name)
-  (setf (clo:fdefinition *client* *environment* function-name)
+(defun fdefinition (function-name &key (environment *environment*))
+  (clo:fdefinition *client* environment function-name))
+
+(defun (setf fdefinition)
+    (new-definition function-name &key (environment *environment*))
+  (setf (clo:fdefinition *client* environment function-name)
         new-definition))
