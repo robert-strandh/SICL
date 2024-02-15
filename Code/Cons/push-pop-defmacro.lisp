58c2e46ba6e5d5912322e67e87669136b2b16dea
Robert Strandh <robert.strandh@gmail.com>
2024-02-15 07:38:24 +0100

    Remove components with code now in Constrictor.

--- Code/Cons/push-pop-defmacro.lisp
+++
@@ -1,9 +0,0 @@
-(cl:in-package #:sicl-cons)
-
-(defmacro push (item place &environment environment)
-  (let* ((global-env (env:global-environment environment)))
-    (push-expander environment item place)))
-
-(defmacro pop (place &environment environment)
-  (let* ((global-env (env:global-environment environment)))
-    (pop-expander environment place)))
