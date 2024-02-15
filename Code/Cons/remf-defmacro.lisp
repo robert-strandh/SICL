0a738d97cdec200d221c7da99690667b1281bcec
Robert Strandh <robert.strandh@gmail.com>
2024-02-15 07:43:47 +0100

    Remove components with code now in Constrictor.

--- Code/Cons/remf-defmacro.lisp
+++
@@ -1,5 +0,0 @@
-(cl:in-package #:sicl-cons)
-
-(defmacro remf (&environment environment place indicator)
-  (let ((global-env (env:global-environment environment)))
-    (remf-expander environment place indicator)))
