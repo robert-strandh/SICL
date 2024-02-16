f694e4e7fc1ab7e169909dc4eb955ba63e30cf1e
Robert Strandh <robert.strandh@gmail.com>
2024-02-16 15:47:55 +0100

    Use reader macro for creating Parcl symbols.

--- Code/New-boot/Phase-2/define-ensure-method-combination-template.lisp
+++ Code/New-boot/Phase-2/define-ensure-method-combination-template.lisp
@@ -1,36 +1,31 @@
 (cl:in-package #:sicl-new-boot-phase-2)
 
+(eval-when (:compile-toplevel) (sb:enable-parcl-symbols client))
+
 (defun define-ensure-method-combination-template (client e1 e2)
-  (let ((symbol
-          (sb:intern-parcl-symbol
-           client
-           "CLOSTROPHILIA"
-           "ENSURE-METHOD-COMBINATION-TEMPLATE")))
-    (setf (clo:fdefinition client e2 symbol)
-          (lambda (&key
-                     name
-                     documentation
-                     variant-signature-determiner
-                     effective-method-form-function)
-            (let ((template
-                    (sicl-environment:find-method-combination-template
-                     name e2)))
-              (when (null template)
-                (let* ((symbol
-                         (sb:intern-parcl-symbol
-                          client
-                          "CLOSTROPHILIA"
-                          "METHOD-COMBINATION-TEMPLATE"))
-                       (class (clo:find-class client e1 symbol t)))
-                  (setf template
-                        (make-instance class
-                          :name name
-                          :documentation documentation
-                          :variant-signature-determiner
-                          variant-signature-determiner
-                          :effective-method-form-function
-                          effective-method-form-function))
-                  (setf (sicl-environment:find-method-combination-template
-                         name e2)
-                        template))))
+  (setf (clo:fdefinition
+         client e2 @clostrophilia:ensure-method-combination-template)
+        (lambda (&key
+                   name
+                   documentation
+                   variant-signature-determiner
+                   effective-method-form-function)
+          (let ((template
+                  (sicl-environment:find-method-combination-template
+                   name e2)))
+            (when (null template)
+              (let* ((class (clo:find-class
+                             client e1
+                             @clostrophilia:method-combination-template t)))
+                (setf template
+                      (make-instance class
+                        :name name
+                        :documentation documentation
+                        :variant-signature-determiner
+                        variant-signature-determiner
+                        :effective-method-form-function
+                        effective-method-form-function))
+                (setf (sicl-environment:find-method-combination-template
+                       name e2)
+                      template)))
             name))))
