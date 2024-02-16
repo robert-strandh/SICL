eebd571a64d968e50b565f3338d1ca44f6df6424
Robert Strandh <robert.strandh@gmail.com>
2024-02-16 15:53:26 +0100

    Use reader macro for creating Parcl symbols.

--- Code/New-boot/Phase-2/define-find-method-combination-template.lisp
+++ Code/New-boot/Phase-2/define-find-method-combination-template.lisp
@@ -1,10 +1,10 @@
 (cl:in-package #:sicl-new-boot-phase-2)
 
+(eval-when (:compile-toplevel) (sb:enable-parcl-symbols client))
+
 (defun define-find-method-combination-template (client e2)
-  (let ((symbol
-          (sb:intern-parcl-symbol
-           client "CLOSTROPHILIA" "FIND-METHOD-COMBINATION-TEMPLATE")))
-    (setf (clo:fdefinition client e2 symbol)
-          (lambda (client name)
-            (declare (ignore client))
-            (sicl-environment:find-method-combination-template name e2)))))
+  (setf (clo:fdefinition
+         client e2 @clostrophilia:find-method-combination-template)
+        (lambda (client name)
+          (declare (ignore client))
+          (sicl-environment:find-method-combination-template name e2))))
