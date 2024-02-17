2f72e3cad5c27fe632bc266ddf1ad10b3ff0b0a0
Robert Strandh <robert.strandh@gmail.com>
2024-02-17 15:01:36 +0100

    Define method on PRINT-OBJECT specialized to ENVIRONMENT.

--- Code/New-boot/Shared/environment.lisp
+++ Code/New-boot/Shared/environment.lisp
@@ -2,3 +2,7 @@
 
 (defclass environment (sicl-environment:run-time-environment)
   ((%name :initarg :name :reader name)))
+
+(defmethod print-object ((object environment) stream)
+  (print-unreadable-object (object stream :type t :identity t)
+    (format stream "~s" (name object))))
