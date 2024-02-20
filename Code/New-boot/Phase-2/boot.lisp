600405d7b903121e2cd544ce11932c5530457040
Robert Strandh <robert.strandh@gmail.com>
2024-02-20 12:55:05 +0100

    Set VALIDATE-SUPERCLASS to return T always in E1.

--- Code/New-boot/Phase-2/boot.lisp
+++ Code/New-boot/Phase-2/boot.lisp
@@ -45,6 +45,16 @@
     (define-ecclesia-functions client (sb:e1 boot) global-environment)
     (sb:ensure-asdf-system
      client environment "clostrophilia-method-combination")
+    ;; VALIDATE-SUPERCLASS is going to be called with the class T as
+    ;; one of the arguments, but in phase 1 we replace the target
+    ;; class T by the host class T so that unspecialized methods, or
+    ;; methods specialized to T use the host class T.  So
+    ;; VALIDATE-SUPERCLASS is not functional as it is in E1.  Sice we
+    ;; do not expect to load code for which VALIDATE-SUPERCLASS
+    ;; returns NIL, we make it always return T.
+    (setf (clo:fdefinition
+           client (sb:e1 boot) @clostrophilia:validate-superclass)
+          (constantly t))
     (sb:ensure-asdf-system
      client environment "clostrophilia-class-hierarchy"))
   boot)
