91533debf67a27ff9d3b09344e87d1b53c4d4449
Robert Strandh <robert.strandh@gmail.com>
2024-02-29 06:43:44 +0100

    Fix incorrect comment and call ENSURE-METHOD-USING-GENERIC-FUNCTION.

--- Code/CLOS/ensure-method-defun.lisp
+++ Code/CLOS/ensure-method-defun.lisp
@@ -2,36 +2,22 @@
 
 ;;; This function is not required by the standard.
 ;;;
-;;; GENERIC-FUNCTION is either a generic-function metaobject, or the
-;;; name of a generic function.  If it is a name, then FBOUNDP,
-;;; FDEFINITION, etc., are called in order to determine whether a
-;;; generic function with that name exists.  If it exists, then it is
-;;; used as is.  If not, ENSURE-GENERIC-FUNCTION is called to turn the
-;;; name into a generic function metaobject.
+;;; GENERIC-FUNCTION-OR-NAME is either a generic-function metaobject,
+;;; or the name of a generic function.  If it is a name, then we call
+;;; ENSURE-GENERIC-FUNCTION to turn it into a generic function.
 
 (defun ensure-method
     (generic-function-or-name
+     &rest initargs
      &key
-       (method-class 'standard-method)
-       (lambda-list nil lambda-list-p)
-       (qualifiers '())
-       (specializers nil specializers-p)
-       (documentation nil)
-       (function nil function-p))
+     &allow-other-keys)
   (let ((generic-function
           (if (or (symbolp generic-function-or-name)
                   (consp  generic-function-or-name))
               (ensure-generic-function generic-function-or-name)
               generic-function-or-name)))
-    (let ((method
-            (^make-instance method-class
-              :lambda-list (extract-lambda-list lambda-list)
-              :qualifiers qualifiers
-              :specializers specializers
-              :documentation documentation
-              :function function)))
-      (add-method generic-function method)
-      method)))
+    (apply #'^ensure-method-using-generic-function
+           generic-function initargs)))
 
 ;;  LocalWords:  specializer specializers SPECIALIZERS designators
 ;;  LocalWords:  designator metaobject
