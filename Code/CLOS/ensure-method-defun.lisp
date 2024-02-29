7a031054ecacf5b74dcecb09e03b4e68da7d7b41
Robert Strandh <robert.strandh@gmail.com>
2024-02-29 06:39:02 +0100

    Move comment to the appropriate component.

--- Code/CLOS/ensure-method-defun.lisp
+++ Code/CLOS/ensure-method-defun.lisp
@@ -8,42 +8,6 @@
 ;;; generic function with that name exists.  If it exists, then it is
 ;;; used as is.  If not, ENSURE-GENERIC-FUNCTION is called to turn the
 ;;; name into a generic function metaobject.
-;;;
-;;; GENERIC-FUNCTION-CLASS is either a class metaobject, or the name
-;;; of a class.  If GENERIC-FUNCTION is a function name and this
-;;; argument is supplied, it is passed as the :GENERIC-FUNCTION-CLASS
-;;; keyword argument to ENSURE-GENERIC-FUNCTION.
-;;;
-;;; METHOD-CLASS is either a class metaobject, or the name of a class.
-;;; Either way, this argument is passed as the first argument to
-;;; MAKE-INSTANCE.  If this argument is not supplied, it defaults to
-;;; the symbol STANDARD-METHOD.
-;;;
-;;; LAMBDA-LIST is the unspecialized lambda list corresponding to the
-;;; lambda list of the method to be created.  An error is signaled if
-;;; this argument is not supplied, or if it is a malformed
-;;; unspecialized lambda list.
-;;;
-;;; QUALIFIERS is a list of method qualifiers.  An error is signaled
-;;; if this argument is not a proper list, or if any element of the
-;;; list is not a non-null atom.  If this argument is not supplied, it
-;;; defaults to the empty list.
-;;;
-;;; SPECIALIZERS is a list of specializer designators.  A specializer
-;;; designator is either a specializer metaobject (designating
-;;; itself), a symbol (designating a class with that name), or a list
-;;; of the form (EQL <object>) (designating an EQL specializer with
-;;; <object> as the specializer object. An error is signaled if this
-;;; argument is not a proper list, or if any element of the list is
-;;; not a specializer designator.  If this argument is not supplied,
-;;; an error is signaled.
-;;;
-;;; DOCUMENTATION is a string or NIL.  An error is signaled if this
-;;; argument is neither a string not NIL.  If this argument is not
-;;; supplied, it defaults to NIL.
-;;;
-;;; FUNCTION is a method function.  If this argument is not supplied,
-;;; an error is signaled.
 
 (defun ensure-method
     (generic-function-or-name
--- Code/CLOS/ensure-method-using-generic-function.lisp
+++ Code/CLOS/ensure-method-using-generic-function.lisp
@@ -1,4 +1,30 @@
 (cl:in-package #:sicl-clos)
+;;;
+;;; UNSPECIALIZED-LAMBDA-LIST is the unspecialized lambda list
+;;; corresponding to the lambda list of the method to be created.  An
+;;; error is signaled if this argument is not supplied, or if it is a
+;;; malformed unspecialized lambda list.
+;;;
+;;; QUALIFIERS is a list of method qualifiers.  An error is signaled
+;;; if this argument is not a proper list, or if any element of the
+;;; list is not a non-null atom.  If this argument is not supplied, it
+;;; defaults to the empty list.
+;;;
+;;; SPECIALIZER-DESIGNATORS is a list of specializer designators.  A
+;;; specializer designator is either a specializer metaobject
+;;; (designating itself), a symbol (designating a class with that
+;;; name), or a list of the form (EQL <object>) (designating an EQL
+;;; specializer with <object> as the specializer object. An error is
+;;; signaled if this argument is not a proper list, or if any element
+;;; of the list is not a specializer designator.  If this argument is
+;;; not supplied, an error is signaled.
+;;;
+;;; DOCUMENTATION is a string or NIL.  An error is signaled if this
+;;; argument is neither a string not NIL.  If this argument is not
+;;; supplied, it defaults to NIL.
+;;;
+;;; FUNCTION is a method function.  If this argument is not supplied,
+;;; an error is signaled.
 
 (defgeneric ensure-method-using-generic-function
     (generic-function
@@ -8,6 +34,8 @@
        specializer-designators
        documentation
        function))
+
+;;; FIXME: check for errors according to comment above.
 
 (defmethod ensure-method-using-generic-function
     ((generic-function standard-generic-function)
