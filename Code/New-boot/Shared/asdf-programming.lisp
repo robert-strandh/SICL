9c8a1c6b42412c165a84ada080cc0014beb1b4ca
Robert Strandh <robert.strandh@gmail.com>
2024-03-01 07:51:30 +0100

    Add keyword parameter for loading the ASDF system file.

--- Code/New-boot/Shared/asdf-programming.lisp
+++ Code/New-boot/Shared/asdf-programming.lisp
@@ -23,13 +23,23 @@
 ;;; ASDF-SYSTEM, in the order determined by dependencies.  ASDF-SYSTEM
 ;;; is either an instance of ASDF/SYSTEM:SYSTEM or a name of an ASDF
 ;;; system.
-(defun ensure-asdf-system (client environment asdf-system)
+;;;
+;;; If the keyword argument LOAD-SYSTEM-FILE is true, then the system
+;;; file itself is loaded as well.  We use this possibility when the
+;;; system file contains code that must be loaded into the
+;;; environment, like when it contains package definitions or other
+;;; code.
+(defun ensure-asdf-system
+    (client environment asdf-system &key load-system-file)
   (let ((asdf-system (asdf:find-system asdf-system)))
     ;; (check-dependencies asdf-system environment)
     (format *trace-output*
             "Loading ASDF system ~s into environment ~a~%"
             (asdf/system:primary-system-name asdf-system)
             (name (trucler:global-environment client environment)))
+    (when load-system-file
+      (let ((path (asdf/system:system-source-file asdf-system)))
+        (load-source-file client path environment)))
     (load-asdf-system-files client environment asdf-system)
     ;; (push asdf-system (loaded-asdf-systems environment))
     (format *trace-output*
