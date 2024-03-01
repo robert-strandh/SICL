c70c9f424c86e1646da4cfb1168f1836b3bc5bfe
Robert Strandh <robert.strandh@gmail.com>
2024-03-01 05:35:01 +0100

    Add a slot PACKAGES to the class BOOT for Parcl extrinsic packages.

--- Code/New-boot/Shared/boot-class.lisp
+++ Code/New-boot/Shared/boot-class.lisp
@@ -1,6 +1,13 @@
 (cl:in-package #:sicl-new-boot)
 
 (defclass boot ()
-  ((%e1 :accessor e1)
+  (;; This slot contains a hash table of extrinsic Parcl packages plus
+   ;; two host packages, namely COMMON-LISP-USER and KEYWORD.
+   ;; Packages created as a result of loading code into a Clostrum
+   ;; environment are contained in this table.
+   (%packages
+    :initform (make-hash-table :test #'equal)
+    :reader packages)
+   (%e1 :accessor e1)
    (%e2 :accessor e2)
    (%e3 :accessor e3)))
