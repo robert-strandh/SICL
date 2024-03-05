d8656a43e15b2db5f6bb66ca1ceadd90cb123a40
Robert Strandh <robert.strandh@gmail.com>
2024-03-05 16:55:05 +0100

    Remove obsolete component with code now in Predicament.

--- Code/Conditions/handler-bind-defmacro.lisp
+++
@@ -1,9 +0,0 @@
-(cl:in-package #:sicl-conditions)
-
-(defmacro handler-bind (bindings &body forms)
-  (flet ((make-binding (binding)
-           (destructuring-bind (condition-type function) binding
-             `(cons ',condition-type ,function))))
-    (let ((cluster (mapcar #'make-binding bindings)))
-      `(let ((*handler-clusters* (cons (list ,@cluster) *handler-clusters*)))
-         ,@forms))))
