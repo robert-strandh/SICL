82c8fd17cfabf1416a206b051eb31091a8ae9311
Robert Strandh <robert.strandh@gmail.com>
2024-03-05 16:55:24 +0100

    Delete code now in Prediicament.

--- Code/Conditions/Portable-condition-system/handlers.lisp
+++ Code/Conditions/Portable-condition-system/handlers.lisp
@@ -2,16 +2,6 @@
 
 (in-package #:portable-condition-system)
 
-;;; HANDLER-BIND
-
-(defmacro handler-bind (bindings &body forms)
-  (flet ((make-binding (binding)
-           (destructuring-bind (condition-type function) binding
-             `(cons ',condition-type ,function))))
-    (let ((cluster (mapcar #'make-binding bindings)))
-      `(let ((*handler-clusters* (cons (list ,@cluster) *handler-clusters*)))
-         ,@forms))))
-
 ;;; HANDLER-CASE - main macro body
 
 (defmacro handler-case (form &rest cases)
@@ -20,9 +10,3 @@
       (0 (make-handler-case-without-no-error-case form cases))
       (1 (make-handler-case-with-no-error-case form cases))
       (t (error "Multiple :NO-ERROR cases found in HANDLER-CASE.")))))
-
-;;; IGNORE-ERRORS
-
-(defmacro ignore-errors (&rest forms)
-  `(handler-case (progn ,@forms)
-     (error (condition) (values nil condition))))
