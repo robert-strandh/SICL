a700ce982fe0673581e530714807beef1557b35c
Robert Strandh <robert.strandh@gmail.com>
2024-03-07 11:18:24 +0100

    Remove obsolete code for type proclamations.

--- Code/Conditions/sicl-conditions-type-proclamations.asd
+++
@@ -1,7 +0,0 @@
-(cl:in-package #:asdf-user)
-
-(defsystem #:sicl-conditions-type-proclamations
-  :depends-on (#:sicl-conditions-support)
-  :serial t
-  :components
-  ((:file "type-proclamations")))
--- Code/Conditions/type-proclamations.lisp
+++
@@ -1,9 +0,0 @@
-(cl:in-package #:sicl-conditions)
-
-;;; FIXME: we need some more specific types here.
-(declaim (ftype (function (t &rest list) nil)
-		error))
-
-(declaim (ftype (function (&optional (or null condition)) nil)
-		abort))
-
