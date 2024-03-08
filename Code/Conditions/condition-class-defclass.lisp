4eae34d279bcaf4dc32d04cf25a7c589c3035ed2
Robert Strandh <robert.strandh@gmail.com>
2024-03-08 16:53:55 +0100

    Define methods on READER/WRITER-METHOD-CLASS for CONDITION-CLASS.

--- Code/Conditions/condition-class-defclass.lisp
+++ Code/Conditions/condition-class-defclass.lisp
@@ -2,3 +2,23 @@
 
 (defclass condition-class (clostrophilia:regular-class)
   ())
+
+(defgeneric clostrophilia:reader-method-class
+    (class direct-slot-definition &rest initargs))
+
+(defgeneric clostrophilia:writer-method-class
+    (class direct-slot-definition &rest initargs))
+
+(defmethod clostrophilia:reader-method-class
+    ((class condition-class)
+     (direct-slot-definition clostrophilia:standard-direct-slot-definition)
+     &rest initargs)
+  (declare (ignore initargs))
+  (find-class 'clostrophilia:standard-reader-method))
+
+(defmethod clostrophilia:writer-method-class
+    ((class condition-class)
+     (direct-slot-definition clostrophilia:standard-direct-slot-definition)
+     &rest initargs)
+  (declare (ignore initargs))
+  (find-class 'clostrophilia:standard-writer-method))
