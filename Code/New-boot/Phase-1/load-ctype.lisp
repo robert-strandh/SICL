69ec72891102a6352933c94d80e0ed11ed4f6718
Robert Strandh <robert.strandh@gmail.com>
2024-03-16 08:53:42 +0100

    Load basic systeems for SICL-ARITHMETIC and SICL-ARRAY.

--- Code/New-boot/Phase-1/load-ctype.lisp
+++ Code/New-boot/Phase-1/load-ctype.lisp
@@ -12,5 +12,21 @@
           (declare (ignore environment))
           (eval form)
           nil))
+  ;; The ctype library needs for the system SICL-ARITHMETIC to be
+  ;; loaded.
+  ;; I have no idea why this is necessary.
+  (let ((symbol (find-symbol "LIST-STRUCTURE" "ECCLESIA")))
+    (setf (clo:fdefinition client global-environment symbol)
+          (fdefinition symbol)))
+  (sb:ensure-asdf-system client environment "sicl-arithmetic-base")
+  ;; The ctype library needs for the classes in the module SICL-ARRAY
+  ;; to be defined.
+  (sb:ensure-asdf-system client environment "sicl-array-support")
+  ;; The SICL-ARRAY module defines vector to have SEQUENCE as a
+  ;; superclass, but it doesn't have to work in this phase, so we
+  ;; define SEQUENCE to be the host class T.
+  (setf (clo:find-class client global-environment 'sequence)
+        (find-class 't))
+  (sb:ensure-asdf-system client environment "sicl-array-load-time")
   (let ((*features* '(:sicl)))
     (sb:ensure-asdf-system client environment "ctype")))
