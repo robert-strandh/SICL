f04f67e4a3f581f09de1b975c7b7db45ba8cb77f
Robert Strandh <robert.strandh@gmail.com>
2024-03-23 06:09:15 +0100

    Removefiles containing MOP class definitions, now in Clostrophilia.

--- Code/CLOS/standard-direct-slot-definition-defclass.lisp
+++
@@ -1,5 +0,0 @@
-(cl:in-package #:sicl-clos)
-
-(defclass standard-direct-slot-definition
-    (standard-slot-definition direct-slot-definition)
-  ())
--- Code/CLOS/standard-effective-slot-definition-defclass.lisp
+++
@@ -1,5 +0,0 @@
-(cl:in-package #:sicl-clos)
-
-(defclass standard-effective-slot-definition
-    (standard-slot-definition effective-slot-definition)
-  ())
