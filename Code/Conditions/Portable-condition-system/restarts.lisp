605f36b41418988613826483b735a85aed08a592
Robert Strandh <robert.strandh@gmail.com>
2024-03-24 17:10:57 +0100

    Remove code for macro WITH-CONDITION-RESTARTS, now in Predicament.

--- Code/Conditions/Portable-condition-system/restarts.lisp
+++ Code/Conditions/Portable-condition-system/restarts.lisp
@@ -15,19 +15,3 @@
          (format stream "Invoke restart ~A." (restart-name restart)))
         (t
          (format stream "Invoke the anonymous restart ~S." restart))))
-
-;;; Restart visibility and association
-
-(defmacro with-condition-restarts (condition (&rest restarts) &body body)
-  (let ((condition-var (gensym "CONDITION"))
-        (restarts-var (gensym "RESTARTS"))
-        (restart (gensym "RESTART")))
-    `(let ((,condition-var ,condition)
-           (,restarts-var ,restarts))
-       (unwind-protect
-            (progn
-              (dolist (,restart ,restarts-var)
-                (push ,condition-var (restart-associated-conditions ,restart)))
-              ,@body)
-         (dolist (,restart ,restarts-var)
-           (pop (restart-associated-conditions ,restart)))))))
--- Code/Conditions/restarts.lisp
+++ Code/Conditions/restarts.lisp
@@ -13,17 +13,3 @@
          (format stream "Invoke restart ~A." (restart-name restart)))
         (t
          (format stream "Invoke the anonymous restart ~S." restart))))
-
-(defmacro with-condition-restarts (condition (&rest restarts) &body body)
-  (let ((condition-var (gensym "CONDITION"))
-        (restarts-var (gensym "RESTARTS"))
-        (restart (gensym "RESTART")))
-    `(let ((,condition-var ,condition)
-           (,restarts-var ,restarts))
-       (unwind-protect
-            (progn
-              (dolist (,restart ,restarts-var)
-                (push ,condition-var (restart-associated-conditions ,restart)))
-              ,@body)
-         (dolist (,restart ,restarts-var)
-           (pop (restart-associated-conditions ,restart)))))))
