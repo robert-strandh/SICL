309f19cc15a84c0c0068231b21efa74fd297eed3
Robert Strandh <robert.strandh@gmail.com>
2024-03-26 14:49:20 +0100

    Do not import any classes from the host.

--- Code/New-boot/Phase-2/import-from-host.lisp
+++ Code/New-boot/Phase-2/import-from-host.lisp
@@ -10,11 +10,6 @@
         do (setf (clo:fdefinition client global-environment name)
                  (fdefinition name))))
 
-(defun import-host-classes (client global-environment)
-  (loop for name in '(symbol)
-        do (setf (clo:find-class client global-environment name)
-                 (find-class 't))))
-
 (defparameter *host-setf-functions*
   `(((setf documentation)
      ,(lambda (documentation object documentation-type)
@@ -28,5 +23,4 @@
 
 (defun import-from-host (client global-environment)
   (import-host-functions client global-environment)
-  (import-host-classes client global-environment)
   (define-setf-functions client global-environment))
