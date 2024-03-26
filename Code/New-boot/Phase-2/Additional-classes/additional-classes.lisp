8aea69728f294652c498d48e4d5220597a8e661f
Robert Strandh <robert.strandh@gmail.com>
2024-03-26 14:50:16 +0100

    New system for loading additional classes into E2.

---
+++ Code/New-boot/Phase-2/Additional-classes/additional-classes.lisp
@@ -0,0 +1,39 @@
+(cl:in-package #:sicl-clos)
+
+(defclass sequence (t)
+  ()
+  (:metaclass built-in-class))
+
+(defclass list (sequence) ()
+  (:metaclass built-in-class))
+
+(defclass hash-table () ())
+
+(defclass readtable () ())
+
+(defclass pathname () ())
+
+(defclass stream () ())
+
+(defclass random-state () ())
+
+(defclass structure-object (standard-object) ())
+
+(defclass number (t) ()
+  (:metaclass built-in-class))
+
+(defclass fixnum (number) ()
+  (:metaclass built-in-class))
+
+(defclass cons (t) ()
+  (:metaclass built-in-class))
+
+(defclass character (t) ()
+  (:metaclass built-in-class))
+
+(defclass complex (number) ()
+  (:metaclass built-in-class))
+
+(defclass symbol () ())
+
+(defclass null (symbol list) ())
