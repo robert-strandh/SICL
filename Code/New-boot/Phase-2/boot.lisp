3ec71182bad6406c87b5918168aa6947bd522d8c
Robert Strandh <robert.strandh@gmail.com>
2024-04-01 16:32:02 +0200

    Define ASSERT to do nothing in E2.  Define SHARED-INITIALIZE-AUX-1.

--- Code/New-boot/Phase-2/boot.lisp
+++ Code/New-boot/Phase-2/boot.lisp
@@ -168,5 +168,18 @@
             (clo:fdefinition
              client (sb:e1 boot) @clostrophilia:finalize-inheritance)))
       (funcall finalize-inheritance symbol-class))
+    (setf (clo:fdefinition
+           client global-environment @clostrophilia:shared-initialize-aux-1)
+          (clo:fdefinition
+           client (sb:e1 boot) @clostrophilia:shared-initialize-aux))
+    ;; ctype uses ASSERT and ASSERT expands to RESTART-CASE which
+    ;; contains a TYPECASE which expands to TYPEP, but we don't have
+    ;; TYPEP since the very purpose of ctype is to define TYPEP.  But
+    ;; assertions should not fail at this point in the process anyway.
+    (setf (clo:macro-function client global-environment 'assert)
+          (lambda (form environment)
+            ;; We might put some trace output here.
+            (declare (ignore form environment))
+            nil))
     (load-ctype client environment global-environment))
   boot)
