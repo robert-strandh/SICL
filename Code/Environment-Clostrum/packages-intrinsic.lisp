1643625810b283f48ed766da2d48cb79dc0f1ba8
Robert Strandh <robert.strandh@gmail.com>
2024-04-17 16:18:08 +0200

    Shadow and export names of Clostrum operators that are also in CL.

--- Code/Environment-Clostrum/packages-intrinsic.lisp
+++ Code/Environment-Clostrum/packages-intrinsic.lisp
@@ -2,6 +2,15 @@
 
 (defpackage #:sicl-environment
   (:use #:common-lisp)
+  (:shadow
+   .
+   #1=(#:fdefinition
+       #:find-class
+       #:fboundp
+       #:macro-function
+       #:compiler-macro-function
+       #:boundp
+       #:symbol-value))
   (:local-nicknames
    (#:clo #:clostrum)
    (#:rt #:sicl-run-time))
@@ -11,4 +20,5 @@
    #:type-expander
    #:define-constant
    #:run-time-environment
-   #:find-method-combination-template))
+   #:find-method-combination-template
+   . #1#))
