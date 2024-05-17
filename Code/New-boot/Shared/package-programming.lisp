c46435bbdd6c2cd4a9fc11a8e69075cbae0f389d
Robert Strandh <robert.strandh@gmail.com>
2024-05-17 15:55:19 +0200

    Enable Parcl symbols in this component.

--- Code/New-boot/Shared/package-programming.lisp
+++ Code/New-boot/Shared/package-programming.lisp
@@ -1,5 +1,7 @@
 (cl:in-package #:sicl-new-boot)
 
+(eval-when (:compile-toplevel) (enable-parcl-symbols client))
+
 (defun current-package (client)
   (let* ((global-environment (environment client))
          (package-cell
