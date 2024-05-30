c10f656ab205fc9a2f33b4c454a73def7e86fd64
Robert Strandh <robert.strandh@gmail.com>
2024-05-30 13:20:23 +0200

    Adapt code and comments to be shared.

--- Code/New-boot/Shared/define-ecclesia-functions.lisp
+++ Code/New-boot/Shared/define-ecclesia-functions.lisp
@@ -1,25 +1,25 @@
-(cl:in-package #:sicl-new-boot-phase-2)
+(cl:in-package #:sicl-new-boot)
 
-(eval-when (:compile-toplevel) (sb:enable-parcl-symbols client))
+(eval-when (:compile-toplevel) (enable-parcl-symbols client))
 
 ;;; Before we can start creating generic functions, we need to define
 ;;; method combinations, but method combinations reqiure a few
 ;;; functions from Ecclesia.  But we can't load Ecclesia first because
 ;;; it contains definitions of generic functions.  As it turn out, we
 ;;; loaded Ecclesia in phase 1, so it is present in environment E1.
-;;; So what we do here is to incorporate the functions we need in E2
+;;; So what we do here is to incorporate the functions we need in E
 ;;; taken from E1.
 
-(defun define-ecclesia-functions (client e1 e2)
+(defun define-ecclesia-functions (client e1 e)
   (let ((symbol @ecclesia:proper-list-p))
-    (setf (clo:fdefinition client e2 symbol)
+    (setf (clo:fdefinition client e symbol)
           (clo:fdefinition client e1 symbol)))
   (let ((symbol @ecclesia:extract-lambda-list-variables))
-    (setf (clo:fdefinition client e2 symbol)
+    (setf (clo:fdefinition client e symbol)
           (clo:fdefinition client e1 symbol)))
   (let ((symbol @ecclesia:separate-function-body))
-    (setf (clo:fdefinition client e2 symbol)
+    (setf (clo:fdefinition client e symbol)
           (clo:fdefinition client e1 symbol)))
   (let ((symbol @ecclesia:list-structure))
-    (setf (clo:fdefinition client e2 symbol)
+    (setf (clo:fdefinition client e symbol)
           (clo:fdefinition client e1 symbol))))
