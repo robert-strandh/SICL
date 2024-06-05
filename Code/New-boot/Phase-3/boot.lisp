33bb2f3b074cfd21bc072e1611ec44c63fbdbcbe
Robert Strandh <robert.strandh@gmail.com>
2024-06-05 15:25:59 +0200

    Load system SLOT-VALUE-ETC and define function FIND-CLASS-T.

--- Code/New-boot/Phase-3/boot.lisp
+++ Code/New-boot/Phase-3/boot.lisp
@@ -23,6 +23,7 @@
     (sb:define-environment-functions client global-environment)
     (sb:define-clostrophilia-find-method-combination-template
         client global-environment)
+    (sb:ensure-asdf-system client environment "clostrophilia-slot-value-etc")
     (sb:define-straddle-functions client global-environment (sb:e2 boot))
     (sb:ensure-asdf-system client environment "sicl-clos-ensure-metaobject")
     (sb:define-ecclesia-functions client (sb:e1 boot) global-environment)
@@ -30,6 +31,10 @@
      client environment "clostrophilia-method-combination")
     (setf (clo:fdefinition client (sb:e2 boot) @sicl-clos:subtypep-1)
           (constantly t))
+    (setf (clo:fdefinition
+           client (sb:e2 boot) @clostrophilia:find-class-t)
+          (lambda ()
+            (clo:find-class client global-environment 't)))
     (sb:ensure-asdf-system
      client environment "clostrophilia-class-hierarchy"))
   boot)
