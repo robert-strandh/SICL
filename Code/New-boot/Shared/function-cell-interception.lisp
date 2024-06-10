5dc6e07cf6fa73fd234b8b7cdfa06927f43db14a
Robert Strandh <robert.strandh@gmail.com>
2024-06-10 11:23:50 +0200

    Define variable *INTERCEPTED-CELLS*

--- Code/New-boot/Shared/function-cell-interception.lisp
+++ Code/New-boot/Shared/function-cell-interception.lisp
@@ -1,5 +1,9 @@
 (cl:in-package #:sicl-new-boot)
 
+;;; This variable contains an association list where the key is the
+;;; name of function and the value is a function cell.
+(defparameter *intercepted-cells* '())
+
 (defmethod clostrum-sys:ensure-operator-cell :around
     ((client client) environment operator-name)
   (call-next-method))
