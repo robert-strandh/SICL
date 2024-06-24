d4bdf30075d84d74516778462b1bb6039420e155
Robert Strandh <robert.strandh@gmail.com>
2024-06-24 16:12:07 +0200

    Define method on TRUCLER:DESCRIBE-FUNCTION to avoid warnings.

--- Code/New-boot/Shared/function-cell-interception.lisp
+++ Code/New-boot/Shared/function-cell-interception.lisp
@@ -11,6 +11,14 @@
         (call-next-method)
         (cdr entry))))
 
+(defmethod trucler:describe-function :around
+    ((client client) environment operator-name)
+  (let ((entry (assoc operator-name *intercepted-cells* :test #'equal)))
+    (if (null entry)
+        (call-next-method)
+        (make-instance 'trucler:global-function-description
+          :name operator-name))))
+
 (defmacro with-intercepted-function-cells ((&body clauses) &body body)
   `(let ((*intercepted-cells*
            (append (list ,@(loop for (name cell) in clauses
