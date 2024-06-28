9a256c0a2b3165a71d2c741b02cc251c00d02280
Robert Strandh <robert.strandh@gmail.com>
2024-06-28 14:03:46 +0200

    Add FIXME comment.

--- Code/New-boot/Phase-1/boot.lisp
+++ Code/New-boot/Phase-1/boot.lisp
@@ -237,6 +237,8 @@
     ;; The ctype library calls the function SICL-TYPE:TYPE-EXPAND, so
     ;; we need to have the package SICL-TYPE defined.
     (sb:ensure-asdf-system client environment "sicl-type-support")
+    ;; FIXME: TYPEXPAND should be defined by code from SICL-TYPE being
+    ;; loaded, rather than by defining it here. 
     (setf (clo:fdefinition client global-environment @sicl-type:typexpand)
           (lambda (type-specifier &optional (environment global-environment))
             (clo:type-expand client environment type-specifier)))
