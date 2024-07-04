ec354dd5cde50a9fd16373353d5b82da7a8c5d27
Robert Strandh <robert.strandh@gmail.com>
2024-07-04 14:02:29 +0200

    Make MACLINA.VM-CROSS:CLIENT a superclass of CLIENT.

--- Code/New-boot/Shared/client.lisp
+++ Code/New-boot/Shared/client.lisp
@@ -2,5 +2,6 @@
 
 (defclass client (cb:macro-transforming-client
                   eclector.concrete-syntax-tree:cst-client
+                  maclina.vm-cross:client
                   parcl-low-class:client)
   ((%environment :initarg :environment :reader environment)))
