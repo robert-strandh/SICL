d57d53518f76d1dcbab6225c481cd9894b63438d
Robert Strandh <robert.strandh@gmail.com>
2024-07-06 14:39:10 +0200

    Remove obsolete system.

--- Code/Boot/Condition-system/packages.lisp
+++
@@ -1,8 +0,0 @@
-(cl:in-package #:common-lisp-user)
-
-(defpackage #:sicl-boot-condition-system
-  (:use #:common-lisp)
-  (:import-from #:sicl-boot
-                #:import-functions-from-host)
-  (:local-nicknames (#:env #:sicl-environment))
-  (:export #:boot))
