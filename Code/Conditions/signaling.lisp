d73a4d55ada90daa182170ec370cd3efd895237f
Robert Strandh <robert.strandh@gmail.com>
2024-07-08 11:05:37 +0200

    Remove obsolete components.

--- Code/Conditions/signaling.lisp
+++
@@ -1,7 +0,0 @@
-(cl:in-package #:sicl-conditions)
-
-(defun cerror (continue-string datum &rest arguments)
-  (with-simple-restart
-      (continue "~A" (apply #'format nil continue-string arguments))
-    (apply #'error datum arguments))
-  nil)
