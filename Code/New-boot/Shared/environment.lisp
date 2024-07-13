8f35e40d9f314b1384505e76dd79c973f563950c
Robert Strandh <robert.strandh@gmail.com>
2024-07-13 16:31:20 +0200

    Define :AROUND method on TRUCLER:DESCRIBE-VARIABLE.

--- Code/New-boot/Shared/environment.lisp
+++ Code/New-boot/Shared/environment.lisp
@@ -6,3 +6,11 @@
 (defmethod print-object ((object environment) stream)
   (print-unreadable-object (object stream :type t :identity t)
     (format stream "~s" (name object))))
+
+(defmethod trucler:describe-variable :around
+    ((client client) environment name)
+  (if (or (keywordp name) (member name '(T NIL)))
+      (make-instance 'trucler:constant-variable-description
+        :name name
+        :value name)
+      (call-next-method)))
