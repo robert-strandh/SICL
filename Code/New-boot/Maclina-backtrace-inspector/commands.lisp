c16a0fea3daf3fd9dd836b23c83a4761a0205927
Robert Strandh <robert.strandh@gmail.com>
2024-07-21 10:30:37 +0200

    New component containing commands for Maclina backtrace inspector.

---
+++ Code/New-boot/Maclina-backtrace-inspector/commands.lisp
@@ -0,0 +1,29 @@
+(cl:in-package #:maclina-backtrace-inspector)
+
+(define-inspector-command (com-quit :name t) ()
+  (clim:frame-exit clim:*application-frame*))
+
+(define-inspector-command (com-show-source :name t)
+    ((entry 'cbae:stack-entry))
+  (setf (current-entry clim:*application-frame*)
+        entry))
+
+(clim:define-presentation-to-command-translator show-source
+    (cbae:stack-entry
+     com-show-source
+     inspector
+     :documentation "Show Source")
+    (object)
+  (list object))
+
+(define-inspector-command (com-inspect-argument :name t)
+    ((argument 'argument))
+  (clouseau:inspect argument))
+
+(clim:define-presentation-to-command-translator inspect-argument
+    (argument
+     com-inspect-argument
+     inspector
+     :documentation "Inspect Argument")
+    (object)
+  (list object))
