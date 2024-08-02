c666a786ef985beffd641fe09a6d9431d3a7cabc
Robert Strandh <robert.strandh@gmail.com>
2024-08-02 15:29:31 +0200

    Call function FINALIZE-INHERITANCE from main boot entry point.

--- Code/New-boot/Phase-5/boot.lisp
+++ Code/New-boot/Phase-5/boot.lisp
@@ -1,4 +1,8 @@
 (cl:in-package #:sicl-new-boot-phase-5)
 
 (defun boot (boot)
-  (declare (ignore boot)))
+  (format *trace-output* "**************** Phase 5~%")
+  (let ((e3 (sb:e3 boot))
+        (e4 (sb:e4 boot))
+        (client (make-instance 'client)))
+    (finalize-inheritance client e3 e4)))
