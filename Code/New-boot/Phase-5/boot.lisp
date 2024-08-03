2e5928371084343ea99c01e3ecff1de71c076318
Robert Strandh <robert.strandh@gmail.com>
2024-08-03 09:01:04 +0200

    Enable Parcl symbols in main boot function.

--- Code/New-boot/Phase-5/boot.lisp
+++ Code/New-boot/Phase-5/boot.lisp
@@ -1,5 +1,7 @@
 (cl:in-package #:sicl-new-boot-phase-5)
 
+(eval-when (:compile-toplevel) (sb:enable-parcl-symbols client))
+
 (defun boot (boot)
   (format *trace-output* "**************** Phase 5~%")
   (let ((e3 (sb:e3 boot))
