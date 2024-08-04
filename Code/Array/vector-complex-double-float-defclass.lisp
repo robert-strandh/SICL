1305b30d33007ed8b4e76ad492cec6e35d599d0c
Robert Strandh <robert.strandh@gmail.com>
2024-08-04 14:24:45 +0200

    Remove obsolete vector class definitions, now in Regalia.

--- Code/Array/vector-complex-double-float-defclass.lisp
+++
@@ -1,4 +0,0 @@
-(cl:in-package #:sicl-array)
-
-(defclass vector-complex-double-float (vector array-complex-double-float)
-  ())
--- Code/Array/vector-complex-single-float-defclass.lisp
+++
@@ -1,4 +0,0 @@
-(cl:in-package #:sicl-array)
-
-(defclass vector-complex-single-float (vector array-complex-single-float)
-  ())
