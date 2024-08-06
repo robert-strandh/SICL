688f2a0f05b2761f752cd14f6e203741cb167b8f
Robert Strandh <robert.strandh@gmail.com>
2024-08-06 09:18:02 +0200

    Remove obsolete system and component.

--- Code/Symbol/sicl-symbol-type-proclamations.asd
+++
@@ -1,6 +0,0 @@
-(cl:in-package #:asdf-user)
-
-(defsystem #:sicl-symbol-type-proclamations
-  :depends-on (#:sicl-symbol-support)
-  :serial t
-  :components ((:file "type-proclamations")))
--- Code/Symbol/type-proclamations.lisp
+++
@@ -1,7 +0,0 @@
-(cl:in-package #:sicl-symbol)
-
-(proclaim '(ftype (function (&optional (or string (integer 0))) symbol)
-            gensym))
-
-(proclaim '(ftype (function (t) t)
-            symbolp))
