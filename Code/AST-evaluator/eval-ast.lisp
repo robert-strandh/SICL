d9ae79335ac103e7d738d4a7405bceb8dff6dca7
Robert Strandh <robert.strandh@gmail.com>
2024-09-25 07:24:16 +0200

    Remove old AST evaluator.

--- Code/AST-evaluator/eval-ast.lisp
+++
@@ -1,5 +0,0 @@
-(cl:in-package #:sicl-ast-evaluator)
-
-(defun eval-ast (client environment ast)
-  (let ((code (translate-top-level-ast client ast)))
-    (funcall (compile nil code) environment)))
