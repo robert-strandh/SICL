7a13ec7176bbefbc84222aff56c9862f442a0748
Robert Strandh <robert.strandh@gmail.com>
2024-09-25 07:28:13 +0200

    Remove obsolete system, now replaced by Iconoclast.

--- Code/Cleavir/Abstract-syntax-tree/Visualizer/color.lisp
+++
@@ -1,9 +0,0 @@
-(cl:in-package #:cleavir-ast-visualizer)
-
-(defgeneric background-color (ast))
-
-(defmethod background-color (ast)
-  clim:+white+)
-
-(defmethod background-color ((ast cleavir-ast:literal-ast))
-  clim:+pink+)
