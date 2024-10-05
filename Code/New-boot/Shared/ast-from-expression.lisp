544d3b028a64ea706c7ff968c2d03a5c609326c0
Robert Strandh <robert.strandh@gmail.com>
2024-10-05 15:56:56 +0200

    New file containing definition of AST-FROM-EXPRESSION.

---
+++ Code/New-boot/Shared/ast-from-expression.lisp
@@ -0,0 +1,16 @@
+(cl:in-package #:sicl-new-boot)
+
+(defun ast-from-expression (expression boot)
+  (let* ((*boot* boot)
+         (global-environment (e4 boot))
+         (package (find-package '#:sicl-new-boot-phase-4))
+         (client-symbol (find-symbol "CLIENT" package))
+         (client-object (make-instance client-symbol
+                          :environment global-environment))
+         (environment
+           (make-instance 'trucler-reference:environment
+             :global-environment global-environment))
+         (eclector.base:*client* client-object)
+         (abp:*builder* (cb::make-builder client-object environment))
+         (cst (cst:cst-from-expression expression)))
+    (cst-to-ast client-object cst environment)))
