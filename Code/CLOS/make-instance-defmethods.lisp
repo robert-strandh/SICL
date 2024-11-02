a38d16f124d2e535190d73cb4d443052431291c0
Robert Strandh <robert.strandh@gmail.com>
2024-11-02 06:06:34 +0100

    Replace reference to the HyperSpec by reference to the standard.

--- Code/CLOS/make-instance-defmethods.lisp
+++ Code/CLOS/make-instance-defmethods.lisp
@@ -3,10 +3,10 @@
 (defmethod make-instance ((class symbol) &rest initargs)
   (apply #'make-instance (find-class class) initargs))
 
-;;; The HyperSpec only recognizes methods on MAKE-INSTANCE specialized
-;;; to SYMBOL and STANDARD-CLASS.  However, the AMOP clearly says
-;;; that ENSURE-GENERIC-FUNCTION calls MAKE-INSTANCE, and the default
-;;; class that is instantiated is STANDARD-GENERIC-FUNCTION.  But
+;;; The standaard recognizes only methods on MAKE-INSTANCE specialized
+;;; to SYMBOL and STANDARD-CLASS.  However, the AMOP clearly says that
+;;; ENSURE-GENERIC-FUNCTION calls MAKE-INSTANCE, and the default class
+;;; that is instantiated is STANDARD-GENERIC-FUNCTION.  But
 ;;; STANDARD-GENERIC-FUNCTION is not an instance of STANDARD-CLASS,
 ;;; and instead of FUNCALLABLE-STANDARD-CLASS.
 ;;;
