0cda3ca7ceb959aa83609ee6ad3f7730c473203c
Robert Strandh <robert.strandh@gmail.com>
2024-11-05 06:57:36 +0100

    Set up environments and client.

--- Code/New-boot/Phase-5/boot.lisp
+++ Code/New-boot/Phase-5/boot.lisp
@@ -4,7 +4,13 @@
 
 (defun boot (boot)
   (format *trace-output* "**************** Phase 5~%")
-  (let ((e3 (sb:e3 boot))
-        (e4 (sb:e4 boot))
-        (client (make-instance 'client)))
+  (let* ((e3 (sb:e3 boot))
+         (e4 (sb:e4 boot))
+         (environment
+           (make-instance 'trucler-reference:environment
+             :global-environment e4))
+         (client (make-instance 'client))
+         (env:*client* client)
+         (env:*environment* e4))
+    (reinitialize-instance client :environment e4)
     (finalize-inheritance client e3 e4)))
