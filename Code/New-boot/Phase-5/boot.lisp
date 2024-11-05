1c7eaf3d2f0aa1cd06083cacaa42cadf45ebc636
Robert Strandh <robert.strandh@gmail.com>
2024-11-05 07:05:13 +0100

    Load system for satiation into environment E3.

--- Code/New-boot/Phase-5/boot.lisp
+++ Code/New-boot/Phase-5/boot.lisp
@@ -6,11 +6,13 @@
   (format *trace-output* "**************** Phase 5~%")
   (let* ((e3 (sb:e3 boot))
          (e4 (sb:e4 boot))
-         (environment
-           (make-instance 'trucler-reference:environment
-             :global-environment e4))
          (client (make-instance 'client))
          (env:*client* client)
          (env:*environment* e4))
     (reinitialize-instance client :environment e4)
-    (finalize-inheritance client e3 e4)))
+    (finalize-inheritance client e3 e4)
+    (sb:ensure-asdf-system
+     client
+     (make-instance 'trucler-reference:environment
+       :global-environment e3)
+     "sicl-clos-satiation")))
