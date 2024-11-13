d0f04864a81b8ef8e7e89f58a9ba112b600de5fe
Robert Strandh <robert.strandh@gmail.com>
2024-11-13 16:04:08 +0100

    Add BOOT parameter to new functions.

--- Code/New-boot/Shared/debugging-tools.lisp
+++ Code/New-boot/Shared/debugging-tools.lisp
@@ -11,14 +11,14 @@
 
 ;;; The name of this function means "find symbol" but is shorter for
 ;;; convenience.
-(defun fs (name package-name)
-  (let ((package (gethash package-name (sicl-new-boot::packages *b*))))
+(defun fs (name package-name boot)
+  (let ((package (gethash package-name (sicl-new-boot::packages boot))))
     (parcl-low:find-symbol (make-instance 'client) package name)))
 
 ;;; The name of this function means "find function" but is shorter for
 ;;; convenience.
-(defun ff (function-name package-name environment)
+(defun ff (function-name package-name environment boot)
   (clostrum:fdefinition
    (make-instance 'client)
    environment
-   (fs function-name package-name)))
+   (fs function-name package-name boot)))
