e55fa99b116113e7a16d2967a95d0a3b9f6c7155
Robert Strandh <robert.strandh@gmail.com>
2024-11-15 17:22:22 +0100

    Define method specialized to FUNCALLABLE-STANDARD-CLASS.

--- Code/New-boot/Class-finalization/class-finalization.lisp
+++ Code/New-boot/Class-finalization/class-finalization.lisp
@@ -5,3 +5,9 @@
      &rest initargs
      &key &allow-other-keys)
   (finalize-inheritance class))
+
+(defmethod initialize-instance :after
+    ((class funcallable-standard-class)
+     &rest initargs
+     &key &allow-other-keys)
+  (finalize-inheritance class))
