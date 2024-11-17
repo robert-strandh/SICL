b875b1ce458ce8684936dfba94c795a8cd63b94d
Robert Strandh <robert.strandh@gmail.com>
2024-11-17 14:59:23 +0100

    Define function GENERIC-FUNCTION-IS-A-SLOT-READER-P.

--- Code/New-boot/Phase-5/satiation.lisp
+++ Code/New-boot/Phase-5/satiation.lisp
@@ -4,3 +4,14 @@
     (class metaobject-class class-precedence-list-function)
   (member metaobject-class (funcall class-precedence-list-function class)
           :test #'eq))
+
+(defun generic-function-is-a-slot-reader-p
+    (generic-function
+     generic-function-methods-function
+     class-of-function
+     standard-slot-reader-class)
+  (let ((methods
+          (funcall generic-function-methods-function generic-function)))
+    (and (= (length methods) 1)
+         (eq (funcall class-of-function (first methods))
+             standard-slot-reader-class))))
