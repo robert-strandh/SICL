d770d7b84f08e699a6373a0ea5ea5f21b95882f9
Robert Strandh <robert.strandh@gmail.com>
2024-11-17 14:53:25 +0100

    Define functino CLASS-IS-SUBCLASS-OF-METAOBJECT-P.

--- Code/New-boot/Phase-5/satiation.lisp
+++ Code/New-boot/Phase-5/satiation.lisp
@@ -1,1 +1,6 @@
 (cl:in-package #:sicl-new-boot-phase-5)
+
+(defun class-is-subclass-of-metaobject-p
+    (class metaobject-class class-precedence-list-function)
+  (member metaobject-class (funcall class-precedence-list-function class)
+          :test #'eq))
