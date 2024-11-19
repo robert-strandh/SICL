3ed9b9f35f379f7876a607977667d2b07ecd89b0
Robert Strandh <robert.strandh@gmail.com>
2024-11-19 14:58:14 +0100

    Define function FIND-ALL-STANDARD-GENERIC-FUNCTIONS.

--- Code/New-boot/Phase-5/satiation.lisp
+++ Code/New-boot/Phase-5/satiation.lisp
@@ -71,4 +71,13 @@
                 (funcall method-specializers-function method))
               (specializer (first specializers)))
          (class-is-subclass-of-metaobject-p
-          specializer metaobject-class class-precedence-list-function)))) 
+          specializer metaobject-class class-precedence-list-function))))
+
+(defun find-all-standard-generic-functions
+    (hash-table standard-generic-function-class)
+  (loop for entry being each hash-value of hash-table
+        for cell = (clostrum-basic::cell entry)
+        for operator = (car cell)
+        when (and (typep operator 'sb:header)
+                  (eq (sb:class operator) standard-generic-function-class))
+          collect operator))
