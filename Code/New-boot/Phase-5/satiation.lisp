deae7f1e00edc9327e88d29bd5f32d268e446582
Robert Strandh <robert.strandh@gmail.com>
2024-11-22 15:02:39 +0100

    Remove obsolete code for satiating only accessor.

--- Code/New-boot/Phase-5/satiation.lisp
+++ Code/New-boot/Phase-5/satiation.lisp
@@ -2,133 +2,6 @@
 
 (eval-when (:compile-toplevel) (sb:enable-parcl-symbols client))
 
-(defun class-is-subclass-of-metaobject-p
-    (class metaobject-class class-precedence-list-function)
-  (let ((class-precedence-list
-          (funcall class-precedence-list-function class)))
-    (member metaobject-class class-precedence-list :test #'eq)))
-
-(defun generic-function-is-a-slot-reader-p
-    (generic-function
-     generic-function-methods-function
-     class-of-function
-     standard-reader-method-class)
-  (let ((methods
-          (funcall generic-function-methods-function generic-function)))
-    (and (= (length methods) 1)
-         (eq (funcall class-of-function (first methods))
-             standard-reader-method-class))))
-
-(defun generic-function-is-a-slot-writer-p
-    (generic-function
-     generic-function-methods-function
-     class-of-function
-     standard-writer-method-class)
-  (let ((methods
-          (funcall generic-function-methods-function generic-function)))
-    (and (= (length methods) 1)
-         (eq (funcall class-of-function (first methods))
-             standard-writer-method-class))))
-
-(defun generic-function-is-a-metaobject-slot-reader-p
-    (generic-function
-     generic-function-methods-function
-     class-of-function
-     standard-reader-method-class
-     method-specializers-function
-     metaobject-class
-     class-precedence-list-function)
-  (and (generic-function-is-a-slot-reader-p
-        generic-function
-        generic-function-methods-function
-        class-of-function
-        standard-reader-method-class)
-       (let* ((methods
-                (funcall generic-function-methods-function generic-function))
-              (method (first methods))
-              (specializers
-                (funcall method-specializers-function method))
-              (specializer (first specializers)))
-         (class-is-subclass-of-metaobject-p
-          specializer metaobject-class class-precedence-list-function)))) 
-
-(defun generic-function-is-a-metaobject-slot-writer-p
-    (generic-function
-     generic-function-methods-function
-     class-of-function
-     standard-writer-method-class
-     method-specializers-function
-     metaobject-class
-     class-precedence-list-function)
-  (and (generic-function-is-a-slot-writer-p
-        generic-function
-        generic-function-methods-function
-        class-of-function
-        standard-writer-method-class)
-       (let* ((methods
-                (funcall generic-function-methods-function generic-function))
-              (method (first methods))
-              (specializers
-                (funcall method-specializers-function method))
-              (specializer (second specializers)))
-         (class-is-subclass-of-metaobject-p
-          specializer metaobject-class class-precedence-list-function))))
-
-(defun find-all-standard-generic-functions
-    (hash-table standard-generic-function-class)
-  (loop for entry being each hash-value using (hash-key key) of hash-table
-        for cell = (clostrum-basic::cell entry)
-        for operator = (car cell)
-        when (and (typep operator 'sb:header)
-                  (eq (sb:class operator) standard-generic-function-class))
-          collect (cons key operator)))
-
-(defun satiate-metaobject-slot-accessors-1 (client e3 e4)
-  (let* ((generic-function-methods-function
-           (clo:fdefinition
-            client e3 @clostrophilia:generic-function-methods))
-         (class-of-function
-           (clo:fdefinition
-            client e3 @clostrophilia:class-of))
-         (standard-generic-function-class
-           (clo:find-class client e3 'standard-generic-function))
-         (standard-reader-method-class
-           (clo:find-class client e3 @clostrophilia:standard-reader-method))
-         (standard-writer-method-class
-           (clo:find-class client e3 @clostrophilia:standard-writer-method))
-         (method-specializers-function
-           (clo:fdefinition client e3 @clostrophilia:method-specializers))
-         (metaobject-class
-           (clo:find-class client e4 @clostrophilia:metaobject))
-         (class-precedence-list-function
-           (clo:fdefinition client e3 @clostrophilia:class-precedence-list))
-         (table (clostrum-basic::functions e4))
-         (standard-generic-functions
-           (find-all-standard-generic-functions
-            table standard-generic-function-class))
-         (satiate-function
-           (clo:fdefinition
-            client e3 @clostrophilia:satiate-generic-function)))
-    (loop for (name . function) in standard-generic-functions
-          when (or (generic-function-is-a-metaobject-slot-reader-p
-                    function
-                    generic-function-methods-function
-                    class-of-function
-                    standard-reader-method-class
-                    method-specializers-function
-                    metaobject-class
-                    class-precedence-list-function)
-                   (generic-function-is-a-metaobject-slot-writer-p
-                    function
-                    generic-function-methods-function
-                    class-of-function
-                    standard-writer-method-class
-                    method-specializers-function
-                    metaobject-class
-                    class-precedence-list-function))
-            do (format *trace-output* "Satiating: ~s~%" name)
-               (funcall satiate-function function))))
-
 ;;; A "metaobject method" is what we call a method that has at least
 ;;; one parameter specialized to a subclass of METAOBJECT.  A
 ;;; "metaobject function" is a generic function with at least one
@@ -191,8 +64,9 @@
          (satiate-function
            (clo:fdefinition
             client e3 @clostrophilia:satiate-generic-function)))
-    (loop for entry being each hash-value of table
+    (loop for entry being each hash-value of table using (hash-key name)
           for cell = (clostrum-basic::cell entry)
           for operator = (car cell)
           when (operator-is-a-metaobject-function-p operator)
-            do (funcall satiate-function operator))))
+            do (format *trace-output* "Satiating ~s~%" name)
+               (funcall satiate-function operator))))
