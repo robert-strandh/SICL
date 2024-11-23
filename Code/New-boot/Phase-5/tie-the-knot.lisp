4157ebcdadc6028c3603d37c7e303ba3f4d3e4ef
Robert Strandh <robert.strandh@gmail.com>
2024-11-23 13:55:44 +0100

    Define function TIE-THE-KNOT and auxiliary functions/variables.

--- Code/New-boot/Phase-5/tie-the-knot.lisp
+++ Code/New-boot/Phase-5/tie-the-knot.lisp
@@ -1,6 +1,6 @@
 (cl:in-package #:sicl-new-boot-phase-5)
 
-;;; Create a hash table mapping each classe in E3 to the analogous
+;;; Create a hash table mapping each class in E3 to the analogous
 ;;; class in E4.
 (defun create-class-mapping (e3 e4)
   (let ((table3 (clostrum-basic::types e3))
@@ -16,3 +16,57 @@
                         (class4 (car cell4)))
                    (setf (gethash type3 result) class4))))
     result))
+
+;;; This variable holds a hash table mapping each class in E3 to the
+;;; analogous class in E4.
+(defvar *class-map*)
+
+;;; This variable holds a hash table of visited objects.
+(defvar *visited*)
+
+;;; This variable holds the list of objects yet to be processed.
+(defvar *worklist*)
+
+(defun load-worklist-with-classes (e4)
+  (loop with table = (clostrum-basic::types e4)
+        for entry being each hash-value of table
+        for cell = (clostrum-basic::cell entry)
+        for class = (car cell)
+        do (push class *worklist*)))
+
+(defun replacement (item)
+  (unless (gethash item *visited*)
+    (setf (gethash item *visited*) t)
+    (push item *worklist*))
+  item)
+
+(defgeneric process-item (item))
+
+(defmethod process-item :before (item)
+  (format *trace-output* "Processing: ~s~%" item))
+
+(defmethod process-item (item)
+  nil)
+
+(defmethod process-item ((item sb:header))
+  (setf (gethash item *visited*) t)
+  (let* ((class (sb:class item))
+         (new-class (gethash class *class-map*)))
+    (unless (null new-class)
+      (setf (sb:class item) new-class))
+    (let ((rack (sb:rack item)))
+      (loop with length = (length rack)
+            for i from 0 below length
+            do (setf (aref rack i) (replacement (aref rack i)))))))
+    
+(defmethod process-item ((item cons))
+  (setf (car item) (replacement (car item)))
+  (setf (cdr item) (replacement (cdr item))))
+
+(defun tie-the-knot (e3 e4)
+  (let ((*worklist* '())
+        (*class-map* (create-class-mapping e3 e4))
+        (*visited* (make-hash-table :test #'eq)))
+    (load-worklist-with-classes e4)
+    (loop until (null *worklist*)
+          do (process-item (pop *worklist*)))))
