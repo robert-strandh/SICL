c5b96b18ce493b1d5c8ac7928ab7aa445b384733
Robert Strandh <robert.strandh@gmail.com>
2024-11-24 12:02:09 +0100

    Improve code for tying the knot.

--- Code/New-boot/Phase-5/tie-the-knot.lisp
+++ Code/New-boot/Phase-5/tie-the-knot.lisp
@@ -1,5 +1,7 @@
 (cl:in-package #:sicl-new-boot-phase-5)
 
+(eval-when (:compile-toplevel) (sb:enable-parcl-symbols client))
+
 ;;; Create a hash table mapping each class in E3 to the analogous
 ;;; class in E4.
 (defun create-class-mapping (e3 e4)
@@ -39,38 +41,53 @@
        (not (typep (sb:class object) 'sb:header))))
 
 (defun replacement (item)
-  (unless (gethash item *visited*)
-    (setf (gethash item *visited*) t)
-    (push item *worklist*))
-  item)
+  (if (object-is-an-impure-ersatz-object-p item)
+      ;; It had better be an E3 class.
+      (let ((new-item (gethash item *class-map*)))
+        (assert (not (null new-item)))
+        new-item)
+      (progn (unless (gethash item *visited*)
+               (setf (gethash item *visited*) t)
+               (push item *worklist*))
+             item)))
 
 (defgeneric process-item (item))
-
-(defmethod process-item :before (item)
-  (format *trace-output* "Processing: ~s~%" item))
 
 (defmethod process-item (item)
   nil)
 
+(defvar *class-slots-function*)
+
 (defmethod process-item ((item sb:header))
-  (setf (gethash item *visited*) t)
-  (let* ((class (sb:class item))
-         (new-class (gethash class *class-map*)))
-    (unless (null new-class)
-      (setf (sb:class item) new-class))
-    (let ((rack (sb:rack item)))
-      (loop with length = (length rack)
-            for i from 0 below length
-            do (setf (aref rack i) (replacement (aref rack i)))))))
+  (assert (not (object-is-an-impure-ersatz-object-p item)))
+  (if (object-is-an-impure-ersatz-object-p (sb:class item))
+      ;; This is the case we want to fix up.
+      (let* ((class (sb:class item))
+             (new-class (gethash class *class-map*)))
+        (assert (not (null new-class)))
+        ;; Update the class slot.
+        (setf (sb:class item) new-class)
+        ;; Update the slot with the list of effective slots.
+        (setf (aref (sb:rack item) 1)
+              (funcall *class-slots-function* new-class))
+        (setf (gethash item *visited*) t)
+        (let ((rack (sb:rack item)))
+          (loop with length = (length rack)
+                for i from 0 below length
+                do (setf (aref rack i) (replacement (aref rack i))))))
+      ;; Otherwise this object already has a pure class.  Do nothing.
+      nil))
     
 (defmethod process-item ((item cons))
   (setf (car item) (replacement (car item)))
   (setf (cdr item) (replacement (cdr item))))
 
-(defun tie-the-knot (e3 e4)
+(defun tie-the-knot (client e3 e4)
   (let ((*worklist* '())
         (*class-map* (create-class-mapping e3 e4))
-        (*visited* (make-hash-table :test #'eq)))
+        (*visited* (make-hash-table :test #'eq))
+        (*class-slots-function*
+          (clostrum:fdefinition client e3 @clostrophilia:class-slots)))
     (load-worklist-with-classes e4)
     (loop until (null *worklist*)
           do (process-item (pop *worklist*)))))
