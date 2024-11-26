5c99091233e9f1b835abda55c1f4a8cbbda08eb2
Robert Strandh <robert.strandh@gmail.com>
2024-11-26 11:06:33 +0100

    Eliminate forward referring functions.

--- Code/New-boot/Phase-5/boot.lisp
+++ Code/New-boot/Phase-5/boot.lisp
@@ -13,9 +13,32 @@
          (c3 (make-instance 'client))
          (c4 (make-instance 'client))
          (env:*client* c4)
-         (env:*environment* e4))
+         (env:*environment* e4)
+         (client c4))
     (reinitialize-instance c3 :environment e3)
     (reinitialize-instance c4 :environment e4)
     (sb:ensure-asdf-system
      c3 (wrap-environment e3) "sicl-clos-satiation")
-    (satiate-metaobject-functions-1 c3 e3 e4)))
+    (satiate-metaobject-functions-1 c3 e3 e4)
+    ;; In phase 4, we set FIND-CLASS+1 in E3 to refer to FIND-CLASS in
+    ;; E4, because MAKE-INSTANCE calls FIND-CLASS+1.  In the following
+    ;; code, we detect functions that are shared between E3 and E4,
+    ;; and we assume that the function in E4 must be changed, but
+    ;; that's not the case for FIND-CLASS.  So we remove FIND-CLASS+1
+    ;; from E3, and we set FIND-CLASS+1 in E4, to refer to FIND-CLASS
+    ;; in E4.
+    (flet ((fixup (name+1 name)
+             (clo:fmakunbound c3 e3 name+1)
+             (setf (clo:fdefinition c4 e4 name+1)
+                   (clo:fdefinition c4 e4 name))))
+      (fixup @sicl-clos:find-class+1 @sicl-clos:find-class)
+      (fixup @clostrophilia:slot-boundp-using-location+1
+             @clostrophilia:slot-boundp-using-location)
+      (let ((s+1 @clostrophilia:slot-value-using-location+1)
+            (s @clostrophilia:slot-value-using-location))
+        (fixup s+1 s)
+        (fixup `(setf ,s+1) `(setf ,s)))
+      (fixup @clostrophilia:slot-makunbound-using-location+1
+             @clostrophilia:slot-makunbound-using-location)
+      (fixup @clostrophilia:ensure-generic-function+1
+             'ensure-generic-function))))
