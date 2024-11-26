84f5297b07b432e1184f9cefe10f00efc5e43e5a
Robert Strandh <robert.strandh@gmail.com>
2024-11-26 13:48:36 +0100

    Call function for fixing up backward-referring functions.

--- Code/New-boot/Phase-5/boot.lisp
+++ Code/New-boot/Phase-5/boot.lisp
@@ -20,4 +20,5 @@
     (sb:ensure-asdf-system
      c3 (wrap-environment e3) "sicl-clos-satiation")
     (satiate-metaobject-functions-1 c3 e3 e4)
-    (fix-forward-referring-functions client e3 e4)))
+    (fix-forward-referring-functions client e3 e4)
+    (fix-backward-referring-functions client e3 e4)))
