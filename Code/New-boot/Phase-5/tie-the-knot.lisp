14a3f861750b0fa6fc576246f82866090a5686af
Robert Strandh <robert.strandh@gmail.com>
2024-11-27 15:42:59 +0100

    Set the value of *CLASS-UNIQUE-NUMBER* in E4 to its value in E3.

--- Code/New-boot/Phase-5/tie-the-knot.lisp
+++ Code/New-boot/Phase-5/tie-the-knot.lisp
@@ -14,7 +14,13 @@
   ;; And similar for *FUNCALLABLE-STANDARD-OBJECT*.
   (clo:make-variable
    client e4 @clostrophilia:*funcallable-standard-object*
-   (clo:find-class client e4 @clostrophilia:funcallable-standard-object)))
+   (clo:find-class client e4 @clostrophilia:funcallable-standard-object))
+  ;; The variable *CLASS-UNIQUE-NUMBER* in E3 reflects the value that
+  ;; it should have in E4 when the graph is cyclic.
+  (setf (clo:symbol-value
+         client e4 @clostrophilia:*class-unique-number*)
+        (clo:symbol-value
+         client e3 @clostrophilia:*class-unique-number*)))
 
 ;;; In phase 4, we set a number of function names of the form NAME+1
 ;;; in E3 to refer to the analogous function NAME in E4.  In
