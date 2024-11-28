95966920f346877dd5699abcf9cbb65b5781d8eb
Robert Strandh <robert.strandh@gmail.com>
2024-11-28 14:40:27 +0100

    Fix incorrect package names and variable name.

--- Code/New-boot/Phase-5/tie-the-knot.lisp
+++ Code/New-boot/Phase-5/tie-the-knot.lisp
@@ -24,13 +24,13 @@
   ;; The variable *STANDARD-METHOD-COMBINATION** in E3 reflects the
   ;; value that it should have in E4 when the graph is cyclic.
   (setf (clo:symbol-value
-         client e4 @clostrophilia:*standard-method-combination*)
+         client e4 @sicl-clos:*standard-method-combination*)
         (clo:symbol-value
-         client e3 @clostrophilia:*standard-method-combination*))
+         client e3 @sicl-clos:*standard-method-combination*))
   (setf (clo:symbol-value
-         client e4 @clostrophilia:*eql-specialziers*)
+         client e4 @sicl-clos:*eql-specializers*)
         (clo:symbol-value
-         client e3 @clostrophilia:*eql-specialziers*)))
+         client e3 @sicl-clos:*eql-specializers*)))
 
 ;;; In phase 4, we set a number of function names of the form NAME+1
 ;;; in E3 to refer to the analogous function NAME in E4.  In
