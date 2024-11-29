f2528668d9c341ef8409168821b155daae8a6cd3
Robert Strandh <robert.strandh@gmail.com>
2024-11-29 16:33:09 +0100

    Restore SLOT-VALUE-USING-LOCATION+1 in E3, and add comment.

--- Code/New-boot/Phase-5/boot.lisp
+++ Code/New-boot/Phase-5/boot.lisp
@@ -22,4 +22,25 @@
     (satiate-metaobject-functions-1 c3 e3 e4)
     (fix-forward-referring-functions client e3 e4)
     (fix-backward-referring-functions client e3 e4)
-    (fix-variables client e3 e4)))
+    (fix-variables client e3 e4)
+    ;; Normally, when a method function of a method in E4 calls some
+    ;; other function, the callee will also be in E4.  However, when
+    ;; the effective method involving a slot accessor is computer, the
+    ;; code for computing the effective method substitutes a different
+    ;; method function that calls SLOT-VALUE-USING-LOCATION+1, and
+    ;; since the code for computing the effective method is so far in
+    ;; E3, such a slot accessor will call SLOT-VALUE-USING-LOCATION+1
+    ;; in E3.  But we made SLOT-VALUE-USING-LOCATION+1 in E3 unbound
+    ;; when we fixed forward-referring functions, and we did that so
+    ;; that it would not be incorrectly handled by the code for fixing
+    ;; backward-referring functions.  Unfortunately, that means that,
+    ;; at this point, slot accessors in E4 are not operational.  So we
+    ;; must restore SLOT-VALUE-USING-LOCATION+1 to be an alias for
+    ;; SLOT-VALUE-USING-LOCATION in E4 until we run the satiation code
+    ;; in E4.
+    (let ((s+1 @clostrophilia:slot-value-using-location+1)
+          (s @clostrophilia:slot-value-using-location))
+      (setf (clo:fdefinition c3 e3 s+1)
+            (clo:fdefinition c4 e4 s))
+      (setf (clo:fdefinition c3 e3 `(setf ,s+1))
+            (clo:fdefinition c4 e4 `(setf ,s))))))
