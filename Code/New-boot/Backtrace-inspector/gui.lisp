268edc627ae964f006f6e20b041979be30785e5c
Robert Strandh <robert.strandh@gmail.com>
2024-12-03 14:13:51 +0100

    Display function and arguments even when there is no source info.

--- Code/New-boot/Backtrace-inspector/gui.lisp
+++ Code/New-boot/Backtrace-inspector/gui.lisp
@@ -87,40 +87,46 @@
 
 (defun display-source (frame pane)
   (let ((entry (current-entry frame)))
-    (unless (or (null entry)
-                (null (cb:origin entry)))
-      (let* ((origin (cb:origin entry))
-             (start (car (cst:source origin)))
-             (start-line-index (sicl-source-tracking:line-index start))
-             (start-character-index (sicl-source-tracking:character-index start))
-             (end (cdr (cst:source origin)))
-             (end-line-index (sicl-source-tracking:line-index end))
-             (end-character-index (sicl-source-tracking:character-index end))
-             (lines (sicl-source-tracking:lines start)))
-        (setf (clim:stream-drawing-p pane) nil)
-        ;; Display all the lines preceding the source location
-        (loop for i from 0 below start-line-index
-              do (format pane "~a~%" (aref lines i)))
-        (if (= start-line-index end-line-index)
-            (let ((line (aref lines start-line-index)))
-              (format pane "~a" (subseq line 0 start-character-index))
-              (clim:with-drawing-options (pane :ink clim:+red+)
-                (format pane "~a"
-                        (subseq line start-character-index end-character-index)))
-              (format pane "~a~%" (subseq line end-character-index)))
-            (let ((start-line (aref lines start-line-index))
-                  (end-line (aref lines end-line-index)))
-              (format pane "~a" (subseq start-line 0 start-character-index))
-              (clim:with-drawing-options (pane :ink clim:+red+)
-                (format pane "~a~%" (subseq start-line start-character-index))
-                (loop for i from (1+ start-line-index) below end-line-index
+    (unless (null entry)
+      (let ((origin (cb:origin entry)))
+        (unless (null origin)
+          (let ((source (cst:source origin)))
+            (unless (null source)
+              (let* ((start (car source))
+                     (start-line-index
+                       (sicl-source-tracking:line-index start))
+                     (start-character-index
+                       (sicl-source-tracking:character-index start))
+                     (end (cdr source))
+                     (end-line-index
+                       (sicl-source-tracking:line-index end))
+                     (end-character-index
+                       (sicl-source-tracking:character-index end))
+                     (lines (sicl-source-tracking:lines start)))
+                (setf (clim:stream-drawing-p pane) nil)
+                ;; Display all the lines preceding the source location
+                (loop for i from 0 below start-line-index
                       do (format pane "~a~%" (aref lines i)))
-                (format pane "~a" (subseq end-line 0 end-character-index)))
-              (format pane "~a~%" (subseq end-line end-character-index))))
-        (loop for i from (1+ end-line-index) below (length lines)
-              do (format pane "~a~%" (aref lines i)))
-        (setf (clim:stream-drawing-p pane) t)
-        (clim:replay (clim:stream-output-history pane) pane)))))
+                (if (= start-line-index end-line-index)
+                    (let ((line (aref lines start-line-index)))
+                      (format pane "~a" (subseq line 0 start-character-index))
+                      (clim:with-drawing-options (pane :ink clim:+red+)
+                        (format pane "~a"
+                                (subseq line start-character-index end-character-index)))
+                      (format pane "~a~%" (subseq line end-character-index)))
+                    (let ((start-line (aref lines start-line-index))
+                          (end-line (aref lines end-line-index)))
+                      (format pane "~a" (subseq start-line 0 start-character-index))
+                      (clim:with-drawing-options (pane :ink clim:+red+)
+                        (format pane "~a~%" (subseq start-line start-character-index))
+                        (loop for i from (1+ start-line-index) below end-line-index
+                              do (format pane "~a~%" (aref lines i)))
+                        (format pane "~a" (subseq end-line 0 end-character-index)))
+                      (format pane "~a~%" (subseq end-line end-character-index))))
+                (loop for i from (1+ end-line-index) below (length lines)
+                      do (format pane "~a~%" (aref lines i)))
+                (setf (clim:stream-drawing-p pane) t)
+                (clim:replay (clim:stream-output-history pane) pane)))))))))
 
 (defun inspect (stack &key new-process-p)
   (let ((frame (clim:make-application-frame 'inspector
