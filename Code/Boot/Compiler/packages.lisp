92498e6180adc139cb197c02402b7d778781d9c8
Robert Strandh <robert.strandh@gmail.com>
2024-12-10 14:53:18 +0100

    Remove obsolete system.

--- Code/Boot/Compiler/packages.lisp
+++
@@ -1,6 +0,0 @@
-(cl:in-package #:common-lisp-user)
-
-(defpackage #:sicl-boot-compile-and-tie
-  (:use #:common-lisp)
-  (:local-nicknames (#:env #:sicl-environment))
-  (:export #:compile-and-tie))
