00bf7cb44a66f20c929ec95be6aef17904a4c2d4
Robert Strandh <robert.strandh@gmail.com>
2024-12-14 16:36:16 +0100

    Define variable *SETF-CALL-HISTORY-FUNCTION*.

--- Code/New-boot/Phase-5/set-default-discriminating-function.lisp
+++ Code/New-boot/Phase-5/set-default-discriminating-function.lisp
@@ -4,16 +4,25 @@
 
 (defvar *call-history-function*)
 
+(defvar *setf-call-history-function*)
+
 (defun operator-is-a-fresh-generic-function-p (operator)
   (and (typep operator 'sb:header)
        (eq (sb:class operator) *standard-generic-function-class*)
        (null (funcall *call-history-function* operator))))
+
+(defun operator-is-a-candidate-p (name operator)
+  (declare (ignore name))
+  (operator-is-a-fresh-generic-function-p operator))
 
 (defun set-default-discriminating-functions (client e4)
   (let* ((*standard-generic-function-class*
            (clo:find-class client e4 'standard-generic-function))
          (*call-history-function*
            (clo:fdefinition client e4 @clostrophilia:call-history))
+         (*setf-call-history-function*
+           (clo:fdefinition
+            client e4 (list 'setf @clostrophilia:call-history)))
          (table (clostrum-basic::functions e4))
          (symbol @clostrophilia:compute-and-install-discriminating-function)
          (compute-and-install-discriminating-function-function
@@ -21,8 +30,10 @@
     (loop for entry being each hash-value of table using (hash-key name)
           for cell = (clostrum-basic::cell entry)
           for operator = (car cell)
-          when (operator-is-a-fresh-generic-function-p operator)
+          when (operator-is-a-candidate-p name operator)
             do ; (format *trace-output* "Processing ~s~%" name)
+               (funcall *setf-call-history-function*
+                        '() operator)
                (funcall compute-and-install-discriminating-function-function
                         operator))))
 
