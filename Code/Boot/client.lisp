5849ad5b7c2302b85567ba108f4028c523eedac7
Robert Strandh <robert.strandh@gmail.com>
2024-12-22 14:41:15 +0100

    Remove obsolete components.

--- Code/Boot/client.lisp
+++
@@ -1,5 +0,0 @@
-(cl:in-package #:sicl-boot)
-
-(defclass client (sicl-client:sicl
-                  eclector.concrete-syntax-tree:cst-client)
-  ((%environment :initarg :environment :reader environment)))
