5c88f406b5061da231c0d4d2c0d6cdaf53d40cdc
Robert Strandh <robert.strandh@gmail.com>
2025-01-09 11:56:58 +0100

    Fix incorrect syntax for default value of optional parameter.

--- Code/Data-and-control-flow/get-setf-expansion-defun.lisp
+++ Code/Data-and-control-flow/get-setf-expansion-defun.lisp
@@ -1,5 +1,5 @@
 (in-package #:sicl-data-and-control-flow)
 
 (defun get-setf-expansion
-    (place &optional (environment (env:*environment*)))
+    (place &optional (environment env:*environment*))
   (env:get-setf-expansion env:*client* environment place))
