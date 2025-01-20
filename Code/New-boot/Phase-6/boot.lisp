389d02beea7fd6460036554838d49c39d6fe46c7
Robert Strandh <robert.strandh@gmail.com>
2025-01-20 17:42:50 +0100

    Move code for loading Quaviver to new component.

--- Code/New-boot/Phase-6/boot.lisp
+++ Code/New-boot/Phase-6/boot.lisp
@@ -65,36 +65,4 @@
       (sb:ensure-asdf-system c4 w4 "cyclosis-intrinsic"))
     (clo:make-variable
      client e4 @predicament:*condition-maker* 'make-condition)
-    (loop for symbol in '(most-negative-short-float
-                          most-positive-short-float
-                          least-negative-short-float
-                          least-negative-normalized-short-float
-                          least-positive-short-float
-                          least-positive-normalized-short-float
-                          most-negative-single-float
-                          most-positive-single-float
-                          least-negative-single-float
-                          least-negative-normalized-single-float
-                          least-positive-single-float
-                          least-positive-normalized-single-float
-                          most-negative-double-float
-                          most-positive-double-float
-                          least-negative-double-float
-                          least-negative-normalized-double-float
-                          least-positive-double-float
-                          least-positive-normalized-double-float
-                          most-negative-long-float
-                          most-positive-long-float
-                          least-negative-long-float
-                          least-negative-normalized-long-float
-                          least-positive-long-float
-                          least-positive-normalized-long-float)
-          for value = (symbol-value symbol)
-          do (clo:make-variable client e4 symbol value))
-    (setf (clo:fdefinition c4 e4 'coerce) #'coerce)
-    (setf (clo:fdefinition c4 e4 'float-digits) #'float-digits)
-    (setf (clo:fdefinition c4 e4 'integer-decode-float)
-          #'integer-decode-float)
-    (setf (clo:fdefinition c4 e4 'byte) #'byte)
-    (setf (clo:fdefinition c4 e4 'ldb-test) #'ldb-test)
-    (setf (clo:fdefinition c4 e4 'float-radix) #'float-radix)))
+    (load-quaviver c4 e4)))
