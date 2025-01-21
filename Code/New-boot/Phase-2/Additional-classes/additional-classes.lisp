86984af22fd9a33484a70daa6d3232af871ce208
Robert Strandh <robert.strandh@gmail.com>
2025-01-21 17:02:07 +0100

    Remove number classes from system ADDITIONAL-CLASSES.

--- Code/New-boot/Phase-2/Additional-classes/additional-classes.lisp
+++ Code/New-boot/Phase-2/Additional-classes/additional-classes.lisp
@@ -21,28 +21,10 @@
 
 (defclass structure-object (standard-object) ())
 
-(defclass number (t) ()
-  (:metaclass built-in-class))
-
-(defclass rational (number) ()
-  (:metaclass built-in-class))
-
-(defclass integer (rational) ()
-  (:metaclass built-in-class))
-
-(defclass ratio (rational) ()
-  (:metaclass built-in-class))
-
-(defclass fixnum (integer) ()
-  (:metaclass built-in-class))
-
 (defclass cons (t) ()
   (:metaclass built-in-class))
 
 (defclass character (t) ()
-  (:metaclass built-in-class))
-
-(defclass complex (number) ()
   (:metaclass built-in-class))
 
 (defclass symbol () ())
