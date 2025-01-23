92a83b722779a28eba053e8cdb94b01c437b4a24
Robert Strandh <robert.strandh@gmail.com>
2025-01-23 17:03:07 +0100

    Define MOST-POSITIVE-SINGLE-FLOAT and MOST-POSITIVE-SHORT-FLOAT.

--- Code/New-boot/Phase-6/load-quaviver.lisp
+++ Code/New-boot/Phase-6/load-quaviver.lisp
@@ -7,9 +7,14 @@
             :bit-pattern buoy-simulate:most-positive-single-float)))
     (clo:make-variable c4 e4 'most-positive-short-float single-float)
     (clo:make-variable c4 e4 'most-positive-single-float single-float))
+  (let ((single-float
+          (make-instance 'sb:simulated-single-float
+            :bit-pattern buoy-simulate:most-negative-single-float)))
+    (clo:make-variable c4 e4 'most-negative-short-float single-float)
+    (clo:make-variable c4 e4 'most-negative-single-float single-float))
   (setf (clo:fdefinition c4 e4 'coerce) #'coerce)
   (setf (clo:fdefinition c4 e4 'byte) #'byte)
   (setf (clo:fdefinition c4 e4 'ldb-test) #'ldb-test)
-;;  #+(or)
+  #+(or)
   (let ((*features* '(:sicl)))
     (sb:ensure-asdf-system c4 w4 "quaviver")))
