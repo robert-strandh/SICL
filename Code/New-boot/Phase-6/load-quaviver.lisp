9497f4e63fbf9986f4fe54d24c7d414faa23e931
Robert Strandh <robert.strandh@gmail.com>
2025-01-24 05:51:14 +0100

    Define FLOAT-RADIX.

--- Code/New-boot/Phase-6/load-quaviver.lisp
+++ Code/New-boot/Phase-6/load-quaviver.lisp
@@ -63,6 +63,10 @@
               (let ((symbol @clostrophilia:standard-instance-access))
                 (buoy-simulate:integer-decode-double-float
                  (funcall (clo:fdefinition c4 e4 symbol) float 0))))))
+  (setf (clo:fdefinition c4 e4 'float-radix)
+        (lambda (float)
+          (declare (ignore float))
+          2))
   (setf (clo:fdefinition c4 e4 'coerce) #'coerce)
   (setf (clo:fdefinition c4 e4 'byte) #'byte)
   (setf (clo:fdefinition c4 e4 'ldb-test) #'ldb-test)
