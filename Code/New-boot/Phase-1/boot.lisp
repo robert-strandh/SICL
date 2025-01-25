d54e98bab353d54f7988b7562552318a22b0bc2a
Robert Strandh <robert.strandh@gmail.com>
2025-01-25 08:19:40 +0100

    Load system SICL-PRIMOP early in each phase.

--- Code/New-boot/Phase-1/boot.lisp
+++ Code/New-boot/Phase-1/boot.lisp
@@ -177,6 +177,7 @@
     (clo:make-variable
      client global-environment '*package* (find-package '#:common-lisp-user))
     (sb:fill-environment client global-environment)
+    (sb:ensure-asdf-system client environment "sicl-primop")
     (sb:ensure-asdf-system
      client environment "sicl-environment-clostrum-package")
     (sb:ensure-asdf-system
