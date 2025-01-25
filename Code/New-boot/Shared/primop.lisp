ba8ee031fdec4003cfb87337b4d7650cddf78da6
Robert Strandh <robert.strandh@gmail.com>
2025-01-25 10:55:38 +0100

    Allow for negative fixnums to be added.

--- Code/New-boot/Shared/primop.lisp
+++ Code/New-boot/Shared/primop.lisp
@@ -7,12 +7,12 @@
 ;;; simulating overflow behavior in most architectures.
 (defmethod primop ((operation (eql :fixnum-add)) &rest arguments)
   (destructuring-bind (x y) arguments
-    (check-type x (integer 0))
-    (check-type y (integer 0))
+    (check-type x fixnum)
+    (check-type y fixnum)
     (let ((sum (+ x y)))
-      (if (typep sum 'fixnum)
-          sum
-          (- sum (ash 1 63))))))
+      (cond ((typep sum 'fixnum) sum)
+            ((minusp sum) (+ sum (ash 1 63)))
+            (t (- sum (ash 1 63)))))))
 
 ;;; Multiply two positive fixnums.  Return the result as two values.
 (defmethod primop ((operation (eql :fixnum-multiply)) &rest arguments)
