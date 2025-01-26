6a9172dcd93efe3fdf37d08bbd79824f06e4f436
Robert Strandh <robert.strandh@gmail.com>
2025-01-26 07:45:16 +0100

    Replace CLEAVIR-PRIMOP with SICL-PRIMOP:PRIMOP.

--- Code/Arithmetic/binary-multiply-defmethods.lisp
+++ Code/Arithmetic/binary-multiply-defmethods.lisp
@@ -4,26 +4,26 @@
   (if (minusp multiplier)
       (if (minusp multiplicand)
           (multiple-value-bind (most-significant least-significant)
-              (cleavir-primop:fixnum-multiply (- multiplier) (- multiplicand))
+              (po:primop :fixnum-multiply (- multiplier) (- multiplicand))
             (if (zerop most-significant)
                 least-significant
                 ;; FIXME: create a bignum once we know how to do that.
                 (error 'create-a-bignum)))
           (multiple-value-bind (most-significant least-significant)
-              (cleavir-primop:fixnum-multiply (- multiplier) multiplicand)
+              (po:primop :fixnum-multiply (- multiplier) multiplicand)
             (if (zerop most-significant)
                 (- least-significant)
                 ;; FIXME: create a bignum once we know how to do that.
                 (error 'create-a-bignum))))
       (if (minusp multiplicand)
           (multiple-value-bind (most-significant least-significant)
-              (cleavir-primop:fixnum-multiply multiplier (- multiplicand))
+              (po:primop :fixnum-multiply multiplier (- multiplicand))
             (if (zerop most-significant)
                 (- least-significant)
                 ;; FIXME: create a bignum once we know how to do that.
                 (error 'create-a-bignum)))
           (multiple-value-bind (most-significant least-significant)
-              (cleavir-primop:fixnum-multiply multiplier multiplicand)
+              (po:primop :fixnum-multiply multiplier multiplicand)
             (if (zerop most-significant)
                 least-significant
                 ;; FIXME: create a bignum once we know how to do that.
