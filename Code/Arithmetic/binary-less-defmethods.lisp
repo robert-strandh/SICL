388198ae20478e22626642ecbbda680cd83435ee
Robert Strandh <robert.strandh@gmail.com>
2025-01-28 07:31:59 +0100

    Add primop for FIXNUM-EQUAL.

--- Code/Arithmetic/binary-less-defmethods.lisp
+++ Code/Arithmetic/binary-less-defmethods.lisp
@@ -1,7 +1,7 @@
 (cl:in-package #:sicl-arithmetic)
 
 (defmethod binary-less ((x fixnum) (y fixnum))
-  (if (cleavir-primop:fixnum-less x y) t nil))
+  (if (po:primop :fixnum-less x y) t nil))
 
 (defmethod binary-less ((x ratio) (y integer))
   (binary-less (floor (numerator x) (denominator x)) y))
