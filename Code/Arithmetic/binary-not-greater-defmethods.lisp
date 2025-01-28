8c82263b19499baa8e8db0d8abbf32481a296046
Robert Strandh <robert.strandh@gmail.com>
2025-01-28 07:32:13 +0100

    Adapt to new primops.

--- Code/Arithmetic/binary-not-greater-defmethods.lisp
+++ Code/Arithmetic/binary-not-greater-defmethods.lisp
@@ -1,7 +1,7 @@
 (cl:in-package #:sicl-arithmetic)
 
 (defmethod binary-not-greater ((x fixnum) (y fixnum))
-  (if (cleavir-primop:fixnum-not-greater x y) t nil))
+  (if (po:primop :fixnum-not-greater x y) t nil))
 
 (defmethod binary-not-greater ((x ratio) (y integer))
   (binary-not-greater (ceiling (numerator x) (denominator x)) y))
