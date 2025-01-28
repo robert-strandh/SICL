70989e94cf2c36c9a45dbc2a466c64f96be1589f
Robert Strandh <robert.strandh@gmail.com>
2025-01-28 15:25:13 +0100

    Avoid the use of CHECK-TYPE which was used incorrectly anyway.

--- Code/Arithmetic/logior-defun.lisp
+++ Code/Arithmetic/logior-defun.lisp
@@ -3,5 +3,5 @@
 (defun logior (&rest arguments)
   (case (length arguments)
     (0 0)
-    (1 (progn (check-type (first arguments) 'integer) (first arguments)))
+    (1 (progn (assert (integerp (first arguments))) (first arguments)))
     (t (reduce #'binary-logior arguments))))
