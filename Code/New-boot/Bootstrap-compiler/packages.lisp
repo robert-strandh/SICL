90afd6271c0769f2364eb92e34df14c250ccd503
Robert Strandh <robert.strandh@gmail.com>
2025-02-06 13:58:46 +0100

    Define several local nicknames.

--- Code/New-boot/Bootstrap-compiler/packages.lisp
+++ Code/New-boot/Bootstrap-compiler/packages.lisp
@@ -2,4 +2,8 @@
 
 (defpackage #:sicl-new-boot-bootstrap-compiler
   (:use #:common-lisp)
+  (:local-nicknames (#:cb #:common-boot)
+                    (#:cbfe #:common-boot-fast-ast-evaluator)
+                    (#:abp #:architecture.builder-protocol)
+                    (#:clo #:clostrum))
   (:export))
