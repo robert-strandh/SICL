9ec003ba16a76566f0cfffae92faddda70a12325
Robert Strandh <robert.strandh@gmail.com>
2025-02-12 07:03:36 +0100

    Add a dynamic-environment input to the instruction.

--- Code/HIR/unwind-instruction.lisp
+++ Code/HIR/unwind-instruction.lisp
@@ -1,8 +1,10 @@
 (cl:in-package #:sicl-hir)
 
-;;; This instruction has one or two inputs and no outputs.  The first
-;;; input is the unique identifier of an exit point as created by the
-;;; EXIT-POINT-INSTRUCTION.  The second input is used only when this
+;;; This instruction has two or three inputs and no outputs.  The
+;;; first input is a dynamic-environment object representing the
+;;; current dynamic environment.  The second input is the unique
+;;; identifier of an exit point as created by the
+;;; EXIT-POINT-INSTRUCTION.  The third input is used only when this
 ;;; instruction is created from a RETURN-FROM, and only when the
 ;;; corresponding BLOCK is in a context where its value is needed, and
 ;;; is the value (perhaps multiple values) to be transferred to the
