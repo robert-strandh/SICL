f128fcb42c4cb6ff388a2a3724b983b63a2104a2
Robert Strandh <robert.strandh@gmail.com>
2025-02-13 14:37:00 +0100

    Define subclasses SINGLE/MULTIPLE-VALUE-REGISTER.

--- Code/HIR/data.lisp
+++ Code/HIR/data.lisp
@@ -8,6 +8,12 @@
     :initarg :writers
     :accessor writers)))
 
+(defclass single-value-register (register)
+  ())
+
+(defclass multiple-value-register (register)
+  ())
+
 (defclass literal ()
   ((%readers
     :initarg :readers
