9a6ef7febe641a7c6e76d06bad8ad934af6efb6f
Robert Strandh <robert.strandh@gmail.com>
2025-02-13 15:28:11 +0100

    Have TRANSLATE create the context for TRANSLATE-AST.

--- Code/Compiler/AST-to-HIR/translate.lisp
+++ Code/Compiler/AST-to-HIR/translate.lisp
@@ -2,6 +2,13 @@
 
 (defgeneric translate-ast (client ast context))
 
-(defun translate (client ast context)
-  (let ((*registers* (make-hash-table :test #'eq)))
+(defun translate (client ast)
+  (let* ((*registers* (make-hash-table :test #'eq))
+         (register
+           (make-instance 'hir:multiple-value-register))
+         (return-instruction
+           (make-instance 'hir:return-instruction
+             :inputs (list register)))
+         (context
+           (make-context :all return-instruction register)))
     (translate-ast client ast context)))
