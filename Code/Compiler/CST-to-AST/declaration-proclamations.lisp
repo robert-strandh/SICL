6baeeada18b7e8f25179aed4db02a3b7f7edb5ea
Robert Strandh <robert.strandh@gmail.com>
2025-02-13 14:44:09 +0100

    Remove obsolete system.

--- Code/Compiler/CST-to-AST/declaration-proclamations.lisp
+++
@@ -1,5 +0,0 @@
-(cl:in-package #:sicl-cst-to-ast)
-
-(defmethod cleavir-cst-to-ast:declaration-proclamations
-    ((client sicl-client:sicl) environment)
-  (env:proclamation client environment 'declaration))
--- Code/Compiler/CST-to-AST/packages.lisp
+++
@@ -1,6 +0,0 @@
-(cl:in-package #:common-lisp-user)
-
-(defpackage #:sicl-cst-to-ast
-  (:use #:common-lisp)
-  (:local-nicknames (#:env #:sicl-environment))
-  (:export #:cst-to-ast))
