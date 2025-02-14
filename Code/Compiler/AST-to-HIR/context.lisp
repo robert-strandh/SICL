dee8b041c4077310ca7ab58c1e411fa2b24ded9d
Robert Strandh <robert.strandh@gmail.com>
2025-02-14 14:39:40 +0100

    Replace CONTEXT class by special variables.

--- Code/Compiler/AST-to-HIR/context.lisp
+++ Code/Compiler/AST-to-HIR/context.lisp
@@ -1,28 +1,7 @@
 (cl:in-package #:sicl-ast-to-hir)
 
-(defclass context ()
-  (;; This slot contains 0, 1, or :ALL.
-   (%values-count
-    :initarg :values-count
-    :reader values-count)
-   (%next-instruction
-    :initarg :next-instruction
-    :reader next-instruction)
-   (%target-register
-    :initarg :target-register
-    :reader target-register)))
+(defvar *values-count*)
 
-(defun make-context (values-count next-instruction target-register)
-  (make-instance 'context
-    :values-count values-count
-    :next-instruction next-instruction
-    :target-register target-register))
+(defvar *next-instruction*)
 
-(defmacro with-context-components ((context-form) &body body)
-  (let ((context-variable (gensym)))
-    `(let* ((,context-variable ,context-form)
-            (values-count (values-count ,context-variable))
-            (next-instruction (next-instruction ,context-variable))
-            (target-register (target-register ,context-variable)))
-       (declare (ignorable values-count next-instruction target-register))
-       ,@body)))
+(defvar *target-register*)
