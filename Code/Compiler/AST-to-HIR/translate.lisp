bc773fd1c3743665521ef6fc519868d55d1b61c6
Robert Strandh <robert.strandh@gmail.com>
2025-02-15 12:46:59 +0100

    Define variable *UNWIND-INSTRUCTIONS-TO-FIX-UP* and add comment.

--- Code/Compiler/AST-to-HIR/translate.lisp
+++ Code/Compiler/AST-to-HIR/translate.lisp
@@ -1,5 +1,16 @@
 (cl:in-package #:sicl-ast-to-hir)
 
+;;; When we translate a GO-WITH-VARIABLE-AST to an UNWIND-INSTRUCTION,
+;;; the target instruction does not necessarily exist yet.  So in the
+;;; SUCCESSORS slot of the UNWIND-INSTRUCTION, we store the contents
+;;; of the INDEX slot of the AST and a vector created when the
+;;; TAGBODY-WITH-VARIABLE-AST is translated.  We must then fix up
+;;; those UNWIND-INSTRUCTIONs by replacing the SUCCESSORS slot by the
+;;; instruction in the vector at the location of the index.  This
+;;; variable holds the list of UNWIND-INSTRUCTIONs to fix up.
+
+(defvar *unwind-instructions-to-fix-up*)
+
 (defgeneric translate-ast (client ast))
 
 (defun translate (client ast)
@@ -13,5 +24,6 @@
          (*values-count* :all)
          (*next-instruction*
            (make-instance 'hir:return-instruction
-             :inputs (list *target-register*))))
+             :inputs (list *target-register*)))
+         (*unwind-instructions-to-fix-up* '()))
     (translate-ast client ast)))
