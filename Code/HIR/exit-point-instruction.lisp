5f496463d2a18ae68d74ca13ef2be65adaee94e2
Robert Strandh <robert.strandh@gmail.com>
2025-02-17 14:40:30 +0100

    Fix comment to reflect the existence of the new RECEIVE-INSTRUCTION.

--- Code/HIR/exit-point-instruction.lisp
+++ Code/HIR/exit-point-instruction.lisp
@@ -1,16 +1,12 @@
 (cl:in-package #:sicl-hir)
 
-;;; This instruction has one input and two or three outputs.  The
-;;; input is a dynamic-environment object representing the current
-;;; dynamic environment.  The first output is a dynamic-environment
-;;; object which is like the input but with a new exit point added.
-;;; The second output is a unique identifier for this exit point.  The
+;;; This instruction has one input and two outputs.  The input is a
+;;; dynamic-environment object representing the current dynamic
+;;; environment.  The first output is a dynamic-environment object
+;;; which is like the input but with a new exit point added.  The
+;;; second output is a unique identifier for this exit point.  The
 ;;; unique identifier is used by the UNWIND-INSTRUCTION to find the
-;;; unique exit point where control is to be transferred.  The third
-;;; output is present only when this instruction was created from
-;;; BLOCK, and only when the BLOCK is in a context where its value is
-;;; needed, and it is the value (possibly multiple values) transferred
-;;; by the optional input of the UNWIND-INSTRUCTION.
+;;; unique exit point where control is to be transferred.
 
 (defclass exit-point-instruction (instruction)
   ())
