f58c895709821c327e98657e6446d033da110d4e
Robert Strandh <robert.strandh@gmail.com>
2025-02-17 16:22:53 +0100

    Introduce variable *BLOCK-VALUES-COUNT* for use by RETURN-FROM.

--- Code/Compiler/AST-to-HIR/block-with-variable-ast.lisp
+++ Code/Compiler/AST-to-HIR/block-with-variable-ast.lisp
@@ -4,8 +4,14 @@
 ;;; VARIABLE-DEFINITION-ASTs of BLOCK-WITH-VARIABLE-ASTs to
 ;;; RECEIVE-INSTRUCTIONs to be used by UNWIND-INSTRUCTIONs resulting
 ;;; from the translation of RETURN-FROM-WITH-VARIABLE-AST
-
 (defparameter *block-receive-instruction* '())
+
+;;; This variable holds an association list that maps
+;;; VARIABLE-DEFINITION-ASTs of BLOCK-WITH-VARIABLE-ASTs to
+;;; VALUES-COUNT (0, 1, or :ALL) to be used as the VALUES-COUNT
+;;; context variable when the FORM-AST of a
+;;; RETURN-FROM-WITH-VARIABLE-AST is translated.
+(defparameter *block-values-count* '())
 
 (defmethod translate-ast (client (ast ico:block-with-variable-ast))
   (let ((variable-definition-ast (ico:variable-definition-ast ast))
@@ -17,6 +23,9 @@
            (*block-receive-instruction*
              (acons variable-definition-ast receive-instruction
                     *block-receive-instruction*))
+           (*block-values-count*
+             (acons variable-definition-ast *values-count*
+                    *block-values-count*))
            (current-dynamic-environment-register
              *dynamic-environment-register*)
            (*dynamic-environment-register*
