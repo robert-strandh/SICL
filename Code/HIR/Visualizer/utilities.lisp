143b13f80f4c8edef8fd016c24dd7972c3acba60
Robert Strandh <robert.strandh@gmail.com>
2025-02-19 14:40:03 +0100

    Follow the code link when instruction is an ENTER-INSTRUCTION.

--- Code/HIR/Visualizer/utilities.lisp
+++ Code/HIR/Visualizer/utilities.lisp
@@ -6,7 +6,9 @@
                (unless (gethash instruction visited)
                  (setf (gethash instruction visited) t)
                  (funcall function instruction)
-                 (mapcar #'aux (ir:successors instruction)))))
+                 (mapcar #'aux (ir:successors instruction))
+                 (when (typep instruction 'ir:enclose-instruction)
+                   (aux (ir:parse-arguments-instruction instruction))))))
       (aux first-instruction))))
 
                
