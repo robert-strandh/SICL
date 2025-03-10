36fa90d37066efeb3f48d7a8a7c78ddcfe0fc6db
Robert Strandh <robert.strandh@gmail.com>
2025-03-10 11:50:55 +0100

    Remove obsolete system.

--- Code/New-boot/Bootstrap-compiler/client.lisp
+++
@@ -1,4 +0,0 @@
-(cl:in-package #:sicl-new-boot-bootstrap-compiler)
-
-(defclass client (sicl-new-boot-phase-4::client)
-  ())
--- Code/New-boot/Bootstrap-compiler/packages.lisp
+++
@@ -1,9 +0,0 @@
-(cl:in-package #:common-lisp-user)
-
-(defpackage #:sicl-new-boot-bootstrap-compiler
-  (:use #:common-lisp)
-  (:local-nicknames (#:cb #:common-boot)
-                    (#:cbfe #:common-boot-fast-ast-evaluator)
-                    (#:abp #:architecture.builder-protocol)
-                    (#:clo #:clostrum))
-  (:export))
