e01e4dd308cd28e4d5832ceaada0802f81054c16
Robert Strandh <robert.strandh@gmail.com>
2025-03-20 17:08:09 +0100

    No longer define short/single float constants specially.

--- Code/New-boot/Phase-6/load-quaviver.lisp
+++ Code/New-boot/Phase-6/load-quaviver.lisp
@@ -6,11 +6,6 @@
   (declare (ignorable w4))
   (let ((single-float
           (make-instance 'sb:simulated-single-float
-            :bit-pattern buoy-simulate:most-positive-single-float)))
-    (clo:make-variable c4 e4 'most-positive-short-float single-float)
-    (clo:make-variable c4 e4 'most-positive-single-float single-float))
-  (let ((single-float
-          (make-instance 'sb:simulated-single-float
             :bit-pattern buoy-simulate:least-positive-single-float)))
     (clo:make-variable c4 e4 'least-positive-short-float single-float)
     (clo:make-variable c4 e4 'least-positive-single-float single-float))
@@ -79,6 +74,5 @@
   (setf (clo:fdefinition c4 e4 'coerce) #'coerce)
   (setf (clo:fdefinition c4 e4 'byte) #'byte)
   (setf (clo:fdefinition c4 e4 'ldb-test) #'ldb-test)
-  #+(or)
   (let ((*features* '(:sicl)))
     (sb:ensure-asdf-system c4 w4 "quaviver")))
