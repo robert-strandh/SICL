8bbfd6216c774ac2deb86f840bc98d514855d715
Robert Strandh <robert.strandh@gmail.com>
2025-03-22 13:22:00 +0100

    Define generic function and method on INTEGER-DECODE-FLOAT.

--- Code/Arithmetic/simulated-single-float-methods.lisp
+++ Code/Arithmetic/simulated-single-float-methods.lisp
@@ -1,5 +1,10 @@
 (cl:in-package #:sicl-arithmetic)
 
+(defgeneric integer-decode-float (float))
+
 (defmethod binary-equal
     ((x simulated-single-float) (y simulated-single-float))
   (binary-equal (bit-pattern x) (bit-pattern y)))
+
+(defmethod integer-decode-float ((float simulated-single-float))
+  (buoy-simulate:integer-decode-single-float (bit-pattern float)))
