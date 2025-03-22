b06c879b94dde84e4d6d6405ea22e797780a9c56
Robert Strandh <robert.strandh@gmail.com>
2025-03-22 13:21:45 +0100

    Improve code layout.

--- Code/Arithmetic/floating-point-constants.lisp
+++ Code/Arithmetic/floating-point-constants.lisp
@@ -1,29 +1,73 @@
 (cl:in-package #:sicl-arithmetic)
 
-(defconstant             most-positive-single-float (float-features:bits-single-float #x7f7fffff))
-(defconstant            least-positive-single-float (float-features:bits-single-float #x00000001))
-(defconstant least-positive-normalized-single-float (float-features:bits-single-float #x00800000))
-(defconstant             most-negative-single-float (float-features:bits-single-float #xff7fffff))
-(defconstant            least-negative-single-float (float-features:bits-single-float #x80000001))
-(defconstant least-negative-normalized-single-float (float-features:bits-single-float #x80800000))
+(defconstant most-positive-single-float
+  (float-features:bits-single-float #x7f7fffff))
 
-(defconstant             most-positive-double-float (float-features:bits-double-float #x7fefffffffffffff))
-(defconstant            least-positive-double-float (float-features:bits-double-float #x0000000000000001))
-(defconstant least-positive-normalized-double-float (float-features:bits-double-float #x0010000000000000))
-(defconstant             most-negative-double-float (float-features:bits-double-float #xffefffffffffffff))
-(defconstant            least-negative-double-float (float-features:bits-double-float #x8000000000000001))
-(defconstant least-negative-normalized-double-float (float-features:bits-double-float #x8010000000000000))
+(defconstant least-positive-single-float
+  (float-features:bits-single-float #x00000001))
 
-(defconstant             most-positive-short-float             most-positive-single-float)
-(defconstant            least-positive-short-float            least-positive-single-float)
-(defconstant least-positive-normalized-short-float least-positive-normalized-single-float)
-(defconstant             most-negative-short-float             most-negative-single-float)
-(defconstant            least-negative-short-float            least-negative-single-float)
-(defconstant least-negative-normalized-short-float least-negative-normalized-single-float)
+(defconstant least-positive-normalized-single-float
+  (float-features:bits-single-float #x00800000))
 
-(defconstant             most-positive-long-float             most-positive-double-float)
-(defconstant            least-positive-long-float            least-positive-double-float)
-(defconstant least-positive-normalized-long-float least-positive-normalized-double-float)
-(defconstant             most-negative-long-float             most-negative-double-float)
-(defconstant            least-negative-long-float            least-negative-double-float)
-(defconstant least-negative-normalized-long-float least-negative-normalized-double-float)
+(defconstant most-negative-single-float
+  (float-features:bits-single-float #xff7fffff))
+
+(defconstant least-negative-single-float
+  (float-features:bits-single-float #x80000001))
+
+(defconstant least-negative-normalized-single-float
+  (float-features:bits-single-float #x80800000))
+
+(defconstant most-positive-double-float
+  (float-features:bits-double-float #x7fefffffffffffff))
+
+(defconstant least-positive-double-float
+  (float-features:bits-double-float #x0000000000000001))
+
+(defconstant least-positive-normalized-double-float
+  (float-features:bits-double-float #x0010000000000000))
+
+(defconstant most-negative-double-float
+  (float-features:bits-double-float #xffefffffffffffff))
+
+(defconstant least-negative-double-float
+  (float-features:bits-double-float #x8000000000000001))
+
+(defconstant least-negative-normalized-double-float
+  (float-features:bits-double-float #x8010000000000000))
+
+(defconstant most-positive-short-float
+  most-positive-single-float)
+
+(defconstant least-positive-short-float
+  least-positive-single-float)
+
+(defconstant least-positive-normalized-short-float
+  least-positive-normalized-single-float)
+
+(defconstant most-negative-short-float
+  most-negative-single-float)
+
+(defconstant least-negative-short-float
+  least-negative-single-float)
+
+(defconstant least-negative-normalized-short-float
+  least-negative-normalized-single-float)
+
+(defconstant most-positive-long-float
+  most-positive-double-float)
+
+(defconstant least-positive-long-float
+  least-positive-double-float)
+
+(defconstant least-positive-normalized-long-float
+  least-positive-normalized-double-float)
+
+(defconstant most-negative-long-float
+  most-negative-double-float)
+
+(defconstant least-negative-long-float
+  least-negative-double-float)
+
+(defconstant least-negative-normalized-long-float
+  least-negative-normalized-double-float)
--- Code/Arithmetic/floating-point.lisp
+++ Code/Arithmetic/floating-point.lisp
@@ -1,12 +1,18 @@
 (cl:in-package #:sicl-arithmetic)
 
 (defgeneric decode-float (float))
-(defmethod decode-float ((float single-float)) (buoy:decode-single-float float))
-(defmethod decode-float ((float double-float)) (buoy:decode-double-float float))
+
+(defmethod decode-float ((float single-float))
+  (buoy:decode-single-float float))
+
+(defmethod decode-float ((float double-float))
+  (buoy:decode-double-float float))
 
 (defgeneric scale-float (float integer))
+
 (defmethod scale-float ((float single-float) (integer integer))
   (buoy:scale-single-float float integer))
+
 (defmethod scale-float ((float double-float) (integer integer))
   (buoy:scale-double-float float integer))
 
@@ -15,12 +21,16 @@
   2)
 
 (defgeneric generic-float-sign (float-1 float-2))
+
 (defmethod generic-float-sign ((float-1 single-float) (float-2 single-float))
   (buoy:single-copysign float-2 float-1))
+
 (defmethod generic-float-sign ((float-1 double-float) (float-2 double-float))
   (buoy:double-copysign float-2 float-1))
+
 (defmethod generic-float-sign ((float-1 single-float) (float-2 double-float))
   (buoy:double-copysign float-2 (float float-1 float-2)))
+
 (defmethod generic-float-sign ((float-1 double-float) (float-2 single-float))
   (buoy:double-copysign (float float-2 float-1) float-1))
 
@@ -28,21 +38,32 @@
   (generic-float-sign float-1 float-2))
 
 (defgeneric float-digits (float))
+
 (defmethod float-digits ((float single-float)) 24)
+
 (defmethod float-digits ((float double-float)) 53)
 
 (defgeneric float-precision (float))
-(defmethod float-precision ((float single-float)) (buoy:single-float-precision float))
-(defmethod float-precision ((float double-float)) (buoy:double-float-precision float))
+
+(defmethod float-precision ((float single-float))
+  (buoy:single-float-precision float))
+
+(defmethod float-precision ((float double-float))
+  (buoy:double-float-precision float))
 
 (defgeneric integer-decode-float (float))
-(defmethod integer-decode-float ((float single-float)) (buoy:integer-decode-single-float float))
-(defmethod integer-decode-float ((float double-float)) (buoy:integer-decode-double-float float))
+
+(defmethod integer-decode-float ((float single-float))
+  (buoy:integer-decode-single-float float))
+
+(defmethod integer-decode-float ((float double-float))
+  (buoy:integer-decode-double-float float))
 
 (defmethod signum ((float single-float))
   (if (zerop float)
       float
       (buoy:single-copysign 1s0 float)))
+
 (defmethod signum ((float double-float))
   (if (zerop float)
       float
