2f27047dc71506707276d0e68bcfe77473a05256
Robert Strandh <robert.strandh@gmail.com>
2025-03-28 15:20:13 +0100

    Define methods for SINGLE-FLOAT and DOUBLE-FLOAT.

--- Code/Arithmetic/binary-equal-defmethods.lisp
+++ Code/Arithmetic/binary-equal-defmethods.lisp
@@ -3,11 +3,24 @@
 (defmethod binary-equal ((x fixnum) (y fixnum))
   (if (po:primop :fixnum-equal x y) t nil))
 
-(defmethod binary-equal ((x fixnum) (y bignum)) nil)
-(defmethod binary-equal ((x bignum) (y fixnum)) nil)
+(defmethod binary-equal ((x fixnum) (y bignum))
+  nil)
 
-(defmethod binary-equal ((x integer) (y ratio)) nil)
-(defmethod binary-equal ((x ratio) (y integer)) nil)
+(defmethod binary-equal ((x bignum) (y fixnum))
+  nil)
+
+(defmethod binary-equal ((x integer) (y ratio))
+  nil)
+
+(defmethod binary-equal ((x ratio) (y integer))
+  nil)
+
 (defmethod binary-equal ((x ratio) (y ratio))
   (and (binary-equal (numerator x) (numerator y))
        (binary-equal (denominator x) (denominator y))))
+
+(defmethod binary-equal ((x single-float) (y single-float))
+  (if (po:primop :single-float-equal x y) t nil))
+
+(defmethod binary-equal ((x double-float) (y double-float))
+  (if (po:primop :double-float-equal x y) t nil))
