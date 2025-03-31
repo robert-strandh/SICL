c478469402c972d1aaec98f0a6711142dfecc076
Robert Strandh <robert.strandh@gmail.com>
2025-03-31 06:52:58 +0200

    Define methods for floating-point arguments.

--- Code/Arithmetic/binary-less-defmethods.lisp
+++ Code/Arithmetic/binary-less-defmethods.lisp
@@ -12,3 +12,27 @@
 (defmethod binary-less ((x ratio) (y ratio))
   (binary-less (* (numerator x) (denominator y))
                (* (numerator y) (denominator x))))
+
+(defmethod binary-less ((x single-float) (y integer))
+  (if (po:primop :single-float-less x (float y 1s0)) t nil))
+
+(defmethod binary-less ((x integer) (y single-float))
+  (if (po:primop :single-float-less (float x 1s0) y) t nil))
+
+(defmethod binary-less ((x single-float) (y single-float))
+  (if (po:primop :single-float-less x y) t nil))
+
+(defmethod binary-less ((x double-float) (y integer))
+  (if (po:primop :double-float-less x (float y 1d0)) t nil))
+
+(defmethod binary-less ((x integer) (y double-float))
+  (if (po:primop :double-float-less (float x 1d0) y) t nil))
+
+(defmethod binary-less ((x double-float) (y single-float))
+  (if (po:primop :double-float-less x (float y 1d0)) t nil))
+
+(defmethod binary-less ((x single-float) (y double-float))
+  (if (po:primop :double-float-less (float x 1d0) y) t nil))
+
+(defmethod binary-less ((x double-float) (y double-float))
+  (if (po:primop :double-float-less x y) t nil))
