6da318bb2db3278481bbace6d5e62f53b7bf6c8a
Robert Strandh <robert.strandh@gmail.com>
2025-03-31 10:34:48 +0200

    Make STANDARD-OBJECT a superclass of RATIO.

--- Code/Arithmetic/ratio-defclass.lisp
+++ Code/Arithmetic/ratio-defclass.lisp
@@ -4,6 +4,6 @@
 
 (defgeneric denominator (ratio))
 
-(defclass ratio (rational)
+(defclass ratio (rational standard-object)
   ((%numerator :initarg :numerator :reader numerator)
    (%denominator :initarg :denominator :reader denominator)))
