cc84d61b3c03a531d63f3d49b22cd69d5244c6d5
Robert Strandh <robert.strandh@gmail.com>
2025-03-31 06:26:12 +0200

    Define methods for floating-point arguments.

--- Code/Arithmetic/binary-equal-defmethods.lisp
+++ Code/Arithmetic/binary-equal-defmethods.lisp
@@ -19,8 +19,26 @@
   (and (binary-equal (numerator x) (numerator y))
        (binary-equal (denominator x) (denominator y))))
 
+(defmethod binary-equal ((x single-float) (y integer))
+  (if (po:primop :single-float-equal x (float y 1s0)) t nil))
+
+(defmethod binary-equal ((x integer) (y single-float))
+  (if (po:primop :single-float-equal (float x 1s0) y) t nil))
+
 (defmethod binary-equal ((x single-float) (y single-float))
   (if (po:primop :single-float-equal x y) t nil))
+
+(defmethod binary-equal ((x double-float) (y integer))
+  (if (po:primop :double-float-equal x (float y 1d0)) t nil))
+
+(defmethod binary-equal ((x integer) (y double-float))
+  (if (po:primop :double-float-equal (float x 1d0) y) t nil))
+
+(defmethod binary-equal ((x double-float) (y single-float))
+  (if (po:primop :double-float-equal x (float y 1d0)) t nil))
+
+(defmethod binary-equal ((x single-float) (y double-float))
+  (if (po:primop :double-float-equal (float x 1d0) y) t nil))
 
 (defmethod binary-equal ((x double-float) (y double-float))
   (if (po:primop :double-float-equal x y) t nil))
