e4fc046b5db9515139be4843cf3e15c08509090d
Robert Strandh <robert.strandh@gmail.com>
2025-04-03 17:35:17 +0200

    Define methods on BINARY-ADD for floating-point arguments.

--- Code/Arithmetic/binary-add-defmethods.lisp
+++ Code/Arithmetic/binary-add-defmethods.lisp
@@ -30,3 +30,21 @@
   (let ((xnum (numerator x)) (xden (denominator x))
         (ynum (numerator x)) (yden (denominator y)))
     (/ (+ (* xnum yden) (* ynum xden)) (* xden yden))))
+
+(defmethod binary-add ((x single-float) (y integer))
+  (po:primop :single-float-add x (float y 1s0)))
+
+(defmethod binary-add ((x integer) (y single-float))
+  (po:primop :single-float-add (float x 1s0) y))
+
+(defmethod binary-add ((x single-float) (y single-float))
+  (po:primop :single-float-add x y))
+
+(defmethod binary-add ((x double-float) (y integer))
+  (po:primop :double-float-add x (float y 1d0)))
+
+(defmethod binary-add ((x integer) (y double-float))
+  (po:primop :double-float-add (float x 1d0) y))
+
+(defmethod binary-add ((x double-float) (y double-float))
+  (po:primop :double-float-add x y))
