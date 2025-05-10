8ff2a89a5ac903bb760fd3a6660e0e9d3561ad99
Robert Strandh <robert.strandh@gmail.com>
2025-05-10 15:29:07 +0200

    Remove obsolete system.

--- Code/Cleavir/Attributes/packages.lisp
+++
@@ -1,6 +0,0 @@
-(cl:in-package #:common-lisp-user)
-
-(defpackage #:cleavir-attributes
-  (:use #:common-lisp)
-  (:export #:make-attributes #:default-attributes
-           #:has-boolean-attribute-p))
