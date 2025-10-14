15ff26ea43553332d1081ef174826507a48d77ed
Robert Strandh <robert.strandh@gmail.com>
2025-10-14 14:02:29 +0200

    Rename ENVIRONMENT to W4.

--- Code/New-boot/Phase-4/boot.lisp
+++ Code/New-boot/Phase-4/boot.lisp
@@ -12,8 +12,8 @@
 (defun boot (boot)
   (format *trace-output* "**************** Phase 4~%")
   (let* ((c4 (make-instance 'client))
-         (environment (create-environment c4))
-         (e4 (trucler:global-environment c4 environment))
+         (w4 (create-environment c4))
+         (e4 (trucler:global-environment c4 w4))
          (e3 (sb:e3 boot))
          (env:*client* c4)
          (env:*environment* e4))
@@ -45,26 +45,26 @@
             #'sb:standard-instance-access)
       (setf (clo:fdefinition c4 e4 `(setf ,symbol))
             #'(setf sb:standard-instance-access)))
-    (sb:ensure-asdf-system c4 environment "sicl-primop")
+    (sb:ensure-asdf-system c4 w4 "sicl-primop")
     (setf (clo:fdefinition c4 e4 @sicl-primop:primop)
           #'sb:primop)
-    (sb:ensure-asdf-system c4 environment "clostrophilia-slot-value-etc")
+    (sb:ensure-asdf-system c4 w4 "clostrophilia-slot-value-etc")
     (sb:define-straddle-functions c4 e4 e3)
-    (sb:ensure-asdf-system c4 environment "sicl-clos-ensure-metaobject")
+    (sb:ensure-asdf-system c4 w4 "sicl-clos-ensure-metaobject")
     (setf (clo:fdefinition
            c4 e3 @clostrophilia:ensure-generic-function+1)
           (clo:fdefinition
            c4 e4 'ensure-generic-function))
     (sb:define-ecclesia-functions c4 (sb:e2 boot) e4)
-    (sb:ensure-asdf-system c4 environment "clostrophilia-method-combination")
+    (sb:ensure-asdf-system c4 w4 "clostrophilia-method-combination")
     (setf (clo:fdefinition c4 e3 @clostrophilia:find-class-t)
           (lambda ()
             (clo:find-class c4 e4 't)))
-    (sb:ensure-asdf-system c4 environment "clostrophilia-class-hierarchy")
-    (sb:ensure-asdf-system c4 environment "sicl-arithmetic-base")
-    (sb:ensure-asdf-system c4 environment
+    (sb:ensure-asdf-system c4 w4 "clostrophilia-class-hierarchy")
+    (sb:ensure-asdf-system c4 w4 "sicl-arithmetic-base")
+    (sb:ensure-asdf-system c4 w4
                            "sicl-arithmetic-class-hierarchy")
-    (sb:ensure-asdf-system c4 environment "sicl-arithmetic-operations") 
+    (sb:ensure-asdf-system c4 w4 "sicl-arithmetic-operations") 
     (setf (clo:symbol-value c4 e3 @clostrophilia:*class-t+1*)
           (clo:find-class c4 e4 't))
     (setf (clo:macro-function c4 e4 @asdf-user:defsystem)
@@ -72,8 +72,8 @@
     (setf (clo:macro-function c4 e4 @asdf:defsystem)
           (constantly nil))
     (sb:ensure-asdf-system
-     c4 environment "predicament-base" :load-system-file t)
-    (sb:ensure-asdf-system c4 environment "predicament-packages-intrinsic")
+     c4 w4 "predicament-base" :load-system-file t)
+    (sb:ensure-asdf-system c4 w4 "predicament-packages-intrinsic")
     (setf (clo:fdefinition
            c4 (sb:e2 boot)
            @clostrophilia:find-class-standard-object)
@@ -90,27 +90,27 @@
                          @sicl-clos:*standard-method-combination*
                          (funcall function c4 'standard '())))
     (sb:ensure-asdf-system
-     c4 environment "sicl-new-boot-phase-2-additional-classes")
+     c4 w4 "sicl-new-boot-phase-2-additional-classes")
     (define-class-of-and-stamp c4 e3 e4)
     (setf (clo:fdefinition c4 e3 @clostrophilia:class-of+1)
           (clo:fdefinition c4 e4 'class-of))
     (setf (clo:fdefinition c4 e3 @clostrophilia:stamp+1)
           (clo:fdefinition c4 e4 @clostrophilia:stamp))
-    (load-predicament c4 environment e4)
+    (load-predicament c4 w4 e4)
     (clo:make-variable c4 e3
                        @predicament:*condition-maker* 'make-condition)
     (sb:ensure-asdf-system
-     c4 environment "clostrophilia-slot-value-etc-using-class")
+     c4 w4 "clostrophilia-slot-value-etc-using-class")
     ;;; During bootstrapping, we set the unbound slot value to
     ;;; something that is easier to manipulate during debugging.
     (setf (clo:symbol-value c4 e4 @clostrophilia:+unbound-slot-value+)
           99999)
     (sb:ensure-asdf-system
-     c4 environment "clostrophilia-standard-object-initialization")
+     c4 w4 "clostrophilia-standard-object-initialization")
     (setf (clo:fdefinition c4 e3 @sicl-clos:initialize-instance+1)
           (clo:fdefinition c4 e4 'initialize-instance))
     (sb:ensure-asdf-system
-     c4 environment "clostrophilia-standard-object-initialization-aux")
+     c4 w4 "clostrophilia-standard-object-initialization-aux")
     (setf (clo:fdefinition c4 e4 @clostrophilia:shared-initialize-aux-1)
           (clo:fdefinition
            c4 e3 @clostrophilia:shared-initialize-aux))
@@ -119,9 +119,9 @@
     ;; TYPEP since the very purpose of ctype is to define TYPEP.  But
     ;; assertions should not fail at this point in the process anyway.
     (setf (clo:macro-function c4 e4 'assert)
-          (lambda (form environment)
+          (lambda (form w4)
             ;; We might put some trace output here.
-            (declare (ignore form environment))
+            (declare (ignore form w4))
             nil))
     ;; FIXME: TYPEXPAND should be defined by code from SICL-TYPE being
     ;; loaded, rather than by defining it here.
@@ -135,43 +135,43 @@
         ((make-instance
              (clo:ensure-operator-cell c4 e3 'make-instance)))
       (sb:ensure-asdf-system
-       c4 environment "clostrophilia-class-finalization")
+       c4 w4 "clostrophilia-class-finalization")
       (sb:ensure-asdf-system
-       c4 environment "clostrophilia-method-combination-base"))
+       c4 w4 "clostrophilia-method-combination-base"))
     (setf (clo:fdefinition c4 e3 @sicl-clos:find-class+1)
           (clo:fdefinition c4 e4 'find-class))
     (setf (clo:fdefinition c4 e4 'compile)
           (lambda (should-be-nil lambda-expression)
             (assert (null should-be-nil))
             (let ((cst (cst:cst-from-expression lambda-expression)))
-              (sb:eval-cst c4 cst environment))))
+              (sb:eval-cst c4 cst w4))))
     (clo:make-variable c4 e4 'lambda-list-keywords lambda-list-keywords)
     (sb:with-intercepted-function-cells
         ((make-instance
           (clo:ensure-operator-cell c4 e3 'make-instance)))
       (sb:ensure-asdf-system
-       c4 environment "clostrophilia-generic-function-invocation")
-      (sb:ensure-asdf-system c4 environment "acclimation")
-      (sb:ensure-asdf-system c4 environment "ecclesia"))
+       c4 w4 "clostrophilia-generic-function-invocation")
+      (sb:ensure-asdf-system c4 w4 "acclimation")
+      (sb:ensure-asdf-system c4 w4 "ecclesia"))
     (sb:ensure-asdf-system
-     c4 environment "clostrophilia-dependent-maintenance")
+     c4 w4 "clostrophilia-dependent-maintenance")
     (setf (clo:fdefinition c4 e4 @clostrophilia:subtypep-1)
           (constantly t))
     (setf (clo:fdefinition c4 e4 @sicl-clos:subtypep-1)
           (constantly t))
     (sb:ensure-asdf-system
-     c4 environment "clostrophilia-generic-function-initialization")
+     c4 w4 "clostrophilia-generic-function-initialization")
     (setf (clo:fdefinition c4 e4 @clostrophilia:allocate-general-instance)
           #'sb:allocate-general-instance)
     (sb:with-intercepted-function-cells
         ((make-instance
           (clo:ensure-operator-cell c4 e3 'make-instance)))
       (sb:ensure-asdf-system
-       c4 environment "clostrophilia-class-initialization"))
+       c4 w4 "clostrophilia-class-initialization"))
     (sb:ensure-asdf-system
-     c4 environment "clostrophilia-method-initialization")
+     c4 w4 "clostrophilia-method-initialization")
     (sb:ensure-asdf-system
-     c4 environment "clostrophilia-slot-definition-initialization")
+     c4 w4 "clostrophilia-slot-definition-initialization")
     ;; We don't expect to see any floating-point numbers during
     ;; bootstrapping.
     (setf (clo:fdefinition c4 e4 @sicl-arithmetic:single-float-p)
@@ -185,10 +185,10 @@
     ;; into it later.  We solve this dilemma by temporarily setting
     ;; MAKE-INSTANCE in E4 to be the same as MAKE-INSTANCE in E3, and
     ;; then we remove that definition after ctype has been loaded.
-    (setf (clo:fdefinition c4 (sb:e4 boot) 'make-instance)
+    (setf (clo:fdefinition c4 e4 'make-instance)
           (clo:fdefinition c4 e3 'make-instance))
-    (load-ctype c4 environment e4)
-    (clo:fmakunbound c4 (sb:e4 boot) 'make-instance)
+    (load-ctype c4 w4 e4)
+    (clo:fmakunbound c4 e4 'make-instance)
     ;; The ctype library defines SUBCLASSP to call
     ;; SICL-CLOS:CLASS-PRECEDENCE-LIST with the subclass as an
     ;; argument.  But in E4, the arguments to SUBCLASSP are ersatz
@@ -201,9 +201,9 @@
                      c4 e3 @sicl-clos:class-precedence-list)))
               (member super (funcall class-precedence-list sub)))))
     (sb:ensure-asdf-system
-     c4 environment "sicl-clos-ensure-metaobject-using")
+     c4 w4 "sicl-clos-ensure-metaobject-using")
     (setf (clo:fdefinition c4 e4
                            @clostrophilia:set-funcallable-instance-function)
           (fdefinition 'closer-mop:set-funcallable-instance-function))
-    (sb:ensure-asdf-system c4 environment "sicl-clos-make-instance"))
+    (sb:ensure-asdf-system c4 w4 "sicl-clos-make-instance"))
   boot)
