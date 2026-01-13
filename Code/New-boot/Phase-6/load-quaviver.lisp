df39079a2f03182fe60d828a6e2915c200c5a42d
Robert Strandh <robert.strandh@gmail.com>
2026-01-13 13:28:31 +0100

    Replace host floats by bootstrapping-specific floats.

--- Code/New-boot/Phase-6/load-quaviver.lisp
+++ Code/New-boot/Phase-6/load-quaviver.lisp
@@ -2,20 +2,31 @@
 
 (eval-when (:compile-toplevel) (sb:enable-parcl-symbols c4))
 
+(defun bits-to-single-float (c4 e4 bits)
+  (make-instance 'sb:header
+    :class (clo:find-class c4 e4 'single-float)
+    :rack (make-array 1 :initial-element bits)))
+
+(defun bits-to-double-float (c4 e4 bits)
+  (make-instance 'sb:header
+    :class (clo:find-class c4 e4 'double-float)
+    :rack (make-array 1 :initial-element bits)))
+
 (defun load-quaviver (c4 w4 e4)
   (declare (ignorable w4))
   (setf (clo:fdefinition c4 e4 'float-radix)
         (lambda (float)
           (declare (ignore float))
           2))
-  (sb:ensure-asdf-system c4 w4 "sicl-type-coerce")
+  ;; (sb:ensure-asdf-system c4 w4 "sicl-type-coerce")
   (sb:ensure-asdf-system c4 w4 "sicl-arithmetic-floating-point")
   (setf (clo:fdefinition c4 e4 'byte) #'byte)
   (setf (clo:fdefinition c4 e4 'ldb-test) #'ldb-test)
-  ;; This is not quite correct because it assumes that the host floats
-  ;; are the same as the target floats.
-  (setf (clo:fdefinition c4 e4 'float) #'float)
-  (setf (clo:fdefinition c4 e4 'float-digits) #'float-digits)
-  (setf (clo:fdefinition c4 e4 'integer-decode-float) #'integer-decode-float)
+  (setf (clo:fdefinition c4 e4 'coerce)
+        (lambda (value type)
+          (assert (zerop value))
+          (ecase type
+            (single-float (bits-to-single-float c4 e4 0))
+            (double-float (bits-to-double-float c4 e4 0)))))
   (let ((*features* '(:sicl)))
     (sb:ensure-asdf-system c4 w4 "quaviver")))
