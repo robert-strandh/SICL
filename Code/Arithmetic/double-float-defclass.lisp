4ded23f15ced1240bdf96e5caed4d197a4c5c98e
Robert Strandh <robert.strandh@gmail.com>
2026-01-13 12:43:57 +0100

    Redfine classes SINGLE-FLOAT and DOUBLE-FLOAT for bootstrapping.

--- Code/Arithmetic/double-float-defclass.lisp
+++ Code/Arithmetic/double-float-defclass.lisp
@@ -1,7 +1,14 @@
 (cl:in-package #:sicl-arithmetic)
 
+;;; During bootstrapping, we represent a double float as a standard
+;;; object with a slot containing an integer representing the bits of
+;;; the representation of the float.
+
 (defclass double-float (float standard-object)
   ())
+
+;; (defclass double-float (float standard-object)
+;;   ())
 
 (defun double-float-p (object)
   (typep object 'double-float))
