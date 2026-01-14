60c04ee4e679f3f2895e52c7c00a869dba13fe94
Robert Strandh <robert.strandh@gmail.com>
2026-01-14 06:48:11 +0100

    No longer prvide bootstrapping-specific definitions here.

--- Code/Arithmetic/single-float-defclass.lisp
+++ Code/Arithmetic/single-float-defclass.lisp
@@ -1,15 +1,8 @@
 (cl:in-package #:sicl-arithmetic)
 
-;;; During bootstrapping, we represent a single float as a standard
-;;; object with a slot containing an integer representing the bits of
-;;; the representation of the float.
-
-(defclass single-float (float standard-object)
-  ())
-
-;; (defclass single-float (float)
-;;   ()
-;;   (:metaclass built-in-class))
+(defclass single-float (float)
+  ()
+  (:metaclass built-in-class))
 
 (defun single-float-p (object)
   (typep object 'single-float))
