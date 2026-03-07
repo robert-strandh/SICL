341955e97cb8a632ed20b6f38eaf335b8c34f480
Robert Strandh <robert.strandh@gmail.com>
2026-03-07 13:21:34 +0100

    Replace calls to functions in package BUOY by primops.

--- Code/Arithmetic/floating-point.lisp
+++ Code/Arithmetic/floating-point.lisp
@@ -3,18 +3,18 @@
 (defgeneric decode-float (float))
 
 (defmethod decode-float ((float single-float))
-  (buoy:decode-single-float float))
+  (sicl-primop:primop :decode-single-float float))
 
 (defmethod decode-float ((float double-float))
-  (buoy:decode-double-float float))
+  (sicl-primop:primop :decode-double-float float))
 
 (defgeneric scale-float (float integer))
 
 (defmethod scale-float ((float single-float) (integer integer))
-  (buoy:scale-single-float float integer))
+  (sicl-primop:primop :scale-single-float float))
 
 (defmethod scale-float ((float double-float) (integer integer))
-  (buoy:scale-double-float float integer))
+  (sicl-primop:primop :scale-double-float float))
 
 (defun float-radix (float)
   (check-type float float)
@@ -23,16 +23,16 @@
 (defgeneric generic-float-sign (float-1 float-2))
 
 (defmethod generic-float-sign ((float-1 single-float) (float-2 single-float))
-  (buoy:single-copysign float-2 float-1))
+  (sicl-primop:primop :single-copysign float-2 float-1))
 
 (defmethod generic-float-sign ((float-1 double-float) (float-2 double-float))
-  (buoy:double-copysign float-2 float-1))
+  (sicl-primop:primop :double-copysign float-2 float-1))
 
 (defmethod generic-float-sign ((float-1 single-float) (float-2 double-float))
-  (buoy:double-copysign float-2 (float float-1 float-2)))
+  (sicl-primop:primop :double-copysign float-2 (float float-1 float-2)))
 
 (defmethod generic-float-sign ((float-1 double-float) (float-2 single-float))
-  (buoy:double-copysign (float float-2 float-1) float-1))
+  (sicl-primop:primop :double-copysign(float float-2 float-1) float-1))
 
 (defun float-sign (float-1 &optional (float-2 (float 1 float-1)))
   (generic-float-sign float-1 float-2))
@@ -46,26 +46,26 @@
 (defgeneric float-precision (float))
 
 (defmethod float-precision ((float single-float))
-  (buoy:single-float-precision float))
+  (sicl-primop:primop :single-float-precision float))
 
 (defmethod float-precision ((float double-float))
-  (buoy:double-float-precision float))
+  (sicl-primop:primop :double-float-precision float))
 
 (defgeneric integer-decode-float (float))
 
 (defmethod integer-decode-float ((float single-float))
-  (buoy:integer-decode-single-float float))
+  (sicl-primop:primop :integer-decode-single-float float))
 
 (defmethod integer-decode-float ((float double-float))
-  (buoy:integer-decode-double-float float))
+  (sicl-primop:primop :integer-decode-double-float float))
 
 (defmethod signum ((float single-float))
   (if (zerop float)
       float
-      (buoy:single-copysign 1s0 float)))
+      (sicl-primop:primop :single-copysign 1s0 float)))
 
 (defmethod signum ((float double-float))
   (if (zerop float)
       float
-      (buoy:double-copysign 1d0 float)))
+      (sicl-primop:primop :double-copysign 1d0 float)))
   
