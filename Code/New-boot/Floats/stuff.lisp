fb8d8d0573e9f0606468e049902b13d10d5d7960
Robert Strandh <robert.strandh@gmail.com>
2026-03-09 11:45:42 +0100

    Defiine BITS-TO-SINGLE-FLOAT and BITS-TO-DOUBLE-FLOAT.

--- Code/New-boot/Floats/stuff.lisp
+++ Code/New-boot/Floats/stuff.lisp
@@ -22,49 +22,6 @@
   (make-instance 'double-float
     :sign sign :floatr floatr))
 
-(defun bits-to-single-float (bits)
-  (let* ((sign-bit (ldb (byte 1 31) bits))
-         (exponent-bits (ldb (byte 8 23) bits))
-         (mantissa-bits (ldb (byte 23 0) bits))
-         (numerator
-           (cond ((zerop exponent-bits)
-                  ;; We have a subnormal float.
-                  mantissa-bits)
-                 ((>= exponent-bits (+ 127 23))
-                  (ash (+ mantissa-bits (ash 1 23))
-                       (- exponent-bits (+ 127 23))))
-                 (t
-                  (+ mantissa-bits (ash 1 23)))))
-         (denominator
-           (if (>= exponent-bits (+ 127 23))
-               1
-               (ash 1 (- (+ 127 23) exponent-bits)))))
-    (make-instance 'single-float
-      :sign (if (zerop sign-bit) 1 -1)
-      :floatr (make-ratio numerator denominator))))
-
-(defun bits-to-double-float (bits)
-  (let* ((sign-bit (ldb (byte 1 63) bits))
-         (exponent-bits (ldb (byte 11 52) bits))
-         (mantissa-bits (ldb (byte 52 0) bits))
-         (numerator
-           (cond ((zerop exponent-bits)
-                  ;; We have a subnormal float.
-                  mantissa-bits)
-                 ((>= exponent-bits (+ 1023 52))
-                  (ash (+ mantissa-bits (ash 1 52))
-                       (- exponent-bits (+ 1023 52))))
-                 (t
-                  (+ mantissa-bits (ash 1 52)))))
-         (denominator
-           (if (>= exponent-bits (+ 1023 52))
-               1
-               (ash 1 (- (+ 1023 52) exponent-bits)))))
-    (make-instance 'double-float
-      :sign (if (zerop sign-bit) 1 -1)
-      :floatr (make-ratio numerator denominator))))
-
-
 (defun coerce (value type)
   (assert (zerop value))
   (ecase type
