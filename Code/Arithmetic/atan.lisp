7a0172c7be70562be01ca252f204c3b989d68b02
Robert Strandh <robert.strandh@gmail.com>
2026-03-21 10:53:30 +0100

    New component containing definition of generic function ATAN.

---
+++ Code/Arithmetic/atan.lisp
@@ -0,0 +1,17 @@
+(cl:in-package #:sicl-arithmetic)
+
+(defgeneric atan (argument))
+
+(defmethod atan ((argument single-float))
+  (sicl-primop:primop :single-float-atan argument))
+
+(defmethod atan ((argument double-float))
+  (sicl-primop:primop :double-float-atan argument))
+
+(defmethod atan ((argument rational))
+  (atan (float argument)))
+
+(defmethod atan ((argument number))
+  (/ (log (/ (- (complex 0 1) argument)
+             (+ (complex 0 1) argument)))
+     (complex 0 2))
