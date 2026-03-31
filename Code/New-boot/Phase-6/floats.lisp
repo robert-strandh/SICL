9972f53a37dbc5b61eef1d82fe8b69d436d4cc73
Robert Strandh <robert.strandh@gmail.com>
2026-03-31 14:04:25 +0200

    Define method on PRIMOP for :SINGLE-FLOAT-DIVIDE.

--- Code/New-boot/Phase-6/floats.lisp
+++ Code/New-boot/Phase-6/floats.lisp
@@ -85,6 +85,16 @@
       (make-double-float sign floatr))))
 
 (defmethod sb:primop
+    ((operation (eql :single-float-divide)) &rest arguments)
+  (multiple-value-bind (sign1 floatr1)
+      (float-components (first arguments))
+    (multiple-value-bind (sign2 floatr2)
+        (float-components (second arguments))
+      (let* ((quotient (/ floatr1 floatr2))
+             (floatr (buoy-simulate:floatr32-from-rational quotient)))
+        (make-double-float (* sign1 sign2) floatr)))))
+
+(defmethod sb:primop
     ((operation (eql :double-float-divide)) &rest arguments)
   (multiple-value-bind (sign1 floatr1)
       (float-components (first arguments))
