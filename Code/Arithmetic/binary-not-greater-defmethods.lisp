5095bf53c46d22a29d0cb222d02446b6fd52e889
Robert Strandh <robert.strandh@gmail.com>
2026-04-04 13:44:36 +0200

    Add methods for comparing rationals and floats

--- Code/Arithmetic/binary-not-greater-defmethods.lisp
+++ Code/Arithmetic/binary-not-greater-defmethods.lisp
@@ -12,3 +12,15 @@
 (defmethod binary-not-greater ((x ratio) (y ratio))
   (binary-not-greater (* (numerator x) (denominator y))
                       (* (numerator y) (denominator x))))
+
+(defmethod binary-not-greater ((x rational) (y single-float))
+  (binary-not-greater (float x 1f0) y))
+
+(defmethod binary-not-greater ((x rational) (y double-float))
+  (binary-not-greater (float x 1d0) y))
+
+(defmethod binary-not-greater ((x single-float) (y rational))
+  (binary-not-greater x (float y 1f0)))
+
+(defmethod binary-not-greater ((x double-float) (y rational))
+  (binary-not-greater x (float y 1d0)))
