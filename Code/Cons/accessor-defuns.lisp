d7c8b394d3464cceae8ff75c0ba5dfbe9ab49e57
Robert Strandh <robert.strandh@gmail.com>
2026-06-02 17:52:17 +0200

    Replace CLEAVIR-PRIMOP:<mumble> by SICL-PRIMOP:PRIMOP :<mumble>.

--- Code/Cons/accessor-defuns.lisp
+++ Code/Cons/accessor-defuns.lisp
@@ -2,24 +2,24 @@
 
 (defun car (list)
   (if (consp list)
-      (cleavir-primop:car list)
+      (sicl-primop:primop :car list)
       (if (null list)
           list
           (error 'must-be-list :datum list))))
 
 (defun cdr (list)
   (if (consp list)
-      (cleavir-primop:cdr list)
+      (sicl-primop:primop :cdr list)
       (if (null list)
           list
           (error 'must-be-list :datum list))))
   
 (defun rplaca (cons object)
   (if (consp cons)
-      (progn (cleavir-primop:rplaca cons object) cons)
+      (progn (sicl-primop:primop :rplaca cons object) cons)
       (error 'must-be-cons cons)))
 
 (defun rplacd (cons object)
   (if (consp cons)
-      (progn (cleavir-primop:rplacd cons object) cons)
+      (progn (sicl-primop:primop :rplacd cons object) cons)
       (error 'must-be-cons cons)))
