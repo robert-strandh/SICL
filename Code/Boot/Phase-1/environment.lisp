21a6a2eec96f62aabea90f69faf0230f468c6c11
Robert Strandh <robert.strandh@gmail.com>
2026-08-13 15:08:41 +0200

    Remove obsolete directory.

--- Code/Boot/Phase-1/environment.lisp
+++
@@ -1,7 +0,0 @@
-(cl:in-package #:sicl-boot-phase-1)
-
-(defclass environment (sicl-boot:environment)
-  ())
-
-(defclass client (sicl-boot:client)
-  ())
--- Code/Boot/Phase-1/pre-fill-environment.lisp
+++
@@ -1,5 +0,0 @@
-(cl:in-package #:sicl-boot-phase-1)
-
-(defun pre-fill-environments (e0 e1)
-  (declare (ignore e0 e1))
-  nil)
