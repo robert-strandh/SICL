bedea8a434a3839e72eed6d28c492ef064abdf8a
Robert Strandh <robert.strandh@gmail.com>
2026-08-25 09:31:47 +0200

    Delete obsolete files.

--- Code/Boot/Phase-6/enable-defmethod.lisp
+++
@@ -1,6 +0,0 @@
-(cl:in-package #:sicl-boot-phase-6)
-
-(defun enable-defmethod (e5)
-  (load-source-file "CLOS/add-remove-direct-method.lisp" e5)
-  (load-source-file "CLOS/dependent-maintenance.lisp" e5)
-  (load-source-file "CLOS/add-remove-method.lisp" e5))
