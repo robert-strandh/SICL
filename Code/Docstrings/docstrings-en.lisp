49fbc0c9cb1a97daa0b29368802046ea20433474
Robert Strandh <robert.strandh@gmail.com>
2026-08-26 14:34:42 +0200

    Remove documentation strings now provided by Constrictor.

--- Code/Docstrings/docstrings-en.lisp
+++ Code/Docstrings/docstrings-en.lisp
@@ -18,62 +18,6 @@
         (fmt "Lambda list: (OBJECT)~@
               When OBJECT is a CONS cell, return the CDR of that cell.~@
               When OBJECT is NIL, return NIL."))
-
-(defmacro make-c*r-documentation (function-name)
-  (let* ((name (symbol-name function-name))
-         (letters (reverse (cdr (butlast (coerce name 'list))))))
-    (flet ((primitive (letter)
-             (if (eql letter #\A) 'car 'cdr)))
-      `(fundoc ',function-name
-                ,(fmt "Lambda list: (OBJECT)~@
-                       Equivalent to ~a"
-                      (loop with form = 'object
-                            for letter in letters
-                            do (setf form
-                                     (list (primitive letter) form))
-                            finally (return form)))))))
-             
-(make-c*r-documentation caar)
-(make-c*r-documentation cadr)
-(make-c*r-documentation cdar)
-(make-c*r-documentation cddr)
-(make-c*r-documentation caaar)
-(make-c*r-documentation caadr)
-(make-c*r-documentation cadar)
-(make-c*r-documentation caddr)
-(make-c*r-documentation cdaar)
-(make-c*r-documentation cdadr)
-(make-c*r-documentation cddar)
-(make-c*r-documentation cdddr)
-(make-c*r-documentation caaaar)
-(make-c*r-documentation caaadr)
-(make-c*r-documentation caadar)
-(make-c*r-documentation caaddr)
-(make-c*r-documentation cadaar)
-(make-c*r-documentation cadadr)
-(make-c*r-documentation caddar)
-(make-c*r-documentation cadddr)
-(make-c*r-documentation cdaaar)
-(make-c*r-documentation cdaadr)
-(make-c*r-documentation cdadar)
-(make-c*r-documentation cdaddr)
-(make-c*r-documentation cddaar)
-(make-c*r-documentation cddadr)
-(make-c*r-documentation cdddar)
-(make-c*r-documentation cddddr)
-
-(fundoc 'list
-        (fmt "Lambda list: (&rest OBJECTS)~@
-              Return a list containing the objects in OBJECTS.~@"))
-
-(fundoc 'list*
-        (fmt "Lambda list: (&rest OBJECTS)~@
-              At least one argument must be given.~@
-              Return a list containing the objects in OBJECTS,~@
-              except that the last object in OBJECTS becomes the~@
-              CDR of the last CONS cell created.~@
-              When given a single argument, return that argument."))
-
 (fundoc 'first
         (fmt "Lambda list: (LIST)~@
               Return the first element of the list LIST.~@
