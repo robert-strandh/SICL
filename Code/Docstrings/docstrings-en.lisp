aa673cf0fea088df09319d35f1529a4ba90e6aa9
Robert Strandh <robert.strandh@gmail.com>
2026-08-27 09:17:56 +0200

    Move documentatoin string to file with function definition.

--- Code/Docstrings/docstrings-en.lisp
+++ Code/Docstrings/docstrings-en.lisp
@@ -53,18 +53,6 @@
               Return a new CONS cell with OBJECT-1 in the~@
               CAR field and OBJECT-2 in the CDR field."))
 
-(fundoc '<=
-        (fmt "Lambda list: (&rest NUMBERS).~@
-              At least one argument is required.~@
-              Return true if the numbers in NUMBERS are in~@
-              monotonically non-decreasing order.~@
-              The consequences are undefined if some of the objects~@
-              in numbers are not real numbers, but if that is the case~@
-              and a condition is signaled, that condition is an error~@
-              of type TYPE-ERROR.~@
-              Might signal an error of type ARITHMETIC-ERROR if~@
-              unable to fulfill its contract."))
-
 (fundoc '=
         (fmt "Lambda list: (&rest NUMBERS).~@
               At least one argument is required.~@
